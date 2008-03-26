0056f508159537715697a00fd88d31945f11a069
depstein <depstein@kuali.org>
2008-03-26 15:21:24 -0400

    git-svn-id: https://svn.kuali.org/repos/student/poc/personidentity/personidentity-api/branches/personidentity-api-dev@25 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/poc/xsd/personidentity/person/dto/PersonCreateInfo.java
+++ src/main/java/org/kuali/student/poc/xsd/personidentity/person/dto/PersonCreateInfo.java
@@ -1,5 +1,209 @@
 package org.kuali.student.poc.xsd.personidentity.person.dto;
 
-public class PersonCreateInfo {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import org.kuali.student.poc.util.jaxb.JaxbAttributeMapListAdapter;
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PersonCreateInfo implements Serializable{
+
+	private static final long serialVersionUID = 2398792732220533518L;
+	
+	@XmlElement
+	@XmlElementWrapper(name="names")
+	private List<PersonName> name;
+	@XmlElement
+	private char gender;
+	@XmlElement
+	private Date birthDate;
+	@XmlElement
+	@XmlElementWrapper(name="referenceIds")
+	private List<PersonReferenceId> referenceId;
+	@XmlElement
+	private PersonCitizenship citizenship;
+	@XmlElement
+	@XmlJavaTypeAdapter(JaxbAttributeMapListAdapter.class)
+	private Map<String,String> attributes;
+	
+	@XmlElement
+	private Date createTime;
+	@XmlElement
+	private String createUserId;
+	@XmlElement
+	private String createUserComment;
+	@XmlElement
+	private Date updateTime;
+	@XmlElement
+	private String updateUserId;
+	@XmlElement
+	private String updateUserComment;
+
+	/**
+	 * @return the name
+	 */
+	public List<PersonName> getName() {
+		if(name == null){
+			name  = new ArrayList<PersonName>();
+		}
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(List<PersonName> name) {
+		this.name = name;
+	}
+	/**
+	 * @return the gender
+	 */
+	public char getGender() {
+		return gender;
+	}
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	/**
+	 * @return the birthDate
+	 */
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	/**
+	 * @param birthDate the birthDate to set
+	 */
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	/**
+	 * @return the referenceId
+	 */
+	public List<PersonReferenceId> getReferenceId() {
+		if(referenceId == null){
+			referenceId  = new ArrayList<PersonReferenceId>();
+		}
+		return referenceId;
+	}
+	/**
+	 * @param referenceId the referenceId to set
+	 */
+	public void setReferenceId(List<PersonReferenceId> referenceId) {
+		this.referenceId = referenceId;
+	}
+	/**
+	 * @return the citizenship
+	 */
+	public PersonCitizenship getCitizenship() {
+		return citizenship;
+	}
+	/**
+	 * @param citizenship the citizenship to set
+	 */
+	public void setCitizenship(PersonCitizenship citizenship) {
+		this.citizenship = citizenship;
+	}
+	/**
+	 * @return the attributes
+	 */
+	public Map<String, String> getAttributes() {
+		if(attributes == null){
+			attributes  = new HashMap<String, String> ();
+		}
+		return attributes;
+	}
+	/**
+	 * @param attributes the attributes to set
+	 */
+	public void setAttributes(Map<String, String> attributes) {
+		this.attributes = attributes;
+	}
+	/**
+	 * @return the createTime
+	 */
+	public Date getCreateTime() {
+		return createTime;
+	}
+	/**
+	 * @param createTime the createTime to set
+	 */
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	/**
+	 * @return the createUserId
+	 */
+	public String getCreateUserId() {
+		return createUserId;
+	}
+	/**
+	 * @param createUserId the createUserId to set
+	 */
+	public void setCreateUserId(String createUserId) {
+		this.createUserId = createUserId;
+	}
+	/**
+	 * @return the createUserComment
+	 */
+	public String getCreateUserComment() {
+		return createUserComment;
+	}
+	/**
+	 * @param createUserComment the createUserComment to set
+	 */
+	public void setCreateUserComment(String createUserComment) {
+		this.createUserComment = createUserComment;
+	}
+	/**
+	 * @return the updateTime
+	 */
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+	/**
+	 * @param updateTime the updateTime to set
+	 */
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+	/**
+	 * @return the updateUserId
+	 */
+	public String getUpdateUserId() {
+		return updateUserId;
+	}
+	/**
+	 * @param updateUserId the updateUserId to set
+	 */
+	public void setUpdateUserId(String updateUserId) {
+		this.updateUserId = updateUserId;
+	}
+	/**
+	 * @return the updateUserComment
+	 */
+	public String getUpdateUserComment() {
+		return updateUserComment;
+	}
+	/**
+	 * @param updateUserComment the updateUserComment to set
+	 */
+	public void setUpdateUserComment(String updateUserComment) {
+		this.updateUserComment = updateUserComment;
+	}
+	
+	
+	
 }
