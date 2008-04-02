a94a7af3f02b0eb2b3437df375339e978d7b3e56
rdiaz <rdiaz@kuali.org>
2008-04-01 20:05:56 -0400

    Parent POM is working and ready. Added inspect-rule-failure module.
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@89 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ inspect-rule-failure/src/main/java/org/kuali/student/rules/ast/GenerateRuleFailureMessage.java
@@ -0,0 +1,101 @@
+package org.kuali.student.rules.ast;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.*;
+
+import org.drools.RuleBase;
+import org.drools.RuleBaseFactory;
+import org.drools.WorkingMemory;
+import org.drools.compiler.PackageBuilder;
+import org.drools.rule.Package;
+
+/**
+ * This is a sample file to launch a rule package from a rule source file.
+ */
+public class GenerateRuleFailureMessage {
+	
+	static HashMap<String, Boolean> nodeValueMap;
+	static HashMap<String, String> nodeFailureMessageMap;
+	static HashMap<String, String> nodeFailureMessage4ComplexProposition;
+	static String functionString;
+	
+    public static final void main(String[] args) {
+    	setRuleParams();
+    	// if there are Sets, pre-process rule messages
+    	//preProcessRuleFailureMessages();
+    	executeRule(functionString, nodeValueMap, nodeFailureMessageMap);
+    }
+    public static String executeRule(String functionString, HashMap<String, Boolean> nodeValueMap, HashMap<String, String> nodeFailureMessageMap) {
+    	BinaryTree ASTtree = null;
+    	try {
+        	//load up the rulebase
+            RuleBase ruleBase = readRule();
+            WorkingMemory workingMemory = ruleBase.newStatefulSession();
+            
+            // go parse function in buildTree
+            ASTtree = new BinaryTree(nodeValueMap, nodeFailureMessageMap);
+            BooleanNode root = ASTtree.buildTree( functionString );
+            ASTtree.traverseTreePostOrder(root, null);
+            
+            List<BooleanNode> treeNodes = ASTtree.getAllNodes();
+            // Iterate over List<Node> and insert in memory
+            for (BooleanNode bnode : treeNodes) {
+            	//System.out.println(bnode.getLabel() + "   " + bnode.getValue() + "   " + bnode.getRuleFailureMessage());
+            	workingMemory.insert( bnode );
+            }
+            System.out.println("ABOUT TO FIRE ALL RULES");
+            workingMemory.fireAllRules();
+            System.out.println( ASTtree.getRoot().getRuleFailureMessage() + "   IM ROOT");
+            
+            
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        return ASTtree.getRoot().getRuleFailureMessage();
+    }
+    
+    private static void setRuleParams (){
+		
+    	functionString = "A*B*C*D";
+    	
+    	nodeValueMap = new HashMap<String, Boolean>();
+		nodeValueMap.put("A", false);
+		nodeValueMap.put("B", true);
+		nodeValueMap.put("C", true);
+		nodeValueMap.put("D", true);
+		
+		nodeFailureMessageMap = new HashMap<String, String>();
+		nodeFailureMessageMap.put("A", "Need MATH 200");
+		nodeFailureMessageMap.put("B", "Need MATH 110");
+		nodeFailureMessageMap.put("C", "Need 15 credits or more of 1st year science");
+		nodeFailureMessageMap.put("D", "Need English 6000");
+		
+		nodeFailureMessage4ComplexProposition = new HashMap<String, String>();
+		nodeFailureMessage4ComplexProposition.put("A", "Set");
+		nodeFailureMessage4ComplexProposition.put("B", "null");
+		nodeFailureMessage4ComplexProposition.put("C", "null");
+		nodeFailureMessage4ComplexProposition.put("D", "null");
+	}
+    
+
+    /**
+     * Please note that this is the "low level" rule assembly API.
+     */
+	private static RuleBase readRule() throws Exception {
+		//read in the source
+		Reader source = new InputStreamReader( GenerateRuleFailureMessage.class.getResourceAsStream( "/BoolBinaryTreeLogger.drl" ) );
+		
+		PackageBuilder builder = new PackageBuilder();
+
+		builder.addPackageFromDrl( source );
+
+		Package pkg = builder.getPackage();
+		
+		//add the package to a rulebase (deploy the rule package).
+		RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+		ruleBase.addPackage( pkg );
+		return ruleBase;
+	}
+    
+}
---
+++ inspect-rule-failure/src/test/java/org/kuali/student/rules/ast/GenerateRuleFailureMessageTest.java
@@ -0,0 +1,70 @@
+package org.kuali.student.rules.ast;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kuali.student.rules.ast.GenerateRuleFailureMessage;
+
+import static org.junit.Assert.assertEquals;
+
+public class GenerateRuleFailureMessageTest {
+	
+	private HashMap<String, Boolean> nodeValueMap;
+	private HashMap<String, String> nodeFailureMessageMap;
+	private String functionString;
+	
+	@Before
+	public void setUp() throws Exception {
+		nodeValueMap = new HashMap<String, Boolean>();
+		nodeFailureMessageMap = new HashMap<String, String>();
+	}
+	
+	@Test
+	public void testExecuteRuleFailureMessage1()
+	{
+		functionString = "A*B*C*D";
+    	
+		nodeValueMap.put("A", false);
+		nodeValueMap.put("B", true);
+		nodeValueMap.put("C", true);
+		nodeValueMap.put("D", true);
+		
+		nodeFailureMessageMap.put("A", "Need MATH 200");
+		nodeFailureMessageMap.put("B", "Need MATH 110");
+		nodeFailureMessageMap.put("C", "Need 15 credits or more of 1st year science");
+		nodeFailureMessageMap.put("D", "Need English 6000");
+		
+		String expected = "Need MATH 200";
+		String actual = GenerateRuleFailureMessage.executeRule(functionString, nodeValueMap, nodeFailureMessageMap);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testExecuteRuleFailureMessage2()
+	{
+		functionString = "A+(B*C)+D";
+    	
+		nodeValueMap.put("A", false);
+		nodeValueMap.put("B", true);
+		nodeValueMap.put("C", false);
+		nodeValueMap.put("D", false);
+		
+		nodeFailureMessageMap.put("A", "Need MATH 200");
+		nodeFailureMessageMap.put("B", "Need MATH 110");
+		nodeFailureMessageMap.put("C", "Need 15 credits or more of 1st year science");
+		nodeFailureMessageMap.put("D", "Need English 6000");
+		
+		String expected = "Need MATH 200 OR Need 15 credits or more of 1st year science OR Need English 6000";
+		String actual = GenerateRuleFailureMessage.executeRule(functionString, nodeValueMap, nodeFailureMessageMap);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
