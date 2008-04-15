738229883adf701a44e9d6e89891e095acbb8eb6
wgomes <wgomes@kuali.org>
2008-04-15 15:26:07 -0400

    Implement using JaxWsClientFactory interface
    git-svn-id: https://svn.kuali.org/repos/student/poc/common/branches/common-dev@216 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ws/src/main/java/org/kuali/student/poc/common/ws/beans/JaxWsClientFactoryBean.java
+++ common-ws/src/main/java/org/kuali/student/poc/common/ws/beans/JaxWsClientFactoryBean.java
@@ -6,120 +6,102 @@ import javax.xml.namespace.QName;
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.Service;
 
-import org.springframework.beans.factory.FactoryBean;
 import org.springframework.core.io.ClassPathResource;
 
-public class JaxWsClientFactoryBean implements FactoryBean {
-	private Class<?> serviceEndpointInterface = null;
-	private String wsdlDocumentLocation="";
-	private String serviceNamespaceURI="";
-	private String serviceName="";
-	private String serviceUrl="";
-	private static final String CLASSPATH_PREFIX = "classpath:";
-	@Override
-	public Object getObject() throws Exception {
-		URL url;
-		if(wsdlDocumentLocation.startsWith(CLASSPATH_PREFIX)){
-			ClassPathResource cpr = new ClassPathResource(wsdlDocumentLocation.substring(CLASSPATH_PREFIX.length()));
-			url = cpr.getURL();
-		}else{
-			url = new URL(wsdlDocumentLocation);
-		}
-		QName serviceQName = new QName(this.serviceNamespaceURI,
-				this.serviceName);
-		Service service = Service.create(url, serviceQName);
-		Object client = service.getPort(serviceEndpointInterface);
-		//Override the service URL
-		if(this.serviceUrl!=null&&!"".equals(this.serviceUrl)){
-			((BindingProvider)client).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, this.serviceUrl);
-		}
-		
-		return client;
-	}
+public class JaxWsClientFactoryBean implements JaxWsClientFactory {
+    private Class<?> serviceEndpointInterface = null;
+    private String wsdlDocumentLocation = "";
+    private QName serviceQName = null;
+    private String serviceUrl = "";
+    private static final String CLASSPATH_PREFIX = "classpath:";
 
-	@Override
-	public Class<?> getObjectType() {
-		return serviceEndpointInterface;
-	}
+    @Override
+    public Object getObject() throws Exception {
+        URL url;
+        if (wsdlDocumentLocation.startsWith(CLASSPATH_PREFIX)) {
+            ClassPathResource cpr = new ClassPathResource(wsdlDocumentLocation.substring(CLASSPATH_PREFIX.length()));
+            url = cpr.getURL();
+        } else {
+            url = new URL(wsdlDocumentLocation);
+        }
+        Service service = Service.create(url, serviceQName);
+        Object client = service.getPort(serviceEndpointInterface);
+        // Override the service URL
+        if (this.serviceUrl != null && !"".equals(this.serviceUrl)) {
+            ((BindingProvider) client).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, this.serviceUrl);
+        }
 
-	/**
-	 * @return the serviceEndpointInterface
-	 */
-	public Class<?> getServiceEndpointInterface() {
-		return serviceEndpointInterface;
-	}
+        return client;
+    }
 
-	/**
-	 * @param serviceEndpointInterface
-	 *            the serviceEndpointInterface to set
-	 */
-	public void setServiceEndpointInterface(
-			Class<?> serviceEndpointInterface) {
-		this.serviceEndpointInterface = serviceEndpointInterface;
-	}
+    @Override
+    public Class<?> getObjectType() {
+        return serviceEndpointInterface;
+    }
 
+    /**
+     * @return the serviceEndpointInterface
+     */
+    public Class<?> getServiceEndpointInterface() {
+        return serviceEndpointInterface;
+    }
 
-	/**
-	 * @return the serviceNamespaceURI
-	 */
-	public String getServiceNamespaceURI() {
-		return serviceNamespaceURI;
-	}
+    /**
+     * @param serviceEndpointInterface
+     *            the serviceEndpointInterface to set
+     */
+    public void setServiceEndpointInterface(Class<?> serviceEndpointInterface) {
+        this.serviceEndpointInterface = serviceEndpointInterface;
+    }
 
-	/**
-	 * @param serviceNamespaceURI
-	 *            the serviceNamespaceURI to set
-	 */
-	public void setServiceNamespaceURI(String serviceNamespaceURI) {
-		this.serviceNamespaceURI = serviceNamespaceURI;
-	}
+    /**
+     * @return the serviceName
+     */
+    public QName getServiceName() {
+        return serviceQName;
+    }
 
-	/**
-	 * @return the serviceName
-	 */
-	public String getServiceName() {
-		return serviceName;
-	}
+    /**
+     * @param serviceName
+     *            the serviceName to set
+     */
+    public void setServiceName(QName serviceName) {
+        this.serviceQName = serviceName;
+    }
 
-	/**
-	 * @param serviceName
-	 *            the serviceName to set
-	 */
-	public void setServiceName(String serviceName) {
-		this.serviceName = serviceName;
-	}
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
 
-	@Override
-	public boolean isSingleton() {
-		return false;
-	}
+    /**
+     * @return the wsdlDocumentLocation
+     */
+    public String getWsdlLocation() {
+        return wsdlDocumentLocation;
+    }
 
-	/**
-	 * @return the wsdlDocumentLocation
-	 */
-	public String getWsdlDocumentLocation() {
-		return wsdlDocumentLocation;
-	}
+    /**
+     * @param wsdlDocumentLocation
+     *            the wsdlDocumentLocation to set
+     */
+    public void setWsdlLocation(String wsdlDocumentLocation) {
+        this.wsdlDocumentLocation = wsdlDocumentLocation;
+    }
 
-	/**
-	 * @param wsdlDocumentLocation the wsdlDocumentLocation to set
-	 */
-	public void setWsdlDocumentLocation(String wsdlDocumentLocation) {
-		this.wsdlDocumentLocation = wsdlDocumentLocation;
-	}
+    /**
+     * @return the serviceUrl
+     */
+    public String getAddress() {
+        return serviceUrl;
+    }
 
-	/**
-	 * @return the serviceUrl
-	 */
-	public String getServiceUrl() {
-		return serviceUrl;
-	}
-
-	/**
-	 * @param serviceUrl the serviceUrl to set
-	 */
-	public void setServiceUrl(String serviceUrl) {
-		this.serviceUrl = serviceUrl;
-	}
+    /**
+     * @param serviceUrl
+     *            the serviceUrl to set
+     */
+    public void setAddress(String add) {
+        this.serviceUrl = add;
+    }
 
 }
