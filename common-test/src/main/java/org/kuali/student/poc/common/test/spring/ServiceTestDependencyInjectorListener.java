8cb71870f2af94a315f788a8dd5a1c94ca15575a
wgomes <wgomes@kuali.org>
2008-04-15 15:26:55 -0400

    Use new JaxWsClientFactory interface
    git-svn-id: https://svn.kuali.org/repos/student/poc/common/branches/common-dev@217 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-test/src/main/java/org/kuali/student/poc/common/test/spring/ServiceTestDependencyInjectorListener.java
+++ common-test/src/main/java/org/kuali/student/poc/common/test/spring/ServiceTestDependencyInjectorListener.java
@@ -3,9 +3,9 @@ package org.kuali.student.poc.common.test.spring;
 import java.lang.reflect.Field;
 
 import javax.jws.WebService;
+import javax.xml.namespace.QName;
 
-import org.kuali.student.poc.common.cxf.security.SecureJaxWsProxyFactoryBean;
-import org.kuali.student.poc.common.ws.beans.JaxWsClientFactoryBean;
+import org.kuali.student.poc.common.ws.beans.JaxWsClientFactory;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
@@ -20,31 +20,33 @@ public class ServiceTestDependencyInjectorListener extends DependencyInjectionTe
                 Client a = f.getAnnotation(Client.class);
                 Class<?> serviceImplClass = Class.forName(a.value());
                 String serviceName = "";
-                String serviceWsdlDocumentLocation = "http://localhost:" + a.port() + "/Service/Service?wsdl";
+                String serviceWsdlLocation = "http://localhost:" + a.port() + "/Service/Service?wsdl";
                 String serviceNamespaceURI = "";
+                String serviceAddress = "http://localhost:" + a.port() + "/Service/Service";
+
                 if (serviceImplClass.isAnnotationPresent(WebService.class)) {
                     WebService wsAnnotation = serviceImplClass.getAnnotation(WebService.class);
                     serviceName = wsAnnotation.serviceName();
                     serviceNamespaceURI = wsAnnotation.targetNamespace();
                 }
 
-                if (!a.secure()) {
-                    JaxWsClientFactoryBean clientFactory = new JaxWsClientFactoryBean();
-                    clientFactory.setServiceEndpointInterface(f.getType());
-                    clientFactory.setServiceName(serviceName);
-                    clientFactory.setServiceNamespaceURI(serviceNamespaceURI);
-                    clientFactory.setWsdlDocumentLocation(serviceWsdlDocumentLocation);
-                    f.set(testContext.getTestInstance(), clientFactory.getObject());
-                } else if (a.secure()) {
-                    //TODO: Create secure client based on jaxws-rt or cxf
-                    
-                    SecureJaxWsProxyFactoryBean proxyFactory = new SecureJaxWsProxyFactoryBean();
+                Class<?> clientImplClass = Class.forName("org.kuali.student.poc.common.ws.beans.JaxWsClientFactoryBean");
 
-                    proxyFactory.setServiceClass(serviceImplClass.getInterfaces()[0]);
-                    proxyFactory.setAddress("http://localhost.umd.edu:" + a.port() + "/Service/ServiceSecure");
-
-                    f.set(testContext.getTestInstance(), proxyFactory.create());
+                if (a.secure()) {
+                    try {
+                        clientImplClass = Class.forName("org.kuali.student.poc.common.cxf.security.SecureJaxWsProxyFactoryBean");
+                        serviceAddress += "Secure";
+                    } catch (ClassNotFoundException cnfe) {}
                 }
+
+                JaxWsClientFactory clientFactory = (JaxWsClientFactory) clientImplClass.newInstance();
+
+                clientFactory.setServiceEndpointInterface(f.getType());
+                clientFactory.setServiceName(new QName(serviceNamespaceURI, serviceName));
+                clientFactory.setWsdlLocation(serviceWsdlLocation);
+                clientFactory.setAddress(serviceAddress);
+
+                f.set(testContext.getTestInstance(), clientFactory.getObject());
             }
         }
 
