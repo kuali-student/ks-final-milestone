eeb2ea68dd9437bbbf83ba7ea38426fbdd29c10c
lcarlsen <lcarlsen@kuali.org>
2008-04-18 01:23:49 -0400

    Initial checkin of slow test cases
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@269 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ brms-repository/src/test/java/org/kuali/student/brms/repository/drools/DroolsJackrabbitRepositoryStartupShutdownTest.java
@@ -0,0 +1,84 @@
+package org.kuali.student.brms.repository.drools;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.net.URL;
+
+import javax.jcr.Credentials;
+import javax.jcr.SimpleCredentials;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DroolsJackrabbitRepositoryStartupShutdownTest {
+    @BeforeClass
+    public static void setUpOnce() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownOnce() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    private static Credentials getCredentials() {
+        String id = "superuser";
+        char[] password = "superuser".toCharArray();
+        return new SimpleCredentials(id, password);
+    }
+    
+    private void assertLogin( DroolsJackrabbitRepository repo ) {
+        try {
+            repo.login( getCredentials() );
+            assertTrue( true );
+        } catch( Exception e ) {
+            fail( "Unable to login to repository: " + e.getMessage() );
+        }
+    }
+    
+    @Test
+    public void testStartupRepositoryWithUrlConfiguration() throws Exception {
+        // Get repository.xml from /repository
+        URL url = DroolsJackrabbitRepositoryStartupShutdownTest.class.getResource("/repository");
+        DroolsJackrabbitRepository repo = new DroolsJackrabbitRepository( url );
+        repo.clearAll();
+        repo.startupRepository();
+        assertLogin( repo );
+        assertNotNull( repo.getRepository().listStates() );
+        repo.shutdownRepository();
+    }    
+
+    @Test
+    public void testStartupDefaultRepository() throws Exception {
+        DroolsJackrabbitRepository repo = new DroolsJackrabbitRepository();
+        repo.clearAll();
+        repo.startupRepository();
+        assertLogin( repo );
+        assertNotNull( repo.getRepository().listStates() );
+        repo.shutdownRepository();
+    }    
+    
+    @Test
+    public void testShutdownRepository() throws Exception {
+        DroolsJackrabbitRepository repo = new DroolsJackrabbitRepository();
+        repo.clearAll();
+        repo.startupRepository();
+        repo.login( getCredentials() );
+        assertLogin( repo );
+        repo.shutdownRepository();
+        assertFalse( repo.getRepository().getSession().isLive() );
+    }
+
+}
