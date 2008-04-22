462f9b91e51862e933f4d7feb852fa975a612385
lcarlsen <lcarlsen@kuali.org>
2008-04-22 13:41:22 -0400

    Code cleanup
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@334 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- brms-repository/src/test/java/org/kuali/student/brms/repository/drools/DroolsJackrabbitRepositoryTest.java
+++ brms-repository/src/test/java/org/kuali/student/brms/repository/drools/DroolsJackrabbitRepositoryTest.java
@@ -4,10 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
-
-import java.net.URL;
 
 import javax.jcr.Credentials;
 import javax.jcr.SimpleCredentials;
