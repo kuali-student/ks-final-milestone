95e27f5454500dd1737949e5918f0bcba2762e53
lcarlsen <lcarlsen@kuali.org>
2008-04-23 14:48:43 -0400

    Renamed from BuilderResult
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@357 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ brms-repository/src/main/java/org/kuali/student/brms/repository/rule/CompilerResult.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.student.brms.repository.rule;
+
+public class CompilerResult implements java.io.Serializable {
+    public String assetUuid;
+    public String assetName;
+    public String assetFormat;
+    public String assetMessage;
+
+    public CompilerResult(String uuid, String assetName, String assetFormat, String message) {
+        this.assetUuid = uuid;
+        this.assetName = assetName;
+        this.assetFormat = assetFormat;
+        this.assetMessage = message;
+    }
+
+    public String getUuid() {
+        return this.assetUuid;
+    }
+
+    public String getAssetName() {
+        return this.assetName;
+    }
+
+    public String getAssetFormat() {
+        return this.assetFormat;
+    }
+
+    public String getMessage() {
+        return this.assetMessage;
+    }
+
+    public String toString() {
+        return "\nAsset: " + this.assetName + "\nFormat: " + this.assetFormat + "\nUUID: " + this.assetUuid + "\nMessage: " + this.assetMessage;
+    }
+}
