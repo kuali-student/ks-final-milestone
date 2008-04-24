2309c736d3350e1e4d12a0eb2fc5d20ccb0dae08
depstein <depstein@kuali.org>
2008-04-24 15:54:48 -0400

    added new method to get a uuid from a string and create a new one if the string is not a valid uuid
    git-svn-id: https://svn.kuali.org/repos/student/poc/common/branches/common-dev@398 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-util/src/main/java/org/kuali/student/poc/common/util/UUIDHelper.java
+++ common-util/src/main/java/org/kuali/student/poc/common/util/UUIDHelper.java
@@ -4,4 +4,15 @@ public class UUIDHelper {
 	public static String genStringUUID() {
 		return java.util.UUID.randomUUID().toString();
 	}
+
+	public static String genStringUUID(String originalUUID) {
+		if (originalUUID != null) {
+			try {
+				return java.util.UUID.fromString(originalUUID).toString();
+			} catch (IllegalArgumentException e) {
+
+			}
+		}
+		return genStringUUID();
+	}
 }
