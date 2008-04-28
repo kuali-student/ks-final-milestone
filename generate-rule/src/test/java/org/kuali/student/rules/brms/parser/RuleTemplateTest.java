5b44884e17d6846b92fbc7ec5fba7ca0a9848d67
rdiaz <rdiaz@kuali.org>
2008-04-28 12:15:18 -0400

    update exception
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@451 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- generate-rule/src/test/java/org/kuali/student/rules/brms/parser/RuleTemplateTest.java
+++ generate-rule/src/test/java/org/kuali/student/rules/brms/parser/RuleTemplateTest.java
@@ -9,61 +9,51 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class RuleTemplateTest {
-	RuleTemplate rt;
-	
-	@Before
-	public void setUp() throws Exception {
-		rt = new RuleTemplate();
-		
-		rt.setRuleName("The Rule Name");
-	    
-		ArrayList<String> ruleAttributes = new ArrayList<String>();
-		ruleAttributes.add("#no-loop true" );
-		ruleAttributes.add("#salience 1" );
-		rt.setRuleAttributes(ruleAttributes);
-		
+    RuleTemplate rt;
+    
+    @Before
+    public void setUp() throws Exception {
+        rt = new RuleTemplate();
+        
+        rt.setRuleName("The Rule Name");
+        
+        ArrayList<String> ruleAttributes = new ArrayList<String>();
+        ruleAttributes.add("#no-loop true" );
+        ruleAttributes.add("#salience 1" );
+        rt.setRuleAttributes(ruleAttributes);
+        
         ArrayList<String> lhs = new ArrayList<String>();
-        lhs.add("n : BooleanNode()" );
+        lhs.add("constraint : Constraint(constraintID == \"A constraintID from DB\")" );
+        lhs.add("Boolean(booleanValue == Boolean.FALSE) from constraint.apply()");
         rt.setLHS(lhs);
         
         ArrayList<String> rhs = new ArrayList<String>();
-        rhs.add("String logMessage = n.getLeftNode().getRuleFailureMessage() + \" OR \" + n.getRightNode().getRuleFailureMessage();"  );
-        rhs.add("n.setRuleFailureMessage(logMessage);");
-        rhs.add("System.out.println( n.getRuleFailureMessage() + \"   rule OR\");"  );
-        rhs.add("BooleanNode parent = n.getParent();"  );
-        rhs.add("if (parent != null){"  );
-        rhs.add("    update(parent);"   );
-        rhs.add("}"  );
+        rhs.add("Propositions.setProposition(\"A\", true);");
         
         rt.setRHS(rhs);
-	}
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public void testProcess() {
-		String expected = "\r\nrule \"The Rule Name\"\r\n" +
-							"\t#no-loop true\r\n" +
-							"\t#salience 1\r\n"  +
-							"when\r\n" +
-							"\tn : BooleanNode()\r\n" +
-							"then\r\n" +
-							"\tString logMessage = n.getLeftNode().getRuleFailureMessage() + \" OR \" + n.getRightNode().getRuleFailureMessage();\r\n" +
-							"\tn.setRuleFailureMessage(logMessage);\r\n" +
-							"\tSystem.out.println( n.getRuleFailureMessage() + \"   rule OR\");\r\n" +
-							"\tBooleanNode parent = n.getParent();\r\n" +
-							"\tif (parent != null){\r\n" +
-							"\t    update(parent);\r\n" +
-							"\t}\r\n" +
-							"end";
-		
-		System.out.println("Char count expected " + expected.length());
-		String actual = rt.process("RuleTemplate.vm");
-		
-		System.out.println("\nChar count actual " + actual.length());
-		assertEquals(expected , actual);
-	}
+    @Test
+    public void testProcess() throws Exception {
+        String expected = "\r\nrule \"The Rule Name\"\r\n" +
+                            "\t#no-loop true\r\n" +
+                            "\t#salience 1\r\n"  +
+                            "when\r\n" +
+                            "\tconstraint : Constraint(constraintID == \"A constraintID from DB\")\r\n" +
+                            "\tBoolean(booleanValue == Boolean.FALSE) from constraint.apply()\r\n" +
+                            "then\r\n" +
+                            "\tPropositions.setProposition(\"A\", true);\r\n" +
+                            "end";
+        
+        System.out.println("Char count expected " + expected.length());
+        String actual = rt.process("RuleTemplate.vm");
+        
+        System.out.println("\nChar count actual " + actual.length());
+        assertEquals(expected , actual);
+    }
 
 }
