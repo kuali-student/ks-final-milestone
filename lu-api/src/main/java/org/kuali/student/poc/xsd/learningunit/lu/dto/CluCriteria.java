1b96e69a61b7f7bdb85749dfdf2ddf83af161e10
depstein <depstein@kuali.org>
2008-04-28 15:44:16 -0400

    git-svn-id: https://svn.kuali.org/repos/student/poc/learningunitmanagement/lum-api/branches/lum-api-dev@459 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lu-api/src/main/java/org/kuali/student/poc/xsd/learningunit/lu/dto/CluCriteria.java
+++ lu-api/src/main/java/org/kuali/student/poc/xsd/learningunit/lu/dto/CluCriteria.java
@@ -1,31 +1,53 @@
 package org.kuali.student.poc.xsd.learningunit.lu.dto;
 
 import java.io.Serializable;
+import java.util.Map;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import org.kuali.student.poc.common.ws.binding.JaxbAttributeMapListAdapter;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class CluCriteria implements Serializable {
 
 	private static final long serialVersionUID = -2109861706103417878L;
 	@XmlElement
-	private String query;
+	private String luTypeKey;
+	@XmlElement
+	@XmlJavaTypeAdapter(JaxbAttributeMapListAdapter.class)
+	private Map<String, String> searchKeyValue;
 
 	/**
-	 * @return the query
+	 * @return the luTypeKey
 	 */
-	public String getQuery() {
-		return query;
+	public String getLuTypeKey() {
+		return luTypeKey;
 	}
 
 	/**
-	 * @param query
-	 *            the query to set
+	 * @param luTypeKey
+	 *            the luTypeKey to set
 	 */
-	public void setQuery(String query) {
-		this.query = query;
+	public void setLuTypeKey(String luTypeKey) {
+		this.luTypeKey = luTypeKey;
+	}
+
+	/**
+	 * @return the searchKeyValue
+	 */
+	public Map<String, String> getSearchKeyValue() {
+		return searchKeyValue;
+	}
+
+	/**
+	 * @param searchKeyValue
+	 *            the searchKeyValue to set
+	 */
+	public void setSearchKeyValue(Map<String, String> searchKeyValue) {
+		this.searchKeyValue = searchKeyValue;
 	}
 
 	/*
@@ -37,7 +59,10 @@ public class CluCriteria implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((query == null) ? 0 : query.hashCode());
+		result = prime * result
+				+ ((luTypeKey == null) ? 0 : luTypeKey.hashCode());
+		result = prime * result
+				+ ((searchKeyValue == null) ? 0 : searchKeyValue.hashCode());
 		return result;
 	}
 
@@ -55,12 +80,16 @@ public class CluCriteria implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		final CluCriteria other = (CluCriteria) obj;
-		if (query == null) {
-			if (other.query != null)
+		if (luTypeKey == null) {
+			if (other.luTypeKey != null)
 				return false;
-		} else if (!query.equals(other.query))
+		} else if (!luTypeKey.equals(other.luTypeKey))
+			return false;
+		if (searchKeyValue == null) {
+			if (other.searchKeyValue != null)
+				return false;
+		} else if (!searchKeyValue.equals(other.searchKeyValue))
 			return false;
 		return true;
 	}
-
 }
