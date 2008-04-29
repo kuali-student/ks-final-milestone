317b105b011b36b3c737f751f7d8c0788b7fc282
ddean <ddean@kuali.org>
2008-04-29 10:51:36 -0400

    adding search
    git-svn-id: https://svn.kuali.org/repos/student/poc/learningunitmanagement/lum-ri/branches/lum-ri-dev@497 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- luipersonrelation-ri/src/main/java/org/kuali/student/poc/learningunit/luipersonrelation/dao/LuiPersonRelationDAO.java
+++ luipersonrelation-ri/src/main/java/org/kuali/student/poc/learningunit/luipersonrelation/dao/LuiPersonRelationDAO.java
@@ -1,5 +1,6 @@
 package org.kuali.student.poc.learningunit.luipersonrelation.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.kuali.student.poc.learningunit.luipersonrelation.entity.LuiPersonRelation;
@@ -19,4 +20,8 @@ public interface LuiPersonRelationDAO {
     public List<LuiPersonRelation> findLuiPersonRelations(String personId, String luiId, String luiRelationType, String relationState);
 
     public LuiPersonRelation updateLuiPersonRelation(LuiPersonRelation luiPersonRelation);
+
+    public List<String> searchForLuiPersonRelationIds(String luiId, String personId, String luiPersonRelationType, String relationState, Date effectiveStartDate, Date effectiveEndDate);
+
+    public List<LuiPersonRelation> searchForLuiPersonRelations(String luiId, String personId, String luiPersonRelationType, String relationState, Date effectiveStartDate, Date effectiveEndDate);
 }
--- luipersonrelation-ri/src/test/java/org/kuali/student/poc/learningunit/luipersonrelation/TestLuiPersonRelationDAOImpl.java
+++ luipersonrelation-ri/src/test/java/org/kuali/student/poc/learningunit/luipersonrelation/TestLuiPersonRelationDAOImpl.java
@@ -3,7 +3,9 @@ package org.kuali.student.poc.learningunit.luipersonrelation;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Date;
+import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.kuali.student.poc.common.test.spring.AbstractTransactionalDaoTest;
 import org.kuali.student.poc.common.test.spring.Dao;
@@ -62,5 +64,31 @@ public class TestLuiPersonRelationDAOImpl extends AbstractTransactionalDaoTest {
         
         assertEquals(lpr2.getRelationState(),lpr.getRelationState());
         
+    }
+    
+    @Test
+    public void testSearchLuiPersonRelation() {
+        LuiPersonRelation lpr = new LuiPersonRelation();
+        Date toSearch = new Date();
+        
+        lpr.setEffectiveEndDate(toSearch);
+        lpr.setEffectiveStartDate(toSearch);
+        lpr.setLuiId("whoever1");
+        lpr.setPersonId("whoever2");
+        lpr.setLuiPersonRelationType("whoever3");
+        lpr.setRelationState("whoever4");
+        
+        dao.createLuiPersonRelation(lpr);
+        
+        List<String> searchIds = dao.searchForLuiPersonRelationIds(null, null, null, null, toSearch, null);
+        assertEquals(1, searchIds.size());
+        List<LuiPersonRelation> searchLuis = dao.searchForLuiPersonRelations(null, null, null, null, null, toSearch);
+        assertEquals(1, searchLuis.size());
+        assertEquals(searchIds.get(0), searchLuis.get(0).getId());
+        
+        searchIds = dao.searchForLuiPersonRelationIds("whoever1", null, "whoever3", null, toSearch, null);
+        assertEquals(searchIds.get(0), searchLuis.get(0).getId());
+        searchLuis = dao.searchForLuiPersonRelations(null, "whoever2", null, "whoever4", null, toSearch);
+        assertEquals(searchIds.get(0), searchLuis.get(0).getId());
     }
 }
