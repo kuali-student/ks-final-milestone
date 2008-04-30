1d3b99fe4cf9c103c3a87664a73e86fe441558c0
lcarlsen <lcarlsen@kuali.org>
2008-04-29 23:37:31 -0400

    Added copyrights, Java doc and deepCopy parameter check
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@530 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- brms-repository/src/main/java/org/kuali/student/brms/repository/util/ObjectUtil.java
+++ brms-repository/src/main/java/org/kuali/student/brms/repository/util/ObjectUtil.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kuali.student.brms.repository.util;
 
 import java.io.ByteArrayInputStream;
@@ -6,6 +21,12 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
+/**
+ * This is an object utility class. 
+ * 
+ * @author Kuali Student Team (len.kuali@googlegroups.com)
+ *
+ */
 public class ObjectUtil {
 
     /**
@@ -21,7 +42,11 @@ public class ObjectUtil {
      * @return
      * @throws Exception
      */
-    public static Object deepCopy(Serializable obj) throws Exception {
+    public static Object deepCopy( Serializable object ) throws Exception {
+        if ( object == null ) {
+            throw new IllegalArgumentException( "object cannot be null" );
+        }
+        
         ByteArrayOutputStream bos = null;
         ObjectOutputStream oos = null;
         ObjectInputStream ois = null;
@@ -30,7 +55,7 @@ public class ObjectUtil {
             bos = new ByteArrayOutputStream();
             oos = new ObjectOutputStream(bos);
             // Serialize the object
-            oos.writeObject(obj);
+            oos.writeObject( object );
             oos.flush();
             ByteArrayInputStream bin = new ByteArrayInputStream(bos.toByteArray());
             ois = new ObjectInputStream(bin);
