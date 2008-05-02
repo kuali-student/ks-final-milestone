a471ece71e6ab3c1696c739b7aafad75236a64ce
depstein <depstein@kuali.org>
2008-05-02 15:25:14 -0400

    Upgraded to CXF 2.1 fixed Jetty problem with tests where two service tests run after each other failed
    git-svn-id: https://svn.kuali.org/repos/student/poc/learningunitmanagement/lum-api/branches/lum-api-dev@585 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- luipersonrelation-api/src/main/java/org/kuali/student/poc/xsd/learningunit/luipersonrelation/dto/LuiPersonRelationCriteria.java
+++ luipersonrelation-api/src/main/java/org/kuali/student/poc/xsd/learningunit/luipersonrelation/dto/LuiPersonRelationCriteria.java
@@ -7,9 +7,6 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 
-import org.kuali.student.poc.xsd.learningunit.lu.dto.LuiDisplay;
-import org.kuali.student.poc.xsd.personidentity.person.dto.PersonDisplay;
-
 @XmlAccessorType(XmlAccessType.FIELD)
 public class LuiPersonRelationCriteria implements Serializable {
 
