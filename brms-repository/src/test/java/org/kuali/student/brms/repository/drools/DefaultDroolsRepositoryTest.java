54b67256dba9fb426841f2263377b72af01903a0
lcarlsen <lcarlsen@kuali.org>
2008-05-01 20:02:22 -0400

    Initial checkin
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@572 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ brms-repository/src/test/java/org/kuali/student/brms/repository/drools/DefaultDroolsRepositoryTest.java
@@ -0,0 +1,37 @@
+package org.kuali.student.brms.repository.drools;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URL;
+
+import org.junit.Test;
+
+public class DefaultDroolsRepositoryTest {
+
+    @Test
+    public void testStartupShutdownRepository() throws Exception {
+        DefaultDroolsRepository repo = new DefaultDroolsRepository();
+        assertNotNull( repo.getRepository().listStates() );
+        repo.shutdownRepository();
+    }    
+
+    @Test
+    public void testStartupShutdownRepositoryWithUrlConfiguration() throws Exception {
+        // Get repository.xml from /repository
+        URL url = DefaultDroolsRepositoryTest.class.getResource("/repository");
+        DefaultDroolsRepository repo = new DefaultDroolsRepository( url );
+        assertNotNull( repo.getRepository().listStates() );
+        repo.shutdownRepository();
+    }    
+
+    @Test
+    public void testStartupShutdownRepositoryWithPathConfiguration() throws Exception {
+        // Get repository.xml from /repository
+        URL url = DefaultDroolsRepositoryTest.class.getResource("/repository");
+        // E.g. path=file:/C:/drools/repository
+        String path = url.toExternalForm();
+        DefaultDroolsRepository repo = new DefaultDroolsRepository( path );
+        assertNotNull( repo.getRepository().listStates() );
+        repo.shutdownRepository();
+    }    
+}
