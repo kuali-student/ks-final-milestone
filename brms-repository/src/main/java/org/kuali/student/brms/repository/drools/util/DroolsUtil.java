60f7dd4cb2130d4400fe7cc2c7cbdd6126a497b5
lcarlsen <lcarlsen@kuali.org>
2008-05-02 20:07:59 -0400

    Removed unused import
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@593 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- brms-repository/src/main/java/org/kuali/student/brms/repository/drools/util/DroolsUtil.java
+++ brms-repository/src/main/java/org/kuali/student/brms/repository/drools/util/DroolsUtil.java
@@ -20,7 +20,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInput;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.StringReader;
 import java.util.ArrayList;
