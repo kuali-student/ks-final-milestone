a8b23f257616c33ba3a3a4ed85ca727ca5ebf188
lcarlsen <lcarlsen@kuali.org>
2008-05-05 04:34:40 -0400

    Added new test cases
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@602 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- brms-repository/src/test/java/org/kuali/student/brms/repository/rule/RuleSetTest.java
+++ brms-repository/src/test/java/org/kuali/student/brms/repository/rule/RuleSetTest.java
@@ -42,7 +42,7 @@ public class RuleSetTest {
             RuleUtil.createRuleSet( null );
             fail( "Should not be able to create a rule set with a null name" );
         }
-        catch( RuntimeException e ) {
+        catch( IllegalArgumentException e ) {
             assertTrue( true );
         }
     }
@@ -53,7 +53,7 @@ public class RuleSetTest {
             RuleUtil.createRuleSet( null, "ruleSetName", -1L );
             fail( "Should not be able to create a rule set with a null UUID" );
         }
-        catch( RuntimeException e ) {
+        catch( IllegalArgumentException e ) {
             assertTrue( true );
         }
     }
@@ -64,7 +64,7 @@ public class RuleSetTest {
             RuleUtil.createRuleSet( null, null, -1L );
             fail( "Should not be able to create a rule set with a null UUID and a null name" );
         }
-        catch( RuntimeException e ) {
+        catch( IllegalArgumentException e ) {
             assertTrue( true );
         }
     }
@@ -102,6 +102,14 @@ public class RuleSetTest {
     }
 
     @Test
+    public void testUuidNotEquals() {
+        RuleSet ruleSet1 = RuleUtil.createRuleSet( "123", "ruleSet1", 1L );
+        RuleSet ruleSet2 = RuleUtil.createRuleSet( "987", "ruleSet1", 1L );
+
+        assertFalse( ruleSet1.equals( ruleSet2 ) );
+    }
+
+    @Test
     public void testRuleSetHeaderEquals() {
         RuleSet ruleSet1 = RuleUtil.createRuleSet( "ruleSet1" );
         ruleSet1.addHeader("import java.util.Calendar");
@@ -109,6 +117,87 @@ public class RuleSetTest {
         ruleSet2.addHeader("import java.util.Calendar");
 
         assertEquals( ruleSet1, ruleSet2 );
+    }
+
+    @Test
+    public void testRuleSetAddOneHeader() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        String header = "import java.util.Calendar;";
+        ruleSet.addHeader(header);
+
+        assertEquals( 1, ruleSet.getHeaderList().size() );
+        assertEquals( header, ruleSet.getHeaderList().get(0) );
+    }
+
+    public void testRuleSetAddHeader_WithMissingSimicolon() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        String header = "import java.util.Calendar";
+        ruleSet.addHeader(header);
+
+        String expectedHeader = header +";";
+        assertEquals( 1, ruleSet.getHeaderList().size() );
+        assertEquals( expectedHeader, ruleSet.getHeaderList().get(0) );
+    }
+
+    @Test
+    public void testRuleSetAddThreeHeaders() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        String header1 = "import java.util.Calendar;";
+        String header2 = "import java.util.Map;";
+        String header3 = "import java.util.List;";
+        ruleSet.addHeader(header1);
+        ruleSet.addHeader(header2);
+        ruleSet.addHeader(header3);
+
+        assertEquals( 3, ruleSet.getHeaderList().size() );
+        assertEquals( header1, ruleSet.getHeaderList().get(0) );
+        assertEquals( header2, ruleSet.getHeaderList().get(1) );
+        assertEquals( header3, ruleSet.getHeaderList().get(2) );
+    }
+
+    @Test
+    public void testRuleSetContainsThreeHeaders() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        String header1 = "import java.util.Calendar;";
+        String header2 = "import java.util.Map;";
+        String header3 = "import java.util.List;";
+        ruleSet.addHeader(header1);
+        ruleSet.addHeader(header2);
+        ruleSet.addHeader(header3);
+
+        assertEquals( 3, ruleSet.getHeaderList().size() );
+        assertTrue( ruleSet.containsHeader(header1) );
+        assertTrue( ruleSet.containsHeader(header2) );
+        assertTrue( ruleSet.containsHeader(header3) );
+    }
+
+    @Test
+    public void testRuleSetClearHeaders() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        ruleSet.addHeader("import java.util.Calendar");
+        ruleSet.clearHeaders();
+
+        assertEquals( 0, ruleSet.getHeaderList().size() );
+    }
+
+    @Test
+    public void testRuleSetRemoveHeader_WithSemicolon() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        String header = "import java.util.Calendar;";
+        ruleSet.addHeader(header);
+        ruleSet.removeHeader(header);
+
+        assertEquals( 0, ruleSet.getHeaderList().size() );
+    }
+
+    @Test
+    public void testRuleSetRemoveHeader_WithoutSemicolon() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        String header = "import java.util.Calendar";
+        ruleSet.addHeader(header);
+        ruleSet.removeHeader(header);
+
+        assertEquals( 0, ruleSet.getHeaderList().size() );
     }
 
     @Test
@@ -155,15 +244,97 @@ public class RuleSetTest {
     }
 
     @Test
-    public void testRuleSetGetRules_NonModifiableHeaders() {
+    public void testRuleSetAddOneRule() {
         RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
-        String header = "import java.util.Calendar;";
-        ruleSet.addHeader( header );
-        ruleSet.getHeaderList().clear();
-        
-        // Test that the rule is not modifiable
-        assertEquals( 1, ruleSet.getHeaderList().size() );
-        assertEquals( header, ruleSet.getHeaderList().get(0) );
+        Rule rule = RuleUtil.createRule( "rule1" );
+        ruleSet.addRule( rule );
+        assertEquals( 1, ruleSet.getRules().size() );
+        assertEquals( rule, ruleSet.getRules().get(0) );
+    }
+
+    @Test
+    public void testRuleSetAddThreeRules() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        Rule rule1 = RuleUtil.createRule( "rule1" );
+        Rule rule2 = RuleUtil.createRule( "rule2" );
+        Rule rule3 = RuleUtil.createRule( "rule3" );
+        ruleSet.addRule( rule1 );
+        ruleSet.addRule( rule2 );
+        ruleSet.addRule( rule3 );
+
+        assertEquals( 3, ruleSet.getRules().size() );
+        assertEquals( rule1, ruleSet.getRules().get(0) );
+        assertEquals( rule2, ruleSet.getRules().get(1) );
+        assertEquals( rule3, ruleSet.getRules().get(2) );
+    }
+
+    @Test
+    public void testRuleSetAddDuplicateRule() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        Rule rule = RuleUtil.createRule( "rule1" );
+        Rule duplicateRule = RuleUtil.createRule( "rule1" );
+        ruleSet.addRule( rule );
+        ruleSet.addRule( duplicateRule );
+
+        // Duplicates are not added
+        assertEquals( 1, ruleSet.getRules().size() );
+        assertEquals( rule, ruleSet.getRules().get(0) );
+    }
+
+    @Test
+    public void testRuleSetContainsThreeRules() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        Rule rule1 = RuleUtil.createRule( "rule1" );
+        Rule rule2 = RuleUtil.createRule( "rule2" );
+        Rule rule3 = RuleUtil.createRule( "rule3" );
+        ruleSet.addRule( rule1 );
+        ruleSet.addRule( rule2 );
+        ruleSet.addRule( rule3 );
+
+        assertEquals( 3, ruleSet.getRules().size() );
+        assertTrue( ruleSet.containsRule(rule1.getName()) );
+        assertTrue( ruleSet.containsRule(rule2.getName()) );
+        assertTrue( ruleSet.containsRule(rule3.getName()) );
+    }
+
+    @Test
+    public void testRuleSetClearRules() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        Rule rule = RuleUtil.createRule( "rule1" );
+        ruleSet.addRule( rule );
+        ruleSet.clearRules();
+        assertEquals( 0, ruleSet.getRules().size() );
+    }
+
+    @Test
+    public void testRuleSetRemoveOneRule() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        Rule rule1 = RuleUtil.createRule( "rule1" );
+        Rule rule2 = RuleUtil.createRule( "rule2" );
+        Rule rule3 = RuleUtil.createRule( "rule3" );
+        ruleSet.addRule( rule1 );
+        ruleSet.addRule( rule2 );
+        ruleSet.addRule( rule3 );
+
+        ruleSet.removeRule( rule2.getName() );
+        assertEquals( 2, ruleSet.getRules().size() );
+        assertFalse( ruleSet.getRules().contains( rule2 ) );
+    }
+
+    @Test
+    public void testRuleSetRemoveThreeRules() {
+        RuleSet ruleSet = RuleUtil.createRuleSet( "ruleSet1" );
+        Rule rule1 = RuleUtil.createRule( "rule1" );
+        Rule rule2 = RuleUtil.createRule( "rule2" );
+        Rule rule3 = RuleUtil.createRule( "rule3" );
+        ruleSet.addRule( rule1 );
+        ruleSet.addRule( rule2 );
+        ruleSet.addRule( rule3 );
+
+        ruleSet.removeRule( rule2.getName() );
+        ruleSet.removeRule( rule1.getName() );
+        ruleSet.removeRule( rule3.getName() );
+        assertEquals( 0, ruleSet.getRules().size() );
     }
 
     @Test
@@ -180,7 +351,7 @@ public class RuleSetTest {
     }
 
     @Test
-    public void testRuleSetsNotEquals() {
+    public void testRuleSetsNotEquals_DifferentRuleName() {
         RuleSet ruleSet1 = RuleUtil.createRuleSet( "ruleSet1" );
         ruleSet1.addHeader( "import java.util.Calendar" );
         ruleSet1.addRule( RuleUtil.createRule( "rule1" ) );
