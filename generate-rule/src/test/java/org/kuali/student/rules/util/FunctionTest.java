ab349c95c885810ba18c39bdd6ca14dacf3ba864
rdiaz <rdiaz@kuali.org>
2008-05-05 17:09:52 -0400

    removed spaces from a "rule function" and added a test
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@637 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- generate-rule/src/test/java/org/kuali/student/rules/util/FunctionTest.java
+++ generate-rule/src/test/java/org/kuali/student/rules/util/FunctionTest.java
@@ -10,29 +10,42 @@ import org.junit.Test;
 
 public class FunctionTest {
 
-	private String theFunction = "A0*B4+(C*D)";
-	private Function f;
-	private List<String> funcVars;
-	private String var;
-	private String[] arrVar = {"A0","B4","C","D"};
-	
-	@Before
-	public void setUp() throws Exception {
-		f = new Function(theFunction);
-		funcVars = f.getVariables();
-	}
+    private String theFunction = "A0*B4+ (C*D )";
+    private Function f;
+    private List<String> funcVars;
+    private List<String> funcSymbols;
+    private String var;
+    private String[] arrVar = {"A0","B4","C","D"};
+    private String[] arrSymbols = {"A0","*","B4","+","(","C","*","D",")"};
+    
+    @Before
+    public void setUp() throws Exception {
+        f = new Function(theFunction);
+        funcVars = f.getVariables();
+        funcSymbols = f.getSymbols();
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public void testFunction() {
-		
-		for(int i=0; i < funcVars.size(); i++ ){
-			var = funcVars.get(i);
-			assertEquals(var, arrVar[i]);
-		}
-	}
+    @Test
+    public void testGetVariables() {
+        
+        for(int i=0; i < funcVars.size(); i++ ){
+            var = funcVars.get(i);
+            assertEquals(var, arrVar[i]);
+        }
+    }
+    
+    @Test
+    public void testGetSymbols() {
+        
+        for(int i=0; i < funcSymbols.size(); i++ ){
+            var = funcSymbols.get(i);
+            System.out.println("var : " + var);
+            assertEquals(var, arrSymbols[i]);
+        }
+    }
 
 }
