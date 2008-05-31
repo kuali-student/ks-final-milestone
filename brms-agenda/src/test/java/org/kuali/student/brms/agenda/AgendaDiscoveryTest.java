e39db4f51b9fa99bdd99e6495f5f8544440724f7
lcarlsen <lcarlsen@kuali.org>
2008-05-31 13:57:54 -0400

    Added an example
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@745 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ brms-agenda/src/test/java/org/kuali/student/brms/agenda/AgendaDiscoveryTest.java
@@ -0,0 +1,49 @@
+package org.kuali.student.brms.agenda;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.kuali.student.brms.agenda.entity.Agenda;
+import org.kuali.student.brms.agenda.entity.Anchor;
+import org.kuali.student.brms.agenda.entity.AnchorType;
+
+public class AgendaDiscoveryTest {
+
+    @Test
+    public void testGetAgenda_StudentPlansCourse() {
+        AgendaDiscovery agendaDiscovery = new AgendaDiscovery();
+        AgendaRequest request = new AgendaRequest( "student", "course", "offered", "planned" );
+        AnchorType type = new AnchorType( "course", "clu.type.course" );
+        Anchor anchor = new Anchor( "math301", type );
+        
+        Agenda agenda = agendaDiscovery.getAgenda( request, anchor );
+        assertNotNull( agenda );
+
+        System.out.println( "**************************************************" );
+        System.out.println( "AgendaType = " + agenda.getAgendaType() );
+        System.out.println( "AgendaType RuleTypes = " + agenda.getAgendaType().getBusinessRuleTypes() );
+
+        System.out.println( "Agenda = " + agenda );
+        System.out.println( "Agenda Rules = " + agenda.getBusinessRules() );
+        System.out.println( "**************************************************" );
+    }
+
+    @Test
+    public void testGetAgenda_StudentEnrollsInOfferedCourse() {
+        AgendaDiscovery agendaDiscovery = new AgendaDiscovery();
+        AgendaRequest request = new AgendaRequest( "student", "course", "offered", "enrolled" );
+        AnchorType type = new AnchorType( "course", "clu.type.course" );
+        Anchor anchor = new Anchor( "math301", type );
+        
+        Agenda agenda = agendaDiscovery.getAgenda( request, anchor );
+        assertNotNull( agenda );
+
+        System.out.println( "**************************************************" );
+        System.out.println( "AgendaType = " + agenda.getAgendaType() );
+        System.out.println( "AgendaType RuleTypes = " + agenda.getAgendaType().getBusinessRuleTypes() );
+
+        System.out.println( "Agenda = " + agenda );
+        System.out.println( "Agenda Rules = " + agenda.getBusinessRules() );
+        System.out.println( "**************************************************" );
+    }
+}
