88d90109c507c99776e979c2c563f39efafed64a
kmuthusw <kmuthusw@kuali.org>
2008-06-04 15:11:18 -0400

    Refactored from org.kuali.student.rules.BRMS.mapper
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@776 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ generate-rule/src/main/java/org/kuali/student/rules/statement/Proposition.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.kuali.student.rules.statement;
+
+import org.kuali.student.rules.BRMSCore.entity.ComparisonOperator;
+
+/**
+ * Apply a logical constraint.
+ * 
+ * @author Kuali Student Team (kamal.kuali@gmail.com)
+ *
+ */
+public interface Proposition<T extends Comparable<? super T>> {
+	
+	
+	
+	/**
+	 * Evaluates the proposition to a truth value
+	 * @return <code>true</code> if the constraint is met.
+	 */
+	public Boolean apply(ComparisonOperator operator, T expectedValue);
+	
+	/**
+	 * Returns the cached value of apply method's result
+	 * @return <code>true</code> if the constraint is met.
+	 * @exception IllegalPropositionStateException
+	 */
+	public Boolean getResult();	
+	
+	/**
+	 * An explanation of the results of the constraint.
+	 * @return the advice
+	 */
+	public PropositionReport getReport();
+	
+}
