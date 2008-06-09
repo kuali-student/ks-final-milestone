ec827daf5e5a95e298e1e173c456b46ae0a7977f
rdiaz <rdiaz@kuali.org>
2008-06-09 16:24:15 -0400

    updated to use proposition container
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@793 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- inspect-rule-failure/src/test/java/org/kuali/student/rules/runtime/ast/GenerateRuleFailureMessageTest.java
+++ inspect-rule-failure/src/test/java/org/kuali/student/rules/runtime/ast/GenerateRuleFailureMessageTest.java
@@ -6,46 +6,98 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.kuali.student.rules.runtime.ast.GenerateRuleFailureMessage;
+import org.kuali.student.rules.statement.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class GenerateRuleFailureMessageTest {
+
+	private String functionalRuleString;
 	
-	private HashMap<String, Boolean> nodeValueMap;
-	private HashMap<String, String> nodeFailureMessageMap;
-	private String functionString;
+	private SubsetProposition<Integer, String> subsetPropA = new SubsetProposition<Integer, String>("A", null, null, null, null );
+	private SubsetProposition<Integer, String> subsetPropB = new SubsetProposition<Integer, String>("B", null, null, null, null );
+	private SubsetProposition<Integer, String> subsetPropC = new SubsetProposition<Integer, String>("C", null, null, null, null );
+	private SubsetProposition<Integer, String> subsetPropD = new SubsetProposition<Integer, String>("D", null, null, null, null );
 	
+	private PropositionReport propositionReportA = new PropositionReport();
+	private PropositionReport propositionReportB = new PropositionReport();
+	private PropositionReport propositionReportC = new PropositionReport();
+	private PropositionReport propositionReportD = new PropositionReport();
+    
 	@Before
 	public void setUp() throws Exception {
-		nodeValueMap = new HashMap<String, Boolean>();
-		nodeFailureMessageMap = new HashMap<String, String>();
 	}
 	
 	@Test
 	public void testExecuteRuleFailureMessage1()
 	{
-		functionString = "A*B*C*D";
-    	
-		nodeValueMap.put("A", false);
-		nodeValueMap.put("B", true);
-		nodeValueMap.put("C", true);
-		nodeValueMap.put("D", true);
-		
-		nodeFailureMessageMap.put("A", "Need MATH 200");
-		nodeFailureMessageMap.put("B", "Need MATH 110");
-		nodeFailureMessageMap.put("C", "Need 15 credits or more of 1st year science");
-		nodeFailureMessageMap.put("D", "Need English 6000");
-		
-		String expected = "Need MATH 200";
-		String actual = GenerateRuleFailureMessage.executeRule(functionString, nodeValueMap, nodeFailureMessageMap);
-		
-		assertEquals(expected, actual);
+	    functionalRuleString = "A*B*C*D";
+	    
+	    PropositionContainer pc = new PropositionContainer();
+	    pc.setFunctionalRuleString(functionalRuleString);
+	    
+	    subsetPropA.setResult(false);
+	    subsetPropB.setResult(true);
+	    subsetPropC.setResult(true);
+	    subsetPropD.setResult(true);
+	    
+	    propositionReportA.setFailureMessage("Need MATH 200");
+	    propositionReportB.setFailureMessage("Need MATH 110");
+	    propositionReportC.setFailureMessage("Need 15 credits or more of 1st year science");
+	    propositionReportD.setFailureMessage("Need English 6000");
+	    
+	    subsetPropA.setReport(propositionReportA);
+	    subsetPropB.setReport(propositionReportB);
+	    subsetPropC.setReport(propositionReportC);
+	    subsetPropD.setReport(propositionReportD);
+	    
+	    pc.setProposition(subsetPropA.getPropositionName(), subsetPropA);
+	    pc.setProposition(subsetPropB.getPropositionName(), subsetPropB);
+	    pc.setProposition(subsetPropC.getPropositionName(), subsetPropC);
+	    pc.setProposition(subsetPropD.getPropositionName(), subsetPropD);
+	    
+	    String expected = "Need MATH 200";
+        String actual = GenerateRuleFailureMessage.executeRule(pc);
+	    assertEquals(expected, actual);
+	    
 	}
 	
 	@Test
 	public void testExecuteRuleFailureMessage2()
 	{
-		functionString = "A+(B*C)+D";
+	    // New code for PropositionContainer starts here
+	    functionalRuleString = "A+(B*C)+D";
+        
+        PropositionContainer pc = new PropositionContainer();
+        pc.setFunctionalRuleString(functionalRuleString);
+        
+        // need to set proposition result and failure message for testing.
+        subsetPropA.setResult(false);
+        subsetPropB.setResult(true);
+        subsetPropC.setResult(false);
+        subsetPropD.setResult(false);
+        
+        propositionReportA.setFailureMessage("Need MATH 200");
+        propositionReportB.setFailureMessage("Need MATH 110");
+        propositionReportC.setFailureMessage("Need 15 credits or more of 1st year science");
+        propositionReportD.setFailureMessage("Need English 6000");
+        
+        subsetPropA.setReport(propositionReportA);
+        subsetPropB.setReport(propositionReportB);
+        subsetPropC.setReport(propositionReportC);
+        subsetPropD.setReport(propositionReportD);
+        
+        pc.setProposition(subsetPropA.getPropositionName(), subsetPropA);
+        pc.setProposition(subsetPropB.getPropositionName(), subsetPropB);
+        pc.setProposition(subsetPropC.getPropositionName(), subsetPropC);
+        pc.setProposition(subsetPropD.getPropositionName(), subsetPropD);
+        
+        String expected = "Need MATH 200 OR Need 15 credits or more of 1st year science OR Need English 6000";
+        String actual = GenerateRuleFailureMessage.executeRule(pc);
+        assertEquals(expected, actual);
+        
+        
+/*	    functionalRuleString = "A+(B*C)+D";
     	
 		nodeValueMap.put("A", false);
 		nodeValueMap.put("B", true);
@@ -58,9 +110,9 @@ public class GenerateRuleFailureMessageTest {
 		nodeFailureMessageMap.put("D", "Need English 6000");
 		
 		String expected = "Need MATH 200 OR Need 15 credits or more of 1st year science OR Need English 6000";
-		String actual = GenerateRuleFailureMessage.executeRule(functionString, nodeValueMap, nodeFailureMessageMap);
+		String actual = GenerateRuleFailureMessage.executeRule(functionalRuleString, nodeValueMap, nodeFailureMessageMap);
 		
-		assertEquals(expected, actual);
+		assertEquals(expected, actual);*/
 	}
 	
 	@After
