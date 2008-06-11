ca91961676751fef6441a220c6a282e611eb51fc
kmuthusw <kmuthusw@kuali.org>
2008-06-10 23:03:36 -0400

    Cleaned up code for GenerateRuleSet
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@821 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ generate-rule/src/test/java/org/kuali/student/rules/statement/PropositionContainerTest.java
@@ -0,0 +1,21 @@
+package org.kuali.student.rules.statement;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.kuali.student.rules.statement.SubsetProposition;
+
+
+public class PropositionContainerTest{
+
+    Set<String> set1 = new HashSet<String>( Arrays.asList("MATH101, MATH102".split(",")) );
+
+    Set<String> set2 = new HashSet<String>( Arrays.asList("MATH101,MATH103,CHEM101".split(",")));
+
+
+    
+    
+}
