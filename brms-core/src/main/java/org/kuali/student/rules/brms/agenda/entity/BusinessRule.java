1ac0625bb8a979edeeab67cdefe0a5eb8bd27343
kmuthusw <kmuthusw@kuali.org>
2008-06-16 16:25:32 -0400

    Merged brms-agenda into brms-core. Refactored FunctionalBusinessRuleService to be a spring singleton.
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev@870 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ brms-core/src/main/java/org/kuali/student/rules/brms/agenda/entity/BusinessRule.java
@@ -0,0 +1,63 @@
+package org.kuali.student.rules.brms.agenda.entity;
+
+import org.kuali.student.rules.brms.core.entity.FunctionalBusinessRule;
+
+public class BusinessRule
+{
+	private String id;
+	
+	private Anchor anchor;
+	
+	private BusinessRuleType businessRuleType;
+
+	private String functionalRuleString;
+	
+    public BusinessRule(String id, BusinessRuleType businessRuleType, String functionalRuleString ) {
+        this.id = id;
+        this.businessRuleType = businessRuleType;
+        this.functionalRuleString = functionalRuleString;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Anchor getAnchor() {
+        return anchor;
+    }
+
+    public void setAnchor(Anchor anchor) {
+        this.anchor = anchor;
+    }
+
+    public BusinessRuleType getBusinessRuleType() {
+        return businessRuleType;
+    }
+
+    
+    /**
+     * @return the functionalRule
+     */
+    public String getFunctionalRuleString() {
+        return functionalRuleString;
+    }
+
+    /**
+     * @param functionalRule the functionalRule to set
+     */
+    public void setFunctionalRule(String functionalRuleString) {
+        this.functionalRuleString = functionalRuleString;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append( "BusinessRule[id=" ); 
+        sb.append( id );
+        sb.append( ", type=" );
+        sb.append( businessRuleType.getType() );
+        sb.append( ", anchor=" );
+        sb.append( anchor == null ? "null" : anchor.getName() );
+        sb.append( "]" );
+        return sb.toString();
+    }
+}
---
+++ brms-core/src/test/java/org/kuali/student/rules/brms/agenda/AgendaDiscoveryTest.java
@@ -0,0 +1,76 @@
+package org.kuali.student.rules.brms.agenda;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kuali.student.rules.brms.agenda.AgendaDiscovery;
+import org.kuali.student.rules.brms.agenda.AgendaRequest;
+import org.kuali.student.rules.brms.agenda.entity.Agenda;
+import org.kuali.student.rules.brms.agenda.entity.Anchor;
+import org.kuali.student.rules.brms.agenda.entity.AnchorType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:application-context.xml"})
+public class AgendaDiscoveryTest {
+
+    @Autowired
+    AgendaDiscovery agendaDiscovery;
+    
+    @Test
+    public void testGetAgenda_StudentPlansCourse() {
+
+        AgendaRequest request = new AgendaRequest( "student", "course", "offered", "planned" );
+        AnchorType type = new AnchorType( "course", "clu.type.course" );
+        Anchor anchor = new Anchor( "math301", "Math-301", type );
+        
+        Agenda agenda = agendaDiscovery.getAgenda( request, anchor );
+        assertNotNull( agenda );
+
+        System.out.println( "**************************************************" );
+        System.out.println( "AgendaType = " + agenda.getAgendaType() );
+        System.out.println( "AgendaType RuleTypes = " + agenda.getAgendaType().getBusinessRuleTypes() );
+
+        System.out.println( "Agenda = " + agenda );
+        System.out.println( "Agenda Rules = " + agenda.getBusinessRules() );
+        System.out.println( "**************************************************" );
+    }
+
+    @Test
+    public void testGetAgenda_StudentEnrollsInOfferedCourse() {
+
+        AgendaRequest request = new AgendaRequest( "student", "course", "offered", "enrolled" );
+        AnchorType type = new AnchorType( "course", "clu.type.course" );
+        Anchor anchor = new Anchor( "math301", "Math-301", type );
+        
+        Agenda agenda = agendaDiscovery.getAgenda( request, anchor );
+        assertNotNull( agenda );
+
+        System.out.println( "**************************************************" );
+        System.out.println( "AgendaType = " + agenda.getAgendaType() );
+        System.out.println( "AgendaType RuleTypes = " + agenda.getAgendaType().getBusinessRuleTypes() );
+
+        System.out.println( "Agenda = " + agenda );
+        System.out.println( "Agenda Rules = " + agenda.getBusinessRules() );
+        System.out.println( "**************************************************" );
+    }
+
+    /**
+     * @return the agendaDiscovery
+     */
+    public AgendaDiscovery getAgendaDiscovery() {
+        return agendaDiscovery;
+    }
+
+    /**
+     * @param agendaDiscovery the agendaDiscovery to set
+     */
+    public void setAgendaDiscovery(AgendaDiscovery agendaDiscovery) {
+        this.agendaDiscovery = agendaDiscovery;
+    }
+    
+    
+}
