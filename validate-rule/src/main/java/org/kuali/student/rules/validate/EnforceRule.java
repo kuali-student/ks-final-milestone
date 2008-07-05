01545cfe8fd76b0b92b3d7bb4035f5c836966b35
lcarlsen <lcarlsen@kuali.org>
2008-07-04 20:21:57 -0400

    Removed unused imports
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev2@1043 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- validate-rule/src/main/java/org/kuali/student/rules/validate/EnforceRule.java
+++ validate-rule/src/main/java/org/kuali/student/rules/validate/EnforceRule.java
@@ -1,10 +1,8 @@
 package org.kuali.student.rules.validate;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
 import org.drools.RuleBase;
