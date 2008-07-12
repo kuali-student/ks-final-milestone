af723b9965726cc3dc873564820cc2be62856547
lcarlsen <lcarlsen@kuali.org>
2008-07-11 20:56:55 -0400

    Added new method
    git-svn-id: https://svn.kuali.org/repos/student/poc/brms/brms-ri/branches/brms-ri-dev2@1125 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- rules-common/src/main/java/org/kuali/student/rules/internal/common/spring/interceptors/cache/EHCacheInterceptor.java
+++ rules-common/src/main/java/org/kuali/student/rules/internal/common/spring/interceptors/cache/EHCacheInterceptor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kuali.student.rules.internal.common.spring.interceptors.cache;
 
 import org.springframework.beans.factory.InitializingBean;
@@ -6,37 +21,36 @@ import org.springframework.util.Assert;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.Element;
 
-public class EHCacheInterceptor extends AbstractCacheInterceptor
-    implements InitializingBean
-{
+public class EHCacheInterceptor 
+    extends AbstractCacheInterceptor implements InitializingBean {
     private Cache cache;
 
     /**
      * sets cache name to be used
      */
-    public void setCache( Cache cache )
-    {
+    public void setCache(Cache cache) {
         this.cache = cache;
     }
 
     /**
      * Checks if required attributes are provided.
      */
-    public void afterPropertiesSet() throws Exception
-    {
-        Assert.notNull( cache, "A cache is required. Use setCache(Cache)." );
+    public void afterPropertiesSet() throws Exception {
+        Assert.notNull(cache, "A cache is required. Use setCache(Cache).");
     }
 
-    public Object getCacheObject( Object key )
-    {
-        Element element = this.cache.get( key );
-        return ( element == null ? null : element.getValue() );
+    public Object getCacheObject(Object key) {
+        Element element = this.cache.get(key);
+        return (element == null ? null : element.getValue());
     }
 
-    public void addToCache( Object key, Object obj )
-    {
-        Element element = new Element( key, obj );
-        this.cache.put( element );
+    public void addToCache(Object key, Object obj) {
+        Element element = new Element(key, obj);
+        this.cache.put(element);
+    }
+
+    protected void removeFromCache(Object key) {
+        this.cache.remove(key);
     }
 
 }
