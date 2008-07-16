625b9fdfc820d2b5a5ccf67c0928113967099d12
gtaylor <gtaylor@kuali.org>
2008-07-16 09:42:35 -0400

    [NO JIRA] Updated the EditLabel and SearchListBox
    git-svn-id: https://svn.kuali.org/repos/student/commons/ks-commons-ui/branches/ks-commons-ui-dev@1171 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/commons/ui/widgets/SearchListBox.java
+++ src/main/java/org/kuali/student/commons/ui/widgets/SearchListBox.java
@@ -1,5 +1,9 @@
 package org.kuali.student.commons.ui.widgets;
 
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.ui.ChangeListener;
+import com.google.gwt.user.client.ui.ChangeListenerCollection;
+import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.ListBox;
 
 /**
@@ -7,34 +11,87 @@ import com.google.gwt.user.client.ui.ListBox;
  *         </p>
  *         The standard gwt ListBox with a couple extra methods I commonly use.
  */
-public class SearchListBox extends ListBox {
+public class SearchListBox extends ListBox implements HasText {
 
-    public int findIndex(String value) {
-        int iRet = -1;
-        if (value == null) {
-            return iRet;
+    
+    private ChangeListenerCollection changeListeners;
+
+    
+    protected void fireChanges(){
+        if (changeListeners != null) {
+            changeListeners.fireChange(this);
         }
-        for (int i = 0; i < getItemCount(); i++) {
-            if (value.equals(getValue(i))) {
+    }
+    
+    public void addChangeListener(ChangeListener listener) {
+        if (changeListeners == null) {
+          changeListeners = new ChangeListenerCollection();
+          sinkEvents(Event.ONCHANGE);
+        }
+        changeListeners.add(listener);
+    }
+    
+    public int findIndex(String value){ 
+        int iRet = -1;  
+        if(value == null) return iRet;      
+        for(int i=0; i< this.getItemCount(); i++){
+            if(value.equals(this.getValue(i))){
                 iRet = i;
             }
-        }
-        return iRet;
+        }       
+        return iRet;                
+    }
+    
+    public int findIndexOfText(String value){    
+        int iRet = -1;  
+        if(value == null) return iRet;      
+        for(int i=0; i< this.getItemCount(); i++){
+            if(value.equals(this.getItemText(i))){
+                iRet = i;
+            }
+        }       
+        return iRet;                
+    }
+    
+    /**
+     * This overridden method ...
+     * 
+     * @see com.google.gwt.user.client.ui.ListBox#setItemSelected(int, boolean)
+     */   
+    public void setItemSelected(int index, boolean selected) {
+        super.setItemSelected(index, selected);
+        this.fireChanges();
     }
 
-    public void setItemSelected(String value, boolean selected) throws java.lang.IndexOutOfBoundsException {
-        int index = findIndex(value);
-        if (index >= 0) {
+    public void setItemSelected(String value, boolean selected)
+        throws java.lang.IndexOutOfBoundsException
+    {
+        int index = this.findIndex(value);      
+        if(index >= 0)  {   
             this.setItemSelected(index, selected);
+            this.fireChanges();
         }
     }
-
-    public String getSelectedValue() {
-        return getValue(getSelectedIndex());
+    
+    public String getSelectedValue(){
+        return this.getValue(this.getSelectedIndex());
+    }
+    
+    public String getSelectedText(){        
+        return this.getItemText(this.getSelectedIndex());
     }
 
-    public String getSelectedText() {
-        return getItemText(getSelectedIndex());
+    
+    public String getText() {        
+        return this.getSelectedText();
+    }
+    
+    public void setText(String text) {
+        int index = this.findIndexOfText(text);
+        if(index >= 0)  {
+            this.setSelectedIndex(this.findIndexOfText(text));
+            this.fireChanges();
+        }
     }
 
 }
