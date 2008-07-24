8d2bfcd35029aa76c5299c49df4023132eb740c2
wiljohns <wiljohns@kuali.org>
2008-07-24 18:05:41 -0400

    added ks-commonwidgets.css, ks-tabpanels.css, some styling, added a rounded composite widget
    git-svn-id: https://svn.kuali.org/repos/student/commons/ks-commons-ui/branches/ks-commons-ui-dev@1244 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/commons/ui/widgets/RoundedComposite.java
+++ src/main/java/org/kuali/student/commons/ui/widgets/RoundedComposite.java
@@ -3,6 +3,7 @@ package org.kuali.student.commons.ui.widgets;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class RoundedComposite extends Composite {
@@ -12,19 +13,26 @@ public class RoundedComposite extends Composite {
     final Image bottomLeft = new Image("images/rounded-panel-bottomleft.gif");
     final Image bottomRight = new Image("images/rounded-panel-bottomright.gif");
     
+    final SimplePanel content = new SimplePanel();
     Widget widget;
     
     public RoundedComposite() {
         panel.setWidget(0, 0, topLeft);
         panel.setWidget(0, 2, topRight);
+        panel.setWidget(1, 1, content);
         panel.setWidget(2, 0, bottomLeft);
         panel.setWidget(2, 2, bottomRight);
         
+        panel.addStyleName("KS-RoundedComposite");
         panel.getCellFormatter().addStyleName(0, 1, "KS-RoundedComposite-TopMiddle");
         panel.getCellFormatter().addStyleName(1, 0, "KS-RoundedComposite-Left");
+        panel.getCellFormatter().addStyleName(1, 1, "KS-RoundedComposite-Center");
         panel.getCellFormatter().addStyleName(1, 2, "KS-RoundedComposite-Right");
         panel.getCellFormatter().addStyleName(2, 1, "KS-RoundedComposite-BottomMiddle");
-            
+        
+        panel.setCellPadding(0);
+        panel.setCellSpacing(0);
+        
         super.initWidget(panel);
     }
 
@@ -34,7 +42,10 @@ public class RoundedComposite extends Composite {
 
     public void setWidget(Widget widget) {
         this.widget = widget;
-        panel.setWidget(1, 1, widget);
+        content.setWidget(widget);
     }
     
+    public void remove(Widget widget) {
+        content.remove(widget);
+    }
 }
