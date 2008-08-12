fe755a23a4aee45189f057d7001639d1e6229649
lcarlsen <lcarlsen@kuali.org>
2008-08-12 15:04:46 -0400

    Removed unused imports
    git-svn-id: https://svn.kuali.org/repos/student/brms/brms-impl/branches/brms-impl-dev@1375 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- rule-repository/src/main/java/org/kuali/student/rules/repository/util/ObjectUtil.java
+++ rule-repository/src/main/java/org/kuali/student/rules/repository/util/ObjectUtil.java
@@ -19,7 +19,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 
 /**
  * This is an object utility class. 
