a6bd92794821327e0d3a6fba40f9d7baa11e154f
gtaylor <gtaylor@kuali.org>
2008-08-14 16:41:31 -0400

    [NO JIRA]courses no longer disappear
    git-svn-id: https://svn.kuali.org/repos/student/poc/ks-poc-ui/branches/ks-poc-ui-dev@1430 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/ui/personidentity/client/view/lu/CourseSearchResultPanel.java
+++ src/main/java/org/kuali/student/ui/personidentity/client/view/lu/CourseSearchResultPanel.java
@@ -1,27 +1,21 @@
 package org.kuali.student.ui.personidentity.client.view.lu;
 
-import java.util.List;
-
 import org.kuali.student.commons.ui.mvc.client.ApplicationContext;
 import org.kuali.student.commons.ui.mvc.client.MVC;
 import org.kuali.student.commons.ui.mvc.client.model.Model;
 import org.kuali.student.commons.ui.mvc.client.widgets.ModelBinding;
-import org.kuali.student.commons.ui.propertychangesupport.PropertyChangeEvent;
-import org.kuali.student.commons.ui.propertychangesupport.PropertyChangeListener;
-import org.kuali.student.commons.ui.propertychangesupport.PropertyChangeListenerProxy;
 import org.kuali.student.commons.ui.widgets.tables.ModelTableSelectionListener;
 import org.kuali.student.ui.personidentity.client.controller.LearningUnitController;
 import org.kuali.student.ui.personidentity.client.model.lu.GwtLuiInfo;
 import org.kuali.student.ui.personidentity.client.model.lu.LuModelState;
 import org.kuali.student.ui.personidentity.client.view.AdminEditPanel;
 
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class CourseSearchResultPanel extends Composite{
-    final VerticalPanel panel = new VerticalPanel();
+public class CourseSearchResultPanel extends FlowPanel{
+    //final VerticalPanel panel = new VerticalPanel();
     final CourseSearchResultTable table = new CourseSearchResultTable();
     ModelBinding<GwtLuiInfo> binding = null;
     Label	lSearchText = new Label(ApplicationContext.getViews().get(AdminEditPanel.VIEW_NAME).getMessages().get("searchInstructions"));
@@ -42,11 +36,11 @@ public class CourseSearchResultPanel extends Composite{
 		lSearchText.addStyleName("KS-Label");
 		lSearchText.addStyleName("KS-Search-Message");
 		
-		panel.add(lSearchText);
-		panel.add(table);
+		this.add(lSearchText);
+		this.add(table);
 		table.setVisible(false);
 		
-		super.initWidget(panel);
+		
 	}
 	
 	public void onLoad() {
