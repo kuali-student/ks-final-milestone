1d63fbabc2997c2324bb32b8fdb16a786a06170b
gstruthe <gstruthe@kuali.org>
2008-08-18 15:02:11 -0400

    deleted wsdl and xsd packages from personidentity-api to conform to naming conventions described here https://test.kuali.org/confluence/display/KULSTU/Project+Structure. deleted references to wsdl and xsd package
    git-svn-id: https://svn.kuali.org/repos/student/poc/learningunitmanagement/lum-ri/branches/lum-ri-dev@1470 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- luipersonrelation-ri/src/main/java/org/kuali/student/poc/learningunit/luipersonrelation/service/LuiPersonRelationServiceImpl.java
+++ luipersonrelation-ri/src/main/java/org/kuali/student/poc/learningunit/luipersonrelation/service/LuiPersonRelationServiceImpl.java
@@ -19,7 +19,7 @@ import org.kuali.student.poc.learningunit.luipersonrelation.dao.LuiPersonRelatio
 import org.kuali.student.poc.learningunit.luipersonrelation.entity.LuiPersonRelation;
 import org.kuali.student.poc.wsdl.learningunit.lu.LuService;
 import org.kuali.student.poc.wsdl.learningunit.luipersonrelation.LuiPersonRelationService;
-import org.kuali.student.poc.wsdl.personidentity.person.PersonService;
+import org.kuali.student.poc.personidentity.person.PersonService;
 import org.kuali.student.poc.xsd.learningunit.lu.dto.LuiDisplay;
 import org.kuali.student.poc.xsd.learningunit.lu.dto.LuiInfo;
 import org.kuali.student.poc.xsd.learningunit.lu.dto.Status;
@@ -31,7 +31,7 @@ import org.kuali.student.poc.xsd.learningunit.luipersonrelation.dto.LuiPersonRel
 import org.kuali.student.poc.xsd.learningunit.luipersonrelation.dto.LuiPersonRelationUpdateInfo;
 import org.kuali.student.poc.xsd.learningunit.luipersonrelation.dto.RelationStateInfo;
 import org.kuali.student.poc.xsd.learningunit.luipersonrelation.dto.ValidationResult;
-import org.kuali.student.poc.xsd.personidentity.person.dto.PersonDisplay;
+import org.kuali.student.poc.personidentity.person.dto.PersonDisplay;
 import org.kuali.student.rules.brms.agenda.AgendaDiscovery;
 import org.kuali.student.rules.brms.agenda.AgendaRequest;
 import org.kuali.student.rules.brms.agenda.entity.Agenda;
