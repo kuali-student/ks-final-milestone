e80c78789085bf1ab7417a428729ad27c909210c
gstruthe <gstruthe@kuali.org>
2008-09-09 21:26:41 -0400

    Added tests and bad wsdl files for duplicate and extra wsdl operations, elements, complexTypes, and sequences. BadDuplicateTypePersonService is intentionally invalid wsdl.
    git-svn-id: https://svn.kuali.org/repos/student/poc/learningunitmanagement/lum-ri/branches/lum-ri-dev@1724 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- luipersonrelation-ri/src/test/java/org/kuali/student/poc/learningunit/luipersonrelation/TestPersonServiceWSDL.java
+++ luipersonrelation-ri/src/test/java/org/kuali/student/poc/learningunit/luipersonrelation/TestPersonServiceWSDL.java
@@ -1,11 +1,13 @@
 package org.kuali.student.poc.learningunit.luipersonrelation;
 import javax.xml.namespace.QName;
+import javax.xml.ws.WebServiceException;
 import javax.xml.ws.WebServiceRef;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import org.apache.cxf.service.factory.ServiceConstructionException;
 import org.junit.Test;
 import org.kuali.student.poc.common.test.spring.AbstractServiceTest;
 import org.kuali.student.poc.common.test.spring.Client;
@@ -17,10 +19,11 @@ import org.kuali.student.poc.xsd.personidentity.person.dto.PersonDisplay;
 import javax.xml.ws.soap.SOAPFaultException;
 
 /**
- * Tests runtime behavior for wsdl errors. Uses PersonService but what's under test is jax-ws handling of missing wsdl elements.
+ * Tests CXF 2.1 runtime behavior for wsdl errors. Uses PersonService but what's under test is jax-ws handling 
+ * of missing, duplicate, and extra wsdl operations, elements, complexTypes, and sequences.
  * Interface errors such as missing methods and missing @WebMethod tags are not tested because they can't be detected without a service impl.
  * @author garystruthers
- *
+ * IMPORTANT: other versions of cxf and other implementations of jax-ws may behave differently.
  */
 public class TestPersonServiceWSDL {
 	/**
@@ -45,7 +48,7 @@ public class TestPersonServiceWSDL {
 	 * the Service can't be reached
 	 */
 	@Test(expected=SOAPFaultException.class)
-	public void checkGoodWSDL() throws Exception {
+	public void goodWSDL() throws Exception {
 		JaxWsClientFactory client = createClient();
 		client.setWsdlLocation(wsdlLocation);
 			
@@ -54,8 +57,41 @@ public class TestPersonServiceWSDL {
 		PersonService personService = (PersonService)obj;
 		PersonDisplay personDisplay = personService.fetchPersonDisplay("Person 666");
 	}
-
-
+	/*
+	 * BadDuplicateOpPersonService.wsdl is the same as PersonService.wsdl 
+	 * except that wsdl:operation fetchPersonDisplay is duplicated.
+	 * This is invalid and is detected.
+	 * PersonService is not returned by client.getObject
+	 * @throws IllegalArgumentException, confirming a wsdl with duplicate operations can't be used
+	 * 
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void duplicateOpWSDL() throws Exception {
+		JaxWsClientFactory client = createClient();
+		client.setWsdlLocation("classpath:BadDuplicateOpPersonService.wsdl");
+			
+		Object obj = client.getObject();
+	}
+	/*
+	 * BadExtraOpPersonService.wsdl is the same as PersonService.wsdl 
+	 * except that wsdl:operation fetchPersonDisplay has a copy called
+	 * fetchPersonDisplay.
+	 * This is valid wsdl but the operation isn't in the interface and 
+	 * the error is not detected.
+	 * PersonService <i>is</i> returned by client.getObject
+	 * @throws SOAPFaultException, the extra operation is not detected
+	 * 
+	 */	
+	@Test(expected=SOAPFaultException.class)
+	public void extraOpWSDL() throws Exception {
+		JaxWsClientFactory client = createClient();
+		client.setWsdlLocation("classpath:BadExtraOpPersonService.wsdl");
+			
+		Object obj = client.getObject();
+		assertNotNull(obj);
+		PersonService personService = (PersonService)obj;
+		PersonDisplay personDisplay = personService.fetchPersonDisplay("Person 666");
+	}
 	/*
 	 * BadPersonService.wsdl is the same as PersonService.wsdl 
 	 * except that wsdl:operation fetchPersonDisplay is commented out
@@ -64,11 +100,29 @@ public class TestPersonServiceWSDL {
 	 * 
 	 */
 	@Test(expected=NullPointerException.class)
-	public void checkMissingOpWSDL() throws Exception {
+	public void missingOpWSDL() throws Exception {
 		JaxWsClientFactory client = createClient();
 		client.setWsdlLocation("classpath:BadPersonService.wsdl"); //needs to be updated when PersonService.wsdl is
 		
 		Object obj = client.getObject();
+	}
+	/*
+	 * BadDuplicateBeanPersonService.wsdl is the same as PersonService.wsdl 
+	 * except that wsdl:element validatePersonInfoForPersonTypeResponse is duplicated.
+	 * This is invalid wsdl but the error is not detected.
+	 * PersonService <i>is</i> returned by client.getObject
+	 * @throws SOAPFaultException, the invalid wsdl is not detected
+	 * 
+	 */	
+	@Test(expected=SOAPFaultException.class)
+	public void duplicateBeanWSDL() throws Exception {
+		JaxWsClientFactory client = createClient();
+		client.setWsdlLocation("classpath:BadDuplicateBeanPersonService.wsdl"); //needs to be updated when PersonService.wsdl is
+			
+		Object obj = client.getObject();
+		assertNotNull(obj);
+		PersonService personService = (PersonService)obj;
+		PersonDisplay personDisplay = personService.fetchPersonDisplay("Person 666");
 	}
 	/*
 	 * BadBeanPersonService.wsdl is the same as PersonService.wsdl 
@@ -78,7 +132,7 @@ public class TestPersonServiceWSDL {
 	 * 
 	 */	
 	@Test(expected=NullPointerException.class)
-	public void checkBadBeanWSDL() throws Exception {
+	public void missingBeanWSDL() throws Exception {
 		JaxWsClientFactory client = createClient();
 		client.setWsdlLocation("classpath:BadBeanPersonService.wsdl"); //needs to be updated when PersonService.wsdl is
 			
@@ -93,7 +147,7 @@ public class TestPersonServiceWSDL {
 	 * 
 	 */	
 	@Test(expected=NullPointerException.class)
-	public void checkTypeBeanWSDL() throws Exception {
+	public void missingComplexTypeWSDL() throws Exception {
 		JaxWsClientFactory client = createClient();
 		client.setWsdlLocation("classpath:BadTypePersonService.wsdl"); //needs to be updated when PersonService.wsdl is
 			
@@ -108,7 +162,7 @@ public class TestPersonServiceWSDL {
 	 * 
 	 */		
 	@Test(expected=SOAPFaultException.class)
-	public void checkListTypeBeanWSDL() throws Exception {
+	public void missingSequenceWSDL() throws Exception {
 		JaxWsClientFactory client = createClient();
 		client.setWsdlLocation("classpath:BadListTypePersonService.wsdl"); //needs to be updated when PersonService.wsdl is
 					
@@ -117,5 +171,57 @@ public class TestPersonServiceWSDL {
 		PersonService personService = (PersonService)obj;
 		PersonDisplay personDisplay = personService.fetchPersonDisplay("Person 666");
 	}
+	/*
+	 * BadDuplicateTypePersonService.wsdl is the same as PersonService.wsdl 
+	 * except that wsdl:complexType validatePersonInfoForPersonType is duplicated.
+	 * This is invalid and is detected.
+	 * PersonService is not returned by client.getObject
+	 * @throws WebServiceException, confirming a wsdl with duplicate complexTypes can't be used
+	 * 
+	 */
+	@Test(expected=WebServiceException.class)
+	public void duplicateComplexTypeWSDL() throws Exception {
+		JaxWsClientFactory client = createClient();
+		client.setWsdlLocation("classpath:BadDuplicateTypePersonService.wsdl"); //needs to be updated when PersonService.wsdl is
+					
+		Object obj = client.getObject();
 
+	}
+	/*
+	 * BadExtraTypePersonService.wsdl is the same as PersonService.wsdl 
+	 * except that wsdl:complexType validatePersonInfoForPersonType has a copy
+	 * called validatePersonInfoForPersonTypeExtra.
+	 * PersonService <i>is</i> returned by client.getObject
+	 * confirming a wsdl file with an extra complexType has the same test behavior as a good wsdl.
+	 * @throws SOAPFaultException, 
+	 * 
+	 */	
+	@Test(expected=SOAPFaultException.class)
+	public void extraComplexTypeWSDL() throws Exception {
+		JaxWsClientFactory client = createClient();
+		client.setWsdlLocation("classpath:BadExtraTypePersonService.wsdl");
+			
+		Object obj = client.getObject();
+		assertNotNull(obj);
+		PersonService personService = (PersonService)obj;
+		PersonDisplay personDisplay = personService.fetchPersonDisplay("Person 666");
+	}
+	/*
+	 * BadDuplicateSequencePersonService.wsdl is the same as PersonService.wsdl 
+	 * except that wsdl:sequence is duplicated within a complexType.
+	 * This is invalid wsdl but the error is not detected.
+	 * PersonService <i>is</i> returned by client.getObject
+	 * @throws SOAPFaultException, the invalid wsdl is not detected
+	 * 
+	 */	
+	@Test(expected=SOAPFaultException.class)
+	public void duplicateSequenceWSDL() throws Exception {
+		JaxWsClientFactory client = createClient();
+		client.setWsdlLocation("classpath:BadDuplicateSequencePersonService.wsdl"); //needs to be updated when PersonService.wsdl is
+					
+		Object obj = client.getObject();
+		assertNotNull(obj);
+		PersonService personService = (PersonService)obj;
+		PersonDisplay personDisplay = personService.fetchPersonDisplay("Person 666");
+	}
 }
