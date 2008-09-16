07c555d37e4cb729deaed6f7873984a5ea2809e4
zzraly <zzraly@kuali.org>
2008-09-16 15:06:29 -0400

    refactor and upgrade to use web services
    git-svn-id: https://svn.kuali.org/repos/student/brms/brms-web/branches/brms-web-dev@1769 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ rule-management-developer/src/main/java/org/kuali/student/rules/devgui/server/gwt/DevelopersGuiServiceImplGWT.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package org.kuali.student.rules.devgui.server.gwt;
+
+import java.util.List;
+
+import org.kuali.student.core.spring.BeanFactory;
+import org.kuali.student.rules.devgui.client.model.BusinessRuleInfo;
+import org.kuali.student.rules.devgui.client.model.RulesHierarchyInfo;
+import org.kuali.student.rules.devgui.client.service.DevelopersGuiService;
+import org.kuali.student.rules.devgui.server.impl.DevelopersGuiServiceImpl;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * @author zzraly
+ */
+public class DevelopersGuiServiceImplGWT extends RemoteServiceServlet implements DevelopersGuiService {
+
+    private static final long serialVersionUID = 822326113643828855L;
+
+    private final DevelopersGuiService serviceImpl = (DevelopersGuiService) BeanFactory.getInstance().getBean("developersGuiService");
+
+    // private final DevelopersGuiServiceImpl serviceImpl = new DevelopersGuiServiceImpl();
+
+    public List<RulesHierarchyInfo> findRulesHierarchyInfo() {
+        return serviceImpl.findRulesHierarchyInfo();
+    }
+
+    public BusinessRuleInfo fetchDetailedBusinessRuleInfo(String ruleId) {
+        return serviceImpl.fetchDetailedBusinessRuleInfo(ruleId);
+    }
+
+    public List<String> findAgendaTypes() {
+        return serviceImpl.findAgendaTypes();
+    }
+
+    public List<String> findDeterminationKeysByAgendaType(String businessRuleType) {
+        return serviceImpl.findDeterminationKeysByAgendaType(businessRuleType);
+    }
+
+    public List<String> findBusinessRuleTypesByDeterminationKeySet(String determinationKeys) {
+        return serviceImpl.findBusinessRuleTypesByDeterminationKeySet(determinationKeys);
+    }
+
+    /**
+     * @return the serviceImpl
+     */
+    public DevelopersGuiService getServiceImpl() {
+        return serviceImpl;
+    }
+
+    /**
+     * @param serviceImpl
+     *            the serviceImpl to set
+     */
+    public void setServiceImpl(DevelopersGuiServiceImpl serviceImpl) {
+    // this.serviceImpl = serviceImpl;
+    }
+
+    public List<BusinessRuleInfo> findBusinessRules() {
+        return serviceImpl.findBusinessRules();
+    }
+
+}
