a631b5e99cba2b6692bd733a131c8ef7c141e855
lcarlsen <lcarlsen@kuali.org>
2008-09-16 21:22:31 -0400

    Initial checkin
    git-svn-id: https://svn.kuali.org/repos/student/brms/brms-impl/branches/brms-impl-dev@1776 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ rule-common-impl/src/main/java/org/kuali/student/rules/internal/common/statement/AverageProposition.java
@@ -0,0 +1,36 @@
+package org.kuali.student.rules.internal.common.statement;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.kuali.student.rules.internal.common.entity.ComparisonOperator;
+
+public class AverageProposition<E extends Number> extends SumProposition<E> {
+
+    private BigDecimal listSize;
+    
+	public AverageProposition(String propositionName, 
+    						  ComparisonOperator operator, 
+    						  String expectedValue, 
+    						  List<E> factSet) {
+    	super(propositionName, operator, expectedValue, factSet);
+    	if (factSet == null || factSet.size() == 0) {
+    		throw new IllegalArgumentException("Fact set cannot be null");
+    	}
+    	listSize = new BigDecimal(factSet.size());
+    }
+
+    @Override
+    public Boolean apply() {
+        BigDecimal expectedValue = new BigDecimal(expectedValueAsString);
+
+        BigDecimal average = sum().divide(listSize);
+        //System.out.println("AverageProposition: sum="+average);
+
+        result = checkTruthValue(average, expectedValue);
+
+        cacheReport("Total is short by %s", average, expectedValue);
+
+        return result;
+    }
+}
