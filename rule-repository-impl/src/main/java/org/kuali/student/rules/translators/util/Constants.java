74c2be2ba87352cc39c9c1c221d694cac5fe51c8
lcarlsen <lcarlsen@kuali.org>
2008-09-18 21:26:40 -0400

    Removed unused constants
    git-svn-id: https://svn.kuali.org/repos/student/brms/brms-impl/branches/brms-impl-dev@1829 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- rule-repository-impl/src/main/java/org/kuali/student/rules/translators/util/Constants.java
+++ rule-repository-impl/src/main/java/org/kuali/student/rules/translators/util/Constants.java
@@ -5,9 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Constants {
-	public final static String FACT_STRUCTURE_ID = "kuali.student.fact.id";
 	public final static String DEF_CRITERIA_KEY = "kuali.student.criteria.key";
-	public final static String EXE_FACT_KEY = "kuali.student.fact.key";
 
 	private final static int size = Constants.class.getFields().length;
 	private final static Map<String,String> map = new HashMap<String,String>(size);;
