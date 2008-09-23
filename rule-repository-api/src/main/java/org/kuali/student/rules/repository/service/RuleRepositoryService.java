105d79fbf62eed9feb9df254ae21ea82ea00cec0
lcarlsen <lcarlsen@kuali.org>
2008-09-23 15:25:58 -0400

    Uncommented checkinRuleSet method
    git-svn-id: https://svn.kuali.org/repos/student/brms/brms-api/branches/brms-api-dev@1855 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- rule-repository-api/src/main/java/org/kuali/student/rules/repository/service/RuleRepositoryService.java
+++ rule-repository-api/src/main/java/org/kuali/student/rules/repository/service/RuleRepositoryService.java
@@ -50,27 +50,11 @@ public interface RuleRepositoryService {
     //public void clearRepository();
 
     /**
-     * <p>
      * Creates a new category in the repository.
-     * </p>
-     * Example: Create a new category for rules
      * 
-     * <pre>
-     * boolean b = rulesRepository.createCategory(&quot;/&quot;, &quot;EnrollmentRules&quot;, &quot;A test category 1.0 description&quot;);
-     * b = rulesRepository.createCategory(&quot;/EnrollmentRules&quot;, &quot;Math&quot;, &quot;A Math category description&quot;);
-     *         
-     * List&lt;String&gt; category = rulesRepository.loadChildCategories(&quot;/&quot;);
-     * ... 
-     * category = rulesRepository.loadChildCategories(&quot;/EnrollmentRules&quot;);
-     * ...
-     * </pre>
-     * 
-     * @param path
-     *            Category path
-     * @param name
-     *            Category name
-     * @param description
-     *            Category description
+     * @param path Category path
+     * @param name Category name
+     * @param description Category description
      * @return True if category successfully created, otherwise false
      * @throws CategoryExistsException Thrown if rule set already exists
      * @throws RuleEngineRepositoryException
@@ -79,6 +63,11 @@ public interface RuleRepositoryService {
     public Boolean createCategory(@WebParam(name="path")String path, @WebParam(name="name")String name, @WebParam(name="description")String description) 
         throws CategoryExistsException;
 
+    /**
+     * Removes a category.
+     * 
+     * @param categoryPath Category path
+     */
     @WebMethod
     @Oneway
     public void removeCategory(@WebParam(name="path")String path);
@@ -88,35 +77,9 @@ public interface RuleRepositoryService {
 	    throws CategoryExistsException;
 
     /**
-     * <p>
      * Creates, compiles and checks in a rule set into the repository.
-     * </p>
-     * Example: Create a rule set and a rule and store it in the repository.
      * 
-     * <pre>
-     * // Create rule set
-     * RuleSet ruleSet = RuleSetFactory.getInstance().createRuleSet(&quot;MyNewRuleSet&quot;);
-     * ruleSet.setDescription(&quot;My new rule set&quot;);
-     * 
-     * // Create rule
-     * Rule rule = RuleFactory.getInstance().createRule(&quot;MyNewRule&quot;);
-     * rule.setDescription(&quot;My new rule&quot;);
-     * rule.setCategory(null);
-     * rule.setFormat(&quot;drl&quot;);
-     * rule.setContent(&quot;rule \&quot;new_rule\&quot; when then end&quot;);
-     * ruleSet.addRule(rule);
-     * 
-     * // Create a category for the rules
-     * rulesRepository.createCategory(&quot;/&quot;, &quot;MyCategory&quot;, &quot;My new rule category&quot;);
-     * 
-     * // Create and store the rule set in the repository
-     * String ruleSetUUID = rulesRepository.createRuleSet(ruleSet);
-     * RuleSet ruleSet2 = rulesRepository.loadRuleSet(ruleSetUUID);
-     * ...
-     * </pre>
-     * 
-     * @param ruleSet
-     *            Rule set to create
+     * @param ruleSet Rule set to create
      * @return Rule set uuid
      * @throws RuleExistsException Thrown if a rule within the rule set already exists
      * @throws RuleSetExistsException Thrown if rule set already exists
@@ -147,85 +110,30 @@ public interface RuleRepositoryService {
     public void removeRuleSetSnapshot(@WebParam(name="ruleSetName")String ruleSetName, @WebParam(name="snapshotName")String snapshotName);
 
     /**
-     * <p>
      * Updates a rule set in the repository and returns a new rule set.
-     * </p>
-     * Example: Load a rule set and update its header.
      * 
-     * <pre>
-     * // Load rule set
-     * RuleSet ruleSet = rulesRepository.loadRuleSet(ruleSetUUID);
-     * 
-     * // Update rule set with new header
-     * String header = &quot;import java.util.List;&quot;;
-     * ruleSet.addHeader(header);
-     * 
-     * // Update rule set
-     * rulesRepository.updateRuleSet(ruleSet);
-     * ...
-     * </pre>
-     * 
-     * @param ruleSet
-     *            A rule set to update
+     * @param ruleSet A rule set to update
      * @throws RuleEngineRepositoryException
      */
     //@WebMethod
     //public RuleSetDTO updateRuleSet(@WebParam(name="ruleSet")RuleSetDTO ruleSet) throws RuleEngineRepositoryException;
 
     /**
-     * <p>
-     * Checks in a rule set into the repository. Rule set version is incremented by 1
-     * </p>
-     * Example: Create and check in a rule set.
+     * Checks in a rule set into the repository.
+     * Checkin rule set will create a new version of the rule set.
+     * Rule set version is incremented by 1.
      * 
-     * <pre>
-     * // Create rule set
-     * RuleSet ruleSet = RuleSetFactory.getInstance().createRuleSet(&quot;MyNewRuleSet&quot;);
-     * ruleSet.setDescription(&quot;My new rule set&quot;);
-     * 
-     * // Rule Set Version 1
-     * String ruleSetUUID = rulesRepository.createRuleSet(ruleSet);
-     * 
-     * // Check in rule set version 2
-     * rulesRepository.checkinRuleSet(ruleSetUUID, &quot;Checkin Rule Set Version 2&quot;);
-     * ...
-     * </pre>
-     * 
-     * @param uuid
-     *            Rule set uuid
-     * @param comment
-     *            Checkin comments
+     * @param uuid Rule set uuid
+     * @param comment Checkin comments
      * @throws RuleEngineRepositoryException
      */
-    //@WebMethod
-    //@Oneway
-    //public void checkinRuleSet(@WebParam(name="ruleSetUUID")String ruleSetUUID, @WebParam(name="comment")String comment) throws RuleEngineRepositoryException;
+    @WebMethod
+    public long checkinRuleSet(@WebParam(name="ruleSetUUID")String ruleSetUUID, @WebParam(name="comment")String comment) throws RuleEngineRepositoryException;
 
     /**
-     * <p>
      * Loads a rule set (including all rules) by UUID from the repository.
-     * </p>
-     * Example: Load a rule set by UUID.
      * 
-     * <pre>
-     * // Create rule set
-     * RuleSet ruleSet = RuleSetFactory.getInstance().createRuleSet(&quot;MyNewRuleSet&quot;);
-     * ...
-     * 
-     * // Create rule
-     * Rule rule = RuleFactory.getInstance().createRule(&quot;MyNewRule&quot;);
-     * ...
-     * 
-     * // Create rule set
-     * String ruleSetUUID = rulesRepository.createRuleSet(ruleSet);
-     * 
-     * // Load rule set
-     * RuleSet ruleSet = rulesRepository.loadRuleSet(ruleSetUUID);
-     * ...
-     * </pre>
-     * 
-     * @param uuid
-     *            Rule set uuid
+     * @param uuid Rule set uuid
      * @return A rule set
      * @throws RuleEngineRepositoryException
      */
@@ -235,8 +143,7 @@ public interface RuleRepositoryService {
     /**
      * Loads a rule by uuid.
      * 
-     * @param uuid
-     *            Rule uuid
+     * @param uuid Rule uuid
      * @return A rule
      * @throws RuleEngineRepositoryException Thrown if loading rule fails
      */
@@ -244,19 +151,9 @@ public interface RuleRepositoryService {
     //public RuleDTO fetchRule(@WebParam(name="uuid")String uuid);
     
     /**
-     * <p>
      * Loads a compiled rule set from the repository.
-     * </p>
-     * Example: Load a compiled rule set (e.g. Drools package).
      * 
-     * <pre>
-     * org.drools.rule.Package binPkg = (org.drools.rule.Package)
-     *     rulesRepository.loadCompiledRuleSet(ruleSetUUID);
-     * ...
-     * </pre>
-     * 
-     * @param ruleSetUUID
-     *            Rule set uuid
+     * @param ruleSetUUID Rule set uuid
      * @return A compiled rule set as a byte array (e.g. <code>org.drools.rule.Package</code>)
      * @throws RuleEngineRepositoryException
      */
@@ -264,29 +161,11 @@ public interface RuleRepositoryService {
     public byte[] fetchCompiledRuleSet(@WebParam(name="ruleSetUUID")String ruleSetUUID) throws RuleEngineRepositoryException;
 
     /**
-     * <p>
      * Creates a new rule set snapshot for deployment and stores it in the repository.
-     * </p>
-     * Example: Create and load a rule set snapshot.
      * 
-     * <pre>
-     * // Load rule set
-     * RuleSet ruleSet = repository.loadRuleSet(ruleSetUUID);
-     * 
-     * repository.createRuleSetSnapshot(&quot;MyRuleSet&quot;, &quot;MyRuleSetSnapshot1&quot;,
-     *     &quot;Snapshot Version 1&quot;);
-     * 
-     * org.drools.rule.Package pkg = (org.drools.rule.Package)
-     *     repository.loadCompiledRuleSetSnapshot(&quot;MyRuleSet&quot;, &quot;MyRuleSetSnapshot1&quot;);
-     * ...
-     * </pre>
-     * 
-     * @param ruleSetName
-     *            Rule set name
-     * @param snapshotName
-     *            Snapshot name
-     * @param comment
-     *            Comments for creating the snapshot
+     * @param ruleSetName Rule set name
+     * @param snapshotName Snapshot name
+     * @param comment Comments for creating the snapshot
      * @throws RuleEngineRepositoryException
      */
     @WebMethod
@@ -294,66 +173,70 @@ public interface RuleRepositoryService {
     public void createRuleSetSnapshot(@WebParam(name="ruleSetName")String ruleSetName, @WebParam(name="snapshotName")String snapshotName, @WebParam(name="comment")String comment);
 
     /**
-     * Replaces an existing rule set snapshot and stores it in the repository.
+     * Rebuilds (recompiles) an existing rule set snapshot and stores it in the repository.
      * 
-     * @param ruleSetName
-     *            Rule set name
-     * @param snapshotName
-     *            Snapshot name
-     * @param comment
-     *            Comments for creating the snapshot
+     * @param ruleSetName Rule set name
+     * @param snapshotName Snapshot name
+     * @param comment Comments for creating the snapshot
      * @throws RuleEngineRepositoryException
      */
     @WebMethod
     @Oneway
-    public void replaceRuleSetSnapshot(@WebParam(name="ruleSetName")String ruleSetName, @WebParam(name="snapshotName")String snapshotName, @WebParam(name="comment")String comment);
+    public void rebuildRuleSetSnapshot(@WebParam(name="ruleSetName")String ruleSetName, @WebParam(name="snapshotName")String snapshotName, @WebParam(name="comment")String comment);
 
     /**
-     * <p>
      * Loads a compiled rule set snapshot from the repository.
-     * </p>
-     * Example: Load and execute a compiled rule set snapshot.
      * 
-     * <pre>
-     * // Load rule set
-     * RuleSet ruleSet = rulesRepository.loadRuleSet(ruleSetUUID);
-     * 
-     * rulesRepository.createRuleSetSnapshot(&quot;MyRuleSet&quot;, &quot;MyRuleSetSnapshot1&quot;,
-     *     false, &quot;Snapshot Version 1&quot;);
-     * 
-     * org.drools.rule.Package pkg = (org.drools.rule.Package)
-     *     rulesRepository.loadCompiledRuleSetSnapshot(&quot;MyRuleSet&quot;, &quot;MyRuleSetSnapshot1&quot;);
-     * 
-     * RuleBase rb = RuleBaseFactory.newRuleBase();
-     * rb.addPackage( pkg );
-     * StatelessSession sess = rb.newStatelessSession();
-     * sess.execute( ... );
-     * ...
-     * </pre>
-     * 
-     * @param ruleSetName
-     *            Rule set name
-     * @param snapshotName
-     *            Snapshot name
+     * @param ruleSetName Rule set name
+     * @param snapshotName Snapshot name
      * @return Compiled rule set as a byte array (e.g. <code>org.drools.rule.Package</code>)
      * @throws RuleEngineRepositoryException
      */
     @WebMethod
     public byte[] fetchCompiledRuleSetSnapshot(@WebParam(name="ruleSetName")String ruleSetName, @WebParam(name="snapshotName")String snapshotName) throws RuleEngineRepositoryException;
 
+    /**
+     * Loads a rule set snapshot.
+     * 
+     * @param ruleSetName Rule set name
+     * @param snapshotName Rule set's snapshot name
+     * @return A rule set snapshot
+     */
     @WebMethod
     public RuleSetDTO fetchRuleSetSnapshot(@WebParam(name="ruleSetName")String ruleSetName, @WebParam(name="snapshotName")String snapshotName) throws RuleEngineRepositoryException;
 
+    /**
+     * Creates a new status if it doesn't already exists.
+     * 
+     * @param name Status name
+     * @return New status uuid
+     */
     @WebMethod
     public String createState(@WebParam(name="name")String name);
     
+    /**
+     * Loads all states.
+     * 
+     * @return Array of all states (statuses)
+     */
+    @WebMethod
+    public String[] fetchStates();
+
+    /**
+     * Removes a status from the repository.
+     * 
+     * @param uuid Status name
+     */
     @WebMethod
     @Oneway
     public void removeState(@WebParam(name="name")String name);
     
-    @WebMethod
-    public String[] fetchStates();
-
+    /**
+     * Changes a rule set status by uuid.
+     * 
+     * @param uuid Rule set uuid
+     * @param newState New rule set status
+     */
     @WebMethod
     @Oneway
     public void changeRuleSetState(@WebParam(name="ruleSetUUID")String ruleSetUUID, @WebParam(name="newState")String newState);
