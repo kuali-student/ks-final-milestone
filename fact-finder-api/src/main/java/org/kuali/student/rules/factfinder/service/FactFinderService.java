6f796c789b806d42d42eb12671353bf1608d4cce
kmuthusw <kmuthusw@kuali.org>
2008-10-17 14:38:52 -0400

    Changed to match the new fact service contract
    git-svn-id: https://svn.kuali.org/repos/student/brms/brms-api/branches/brms-api-dev@2028 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- fact-finder-api/src/main/java/org/kuali/student/rules/factfinder/service/FactFinderService.java
+++ fact-finder-api/src/main/java/org/kuali/student/rules/factfinder/service/FactFinderService.java
@@ -7,9 +7,9 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
 import org.kuali.student.poc.common.ws.exceptions.OperationFailedException;
-import org.kuali.student.rules.factfinder.dto.FactDataDTO;
+import org.kuali.student.rules.factfinder.dto.FactResultDTO;
 import org.kuali.student.rules.factfinder.dto.FactStructureDTO;
-import org.kuali.student.rules.factfinder.dto.FactStructureDisplayDTO;
+import org.kuali.student.rules.factfinder.dto.FactTypeInfoDTO;
 
 @WebService(name = "FactFinderService", targetNamespace = "http://student.kuali.org/poc/wsdl/brms/factfinder")
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
@@ -17,31 +17,31 @@ public interface FactFinderService {
 
     /* Setup */
     /**
-     * Retrieves the list of Agenda types
+     * Retrieves the list of Facts Types known by this service.
      * 
      * @return list of Agenda types
      * @throws OperationFailedException
      */
     @WebMethod
-    public FactDataDTO findFact(FactStructureDTO factStructureDTO) throws OperationFailedException;
+    public List<FactTypeInfoDTO> findFactTypes() throws OperationFailedException;
 
     /**
-     * Retrieves the fact structure information associated with a fact
+     * Retrieves information about a type of Fact, listing all the keys required for its computation.
      * 
      * @return list of business rule types
      * @throws OperationFailedException
      */
     @WebMethod
-    public FactStructureDTO fetchFactStructureInfo(String factStructureId) throws OperationFailedException;
+    public FactTypeInfoDTO fetchFactType(String factTypeKey) throws OperationFailedException;
 
     
     /**
      * 
-     * This method returns the list of all the known facts in the system
+     * Retrieves the result of computation of the Fact by the service.
      * 
      * @return
      * @throws OperationFailedException
      */
     @WebMethod
-    public List<FactStructureDisplayDTO> fetchAllFactsDisplayInfo() throws OperationFailedException;    
+    public FactResultDTO fetchFact(String factTypeKey, FactStructureDTO factStructure) throws OperationFailedException;    
 }
