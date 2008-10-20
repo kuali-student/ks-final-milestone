e290ac35499ecbd5a2acbb1b77904ab115310a13
kmuthusw <kmuthusw@kuali.org>
2008-10-20 10:30:33 -0400

    Updated to reflect new fact model
    git-svn-id: https://svn.kuali.org/repos/student/brms/brms-impl/branches/brms-impl-dev@2052 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ fact-finder-impl/src/main/java/org/kuali/student/rules/factfinder/service/impl/FactFinderServiceImpl.java
@@ -0,0 +1,29 @@
+package org.kuali.student.rules.factfinder.service.impl;
+
+import java.util.List;
+
+import org.kuali.student.poc.common.ws.exceptions.OperationFailedException;
+import org.kuali.student.rules.factfinder.dto.FactResultDTO;
+import org.kuali.student.rules.factfinder.dto.FactStructureDTO;
+import org.kuali.student.rules.factfinder.dto.FactTypeInfoDTO;
+import org.kuali.student.rules.factfinder.service.FactFinderService;
+
+public class FactFinderServiceImpl implements FactFinderService {
+
+    @Override
+    public FactResultDTO fetchFact(String factTypeKey, FactStructureDTO factStructure) throws OperationFailedException {
+        // TODO Kamal - THIS METHOD NEEDS JAVADOCS
+        return null;                }
+
+    @Override
+    public FactTypeInfoDTO fetchFactType(String factTypeKey) throws OperationFailedException {
+        // TODO Kamal - THIS METHOD NEEDS JAVADOCS
+        return null;
+    }
+
+    @Override
+    public List<FactTypeInfoDTO> findFactTypes() throws OperationFailedException {
+        // TODO Kamal - THIS METHOD NEEDS JAVADOCS
+        return null;                
+    }
+}
