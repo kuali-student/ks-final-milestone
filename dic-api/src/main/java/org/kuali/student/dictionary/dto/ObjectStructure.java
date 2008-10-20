26c095f93d5ba8c8e1a899903e7917fa3dccf71a
gtaylor <gtaylor@kuali.org>
2008-10-20 10:15:41 -0400

    [NO JIRA] filling out
    git-svn-id: https://svn.kuali.org/repos/student/dictionary/trunk@2049 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- dic-api/src/main/java/org/kuali/student/dictionary/dto/ObjectStructure.java
+++ dic-api/src/main/java/org/kuali/student/dictionary/dto/ObjectStructure.java
@@ -12,20 +12,20 @@ public class ObjectStructure implements Serializable{
     private static final long serialVersionUID = 7664540007997918206L;
     
     @XmlAttribute
-    String type;
+    TypeSelector type;
     @XmlAttribute
     String objectTypeKey;
     
     /**
      * @return the type
      */
-    public String getType() {
+    public TypeSelector getType() {
         return type;
     }
     /**
      * @param type the type to set
      */
-    public void setType(String type) {
+    public void setType(TypeSelector type) {
         this.type = type;
     }
     /**
@@ -39,6 +39,52 @@ public class ObjectStructure implements Serializable{
      */
     public void setObjectTypeKey(String objectTypeKey) {
         this.objectTypeKey = objectTypeKey;
+    }
+    /**
+     * This overridden method ...
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((objectTypeKey == null) ? 0 : objectTypeKey.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+    /**
+     * This overridden method ...
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ObjectStructure other = (ObjectStructure) obj;
+        if (objectTypeKey == null) {
+            if (other.objectTypeKey != null) {
+                return false;
+            }
+        } else if (!objectTypeKey.equals(other.objectTypeKey)) {
+            return false;
+        }
+        if (type == null) {
+            if (other.type != null) {
+                return false;
+            }
+        } else if (!type.equals(other.type)) {
+            return false;
+        }
+        return true;
     }
     
 }
