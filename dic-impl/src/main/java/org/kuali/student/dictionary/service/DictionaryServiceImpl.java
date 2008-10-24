b96cb40cd8ed0e817bb7fd0cccec20a4d41d7226
bsmith <bsmith@kuali.org>
2008-10-23 20:18:52 -0400

    Small changes for testing
    git-svn-id: https://svn.kuali.org/repos/student/dictionary/trunk@2105 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- dic-impl/src/main/java/org/kuali/student/dictionary/service/DictionaryServiceImpl.java
+++ dic-impl/src/main/java/org/kuali/student/dictionary/service/DictionaryServiceImpl.java
@@ -1,5 +1,6 @@
 package org.kuali.student.dictionary.service;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,12 +14,17 @@ import org.kuali.student.dictionary.dto.*;
 @WebService(endpointInterface = "org.kuali.student.dictionary.service.DictionaryService", serviceName = "DictionaryService", portName = "DictionaryService", targetNamespace = "http://org.kuali.student/dictonary")
 public class DictionaryServiceImpl implements DictionaryService {
 
+	private String xmlFile = "/Dictionary.xml";
+	public void setXMLFile(String file){
+		xmlFile = file;
+	}
+	
     public List<EnumeratedValue> getEnumeration(String enumerationKey, String enumContextKey, String contextValue) {
 		List<EnumeratedValue> eVals = new ArrayList<EnumeratedValue>();
 		try {
 			JAXBContext jc = JAXBContext.newInstance("org.kuali.student.dictionary.dto");
 			Unmarshaller u = jc.createUnmarshaller();
-			EnumeratedValues enums = (EnumeratedValues)u.unmarshal(super.getClass().getResource(enumerationKey));
+			EnumeratedValues enums = (EnumeratedValues)u.unmarshal(DictionaryServiceImpl.class.getResource(enumerationKey));
 			for(EnumeratedValue e: enums.getEnumeratedValue()){
 				for(Context c: e.getContexts().getContext()){
 					if(c.getType().equals(enumContextKey) && c.getValue().equals(contextValue)){
@@ -41,8 +47,9 @@ public class DictionaryServiceImpl implements DictionaryService {
             JAXBContext jc = JAXBContext.newInstance("org.kuali.student.dictionary.dto");
 
             Unmarshaller u = jc.createUnmarshaller();
-            Dictionary dict = (Dictionary)u.unmarshal(super.getClass().getResource("/Dictionary.xml"));
-
+            System.out.println(super.getClass().getResource("/Dictionary.xml"));
+            Dictionary dict = (Dictionary)u.unmarshal(DictionaryServiceImpl.class.getResource(xmlFile));
+            
             for(ObjectStructure struc: dict.getObjectStructure()){
                 if(objectTypeKey.equals(struc.getObjectTypeKey())){
                 	theStruct = struc;
@@ -62,14 +69,15 @@ public class DictionaryServiceImpl implements DictionaryService {
             JAXBContext jc = JAXBContext.newInstance("org.kuali.student.dictionary.dto");
 
             Unmarshaller u = jc.createUnmarshaller();
-            Dictionary dict = (Dictionary)u.unmarshal(super.getClass().getResource("/Dictionary.xml"));
-
+           Dictionary dict = (Dictionary)u.unmarshal(DictionaryServiceImpl.class.getResource(xmlFile));
+           // Dictionary dict = (Dictionary)u.unmarshal(new FileInputStream("C:\\Users\\bsmith\\KualiStudent_10-16-08\\dictionary\\dic-api\\src\\main\\resources\\Dictionary.xml"));
+            
             for(ObjectStructure struc: dict.getObjectStructure()){
                 types.add(struc.getObjectTypeKey());
             }
     	}
-    	catch (JAXBException e) {
-            e.printStackTrace();
+    	catch (Exception e) {
+           throw new RuntimeException(e);
         }
         return types;
     }
