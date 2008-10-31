61a2b4a88b69f623b47104544bdebb126b57822a
lcarlsen <lcarlsen@kuali.org>
2008-10-31 16:43:19 -0400

    Added null pointer check
    git-svn-id: https://svn.kuali.org/repos/student/poc/common/branches/common-dev@2171 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ws/src/main/java/org/kuali/student/poc/common/ws/binding/JaxbAttributeMapListAdapter.java
+++ common-ws/src/main/java/org/kuali/student/poc/common/ws/binding/JaxbAttributeMapListAdapter.java
@@ -8,6 +8,7 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 public class JaxbAttributeMapListAdapter extends
 		XmlAdapter<JaxbAttributeList, Map<String, String>> {
 	public Map<String, String> unmarshal(JaxbAttributeList value) {
+		if(value == null) return null;
 		Map<String, String> resultMap = new HashMap<String, String>();
 		for (JaxbAttribute a : value.getAttribute()) {
 			resultMap.put(a.type, a.value);
@@ -16,6 +17,7 @@ public class JaxbAttributeMapListAdapter extends
 	}
 
 	public JaxbAttributeList marshal(Map<String, String> value) {
+		if(value == null) return null;
 		JaxbAttributeList attributes = new JaxbAttributeList();
 		for (Map.Entry<String, String> e : value.entrySet()) {
 			attributes.getAttribute().add(
