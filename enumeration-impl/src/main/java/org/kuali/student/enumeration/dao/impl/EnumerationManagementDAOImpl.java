65af8510c07b946877b79378a1258aeb358f6301
jyin <jyin@kuali.org>
2008-11-05 14:02:58 -0500

    dao
    git-svn-id: https://svn.kuali.org/repos/student/enumeration@2199 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- enumeration-impl/src/main/java/org/kuali/student/enumeration/dao/impl/EnumerationManagementDAOImpl.java
+++ enumeration-impl/src/main/java/org/kuali/student/enumeration/dao/impl/EnumerationManagementDAOImpl.java
@@ -84,27 +84,4 @@ public class EnumerationManagementDAOImpl implements EnumerationManagementDAO {
 
         query.executeUpdate();
     }
-
-    /*
-     * public EnumeratedValue createEnumeratedValue(EnumeratedValue value) { this.entityManager.persist(value); return value; }
-     * public EnumeratedValueContext createEnumeratedValueContext(EnumeratedValueContext context) {
-     * this.entityManager.persist(context); return context; } public Enumerations createEnumerations(Enumerations entry) {
-     * this.entityManager.persist(entry); return entry; } public boolean deleteEnumeratedValue(EnumeratedValue value) {
-     * entityManager.remove(value); return true; } public boolean deleteEnumeratedValueContext(EnumeratedValueContext
-     * context) { entityManager.remove(context); return true; } public boolean deleteEnumerations(Enumerations entry) {
-     * entityManager.remove(entry); return true; } public EnumeratedValue lookupEnumeratedValue(String id) { return
-     * entityManager.find(EnumeratedValue.class, id); } public EnumeratedValueContext lookupEnumeratedValueContext(String id) {
-     * return entityManager.find(EnumeratedValueContext.class, id); } public Enumerations lookupEnumerations(String id) {
-     * return entityManager.find(Enumerations.class, id); } public EnumeratedValue updateEnumeratedValue(EnumeratedValue
-     * value) { this.entityManager.merge(value); return value; } public EnumeratedValueContext
-     * updateEnumeratedValueContext(EnumeratedValueContext context) { this.entityManager.merge(context); return context; }
-     * public Enumerations updateEnumerations(Enumerations entry) { this.entityManager.merge(entry); return entry; } public
-     * List<EnumeratedValueContext> getEnumeratedValueContexts(String enumeratedValueId) { try {
-     * entityManager.createNamedQuery("select e from EnumeratedValueContext " + "e where e.enumeratedValueId =
-     * :enumeratedValueId ").setParameter("enumeratedValueId", enumeratedValueId).getSingleResult(); } catch
-     * (NoResultException e) { return null; } return null; } public List<EnumeratedValue> getEnumeratedValues(String
-     * enumerationId) { try { entityManager.createNamedQuery("select e from EnumeratedValue " + "e where e.enumerationId =
-     * :enumerationId ").setParameter("enumerationId", enumerationId).getSingleResult(); } catch (NoResultException e) {
-     * return null; } return null; }
-     */
 }
