7dcf09827dea7460112a6011bcaa7a2b4c5f43bf
jyin <jyin@kuali.org>
2008-11-05 20:47:58 -0500

    remove all old logic
    git-svn-id: https://svn.kuali.org/repos/student/enumeration@2203 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- enumeration-impl/src/main/java/org/kuali/student/enumeration/dao/EnumerationManagementDAO.java
+++ enumeration-impl/src/main/java/org/kuali/student/enumeration/dao/EnumerationManagementDAO.java
@@ -20,16 +20,5 @@ public interface EnumerationManagementDAO {
     public EnumeratedValue updateEnumeratedValue(String enumerationKey, String code, EnumeratedValue value);
 
     public void removeEnumeratedValue(String enumerationKey, String code);
-    /*
-     * public Enumerations createEnumerations(Enumerations entry); public boolean deleteEnumerations(Enumerations entry);
-     * public Enumerations updateEnumerations(Enumerations entry); public Enumerations lookupEnumerations(String id); public
-     * EnumeratedValue createEnumeratedValue(EnumeratedValue value); // Web service public boolean
-     * deleteEnumeratedValue(EnumeratedValue value); public EnumeratedValue updateEnumeratedValue(EnumeratedValue value);
-     * public List<EnumeratedValue> getEnumeratedValues(String id); // Web service public EnumeratedValueContext
-     * createEnumeratedValueContext(EnumeratedValueContext context); public boolean
-     * deleteEnumeratedValueContext(EnumeratedValueContext context); public EnumeratedValueContext
-     * updateEnumeratedValueContext(EnumeratedValueContext context); public EnumeratedValueContext
-     * lookupEnumeratedValueContext(String id); public List<EnumeratedValueContext> getEnumeratedValueContexts(String
-     * enumeratedValueId);
-     */
+
 }
