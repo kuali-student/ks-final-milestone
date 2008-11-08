172ed6cd117e31f57a0d9391caee011f1d9dc90f
jyin <jyin@kuali.org>
2008-11-07 19:16:57 -0500

    rename pojo
    git-svn-id: https://svn.kuali.org/repos/student/enumeration@2236 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ enumeration-impl/src/main/java/org/kuali/student/enumeration/entity/EnumerationMetaDAO.java
@@ -0,0 +1,56 @@
+package org.kuali.student.enumeration.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+
+import org.kuali.student.poc.common.util.UUIDHelper;
+
+@Entity
+public class EnumerationMetaDAO {
+    @Id
+    String id;
+
+    String enumerationKey;
+    String name;
+    String desc;
+    /**
+     * AutoGenerate the id
+     */
+    @PrePersist
+    public void prePersist() {
+        this.id = UUIDHelper.genStringUUID();
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEnumerationKey() {
+        return enumerationKey;
+    }
+
+    public void setEnumerationKey(String key) {
+        this.enumerationKey = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+}
