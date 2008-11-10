a5d466fed84b788c0a6cf726a009c7d0a0807223
jyin <jyin@kuali.org>
2008-11-10 14:06:25 -0500

    demo
    git-svn-id: https://svn.kuali.org/repos/student/enumeration@2252 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- enumeration-impl/src/test/java/org/kuali/student/enumeration/dao/impl/EnumerationManagementDAOImplTest.java
+++ enumeration-impl/src/test/java/org/kuali/student/enumeration/dao/impl/EnumerationManagementDAOImplTest.java
@@ -9,12 +9,11 @@ import org.junit.runner.RunWith;
 import org.kuali.student.enumeration.entity.EnumerationMetaEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 //TODO remove the comment
-//@PersistenceFileLocation("classpath:META-INF/enumeration-persistence.xml")
+@PersistenceFileLocation("classpath:META-INF/enumeration-persistence.xml")
 public class EnumerationManagementDAOImplTest extends TestCase{
-//    @Dao(value = "org.kuali.student.enumeration.dao.impl.EnumerationManagementDAOImpl", testDataFile = "classpath:test-beans.xml")
+    @Dao(value = "org.kuali.student.enumeration.dao.impl.EnumerationManagementDAOImpl", testDataFile = "classpath:test-beans.xml")
     public EnumerationManagementDAOImpl enumerationManagementDAO;
 
     //TODO delete all records from tables 
