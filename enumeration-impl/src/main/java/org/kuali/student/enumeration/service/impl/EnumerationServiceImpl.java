1b62c147309e91e8fbb14f5da7133e2c113bc6d6
jyin <jyin@kuali.org>
2008-11-10 19:14:08 -0500

    service impl
    git-svn-id: https://svn.kuali.org/repos/student/enumeration@2270 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- enumeration-impl/src/main/java/org/kuali/student/enumeration/service/impl/EnumerationServiceImpl.java
+++ enumeration-impl/src/main/java/org/kuali/student/enumeration/service/impl/EnumerationServiceImpl.java
@@ -6,147 +6,86 @@ import java.util.List;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
-import org.kuali.student.enumeration.dao.impl.EnumerationManagementDAOImpl;
+import org.kuali.student.enumeration.dao.EnumerationManagementDAO;
 import org.kuali.student.enumeration.dto.EnumeratedValue;
 import org.kuali.student.enumeration.dto.EnumeratedValueList;
 import org.kuali.student.enumeration.dto.EnumerationMeta;
 import org.kuali.student.enumeration.dto.EnumerationMetaList;
+import org.kuali.student.enumeration.entity.EnumeratedValueEntity;
+import org.kuali.student.enumeration.entity.EnumerationMetaEntity;
 import org.kuali.student.enumeration.service.EnumerationService;
+import org.kuali.student.enumeration.service.impl.util.POJOConverter;
 
 @WebService(name = "EnumerationService", targetNamespace = "http://student.kuali.org/wsdl/EnumerationService")
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
 public class EnumerationServiceImpl implements EnumerationService{
-    private EnumerationManagementDAOImpl enumDAO;
+    private EnumerationManagementDAO enumDAO;
     
-    public EnumerationManagementDAOImpl getEnumDAO() {
+    public EnumerationManagementDAO getEnumDAO() {
         return enumDAO;
     }
 
-    public void setEnumDAO(EnumerationManagementDAOImpl enumDAO) {
+    public void setEnumDAO(EnumerationManagementDAO enumDAO) {
         this.enumDAO = enumDAO;
     }
     public EnumerationMetaList findEnumerationMetas() {
-        List<org.kuali.student.enumeration.entity.EnumerationMetaEntity> listDAOEntity =  this.enumDAO.findEnumerationMetas();
+        List<EnumerationMetaEntity> listDAOEntity =  this.enumDAO.findEnumerationMetas();
         
         EnumerationMetaList metaList = new EnumerationMetaList();
-        List<org.kuali.student.enumeration.dto.EnumerationMeta> listDTOEntity = null;
-        //POJOConverter.convert(listDAOEntity, listDTOEntity);
+        List<EnumerationMeta> listDTOEntity = null;
+        POJOConverter.map(listDAOEntity, listDTOEntity);
         metaList.setEnumerationMeta(listDTOEntity);
         
         return metaList;
     }    
     
     
-    public EnumeratedValue addEnumeratedValue(String enumerationKey, EnumeratedValue value) {
+    public EnumeratedValue addEnumeratedValue(String enumerationKey, EnumeratedValue valueDTO) {
+        EnumeratedValueEntity valueEntity = null;
+        POJOConverter.map(valueDTO, valueEntity);
+        enumDAO.addEnumeratedValue(enumerationKey, valueEntity);
         return null;
     }
 
     public EnumeratedValueList fetchEnumeration(String enumerationKey, String enumContextKey, String contextValue, Date contextDate) {
-        return null;
+        EnumeratedValueList enumeratedValueListDTO = new EnumeratedValueList();
+        
+        
+        List<EnumeratedValueEntity> enumeratedValueEntityList = enumDAO.fetchEnumeration(enumerationKey, enumContextKey, contextValue, contextDate);
+        
+        List<EnumeratedValue> enumeratedValueList = null; 
+        
+        POJOConverter.mapList(enumeratedValueEntityList, EnumeratedValue.class);
+        
+        enumeratedValueListDTO.setEnumeratedValue(enumeratedValueList);
+        return enumeratedValueListDTO;
     }
 
     public EnumerationMeta fetchEnumerationMeta(String enumerationKey) {
-        return null;
+        
+        EnumerationMetaEntity enumerationMetaEntity = enumDAO.fetchEnumerationMeta(enumerationKey);
+        
+        EnumerationMeta enumerationMeta = new EnumerationMeta();  
+        POJOConverter.map(enumerationMetaEntity, enumerationMeta);
+        return enumerationMeta;
     }
 
 
 
     public boolean removeEnumeratedValue(String enumerationKey, String code) {
-        return false;
+        enumDAO.removeEnumeratedValue(enumerationKey, code);
+        
+        return true;
     }
 
     public EnumeratedValue updateEnumeratedValue(String enumerationKey, String code, EnumeratedValue value) {
-        return null;
-    }
-    
-}
-/*	
-
-
-	@WebMethod
-    public List<Enumerations> getEnumerations(){
-    	
-    	return null;
-    }
-
-    @WebMethod
-    public List<EnumeratedValue> getEnumeration(@WebParam(name = "enumerationKey") String key) throws OperationFailedException{
-        List<org.kuali.student.enumeration.entity.EnumeratedValue> enumValueListDAO = enumDAO.getEnumeratedValues(key);
         
-        List<EnumeratedValue> enumValueList = new ArrayList<EnumeratedValue>();
+        EnumeratedValueEntity enumeratedValueEntity = new EnumeratedValueEntity();
         
-        for(org.kuali.student.enumeration.entity.EnumeratedValue e: enumValueListDAO){
-        	EnumeratedValue newEnumValue = this.convertDAOtoDTOEnumeratedValue(e);
-        	
-			//add the converted EnumeratedValue to the list
-        	enumValueList.add(newEnumValue);
-        }
-        return enumValueList;
-    }
-
-    @WebMethod
-    public EnumeratedValue addEnumeratedValue(@WebParam(name = "enumeratedValue")EnumeratedValue value){
-    	//Convert DTO
-    	org.kuali.student.enumeration.entity.EnumeratedValue newEnumValueDAO = this.convertDTOtoDAOEnumeratedValue(value);
-    	//create EnumeratedValue
-    	org.kuali.student.enumeration.entity.EnumeratedValue returnValue = enumDAO.createEnumeratedValue(newEnumValueDAO);
-    	//convert return value
-    	EnumeratedValue convertedReturn = this.convertDAOtoDTOEnumeratedValue(returnValue);
-    	return convertedReturn;
-    }
-
-    @WebMethod
-    public EnumeratedValue updateEnumeratedValue(@WebParam(name = "enumeratedValueNew")EnumeratedValue newValue){
-    	
-    	//what is the old value used for?  Ignoring oldValue parameter
-    	org.kuali.student.enumeration.entity.EnumeratedValue newEnumValueDAO = this.convertDTOtoDAOEnumeratedValue(newValue);
-    	org.kuali.student.enumeration.entity.EnumeratedValue returnValue = enumDAO.updateEnumeratedValue(newEnumValueDAO);
-    	EnumeratedValue convertedReturn = this.convertDAOtoDTOEnumeratedValue(returnValue);
-    	return convertedReturn;
-    }
-
- //   public Status removeEnumeratedValue(String key);
-
-    private EnumeratedValue convertDAOtoDTOEnumeratedValue(org.kuali.student.enumeration.entity.EnumeratedValue value){
-    	EnumeratedValue newEnumValue = new EnumeratedValue();
-    	newEnumValue.setValue(value.getValue());
-    	newEnumValue.setId(value.getId());
-    	newEnumValue.setCode(value.getCode());
-    	newEnumValue.setAbbrevValue(value.getAbbrevValue());
-    	newEnumValue.setEnumerationId(value.getEnumerationId());
-    	newEnumValue.setSortKey(new BigInteger(Integer.toString(value.getSortKey())));
-    	try {
-    		DatatypeFactory df = DatatypeFactory.newInstance();
-			
-    		//effective date
-			GregorianCalendar gCal = new GregorianCalendar();
-			gCal.setTime(value.getEffectiveDate());
-			XMLGregorianCalendar XMLcal = df.newXMLGregorianCalendar(gCal);
-			newEnumValue.setEffectiveDate(XMLcal);
-			
-			//expiration date
-			gCal.setTime(value.getExpirationDate());
-			XMLcal = df.newXMLGregorianCalendar(gCal);
-			newEnumValue.setExpirationDate(XMLcal);
-			
-		} catch (DatatypeConfigurationException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		return newEnumValue;
-    }
-    
-    private org.kuali.student.enumeration.entity.EnumeratedValue convertDTOtoDAOEnumeratedValue(EnumeratedValue value){
-    	org.kuali.student.enumeration.entity.EnumeratedValue newEnumValueDAO = new org.kuali.student.enumeration.entity.EnumeratedValue();
-    	newEnumValueDAO.setValue(value.getValue());
-    	newEnumValueDAO.setId(value.getId());
-    	newEnumValueDAO.setCode(value.getCode());
-    	newEnumValueDAO.setAbbrevValue(value.getAbbrevValue());
-    	newEnumValueDAO.setEnumerationId(value.getEnumerationId());
-    	newEnumValueDAO.setEffectiveDate(value.getEffectiveDate().toGregorianCalendar().getTime());
-    	newEnumValueDAO.setExpirationDate(value.getExpirationDate().toGregorianCalendar().getTime());
-    	newEnumValueDAO.setSortKey(value.getSortKey().intValue());
-    	return newEnumValueDAO;
+        POJOConverter.map(value, enumeratedValueEntity);
+        enumeratedValueEntity =  enumDAO.updateEnumeratedValue(enumerationKey, code, enumeratedValueEntity);
+        
+        POJOConverter.map(enumeratedValueEntity,value);
+        return value;
     }
 }
-*/
