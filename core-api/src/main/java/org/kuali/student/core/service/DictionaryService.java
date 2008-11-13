d3e23a4711a6872f2850e6cf2859596621293142
depstein <depstein@kuali.org>
2008-11-13 16:56:52 -0500

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/team2/ks-core/branches/ks-core-dev@2342 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core-api/src/main/java/org/kuali/student/core/service/DictionaryService.java
+++ core-api/src/main/java/org/kuali/student/core/service/DictionaryService.java
@@ -13,37 +13,76 @@ import org.kuali.student.poc.common.ws.exceptions.MissingParameterException;
 import org.kuali.student.poc.common.ws.exceptions.OperationFailedException;
 
 public interface DictionaryService {
-    /** 
-     * Retrieves the list of object type identifiers known by this service. Example: cluInfo.
-     * @param None No parameters
-     * @return list of object type identifiers
-     * @throws OperationFailedException unable to complete request
+	/**
+	 * Retrieves the list of object type identifiers known by this service.
+	 * Example: cluInfo.
+	 * 
+	 * @param None
+	 *            No parameters
+	 * @return list of object type identifiers
+	 * @throws OperationFailedException
+	 *             unable to complete request
 	 */
-    public List<String> findObjectTypes() throws OperationFailedException;
+	public List<String> findObjectTypes() throws OperationFailedException;
 
-    /** 
-     * Retrieves the basic dictionary information about a particular object structure. Including all variations based on a certain type and state. Example: Given that a CLU is of type "Course" and in the state of "Proposed," tell which fields are read only, mandatory, not applicable, have enumerations available, etc.
-     * @param objectTypeKey identifier of the object type
-     * @return describes the fields for the input object type
-     * @throws DoesNotExistException specified objectTypeKey not found
-     * @throws InvalidParameterException invalid objectTypeKey
-     * @throws MissingParameterException missing objectTypeKey
-     * @throws OperationFailedException unable to complete request
+	/**
+	 * Retrieves the basic dictionary information about a particular object
+	 * structure. Including all variations based on a certain type and state.
+	 * Example: Given that a CLU is of type "Course" and in the state of
+	 * "Proposed," tell which fields are read only, mandatory, not applicable,
+	 * have enumerations available, etc.
+	 * 
+	 * @param objectTypeKey
+	 *            identifier of the object type
+	 * @return describes the fields for the input object type
+	 * @throws DoesNotExistException
+	 *             specified objectTypeKey not found
+	 * @throws InvalidParameterException
+	 *             invalid objectTypeKey
+	 * @throws MissingParameterException
+	 *             missing objectTypeKey
+	 * @throws OperationFailedException
+	 *             unable to complete request
 	 */
-    public ObjectStructure fetchObjectStructure(@WebParam(name="objectTypeKey")String objectTypeKey) throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException;
+	public ObjectStructure fetchObjectStructure(
+			@WebParam(name = "objectTypeKey")
+			String objectTypeKey) throws DoesNotExistException,
+			InvalidParameterException, MissingParameterException,
+			OperationFailedException;
 
-    /** 
-     * Retrieves the list of enumeration values for a particular enumeration with a certain context for a particular date. The values returned should be those where the supplied date is between the effective and expiration dates. Certain enumerations may not support this functionality.
-     * @param enumerationKey identifier of the enumeration
-     * @param contextType identifier of the enumeration context type
-     * @param contextValue value of the enumeration context
-     * @param contextDate date and time to get the enumeration for
-     * @return list of enumerated codes and values
-     * @throws DoesNotExistException enumerationKey not found
-     * @throws InvalidParameterException invalid enumerationKey, contextType, contextValue, contextDate
-     * @throws MissingParameterException missing enumerationKey, contextType, contextValue, contextDate
-     * @throws OperationFailedException unable to complete request
+	/**
+	 * Retrieves the list of enumeration values for a particular enumeration
+	 * with a certain context for a particular date. The values returned should
+	 * be those where the supplied date is between the effective and expiration
+	 * dates. Certain enumerations may not support this functionality.
+	 * 
+	 * @param enumerationKey
+	 *            identifier of the enumeration
+	 * @param contextType
+	 *            identifier of the enumeration context type
+	 * @param contextValue
+	 *            value of the enumeration context
+	 * @param contextDate
+	 *            date and time to get the enumeration for
+	 * @return list of enumerated codes and values
+	 * @throws DoesNotExistException
+	 *             enumerationKey not found
+	 * @throws InvalidParameterException
+	 *             invalid enumerationKey, contextType, contextValue,
+	 *             contextDate
+	 * @throws MissingParameterException
+	 *             missing enumerationKey, contextType, contextValue,
+	 *             contextDate
+	 * @throws OperationFailedException
+	 *             unable to complete request
 	 */
-    public List<EnumeratedValue> fetchEnumeration(@WebParam(name="enumerationKey")String enumerationKey, @WebParam(name="contextType")String contextType, @WebParam(name="contextValue")String contextValue, @WebParam(name="contextDate")Date contextDate) throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException;
+	public List<EnumeratedValue> fetchEnumeration(
+			@WebParam(name = "enumerationKey")
+			String enumerationKey, @WebParam(name = "contextType")
+			String contextType, @WebParam(name = "contextValue")
+			String contextValue, @WebParam(name = "contextDate")
+			Date contextDate) throws DoesNotExistException,
+			InvalidParameterException, MissingParameterException,
+			OperationFailedException;
 
 }
