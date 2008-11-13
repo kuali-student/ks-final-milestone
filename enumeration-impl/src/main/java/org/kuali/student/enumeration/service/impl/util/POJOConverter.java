e095e6e8483d4c90c47cccc7555fe01a40dd4c51
jyin <jyin@kuali.org>
2008-11-13 13:12:35 -0500

    dozer
    git-svn-id: https://svn.kuali.org/repos/student/enumeration@2310 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- enumeration-impl/src/main/java/org/kuali/student/enumeration/service/impl/util/POJOConverter.java
+++ enumeration-impl/src/main/java/org/kuali/student/enumeration/service/impl/util/POJOConverter.java
@@ -3,27 +3,33 @@ package org.kuali.student.enumeration.service.impl.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.sf.dozer.util.mapping.DozerBeanMapper;
+import net.sf.dozer.util.mapping.MapperIF;
+
 public class POJOConverter {
-    // private static MapperIF mapper;
+    private static MapperIF mapper;
+
     static {
         List<String> mappingFiles = new ArrayList<String>();
         mappingFiles.add("map.xml");
 
-        // mapper = new DozerBeanMapper(mappingFiles);
+        mapper = new DozerBeanMapper(mappingFiles);
 
     }
 
     public static Object map(Object from, Object to) {
-        // mapper.map(from, to);
-        return null;
+        mapper.map(from, to);
+        return to;
     }
 
     static public <E> List<E> mapList(List<?> sourceList, Class<E> destListClass) {
         List<E> result = new ArrayList<E>();
         for (Object e : sourceList) {
-    //        result.add((E) mapper.map(e, destListClass));
+            result.add((E) mapper.map(e, destListClass));
         }
         return result;
     }
-
+    public static void main(String[] a){
+        System.out.println("asdf");
+    }
 }
