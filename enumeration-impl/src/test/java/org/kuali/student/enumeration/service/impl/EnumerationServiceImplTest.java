2bfd08f1f45c5861d0aa99af5bdc0a33a14e60bd
bsmith <bsmith@kuali.org>
2008-11-19 13:02:47 -0500

    initial commit
    git-svn-id: https://svn.kuali.org/repos/student/enumeration@2473 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ enumeration-impl/src/test/java/org/kuali/student/enumeration/service/impl/EnumerationServiceImplTest.java
@@ -0,0 +1,28 @@
+package org.kuali.student.enumeration.service.impl;
+
+import org.junit.Test;
+import org.kuali.student.enumeration.dao.impl.EnumerationManagementDAOImpl;
+import org.kuali.student.enumeration.dto.EnumerationMeta;
+import org.kuali.student.poc.common.test.spring.Dao;
+import org.kuali.student.poc.common.test.spring.PersistenceFileLocation;
+
+import junit.framework.TestCase;
+@PersistenceFileLocation("classpath:META-INF/enumerationmanagement-persistence.xml")
+public class EnumerationServiceImplTest extends TestCase{
+    @Dao(value = "org.kuali.student.enumeration.dao.impl.EnumerationManagementDAOImpl", testDataFile = "classpath:test-beans.xml")
+    public EnumerationManagementDAOImpl enumerationManagementDAO;
+	
+    public EnumerationServiceImpl enumService = new EnumerationServiceImpl();
+    
+	@Test
+	public void testSetEnumDAO(){
+		enumService.setEnumDAO(enumerationManagementDAO);
+	}
+	
+	@Test
+	public void testFindEnumerationMetas(){
+		enumService.setEnumDAO(enumerationManagementDAO);
+		EnumerationMeta meta = enumService.fetchEnumerationMeta("key 1");
+		assertEquals(meta.getDesc(), "desc 1");
+	}
+}
