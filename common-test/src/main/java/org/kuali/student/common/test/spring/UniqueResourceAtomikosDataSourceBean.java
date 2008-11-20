16e951a0b01eb861653aca3a8b58a63707f66ef2
depstein <depstein@kuali.org>
2008-11-20 10:51:41 -0500

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/team2/ks-core/branches/ks-core-dev@2512 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-test/src/main/java/org/kuali/student/common/test/spring/UniqueResourceAtomikosDataSourceBean.java
+++ common-test/src/main/java/org/kuali/student/common/test/spring/UniqueResourceAtomikosDataSourceBean.java
@@ -2,12 +2,14 @@ package org.kuali.student.common.test.spring;
 
 import com.atomikos.jdbc.AtomikosDataSourceBean;
 
+/**
+ * This class generates a (mostly) unique resource name for use in testing when the testing framework
+ * forks tests and two tests using the same dataSource name run into conflicts
+ */
 public class UniqueResourceAtomikosDataSourceBean extends
 		AtomikosDataSourceBean {
 	private static int count = 0;
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	/* (non-Javadoc)
@@ -15,7 +17,6 @@ public class UniqueResourceAtomikosDataSourceBean extends
 	 */
 	@Override
 	public void setUniqueResourceName(String resourceName) {
-		// TODO Auto-generated method stub
 		super.setUniqueResourceName(resourceName+count++);
 	}
 
