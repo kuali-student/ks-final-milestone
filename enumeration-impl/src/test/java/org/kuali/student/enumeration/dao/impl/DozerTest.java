d36c59fc8db8c851d283cc59fcb112c4c2950ff8
bsmith <bsmith@kuali.org>
2008-11-21 12:58:44 -0500

    git-svn-id: https://svn.kuali.org/repos/student/enumeration@2557 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- enumeration-impl/src/test/java/org/kuali/student/enumeration/dao/impl/DozerTest.java
+++ enumeration-impl/src/test/java/org/kuali/student/enumeration/dao/impl/DozerTest.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
+
 import org.junit.Test;
 import org.kuali.student.enumeration.dto.*;
 import org.kuali.student.enumeration.entity.*;
