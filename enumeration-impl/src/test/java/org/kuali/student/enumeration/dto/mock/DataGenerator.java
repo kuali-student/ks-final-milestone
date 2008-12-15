71e167c1d856e3b380123afdc5e8cceaca7efe11
bsmith <bsmith@kuali.org>
2008-12-15 14:28:17 -0500

    git-svn-id: https://svn.kuali.org/repos/student/enumeration@2693 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- enumeration-impl/src/test/java/org/kuali/student/enumeration/dto/mock/DataGenerator.java
+++ enumeration-impl/src/test/java/org/kuali/student/enumeration/dto/mock/DataGenerator.java
@@ -2,20 +2,17 @@ package org.kuali.student.enumeration.dto.mock;
 
 import java.util.List;
 
-import junit.framework.TestCase;
 
-import org.junit.Test;
 import org.kuali.student.enumeration.dto.EnumeratedValue;
 import org.kuali.student.enumeration.service.impl.EnumerationServiceImpl;
 
-public class TestDataGenerator extends TestCase{
+public class DataGenerator{
 	
-	@Test
 	public static void generate(EnumerationServiceImpl enumService){
 		MockEnumeratedValueDTOs dtos = new MockEnumeratedValueDTOs("Semester");
 		List<EnumeratedValue> values = dtos.getEnumeratedValues();
 		for(EnumeratedValue value: values){
-				enumService.addEnumeratedValue("SemesterEnum", value);
+			enumService.addEnumeratedValue("SemesterEnum", value);
 		}
 		
 		dtos = new MockEnumeratedValueDTOs("City");
