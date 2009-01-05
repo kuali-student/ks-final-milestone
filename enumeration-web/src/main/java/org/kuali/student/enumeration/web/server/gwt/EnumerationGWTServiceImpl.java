7c8bf52ad26dfd273e5375d142e1d77fcc1b5480
wiljohns <wiljohns@kuali.org>
2009-01-05 15:03:43 -0500

    replaced contents with the non-gwt service impl
    git-svn-id: https://svn.kuali.org/repos/student/enumeration@2711 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- enumeration-web/src/main/java/org/kuali/student/enumeration/web/server/gwt/EnumerationGWTServiceImpl.java
+++ enumeration-web/src/main/java/org/kuali/student/enumeration/web/server/gwt/EnumerationGWTServiceImpl.java
@@ -5,45 +5,41 @@ import java.util.Date;
 import org.kuali.student.enumeration.dto.EnumeratedValue;
 import org.kuali.student.enumeration.dto.EnumeratedValueList;
 import org.kuali.student.enumeration.dto.EnumerationMetaList;
+import org.kuali.student.enumeration.service.EnumerationService;
 import org.kuali.student.enumeration.web.client.service.EnumerationGWTService;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 public class EnumerationGWTServiceImpl extends RemoteServiceServlet implements EnumerationGWTService {
-    private EnumerationGWTService serviceImpl;// = (EnumerationGWTService) BeanFactory.getInstance().getBean("developersGuiService");
 
-    public EnumerationMetaList fetchEnumerationMetas() {
-        return serviceImpl.fetchEnumerationMetas();
-    }    
-    public EnumeratedValueList fetchEnumeration(String enumerationKey,String enumContextKey,String contextValue,Date   contextDate ){
-        return serviceImpl.fetchEnumeration(enumerationKey, enumContextKey, contextValue, contextDate);
-    }
-    
-    public EnumeratedValue addEnumeratedValue(String enumerationKey,EnumeratedValue value){
-        return serviceImpl.addEnumeratedValue(enumerationKey, value);
-    }
+	 EnumerationService enumerationService; 
+	    public EnumerationMetaList fetchEnumerationMetas() {
+	        
+	        return enumerationService.findEnumerationMetas();
+	    }
+	    public EnumeratedValueList fetchEnumeration(String enumerationKey,String enumContextKey,String contextValue,Date   contextDate ){
+	        return enumerationService.fetchEnumeration(enumerationKey, enumContextKey, contextValue, contextDate);
+	    }
+	    
+	    public EnumeratedValue addEnumeratedValue(String enumerationKey,EnumeratedValue value){
+	     return enumerationService.addEnumeratedValue(enumerationKey, value);
 
-    public EnumeratedValue updateEnumeratedValue(String enumerationKey,String code,EnumeratedValue value){
-        return serviceImpl.updateEnumeratedValue(enumerationKey, code, value);
-    }
+	    }
 
-    public boolean removeEnumeratedValue(String enumerationKey,String code){
-        return serviceImpl.removeEnumeratedValue(enumerationKey, code);
-    }
-    
-    /**
-     * @return the serviceImpl
-     */
-    public EnumerationGWTService getServiceImpl() {
-        return serviceImpl;
-    }
+	    public EnumeratedValue updateEnumeratedValue(String enumerationKey,String code,EnumeratedValue value){
+	        return enumerationService.updateEnumeratedValue(enumerationKey, code, value);
+	    }
 
-    /**
-     * @param serviceImpl
-     *            the serviceImpl to set
-     */
-    public void setServiceImpl(EnumerationGWTService serviceImpl) {
-       this.serviceImpl = serviceImpl;
-    }
+	    public boolean removeEnumeratedValue(String enumerationKey,String code){
+	        return enumerationService.removeEnumeratedValue(enumerationKey, code);
+	    }
 
+	    
+	    
+	    public EnumerationService getEnumerationService() {
+	        return enumerationService;
+	    }
+	    public void setEnumerationService(EnumerationService enumerationService) {
+	        this.enumerationService = enumerationService;
+	    }
 }
