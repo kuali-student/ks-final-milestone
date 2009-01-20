9d3f2293445f8bc19be97ca5dea7b9c58802a6ed
wgomes <wgomes@kuali.org>
2009-01-20 11:21:41 -0500

    Fix package name
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/team2/ks-core/branches/ks-core-dev@2828 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-test/src/main/java/org/kuali/student/common/test/spring/AbstractIntegrationServiceTest.java
+++ common-test/src/main/java/org/kuali/student/common/test/spring/AbstractIntegrationServiceTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.student.poc.common.test.spring;
+package org.kuali.student.common.test.spring;
 
 import org.junit.runner.RunWith;
 
