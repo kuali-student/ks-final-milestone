f08ed0094d268794fc13d96b0510af32e7314693
depstein <depstein@kuali.org>
2009-01-22 16:47:32 -0500

    added org KU test data to dao test
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/team2/ks-core/branches/ks-core-dev@2872 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core-impl/src/test/java/org/kuali/student/core/organization/dao/TestOrganizationDao.java
+++ core-impl/src/test/java/org/kuali/student/core/organization/dao/TestOrganizationDao.java
@@ -1,5 +1,12 @@
 package org.kuali.student.core.organization.dao;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.kuali.student.common.test.spring.AbstractTransactionalDaoTest;
 import org.kuali.student.common.test.spring.Dao;
@@ -7,14 +14,39 @@ import org.kuali.student.common.test.spring.PersistenceFileLocation;
 import org.kuali.student.core.organization.entity.Org;
 import org.kuali.student.core.organization.entity.OrgHierarchy;
 import org.kuali.student.core.organization.entity.OrgType;
+import org.springframework.core.io.ClassPathResource;
 
 @PersistenceFileLocation("classpath:META-INF/organization-persistence.xml")
 public class TestOrganizationDao extends AbstractTransactionalDaoTest {
 	@Dao(value = "org.kuali.student.core.organization.dao.impl.OrganizationDaoImpl")
 	public OrganizationDao dao;
 
+	private boolean preloaded = false;
+	
+	private void loadSql(String sql){
+		em.createNativeQuery(sql).executeUpdate();
+	}
+	
+	@Before
+	public void loadOrgTestData() throws IOException {
+		if(!preloaded){
+			ClassPathResource cpr = new ClassPathResource("ks-org.sql");
+			BufferedReader in
+			   = new BufferedReader(new FileReader(cpr.getFile()));
+			String ln;
+			while((ln=in.readLine())!=null){
+				if(!ln.startsWith("/")&&!ln.isEmpty()){
+					loadSql(ln);
+				}
+			}
+			preloaded=true;
+		}
+	}
+	
 	@Test
-	public void testCreateOrganization() {
+	public void testCreateOrganization(){
+
+		
 		
 		OrgType orgType = new OrgType();
 		orgType.setKey("kuali.org.CorporateEntity");
@@ -35,6 +67,8 @@ public class TestOrganizationDao extends AbstractTransactionalDaoTest {
 		dao.create(orgType);
 		dao.create(org);
 		dao.create(orgHierarchy);
+		
+		assertNotNull(dao.fetch(OrgHierarchy.class, "kuali.org.hierarchy.Curriculum"));
 		
 	}
 }
