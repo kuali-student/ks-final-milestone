382dec5842ba0d0d9f7b39f051d9ca337eaf39f2
ddean <ddean@kuali.org>
2009-01-22 18:52:04 -0500

    init commit
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/team2/ks-core/branches/ks-core-dev@2874 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core-impl/src/main/java/org/kuali/student/core/organization/dao/OrganizationDao.java
+++ core-impl/src/main/java/org/kuali/student/core/organization/dao/OrganizationDao.java
@@ -1,7 +1,14 @@
 package org.kuali.student.core.organization.dao;
 
+import java.util.List;
+
 import org.kuali.student.core.dao.CrudDao;
+import org.kuali.student.core.organization.entity.OrgHierarchy;
+import org.kuali.student.core.organization.entity.OrgPersonRelation;
 
 public interface OrganizationDao extends CrudDao{
 
+    List<OrgPersonRelation> getAllOrgPersonRelationsByPerson(String personId);
+    List<OrgHierarchy> getOrgHierarchies();
+    OrgHierarchy getOrgHierarchy(String orgHierarchyKey);
 }
--- core-impl/src/main/java/org/kuali/student/core/organization/dao/impl/OrganizationDaoImpl.java
+++ core-impl/src/main/java/org/kuali/student/core/organization/dao/impl/OrganizationDaoImpl.java
@@ -1,10 +1,15 @@
 package org.kuali.student.core.organization.dao.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.kuali.student.core.dao.impl.AbstractCrudDaoImpl;
 import org.kuali.student.core.organization.dao.OrganizationDao;
+import org.kuali.student.core.organization.entity.OrgHierarchy;
+import org.kuali.student.core.organization.entity.OrgPersonRelation;
 
 public class OrganizationDaoImpl extends AbstractCrudDaoImpl implements OrganizationDao {
 
@@ -13,4 +18,26 @@ public class OrganizationDaoImpl extends AbstractCrudDaoImpl implements Organiza
 	public void setEm(EntityManager em) {
 		super.setEm(em);
 	}
+
+    @Override
+    public List<OrgPersonRelation> getAllOrgPersonRelationsByPerson(String personId) {
+        Query query = em.createQuery("select opr from OrgPersonRelation opr where personId = :personId");
+        query.setParameter("personId", personId);
+        @SuppressWarnings("unchecked")
+        List<OrgPersonRelation> resultList = query.getResultList();
+        return resultList;
+    }
+
+    @Override
+    public List<OrgHierarchy> getOrgHierarchies() {
+        @SuppressWarnings("unchecked")
+        List<OrgHierarchy> resultList = em.createQuery("select oh from OrgHierarchy oh").getResultList();
+        return resultList;
+    }
+
+    @Override
+    public OrgHierarchy getOrgHierarchy(String orgHierarchyKey) {
+        OrgHierarchy singleResult = (OrgHierarchy)em.createQuery("select oh from OrgHierarchy oh where key = :key").setParameter("key", orgHierarchyKey).getSingleResult();
+        return singleResult;
+    }
 }
