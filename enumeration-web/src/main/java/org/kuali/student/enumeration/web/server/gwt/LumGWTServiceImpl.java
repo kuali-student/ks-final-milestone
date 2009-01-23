6227f0508162653da22d6f9fb4138ec075ba85e3
jyin <jyin@kuali.org>
2009-01-22 19:47:05 -0500

    git-svn-id: https://svn.kuali.org/repos/student/enumeration@2875 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ enumeration-web/src/main/java/org/kuali/student/enumeration/web/server/gwt/LumGWTServiceImpl.java
@@ -0,0 +1,10 @@
+package org.kuali.student.lum.web.server.gwt;
+
+import org.kuali.student.lum.web.client.service.LumGWTService;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class LumGWTServiceImpl extends RemoteServiceServlet implements LumGWTService {
+
+	    
+}
