b672f102faa87c180f094d1ddda3136e5b9dfc76
ddean <ddean@kuali.org>
2009-01-23 09:05:02 -0500

    adding more orgpersonrelation methods
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/team2/ks-core/branches/ks-core-dev@2877 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core-impl/src/main/java/org/kuali/student/core/organization/dao/OrganizationDao.java
+++ core-impl/src/main/java/org/kuali/student/core/organization/dao/OrganizationDao.java
@@ -3,11 +3,11 @@ package org.kuali.student.core.organization.dao;
 import java.util.List;
 
 import org.kuali.student.core.dao.CrudDao;
-import org.kuali.student.core.organization.entity.OrgHierarchy;
 import org.kuali.student.core.organization.entity.OrgPersonRelation;
 
 public interface OrganizationDao extends CrudDao{
 
     public List<OrgPersonRelation> getAllOrgPersonRelationsByPerson(String personId);
-    public List<OrgHierarchy> getOrgHierarchies();
+    public List<OrgPersonRelation> getAllOrgPersonRelationsByOrg(String orgId);
+    public List<String> getPersonIdsForOrgByRelationType(String orgId, String orgPersonRelationTypeKey);
 }
--- core-impl/src/main/java/org/kuali/student/core/organization/dao/impl/OrganizationDaoImpl.java
+++ core-impl/src/main/java/org/kuali/student/core/organization/dao/impl/OrganizationDaoImpl.java
@@ -8,7 +8,6 @@ import javax.persistence.Query;
 
 import org.kuali.student.core.dao.impl.AbstractCrudDaoImpl;
 import org.kuali.student.core.organization.dao.OrganizationDao;
-import org.kuali.student.core.organization.entity.OrgHierarchy;
 import org.kuali.student.core.organization.entity.OrgPersonRelation;
 
 public class OrganizationDaoImpl extends AbstractCrudDaoImpl implements OrganizationDao {
@@ -21,7 +20,7 @@ public class OrganizationDaoImpl extends AbstractCrudDaoImpl implements Organiza
 
     @Override
     public List<OrgPersonRelation> getAllOrgPersonRelationsByPerson(String personId) {
-        Query query = em.createQuery("select opr from OrgPersonRelation opr where personId = :personId");
+        Query query = em.createQuery("select distinct opr from OrgPersonRelation opr where personId = :personId");
         query.setParameter("personId", personId);
         @SuppressWarnings("unchecked")
         List<OrgPersonRelation> resultList = query.getResultList();
@@ -29,9 +28,21 @@ public class OrganizationDaoImpl extends AbstractCrudDaoImpl implements Organiza
     }
 
     @Override
-    public List<OrgHierarchy> getOrgHierarchies() {
+    public List<OrgPersonRelation> getAllOrgPersonRelationsByOrg(String orgId) {
+        Query query = em.createQuery("select distinct opr from OrgPersonRelation opr join opr.org o where o.id = :orgId");
+        query.setParameter("orgId", orgId);
         @SuppressWarnings("unchecked")
-        List<OrgHierarchy> resultList = em.createQuery("select oh from OrgHierarchy oh").getResultList();
+        List<OrgPersonRelation> resultList = query.getResultList();
+        return resultList;
+    }
+
+    @Override
+    public List<String> getPersonIdsForOrgByRelationType(String orgId, String orgPersonRelationTypeKey) {
+        Query query = em.createQuery("select distinct opr.personId from OrgPersonRelation opr join opr.org o join opr.type t where o.id = :orgId and t.key = :orgPersonRelationTypeKey");
+        query.setParameter("orgId", orgId);
+        query.setParameter("orgPersonRelationTypeKey", orgPersonRelationTypeKey);
+        @SuppressWarnings("unchecked")
+        List<String> resultList = query.getResultList();
         return resultList;
     }
 
