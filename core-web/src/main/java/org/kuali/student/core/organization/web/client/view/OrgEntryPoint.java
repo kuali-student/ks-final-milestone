7cfffb51fd23f704df238b1d24f3f8fd1d16781a
depstein <depstein@kuali.org>
2009-01-27 09:58:35 -0500

    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@2917 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core-web/src/main/java/org/kuali/student/core/organization/web/client/view/OrgEntryPoint.java
+++ core-web/src/main/java/org/kuali/student/core/organization/web/client/view/OrgEntryPoint.java
@@ -1,13 +1,40 @@
 package org.kuali.student.core.organization.web.client.view;
 
+import java.util.List;
+
+import org.kuali.student.core.organization.dto.OrgHierarchyInfo;
+import org.kuali.student.core.organization.web.client.service.OrgRpcService;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class OrgEntryPoint implements EntryPoint{
 
+	HorizontalPanel panel;
+	
 	public void onModuleLoad() {
 		RootPanel.get().add(new Label("Hello World!"));
+		panel = new HorizontalPanel();
+		RootPanel.get().add(panel);
+		
+		OrgRpcService.Util.getInstance().getOrgHierarchies(new AsyncCallback<List<OrgHierarchyInfo>>(){
+
+			public void onFailure(Throwable caught) {
+				Window.alert(caught.getMessage());
+			}
+
+			public void onSuccess(List<OrgHierarchyInfo> result) {
+				for(OrgHierarchyInfo orgHierarchyInfo:result){
+					panel.add(new Label(orgHierarchyInfo.getName()));
+				}
+			}
+			
+		});
+		
 	}
 
 }
