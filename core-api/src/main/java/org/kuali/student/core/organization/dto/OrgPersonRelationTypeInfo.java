39c5e5da91058adb3e2632b4d5c4232c47b33b7b
jim7 <jim7@kuali.org>
2009-01-30 05:32:55 -0500

    Fill out the dto info for OrgPersonRelationType
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@2959 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core-api/src/main/java/org/kuali/student/core/organization/dto/OrgPersonRelationTypeInfo.java
+++ core-api/src/main/java/org/kuali/student/core/organization/dto/OrgPersonRelationTypeInfo.java
@@ -4,10 +4,36 @@ import java.io.Serializable;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 
 import org.kuali.student.core.dto.TypeInfo;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class OrgPersonRelationTypeInfo extends TypeInfo implements Serializable {
 	private static final long serialVersionUID = -131212190181253863L;
+	@XmlElement
+	private String revName;
+	@XmlElement
+	private String revDesc;
+	@XmlElement
+	private String orgPositionRestriction;
+	
+	public String getRevName(){
+		return revName;
+	}
+	public void setRevName(String revName){
+		this.revName = revName;
+	}
+	public String getRevDesc(){
+		return revDesc;
+	}
+	public void setRevDesc(String revDesc){
+		this.revDesc = revDesc;
+	}
+	public void setOrgPositionRestriction(String orgPositionRestriction) {
+		this.orgPositionRestriction = orgPositionRestriction;
+	}
+	public String getOrgPositionRestriction() {
+		return orgPositionRestriction;
+	}
 }
