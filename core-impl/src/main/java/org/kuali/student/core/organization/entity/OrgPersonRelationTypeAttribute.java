9301b181023f2e66bb21fe902db2ef24be3ec24b
depstein <depstein@kuali.org>
2009-02-05 11:16:44 -0500

    ID was being duplicated so removed and added attribute override for col name
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3043 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core-impl/src/main/java/org/kuali/student/core/organization/entity/OrgPersonRelationTypeAttribute.java
+++ core-impl/src/main/java/org/kuali/student/core/organization/entity/OrgPersonRelationTypeAttribute.java
@@ -1,8 +1,8 @@
 package org.kuali.student.core.organization.entity;
 
+import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -12,11 +12,8 @@ import org.kuali.student.core.entity.TypeAttributeDef;
 
 @Entity
 @Table(name="KS_ORG_PERS_REL_TYPE_ATTR_T")
+@AttributeOverride(name="id", column=@Column(name="OPRTA_KEY"))
 public class OrgPersonRelationTypeAttribute extends Attribute<OrgPersonRelationType, TypeAttributeDef>{
-
-	@Id
-	@Column(name = "OPRTA_KEY")
-	private String key;
 
 	@ManyToOne
 	@JoinColumn(name = "OWNER")
@@ -32,12 +29,5 @@ public class OrgPersonRelationTypeAttribute extends Attribute<OrgPersonRelationT
 		this.owner = owner;
 	}
 
-	public void setKey(String key) {
-		this.key = key;
-	}
-
-	public String getKey() {
-		return key;
-	}
 
 }
