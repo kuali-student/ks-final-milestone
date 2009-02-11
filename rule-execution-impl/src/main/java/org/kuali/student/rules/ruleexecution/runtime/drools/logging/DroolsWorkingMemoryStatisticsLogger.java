2dd3aa3a1a30eda1fd1f31320384c77ea42a5875
lcarlsen <lcarlsen@kuali.org>
2009-02-11 17:04:20 -0500

    Removed unused import
    git-svn-id: https://svn.kuali.org/repos/student/brms/brms-impl/branches/brms-impl-dev@3113 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- rule-execution-impl/src/main/java/org/kuali/student/rules/ruleexecution/runtime/drools/logging/DroolsWorkingMemoryStatisticsLogger.java
+++ rule-execution-impl/src/main/java/org/kuali/student/rules/ruleexecution/runtime/drools/logging/DroolsWorkingMemoryStatisticsLogger.java
@@ -1,6 +1,5 @@
 package org.kuali.student.rules.ruleexecution.runtime.drools.logging;
 
-import java.util.Date;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
