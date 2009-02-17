f9a5ccee11b14d646df70d3d1f4613592e946fd3
depstein <depstein@kuali.org>
2009-02-17 15:16:21 -0500

    fixed some jaxb errors and added hasattributes
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3183 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core-api/src/main/java/org/kuali/student/core/rules/dto/ReqComponentTypeInfo.java
+++ core-api/src/main/java/org/kuali/student/core/rules/dto/ReqComponentTypeInfo.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 package org.kuali.student.core.rules.dto;
- 
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
@@ -26,92 +26,97 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import org.kuali.student.core.ws.binding.JaxbAttributeMapListAdapter;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ReqComponentTypeInfo implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @XmlElement
-    private String name;
+	@XmlElement
+	private String name;
 
-    @XmlElement
-    private String desc;
+	@XmlElement
+	private String desc;
 
-    @XmlElement
-    private List<ReqCompFieldTypeInfo> reqCompFieldTypes;
+	@XmlElement
+	private List<ReqCompFieldTypeInfo> reqCompFieldTypes;
 
-    @XmlElement
-    private Date effectiveDate;
+	@XmlElement
+	private Date effectiveDate;
 
-    @XmlElement
-    private Date expirationDate;
+	@XmlElement
+	private Date expirationDate;
 
-    @XmlElement
-    private Map<String,String> attributes;
+	@XmlElement
+	@XmlJavaTypeAdapter(JaxbAttributeMapListAdapter.class)
+	private Map<String, String> attributes;
 
-    @XmlAttribute
-    private String key;
+	@XmlAttribute
+	private String key;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getDesc() {
-        return desc;
-    }
+	public String getDesc() {
+		return desc;
+	}
 
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
 
-    public List<ReqCompFieldTypeInfo> getReqCompFieldTypes() {
-        if (reqCompFieldTypes == null) {
-            reqCompFieldTypes = new ArrayList<ReqCompFieldTypeInfo>();
-        }
-        return reqCompFieldTypes;
-    }
+	public List<ReqCompFieldTypeInfo> getReqCompFieldTypes() {
+		if (reqCompFieldTypes == null) {
+			reqCompFieldTypes = new ArrayList<ReqCompFieldTypeInfo>();
+		}
+		return reqCompFieldTypes;
+	}
 
-    public void setReqCompFieldTypes(List<ReqCompFieldTypeInfo> reqCompFieldTypes) {
-        this.reqCompFieldTypes = reqCompFieldTypes;
-    }
+	public void setReqCompFieldTypes(
+			List<ReqCompFieldTypeInfo> reqCompFieldTypes) {
+		this.reqCompFieldTypes = reqCompFieldTypes;
+	}
 
-    public Date getEffectiveDate() {
-        return effectiveDate;
-    }
+	public Date getEffectiveDate() {
+		return effectiveDate;
+	}
 
-    public void setEffectiveDate(Date effectiveDate) {
-        this.effectiveDate = effectiveDate;
-    }
+	public void setEffectiveDate(Date effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
 
-    public Date getExpirationDate() {
-        return expirationDate;
-    }
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
 
-    public void setExpirationDate(Date expirationDate) {
-        this.expirationDate = expirationDate;
-    }
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
 
-    public Map<String,String> getAttributes() {
-        if (attributes == null) {
-            attributes = new HashMap<String,String>();
-        }
-        return attributes;
-    }
+	public Map<String, String> getAttributes() {
+		if (attributes == null) {
+			attributes = new HashMap<String, String>();
+		}
+		return attributes;
+	}
 
-    public void setAttributes(Map<String,String> attributes) {
-        this.attributes = attributes;
-    }
+	public void setAttributes(Map<String, String> attributes) {
+		this.attributes = attributes;
+	}
 
-    public String getKey() {
-        return key;
-    }
+	public String getKey() {
+		return key;
+	}
 
-    public void setKey(String key) {
-        this.key = key;
-    }
+	public void setKey(String key) {
+		this.key = key;
+	}
 }
