9d364dffa973c40a7460c1cf9bfad1abbd1af9e2
bsmith <bsmith@kuali.org>
2009-02-18 14:36:02 -0500

    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3205 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/menus/KSAccordionMenu.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/menus/KSAccordionMenu.java
@@ -1,28 +1,45 @@
 package org.kuali.student.common.ui.client.widgets.menus;
 
-import org.kuali.student.common.ui.client.widgets.KSStackPanel;
-
-import com.google.gwt.user.client.ui.SimplePanel;
+import org.kuali.student.common.ui.client.widgets.KSAccordionPanel;
+import org.kuali.student.common.ui.client.widgets.KSLabel;
 
 public class KSAccordionMenu extends KSMenu{
 	
-	KSStackPanel menu = new KSStackPanel();
+	private KSAccordionPanel menu = new KSAccordionPanel();
+	private String indent = "";
 	
 	@Override
 	protected void populateMenu() {
 		this.initWidget(menu);
-		menu.clear();
 		for(MenuItem i: items){
-			if(i.getSubItems().isEmpty()){
-				menu.add(new SimplePanel(), i.getLabel());
+			KSLabel categoryLabel = new KSLabel(indent + i.getLabel());
+/*			if(labelStyle != null){
+				categoryLabel.addStyleName(labelStyle);
+			}*/
+			if(i.getClickHandler() != null){
+				categoryLabel.addClickHandler(i.getClickHandler());
+			}
+			
+			if(i.getSubItems().isEmpty()){		
+				menu.addPanel(categoryLabel);
 			}
 			else{
 				KSAccordionMenu subMenu = new KSAccordionMenu();
+				//subMenu.setLabelStyle("KS-Accordion-SubMenu-Indent");
+				subMenu.setIndent(indent + "      ");
 				subMenu.setItems(i.getSubItems());
-				menu.add(subMenu, i.getLabel());
+				menu.addPanel(categoryLabel, subMenu);
 			}
 		}
 		
+	}
+	
+	public KSAccordionPanel getMenu(){
+		return menu;
+	}
+	
+	public void setIndent(String indent){
+		this.indent = indent;
 	}
 	
 }
