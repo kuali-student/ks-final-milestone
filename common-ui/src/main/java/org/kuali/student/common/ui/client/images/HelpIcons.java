3406f98ecdcc967a24f2113510671dbc88220159
gstruthe <gstruthe@kuali.org>
2009-02-18 15:27:43 -0500

    Help icons
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3213 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/images/HelpIcons.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/images/HelpIcons.java
@@ -2,19 +2,20 @@ package org.kuali.student.common.ui.client.images;
 
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import com.google.gwt.user.client.ui.ImageBundle;
+import com.google.gwt.user.client.ui.ImageBundle.Resource;
 
 public interface HelpIcons extends ImageBundle {
 
-    // TODO set the resource value to an image of a question mark
-    @Resource("the/path/to/help-default.gif")
+    // set the resource value to an image of a question mark
+    @Resource("help-16x16.png")
     AbstractImagePrototype defaultIcon();
     
-    // TODO set the resource value to an image of a green checkmark
-    @Resource("the/path/to/help-ok.gif")
+    // set the resource value to an image of a green checkmark
+    @Resource("agt_action_success.png")
     AbstractImagePrototype okIcon();
     
-    // TODO set the resource value to an image of a yellow triangle with exclamation point
-    @Resource("the/path/to/help-error.gif")
+    // set the resource value to an image of a yellow triangle with exclamation point
+    @Resource("messagebox_warning.png")
     AbstractImagePrototype errorIcon();
     
 }
