814d5e83eff333f71583d14534b4f41955b3879d
bsmith <bsmith@kuali.org>
2009-02-19 18:15:36 -0500

    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3259 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSButton.java
@@ -0,0 +1,13 @@
+package org.kuali.student.common.ui.client.widgets;
+
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+
+public abstract class KSButton extends Composite{
+
+	public abstract void init(String html);
+	public abstract void init(String html, ClickHandler handler);
+
+}
