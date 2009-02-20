797cf5e0e9a5830efa5175ada2221a087509a44e
depstein <depstein@kuali.org>
2009-02-20 10:54:55 -0500

    ''
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@3267 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lum-impl/src/main/java/org/kuali/student/lum/lu/entity/CluSet.java
+++ lum-impl/src/main/java/org/kuali/student/lum/lu/entity/CluSet.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,43 +12,50 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.kuali.student.core.dto.RichText;
+import org.kuali.student.core.entity.AttributeOwner;
+import org.kuali.student.core.entity.MetaEntity;
 
 @Entity
 @Table(name = "KS_CLU_SET_T")
-public class CluSet {
+public class CluSet extends MetaEntity implements
+		AttributeOwner<CluSetAttribute> {
 	@Id
 	@Column(name = "ID")
 	private String id;
 
-    @Column(name = "CLU_SET_NAME")
-    private String name;
+	@Column(name = "CLU_SET_NAME")
+	private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "DESCRIPTION")
-    private RichText desc;
-    
+	@ManyToOne
+	@JoinColumn(name = "DESCRIPTION")
+	private RichText desc;
+
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "EFFECTIVE_DT")
-    private Date effectiveDate;
+	private Date effectiveDate;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "EXPIRATION_DT")
 	private Date expirationDate;
 
-    //private CluCriteria cluCriteria;//TODO Criteria
+	// private CluCriteria cluCriteria;//TODO Criteria
 
-    @ManyToMany
-	@JoinTable(name="KS_CLU_SET_JOIN_T",joinColumns=@JoinColumn(name="CLU_SET_PARENT_ID"),inverseJoinColumns=@JoinColumn(name="CLU_SET_CHILD_ID"))
+	@ManyToMany
+	@JoinTable(name = "KS_CLU_SET_JOIN_T", joinColumns = @JoinColumn(name = "CLU_SET_PARENT_ID"), inverseJoinColumns = @JoinColumn(name = "CLU_SET_CHILD_ID"))
 	private List<CluSet> cluSets;
-	
+
 	@ManyToMany
 	@JoinTable(name = "KS_CLU_SET_CLU_T", joinColumns = @JoinColumn(name = "CLU_SET_ID"), inverseJoinColumns = @JoinColumn(name = "CLU_ID"))
 	private List<Clu> clus;
+	
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+	private List<CluSetAttribute> attributes;
 
 	public String getId() {
 		return id;
@@ -98,14 +106,25 @@ public class CluSet {
 	}
 
 	public List<CluSet> getCluSets() {
-		if(cluSets==null){
-			cluSets=new ArrayList<CluSet>();
+		if (cluSets == null) {
+			cluSets = new ArrayList<CluSet>();
 		}
 		return cluSets;
 	}
 
 	public void setCluSets(List<CluSet> cluSets) {
 		this.cluSets = cluSets;
+	}
+
+	public List<CluSetAttribute> getAttributes() {
+		if (attributes == null) {
+			attributes = new ArrayList<CluSetAttribute>();
+		}
+		return attributes;
+	}
+
+	public void setAttributes(List<CluSetAttribute> attributes) {
+		this.attributes = attributes;
 	}
 
 }
---
+++ lum-impl/src/main/java/org/kuali/student/lum/lu/entity/CluSetAttribute.java
@@ -0,0 +1,28 @@
+package org.kuali.student.lum.lu.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.kuali.student.core.entity.Attribute;
+
+@Entity
+@Table(name = "KS_CLU_SET_ATTR_T")
+public class CluSetAttribute extends Attribute<CluSet> {
+
+	@ManyToOne
+	@JoinColumn(name = "OWNER")
+	private CluSet owner;
+
+	@Override
+	public CluSet getOwner() {
+		return owner;
+	}
+
+	@Override
+	public void setOwner(CluSet owner) {
+		this.owner = owner;
+	}
+
+}
