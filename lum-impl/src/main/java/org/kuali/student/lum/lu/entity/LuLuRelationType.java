b70190d0002ef801e0dcf55844dbc55fadf2358a
depstein <depstein@kuali.org>
2009-02-20 15:35:35 -0500

    ''
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@3287 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lum-impl/src/main/java/org/kuali/student/lum/lu/entity/LuLuRelationType.java
+++ lum-impl/src/main/java/org/kuali/student/lum/lu/entity/LuLuRelationType.java
@@ -1,33 +1,55 @@
 package org.kuali.student.lum.lu.entity;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "KS_LU_REL_TYPE_T")
 public class LuLuRelationType {
 	@Id
 	@Column(name = "ID")
-	private String luLuRelationTypeKey;
+	private String id;
 
 	@ManyToMany
 	@JoinTable(name = "KS_LU_REL_TYPE_LU_TYPE_T", joinColumns = @JoinColumn(name = "LU_REL_TYPE_ID", referencedColumnName = "ID"), inverseJoinColumns = @JoinColumn(name = "LU_TYPE_ID", referencedColumnName = "ID"))
 	private List<LuType> luTypes;
 
-	public String getLuLuRelationTypeKey() {
-		return luLuRelationTypeKey;
-	}
+	@Column(name = "LU_REL_NAME")
+	private String name;
 
-	public void setLuLuRelationTypeKey(String luLuRelationTypeKey) {
-		this.luLuRelationTypeKey = luLuRelationTypeKey;
-	}
+	@Column(name = "DESCRIPTION")
+	private String desc;
+
+	@Column(name = "REV_NAME")
+	private String revName;
+
+	@Column(name = "REV_DESC")
+	private String revDesc;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "EFFECTIVE_DT")
+	private Date effectiveDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "EXPIRATION_DT")
+	private Date expirationDate;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "OWNER")
+	private List<LuLuRelationTypeAttribute> attributes;
 
 	public List<LuType> getLuTypes() {
 		return luTypes;
@@ -35,5 +57,72 @@ public class LuLuRelationType {
 
 	public void setLuTypes(List<LuType> luTypes) {
 		this.luTypes = luTypes;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getRevName() {
+		return revName;
+	}
+
+	public void setRevName(String revName) {
+		this.revName = revName;
+	}
+
+	public String getRevDesc() {
+		return revDesc;
+	}
+
+	public void setRevDesc(String revDesc) {
+		this.revDesc = revDesc;
+	}
+
+	public Date getEffectiveDate() {
+		return effectiveDate;
+	}
+
+	public void setEffectiveDate(Date effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+
+	public List<LuLuRelationTypeAttribute> getAttributes() {
+		if (attributes == null) {
+			attributes = new ArrayList<LuLuRelationTypeAttribute>();
+		}
+		return attributes;
+	}
+
+	public void setAttributes(List<LuLuRelationTypeAttribute> attributes) {
+		this.attributes = attributes;
 	}
 }
---
+++ lum-impl/src/main/java/org/kuali/student/lum/lu/entity/LuLuRelationTypeAttribute.java
@@ -0,0 +1,12 @@
+package org.kuali.student.lum.lu.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.kuali.student.core.entity.Attribute;
+
+@Entity
+@Table(name = "KS_LU_REL_TYPE_ATTR_T")
+public class LuLuRelationTypeAttribute extends Attribute<LuLuRelationType> {
+
+}
