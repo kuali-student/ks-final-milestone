91519cb3537660ea8c096856c24bc79d15bea104
rdiaz <rdiaz@kuali.org>
2009-02-20 16:24:09 -0500

    new adds
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@3295 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ lum-impl/src/main/java/org/kuali/student/lum/lu/entity/LrTypeAttribute.java
@@ -0,0 +1,11 @@
+package org.kuali.student.lum.lu.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.kuali.student.core.entity.Attribute;
+@Entity
+@Table(name="KS_R_TYPE_ATTR_T")
+public class LrTypeAttribute extends Attribute<LrType> {
+
+}
