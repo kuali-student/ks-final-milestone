ba4a67e3f93d2e3a61307afcde1c7a1e81d433df
depstein <depstein@kuali.org>
2009-02-20 15:30:46 -0500

    ''
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@3286 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ lum-impl/src/main/java/org/kuali/student/lum/lu/entity/LuiLuiRelationAttribute.java
@@ -0,0 +1,11 @@
+package org.kuali.student.lum.lu.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.kuali.student.core.entity.Attribute;
+@Entity
+@Table(name = "KS_LUI_REL_ATTR_T")
+public class LuiLuiRelationAttribute extends Attribute<LuiLuiRelation> {
+
+}
