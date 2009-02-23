69225c28cfa2ffe7f1e51b16186b53093c41d80c
wgomes <wgomes@kuali.org>
2009-02-23 14:33:10 -0500

    Add form layout styles
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3313 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSStyles.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSStyles.java
@@ -71,5 +71,8 @@ public interface KSStyles {
 
 	public static final String KS_COLLAPSABLEFLOATPANEL = "KS-CollapsableFloatPanel-ButtonPanel";
 	
+	public static final String KS_FORMLAYOUT_DESC = "KS-FormLayout-Description";
+	public static final String KS_FORMLAYOUT_FIELD = "KS-FormLayout-Field";
+	public static final String KS_FORMLAYOUT_LABEL = "KS-FormLayout-Label";
 	
 }
