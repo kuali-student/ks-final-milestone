34c4eb9e1ca0fc3deacbd8428a85b5f6922ce6a8
depstein <depstein@kuali.org>
2009-02-24 11:01:22 -0500

    added lists to criteriaset
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3326 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core-api/src/main/java/org/kuali/student/core/search/dto/CriteriaSet.java
+++ core-api/src/main/java/org/kuali/student/core/search/dto/CriteriaSet.java
@@ -1,6 +1,8 @@
 package org.kuali.student.core.search.dto;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -11,27 +13,39 @@ import javax.xml.bind.annotation.XmlElement;
 public class CriteriaSet implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@XmlElement
-	private Criterion criterion; 
+	private List<Criterion> criterion;
 	@XmlElement
-	private CriteriaSet criteria; 
+	private List<CriteriaSet> criteria;
 	@XmlAttribute
-	private String operator; 
-	public Criterion getCriterion(){
-		return criterion;
-	}
-	public void setCriterion(Criterion criterion){
-		this.criterion = criterion;
-	}
-	public CriteriaSet getCriteria(){
-		return criteria;
-	}
-	public void setCriteria(CriteriaSet criteria){
-		this.criteria = criteria;
-	}
-	public String getOperator(){
+	private String operator;
+
+	public String getOperator() {
 		return operator;
 	}
-	public void setOperator(String operator){
+
+	public void setOperator(String operator) {
 		this.operator = operator;
+	}
+
+	public List<Criterion> getCriterion() {
+		if (criterion == null) {
+			criterion = new ArrayList<Criterion>();
+		}
+		return criterion;
+	}
+
+	public void setCriterion(List<Criterion> criterion) {
+		this.criterion = criterion;
+	}
+
+	public List<CriteriaSet> getCriteria() {
+		if (criteria == null) {
+			criteria = new ArrayList<CriteriaSet>();
+		}
+		return criteria;
+	}
+
+	public void setCriteria(List<CriteriaSet> criteria) {
+		this.criteria = criteria;
 	}
 }
