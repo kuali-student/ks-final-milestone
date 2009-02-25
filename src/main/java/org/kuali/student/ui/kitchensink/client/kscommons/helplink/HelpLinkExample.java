f3e8cb8c60e40b613a2d6b908ed00fb61db4511a
hjohnson <hjohnson@kuali.org>
2009-02-25 16:04:58 -0500

    Add HelpLink classes
    git-svn-id: https://svn.kuali.org/repos/student/examples/ui/ks-ui-kitchensink/branches/ks-ui-kitchensink-dev@3369 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/helplink/HelpLinkExample.java
@@ -0,0 +1,46 @@
+package org.kuali.student.ui.kitchensink.client.kscommons.helplink;
+
+import static org.kuali.student.ui.kitchensink.client.KitchenSinkStyleConstants.STYLE_EXAMPLE;
+
+import org.kuali.student.common.ui.client.dto.HelpInfo;
+import org.kuali.student.common.ui.client.widgets.KSHelpLink;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.SimplePanel;
+
+public class HelpLinkExample extends Composite {
+
+    final SimplePanel main = new SimplePanel();
+    final FlexTable table = new FlexTable();
+    private HelpInfo helpInfo;
+
+    final KSHelpLink helpLink =  GWT.create(KSHelpLink.class);
+
+
+    public HelpLinkExample() {
+
+        main.addStyleName(STYLE_EXAMPLE);
+
+        helpInfo = buildTestHelpInfo();
+        helpLink.setHelpInfo(helpInfo);
+           Window.alert("Title" +   helpLink.getHelpInfo().getTitle()
+        + "\n" + helpLink.getHelpInfo().getUrl());     
+                
+                main.add(helpLink);
+
+        super.initWidget(main);
+    }
+
+    private HelpInfo buildTestHelpInfo(){
+        HelpInfo testInfo = new HelpInfo();
+        testInfo.setId("123456");
+        testInfo.setTitle("Help Title");
+        testInfo.setShortVersion("HELP TEXT HELP TEXT HELP TEXT HELP TEXT");
+        testInfo.setUrl("http://www.kuali.org");
+        return testInfo;
+    }
+
+}
---
+++ src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/helplink/HelpLinkExampleDescriptor.java
@@ -0,0 +1,24 @@
+package org.kuali.student.ui.kitchensink.client.kscommons.helplink;
+
+import org.kuali.student.ui.kitchensink.client.KitchenSinkExample;
+
+import com.google.gwt.user.client.ui.Widget;
+
+public class HelpLinkExampleDescriptor extends KitchenSinkExample {
+    public HelpLinkExampleDescriptor() {
+        super();
+        super.addResource("java", "HelpLinkExample.java", "kscommons/helplink/HelpLinkExample.java", "Example usage of KSHelpLink.");
+    }
+    public String getDescription() {       
+        return "This will add a customizable link for detailed help information. Currently the help text is a hard coded example but will eventaully be an RPC call  ";
+    }
+
+    public Widget getExampleWidget() {
+        return new HelpLinkExample();
+    }
+ 
+    public String getTitle() {
+        return "Help Link";
+    }
+
+}
