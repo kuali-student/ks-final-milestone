73afa186ca6e442441fad6182ab0bb5e38832ea1
gstruthe <gstruthe@kuali.org>
2009-02-25 18:54:42 -0500

    KSDropDown Will G's way
    git-svn-id: https://svn.kuali.org/repos/student/examples/ui/ks-ui-kitchensink/branches/ks-ui-kitchensink-dev@3376 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/dropdown/DropDownExample.java
+++ src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/dropdown/DropDownExample.java
@@ -16,7 +16,7 @@ public class DropDownExample extends Composite {
 
     final HorizontalPanel main = new HorizontalPanel();
      
-    final KSDropDown dropDown = KSWidgetFactory.getDropDownInstance(false);
+    final KSDropDown dropDown = new KSDropDown(false);
 
     public DropDownExample() {
         
