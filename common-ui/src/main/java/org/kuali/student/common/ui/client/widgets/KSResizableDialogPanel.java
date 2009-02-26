9e9133173463a7cd7a9b98bc608362385f6ca09e
bsmith <bsmith@kuali.org>
2009-02-26 18:07:32 -0500

    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3427 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSResizableDialogPanel.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSResizableDialogPanel.java
@@ -1,5 +1,26 @@
 package org.kuali.student.common.ui.client.widgets;
 
-public class KSResizableDialogPanel extends KSDialogPanel{
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
 
+public class KSResizableDialogPanel extends KSPopupPanel{
+	 private final SimplePanel content = new SimplePanel();
+	 
+	 private final KSResizablePanel resizePanel = new KSResizablePanel();
+	 
+	 public KSResizableDialogPanel(){
+			
+	        resizePanel.add(content);
+	        super.setWidget(resizePanel);
+
+			setupDefaultStyle();
+	}
+	 
+    public void setWidget(Widget w) {
+        content.setWidget(w);
+    }
+	
+	private void setupDefaultStyle(){
+		
+	}
 }
