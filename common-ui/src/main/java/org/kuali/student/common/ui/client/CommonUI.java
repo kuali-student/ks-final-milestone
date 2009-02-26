5ec744b75710cc56ce559ea0f1b83d449d7f3f58
jyin <jyin@kuali.org>
2009-02-26 17:15:48 -0500

    main entry
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3419 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/CommonUI.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/CommonUI.java
@@ -18,53 +18,7 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class CommonUI implements EntryPoint {
 
 	public void onModuleLoad() {
-        KSResizablePanel r = new KSResizablePanel();
-        final RichTextArea ll = new RichTextArea();
-        ll.setPixelSize(200,200);
-        ll.setHTML("asdf");
-        
-        r.setWidget(ll);
-        RootPanel.get().add(r);
-        
-        final FlowPanel p = new FlowPanel(); 
-        p.add(new Label("1"));
-        p.add(new Label("2"));
-        p.add(new Label("3"));
-        p.setPixelSize(200,200);
-        
-        final KSResizablePanel resizePop = new KSResizablePanel();
-        final PopupPanel pop = new PopupPanel(true);
-        final RichTextArea l = new RichTextArea();
-        l.setPixelSize(200,200);
-        l.setHTML("asdf");
-        //pop.addStyleName(KSStyles.KS_POPUP);
-        
-        Button bn = new Button("Click to show a popup panel");
-        
-        RootPanel.get().add(bn);
-        bn.addClickHandler(new ClickHandler(){
-            public void onClick(ClickEvent event) {
-                DeferredCommand.addCommand(new Command() {
-                    public void execute() {
-                        resizePop.setWidget(p);
 
-                        pop.add(resizePop);                
-                        pop.setPixelSize(l.getOffsetWidth(), l.getOffsetHeight());
-                    }
-                });
-               
-                pop.setPopupPositionAndShow(new PopupPanel.PositionCallback() {
-                    public void setPosition(int offsetWidth, int offsetHeight) {
-                        
-                      int left = (Window.getClientWidth() - offsetWidth) / 3;
-                      int top = (Window.getClientHeight() - offsetHeight) / 3;
-                      pop.setPopupPosition(left, top);
-                    }
-                  });
-
-            }
-            
-        });
 	
 	}
 	
