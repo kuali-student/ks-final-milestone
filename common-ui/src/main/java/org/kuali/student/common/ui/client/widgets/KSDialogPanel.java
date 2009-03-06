7fc206b5051b712ac318aeea9b2159c207fa3c3b
gstruthe <gstruthe@kuali.org>
2009-03-05 20:39:30 -0500

    KSDialogPanel Will G's way
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3604 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSDialogPanel.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSDialogPanel.java
@@ -1,5 +1,7 @@
 package org.kuali.student.common.ui.client.widgets;
 
+import org.kuali.student.common.ui.client.widgets.impl.KSDialogPanelImpl;
+
 import com.google.gwt.event.dom.client.MouseDownEvent;
 import com.google.gwt.event.dom.client.MouseDownHandler;
 import com.google.gwt.event.dom.client.MouseMoveEvent;
@@ -10,6 +12,7 @@ import com.google.gwt.event.dom.client.MouseOverEvent;
 import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.event.dom.client.MouseUpEvent;
 import com.google.gwt.event.dom.client.MouseUpHandler;
+import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -18,136 +21,105 @@ import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
-public class KSDialogPanel extends KSPopupPanel{
-    private final KSResizablePanel resizableContent = new KSResizablePanel();
-    //private final SimplePanel content = new SimplePanel();
-    private boolean resizable = true;
-    private Label dialogTitleLabel = new Label();
-    private final SimplePanel content = new SimplePanel();	
-    private final VerticalPanel dialogContainer = new VerticalPanel();
-	private final HorizontalPanel headerPanel = new HorizontalPanel();
-	private final FocusPanel focusPanel = new FocusPanel();
-	private boolean loaded = false;
-	private Widget w;
+public class KSDialogPanel extends KSPopupPanel{ 
+    private final KSDialogPanelAbstract dialogPanel = new KSDialogPanelImpl();
+
 	
 	public KSDialogPanel(){
-		
-        dialogContainer.add(resizableContent);
-        super.setWidget(dialogContainer);
+	}
 
-		setupDefaultStyle();
-        SimpleWindowMover resizeHandler = new SimpleWindowMover();
-        focusPanel.addMouseDownHandler(resizeHandler);
-        focusPanel.addMouseMoveHandler(resizeHandler);
-        focusPanel.addMouseUpHandler(resizeHandler);
-	}
-	
-	
-	public void setHeader(String headerText){
-		headerPanel.setHorizontalAlignment(HorizontalPanel.ALIGN_CENTER);
-		dialogTitleLabel = new Label(headerText, false);
-		focusPanel.add(dialogTitleLabel);
-		headerPanel.add(focusPanel);
-		dialogContainer.insert(headerPanel, 0);
-	}
-	
-	public boolean removeHeader(){
-		return dialogContainer.remove(headerPanel);
-	}
-	
-	private void setupDefaultStyle(){
-		headerPanel.addStyleName(KSStyles.KS_POPUP_HEADER);
-		
-	}
-	
+
+    public void addCloseHandler(CloseHandler handler) {
+        dialogPanel.addCloseHandler(handler);
+    }
+
+
+    public void addStyleName(String style) {
+        dialogPanel.addStyleName(style);
+    }
+
+
+    public void center() {
+        dialogPanel.center();
+    }
+
+
+    public boolean equals(Object obj) {
+        return dialogPanel.equals(obj);
+    }
+
+
+    public int getX() {
+        return dialogPanel.getX();
+    }
+
+
+    public int getY() {
+        return dialogPanel.getY();
+    }
+
+
+    public int hashCode() {
+        return dialogPanel.hashCode();
+    }
+
+
+    public void hide() {
+        dialogPanel.hide();
+    }
+
+
+    public boolean isShowing() {
+        return dialogPanel.isShowing();
+    }
+
+
+    public boolean removeHeader() {
+        return dialogPanel.removeHeader();
+    }
+
+
+    public void setAutoHide(boolean autoHide) {
+        dialogPanel.setAutoHide(autoHide);
+    }
+
+
+    public void setHeader(String headerText) {
+        dialogPanel.setHeader(headerText);
+    }
+
+
+    public void setLocation(int x, int y) {
+        dialogPanel.setLocation(x, y);
+    }
+
+
+    public void setModal(boolean modal) {
+        dialogPanel.setModal(modal);
+    }
+
+
+    public void setPixelSize(int w, int h) {
+        dialogPanel.setPixelSize(w, h);
+    }
+
+ 
+    public void setResizable(boolean resizable) {
+        dialogPanel.setResizable(resizable);
+    }
+
+
     public void setWidget(Widget w) {
-    	this.w = w;
-    	loaded = false;
-    }
-    
-    public void show(){
-    	super.show();
-    	
-        if(resizable){
-        	if(!loaded){
-        		resizableContent.setWidget(w);
-        		loaded = true;
-        	}
-        }else{
-        	if(!loaded){
-        		content.setWidget(w);
-        		loaded = true;
-        	}
-        }
-
-    }
-    
-    public void center(){
-    	super.center();
-        if(resizable){
-        	if(!loaded){
-        		resizableContent.setWidget(w);
-        		loaded = true;
-        	}
-        }else{
-        	if(!loaded){
-        		content.setWidget(w);
-        		loaded = true;
-        	}
-        }
-
+        dialogPanel.setWidget(w);
     }
 
-    
-    public void setResizable(boolean resizable){
-    	this.resizable = resizable;
-    	if(resizable){
-    		dialogContainer.remove(content);
-    		dialogContainer.add(resizableContent);
-        }else{
-        	dialogContainer.remove(resizableContent);
-    		dialogContainer.add(content);         
-        }
-        loaded = false;
+
+    public void show() {
+        dialogPanel.show();
     }
-    
-    class SimpleWindowMover implements MouseUpHandler, MouseMoveHandler, MouseDownHandler {
-        private int offsetX, offsetY;
-        private boolean isMoving = false;
 
-        public SimpleWindowMover() {}
 
-        @Override
-        public void onMouseUp(MouseUpEvent event) {
-            if (isMoving) {
-
-                isMoving = false;
-            }
-            DOM.releaseCapture(focusPanel.getElement());
-        }
-
-        @Override
-        public void onMouseMove(MouseMoveEvent event) {
-            if (isMoving) {
-                setLocation(event.getClientX() - offsetX, event.getClientY() - offsetY);
-            }
-        }
-
-        @Override
-        public void onMouseDown(MouseDownEvent event) {
-            offsetX = event.getClientX() - getX();
-            offsetY = event.getClientY() - getY();
-
-            isMoving = true;
-
-            DOM.setCapture(focusPanel.getElement());
-        }
+    public String toString() {
+        return dialogPanel.toString();
     }
-    
-
-	
-
-	
-
-	
 }
