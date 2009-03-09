3301dc9aab63753bb336c6e6c9935dfa2745973d
gstruthe <gstruthe@kuali.org>
2009-03-09 19:23:23 -0400

    deprecate ksStackPanel
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3647 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSStackPanel.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSStackPanel.java
@@ -3,6 +3,9 @@ package org.kuali.student.common.ui.client.widgets;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.StackPanel;
 
+/**
+ * @deprecated
+ */
 public abstract class KSStackPanel extends Composite{
 
 	public abstract StackPanel getStackPanel(); 
