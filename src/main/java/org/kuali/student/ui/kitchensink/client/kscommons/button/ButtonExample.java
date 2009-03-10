c8b31b0289e16cd864e7e5ef6dce0149c9f37dcb
wiljohns <wiljohns@kuali.org>
2009-03-10 12:30:08 -0400

    added button with image
    git-svn-id: https://svn.kuali.org/repos/student/examples/ui/ks-ui-kitchensink/branches/ks-ui-kitchensink-dev@3658 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/button/ButtonExample.java
+++ src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/button/ButtonExample.java
@@ -5,6 +5,7 @@ import static org.kuali.student.ui.kitchensink.client.KitchenSinkStyleConstants.
 import static org.kuali.student.ui.kitchensink.client.KitchenSinkStyleConstants.STYLE_BUTTON_RED;
 import static org.kuali.student.ui.kitchensink.client.KitchenSinkStyleConstants.STYLE_EXAMPLE;
 
+import org.kuali.student.common.ui.client.images.KSImages;
 import org.kuali.student.common.ui.client.widgets.KSButton;
 import org.kuali.student.common.ui.client.widgets.KSLabel;
 
@@ -26,14 +27,18 @@ public class ButtonExample extends Composite {
     final KSButton button2 = new KSButton("Bordered Button",new MyClickHandler());
     final KSButton button3 = new KSButton("Large Button",new MyClickHandler());
     final KSButton button4 = new KSButton("Fancy Button",new MyClickHandler());
+    final KSButton imageButton = new KSButton("Image Button",new MyClickHandler());
 
     public ButtonExample() {
-               
+        
         buttonPanel.add(title);
-        buttonPanel.add( button1);
-        buttonPanel.add( button2);
-        buttonPanel.add( button3);
-        buttonPanel.add( button4);
+        buttonPanel.add(button1);
+        buttonPanel.add(button2);
+        buttonPanel.add(button3);
+        buttonPanel.add(button4);
+        
+        imageButton.setImage(KSImages.INSTANCE.okIcon().createImage());
+        buttonPanel.add(imageButton);
         
         buttonPanel.addStyleName(STYLE_EXAMPLE);
         button2.addStyleName(STYLE_BUTTON_RED);
