13feef50f2021e725c6cf6e2f160dcd4de4e6364
bsmith <bsmith@kuali.org>
2009-03-11 15:11:42 -0400

    javadoc
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3699 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSHelpDialog.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSHelpDialog.java
@@ -13,17 +13,37 @@ import com.google.gwt.user.client.ui.Frame;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+/**
+ * A dialog which is used to display help information.
+ * 
+ * @author Kuali Student Team
+ *
+ */
 public class KSHelpDialog extends KSHelpDialogAbstract{  
 	
 	private final KSHelpDialogAbstract dialog = GWT.create(KSHelpDialogImpl.class); 
 
 	
+	/**
+	 * Creates a help dialog.
+	 * 
+	 */
 	public KSHelpDialog() {}
 	
+	/**
+	 * Creates a help dialog that displays the help information passed in.
+	 * 
+	 * @param helpInfo
+	 */
 	public KSHelpDialog(HelpInfo helpInfo) {
 	    dialog.init(helpInfo);
 	}
 
+    /**
+     * Initializes the help dialog with the help information passed in.
+     * 
+     * @see org.kuali.student.common.ui.client.widgets.KSHelpDialogAbstract#init(org.kuali.student.common.ui.client.dto.HelpInfo)
+     */
     @Override
     protected void init(HelpInfo helpInfo) {
         dialog.init(helpInfo);
