9f2975146c7855d428854bf2fd74887bf724a730
bsmith <bsmith@kuali.org>
2009-03-11 12:41:38 -0400

    javadoc
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3686 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSDatePicker.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSDatePicker.java
@@ -8,15 +8,31 @@ import org.kuali.student.common.ui.client.widgets.impl.KSDatePickerImpl;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Composite;
 
+/**
+ * The KSDatePicker widget provides an easy way for a date to be entered by a user.  When this widget obtains focus,
+ * a datepicker popup calendar appears below the text field.  The user may either select the date using the calendar,
+ * or fill in the date manually (which will automatically selects the date for them in the calendar).
+ * 
+ * 03/11/2009 - This widget currently only supports dates entered in the mm/dd/yyyy format.  Unresolved backspace bug in Firefox.
+ * 
+ * @author Kuali Student Team
+ *
+ */
 public class KSDatePicker extends KSDatePickerAbstract{ 
     KSDatePickerAbstract datePicker = GWT.create(KSDatePickerImpl.class);
     
+    /**
+     * Creates a KSDatePicker widget.
+     * 
+     */
     public KSDatePicker() {
         this.initWidget(datePicker);
         
     }
 
     /**
+     * Get the currently selected date.
+     * 
      * @see org.kuali.student.common.ui.client.widgets.KSDatePickerAbstract#getSelectedDate()
      */
     @Override
@@ -25,6 +41,8 @@ public class KSDatePicker extends KSDatePickerAbstract{
     }
 
     /**
+     * Sets the date and selects it.
+     * 
      * @see org.kuali.student.common.ui.client.widgets.KSDatePickerAbstract#setDate(java.util.Date)
      */
     @Override
