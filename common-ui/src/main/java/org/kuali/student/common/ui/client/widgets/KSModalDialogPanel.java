679945c8e25249686fdce0c8e6b96d2b0f3318f3
bsmith <bsmith@kuali.org>
2009-03-11 19:21:06 -0400

    javadoc
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3713 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSModalDialogPanel.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSModalDialogPanel.java
@@ -9,6 +9,14 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.Widget;
 
+/**
+ * KSModalDialogPanel is a modal popup window which has a dark glass effect behind it.  The user cannot click out
+ * of the window to close it and must complete some action.  It is up to the the dialog content or calling class 
+ * to provide a way to close the window after interaction has finished.
+ * 
+ * @author Kuali Student Team
+ *
+ */
 public class KSModalDialogPanel extends KSModalDialogPanelAbstract{ 
 	private KSModalDialogPanelAbstract panel = GWT.create(KSModalDialogPanelImpl.class);
 	
@@ -16,26 +24,52 @@ public class KSModalDialogPanel extends KSModalDialogPanelAbstract{
 
 		
 	}
+	
+	/**
+	 * Shows the modal dialog (centered) and the modal dialog glass panel.
+	 * 
+	 * @see org.kuali.student.common.ui.client.widgets.KSModalDialogPanelAbstract#show()
+	 */
 	@Override
 	public void show(){
 		panel.show();
 	}
+	
+	/**
+	 * Hides the modal dialog and the modal dialog glass panel.
+	 * 
+	 * @see org.kuali.student.common.ui.client.widgets.KSDialogPanel#hide()
+	 */
 	@Override
 	public void hide(){
 	    panel.hide();
 	}
+	
+	/**
+	 * Adds a style name to the modal dialog panel.
+	 * 
+	 * @see org.kuali.student.common.ui.client.widgets.KSDialogPanel#addStyleName(java.lang.String)
+	 */
 	@Override
     public void addStyleName(String style) {
         panel.addStyleName(style);
     }
+	
+	/**
+	 * Sets the header for this modal dialog, this also allows the dialog to be movable.
+	 * 
+	 * @see org.kuali.student.common.ui.client.widgets.KSDialogPanel#setHeader(java.lang.String)
+	 */
 	@Override
     public void setHeader(String headerText) {
         panel.setHeader(headerText);
     }
-	@Override
-    public void setModal(boolean modal) {
-        panel.setModal(modal);
-    }
+	
+	/**
+	 * Sets the widget to be used in the modal dialog panel.
+	 * 
+	 * @see org.kuali.student.common.ui.client.widgets.KSDialogPanel#setWidget(com.google.gwt.user.client.ui.Widget)
+	 */
 	@Override
     public void setWidget(Widget w) {
         panel.setWidget(w);
