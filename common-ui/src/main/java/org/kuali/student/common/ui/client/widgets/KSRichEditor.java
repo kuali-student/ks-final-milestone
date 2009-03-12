e0eb02a22e66d3c3cce05ecfdd0fdf4539d96c33
bsmith <bsmith@kuali.org>
2009-03-12 12:56:20 -0400

    javadoc
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3725 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSRichEditor.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSRichEditor.java
@@ -30,46 +30,89 @@ import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /** 
- * KS default rich text editor
+ * KSRichEditor is the KS default rich text editor.  The editor provides a variety of text options commonly
+ * found in text editors.  It also features a toolbar which only show when the editor is in focus and a pop-out
+ * window which allows a user to have more space to work (and can be resized).  Any changes made in the pop-out
+ * editor are reflected in the base editor when the user is finished.
  * 
- * TODO implement with a clean toolbar and i18n
+ * TODO implement i18n
  */
 public class KSRichEditor extends KSRichEditorAbstract {
     private KSRichEditorAbstract richEditor = GWT.create(KSRichEditorImpl.class);
     
+	/**
+	 * Creates a new KSRichEditor.
+	 * 
+	 */
 	public KSRichEditor(){
 	    init(false);
 	    initWidget(richEditor);
 	}
 
 
+	/**
+	 * Creates a new KSRichEditor with the if the text editor is used in a popup
+	 * flag set.  Text editors that are have this flag set to true do not have a
+	 * pop-out option and always have the toolbar showing.
+	 * 
+	 * @param isUsedInPopup true if this will be used in a popup, false otherwise.
+	 */
 	public KSRichEditor(boolean isUsedInPopup) {
 	    init(true);
 	    initWidget(richEditor);
 	}
 	
+	/**
+	 * Gets the RichTextArea widget used for text input in this rich editor widget.
+	 * 
+	 * @see org.kuali.student.common.ui.client.widgets.KSRichEditorAbstract#getRichTextArea()
+	 */
 	public RichTextArea getRichTextArea(){
 	    return richEditor.getRichTextArea();
 	}
 	
-	// delegate methods to RichTextArea
+	/**
+	 * Get the HTML version of the text input (retains all formatting).
+	 * 
+	 * @see org.kuali.student.common.ui.client.widgets.KSRichEditorAbstract#getHTML()
+	 */
 	public String getHTML() {
 		return richEditor.getHTML();
 	}
 
+	/**
+	 * Get the pure text version of the text input (retains NO formatting).
+	 * 
+	 * @see org.kuali.student.common.ui.client.widgets.KSRichEditorAbstract#getText()
+	 */
 	public String getText() {
 		return richEditor.getText();
 	}
 
+	/**
+	 * Set the HTML of this rich text editor.
+	 * 
+	 * @see org.kuali.student.common.ui.client.widgets.KSRichEditorAbstract#setHTML(java.lang.String)
+	 */
 	public void setHTML(String html) {
 	    richEditor.setHTML(html);
 	}
 
+	/**
+	 * Set the text of this rich text editor (this is text with no formatting).
+	 * 
+	 * @see org.kuali.student.common.ui.client.widgets.KSRichEditorAbstract#setText(java.lang.String)
+	 */
 	public void setText(String text) {
 	    richEditor.setText(text);
 	}
 
 
+    /**
+     * Initialize this text editor with the specified option.
+     * 
+     * @see org.kuali.student.common.ui.client.widgets.KSRichEditorAbstract#init(boolean)
+     */
     protected void init(boolean isUsedInPopup) {
         richEditor.init(isUsedInPopup);
     }
