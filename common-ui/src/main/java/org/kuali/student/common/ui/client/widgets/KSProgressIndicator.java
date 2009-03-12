0e0a3f1544d64ddefcbee94e24bc9841c371a03c
bsmith <bsmith@kuali.org>
2009-03-11 20:16:30 -0400

    javadoc
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3719 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSProgressIndicator.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSProgressIndicator.java
@@ -5,6 +5,15 @@ import org.kuali.student.common.ui.client.widgets.impl.KSProgressIndicatorImpl;
 
 import com.google.gwt.core.client.GWT;
 
+/**
+ * KSProgressIndicator can be shown to indicate processing of some request.  It can then be hidden again, to
+ * indicate process completion.
+ * 
+ * The indicator contains a twiddler/spinner image and a text label.
+ * 
+ * @author Kuali Student Team
+ *
+ */
 public class KSProgressIndicator extends KSProgressIndicatorAbstract{ 
     KSProgressIndicatorAbstract indicator = GWT.create(KSProgressIndicatorImpl.class);
     
@@ -13,18 +22,33 @@ public class KSProgressIndicator extends KSProgressIndicatorAbstract{
         
     }
 
+    /**
+     * Hides the progress indicator.
+     * 
+     * @see org.kuali.student.common.ui.client.widgets.KSProgressIndicatorAbstract#hide()
+     */
     @Override
     public void hide() {
         indicator.hide();
         
     }
 
+    /**
+     * Shows the progress indicator.
+     * 
+     * @see org.kuali.student.common.ui.client.widgets.KSProgressIndicatorAbstract#show()
+     */
     @Override
     public void show() {
         indicator.show();
         
     }
 
+    /**
+     * Sets the text for the progress indicator, explaining what is being processed.
+     * 
+     * @see org.kuali.student.common.ui.client.widgets.KSProgressIndicatorAbstract#setText(java.lang.String)
+     */
     @Override
     public void setText(String labelText) {
         indicator.setText(labelText);
