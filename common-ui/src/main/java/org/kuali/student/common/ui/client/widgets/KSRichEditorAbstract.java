dbbd9b19b12fcbe84fc0bf6c517299563d7aecb0
bsmith <bsmith@kuali.org>
2009-03-13 13:33:49 -0400

    javadoc
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3762 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSRichEditorAbstract.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSRichEditorAbstract.java
@@ -4,30 +4,8 @@ package org.kuali.student.common.ui.client.widgets;
 
 
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.BlurEvent;
-import com.google.gwt.event.dom.client.BlurHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.FocusEvent;
-import com.google.gwt.event.dom.client.FocusHandler;
-import com.google.gwt.event.dom.client.MouseOutEvent;
-import com.google.gwt.event.dom.client.MouseOutHandler;
-import com.google.gwt.event.dom.client.MouseOverEvent;
-import com.google.gwt.event.dom.client.MouseOverHandler;
-import com.google.gwt.event.logical.shared.CloseEvent;
-import com.google.gwt.event.logical.shared.CloseHandler;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.DockPanel;
-import com.google.gwt.user.client.ui.Grid;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RichTextArea;
-import com.google.gwt.user.client.ui.SimplePanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 /** 
  * KS default rich text editor
--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSRichTextToolbar.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSRichTextToolbar.java
@@ -4,37 +4,7 @@ package org.kuali.student.common.ui.client.widgets;
 import org.kuali.student.common.ui.client.widgets.impl.KSRichTextToolbarImpl;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.BlurEvent;
-import com.google.gwt.event.dom.client.BlurHandler;
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.FocusEvent;
-import com.google.gwt.event.dom.client.FocusHandler;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.event.dom.client.MouseDownEvent;
-import com.google.gwt.event.dom.client.MouseDownHandler;
-import com.google.gwt.event.dom.client.MouseOutEvent;
-import com.google.gwt.event.dom.client.MouseOutHandler;
-import com.google.gwt.event.dom.client.MouseUpEvent;
-import com.google.gwt.event.dom.client.MouseUpHandler;
-import com.google.gwt.i18n.client.Constants;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.AbstractImagePrototype;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.FocusPanel;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.ImageBundle;
-import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.RichTextArea;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.ToggleButton;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 
 /**
  * KSRichTextToolbar provides an interface for adding different text formatting styles to text in a
@@ -64,7 +34,7 @@ public class KSRichTextToolbar extends KSRichTextToolbarAbstract{
     /**
      * Returns true if the toolbar is being interacted with, false otherwise.
      * 
-     * @see org.kuali.student.common.ui.client.widgets.KSRichTextToolbarAbstract#inUse()
+     * @return true if the toolbar is being interacted with, false otherwise.
      */
     public boolean inUse(){
         return richTextToolbar.inUse();
@@ -75,7 +45,8 @@ public class KSRichTextToolbar extends KSRichTextToolbarAbstract{
     /**
      * Initializes this toolbar with the specified options.
      * 
-     * @see org.kuali.student.common.ui.client.widgets.KSRichTextToolbarAbstract#init(com.google.gwt.user.client.ui.RichTextArea)
+     * @see org.kuali.student.common.ui.client.widgets.KSRichTextToolbarAbstract#init(com.google.gwt.user.client.ui.RichTextArea
+     *      
      */
     @Override
     protected void init(RichTextArea richText) {
