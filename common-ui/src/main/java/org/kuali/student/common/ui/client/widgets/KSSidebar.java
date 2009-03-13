317ee905853cf852dd7df3267e9e353e708f804a
bsmith <bsmith@kuali.org>
2009-03-13 16:39:09 -0400

    cleanup
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3778 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSSidebar.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSSidebar.java
@@ -1,39 +1,10 @@
 package org.kuali.student.common.ui.client.widgets;
 
-import java.util.HashMap;
-
 import org.kuali.student.common.ui.client.widgets.KSFloatPanel.FloatLocation;
-import org.kuali.student.common.ui.client.widgets.impl.KSCollapsableFloatPanelImpl;
-import org.kuali.student.common.ui.client.widgets.impl.KSFloatPanelImpl;
-import org.kuali.student.common.ui.client.widgets.impl.KSRichTextToolbarImpl;
 import org.kuali.student.common.ui.client.widgets.impl.KSSidebarImpl;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.BlurEvent;
-import com.google.gwt.event.dom.client.BlurHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.MouseOutEvent;
-import com.google.gwt.event.dom.client.MouseOutHandler;
-import com.google.gwt.event.dom.client.MouseOverEvent;
-import com.google.gwt.event.dom.client.MouseOverHandler;
-import com.google.gwt.event.logical.shared.ResizeEvent;
-import com.google.gwt.event.logical.shared.ResizeHandler;
-import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.DeferredCommand;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.FocusPanel;
-import com.google.gwt.user.client.ui.HasVerticalAlignment;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.ScrollPanel;
-import com.google.gwt.user.client.ui.SimplePanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.user.client.ui.HasVerticalAlignment.VerticalAlignmentConstant;
 
 /**
  * The KSSidebar is a float panel that can be place on the right side or the left side of the screen.  This
