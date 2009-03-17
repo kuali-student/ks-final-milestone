623e963adabcaa5d80527e4160965b53ee6c6ddf
hjohnson <hjohnson@kuali.org>
2009-03-17 13:22:26 -0400

    Update code source reference
    git-svn-id: https://svn.kuali.org/repos/student/examples/ui/ks-ui-kitchensink/branches/ks-ui-kitchensink-dev@3797 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/tabpanel/TabPanelExampleDescriptor.java
+++ src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/tabpanel/TabPanelExampleDescriptor.java
@@ -7,7 +7,7 @@ import com.google.gwt.user.client.ui.Widget;
 public class TabPanelExampleDescriptor extends KitchenSinkExample { 
     public TabPanelExampleDescriptor() {
         super();
-        super.addResource("java", "TabPanelExample.java", "kscommons/accordionpanel/TabPanelExample.java", "Example usage of KSTabPanel.");
+        super.addResource("java", "TabPanelExample.java", "kscommons/tabpanel/TabPanelExample.java", "Example usage of KSTabPanel.");
         super.addResource("css", "KSTabPanel.css", "KSTabPanel.css", "Default styling of KSTabPanel.");
     }
     public String getDescription() {       
