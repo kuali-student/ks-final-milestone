76151b75a7fa9212c1c3cedf9cd820294aeece19
jyin <jyin@kuali.org>
2009-03-17 17:36:25 -0400

    new files and folder refactor
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3806 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/table/NodeWidget.java
@@ -0,0 +1,22 @@
+package org.kuali.student.common.ui.client.widgets.table;
+
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTML;
+
+public     class NodeWidget extends Composite{
+    private Node node;
+    HTML html = new HTML();
+    public NodeWidget(Node n){
+        node = n;
+        html.setHTML(node.getUserObject().toString());
+        super.initWidget(html);
+    }
+    public Node getNode(){
+        return node;
+    }
+    public void setNode(Node n){
+        node = n;
+        html.setHTML(node.getUserObject().toString());
+    }
+}
