dfccef1ce28fbe656dde46cbf90709425b5ae62a
bsmith <bsmith@kuali.org>
2009-03-18 19:39:40 -0400

    removed example css
    git-svn-id: https://svn.kuali.org/repos/student/examples/ui/ks-ui-kitchensink/branches/ks-ui-kitchensink-dev@3833 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/sidebar/SidebarExampleDescriptor.java
+++ src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/sidebar/SidebarExampleDescriptor.java
@@ -9,7 +9,6 @@ public class SidebarExampleDescriptor extends KitchenSinkExample {
         super();
         super.addResource("java", "SidebarExample.java", "kscommons/sidebar/SidebarExample.java", "Example usage of KSSidebar.");
         super.addResource("css", "KSSidebar.css", "KSSidebar.css", "Default styling of KSSidebar.");
-        super.addResource("css", "SidebarExample.css", "examplecss/SidebarExample.css", "Example styling of KSSidebar.");
     }
     public String getDescription() {       
         return "Sidebar is a panel that can appear on either the right or left hand side.  Clicking on one of its tabs will pop the panel out and show specific content.  Clicking on the same tab again will close the sidebar."; 
