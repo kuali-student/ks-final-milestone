ba4dbb1c18e6c5499c71250e5f7468c4615938ae
hjohnson <hjohnson@kuali.org>
2009-03-19 19:09:14 -0400

    Add Submit button to DatePicker example
    git-svn-id: https://svn.kuali.org/repos/student/examples/ui/ks-ui-kitchensink/branches/ks-ui-kitchensink-dev@3853 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/datepicker/DatePickerExample.java
+++ src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/datepicker/DatePickerExample.java
@@ -2,35 +2,43 @@ package org.kuali.student.ui.kitchensink.client.kscommons.datepicker;
 
 import static org.kuali.student.ui.kitchensink.client.KitchenSinkStyleConstants.STYLE_EXAMPLE;
 
+import org.kuali.student.common.ui.client.widgets.KSButton;
 import org.kuali.student.common.ui.client.widgets.KSDatePicker;
 import org.kuali.student.common.ui.client.widgets.KSLabel;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class DatePickerExample extends Composite {
 
-    final VerticalPanel main = new VerticalPanel();
+    private final VerticalPanel main = new VerticalPanel();
+    private final KSButton button = new KSButton("Submit");
     
-    final KSDatePicker datePicker = new KSDatePicker();
-    final KSLabel label = new KSLabel("Click in the box to open the date picker: ", false);
+    private final KSDatePicker datePicker = new KSDatePicker();
+    private final KSLabel label = new KSLabel("Click in the box to open the date picker: ", false);
+    
+    private static final DateTimeFormat DF = DateTimeFormat.getMediumDateFormat();
 
     public DatePickerExample() {
         
         main.addStyleName(STYLE_EXAMPLE);
         
-// Can't do this as have no access to DatePicker in KSDate Picker
-//        ksDatePicker.addValueChangeHandler(new ValueChangeHandler<Date>(){
-//
-//            public void onValueChange(ValueChangeEvent<Date> event) {
-//                
-//              Window.alert("You picked " + ksDatePicker.getValue());
-//            }   
-//        });
-               
+        button.addClickHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent arg0) {
+                Window.alert("You selected " + DF.format(datePicker.getDate()));
+                
+            }} );
+        
         main.add(label);
         main.add(datePicker);
-
+        main.add(button);
+        
         super.initWidget(main);
     }
 }
