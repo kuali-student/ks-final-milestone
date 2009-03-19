ffedb03ae05783607f294084cd2ca17fc9abbef1
wgomes <wgomes@kuali.org>
2009-03-19 13:56:17 -0400

    Update to use new KSDropDown impl based on select item widget.
    git-svn-id: https://svn.kuali.org/repos/student/examples/ui/ks-ui-kitchensink/branches/ks-ui-kitchensink-dev@3843 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/dropdown/DropDownExample.java
+++ src/main/java/org/kuali/student/ui/kitchensink/client/kscommons/dropdown/DropDownExample.java
@@ -1,30 +1,36 @@
 package org.kuali.student.ui.kitchensink.client.kscommons.dropdown;
 
-import org.kuali.student.common.ui.client.widgets.KSDropDown;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
+import org.kuali.student.common.ui.client.widgets.KSDropDown;
+import org.kuali.student.common.ui.client.widgets.list.KSSelectItemWidgetAbstract;
+import org.kuali.student.common.ui.client.widgets.list.ListItems;
+import org.kuali.student.common.ui.client.widgets.list.SelectionChangeHandler;
+
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.ListBox;
 
 public class DropDownExample extends Composite {
 
     final HorizontalPanel main = new HorizontalPanel();
      
     final KSDropDown dropDown = new KSDropDown();
+    
+    ListItems schools;
 
     public DropDownExample() {
         
         loadList();
 
-        dropDown.addChangeHandler(new ChangeHandler() {
+        dropDown.setListItems(schools);
+        dropDown.addSelectionChangeHandler(new SelectionChangeHandler() {
             @Override
-            public void onChange(ChangeEvent arg0) {
-                ListBox lb = (ListBox)arg0.getSource();
-                int i = lb.getSelectedIndex();
-                Window.alert("You picked <" + lb.getItemText(i) + ">");
+            public void onSelectionChange(KSSelectItemWidgetAbstract w) {
+                List<String> ids = w.getSelectedItems();
+                Window.alert("You picked <" + schools.getItemText(ids.get(0)) + ">");
                 
             }});
         main.add(dropDown);
@@ -32,17 +38,59 @@ public class DropDownExample extends Composite {
         super.initWidget(main);
     }
 
-    private void loadList() {
-       dropDown.addItem("University of British Columbia");
-       dropDown.addItem("Florida State University");
-       dropDown.addItem("Naval Postgraduate School");
-       dropDown.addItem("San Joaquin Delta College");
-       dropDown.addItem("University of California, Berkeley");
-       dropDown.addItem("University of Maryland, College Park");
-       dropDown.addItem("Massachusetts Institute of Technology");
-       dropDown.addItem("University of Southern California");
-       dropDown.addItem("University of Washington");
-       dropDown.addItem("Carnegie Mellon University");
-        
+    private void loadList() {       
+        schools = new ListItems(){
+            List<String> names = Arrays.asList(
+                "University of British Columbia",
+                "Florida State University",
+                "Naval Postgraduate School",
+                "San Joaquin Delta College",
+                "University of California, Berkeley",
+                "University of Maryland, College Park",
+                "Massachusetts Institute of Technology",
+                "University of Southern California",
+                "University of Washington",
+                "Carnegie Mellon University");
+
+            @Override
+            public List<String> getAttrKeys() {
+                List<String> attributes = new ArrayList<String>();
+                attributes.add("Name");
+                return attributes;
+            }
+
+            @Override
+            public String getItemAttribute(String id, String attrkey) {
+                String value = null;
+                Integer index;
+                try{
+                    index = Integer.valueOf(id);
+                    value = names.get(index);
+                } catch (Exception e) {
+                }
+                
+                return value;
+            }
+
+            @Override
+            public int getItemCount() {    
+                return names.size();
+            }
+
+            @Override
+            public List<String> getItemIds() {
+                List<String> ids = new ArrayList<String>();
+                for(int i=0; i < names.size(); i++){
+                    ids.add(String.valueOf(i));
+                }
+                return ids;
+            }
+
+            @Override
+            public String getItemText(String id) {
+                return getItemAttribute(id, "Name");
+            }
+        };
+            
     }
 }
