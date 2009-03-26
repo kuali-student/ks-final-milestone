746a2b3124c26773905a36500f74c08dfd2a8eee
hjohnson <hjohnson@kuali.org>
2009-03-26 19:27:19 -0400

    Start styling of header. Add breadcrumb trail
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@3911 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- common-ui/src/main/java/org/kuali/student/common/ui/client/application/Header.java
+++ common-ui/src/main/java/org/kuali/student/common/ui/client/application/Header.java
@@ -1,15 +1,69 @@
 package org.kuali.student.common.ui.client.application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.student.common.ui.client.widgets.KSImage;
+import org.kuali.student.common.ui.client.widgets.breadcrumb.KSBreadcrumb;
+import org.kuali.student.common.ui.client.widgets.breadcrumb.KSBreadcrumbItem;
+
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Hyperlink;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Header extends Composite {
-	private final FlowPanel panel = new FlowPanel();
-	
-	public Header() {
-		super.initWidget(panel);
-		panel.addStyleName("KS-Header");
-		panel.add(new Image("images/logo-ks.gif"));
-	}
+    private final VerticalPanel main = new VerticalPanel();
+    
+    private final HorizontalPanel headerPanel = new HorizontalPanel();
+//    private final HorizontalPanel linksPanel = new HorizontalPanel();
+    private final DockPanel linksDockPanel = new DockPanel();
+    private final SimplePanel logoPanel = new SimplePanel();
+    
+    private final Hyperlink logout = new Hyperlink();
+    private final Hyperlink preferences = new Hyperlink();
+
+    private final KSBreadcrumb breadcrumb = new KSBreadcrumb();
+
+    //	private final KSImage logo = new KSImage("images/KULSTP.jpg");
+    private final KSImage logo = new KSImage("images/logo-ks.gif");
+
+    public Header() {
+        super.initWidget(main);
+
+        KSBreadcrumbItem item = new KSBreadcrumbItem("Home", "Home page");
+        List<KSBreadcrumbItem> items = new ArrayList<KSBreadcrumbItem>();
+        items.add(item);
+        breadcrumb.setUnLinkedBreadcrumbList(items);
+        breadcrumb.addStyleName("KS-Header-Breadcrumb");
+
+        logout.setText("Logout");
+        preferences.setText("Preferences");
+        
+        HorizontalPanel test = new HorizontalPanel();
+        test.add(preferences);
+        test.add(logout);
+        linksDockPanel.add(test ,DockPanel.EAST);
+       
+//        linksDockPanel.add(logout,DockPanel.EAST);
+
+        linksDockPanel.addStyleName("KS-Header-Link");
+        test.addStyleName("KS-Right-Align");
+
+        linksDockPanel.setHorizontalAlignment(DockPanel.ALIGN_RIGHT);
+        linksDockPanel.setVerticalAlignment(DockPanel.ALIGN_BOTTOM);
+
+        logoPanel.add(logo);
+        
+        headerPanel.add(logoPanel);
+
+        
+        headerPanel.addStyleName("KS-Header");
+
+        main.add(headerPanel);
+            main.add(linksDockPanel);    
+            main.add(breadcrumb);
+    }
 }
