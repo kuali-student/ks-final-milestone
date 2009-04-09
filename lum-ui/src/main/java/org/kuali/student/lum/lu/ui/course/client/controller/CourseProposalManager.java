22dfadcbf6c17280986a90a03d46f7aa9a7dae1d
wgomes <wgomes@kuali.org>
2009-04-09 14:24:49 -0400

    Redesign course screens
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@4051 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/controller/CourseProposalManager.java
+++ lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/controller/CourseProposalManager.java
@@ -15,6 +15,10 @@
  */
 package org.kuali.student.lum.lu.ui.course.client.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.student.common.ui.client.application.ApplicationComposite;
 import org.kuali.student.common.ui.client.event.SaveEvent;
 import org.kuali.student.common.ui.client.event.SaveHandler;
 import org.kuali.student.common.ui.client.mvc.Controller;
@@ -22,16 +26,20 @@ import org.kuali.student.common.ui.client.mvc.Model;
 import org.kuali.student.common.ui.client.mvc.ModelRequestCallback;
 import org.kuali.student.common.ui.client.mvc.View;
 import org.kuali.student.common.ui.client.mvc.ViewComposite;
-import org.kuali.student.common.ui.client.mvc.test.Person;
+import org.kuali.student.common.ui.client.widgets.KSLabel;
+import org.kuali.student.common.ui.client.widgets.menus.KSAccordionMenu;
+import org.kuali.student.common.ui.client.widgets.menus.KSMenu;
+import org.kuali.student.common.ui.client.widgets.menus.KSMenuItemData;
 import org.kuali.student.core.dto.Idable;
 import org.kuali.student.lum.lu.dto.CluInfo;
 import org.kuali.student.lum.lu.ui.course.client.view.BeginCourseProposal;
+import org.kuali.student.lum.lu.ui.course.client.view.CourseAuthor;
 import org.kuali.student.lum.lu.ui.course.client.view.CourseInformation;
 import org.kuali.student.lum.lu.ui.main.client.controller.LUMApplicationManager.LUMViews;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 
 /**
@@ -43,17 +51,33 @@ import com.google.gwt.user.client.ui.SimplePanel;
 public class CourseProposalManager extends Controller implements View {
     private BeginCourseProposal beginProposal = new BeginCourseProposal();
     private View courseInformation = new CourseInformation(this);
+    private View courseAuthor = new CourseAuthor(this);
     Model<CluInfo> cluInfo = new Model<CluInfo>();
-    SimplePanel simplePanel = new SimplePanel();
+    ApplicationComposite app = new ApplicationComposite();
+    SimplePanel contentPanel = new SimplePanel();
     
     private CourseProposalType type;
+    
+    private View dummyView = new DummyView(this); 
+        
+    private class DummyView extends ViewComposite{
+        /**
+         * @param controller
+         * @param name
+         */
+        public DummyView(Controller controller) {
+            super(controller, "Dummy");
+            super.initWidget(new KSLabel("Not implemented"));
+        }
+        
+    }
     
     public enum CourseProposalType {
         NEW_COURSE, TEMPLATE, COPY_COURSE, COPY_TEMPLATE
     }
     
     public enum CourseViews{
-        COURSE_INFORMATION
+        COURSE_INFORMATION, COURSE_AUTHOR, COURSE_DUMMY
     }
     
     public CourseProposalManager(Controller parentController){
@@ -67,22 +91,83 @@ public class CourseProposalManager extends Controller implements View {
         beginProposal.addSaveHandler(new SaveHandler(){
             public void onSave(SaveEvent saveEvent) {                
                 cluInfo.add(beginProposal.getCourseProposalClu());
-                showView(CourseViews.COURSE_INFORMATION);
+                showView(CourseViews.COURSE_AUTHOR);
             }            
         });
-        super.initWidget(simplePanel);
+        setup();
+        super.initWidget(app);
     }
 
+    public void setup(){
+        HorizontalPanel hPanel = new HorizontalPanel();
+        KSMenu menuPanel = new KSAccordionMenu();
+        
+        List<KSMenuItemData> menuItems = new ArrayList<KSMenuItemData>();
+        
+        KSMenuItemData proposalInfo = new KSMenuItemData("1. Proposal Information"); 
+        addSubItem(proposalInfo, "Author + Collaborators", CourseViews.COURSE_AUTHOR);
+        addSubItem(proposalInfo, "Governance", CourseViews.COURSE_DUMMY);
+        addSubItem(proposalInfo, "Course Format", CourseViews.COURSE_DUMMY);
+        
+        KSMenuItemData academicContent = new KSMenuItemData("2. Academic Content");
+        addSubItem(academicContent, "Course Information", CourseViews.COURSE_INFORMATION);
+        addSubItem(academicContent, "Learning Objectives", CourseViews.COURSE_DUMMY);
+        addSubItem(academicContent, "Syllabus", CourseViews.COURSE_DUMMY);
+        addSubItem(academicContent, "Learning Results", CourseViews.COURSE_DUMMY);
+        
+        KSMenuItemData studentElig = new KSMenuItemData("3. Student Eligibility");
+        addSubItem(studentElig, "Course Restrictions", CourseViews.COURSE_DUMMY);
+        addSubItem(studentElig, "Pre + Co Requisites", CourseViews.COURSE_DUMMY);
+       
+        KSMenuItemData admin = new KSMenuItemData("4. Administrative");
+        addSubItem(admin, "Credits", CourseViews.COURSE_DUMMY);
+        addSubItem(admin, "Active Dates", CourseViews.COURSE_DUMMY);
+        addSubItem(admin, "Financials", CourseViews.COURSE_DUMMY);
+        addSubItem(admin, "Program Requirements", CourseViews.COURSE_DUMMY);
+
+        KSMenuItemData attachments = new KSMenuItemData("5. Attachments");
+        addSubItem(attachments, "Supporting Documents", CourseViews.COURSE_DUMMY);
+        
+        menuItems.add(proposalInfo);
+        menuItems.add(academicContent);
+        menuItems.add(studentElig);
+        menuItems.add(admin);
+        menuItems.add(attachments);
+        
+        menuPanel.setItems(menuItems);
+        
+        hPanel.add(menuPanel);
+        hPanel.add(contentPanel);
+        app.setContent(hPanel);
+    }
+    
+    private void addSubItem(final KSMenuItemData group, String title, final CourseViews view) {
+
+        KSMenuItemData item = new KSMenuItemData(title);
+    
+        item.setClickHandler(getClickHandler(view));              
+        group.addSubItem(item);
+    }
+    
+    private ClickHandler getClickHandler(final CourseViews v){
+        return new ClickHandler() {
+            public void onClick(ClickEvent arg0) {
+                showView(v);
+            }
+        };
+    }                    
     
     /**
      * @see org.kuali.student.common.ui.client.mvc.Controller#getView(java.lang.Enum)
      */   
     protected <V extends Enum<?>> View getView(V viewType) {
         switch ((CourseViews)viewType){
+            case COURSE_AUTHOR:
+                return courseAuthor;
             case COURSE_INFORMATION:
-                return courseInformation;
+                return courseInformation; 
             default:
-                return null;
+                return dummyView;
         }
     }
 
@@ -96,7 +181,9 @@ public class CourseProposalManager extends Controller implements View {
      * @see org.kuali.student.common.ui.client.mvc.Controller#renderView(org.kuali.student.common.ui.client.mvc.View)
      */
     protected void renderView(View view) {
-        simplePanel.setWidget((ViewComposite)view);
+        if (view != null){
+            contentPanel.setWidget((ViewComposite)view);
+        }
     }
 
     /**
@@ -104,7 +191,7 @@ public class CourseProposalManager extends Controller implements View {
      */
     @Override
     public void showDefaultView() {       
-        this.showView(CourseViews.COURSE_INFORMATION);
+        this.showView(CourseViews.COURSE_AUTHOR);
     }
 
     /**
@@ -159,7 +246,4 @@ public class CourseProposalManager extends Controller implements View {
         }
     }
     
-
-    
-
 }
--- lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/view/CourseInformation.java
+++ lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/view/CourseInformation.java
@@ -38,100 +38,28 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  * @author Kuali Student Team
  *
  */
-public class CourseInformation extends ViewComposite{
-    VerticalPanel vPanel = new VerticalPanel();
-    KSFormLayoutPanel form;
-    private Model<CluInfo> clus = null;
-    
-    KSButton saveButton = new KSButton("Save Changes", new ClickHandler(){
-        public void onClick(ClickEvent event) {
-            // TODO: Validate data && update model
-            form.setEditMode(EditMode.VIEW_ONLY);
-            saveButton.setVisible(false);
-            editButton.setVisible(true);
-        }                
-    });
-    
-    KSButton editButton = new KSButton("Edit", new ClickHandler(){
-        public void onClick(ClickEvent event) {
-            form.setEditMode(EditMode.EDITABLE);
-            saveButton.setVisible(true);
-            editButton.setVisible(false);
-        }        
-    });
+public class CourseInformation extends CourseSection{   
     
     /**
      * @param controller
      * @param name
      */
     public CourseInformation(Controller controller) {
-        super(controller, "CourseInformation");
-        super.initWidget(vPanel);
+        super(controller, "CourseInformation", "Course Information");
     }    
     
-    @Override
-    protected void onLoad() {
-        if (clus == null) {
-            getController().requestModel(CluInfo.class, new ModelRequestCallback<CluInfo>() {
-                public void onModelReady(Model<CluInfo> model) {
-                    clus = model;
-                    setup();
-                }
-
-                public void onRequestFail(Throwable cause) {
-                    throw new RuntimeException("Unable to connect to model", cause);
-                }
-
-            });
-        }
+    public void init() {
+        addTextField("courseNumber", "Course Number");
+        addTextField("proposedTitle", "Proposed Course Title");
+        addTextField("transcriptTitle", "Transcript Title");
+        
+        addFormField("description","Decription", new KSTextArea());        
+        addFormField("rationale","Rationale", new KSTextArea());        
     }
 
-    protected void setup() {
-        /*
-        clus.addModelChangeHandler(new ModelChangeHandler<CluInfo>() {
-            public void onModelChange(ModelChangeEvent<CluInfo> event) {
-                redraw();
-            }
-        });
-        */
-
-        if (form == null){
-            form = new KSFormLayoutPanel();
-            form.addFormField(getFormField("courseNumber", "Course Number"));
-            form.addFormField(getFormField("proposedTitle", "Proposed Course Title"));
-            form.addFormField(getFormField("transcriptTitle", "Transcript Title"));
-            
-            KSFormField ff = new KSFormField("description","Decription");
-            ff.setWidget(new KSTextArea());
-            form.addFormField(ff);
-            
-            ff = new KSFormField("rationale","Rationale");
-            ff.setWidget(new KSTextArea());
-            form.addFormField(ff);
-    
-            vPanel.add(new KSLabel("Course Information"));
-            vPanel.add(form);
-            
-            saveButton.setVisible(true);
-            editButton.setVisible(false);
-            vPanel.add(saveButton);
-            vPanel.add(editButton);
-        }
-        redraw();
-    }
-
-    public KSFormField getFormField(String name, String label){
-        KSFormField ff = new KSFormField();
-        ff.setName(name);
-        ff.setLabelText(label);
-        ff.setWidget(new KSTextBox());
-        return ff;
-    }
-
-    protected void redraw() {
-        for (CluInfo clu : clus.getValues()) {
-            form.setFieldValue("proposedTitle", clu.getOfficialIdentifier().getLongName());
-        }
+    public void redraw() {
+        CluInfo clu = getCourseInformation();
+        setFieldValue("proposedTitle", clu.getOfficialIdentifier().getLongName());
     }
 
 }
