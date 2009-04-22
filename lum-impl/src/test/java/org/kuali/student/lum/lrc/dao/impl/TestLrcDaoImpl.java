064f12a0289305b44c56093833b93af11118cd82
glindhol <glindhol@kuali.org>
2009-04-21 23:52:05 -0400

    Learning result: Need at least one test
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@4193 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lum-impl/src/test/java/org/kuali/student/lum/lrc/dao/impl/TestLrcDaoImpl.java
+++ lum-impl/src/test/java/org/kuali/student/lum/lrc/dao/impl/TestLrcDaoImpl.java
@@ -1,5 +1,8 @@
 package org.kuali.student.lum.lrc.dao.impl;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 import org.kuali.student.common.test.spring.AbstractTransactionalDaoTest;
 import org.kuali.student.common.test.spring.Dao;
 import org.kuali.student.common.test.spring.PersistenceFileLocation;
@@ -10,5 +13,8 @@ public class TestLrcDaoImpl extends AbstractTransactionalDaoTest {
 	@Dao(value = "org.kuali.student.lum.lrc.dao.impl.LrcDaoImpl", testSqlFile = "classpath:ks-lrc.sql")
 	public LrcDao dao;
 
-
+	@Test
+	public void testLrc() {
+		assertTrue(true);
+	}
 }
