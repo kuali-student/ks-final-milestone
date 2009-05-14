f4d0caeb55254609ff712e11df2fc9607eaea44f
glindhol <glindhol@kuali.org>
2009-05-14 15:34:21 -0400

    Learning result: Do TestLrCDaoImpl
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@4472 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lum-impl/src/main/java/org/kuali/student/lum/lrc/entity/ResultValue.java
+++ lum-impl/src/main/java/org/kuali/student/lum/lrc/entity/ResultValue.java
@@ -35,12 +35,22 @@ import javax.persistence.TemporalType;
 import org.kuali.student.core.entity.MetaEntity;
 import org.kuali.student.core.entity.RichText;
 
-/**
- * This is a description of what this class does - lindholm don't forget to fill this in.
+/*
+ * Copyright 2009 The Kuali Foundation
  *
- * @author Kuali Rice Team (kuali-rice@googlegroups.com)
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 @Entity
 @Table(name="KSLU_LRC_RESULT_VALUE")
 @Inheritance(strategy=InheritanceType.JOINED)
--- lum-impl/src/test/java/org/kuali/student/lum/lrc/service/impl/TestLrcServiceImpl.java
+++ lum-impl/src/test/java/org/kuali/student/lum/lrc/service/impl/TestLrcServiceImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2009 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kuali.student.lum.lrc.service.impl;
 
 import static org.junit.Assert.assertEquals;
@@ -311,6 +326,13 @@ public class TestLrcServiceImpl extends AbstractServiceTest {
     public void testGetResultComponentType() throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException {
         ResultComponentTypeInfo rcti = client.getResultComponentType("resultComponentType.credential");
         assertNotNull(rcti);
+
+        try {
+            rcti = client.getResultComponentType("resultComponentType.credentialYYY");
+            assertTrue(false);
+        } catch (DoesNotExistException e) {
+            assertTrue(true);
+        }
     }
 
 	@Test
@@ -334,26 +356,26 @@ public class TestLrcServiceImpl extends AbstractServiceTest {
 
     @Test
     public void testGetCredentialByKeyList() throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException {
-        ArrayList<String> keys = new ArrayList<String>();
-        keys.add("LRC-RESULT_VALUE-CREDENTIAL-1");
-        keys.add("LRC-RESULT_VALUE-CREDENTIAL-2");
-        keys.add("LRC-RESULT_VALUE-CREDENTIAL-1x");
-        List<CredentialInfo> credentials = client.getCredentialsByKeyList(keys);
+        ArrayList<String> ids = new ArrayList<String>();
+        ids.add("LRC-RESULT_VALUE-CREDENTIAL-1");
+        ids.add("LRC-RESULT_VALUE-CREDENTIAL-2");
+        ids.add("LRC-RESULT_VALUE-CREDENTIAL-1x");
+        List<CredentialInfo> credentials = client.getCredentialsByKeyList(ids);
         assertNotNull(credentials);
         assertEquals(2, credentials.size());
 
-        keys.clear();
-        keys.add("LRC-RESULT_VALUE-CREDENTIAL-1x");
+        ids.clear();
+        ids.add("LRC-RESULT_VALUE-CREDENTIAL-1x");
         try {
-            credentials = client.getCredentialsByKeyList(keys);
+            credentials = client.getCredentialsByKeyList(ids);
             assertTrue(false);
         } catch (DoesNotExistException e) {
             assertTrue(true);
         }
 
-        keys.clear();
+        ids.clear();
         try {
-            credentials = client.getCredentialsByKeyList(keys);
+            credentials = client.getCredentialsByKeyList(ids);
             assertTrue(false);
         } catch (MissingParameterException e) {
             assertTrue(true);
