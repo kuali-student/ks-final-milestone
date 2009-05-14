5d0c74f90582c4a688f371c211863019539844d2
wgomes <wgomes@kuali.org>
2009-05-14 14:07:22 -0400

    Remove PersonServiceClient interface
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@4466 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core-ui/src/main/java/org/kuali/student/core/person/ui/client/service/PersonRpc.java
+++ core-ui/src/main/java/org/kuali/student/core/person/ui/client/service/PersonRpc.java
@@ -1,10 +1,16 @@
 package org.kuali.student.core.person.ui.client.service;
 
+import java.util.List;
+
+import org.kuali.student.core.person.dto.PersonInfo;
+import org.kuali.student.core.search.dto.QueryParamValue;
+import org.kuali.student.core.search.dto.Result;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 
-public interface PersonRpc extends RemoteService, PersonServiceClient {
+public interface PersonRpc extends RemoteService {
 	
 	public static final String SERVICE_URI = "PersonRpc";
 
@@ -19,4 +25,7 @@ public interface PersonRpc extends RemoteService, PersonServiceClient {
 			return instance;
 		}
 	}
+	
+    public List<Result> searchForResults(String searchTypeKey, List<QueryParamValue> queryParamValues);
+    public PersonInfo fetchPerson(String personId);
 }
