9ff09d921a170e7f9b1ef4f1e5de9d3a1fc12f88
ddean <ddean@kuali.org>
2009-06-03 14:22:00 -0400

    removing the event stuff since i don't use it anymore
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@4641 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core-ui/src/main/java/org/kuali/student/core/organization/ui/client/view/StateHandler.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.student.core.organization.ui.client.view;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface StateHandler extends EventHandler {
-
-    public void onStateChange(StateEvent event);
-}
