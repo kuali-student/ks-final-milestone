54e02161f9ed27048669ed922314c80193d260d2
kmuthusw <kmuthusw@kuali.org>
2009-06-04 14:02:29 -0400

    Changed module name to match artificat-Id Removing old modules
    git-svn-id: https://svn.kuali.org/repos/student/brms/branches/brms-dev@4660 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- brms-impl/src/main/java/org/kuali/student/brms/internal/common/statement/propositions/functions/Function.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.student.brms.internal.common.statement.propositions.functions;
-
-public interface Function<T> {
-	public T compute();
-
-	public void setOperation(String operationType);
-}
