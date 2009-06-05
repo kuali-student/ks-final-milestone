f24cd1d0a21db58c63a0c3bb6e07ea8db2ec1ba9
glindhol <glindhol@kuali.org>
2009-06-05 17:14:44 -0400

    Comment Service: Set up the  infrastructure for CommentService implementation
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@4699 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-impl/src/test/java/org/kuali/student/core/comment/service/impl/TestCommentServiceImpl.java
+++ ks-core-impl/src/test/java/org/kuali/student/core/comment/service/impl/TestCommentServiceImpl.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.student.core.dictionary.service.impl;
+package org.kuali.student.core.comment.service.impl;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
