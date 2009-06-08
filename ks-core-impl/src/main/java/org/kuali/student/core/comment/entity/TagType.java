de60ee359a593c50c71ff290d812a95d2af29f06
neerav <neerav@kuali.org>
2009-06-08 14:15:35 -0400

    Comment Service
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@4716 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-core-impl/src/main/java/org/kuali/student/core/comment/entity/TagType.java
@@ -0,0 +1,120 @@
+package org.kuali.student.core.comment.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.kuali.student.core.entity.Type;
+
+
+@Entity
+@Table(name = "KSCO_TAG_TYPE")
+public class TagType extends Type<TagTypeAttribute> {
+
+    @Id
+    @Column(name = "ID")
+    private String id;
+    
+    @Column(name = "NAME")
+    private String name;
+    
+    @Column(name = "DESCRIPTION")
+    private String desc;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "EFF_DT")
+    private Date effectiveDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "EXPIR_DT")
+    private Date expirationDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private List<TagTypeAttribute> attributes;
+    
+    @OneToMany(cascade=CascadeType.ALL, mappedBy="type")
+    private List<Tag> tags;
+
+    public String getId(){
+        return id;
+    }
+    
+    public void setId(String id){
+        this.id=id;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name=name;
+    }
+    
+    
+    public String getDesc(){
+        return desc;
+    }
+    
+    public void setDesc(String desc){
+        this.desc=desc;
+    }
+    
+    /**
+     * @return the effectiveDate
+     */
+    public Date getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    /**
+     * @param effectiveDate the effectiveDate to set
+     */
+    public void setEffectiveDate(Date effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    /**
+     * @return the expirationDate
+     */
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    /**
+     * @param expirationDate the expirationDate to set
+     */
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public List<TagTypeAttribute> getAttributes() {
+        
+        return attributes;
+    }
+
+    @Override
+    public void setAttributes(List<TagTypeAttribute> attributes) {
+        this.attributes=attributes;
+        
+    }
+    
+    public List<Tag> getTag(){
+        return tags;
+    }
+    
+    public void setTag(List<Tag> tags){
+        this.tags=tags;
+    }
+    
+    
+}
