9265ebc73b9ae497ba447d0f51d2ce928e36fa1f
jyin <jyin@kuali.org>
2009-06-16 13:59:59 -0400

    removed layout group
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@4825 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-ui/src/main/java/org/kuali/student/common/ui/client/configurable/ConfigurableLayoutGroup.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.student.common.ui.client.configurable;
-
-import org.kuali.student.core.dto.Idable;
-
-public abstract class ConfigurableLayoutGroup <T extends Idable> extends LayoutGroup<T>{
-    public abstract ConfigurableLayoutGroup<T> addField(ConfigurableField<T> field);
-    public abstract ConfigurableLayoutGroup<T> addGroup(ConfigurableLayoutGroup<T> field);
-}
