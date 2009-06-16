440d1332f7106679f01e1d7bb5847d4af3fed749
jyin <jyin@kuali.org>
2009-06-16 14:00:32 -0400

    refactor for nested layout
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@4826 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-ui/src/main/java/org/kuali/student/common/ui/client/configurableui/PropertyBinder.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.student.common.ui.client.configurableui;
-
-import org.kuali.student.core.dto.Idable;
-
-public interface PropertyBinder <T extends Idable, R extends Object> {
-    public Object getValue(T object);
-    public void setValue(T object, R value);
-
-}
--- ks-common-ui/src/main/java/org/kuali/student/common/ui/client/configurableui/UILayout.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.student.common.ui.client.configurableui;
-
-import org.kuali.student.core.dto.Idable;
-
-import com.google.gwt.user.client.ui.Composite;
-
-public class UILayout <T extends Idable> extends Composite{
-
-}
