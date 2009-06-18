316bf6c5462ba76a79a572e43d36913cdbde3cdb
jyin <jyin@kuali.org>
2009-06-18 18:33:43 -0400

    deprecated
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@4854 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSHelpDialogAbstract.java
+++ ks-common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/KSHelpDialogAbstract.java
@@ -1,7 +1,9 @@
 package org.kuali.student.common.ui.client.widgets;
 
 import org.kuali.student.common.ui.client.dto.HelpInfo;
-
+/**
+ * @deprecated
+ * */
 public abstract class KSHelpDialogAbstract extends KSInfoDialogPanel {
 
  /**
