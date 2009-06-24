ba5773d5aa238c003f4780da0d05e43e31f93d68
depstein <depstein@kuali.org>
2009-06-24 16:17:20 -0400

    updated post processor to new doc format and added preliminary collab post processor
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/team2/ks-rice-standalone/trunk@4933 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/lum/workflow/CourseProposalCollaboratorPostProcessor.java
+++ src/main/java/org/kuali/student/lum/workflow/CourseProposalCollaboratorPostProcessor.java
@@ -1,13 +1,11 @@
 package org.kuali.student.lum.workflow;
-import java.security.InvalidParameterException;
+
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.xml.namespace.QName;
-
-import org.kuali.rice.core.resourceloader.GlobalResourceLoader;
 import org.kuali.rice.kew.dto.DocumentContentDTO;
+import org.kuali.rice.kew.exception.WorkflowException;
 import org.kuali.rice.kew.postprocessor.ActionTakenEvent;
 import org.kuali.rice.kew.postprocessor.AfterProcessEvent;
 import org.kuali.rice.kew.postprocessor.BeforeProcessEvent;
@@ -17,20 +15,13 @@ import org.kuali.rice.kew.postprocessor.DocumentRouteLevelChange;
 import org.kuali.rice.kew.postprocessor.DocumentRouteStatusChange;
 import org.kuali.rice.kew.postprocessor.PostProcessor;
 import org.kuali.rice.kew.postprocessor.ProcessDocReport;
+import org.kuali.rice.kew.service.WorkflowDocument;
 import org.kuali.rice.kew.service.WorkflowInfo;
 import org.kuali.rice.kew.util.KEWConstants;
-import org.kuali.student.core.exceptions.DataValidationErrorException;
-import org.kuali.student.core.exceptions.DoesNotExistException;
-import org.kuali.student.core.exceptions.MissingParameterException;
-import org.kuali.student.core.exceptions.OperationFailedException;
-import org.kuali.student.core.exceptions.PermissionDeniedException;
-import org.kuali.student.core.exceptions.VersionMismatchException;
-import org.kuali.student.lum.lu.dto.CluInfo;
-import org.kuali.student.lum.lu.service.LuService;
-public class CluPostProcessor implements PostProcessor{
 
+public class CourseProposalCollaboratorPostProcessor implements PostProcessor{
 	private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger
-	.getLogger(CluPostProcessor.class);
+	.getLogger(CourseProposalCollaboratorPostProcessor.class);
 	
 	public ProcessDocReport afterProcess(AfterProcessEvent arg0)
 			throws Exception {
@@ -60,10 +51,10 @@ public class CluPostProcessor implements PostProcessor{
 	public ProcessDocReport doRouteStatusChange(DocumentRouteStatusChange statusChangeEvent)
 			throws Exception {
 		if (statusChangeEvent.getNewRouteStatus().equals(KEWConstants.ROUTE_HEADER_APPROVED_CD)) {			
-			LOG.info("CluApprovalPostProcessor: Status change to APPROVED");
+			LOG.info("CourseProposalCollaboratorPostProcessor: Status change to APPROVED");
 			WorkflowInfo workflowInfo = new WorkflowInfo();
 			DocumentContentDTO document = workflowInfo.getDocumentContent(statusChangeEvent.getRouteHeaderId());
-			updateCluStatus(document);						
+			addAdhocFYI(document);						
 		}
         return new ProcessDocReport(true, "");
 	}
@@ -74,16 +65,30 @@ public class CluPostProcessor implements PostProcessor{
 		return null;
 	}
 	
-    private void updateCluStatus(DocumentContentDTO document) throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException, DataValidationErrorException, org.kuali.student.core.exceptions.InvalidParameterException, VersionMismatchException{
-    	LuService luService = (LuService) GlobalResourceLoader.getService(new QName("http://edu.umd.ks/poc/lum/lu","LuService"));
-    	
-    	Pattern pattern = Pattern.compile("<cluId>\\s*([^<\\s]+)");
+    private void addAdhocFYI(DocumentContentDTO document) {
+    	Pattern pattern = Pattern.compile("<docId>\\s*([^<\\s]+)");
 		Matcher matcher = pattern.matcher(document.getApplicationContent());
 		matcher.find();
-		String cluId = matcher.group(1);
-    	
-		CluInfo clu = luService.getClu(cluId);
-    	clu.setState("Approved by Workflow");
-		luService.updateClu(cluId, clu);
+		String docId = matcher.group(1);
+
+    	pattern = Pattern.compile("<recipientPrincipalId>\\s*([^<\\s]+)");
+		matcher = pattern.matcher(document.getApplicationContent());
+		matcher.find();
+		String recipientPrincipalId = matcher.group(1);
+
+	   	pattern = Pattern.compile("<principalId>\\s*([^<\\s]+)");
+		matcher = pattern.matcher(document.getApplicationContent());
+		matcher.find();
+		String principalId = matcher.group(1);
+		
+		String annotation = "Collaborator Approved";
+	    
+       	try {
+			WorkflowDocument workflowDocument = new WorkflowDocument(docId, recipientPrincipalId);
+			workflowDocument.adHocRouteDocumentToPrincipal(KEWConstants.ACTION_REQUEST_FYI_REQ, annotation, principalId, "Request to Collaborate", true);
+		} catch (WorkflowException e) {
+			e.printStackTrace();
+		}
+		
     }
 }
