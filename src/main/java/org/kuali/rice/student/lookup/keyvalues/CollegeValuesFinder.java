3abbc280955285d057a41e16eca0b3a5913ebce8
depstein <depstein@kuali.org>
2009-06-29 14:58:33 -0400

    added college finder
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/team2/ks-rice-standalone/branches/kns-module-impl-br@5028 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/rice/student/lookup/keyvalues/CollegeValuesFinder.java
+++ src/main/java/org/kuali/rice/student/lookup/keyvalues/CollegeValuesFinder.java
@@ -6,8 +6,15 @@ package org.kuali.rice.student.lookup.keyvalues;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.namespace.QName;
+
+import org.kuali.rice.core.resourceloader.GlobalResourceLoader;
 import org.kuali.rice.kns.lookup.keyvalues.KeyValuesBase;
 import org.kuali.rice.kns.web.ui.KeyLabelPair;
+import org.kuali.student.core.organization.service.OrganizationService;
+import org.kuali.student.core.search.dto.QueryParamValue;
+import org.kuali.student.core.search.dto.Result;
+import org.kuali.student.core.search.dto.ResultCell;
 
 /**
  * @author delyea
@@ -17,8 +24,35 @@ public class CollegeValuesFinder extends KeyValuesBase {
 
 	public List<KeyLabelPair> getKeyValues() {
 		List<KeyLabelPair> colleges = new ArrayList<KeyLabelPair>();
-		// TODO Auto-generated method stub
-		colleges.add(new KeyLabelPair("",""));
+
+	   	OrganizationService orgService = (OrganizationService) GlobalResourceLoader.getService(new QName("http://org.kuali.student/core/organization","OrganizationService"));
+	   		   	
+	   	String searchTypeKey="org.search.orgQuickViewByOrgType";
+	   	
+	   	List<QueryParamValue> queryParamValues = new ArrayList<QueryParamValue>();
+	   	QueryParamValue qpOrgType = new QueryParamValue();
+	   	qpOrgType.setKey("org.queryParam.orgType");
+	   	qpOrgType.setValue("kuali.org.College");
+	   	queryParamValues.add(qpOrgType);
+	   	
+	   	try {
+			List<Result> results = orgService.searchForResults(searchTypeKey, queryParamValues);
+			for(Result result:results){
+				String orgId="";
+				String orgShortName="";
+				for(ResultCell resultCell:result.getResultCells()){
+					if("org.resultColumn.orgId".equals(resultCell.getKey())){
+						orgId=resultCell.getValue();
+					}else if("org.resultColumn.orgShortName".equals(resultCell.getKey())){
+						orgShortName=resultCell.getValue();
+					}
+				}
+				colleges.add(new KeyLabelPair(orgId, orgShortName));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+
 		return colleges;
 	}
 
