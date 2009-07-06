cf9ab6bd00ab0a073ccdcc4448f6c4a022b39e6c
depstein <depstein@kuali.org>
2009-07-06 15:37:33 -0400

    Checked if file exists before being passed to list of locations
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@5113 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-util/src/main/java/org/kuali/student/common/util/ModPropertyPlaceholderConfigurer.java
+++ ks-common-util/src/main/java/org/kuali/student/common/util/ModPropertyPlaceholderConfigurer.java
@@ -69,16 +69,19 @@ public class ModPropertyPlaceholderConfigurer extends
 			try{
 				Resource customConfigResource = new DefaultResourceLoader().getResource(location);
 		
-				Resource[] finalLocations = new Resource[locations.length+1];
-				int i=0;
-				for(Resource resource:locations){
-					finalLocations[i]=resource;
-					i++;
+				if(customConfigResource.exists()){
+					Resource[] finalLocations = new Resource[locations.length+1];
+					int i=0;
+					for(Resource resource:locations){
+						finalLocations[i] = resource;
+						i++;
+					}
+					finalLocations[i] = customConfigResource;
+					
+					super.setLocations(finalLocations);
+				}else{
+					logger.info("File does not exist:"+location);
 				}
-				finalLocations[i]=customConfigResource;
-				
-				super.setLocations(finalLocations);
-			
 			}catch(Exception e){
 				logger.info("Could not load custom properties from file:"+location);
 			}
