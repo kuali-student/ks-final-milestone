5e7b58ede1536bc1ff95463aa9e5c6684af8af7e
jyin <jyin@kuali.org>
2009-07-08 18:19:34 -0400

    fill the code
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@5139 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-util/src/main/java/org/kuali/student/common/validator/ModelDTOConstraintDataProvider.java
+++ ks-common-util/src/main/java/org/kuali/student/common/validator/ModelDTOConstraintDataProvider.java
@@ -1,28 +1,84 @@
 package org.kuali.student.common.validator;
 
-public class ModelDTOConstraintDataProvider implements ConstraintDataProvider {
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTO;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue.BooleanType;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue.ByteType;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue.CharacterType;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue.DateType;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue.DoubleType;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue.FloatType;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue.IntegerType;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue.ListType;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue.LongType;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue.MapType;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue.ModelDTOType;
+import org.kuali.student.common.ui.client.mvc.dto.ModelDTOValue.StringType;
 
+public class ModelDTOConstraintDataProvider implements ConstraintDataProvider {
+    ModelDTO modelDTO = null;    
+    
 	@Override
 	public String getObjectId() {
-		// TODO Auto-generated method stub
-		return null;
+	    if(modelDTO == null){
+	        return null;
+	    }
+	    ModelDTOValue modelDTOValue = modelDTO.get("id");
+	    if(modelDTOValue == null){
+	        return null;
+	    }
+		return ((StringType)modelDTOValue).get();
 	}
 
 	@Override
 	public Object getValue(String fieldKey) {
-		// TODO Auto-generated method stub
+		if(modelDTO == null){
+		    return null;    
+		}
+		ModelDTOValue modelDTOValue = modelDTO.get(fieldKey);
+		switch(modelDTOValue.getType()){
+            case STRING:
+                return ((StringType)modelDTOValue).get();
+            case CHARACTER:
+                return ((CharacterType)modelDTOValue).get();
+            case INTEGER:
+                return ((IntegerType)modelDTOValue).get();
+            case LONG:
+                return ((LongType)modelDTOValue).get();
+            case FLOAT:
+                return ((FloatType)modelDTOValue).get();
+            case DOUBLE:
+                return ((DoubleType)modelDTOValue).get();
+            case BYTE:
+                return ((ByteType)modelDTOValue).get();
+            case BOOLEAN:
+                return ((BooleanType)modelDTOValue).get();
+            case DATE:
+                return ((DateType)modelDTOValue).get();
+            case LIST:
+                return ((ListType)modelDTOValue).get();
+            case MAP:
+                return ((MapType)modelDTOValue).get();
+            case MODELDTO:
+                return ((ModelDTOType)modelDTOValue).get();
+		}
 		return null;
 	}
 
 	@Override
 	public Boolean hasField(String fieldKey) {
-		// TODO Auto-generated method stub
-		return null;
+		if(modelDTO == null){
+		    return false;
+		}
+		return modelDTO.get(fieldKey) != null;
 	}
 
 	@Override
 	public void initialize(Object o) {
-		// TODO Auto-generated method stub
+		if(o instanceof ModelDTO == false){
+		    return;
+		}
+		modelDTO = (ModelDTO) o;
 		
 	}
 
