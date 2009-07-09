2676313d24f4426529af3d8efeab0b432f8dd161
jyin <jyin@kuali.org>
2009-07-09 15:50:29 -0400

    throws exceptions
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@5161 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-ui/src/main/java/org/kuali/student/common/ui/client/validator/ModelDTOConstraintDataProvider.java
+++ ks-common-ui/src/main/java/org/kuali/student/common/ui/client/validator/ModelDTOConstraintDataProvider.java
@@ -19,74 +19,75 @@ import org.kuali.student.common.ui.server.mvc.dto.MapContext;
 import org.kuali.student.common.validator.ConstraintDataProvider;
 
 public class ModelDTOConstraintDataProvider implements ConstraintDataProvider {
-    ModelDTO modelDTO = null;    
-    
-	@Override
-	public String getObjectId() {
-	   
-	    if(modelDTO == null){
-	        return null;
-	    }
-	    ModelDTOValue modelDTOValue = modelDTO.get("id");
-	    if(modelDTOValue == null){
-	        return null;
-	    }
-		return ((StringType)modelDTOValue).get();
-	}
+    ModelDTO modelDTO = null;
 
-	@Override
-	public Object getValue(String fieldKey) {
-	
-		if(modelDTO == null){
-		    return null;    
-		}
-	
-		ModelDTOValue modelDTOValue = modelDTO.get(fieldKey);
-		if(modelDTOValue == null){
-		    return null;
-		}
-		switch(modelDTOValue.getType()){
+    @Override
+    public String getObjectId() {
+
+        if (modelDTO == null) {
+            return null;
+        }
+        ModelDTOValue modelDTOValue = modelDTO.get("id");
+        if (modelDTOValue == null) {
+            return null;
+        }
+        return ((StringType) modelDTOValue).get();
+    }
+
+    @Override
+    public Object getValue(String fieldKey) {
+
+        if (modelDTO == null) {
+            return null;
+        }
+
+        ModelDTOValue modelDTOValue = modelDTO.get(fieldKey);
+        if (modelDTOValue == null) {
+            return null;
+        }
+        switch (modelDTOValue.getType()) {
             case STRING:
-                return ((StringType)modelDTOValue).get();
+                return ((StringType) modelDTOValue).get();
             case CHARACTER:
-                return ((CharacterType)modelDTOValue).get();
+                return ((CharacterType) modelDTOValue).get();
             case INTEGER:
-                return ((IntegerType)modelDTOValue).get();
+                return ((IntegerType) modelDTOValue).get();
             case LONG:
-                return ((LongType)modelDTOValue).get();
+                return ((LongType) modelDTOValue).get();
             case FLOAT:
-                return ((FloatType)modelDTOValue).get();
+                return ((FloatType) modelDTOValue).get();
             case DOUBLE:
-                return ((DoubleType)modelDTOValue).get();
+                return ((DoubleType) modelDTOValue).get();
             case BYTE:
-                return ((ByteType)modelDTOValue).get();
+                return ((ByteType) modelDTOValue).get();
             case BOOLEAN:
-                return ((BooleanType)modelDTOValue).get();
+                return ((BooleanType) modelDTOValue).get();
             case DATE:
-                return ((DateType)modelDTOValue).get();
+                return ((DateType) modelDTOValue).get();
             case LIST:
-                return ((ListType)modelDTOValue).get();
+                return ((ListType) modelDTOValue).get();
             case MAP:
-                return ((MapType)modelDTOValue).get();
+                return ((MapType) modelDTOValue).get();
             case MODELDTO:
-                return ((ModelDTOType)modelDTOValue).get();
-		}
-		return null;
-	}
+                return ((ModelDTOType) modelDTOValue).get();
+        }
+        return null;
+    }
 
-	@Override
-	public Boolean hasField(String fieldKey) {
-		if(modelDTO == null){
-		    return false;
-		}
-		return modelDTO.get(fieldKey) != null;
-	}
+    @Override
+    public Boolean hasField(String fieldKey) {
+        if (modelDTO == null) {
+            return false;
+        }
+        return modelDTO.get(fieldKey) != null;
+    }
 
-	@Override
-	public void initialize(Object o) {
-	    if(o instanceof ModelDTO){
-	        System.out.println(o);
-	        modelDTO = (ModelDTO)o;
-	    }
-	}
+    @Override
+    public void initialize(Object o) {
+        if (o instanceof ModelDTO) {
+            modelDTO = (ModelDTO) o;
+        } else {
+            throw new IllegalArgumentException("The object must be an instance of ModelDTO");
+        }
+    }
 }
