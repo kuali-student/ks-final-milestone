a8002f8cf22da93de1b1f3d86cc32cc6d81fc5fd
gstruthe <gstruthe@kuali.org>
2009-07-29 20:47:40 -0400

    interfaces for CommentService
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@5360 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/client/async/CommentRpcServiceAsync.java
@@ -0,0 +1,57 @@
+package org.kuali.student.lum.lu.ui.comment;
+
+
+import java.util.List;
+
+import javax.jws.WebParam;
+
+
+import org.kuali.student.common.ui.client.service.BaseRpcService;
+import org.kuali.student.common.ui.client.service.BaseRpcServiceAsync;
+import org.kuali.student.core.comment.dto.CommentInfo;
+import org.kuali.student.lum.lu.ui.course.client.service.CluProposal;
+
+
+ 
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+public interface CommentRpcServiceAsync extends BaseRpcServiceAsync {
+    /**
+     * Adds a comment to a reference.
+     * @param referenceId identifier of reference
+     * @param referenceTypeKey reference type
+     * @param commentInfo detailed information about the comment
+     * @param detailed information about the comment
+     * @throws Exception 
+     */
+    public void addComment(String referenceId, String referenceTypeKey, CommentInfo commentInfo, AsyncCallback<CommentInfo> callback) throws Exception;
+    /**
+     * Retrieves comment information for a reference. The expected behavior is that if the caller is not authorized to invoke the getComments operation, a PERMISSION_DENIED error is returned. Assuming that the caller is authorized to invoke getComments, only comments that the caller is authorized to view are included in the returned commentInfoList; comments that the caller is unauthorized to view are filtered out of the return parameter.
+     * @param referenceId reference identifier
+     * @param referenceTypeKey reference type
+     * @param list of comment information
+     * @throws Exception
+     */
+    public void getComments(String referenceId, String referenceTypeKey, AsyncCallback<CommentInfo> callback) throws Exception;
+    /**
+     * Retrieves comment information for a reference of a particular type. The expected behavior is that if the caller is not authorized to invoke the getCommentsByType operation, a PERMISSION_DENIED error is returned. Assuming that the caller is authorized to invoke getCommentsByType, only comments that the caller is authorized to view are included in the returned commentInfoList; comments that the caller is unauthorized to view are filtered out of the return parameter.
+     * @param referenceId reference identifier
+     * @param referenceTypeKey reference type
+     * @param commentTypeKey comment type
+     * @param list of comment information
+     * @throws Exception
+     */
+    public void getCommentsByType(String referenceId, String referenceTypeKey, String commentTypeKey, AsyncCallback<CommentInfo> callback) throws Exception;
+
+    /**
+     * Updates a comment for a reference.
+     * @param referenceId identifier of reference
+     * @param referenceTypeKey reference type
+     * @param commentInfo detailed information about the comment
+     * @param detailed information about the comment
+     * @throws Exception 
+     */
+    public void updateComment(String referenceId, String referenceTypeKey, CommentInfo commentInfo, AsyncCallback<CommentInfo> callback) throws Exception;
+
+}
