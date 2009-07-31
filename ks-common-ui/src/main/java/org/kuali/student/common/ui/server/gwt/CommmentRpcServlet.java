5f064685232288a8a8a1dced3a625e67672b7a2d
bsmith <bsmith@kuali.org>
2009-07-31 14:00:16 -0400

    CommentRpcServlet
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@5406 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-common-ui/src/main/java/org/kuali/student/common/ui/server/gwt/CommmentRpcServlet.java
@@ -0,0 +1,37 @@
+package org.kuali.student.common.ui.server.gwt;
+
+import java.util.List;
+
+import org.kuali.student.common.ui.client.service.CommentRpcService;
+import org.kuali.student.core.comment.dto.CommentInfo;
+import org.kuali.student.core.comment.service.CommentService;
+
+public class CommmentRpcServlet extends BaseRpcGwtServletAbstract<CommentService> implements CommentRpcService {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public CommentInfo addComment(String referenceId, String referenceTypeKey,
+			CommentInfo commentInfo) throws Exception {
+		return service.addComment(referenceId, referenceTypeKey, commentInfo);
+	}
+
+	@Override
+	public List<CommentInfo> getComments(String referenceId,
+			String referenceTypeKey) throws Exception {
+		return service.getComments(referenceId, referenceTypeKey);
+	}
+
+	@Override
+	public List<CommentInfo> getCommentsByType(String referenceId,
+			String referenceTypeKey, String commentTypeKey) throws Exception {
+		return service.getCommentsByType(referenceId, referenceTypeKey, commentTypeKey);
+	}
+
+	@Override
+	public CommentInfo updateComment(String referenceId,
+			String referenceTypeKey, CommentInfo commentInfo) throws Exception {
+		return service.updateComment(referenceId, referenceTypeKey, commentInfo);
+	}
+
+}
