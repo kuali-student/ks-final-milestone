c232224fd99847f5867504351e51a51ecd63a4bf
nwright <nwright@kuali.org>
2009-09-08 15:14:03 -0400

    put away jars that need to to be added to maven
    git-svn-id: https://svn.kuali.org/repos/student/tools/maven-dictionary-generator@5681 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ lib-jars-to-add-to-local-maven-repository/add-jars-to-maven.cmd
@@ -0,0 +1,6 @@
+rem this should add to the local maven repository
+rem the google gdata files
+call C:\apache-maven-2.1.0\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-base -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-base-1.0.jar
+call C:\apache-maven-2.1.0\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-spreadsheet -Dversion=3.0 -Dpackaging=jar -Dfile= -Dfile=gdata-spreadsheet-3.0.jar
+call C:\apache-maven-2.1.0\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-core -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-core-1.0.jar
+pause
