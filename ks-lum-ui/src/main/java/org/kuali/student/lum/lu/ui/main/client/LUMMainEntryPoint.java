10400d8418847976f78d3f951bfbd7a05d59947b
wgomes <wgomes@kuali.org>
2009-09-09 16:05:05 -0400

    Remove old load dictionary code
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@5689 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/main/client/LUMMainEntryPoint.java
+++ ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/main/client/LUMMainEntryPoint.java
@@ -1,19 +1,14 @@
 package org.kuali.student.lum.lu.ui.main.client;
 
 
-import java.util.List;
-
 import org.kuali.student.common.ui.client.application.Application;
 import org.kuali.student.common.ui.client.application.ApplicationComposite;
 import org.kuali.student.common.ui.client.application.ApplicationContext;
+import org.kuali.student.common.ui.client.dictionary.DictionaryLoader;
 import org.kuali.student.common.ui.client.service.MessagesRpcService;
 import org.kuali.student.common.ui.client.service.SecurityRpcService;
 import org.kuali.student.common.ui.client.service.SecurityRpcServiceAsync;
-import org.kuali.student.core.dictionary.dto.ObjectStructure;
 import org.kuali.student.core.messages.dto.MessageList;
-import org.kuali.student.common.ui.client.dictionary.DictionaryHelper;
-import org.kuali.student.common.ui.client.dictionary.DictionaryManager;
-import org.kuali.student.lum.lu.ui.course.client.service.LuRpcService;
 import org.kuali.student.lum.lu.ui.main.client.controller.LUMApplicationManager;
 
 import com.google.gwt.core.client.EntryPoint;
@@ -24,6 +19,7 @@ import com.google.gwt.libideas.resources.client.ResourcePrototype;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.SerializationException;
 import com.google.gwt.user.client.rpc.SerializationStreamFactory;
+import com.google.gwt.user.client.rpc.SerializationStreamReader;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class LUMMainEntryPoint implements EntryPoint{
@@ -42,7 +38,9 @@ public class LUMMainEntryPoint implements EntryPoint{
 
         try {
             loadMessages(context);            
-            loadDictionary();
+            
+            DictionaryLoader.loadDictionary();          
+            
             loadApp(context);
         } catch (Exception e) {
             e.printStackTrace();
@@ -55,8 +53,9 @@ public class LUMMainEntryPoint implements EntryPoint{
         app.setContent(manager);
         RootPanel.get().add(app);
         if(manager.getCurrentView() == null)
-            manager.showDefaultView();        
+            manager.showDefaultView();
     }
+    
     private void loadMessages(final ApplicationContext context) throws SerializationException {
         MessageList messageList =  getMsgSerializedObject( );
         context.addMessages(messageList.getMessages());
@@ -67,34 +66,11 @@ public class LUMMainEntryPoint implements EntryPoint{
     {
         String serialized = getString( "i18nMessages" );
         SerializationStreamFactory ssf = GWT.create( MessagesRpcService.class); // magic
-        return (T)ssf.createStreamReader( serialized ).readObject();
+        SerializationStreamReader ssr = ssf.createStreamReader( serialized );
+        T ret = (T)ssr.readObject();
+        return ret;
     } 
-
-    private void loadDictionary() {
-
-        try {
-            GWT.log("Dictionary load starts", null);
-            List<String> types = getDictSerializedObject("objectTypes");
-            for (String key: types) {
-                ObjectStructure structure =  getDictSerializedObject( DictionaryHelper.buildJavaScriptKey(key));
-                DictionaryManager.getInstance().loadStructure(structure);
-            }
-            logDictionaryLoad();
-            GWT.log("Dictionary load ends", null);
-        } catch (SerializationException e) {
-            GWT.log("loadDictionary failed " ,  e);
-            e.printStackTrace();
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public  <T> T getDictSerializedObject(String key  ) throws SerializationException
-    {
-        SerializationStreamFactory ssf = GWT.create( LuRpcService.class); // magic
-        String serialized = getString( key );
-        return (T)ssf.createStreamReader( serialized ).readObject();
-    }
-    
+   
     public  native String getString(String name) /*-{
         return eval("$wnd."+name);
     }-*/;
@@ -105,6 +81,7 @@ public class LUMMainEntryPoint implements EntryPoint{
         securityRpc.getPrincipalUsername(new AsyncCallback<String>(){
             public void onFailure(Throwable caught) {
                 context.setUserId("Unknown");
+                initScreen();
             }
 
             @Override
@@ -127,19 +104,6 @@ public class LUMMainEntryPoint implements EntryPoint{
          return injectString;
     }
     
-    private void logDictionaryLoad() {
-        for (String s : DictionaryManager.getInstance().getTypes()) {
-            GWT.log("DictionaryManager Loaded OBJECT TYPE: " + s, null);
-//            String objectKey = s.substring(0, s.indexOf(":"));
-//            String type = s.substring(s.indexOf(":")+1, s.lastIndexOf(":"));
-//            String state = s.substring(s.lastIndexOf(":")+1);
-//            Map<String, Field> fields = LUDictionaryManager.getInstance().getFields(objectKey, type, state);
-//            for (String f : fields.keySet()) {
-//                GWT.log("Contains field: " + f, null);
-//                
-//            }
-        }
-    }
     
 
 }
