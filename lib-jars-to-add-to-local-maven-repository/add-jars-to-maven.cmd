d06e0269088efcae751f5f43fe2e250910622493
nwright <nwright@kuali.org>
2009-09-15 14:02:40 -0400

    added the missing google-collect jar to the ones that need to be manually added to the maven repository
    git-svn-id: https://svn.kuali.org/repos/student/tools/maven-dictionary-generator@5728 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib-jars-to-add-to-local-maven-repository/add-jars-to-maven.cmd
+++ lib-jars-to-add-to-local-maven-repository/add-jars-to-maven.cmd
@@ -3,4 +3,6 @@ rem the google gdata files
 call C:\apache-maven-2.1.0\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-base -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-base-1.0.jar
 call C:\apache-maven-2.1.0\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-spreadsheet -Dversion=3.0 -Dpackaging=jar -Dfile= -Dfile=gdata-spreadsheet-3.0.jar
 call C:\apache-maven-2.1.0\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-core -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-core-1.0.jar
+call C:\apache-maven-2.1.0\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-collect -Dversion=1.0 -Dpackaging=jar -Dfile=google-collect-1.0-rc1.jar
+
 pause
