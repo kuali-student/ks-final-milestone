d4ba45334547391dabd3c19ebdb657df0d368915
wgomes <wgomes@kuali.org>
2009-09-24 14:48:24 -0400

    Add comment types for lum
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@5847 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-web/src/main/resources/ks-comment.sql
+++ ks-core-web/src/main/resources/ks-comment.sql
@@ -4,7 +4,9 @@ INSERT INTO KS_RICH_TEXT_T (ID, FORMATTED, PLAIN) VALUES ('RT-DESC-COMMENT-1', '
 
 
 // Reference Type
-INSERT INTO KSCO_REFERence_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('referenceType.type1', 'A Basic Reference 1', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'}, 'Reference 1')
+INSERT INTO KSCO_REFERENCE_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('referenceType.clu', 'Clu reference type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'}, 'Clu')
+/
+INSERT INTO KSCO_REFERENCE_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('referenceType.type1', 'A Basic Reference 1', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'}, 'Reference 1')
 /
 
 
@@ -17,6 +19,10 @@ INSERT INTO KSCO_REFERENCE(ID,REFERENCE_ID,REFERENCE_TYPE) VALUES('1234569ASDF',
 /
 
 // Comment Type
+INSERT INTO KSCO_COMMENT_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('commentType.kuali.lu.type.CreditCourse.draft', 'Credit Course Proposal', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'}, 'Proposal')
+/
+INSERT INTO KSCO_COMMENT_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('commentType.kuali.lu.type.CreditCourse.submitted', 'Credit Course Submitted', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'}, 'Submitted')
+/
 INSERT INTO KSCO_COMMENT_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('commentType.type1', 'A Basic Comment 1', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'}, 'Comment 1')
 /
 INSERT INTO KSCO_COMMENT_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('commentType.type2', 'A Basic Comment 2', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'}, 'Comment 2')
@@ -30,6 +36,8 @@ INSERT INTO KSCO_COMMENT (ID, RT_DESCR_ID, REFERENCE, EFF_DT, EXPIR_DT, TYPE, ST
 /
 
 // Tag Type
+INSERT INTO KSCO_TAG_TYPE(TYPE_KEY,NAME,TYPE_DESC,EFF_DT,EXPIR_DT) values ('tagType.default','Default','Default tag type',{ts '2000-01-01 00:00:00.0'},{ts '2000-12-31 00:00:00.0'})
+/
 INSERT INTO KSCO_TAG_TYPE(TYPE_KEY,NAME,TYPE_DESC,EFF_DT,EXPIR_DT) values ('tagType.default','Default','Default tag type',{ts '2000-01-01 00:00:00.0'},{ts '2000-12-31 00:00:00.0'})
 /
 
