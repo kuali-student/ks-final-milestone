84174339884f312ddafc2f3b21979bd19d8a5d6e
nwright <nwright@kuali.org>
2009-09-25 13:14:54 -0400

    got command line generator working for search and fixed usage for dictionary
    git-svn-id: https://svn.kuali.org/repos/student/tools/maven-dictionary-generator@5869 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- one-jar/run-dictionary-generator.cmd
+++ one-jar/run-dictionary-generator.cmd
@@ -1,5 +1,6 @@
 rem this is the usage information
 java -jar kuali-dictionary-generator.jar
+pause
 rem this will run the dicitonary generator
 java -jar kuali-dictionary-generator.jar  "..\trunk\src\test\resources\dictionary\type-state configuration.xls" "..\trunk\src\test\resources\dictionary\lu-dictionary-config-generated-excel.xml"
 pause
