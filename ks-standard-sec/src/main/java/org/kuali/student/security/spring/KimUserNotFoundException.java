735b01d9c578862ede9c4cc1cf74a7be7dda63b2
rdiaz <rdiaz@kuali.org>
2009-10-05 15:26:49 -0400

    updated comments
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-security/branches/ks-security-dev@6045 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-standard-sec/src/main/java/org/kuali/student/security/spring/KimUserNotFoundException.java
+++ ks-standard-sec/src/main/java/org/kuali/student/security/spring/KimUserNotFoundException.java
@@ -5,11 +5,10 @@ import org.springframework.security.AccountStatusException;
 
 /**
  * 
- * This is a custom exception designed to be trwon for developement purposes when only Kim is used for 
- * authentication. In production system this exception has be replaced with UsernameNotFoundException.
- * This exception is thrown so that when rice kim fails to authenticate the user we donot want the control to pass
- * onto the default user service.  
- * AccountStatusException is extended because spring security has the desired behaviour only for AccountStatusException.
+ * This is a custom exception designed to be thrown when only Kim is used for authentication
+ * This exception is thrown so that when rice-kim fails to authenticate the user, we do not want
+ * the next authentication provider on the list to be used.  
+ * AccountStatusException is extended because spring security has the desired behavior only for AccountStatusException.
  * @author Kuali Student Team: NeeravA
  *
  */
