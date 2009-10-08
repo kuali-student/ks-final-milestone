a5b8032f31ed072b953d54998ec5acde88c23aaa
wiljohns <wiljohns@kuali.org>
2009-10-08 18:27:00 -0400

    fixed Home url
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@6152 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-ui/src/main/java/org/kuali/student/common/ui/client/application/Header.java
+++ ks-common-ui/src/main/java/org/kuali/student/common/ui/client/application/Header.java
@@ -146,7 +146,7 @@ public class Header extends Composite {
         actionlistPanel.add(buildActionListPanel()); // Open Action List Panel from rice
         // Always have logout and preferences options
         linkItems.add(new HeaderLinkItem("Preferences", "Create, modify or delete user preferences", "Preferences not yet implemented"));
-        linkItems.add(new HeaderLinkItem("Home", "Return to home page", "/"));
+        linkItems.add(new HeaderLinkItem("Home", "Return to home page", GWT.getModuleBaseURL() + "../"));
         linkItems.add(new HeaderLinkItem("Logout", "End current Kuali Student session", GWT.getModuleBaseURL()+"../j_spring_security_logout"/*"Logout not yet implemented"*/));
 
         for (HeaderLinkItem i : linkItems) {
