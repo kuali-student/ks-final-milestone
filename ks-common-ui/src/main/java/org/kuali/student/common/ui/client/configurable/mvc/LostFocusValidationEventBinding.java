04f042d0e07a4d393697dca6d29bdedc06736fa2
bsmith <bsmith@kuali.org>
2009-10-08 17:54:13 -0400

    on value change event added, temporary fix
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@6147 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-ui/src/main/java/org/kuali/student/common/ui/client/configurable/mvc/LostFocusValidationEventBinding.java
+++ ks-common-ui/src/main/java/org/kuali/student/common/ui/client/configurable/mvc/LostFocusValidationEventBinding.java
@@ -5,6 +5,9 @@ import org.kuali.student.common.ui.client.mvc.Callback;
 import com.google.gwt.event.dom.client.BlurEvent;
 import com.google.gwt.event.dom.client.BlurHandler;
 import com.google.gwt.event.dom.client.HasBlurHandlers;
+import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.ui.Widget;
 
 public class LostFocusValidationEventBinding  implements ValidationEventBinding {
@@ -18,6 +21,16 @@ public class LostFocusValidationEventBinding  implements ValidationEventBinding 
                 	fd.getValidationRequestCallback().exec(true);
                 }
             });
+        }
+        else if(w instanceof HasValueChangeHandlers){
+        	((HasValueChangeHandlers) w).addValueChangeHandler(new ValueChangeHandler(){
+
+				@Override
+				public void onValueChange(ValueChangeEvent event) {
+					fd.setHasHadFocus(true);
+					fd.getValidationRequestCallback().exec(true);	
+				}
+			});
         } else {
             //TODO address the exception
           //  throw new IllegalArgumentException("It must implements HasBlurHandlers interface.");
