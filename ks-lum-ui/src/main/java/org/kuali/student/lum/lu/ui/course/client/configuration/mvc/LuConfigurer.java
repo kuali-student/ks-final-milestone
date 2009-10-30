70a60f9c3d773e3e9207cb4e4c726f9dfa65ae91
zzraly <zzraly@kuali.org>
2009-10-30 13:48:56 -0400

    Reverted temporary change that should not be checked in
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@6467 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/configuration/mvc/LuConfigurer.java
+++ ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/configuration/mvc/LuConfigurer.java
@@ -95,13 +95,10 @@ public class LuConfigurer {
         layout.addSection(new String[] {getLabel(LUConstants.PROPOSAL_INFORMATION_LABEL_KEY)}, generateGovernanceSection());
         layout.addSection(new String[] {getLabel(LUConstants.PROPOSAL_INFORMATION_LABEL_KEY)}, generateCourseLogisticsSection());
         layout.addSection(new String[] {getLabel(LUConstants.ACADEMIC_CONTENT_LABEL_KEY)}, generateCourseInfoSection());
-    //    layout.addSection(new String[] {getLabel(LUConstants.ACADEMIC_CONTENT_LABEL_KEY)}, generateLearningObjectivesSection());
-    //    layout.addSection(new String[] {getLabel(LUConstants.STUDENT_ELIGIBILITY_LABEL_KEY)}, generateCourseRequisitesSection());
+        layout.addSection(new String[] {getLabel(LUConstants.ACADEMIC_CONTENT_LABEL_KEY)}, generateLearningObjectivesSection());
+        layout.addSection(new String[] {getLabel(LUConstants.STUDENT_ELIGIBILITY_LABEL_KEY)}, generateCourseRequisitesSection());
         layout.addSection(new String[] {getLabel(LUConstants.ADMINISTRATION_LABEL_KEY)}, generateActiveDatesSection());
-        layout.addSection(new String[] {getLabel(LUConstants.ADMINISTRATION_LABEL_KEY)}, generateFinancialsSection());
-
-//        addProgramRequirements(layout);
-        
+        layout.addSection(new String[] {getLabel(LUConstants.ADMINISTRATION_LABEL_KEY)}, generateFinancialsSection());        
         layout.addSection(new String[] {getLabel(LUConstants.SUMMARY_LABEL_KEY)}, generateSummarySection());
 
         layout.addTool(new CollaboratorTool());
