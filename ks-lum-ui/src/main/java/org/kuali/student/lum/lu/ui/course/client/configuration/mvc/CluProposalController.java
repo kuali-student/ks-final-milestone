89b770cec5608aa476d0d0963b2b46aada855cbb
depstein <depstein@kuali.org>
2009-10-30 15:33:16 -0400

    Made sure all sections in a cluProposal are updated before save is called, added save call when doing approve/submit and a callback to the workflow action so all workflow actions cause a save first
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@6470 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/configuration/mvc/CluProposalController.java
+++ ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/configuration/mvc/CluProposalController.java
@@ -279,9 +279,9 @@ public class CluProposalController extends PagedSectionLayout{
             
             getCurrentView().updateModel();
             
-//            for(View sectionView : orderedSectionViews){
-//            	sectionView.updateModel();
-//            }
+            for(View sectionView : orderedSectionViews){
+            	sectionView.updateModel();
+            }
             
             saveProposalClu(saveActionEvent);
 //            processingSave=true;
