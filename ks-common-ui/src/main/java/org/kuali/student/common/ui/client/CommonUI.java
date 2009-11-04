1398a78ea8bbce9be17c284445a5f54849a77e25
bsmith <bsmith@kuali.org>
2009-11-03 19:43:08 -0500

    updated common ui style injection through theme
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@6512 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-ui/src/main/java/org/kuali/student/common/ui/client/CommonUI.java
+++ ks-common-ui/src/main/java/org/kuali/student/common/ui/client/CommonUI.java
@@ -14,31 +14,16 @@
  */
 package org.kuali.student.common.ui.client;
 
-import org.kuali.student.common.ui.client.css.KSCommonResources;
+import org.kuali.student.common.ui.client.theme.Theme;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.StyleElement;
 import com.google.gwt.libideas.client.StyleInjector;
-import com.google.gwt.libideas.resources.client.CssResource;
-import com.google.gwt.libideas.resources.client.ResourcePrototype;
 
 public class CommonUI implements EntryPoint {
     public StyleElement commonStyle;
 
 	public void onModuleLoad() {
-	    final String injectString = this.getCssString();
-        commonStyle = StyleInjector.injectStylesheet(injectString);
-	}
-	
-	public String getCssString(){
-	       String injectString = "";
-	        for(ResourcePrototype r: KSCommonResources.INSTANCE.getResources()){
-	            if(r instanceof CssResource){
-	                if(((CssResource)r).getText() != null){
-	                    injectString = injectString + "\n" + (((CssResource)r).getText());
-	                }
-	            }
-	        }
-	        return injectString;
+        commonStyle = StyleInjector.injectStylesheet(Theme.INSTANCE.getCommonCss().getCssString());
 	}
 }
