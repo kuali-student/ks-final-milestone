fd5a531d78be4f5a35953b72e96fade6febdb651
wiljohns <wiljohns@kuali.org>
2009-11-05 01:44:40 -0500

    cleanup required for initial testing of orch code, have to break it apart into multiple commits or subclipse throws an error
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@6546 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-ui/src/main/java/org/kuali/student/core/organization/ui/client/mvc/model/OrgProposalModel.java
+++ ks-core-ui/src/main/java/org/kuali/student/core/organization/ui/client/mvc/model/OrgProposalModel.java
@@ -1,6 +1,6 @@
 package org.kuali.student.core.organization.ui.client.mvc.model;
 
-import org.kuali.student.common.assembly.Data;
+import org.kuali.student.common.assembly.client.Data;
 
 public class OrgProposalModel extends Data {
 
