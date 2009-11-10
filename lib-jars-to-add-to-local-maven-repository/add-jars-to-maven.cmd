265f3042444b3213902a441437c3092fb6e869b0
nwright <nwright@kuali.org>
2009-11-10 13:52:29 -0500

    got class narrowing done
    git-svn-id: https://svn.kuali.org/repos/student/tools/maven-dictionary-generator@6619 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib-jars-to-add-to-local-maven-repository/add-jars-to-maven.cmd
+++ lib-jars-to-add-to-local-maven-repository/add-jars-to-maven.cmd
@@ -1,8 +1,12 @@
 rem this should add to the local maven repository
 rem the google gdata files
-call C:\apache-maven-2.1.0\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-base -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-base-1.0.jar
-call C:\apache-maven-2.1.0\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-spreadsheet -Dversion=3.0 -Dpackaging=jar -Dfile= -Dfile=gdata-spreadsheet-3.0.jar
-call C:\apache-maven-2.1.0\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-core -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-core-1.0.jar
-call C:\apache-maven-2.1.0\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-collect -Dversion=1.0 -Dpackaging=jar -Dfile=google-collect-1.0-rc1.jar
+SETLOCAL
+set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_16
+call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-base -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-base-1.0.jar
+call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-spreadsheet -Dversion=3.0 -Dpackaging=jar -Dfile= -Dfile=gdata-spreadsheet-3.0.jar
+call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-core -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-core-1.0.jar
+call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-collect -Dversion=1.0 -Dpackaging=jar -Dfile=google-collect-1.0-rc1.jar
+call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgroupId=com.sun.xml.bind -DartifactId=jaxb-xjc -Dversion=2.1.8 -Dpackaging=jar -Dfile=jaxb-xjc-2.1.8.jar
+call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgroupId=com.sun.xml.bind -DartifactId=jaxb-xjc -Dversion=2.1.8 -Dpackaging=jar -Dfile=jaxb-xjc-2.1.8-sources.jar -Dclassifier=sources
 
 pause
