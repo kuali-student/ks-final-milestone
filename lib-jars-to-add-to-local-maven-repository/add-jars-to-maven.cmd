4e48e878a9f6f6bcc469af0856dad334653317a0
nwright <nwright@kuali.org>
2009-11-10 13:57:06 -0500

    added back in code model instead of jaxb just in case
    git-svn-id: https://svn.kuali.org/repos/student/tools/maven-dictionary-generator@6620 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib-jars-to-add-to-local-maven-repository/add-jars-to-maven.cmd
+++ lib-jars-to-add-to-local-maven-repository/add-jars-to-maven.cmd
@@ -6,7 +6,13 @@ call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgroupId=com.google.gda
 call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-spreadsheet -Dversion=3.0 -Dpackaging=jar -Dfile= -Dfile=gdata-spreadsheet-3.0.jar
 call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-core -Dversion=1.0 -Dpackaging=jar -Dfile=gdata-core-1.0.jar
 call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-collect -Dversion=1.0 -Dpackaging=jar -Dfile=google-collect-1.0-rc1.jar
+
+@rem this next two jaxb-xjc contain codemodel
 call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgroupId=com.sun.xml.bind -DartifactId=jaxb-xjc -Dversion=2.1.8 -Dpackaging=jar -Dfile=jaxb-xjc-2.1.8.jar
 call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgroupId=com.sun.xml.bind -DartifactId=jaxb-xjc -Dversion=2.1.8 -Dpackaging=jar -Dfile=jaxb-xjc-2.1.8-sources.jar -Dclassifier=sources
+rem call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgeneratePom=true -DgroupId=com.sun.codemodel -DartifactId=codemodel -Dversion=2.2 -Dpackaging=jar -Dfile=codemodel-2.2.jar
+rem call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgeneratePom=true -DgroupId=com.sun.codemodel -DartifactId=codemodel -Dversion=2.2 -Dpackaging=jar -Dfile=codemodel-2.2-sources.jar -Dclassifier=sources
+rem call C:\apache-maven-2.2.1\bin\mvn install:install-file -DgeneratePom=true -DgroupId=com.sun.codemodel -DartifactId=codemodel -Dversion=2.2 -Dpackaging=jar -Dfile=codemodel-2.2-javadoc.jar -Dclassifier=javadoc
+
 
 pause
