a298f152669f33fc13e1d962fd8cda2b10aa8b69
lcarlsen <lcarlsen@kuali.org>
2010-01-19 21:15:55 -0500

    Added new statement service
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@7389 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-core-impl/src/main/java/org/kuali/student/core/statement/dao/impl/StatementDaoImpl.java
@@ -0,0 +1,8 @@
+package org.kuali.student.core.statement.dao.impl;
+
+import org.kuali.student.core.dao.impl.AbstractSearchableCrudDaoImpl;
+import org.kuali.student.core.statement.dao.StatementDao;
+
+public class StatementDaoImpl extends AbstractSearchableCrudDaoImpl implements StatementDao {
+
+}
---
+++ ks-core-impl/src/main/java/org/kuali/student/core/statement/service/impl/StatementAssembler.java
@@ -0,0 +1,7 @@
+package org.kuali.student.core.statement.service.impl;
+
+import org.kuali.student.core.service.impl.BaseAssembler;
+
+public class StatementAssembler extends BaseAssembler {
+
+}
