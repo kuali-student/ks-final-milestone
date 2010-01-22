059a8b9990b2d3adfd1088ce15ffc13ee351abd2
jcaddel <jcaddel@kuali.org>
2010-01-22 14:28:24 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/student/tags/1.0.0-m3@7504 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-impl/src/main/java/org/kuali/student/core/statement/dao/impl/StatementDaoImpl.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.student.core.statement.dao.impl;
-
-import org.kuali.student.core.dao.impl.AbstractSearchableCrudDaoImpl;
-import org.kuali.student.core.statement.dao.StatementDao;
-
-public class StatementDaoImpl extends AbstractSearchableCrudDaoImpl implements StatementDao {
-
-}
--- ks-core/ks-core-impl/src/main/java/org/kuali/student/core/statement/service/impl/StatementAssembler.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.student.core.statement.service.impl;
-
-import org.kuali.student.core.service.impl.BaseAssembler;
-
-public class StatementAssembler extends BaseAssembler {
-
-}
