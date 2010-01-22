086d4efd7e16d5dc156696ebff9119edfee37116
wgomes <wgomes@kuali.org>
2010-01-22 10:18:54 -0500

    Fix circular reference for lo for spring based orch dictionary.
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@7494 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum-ui/src/main/java/org/kuali/student/lum/lu/assembly/CreditCourseProposalAssembler.java
+++ ks-lum-ui/src/main/java/org/kuali/student/lum/lu/assembly/CreditCourseProposalAssembler.java
@@ -280,8 +280,9 @@ public class CreditCourseProposalAssembler implements Assembler<Data, Void> {
     public Metadata getMetadata(String type, String state) throws AssemblyException {
         // TODO overriding the specified type isn't a good thing, but in other cases the type needs to be specified by the caller
         //this needs to be filtered, but not sure how to do that if an assembler needs metadata itself
-        //Metadata metadata = metadataService.getMetadata(CREDIT_COURSE_PROPOSAL_DATA_TYPE, PROPOSAL_TYPE_CREATE_COURSE, state);
-        Metadata metadata = new CreditCourseProposalMetadata().getMetadata(PROPOSAL_TYPE_CREATE_COURSE, state);
+        Metadata metadata = metadataService.getMetadata(CREDIT_COURSE_PROPOSAL_DATA_TYPE, PROPOSAL_TYPE_CREATE_COURSE, state);
+        
+        //Metadata metadata = new CreditCourseProposalMetadata().getMetadata(PROPOSAL_TYPE_CREATE_COURSE, state);
         
         //Get permissions for course
         Map<String, String> permissions = getPermissions("kuali.lu.type.CreditCourse");
