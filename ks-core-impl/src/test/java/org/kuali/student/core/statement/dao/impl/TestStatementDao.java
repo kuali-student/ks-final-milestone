88e194362d47ac7039b1e65f1f25a3f75a13a9cd
stse <stse@kuali.org>
2010-01-25 15:55:58 -0500

    Test case for retrieving statement header template.
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@7595 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-impl/src/test/java/org/kuali/student/core/statement/dao/impl/TestStatementDao.java
+++ ks-core-impl/src/test/java/org/kuali/student/core/statement/dao/impl/TestStatementDao.java
@@ -1,5 +1,6 @@
 package org.kuali.student.core.statement.dao.impl;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -16,7 +17,12 @@ import org.kuali.student.core.exceptions.MissingParameterException;
 import org.kuali.student.core.exceptions.OperationFailedException;
 import org.kuali.student.core.exceptions.PermissionDeniedException;
 import org.kuali.student.core.statement.dao.StatementDao;
+import org.kuali.student.core.statement.entity.ReqComponent;
+import org.kuali.student.core.statement.entity.ReqComponentType;
+import org.kuali.student.core.statement.entity.ReqComponentTypeNLTemplate;
+import org.kuali.student.core.statement.entity.Statement;
 import org.kuali.student.core.statement.entity.StatementType;
+import org.kuali.student.core.statement.entity.StatementTypeHeaderTemplate;
 
 @PersistenceFileLocation("classpath:META-INF/statement-persistence.xml")
 public class TestStatementDao extends AbstractTransactionalDaoTest {
@@ -31,5 +37,63 @@ public class TestStatementDao extends AbstractTransactionalDaoTest {
         assertEquals(3, stmtTypeList.size());
     }   
     
+    @Test
+    public void testGetReqComponentTypes() throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException{
+        List<ReqComponentType> reqCompTypeList = dao.find(ReqComponentType.class);
+        
+        assertNotNull(reqCompTypeList);
+        assertEquals(8, reqCompTypeList.size());
+    }
     
+    @Test
+    public void testGetReqCompNLTemplate() throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException{
+        ReqComponentType reqComp  = dao.fetch(ReqComponentType.class, "kuali.reqCompType.courseList.nof");
+
+        List<ReqComponentTypeNLTemplate> templates = reqComp.getNlUsageTemplates();
+
+        assertEquals(templates.size(), 3);
+        
+        ReqComponentTypeNLTemplate template = null;
+        if (templates.get(0).getNlUsageTypeKey().equals("KUALI.CATALOG")) {
+            template = templates.get(0);
+        } else {
+            template = templates.get(1);
+        }
+        assertEquals("KUALI.CATALOG", template.getNlUsageTypeKey());
+        assertTrue(template.getTemplate().startsWith("Student must have completed $expectedValue"));
+    }
+
+    @Test
+    public void testGetStatementHeaderTemplate() throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException{
+        StatementType stmtType  = dao.fetch(StatementType.class, "kuali.luStatementType.prereqAcademicReadiness");
+
+        List<StatementTypeHeaderTemplate> templates = stmtType.getHeaders();
+
+        StatementTypeHeaderTemplate header = templates.get(0);
+        assertEquals(templates.size(), 2);
+        assertEquals("KUALI.CATALOG", header.getNlUsageTypeKey());
+        // FIXME should the statement header template be "$clu.getLongName()" instead of 
+        //       "$clu.getOfficialIdentifier().getLongName()"? 
+        //       in the old test sql file ks-lu.sql it is "$clu.getOfficialIdentifier().getLongName()" 
+        //       but in ks-statement.sql it is "$clu.getLongName()".
+        assertEquals(header.getTemplate(), "Requirement for $clu.getLongName(): ");
+    }
+
+    @Test
+    public void testGetReqComponents() throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException{
+        List<String> reqComponentIdList = Arrays.asList(new String[] {"REQCOMP-NL-1","REQCOMP-NL-2", "REQCOMP-NL-3", "REQCOMP-NL-4"});
+        List<ReqComponent> reqCompList = dao.getReqComponents(reqComponentIdList);
+        
+        assertNotNull(reqCompList);
+        assertEquals(4, reqCompList.size());
+    }
+    
+    @Test
+    public void testGetStatements() throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException{
+        List<String> statementIdList = Arrays.asList(new String[] {"STMT-1","STMT-2"});
+        List<Statement> stmtList = dao.getStatements(statementIdList);
+        
+        assertNotNull(stmtList);
+        assertEquals(2, stmtList.size());
+    }   
 }
