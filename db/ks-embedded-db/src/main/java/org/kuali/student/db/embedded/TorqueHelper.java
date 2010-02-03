bccc4195034275227417ffacff7cb64183fbc78b
jcaddel <jcaddel@kuali.org>
2010-02-03 12:54:10 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/branches/1.0.0-m3@7812 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- db/ks-embedded-db/src/main/java/org/kuali/student/db/embedded/TorqueHelper.java
+++ db/ks-embedded-db/src/main/java/org/kuali/student/db/embedded/TorqueHelper.java
@@ -1,5 +1,12 @@
 package org.kuali.student.db.embedded;
 
 public class TorqueHelper {
+	public static void main(String[] args) {
+		try {
+
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
 
 }
