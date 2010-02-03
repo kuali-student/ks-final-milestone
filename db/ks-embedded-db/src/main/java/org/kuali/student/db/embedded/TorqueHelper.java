7d84902e8c442c6332573d61cfbf0441f3e3bfba
jcaddel <jcaddel@kuali.org>
2010-02-03 13:04:15 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/branches/1.0.0-m3@7813 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- db/ks-embedded-db/src/main/java/org/kuali/student/db/embedded/TorqueHelper.java
+++ db/ks-embedded-db/src/main/java/org/kuali/student/db/embedded/TorqueHelper.java
@@ -2,8 +2,14 @@ package org.kuali.student.db.embedded;
 
 public class TorqueHelper {
 	public static void main(String[] args) {
-		try {
+		new TorqueHelper().run();
+	}
 
+	public void run() {
+		try {
+			String filename = "C:/workspace/1.0.0-m3/db/ks-embedded-db/src/main/torque/schema/schema.xml";
+			String contents = IOUtil.getInstance().read(filename);
+			System.out.println(contents);
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
