26901d5b1bd45870b88ed2f0d9780a3605a3a901
jcaddel <jcaddel@kuali.org>
2010-02-09 15:31:21 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/branches/1.0.0-m3@7938 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- security/ks-standard-sec/src/main/java/org/kuali/student/security/cxf/interceptors/SamlTokenCxfInInterceptor.java
+++ security/ks-standard-sec/src/main/java/org/kuali/student/security/cxf/interceptors/SamlTokenCxfInInterceptor.java
@@ -15,7 +15,6 @@
 
 package org.kuali.student.security.cxf.interceptors;
 
-import java.io.StringWriter;
 import java.util.Map;
 import java.util.Vector;
 
@@ -23,10 +22,6 @@ import javax.xml.namespace.QName;
 import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.apache.cxf.binding.soap.SoapMessage;
 import org.apache.cxf.headers.Header;
@@ -41,72 +36,72 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+public class SamlTokenCxfInInterceptor extends WSS4JInInterceptor {
 
-public class SamlTokenCxfInInterceptor extends WSS4JInInterceptor{
-    
-    private String samlIssuerForUser = null;
-    
-    public SamlTokenCxfInInterceptor(Map<String, Object> properties) {
-        super(properties);
-    }
-    
-    @Override
-    protected void computeAction(SoapMessage msg, RequestData reqData) {
-        super.computeAction(msg, reqData);
-    }
+	private String samlIssuerForUser = null;
 
-    @Override
-    protected void doResults(SoapMessage msg, String actor, SOAPMessage doc, Vector wsResult) throws SOAPException, XMLStreamException {
-        super.doResults(msg, actor, doc, wsResult);
-        
-        QName wsseQN = new QName("http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd", "Security");
-        if(msg.hasHeader(wsseQN)){
-            Header wsseHeader = msg.getHeader(wsseQN);
-            
-            if(wsseHeader != null){
-                Node domSecurityHeader = (Node) wsseHeader.getObject();
-                NodeList nodeList = domSecurityHeader.getChildNodes();
-                Node childNode = null;
-                
-                for(int i=0; i < nodeList.getLength(); i++){
-                    childNode = nodeList.item(i);
-                    
-                    if((childNode.getNodeName().equals("Assertion")) && (childNode.getNodeType() == Node.ELEMENT_NODE)){
-                        SAMLTokenProcessor stp = new SAMLTokenProcessor();
-                        
-                        try {
-                            SAMLAssertion samlAssertion = stp.handleSAMLToken((Element)childNode);
-                            
-                            if(samlAssertion.getIssuer().equals(samlIssuerForUser)){
-                                CasAuthenticationToken cat = (CasAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-                                cat.setDetails(samlAssertion);
-                                break;
-                            }                           
-                        }catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }    
-            }
-        }
-    }
+	public SamlTokenCxfInInterceptor(Map<String, Object> properties) {
+		super(properties);
+	}
 
-    @Override
-    public void handleMessage(SoapMessage msg) throws Fault {
-        super.handleMessage(msg);
-    }
+	@Override
+	protected void computeAction(SoapMessage msg, RequestData reqData) {
+		super.computeAction(msg, reqData);
+	}
 
-    @Override
-    public void setIgnoreActions(boolean i) {
-        super.setIgnoreActions(i);
-    }
+	@Override
+	@SuppressWarnings("unchecked")
+	protected void doResults(SoapMessage msg, String actor, SOAPMessage doc, Vector wsResult) throws SOAPException, XMLStreamException {
+		super.doResults(msg, actor, doc, wsResult);
 
-    public String getSamlIssuerForUser() {
-        return samlIssuerForUser;
-    }
+		QName wsseQN = new QName("http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd", "Security");
+		if (msg.hasHeader(wsseQN)) {
+			Header wsseHeader = msg.getHeader(wsseQN);
 
-    public void setSamlIssuerForUser(String samlIssuerForUser) {
-        this.samlIssuerForUser = samlIssuerForUser;
-    }
+			if (wsseHeader != null) {
+				Node domSecurityHeader = (Node) wsseHeader.getObject();
+				NodeList nodeList = domSecurityHeader.getChildNodes();
+				Node childNode = null;
+
+				for (int i = 0; i < nodeList.getLength(); i++) {
+					childNode = nodeList.item(i);
+
+					if ((childNode.getNodeName().equals("Assertion")) && (childNode.getNodeType() == Node.ELEMENT_NODE)) {
+						SAMLTokenProcessor stp = new SAMLTokenProcessor();
+
+						try {
+							SAMLAssertion samlAssertion = stp.handleSAMLToken((Element) childNode);
+
+							if (samlAssertion.getIssuer().equals(samlIssuerForUser)) {
+								CasAuthenticationToken cat = (CasAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+								cat.setDetails(samlAssertion);
+								break;
+							}
+						} catch (Exception e) {
+							throw new RuntimeException(e);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void handleMessage(SoapMessage msg) throws Fault {
+		super.handleMessage(msg);
+	}
+
+	@Override
+	public void setIgnoreActions(boolean i) {
+		super.setIgnoreActions(i);
+	}
+
+	public String getSamlIssuerForUser() {
+		return samlIssuerForUser;
+	}
+
+	public void setSamlIssuerForUser(String samlIssuerForUser) {
+		this.samlIssuerForUser = samlIssuerForUser;
+	}
 
 }
