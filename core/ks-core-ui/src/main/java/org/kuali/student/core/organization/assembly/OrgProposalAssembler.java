d320890d9f19ecbff180dc0039c1963f92658e9a
jcaddel <jcaddel@kuali.org>
2010-02-11 15:49:56 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/branches/1.0.0-m3@7995 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core/ks-core-ui/src/main/java/org/kuali/student/core/organization/assembly/OrgProposalAssembler.java
+++ core/ks-core-ui/src/main/java/org/kuali/student/core/organization/assembly/OrgProposalAssembler.java
@@ -22,139 +22,141 @@ import org.kuali.student.core.search.newdto.SearchRequest;
 import org.kuali.student.core.search.newdto.SearchResult;
 import org.kuali.student.core.validation.dto.ValidationResultInfo;
 
-public class OrgProposalAssembler implements Assembler<Data, Org>{
-    
-    private OrganizationService orgService;
+public class OrgProposalAssembler implements Assembler<Data, Org> {
 
-    @Override
-    public Data assemble(Org input) throws AssemblyException {
-        if (input == null) {
-            return null;
-        }
-        Data result = new Data();
-        
-        return result;
-    }
+	private OrganizationService orgService;
 
-    @Override
-    public Org disassemble(Data input) throws AssemblyException {
-        if (input == null) {
-            return null;
-        }
-        Org result = new Org();
-        copyIfExists(input,result,"orgInfo");
-        return result;
-    }
+	@Override
+	public Data assemble(Org input) throws AssemblyException {
+		if (input == null) {
+			return null;
+		}
+		Data result = new Data();
 
-    @Override
-    public Data get(String id) throws AssemblyException {
-        // TODO Neerav Agrawal - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+		return result;
+	}
 
-    @Deprecated
-    public Metadata getMetadata() throws AssemblyException {
-    	// TODO remove this method
-    	return null;
-    }
-    @Override
-    public Metadata getMetadata(String type, String state) throws AssemblyException {
-        // TODO Neerav Agrawal - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	@Override
+	public Org disassemble(Data input) throws AssemblyException {
+		if (input == null) {
+			return null;
+		}
+		Org result = new Org();
+		copyIfExists(input, result, "orgInfo");
+		return result;
+	}
 
-    @Override
-    public SaveResult<Data> save(Data input) throws AssemblyException {
-        // TODO Neerav Agrawal - THIS METHOD NEEDS JAVADOCS
-        OrgInfoData orgInfoData = buildOrgInfo(disassemble(input));
-        SaveResult<Data> result = new SaveResult<Data>();
-        List<ValidationResultInfo> validationResults = validate(input);
-        result.setValidationResults(validationResults);
-        try {
-            saveOrg(orgInfoData);
-//            Org resultOrg = buildOrgDTO(orgInfoData);
-            Org resultOrg = new Org();
-            Data resultData = new Data();
-            resultData.set("orgInfo", resultOrg);
-            result.setValue(resultData);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        return result;
-    }
-    
-    public void setOrgService(OrganizationService service){
-        orgService = service;
-    }
-    private OrgInfoData buildOrgInfo(Org org){
-        OrgInfo orgInfo = null;
-        OrgInfoData result = null;
-        orgInfo = new OrgInfo();
-        result = new OrgInfoData();
-                
-        orgInfo.setType(org.getType());
-        orgInfo.setLongDesc(org.getDescription());
-        orgInfo.setShortName(org.getAbbreviation());
-        orgInfo.setLongName(org.getName());
-        orgInfo.setEffectiveDate(org.getEffectiveDate());
-        orgInfo.setExpirationDate(org.getExpirationDate());
-        
-        result.setModificationState(ModificationState.CREATED);
-        result.setOrgInfo(orgInfo);
-        return result;
-    }
+	@Override
+	public Data get(String id) throws AssemblyException {
+		// TODO Neerav Agrawal - THIS METHOD NEEDS JAVADOCS
+		return null;
+	}
 
-    private void saveOrg(OrgInfoData input) throws AlreadyExistsException, DataValidationErrorException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException {
-        OrgInfo result = null;
-        OrgInfo orgInfo = input.getOrgInfo();
-        if (input.getModificationState() != null) {
-            switch (input.getModificationState()) {
-                case CREATED:
-                    result = orgService.createOrganization(orgInfo.getType(), orgInfo);
-                    break;
-                default:
-            }
-        }
-        if (result != null) {
-            input.setOrgInfo(result);
-        }
-    }
+	@Deprecated
+	public Metadata getMetadata() throws AssemblyException {
+		// TODO remove this method
+		return null;
+	}
 
-    @Override
-    public List<ValidationResultInfo> validate(Data input) throws AssemblyException {
-        // TODO Neerav Agrawal - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	@Override
+	public Metadata getMetadata(String type, String state) throws AssemblyException {
+		// TODO Neerav Agrawal - THIS METHOD NEEDS JAVADOCS
+		return null;
+	}
 
-    private <T extends Data> T copyIfExists(Data source, T target, String key) {
-        Data d = source.get(key);
-        if (d == null) {
-            return null;
-        } else {
-            d.copy(target, false);
-            source.set(key, target);
-            return target;
-        }
-    }
-    
-    private Org buildOrgDTO(OrgInfoData orgInfoData){
-        Org org = new Org();
-        org.setId(orgInfoData.getOrgInfo().getId());
-        org.setType(orgInfoData.getOrgInfo().getType());
-        org.setName(orgInfoData.getOrgInfo().getLongName());
-        org.setAbbreviation(orgInfoData.getOrgInfo().getShortName());
-        org.setDescription(orgInfoData.getOrgInfo().getLongDesc());
-        org.setEffectiveDate(orgInfoData.getOrgInfo().getEffectiveDate());
-        org.setExpirationDate(orgInfoData.getOrgInfo().getExpirationDate());
-       
-        return org;
-    }
+	@Override
+	public SaveResult<Data> save(Data input) throws AssemblyException {
+		// TODO Neerav Agrawal - THIS METHOD NEEDS JAVADOCS
+		OrgInfoData orgInfoData = buildOrgInfo(disassemble(input));
+		SaveResult<Data> result = new SaveResult<Data>();
+		List<ValidationResultInfo> validationResults = validate(input);
+		result.setValidationResults(validationResults);
+		try {
+			saveOrg(orgInfoData);
+			// Org resultOrg = buildOrgDTO(orgInfoData);
+			Org resultOrg = new Org();
+			Data resultData = new Data();
+			resultData.set("orgInfo", resultOrg);
+			result.setValue(resultData);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	public void setOrgService(OrganizationService service) {
+		orgService = service;
+	}
+
+	private OrgInfoData buildOrgInfo(Org org) {
+		OrgInfo orgInfo = null;
+		OrgInfoData result = null;
+		orgInfo = new OrgInfo();
+		result = new OrgInfoData();
+
+		orgInfo.setType(org.getType());
+		orgInfo.setLongDesc(org.getDescription());
+		orgInfo.setShortName(org.getAbbreviation());
+		orgInfo.setLongName(org.getName());
+		orgInfo.setEffectiveDate(org.getEffectiveDate());
+		orgInfo.setExpirationDate(org.getExpirationDate());
+
+		result.setModificationState(ModificationState.CREATED);
+		result.setOrgInfo(orgInfo);
+		return result;
+	}
+
+	private void saveOrg(OrgInfoData input) throws AlreadyExistsException, DataValidationErrorException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException {
+		OrgInfo result = null;
+		OrgInfo orgInfo = input.getOrgInfo();
+		if (input.getModificationState() != null) {
+			switch (input.getModificationState()) {
+			case CREATED:
+				result = orgService.createOrganization(orgInfo.getType(), orgInfo);
+				break;
+			default:
+			}
+		}
+		if (result != null) {
+			input.setOrgInfo(result);
+		}
+	}
+
+	@Override
+	public List<ValidationResultInfo> validate(Data input) throws AssemblyException {
+		// TODO Neerav Agrawal - THIS METHOD NEEDS JAVADOCS
+		return null;
+	}
+
+	private <T extends Data> T copyIfExists(Data source, T target, String key) {
+		Data d = source.get(key);
+		if (d == null) {
+			return null;
+		} else {
+			d.copy(target, false);
+			source.set(key, target);
+			return target;
+		}
+	}
+
+	private Org buildOrgDTO(OrgInfoData orgInfoData) {
+		Org org = new Org();
+		org.setId(orgInfoData.getOrgInfo().getId());
+		org.setType(orgInfoData.getOrgInfo().getType());
+		org.setName(orgInfoData.getOrgInfo().getLongName());
+		org.setAbbreviation(orgInfoData.getOrgInfo().getShortName());
+		org.setDescription(orgInfoData.getOrgInfo().getLongDesc());
+		org.setEffectiveDate(orgInfoData.getOrgInfo().getEffectiveDate());
+		org.setExpirationDate(orgInfoData.getOrgInfo().getExpirationDate());
+
+		return org;
+	}
 
 	@Override
 	public SearchResult search(SearchRequest searchRequest) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-   
+
 }
