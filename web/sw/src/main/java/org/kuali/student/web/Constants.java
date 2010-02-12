8b13d445744f6c9fc7755b8b2cb40fbd2b3402f4
jcaddel <jcaddel@kuali.org>
2010-02-11 23:55:20 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/branches/1.0.0-m3@8041 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- web/sw/src/main/java/org/kuali/student/web/Constants.java
+++ web/sw/src/main/java/org/kuali/student/web/Constants.java
@@ -5,5 +5,5 @@ package org.kuali.student.web;
  */
 public final class Constants {
 	public static final String RPC_METHOD_KEY = "rpc.method";
-	public static final String RPC_PARAMETERS_KEY = "rpc.values";
+	public static final String RPC_PARAMETERS_KEY = "rpc.params";
 }
