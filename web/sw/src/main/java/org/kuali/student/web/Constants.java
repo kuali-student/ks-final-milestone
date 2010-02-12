ad512bf847fb2eb36266833c0e75347bb88de59e
jcaddel <jcaddel@kuali.org>
2010-02-11 23:53:42 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/branches/1.0.0-m3@8040 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- web/sw/src/main/java/org/kuali/student/web/Constants.java
+++ web/sw/src/main/java/org/kuali/student/web/Constants.java
@@ -4,6 +4,6 @@ package org.kuali.student.web;
  * 
  */
 public final class Constants {
-	public static final String RPC_METHOD_KEY = "gwt.method.call";
-	public static final String RPC_PARAMETERS_KEY = "gwt.method.params";
+	public static final String RPC_METHOD_KEY = "rpc.method";
+	public static final String RPC_PARAMETERS_KEY = "rpc.values";
 }
