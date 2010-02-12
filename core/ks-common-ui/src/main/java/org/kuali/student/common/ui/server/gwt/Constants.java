f12d694e71a04d5aaa80e9d06fa5aeb79bb0cfe4
jcaddel <jcaddel@kuali.org>
2010-02-12 13:02:02 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/branches/1.0.0-m3@8063 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core/ks-common-ui/src/main/java/org/kuali/student/common/ui/server/gwt/Constants.java
+++ core/ks-common-ui/src/main/java/org/kuali/student/common/ui/server/gwt/Constants.java
@@ -6,5 +6,5 @@ package org.kuali.student.common.ui.server.gwt;
 public final class Constants {
 	public static final String RPC_METHOD_KEY = "rpc.method";
 	public static final String RPC_PARAMETERS_KEY = "rpc.params";
-	public static final String HTTP_REQUEST_DEBUG_MODE = "ks.qa.web.httprequest.debug.mode";
+	public static final String HTTP_REQUEST_DEBUG_MODE = "ks.web.httprequest.debug.mode";
 }
