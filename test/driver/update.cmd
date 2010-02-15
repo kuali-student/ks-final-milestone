f47d90563c24675bece7d66be03cdaa71e8f7609
jcaddel <jcaddel@kuali.org>
2010-02-15 12:20:12 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@8165 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- test/driver/update.cmd
+++ test/driver/update.cmd
@@ -1,7 +1,5 @@
-set QA=https://test.kuali.org/svn/student/deploymentlab/branches/1.0.0.x/
-set DEV=https://test.kuali.org/svn/student/
-svn delete -m "Removing" %QA%/security/ks-standard-sec/src 
-svn copy -m "Copying" --username jcaddel --password gw570229 %DEV%/ks-security/branches/ks-security-dev/ks-standard-sec/src %QA%/security/ks-standard-sec/src
-
-svn delete -m "Removing" %QA%/security/ks-standard-sec/src 
-svn copy -m "Copying" --username jcaddel --password gw570229 %DEV%/ks-security/branches/ks-security-dev/ks-standard-sec/src %QA%/security/ks-standard-sec/src
+call security
+call core
+call brms
+call lum
+call web
