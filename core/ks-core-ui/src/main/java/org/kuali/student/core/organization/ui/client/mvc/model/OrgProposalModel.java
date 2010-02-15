e3dab7f203bd4b9c4ca5b85036e50c121282faf4
jcaddel <jcaddel@kuali.org>
2010-02-14 22:31:15 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@8094 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core/ks-core-ui/src/main/java/org/kuali/student/core/organization/ui/client/mvc/model/OrgProposalModel.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.student.core.organization.ui.client.mvc.model;
-
-import org.kuali.student.common.assembly.client.Data;
-
-public class OrgProposalModel extends Data {
-
-	private static final long serialVersionUID = 1L;
-
-}
