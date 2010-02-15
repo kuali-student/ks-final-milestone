9254a5d13f468fe28cabc0cdb3ccc424943c0c46
jcaddel <jcaddel@kuali.org>
2010-02-15 12:20:51 -0500

    Removing
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/branches/1.0.0.x@8168 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- core/ks-common-impl/src/main/java/org/kuali/student/core/search/service/impl/SearchDispatcher.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.student.core.search.service.impl;
-
-import org.kuali.student.core.search.newdto.SearchRequest;
-import org.kuali.student.core.search.newdto.SearchResult;
-
-public interface SearchDispatcher {
-	public SearchResult dispatchSearch(SearchRequest searchRequest);
-}
