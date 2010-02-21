4a9d9040334acb91bdf0a3c14efa4e3f82b3c452
jcaddel <jcaddel@kuali.org>
2010-02-20 21:51:23 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@9291 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- test/release-test-pom.cmd
+++ test/release-test-pom.cmd
@@ -1,2 +1,2 @@
-call mvn -N -Drelease.arguments=-N release:prepare
-call mvn -N -Drelease.arguments=-N release:perform
+call mvn -N -Drelease.arguments=-N -Drelease.distribution.repository=svn:https://test.kuali.org/svn/student/deploymentlab/repository release:prepare
+call mvn -N -Drelease.arguments=-N -Drelease.distribution.repository=svn:https://test.kuali.org/svn/student/deploymentlab/repository release:perform
