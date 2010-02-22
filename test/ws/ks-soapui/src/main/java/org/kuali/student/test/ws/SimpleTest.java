52ec8f750b64ff499217f6c10ce82274624da27a
jcaddel <jcaddel@kuali.org>
2010-02-21 21:22:22 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@9730 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ test/ws/ks-soapui/src/main/java/org/kuali/student/test/ws/SimpleTest.java
@@ -0,0 +1,53 @@
+package org.kuali.student.test.ws;
+
+import java.util.List;
+
+import com.eviware.soapui.impl.support.AbstractHttpRequestInterface;
+import com.eviware.soapui.impl.wsdl.WsdlInterface;
+import com.eviware.soapui.impl.wsdl.WsdlOperation;
+import com.eviware.soapui.impl.wsdl.WsdlProject;
+import com.eviware.soapui.impl.wsdl.WsdlRequest;
+import com.eviware.soapui.impl.wsdl.WsdlSubmit;
+import com.eviware.soapui.impl.wsdl.WsdlSubmitContext;
+import com.eviware.soapui.model.iface.Operation;
+import com.eviware.soapui.model.iface.Response;
+
+public class SimpleTest {
+	public static void main(String[] args) {
+		new SimpleTest().run();
+	}
+
+	public void run() {
+		try {
+			// create new project
+			WsdlProject project = new WsdlProject();
+
+			// import amazon wsdl
+			WsdlInterface iface = project.importWsdl("http://soap.amazon.com/schemas2/AmazonWebServices.wsdl", true)[0];
+
+			// get "GetPage" operation
+			List<Operation> operations = iface.getOperationList();
+
+			for (Operation operation : operations) {
+				WsdlOperation op = (WsdlOperation) operation;
+				System.out.println(getString(op));
+			}
+			// assertNotNull( content );
+			// assertTrue( content.indexOf( "404 Not Found" ) > 0 );
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected String getString(WsdlOperation op) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(op.getAction() + " ");
+		sb.append(op.getAnonymous() + " ");
+		sb.append(op.getBindingOperationName() + " ");
+		sb.append(op.getInputName() + " ");
+		sb.append(op.getOutputName() + " ");
+		sb.append(op.getStyle() + " ");
+		sb.append(op.getType() + " ");
+		return sb.toString();
+	}
+}
