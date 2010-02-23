d8a91d30f67a302ca3943f790f04c76e753e8724
rdiaz <rdiaz@kuali.org>
2010-02-23 14:21:00 -0500

    GWT hack to fix the "Syntax error, parameterized types are only available if source level is 1.5"
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@9849 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-common-util/src/main/java/org/kuali/student/core/gwt/JDTCompiler15.java
@@ -0,0 +1,17 @@
+package org.kuali.student.core.gwt;
+
+import org.apache.tools.ant.taskdefs.Javac;
+import org.eclipse.jdt.core.JDTCompilerAdapter;
+
+public class JDTCompiler15 extends JDTCompilerAdapter {
+    @Override
+    public void setJavac(Javac attributes) {
+            if (attributes.getTarget() == null) {
+                    attributes.setTarget("1.5");
+            }
+            if (attributes.getSource() == null) {
+                    attributes.setSource("1.5");
+            }
+            super.setJavac(attributes);
+    }
+}
