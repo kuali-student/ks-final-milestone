f1ecf4aa7239f68a03806d72e4b46a8a1cca1dd3
jcaddel <jcaddel@kuali.org>
2010-03-01 16:15:50 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/branches/1.0.0.x@10052 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- db/ks-embedded-db/src/main/sql/oracle/data/KSLU_LR_TYPE.sql
+++
@@ -1,8 +0,0 @@
-TRUNCATE TABLE KSLU_LR_TYPE DROP STORAGE
-/
-INSERT INTO KSLU_LR_TYPE (EFF_DT,EXPIR_DT,NAME,TYPE_DESC,TYPE_KEY)
-  VALUES (TO_DATE( '20000101000000', 'YYYYMMDDHH24MISS' ),TO_DATE( '20001231000000', 'YYYYMMDDHH24MISS' ),'Final Grade','Final learning result for an LU. A stereotypical usage is the final grade in a course.','lrType.finalGrade')
-/
-INSERT INTO KSLU_LR_TYPE (EFF_DT,EXPIR_DT,NAME,TYPE_DESC,TYPE_KEY)
-  VALUES (TO_DATE( '20000101000000', 'YYYYMMDDHH24MISS' ),TO_DATE( '20001231000000', 'YYYYMMDDHH24MISS' ),'Midterm Grade','Midterm learning result for an LU. A stereotypical usage is the midterm grade in a course.','lrType.midtermGrade')
-/
