79e19e3603524844eb1fc2ccf5b2efda098bf626
jcaddel <jcaddel@kuali.org>
2010-03-03 19:15:12 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/maven/trunk@10267 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- resources/build-ks.cmd
+++ resources/build-ks.cmd
@@ -1,7 +1,16 @@
 date /t
 time /t
 
-cd ..\student
+cd ..\..\ks-security
+call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties -Dks.gwt.compile.phase=none clean install
+
+cd ..\ks-core
+call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties -Dks.gwt.compile.phase=none clean install
+
+cd ..\ks-brms
+call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties -Dks.gwt.compile.phase=none clean install
+
+cd ..\ks-lum
 call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties -Dks.gwt.compile.phase=none clean install
 
 cd ..\ks-web\ks-embedded
