7263bee5197b85da39c255fc9a8741631dea6a06
jcaddel <jcaddel@kuali.org>
2010-03-04 19:21:08 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/maven/trunk@10283 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- resources/build-ks.cmd
+++ resources/build-ks.cmd
@@ -2,16 +2,16 @@ date /t
 time /t
 
 cd ..\..\ks-security
-call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties -Dks.gwt.compile.phase=none clean install
+call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties -Dcxf-java2ws-plugin.execution.phase=process-sources -Dks.gwt.compile.phase=none clean install
 
 cd ..\ks-core
-call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties -Dks.gwt.compile.phase=none clean install
+call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties -Dcxf-java2ws-plugin.execution.phase=process-sources -Dks.gwt.compile.phase=none clean install
 
 cd ..\ks-brms
-call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties -Dks.gwt.compile.phase=none clean install
+call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties -Dcxf-java2ws-plugin.execution.phase=process-sources -Dks.gwt.compile.phase=none clean install
 
 cd ..\ks-lum
-call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties -Dks.gwt.compile.phase=none clean install
+call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties -Dcxf-java2ws-plugin.execution.phase=process-sources -Dks.gwt.compile.phase=none clean install
 
 cd ..\ks-web\ks-embedded
 call mvn -Dmaven.test.skip=true -o -Pkuali-developer-properties clean install
