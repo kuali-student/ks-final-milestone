11b0cdacbe7ff65f828028bf4b31c7a19bbaa15e
rdiaz <rdiaz@kuali.org>
2010-03-10 17:29:13 -0500

    added a null check for Data in clearIdsRecursively method
    git-svn-id: https://svn.kuali.org/repos/student/ks-lum/branches/ks-lum-dev@10403 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum-ui/src/main/java/org/kuali/student/lum/lu/assembly/ModifyCreditCourseProposalManager.java
+++ ks-lum-ui/src/main/java/org/kuali/student/lum/lu/assembly/ModifyCreditCourseProposalManager.java
@@ -91,6 +91,7 @@ public class ModifyCreditCourseProposalManager{
 	private void clearIdsRecursively(Data data){
 		//Set runtime create to true
     	//AssemblerUtils.setCreated(data, true);
+		if(data==null) return;
 		
     	//Loop through all properties
     	for(Iterator<Property> i=data.iterator();i.hasNext();){
