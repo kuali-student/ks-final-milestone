8f2d39e45c9035f785a653047afb36abcb0d29af
ssim <ssim@kuali.org>
2010-03-20 05:03:56 -0400

    Table Updates. Needs more CSS and a Builder
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/branches/proposalhistory@10672 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- KS-ProposalHistory/src/java/org/kuali/student/client/proposal.java
+++ KS-ProposalHistory/src/java/org/kuali/student/client/proposal.java
@@ -20,6 +20,7 @@ package org.kuali.student.client;
  	import com.google.gwt.user.client.ui.TabPanel;
  	import com.google.gwt.user.client.ui.FlexTable;
  	import com.google.gwt.user.client.ui.Hyperlink;
+        import com.google.gwt.user.client.ui.Widget;
  	
  	
  	/**
@@ -43,17 +44,21 @@ package org.kuali.student.client;
  	                RootPanel rootPanel = RootPanel.get();
  	
  	                rootPanel.addStyleName("body");
- 	
- 	                HTMLPanel title = new HTMLPanel("<h3>Kuali Reference University</h3>");
- 	                title.addStyleName("ks-header-container");
+ 	                rootPanel.addStyleName("ks-page-container");
+
+ 	                HTMLPanel title = new HTMLPanel("<h3>Kuali Reference University</h3> build March 19 2010");
+
+                        title.addStyleName("ks-header-container");
  	                title.addStyleName("ks-header");
- 	                rootPanel.add(title, 0, 0);
+                        
+ 	                
  	                title.setSize("689px", "16px");
- 	
+ 	                rootPanel.add(title, 0, 0);
+
  	                Label CLUTitle = new Label("Introduction to Geology that is a very long title about specific things");
  	                rootPanel.add(CLUTitle, 21, 76);
  	                CLUTitle.setSize("441px", "13px");
- 	
+ 	                CLUTitle.addStyleName("ks-heading-course-title");
  	                //
  	                // Drop Down Combo Boxes
  	                // Replacement KS Widget:
@@ -84,47 +89,44 @@ package org.kuali.student.client;
  	                // Breadcrumbs...
  	
  	                // Panel Title
- 	                reviewPanel.add(new HTML("Proposal History"));
- 	                HTML html = new HTML("View changes for individual fields in the change log. Submit proposal using Workflow Actions menu above.");
- 	                html.setHeight("37px");
- 	                reviewPanel.add(html);
+ 	                HTML panelTitle = new HTML("<h2>Review Proposal</h2>");
+ 	                panelTitle.addStyleName("ks-heading-page-title");
+                        reviewPanel.add(panelTitle);
+
+                        
+
+                        HTML introHtml = new HTML("View changes for individual fields in the change log. Submit proposal using Workflow Actions menu above.");
+ 	                introHtml.setHeight("40px");
+ 	                reviewPanel.add(introHtml);
  	
  	                Hyperlink hyperlink = new Hyperlink("proposal History", false, "newProposalHistoryToken");
  	                reviewPanel.add(hyperlink);
  	
  	
- 	                // Version Table
- 	                FlexTable versionTable = new FlexTable();
- 	                versionTable.setText(0, 0, "Version");
- 	                versionTable.setText(0, 1, "Date");
- 	                versionTable.setText(0, 2, "Change By");
- 	                versionTable.setText(1, 0, "Current Working Draft");
- 	                versionTable.setText(1, 1, "january 7, 2010");
- 	                versionTable.setText(1, 2, "Julia Davinchi");
- 	                versionTable.setText(2, 0, "Changes By Geographic Dept");
- 	                versionTable.setText(2, 1, "October 5, 2009");
- 	                versionTable.setText(2, 2, "Martha Lee");
- 	
- 	                reviewPanel.add(versionTable);
- 	                versionTable.setSize("564px", "125px");
- 	
- 	
+ 	                
  	                // Add the Panels
  	                TabPanel tabPanel = new TabPanel();
 
-                        tabPanel.addStyleName("ks-page-navigation");
+                        // tabPanel.addStyleName("ks-page-navigation");
+
+                        // tabPanel.addStyleName("ks-page-tabs-left");
+                        // tabPanel.addStyleName("active-tab");
 
  	                // tabPanel.setSize("649px", "214px");
- 	                tabPanel.add(editPanel, "Edit Proposal");
- 	                tabPanel.add(reviewPanel, "Review Proposal");
- 	                
+ 	                tabPanel.add(editPanel, "   Edit Proposal   ");
+
+                        tabPanel.add(reviewPanel, "   Review Proposal   ");
+ 	                reviewPanel.add(createVersionsTable());
  	
- 	                tabPanel.add(new HTML("Supporting Documents"), "Supporting Documents");
- 	                tabPanel.selectTab(2);
- 	
+ 	                tabPanel.add(new HTML("   Supporting Documents   "), "Supporting Documents");
+ 	                tabPanel.selectTab(1);
+
+
+
+
  	
  	                // Add the Tab Panel to Root
- 	                rootPanel.add(tabPanel, 19, 179);
+ 	                rootPanel.add(tabPanel, 0, 179);
  	
  	
  	
@@ -146,5 +148,125 @@ package org.kuali.student.client;
  	                        }
  	                });
  	        }
+
+
+                public Widget createVersionsTable() {
+
+                    // Version Table
+                    FlexTable versionTable = new FlexTable();
+
+                    // Setup the Headers
+                    versionTable.getColumnFormatter().setWidth(0, "200px");
+                    versionTable.getColumnFormatter().setWidth(1, "100px");
+                    versionTable.getColumnFormatter().setWidth(2, "100px");
+                    versionTable.getColumnFormatter().setWidth(3, "100px");
+
+                    Label titleHeaderLabel = new Label("Title");
+                    titleHeaderLabel.setHorizontalAlignment(Label.ALIGN_CENTER);
+
+                    // titleHeaderLabel.setStyleName("ks-table th");
+                    versionTable.getRowFormatter().addStyleName(0, "ks-table-th");
+
+                    versionTable.setWidget(0, 0,titleHeaderLabel);
+                    Label nameHeaderLabel = new Label("Name");
+                    nameHeaderLabel.setHorizontalAlignment(Label.ALIGN_CENTER);
+                    versionTable.setWidget(0, 1,nameHeaderLabel);
+                    Label dateHeaderLabel = new Label("Last Updated");
+                    dateHeaderLabel.setHorizontalAlignment(Label.ALIGN_CENTER);
+                    versionTable.setWidget(0, 2,dateHeaderLabel);
+                    Label changeHeaderLabel = new Label("Change Log");
+                    changeHeaderLabel.setHorizontalAlignment(Label.ALIGN_CENTER);
+                    versionTable.setWidget(0, 3,changeHeaderLabel);
+
+
+                    
+                    versionTable.addStyleName("ks-table-container");
+                    versionTable.addStyleName("ks-table");
+                    
+                    // Populate the Table
+
+                    // Governance Sub Section
+                    versionTable.setText(1, 0, "Governance");
+                    versionTable.setText(1, 1, "");
+                    versionTable.setText(1, 2, "");
+                    versionTable.setText(1, 3, "");
+                    versionTable.getRowFormatter().addStyleName(1, "td-subhead");
+
+
+                    versionTable.setText(2, 0, "Curriculum Oversight");
+                    versionTable.setText(2, 1, "March 18, 2010");
+                    versionTable.setText(2, 2, "Stuart Sim");
+                    versionTable.setText(2, 3, "");
+
+                    versionTable.setText(3, 0, "Campus Locations");
+                    versionTable.setText(3, 1, "March 17, 2010");
+                    versionTable.setText(3, 2, "Stuart Sim");
+                    versionTable.setText(3, 3, "");
+
+                    versionTable.setText(4, 0, "Administering Organization");
+                    versionTable.setText(4, 1, "March 14, 2010");
+                    versionTable.setText(4, 2, "Stuart Sim");
+                    versionTable.setText(4, 3, "");
+
+
+                    // Course Logistics Section
+
+                    // Governance Sub Section
+                    versionTable.setText(5, 0, "Course Logistics");
+                    versionTable.setText(5, 1, "");
+                    versionTable.setText(5, 2, "");
+                    versionTable.setText(5, 3, "");
+                    versionTable.getRowFormatter().addStyleName(1, "td-subhead"); // BROKEN - css not being picked up?
+
+
+                    versionTable.setText(6, 0, "First Expected Offering");
+                    versionTable.setText(6, 1, "March 4, 2010");
+                    versionTable.setText(6, 2, "Tim Duck");
+                    versionTable.setText(6, 3, "");
+
+                    versionTable.setText(7, 0, "Instructor");
+                    versionTable.setText(7, 1, "March 4, 2010");
+                    versionTable.setText(7, 2, "Stuart Sim");
+                    versionTable.setText(7, 3, "");
+
+                    versionTable.setText(8, 0, "Scheduling Duration");
+                    versionTable.setText(8, 1, "March 3, 2010");
+                    versionTable.setText(8, 2, "Claire Barnes");
+                    versionTable.setText(8, 3, "");
+
+                    versionTable.setText(9, 0, "Scheduling Duration Type");
+                    versionTable.setText(9, 1, "March 3, 2010");
+                    versionTable.setText(9, 2, "Claire Barnes");
+                    versionTable.setText(9, 3, "");
+
+                    versionTable.setText(10, 0, "Course Format 1");
+                    versionTable.setText(10, 1, "March 5, 2010");
+                    versionTable.setText(10, 2, "Bob Smith");
+                    versionTable.setText(10, 3, "");
+
+                    versionTable.setText(11, 0, "Course Activity 1");
+                    versionTable.setText(11, 1, "March 2, 2010");
+                    versionTable.setText(11, 2, "Bob Smith");
+                    versionTable.setText(11, 3, "");
+
+
+
+
+                    // Design Note TODO
+                    // The above will be replaced by a table builder for
+                    // the Proposal and Clu Info Structures
+                    // i.e. add Section(sectionName), addRow(section, rowdata)
+
+
+                    versionTable.setSize("560px", "200px");
+
+
+
+
+                    return versionTable;
+
+
+
+                }
  	
  	}
