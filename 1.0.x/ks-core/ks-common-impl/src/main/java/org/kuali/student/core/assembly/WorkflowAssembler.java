f20e292cb92fb9583a0a2df709c63ec044bdce52
jcaddel <jcaddel@kuali.org>
2010-03-23 13:48:16 -0400

    Removing ks-core
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@10987 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- 1.0.x/ks-core/ks-common-impl/src/main/java/org/kuali/student/core/assembly/WorkflowAssembler.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.student.core.assembly;
-
-public interface WorkflowAssembler<TargetType, SourceType> extends Assembler<TargetType, SourceType>{
-/*	public void removeCollaborator(String id);
-	public void removeAdHoc(String id);
-	public void */
-}
