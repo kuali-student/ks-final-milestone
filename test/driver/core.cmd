42f18952bf099eb346e9781f088874df7784f89f
jcaddel <jcaddel@kuali.org>
2010-03-23 02:45:56 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@10887 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- test/driver/core.cmd
+++ test/driver/core.cmd
@@ -1,4 +1,4 @@
-set QA=https://test.kuali.org/svn/student/deploymentlab/trunk/1.0.x/core
+set QA=https://test.kuali.org/svn/student/deploymentlab/trunk/1.0.x/ks-core
 set DEV=https://test.kuali.org/svn/student/ks-core/branches/ks-core-dev
 call removeThenCopy %QA% %DEV% ks-common-impl
 call removeThenCopy %QA% %DEV% ks-common-test
