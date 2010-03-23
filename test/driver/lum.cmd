d305e8121e29b50e8d09c4b85bf989c01ec03539
jcaddel <jcaddel@kuali.org>
2010-03-23 02:49:25 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@10896 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- test/driver/lum.cmd
+++ test/driver/lum.cmd
@@ -1,6 +1,3 @@
 set QA=https://test.kuali.org/svn/student/deploymentlab/trunk/1.0.x/ks-lum
 set DEV=https://test.kuali.org/svn/student/ks-lum/branches/ks-lum-dev
-call removeThenCopy %QA% %DEV% ks-lum-api
-call removeThenCopy %QA% %DEV% ks-lum-impl
-call removeThenCopy %QA% %DEV% ks-lum-rice
-call removeThenCopy %QA% %DEV% ks-lum-ui
+call removeThenCopy %QA% %DEV%
--- test/driver/web.cmd
+++ test/driver/web.cmd
@@ -1,4 +1,3 @@
 set QA=https://test.kuali.org/svn/student/deploymentlab/trunk/1.0.x/ks-web
 set DEV=https://test.kuali.org/svn/student/ks-web/branches/ks-web-dev
-call removeThenCopy %QA% %DEV% ks-embedded
-call removeThenCopy %QA% %DEV% ks-standalone
+call removeThenCopy %QA% %DEV%
