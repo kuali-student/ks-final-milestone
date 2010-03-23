16d6ece3eaa4c2ac9f2c835faa91b47b2e56ed12
jcaddel <jcaddel@kuali.org>
2010-03-23 13:45:35 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@10971 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- test/driver/cfg-dbs.cmd
+++ test/driver/cfg-dbs.cmd
@@ -1,4 +1,14 @@
-set MODULE=ks-core-db
 set QUALIFIER=tags
 set VERSION=1.0.0-m5
-call removeThenCopy %MODULE% %QUALIFIER% %VERSION%
+
+set MODULE=ks-core-db
+call removeThenCopyDb %MODULE% %QUALIFIER% %VERSION%
+
+set MODULE=ks-embedded
+call removeThenCopyDb %MODULE% %QUALIFIER% %VERSION%
+
+set MODULE=ks-rice-db
+call removeThenCopyDb %MODULE% %QUALIFIER% %VERSION%
+
+set MODULE=ks-lum-db
+call removeThenCopyDb %MODULE% %QUALIFIER% %VERSION%
--- test/driver/removeThenCopyDb.cmd
+++ test/driver/removeThenCopyDb.cmd
@@ -1,2 +1,7 @@
-svn delete -m "Removing" %1/%3
-svn copy -m "Copying from dev branch" --username jcaddel --password gw570229 %2/%3/tags/%3-1.0.0-m5 %1/%3
+set MODULE=%1
+set QUALIFIER=%2
+set VERSION=%3
+set QA=https://test.kuali.org/svn/student/deploymentlab/trunk/1.0.x/ks-cfg-dbs/%MODULE%
+set DEV=https://test.kuali.org/svn/student/ks-cfg-dbs/%MODULE%/%QUALIFIER%/%MODULE%-%VERSION%
+svn delete -m "Removing %MODULE%" %QA%
+svn copy -m "Copying ks-cfg-dbs/%MODULE%/%QUALIFIER%/%MODULE%-%VERSION% from development svn" --username jcaddel --password gw570229 %DEV% %QA%
