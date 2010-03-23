ab809a2fed6a7485260dc99a5ec5dfc8ac10cb78
jcaddel <jcaddel@kuali.org>
2010-03-23 12:49:21 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@10949 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- test/driver/security.cmd
+++ test/driver/security.cmd
@@ -1,3 +1,3 @@
 set QA=https://test.kuali.org/svn/student/deploymentlab/trunk/1.0.x/ks-security
-set DEV=https://test.kuali.org/svn/student/ks-security/branches/ks-security-dev
+set DEV=https://test.kuali.org/svn/student/ks-security/tags/ks-security-1.0.0-m4
 call removeThenCopy %QA% %DEV%
