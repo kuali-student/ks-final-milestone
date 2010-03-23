97e35259f9e09ff82ba5f6f06613d86242fe8331
jcaddel <jcaddel@kuali.org>
2010-03-23 12:42:35 -0400

    Removing
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@10933 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- 1.0.x/ks-core/ks-common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/field/layout/layouts/FieldLayoutComponent.java
+++
@@ -1,6 +0,0 @@
-package org.kuali.student.common.ui.client.widgets.field.layout.layouts;
-
-public interface FieldLayoutComponent {
-	public String getKey();
-	public void setKey(String key);
-}
--- 1.0.x/ks-core/ks-common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/field/layout/layouts/TopLevelLayout.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.student.common.ui.client.widgets.field.layout.layouts;
-
-public class TopLevelLayout extends VerticalFieldLayout{
-
-	public TopLevelLayout(){
-		this.setStyleName("ks-page-content");
-	}
-}
