ef57dbe33d0ca1b02420e3609380791e1170272a
jcaddel <jcaddel@kuali.org>
2010-03-24 13:32:42 -0400

    Removed gargoylesoftware references
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@11046 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- 1.0.x/ks-core/ks-core-web/src/test/java/org/kuali/student/core/web/Page.java
+++ 1.0.x/ks-core/ks-core-web/src/test/java/org/kuali/student/core/web/Page.java
@@ -14,21 +14,6 @@
  */
 package org.kuali.student.core.web;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
-import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
-
-import org.junit.Test;
-import static org.junit.Assert.assertNotNull;
-
 /*NOTES TO SELF
  * !moved htmlunit dependency from test pom to web pom
  * 
@@ -43,70 +28,4 @@ import static org.junit.Assert.assertNotNull;
  */
 
 public class Page {
-	
-	private WebClient webClient = new WebClient();
-	private HtmlPage page;
-	
-	public Page(){
-	}
-	
-	public Page(String url) throws FailingHttpStatusCodeException, MalformedURLException, IOException{
-		page = webClient.getPage(url);
-		webClient.waitForBackgroundJavaScript(10000);
-	}
-	
-	public Page(URL url) throws FailingHttpStatusCodeException, IOException{
-		page = webClient.getPage(url);
-		webClient.waitForBackgroundJavaScript(10000);
-	}
-	
-	public void setPage(String url) throws FailingHttpStatusCodeException, MalformedURLException, IOException{
-		page = webClient.getPage(url);
-		webClient.waitForBackgroundJavaScript(10000);
-	}
-	
-	public void setPage(URL url) throws FailingHttpStatusCodeException, IOException{
-		page = webClient.getPage(url);
-		webClient.waitForBackgroundJavaScript(10000);
-	}
-	
-	public HtmlPage getPage(){
-		return page;
-	}
-	
-	public void setWebClient(WebClient webClient){
-		this.webClient = webClient;
-	}
-	
-	public WebClient getWebClient(){
-		return webClient;
-	}
-	
-	public HtmlPage logIn(String user, String password) throws IOException{
-		HtmlForm form = page.getElementByName("f");
-		HtmlTextInput userInput = form.getInputByName("j_username");
-		HtmlPasswordInput passwordInput = form.getInputByName("j_password");
-		HtmlInput submit = form.getInputByName("submit");
-		
-		userInput.setValueAttribute(user);
-		passwordInput.setValueAttribute(password);
-		
-		page = submit.click();
-		
-		webClient.waitForBackgroundJavaScript(10000);
-		
-		return page;
-	}
-	
-	//use to automatically log in for now
-	public HtmlPage bypass() throws IOException{
-		return this.logIn("a", "a");
-	}
-	
-	@Test
-	public void defaultTest(){
-		assertNotNull(this.webClient);
-	}
-
 }
-
--- 1.0.x/ks-core/ks-core-web/src/test/java/org/kuali/student/core/web/TestLogIn.java
+++ 1.0.x/ks-core/ks-core-web/src/test/java/org/kuali/student/core/web/TestLogIn.java
@@ -14,29 +14,5 @@
  */
 package org.kuali.student.core.web;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
-
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-
 public class TestLogIn {
-	
-	Page page;
-	
-	@Test
-	public void loadPage() throws FailingHttpStatusCodeException, MalformedURLException, IOException{
-		page = new Page("http://localhost:8181/ks-core-web/spring_security_login");
-		assertTrue(page.getPage().getTitleText().equals("Login Page"));
-	}
-	
-	@Test
-	public void testLogIn() throws FailingHttpStatusCodeException, MalformedURLException, IOException{
-		Page toLogIn = new Page("http://localhost:8181/ks-core-web/spring_security_login");
-		toLogIn.logIn("same", "same");
-		assertTrue(toLogIn.getPage().getTitleText().contains("Kuali Student: Organization"));
-	}
-
 }
