1dcf2d2fcd26c87857dc0477fe548d1dd68a16da
jcaddel <jcaddel@kuali.org>
2010-03-24 18:00:38 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@11062 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- 1.0.x/ks-psrg/src/main/java/org/kuali/student/psrg/web/RequestRecorderFilterListener.java
+++ 1.0.x/ks-psrg/src/main/java/org/kuali/student/psrg/web/RequestRecorderFilterListener.java
@@ -1,8 +1,8 @@
 package org.kuali.student.psrg.web;
 
 /**
- * 
- *
+ * Implementations of this interface will get notified when a servlet filter
+ * event occurs
  */
 public interface RequestRecorderFilterListener {
 	public void onBeforeDoFilter(HttpServletFilterContext context);
