6094ea7fe167119afa86634c5a50a2c19a7cb4e3
jcaddel <jcaddel@kuali.org>
2010-04-07 12:30:25 -0400

    [maven-scm] copy for tag maven-soapui-plugin-1.0.0
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/tags/maven-soapui-plugin-1.0.0@11519 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- 1.0.x/ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/tools/client/service/CluSetManagementRpcServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.student.lum.lu.ui.tools.client.service;
-
-import org.kuali.student.common.ui.client.service.BaseDataOrchestrationRpcServiceAsync;
-
-public interface CluSetManagementRpcServiceAsync extends BaseDataOrchestrationRpcServiceAsync{
-
-}
