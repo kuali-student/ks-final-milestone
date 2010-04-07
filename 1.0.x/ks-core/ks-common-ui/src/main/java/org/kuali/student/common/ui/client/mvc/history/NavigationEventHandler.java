c90cca19883c2c263ef9ec90b79c1ed32768cbbc
jcaddel <jcaddel@kuali.org>
2010-04-07 06:01:30 -0400

    [maven-scm] copy for tag ks-soapui-1.0.0
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/tags/ks-soapui-1.0.0@11500 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- 1.0.x/ks-core/ks-common-ui/src/main/java/org/kuali/student/common/ui/client/mvc/history/NavigationEventHandler.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.student.common.ui.client.mvc.history;
-
-import org.kuali.student.common.ui.client.mvc.UncheckedApplicationEventHandler;
-
-public interface NavigationEventHandler extends UncheckedApplicationEventHandler {
-    public void onNavigationEvent(NavigationEvent event);
-}
