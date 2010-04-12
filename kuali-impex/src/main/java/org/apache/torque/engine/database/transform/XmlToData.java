31475b414c43a8267c46e92e7a8c45c6a13011b7
jcaddel <jcaddel@kuali.org>
2010-04-12 19:44:27 -0400

    Just formatting
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@11715 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- kuali-impex/src/main/java/org/apache/torque/engine/database/transform/XmlToData.java
+++ kuali-impex/src/main/java/org/apache/torque/engine/database/transform/XmlToData.java
@@ -44,213 +44,169 @@ import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 /**
- * A Class that is used to parse an input xml schema file and creates and
- * AppData java structure.
- *
+ * A Class that is used to parse an input xml schema file and creates and AppData java structure.
+ * 
  * @author <a href="mailto:leon@opticode.co.za">Leon Messerschmidt</a>
  * @author <a href="mailto:jvanzyl@apache.org">Jason van Zyl</a>
  * @author <a href="mailto:mpoeschl@marmot.at">Martin Poeschl</a>
  * @author <a href="mailto:fedor.karpelevitch@home.com">Fedor Karpelevitch</a>
  * @version $Id: XmlToData.java,v 1.1 2007-10-21 07:57:26 abyrne Exp $
  */
-public class XmlToData extends DefaultHandler implements EntityResolver
-{
-    /** Logging class from commons.logging */
-    private static Log log = LogFactory.getLog(XmlToData.class);
-    private Database database;
-    private List data;
-    private String dtdFileName;
-    private File dtdFile;
-    private InputSource dataDTD;
+public class XmlToData extends DefaultHandler implements EntityResolver {
+	/** Logging class from commons.logging */
+	private static Log log = LogFactory.getLog(XmlToData.class);
+	private Database database;
+	private List data;
+	private String dtdFileName;
+	private File dtdFile;
+	private InputSource dataDTD;
 
-    private static SAXParserFactory saxFactory;
+	private static SAXParserFactory saxFactory;
 
-    static
-    {
-        saxFactory = SAXParserFactory.newInstance();
-        saxFactory.setValidating(true);
-    }
+	static {
+		saxFactory = SAXParserFactory.newInstance();
+		saxFactory.setValidating(true);
+	}
 
-    /**
-     * Default custructor
-     */
-    public XmlToData(Database database, String dtdFilePath)
-            throws MalformedURLException, IOException
-    {
-        this.database = database;
-        dtdFile = new File(dtdFilePath);
-        this.dtdFileName = "file://" + dtdFile.getName();
-        dataDTD = new InputSource(dtdFile.toURL().openStream());
-    }
+	/**
+	 * Default custructor
+	 */
+	public XmlToData(Database database, String dtdFilePath) throws MalformedURLException, IOException {
+		this.database = database;
+		dtdFile = new File(dtdFilePath);
+		this.dtdFileName = "file://" + dtdFile.getName();
+		dataDTD = new InputSource(dtdFile.toURL().openStream());
+	}
 
-    /**
+	/**
      *
      */
-    public List parseFile(String xmlFile)
-            throws Exception
-    {
-        data = new ArrayList();
+	public List parseFile(String xmlFile) throws Exception {
+		data = new ArrayList();
 
-        SAXParser parser = saxFactory.newSAXParser();
+		SAXParser parser = saxFactory.newSAXParser();
 
-        FileReader fr = new FileReader (xmlFile);
-        BufferedReader br = new BufferedReader (fr);
-        try
-        {
-            InputSource is = new InputSource (br);
-            is.setSystemId( xmlFile );
-            parser.parse(is, this);
-        }
-        finally
-        {
-            br.close();
-        }
-        return data;
-    }
+		FileReader fr = new FileReader(xmlFile);
+		BufferedReader br = new BufferedReader(fr);
+		try {
+			InputSource is = new InputSource(br);
+			is.setSystemId(xmlFile);
+			parser.parse(is, this);
+		} finally {
+			br.close();
+		}
+		return data;
+	}
 
-    /**
-     * Handles opening elements of the xml file.
-     */
-    public void startElement(String uri, String localName, String rawName,
-                             Attributes attributes)
-            throws SAXException
-    {
-        try
-        {
-            if (rawName.equals("dataset"))
-            {
-                //ignore <dataset> for now.
-            }
-            else
-            {
-                Table table = database.getTableByJavaName(rawName);
+	/**
+	 * Handles opening elements of the xml file.
+	 */
+	public void startElement(String uri, String localName, String rawName, Attributes attributes) throws SAXException {
+		try {
+			if (rawName.equals("dataset")) {
+				// ignore <dataset> for now.
+			} else {
+				Table table = database.getTableByJavaName(rawName);
 
-                if (table == null)
-                {
-                    throw new SAXException("Table '" + rawName + "' unknown");
-                }
-                List columnValues = new ArrayList();
-                for (int i = 0; i < attributes.getLength(); i++)
-                {
-                    Column col = table
-                        .getColumnByJavaName(attributes.getQName(i));
+				if (table == null) {
+					throw new SAXException("Table '" + rawName + "' unknown");
+				}
+				List columnValues = new ArrayList();
+				for (int i = 0; i < attributes.getLength(); i++) {
+					Column col = table.getColumnByJavaName(attributes.getQName(i));
 
-                    if (col == null)
-                    {
-                        throw new SAXException("Column "
-                                + attributes.getQName(i) + " in table "
-                                + rawName + " unknown.");
-                    }
+					if (col == null) {
+						throw new SAXException("Column " + attributes.getQName(i) + " in table " + rawName + " unknown.");
+					}
 
-                    String value = attributes.getValue(i);
-                    columnValues.add(new ColumnValue(col, value));
-                }
-                data.add(new DataRow(table, columnValues));
-            }
-        }
-        catch (Exception e)
-        {
-            throw new SAXException(e);
-        }
-    }
+					String value = attributes.getValue(i);
+					columnValues.add(new ColumnValue(col, value));
+				}
+				data.add(new DataRow(table, columnValues));
+			}
+		} catch (Exception e) {
+			throw new SAXException(e);
+		}
+	}
 
-    /**
-     * called by the XML parser
-     *
-     * @return an InputSource for the database.dtd file
-     */
-    public InputSource resolveEntity(String publicId, String systemId)
-            throws SAXException
-    {
-        try
-        {
-            if (dataDTD != null && dtdFileName.equals(systemId))
-            {
-                log.info("Resolver: used " + dtdFile.getPath());
-                return dataDTD;
-            }
-            else
-            {
-                log.info("Resolver: used " + systemId);
-                return getInputSource(systemId);
-            }
-        }
-        catch (IOException e)
-        {
-            throw new SAXException(e);
-        }
-    }
+	/**
+	 * called by the XML parser
+	 * 
+	 * @return an InputSource for the database.dtd file
+	 */
+	public InputSource resolveEntity(String publicId, String systemId) throws SAXException {
+		try {
+			if (dataDTD != null && dtdFileName.equals(systemId)) {
+				log.info("Resolver: used " + dtdFile.getPath());
+				return dataDTD;
+			} else {
+				log.info("Resolver: used " + systemId);
+				return getInputSource(systemId);
+			}
+		} catch (IOException e) {
+			throw new SAXException(e);
+		}
+	}
 
-    /**
-     * get an InputSource for an URL String
-     *
-     * @param urlString
-     * @return an InputSource for the URL String
-     */
-    public InputSource getInputSource(String urlString)
-            throws IOException
-    {
-        URL url = new URL(urlString);
-        InputSource src = new InputSource(url.openStream());
-        return src;
-    }
+	/**
+	 * get an InputSource for an URL String
+	 * 
+	 * @param urlString
+	 * @return an InputSource for the URL String
+	 */
+	public InputSource getInputSource(String urlString) throws IOException {
+		URL url = new URL(urlString);
+		InputSource src = new InputSource(url.openStream());
+		return src;
+	}
 
-    /**
+	/**
      *
      */
-    public class DataRow
-    {
-        private Table table;
-        private List columnValues;
+	public class DataRow {
+		private Table table;
+		private List columnValues;
 
-        public DataRow(Table table, List columnValues)
-        {
-            this.table = table;
-            this.columnValues = columnValues;
-        }
+		public DataRow(Table table, List columnValues) {
+			this.table = table;
+			this.columnValues = columnValues;
+		}
 
-        public Table getTable()
-        {
-            return table;
-        }
+		public Table getTable() {
+			return table;
+		}
 
-        public List getColumnValues()
-        {
-            return columnValues;
-        }
-    }
+		public List getColumnValues() {
+			return columnValues;
+		}
+	}
 
-    /**
+	/**
      *
      */
-    public class ColumnValue
-    {
-        private Column col;
-        private String val;
+	public class ColumnValue {
+		private Column col;
+		private String val;
 
-        public ColumnValue(Column col, String val)
-        {
-            this.col = col;
-            this.val = val;
-        }
+		public ColumnValue(Column col, String val) {
+			this.col = col;
+			this.val = val;
+		}
 
-        public Column getColumn()
-        {
-            return col;
-        }
+		public Column getColumn() {
+			return col;
+		}
 
-        public String getValue()
-        {
-            return val;
-        }
+		public String getValue() {
+			return val;
+		}
 
-        public String getEscapedValue()
-        {
-            StringBuffer sb = new StringBuffer();
-            sb.append("'");
-            sb.append(StringUtils.replace(val, "'", "''"));
-            sb.append("'");
-            return sb.toString();
-        }
-    }
+		public String getEscapedValue() {
+			StringBuffer sb = new StringBuffer();
+			sb.append("'");
+			sb.append(StringUtils.replace(val, "'", "''"));
+			sb.append("'");
+			return sb.toString();
+		}
+	}
 }
