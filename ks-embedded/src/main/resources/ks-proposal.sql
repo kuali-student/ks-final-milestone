5da937511174f66d1fb5821e8bd22eb088456276
rjjohnson84 <rjjohnson84@kuali.org>
2010-04-13 09:40:04 -0400

    [KSCOR-166] Removed reference to ProposalDao and replaced with calls to ProposalService. Removed unnecessary throw of DoesNotExistException from ProposalDaoImpl
    git-svn-id: https://svn.kuali.org/repos/student/ks-web/branches/ks-web-dev@11720 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-embedded/src/main/resources/ks-proposal.sql
+++ ks-embedded/src/main/resources/ks-proposal.sql
@@ -5,5 +5,5 @@ INSERT INTO KSPR_PROPOSAL_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) val
 // Proposal Reference Types
 INSERT INTO KSPR_PROPOSAL_REFTYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('kuali.proposal.referenceType.clu', 'Clu proposal reference type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'}, 'Clu Proposal Reference')
 /
-INSERT INTO kspr_proposal_docrel_type (ID, description, eff_dt, expir_dt, NAME) VALUES ('proposalDocRelationType.doctype1', 'Default proposal document relation type', {ts '2000-01-01 00:00:00.0'}, null, 'proposalDocRelationType.doctype1')
+INSERT INTO kspr_proposal_docrel_type (ID, description, eff_dt, expir_dt, NAME) VALUES ('kuali.proposal.docRelationType.attachment', 'Default proposal document relation type', {ts '2000-01-01 00:00:00.0'}, null, 'kuali.proposal.docRelationType.attachment')
 /
