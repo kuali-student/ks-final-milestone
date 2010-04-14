d671fa6739bde8f7345eed89de05ad36f32a945d
depstein <depstein@kuali.org>
2010-04-14 11:17:54 -0400

    Added ECL license header to files using the maven license plugin
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@11750 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-ui/src/main/java/org/kuali/student/common/ui/client/configurable/mvc/impl/DefaultWidgetFactoryImpl.java
+++ ks-common-ui/src/main/java/org/kuali/student/common/ui/client/configurable/mvc/impl/DefaultWidgetFactoryImpl.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2010 The Kuali Foundation Licensed under the
  * Educational Community License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License. You may
@@ -12,6 +12,7 @@
  * or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
 package org.kuali.student.common.ui.client.configurable.mvc.impl;
 
 import org.kuali.student.common.ui.client.configurable.mvc.DefaultWidgetFactory;
