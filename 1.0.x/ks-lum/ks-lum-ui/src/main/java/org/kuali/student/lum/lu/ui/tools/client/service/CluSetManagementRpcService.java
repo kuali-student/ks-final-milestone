5923509e1c67bc2217f8b5435e9971994e71c0d5
jcaddel <jcaddel@kuali.org>
2010-04-29 13:23:31 -0400

    Removing ks-lum
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@12232 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- 1.0.x/ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/tools/client/service/CluSetManagementRpcService.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.student.lum.lu.ui.tools.client.service;
-
-import org.kuali.student.common.ui.client.service.BaseDataOrchestrationRpcService;
-
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-@RemoteServiceRelativePath("rpcservices/CluSetManagementRpcService")
-public interface CluSetManagementRpcService extends BaseDataOrchestrationRpcService{
-
-}
--- 1.0.x/ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/ui/theme/standard/client/LumWidgetsImpl.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.student.lum.ui.theme.standard.client;
-
-import org.kuali.student.lum.lu.ui.main.client.theme.LumWidgets;
-
-public class LumWidgetsImpl implements LumWidgets{
-
-}
