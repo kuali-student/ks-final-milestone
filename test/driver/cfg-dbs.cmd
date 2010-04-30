25afb76b49955de30250b211665fecd3ed981567
jcaddel <jcaddel@kuali.org>
2010-04-30 18:09:50 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@12532 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- test/driver/cfg-dbs.cmd
+++ test/driver/cfg-dbs.cmd
@@ -1,5 +1,11 @@
-set QUALIFIER=tags
-set VERSION=1.0.0-rc1.1
+@echo off
+rem Dev settings
+set QUALIFIER=trunk
+set VERSION=
+
+rem Deployment lab settings
+rem set QUALIFIER=tags
+rem set VERSION=1.0.0-rc1.1
 
 set MODULE=ks-core-db
 call removeThenCopyDb %MODULE% %QUALIFIER% %VERSION%
