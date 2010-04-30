d5fe492ec05cce9569d99647981ca767b25a1b67
jcaddel <jcaddel@kuali.org>
2010-04-30 19:07:27 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk/test/driver/trunk@12694 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- update.cmd
+++ update.cmd
@@ -8,6 +8,6 @@ call removeThenCopy ks-core %QUALIFIER% %VERSION%
 call removeThenCopy brms %QUALIFIER% %VERSION%
 call removeThenCopy ks-lum %QUALIFIER% %VERSION%
 call removeThenCopy ks-web %QUALIFIER% %VERSION%
-call cfg-dbs
+call dbs
 date /t 
 time /t
