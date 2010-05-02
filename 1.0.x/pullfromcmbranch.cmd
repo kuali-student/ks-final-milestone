d5ea08e0ac7140754997cac5c27c6839bc1407d7
jcaddel <jcaddel@kuali.org>
2010-05-02 17:48:56 -0400

    Script to pull changes from /branches/ks-pom-1.0.x into /trunk/1.0.x
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@12973 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ 1.0.x/pullfromcmbranch.cmd
@@ -0,0 +1,16 @@
+@echo off
+if "%1"=="" goto oops
+
+set REVISION=%1
+@echo on
+svn merge -r %REVISION%:HEAD https://test.kuali.org/svn/student/deploymentlab/branches/ks-cm-1.0.x
+@echo off
+
+goto end
+
+:oops
+
+echo ------------------------------------------------------------------
+echo Error!!! You must supply a revision number: pullfromcmbranch 12967
+echo ------------------------------------------------------------------
+:end
