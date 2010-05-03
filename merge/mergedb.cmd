4883771be2a80c2e370f7b5857dc8f911147f686
jcaddel <jcaddel@kuali.org>
2010-05-03 19:27:45 -0400

    Fixed path issue
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk/test/driver/trunk@13037 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- merge/mergedb.cmd
+++ merge/mergedb.cmd
@@ -4,7 +4,7 @@ set REVISION=%2
 set BASEDIR=..\..\..\..\1.0.x\ks-cfg-dbs
 set BASEURL=https://test.kuali.org/svn/student/ks-cfg-dbs
 
-set DIR=%BASEDIR%\%MODULE%
+set DIR=%BASEDIR%\%MODULE%\src\main\resources\impex
 set URL=%BASEURL%/%MODULE%/trunk
 
 echo %URL%
