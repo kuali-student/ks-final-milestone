b7eefb8663eda3bc2270fd3d3a4ab41a94d96b35
neerav <neerav@kuali.org>
2010-05-04 16:05:18 -0400

    Adding reloadPage call to enable scroll table for gwt 2.0
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@13081 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-ui/src/main/java/org/kuali/student/core/organization/ui/client/mvc/view/PositionTable.java
+++ ks-core-ui/src/main/java/org/kuali/student/core/organization/ui/client/mvc/view/PositionTable.java
@@ -161,6 +161,7 @@ public class PositionTable extends Composite{
         layout.clear();
 //        layout.add(pagingOptions);
         layout.add(pagingScrollTable);
+        pagingScrollTable.reloadPage();		//FIXME Undesirable solution to work with GWT 2.0
         pagingScrollTable.fillWidth();
     }
 
