70c88c4819be62b110e8c5fbf6785ebb4b3e13e6
glindhol <glindhol@kuali.org>
2010-05-11 19:40:47 -0400

    [KSCOR-204, UI Cleanup]: TODO Cleanup in common-ui
    git-svn-id: https://svn.kuali.org/repos/student/ks-core/branches/ks-core-dev@13344 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-ui/src/main/java/org/kuali/student/common/ui/client/configurable/mvc/sections/InfoMessage.java
+++ ks-common-ui/src/main/java/org/kuali/student/common/ui/client/configurable/mvc/sections/InfoMessage.java
@@ -17,36 +17,35 @@ package org.kuali.student.common.ui.client.configurable.mvc.sections;
 
 import org.kuali.student.common.ui.client.theme.Theme;
 import org.kuali.student.common.ui.client.widgets.KSImage;
-import org.kuali.student.common.ui.client.widgets.KSLabel;
 import org.kuali.student.common.ui.client.widgets.layout.HorizontalBlockFlowPanel;
 
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
 
 public class InfoMessage extends Composite{
 	private HorizontalBlockFlowPanel layout = new HorizontalBlockFlowPanel();
 	private KSImage icon = Theme.INSTANCE.getCommonImages().getWarningDiamondIcon();
-	//TODO make changes to label to take in special text or an object that can links in it
-	private KSLabel message = new KSLabel();
-	
+	private HTMLPanel message = new HTMLPanel("");
+
 	public InfoMessage(String text, boolean messageVisible){
 		layout.add(icon);
-		this.message.setText(text);
+        message.getElement().setInnerHTML(text);
 		layout.add(message);
 		layout.addStyleName("ks-message-static");
 		this.initWidget(layout);
 		this.setVisible(messageVisible);
 	}
-	
-	
+
+
 	public InfoMessage(){
 		layout.add(icon);
 		layout.add(message);
 		this.initWidget(layout);
 		this.setVisible(false);
 	}
-	
+
 	public void setMessage(String text, boolean messageVisible){
-		message.setText(text);
+	    message.getElement().setInnerHTML(text);
 		this.setVisible(messageVisible);
 	}
 }
