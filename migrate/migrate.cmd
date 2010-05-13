cffc61a0775fe79da0d62659648109241e6f3f83
jcaddel <jcaddel@kuali.org>
2010-05-13 17:15:11 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk/test/driver/trunk@13589 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ migrate/migrate.cmd
@@ -0,0 +1,5 @@
+svn copy -m "Migrating Security" https://test.kuali.org/svn/student/ks-security/branches/ks-security-dev https://test.kuali.org/svn/student/trunk/ks-security
+svn copy -m "Migrating Core" https://test.kuali.org/svn/student/ks-core/branches/ks-core-dev https://test.kuali.org/svn/student/trunk/ks-core
+svn copy -m "Migrating BRMS" https://test.kuali.org/svn/student/brms/branches/brms-dev https://test.kuali.org/svn/student/trunk/brms
+svn copy -m "Migrating LUM" https://test.kuali.org/svn/student/ks-lum/branches/ks-lum-dev https://test.kuali.org/svn/student/trunk/ks-lum
+svn copy -m "Migrating Web" https://test.kuali.org/svn/student/ks-web/branches/ks-web-dev https://test.kuali.org/svn/student/trunk/ks-web
