2613114975d2cb40dc314f83f5458c79081bc003
depstein <depstein@kuali.org>
2010-05-18 09:47:41 -0400

    added TestStub
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-poc-1.1.x-br@13798 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum/ks-lum-impl/src/test/java/org/kuali/student/lum/course/service/impl/TestCourseServiceImpl.java
@@ -0,0 +1,19 @@
+package org.kuali.student.lum.course.service.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kuali.student.lum.course.service.CourseService;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:course-test-config.xml"})
+public class TestCourseServiceImpl {
+
+	CourseService courseService;
+	
+	@Test
+	public void testCreate(){
+		
+	}
+}
