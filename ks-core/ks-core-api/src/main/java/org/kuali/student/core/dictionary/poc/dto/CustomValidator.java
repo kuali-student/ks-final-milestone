aee1bf47775814e71a7148e9002162c97873b832
depstein <depstein@kuali.org>
2010-05-25 15:25:05 -0400

    got rid of bad dependencies
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-poc-1.1.x-br@13982 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-api/src/main/java/org/kuali/student/core/dictionary/poc/dto/CustomValidator.java
+++ ks-core/ks-core-api/src/main/java/org/kuali/student/core/dictionary/poc/dto/CustomValidator.java
@@ -1,13 +1,10 @@
 package org.kuali.student.core.dictionary.poc.dto;
 
 import java.util.List;
-import java.util.Stack;
 
-import org.kuali.student.common.validator.poc.ConstraintDataProvider;
 import org.kuali.student.core.validation.dto.ValidationResultInfo;
 
 public interface CustomValidator {
 	public List<ValidationResultInfo> validateField(FieldDefinition field,
-			ObjectStructureDefinition objStruct,
-			ConstraintDataProvider dataProvider, Stack<String> elementStack);
+			ObjectStructureDefinition objStruct, Object value);
 }
