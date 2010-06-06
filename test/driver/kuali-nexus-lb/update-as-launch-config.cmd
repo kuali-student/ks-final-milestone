58cb7f52a773daada3d7eb43e3c333a6d2c0d60b
jcaddel <jcaddel@kuali.org>
2010-06-06 11:42:39 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@14199 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ test/driver/kuali-nexus-lb/update-as-launch-config.cmd
@@ -0,0 +1,20 @@
+@echo off
+if "%1"=="" goto oops
+if "%2"=="" goto oops
+set VERSION=%1
+set AMI=%2
+
+echo -----------------------------------------------------
+call as-version
+call as-create-launch-config kuali-nexus-lc-%VERSION% --image-id %AMI% --instance-type m1.small --group nexus.ks,default --key ks-key
+call as-update-auto-scaling-group kuali-nexus-as --launch-configuration kuali-nexus-lc-%VERSION%
+echo -----------------------------------------------------
+
+goto end
+
+:oops
+
+echo ------------------------------------------------------------
+echo Error!!! You must supply version and AMI Id: 01 ami-c4bf57ad
+echo ------------------------------------------------------------
+:end
