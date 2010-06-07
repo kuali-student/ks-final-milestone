8539b33e50b44b2ba1bbd1ba64cb5c877c5d1655
neerav <neerav@kuali.org>
2010-06-07 13:35:28 -0400

    Reverting : CommonLookup inheritance
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-poc-1.1.x-br@14213 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-api/src/main/java/org/kuali/student/core/dictionary/poc/dto/LookupConstraint.java
+++ ks-core/ks-core-api/src/main/java/org/kuali/student/core/dictionary/poc/dto/LookupConstraint.java
@@ -5,20 +5,30 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 
-import org.kuali.student.core.assembly.data.CommonLookup;
-
 @XmlAccessorType(XmlAccessType.FIELD)
-public class LookupConstraint extends CommonLookup {
-
-	private static final long serialVersionUID = 1L;
-	
-	//Inherit fields from CommonLookup
-//	private String searchTypeId; // id of search type defined in search xml
-//	private String resultReturnKey; // key of searchResultColumn to map back to
+public class LookupConstraint extends BaseConstraint {
+	private String searchTypeId; // id of search type defined in search xml
+	private String resultReturnKey; // key of searchResultColumn to map back to
 									// this field
 	protected List<LookupConstraintParamMapping> lookupParams; // maps fields to
 																// search
 																// params?
+
+	public String getSearchTypeId() {
+		return searchTypeId;
+	}
+
+	public void setSearchTypeId(String searchTypeId) {
+		this.searchTypeId = searchTypeId;
+	}
+
+	public String getResultReturnKey() {
+		return resultReturnKey;
+	}
+
+	public void setResultReturnKey(String resultReturnKey) {
+		this.resultReturnKey = resultReturnKey;
+	}
 
 	public List<LookupConstraintParamMapping> getLookupParams() {
 		return lookupParams;
