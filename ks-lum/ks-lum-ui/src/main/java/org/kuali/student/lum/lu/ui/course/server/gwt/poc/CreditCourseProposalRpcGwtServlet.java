2f27a74c1142e1e10aec1fc9d48a897893fae11c
wgomes <wgomes@kuali.org>
2010-06-16 16:21:50 -0400

    Remove poc assembler. Add format type.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-poc-1.1.x-br@14357 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/server/gwt/poc/CreditCourseProposalRpcGwtServlet.java
+++ ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/server/gwt/poc/CreditCourseProposalRpcGwtServlet.java
@@ -23,6 +23,7 @@ import org.kuali.student.core.assembly.data.Data;
 import org.kuali.student.core.proposal.dto.ProposalInfo;
 import org.kuali.student.core.proposal.service.ProposalService;
 import org.kuali.student.lum.course.dto.CourseInfo;
+import org.kuali.student.lum.course.dto.FormatInfo;
 import org.kuali.student.lum.course.service.CourseService;
 import org.kuali.student.lum.lu.assembly.ModifyCreditCourseProposalManager;
 import org.kuali.student.lum.lu.ui.course.client.service.CreditCourseProposalRpcService;
@@ -35,6 +36,8 @@ public class CreditCourseProposalRpcGwtServlet extends
 	final static Logger LOG = Logger.getLogger(CreditCourseProposalRpcGwtServlet.class);
 
 	public static final String COURSE_TYPE = "kuali.lu.type.CreditCourse";
+	public static final String COURSE_FORMAT_TYPE = "kuali.lu.type.CreditCourseFormatShell";
+	
 	private static final String WF_TYPE_CLU_DOCUMENT = "CluCreditCourseProposal";
 	private static final String DEFAULT_METADATA_STATE = "draft";
 
@@ -64,6 +67,13 @@ public class CreditCourseProposalRpcGwtServlet extends
 	
 		courseInfo.setType(COURSE_TYPE);
 		courseInfo.setState(DEFAULT_METADATA_STATE);
+		
+		//Set types (should this happen here)
+        courseInfo.setType(COURSE_TYPE);
+        for (FormatInfo format:courseInfo.getFormats()){
+        	format.setType(COURSE_FORMAT_TYPE);
+        }            
+		
 		if (courseInfo.getId() == null){
 			courseInfo = courseService.createCourse(courseInfo);
 		} else {
