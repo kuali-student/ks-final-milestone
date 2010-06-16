14a501b9334208b22ce6c85a4ef2e27db0209f08
wgomes <wgomes@kuali.org>
2010-06-16 16:19:10 -0400

    Fix when proposal info object gets created.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-poc-1.1.x-br@14356 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/server/gwt/poc/CourseProposalFilter.java
+++ ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/server/gwt/poc/CourseProposalFilter.java
@@ -9,6 +9,7 @@ import org.kuali.student.core.assembly.transform.AbstractDataFilter;
 import org.kuali.student.core.assembly.transform.MetadataFilter;
 import org.kuali.student.core.proposal.dto.ProposalInfo;
 import org.kuali.student.core.proposal.service.ProposalService;
+import org.kuali.student.lum.course.service.CourseService;
 
 /**
  * This is only a POC filter which just creates a proposal object and reference ino the proposal service.
@@ -22,31 +23,51 @@ public class CourseProposalFilter extends AbstractDataFilter implements Metadata
     public static final String PROPOSAL_TYPE_CREATE_COURSE = "kuali.proposal.type.course.create";
 		
 	private ProposalService proposalService;
-	
+	private CourseService courseService;
+
+	/**
+	 *	This creates a proposal object if it does not exist and sets a dynamic attribute
+	 *  for proposal id. If one does it exist it updates the proposal object with new values.
+	 */
 	@Override
-	public void applyOutboundDataFilter(Data data, Metadata metadata,
+	public void applyInboundDataFilter(Data data, Metadata metadata,
 			Map<String, String> properties) throws Exception {
 		ProposalInfo proposalInfo;
 		
 		String proposalId = data.query("proposalId");
-		String proposalTitle = data.query("courseTitle");
-		String proposalRationale = data.query("proposalRationale");
-		if (proposalId != null && !proposalId.isEmpty()){
-			proposalInfo = proposalService.getProposal(proposalId);
-			proposalInfo.setName(proposalTitle);
-			proposalService.updateProposal(proposalId, proposalInfo);
-		} else {
+		
+		if (proposalId == null) {
 			proposalInfo = new ProposalInfo();
 			proposalInfo.setType(PROPOSAL_TYPE_CREATE_COURSE);
-			proposalInfo.setName(proposalTitle);
-			proposalInfo.setProposalReferenceType(PROPOSAL_REFERENCE_TYPE);
-			proposalInfo.setRationale(proposalRationale);
-			proposalInfo.getProposalReference().add((String)data.query("id"));
 			proposalInfo = proposalService.createProposal(proposalInfo.getType(), proposalInfo);
 			data.set("proposalId", proposalInfo.getId());
 		}			
 	}
 	
+	
+	@Override
+	public void applyOutboundDataFilter(Data data, Metadata metadata,
+			Map<String, String> properties) throws Exception {
+		String courseId = data.query("id");
+		String proposalId = data.query("proposalId");
+		String proposalTitle = data.query("courseTitle");		
+		String proposalRationale = data.query("proposalRationale");
+		
+		if (proposalId != null && !proposalId.isEmpty()){
+			ProposalInfo proposalInfo = proposalService.getProposal(proposalId);
+			proposalInfo.setName(proposalTitle);
+			proposalInfo.setRationale(proposalRationale);
+
+			if (proposalInfo.getProposalReference().isEmpty()){
+				proposalInfo.setProposalReferenceType(PROPOSAL_REFERENCE_TYPE);
+				proposalInfo.getProposalReference().add(courseId);
+			}
+			
+			proposalService.updateProposal(proposalId, proposalInfo);
+		}
+	}
+
+
 	public void setProposalService(ProposalService proposalService) {
 		this.proposalService = proposalService;
 	}
