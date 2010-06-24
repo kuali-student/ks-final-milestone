206c8c2b0de77b335a9975171d2047b8c9107e40
zzraly <zzraly@kuali.org>
2010-06-24 07:18:58 -0400

    [KSLUM-284] Create/Update 'nof' Course Requirement Component Type; [KSLUM-285] Create/Update 'nof' Credit Requirement Component Type
    git-svn-id: https://svn.kuali.org/repos/student/trunk@14508 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-impl/src/main/java/org/kuali/student/lum/statement/config/context/lu/NLHelper.java
+++ ks-lum/ks-lum-impl/src/main/java/org/kuali/student/lum/statement/config/context/lu/NLHelper.java
@@ -2,11 +2,19 @@ package org.kuali.student.lum.statement.config.context.lu;
 
 public class NLHelper {
 
-	public static String getProperGrammer(Number number, String singularText, String pluralText) {
+	public static String getProperGrammar(Number number, String singularText, String pluralText) {
 		return (number.intValue() == 1 ? singularText : pluralText);
 	}
 	
-	public static String getProperGrammer(String number, String singularText, String pluralText) {
-		return getProperGrammer(Integer.valueOf(number), singularText, pluralText);
+    public static String getProperGrammar(Number number, String singularText) {
+        return (number.intValue() == 1 ? singularText : singularText + 's');
+    }	
+
+	public static String getProperGrammar(String number, String singularText, String pluralText) {
+		return getProperGrammar(Integer.valueOf(number), singularText, pluralText);
+	}
+	
+	public static String getProperGrammar(String number, String singularText) {
+		return getProperGrammar(Integer.valueOf(number), singularText);
 	}
 }
