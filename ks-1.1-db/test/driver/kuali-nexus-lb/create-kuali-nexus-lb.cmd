921339eb190811329e80f6a5022c47a009dc364e
jcaddel <jcaddel@kuali.org>
2010-07-21 18:52:51 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab@14802 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-1.1-db/test/driver/kuali-nexus-lb/create-kuali-nexus-lb.cmd
+++
@@ -1,8 +0,0 @@
-call ec2-version
-call elb-version
-call as-version
-call elb-create-lb kuali-nexus-lb --headers --listener "lb-port=80,instance-port=80,protocol=TCP" --availability-zones "us-east-1a,us-east-1b,us-east-1c,us-east-1d"
-call elb-configure-healthcheck kuali-nexus-lb --headers --target "TCP:80" --interval 5 --timeout 1 --unhealthy-threshold 3 --healthy-threshold 3
-call as-create-launch-config kuali-nexus-lc --image-id ami-c4bf57ad --instance-type m1.small --group nexus.ks,default --key ks-key
-call as-create-auto-scaling-group kuali-nexus-as --launch-configuration kuali-nexus-lc --availability-zones us-east-1a,us-east-1b,us-east-1c,us-east-1d --min-size 1 --max-size 3 --load-balancers kuali-nexus-lb
-call as-set-desired-capacity kuali-nexus-as --desired-capacity 2
