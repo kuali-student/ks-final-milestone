102f812b36ef6f24114c765cede1e7d1ffe57471
jcaddel <jcaddel@kuali.org>
2010-07-21 22:57:57 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@14933 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- test/driver/svn-scripts/migrate-db/migrate-one-db.cmd
+++ test/driver/svn-scripts/migrate-db/migrate-one-db.cmd
@@ -2,8 +2,8 @@ set BASE_URL=%1
 set DB=%2
 
 svn move -m "Renaming %DB% to %DB%-impex" %BASE_URL%/ks-cfg-dbs/%DB% %BASE_URL%/ks-cfg-dbs/%DB%-impex 
-svn move -m "Moving %DB%-impex to %DB%/src/main/impex/datasets/reference" --parents %BASE_URL%/ks-cfg-dbs/%DB%-impex %BASE_URL%/ks-cfg-dbs/%DB%/src/main/impex/datasets/reference
-svn move -m "Moving %DB% schema.xml out of the datasets directory" %BASE_URL%/ks-cfg-dbs/%DB%/src/main/impex/datasets/reference/schema.xml %BASE_URL%/ks-cfg-dbs/%DB%/src/main/impex/schema.xml 
-svn delete -m "Removing unnecessary %DB% .project file" %BASE_URL%/ks-cfg-dbs/%DB%/src/main/impex/datasets/reference/.project 
-svn delete -m "Removing unnecessary %DB% data.dtd file" %BASE_URL%/ks-cfg-dbs/%DB%/src/main/impex/datasets/reference/data.dtd 
-svn delete -m "Removing unnecessary %DB% database.dtd file" %BASE_URL%/ks-cfg-dbs/%DB%/src/main/impex/datasets/reference/database.dtd 
+svn move -m "Moving %DB%-impex to %DB%/src/main/impex/data" --parents %BASE_URL%/ks-cfg-dbs/%DB%-impex %BASE_URL%/ks-cfg-dbs/%DB%/src/main/impex/data
+svn move -m "Moving %DB% schema.xml out of the data directory" %BASE_URL%/ks-cfg-dbs/%DB%/src/main/impex/data/schema.xml %BASE_URL%/ks-cfg-dbs/%DB%/src/main/impex/schema.xml 
+svn delete -m "Removing unnecessary %DB% .project file" %BASE_URL%/ks-cfg-dbs/%DB%/src/main/impex/data/.project 
+svn delete -m "Removing unnecessary %DB% data.dtd file" %BASE_URL%/ks-cfg-dbs/%DB%/src/main/impex/data/data.dtd 
+svn delete -m "Removing unnecessary %DB% database.dtd file" %BASE_URL%/ks-cfg-dbs/%DB%/src/main/impex/data/database.dtd 
