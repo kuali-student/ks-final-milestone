32813b9d2b3ad6cd87826a03a1f9f33de89a0d84
wgomes <wgomes@kuali.org>
2010-08-16 12:32:02 -0400

    Refactor old metadata service test to old package
    git-svn-id: https://svn.kuali.org/repos/student/trunk@15479 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-common-impl/src/test/java/org/kuali/student/common/assembly/dictionary/old/MockDictionaryService.java
+++ ks-core/ks-common-impl/src/test/java/org/kuali/student/common/assembly/dictionary/old/MockDictionaryService.java
@@ -13,7 +13,7 @@
  * permissions and limitations under the License.
  */
 
-package org.kuali.student.common.assembly.dictionary;
+package org.kuali.student.common.assembly.dictionary.old;
 
 import java.util.List;
 
