5e48b83eecf5434ee00e58c46e617a3ca006e58a
bsmith <bsmith@kuali.org>
2010-08-17 15:59:33 -0400

    An inline view controller so a separate model and validation can be used with in the context of another controller
    git-svn-id: https://svn.kuali.org/repos/student/trunk@15510 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-common-ui/src/main/java/org/kuali/student/common/ui/client/mvc/Controller.java
+++ ks-core/ks-common-ui/src/main/java/org/kuali/student/common/ui/client/mvc/Controller.java
@@ -60,7 +60,7 @@ public abstract class Controller extends Composite implements HistorySupport, Br
     private String defaultModelId = null;
     private ViewContext context = new ViewContext();
     private final Map<String, ModelProvider<? extends Model>> models = new HashMap<String, ModelProvider<? extends Model>>();
-
+    private boolean fireNavEvents = true;
     private HandlerManager applicationEventHandlers = new HandlerManager(this);
 
     protected Controller(final String controllerId) {
@@ -178,7 +178,9 @@ public abstract class Controller extends Composite implements HistorySupport, Br
 			            //fireApplicationEvent(new ViewChangeEvent(currentView, view));
 			            currentView = view;
 			            GWT.log("renderView " + viewType.toString(), null);
-			            fireNavigationEvent();
+			            if(fireNavEvents){
+			            	fireNavigationEvent();
+			            }
 			            renderView(view);
 			        	onReadyCallback.exec(true);
 			        	
@@ -501,6 +503,10 @@ public abstract class Controller extends Composite implements HistorySupport, Br
     
     public void resetCurrentView(){
     	currentView = null;
+    }
+    
+    public void fireNavEvents(boolean fireEvents){
+    	fireNavEvents = fireEvents;
     }
     
 
