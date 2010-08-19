8da5f172377fdfcd7f154fef6a9dbdae87355324
jcaddel <jcaddel@kuali.org>
2010-08-19 13:17:48 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@15562 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ maven-impex-plugin/src/main/java/org/apache/torque/mojo/MojoDatabaseListener.java
@@ -0,0 +1,116 @@
+package org.apache.torque.mojo;
+
+import org.kuali.core.db.torque.PrettyPrint;
+import org.kuali.core.db.torque.Utils;
+import org.kuali.db.DatabaseEvent;
+import org.kuali.db.DatabaseListener;
+import org.apache.commons.lang.StringUtils;
+import org.apache.maven.plugin.logging.Log;
+
+/**
+ * A mojo friendly listener for database events
+ */
+public class MojoDatabaseListener implements DatabaseListener {
+	Utils utils = new Utils();
+	PrettyPrint prettyPrint;
+	Log log;
+	int updateStatusInterval = 150;
+
+	public MojoDatabaseListener() {
+		this(null);
+	}
+
+	public MojoDatabaseListener(Log log) {
+		super();
+		this.log = log;
+	}
+
+	@Override
+	public void messageLogged(DatabaseEvent event) {
+		switch (event.getPriority()) {
+		case DEBUG:
+			getLog().debug(event.getMessage());
+			break;
+		case INFO:
+			getLog().info(event.getMessage());
+			break;
+		case WARN:
+			getLog().warn(event.getMessage());
+			break;
+		case ERROR:
+			getLog().error(event.getMessage(), event.getException());
+			break;
+		default:
+			getLog().warn("Unknown message priority " + event.getPriority() + " for message: " + event.getMessage());
+			break;
+		}
+	}
+
+	@Override
+	public void beginTransaction(DatabaseEvent event) {
+		String message = event.getTransaction().getResourceLocation();
+		if (!StringUtils.isEmpty(event.getTransaction().getSqlCommand())) {
+			message = "Executing SQL";
+		}
+		prettyPrint = new PrettyPrint("[INFO] " + message);
+		utils.left(prettyPrint);
+	}
+
+	@Override
+	public void finishTransaction(DatabaseEvent event) {
+		utils.right(prettyPrint);
+		prettyPrint = null;
+	}
+
+	@Override
+	public void beforeExecuteSQL(DatabaseEvent event) {
+		int totalStatements = event.getTotalStatements();
+		if ((totalStatements % updateStatusInterval) == 0) {
+			System.out.print(".");
+			prettyPrint.setMsg(prettyPrint.getMsg() + ".");
+		}
+	}
+
+	@Override
+	public void afterExecuteSQL(DatabaseEvent event) {
+	}
+
+	@Override
+	public void afterProcessingSQLResults(DatabaseEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public PrettyPrint getPrettyPrint() {
+		return prettyPrint;
+	}
+
+	public void setPrettyPrint(PrettyPrint pp) {
+		this.prettyPrint = pp;
+	}
+
+	public Log getLog() {
+		return log;
+	}
+
+	public void setLog(Log log) {
+		this.log = log;
+	}
+
+	public Utils getUtils() {
+		return utils;
+	}
+
+	public void setUtils(Utils utils) {
+		this.utils = utils;
+	}
+
+	public int getUpdateStatusInterval() {
+		return updateStatusInterval;
+	}
+
+	public void setUpdateStatusInterval(int updateStatusInterval) {
+		this.updateStatusInterval = updateStatusInterval;
+	}
+
+}
---
+++ maven-impex-plugin/src/main/java/org/apache/torque/mojo/MorphDataMojo.java
@@ -0,0 +1,126 @@
+package org.apache.torque.mojo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.commons.io.FileUtils.*;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.plexus.util.DirectoryScanner;
+import org.codehaus.plexus.util.StringUtils;
+import org.kuali.core.db.torque.PrettyPrint;
+import org.kuali.core.db.torque.Utils;
+
+/**
+ * Converts Ant impex data XML files into maven-impex-plugin data XML files
+ * 
+ * @goal morphdata
+ * @phase generate-sources
+ */
+public class MorphDataMojo extends MorpherMojo {
+
+	/**
+	 * The directory in which the morphed XML will be generated.
+	 * 
+	 * @parameter expression="${newDataOutputDir}" default-value="${project.build.directory}/generated-impex/xml"
+	 * @required
+	 */
+	private File newDataOutputDir;
+
+	/**
+	 * The directory containing the source (non-morphed) data XML files
+	 * 
+	 * @parameter expression="${oldDataXMLDir}" default-value="${basedir}/src/main/impex"
+	 * @required
+	 */
+	private File oldDataXMLDir;
+
+	/**
+	 * The default set of files in that directory to include (ant style notation)
+	 * 
+	 * @parameter expression="${oldDataXMLIncludes}" default-value="*.xml"
+	 * @required
+	 */
+	private String oldDataXMLIncludes;
+
+	/**
+	 * The default set of files in that directory to exclude (ant style notation)
+	 * 
+	 * @parameter expression="${oldDataXMLExcludes}" default-value="*schema.xml"
+	 */
+	private String oldDataXMLExcludes;
+
+	protected String[] getFiles() throws IOException {
+		DirectoryScanner ds = new DirectoryScanner();
+		ds.setIncludes(new String[] { getOldDataXMLIncludes() });
+		if (getOldDataXMLExcludes() != null) {
+			ds.setExcludes(new String[] { getOldDataXMLExcludes() });
+		}
+		ds.setBasedir(getOldDataXMLDir());
+		ds.scan();
+		return ds.getIncludedFiles();
+	}
+
+	protected void executeMorph() throws MojoExecutionException {
+		Utils utils = new Utils();
+		try {
+			String[] files = getFiles();
+			PrettyPrint pp = new PrettyPrint("[INFO] Converting " + files.length + " data XML files");
+			utils.left(pp);
+			String inputPath = getOldDataXMLDir().getAbsolutePath();
+			forceMkdir(getNewDataOutputDir());
+			List<String> input = new ArrayList<String>();
+			List<String> output = new ArrayList<String>();
+			for (String file : files) {
+				input.add(inputPath + FS + file);
+				output.add(getNewDataOutputDir() + FS + file);
+			}
+			for (int i = 0; i < input.size(); i++) {
+				String filename = input.get(i);
+				String contents = readFileToString(new File(filename), getEncoding());
+				contents = getMorphedContents(contents);
+				writeStringToFile(new File(output.get(i)), contents, getEncoding());
+			}
+			utils.right(pp);
+		} catch (IOException e) {
+			throw new MojoExecutionException("Unexpected error", e);
+		}
+	}
+
+	protected String getMorphedContents(String contents) {
+		return StringUtils.replace(contents, "\"data.dtd\"", '"' + getProject().getArtifactId() + "-data.dtd\"");
+	}
+
+	public File getNewDataOutputDir() {
+		return newDataOutputDir;
+	}
+
+	public void setNewDataOutputDir(File newDataOutputDir) {
+		this.newDataOutputDir = newDataOutputDir;
+	}
+
+	public File getOldDataXMLDir() {
+		return oldDataXMLDir;
+	}
+
+	public void setOldDataXMLDir(File oldDataXMLDir) {
+		this.oldDataXMLDir = oldDataXMLDir;
+	}
+
+	public String getOldDataXMLIncludes() {
+		return oldDataXMLIncludes;
+	}
+
+	public void setOldDataXMLIncludes(String oldDataXMLIncludes) {
+		this.oldDataXMLIncludes = oldDataXMLIncludes;
+	}
+
+	public String getOldDataXMLExcludes() {
+		return oldDataXMLExcludes;
+	}
+
+	public void setOldDataXMLExcludes(String oldDataXMLExcludes) {
+		this.oldDataXMLExcludes = oldDataXMLExcludes;
+	}
+}
