76a3aeee710f2725afdbc81aaa09a5cc43057613
ikuplevich <ikuplevich@kuali.org>
2010-08-18 22:17:07 -0400

    Introduced TableSection on CatalogInformationViewConfiguration
    git-svn-id: https://svn.kuali.org/repos/student/trunk@15546 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/edit/CatalogInformationEditConfiguration.java
+++ ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/edit/CatalogInformationEditConfiguration.java
@@ -7,35 +7,26 @@ import org.kuali.student.lum.program.client.ProgramConstants;
 import org.kuali.student.lum.program.client.ProgramSections;
 import org.kuali.student.lum.program.client.configuration.base.AbstractConfiguration;
 import org.kuali.student.lum.program.client.properties.ProgramProperties;
-import org.kuali.student.lum.program.client.view.ProgramViewConfigurer;
 
 public class CatalogInformationEditConfiguration extends AbstractConfiguration<ProgramEditConfigurer> {
-    private VerticalSectionView editView;
+    private VerticalSectionView mainView;
 
-    public CatalogInformationEditConfiguration() {
-    	super();
-    }
-    
     @Override
     public View getView() {
-        if (editView == null) {
-        	editView = createEditView();
-        }
-        return editView;
+        createEditView();
+        return mainView;
     }
-    
+
     private VerticalSectionView createEditView() {
-    	VerticalSectionView view = new VerticalSectionView(ProgramSections.CATALOG_INFO_EDIT, ProgramProperties.get().program_menu_sections_catalogInfo(), ProgramConstants.PROGRAM_MODEL_ID);
-        configurer.addField(view, ProgramConstants.DESCRIPTION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_descr()));
-        configurer.addField(view, ProgramConstants.CATALOG_DESCRIPTION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_catalogDescr()));
-//@TODO show Core Faculty Members if not empty
-        configurer.addField(view, ProgramConstants.CORE_FACULTY_MEMBERS, new MessageKeyInfo(ProgramProperties.get().catalogInformation_publishedInstructors()));
-//@TODO "Not Published" disallows Catalog checkboxes
-        configurer.addField(view, ProgramConstants.PUBLICATION_TARGETS, new MessageKeyInfo(ProgramProperties.get().catalogInformation_catalogPublicationTargets()));
-        configurer.addField(view, ProgramConstants.FULL_PART_TIME, new MessageKeyInfo(ProgramProperties.get().catalogInformation_intensity()));
-        configurer.addField(view, ProgramConstants.DURATION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_stdDuration()));
-        configurer.addField(view, ProgramConstants.MORE_INFORMATION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_referenceUrl()));
-    	return view;
+        mainView= new VerticalSectionView(ProgramSections.CATALOG_INFO_EDIT, ProgramProperties.get().program_menu_sections_catalogInfo(), ProgramConstants.PROGRAM_MODEL_ID);
+        configurer.addField(mainView, ProgramConstants.DESCRIPTION + "/plain", new MessageKeyInfo(ProgramProperties.get().catalogInformation_descr()));
+        configurer.addField(mainView, ProgramConstants.CATALOG_DESCRIPTION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_catalogDescr()));
+        configurer.addField(mainView, ProgramConstants.CORE_FACULTY_MEMBERS, new MessageKeyInfo(ProgramProperties.get().catalogInformation_publishedInstructors()));
+        configurer.addField(mainView, ProgramConstants.PUBLICATION_TARGETS, new MessageKeyInfo(ProgramProperties.get().catalogInformation_catalogPublicationTargets()));
+        configurer.addField(mainView, ProgramConstants.FULL_PART_TIME, new MessageKeyInfo(ProgramProperties.get().catalogInformation_intensity()));
+        configurer.addField(mainView, ProgramConstants.DURATION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_stdDuration()));
+        configurer.addField(mainView, ProgramConstants.MORE_INFORMATION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_referenceUrl()));
+        return mainView;
     }
 
 }
--- ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/view/CatalogInformationViewConfiguration.java
+++ ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/view/CatalogInformationViewConfiguration.java
@@ -1,8 +1,8 @@
 package org.kuali.student.lum.program.client.view;
 
+import org.kuali.student.common.ui.client.configurable.mvc.sections.TableSection;
 import org.kuali.student.common.ui.client.configurable.mvc.views.VerticalSectionView;
 import org.kuali.student.common.ui.client.mvc.View;
-import org.kuali.student.common.ui.client.widgets.KSLabel;
 import org.kuali.student.common.ui.client.widgets.field.layout.element.MessageKeyInfo;
 import org.kuali.student.lum.program.client.ProgramConstants;
 import org.kuali.student.lum.program.client.ProgramSections;
@@ -24,12 +24,14 @@ public class CatalogInformationViewConfiguration extends AbstractConfiguration<P
 
     private void createShowView() {
         mainSection = new VerticalSectionView(ProgramSections.CATALOG_INFO_VIEW, ProgramProperties.get().program_menu_sections_catalogInfo(), ProgramConstants.PROGRAM_MODEL_ID);
-        configurer.addReadOnlyField(mainSection, ProgramConstants.DESCRIPTION + "/plain", new MessageKeyInfo(ProgramProperties.get().catalogInformation_descr()));
-        configurer.addReadOnlyField(mainSection, ProgramConstants.CATALOG_DESCRIPTION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_catalogDescr()));
-        configurer.addReadOnlyField(mainSection, ProgramConstants.CORE_FACULTY_MEMBERS, new MessageKeyInfo(ProgramProperties.get().catalogInformation_publishedInstructors()));
-        configurer.addReadOnlyField(mainSection, ProgramConstants.PUBLICATION_TARGETS, new MessageKeyInfo(ProgramProperties.get().catalogInformation_catalogPublicationTargets()));
-        configurer.addReadOnlyField(mainSection, ProgramConstants.FULL_PART_TIME, new MessageKeyInfo(ProgramProperties.get().catalogInformation_intensity()));
-        configurer.addReadOnlyField(mainSection, ProgramConstants.DURATION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_stdDuration()));
-        configurer.addReadOnlyField(mainSection, ProgramConstants.MORE_INFORMATION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_referenceUrl()));
+        TableSection section = new TableSection();
+        configurer.addReadOnlyField(section, ProgramConstants.DESCRIPTION + "/plain", new MessageKeyInfo(ProgramProperties.get().catalogInformation_descr()));
+        configurer.addReadOnlyField(section, ProgramConstants.CATALOG_DESCRIPTION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_catalogDescr()));
+        configurer.addReadOnlyField(section, ProgramConstants.CORE_FACULTY_MEMBERS, new MessageKeyInfo(ProgramProperties.get().catalogInformation_publishedInstructors()));
+        configurer.addReadOnlyField(section, ProgramConstants.PUBLICATION_TARGETS, new MessageKeyInfo(ProgramProperties.get().catalogInformation_catalogPublicationTargets()));
+        configurer.addReadOnlyField(section, ProgramConstants.FULL_PART_TIME, new MessageKeyInfo(ProgramProperties.get().catalogInformation_intensity()));
+        configurer.addReadOnlyField(section, ProgramConstants.DURATION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_stdDuration()));
+        configurer.addReadOnlyField(section, ProgramConstants.MORE_INFORMATION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_referenceUrl()));
+        mainSection.addSection(section);
     }
 }
