f9caf499b9327d23374d2fadb5107db4327d7888
wgomes <wgomes@kuali.org>
2010-08-20 09:47:28 -0400

    Fix test assertions
    git-svn-id: https://svn.kuali.org/repos/student/trunk@15605 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-impl/src/test/java/org/kuali/student/core/dictionary/service/TestProposalInfoDictionary.java
+++ ks-core/ks-core-impl/src/test/java/org/kuali/student/core/dictionary/service/TestProposalInfoDictionary.java
@@ -14,42 +14,34 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import static org.junit.Assert.*;
 
-public class TestProposalInfoDictionary
-{
+public class TestProposalInfoDictionary {
 
- @Test
- public void testLoadProposalInfoDictionary ()
- {
-  Set<Class<?>> startingClasses = new LinkedHashSet ();
-  startingClasses.add (ProposalInfo.class);
-  String contextFile = "ks-proposalInfo-dictionary-context";
-  String outFile = "target/" + contextFile + ".txt";
-  DictionaryTesterHelper helper = new DictionaryTesterHelper (outFile,
-                                                              startingClasses,
-                                                              contextFile
-                                                              + ".xml",
-                                                              false);
-  helper.doTest ();
- }
+	@Test
+	public void testLoadProposalInfoDictionary() {
+		Set<Class<?>> startingClasses = new LinkedHashSet<Class<?>>();
+		startingClasses.add(ProposalInfo.class);
+		String contextFile = "ks-proposalInfo-dictionary-context";
+		String outFile = "target/" + contextFile + ".txt";
+		DictionaryTesterHelper helper = new DictionaryTesterHelper(outFile,
+				startingClasses, contextFile + ".xml", false);
+		helper.doTest();
+	}
 
- @Test
- public void testProposalInfoValidation () throws OperationFailedException
- {
-  ApplicationContext ac = new ClassPathXmlApplicationContext (
-    "classpath:ks-proposalInfo-dictionary-context.xml");
-  System.out.println ("h2. Validation Test");
-  DefaultValidatorImpl val = new DefaultValidatorImpl ();
-  val.setDateParser (new ServerDateParser ());
-  val.setSearchDispatcher (new MockSearchDispatcher ());
-  ProposalInfo info = new ProposalInfo ();
-  ObjectStructureDefinition os = (ObjectStructureDefinition) ac.getBean (
-    info.getClass ().getName ());
-  List<ValidationResultInfo> validationResults = val.validateObject (info, os);
-  System.out.println ("h3. With just a blank ProposalInfo");
-//  for (ValidationResultInfo vr : validationResults)
-//  {
-//   System.out.println (vr.getElement () + " " + vr.getMessage ());
-//  }
-  assertEquals (2, validationResults.size ());
- }
+	@Test
+	public void testProposalInfoValidation() throws OperationFailedException {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:ks-proposalInfo-dictionary-context.xml");
+		System.out.println("h2. Validation Test");
+		DefaultValidatorImpl val = new DefaultValidatorImpl();
+		val.setDateParser(new ServerDateParser());
+		val.setSearchDispatcher(new MockSearchDispatcher());
+		ProposalInfo info = new ProposalInfo();
+		ObjectStructureDefinition os = (ObjectStructureDefinition) ac.getBean(info.getClass().getName());
+		List<ValidationResultInfo> validationResults = val.validateObject(info,	os);
+		System.out.println("h3. With just a blank ProposalInfo");
+		// for (ValidationResultInfo vr : validationResults)
+		// {
+		// System.out.println (vr.getElement () + " " + vr.getMessage ());
+		// }
+		assertEquals(3, validationResults.size());
+	}
 }
