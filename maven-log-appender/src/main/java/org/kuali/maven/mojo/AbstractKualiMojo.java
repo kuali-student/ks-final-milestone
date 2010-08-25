81a1afa1144524ab9f7f7189162ab2a7073763e3
jcaddel <jcaddel@kuali.org>
2010-08-25 15:59:38 -0400

    ''
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@15730 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ maven-log-appender/src/main/java/org/kuali/maven/mojo/AbstractKualiMojo.java
@@ -0,0 +1,157 @@
+package org.kuali.maven.mojo;
+
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Settings;
+import org.kuali.maven.mojo.MavenLogger;
+
+/**
+ * Mojo essentials. Contains the "skip" logic that is the de facto standard for maven plugins. Contains a number of
+ * maven related properties that are common to most mojos. Also sets up logging so that if libraries called by a mojo
+ * issue log statements to Jakarta Commons Logging or Log4j, those log messages are show in maven's output
+ */
+public abstract class AbstractKualiMojo extends AbstractMojo {
+	public static final String FS = System.getProperty("file.separator");
+	public static final String SKIP_PACKAGING_TYPE = "pom";
+
+	/**
+	 * When <code>true</code>, skip the execution.
+	 * 
+	 * @since 1.0
+	 * @parameter default-value="false"
+	 */
+	private boolean skip;
+
+	/**
+	 * Setting this parameter to <code>true</code> will force the execution of this mojo, even if it would get skipped
+	 * usually.
+	 * 
+	 * @parameter expression="${forceMorpherExecution}" default-value=false
+	 * @required
+	 */
+	private boolean forceMojoExecution;
+
+	/**
+	 * @parameter expression="${encoding}" default-value="${project.build.sourceEncoding}"
+	 */
+	private String encoding;
+
+	/**
+	 * The Maven project this plugin runs in.
+	 * 
+	 * @parameter expression="${project}"
+	 * @required
+	 * @readonly
+	 */
+	private MavenProject project;
+
+	/**
+	 * @parameter expression="${settings}"
+	 * @required
+	 * @since 1.0
+	 * @readonly
+	 */
+	private Settings settings;
+
+	/**
+	 * @parameter default-value="${session}"
+	 * @required
+	 * @readonly
+	 */
+	private MavenSession mavenSession;
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		MavenLogger.startPluginLog(this);
+		if (skipMojo()) {
+			return;
+		}
+		executeMojo();
+		MavenLogger.endPluginLog(this);
+	}
+
+	protected abstract void executeMojo() throws MojoExecutionException, MojoFailureException;
+
+	/**
+	 * <p>
+	 * Determine if the mojo execution should get skipped.
+	 * </p>
+	 * This is the case if:
+	 * <ul>
+	 * <li>{@link #skip} is <code>true</code></li>
+	 * <li>if the mojo gets executed on a project with packaging type 'pom' and {@link #forceMojoExecution} is
+	 * <code>false</code></li>
+	 * </ul>
+	 * 
+	 * @return <code>true</code> if the mojo execution should be skipped.
+	 */
+	protected boolean skipMojo() {
+		if (skip) {
+			getLog().info("Skipping execution");
+			return true;
+		}
+
+		if (!forceMojoExecution && project != null && SKIP_PACKAGING_TYPE.equals(project.getPackaging())) {
+			getLog().info("Skipping execution for project with packaging type '" + SKIP_PACKAGING_TYPE + "'");
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Returns the maven project.
+	 * 
+	 * @return The maven project where this plugin runs in.
+	 */
+	public MavenProject getProject() {
+		return project;
+	}
+
+	public String getEncoding() {
+		return encoding;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	public boolean isSkip() {
+		return skip;
+	}
+
+	public void setSkip(boolean skip) {
+		this.skip = skip;
+	}
+
+	public boolean isForceMojoExecution() {
+		return forceMojoExecution;
+	}
+
+	public void setForceMojoExecution(boolean forceMojoExecution) {
+		this.forceMojoExecution = forceMojoExecution;
+	}
+
+	public Settings getSettings() {
+		return settings;
+	}
+
+	public void setSettings(Settings settings) {
+		this.settings = settings;
+	}
+
+	public MavenSession getMavenSession() {
+		return mavenSession;
+	}
+
+	public void setMavenSession(MavenSession mavenSession) {
+		this.mavenSession = mavenSession;
+	}
+
+	public void setProject(MavenProject project) {
+		this.project = project;
+	}
+}
---
+++ maven-log-appender/src/main/java/org/kuali/maven/mojo/MavenLogger.java
@@ -0,0 +1,185 @@
+package org.kuali.maven.mojo;
+
+import org.apache.commons.logging.Log;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Level;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.logging.SystemStreamLog;
+
+/**
+ * A utility class for setting up logging for maven plugins so that any logging calls issued via Jakarta's Commons
+ * Logging or Log4j get routed through the maven logging system. This allows mojo's to invoke logic from other libraries
+ * and get the logging statements issued by those libraries issued to maven's console output
+ */
+public class MavenLogger extends AppenderSkeleton implements Log {
+
+	private static org.apache.maven.plugin.logging.Log systemStreamLog = new SystemStreamLog();
+	private static org.apache.maven.plugin.logging.Log mavenLog = systemStreamLog;
+
+	public MavenLogger() {
+		this(null);
+	}
+
+	/**
+	 * Jakarta Commons Logging requires a constructor that takes a string
+	 */
+	public MavenLogger(String name) {
+		super();
+	}
+
+	/**
+	 * Start logging for this plugin
+	 */
+	public static void startPluginLog(AbstractMojo mojo) {
+		mavenLog = mojo.getLog();
+	}
+
+	/**
+	 * End logging for this plugin
+	 */
+	public static void endPluginLog(AbstractMojo mojo) {
+		mavenLog = systemStreamLog;
+	}
+
+	// Log4j methods
+
+	@Override
+	protected void append(LoggingEvent event) {
+		Level level = event.getLevel();
+		if (Level.DEBUG.equals(level) && !(mavenLog.isDebugEnabled())) {
+			return;
+		}
+
+		String text = this.layout.format(event);
+		Throwable throwable = null;
+		if (event.getThrowableInformation() != null) {
+			throwable = event.getThrowableInformation().getThrowable();
+		}
+
+		if (Level.DEBUG.equals(level) || Level.TRACE.equals(level)) {
+			mavenLog.debug(text, throwable);
+		} else if (Level.INFO.equals(level)) {
+			mavenLog.info(text, throwable);
+		} else if (Level.WARN.equals(level)) {
+			mavenLog.warn(text, throwable);
+		} else if (Level.ERROR.equals(level) || Level.FATAL.equals(level)) {
+			mavenLog.error(text, throwable);
+		} else {
+			mavenLog.error(text, throwable);
+		}
+	}
+
+	@Override
+	public void close() {
+		mavenLog = null;
+	}
+
+	@Override
+	public boolean requiresLayout() {
+		return true;
+	}
+
+	// Jakarta Commons Logging methods
+	@Override
+	public boolean isDebugEnabled() {
+		return mavenLog.isDebugEnabled();
+	}
+
+	@Override
+	public boolean isErrorEnabled() {
+		return mavenLog.isErrorEnabled();
+	}
+
+	@Override
+	public boolean isFatalEnabled() {
+		return mavenLog.isErrorEnabled();
+	}
+
+	@Override
+	public boolean isInfoEnabled() {
+		return mavenLog.isInfoEnabled();
+	}
+
+	@Override
+	public boolean isTraceEnabled() {
+		return mavenLog.isDebugEnabled();
+	}
+
+	@Override
+	public boolean isWarnEnabled() {
+		return mavenLog.isWarnEnabled();
+	}
+
+	@Override
+	public void trace(Object message) {
+		mavenLog.debug(getString(message));
+	}
+
+	@Override
+	public void trace(Object message, Throwable t) {
+		mavenLog.debug(getString(message), t);
+
+	}
+
+	@Override
+	public void debug(Object message) {
+		mavenLog.debug(getString(message));
+	}
+
+	@Override
+	public void debug(Object message, Throwable t) {
+		mavenLog.debug(getString(message), t);
+	}
+
+	@Override
+	public void info(Object message) {
+		mavenLog.info(getString(message));
+	}
+
+	@Override
+	public void info(Object message, Throwable t) {
+		mavenLog.info(getString(message), t);
+	}
+
+	@Override
+	public void warn(Object message) {
+		mavenLog.warn(getString(message));
+	}
+
+	@Override
+	public void warn(Object message, Throwable t) {
+		mavenLog.warn(getString(message), t);
+	}
+
+	@Override
+	public void error(Object message) {
+		mavenLog.error(getString(message));
+	}
+
+	@Override
+	public void error(Object message, Throwable t) {
+		mavenLog.error(getString(message), t);
+	}
+
+	@Override
+	public void fatal(Object message) {
+		mavenLog.error(getString(message));
+	}
+
+	@Override
+	public void fatal(Object message, Throwable t) {
+		mavenLog.error(getString(message), t);
+	}
+
+	/**
+	 * Check for null then call toString on the object
+	 */
+	protected String getString(Object message) {
+		if (message == null) {
+			return null;
+		}
+		return message.toString();
+	}
+
+}
