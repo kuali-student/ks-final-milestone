be6147b5ec286558914f0555590f47207762fddb
jcaddel <jcaddel@kuali.org>
2010-08-25 11:38:33 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@15691 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/maven-impex-plugin/src/main/java/org/apache/torque/mojo/BaseMojo.java
+++ impex/maven-impex-plugin/src/main/java/org/apache/torque/mojo/BaseMojo.java
@@ -1,14 +1,19 @@
 package org.apache.torque.mojo;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Settings;
 
 /**
- * Some base logic for converting files
+ * Mojo essentials. Contains the "skip" logic that is the de facto standard for maven plugins. Contains a number of
+ * maven related properties that are common to most mojos
  */
-public abstract class MorpherMojo extends AbstractMojo {
+public abstract class BaseMojo extends AbstractMojo {
 	public static final String FS = System.getProperty("file.separator");
+	public static final String SKIP_PACKAGING_TYPE = "pom";
 
 	/**
 	 * When <code>true</code>, skip the execution.
@@ -41,15 +46,30 @@ public abstract class MorpherMojo extends AbstractMojo {
 	 */
 	private MavenProject project;
 
+	/**
+	 * @parameter expression="${settings}"
+	 * @required
+	 * @since 1.0
+	 * @readonly
+	 */
+	private Settings settings;
+
+	/**
+	 * @parameter default-value="${session}"
+	 * @required
+	 * @readonly
+	 */
+	private MavenSession mavenSession;
+
 	@Override
-	public void execute() throws MojoExecutionException {
+	public void execute() throws MojoExecutionException, MojoFailureException {
 		if (skipMojo()) {
 			return;
 		}
-		executeMorph();
+		executeMojo();
 	}
 
-	protected abstract void executeMorph() throws MojoExecutionException;
+	protected abstract void executeMojo() throws MojoExecutionException, MojoFailureException;
 
 	/**
 	 * <p>
@@ -66,12 +86,12 @@ public abstract class MorpherMojo extends AbstractMojo {
 	 */
 	protected boolean skipMojo() {
 		if (skip) {
-			getLog().info("Skip morpher execution");
+			getLog().info("Skipping execution");
 			return true;
 		}
 
-		if (!forceMojoExecution && project != null && "pom".equals(project.getPackaging())) {
-			getLog().info("Skipping execution for project with packaging type 'pom'");
+		if (!forceMojoExecution && project != null && SKIP_PACKAGING_TYPE.equals(project.getPackaging())) {
+			getLog().info("Skipping execution for project with packaging type '" + SKIP_PACKAGING_TYPE + "'");
 			return true;
 		}
 
@@ -95,4 +115,39 @@ public abstract class MorpherMojo extends AbstractMojo {
 		this.encoding = encoding;
 	}
 
+	public boolean isSkip() {
+		return skip;
+	}
+
+	public void setSkip(boolean skip) {
+		this.skip = skip;
+	}
+
+	public boolean isForceMojoExecution() {
+		return forceMojoExecution;
+	}
+
+	public void setForceMojoExecution(boolean forceMojoExecution) {
+		this.forceMojoExecution = forceMojoExecution;
+	}
+
+	public Settings getSettings() {
+		return settings;
+	}
+
+	public void setSettings(Settings settings) {
+		this.settings = settings;
+	}
+
+	public MavenSession getMavenSession() {
+		return mavenSession;
+	}
+
+	public void setMavenSession(MavenSession mavenSession) {
+		this.mavenSession = mavenSession;
+	}
+
+	public void setProject(MavenProject project) {
+		this.project = project;
+	}
 }
