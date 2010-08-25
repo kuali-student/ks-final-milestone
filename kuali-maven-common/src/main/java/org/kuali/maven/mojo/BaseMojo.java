249f9bb5fa683bbafff65ca68fb4383fd5108e37
jcaddel <jcaddel@kuali.org>
2010-08-25 17:20:27 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@15749 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- kuali-maven-common/src/main/java/org/kuali/maven/mojo/BaseMojo.java
+++ kuali-maven-common/src/main/java/org/kuali/maven/mojo/BaseMojo.java
@@ -1,11 +1,9 @@
 package org.kuali.maven.mojo;
 
-import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.settings.Settings;
 import org.kuali.maven.mojo.MavenLogger;
 
 /**
@@ -17,51 +15,11 @@ public abstract class BaseMojo extends AbstractMojo {
 	public static final String FS = System.getProperty("file.separator");
 	public static final String SKIP_PACKAGING_TYPE = "pom";
 
-	/**
-	 * When <code>true</code>, skip the execution.
-	 * 
-	 * @since 1.0
-	 * @parameter default-value="false"
-	 */
-	private boolean skip;
+	public abstract boolean isSkip();
 
-	/**
-	 * Setting this parameter to <code>true</code> will force the execution of this mojo, even if it would get skipped
-	 * usually.
-	 * 
-	 * @parameter expression="${forceMorpherExecution}" default-value=false
-	 * @required
-	 */
-	private boolean forceMojoExecution;
+	public abstract boolean isForceMojoExecution();
 
-	/**
-	 * @parameter expression="${encoding}" default-value="${project.build.sourceEncoding}"
-	 */
-	private String encoding;
-
-	/**
-	 * The Maven project this plugin runs in.
-	 * 
-	 * @parameter expression="${project}"
-	 * @required
-	 * @readonly
-	 */
-	private MavenProject project;
-
-	/**
-	 * @parameter expression="${settings}"
-	 * @required
-	 * @since 1.0
-	 * @readonly
-	 */
-	private Settings settings;
-
-	/**
-	 * @parameter default-value="${session}"
-	 * @required
-	 * @readonly
-	 */
-	private MavenSession mavenSession;
+	public abstract MavenProject getProject();
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
@@ -89,69 +47,16 @@ public abstract class BaseMojo extends AbstractMojo {
 	 * @return <code>true</code> if the mojo execution should be skipped.
 	 */
 	protected boolean skipMojo() {
-		if (skip) {
+		if (isSkip()) {
 			getLog().info("Skipping execution");
 			return true;
 		}
 
-		if (!forceMojoExecution && project != null && SKIP_PACKAGING_TYPE.equals(project.getPackaging())) {
+		if (!isForceMojoExecution() && getProject() != null && SKIP_PACKAGING_TYPE.equals(getProject().getPackaging())) {
 			getLog().info("Skipping execution for project with packaging type '" + SKIP_PACKAGING_TYPE + "'");
 			return true;
 		}
 
 		return false;
-	}
-
-	/**
-	 * Returns the maven project.
-	 * 
-	 * @return The maven project where this plugin runs in.
-	 */
-	public MavenProject getProject() {
-		return project;
-	}
-
-	public String getEncoding() {
-		return encoding;
-	}
-
-	public void setEncoding(String encoding) {
-		this.encoding = encoding;
-	}
-
-	public boolean isSkip() {
-		return skip;
-	}
-
-	public void setSkip(boolean skip) {
-		this.skip = skip;
-	}
-
-	public boolean isForceMojoExecution() {
-		return forceMojoExecution;
-	}
-
-	public void setForceMojoExecution(boolean forceMojoExecution) {
-		this.forceMojoExecution = forceMojoExecution;
-	}
-
-	public Settings getSettings() {
-		return settings;
-	}
-
-	public void setSettings(Settings settings) {
-		this.settings = settings;
-	}
-
-	public MavenSession getMavenSession() {
-		return mavenSession;
-	}
-
-	public void setMavenSession(MavenSession mavenSession) {
-		this.mavenSession = mavenSession;
-	}
-
-	public void setProject(MavenProject project) {
-		this.project = project;
 	}
 }
