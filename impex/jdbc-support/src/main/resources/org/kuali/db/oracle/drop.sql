b202c3d307a1769a5509644bbbcc9aab9eeb3d0f
jcaddel <jcaddel@kuali.org>
2010-08-27 17:56:28 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@15814 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/jdbc-support/src/main/resources/org/kuali/db/oracle/drop.sql
+++ impex/jdbc-support/src/main/resources/org/kuali/db/oracle/drop.sql
@@ -1,6 +1,6 @@
 -- Drop the user and ignore the Oracle 'user does not exist' error
 -- Dropping the user removes all objects associated with this schema (tables, views, indexes, sequences, etc)
--- This is roughly the equivalent to MySQL's: DROP DATABASE ksembedded IF EXISTS
+-- This is roughly the equivalent to MySQL's: DROP DATABASE [databaseName] IF EXISTS
 BEGIN 
   EXECUTE IMMEDIATE 'DROP USER ${database} CASCADE'; 
 EXCEPTION WHEN OTHERS THEN 
