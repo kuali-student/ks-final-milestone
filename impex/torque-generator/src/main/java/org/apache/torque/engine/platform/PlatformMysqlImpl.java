d4b51e05b2a47cd35df64086b41a6c169ed3d701
jcaddel <jcaddel@kuali.org>
2010-09-02 16:34:23 -0400

    Just formatting
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@15953 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/torque-generator/src/main/java/org/apache/torque/engine/platform/PlatformMysqlImpl.java
+++ impex/torque-generator/src/main/java/org/apache/torque/engine/platform/PlatformMysqlImpl.java
@@ -29,107 +29,100 @@ import org.apache.torque.engine.database.model.SchemaType;
 
 /**
  * MySql Platform implementation.
- *
+ * 
  * @author <a href="mailto:mpoeschl@marmot.at">Martin Poeschl</a>
  * @version $Id: PlatformMysqlImpl.java,v 1.1.6.1 2008-04-18 17:04:37 jkeller Exp $
  */
-public class PlatformMysqlImpl extends PlatformDefaultImpl
-{
-    /**
-     * Default constructor.
-     */
-    public PlatformMysqlImpl()
-    {
-        super();
-        initialize();
-    }
+public class PlatformMysqlImpl extends PlatformDefaultImpl {
+	/**
+	 * Default constructor.
+	 */
+	public PlatformMysqlImpl() {
+		super();
+		initialize();
+	}
 
-    /**
-     * Initializes db specific domain mapping.
-     */
-    private void initialize()
-    {
-        setSchemaDomainMapping(new Domain(SchemaType.NUMERIC, "DECIMAL"));
-        setSchemaDomainMapping(new Domain(SchemaType.LONGVARCHAR, "MEDIUMTEXT"));
-        setSchemaDomainMapping(new Domain(SchemaType.DATE, "DATETIME"));
-        setSchemaDomainMapping(new Domain(SchemaType.BINARY, "BLOB"));
-        setSchemaDomainMapping(new Domain(SchemaType.VARBINARY, "MEDIUMBLOB"));
-        setSchemaDomainMapping(new Domain(SchemaType.LONGVARBINARY, "LONGBLOB"));
-        setSchemaDomainMapping(new Domain(SchemaType.BLOB, "LONGBLOB"));
-        setSchemaDomainMapping(new Domain(SchemaType.CLOB, "LONGTEXT"));
-    }
+	/**
+	 * Initializes db specific domain mapping.
+	 */
+	private void initialize() {
+		setSchemaDomainMapping(new Domain(SchemaType.NUMERIC, "DECIMAL"));
+		setSchemaDomainMapping(new Domain(SchemaType.LONGVARCHAR, "MEDIUMTEXT"));
+		setSchemaDomainMapping(new Domain(SchemaType.DATE, "DATETIME"));
+		setSchemaDomainMapping(new Domain(SchemaType.BINARY, "BLOB"));
+		setSchemaDomainMapping(new Domain(SchemaType.VARBINARY, "MEDIUMBLOB"));
+		setSchemaDomainMapping(new Domain(SchemaType.LONGVARBINARY, "LONGBLOB"));
+		setSchemaDomainMapping(new Domain(SchemaType.BLOB, "LONGBLOB"));
+		setSchemaDomainMapping(new Domain(SchemaType.CLOB, "LONGTEXT"));
+	}
 
-    /**
-     * @see Platform#getAutoIncrement()
-     */
-    public String getAutoIncrement()
-    {
-        return "AUTO_INCREMENT";
-    }
+	/**
+	 * @see Platform#getAutoIncrement()
+	 */
+	public String getAutoIncrement() {
+		return "AUTO_INCREMENT";
+	}
 
-    /**
-     * @see Platform#hasSize(String)
-     */
-    public boolean hasSize(String sqlType)
-    {
-        return !("MEDIUMTEXT".equals(sqlType) || "LONGTEXT".equals(sqlType)
-                || "BLOB".equals(sqlType) || "MEDIUMBLOB".equals(sqlType)
-                || "LONGBLOB".equals(sqlType));
-    }
+	/**
+	 * @see Platform#hasSize(String)
+	 */
+	public boolean hasSize(String sqlType) {
+		return !("MEDIUMTEXT".equals(sqlType) || "LONGTEXT".equals(sqlType) || "BLOB".equals(sqlType) || "MEDIUMBLOB".equals(sqlType) || "LONGBLOB".equals(sqlType));
+	}
 
 	@Override
 	public String filterInvalidDefaultValues(String defaultValue) {
-		if ( defaultValue != null ) {
-			defaultValue = defaultValue.replace( "SYS_GUID()", "" );
-			defaultValue = defaultValue.replace( "SYSDATE", "" );
-			defaultValue = defaultValue.replace( "USERENV(\'SESSIONID\')", "" );
+		if (defaultValue != null) {
+			defaultValue = defaultValue.replace("SYS_GUID()", "");
+			defaultValue = defaultValue.replace("SYSDATE", "");
+			defaultValue = defaultValue.replace("USERENV(\'SESSIONID\')", "");
 			defaultValue = defaultValue.trim();
 		}
-		
+
 		return defaultValue;
 	}
 
 	@Override
 	public Long getSequenceNextVal(Connection con, String schema, String sequenceName) {
 		try {
-			PreparedStatement ps = con.prepareStatement( "SELECT auto_increment FROM information_schema.tables WHERE table_schema = ? AND table_name = ?" );
+			PreparedStatement ps = con.prepareStatement("SELECT auto_increment FROM information_schema.tables WHERE table_schema = ? AND table_name = ?");
 			Long nextVal = 0L;
-			ps.setString( 1, schema );
-			ps.setString( 2, sequenceName );
+			ps.setString(1, schema);
+			ps.setString(2, sequenceName);
 			ResultSet rs = ps.executeQuery();
-			if ( rs.next() ) {
-				nextVal = rs.getLong( 1 );
+			if (rs.next()) {
+				nextVal = rs.getLong(1);
 			}
 			rs.close();
 			ps.close();
-			System.out.println( "Next Val for " + schema + "." + sequenceName + "=" + nextVal);
+			System.out.println("Next Val for " + schema + "." + sequenceName + "=" + nextVal);
 			return nextVal;
-		} catch ( SQLException ex ) {
-			System.err.println( "Unable to extract sequence definition: " + schema + "." + sequenceName );
+		} catch (SQLException ex) {
+			System.err.println("Unable to extract sequence definition: " + schema + "." + sequenceName);
 			ex.printStackTrace();
 			return 0L;
 		}
 	}
 
 	@Override
-	public String getViewDefinition( Connection con, String schema, String viewName) {
+	public String getViewDefinition(Connection con, String schema, String viewName) {
 		try {
-			PreparedStatement ps = con.prepareStatement( "SELECT view_definition FROM information_schema.views WHERE table_schema = ? AND table_name = ?" );
+			PreparedStatement ps = con.prepareStatement("SELECT view_definition FROM information_schema.views WHERE table_schema = ? AND table_name = ?");
 			String definition = "";
-			ps.setString( 1, schema );
-			ps.setString( 2, viewName );
+			ps.setString(1, schema);
+			ps.setString(2, viewName);
 			ResultSet rs = ps.executeQuery();
-			if ( rs.next() ) {
-				definition = rs.getString( 1 );
+			if (rs.next()) {
+				definition = rs.getString(1);
 			}
 			rs.close();
 			ps.close();
 			return definition;
-		} catch ( SQLException ex ) {
-			System.err.println( "Unable to extract view definition: " + schema + "." + viewName );
+		} catch (SQLException ex) {
+			System.err.println("Unable to extract view definition: " + schema + "." + viewName);
 			ex.printStackTrace();
 			return "";
 		}
 	}
-	
+
 }
