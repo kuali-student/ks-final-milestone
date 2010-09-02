3a3c4db6c337129c981c3596a841740f2e2aee13
jcaddel <jcaddel@kuali.org>
2010-09-02 16:35:16 -0400

    Just formatting
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@15954 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/torque-generator/src/main/java/org/apache/torque/engine/platform/PlatformOracleImpl.java
+++ impex/torque-generator/src/main/java/org/apache/torque/engine/platform/PlatformOracleImpl.java
@@ -31,83 +31,75 @@ import org.apache.torque.engine.database.model.SchemaType;
 
 /**
  * Oracle Platform implementation.
- *
+ * 
  * @author <a href="mailto:mpoeschl@marmot.at">Martin Poeschl</a>
  * @version $Id: PlatformOracleImpl.java,v 1.1.6.1 2008-04-18 17:04:37 jkeller Exp $
  */
-public class PlatformOracleImpl extends PlatformDefaultImpl
-{
-    /**
-     * Default constructor.
-     */
-    public PlatformOracleImpl()
-    {
-        super();
-        initialize();
-    }
+public class PlatformOracleImpl extends PlatformDefaultImpl {
+	/**
+	 * Default constructor.
+	 */
+	public PlatformOracleImpl() {
+		super();
+		initialize();
+	}
 
-    /**
-     * Initializes db specific domain mapping.
-     */
-    private void initialize()
-    {
-        setSchemaDomainMapping(new Domain(SchemaType.TINYINT, "NUMBER", "3", "0"));
-        setSchemaDomainMapping(new Domain(SchemaType.SMALLINT, "NUMBER", "5", "0"));
-        setSchemaDomainMapping(new Domain(SchemaType.INTEGER, "NUMBER", "10", "0"));
-        setSchemaDomainMapping(new Domain(SchemaType.BOOLEANINT, "NUMBER", "1", "0"));
-        setSchemaDomainMapping(new Domain(SchemaType.BIGINT, "NUMBER", "20", "0"));
-        setSchemaDomainMapping(new Domain(SchemaType.REAL, "NUMBER"));
-        setSchemaDomainMapping(new Domain(SchemaType.DOUBLE, "FLOAT"));
-        setSchemaDomainMapping(new Domain(SchemaType.DECIMAL, "NUMBER"));
-        setSchemaDomainMapping(new Domain(SchemaType.NUMERIC, "NUMBER"));
-        setSchemaDomainMapping(new Domain(SchemaType.VARCHAR, "VARCHAR2"));
-        setSchemaDomainMapping(new Domain(SchemaType.LONGVARCHAR, "VARCHAR2", "2000"));
-        setSchemaDomainMapping(new Domain(SchemaType.TIME, "DATE"));
-        setSchemaDomainMapping(new Domain(SchemaType.TIMESTAMP, "TIMESTAMP"));
-        setSchemaDomainMapping(new Domain(SchemaType.BINARY, "LONG RAW"));
-        setSchemaDomainMapping(new Domain(SchemaType.VARBINARY, "BLOB"));
-        setSchemaDomainMapping(new Domain(SchemaType.LONGVARBINARY, "LONG RAW"));
-    }
+	/**
+	 * Initializes db specific domain mapping.
+	 */
+	private void initialize() {
+		setSchemaDomainMapping(new Domain(SchemaType.TINYINT, "NUMBER", "3", "0"));
+		setSchemaDomainMapping(new Domain(SchemaType.SMALLINT, "NUMBER", "5", "0"));
+		setSchemaDomainMapping(new Domain(SchemaType.INTEGER, "NUMBER", "10", "0"));
+		setSchemaDomainMapping(new Domain(SchemaType.BOOLEANINT, "NUMBER", "1", "0"));
+		setSchemaDomainMapping(new Domain(SchemaType.BIGINT, "NUMBER", "20", "0"));
+		setSchemaDomainMapping(new Domain(SchemaType.REAL, "NUMBER"));
+		setSchemaDomainMapping(new Domain(SchemaType.DOUBLE, "FLOAT"));
+		setSchemaDomainMapping(new Domain(SchemaType.DECIMAL, "NUMBER"));
+		setSchemaDomainMapping(new Domain(SchemaType.NUMERIC, "NUMBER"));
+		setSchemaDomainMapping(new Domain(SchemaType.VARCHAR, "VARCHAR2"));
+		setSchemaDomainMapping(new Domain(SchemaType.LONGVARCHAR, "VARCHAR2", "2000"));
+		setSchemaDomainMapping(new Domain(SchemaType.TIME, "DATE"));
+		setSchemaDomainMapping(new Domain(SchemaType.TIMESTAMP, "TIMESTAMP"));
+		setSchemaDomainMapping(new Domain(SchemaType.BINARY, "LONG RAW"));
+		setSchemaDomainMapping(new Domain(SchemaType.VARBINARY, "BLOB"));
+		setSchemaDomainMapping(new Domain(SchemaType.LONGVARBINARY, "LONG RAW"));
+	}
 
-    /**
-     * @see Platform#getMaxColumnNameLength()
-     */
-    public int getMaxColumnNameLength()
-    {
-        return 30;
-    }
+	/**
+	 * @see Platform#getMaxColumnNameLength()
+	 */
+	public int getMaxColumnNameLength() {
+		return 30;
+	}
 
-    /**
-     * @see Platform#getNativeIdMethod()
-     */
-    public String getNativeIdMethod()
-    {
-        return Platform.SEQUENCE;
-    }
+	/**
+	 * @see Platform#getNativeIdMethod()
+	 */
+	public String getNativeIdMethod() {
+		return Platform.SEQUENCE;
+	}
 
-    /**
-     * @see Platform#getAutoIncrement()
-     */
-    public String getAutoIncrement()
-    {
-        return "";
-    }
+	/**
+	 * @see Platform#getAutoIncrement()
+	 */
+	public String getAutoIncrement() {
+		return "";
+	}
 
-    @Override
-    public List<String> getPrimaryKeys(DatabaseMetaData dbMeta, String dbSchema, String tableName) throws SQLException {
-    	return super.getPrimaryKeys( dbMeta, dbSchema.toUpperCase(), tableName );
-    }
-    
-    public List<String> getTableNames(DatabaseMetaData dbMeta, String databaseSchema) throws SQLException {
-    	return super.getTableNames( dbMeta, databaseSchema.toUpperCase() );
-    }
-    
 	@Override
-	public boolean isSpecialDefault( String defaultValue ) {
+	public List<String> getPrimaryKeys(DatabaseMetaData dbMeta, String dbSchema, String tableName) throws SQLException {
+		return super.getPrimaryKeys(dbMeta, dbSchema.toUpperCase(), tableName);
+	}
+
+	public List<String> getTableNames(DatabaseMetaData dbMeta, String databaseSchema) throws SQLException {
+		return super.getTableNames(dbMeta, databaseSchema.toUpperCase());
+	}
+
+	@Override
+	public boolean isSpecialDefault(String defaultValue) {
 		defaultValue = defaultValue.toUpperCase();
-		if ( defaultValue.contains( "SYS_GUID()" )
-				|| defaultValue.contains( "SYSDATE" )
-				|| defaultValue.contains( "USERENV(\'SESSIONID\')" ) ) {
+		if (defaultValue.contains("SYS_GUID()") || defaultValue.contains("SYSDATE") || defaultValue.contains("USERENV(\'SESSIONID\')")) {
 			return true;
 		}
 		return false;
@@ -116,44 +108,43 @@ public class PlatformOracleImpl extends PlatformDefaultImpl
 	@Override
 	public Long getSequenceNextVal(Connection con, String schema, String sequenceName) {
 		try {
-			PreparedStatement ps = con.prepareStatement( "SELECT last_number FROM all_sequences WHERE sequence_owner = ? AND sequence_name = ?" );
+			PreparedStatement ps = con.prepareStatement("SELECT last_number FROM all_sequences WHERE sequence_owner = ? AND sequence_name = ?");
 			Long nextVal = 0L;
-			ps.setString( 1, schema.toUpperCase() );
-			ps.setString( 2, sequenceName.toUpperCase() );
+			ps.setString(1, schema.toUpperCase());
+			ps.setString(2, sequenceName.toUpperCase());
 			ResultSet rs = ps.executeQuery();
-			if ( rs.next() ) {
-				nextVal = rs.getLong( 1 );
+			if (rs.next()) {
+				nextVal = rs.getLong(1);
 			}
 			rs.close();
 			ps.close();
 			return nextVal;
-		} catch ( SQLException ex ) {
-			System.err.println( "Unable to extract sequence definition: " + schema + "." + sequenceName );
+		} catch (SQLException ex) {
+			System.err.println("Unable to extract sequence definition: " + schema + "." + sequenceName);
 			ex.printStackTrace();
 			return 0L;
 		}
 	}
 
 	@Override
-	public String getViewDefinition( Connection con, String schema, String viewName) {
+	public String getViewDefinition(Connection con, String schema, String viewName) {
 		try {
-			PreparedStatement ps = con.prepareStatement( "SELECT text FROM all_views WHERE owner = ? AND view_name = ?" );
+			PreparedStatement ps = con.prepareStatement("SELECT text FROM all_views WHERE owner = ? AND view_name = ?");
 			String definition = "";
-			ps.setString( 1, schema.toUpperCase() );
-			ps.setString( 2, viewName.toUpperCase() );
+			ps.setString(1, schema.toUpperCase());
+			ps.setString(2, viewName.toUpperCase());
 			ResultSet rs = ps.executeQuery();
-			if ( rs.next() ) {
-				definition = rs.getString( 1 );
+			if (rs.next()) {
+				definition = rs.getString(1);
 			}
 			rs.close();
 			ps.close();
 			return definition;
-		} catch ( SQLException ex ) {
-			System.err.println( "Unable to extract view definition: " + schema + "." + viewName );
+		} catch (SQLException ex) {
+			System.err.println("Unable to extract view definition: " + schema + "." + viewName);
 			ex.printStackTrace();
 			return "";
 		}
 	}
-
 
 }
