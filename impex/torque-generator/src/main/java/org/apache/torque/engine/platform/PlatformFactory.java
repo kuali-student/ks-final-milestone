14f0a24e6012f93b9c017cdcd790cb4bacdd4edb
jcaddel <jcaddel@kuali.org>
2010-09-03 15:47:39 -0400

    Just formatting
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@16003 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/torque-generator/src/main/java/org/apache/torque/engine/platform/PlatformFactory.java
+++ impex/torque-generator/src/main/java/org/apache/torque/engine/platform/PlatformFactory.java
@@ -25,74 +25,64 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * Factory class responsible to create Platform objects that
- * define RDBMS platform specific behaviour.
- *
+ * Factory class responsible to create Platform objects that define RDBMS platform specific behaviour.
+ * 
  * @author Thomas Mahler
  * @author <a href="mailto:mpoeschl@marmot.at">Martin Poeschl</a>
  * @version $Id: PlatformFactory.java,v 1.1 2007-10-21 07:57:27 abyrne Exp $
  */
-public class PlatformFactory
-{
-    private static HashMap platforms = new HashMap();
-    private static Log log = LogFactory.getLog(PlatformFactory.class);
+public class PlatformFactory {
+	private static HashMap platforms = new HashMap();
+	private static Log log = LogFactory.getLog(PlatformFactory.class);
 
-    /**
-     * Returns the Platform for a platform name.
-     *
-     * @param dbms name of the platform
-     */
-    public static Platform getPlatformFor(String dbms)
-    {
-        Platform result = null;
-        String platformName = null;
+	/**
+	 * Returns the Platform for a platform name.
+	 * 
+	 * @param dbms
+	 *            name of the platform
+	 */
+	public static Platform getPlatformFor(String dbms) {
+		Platform result = null;
+		String platformName = null;
 
-        result = (Platform) getPlatforms().get(dbms);
-        if (result == null)
-        {
-            try
-            {
-                platformName = getClassnameFor(dbms);
-                Class platformClass = Class.forName(platformName);
-                result = (Platform) platformClass.newInstance();
+		result = (Platform) getPlatforms().get(dbms);
+		if (result == null) {
+			try {
+				platformName = getClassnameFor(dbms);
+				Class platformClass = Class.forName(platformName);
+				result = (Platform) platformClass.newInstance();
 
-            }
-            catch (Throwable t)
-            {
-                log.warn("problems with platform " + platformName
-                        + ": " + t.getMessage());
-                log.warn("Torque will use PlatformDefaultImpl instead");
+			} catch (Throwable t) {
+				log.warn("problems with platform " + platformName + ": " + t.getMessage());
+				log.warn("Torque will use PlatformDefaultImpl instead");
 
-                result = new PlatformDefaultImpl();
-            }
-            getPlatforms().put(dbms, result); // cache the Platform
-        }
-        return result;
-    }
+				result = new PlatformDefaultImpl();
+			}
+			getPlatforms().put(dbms, result); // cache the Platform
+		}
+		return result;
+	}
 
-    /**
-     * compute the name of the concrete Class representing the Platform
-     * specified by <code>platform</code>
-     *
-     * @param platform the name of the platform as specified in the repository
-     */
-    private static String getClassnameFor(String platform)
-    {
-        String pf = "Default";
-        if (platform != null)
-        {
-            pf = platform;
-        }
-        return "org.apache.torque.engine.platform.Platform" + pf.substring(0, 1).toUpperCase() + pf.substring(1) + "Impl";
-    }
+	/**
+	 * compute the name of the concrete Class representing the Platform specified by <code>platform</code>
+	 * 
+	 * @param platform
+	 *            the name of the platform as specified in the repository
+	 */
+	private static String getClassnameFor(String platform) {
+		String pf = "Default";
+		if (platform != null) {
+			pf = platform;
+		}
+		return "org.apache.torque.engine.platform.Platform" + pf.substring(0, 1).toUpperCase() + pf.substring(1) + "Impl";
+	}
 
-    /**
-     * Gets the platforms.
-     *
-     * @return Returns a HashMap
-     */
-    private static HashMap getPlatforms()
-    {
-        return platforms;
-    }
+	/**
+	 * Gets the platforms.
+	 * 
+	 * @return Returns a HashMap
+	 */
+	private static HashMap getPlatforms() {
+		return platforms;
+	}
 }
