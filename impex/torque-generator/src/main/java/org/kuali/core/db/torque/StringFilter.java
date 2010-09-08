204685203fc4378c36d9eb1fc43a3104e01f7e44
jcaddel <jcaddel@kuali.org>
2010-09-08 17:12:05 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@16077 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/torque-generator/src/main/java/org/kuali/core/db/torque/StringFilter.java
+++ impex/torque-generator/src/main/java/org/kuali/core/db/torque/StringFilter.java
@@ -10,9 +10,22 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+/**
+ * This class provides logic for filtering strings based on regular expressions. Given a list of includePatterns and
+ * excludePatterns the filter method will remove any strings from an Iterator<String> if there is not a match on an
+ * inclusion pattern or there is a match on an exclusion pattern,
+ */
 public class StringFilter {
 	private static final Log log = LogFactory.getLog(StringFilter.class);
+
+	/**
+	 * List of regular expressions. A string must match one of these to be included
+	 */
 	List<String> includePatterns;
+
+	/**
+	 * List of regular expressions. If there is a match on any of these the string is filtered out
+	 */
 	List<String> excludePatterns;
 	List<Pattern> includes;
 	List<Pattern> excludes;
@@ -27,6 +40,9 @@ public class StringFilter {
 		this.excludePatterns = excludePatterns;
 	}
 
+	/**
+	 * Compile the string patterns into Pattern objects
+	 */
 	protected void compilePatterns() {
 		includes = getPatterns(includePatterns);
 		excludes = getPatterns(excludePatterns);
@@ -48,7 +64,11 @@ public class StringFilter {
 		this.excludePatterns = excludePatterns;
 	}
 
+	/**
+	 * Convert a List<String> into List<Pattern>
+	 */
 	protected List<Pattern> getPatterns(List<String> patterns) {
+		// If List<String> is empty return an empty List<Pattern>
 		if (isEmpty(patterns)) {
 			return new ArrayList<Pattern>();
 		}
@@ -60,43 +80,69 @@ public class StringFilter {
 		return regexPatterns;
 	}
 
+	/**
+	 * Return true if the string matches any of the patterns
+	 */
 	protected boolean isMatch(String s, List<Pattern> patterns) {
+		// If patterns is null or zero size, there is nothing to do
 		if (isEmpty(patterns)) {
 			return false;
 		}
+		// Loop through the patterns looking for a match
 		for (Pattern pattern : patterns) {
 			Matcher matcher = pattern.matcher(s);
+			// We found a match, return true
 			if (matcher.matches()) {
 				return true;
 			}
 		}
+		// We cycled through all of the patterns without finding a match
 		return false;
 	}
 
+	/**
+	 * Return true if no inclusion patterns have been specified.<br>
+	 * Return true if there have been inclusion patterns specified and there is a match on at least one of those
+	 * patterns.<br>
+	 * Return false otherwise.<br>
+	 */
 	protected boolean isInclude(String s) {
 		return isEmpty(includePatterns) || isMatch(s, includes);
 	}
 
+	/**
+	 * Return true if the string matches one or more of the exclude patterns
+	 */
 	protected boolean isExclude(String s) {
 		return isMatch(s, excludes);
 	}
 
+	/**
+	 * Return true if the List is null or has zero elements
+	 */
 	protected boolean isEmpty(List<?> list) {
 		return list == null || list.size() == 0;
 	}
 
+	/**
+	 * Invoke Iterator.remove() on any strings that do not match an inclusion pattern or do match an exclusion pattern
+	 */
 	public void filter(Iterator<String> itr) {
+		// Compile our Strings into Patterns
 		compilePatterns();
 		while (itr.hasNext()) {
 			String s = itr.next();
 			if (!isInclude(s)) {
-				log.debug("No inclusion pattern match. Skipping '" + s + "'");
+				// This string does not match inclusion criteria
+				log.debug("No inclusion pattern match. Removing '" + s + "'");
 				itr.remove();
 				continue;
 			}
 			if (isExclude(s)) {
-				log.debug("Exclusion pattern matched.  Skipping '" + s + "'");
+				// This string matches an exclusion pattern
+				log.debug("Exclusion pattern matched.  Removing '" + s + "'");
 				itr.remove();
+				continue;
 			}
 		}
 	}
