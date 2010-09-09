ef743769e216a0795c9532d56ccda75151625fc6
lpan <lpan@kuali.org>
2010-09-09 19:45:17 -0400

    add link back to parent md.
    git-svn-id: https://svn.kuali.org/repos/student/trunk@16098 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/view/variation/VariationViewController.java
+++ ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/view/variation/VariationViewController.java
@@ -1,20 +1,45 @@
 package org.kuali.student.lum.program.client.view.variation;
 
 import org.kuali.student.common.ui.client.mvc.DataModel;
+import org.kuali.student.lum.program.client.ProgramConstants;
 import org.kuali.student.lum.program.client.ProgramController;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.dom.client.Style;
 
 public class VariationViewController extends ProgramController{
 
+	public static final String parentLinkUrlBase = GWT.getModuleBaseURL() + "/HOME/CURRICULUM_HOME/PROGRAM_VIEW";
+	
 	public VariationViewController(DataModel programModel) {
 		super(programModel);
-        configurer = GWT.create(VariationViewConfigurer.class);
+        configurer = GWT.create(VariationViewConfigurer.class);   
 	}
 
     @Override
     protected void configureView() {
         super.configureView();
+        
+        String mdId = "";
+        String mdTitle = "";
+
+        //TODO: Hard-coded values of mdId & mdTitle for testing; will be removed
+        mdId = "d4ea77dd-b492-4554-b104-863e42c5f8b7";
+        mdTitle = "Undergrad Astronomy";
+        if(this.programModel.getRoot()!= null){
+        	mdId = this.programModel.get("/id");
+        	mdTitle = this.programModel.get("/" + ProgramConstants.LONG_TITLE);
+        }
+        
+        this.setContentTitle("Specialization of " + mdTitle);
+
+        HTML parentLink = new HTML("Parent Program");
+		parentLink.setHTML("<br><b>Parent Program: </b><a href=\"" + GWT.getModuleBaseURL()+"LUMMain.jsp#/HOME/CURRICULUM_HOME/PROGRAM_VIEW&docId=" + mdId + "\"><b>" + mdTitle + "</b></a>");
+        parentLink.getElement().getStyle().setPaddingRight(20d, Style.Unit.PX);
+        parentLink.getElement().getStyle().setFontSize(3, Style.Unit.PX);
+        this.addContentWidget(parentLink);
+        
         initialized = true;
     }
 }
