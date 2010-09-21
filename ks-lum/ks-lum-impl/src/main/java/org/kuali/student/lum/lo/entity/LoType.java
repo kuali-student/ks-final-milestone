e1caf23151903965081ddfbad839b7704f3164aa
depstein <depstein@kuali.org>
2010-09-21 12:18:24 -0400

    Fixed entity collection initialization
    git-svn-id: https://svn.kuali.org/repos/student/trunk@16310 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-impl/src/main/java/org/kuali/student/lum/lo/entity/LoType.java
+++ ks-lum/ks-lum-impl/src/main/java/org/kuali/student/lum/lo/entity/LoType.java
@@ -15,7 +15,6 @@
 
 package org.kuali.student.lum.lo.entity;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -143,9 +142,6 @@ public class LoType implements AttributeOwner<LoTypeAttribute> {
 	 */
 	@Override
 	public List<LoTypeAttribute> getAttributes() {
-		if (null == attributes) {
-			attributes = new ArrayList<LoTypeAttribute>(0);
-		}
 		return attributes;
 	}
 
