5ae20dba95cdbe32bda61fb5b5ec2ab09495bd39
depstein <depstein@kuali.org>
2010-09-23 14:09:15 -0400

    updated sql
    git-svn-id: https://svn.kuali.org/repos/student/trunk@16369 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-sql/src/main/resources/ks-core-comment-types.sql
+++ ks-core/ks-core-sql/src/main/resources/ks-core-comment-types.sql
@@ -1,7 +1,7 @@
-insert into KSEMBEDDED.KSCO_COMMENT_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('commentType.kuali.lu.type.CreditCourse.draft', 'Credit Course Proposal', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Proposal');
-insert into KSEMBEDDED.KSCO_COMMENT_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('commentType.kuali.lu.type.CreditCourse.submitted', 'Credit Course Submitted', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Submitted');
+insert into KSCO_COMMENT_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('commentType.kuali.lu.type.CreditCourse.draft', 'Credit Course Proposal', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Proposal');
+insert into KSCO_COMMENT_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('commentType.kuali.lu.type.CreditCourse.submitted', 'Credit Course Submitted', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Submitted');
 
-insert into KSEMBEDDED.KSCO_REFERENCE_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('referenceType.clu', 'Clu reference type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Clu');
-insert into KSEMBEDDED.KSCO_REFERENCE_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('referenceType.clu.proposal', 'Clu reference type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Clu Proposal');
+insert into KSCO_REFERENCE_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('referenceType.clu', 'Clu reference type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Clu');
+insert into KSCO_REFERENCE_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('referenceType.clu.proposal', 'Clu reference type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Clu Proposal');
 
-insert into KSEMBEDDED.KSCO_TAG_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('tagType.default', 'Default tag type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Default');
+insert into KSCO_TAG_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('tagType.default', 'Default tag type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Default');
--- ks-core/ks-core-sql/src/main/resources/ks-core-proposal-types.sql
+++ ks-core/ks-core-sql/src/main/resources/ks-core-proposal-types.sql
@@ -1,7 +1,7 @@
-insert into KSEMBEDDED.KSPR_PROPOSAL_DOCREL_TYPE (ID, DESCRIPTION, EFF_DT, EXPIR_DT, NAME) values ('kuali.proposal.docRelationType.attachment', 'Default proposal document relation type', {ts '2000-01-01 00:00:00.0'},null, 'kuali.proposal.docRelationType.attachment');
+insert into KSPR_PROPOSAL_DOCREL_TYPE (ID, DESCRIPTION, EFF_DT, EXPIR_DT, NAME) values ('kuali.proposal.docRelationType.attachment', 'Default proposal document relation type', {ts '2000-01-01 00:00:00.0'},null, 'kuali.proposal.docRelationType.attachment');
 
-insert into KSEMBEDDED.KSPR_PROPOSAL_REFTYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('kuali.proposal.referenceType.clu', 'Clu proposal reference type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Clu Proposal Reference');
+insert into KSPR_PROPOSAL_REFTYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('kuali.proposal.referenceType.clu', 'Clu proposal reference type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Clu Proposal Reference');
 
-insert into KSEMBEDDED.KSPR_PROPOSAL_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('kuali.proposal.type.course.create', 'A create course proposal type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Create Course Proposal');
-insert into KSEMBEDDED.KSPR_PROPOSAL_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('kuali.proposal.type.course.modify', 'A modify course proposal type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Modify Course Proposal');
-insert into KSEMBEDDED.KSPR_PROPOSAL_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('kuali.proposal.type.course.retire', 'A retire course proposal type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Retire Course Proposal');
+insert into KSPR_PROPOSAL_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('kuali.proposal.type.course.create', 'A create course proposal type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Create Course Proposal');
+insert into KSPR_PROPOSAL_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('kuali.proposal.type.course.modify', 'A modify course proposal type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Modify Course Proposal');
+insert into KSPR_PROPOSAL_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME) values ('kuali.proposal.type.course.retire', 'A retire course proposal type', {ts '2000-01-01 00:00:00.0'}, {ts '2000-12-31 00:00:00.0'},'Retire Course Proposal');
