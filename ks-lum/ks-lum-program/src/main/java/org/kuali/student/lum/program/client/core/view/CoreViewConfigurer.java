26e6b97fe89f24194aa12518cf10edc8944cae81
ikuplevich <ikuplevich@kuali.org>
2010-09-29 19:14:06 -0400

    Added view configurations for Core Program.
    git-svn-id: https://svn.kuali.org/repos/student/trunk@16482 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/core/view/CoreViewConfigurer.java
@@ -0,0 +1,23 @@
+package org.kuali.student.lum.program.client.core.view;
+
+import com.google.gwt.core.client.GWT;
+import org.kuali.student.lum.common.client.configuration.Configuration;
+import org.kuali.student.lum.common.client.configuration.ConfigurationManager;
+import org.kuali.student.lum.program.client.AbstractProgramConfigurer;
+import org.kuali.student.lum.program.client.view.SupportingDocsViewConfiguration;
+
+/**
+ * @author Igor
+ */
+public class CoreViewConfigurer extends AbstractProgramConfigurer {
+
+    public CoreViewConfigurer() {
+        programSectionConfigManager = new ConfigurationManager(this);
+        programSectionConfigManager.registerConfiguration(GWT.<Configuration>create(CoreInformationViewConfiguration.class));
+        programSectionConfigManager.registerConfiguration(GWT.<Configuration>create(CoreManagingBodiesViewConfiguration.class));
+        programSectionConfigManager.registerConfiguration(GWT.<Configuration>create(CoreCatalogInformationViewConfiguration.class));
+        programSectionConfigManager.registerConfiguration(GWT.<Configuration>create(CoreLearningObjectivesViewConfiguration.class));
+        programSectionConfigManager.registerConfiguration(GWT.<Configuration>create(SupportingDocsViewConfiguration.class));
+        programSectionConfigManager.registerConfiguration(GWT.<Configuration>create(CoreViewAllConfiguration.class));
+    }
+}
