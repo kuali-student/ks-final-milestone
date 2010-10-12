caeeb440de549125dbed423805999cfce90d0bbe
nwright <nwright@kuali.org>
2010-10-12 11:22:41 -0400

    added test case to determine if validator is mis-firing https://jira.kuali.org/browse/KSLUM-497
    git-svn-id: https://svn.kuali.org/repos/student/trunk@16679 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-impl/src/test/java/org/kuali/student/core/dictionary/service/TestStatementDictionary.java
+++ ks-core/ks-core-impl/src/test/java/org/kuali/student/core/dictionary/service/TestStatementDictionary.java
@@ -1,5 +1,6 @@
 package org.kuali.student.core.dictionary.service;
 
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -8,6 +9,7 @@ import org.kuali.student.common.validator.DefaultValidatorImpl;
 import org.kuali.student.common.validator.ServerDateParser;
 import org.kuali.student.core.dictionary.dto.ObjectStructureDefinition;
 import org.kuali.student.core.exceptions.OperationFailedException;
+import org.kuali.student.core.statement.dto.ReqCompFieldInfo;
 import org.kuali.student.core.statement.dto.ReqComponentInfo;
 import org.kuali.student.core.statement.dto.StatementInfo;
 import org.kuali.student.core.statement.dto.StatementTreeViewInfo;
@@ -48,5 +50,48 @@ public class TestStatementDictionary {
    System.out.println (vr.getElement () + " " + vr.getMessage ());
   }
 		assertEquals(1, validationResults.size());
+	}
+
+ @Test
+	public void testRequirementComponentInfoValidation() throws OperationFailedException {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:ks-statement-dictionary-context.xml");
+		System.out.println("h2. Validation Test");
+		DefaultValidatorImpl val = new DefaultValidatorImpl();
+		val.setDateParser(new ServerDateParser());
+		val.setSearchDispatcher(new MockSearchDispatcher());
+		ReqComponentInfo info = new ReqComponentInfo();
+		ObjectStructureDefinition os = (ObjectStructureDefinition) ac.getBean(info.getClass().getName());
+		List<ValidationResultInfo> validationResults = val.validateObject(info,	os);
+		System.out.println("h3. With just a blank ReqComponentInfo");
+		for (ValidationResultInfo vr : validationResults)
+  {
+   System.out.println (vr.getElement () + " " + vr.getMessage ());
+  }
+		assertEquals(0, validationResults.size());
+
+  ReqCompFieldInfo fieldInfo = new ReqCompFieldInfo ();
+  fieldInfo.setType ("kuali.reqComponent.field.type.gradeType.id");
+  fieldInfo.setValue ("kuali.resultComponent.grade.letter");
+  info.setReqCompFields (Arrays.asList (fieldInfo));
+  validationResults = val.validateObject(info,	os);
+		System.out.println("h3. With just a blank ReqComponentInfo");
+		for (ValidationResultInfo vr : validationResults)
+  {
+   System.out.println (vr.getElement () + " " + vr.getMessage ());
+  }
+		assertEquals(0, validationResults.size());
+
+  fieldInfo = new ReqCompFieldInfo ();
+  fieldInfo.setType ("kuali.reqComponent.field.type.gradeType.id");
+  fieldInfo.setValue ("bad with an embedded space in value");
+  info.setReqCompFields (Arrays.asList (fieldInfo));
+  validationResults = val.validateObject(info,	os);
+		System.out.println("h3. With just a blank ReqComponentInfo");
+		for (ValidationResultInfo vr : validationResults)
+  {
+   System.out.println (vr.getElement () + " " + vr.getMessage ());
+  }
+		assertEquals(1, validationResults.size());
+
 	}
 }
