14b39b815321c71a0d8b7e6ec381913347a0dcb0
hjohnson <hjohnson@kuali.org>
2010-10-25 19:42:35 -0400

    KSLUM-458: Switch the publication target db changes to use an update script
    git-svn-id: https://svn.kuali.org/repos/student/trunk@17088 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum/ks-lum-sql/src/main/resources/upgrades/R1.1.0-M10 to R1.1.0/2010-10-25-1-lum-bootstrap.sql
@@ -0,0 +1,5 @@
+update KSLU_CLU_PUBL set CLU_PUB_TYPE_ID='kuali.lu.publication.UndergradCatalog' where ID='MAJOR-101'
+/
+
+update KSLU_CLU_PUBL_TYPE delete where TYPE_KEY='kuali.lu.publication.Catalog'
+/
