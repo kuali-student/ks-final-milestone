b550fb16fd4bf86cdb80a2e7a2c7d9d1b1922ee3
hjohnson <hjohnson@kuali.org>
2010-10-25 20:12:38 -0400

    KSLUM-458: revert delete of publ type
    git-svn-id: https://svn.kuali.org/repos/student/trunk@17091 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-sql/src/main/resources/upgrades/R1.1.0-M10 to R1.1.0/2010-10-25-1-lum-bootstrap.sql
+++ ks-lum/ks-lum-sql/src/main/resources/upgrades/R1.1.0-M10 to R1.1.0/2010-10-25-1-lum-bootstrap.sql
@@ -1,5 +1,2 @@
 update KSLU_CLU_PUBL set CLU_PUB_TYPE_ID='kuali.lu.publication.UndergradCatalog' where ID='MAJOR-101'
 /
-
-update KSLU_CLU_PUBL_TYPE delete where TYPE_KEY='kuali.lu.publication.Catalog'
-/
