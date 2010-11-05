59acdfd823712c5fba851b5a8576a73432a36c5e
jim7 <jim7@kuali.org>
2010-11-05 17:22:00 -0400

    Call Baccalaureate program 'Baccalaureate'
    git-svn-id: https://svn.kuali.org/repos/student/trunk@17424 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum/ks-lum-sql/src/main/resources/upgrades/R1.1.0-M10 to R1.1.0/2010-11-05-1-lum-bootstrap.sql
@@ -0,0 +1,8 @@
+UPDATE KSLU_RICH_TEXT_T
+    SET PLAIN = 'Baccalaureate'
+    WHERE ID = '9623c41c-ae72-473a-a1ac-6874c433967d'
+/
+UPDATE KSLU_RICH_TEXT_T
+    SET FORMATTED = 'Baccalaureate'
+    WHERE ID = '9623c41c-ae72-473a-a1ac-6874c433967d'
+/
