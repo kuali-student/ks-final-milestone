b98a4adad11a5c7710bc943a786d2ea9a6b1b73b
kcampos <kcampos@kuali.org>
2010-11-12 13:59:39 -0500

    updates
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@17512 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- test/utilities/run_ks_loader.rb
+++ test/utilities/run_ks_loader.rb
@@ -1,8 +1,11 @@
+#!/usr/bin/env ruby
+
 # This sets up and run's the ks-loader
 # https://test.kuali.org/svn/student/tools/ks-loader
 
 environment = ARGV[0]
-if(environment !~ /dev|staging/)
+if(environment != 'dev' and
+   environment != 'staging')
   puts "Need to pass argument, either: dev or staging"
   exit 1;
 end
@@ -10,10 +13,23 @@ end
 url = (environment == 'staging' ? ENV['STAGING_URL'] : ENV['DEV_URL'])
 
 # Make sure we have the latest data
-`svn up /root/repos/student/tools/ks-loader`
+out=`svn up /root/repos/student/tools/ks-loader`
+puts out
 
 # Run ks loader
+puts "Cmd: java -jar ~/ks-loader.jar organization #{ENV['KS_LOADER_RESOURCES']}/Organizations for BSCI program.xls #{url}"
 out=`java -jar ~/ks-loader.jar organization "#{ENV['KS_LOADER_RESOURCES']}/Organizations for BSCI program.xls" #{url}`
-# error check
+puts "Org load:"
+puts out
+
+puts ""
+puts "Cmd: java -jar ~/ks-loader.jar course #{ENV['KS_LOADER_RESOURCES']}/Courses for BSCI program.xls #{url}"
 out=`java -jar ~/ks-loader.jar course "#{ENV['KS_LOADER_RESOURCES']}/Courses for BSCI program.xls" #{url}`
+puts "Course load:"
+puts out
+
+puts ""
+puts "Cmd: java -jar ~/ks-loader.jar atp #{ENV['KS_LOADER_RESOURCES']}/ATP Calendar.xls #{url}"
 out=`java -jar ~/ks-loader.jar atp "#{ENV['KS_LOADER_RESOURCES']}/ATP Calendar.xls" #{url}`
+puts "ATP load:"
+puts out
