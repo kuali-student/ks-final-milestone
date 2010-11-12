1460b211f71e7184d1317ba871c2d60180d4e81d
bsmith <bsmith@kuali.org>
2010-11-11 20:09:00 -0500

    Visual design and css tweaks
    git-svn-id: https://svn.kuali.org/repos/student/trunk@17501 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/views/CategoryManagementView.java
+++ ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/views/CategoryManagementView.java
@@ -1,5 +1,6 @@
 package org.kuali.student.lum.lu.ui.course.client.views;
 
+import org.kuali.student.common.ui.client.configurable.mvc.SectionTitle;
 import org.kuali.student.common.ui.client.mvc.Controller;
 import org.kuali.student.common.ui.client.mvc.ViewComposite;
 import org.kuali.student.common.ui.client.util.WindowTitleUtils;
@@ -12,15 +13,13 @@ import com.google.gwt.user.client.ui.FlowPanel;
 public class CategoryManagementView extends ViewComposite{
 
 	private FlowPanel layout = new FlowPanel();
-	private KSDocumentHeader header = new KSDocumentHeader();
 	
 	public CategoryManagementView(Controller controller, String name,
 			Enum<?> viewType) {
 		super(controller, name, viewType);
 		this.initWidget(layout);
-		layout.add(header);
-		header.addStyleName("Lum-DocumentHeader-Spacing");
-		header.setTitle(name);
+		layout.add(SectionTitle.generateH1Title(name));
+		
 		layout.add(new CategoryManagement(true,SelectionPolicy.MULTI_ROW));
 	}
 	
