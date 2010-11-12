89564f872b18745661a5e8c8e173478489874d54
jim7 <jim7@kuali.org>
2010-11-12 15:14:41 -0500

    KSLAB-1425 - sql upgrade scripts to add required fields, and    dictionary changes to remove requiredness for many fields, per discussion w/ Cathy & DanS today:      No longer required in dictionary for CORE      - catalogPublicationTargets      - divisionsStudentOversight      - learningObjectives      - unitsStudentOversight      - transcriptTitle      No longer required in dictionary for BACC (Credential)      - divisionsStudentOversight      - learningObjectives      - unitsStudentOversight      - transcriptTitle KSLAB-1418
    git-svn-id: https://svn.kuali.org/repos/student/trunk@17517 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum/ks-lum-sql/src/main/resources/upgrades/R1.1.0-M10 to R1.1.0/2010-11-12-1-lum-bootstrap.sql
@@ -0,0 +1,4 @@
+update KSLU_CLU_IDENT set CD = 'CORE' where ID = '82388fd5-6ee3-4936-9497-413Dea7e8098'
+/
+insert into KSLU_LU_CODE (ID, VER_NBR, DESCR, TYPE, VALUE, CLU_ID) values ('54dadca0-36e2-4835-8bcd-f1e46647d1e5', '0', 'Classification code', 'kuali.lu.code.UniversityClassification', 'UDCP', '00f5f8c5-fff1-4c8b-92fc-789b891e0849')
+/
