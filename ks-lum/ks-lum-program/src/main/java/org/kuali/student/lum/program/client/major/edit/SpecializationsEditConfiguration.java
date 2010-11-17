9bf6b1fc39c9da08c8a7ac8b12f1b80b91f58d91
jim7 <jim7@kuali.org>
2010-11-17 00:05:24 -0500

    Fix the build
    git-svn-id: https://svn.kuali.org/repos/student/trunk@17599 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/major/edit/SpecializationsEditConfiguration.java
+++ ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/major/edit/SpecializationsEditConfiguration.java
@@ -1,31 +1,32 @@
 package org.kuali.student.lum.program.client.major.edit;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.FlexTable;
 import org.kuali.student.common.ui.client.configurable.mvc.sections.VerticalSection;
 import org.kuali.student.common.ui.client.configurable.mvc.views.VerticalSectionView;
 import org.kuali.student.common.ui.client.mvc.DataModel;
 import org.kuali.student.common.ui.client.widgets.KSButton;
-import org.kuali.student.common.ui.client.widgets.KSButtonAbstract.ButtonStyle;
 import org.kuali.student.common.ui.client.widgets.KSCheckBox;
+import org.kuali.student.common.ui.client.widgets.KSButtonAbstract.ButtonStyle;
 import org.kuali.student.common.ui.client.widgets.field.layout.element.MessageKeyInfo;
 import org.kuali.student.lum.common.client.configuration.AbstractSectionConfiguration;
 import org.kuali.student.lum.common.client.widgets.AppLocations;
 import org.kuali.student.lum.program.client.ProgramConstants;
 import org.kuali.student.lum.program.client.ProgramSections;
+import org.kuali.student.lum.program.client.ProgramStatus;
 import org.kuali.student.lum.program.client.events.AddSpecializationEvent;
 import org.kuali.student.lum.program.client.major.MajorManager;
-import org.kuali.student.lum.program.client.permissions.ModelPermissionType;
 import org.kuali.student.lum.program.client.properties.ProgramProperties;
 import org.kuali.student.lum.program.client.variation.VariationsBinding;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.FlexTable;
 
 /**
  * @author Igor
  */
 public class SpecializationsEditConfiguration extends AbstractSectionConfiguration {
 
-    private KSButton addSpecializationButton = new KSButton(ProgramProperties.get().variationInformation_button_addSpecialization(), ButtonStyle.SECONDARY);
+    private final KSButton addSpecializationButton = new KSButton(ProgramProperties.get().variationInformation_button_addSpecialization(), ButtonStyle.SECONDARY);
 
     public SpecializationsEditConfiguration() {
         rootSection = new VerticalSectionView(ProgramSections.SPECIALIZATIONS_EDIT, ProgramProperties.get().program_menu_sections_specializations(), ProgramConstants.PROGRAM_MODEL_ID);
@@ -53,7 +54,9 @@ public class SpecializationsEditConfiguration extends AbstractSectionConfigurati
 
     @Override
     public boolean checkPermission(DataModel model) {
-        return ModelPermissionType.DRAFT_STATUS.check(model);
+        // return ModelPermissionType.DRAFT_STATUS.check(model);
+        ProgramStatus programStatus = ProgramStatus.of((String) model.get(ProgramConstants.STATE));
+        return programStatus != ProgramStatus.DRAFT;
     }
 
     @Override
