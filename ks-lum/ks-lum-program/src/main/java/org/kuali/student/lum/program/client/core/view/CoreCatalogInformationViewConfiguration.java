41879c3b7686e3c242319eeb9fccbbc3b84f5479
skozlovich <skozlovich@kuali.org>
2010-11-24 18:11:55 -0500

    KSLAB-1554 Fixed the Catalog Description field's view.
    git-svn-id: https://svn.kuali.org/repos/student/trunk@17749 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/core/view/CoreCatalogInformationViewConfiguration.java
+++ ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/core/view/CoreCatalogInformationViewConfiguration.java
@@ -33,7 +33,7 @@ public class CoreCatalogInformationViewConfiguration extends AbstractSectionConf
     protected void buildLayout() {
         TableSection section = new TableSection(SectionTitle.generateEmptyTitle());
         configurer.addReadOnlyField(section, ProgramConstants.DESCRIPTION + "/plain", new MessageKeyInfo(ProgramProperties.get().catalogInformation_descr()));
-        configurer.addReadOnlyField(section, ProgramConstants.CATALOG_DESCRIPTION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_catalogDescr()));
+        configurer.addReadOnlyField(section, ProgramConstants.CATALOG_DESCRIPTION + "/plain", new MessageKeyInfo(ProgramProperties.get().catalogInformation_catalogDescr()));
         configurer.addReadOnlyField(section, ProgramConstants.PUBLICATION_TARGETS, new MessageKeyInfo(ProgramProperties.get().catalogInformation_catalogPublicationTargets()));
         configurer.addReadOnlyField(section, ProgramConstants.MORE_INFORMATION, new MessageKeyInfo(ProgramProperties.get().catalogInformation_referenceUrl()));
         rootSection.addSection(section);
