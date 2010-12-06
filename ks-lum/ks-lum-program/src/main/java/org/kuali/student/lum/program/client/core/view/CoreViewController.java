7423543dae446ad3a01ae27f4373fdd8fcf65f2a
ikuplevich <ikuplevich@kuali.org>
2010-12-05 22:25:44 -0500

    Fixed for KSLAB-1570 and KSLAB-1569
    git-svn-id: https://svn.kuali.org/repos/student/trunk@18019 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/core/view/CoreViewController.java
+++ ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/core/view/CoreViewController.java
@@ -1,5 +1,9 @@
 package org.kuali.student.lum.program.client.core.view;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.shared.HandlerManager;
 import org.kuali.student.common.ui.client.application.KSAsyncCallback;
 import org.kuali.student.common.ui.client.application.ViewContext;
 import org.kuali.student.common.ui.client.mvc.DataModel;
@@ -15,11 +19,6 @@ import org.kuali.student.lum.program.client.core.CoreController;
 import org.kuali.student.lum.program.client.events.ModelLoadedEvent;
 import org.kuali.student.lum.program.client.events.ProgramViewEvent;
 import org.kuali.student.lum.program.client.major.ActionType;
-
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
-import com.google.gwt.event.shared.HandlerManager;
 
 /**
  * @author Igor
@@ -44,17 +43,18 @@ public class CoreViewController extends CoreController {
             @Override
             public void onChange(ChangeEvent event) {
                 ActionType actionType = ActionType.of(actionBox.getSelectedValue());
-            	ViewContext viewContext = getViewContext();                
+                ViewContext viewContext = getViewContext();
                 if (actionType == ActionType.MODIFY) {
                     ProgramRegistry.setSection(ProgramSections.getEditSection(getCurrentViewEnum()));
                     HistoryManager.navigate(AppLocations.Locations.EDIT_CORE_PROGRAM.getLocation(), viewContext);
-                } else if (actionType == ActionType.MODIFY_VERSION){
-                	String versionIndId = programModel.get(ProgramConstants.VERSION_IND_ID);
-                	viewContext.setId(versionIndId);
+                } else if (actionType == ActionType.MODIFY_VERSION) {
+                    String versionIndId = programModel.get(ProgramConstants.VERSION_IND_ID);
+                    viewContext.setId(versionIndId);
                     viewContext.setIdType(IdType.COPY_OF_OBJECT_ID);
-                	HistoryManager.navigate(AppLocations.Locations.EDIT_CORE_PROGRAM.getLocation(), viewContext);                    
+                    ProgramRegistry.setSection(ProgramSections.getEditSection(getCurrentViewEnum()));
+                    HistoryManager.navigate(AppLocations.Locations.EDIT_CORE_PROGRAM.getLocation(), viewContext);
                 }
-                
+
             }
         });
         eventBus.addHandler(ProgramViewEvent.TYPE, new ProgramViewEvent.Handler() {
@@ -66,10 +66,10 @@ public class CoreViewController extends CoreController {
         eventBus.addHandler(ModelLoadedEvent.TYPE, new ModelLoadedEvent.Handler() {
             @Override
             public void onEvent(ModelLoadedEvent event) {
-            	resetActionList();            	
+                resetActionList();
             }
-        });                
-    }    
+        });
+    }
 
     @Override
     protected void configureView() {
@@ -77,22 +77,22 @@ public class CoreViewController extends CoreController {
         addContentWidget(actionBox);
         initialized = true;
     }
-    
+
     protected void resetActionList() {
-    	//Only allow modify with version option for an active course that id also the latest version
-    	ProgramStatus status = ProgramStatus.of(programModel.<String>get(ProgramConstants.STATE));
+        //Only allow modify with version option for an active course that id also the latest version
+        ProgramStatus status = ProgramStatus.of(programModel.<String>get(ProgramConstants.STATE));
         String versionIndId = programModel.get(ProgramConstants.VERSION_IND_ID);
         Long sequenceNumber = programModel.get(ProgramConstants.VERSION_SEQUENCE_NUMBER);
-                       
+
         actionBox.clear();
-        if (status == ProgramStatus.ACTIVE){
-        	programRemoteService.isLatestVersion(versionIndId, sequenceNumber, new KSAsyncCallback<Boolean>(){
-				public void onSuccess(Boolean isLatest) {
-			        actionBox.setList(ActionType.getValues(isLatest));				
-				}        	
-	        });
+        if (status == ProgramStatus.ACTIVE) {
+            programRemoteService.isLatestVersion(versionIndId, sequenceNumber, new KSAsyncCallback<Boolean>() {
+                public void onSuccess(Boolean isLatest) {
+                    actionBox.setList(ActionType.getValues(isLatest));
+                }
+            });
         } else {
-        	actionBox.setList(ActionType.getValues(false));
+            actionBox.setList(ActionType.getValues(false));
         }
     }
 }
--- ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/credential/view/CredentialViewController.java
+++ ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/credential/view/CredentialViewController.java
@@ -1,5 +1,9 @@
 package org.kuali.student.lum.program.client.credential.view;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.shared.HandlerManager;
 import org.kuali.student.common.ui.client.application.KSAsyncCallback;
 import org.kuali.student.common.ui.client.application.ViewContext;
 import org.kuali.student.common.ui.client.mvc.DataModel;
@@ -15,11 +19,6 @@ import org.kuali.student.lum.program.client.credential.CredentialController;
 import org.kuali.student.lum.program.client.events.ModelLoadedEvent;
 import org.kuali.student.lum.program.client.events.ProgramViewEvent;
 import org.kuali.student.lum.program.client.major.ActionType;
-
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
-import com.google.gwt.event.shared.HandlerManager;
 
 /**
  * @author Igor
@@ -44,16 +43,17 @@ public class CredentialViewController extends CredentialController {
             @Override
             public void onChange(ChangeEvent event) {
                 ActionType actionType = ActionType.of(actionBox.getSelectedValue());
-            	ViewContext viewContext = getViewContext();
+                ViewContext viewContext = getViewContext();
                 if (actionType == ActionType.MODIFY) {
                     ProgramRegistry.setSection(ProgramSections.getEditSection(getCurrentViewEnum()));
                     HistoryManager.navigate(AppLocations.Locations.EDIT_BACC_PROGRAM.getLocation(), getViewContext());
-                } else if (actionType == ActionType.MODIFY_VERSION){
-                	String versionIndId = programModel.get(ProgramConstants.VERSION_IND_ID);
-                	viewContext.setId(versionIndId);
+                } else if (actionType == ActionType.MODIFY_VERSION) {
+                    String versionIndId = programModel.get(ProgramConstants.VERSION_IND_ID);
+                    viewContext.setId(versionIndId);
                     viewContext.setIdType(IdType.COPY_OF_OBJECT_ID);
-                	HistoryManager.navigate(AppLocations.Locations.EDIT_BACC_PROGRAM.getLocation(), viewContext);                    
-                }                
+                    ProgramRegistry.setSection(ProgramSections.getEditSection(getCurrentViewEnum()));
+                    HistoryManager.navigate(AppLocations.Locations.EDIT_BACC_PROGRAM.getLocation(), viewContext);
+                }
             }
         });
         eventBus.addHandler(ProgramViewEvent.TYPE, new ProgramViewEvent.Handler() {
@@ -65,9 +65,9 @@ public class CredentialViewController extends CredentialController {
         eventBus.addHandler(ModelLoadedEvent.TYPE, new ModelLoadedEvent.Handler() {
             @Override
             public void onEvent(ModelLoadedEvent event) {
-            	resetActionList();            	
+                resetActionList();
             }
-        });        
+        });
     }
 
     @Override
@@ -76,22 +76,22 @@ public class CredentialViewController extends CredentialController {
         addContentWidget(actionBox);
         initialized = true;
     }
-    
+
     protected void resetActionList() {
-    	//Only allow modify with version option for an active course that id also the latest version
-    	ProgramStatus status = ProgramStatus.of(programModel.<String>get(ProgramConstants.STATE));
+        //Only allow modify with version option for an active course that id also the latest version
+        ProgramStatus status = ProgramStatus.of(programModel.<String>get(ProgramConstants.STATE));
         String versionIndId = programModel.get(ProgramConstants.VERSION_IND_ID);
         Long sequenceNumber = programModel.get(ProgramConstants.VERSION_SEQUENCE_NUMBER);
-                       
+
         actionBox.clear();
-        if (status == ProgramStatus.ACTIVE){
-        	programRemoteService.isLatestVersion(versionIndId, sequenceNumber, new KSAsyncCallback<Boolean>(){
-				public void onSuccess(Boolean isLatest) {
-			        actionBox.setList(ActionType.getValues(isLatest));				
-				}        	
-	        });
+        if (status == ProgramStatus.ACTIVE) {
+            programRemoteService.isLatestVersion(versionIndId, sequenceNumber, new KSAsyncCallback<Boolean>() {
+                public void onSuccess(Boolean isLatest) {
+                    actionBox.setList(ActionType.getValues(isLatest));
+                }
+            });
         } else {
-        	actionBox.setList(ActionType.getValues(false));
+            actionBox.setList(ActionType.getValues(false));
         }
     }
 }
