cdc9ce362cb49968943fa0ae60c7716f84d25db5
jcaddel <jcaddel@kuali.org>
2010-12-06 14:43:57 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@18027 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- maven-cloudfront-plugin/src/main/java/org/kuali/maven/mojo/s3/CloudFrontHtmlGenerator.java
+++ maven-cloudfront-plugin/src/main/java/org/kuali/maven/mojo/s3/CloudFrontHtmlGenerator.java
@@ -57,11 +57,11 @@ public class CloudFrontHtmlGenerator {
     }
 
     protected String getDocType() {
-        return "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">";
+        return "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n";
     }
 
     protected String getMeta() {
-        return "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>";
+        return "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>\n";
     }
 
     /**
