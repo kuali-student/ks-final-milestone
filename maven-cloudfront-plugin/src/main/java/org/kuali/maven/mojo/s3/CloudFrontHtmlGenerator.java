8ed6f53fc1ca8c3509746a997e07484701f217f4
jcaddel <jcaddel@kuali.org>
2010-12-07 04:33:01 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@18057 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- maven-cloudfront-plugin/src/main/java/org/kuali/maven/mojo/s3/CloudFrontHtmlGenerator.java
+++ maven-cloudfront-plugin/src/main/java/org/kuali/maven/mojo/s3/CloudFrontHtmlGenerator.java
@@ -65,7 +65,7 @@ public class CloudFrontHtmlGenerator {
     }
 
     protected String getGoogleAnalyticsJavascript() {
-        return "<script type=\"text/javascript\">var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-16781661-1']); _gaq.push(['_setDomainName', '.kuali.org']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();</script>";
+        return "<script type=\"text/javascript\">var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-16781661-1']); _gaq.push(['_setDomainName', '.kuali.org']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();</script>\n";
     }
 
     /**
