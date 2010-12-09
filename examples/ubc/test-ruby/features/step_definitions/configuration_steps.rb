bcc3cc6bebec49f34924f4346285b7422366d28e
jcaddel <jcaddel@kuali.org>
2010-12-09 04:58:46 -0500

    [maven-release-plugin]  copy for tag impex-1.0.10
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/tags/impex-1.0.10@18206 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- examples/ubc/test-ruby/features/step_definitions/configuration_steps.rb
+++
@@ -1,8 +0,0 @@
-Given /^I have a new campus$/ do
-#  Enumeration.exists?("1000000").should be_true
-
-end
-
-Then /^I should see the configured campus$/ do
-  assert_contain(CDM_CONFIGURED)
-end
--- test/ws/ks-soapui/src/main/java/org/kuali/student/test/ws/WsdlTest.java
+++
@@ -1,4 +0,0 @@
-package org.kuali.student.test.ws;
-
-public class WsdlTest {
-}
