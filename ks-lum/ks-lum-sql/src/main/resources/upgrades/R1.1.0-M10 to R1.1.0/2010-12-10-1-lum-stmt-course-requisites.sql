0f3072a30e0e12040f2b3fd9c4f77271ef079b97
depstein <depstein@kuali.org>
2010-12-13 11:11:09 -0500

    fixed bad sql with extra ';' statement delimiters
    git-svn-id: https://svn.kuali.org/repos/student/trunk@18266 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-sql/src/main/resources/upgrades/R1.1.0-M10 to R1.1.0/2010-12-10-1-lum-stmt-course-requisites.sql
+++ ks-lum/ks-lum-sql/src/main/resources/upgrades/R1.1.0-M10 to R1.1.0/2010-12-10-1-lum-stmt-course-requisites.sql
@@ -1,12 +1,12 @@
 insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
-values ('81d26859-c5bf-463b-b18e-802ca4f2619a', 'kuali.statement.type.course.academicReadiness.studentEligibilityPrereq', 'en', 'course', '<b>Students must meet one of the following:</b><br/>- Must be registered majors in:<br/>\&nbsp;\&nbsp;\&nbsp;X major<br/>- Must obtian permission of course instructor<br/>- Must be of Junior standing<br/>- Must have earned a GPA of X or above<br/>- Must be students at the following campus(es)<br/>\&nbsp;\&nbsp;\&nbsp;X Campus</br><br/>Students must have completed:<br/>MATH 100 Introduction to Mathematics\&nbsp;\&nbsp;\&nbsp;3 credits<br/>', '4251E3C1-017B-DCBA-5FE9-603C76F486C5', 8);
+values ('81d26859-c5bf-463b-b18e-802ca4f2619a', 'kuali.statement.type.course.academicReadiness.studentEligibilityPrereq', 'en', 'course', '<b>Students must meet one of the following:</b><br/>- Must be registered majors in:<br/>\&nbsp;\&nbsp;\&nbsp;X major<br/>- Must obtian permission of course instructor<br/>- Must be of Junior standing<br/>- Must have earned a GPA of X or above<br/>- Must be students at the following campus(es)<br/>\&nbsp;\&nbsp;\&nbsp;X Campus</br><br/>Students must have completed:<br/>MATH 100 Introduction to Mathematics\&nbsp;\&nbsp;\&nbsp;3 credits<br/>', '4251E3C1-017B-DCBA-5FE9-603C76F486C5', 8)
 /
 insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
-values ('27494861-d70f-43a0-90e2-8867dea2bb60', 'kuali.statement.type.course.academicReadiness.coreq', 'en', 'course', '<b>Students must enroll in:</b><br/> MATH 102 Mathematics 2\&nbsp;\&nbsp;\&nbsp;3 credits<br/>', 'D1231CDA-ECE8-AEF5-3470-A66D36A12A74', 2);
+values ('27494861-d70f-43a0-90e2-8867dea2bb60', 'kuali.statement.type.course.academicReadiness.coreq', 'en', 'course', '<b>Students must enroll in:</b><br/> MATH 102 Mathematics 2\&nbsp;\&nbsp;\&nbsp;3 credits<br/>', 'D1231CDA-ECE8-AEF5-3470-A66D36A12A74', 2)
 /
 insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
-values ('611092d9-5883-42dc-b22b-ed12c24f22bc', 'kuali.statement.type.course.recommendedPreparation', 'en', 'course', '<b>It is recommended that students have completed:</b><br/> MATH 103 Mathematics 3\&nbsp;\&nbsp;\&nbsp;3 credits<br/>', '06480D18-7A91-8CAE-7F88-493BE59F0C71', 3);
+values ('611092d9-5883-42dc-b22b-ed12c24f22bc', 'kuali.statement.type.course.recommendedPreparation', 'en', 'course', '<b>It is recommended that students have completed:</b><br/> MATH 103 Mathematics 3\&nbsp;\&nbsp;\&nbsp;3 credits<br/>', '06480D18-7A91-8CAE-7F88-493BE59F0C71', 3)
 /
 insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
-values ('a77b63d9-a7e7-467a-bcbf-d499e0f0651f', 'kuali.statement.type.course.academicReadiness.antireq', 'en', 'course', '<b>Students must not have completed:</b><br/> MATH 150 Theoretical Mathematics\&nbsp;\&nbsp;\&nbsp;3 credits<br/>', '5174A1FF-FA83-56D6-91AD-FE61E4BD4653', 2);
+values ('a77b63d9-a7e7-467a-bcbf-d499e0f0651f', 'kuali.statement.type.course.academicReadiness.antireq', 'en', 'course', '<b>Students must not have completed:</b><br/> MATH 150 Theoretical Mathematics\&nbsp;\&nbsp;\&nbsp;3 credits<br/>', '5174A1FF-FA83-56D6-91AD-FE61E4BD4653', 2)
 /
