36a10b8f24f0c6cee0ee84ab315051d3669b3d43
jcaddel <jcaddel@kuali.org>
2010-12-15 15:34:03 -0500

    Checkstyle updates
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@18357 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- maven-cloudfront-plugin/src/main/java/org/kuali/maven/mojo/s3/Tag.java
+++ maven-cloudfront-plugin/src/main/java/org/kuali/maven/mojo/s3/Tag.java
@@ -1,55 +1,107 @@
 package org.kuali.maven.mojo.s3;
 
 /**
- * Lightweight pojo for helping generate html
+ * Lightweight pojo for helping generate html.
  */
 public class Tag {
 
-    String name;
-    String id;
-    String clazz;
+    /**
+     * The html tag name.
+     */
+    private String name;
 
+    /**
+     * The html id.
+     */
+    private String id;
+
+    /**
+     * The html class.
+     */
+    private String clazz;
+
+    /**
+     * No args constructor.
+     */
     public Tag() {
         this(null);
     }
 
-    public Tag(String name) {
+    /**
+     * @param name
+     * the name to set
+     */
+    public Tag(final String name) {
         this(name, null);
     }
 
-    public Tag(String name, String clazz) {
+    /**
+     * @param name
+     * the name to set
+     * @param clazz
+     * the clazz to set
+     */
+    public Tag(final String name, final String clazz) {
         this(name, clazz, null);
     }
 
-    public Tag(String name, String clazz, String id) {
+    /**
+     * @param name
+     * the name to set
+     * @param clazz
+     * the clazz to set
+     * @param id
+     * the id to set
+     */
+    public Tag(final String name, final String clazz, final String id) {
         super();
         this.name = name;
         this.id = id;
         this.clazz = clazz;
     }
 
+    /**
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    /**
+     * @param name
+     * the name to set
+     */
+    public void setName(final String name) {
         this.name = name;
     }
 
+    /**
+     * @return the id
+     */
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
+    /**
+     * @param id
+     * the id to set
+     */
+    public void setId(final String id) {
         this.id = id;
     }
 
+    /**
+     * @return the clazz
+     */
     public String getClazz() {
         return clazz;
     }
 
-    public void setClazz(String clazz) {
+    /**
+     * @param clazz
+     * the clazz to set
+     */
+    public void setClazz(final String clazz) {
         this.clazz = clazz;
     }
-
 }
