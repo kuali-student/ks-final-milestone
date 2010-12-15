5c766c011e138eb7d9c9af3f823d19360a15f5b3
jcaddel <jcaddel@kuali.org>
2010-12-15 15:04:51 -0500

    Checkstyle updates
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@18356 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- maven-cloudfront-plugin/src/main/java/org/kuali/maven/mojo/s3/package-info.java
+++ maven-cloudfront-plugin/src/main/java/org/kuali/maven/mojo/s3/package-info.java
@@ -1,6 +1,6 @@
 /**
- * Logic for examining an Amazon S3 bucket and creating an html style directory
- * listing that displays the contents of the bucket in a folder structure.
+ * Logic for examining an Amazon S3 bucket and creating html style directory
+ * listings that display the contents of the bucket in a folder structure.
  *
  * @author Jeff Caddel
  */
