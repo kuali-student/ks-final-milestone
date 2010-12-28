50568c0e940536852266bc24eabe22347f4282b1
jcaddel <jcaddel@kuali.org>
2010-12-28 10:22:36 -0500

    Fixed a misleading javadoc comment
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@18801 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/maven-impex-plugin/src/main/java/org/apache/torque/mojo/MorphSchemaMojo.java
+++ impex/maven-impex-plugin/src/main/java/org/apache/torque/mojo/MorphSchemaMojo.java
@@ -8,62 +8,62 @@ import org.apache.torque.mojo.morph.Morpher;
 import org.apache.torque.mojo.morph.SchemaMorpher;
 
 /**
- * Convert an Ant impex schema XML file into a maven-impex-plugin schema XML
- * file
- * 
+ * Convert an Ant impex schema XML file into a maven-impex-plugin schema XML file
+ *
  * @goal morphschema
  * @phase generate-sources
  */
 public class MorphSchemaMojo extends AbstractMorphSingleMojo {
 
-	/**
-	 * The XML file describing the database schema
-	 * 
-	 * @parameter expression="${newSchemaXMLFile}" default-value=
-	 *            "${project.build.directory}/generated-impex/${project.artifactId}.xml"
-	 * @required
-	 */
-	private File newSchemaXMLFile;
+    /**
+     * The XML file describing the database schema (Maven style)
+     *
+     * @parameter expression="${newSchemaXMLFile}" default-value=
+     * "${project.build.directory}/generated-impex/${project.artifactId}.xml"
+     * @required
+     */
+    private File newSchemaXMLFile;
 
-	/**
-	 * The path to the directory where the schema XML files are located
-	 * 
-	 * @parameter expression="${oldSchemaXMLFile}"
-	 *            default-value="${basedir}/src/main/impex/schema.xml"
-	 * @required
-	 */
-	private File oldSchemaXMLFile;
+    /**
+     * The XML file describing the database schema (Ant style)
+     *
+     * @parameter expression="${oldSchemaXMLFile}" default-value="${basedir}/src/main/impex/schema.xml"
+     * @required
+     */
+    private File oldSchemaXMLFile;
 
-	@Override
-	protected void beforeExecution() {
-		setNewFile(newSchemaXMLFile);
-		setOldFile(oldSchemaXMLFile);
-	}
+    @Override
+    protected void beforeExecution() {
+        setNewFile(newSchemaXMLFile);
+        setOldFile(oldSchemaXMLFile);
+    }
 
-	protected void executeMojo() throws MojoExecutionException {
-		getLog().info("------------------------------------------------------------------------");
-		getLog().info("Converting schema XML file");
-		getLog().info("------------------------------------------------------------------------");
-		super.executeMojo();
-	}
+    @Override
+    protected void executeMojo() throws MojoExecutionException {
+        getLog().info("------------------------------------------------------------------------");
+        getLog().info("Converting schema XML file");
+        getLog().info("------------------------------------------------------------------------");
+        super.executeMojo();
+    }
 
-	protected Morpher getMorpher(MorphRequest request, String artifactId) {
-		return new SchemaMorpher(request, artifactId);
-	}
+    @Override
+    protected Morpher getMorpher(final MorphRequest request, final String artifactId) {
+        return new SchemaMorpher(request, artifactId);
+    }
 
-	public File getNewSchemaXMLFile() {
-		return newSchemaXMLFile;
-	}
+    public File getNewSchemaXMLFile() {
+        return newSchemaXMLFile;
+    }
 
-	public void setNewSchemaXMLFile(File newSchemaXMLFile) {
-		this.newSchemaXMLFile = newSchemaXMLFile;
-	}
+    public void setNewSchemaXMLFile(final File newSchemaXMLFile) {
+        this.newSchemaXMLFile = newSchemaXMLFile;
+    }
 
-	public File getOldSchemaXMLFile() {
-		return oldSchemaXMLFile;
-	}
+    public File getOldSchemaXMLFile() {
+        return oldSchemaXMLFile;
+    }
 
-	public void setOldSchemaXMLFile(File oldSchemaXMLFile) {
-		this.oldSchemaXMLFile = oldSchemaXMLFile;
-	}
+    public void setOldSchemaXMLFile(final File oldSchemaXMLFile) {
+        this.oldSchemaXMLFile = oldSchemaXMLFile;
+    }
 }
