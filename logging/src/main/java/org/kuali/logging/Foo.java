370ac756db1eab058a08031d3ab307b48fd52e93
jcaddel <jcaddel@kuali.org>
2010-12-29 20:52:00 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@18850 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- logging/src/main/java/org/kuali/logging/Foo.java
+++ logging/src/main/java/org/kuali/logging/Foo.java
@@ -4,14 +4,13 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class Foo {
-    private static final Log log = LogFactory.getLog(Foo.class);
 
     public static void main(final String[] args) {
         try {
-            log.info("hello world");
+            Log log = LogFactory.getLog(Foo.class);
+            log.info(log.getClass().getCanonicalName());
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
-
 }
