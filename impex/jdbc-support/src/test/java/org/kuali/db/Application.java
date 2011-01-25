3388bf6d39ce3e3484fa8603403e67b985bacbac
jcaddel <jcaddel@kuali.org>
2011-01-25 16:02:26 -0500

    Spring style injection (as opposed to Maven plugin style)
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@19302 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ impex/jdbc-support/src/test/java/org/kuali/db/Application.java
@@ -0,0 +1,18 @@
+package org.kuali.db;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+	public static void main(String[] args) {
+		try {
+			ApplicationContext context = new ClassPathXmlApplicationContext("impex-context.xml");
+			ConnectionHandler ch = (ConnectionHandler) context.getBean("impexConnectionHandler");
+			System.out.println(ch.getCredentials().getUsername());
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+}
