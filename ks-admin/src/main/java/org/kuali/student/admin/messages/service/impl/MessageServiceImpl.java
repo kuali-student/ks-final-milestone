64c699a5589b97efea191c9a1348d4af25ab18a4
depstein <depstein@kuali.org>
2011-01-25 12:32:56 -0500

    Added some simple message caching for ks-admin message service
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.1-perf@19299 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-admin/src/main/java/org/kuali/student/admin/messages/service/impl/MessageServiceImpl.java
+++ ks-admin/src/main/java/org/kuali/student/admin/messages/service/impl/MessageServiceImpl.java
@@ -2,6 +2,7 @@ package org.kuali.student.admin.messages.service.impl;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -10,6 +11,8 @@ import java.util.Map;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
+import org.kuali.rice.core.util.MaxAgeSoftReference;
+import org.kuali.rice.core.util.MaxSizeMap;
 import org.kuali.rice.kns.service.BusinessObjectService;
 import org.kuali.rice.kns.service.KNSServiceLocator;
 import org.kuali.student.core.enumerationmanagement.bo.EnumeratedValue;
@@ -19,11 +22,17 @@ import org.kuali.student.core.messages.dto.Message;
 import org.kuali.student.core.messages.dto.MessageGroupKeyList;
 import org.kuali.student.core.messages.dto.MessageList;
 import org.kuali.student.core.messages.service.MessageService;
+import org.springframework.beans.factory.InitializingBean;
 
 @WebService(endpointInterface = "org.kuali.student.core.messages.service.MessageService", serviceName = "MessageService", portName = "MessageService", targetNamespace = "http://student.kuali.org/wsdl/messages")
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
-public class MessageServiceImpl implements MessageService {
+public class MessageServiceImpl implements MessageService, InitializingBean {
 
+	protected boolean cachingEnabled = false;
+	protected int msgsCacheMaxSize = 20;
+	protected int msgsCacheMaxAgeSeconds = 90;
+	protected Map<String,MaxAgeSoftReference<MessageList>> msgsCache;
+	
     private BusinessObjectService businessObjectService;
 
     public Message addMessage(Message messageInfo) {
@@ -89,6 +98,17 @@ public class MessageServiceImpl implements MessageService {
     @SuppressWarnings("unchecked")
     public MessageList getMessages(String localeKey, String messageGroupKey) {
         
+    	if(cachingEnabled){
+    		//Get From Cache
+    		MaxAgeSoftReference<MessageList> ref = msgsCache.get("localeKey="+localeKey+", messageGroupKey="+messageGroupKey);
+    		if ( ref != null ) {
+    			MessageList messageList = ref.get();
+    			if(messageList!=null){
+    				return messageList;
+    			}
+    		}
+		}
+    	
     	Map<String,String> fieldValues = new HashMap<String,String>();
     	fieldValues.put("locale", localeKey);
     	fieldValues.put("groupName", messageGroupKey);
@@ -104,12 +124,17 @@ public class MessageServiceImpl implements MessageService {
     	MessageList messageList = new MessageList();
     	messageList.setMessages(messages);
     	
+    	if(cachingEnabled){
+    		//Store to cache
+    		msgsCache.put("localeKey="+localeKey+", messageGroupKey="+messageGroupKey, new MaxAgeSoftReference<MessageList>( msgsCacheMaxAgeSeconds, messageList) );
+    	}
+    	
     	return messageList;
     	
     }
 
     public MessageList getMessagesByGroups(String localeKey, MessageGroupKeyList messageGroupKeyList) {
-        
+    	
     	List<Message> messages = new ArrayList<Message>();
     	
     	for(String messageGroupKey: messageGroupKeyList.getMessageGroupKeys()){
@@ -120,7 +145,7 @@ public class MessageServiceImpl implements MessageService {
     	messageList.setMessages(messages);
     	
     	return messageList;
-    	
+
     }
 
     public Message updateMessage(String localeKey, String messageGroupKey, String messageKey, Message messageInfo) {
@@ -172,4 +197,15 @@ public class MessageServiceImpl implements MessageService {
         return businessObjectService;
     }
 
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if(cachingEnabled){
+			msgsCache = Collections.synchronizedMap( new MaxSizeMap<String,MaxAgeSoftReference<MessageList>>( msgsCacheMaxSize ) );
+		}
+	}
+
+	public void setCachingEnabled(boolean cachingEnabled) {
+		this.cachingEnabled = cachingEnabled;
+	}
+	
 }
