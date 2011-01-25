0eaa16fa7935b1e1a8e86666c1d5a462d6d2331e
jcaddel <jcaddel@kuali.org>
2011-01-25 17:05:15 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@19307 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/jdbc-support/src/test/java/org/kuali/db/Application.java
+++ impex/jdbc-support/src/test/java/org/kuali/db/Application.java
@@ -8,7 +8,7 @@ public class Application {
 	public static void main(String[] args) {
 		try {
 			ApplicationContext context = new ClassPathXmlApplicationContext("impex-context.xml");
-			ConnectionHandler ch = (ConnectionHandler) context.getBean("impexConnectionHandler");
+			ConnectionHandler ch = (ConnectionHandler) context.getBean("impexDbaConnectionHandler");
 			System.out.println(ch.getCredentials().getUsername());
 		} catch (Throwable t) {
 			t.printStackTrace();
