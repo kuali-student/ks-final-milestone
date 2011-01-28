05bced7eab687c029877ab8685c2c47cc2b682a8
jcaddel <jcaddel@kuali.org>
2011-01-28 08:20:32 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@19335 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ examples/jdbc-support/src/main/java/org/kuali/db/ResetSql.java
@@ -0,0 +1,22 @@
+package org.kuali.db;
+
+public class ResetSql {
+	String dropSql;
+	String createSql;
+
+	public String getDropSql() {
+		return dropSql;
+	}
+
+	public void setDropSql(String dropSql) {
+		this.dropSql = dropSql;
+	}
+
+	public String getCreateSql() {
+		return createSql;
+	}
+
+	public void setCreateSql(String createSql) {
+		this.createSql = createSql;
+	}
+}
