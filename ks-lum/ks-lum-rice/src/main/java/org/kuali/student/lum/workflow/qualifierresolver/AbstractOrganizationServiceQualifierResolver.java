ddf1db9c4bacb3d3d23feb1eccfe5a118dce620f
huangb <huangb@kuali.org>
2011-01-28 11:18:37 -0500

    KSLAB-1737: Move several constants definitions from  AbstractCoCOrgQualifierResolver to AbstractOrganizationServiceQualifierResolver. AbstractCoCOrgQualifierResolver will be removed as the part of code cleanup efforts.
    git-svn-id: https://svn.kuali.org/repos/student/trunk@19350 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-rice/src/main/java/org/kuali/student/lum/workflow/qualifierresolver/AbstractOrganizationServiceQualifierResolver.java
+++ ks-lum/ks-lum-rice/src/main/java/org/kuali/student/lum/workflow/qualifierresolver/AbstractOrganizationServiceQualifierResolver.java
@@ -40,13 +40,21 @@ import org.w3c.dom.NodeList;
  */
 public abstract class AbstractOrganizationServiceQualifierResolver implements QualifierResolver {
     private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(AbstractOrganizationServiceQualifierResolver.class);
-
+    
     protected static final String DOCUMENT_CONTENT_XML_DEFAULT_ORG_ID_KEY = "orgId";
     protected static final String DOCUMENT_CONTENT_XML_ORG_ID_KEY = "organizationIdDocumentContentKey";
 
     // below string MUST match
-    // org.kuali.student.core.assembly.transform.WorkflowFilter.DOCUMENT_CONTENT_XML_ROOT_ELEMENT_NAME constant
+    // org.kuali.student.core.assembly.transform.ProposalWorkflowFilter.DOCUMENT_CONTENT_XML_ROOT_ELEMENT_NAME constant
     public static final String DOCUMENT_CONTENT_XML_ROOT_ELEMENT_NAME = "info";
+
+    public static final String KUALI_ORG_TYPE_CURRICULUM_PARENT = "kuali.org.CurriculumParent";
+    public static final String KUALI_ORG_HIERARCHY_CURRICULUM  = "kuali.org.hierarchy.Curriculum";
+    public static final String KUALI_ORG_DEPARTMENT               = "kuali.org.Department";
+    public static final String KUALI_ORG_COLLEGE                  = "kuali.org.College";
+    public static final String KUALI_ORG_COC                      = "kuali.org.COC";
+    public static final String KUALI_ORG_DIVISION                 = "kuali.org.Division";
+    public static final String KUALI_ORG_PROGRAM                  = "kuali.org.Program";
 
     private OrganizationService organizationService;
 
