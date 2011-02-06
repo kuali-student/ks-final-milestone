6e7bfad2cce9990cbb462071720d97498ae74fab
jcaddel <jcaddel@kuali.org>
2011-02-06 08:06:58 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@19483 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- maven-cloudfront-plugin/src/main/java/org/kuali/maven/mojo/s3/package-info.java
+++ maven-cloudfront-plugin/src/main/java/org/kuali/maven/mojo/s3/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Logic for examining an Amazon S3 bucket and creating html style directory
+ * Logic for examining an Amazon S3 bucket and creating html directory
  * listings that display the contents of the bucket in a folder structure.
  *
  * @author Jeff Caddel
