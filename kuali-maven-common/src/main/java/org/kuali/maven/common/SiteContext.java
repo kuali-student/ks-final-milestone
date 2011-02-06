883bcd768fb9fda473c9e05afeaafa8753923e4d
jcaddel <jcaddel@kuali.org>
2011-02-06 07:50:57 -0500

    ''
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@19479 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ kuali-maven-common/src/main/java/org/kuali/maven/common/SiteContext.java
@@ -0,0 +1,21 @@
+package org.kuali.maven.common;
+
+import org.apache.maven.project.MavenProject;
+
+public interface SiteContext {
+    public MavenProject getTargetProject();
+
+    public String getDownloadSnapshotPrefix();
+
+    public String getDownloadReleasePrefix();
+
+    public String getDownloadPrefix();
+
+    public String getBucket();
+
+    public String getHostname();
+
+    public String getPublicUrlProtocol();
+
+    public String getPublishUrlProtocol();
+}
