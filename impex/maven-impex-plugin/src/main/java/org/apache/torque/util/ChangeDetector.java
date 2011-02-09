f63434df2e5d4542e6b12a02daccc9d40eb209dd
jcaddel <jcaddel@kuali.org>
2011-02-09 08:01:31 -0500

    Better javadoc comment
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@19653 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/maven-impex-plugin/src/main/java/org/apache/torque/util/ChangeDetector.java
+++ impex/maven-impex-plugin/src/main/java/org/apache/torque/util/ChangeDetector.java
@@ -8,53 +8,54 @@ import org.apache.commons.logging.LogFactory;
 
 public class ChangeDetector {
 
-	private static final Log log = LogFactory.getLog(ChangeDetector.class);
-	File controlFile;
-	List<File> files;
+    private static final Log log = LogFactory.getLog(ChangeDetector.class);
+    File controlFile;
+    List<File> files;
 
-	public ChangeDetector() {
-		this(null, null);
-	}
+    public ChangeDetector() {
+        this(null, null);
+    }
 
-	public ChangeDetector(File controlFile, List<File> files) {
-		super();
-		this.controlFile = controlFile;
-		this.files = files;
-	}
+    public ChangeDetector(final File controlFile, final List<File> files) {
+        super();
+        this.controlFile = controlFile;
+        this.files = files;
+    }
 
-	/**
-	 * Return true if any file in the list of files has a last modified timestamp newer than the control file or if the
-	 * control file does not exist
-	 */
-	public boolean isChanged() {
-		if (!getControlFile().exists()) {
-			log.debug("File " + getControlFile().getAbsolutePath() + " does not exist.  Returning true");
-			return true;
-		}
-		long lastModified = getControlFile().lastModified();
-		for (File file : getFiles()) {
-			if (file.lastModified() > lastModified) {
-				log.debug("File " + file.getAbsolutePath() + " was modified after " + getControlFile().getAbsolutePath() + " was last modified");
-				return true;
-			}
-		}
-		log.debug("No files were modified.");
-		return false;
-	}
+    /**
+     * Return true if the control file does not exist, or if any file in the list of files has a last modified timestamp
+     * newer than the control file
+     */
+    public boolean isChanged() {
+        if (!getControlFile().exists()) {
+            log.debug("File " + getControlFile().getAbsolutePath() + " does not exist.  Returning true");
+            return true;
+        }
+        long lastModified = getControlFile().lastModified();
+        for (File file : getFiles()) {
+            if (file.lastModified() > lastModified) {
+                log.debug("File " + file.getAbsolutePath() + " was modified after "
+                        + getControlFile().getAbsolutePath() + " was last modified");
+                return true;
+            }
+        }
+        log.debug("No files were modified.");
+        return false;
+    }
 
-	public File getControlFile() {
-		return controlFile;
-	}
+    public File getControlFile() {
+        return controlFile;
+    }
 
-	public void setControlFile(File controlFile) {
-		this.controlFile = controlFile;
-	}
+    public void setControlFile(final File controlFile) {
+        this.controlFile = controlFile;
+    }
 
-	public List<File> getFiles() {
-		return files;
-	}
+    public List<File> getFiles() {
+        return files;
+    }
 
-	public void setFiles(List<File> files) {
-		this.files = files;
-	}
+    public void setFiles(final List<File> files) {
+        this.files = files;
+    }
 }
