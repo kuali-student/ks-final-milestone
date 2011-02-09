332c016c2d1b301e9bbe7e7f22f1825d3fcd2f17
jcaddel <jcaddel@kuali.org>
2011-02-09 07:59:26 -0500

    More accurate comment
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@19652 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/maven-impex-plugin/src/main/java/org/apache/torque/util/SimpleScanner.java
+++ impex/maven-impex-plugin/src/main/java/org/apache/torque/util/SimpleScanner.java
@@ -10,37 +10,40 @@ import org.codehaus.plexus.util.DirectoryScanner;
  * This class provides a simple method for scanning a directory for files that match include/exclude patterns
  */
 public class SimpleScanner extends DirectoryScanner {
-	private static final String FS = System.getProperty("file.separator");
+    private static final String FS = System.getProperty("file.separator");
 
-	public SimpleScanner() {
-		this(null, null, null);
-	}
+    public SimpleScanner() {
+        this(null, null, null);
+    }
 
-	public SimpleScanner(File baseDir, String include, String exclude) {
-		super();
-		if (baseDir != null) {
-			setBasedir(baseDir);
-		}
-		if (include != null) {
-			setIncludes(new String[] { include });
-		}
-		if (exclude != null) {
-			setExcludes(new String[] { exclude });
-		}
-	}
+    /**
+     * Constructor that makes it simple to specify a single include pattern and/or a single exclude pattern
+     */
+    public SimpleScanner(final File baseDir, final String include, final String exclude) {
+        super();
+        if (baseDir != null) {
+            setBasedir(baseDir);
+        }
+        if (include != null) {
+            setIncludes(new String[] { include });
+        }
+        if (exclude != null) {
+            setExcludes(new String[] { exclude });
+        }
+    }
 
-	/**
-	 * This method returns files that match an include pattern but do not match an exclude pattern
-	 */
-	public List<File> getFiles() {
-		scan();
-		String[] includedFiles = getIncludedFiles();
-		List<File> files = new ArrayList<File>();
-		for (String includedFile : includedFiles) {
-			String filename = getBasedir().getAbsolutePath() + FS + includedFile;
-			File file = new File(filename);
-			files.add(file);
-		}
-		return files;
-	}
+    /**
+     * This method returns files that match an include pattern but do not match an exclude pattern
+     */
+    public List<File> getFiles() {
+        scan();
+        String[] includedFiles = getIncludedFiles();
+        List<File> files = new ArrayList<File>();
+        for (String includedFile : includedFiles) {
+            String filename = getBasedir().getAbsolutePath() + FS + includedFile;
+            File file = new File(filename);
+            files.add(file);
+        }
+        return files;
+    }
 }
