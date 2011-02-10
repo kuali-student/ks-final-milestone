1f4908d489284e7b893d9f63ec6ff7957705db84
wgomes <wgomes@kuali.org>
2011-02-10 16:55:29 -0500

    KSLAB-1744 Forgot to check in fix that actually restored the link removed in KSLUM-527
    git-svn-id: https://svn.kuali.org/repos/student/trunk@19670 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-ui-common/src/main/java/org/kuali/student/lum/common/client/lo/LOBuilder.java
+++ ks-lum/ks-lum-ui-common/src/main/java/org/kuali/student/lum/common/client/lo/LOBuilder.java
@@ -82,37 +82,23 @@ public class LOBuilder extends VerticalSection implements HasValue<List<OutlineN
 		repoKey = loRepoKey;
 		messageGroup = luGroup;
 
-		// searchLink = new KSLabel(getLabel(LUConstants.LO_SEARCH_LINK_KEY));
-		// picker needs to handle labels like this
-		/*
-		 * TODO - KSLUM-527: temporally comment out link to LO Search for M9.
-		 * Need to be fixed later.
-		 */
-		/*
-		 * if(metadata.getInitialLookup() != null) {
-		 * 	searchWindow = new KSPicker(metadata.getInitialLookup(), metadata.getAdditionalLookups());
-		 * 	searchWindow.addValuesChangeHandler(new ValueChangeHandler<List<String>>() {
-		 * 		public void onValueChange(ValueChangeEvent<List<String>> event) {
-		 * 		        List<String> selection = (List<String>)event.getValue();
-		 * 				loList.addSelectedLOs(selection);
-		 * 			}
-		 * 	    }
-		 * 	);
-		 * 	searchMainPanel.add(searchWindow);
-		 * }
-		 */
-		 
+		 if(metadata.getInitialLookup() != null) {
+		 	searchWindow = new KSPicker(metadata.getInitialLookup(), metadata.getAdditionalLookups());
+		  	searchWindow.addValuesChangeHandler(new ValueChangeHandler<List<String>>() {
+		  		public void onValueChange(ValueChangeEvent<List<String>> event) {
+		  		        List<String> selection = (List<String>)event.getValue();
+		  				loList.addSelectedLOs(selection);
+		  			}
+		  	    }
+		  	);
+		  	searchMainPanel.add(searchWindow);
+		  }
+		 		 
 
-		// adding search icon - should this be part of search link? coordinate
-		// with UX
-		// searchImage.addClickHandler(searchClickHandler);
-		// Image searchImage = Theme.INSTANCE.getCommonImages().getSearchIcon();
-		// searchLinkPanel.add(searchImage);
 		instructions = new KSLabel(getLabel(LUUIConstants.LO_INSTRUCTIONS_KEY));
 
         loList = new LearningObjectiveList();
 
-		// searchImage.addStyleName("KS-LOBuilder-Search-Image");
 		searchMainPanel.addStyleName("KS-LOBuilder-Search-Panel");
 
         loList.addStyleName(LUUIConstants.STYLE_SECTION);
