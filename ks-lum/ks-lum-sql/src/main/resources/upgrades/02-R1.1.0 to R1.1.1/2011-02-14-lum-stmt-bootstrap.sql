4c4b6f4b6cd7dd0c222a195a14a60278c37007d0
alubbers <alubbers@kuali.org>
2011-02-16 12:51:09 -0500

    [KSLAB-1705] Removed xml entities from strings
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.1@19787 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-sql/src/main/resources/upgrades/02-R1.1.0 to R1.1.1/2011-02-14-lum-stmt-bootstrap.sql
+++ ks-lum/ks-lum-sql/src/main/resources/upgrades/02-R1.1.0 to R1.1.1/2011-02-14-lum-stmt-bootstrap.sql
@@ -1,5 +1,5 @@
 INSERT INTO KSST_REQ_COM_TYPE (EFF_DT,NAME,TYPE_DESC,TYPE_KEY,VER_NBR)
-  VALUES (TO_DATE( '20000101000000', 'YYYYMMDDHH24MISS' ), 'Enrolled in all required courses', 'Must be concurrently enrolled in all courses from &lt;courses>', 'kuali.reqComponent.type.course.courseset.enrolled.all', 0)
+  VALUES (TO_DATE( '20000101000000', 'YYYYMMDDHH24MISS' ), 'Enrolled in all required courses', 'Must be concurrently enrolled in all courses from <courses>', 'kuali.reqComponent.type.course.courseset.enrolled.all', 0)
 /
 
 
@@ -16,7 +16,7 @@ INSERT INTO KSST_REQ_COM_TYPE_NL_TMPL (ID,LANGUAGE,NL_USUAGE_TYPE_KEY,OWNER,TEMP
   VALUES ('209','en','KUALI.RULE.EXAMPLE','kuali.reqComponent.type.course.courseset.enrolled.all','Must be concurrently enrolled in all courses from (MATH111, 140, 220, and STAT100)',0)
 /
 INSERT INTO KSST_REQ_COM_TYPE_NL_TMPL (ID,LANGUAGE,NL_USUAGE_TYPE_KEY,OWNER,TEMPLATE,VER_NBR)
-  VALUES ('309','en','KUALI.RULE.COMPOSITION','kuali.reqComponent.type.course.courseset.enrolled.all','%lt;reqCompFieldType=kuali.reqComponent.field.type.course.cluSet.id;reqCompFieldLabel=Courses>',0)
+  VALUES ('309','en','KUALI.RULE.COMPOSITION','kuali.reqComponent.type.course.courseset.enrolled.all','<reqCompFieldType=kuali.reqComponent.field.type.course.cluSet.id;reqCompFieldLabel=Courses>',0)
 /
 
 INSERT INTO KSST_RCTYP_JN_RCFLDTYP (REQ_COMP_FIELD_TYPE_ID,REQ_COMP_TYPE_ID)
