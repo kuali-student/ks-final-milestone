0a59dd1e5bdade01a4672e63c778398b72ec1cea
jcaddel <jcaddel@kuali.org>
2011-02-18 14:30:13 -0500

    Updates for the demo environment
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@19845 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- scripts/deploy-application.sh
+++ scripts/deploy-application.sh
@@ -8,6 +8,20 @@ VERSION=$1
 SVN_DIR=$2
 ENVIRONMENT=$3
 APP_WORKSPACE=$4
+WAR_FILE=ks-embedded-$VERSION-SNAPSHOT.war
+LOCAL_WAR_FILE=$APP_WORKSPACE/$SVN_DIR/ks-web/ks-embedded/target/$WAR_FILE
+REMOTE_WAR_FILE=/usr/local/student/downloads/$WAR_FILE
+REMOTE_TOMCAT_DIR=/usr/local/student/embedded/$REMOTE_DIR
+LOCAL_MVN_DIR=$APP_WORKSPACE/$SVN_DIR/ks-cfg-dbs/ks-embedded-db
+DB_URL=jdbc:oracle:thin:@deploy.ks.kuali.org:1521:KS
+DBA_PASSWORD=gw570229
+REMOTE_SERVER=root@deploy.ks.kuali.org
+PEM_FILE=/home/tomcat/ks-key.pem
+SHUTDOWN_CMD=su - $REMOTE_USER -c /usr/local/tomcat_$REMOTE_USER/bin/shutdown.sh
+CLEANUP_CMD=su - $REMOTE_USER -c /usr/local/tomcat_$REMOTE_USER/bin/cleanup.sh
+STARTUP_CMDsu - $REMOTE_USER -c /usr/local/tomcat_$REMOTE_USER/bin/startup.sh
+
+
 
 if [ "$APP_WORKSPACE" = "" ]
 then
@@ -43,28 +57,28 @@ then
   DB_SCHEMA=KSSTAGING
   REMOTE_USER=staging
   REMOTE_DIR=staging
+elif [ "$ENVIRONMENT" = "demo" ]
+then
+  DB_SCHEMA=KSDEMO
+  REMOTE_USER=demo
+  REMOTE_DIR=demo
+  DB_URL=jdbc:oracle:thin:@demo.ks.kuali.org:1521:KS
+  REMOTE_SERVER=root@demo.ks.kuali.org
+  SHUTDOWN_CMD=/usr/local/tomcat/bin/shutdown.sh
+  STARTUP_CMD=/usr/local/tomcat/bin/startup.sh
+  CLEANUP_CMD=/usr/local/tomcat/bin/cleanup.sh
 else
-  echo Error!!  The ENVIRONMENT must be either dev or staging
+  echo Error!!  The ENVIRONMENT must be either dev, staging, or demo
   echo ------------------------------------------------------------------------------------------
   echo Usage: deploy-application.sh 1.1 ks-1.1 dev /opt/hudson/home/jobs/1.1-full-build/workspace
   echo ------------------------------------------------------------------------------------------
   exit 0
 fi
 
-WAR_FILE=ks-embedded-$VERSION-SNAPSHOT.war
-LOCAL_WAR_FILE=$APP_WORKSPACE/$SVN_DIR/ks-web/ks-embedded/target/$WAR_FILE
-REMOTE_WAR_FILE=/usr/local/student/downloads/$WAR_FILE
-REMOTE_TOMCAT_DIR=/usr/local/student/embedded/$REMOTE_DIR
-LOCAL_MVN_DIR=$APP_WORKSPACE/$SVN_DIR/ks-cfg-dbs/ks-embedded-db
-DB_URL=jdbc:oracle:thin:@deploy.ks.kuali.org:1521:KS
-DBA_PASSWORD=gw570229
-REMOTE_SERVER=root@deploy.ks.kuali.org
-PEM_FILE=/home/tomcat/ks-key.pem
-
 # Copy the newly generated war file into the downloads directory
 scp -i $PEM_FILE $LOCAL_WAR_FILE $REMOTE_SERVER:$REMOTE_WAR_FILE
 # Stop Tomcat
-ssh -i $PEM_FILE $REMOTE_SERVER su - $REMOTE_USER -c /usr/local/tomcat_$REMOTE_USER/bin/shutdown.sh
+ssh -i $PEM_FILE $REMOTE_SERVER $SHUTDOWN_CMD
 # Remove the previous backup
 ssh -i $PEM_FILE $REMOTE_SERVER rm -rf $REMOTE_TOMCAT_DIR.bak
 # Remove the current by moving it to a directory with a .bak extension
@@ -80,6 +94,6 @@ cd $LOCAL_MVN_DIR
 mvn clean install -Pks-db,oracle -Dks.impex.username=$DB_SCHEMA -Dks.impex.password=$DB_SCHEMA -Dks.impex.dba.password=$DBA_PASSWORD -Dks.impex.url=$DB_URL
 
 # Perform some cleanup and restart Tomcat
-ssh -i $PEM_FILE $REMOTE_SERVER su - $REMOTE_USER -c /usr/local/tomcat_$REMOTE_USER/bin/cleanup.sh
-ssh -i $PEM_FILE $REMOTE_SERVER su - $REMOTE_USER -c /usr/local/tomcat_$REMOTE_USER/bin/startup.sh
+ssh -i $PEM_FILE $REMOTE_SERVER $SHUTDOWN_CMD
+ssh -i $PEM_FILE $REMOTE_SERVER $STARTUP_CMD
 
