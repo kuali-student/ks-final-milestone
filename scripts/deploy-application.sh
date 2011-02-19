44d6d15fb406b230fda4534e3f6777c30828c6cd
jcaddel <jcaddel@kuali.org>
2011-02-19 08:07:46 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@19865 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- scripts/deploy-application.sh
+++ scripts/deploy-application.sh
@@ -10,7 +10,6 @@ ENVIRONMENT=$3
 APP_WORKSPACE=$4
 WAR_FILE=ks-embedded-$VERSION-SNAPSHOT.war
 LOCAL_WAR_FILE=$APP_WORKSPACE/$SVN_DIR/ks-web/ks-embedded/target/$WAR_FILE
-REMOTE_WAR_FILE=/usr/local/student/downloads/$WAR_FILE
 LOCAL_MVN_DIR=$APP_WORKSPACE/$SVN_DIR/ks-cfg-dbs/ks-embedded-db
 DB_URL=jdbc:oracle:thin:@deploy.ks.kuali.org:1521:KS
 DBA_PASSWORD=gw570229
@@ -61,14 +60,14 @@ then
   STARTUP_CMD="su - $REMOTE_USER -c /usr/local/tomcat_$REMOTE_USER/bin/startup.sh"
 elif [ "$ENVIRONMENT" = "demo" ]
 then
-  DB_SCHEMA=KSDEMO
-  REMOTE_USER=demo
-  REMOTE_DIR=demo
-  DB_URL=jdbc:oracle:thin:@demo.ks.kuali.org:1521:KS
   REMOTE_SERVER=root@demo.ks.kuali.org
-  SHUTDOWN_CMD="/usr/local/tomcat/bin/shutdown.sh 60 -force"
-  STARTUP_CMD=/usr/local/tomcat/bin/startup.sh
-  CLEANUP_CMD=/usr/local/tomcat/bin/cleanup.sh
+  REMOTE_USER_HOME=/usr/local/tomcat
+  REMOTE_TOMCAT_HOME=/usr/local/tomcat
+  REMOTE_DB_SCHEMA=KSDEMO
+  REMOTE_DB_URL=jdbc:oracle:thin:@demo.ks.kuali.org:1521:KS
+  REMOTE_SHUTDOWN_CMD="$REMOTE_TOMCAT_HOME/bin/shutdown.sh 60 -force"
+  REMOTE_STARTUP_CMD=$REMOTE_TOMCAT_HOME/bin/startup.sh
+  REMOTE_CLEANUP_CMD=$REMOTE_TOMCAT_HOME/bin/cleanup.sh
 else
   echo Error!!  The ENVIRONMENT must be either dev, staging, or demo
   echo ------------------------------------------------------------------------------------------
@@ -77,27 +76,27 @@ else
   exit 0
 fi
 
-REMOTE_TOMCAT_DIR=/usr/local/student/embedded/$REMOTE_DIR
 
-# Copy the newly generated war file into the downloads directory
-scp -i $PEM_FILE $LOCAL_WAR_FILE $REMOTE_SERVER:$REMOTE_WAR_FILE
+REMOTE_WAR_DIR=$REMOTE_TOMCAT_HOME/webapps/ROOT
+REMOTE_WAR_FILE=$REMOTE_TOMCAT_HOME/webapps/ROOT.war
+
 # Stop Tomcat
-ssh -i $PEM_FILE $REMOTE_SERVER $SHUTDOWN_CMD
-# Remove the previous backup
-ssh -i $PEM_FILE $REMOTE_SERVER rm -rf $REMOTE_TOMCAT_DIR.bak
-# Remove the current by moving it to a directory with a .bak extension
-ssh -i $PEM_FILE $REMOTE_SERVER mv $REMOTE_TOMCAT_DIR $REMOTE_TOMCAT_DIR.bak
-# Unzip the new WAR into the right directory
-ssh -i $PEM_FILE $REMOTE_SERVER unzip $REMOTE_WAR_FILE -d $REMOTE_TOMCAT_DIR
+ssh -i $PEM_FILE $REMOTE_SERVER $REMOTE_SHUTDOWN_CMD
 
+# Remove the existing .war file and ROOT directory
+ssh -i $PEM_FILE $REMOTE_SERVER rm -rf $REMOTE_WAR_DIR
+ssh -i $PEM_FILE $REMOTE_SERVER rm $REMOTE_WAR_FILE
 
-# Use maven to reload the database using Impex
+# Copy the newly generated war file into the webapps directory
+scp -i $PEM_FILE $LOCAL_WAR_FILE $REMOTE_SERVER:$REMOTE_WAR_FILE
+
+# Drop/create the underlying database using Impex
 M2_HOME=/opt/java/apache-maven-3.0
 export M2_HOME
 cd $LOCAL_MVN_DIR
 mvn clean install -Pks-db,oracle -Dks.impex.username=$DB_SCHEMA -Dks.impex.password=$DB_SCHEMA -Dks.impex.dba.password=$DBA_PASSWORD -Dks.impex.url=$DB_URL
 
 # Perform some cleanup and restart Tomcat
-ssh -i $PEM_FILE $REMOTE_SERVER $CLEANUP_CMD
-ssh -i $PEM_FILE $REMOTE_SERVER $STARTUP_CMD
+ssh -i $PEM_FILE $REMOTE_SERVER $REMOTE_CLEANUP_CMD
+ssh -i $PEM_FILE $REMOTE_SERVER $REMOTE_STARTUP_CMD
 
