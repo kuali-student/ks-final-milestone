b322e3e788b26fd34e15683ef4b5ee21fc282c25
gtaylor <gtaylor@kuali.org>
2011-03-31 15:35:32 -0400

    KSLAB-1853-Moved export sql from initial-db to the upgrade area
    git-svn-id: https://svn.kuali.org/repos/student/trunk@20849 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-sql/src/main/resources/initial-db/messages/data/KSMG_MESSAGE.sql
+++ ks-core/ks-core-sql/src/main/resources/initial-db/messages/data/KSMG_MESSAGE.sql
@@ -1377,18 +1377,3 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,VER_NBR)
 INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,VER_NBR)
   VALUES ('course','fa63be61-f7e0-11df-98cf-0800200c9a66','en','cluStateChangeFailedNotification','Failed to change Course State',1)
 /
-INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,VER_NBR)
-  VALUES ('common','ae763fac-0714-4ecf-8252-cc3369dfb168','en','export','Export',1)
-/
-INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,VER_NBR)
-  VALUES ('common','cf0100e1-38a0-489d-a9dd-7616b01d16fc','en','exportTitle','Export Document',1)
-/
-INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,VER_NBR)
-  VALUES ('common','cb0acf7c-e3a2-45a4-9b67-011b3dbeb4d3','en','exportDialog','Select the document type to export as:',1)
-/
-INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,VER_NBR)
-  VALUES ('common','1ae92d4f-feb0-466f-96d3-a3c0003ebbe0','en','pdfFormat','.pdf (Adobe Acrobat)',1)
-/
-INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,VER_NBR)
-  VALUES ('common','3b0a7bfa-05d0-476c-8a0e-ec6d95b40dbc','en','docFormat','.doc (Microsoft Word)',1)
-/
