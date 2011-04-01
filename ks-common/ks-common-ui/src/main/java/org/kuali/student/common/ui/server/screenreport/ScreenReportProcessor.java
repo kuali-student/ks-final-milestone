aba6aa5a9b737b3149b80bf37bd5c35c65b70daf
sw <sw@kuali.org>
2011-04-01 09:51:23 -0400

    KSLAB-1902 Added report title to interface methods.
    git-svn-id: https://svn.kuali.org/repos/student/trunk@20860 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-ui/src/main/java/org/kuali/student/common/ui/server/screenreport/ScreenReportProcessor.java
+++ ks-common/ks-common-ui/src/main/java/org/kuali/student/common/ui/server/screenreport/ScreenReportProcessor.java
@@ -7,18 +7,18 @@ import org.kuali.student.common.ui.client.util.ExportElement;
 
 public interface ScreenReportProcessor {
 
-    public byte[] createPdf(Data source, String template);
+    public byte[] createPdf(Data source, String template, String reportTitle);
 
-    public byte[] createPdf(List<ExportElement> source, String template);
+    public byte[] createPdf(List<ExportElement> source, String template, String reportTitle);
 
-    public String createXml(Data source, String template);
+    public String createXml(Data source, String template, String reportTitle);
     
-    public byte[] createXls(Data source, String template);
+    public byte[] createXls(Data source, String template, String reportTitle);
 
-    public byte[] createXls(List<ExportElement> source, String template);
+    public byte[] createXls(List<ExportElement> source, String template, String reportTitle);
 
-    public byte[] createDoc(Data source, String template);
+    public byte[] createDoc(Data source, String template, String reportTitle);
 
-    public byte[] createDoc(List<ExportElement> source, String template);
+    public byte[] createDoc(List<ExportElement> source, String template, String reportTitle);
 
 }
