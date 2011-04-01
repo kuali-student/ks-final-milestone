07aeb765ffd2a9891d9e8186d546eb294ac725eb
cmann <cmann@kuali.org>
2011-04-01 09:46:21 -0400

    KSLAB-1479: CSS - Update the Course Formats Activity multiplicity to mirror other multiplicities (try 2: I don't think it likes semicolons)
    git-svn-id: https://svn.kuali.org/repos/student/trunk@20856 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-sql/src/main/resources/upgrades/03-R1.1.1 to R1.1.2/2011-03-30-lum-msg-bootstrap.sql
+++ ks-lum/ks-lum-sql/src/main/resources/upgrades/03-R1.1.1 to R1.1.2/2011-03-30-lum-msg-bootstrap.sql
@@ -1,19 +1,10 @@
--- Course logistics > Course formats > Activities > Duration Type (label) 
--- See KSLAB-1479 
 insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
-  values ('0f5badac-1fd9-4105-8f77-b258cce23408', 'cluCourseFormatsDurationType', 'en', 'course', 'Duration Type', '47F8296F-4AF6-44CD-AE4E-93ED817FAFFA', 1);
+  values ('0f5badac-1fd9-4105-8f77-b258cce23408', 'cluCourseFormatsDurationType', 'en', 'course', 'Duration Type', '47F8296F-4AF6-44CD-AE4E-93ED817FAFFA', 1)
 /
--- Course logistics > Course formats > Activities > Duration Type (help bubble) 
--- See KSLAB-1479  
 insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
-  values ('ffd033f7-c27c-4458-a478-d02ce3b5e636', 'cluCourseFormatsDurationType-help', 'en', 'course', 'First select the duration type (term, month, week, weekend, day) then select the count of the duration for the course. Most courses will have 1 or 2 terms, for example.', '0F2734CD-CD5D-F883-58EF-3F53C57DEE51', 1);
+  values ('ffd033f7-c27c-4458-a478-d02ce3b5e636', 'cluCourseFormatsDurationType-help', 'en', 'course', 'First select the duration type (term, month, week, weekend, day) then select the count of the duration for the course. Most courses will have 1 or 2 terms, for example.', '0F2734CD-CD5D-F883-58EF-3F53C57DEE51', 1)
 /
--- Course logistics > Duration (section header instruction text) 
--- See KSLAB-1479 
 update KSMG_MESSAGE set MSG_VALUE='First select the duration type (term, month, week, weekend, day) then select the count of the duration for the course. Most courses will have 1 or 2 terms, for example.' where MSG_ID='cluDurationLiteral-instruct'
 /
--- Course logistics > Duration > Duration (label) AND 
--- Course logistics > Course formats > Activities > Duration (label)
--- See KSLAB-1479  
 update KSMG_MESSAGE set MSG_VALUE='Duration' where MSG_ID='cluDurationQuantity'
 /
