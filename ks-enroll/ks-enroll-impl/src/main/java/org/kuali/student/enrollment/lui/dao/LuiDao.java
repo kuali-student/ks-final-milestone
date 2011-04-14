45ec025c1265263e3e428e10db9035c19d5a5bb6
ikuplevich <ikuplevich@kuali.org>
2011-04-14 18:54:34 -0400

    Added daos for lui.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@21354 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/enrollment/lui/dao/LuiDao.java
@@ -0,0 +1,9 @@
+package org.kuali.student.enrollment.lui.dao;
+
+import org.kuali.student.enrollment.dao.GenericEntityDao;
+
+/**
+ * @author Igor
+ */
+public class LuiDao extends GenericEntityDao {
+}
