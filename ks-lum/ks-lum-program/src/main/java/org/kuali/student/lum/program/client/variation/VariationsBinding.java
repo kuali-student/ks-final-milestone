ed6bd906d2a521814a13ba499646900b46020ed3
wgomes <wgomes@kuali.org>
2011-04-14 13:45:51 -0400

    KSLAB-1930 Change to allow display of validation warnings on specialization screen.
    git-svn-id: https://svn.kuali.org/repos/student/trunk@21336 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/variation/VariationsBinding.java
+++ ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/variation/VariationsBinding.java
@@ -1,19 +1,18 @@
 package org.kuali.student.lum.program.client.variation;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.FlexTable;
+import java.util.List;
 
 import org.kuali.student.common.assembly.data.Data;
+import org.kuali.student.common.ui.client.application.Application;
 import org.kuali.student.common.ui.client.application.ViewContext;
 import org.kuali.student.common.ui.client.configurable.mvc.binding.ModelWidgetBindingSupport;
 import org.kuali.student.common.ui.client.mvc.DataModel;
 import org.kuali.student.common.ui.client.mvc.history.HistoryManager;
 import org.kuali.student.common.ui.client.widgets.KSButton;
+import org.kuali.student.common.ui.client.widgets.KSLabel;
+import org.kuali.student.common.ui.client.widgets.field.layout.element.SpanPanel;
 import org.kuali.student.common.ui.shared.IdAttributes.IdType;
+import org.kuali.student.common.validation.dto.ValidationResultInfo;
 import org.kuali.student.lum.common.client.configuration.Configuration;
 import org.kuali.student.lum.program.client.ProgramConstants;
 import org.kuali.student.lum.program.client.ProgramRegistry;
@@ -21,6 +20,13 @@ import org.kuali.student.lum.program.client.ProgramUtils;
 import org.kuali.student.lum.program.client.events.UpdateEvent;
 import org.kuali.student.lum.program.client.major.MajorManager;
 import org.kuali.student.lum.program.client.properties.ProgramProperties;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.FlexTable;
 
 /**
  * @author Igor
@@ -49,14 +55,15 @@ public class VariationsBinding extends ModelWidgetBindingSupport<FlexTable> {
     }
 
     @Override
-    public void setWidgetValue(final FlexTable table, final DataModel model, String path) {
-        table.clear();
-        final Data variationMap = model.get(path);
+    public void setWidgetValue(final FlexTable table, final DataModel model, String path) {    	   
+    	table.clear();
+        final Data variationMap = model.get(path);        
         if (variationMap != null) {
             int row = 0;
             for (final Data.Property property : variationMap) {
                 final Data variationData = property.getValue();
                 final int currentRow = row;
+                       
                 Anchor anchor = new Anchor(getVariationName(variationData));
                 anchor.addClickHandler(new ClickHandler() {
                     @Override
@@ -71,6 +78,7 @@ public class VariationsBinding extends ModelWidgetBindingSupport<FlexTable> {
                         HistoryManager.navigate(url, viewContext);
                     }
                 });
+
                 table.setWidget(row, 0, anchor);
                 if (editable) {
                     KSButton removeButton = new KSButton(ProgramProperties.get().common_remove());
@@ -90,6 +98,19 @@ public class VariationsBinding extends ModelWidgetBindingSupport<FlexTable> {
                     }
 
                 }
+
+                //Set warning message if specialization has warnings
+                if (hasWarnings(currentRow)){
+                    table.getRowFormatter().addStyleName(row, "warning");
+                    SpanPanel warning = new SpanPanel();
+                    warning.setHTML("<b> Warning </b> <br/>" + 
+                    		Application.getApplicationContext().getMessage("validation", "validation.variation"));
+                    table.setWidget(row, 2, warning);
+                } else {
+                	table.getRowFormatter().removeStyleName(row, "warning");
+                    table.setWidget(row, 2, new KSLabel(""));                	
+                }
+                
                 row++;
             }
         }
@@ -100,5 +121,22 @@ public class VariationsBinding extends ModelWidgetBindingSupport<FlexTable> {
         nameBuilder.append(data.<Object>get(ProgramConstants.LONG_TITLE));
         nameBuilder.append(" (").append(data.<Object>get(ProgramConstants.CODE)).append(")");
         return nameBuilder.toString();
+    }
+    
+    /**
+     * This checks the validation warnings stored in the app context to see if any warnings exist
+     * on the specialization.
+     * 
+     * @param variationIndex
+     * @return true of variation at variationIndex has any warnings.
+     */
+    private boolean hasWarnings(int variationIndex){
+    	List<ValidationResultInfo> validationWarnings = Application.getApplicationContext().getValidationWarnings();
+    	for (ValidationResultInfo vr:validationWarnings){
+    		if (vr.getElement().contains(ProgramConstants.VARIATIONS + "/" + variationIndex)){
+    	    	return true;
+    		}
+    	}
+    	return false;
     }
 }
