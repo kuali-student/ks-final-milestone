2ba5dce5de392563b22725eca6edf7a917b115b9
depstein <depstein@kuali.org>
2011-04-15 13:50:35 -0400

    Added caching back in but with handling for time/size invalidation and garbage collection(soft reference)
    git-svn-id: https://svn.kuali.org/repos/student/trunk@21434 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-ui/src/main/java/org/kuali/student/common/ui/server/gwt/SearchDispatchRpcGwtServlet.java
+++ ks-common/ks-common-ui/src/main/java/org/kuali/student/common/ui/server/gwt/SearchDispatchRpcGwtServlet.java
@@ -15,18 +15,27 @@
 
 package org.kuali.student.common.ui.server.gwt;
 
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import java.lang.ref.SoftReference;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.kuali.student.common.assembly.transform.IdTranslatorFilter;
 import org.kuali.student.common.exceptions.MissingParameterException;
-import org.kuali.student.common.search.dto.*;
+import org.kuali.student.common.search.dto.SearchParam;
+import org.kuali.student.common.search.dto.SearchRequest;
+import org.kuali.student.common.search.dto.SearchResult;
+import org.kuali.student.common.search.dto.SearchResultCell;
+import org.kuali.student.common.search.dto.SearchResultRow;
 import org.kuali.student.common.search.service.SearchDispatcher;
 import org.kuali.student.common.ui.client.service.SearchRpcService;
+import org.springframework.beans.factory.InitializingBean;
 
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
-public class SearchDispatchRpcGwtServlet extends RemoteServiceServlet implements SearchRpcService {
+public class SearchDispatchRpcGwtServlet extends RemoteServiceServlet implements SearchRpcService, InitializingBean {
 
     private static final long serialVersionUID = 1L;
 
@@ -34,8 +43,12 @@ public class SearchDispatchRpcGwtServlet extends RemoteServiceServlet implements
 
     private SearchDispatcher searchDispatcher;
 
-    private ConcurrentHashMap<String, SearchResult> cache = new ConcurrentHashMap<String, SearchResult>();//FIXME this cache has no timeout!
-
+    protected boolean cachingEnabled = false;
+	protected int searchCacheMaxSize = 20;
+	protected int searchCacheMaxAgeSeconds = 90;
+	protected Map<String,MaxAgeSoftReference<SearchResult>> searchCache;
+	
+    
     public SearchDispatchRpcGwtServlet() {
         super();
     }
@@ -53,9 +66,8 @@ public class SearchDispatchRpcGwtServlet extends RemoteServiceServlet implements
         List<SearchParam> params  = searchRequest.getParams();
         if(params != null && params.size() > 0){
             SearchParam firstParam = params.get(0);
-            if(firstParam.getKey().equals("lu.queryParam.cluVersionIndId")){
+            if(firstParam.getKey().equals("lu.queryParam.cluVersionIndId")){//FIXME can this special case be handled after this call?
                 doIdTranslation(searchResult);
-
             }
         }
         return searchResult;
@@ -63,12 +75,27 @@ public class SearchDispatchRpcGwtServlet extends RemoteServiceServlet implements
 
     @Override
     public SearchResult cachingSearch(SearchRequest searchRequest) {
-//    	String cacheKey = searchRequest.toString(); 
-//        if(cache.containsKey(cacheKey)){
-//            return cache.get(cacheKey);
-//        }
-        SearchResult searchResult = search(searchRequest);
-//        cache.putIfAbsent(cacheKey, searchResult);
+    	String cacheKey = searchRequest.toString();
+    	if(cachingEnabled){
+    		
+    		//Get From Cache
+    		MaxAgeSoftReference<SearchResult> ref = searchCache.get(cacheKey);
+    		if ( ref != null ) {
+    			SearchResult cachedSearchResult = ref.get();
+    			if(cachedSearchResult!=null){
+    				return cachedSearchResult;
+    			}
+    		}
+		}
+
+    	//Perform the actual Search
+    	SearchResult searchResult = search(searchRequest);
+        
+    	if(cachingEnabled){
+    		//Store to cache
+    		searchCache.put(cacheKey, new MaxAgeSoftReference<SearchResult>( searchCacheMaxAgeSeconds, searchResult) );
+    	}
+    	
         return searchResult;
     }
 
@@ -86,6 +113,13 @@ public class SearchDispatchRpcGwtServlet extends RemoteServiceServlet implements
         }
     }
 
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if(cachingEnabled){
+			searchCache = Collections.synchronizedMap( new MaxSizeMap<String,MaxAgeSoftReference<SearchResult>>( searchCacheMaxSize ) );
+		}
+	}
+    
     public void setSearchDispatcher(SearchDispatcher searchDispatcher) {
         this.searchDispatcher = searchDispatcher;
     }
@@ -93,4 +127,76 @@ public class SearchDispatchRpcGwtServlet extends RemoteServiceServlet implements
     public void setIdTranslatorFilter(IdTranslatorFilter idTranslatorFilter) {
         this.idTranslatorFilter = idTranslatorFilter;
     }
+
+	public void setCachingEnabled(boolean cachingEnabled) {
+		this.cachingEnabled = cachingEnabled;
+	}
+
+	public void setSearchCacheMaxSize(int searchCacheMaxSize) {
+		this.searchCacheMaxSize = searchCacheMaxSize;
+	}
+
+	public void setSearchCacheMaxAgeSeconds(int searchCacheMaxAgeSeconds) {
+		this.searchCacheMaxAgeSeconds = searchCacheMaxAgeSeconds;
+	}
+	
+	
+	//Added as inner classes to avoid huge dependency on rice-impl
+	/**
+	 * An extension to SoftReference that stores an expiration time for the 
+	 * value stored in the SoftReference. If no expiration time is passed in
+	 * the value will never be cached.  
+	 */
+	public class MaxAgeSoftReference<T> extends SoftReference<T> {
+		
+		private long expires;
+
+		public MaxAgeSoftReference(long expires, T referent) {
+			super(referent);
+			this.expires = System.currentTimeMillis() + expires * 1000;
+		}
+		
+		public boolean isValid() {
+			return System.currentTimeMillis() < expires;
+		}
+		
+		public T get() {			
+			return isValid() ? super.get() : null;
+		}		
+		
+	}
+	/**
+	 * This class acts like an LRU cache, automatically purging contents when it gets above a certain size. 
+	 * 
+	 * @author Kuali Rice Team (rice.collab@kuali.org)
+	 */
+	public class MaxSizeMap<K,V> extends LinkedHashMap<K,V> {
+		private static final long serialVersionUID = -5354227348838839919L;
+
+		private int maxSize;
+		
+		/**
+		 * @param maxSize
+		 */
+		public MaxSizeMap( int maxSize  ) {
+			this( maxSize, false );
+		}
+		/**
+		 * @param maxSize
+		 * @param accessOrder Whether to sort in the order accessed rather than the order inserted.
+		 */
+		public MaxSizeMap( int maxSize, boolean accessOrder ) {
+			super( maxSize / 2, 0.75f, accessOrder );
+			this.maxSize = maxSize;
+		}
+		
+		/**
+		 * @see java.util.LinkedHashMap#removeEldestEntry(java.util.Map.Entry)
+		 */
+		@Override
+		protected boolean removeEldestEntry(Entry<K,V> eldest) {
+			return size() > maxSize;
+		}
+		
+	}
 }
