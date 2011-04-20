d86cdf161668d4837f2511cdbc0abcaf1ad90556
gtaylor <gtaylor@kuali.org>
2011-04-20 16:51:50 -0400

    KSLAB-1936: updated sql to use existing users.
    git-svn-id: https://svn.kuali.org/repos/student/trunk@21559 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-core/ks-core-sql/src/main/resources/upgrades/02-R1.1.0 to R1.2.0/2011-04-20-core-KSOR-org-relation-bootstrap.sql
@@ -0,0 +1,6 @@
+update KSOR_ORG_PERS_RELTN set PERS_ID = 'testuser1' where PERS_ID = 'KIM-1'
+/
+update KSOR_ORG_PERS_RELTN set PERS_ID = 'testuser3' where PERS_ID = 'KIM-3'
+/
+update KSOR_ORG_PERS_RELTN set PERS_ID = 'testuser4' where PERS_ID = 'KIM-4'
+/
