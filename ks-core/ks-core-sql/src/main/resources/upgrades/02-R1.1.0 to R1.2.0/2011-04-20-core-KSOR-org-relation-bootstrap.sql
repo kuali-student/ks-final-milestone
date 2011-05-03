7023835de514f4d222979c6d934809139c0b0701
jacobusroos <jacobusroos@kuali.org>
2011-05-03 03:42:44 -0400

    KSLAB-1941: Some organisations have people assigned to them with the wrong person_relation_type. Meaning that such organisation do not have possitions opened for such individuals. The script rectify this for 2 organisations by assigning the correct person_Relation_types to the persons.
    git-svn-id: https://svn.kuali.org/repos/student/trunk@21694 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-sql/src/main/resources/upgrades/02-R1.1.0 to R1.2.0/2011-04-20-core-KSOR-org-relation-bootstrap.sql
+++ ks-core/ks-core-sql/src/main/resources/upgrades/02-R1.1.0 to R1.2.0/2011-04-20-core-KSOR-org-relation-bootstrap.sql
@@ -4,3 +4,11 @@ update KSOR_ORG_PERS_RELTN set PERS_ID = 'testuser3' where PERS_ID = 'KIM-3'
 /
 update KSOR_ORG_PERS_RELTN set PERS_ID = 'testuser4' where PERS_ID = 'KIM-4'
 /
+update KSOR_ORG_PERS_RELTN set ORG_PERS_RELTN_TYPE = 'kuali.org.PersonRelation.Chair' where ID = '3'  -- He was of the type kuali.org.PersonRelation.Coordinator
+/
+update KSOR_ORG_PERS_RELTN set ORG_PERS_RELTN_TYPE = 'kuali.org.PersonRelation.Chair' where ID = '4'  -- He was of the type kuali.org.PersonRelation.Professor
+/
+update KSOR_ORG_PERS_RELTN set ORG_PERS_RELTN_TYPE = 'kuali.org.PersonRelation.Chair' where ID = '6'  -- He was of the type kuali.org.PersonRelation.Professor
+/
+update KSOR_ORG_PERS_RELTN set ORG_PERS_RELTN_TYPE = 'kuali.org.PersonRelation.Chair', PERS_ID = 'testuser2'  where ID = '1'  -- He was of the type kuali.org.PersonRelation.Head
+/
