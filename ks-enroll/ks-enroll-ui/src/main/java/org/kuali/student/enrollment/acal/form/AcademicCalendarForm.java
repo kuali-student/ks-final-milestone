1b05d286a3b7be2beff4715dac4ecbaeaffa09ef
huangb <huangb@kuali.org>
2011-05-05 16:21:46 -0400

    KSENROLL-70: Initial version
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@21726 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/acal/form/AcademicCalendarForm.java
@@ -0,0 +1,39 @@
+/* Copyright 2011 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.kuali.student.enrollment.acal.form;
+
+import org.kuali.rice.kns.web.spring.form.UifFormBase;
+
+import org.kuali.student.enrollment.classII.acal.dto.AcademicCalendarInfo;
+
+
+public class AcademicCalendarForm extends UifFormBase {
+    private static final long serialVersionUID = 4898118410378641665L;
+
+    private AcademicCalendarInfo academicCalendarInfo;
+
+    public AcademicCalendarForm() {
+        super();
+    }
+
+    public AcademicCalendarInfo getAcademicCalendarInfo() {
+        return this.academicCalendarInfo;
+    }
+
+    public void setAcademicCalendarInfo(AcademicCalendarInfo academicCalendarInfo) {
+        this.academicCalendarInfo = academicCalendarInfo;
+    }
+
+}
