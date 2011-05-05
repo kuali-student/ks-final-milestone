bc4550e46a76f77a687b1b69dbe0dc44783b0600
lpan <lpan@kuali.org>
2011-05-05 18:12:50 -0400

    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@21732 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/classI/atp/dao/AtpDao.java
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/classI/atp/dao/AtpDao.java
@@ -1,7 +1,8 @@
 package org.kuali.student.r2.core.classI.atp.dao;
 
 import org.kuali.student.enrollment.dao.GenericEntityDao;
+import org.kuali.student.r2.core.classI.atp.model.AtpEntity;
 
-public class AtpDao extends GenericEntityDao{
+public class AtpDao extends GenericEntityDao<AtpEntity>{
 
 }
