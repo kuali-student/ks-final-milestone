b2d59eedd930df469052ffa23636b08ffb0a234b
lpan <lpan@kuali.org>
2011-05-06 17:48:10 -0400

    add atpservicevalidationDecorator config & func validateAtp
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@21752 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/classI/atp/service/decorators/AtpServiceValidationDecorator.java
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/classI/atp/service/decorators/AtpServiceValidationDecorator.java
@@ -1,7 +1,17 @@
 package org.kuali.student.r2.core.classI.atp.service.decorators;
 
+import java.util.List;
+
 import org.kuali.student.r2.common.datadictionary.DataDictionaryValidator;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.dto.ValidationResultInfo;
+import org.kuali.student.r2.common.exceptions.DoesNotExistException;
+import org.kuali.student.r2.common.exceptions.InvalidParameterException;
+import org.kuali.student.r2.common.exceptions.MissingParameterException;
+import org.kuali.student.r2.common.exceptions.OperationFailedException;
+import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
 import org.kuali.student.r2.common.infc.HoldsValidator;
+import org.kuali.student.r2.core.classI.atp.dto.AtpInfo;
 import org.kuali.student.r2.core.classI.atp.service.AtpServiceDecorator;
 
 public class AtpServiceValidationDecorator extends AtpServiceDecorator  implements HoldsValidator{
@@ -15,6 +25,26 @@ public class AtpServiceValidationDecorator extends AtpServiceDecorator  implemen
     @Override
     public void setValidator(DataDictionaryValidator validator) {
         this.validator = validator;
+    }
+
+    @Override
+    public List<ValidationResultInfo> validateAtp(
+            String validationType,
+            AtpInfo atpInfo,
+            ContextInfo context)
+            throws DoesNotExistException,
+            InvalidParameterException,
+            MissingParameterException,
+            OperationFailedException {
+        
+        try{
+            this.validator.validate(DataDictionaryValidator.ValidationType.fromString(validationType), atpInfo, context);
+        }catch(PermissionDeniedException ex){
+            throw new OperationFailedException();
+        }
+        
+        return super.validateAtp(validationType, atpInfo, context);
+
     }
 
 }
