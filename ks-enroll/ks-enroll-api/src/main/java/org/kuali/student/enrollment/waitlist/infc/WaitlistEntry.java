80adad83cd2851e2e2b5de32d7ea1907441c2141
tcoppeto <tcoppeto@kuali.org>
2011-05-11 12:02:55 -0400

    start of Waitlist service
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@21793 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/enrollment/waitlist/infc/WaitlistEntry.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2010 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package org.kuali.student.enrollment.waitlist.infc;
+
+import java.util.Date;
+import org.kuali.student.r2.common.infc.IdEntity;
+
+
+/**
+ * Information about a Waitlist Entry.
+ *
+ * @Author tom
+ * @Since Tue May 10 14:22:34 EDT 2011
+ */ 
+
+public interface WaitlistEntry extends IdEntity {
+
+    /**
+     * Name: Waitlist Id
+     * The Id of the Waitlist.
+     */
+    public String getWaitlistId();
+
+    /**
+     * Name: Student Id
+     * The Id of Student (Person) waitlisted.
+     */
+    public String getStudentId();
+
+    /**
+     * Name: Date
+     * The time this entry was created.
+     */
+    public Date getDate();
+
+    /**
+     * Name: Position
+     * The position of this entry.
+     */
+    public Integer getPosition();
+}
