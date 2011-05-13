781d5654fe56c24cd60b174df1535142b9b87004
lpan <lpan@kuali.org>
2011-05-13 14:20:33 -0400

    modified AtpAssembler
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@21827 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/common/infc/AtpAssembler.java
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/common/infc/AtpAssembler.java
@@ -1,6 +1,8 @@
 package org.kuali.student.r2.common.infc;
 
+import org.kuali.student.r2.common.dto.ContextInfo;
+
 public interface AtpAssembler<E, T> {
-    public E assemble(T baseDTO, E businessDTO);
-    public T disassemble(T baseDTO, E businessDTO);
+    public E assemble(T baseDTO, ContextInfo context);
+    public T disassemble(E businessDTO, ContextInfo context);
 }
