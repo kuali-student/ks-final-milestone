ddde7624ef5d8be09f897c0d4f5b2ee5515321e5
sw <sw@kuali.org>
2011-05-16 09:30:59 -0400

    KSLAB-1899 - Add new Analysis report template for dependency analysis type views.
    git-svn-id: https://svn.kuali.org/repos/student/trunk@21852 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/dependency/client/controllers/DependencyAnalysisController.java
+++ ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/dependency/client/controllers/DependencyAnalysisController.java
@@ -1,49 +1,72 @@
 package org.kuali.student.lum.lu.ui.dependency.client.controllers;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import org.kuali.student.common.ui.client.configurable.mvc.layouts.BasicLayout;
 import org.kuali.student.common.ui.client.mvc.DataModel;
 import org.kuali.student.common.ui.client.util.ExportElement;
-import org.kuali.student.common.ui.client.util.ExportUtils;
 import org.kuali.student.lum.lu.ui.dependency.client.views.DependencyAnalysisView;
 
-public class DependencyAnalysisController extends BasicLayout{
+public class DependencyAnalysisController extends BasicLayout {
 
-	public enum DependencyViews{MAIN};
-    	    
+    public enum DependencyViews {
+        MAIN
+    };
+
     public DependencyAnalysisController(String controllerId) {
-		super(controllerId);		
-		addView(new DependencyAnalysisView(this));
-		setDefaultView(DependencyViews.MAIN);				
-	}
-    
-    public void showExport(boolean show) {
-    	DependencyAnalysisView dependancyView = (DependencyAnalysisView) viewMap.get(DependencyViews.MAIN);
-			if (dependancyView.getHeader() != null) {
-				dependancyView.getHeader().showExport(isExportButtonActive());
-				
-			}
+        super(controllerId);
+        addView(new DependencyAnalysisView(this));
+        setDefaultView(DependencyViews.MAIN);
     }
-    
+
+    public void showExport(boolean show) {
+        DependencyAnalysisView dependancyView = (DependencyAnalysisView) viewMap.get(DependencyViews.MAIN);
+        if (dependancyView.getHeader() != null) {
+            dependancyView.getHeader().showExport(isExportButtonActive());
+
+        }
+    }
+
     @Override
     public boolean isExportButtonActive() {
-            return true;            
+        return true;
     }
 
-	@Override
-	public DataModel getExportDataModel() {
-		// TODO Auto-generated method stub
-		return super.getExportDataModel();
-	}
+    @Override
+    public DataModel getExportDataModel() {
+        // TODO Auto-generated method stub
+        return super.getExportDataModel();
+    }
 
-	@Override
-	public ArrayList<ExportElement> getExportElementsFromView() {
-		DependencyAnalysisView view = (DependencyAnalysisView) this.getCurrentView();
-		ArrayList<ExportElement> exportElements = new ArrayList<ExportElement>(); 
-		exportElements = view.getDepResultPanel().getExportElementsWidget(view.getName(), view.getName());
-		return exportElements;
-	}  
+    @Override
+    public List<ExportElement> getExportElementsFromView() {
+        DependencyAnalysisView view = (DependencyAnalysisView) this.getCurrentView();
+        ExportElement element = new ExportElement();
+        element.setViewName(view.getName());
+        element.setSectionName("");
+        return view.getDepResultPanel().getExportElementSubset(element);
+    }
 
-    	
+    /**
+     * @see org.kuali.student.common.ui.client.reporting.ReportExport#getExportTemplateName()
+     */
+    @Override
+    public String getExportTemplateName() {
+        return "analysis.template";
+    }
+
+    /**
+     * This overridden method return the view name when the controller name is null.
+     * 
+     * @see org.kuali.student.common.ui.client.configurable.mvc.LayoutController#getName()
+     */
+    @Override
+    public String getName() {
+        String name = super.getName();
+        if ((name == null) && (this.getCurrentView() != null)) {
+            name = this.getCurrentView().getName();
+        }
+        return name;
+    }
+
 }
