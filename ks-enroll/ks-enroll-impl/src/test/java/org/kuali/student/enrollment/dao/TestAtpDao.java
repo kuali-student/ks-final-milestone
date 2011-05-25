bae1458dfe697d62ba8183a99b804e083d57efe6
jim7 <jim7@kuali.org>
2011-05-25 14:41:21 -0400

    Test delete of atp
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@21981 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/enrollment/dao/TestAtpDao.java
+++ ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/enrollment/dao/TestAtpDao.java
@@ -2,8 +2,8 @@ package org.kuali.student.enrollment.dao;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.kuali.student.common.test.spring.AbstractTransactionalDaoTest;
 import org.kuali.student.common.test.spring.Dao;
@@ -13,7 +13,7 @@ import org.kuali.student.r2.core.class1.atp.model.AtpEntity;
 import org.kuali.student.r2.core.class1.atp.model.AtpRichTextEntity;
 
 @PersistenceFileLocation("classpath:META-INF/persistence_jta.xml")
-public class TestAtpDao extends AbstractTransactionalDaoTest{
+public class TestAtpDao extends AbstractTransactionalDaoTest {
     @Dao(value = "org.kuali.student.r2.core.class1.atp.dao.AtpDao", testSqlFile = "classpath:ks-atp.sql")
     private AtpDao dao;
     
@@ -28,13 +28,27 @@ public class TestAtpDao extends AbstractTransactionalDaoTest{
     @Test
     public void testCreateAtp() 
     {
+        AtpEntity existingEntity = dao.find("testAtpId1");
+        
         AtpEntity atp = new AtpEntity();
         atp.setName("atpTest");
         atp.setDescr(new AtpRichTextEntity("plain", "formatted"));
+        atp.setAtpState(existingEntity.getAtpState());
+        atp.setAtpType(existingEntity.getAtpType());
         dao.persist(atp);
         assertNotNull(atp.getId());
         AtpEntity atp2 = dao.find(atp.getId());
         assertEquals("atpTest", atp2.getName());         
         assertEquals("plain", atp2.getDescr().getPlain());   
+    }
+    
+    @Test
+    public void testDeleteAtp() 
+    {
+        AtpEntity atp = dao.find("testAtpId2");
+        assertNotNull(atp);
+        dao.remove(atp);
+        atp = dao.find("testAtpId2");
+        assertNull(atp);
     }
 }
