cbc7cba2965d60c2b4e4c1b6024581d6778b48e8
sambitpa <sambitpa@kuali.org>
2011-05-27 12:45:51 -0400

    KSPROJPLAN-500 Grading decorator
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@22023 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/enrollment/grading/service/GradingServiceDecorator.java
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/enrollment/grading/service/GradingServiceDecorator.java
@@ -6,6 +6,7 @@ import org.kuali.student.enrollment.grading.dto.AssignedGradeInfo;
 import org.kuali.student.enrollment.grading.dto.CreditsEarnedInfo;
 import org.kuali.student.enrollment.grading.dto.GradeRosterEntryInfo;
 import org.kuali.student.enrollment.grading.dto.GradeRosterInfo;
+import org.kuali.student.enrollment.lpr.service.LuiPersonRelationService;
 import org.kuali.student.r2.common.datadictionary.dto.DictionaryEntryInfo;
 import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.dto.StatusInfo;
@@ -23,176 +24,170 @@ import org.kuali.student.r2.lum.lrc.dto.ResultValueGroupInfo;
 
 public abstract class GradingServiceDecorator implements GradingService {
 
+
+    protected GradingServiceDecorator nextDecorator;
+            
+    public GradingServiceDecorator getNextDecorator() {
+        return nextDecorator;
+    }
+   
+    public void setNextDecorator(GradingServiceDecorator nextDecorator) {
+        this.nextDecorator = nextDecorator;
+    }
+
     @Override
     public List<String> getDataDictionaryEntryKeys(ContextInfo context) throws OperationFailedException,
             MissingParameterException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+      
+        return nextDecorator.getDataDictionaryEntryKeys(context);
     }
 
     @Override
     public DictionaryEntryInfo getDataDictionaryEntry(String entryKey, ContextInfo context)
             throws OperationFailedException, MissingParameterException, PermissionDeniedException,
             DoesNotExistException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+      
+        return getNextDecorator().getDataDictionaryEntry(entryKey, context);
     }
 
     @Override
     public TypeInfo getGradeRosterType(String gradeRosterTypeKey, ContextInfo context) throws DoesNotExistException,
             InvalidParameterException, MissingParameterException, OperationFailedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().getGradeRosterType(gradeRosterTypeKey, context);
     }
 
     @Override
     public GradeRosterInfo getGradeRoster(String gradeRosterId, ContextInfo context) throws DoesNotExistException,
             InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().getGradeRoster(gradeRosterId, context);
     }
 
     @Override
     public List<GradeRosterInfo> getGradeRostersByGraderAndTerm(String graderId, String termKey, ContextInfo context)
             throws DoesNotExistException, InvalidParameterException, MissingParameterException,
             OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().getGradeRostersByGraderAndTerm(graderId, termKey, context);
     }
 
     @Override
     public List<GradeRosterInfo> getFinalGradeRostersForCourseOffering(String courseOfferingId, ContextInfo context)
             throws DoesNotExistException, InvalidParameterException, MissingParameterException,
             OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().getGradeRostersForActivityOffering(courseOfferingId, context);
     }
 
     @Override
     public List<GradeRosterInfo> getFinalGradeRostersForActivityOffering(String activityOfferingId, ContextInfo context)
             throws DoesNotExistException, InvalidParameterException, MissingParameterException,
             OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().getFinalGradeRostersForActivityOffering(activityOfferingId, context);
     }
 
     @Override
     public List<GradeRosterInfo> getGradeRostersForActivityOffering(String activityOfferingId, ContextInfo context)
             throws DoesNotExistException, InvalidParameterException, MissingParameterException,
             OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+       return getNextDecorator().getGradeRostersForActivityOffering(activityOfferingId, context);
     }
 
     @Override
     public GradeRosterInfo buildInterimGradeRosterByType(List<String> activityOfferingIdList, String rosterTypeKey,
             ContextInfo context) throws AlreadyExistsException, InvalidParameterException, MissingParameterException,
             OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().buildInterimGradeRosterByType(activityOfferingIdList, rosterTypeKey, context);
     }
 
     @Override
     public GradeRosterInfo updateInterimGradeRoster(GradeRosterInfo gradeRoster, ContextInfo context)
             throws DataValidationErrorException, DoesNotExistException, InvalidParameterException,
             MissingParameterException, OperationFailedException, PermissionDeniedException, VersionMismatchException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().updateInterimGradeRoster(gradeRoster, context);
     }
 
     @Override
     public StatusInfo deleteInterimGradeRoster(String gradeRosterId, ContextInfo context) throws DoesNotExistException,
             InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().deleteInterimGradeRoster(gradeRosterId, context);
     }
 
     @Override
     public GradeRosterInfo updateFinalGradeRosterState(String gradeRosterId, String stateKey, ContextInfo context)
             throws DataValidationErrorException, DoesNotExistException, InvalidParameterException,
             MissingParameterException, OperationFailedException, PermissionDeniedException, VersionMismatchException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().updateFinalGradeRosterState(gradeRosterId, stateKey, context);
+        
     }
 
     @Override
     public List<ValidationResultInfo> validateGradeRoster(GradeRosterInfo gradeRoster, ContextInfo context)
             throws DoesNotExistException, InvalidParameterException, MissingParameterException,
             OperationFailedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().validateGradeRoster(gradeRoster, context);
     }
 
     @Override
     public GradeRosterEntryInfo getGradeRosterEntry(String gradeRosterEntryId, ContextInfo context)
             throws DoesNotExistException, InvalidParameterException, MissingParameterException,
             OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().getGradeRosterEntry(gradeRosterEntryId, context);
     }
 
     @Override
     public List<GradeRosterEntryInfo> getGradeRosterEntriesByIdList(List<String> gradeRosterEntryIdList,
             ContextInfo context) throws DoesNotExistException, InvalidParameterException, MissingParameterException,
             OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().getGradeRosterEntriesByIdList(gradeRosterEntryIdList, context);
     }
 
     @Override
     public List<GradeRosterEntryInfo> getGradeRosterEntriesByRosterId(String gradeRosterId, ContextInfo context)
             throws DoesNotExistException, InvalidParameterException, MissingParameterException,
             OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().getGradeRosterEntriesByRosterId(gradeRosterId, context);
     }
 
     @Override
     public List<ResultValueGroupInfo> getValidGradesForStudentByRoster(String studentId, String rosterId,
             ContextInfo context) throws DoesNotExistException, InvalidParameterException, MissingParameterException,
             OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().getValidGradesForStudentByRoster(studentId, rosterId, context);
     }
 
     @Override
     public GradeRosterEntryInfo getFinalGradeForStudentInCourseOffering(String studentId, String courseOfferingId,
             ContextInfo context) throws DoesNotExistException, InvalidParameterException, MissingParameterException,
             OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().getFinalGradeForStudentInCourseOffering(studentId, courseOfferingId, context);
     }
 
     @Override
     public GradeRosterEntryInfo addEntrytoInterimRoster(GradeRosterEntryInfo gradeRosterEntry, String gradeRosterId,
             ContextInfo context) throws AlreadyExistsException, InvalidParameterException, MissingParameterException,
             OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        
+        return getNextDecorator().addEntrytoInterimRoster(gradeRosterEntry, gradeRosterId, context);
     }
 
     @Override
     public StatusInfo removeEntryFromInterimRoster(String gradeRosterEntryId, String gradeRosterId, ContextInfo context)
             throws DoesNotExistException, InvalidParameterException, MissingParameterException,
             OperationFailedException, PermissionDeniedException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().removeEntryFromInterimRoster(gradeRosterEntryId, gradeRosterId, context);
     }
 
     @Override
     public AssignedGradeInfo updateAssignedGrade(String gradeRosterEntryId, AssignedGradeInfo assignedGrade,
             ContextInfo context) throws DataValidationErrorException, DoesNotExistException, InvalidParameterException,
             MissingParameterException, OperationFailedException, PermissionDeniedException, VersionMismatchException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        
+        return getNextDecorator().updateAssignedGrade(gradeRosterEntryId, assignedGrade, context);
     }
 
     @Override
     public CreditsEarnedInfo updateCredit(String gradeRosterEntryId, CreditsEarnedInfo assignedGrade,
             ContextInfo context) throws DataValidationErrorException, DoesNotExistException, InvalidParameterException,
             MissingParameterException, OperationFailedException, PermissionDeniedException, VersionMismatchException {
-        // TODO sambit - THIS METHOD NEEDS JAVADOCS
-        return null;
+        return getNextDecorator().updateCredit(gradeRosterEntryId, assignedGrade, context);
     }
 
 }
