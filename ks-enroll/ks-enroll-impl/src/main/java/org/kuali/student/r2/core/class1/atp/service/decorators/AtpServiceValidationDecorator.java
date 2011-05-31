06630b9d8117364b1c3b2162489ce0b587606461
lpan <lpan@kuali.org>
2011-05-30 21:16:20 -0400

    add cu in atpvalidationDecorator
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@22044 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/class1/atp/service/decorators/AtpServiceValidationDecorator.java
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/class1/atp/service/decorators/AtpServiceValidationDecorator.java
@@ -2,21 +2,27 @@ package org.kuali.student.r2.core.class1.atp.service.decorators;
 
 import java.util.List;
 
+import org.kuali.student.enrollment.acal.dto.AcademicCalendarInfo;
 import org.kuali.student.r2.common.datadictionary.DataDictionaryValidator;
+import org.kuali.student.r2.common.datadictionary.service.DataDictionaryService;
 import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.dto.ValidationResultInfo;
+import org.kuali.student.r2.common.exceptions.AlreadyExistsException;
+import org.kuali.student.r2.common.exceptions.DataValidationErrorException;
 import org.kuali.student.r2.common.exceptions.DoesNotExistException;
 import org.kuali.student.r2.common.exceptions.InvalidParameterException;
 import org.kuali.student.r2.common.exceptions.MissingParameterException;
 import org.kuali.student.r2.common.exceptions.OperationFailedException;
 import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
+import org.kuali.student.r2.common.exceptions.VersionMismatchException;
+import org.kuali.student.r2.common.infc.HoldsDataDictionaryService;
 import org.kuali.student.r2.common.infc.HoldsValidator;
 import org.kuali.student.r2.core.atp.dto.AtpInfo;
 import org.kuali.student.r2.core.atp.dto.AtpMilestoneRelationInfo;
 import org.kuali.student.r2.core.atp.dto.MilestoneInfo;
 import org.kuali.student.r2.core.atp.service.AtpServiceDecorator;
 
-public class AtpServiceValidationDecorator extends AtpServiceDecorator  implements HoldsValidator{
+public class AtpServiceValidationDecorator extends AtpServiceDecorator  implements HoldsValidator, HoldsDataDictionaryService{
     private DataDictionaryValidator validator;
 
     @Override
@@ -28,6 +34,17 @@ public class AtpServiceValidationDecorator extends AtpServiceDecorator  implemen
     public void setValidator(DataDictionaryValidator validator) {
         this.validator = validator;
     }
+    private DataDictionaryService dataDictionaryService;
+
+    @Override
+    public DataDictionaryService getDataDictionaryService() {
+        return dataDictionaryService;
+    }
+
+    @Override
+    public void setDataDictionaryService(DataDictionaryService dataDictionaryService) {
+        this.dataDictionaryService = dataDictionaryService;
+    }
 
     @Override
     public List<ValidationResultInfo> validateAtp(
@@ -38,16 +55,53 @@ public class AtpServiceValidationDecorator extends AtpServiceDecorator  implemen
             InvalidParameterException,
             MissingParameterException,
             OperationFailedException {
-        
-        try{
-            this.validator.validate(DataDictionaryValidator.ValidationType.fromString(validationType), atpInfo, context);
-        }catch(PermissionDeniedException ex){
-            throw new OperationFailedException("Validation of Atp failed due to Permission denied exception", ex);
-        }
-        
-        return this.nextDecorator.validateAtp(validationType, atpInfo, context);
-
+    	return validate(validationType, atpInfo, context);
     }
+    
+    private void validateAtp( AtpInfo atpInfo, ContextInfo context) throws DataValidationErrorException, OperationFailedException, 
+    InvalidParameterException, MissingParameterException{
+	  try
+	  {
+	   List<ValidationResultInfo> errors = this.validateAtp(DataDictionaryValidator.ValidationType.FULL_VALIDATION.toString (), atpInfo, context);
+	   if ( ! errors.isEmpty ())   {
+	    throw new DataValidationErrorException ("Errors", errors);
+	   }
+	  }
+	  catch (DoesNotExistException ex)
+	  {
+	   throw new OperationFailedException("erorr trying to validate", ex);
+	  }   	
+    }
+	@Override
+	public AtpInfo createAtp(
+			String atpKey,
+			AtpInfo atpInfo,
+			ContextInfo context)
+	throws AlreadyExistsException,
+	DataValidationErrorException,
+	InvalidParameterException,
+	MissingParameterException,
+	OperationFailedException,
+	PermissionDeniedException {
+		validateAtp(atpInfo, context);
+		return this.nextDecorator.createAtp(atpKey ,atpInfo,  context);
+	}
+
+	@Override
+	public AtpInfo updateAtp(
+			String atpKey,
+			AtpInfo atpInfo,
+			ContextInfo context)
+	throws DataValidationErrorException,
+	DoesNotExistException,
+	InvalidParameterException,
+	MissingParameterException,
+	OperationFailedException,
+	PermissionDeniedException,
+	VersionMismatchException {
+		validateAtp(atpInfo, context);
+		return this.nextDecorator.updateAtp(atpKey ,atpInfo,  context);
+	}
 
     @Override
     public List<ValidationResultInfo> validateMilestone(String validationType, MilestoneInfo milestoneInfo, ContextInfo context) 
@@ -73,6 +127,16 @@ public class AtpServiceValidationDecorator extends AtpServiceDecorator  implemen
         return this.nextDecorator.validateAtpMilestoneRelation(validationType, atpMilestoneRelationInfo, context);
     }
     
+    private List<ValidationResultInfo> validate(String validationType, Object info, ContextInfo context)
+    throws OperationFailedException, MissingParameterException, InvalidParameterException{
+    	List<ValidationResultInfo> errors;
+       try{
+       	errors = this.validator.validate(DataDictionaryValidator.ValidationType.fromString(validationType), info, context);
+       }catch(PermissionDeniedException ex){
+           throw new OperationFailedException();
+       }
+       return errors;
+    }   
     
 
 }
