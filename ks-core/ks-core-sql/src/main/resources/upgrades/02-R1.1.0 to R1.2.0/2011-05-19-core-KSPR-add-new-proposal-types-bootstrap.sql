70698105aa74891e757545c861256f6e0efe3dda
cmann <cmann@kuali.org>
2011-06-13 11:13:03 -0400

    Incomplete - task : KSLAB-1968 Modify Program by Proposal - Spelling fixes to impex data
    git-svn-id: https://svn.kuali.org/repos/student/trunk@22215 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-sql/src/main/resources/upgrades/02-R1.1.0 to R1.2.0/2011-05-19-core-KSPR-add-new-proposal-types-bootstrap.sql
+++ ks-core/ks-core-sql/src/main/resources/upgrades/02-R1.1.0 to R1.2.0/2011-05-19-core-KSPR-add-new-proposal-types-bootstrap.sql
@@ -1,6 +1,6 @@
-insert into KSPR_PROPOSAL_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME, OBJ_ID, VER_NBR) values ('kuali.proposal.type.majorDicipline.modify', 'Modify Major Discipline Proposal', to_date('2000-01-01','yyyy-mm-dd'), null, 'Modify Major Discipline Proposal', '70c7efa4-d269-472e-8333-41decae7c041', 0)
+insert into KSPR_PROPOSAL_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME, OBJ_ID, VER_NBR) values ('kuali.proposal.type.majorDiscipline.modify', 'Modify Major Discipline Proposal', to_date('2000-01-01','yyyy-mm-dd'), null, 'Modify Major Discipline Proposal', '70c7efa4-d269-472e-8333-41decae7c041', 0)
 /
-insert into KSPR_PROPOSAL_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME, OBJ_ID, VER_NBR) values ('kuali.proposal.type.majorDicipline.create', 'Create Major Discipline Proposal', to_date('2000-01-01','yyyy-mm-dd'), null, 'Create Major Discipline Proposal', 'b9a5ed17-ba00-4eae-bcbb-660d293d8107', 0)
+insert into KSPR_PROPOSAL_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME, OBJ_ID, VER_NBR) values ('kuali.proposal.type.majorDiscipline.create', 'Create Major Discipline Proposal', to_date('2000-01-01','yyyy-mm-dd'), null, 'Create Major Discipline Proposal', 'b9a5ed17-ba00-4eae-bcbb-660d293d8107', 0)
 /
 insert into KSPR_PROPOSAL_TYPE (TYPE_KEY, TYPE_DESC, EFF_DT, EXPIR_DT, NAME, OBJ_ID, VER_NBR) values ('kuali.proposal.type.course.modify.admin', 'Modify Credit Course Admin Proposal', to_date('2000-01-01','yyyy-mm-dd'), null, 'Modify Credit Course Admin Proposal', '3ba80f94-2945-49b5-90f0-a010e8ef9eb5', 0)
 /
