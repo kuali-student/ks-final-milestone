c9bbfe39742af503ea2f4ddb5560a62f252d1148
alubbers <alubbers@kuali.org>
2011-06-16 19:39:47 -0400

    [KSENROLL-96] Updated persistence config to include IssueEntity and related entities, and added tests and data for IssueDao
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@22283 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/enrollment/dao/TestIssueDao.java
@@ -0,0 +1,69 @@
+package org.kuali.student.enrollment.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.kuali.student.common.test.spring.AbstractTransactionalDaoTest;
+import org.kuali.student.common.test.spring.Dao;
+import org.kuali.student.common.test.spring.PersistenceFileLocation;
+import org.kuali.student.enrollment.class1.hold.dao.IssueDao;
+import org.kuali.student.enrollment.class1.hold.model.HoldRichTextEntity;
+import org.kuali.student.enrollment.class1.hold.model.IssueEntity;
+import org.kuali.student.r2.common.util.constants.HoldServiceConstants;
+
+@PersistenceFileLocation("classpath:META-INF/persistence_jta.xml")
+public class TestIssueDao extends AbstractTransactionalDaoTest {
+    @Dao(value = "org.kuali.student.enrollment.class1.hold.dao.IssueDao", testSqlFile = "classpath:ks-hold.sql")
+    private IssueDao dao;
+
+    @Test
+    public void testGetIssue() {
+        IssueEntity obj = dao.find("Hold-Issue-1");
+        assertNotNull(obj);
+        assertEquals("Issue one", obj.getName());
+        assertEquals(HoldServiceConstants.ISSUE_ACIVE_STATE_KEY, obj.getIssueState().getId());
+        assertEquals(HoldServiceConstants.RESIDENCY_ISSUE_TYPE_KEY, obj.getTypeId());
+        assertEquals("Issue Desc 101", obj.getDescr().getPlain());
+    }
+
+    @Test
+    public void testCreateIssue() {
+        IssueEntity existingEntity = dao.find("Hold-Issue-1");
+
+        IssueEntity obj = new IssueEntity();
+        obj.setName("Issue Test");
+        obj.setDescr(new HoldRichTextEntity("plain", "formatted"));
+        obj.setIssueState(existingEntity.getIssueState());
+        obj.setTypeId(existingEntity.getTypeId());
+        dao.persist(obj);
+        assertNotNull(obj.getId());
+        IssueEntity obj2 = dao.find(obj.getId());
+        assertEquals("Issue Test", obj2.getName());
+        assertEquals("plain", obj2.getDescr().getPlain());
+    }
+
+    @Test
+    public void testUpdateIssue() {
+        IssueEntity existingEntity = dao.find("Hold-Issue-1");
+
+        existingEntity.setName("Issue Updated");
+        existingEntity.setDescr(new HoldRichTextEntity("plain", "formatted"));
+        dao.merge(existingEntity);
+
+        IssueEntity obj2 = dao.find(existingEntity.getId());
+        assertEquals("Issue Updated", obj2.getName());
+        assertEquals("plain", obj2.getDescr().getPlain());
+    }
+
+    @Test
+    public void testDeleteIssue() {
+        IssueEntity obj = dao.find("Hold-Issue-2");
+        assertNotNull(obj);
+        dao.remove(obj);
+        IssueEntity old = dao.find("Hold-Issue-2");
+        assertNull(old);
+    }
+
+}
