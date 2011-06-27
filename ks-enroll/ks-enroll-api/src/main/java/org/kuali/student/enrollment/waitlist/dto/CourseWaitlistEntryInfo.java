39713326842622a7c420ceaeed0e814b6a37071a
sambit <sambit@kuali.org>
2011-06-27 12:14:02 -0400

    KSPROJPLAN-576 Course Registration service interface
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@22462 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/enrollment/waitlist/dto/CourseWaitlistEntryInfo.java
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/enrollment/waitlist/dto/CourseWaitlistEntryInfo.java
@@ -4,60 +4,92 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
 import org.kuali.student.enrollment.waitlist.course.infc.CourseWaitlistEntry;
 import org.kuali.student.r2.common.dto.IdEntityInfo;
 import org.kuali.student.r2.common.dto.RelationshipInfo;
 import org.w3c.dom.Element;
 
-public class CourseWaitlistEntryInfo extends RelationshipInfo implements CourseWaitlistEntry, Serializable {
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "CourseWaitlistEntryInfo", propOrder = { "id", "typeKey",
+		"stateKey", "effectiveDate", "expirationDate", "studentId",
+		"courseWaitlistOptionId", "position", "hasCheckedIn", "meta",
+		"attributes", "_futureElements" })
+public class CourseWaitlistEntryInfo extends RelationshipInfo implements
+		CourseWaitlistEntry, Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private String studentId;
+	@XmlElement
+	private String studentId;
+	@XmlElement
+	private String courseWaitlistOptionId;
+	@XmlElement
+	private Integer position;
+	@XmlElement
+	private Boolean hasCheckedIn;
+	@XmlAnyElement
+	private List<Element> _futureElements;
 
-    private String courseWaitlistOptionId;
+	public CourseWaitlistEntryInfo() {
+		super();
+		this.studentId = null;
+		this.courseWaitlistOptionId = null;
+		this.position = null;
+		this.hasCheckedIn = null;
+		this._futureElements = null;
+	}
 
-    private Integer position;
+	public CourseWaitlistEntryInfo(CourseWaitlistEntry courseWaitlistEntry) {
+		super(courseWaitlistEntry);
+		if (null != courseWaitlistEntry) {
+			this.studentId = courseWaitlistEntry.getStudentId();
+			this.courseWaitlistOptionId = courseWaitlistEntry.getCourseWaitlistOptionId();
+			this.position = courseWaitlistEntry.getPosition();
+			this.hasCheckedIn = courseWaitlistEntry.getHasCheckedIn();
+			this._futureElements = null;
+		}
+	}
 
-    private Boolean hasCheckedIn;
-    
-    private List<Element> _futureElements;
+	public void setHasCheckedIn(Boolean hasCheckedIn) {
+		this.hasCheckedIn = hasCheckedIn;
+	}
 
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
 
-    public void setHasCheckedIn(Boolean hasCheckedIn) {
-        this.hasCheckedIn = hasCheckedIn;
-    }
+	public void setCourseWaitlistOptionId(String courseWaitlistOptionId) {
+		this.courseWaitlistOptionId = courseWaitlistOptionId;
+	}
 
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
-    }
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
 
-    public void setCourseWaitlistOptionId(String courseWaitlistOptionId) {
-        this.courseWaitlistOptionId = courseWaitlistOptionId;
-    }
+	@Override
+	public String getStudentId() {
+		return studentId;
+	}
 
-    public void setPosition(Integer position) {
-        this.position = position;
-    }
+	@Override
+	public String getCourseWaitlistOptionId() {
+		return courseWaitlistOptionId;
+	}
 
-    @Override
-    public String getStudentId() {
-        return studentId;
-    }
+	@Override
+	public Integer getPosition() {
+		return position;
+	}
 
-    @Override
-    public String getCourseWaitlistOptionId() {
-        return courseWaitlistOptionId;
-    }
-
-    @Override
-    public Integer getPosition() {
-        return position;
-    }
-
-    @Override
-    public Boolean getHasCheckedIn() {
-        return hasCheckedIn;
-    }
+	@Override
+	public Boolean getHasCheckedIn() {
+		return hasCheckedIn;
+	}
 
 }
--- ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/enrollment/waitlist/dto/CourseWaitlistInfo.java
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/enrollment/waitlist/dto/CourseWaitlistInfo.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 import org.kuali.student.enrollment.waitlist.course.infc.CourseWaitlist;
@@ -13,48 +15,67 @@ import org.kuali.student.r2.common.dto.IdEntityInfo;
 import org.w3c.dom.Element;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "CourseWaitlistInfo", propOrder = {"id", "typeKey", "stateKey", "courseOfferingId",
-        "courseWailtistEntryIds", "waitlistOptions", "meta", "attributes", "_futureElements"})
-        
-public class CourseWaitlistInfo extends IdEntityInfo implements CourseWaitlist, Serializable {
-    
-    private static final long serialVersionUID = 1L;
+@XmlType(name = "CourseWaitlistInfo", propOrder = { "id", "typeKey",
+		"stateKey", "courseOfferingId", "courseWailtistEntryIds",
+		"waitlistOptions", "meta", "attributes", "_futureElements" })
+public class CourseWaitlistInfo extends IdEntityInfo implements CourseWaitlist,
+		Serializable {
 
-    private String courseOfferingId;
+	private static final long serialVersionUID = 1L;
+	@XmlElement
+	private String courseOfferingId;
+	@XmlElement
+	private List<String> courseWailtistEntryIds;
+	@XmlElement
+	private List<CourseWaitlistOption> waitlistOptions;
+	@XmlAnyElement
+	private List<Element> _futureElements;
 
-    private List<String> courseWailtistEntryIds;
+	public CourseWaitlistInfo() {
+		super();
+		this.courseOfferingId = null;
+		this.courseWailtistEntryIds = null;
+		this.waitlistOptions = null;
+		this._futureElements = null;
+	}
 
-    private List<CourseWaitlistOption> waitlistOptions;
-    
-    private List<Element> _futureElements;
+	public CourseWaitlistInfo(CourseWaitlist courseWaitlist) {
+		super(courseWaitlist);
+		if (null != courseWaitlist) {
+			this.courseOfferingId = courseWaitlist.getCourseOfferingId();
+			this.courseWailtistEntryIds = courseWaitlist
+					.getCourseWailtistEntryIds();
+			this.waitlistOptions = courseWaitlist.getWaitlistOptions();
+			this._futureElements = null;
+		}
+	}
 
+	public void setCourseOfferingId(String courseOfferingId) {
+		this.courseOfferingId = courseOfferingId;
+	}
 
-    public void setCourseOfferingId(String courseOfferingId) {
-        this.courseOfferingId = courseOfferingId;
-    }
+	public void setCourseWailtistEntryIds(List<String> courseWailtistEntryIds) {
+		this.courseWailtistEntryIds = courseWailtistEntryIds;
+	}
 
-    public void setCourseWailtistEntryIds(List<String> courseWailtistEntryIds) {
-        this.courseWailtistEntryIds = courseWailtistEntryIds;
-    }
+	public void setWaitlistOptions(List<CourseWaitlistOption> waitlistOptions) {
+		this.waitlistOptions = waitlistOptions;
+	}
 
-    public void setWaitlistOptions(List<CourseWaitlistOption> waitlistOptions) {
-        this.waitlistOptions = waitlistOptions;
-    }
+	@Override
+	public String getCourseOfferingId() {
+		return courseOfferingId;
+	}
 
-    @Override
-    public String getCourseOfferingId() {
-        return courseOfferingId;
-    }
+	@Override
+	public List<String> getCourseWailtistEntryIds() {
+		return courseWailtistEntryIds;
+	}
 
-    @Override
-    public List<String> getCourseWailtistEntryIds() {
-        return courseWailtistEntryIds;
-    }
+	@Override
+	public List<CourseWaitlistOption> getWaitlistOptions() {
 
-    @Override
-    public List<CourseWaitlistOption> getWaitlistOptions() {
-
-        return waitlistOptions;
-    }
+		return waitlistOptions;
+	}
 
 }
