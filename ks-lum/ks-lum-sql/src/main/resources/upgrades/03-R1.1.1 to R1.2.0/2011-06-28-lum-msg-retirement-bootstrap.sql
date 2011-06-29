0787990d31da5f9c9faf7e907b533419c818d13c
wgomes <wgomes@kuali.org>
2011-06-29 11:04:41 -0400

    Remove duplicate sql insert
    git-svn-id: https://svn.kuali.org/repos/student/trunk@22505 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-sql/src/main/resources/upgrades/03-R1.1.1 to R1.2.0/2011-06-28-lum-msg-retirement-bootstrap.sql
+++ ks-lum/ks-lum-sql/src/main/resources/upgrades/03-R1.1.1 to R1.2.0/2011-06-28-lum-msg-retirement-bootstrap.sql
@@ -12,7 +12,5 @@ insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_N
 /
 insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) values ('bf6713d4-793f-41ad-8c7a-b748b6bae8d6', 'courseSpecialCircumstances-instruct', 'en', 'course', 'Information about how the course should be treated (i.e. transferability, usage to meet major requirements, etc.) after it is retired.', SYS_GUID(), 2)
 /
-insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) values ('453a62d0-0602-41c2-84ee-f36cb9d4fb0f', 'requiredOnSubmit', 'en', 'common', 'Required on Submit', SYS_GUID(), 1)
-/
 insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) values ('01d44d37-ddbe-48ec-aed3-2a34076fbe45', 'retirement', 'en', 'course', 'Retirement Information', SYS_GUID(), 1)
 /
