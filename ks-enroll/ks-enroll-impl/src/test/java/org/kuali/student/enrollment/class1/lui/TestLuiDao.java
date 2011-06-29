7b85ae04fa7843549787d95c640424d06e5b9023
huangb <huangb@kuali.org>
2011-06-29 15:43:33 -0400

    remove import org.kuali.student.enrollment.class1.lui.model.LuiEntity2;
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@22519 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/enrollment/class1/lui/TestLuiDao.java
+++ ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/enrollment/class1/lui/TestLuiDao.java
@@ -9,7 +9,7 @@ import org.kuali.student.common.test.spring.Dao;
 import org.kuali.student.common.test.spring.PersistenceFileLocation;
 import org.kuali.student.enrollment.class1.lui.dao.LuiDao;
 import org.kuali.student.enrollment.class1.lui.model.LuiEntity;
-import org.kuali.student.enrollment.class1.lui.model.LuiEntity2;
+
 @Ignore
 @PersistenceFileLocation("classpath:META-INF/acal-persistence.xml")
 public class TestLuiDao extends AbstractTransactionalDaoTest{
