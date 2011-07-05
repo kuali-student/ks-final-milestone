8ca41054a9ff7cc4399b40d997acdf017c8990cd
kmuthusw <kmuthusw@kuali.org>
2011-07-05 11:27:18 -0400

    Moved CurrencyAmount and CourseFee from 1.2 to 1.3
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@22583 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/common/dto/CurrencyAmountInfo.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2010 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package org.kuali.student.r2.common.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.kuali.student.r2.common.infc.CurrencyAmount;
+import org.w3c.dom.Element;
+
+/**
+ * Detailed information about an amount of currency including both the type of units and the quantity.
+ *
+ * @Author Kamal
+ * @Since Mon Jan 11 15:20:51 PST 2010
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "CurrencyAmountInfo", propOrder = {"id", "currencyTypeKey", "currencyQuantity",
+        "meta", "attributes", "_futureElements"})
+public class CurrencyAmountInfo extends HasAttributesAndMetaInfo implements CurrencyAmount, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @XmlAttribute
+    private String id;
+    
+    @XmlElement
+    private String currencyTypeKey;
+
+    @XmlElement
+    private Integer currencyQuantity;
+    
+    @XmlAnyElement
+    private List<Element> _futureElements;
+        
+    /**
+     * The kind of units associated with the quantity, such as US Dollars
+     */
+    public String getCurrencyTypeKey() {
+        return currencyTypeKey;
+    }
+
+    public void setCurrencyTypeKey(String currencyTypeKey) {
+        this.currencyTypeKey = currencyTypeKey;
+    }
+
+    /**
+     * The amount of currency
+     */
+    public Integer getCurrencyQuantity() {
+        return currencyQuantity;
+    }
+
+    public void setCurrencyQuantity(Integer currencyQuantity) {
+        this.currencyQuantity = currencyQuantity;
+    }
+
+	/**
+	 * 	Identifier for the currency amount record.
+	 */
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
---
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/lum/course/infc/CourseFee.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2009 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.student.r2.lum.course.infc;
+
+import java.util.List;
+
+import org.kuali.student.common.dto.CurrencyAmountInfo;
+import org.kuali.student.common.dto.RichTextInfo;
+import org.kuali.student.r2.common.infc.CurrencyAmount;
+import org.kuali.student.r2.common.infc.HasAttributesAndMeta;
+import org.kuali.student.r2.common.infc.HasId;
+
+/**
+ * Information about a fee related to a course.
+ *
+ * @author Kuali Student Team (Kamal)
+ * @Since Mon Jul 05 08:00:00 PDT 2011
+ */
+public interface CourseFee extends  HasId, HasAttributesAndMeta{
+
+    /**
+     * A code that identifies the type of the fee. For example: Lab Fee or Tuition Fee or CMF for Course Materials Fee.
+     * @name: Fee Type
+     */
+    public String getFeeType(); 
+
+    /**
+     * Indicates the structure and interpretation of the fee amounts, i.e. Fixed, Variable, Multiple.
+     * @name Rate Type
+     */
+    public String getRateType();
+
+    /**
+     * The amount or amounts associated with the fee. The number fee amounts and interpretation depends on the rate type.
+     * @name Fee Amounts
+     */
+    public List<? extends CurrencyAmount> getFeeAmounts();
+
+    /**
+     * Narrative description of the Course Fee.
+     * @name Descr
+     */
+    public RichTextInfo getDescr();
+
+}
