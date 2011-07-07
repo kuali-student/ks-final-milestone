f0f4dcbdd54fb6705b758e25323118dc25d4ab50
huangb <huangb@kuali.org>
2011-07-07 10:34:19 -0400

    Initial version to fix the build problem.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@22638 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/core/collection/infc/AbstractEntitiesCollection.java
@@ -0,0 +1,12 @@
+package org.kuali.student.r2.core.collection.infc;
+
+import java.util.List;
+
+
+public interface AbstractEntitiesCollection {
+
+	public List<CollectionEntry> getCollectionEntries();
+		
+	public String getRefObjectId();
+		
+}
---
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/core/collection/infc/CollectionEntry.java
@@ -0,0 +1,17 @@
+package org.kuali.student.r2.core.collection.infc;
+
+import java.util.Date;
+import java.util.List;
+
+public interface CollectionEntry {
+
+	
+	public String getEntryRefObjectId(); 
+
+	
+	public Date getCheckInDate(); 
+
+	
+	public Integer getPosition();
+
+}
