a80dc0a5d2714fb1e00854eb5c61bb49ce0d943a
vgadiyak <vgadiyak@kuali.org>
2011-07-12 15:07:53 -0400

    KSLAB-2011: new public ErrorLevel processValidationResult(ValidationResultInfo vr, String fieldName)
    git-svn-id: https://svn.kuali.org/repos/student/trunk@22735 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/field/layout/element/ValidationProcessable.java
+++ ks-common/ks-common-ui/src/main/java/org/kuali/student/common/ui/client/widgets/field/layout/element/ValidationProcessable.java
@@ -6,6 +6,8 @@ import org.kuali.student.common.validation.dto.ValidationResultInfo.ErrorLevel;
 public interface ValidationProcessable {
 	public ErrorLevel processValidationResult(ValidationResultInfo vr);
 
+	public ErrorLevel processValidationResult(ValidationResultInfo vr, String fieldName);
+
 	public boolean shouldProcessValidationResult(ValidationResultInfo vr);
 	
 	public void clearValidationWarnings();
