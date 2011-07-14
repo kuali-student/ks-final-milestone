16e1d662761ffe8ffb97cd7eb50a395593aa6772
huangb <huangb@kuali.org>
2011-07-13 20:07:52 -0400

    Add some code but does not work yet. Comment it out for now.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@22772 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/keyvalue/CredentialProgramTypeKeyValues.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/keyvalue/CredentialProgramTypeKeyValues.java
@@ -3,6 +3,10 @@ package org.kuali.student.enrollment.class2.acal.keyvalue;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Iterator;
+import java.io.Serializable;
+
+import javax.xml.namespace.QName;
 
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 import org.kuali.rice.core.util.ConcreteKeyValue;
@@ -10,11 +14,15 @@ import org.kuali.rice.kns.lookup.keyvalues.KeyValuesBase;
 import org.kuali.rice.kns.service.KNSServiceLocator;
 
 import org.kuali.student.r2.common.dto.ContextInfo;
-import org.kuali.student.common.exceptions.OperationFailedException;
+import org.kuali.student.r2.common.util.constants.LuServiceConstants;
 import org.kuali.student.r2.lum.lu.service.LuService;
+import org.kuali.student.common.exceptions.OperationFailedException;
 import org.kuali.student.lum.lu.dto.LuTypeInfo;
 
-public class CredentialProgramTypeKeyValues extends KeyValuesBase{
+public class CredentialProgramTypeKeyValues extends KeyValuesBase implements Serializable{
+	public static final String CREDENTIAL_PROGRAM_TYPE_KEY_PREFIX = "kuali.lu.type.credential.";
+	private static final long serialVersionUID = 1L;	
+	
     private transient LuService luService;
     
     public List getKeyValues() {
@@ -38,12 +46,39 @@ public class CredentialProgramTypeKeyValues extends KeyValuesBase{
         */
         keyValues.add(new ConcreteKeyValue("kuali.lu.type.credential.Baccalaureate", "Baccalaureate" ));
         keyValues.add(new ConcreteKeyValue("kuali.lu.type.credential.Masters", "Masters"));
+/*
+        ContextInfo context = ContextInfo.newInstance();
+ 
+        try {
+        	List<LuTypeInfo> luTypeInfoList = luService.getLuTypes(context);
+        	
+        	if (luTypeInfoList == null){
+        		System.out.println(">>Didn't get luTypeInfoList, luTypeInfoList is null.");
+        	}
+        	else if (luTypeInfoList.size()== 0){
+        		System.out.println(">>Didn't get luTypeInfoList, luTypeInfoList is zero.");
+        	}
+        	else if (luTypeInfoList.size()>= 0){
+        		Iterator luTypeInfoIterator = luTypeInfoList.iterator();
+        		while (luTypeInfoIterator.hasNext()){
+        			LuTypeInfo luTypeInfo = (LuTypeInfo)luTypeInfoIterator.next();
+        			String luTypeInfoKey = luTypeInfo.getId();
+        			if (luTypeInfoKey.startsWith(CREDENTIAL_PROGRAM_TYPE_KEY_PREFIX)){
+        				String name = luTypeInfo.getName();
+        				keyValues.add(new ConcreteKeyValue(luTypeInfoKey,name));
+        			}        			
+        		}
+        	}
+        }catch (OperationFailedException ofe){
+        	
+        }
+*/        
         return keyValues;
     }
     
     protected LuService getLuService() {
         if(luService == null) {
-            luService = GlobalResourceLoader.getService("LuService");
+        	luService = (LuService)GlobalResourceLoader.getService(new QName(LuServiceConstants.LU_NAMESPACE,"LuService"));
         }
         return this.luService;
     }
