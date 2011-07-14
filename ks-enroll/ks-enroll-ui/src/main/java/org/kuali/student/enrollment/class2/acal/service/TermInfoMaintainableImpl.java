57eb9df3db6f75b31d491cf9716e8fd24ac0af2e
huangb <huangb@kuali.org>
2011-07-13 20:04:52 -0400

    Initial version
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@22770 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/service/TermInfoMaintainableImpl.java
@@ -0,0 +1,122 @@
+package org.kuali.student.enrollment.class2.acal.service;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.xml.namespace.QName;
+
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.kns.maintenance.KualiMaintainableImpl;
+import org.kuali.rice.kns.util.KNSConstants;
+import org.kuali.student.enrollment.acal.dto.TermInfo;
+import org.kuali.student.enrollment.acal.service.AcademicCalendarService;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.exceptions.AlreadyExistsException;
+import org.kuali.student.r2.common.exceptions.DataValidationErrorException;
+import org.kuali.student.r2.common.exceptions.DoesNotExistException;
+import org.kuali.student.r2.common.exceptions.InvalidParameterException;
+import org.kuali.student.r2.common.exceptions.MissingParameterException;
+import org.kuali.student.r2.common.exceptions.OperationFailedException;
+import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
+import org.kuali.student.r2.common.exceptions.VersionMismatchException;
+import org.kuali.student.r2.common.util.constants.AtpServiceConstants;
+
+public class TermInfoMaintainableImpl extends KualiMaintainableImpl {
+	private static final long serialVersionUID = 1L;	
+	
+    public final static String TERM_KEY_PREFIX = "kuali.term.";
+    public final static String TYPE_KEY_PREFIX = "kuali.atp.type.";
+    public final static String DEFAULT_VALUE_OF_ATP_STATE ="kuali.atp.state.Official";
+    
+    private transient AcademicCalendarService academicCalendarService;
+
+    @Override
+    public void saveBusinessObject() {
+    	System.out.println(">>In TermInfoMaintainableImpl.saveBusinessObject()");
+        TermInfo termInfo = (TermInfo)getDataObject();
+        String termKey = getTermInfoKey (termInfo);
+        System.out.println(">>>termKey = "+termKey);
+        termInfo.setKey(termKey);
+        termInfo.setStateKey(AtpServiceConstants.ATP_OFFICIAL_STATE_KEY);
+
+        try{
+        	if(getMaintenanceAction().equals(KNSConstants.MAINTENANCE_NEW_ACTION) ||
+                getMaintenanceAction().equals(KNSConstants.MAINTENANCE_COPY_ACTION)) {   
+        		getAcademicCalendarService().createTerm(termKey, termInfo, ContextInfo.newInstance());
+        	}
+        	else {
+        		getAcademicCalendarService().updateTerm(termKey, termInfo, ContextInfo.newInstance());
+        	}
+        }catch (AlreadyExistsException aee){
+            
+        }catch (DataValidationErrorException dvee){
+            
+        }catch (InvalidParameterException ipe){
+            
+        }catch (MissingParameterException mpe){
+            
+        }catch (OperationFailedException ofe){
+           
+        }catch (PermissionDeniedException pde){
+            
+        }catch (DoesNotExistException dee){
+            
+        }catch (VersionMismatchException vme){
+            
+        }       
+        
+    }
+    
+    protected AcademicCalendarService getAcademicCalendarService() {
+        if(academicCalendarService == null) {
+       	 academicCalendarService = (AcademicCalendarService) GlobalResourceLoader.getService(new QName("http://student.kuali.org/wsdl/acal","AcademicCalendarService"));
+       }
+
+       return academicCalendarService;
+   }
+    
+    /**
+     * @see org.kuali.rice.kns.maintenance.KualiMaintainableImpl#prepareForSave()
+     */
+    @Override
+    public void prepareForSave() {
+    	System.out.println (">>> in prepareForSave ");
+        if (getMaintenanceAction().equalsIgnoreCase(KNSConstants.MAINTENANCE_NEW_ACTION)) {
+        	TermInfo newTerm = (TermInfo)getDataObject();   	
+        	newTerm.setStateKey(AtpServiceConstants.ATP_OFFICIAL_STATE_KEY);
+        }
+        super.prepareForSave();
+    }
+    
+   /*
+    *  Based on Norm's suggestion at 
+    *  https://wiki.kuali.org/display/STUDENT/How+to+Calculate+Keys+for+Academic+Calendar+Entities
+    *  AcademicCalendarKey should be 
+    *  kuali.academic.calendar.<last part of credentialProgramTypeKey>.<yearOfStartDate>-<yearOfEndDate>
+    */
+   private String getTermInfoKey(TermInfo termInfo){
+       String termKey = new String (TERM_KEY_PREFIX);
+       String theType;
+       
+       String theTypeKey = termInfo.getTypeKey();      
+       if (theTypeKey.startsWith(TYPE_KEY_PREFIX)){
+    	   theType = theTypeKey.substring(15);
+       }
+       else {
+    	   theType = theTypeKey;
+       }        
+       String yearOfStartDate = getYearFromDate(termInfo.getStartDate());
+       String yearOfEndDate = getYearFromDate(termInfo.getEndDate());
+       termKey = termKey.concat("."+yearOfStartDate+"-"+yearOfEndDate+"."+theType);
+       return termKey;       
+       
+   }
+   
+   private String getYearFromDate(Date date){
+   	Calendar cal = Calendar.getInstance();
+   	cal.setTime(date);
+   	int year = cal.get(Calendar.YEAR);
+   	return new Integer(year).toString();
+   }
+
+}
