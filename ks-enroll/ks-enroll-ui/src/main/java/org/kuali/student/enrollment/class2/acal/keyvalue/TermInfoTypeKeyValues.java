0852dcb68055a16fad2a3b101fea00610717c6c1
huangb <huangb@kuali.org>
2011-08-01 16:17:47 -0400

    pull out required term types through AcademicCalendarService.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@23143 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/keyvalue/TermInfoTypeKeyValues.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/keyvalue/TermInfoTypeKeyValues.java
@@ -8,31 +8,101 @@ import javax.xml.namespace.QName;
 
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 import org.kuali.rice.core.util.ConcreteKeyValue;
+import org.kuali.rice.core.util.KeyValue;
 import org.kuali.rice.kns.lookup.keyvalues.KeyValuesBase;
 
-import org.kuali.student.r2.lum.lu.service.LuService;
-import org.kuali.student.r2.common.util.constants.LuServiceConstants;
+import org.kuali.student.enrollment.acal.service.AcademicCalendarService;
+import org.kuali.student.r2.common.util.constants.AtpServiceConstants;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.dto.TypeInfo;
+import org.kuali.student.r2.common.exceptions.AlreadyExistsException;
+import org.kuali.student.r2.common.exceptions.DataValidationErrorException;
+import org.kuali.student.r2.common.exceptions.DoesNotExistException;
+import org.kuali.student.r2.common.exceptions.InvalidParameterException;
+import org.kuali.student.r2.common.exceptions.MissingParameterException;
+import org.kuali.student.r2.common.exceptions.OperationFailedException;
+import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
+import org.kuali.student.r2.common.exceptions.VersionMismatchException;
 
 public class TermInfoTypeKeyValues extends KeyValuesBase implements Serializable{
+	public static final String FALL_TERM_TYPE_KEY = "kuali.atp.type.Fall";
+	public static final String WINTER_TERM_TYPE_KEY = "kuali.atp.type.Winter";
+	public static final String SPRING_TERM_TYPE_KEY = "kuali.atp.type.Spring";
+	public static final String SUMMER_TERM_TYPE_KEY = "kuali.atp.type.Summer";
+	
 	private static final long serialVersionUID = 1L;
 	
-	private transient LuService luService;
+	private transient AcademicCalendarService academicCalendarService;
 	
     public List getKeyValues() {
-        List keyValues = new ArrayList();
+        List <ConcreteKeyValue> keyValues = new ArrayList<ConcreteKeyValue>();
 
-        keyValues.add(new ConcreteKeyValue("kuali.atp.type.Fall", "Fall" ));
-        keyValues.add(new ConcreteKeyValue("kuali.atp.type.Winter", "Winter"));
-        keyValues.add(new ConcreteKeyValue("kuali.atp.type.Spring", "Spring"));
-        keyValues.add(new ConcreteKeyValue("kuali.atp.type.Summer", "Summer"));
+//        keyValues.add(new ConcreteKeyValue("kuali.atp.type.Fall", "Fall" ));
+//        keyValues.add(new ConcreteKeyValue("kuali.atp.type.Winter", "Winter"));
+//        keyValues.add(new ConcreteKeyValue("kuali.atp.type.Spring", "Spring"));
+//        keyValues.add(new ConcreteKeyValue("kuali.atp.type.Summer", "Summer"));
+/*
+        try {
+        	List<TypeInfo> typeInfoList = getAcademicCalendarService().getTermTypesForAcademicCalendarType(AtpServiceConstants.ATP_ACADEMIC_CALENDAR_TYPE_KEY, ContextInfo.newInstance());
+        	for (TypeInfo typeInfo : typeInfoList){
+        		String typeKey = typeInfo.getKey();
+        		if (typeKey.equalsIgnoreCase(FALL_TERM_TYPE_KEY)){
+        			keyValues.add(new ConcreteKeyValue(typeKey, typeInfo.getName()));
+        		}
+        		else if(typeKey.equalsIgnoreCase(WINTER_TERM_TYPE_KEY)){
+        			keyValues.add(new ConcreteKeyValue(typeKey, typeInfo.getName()));
+        		}
+        		else if (typeKey.equalsIgnoreCase(SPRING_TERM_TYPE_KEY)){
+        			keyValues.add(new ConcreteKeyValue(typeKey, typeInfo.getName()));
+        		}
+        		else if (typeKey.equalsIgnoreCase(SUMMER_TERM_TYPE_KEY)){
+        			keyValues.add(new ConcreteKeyValue(typeKey, typeInfo.getName()));
+        		}
+        	}
+            
+        }catch (InvalidParameterException ipe){
+            
+        }catch (MissingParameterException mpe){
+            
+        }catch (OperationFailedException ofe){
+            
+        }catch (DoesNotExistException dee){
+        	
+        }  
+*/        
+        try {
+        	List<TypeInfo> typeInfoList = getAcademicCalendarService().getTermTypes(ContextInfo.newInstance());
+        	for (TypeInfo typeInfo : typeInfoList){
+        		String typeKey = typeInfo.getKey();
+        		if (typeKey.equalsIgnoreCase(FALL_TERM_TYPE_KEY)){
+        			keyValues.add(new ConcreteKeyValue(typeKey, typeInfo.getName()));
+        		}
+        		else if(typeKey.equalsIgnoreCase(WINTER_TERM_TYPE_KEY)){
+        			keyValues.add(new ConcreteKeyValue(typeKey, typeInfo.getName()));
+        		}
+        		else if (typeKey.equalsIgnoreCase(SPRING_TERM_TYPE_KEY)){
+        			keyValues.add(new ConcreteKeyValue(typeKey, typeInfo.getName()));
+        		}
+        		else if (typeKey.equalsIgnoreCase(SUMMER_TERM_TYPE_KEY)){
+         			keyValues.add(new ConcreteKeyValue(typeKey, typeInfo.getName()));
+        		}
+        	}
+            
+        }catch (InvalidParameterException ipe){
+            
+        }catch (MissingParameterException mpe){
+            
+        }catch (OperationFailedException ofe){
+            
+        }
         return keyValues;
     }
     
-    protected LuService getLuService() {
-        if(luService == null) {
-            luService = (LuService)GlobalResourceLoader.getService(new QName(LuServiceConstants.LU_NAMESPACE,"LuService"));
+    protected AcademicCalendarService getAcademicCalendarService() {
+        if(academicCalendarService == null) {
+       	 academicCalendarService = (AcademicCalendarService) GlobalResourceLoader.getService(new QName("http://student.kuali.org/wsdl/acal","AcademicCalendarService"));
+       }
 
-        }
-        return this.luService;
-    }
+       return academicCalendarService;
+   }
 }
