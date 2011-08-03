2e0e4ce15c6371255e3243912ab6df3f8bfee46b
huangb <huangb@kuali.org>
2011-08-03 14:46:22 -0400

    Initial version
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@23195 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/CourseInfoLookupViewHelperServiceImpl.java
@@ -0,0 +1,88 @@
+package org.kuali.student.enrollment.class2.courseoffering.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.kns.uif.service.impl.LookupViewHelperServiceImpl;
+
+import org.kuali.student.common.search.dto.SearchParam;
+import org.kuali.student.common.search.dto.SearchRequest;
+import org.kuali.student.common.search.dto.SearchResult;
+import org.kuali.student.common.search.dto.SearchResultRow;
+import org.kuali.student.common.search.dto.SearchResultCell;
+
+import org.kuali.student.lum.course.dto.CourseInfo;
+import org.kuali.student.lum.course.service.CourseServiceConstants;
+import org.kuali.student.lum.course.service.CourseService;
+import org.kuali.student.lum.lu.service.LuService;
+import org.kuali.student.lum.lu.service.LuServiceConstants;
+
+public class CourseInfoLookupViewHelperServiceImpl extends LookupViewHelperServiceImpl {
+	private static final long serialVersionUID = 1L;	
+	
+    private transient LuService luService;
+    private transient CourseService courseService;
+    
+	 @Override
+	 protected List<?> getSearchResultsWithBounding(Map<String, String> fieldValues, boolean unbounded) {
+		 	List <CourseInfo> courseInfoList = new ArrayList<CourseInfo>();
+		    String courseId = null;
+	        List<SearchParam> searchParams = new ArrayList<SearchParam>();
+	        SearchParam qpv1 = new SearchParam();
+	        qpv1.setKey("lu.queryParam.luOptionalType");
+//	        qpv1.setValue(ProgramClientConstants.CORE_PROGRAM);
+//	        qpv1.setValue("kuali.lu.type.CoreProgram");
+	        qpv1.setValue("kuali.lu.type.CreditCourse");
+	        searchParams.add(qpv1);
+	        
+			SearchRequest searchRequest = new SearchRequest();
+	        searchRequest.setParams(searchParams);
+			searchRequest.setSearchKey("lu.search.mostCurrent.union");	
+			
+	        
+
+			try {
+				SearchResult searchResult = getLuService().search(searchRequest);
+
+		        if (searchResult.getRows().size() > 0) {
+		            for(SearchResultRow srrow : searchResult.getRows()){
+		                List<SearchResultCell> srCells = srrow.getCells();
+		                if(srCells != null && srCells.size() > 0){
+		                    for(SearchResultCell srcell : srCells){
+		                        if (srcell.getKey().equals("lu.resultColumn.cluId")) {
+		                        	courseId = srcell.getValue();
+		                        	CourseInfo course = getCourseService().getCourse(courseId);
+		                        	courseInfoList.add(course);
+		                        }
+		                    }
+		                }
+		            }
+		        }
+
+		        return courseInfoList;
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}	 
+		 
+			 
+	 }
+	 
+	    //Note: here I am using r1 LuService implementation!!!
+	    protected LuService getLuService() {
+	        if(luService == null) {
+	        	luService = (LuService)GlobalResourceLoader.getService(new QName(LuServiceConstants.LU_NAMESPACE,"LuService"));
+	        }
+	        return this.luService;
+	    }
+	    
+	    protected CourseService getCourseService() {
+	        if(courseService == null) {
+	        	courseService = (CourseService)GlobalResourceLoader.getService(new QName(CourseServiceConstants.COURSE_NAMESPACE,"CourseService"));
+	        }
+	        return this.courseService;
+	    }
+}
