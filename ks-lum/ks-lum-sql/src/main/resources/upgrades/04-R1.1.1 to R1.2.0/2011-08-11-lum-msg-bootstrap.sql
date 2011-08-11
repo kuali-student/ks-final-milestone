1c6bc025a0da0b86700e40a599677c32c741b6e8
jannie <jannie@kuali.org>
2011-08-11 09:39:53 -0400

    KSLAB-2204 Program Managing Bodies constraints are no longer being enforced
    git-svn-id: https://svn.kuali.org/repos/student/trunk@23344 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum/ks-lum-sql/src/main/resources/upgrades/04-R1.1.1 to R1.2.0/2011-08-11-lum-msg-bootstrap.sql
@@ -0,0 +1,2 @@
+insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) values ('6b2fc022-cd8a-471c-9381-c35ffef1f8cd', 'validation.programManagingBodiesMatch', 'en', 'validation', '{0} is not a child of {1}.', SYS_GUID(), 1)
+/
