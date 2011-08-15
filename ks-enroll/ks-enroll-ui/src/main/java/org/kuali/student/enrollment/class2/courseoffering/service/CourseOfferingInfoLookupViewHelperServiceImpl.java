47cab7274f5af614fecedda077e753b7f7cb0767
alubbers <alubbers@kuali.org>
2011-08-15 18:37:36 -0400

    Added hack code to create a couple test course offerings if none found to match a specific condition.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@23435 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/CourseOfferingInfoLookupViewHelperServiceImpl.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/CourseOfferingInfoLookupViewHelperServiceImpl.java
@@ -18,12 +18,48 @@ public class CourseOfferingInfoLookupViewHelperServiceImpl extends LookupViewHel
     private static final String TERM_FIELD_NAME = "termKey";
     private static final String SUBJECT_AREA_FIELD_NAME = "subjectArea";
 
+    private static Boolean foundValidCourseOfferings = null;
+
     @Override
     protected List<?> getSearchResultsWithBounding(Map<String, String> fieldValues, boolean unbounded) {
 
         ContextInfo context = ContextInfo.newInstance();
         String termKey = fieldValues.get(TERM_FIELD_NAME);
         String subjectArea = fieldValues.get(SUBJECT_AREA_FIELD_NAME);
+
+
+        /* HACK HACK
+         * Hack code! This is a temporary hack so that at least some course offerings are added to the system
+         * Remove this when there is a reliable way through the UI to create course offerings
+         */
+
+        if(foundValidCourseOfferings == null) {
+            try {
+                List<String> courseOfferingIds = getCourseOfferingService().getCourseOfferingIdsByTermAndSubjectArea("testTermId1", "CHEM", context);
+                foundValidCourseOfferings = !courseOfferingIds.isEmpty();
+            } catch (DoesNotExistException e) {
+                throw new RuntimeException(e);
+            } catch (InvalidParameterException e) {
+                throw new RuntimeException(e);
+            } catch (MissingParameterException e) {
+                throw new RuntimeException(e);
+            } catch (OperationFailedException e) {
+                throw new RuntimeException(e);
+            } catch (PermissionDeniedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if(!foundValidCourseOfferings) {
+            try {
+                getCourseOfferingService().createCourseOfferingFromCanonical("REFERENCECOURSECHEM131", "testTermId1", null, context);
+                getCourseOfferingService().createCourseOfferingFromCanonical("REFERENCECOURSECHEM132", "testTermId1", null, context);
+
+                foundValidCourseOfferings = true;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
 
         List<CourseOfferingInfo> courseOfferings;
 
