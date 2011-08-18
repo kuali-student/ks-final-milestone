6e03b53c123baba45df05fdbac9ac60b18412b84
huangb <huangb@kuali.org>
2011-08-18 14:54:00 -0400

    add all Canonical Activity to Activity Offering Mapping (Lu to Lui mapping) listed at https://wiki.kuali.org/display/STUDENT/Learning+Unit+Instance+Types+and+States#LearningUnitInstanceTypesandStates-Types
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@23500 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-sql/src/main/resources/initial-db/atp/reference-data/KSEN_TYPETYPE_RELTN.sql
+++ ks-enroll/ks-enroll-sql/src/main/resources/initial-db/atp/reference-data/KSEN_TYPETYPE_RELTN.sql
@@ -127,3 +127,68 @@ INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATIO
 INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values ('kuali.atp.type.type.relation.allowed.18', 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.atp.type.SummerEve', 'kuali.atp.type.SessionG2', 2, 'SummerEve can contain SessionG2')
 /
 
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Lecture', 'kuali.lui.type.activity.offering.lecture', 1, 'A canonical Lecture can only have a lecture as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Lab', 'kuali.lui.type.activity.offering.lab', 1, 'A canonical Lab can only have a lab as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Discussion', 'kuali.lui.type.activity.offering.discussion', 1, 'A canonical Discussion can only have a discussion as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Tutorial', 'kuali.lui.type.activity.offering.tutorial', 1, 'A canonical Tutorial can only have a tutorial as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.WebLecture', 'kuali.lui.type.activity.offering.weblecture', 1, 'A canonical WebLecture can only have a weblecture as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.WebDiscussion', 'kuali.lui.type.activity.offering.webdiscussion', 1, 'A canonical WebDiscussion can only have a webdiscussion as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Directed', 'kuali.lui.type.activity.offering.directed', 1, 'A canonical Directed can only have a directed as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Studio', 'kuali.lui.type.activity.offering.studio', 1, 'A canonical Studio can only have a studio as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Correspond', 'kuali.lui.type.activity.offering.correspond', 1, 'A canonical Correspond can only have a correspond as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Activity', 'kuali.lui.type.activity.offering.activity', 1, 'A canonical Activity can only have a activity as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Colloquium', 'kuali.lui.type.activity.offering.colloquium', 1, 'A canonical Colloquium can only have a colloquium as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Demonstration', 'kuali.lui.type.activity.offering.demonstration', 1, 'A canonical Demonstration can only have a demonstration as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Field', 'kuali.lui.type.activity.offering.field', 1, 'A canonical Field can only have a field as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Homework', 'kuali.lui.type.activity.offering.homework', 1, 'A canonical Homework can only have a homework as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Independ', 'kuali.lui.type.activity.offering.independ', 1, 'A canonical Independ can only have a independ as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Internship', 'kuali.lui.type.activity.offering.internship', 1, 'A canonical Internship can only have a internship as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Private', 'kuali.lui.type.activity.offering.private', 1, 'A canonical Private can only have a private as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Recitation', 'kuali.lui.type.activity.offering.recitation', 1, 'A canonical Recitation can only have a recitation as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.Research', 'kuali.lui.type.activity.offering.research', 1, 'A canonical Research can only have a research as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.SelfPaced', 'kuali.lui.type.activity.offering.selfpaced', 1, 'A canonical SelfPaced can only have a selfpaced as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.CompBased', 'kuali.lui.type.activity.offering.compbased', 1, 'A canonical CompBased can only have a compbased as an offering')
+/
+
+INSERT INTO KSEN_TYPETYPE_RELTN (ID, VER_NBR, EFF_DT, EXPIR_DT, TYPETYPE_RELATION_TYPE, OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, NAME) values (sys_guid(), 0, to_timestamp('2011-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), to_timestamp('2100-01-01 00:00:00.0','YYYY-MM-DD HH24.MI.SS.FF'), 'kuali.type.type.relation.type.allowed','kuali.lu.type.activity.VideoConf', 'kuali.lui.type.activity.offering.videoconf', 1, 'A canonical VideoConf can only have a videoconf as an offering')
+/
