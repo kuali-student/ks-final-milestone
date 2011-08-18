d37b2acb9c23162e4a6ec3a316746dcdef3817b8
vgadiyak <vgadiyak@kuali.org>
2011-08-18 14:32:55 -0400

    KSLAB-1975 added side-by-side comparison
    git-svn-id: https://svn.kuali.org/repos/student/trunk@23495 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/major/view/MajorKeyProgramInfoViewConfiguration.java
+++ ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/major/view/MajorKeyProgramInfoViewConfiguration.java
@@ -1,32 +1,46 @@
 package org.kuali.student.lum.program.client.major.view;
 
+import org.kuali.student.common.assembly.data.Metadata;
+import org.kuali.student.common.assembly.data.QueryPath;
 import org.kuali.student.common.ui.client.configurable.mvc.FieldDescriptor;
+import org.kuali.student.common.ui.client.configurable.mvc.FieldDescriptorReadOnly;
 import org.kuali.student.common.ui.client.configurable.mvc.SectionTitle;
 import org.kuali.student.common.ui.client.configurable.mvc.binding.ListToTextBinding;
+import org.kuali.student.common.ui.client.configurable.mvc.binding.ModelWidgetBinding;
 import org.kuali.student.common.ui.client.configurable.mvc.sections.HorizontalSection;
 import org.kuali.student.common.ui.client.configurable.mvc.sections.TableSection;
 import org.kuali.student.common.ui.client.configurable.mvc.sections.VerticalSection;
 import org.kuali.student.common.ui.client.configurable.mvc.views.SectionView;
 import org.kuali.student.common.ui.client.configurable.mvc.views.VerticalSectionView;
+import org.kuali.student.common.ui.client.mvc.Controller;
 import org.kuali.student.common.ui.client.widgets.field.layout.element.MessageKeyInfo;
+import org.kuali.student.common.ui.client.widgets.table.summary.SummaryTableFieldBlock;
+import org.kuali.student.common.ui.client.widgets.table.summary.SummaryTableFieldRow;
+import org.kuali.student.common.ui.client.widgets.table.summary.SummaryTableSection;
 import org.kuali.student.lum.common.client.configuration.AbstractSectionConfiguration;
 import org.kuali.student.lum.program.client.ProgramConstants;
 import org.kuali.student.lum.program.client.ProgramSections;
 import org.kuali.student.lum.program.client.major.MajorEditableHeader;
+import org.kuali.student.lum.program.client.major.edit.MajorEditController;
+import org.kuali.student.lum.program.client.major.proposal.MajorProposalController;
 import org.kuali.student.lum.program.client.properties.ProgramProperties;
+
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author Igor
  */
 public class MajorKeyProgramInfoViewConfiguration extends AbstractSectionConfiguration {
 
+	private Controller controller = null;
+
     public static MajorKeyProgramInfoViewConfiguration create() {
         MajorKeyProgramInfoViewConfiguration instance = new MajorKeyProgramInfoViewConfiguration(new VerticalSectionView(ProgramSections.PROGRAM_DETAILS_VIEW, ProgramProperties.get().program_menu_sections_programInformation(), ProgramConstants.PROGRAM_MODEL_ID));
         return instance;
     }
 
-    public static MajorKeyProgramInfoViewConfiguration createSpecial() {
-        MajorKeyProgramInfoViewConfiguration instance = new MajorKeyProgramInfoViewConfiguration(new VerticalSectionView(ProgramSections.PROGRAM_DETAILS_VIEW, ProgramProperties.get().program_menu_sections_programInformation(), ProgramConstants.PROGRAM_MODEL_ID, new MajorEditableHeader(ProgramProperties.get().program_menu_sections_programInformation(), ProgramSections.PROGRAM_DETAILS_EDIT)));
+    public static MajorKeyProgramInfoViewConfiguration createSpecial(Controller controller) {
+        MajorKeyProgramInfoViewConfiguration instance = new MajorKeyProgramInfoViewConfiguration(new VerticalSectionView(ProgramSections.PROGRAM_DETAILS_VIEW, ProgramProperties.get().program_menu_sections_programInformation(), ProgramConstants.PROGRAM_MODEL_ID, new MajorEditableHeader(ProgramProperties.get().program_menu_sections_programInformation(), ProgramSections.PROGRAM_DETAILS_EDIT)), controller);
         return instance;
     }
 
@@ -35,15 +49,32 @@ public class MajorKeyProgramInfoViewConfiguration extends AbstractSectionConfigu
         rootSection.addStyleName("programInformationView");
     }
 
+    private MajorKeyProgramInfoViewConfiguration(SectionView sectionView, Controller controller) {
+        rootSection = sectionView;
+        rootSection.addStyleName("programInformationView");
+        this.controller = controller;
+    }
+
     @Override
     protected void buildLayout() {
-        HorizontalSection section = new HorizontalSection();
-        section.addSection(createIdentifyingDetailsSection());
-        section.addSection(createProgramTitleSection());
-        section.nextRow();
-        section.addSection(createDatesSection());
-        section.addSection(createOtherInformationSection());
-        rootSection.addSection(section);
+    	if (controller instanceof MajorProposalController || controller instanceof MajorEditController) 
+    	{
+    		VerticalSection section = new VerticalSection();
+    		section.addSection(createIdentifyingDetailsSectionEdit());
+    		section.addSection(createProgramTitleSectionEdit());
+    		section.addSection(createDatesSectionEdit());
+    		section.addSection(createOtherInformationSectionEdit());
+            rootSection.addSection(section);    		
+    	} else
+    	{
+       		HorizontalSection section = new HorizontalSection();
+       		section.addSection(createIdentifyingDetailsSection());
+       		section.addSection(createProgramTitleSection());
+       		section.nextRow();
+       		section.addSection(createDatesSection());
+       		section.addSection(createOtherInformationSection());
+       		rootSection.addSection(section);
+    	}
     }
 
     private TableSection createIdentifyingDetailsSection() {
@@ -95,5 +126,124 @@ public class MajorKeyProgramInfoViewConfiguration extends AbstractSectionConfigu
         return section;
     }
 
+    // Side-by-side comparison (when controller is not null)  
+    private SummaryTableSection createIdentifyingDetailsSectionEdit() { 
+      	SummaryTableSection section = new SummaryTableSection((Controller) controller);     		
+      	section.setEditable(false);
+      	section.addSummaryTableFieldBlock(createIdentifyingDetailsSectionBlock());
 
+        return section;
+    }
+
+  	@SuppressWarnings("unchecked")
+  	public SummaryTableFieldBlock createIdentifyingDetailsSectionBlock() {
+  		SummaryTableFieldBlock block = new SummaryTableFieldBlock();
+  		block.setTitle(ProgramProperties.get().programInformation_identifyingDetails());
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.CODE, new MessageKeyInfo(ProgramProperties.get().programInformation_code())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.CREDENTIAL_PROGRAM_LEVEL, new MessageKeyInfo(ProgramProperties.get().programInformation_level())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.CREDENTIAL_PROGRAM_TYPE_NAME, new MessageKeyInfo(ProgramProperties.get().programInformation_credentialProgram())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.PROGRAM_CLASSIFICATION, new MessageKeyInfo(ProgramProperties.get().programInformation_classification())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.DEGREE_TYPE, new MessageKeyInfo(ProgramProperties.get().programInformation_degreeType())));
+
+  		return block;
+  	}
+    
+    private SummaryTableSection createProgramTitleSectionEdit() { 
+      	SummaryTableSection section = new SummaryTableSection((Controller) controller);     		
+      	section.setEditable(false);
+      	section.addSummaryTableFieldBlock(createProgramTitleSectionEditBlock());
+
+        return section;
+    }
+
+  	@SuppressWarnings("unchecked")
+  	public SummaryTableFieldBlock createProgramTitleSectionEditBlock() {
+  		SummaryTableFieldBlock block = new SummaryTableFieldBlock();
+  		block.setTitle(ProgramProperties.get().programInformation_programTitle());
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.LONG_TITLE, new MessageKeyInfo(ProgramProperties.get().programInformation_titleFull())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.SHORT_TITLE, new MessageKeyInfo(ProgramProperties.get().programInformation_titleShort())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.TRANSCRIPT, new MessageKeyInfo(ProgramProperties.get().programInformation_titleTranscript())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.DIPLOMA, new MessageKeyInfo(ProgramProperties.get().programInformation_titleDiploma())));
+
+  		return block;
+  	}
+  	
+    private SummaryTableSection createDatesSectionEdit() { 
+      	SummaryTableSection section = new SummaryTableSection((Controller) controller);     		
+      	section.setEditable(false);
+      	section.addSummaryTableFieldBlock(createDatesSectionEditBlock());
+
+        return section;
+    }
+
+  	@SuppressWarnings("unchecked")
+  	public SummaryTableFieldBlock createDatesSectionEditBlock() {
+  		SummaryTableFieldBlock block = new SummaryTableFieldBlock();
+  		block.setTitle(ProgramProperties.get().programInformation_dates());
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.START_TERM, new MessageKeyInfo(ProgramProperties.get().programInformation_startTerm())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.END_INSTITUTIONAL_ADMIT_TERM, new MessageKeyInfo(ProgramProperties.get().programInformation_admitTerm())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.END_PROGRAM_ENTRY_TERM, new MessageKeyInfo(ProgramProperties.get().programInformation_entryTerm())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.END_PROGRAM_ENROLL_TERM, new MessageKeyInfo(ProgramProperties.get().programInformation_enrollTerm())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.PROGRAM_APPROVAL_DATE, new MessageKeyInfo(ProgramProperties.get().programInformation_approvalDate())));
+ 
+  		return block;
+  	}
+
+    private SummaryTableSection createOtherInformationSectionEdit() { 
+      	SummaryTableSection section = new SummaryTableSection((Controller) controller);     		
+      	section.setEditable(false);
+      	section.addSummaryTableFieldBlock(createOtherInformationSectionEditBlock());
+
+        return section;
+    }
+
+  	@SuppressWarnings("unchecked")
+  	public SummaryTableFieldBlock createOtherInformationSectionEditBlock() {
+  		SummaryTableFieldBlock block = new SummaryTableFieldBlock();
+  		block.setTitle(ProgramProperties.get().programInformation_otherInformation());
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.LOCATION, new MessageKeyInfo(ProgramProperties.get().programInformation_location())));
+        block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.ACCREDITING_AGENCY,
+        		new MessageKeyInfo(ProgramProperties.get().programInformation_accreditation()), null,
+                null, null, new ListToTextBinding(ProgramConstants.ACCREDITING_AGENCY_ORG_ID_TRANSLATION), false));        
+        block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.CIP_2000, new MessageKeyInfo(ProgramProperties.get().programInformation_cip2000())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.CIP_2010, new MessageKeyInfo(ProgramProperties.get().programInformation_cip2010())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.HEGIS_CODE, new MessageKeyInfo(ProgramProperties.get().programInformation_hegis())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.CREDENTIAL_PROGRAM_INSTITUTION_ID, new MessageKeyInfo(ProgramProperties.get().programInformation_institution())));
+   		
+  		return block;
+  	}
+  	
+  	protected SummaryTableFieldRow getFieldRow(String fieldKey, MessageKeyInfo messageKey) {
+  		return getFieldRow(fieldKey, messageKey, null, null, null, null, false);
+  	}
+     
+  	protected SummaryTableFieldRow getFieldRow(String fieldKey,
+  			MessageKeyInfo messageKey, Widget widget, Widget widget2,
+  			String parentPath, ModelWidgetBinding<?> binding, boolean optional) 
+  	{
+  		QueryPath path = QueryPath.concat(parentPath, fieldKey);
+  		Metadata meta = configurer.getModelDefinition().getMetadata(path);
+
+  		FieldDescriptorReadOnly fd = new FieldDescriptorReadOnly(path.toString(), messageKey, meta);
+  		if (widget != null) {
+  			fd.setFieldWidget(widget);
+  		}
+  		if (binding != null) {
+  			fd.setWidgetBinding(binding);
+  		}
+  		fd.setOptional(optional);
+
+  		FieldDescriptorReadOnly fd2 = new FieldDescriptorReadOnly(path.toString(), messageKey, meta);
+  		if (widget2 != null) {
+  			fd2.setFieldWidget(widget2);
+  		}
+  		if (binding != null) {
+  			fd2.setWidgetBinding(binding);
+  		}
+  		fd2.setOptional(optional);
+
+  		SummaryTableFieldRow fieldRow = new SummaryTableFieldRow(fd, fd2);
+
+  		return fieldRow;
+  	}    
 }
