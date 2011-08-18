793873776fca8e95d22e5274d4f63abf2d7a9f9f
vgadiyak <vgadiyak@kuali.org>
2011-08-18 14:31:58 -0400

    KSLAB-1975 added side-by-side comparison
    git-svn-id: https://svn.kuali.org/repos/student/trunk@23491 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/major/view/ManagingBodiesViewConfiguration.java
+++ ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/major/view/ManagingBodiesViewConfiguration.java
@@ -1,41 +1,69 @@
 package org.kuali.student.lum.program.client.major.view;
 
+import org.kuali.student.common.assembly.data.Metadata;
+import org.kuali.student.common.assembly.data.QueryPath;
+import org.kuali.student.common.ui.client.configurable.mvc.FieldDescriptorReadOnly;
+import org.kuali.student.common.ui.client.configurable.mvc.binding.ModelWidgetBinding;
 import org.kuali.student.common.ui.client.configurable.mvc.sections.CollapsableSection;
 import org.kuali.student.common.ui.client.configurable.mvc.sections.VerticalSection;
 import org.kuali.student.common.ui.client.configurable.mvc.views.SectionView;
 import org.kuali.student.common.ui.client.configurable.mvc.views.VerticalSectionView;
+import org.kuali.student.common.ui.client.mvc.Controller;
 import org.kuali.student.common.ui.client.widgets.field.layout.element.MessageKeyInfo;
+import org.kuali.student.common.ui.client.widgets.table.summary.SummaryTableFieldBlock;
+import org.kuali.student.common.ui.client.widgets.table.summary.SummaryTableFieldRow;
+import org.kuali.student.common.ui.client.widgets.table.summary.SummaryTableSection;
 import org.kuali.student.lum.common.client.configuration.AbstractSectionConfiguration;
 import org.kuali.student.lum.program.client.ProgramConstants;
 import org.kuali.student.lum.program.client.ProgramSections;
 import org.kuali.student.lum.program.client.major.MajorEditableHeader;
+import org.kuali.student.lum.program.client.major.edit.MajorEditController;
+import org.kuali.student.lum.program.client.major.proposal.MajorProposalController;
 import org.kuali.student.lum.program.client.properties.ProgramProperties;
 import org.kuali.student.lum.program.client.widgets.EditableHeader;
+
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author Igor
  */
 public class ManagingBodiesViewConfiguration extends AbstractSectionConfiguration {
 
+	private Controller controller = null;
+
     public static ManagingBodiesViewConfiguration create() {
         return new ManagingBodiesViewConfiguration(new VerticalSectionView(ProgramSections.MANAGE_BODIES_VIEW, ProgramProperties.get().program_menu_sections_managingBodies(), ProgramConstants.PROGRAM_MODEL_ID));
     }
 
-    public static ManagingBodiesViewConfiguration createSpecial() {
+    public static ManagingBodiesViewConfiguration createSpecial(Controller controller) {
         String title = ProgramProperties.get().program_menu_sections_managingBodies();
-        return new ManagingBodiesViewConfiguration(new VerticalSectionView(ProgramSections.MANAGE_BODIES_VIEW, title, ProgramConstants.PROGRAM_MODEL_ID, new MajorEditableHeader(title, ProgramSections.MANAGE_BODIES_EDIT)));
+        return new ManagingBodiesViewConfiguration(new VerticalSectionView(ProgramSections.MANAGE_BODIES_VIEW, title, ProgramConstants.PROGRAM_MODEL_ID, new MajorEditableHeader(title, ProgramSections.MANAGE_BODIES_EDIT)), controller);
     }
 
     private ManagingBodiesViewConfiguration(SectionView sectionView) {
         rootSection = sectionView;
     }
 
+    private ManagingBodiesViewConfiguration(SectionView sectionView, Controller controller) {
+        rootSection = sectionView;
+    	this.controller = controller;
+    }
+
     @Override
     protected void buildLayout() {
-        VerticalSection section = createMainSection();
-        CollapsableSection collapsableSection = createAdditionalSection();
-        rootSection.addSection(section);
-        rootSection.addSection(collapsableSection);
+    	if (controller instanceof MajorProposalController || controller instanceof MajorEditController) 
+    	{
+    		VerticalSection section = createMainSectionEdit(); 
+    		CollapsableSection collapsableSection = createAdditionalSectionEdit();
+            rootSection.addSection(section);
+            rootSection.addSection(collapsableSection);
+    	} else
+    	{
+    		VerticalSection section = createMainSection();
+    		CollapsableSection collapsableSection = createAdditionalSection();
+            rootSection.addSection(section);
+            rootSection.addSection(collapsableSection);
+    	}
     }
 
     private VerticalSection createMainSection() {
@@ -57,4 +85,83 @@ public class ManagingBodiesViewConfiguration extends AbstractSectionConfiguratio
         configurer.addReadOnlyField(section, ProgramConstants.FINANCIAL_CONTROL_UNIT, new MessageKeyInfo(ProgramProperties.get().managingBodies_financialControlUnit()));
         return section;
     }
+    
+    
+    // Side-by-side comparison (when controller is not null)  
+    private SummaryTableSection createMainSectionEdit() { 
+      	SummaryTableSection section = new SummaryTableSection((Controller) controller);     		
+      	section.setEditable(false);
+      	section.addSummaryTableFieldBlock(createMainSectionEditBlock());
+
+        return section;
+    }
+
+  	@SuppressWarnings("unchecked")
+  	public SummaryTableFieldBlock createMainSectionEditBlock() {
+  		SummaryTableFieldBlock block = new SummaryTableFieldBlock();
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.CURRICULUM_OVERSIGHT_DIVISION, new MessageKeyInfo(ProgramProperties.get().managingBodies_curriculumOversightDivision())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.CURRICULUM_OVERSIGHT_UNIT, new MessageKeyInfo(ProgramProperties.get().managingBodies_curriculumOversightUnit())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.STUDENT_OVERSIGHT_DIVISION, new MessageKeyInfo(ProgramProperties.get().managingBodies_studentOversightDivision())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.STUDENT_OVERSIGHT_UNIT, new MessageKeyInfo(ProgramProperties.get().managingBodies_studentOversightUnit())));
+
+  		return block;
+  	}
+
+    private CollapsableSection createAdditionalSectionEdit() { 
+      	SummaryTableSection section = new SummaryTableSection((Controller) controller);     		
+      	section.setEditable(false);
+      	section.addSummaryTableFieldBlock(createAdditionalSectionEditBlock());
+      	
+      	CollapsableSection collapsableSection = new CollapsableSection(ProgramProperties.get().managingBodies_seeAll());
+      	collapsableSection.addSection(section);
+      	
+        return collapsableSection;
+    }
+
+  	@SuppressWarnings("unchecked")
+  	public SummaryTableFieldBlock createAdditionalSectionEditBlock() {
+  		SummaryTableFieldBlock block = new SummaryTableFieldBlock();
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.DEPLOYMENT_DIVISION, new MessageKeyInfo(ProgramProperties.get().managingBodies_deploymentDivision())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.DEPLOYMENT_UNIT, new MessageKeyInfo(ProgramProperties.get().managingBodies_deploymentUnit())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.FINANCIAL_RESOURCES_DIVISION, new MessageKeyInfo(ProgramProperties.get().managingBodies_financialResourcesDivision())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.FINANCIAL_RESOURCES_UNIT, new MessageKeyInfo(ProgramProperties.get().managingBodies_financialResourcesUnit())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.FINANCIAL_CONTROL_DIVISION, new MessageKeyInfo(ProgramProperties.get().managingBodies_financialControlDivision())));
+  		block.addSummaryTableFieldRow(getFieldRow(ProgramConstants.FINANCIAL_CONTROL_UNIT, new MessageKeyInfo(ProgramProperties.get().managingBodies_financialControlUnit())));
+
+  		return block;
+  	}
+
+  	protected SummaryTableFieldRow getFieldRow(String fieldKey, MessageKeyInfo messageKey) {
+  		return getFieldRow(fieldKey, messageKey, null, null, null, null, false);
+  	}
+     
+  	protected SummaryTableFieldRow getFieldRow(String fieldKey,
+  			MessageKeyInfo messageKey, Widget widget, Widget widget2,
+  			String parentPath, ModelWidgetBinding<?> binding, boolean optional) 
+  	{
+  		QueryPath path = QueryPath.concat(parentPath, fieldKey);
+  		Metadata meta = configurer.getModelDefinition().getMetadata(path);
+
+  		FieldDescriptorReadOnly fd = new FieldDescriptorReadOnly(path.toString(), messageKey, meta);
+  		if (widget != null) {
+  			fd.setFieldWidget(widget);
+  		}
+  		if (binding != null) {
+  			fd.setWidgetBinding(binding);
+  		}
+  		fd.setOptional(optional);
+
+  		FieldDescriptorReadOnly fd2 = new FieldDescriptorReadOnly(path.toString(), messageKey, meta);
+  		if (widget2 != null) {
+  			fd2.setFieldWidget(widget2);
+  		}
+  		if (binding != null) {
+  			fd2.setWidgetBinding(binding);
+  		}
+  		fd2.setOptional(optional);
+
+  		SummaryTableFieldRow fieldRow = new SummaryTableFieldRow(fd, fd2);
+
+  		return fieldRow;
+  	}    
 }
