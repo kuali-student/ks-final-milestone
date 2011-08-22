2aee9cf1e1af74bb38f41834197ad1734939f0fb
nina <nina@kuali.org>
2011-08-22 04:19:24 -0400

    KSLAB-2247 MODIFY PROGRAM: Non-admin users should not get lightbox to choose modification method Seems like some of my changes has been overwritten
    git-svn-id: https://svn.kuali.org/repos/student/trunk@23553 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-rice/src/main/config/sql/2011-07-19-lum-Admin-role-and-permissions.sql
+++ ks-lum/ks-lum-rice/src/main/config/sql/2011-07-19-lum-Admin-role-and-permissions.sql
@@ -1,6 +1,6 @@
 -- Create a new Permission template that is linked to the new Kim Typ that is specific to KS-SYS
   INSERT INTO KRIM_PERM_TMPL_T (ACTV_IND,KIM_TYP_ID,NM,NMSPC_CD,OBJ_ID,PERM_TMPL_ID,VER_NBR)
-  VALUES ('Y','19','KS Admin Screens','KS-SYS','eb16081a-49e3-430c-9851-1c3e50806058','4001',1)
+  VALUES ('Y','3002','KS Admin Screens','KS-SYS','eb16081a-49e3-430c-9851-1c3e50806058','4001',1)
 /
  -- Create the new permission 
   INSERT INTO KRIM_PERM_T (ACTV_IND,NMSPC_CD,OBJ_ID,PERM_ID,PERM_TMPL_ID,VER_NBR,NM, desc_txt)
@@ -8,7 +8,7 @@
 /
 --- Create the value for the permission, ie the screen component name
   INSERT INTO KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID,ATTR_VAL,KIM_ATTR_DEFN_ID,KIM_TYP_ID,OBJ_ID,PERM_ID,VER_NBR)
-  VALUES ('4000','useCurriculumReview','115','19','fc29c39d-e478-45c5-b646-88930762f721','3201',1)
+  VALUES ('4000','useCurriculumReview','115','3002','fc29c39d-e478-45c5-b646-88930762f721','3201',1)
 /
   -- Create the KS ADMIIN role
 insert into KRIM_ROLE_T (ROLE_ID, OBJ_ID, VER_NBR, ROLE_NM, NMSPC_CD, DESC_TXT, KIM_TYP_ID, ACTV_IND, LAST_UPDT_DT) values
@@ -29,7 +29,7 @@ INSERT INTO KRIM_ROLE_MBR_T (MBR_ID,MBR_TYP_CD,OBJ_ID,ROLE_ID,ROLE_MBR_ID,VER_NB
 /
 --- Create the value for the permission, ie the screen component name
   INSERT INTO KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID,ATTR_VAL,KIM_ATTR_DEFN_ID,KIM_TYP_ID,OBJ_ID,PERM_ID,VER_NBR)
-  VALUES ('4001','cluModifyItem','115','19','de7477c5-9197-4357-a26d-82fe0abe351d','3202',1)
+  VALUES ('4001','cluModifyItem','115','3002','de7477c5-9197-4357-a26d-82fe0abe351d','3202',1)
 /
   -- Create the link between the role and the permission
 INSERT INTO KRIM_ROLE_PERM_T (ACTV_IND,OBJ_ID,PERM_ID,ROLE_ID,ROLE_PERM_ID,VER_NBR)
