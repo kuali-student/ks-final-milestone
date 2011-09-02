94f2995df5f5f3dbc126e70cb77916bb91296677
vgadiyak <vgadiyak@kuali.org>
2011-09-02 16:35:37 -0400

    KSLAB-2384 Removed controller & changed side-by-side to new only
    git-svn-id: https://svn.kuali.org/repos/student/trunk@23785 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/major/view/ProposalChangeImpactViewConfiguration.java
+++ ks-lum/ks-lum-program/src/main/java/org/kuali/student/lum/program/client/major/view/ProposalChangeImpactViewConfiguration.java
@@ -29,75 +29,21 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public class ProposalChangeImpactViewConfiguration extends AbstractSectionConfiguration {
 
-	private Controller controller = null;
-
-    public static ProposalChangeImpactViewConfiguration createSpecial(Controller controller) { 
-        return new ProposalChangeImpactViewConfiguration(new VerticalSectionView(ProgramSections.PROGRAM_PROPOSAL_CHANGE_IMPACT_VIEW, ProgramProperties.get().program_menu_sections_proposalChangeImpact(), ProgramConstants.PROGRAM_MODEL_ID, new MajorEditableHeader(ProgramProperties.get().program_menu_sections_proposalChangeImpact(), ProgramSections.PROGRAM_PROPOSAL_CHANGE_IMPACT_EDIT)), controller);
+    public static ProposalChangeImpactViewConfiguration createSpecial() { 
+        return new ProposalChangeImpactViewConfiguration(new VerticalSectionView(ProgramSections.PROGRAM_PROPOSAL_CHANGE_IMPACT_VIEW, ProgramProperties.get().program_menu_sections_proposalChangeImpact(), ProgramConstants.PROGRAM_MODEL_ID, new MajorEditableHeader(ProgramProperties.get().program_menu_sections_proposalChangeImpact(), ProgramSections.PROGRAM_PROPOSAL_CHANGE_IMPACT_EDIT)));
     }
 
-    private ProposalChangeImpactViewConfiguration(SectionView sectionView, Controller controller) {
+    private ProposalChangeImpactViewConfiguration(SectionView sectionView) {
         rootSection = sectionView;
-        this.controller = controller;
     }
 
     @Override
     protected void buildLayout() {
-    	VerticalSection section = new VerticalSection();
-   		section.addSection(createProposalInformationSectionEdit());
-        rootSection.addSection(section);
-    }
-    
-    // Side-by-side comparison (when controller is not null)  
-    private SummaryTableSection createProposalInformationSectionEdit() { 
-      	SummaryTableSection section = new SummaryTableSection((Controller) controller);     		
-      	section.setEditable(false);
-      	section.addSummaryTableFieldBlock(createProposalInformationSectionEditBlock());
-
-        return section;
-    }
-
-  	@SuppressWarnings("unchecked")
-  	public SummaryTableFieldBlock createProposalInformationSectionEditBlock() {
-  		SummaryTableFieldBlock block = new SummaryTableFieldBlock();
-  		block.addSummaryTableFieldRow(getFieldRow("proposal/relatedCourseChanges", new MessageKeyInfo(ProgramProperties.get().proposalInformation_cluRelatedCourseChangesType())));
-  		block.addSummaryTableFieldRow(getFieldRow("proposal/impactedUnits", new MessageKeyInfo(ProgramProperties.get().proposalInformation_cluImpactedUnitsType())));
-  		block.addSummaryTableFieldRow(getFieldRow("proposal/impactedArticulationTransferPrograms", new MessageKeyInfo(ProgramProperties.get().proposalInformation_cluImpactedArticulationTransferProgramsType())));
-  		block.addSummaryTableFieldRow(getFieldRow("proposal/studentTransitionPlans", new MessageKeyInfo(ProgramProperties.get().proposalInformation_cluStudentTransitionPlansType())));
-
-  		return block;
-  	}
-
-  	protected SummaryTableFieldRow getFieldRow(String fieldKey, MessageKeyInfo messageKey) {
-  		return getFieldRow(fieldKey, messageKey, null, null, null, null, false);
-  	}
-     
-  	protected SummaryTableFieldRow getFieldRow(String fieldKey,
-  			MessageKeyInfo messageKey, Widget widget, Widget widget2,
-  			String parentPath, ModelWidgetBinding<?> binding, boolean optional) 
-  	{
-  		QueryPath path = QueryPath.concat(parentPath, fieldKey);
-  		Metadata meta = configurer.getModelDefinition().getMetadata(path);
-
-  		FieldDescriptorReadOnly fd = new FieldDescriptorReadOnly(path.toString(), messageKey, meta);
-  		if (widget != null) {
-  			fd.setFieldWidget(widget);
-  		}
-  		if (binding != null) {
-  			fd.setWidgetBinding(binding);
-  		}
-  		fd.setOptional(optional);
-
-  		FieldDescriptorReadOnly fd2 = new FieldDescriptorReadOnly(path.toString(), messageKey, meta);
-  		if (widget2 != null) {
-  			fd2.setFieldWidget(widget2);
-  		}
-  		if (binding != null) {
-  			fd2.setWidgetBinding(binding);
-  		}
-  		fd2.setOptional(optional);
-
-  		SummaryTableFieldRow fieldRow = new SummaryTableFieldRow(fd, fd2);
-
-  		return fieldRow;
-  	} 
+        VerticalSection section = new VerticalSection(); 
+        configurer.addReadOnlyField(section, "proposal/relatedCourseChanges", new MessageKeyInfo(ProgramProperties.get().proposalInformation_cluRelatedCourseChangesType()));        
+        configurer.addReadOnlyField(section, "proposal/impactedUnits", new MessageKeyInfo(ProgramProperties.get().proposalInformation_cluImpactedUnitsType()));        
+        configurer.addReadOnlyField(section, "proposal/impactedArticulationTransferPrograms", new MessageKeyInfo(ProgramProperties.get().proposalInformation_cluImpactedArticulationTransferProgramsType()));
+        configurer.addReadOnlyField(section, "proposal/studentTransitionPlans", new MessageKeyInfo(ProgramProperties.get().proposalInformation_cluStudentTransitionPlansType()));
+        rootSection.addSection(section);     
+    }    
 }
