de8c395556374f6003f274017d3db9a86c3ccdb6
sambitpa <sambitpa@kuali.org>
2011-09-09 21:04:39 -0400

    KSENROLL-147  service test
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@23928 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/enrollment/class2/courseregistration/service/impl/TestCourseRegistrationServiceImpl.java
@@ -0,0 +1,11 @@
+package org.kuali.student.enrollment.class2.courseregistration.service.impl;
+
+
+import org.junit.Before;
+
+public class TestCourseRegistrationServiceImpl {
+
+    @Before
+    public void setUp() throws Exception {}
+
+}
