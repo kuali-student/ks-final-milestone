c2f95fc46663f2472b24e5a0df739117a63796aa
tapresco <tapresco@kuali.org>
2011-09-29 15:25:24 -0400

    KSENROLL-178 Testing getFinalGradeRostersForCourseOffering
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@24222 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/enrollment/class2/grading/service/impl/TestGradingServiceImpl.java
+++ ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/enrollment/class2/grading/service/impl/TestGradingServiceImpl.java
@@ -4,16 +4,26 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kuali.student.enrollment.grading.dto.GradeRosterInfo;
 import org.kuali.student.enrollment.grading.service.GradingService;
 import org.kuali.student.r2.common.dto.ContextInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:grading-test-context.xml"})
+@Transactional
+@TransactionConfiguration(transactionManager = "JtaTxManager", defaultRollback = true)
 public class TestGradingServiceImpl {
 
     private GradingService gradingService;
@@ -57,9 +67,38 @@ public class TestGradingServiceImpl {
     }
 
     @Test
-    @Ignore("Not implemented.") // TODO implement method
     public void testGetFinalGradeRostersForCourseOffering() throws Exception {
 
+        Set<String> expcActivityIds = new HashSet<String>();
+
+        Set<String> expcGraderIds = new HashSet<String>();
+        expcGraderIds.add("admin");
+
+        Set<String> expcEntryIds = new HashSet<String>();
+        expcEntryIds.add("lprRosterEntry2");
+
+        String courseOfferingId = "Lui-1";
+        List<GradeRosterInfo> gradeRosters = gradingService.getFinalGradeRostersForCourseOffering(courseOfferingId, contextInfo);
+        assertNotNull("Roster list is null.", gradeRosters);
+        assertTrue("Number of rosters not as expected.", gradeRosters.size() == 1);
+
+        GradeRosterInfo roster = gradeRosters.get(0);
+        assertEquals("Course Offering ID not as expected.", courseOfferingId, roster.getCourseOfferingId());
+
+        List<String> activityIds = roster.getActivityOfferingIds();
+        assertNotNull("Null activity id list.", activityIds);
+        assertEquals("Different number of activity ids returned than expected.", expcActivityIds.size(), activityIds.size());
+        assertTrue("Unexpected activity ids returned.", activityIds.containsAll(expcActivityIds));
+
+        List<String> graderIds = roster.getGraderIds();
+        assertNotNull("Null grader id list.", graderIds);
+        assertEquals("Different number of grader ids returned than expected.", expcGraderIds.size(), graderIds.size());
+        assertTrue("Unexpected grader ids returned.", graderIds.containsAll(expcGraderIds));
+
+        List<String> entryIds = roster.getGradeRosterEntryIds();
+        assertNotNull("Null entry id list.", entryIds);
+        assertEquals("Different number of entry ids returned than expected.", expcEntryIds.size(), entryIds.size());
+        assertTrue("Unexpected entry ids returned.", entryIds.containsAll(expcEntryIds));
     }
 
     @Test
