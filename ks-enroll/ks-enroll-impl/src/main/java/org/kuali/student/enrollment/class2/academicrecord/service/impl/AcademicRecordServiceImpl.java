28c8f967adda3012ba30eb9e2146ca594f30917b
lpan <lpan@kuali.org>
2011-09-28 22:54:20 -0400

    ksenroll-231: add studentCourseRecord assembler; impl on getAttempted/completedCourseRecordforstudentbyterm
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@24210 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/academicrecord/service/impl/AcademicRecordServiceImpl.java
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/academicrecord/service/impl/AcademicRecordServiceImpl.java
@@ -1,21 +1,28 @@
 package org.kuali.student.enrollment.class2.academicrecord.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.kuali.student.enrollment.academicrecord.dto.GPAInfo;
 import org.kuali.student.enrollment.academicrecord.dto.StudentCourseRecordInfo;
 import org.kuali.student.enrollment.academicrecord.service.AcademicRecordService;
+import org.kuali.student.enrollment.class2.academicrecord.service.assembler.StudentCourseRecordAssembler;
+import org.kuali.student.enrollment.courseoffering.dto.CourseOfferingInfo;
+import org.kuali.student.enrollment.courseregistration.dto.CourseRegistrationInfo;
 import org.kuali.student.enrollment.courseregistration.service.CourseRegistrationService;
 import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.exceptions.DisabledIdentifierException;
 import org.kuali.student.r2.common.exceptions.DoesNotExistException;
 import org.kuali.student.r2.common.exceptions.InvalidParameterException;
 import org.kuali.student.r2.common.exceptions.MissingParameterException;
 import org.kuali.student.r2.common.exceptions.OperationFailedException;
+import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly=true,noRollbackFor={DoesNotExistException.class},rollbackFor={Throwable.class})
 public class AcademicRecordServiceImpl implements AcademicRecordService{
 	private CourseRegistrationService courseRegService;
+	private StudentCourseRecordAssembler courseRecordAssembler;
 	
 	public CourseRegistrationService getCourseRegService() {
 		return courseRegService;
@@ -25,12 +32,37 @@ public class AcademicRecordServiceImpl implements AcademicRecordService{
 		this.courseRegService = courseRegService;
 	}
 
+	
+	public StudentCourseRecordAssembler getCourseRecordAssembler() {
+		return courseRecordAssembler;
+	}
+
+	public void setCourseRecordAssembler(
+			StudentCourseRecordAssembler courseRecordAssembler) {
+		this.courseRecordAssembler = courseRecordAssembler;
+	}
+
 	@Override
 	public List<StudentCourseRecordInfo> getAttemptedCourseRecordsForTerm(
 			String personId, String termKey, ContextInfo context)
 			throws DoesNotExistException, InvalidParameterException,
 			MissingParameterException, OperationFailedException {
-		throw new UnsupportedOperationException("Method not yet implemented!");
+		List<StudentCourseRecordInfo> courseRecords = new ArrayList<StudentCourseRecordInfo>();
+		try {
+			List<CourseRegistrationInfo> regs = courseRegService.getCourseRegistrationsForStudentByTerm(personId, termKey, context);
+			if(regs != null && !regs.isEmpty()){
+				for (CourseRegistrationInfo reg : regs ){
+					StudentCourseRecordInfo courseRecord = courseRecordAssembler.assemble(reg, context);
+					if (courseRecord != null) courseRecords.add(courseRecord);
+				}
+			}
+		} catch (PermissionDeniedException e) {
+			throw new OperationFailedException();
+		} catch (DisabledIdentifierException e) {
+			throw new OperationFailedException();
+		}
+
+		return courseRecords;
 	}
 
 	@Override
@@ -46,7 +78,25 @@ public class AcademicRecordServiceImpl implements AcademicRecordService{
 			String personId, String termKey, ContextInfo context)
 			throws DoesNotExistException, InvalidParameterException,
 			MissingParameterException, OperationFailedException {
-		throw new UnsupportedOperationException("Method not yet implemented!");
+		List<StudentCourseRecordInfo> courseRecords = new ArrayList<StudentCourseRecordInfo>();
+		try {
+			List<CourseRegistrationInfo> regs = courseRegService.getCourseRegistrationsForStudentByTerm(personId, termKey, context);
+			if(regs != null && !regs.isEmpty()){
+				for (CourseRegistrationInfo reg : regs ){
+					StudentCourseRecordInfo courseRecord = courseRecordAssembler.assemble(reg, context);
+					if (courseRecord != null) {
+						if(courseRecord.getAssignedGradeValue()!= null)
+							courseRecords.add(courseRecord);
+					}
+				}
+			}
+		} catch (PermissionDeniedException e) {
+			throw new OperationFailedException();
+		} catch (DisabledIdentifierException e) {
+			throw new OperationFailedException();
+		}
+
+		return courseRecords;
 	}
 
 	@Override
