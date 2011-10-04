9780c390145b6cb9f1951d47f5c4f59ddd65788f
nwright <nwright@kuali.org>
2011-10-04 19:03:54 -0400

    KSPROJPLAN-579, KSPROJPLAN-575, KSPROJPLAN-478, KSPROJPLAN-474, KSPROJPLAN-473 Added/updated types and states for course registration and lui person relation
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@24282 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/LuiPersonRelationServiceConstants.java
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/LuiPersonRelationServiceConstants.java
@@ -26,38 +26,41 @@ public class LuiPersonRelationServiceConstants {
     public static final String REF_OBJECT_URI_LUI_PERSON_RELATION = NAMESPACE + "/"
             + LuiPersonRelationInfo.class.getSimpleName();
     /**
-     * Types and knowns groups of types
+     * Types and known groups of types
      */
     public static final String LPR_KEY_PREFIX = "kuali.lpr";
-
     public static final String INSTRUCTOR_MAIN_TYPE_KEY = "kuali.lpr.type.instructor.main";
     public static final String INSTRUCTOR_ASSISTANT_TYPE_KEY = "kuali.lpr.type.instructor.assistant";
     public static final String INSTRUCTOR_SUPPORT_TYPE_KEY = "kuali.lpr.type.instructor.support";
-    public static final String REGISTRANT_TYPE_KEY = "kuali.lpr.type.student";
+    public static final String REGISTRANT_TYPE_KEY = "kuali.lpr.type.registrant";
     public static final String ENROLLEE_TYPE_KEY = "kuali.lpr.type.enrollee";
     public static final String ADVISOR_TYPE_KEY = "kuali.lpr.type.advisor";
-    public static final String LPRTRANS_ITEM__WAILISTIST_OPTION_KEY = "kuali.lpr.option.waitlist";
-    public static final String LPRTRANS_ITEM_HOLDLIST_OPTION_KEY = "kuali.lpr.option.holdlist";
-    public static final String LPRTRANS_ITEM_UPDATE_TYPE_KEY = "kuali.lpr.trans.item.update";
-    public static final String LPRTRANS_ITEM_ADD_TYPE_KEY = "kuali.lpr.trans.item.add";
-    public static final String LPRTRANS_ITEM_SWAP_TYPE_KEY = "kuali.lpr.trans.item.swap";
-    public static final String LPRTRANS_ITEM_DROP_TYPE_KEY = "kuali.lpr.trans.item.drop";
-    public static final String LPRTRANS_ITEM_WAITLIST_TYPE_KEY = "kuali.lpr.trans.item.waitlist";
-    public static final String LPRTRANS_ITEM_SAVE_TYPE_KEY = "kuali.lpr.trans.item.save";
-
-    public static final String LPRTRANS_REGISTER_TYPE_KEY = "kuali.lpr.trans.registrant";
-
-    public static final String LPRTRANS_ITEM_NEW_STATE_KEY = "kuali.lpr.trans.item.new";
-    public static final String LPRTRANS_ITEM_DRAFT_STATE_KEY = "kuali.lpr.trans.item.draft";
-    public static final String LPRTRANS_ITEM_SUBMITTED_STATE_KEY = "kuali.lpr.trans.submitted";
-    public static final String LPRTRANS_ITEM_FAILED_STATE_KEY = "kuali.lpr.trans.failed";
-    public static final String LPRTRANS_ITEM_SUCCEEDED_STATE_KEY = "kuali.lpr.trans.succeeded";
+    // lpr transaction type
+    public static final String LPRTRANS_REGISTER_TYPE_KEY = "kuali.lpr.trans.type.register";
+    // lpr transaction item request options
+    public static final String LPRTRANS_ITEM__WAITLIST_OPTION_KEY = "kuali.lpr.option.waitlist";
+    public static final String LPRTRANS_ITEM_HOLD_UNTIL_LIST_OPTION_KEY = "kuali.lpr.option.hold.until.list";
+    // lpr transaction item types (operations)
+    public static final String LPRTRANS_ITEM_ADD_TYPE_KEY = "kuali.lpr.trans.item.type.add";
+    public static final String LPRTRANS_ITEM_DROP_TYPE_KEY = "kuali.lpr.trans.item.type.drop";
+    public static final String LPRTRANS_ITEM_SWAP_TYPE_KEY = "kuali.lpr.trans.item.type.swap";
+    public static final String LPRTRANS_ITEM_UPDATE_TYPE_KEY = "kuali.lpr.trans.item.type.update";
+    public static final String LPRTRANS_ITEM_ADD_TO_WAITLIST_TYPE_KEY = "kuali.lpr.trans.item.type.add.to.waitlist";
+    public static final String LPRTRANS_ITEM_ADD_TO_HOLD_UNTIL_LIST_TYPE_KEY = "kuali.lpr.trans.item.type.add.to.hold.until.list";
+    // transaction states
+    public static final String LPRTRANS_NEW_STATE_KEY = "kuali.lpr.trans.item.state.new";
+    public static final String LPRTRANS_SUCCEEDED_STATE_KEY = "kuali.lpr.trans.state.succeeded";
+    public static final String LPRTRANS_FAILED_STATE_KEY = "kuali.lpr.trans.state.failed";
+    public static final String LPRTRANS_DISCARDED_STATE_KEY = "kuali.lpr.trans.state.discarded";
+    // transaction item states
+    public static final String LPRTRANS_ITEM_NEW_STATE_KEY = "kuali.lpr.trans.item.state.new";
+    public static final String LPRTRANS_ITEM_SUCCEEDED_STATE_KEY = "kuali.lpr.trans.item.state.succeeded";
+    public static final String LPRTRANS_ITEM_FAILED_STATE_KEY = "kuali.lpr.trans.item.state.failed";
     public static final String[] LPRTRANS_ITEM_TYPE_KEYS = {LPRTRANS_ITEM_UPDATE_TYPE_KEY, LPRTRANS_ITEM_ADD_TYPE_KEY,
-            LPRTRANS_ITEM_DROP_TYPE_KEY, LPRTRANS_ITEM_SAVE_TYPE_KEY};
+        LPRTRANS_ITEM_DROP_TYPE_KEY};
     public static final String[] COURSE_INSTRUCTOR_TYPE_KEYS = {INSTRUCTOR_MAIN_TYPE_KEY,
-            INSTRUCTOR_ASSISTANT_TYPE_KEY, INSTRUCTOR_SUPPORT_TYPE_KEY};
+        INSTRUCTOR_ASSISTANT_TYPE_KEY, INSTRUCTOR_SUPPORT_TYPE_KEY};
     public static final String[] COURSE_STUDENT_TYPE_KEYS = {REGISTRANT_TYPE_KEY};
-
     public static final String LPRROSTER_COURSE_FINAL_GRADEROSTER_TYPE_KEY = "kuali.lpr.roster.type.course.final";
     public static final String LPRROSTER_COURSE_FINAL_GRADEROSTER_NEW_STATE_KEY = "kuali.lpr.roster.state.course.new";
     /**
@@ -96,13 +99,13 @@ public class LuiPersonRelationServiceConstants {
      */
     public static final String INSTRUCTOR_COURSE_ASSIGNMENT_PROCESS_KEY = "kuali.lpr.process.instructor.course.assignment";
     public static final String[] INSTRUCTOR_COURSE_ASSIGNMENT_STATE_KEYS = {TENATIVE_STATE_KEY, ASSIGNED_STATE_KEY,
-            UNASSIGNED_STATE_KEY};
+        UNASSIGNED_STATE_KEY};
     /**
      * States used for students in courses
      */
     public static final String STUDENT_COURSE_REGISTRATION_PROCESS_KEY = "kuali.lpr.process.student.course.registration";
     public static final String[] STUDENT_COURSE_REGISTRATION_STATE_KEYS = {PLANNED_STATE_KEY, REGISTERED_STATE_KEY,
-            WAITLISTED_STATE_KEY, DROPPED_STATE_KEY, DROPPED_LATE_STATE_KEY};
+        WAITLISTED_STATE_KEY, DROPPED_STATE_KEY, DROPPED_LATE_STATE_KEY};
     /**
      * States used for instructors of PROGRAMS
      */
@@ -113,13 +116,13 @@ public class LuiPersonRelationServiceConstants {
      */
     public static final String STUDENT_PROGRAM_ENROLLMENT_PROCESS_KEY = "kuali.lpr.process.student.program.enrollment";
     public static final String[] STUDENT_PROGRAM_ENROLLMENT_STATE_KEYS = {PLANNED_STATE_KEY, INQUIRED_STATE_KEY,
-            APPLIED_STATE_KEY, WAITLISTED_STATE_KEY, DENIED_STATE_KEY, CONFIRMED_STATE_KEY, CANCELED_STATE_KEY,
-            DEFERED_STATE_KEY, ENROLLED_STATE_KEY, TEMPORARY_ABSENCE_STATE_KEY, WITHDRAWN_STATE_KEY,
-            PROBATION_STATE_KEY};
+        APPLIED_STATE_KEY, WAITLISTED_STATE_KEY, DENIED_STATE_KEY, CONFIRMED_STATE_KEY, CANCELED_STATE_KEY,
+        DEFERED_STATE_KEY, ENROLLED_STATE_KEY, TEMPORARY_ABSENCE_STATE_KEY, WITHDRAWN_STATE_KEY,
+        PROBATION_STATE_KEY};
     /**
      * All process keys
      */
     public static final String[] LPR_PROCESS_KEYS = {STUDENT_COURSE_REGISTRATION_PROCESS_KEY,
-            INSTRUCTOR_COURSE_ASSIGNMENT_PROCESS_KEY, STUDENT_PROGRAM_ENROLLMENT_PROCESS_KEY,
-            PROGRAM_ADVISOR_ASSIGNMENT_PROCESS_KEY};
+        INSTRUCTOR_COURSE_ASSIGNMENT_PROCESS_KEY, STUDENT_PROGRAM_ENROLLMENT_PROCESS_KEY,
+        PROGRAM_ADVISOR_ASSIGNMENT_PROCESS_KEY};
 }
