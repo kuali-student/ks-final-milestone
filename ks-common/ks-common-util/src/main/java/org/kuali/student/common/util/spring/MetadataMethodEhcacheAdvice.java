1afa60ac386eeb2224e67d4d64b26520ce9d344c
depstein <depstein@kuali.org>
2011-10-05 15:08:31 -0400

    KSLAB-2455 - updated caching to return a new object instance to prevent the one in the cache from changing
    git-svn-id: https://svn.kuali.org/repos/student/trunk@24298 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-common/ks-common-util/src/main/java/org/kuali/student/common/util/spring/MetadataMethodEhcacheAdvice.java
@@ -0,0 +1,20 @@
+package org.kuali.student.common.util.spring;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.kuali.student.common.assembly.data.Metadata;
+
+public class MetadataMethodEhcacheAdvice extends MethodArgsToObjectEhcacheAdvice {
+
+	@Override
+	public Object getFromCache(ProceedingJoinPoint pjp) throws Throwable {
+		try{
+			//Return a copy so that if the data is mutated the original remains
+			Metadata result = (Metadata) super.getFromCache(pjp);
+			return new Metadata(result);
+		}catch(ClassCastException e){
+			throw new RuntimeException("This cache should only be used on methods that return Metadata", e);
+		}
+	}
+
+	
+}
