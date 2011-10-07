b289032696fed6ac409312363069ae051eb0e080
tapresco <tapresco@kuali.org>
2011-10-07 16:26:21 -0400

    KSENROLL-178 LRR reference data
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@24357 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-sql/src/main/resources/initial-db/lrr/data/KSEN_RICH_TEXT_T.sql
@@ -0,0 +1,3 @@
+--  INSERTING into KSEN_RICH_TEXT_T
+INSERT INTO KSEN_RICH_TEXT_T (ID, FORMATTED, PLAIN, VER_NBR) VALUES ('grade-final-desc', '<p>Final Grade</p>', 'Final Grade', 0)
+/
---
+++ ks-enroll/ks-enroll-sql/src/main/resources/initial-db/lrr/reference-data/KSEN_LRR.sql
@@ -0,0 +1,3 @@
+--  INSERTING into KSEN_LRR
+INSERT INTO KSEN_LRR(ID, LPR_ID, NAME, RT_DESCR_ID, STATE_ID, TYPE_ID, RESULT_VALUE_ID, VER_NBR) VALUES('student1-grade-final', 'student1', 'student1 grade final', 'grade-final-desc', 'kuali.lrr.state.saved', 'kuali.lrr.type.final.grade.assigned', null, 0)
+/
