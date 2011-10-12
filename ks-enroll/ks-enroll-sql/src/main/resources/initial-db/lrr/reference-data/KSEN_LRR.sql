2c29084ade138c93a693939fc7b95669bd0447ed
vpremcha <vpremcha@kuali.org>
2011-10-12 13:05:35 -0400

    ref data for LRR not needed as we're creating entries when grader saves grade (KSENROLL-251 creates entries)
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@24425 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-sql/src/main/resources/initial-db/lrr/reference-data/KSEN_LRR.sql
+++
@@ -1,7 +0,0 @@
---  INSERTING into KSEN_LRR
-INSERT INTO KSEN_LRR(ID, LPR_ID, NAME, RT_DESCR_ID, STATE_ID, TYPE_ID, RESULT_VALUE_ID, VER_NBR) VALUES('student1-grade-final', 'student1', 'student1 grade final', 'grade-final-desc', 'kuali.lrr.state.saved', 'kuali.lrr.type.final.grade.assigned', 'kuali.result.value.grade.letter.a', 0)
-/
-INSERT INTO KSEN_LRR(ID, LPR_ID, NAME, RT_DESCR_ID, STATE_ID, TYPE_ID, RESULT_VALUE_ID, VER_NBR) VALUES('student2-grade-final', 'student2', 'student2 grade final', 'grade-final-desc', 'kuali.lrr.state.saved', 'kuali.lrr.type.final.grade.assigned', 'kuali.result.value.grade.letter.b', 0)
-/
-INSERT INTO KSEN_LRR(ID, LPR_ID, NAME, RT_DESCR_ID, STATE_ID, TYPE_ID, RESULT_VALUE_ID, VER_NBR) VALUES('student3-grade-final', 'student3', 'student3 grade final', 'grade-final-desc', 'kuali.lrr.state.saved', 'kuali.lrr.type.final.grade.assigned', 'kuali.result.value.grade.pf.f', 0)
-/
