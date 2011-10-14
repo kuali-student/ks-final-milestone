1b070802b8daf27822d5291e044d238b144c4582
vpremcha <vpremcha@kuali.org>
2011-10-14 14:15:48 -0400

    Adding @WebService
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@24487 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class1/lrc/service/impl/LRCServiceImpl.java
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class1/lrc/service/impl/LRCServiceImpl.java
@@ -17,9 +17,11 @@ import org.kuali.student.r2.lum.lrc.service.LRCService;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.jws.WebParam;
+import javax.jws.WebService;
 import java.util.ArrayList;
 import java.util.List;
 
+@WebService(name = "LrcService", serviceName = "LrcService", portName = "LrcService", targetNamespace = "http://student.kuali.org/wsdl/lrc")
 @Transactional(readOnly=true,noRollbackFor={DoesNotExistException.class},rollbackFor={Throwable.class})
 public class LRCServiceImpl implements LRCService {
 
