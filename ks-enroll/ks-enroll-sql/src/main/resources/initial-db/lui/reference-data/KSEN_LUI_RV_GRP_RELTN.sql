d9d3c5dd45ef3759b0e602678487502535fed5d1
vpremcha <vpremcha@kuali.org>
2011-10-18 01:06:44 -0400

    missed file for KSENROLL-258
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@24516 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-sql/src/main/resources/initial-db/lui/reference-data/KSEN_LUI_RV_GRP_RELTN.sql
@@ -0,0 +1,8 @@
+INSERT INTO KSEN_LUI_RV_GRP_RELTN (LUI_ID,RV_GRP_ID) VALUES ('Lui-1','kuali.resultComponent.grade.letter')
+/
+INSERT INTO KSEN_LUI_RV_GRP_RELTN (LUI_ID,RV_GRP_ID) VALUES ('Lui-2','kuali.resultComponent.grade.letter')
+/
+INSERT INTO KSEN_LUI_RV_GRP_RELTN (LUI_ID,RV_GRP_ID) VALUES ('Lui-3','kuali.resultComponent.grade.passFail')
+/
+INSERT INTO KSEN_LUI_RV_GRP_RELTN (LUI_ID,RV_GRP_ID) VALUES ('Lui-4','kuali.resultComponent.grade.letter')
+/
