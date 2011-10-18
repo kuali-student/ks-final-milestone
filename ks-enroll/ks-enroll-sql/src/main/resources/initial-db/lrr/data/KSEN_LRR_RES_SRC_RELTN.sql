b24e66110f4ef0521d1bced4a52d960d39bf99c8
vpremcha <vpremcha@kuali.org>
2011-10-17 21:26:07 -0400

    Grading - fixing rollback exception in dev2 and stg. Last week, deleted KSEN-LRR.sql but not xmls
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@24510 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-sql/src/main/resources/initial-db/lrr/data/KSEN_LRR_RES_SRC_RELTN.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO KSEN_LRR_RES_SRC_RELTN (LRR_ID,RES_SRC_ID) VALUES ('student1-grade-final','ResultSource-Sample1')
-/
-INSERT INTO KSEN_LRR_RES_SRC_RELTN (LRR_ID,RES_SRC_ID) VALUES ('student2-grade-final','ResultSource-Sample1')
-/
-INSERT INTO KSEN_LRR_RES_SRC_RELTN (LRR_ID,RES_SRC_ID) VALUES ('student3-grade-final','ResultSource-Sample1')
-/
-INSERT INTO KSEN_LRR_RES_SRC_RELTN (LRR_ID,RES_SRC_ID) VALUES ('student4-grade-final','ResultSource-Sample1')
-/
