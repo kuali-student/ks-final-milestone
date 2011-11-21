e94bd9ba39c26d7c87e4cd87c5783f2fa7df3982
vgadiyak <vgadiyak@kuali.org>
2011-11-21 13:44:49 -0500

    KSCM-956 KSLAB-2521 new getCluSetsByCluId
    git-svn-id: https://svn.kuali.org/repos/student/trunk@25162 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-api/src/main/java/org/kuali/student/lum/lu/service/LuService.java
+++ ks-lum/ks-lum-api/src/main/java/org/kuali/student/lum/lu/service/LuService.java
@@ -606,6 +606,21 @@ public interface LuService extends DictionaryService, SearchService, VersionMana
 	 */
     public List<CluSetInfo> getCluSetInfoByIdList(@WebParam(name="cluSetIdList")List<String> cluSetIdList) throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException;
 
+    
+    /** 
+     * Retrieve information on CLU sets based on CluId and type of the CluSet (name).
+     * @param cluId Identifier for CLU
+     * @param cluSetNames name for CluSet
+     * @return The retrieved list of CLU set information
+     * @throws DoesNotExistException One or more cluSets not found
+     * @throws InvalidParameterException One or more cluSetIds invalid
+     * @throws MissingParameterException missing cluSetIdList
+     * @throws OperationFailedException unable to complete request
+     * @throws PermissionDeniedException authorization failure
+	 */
+    public List<CluSetInfo> getCluSetsByCluId(@WebParam(name="cluId")List<String> cluIdList) throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException;
+
+    
     /** 
      * Retrieve the list of CLU Set Ids within a CLU Set
      * @param cluSetId Identifier of the CLU set
