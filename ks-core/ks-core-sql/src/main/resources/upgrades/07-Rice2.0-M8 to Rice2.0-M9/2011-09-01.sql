60e34bec03009df119477e4d02421a2f97bcbfb3
huangb <huangb@kuali.org>
2011-12-06 14:17:48 -0500

    KSENROLL-302: upgrade to rice 2.0.0-b1, bad sql- fixed
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@25355 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-sql/src/main/resources/upgrades/07-Rice2.0-M8 to Rice2.0-M9/2011-09-01.sql
+++ ks-core/ks-core-sql/src/main/resources/upgrades/07-Rice2.0-M8 to Rice2.0-M9/2011-09-01.sql
@@ -1,7 +1,7 @@
-alter table KSEMBEDDED.KREW_PPL_FLW_MBR_T drop constraint KREW_PPL_FLW_MBR_TC1
+alter table KREW_PPL_FLW_MBR_T drop constraint KREW_PPL_FLW_MBR_TC1
 /
 
-alter table KSEMBEDDED.KREW_PPL_FLW_MBR_T add (constraint KREW_PPL_FLW_MBR_TC1 unique ( PPL_FLW_ID, MBR_TYP_CD, MBR_ID ))
+alter table KREW_PPL_FLW_MBR_T add (constraint KREW_PPL_FLW_MBR_TC1 unique ( PPL_FLW_ID, MBR_TYP_CD, MBR_ID ))
 /
 
 alter sequence KRIM_ROLE_ID_S increment by 3
