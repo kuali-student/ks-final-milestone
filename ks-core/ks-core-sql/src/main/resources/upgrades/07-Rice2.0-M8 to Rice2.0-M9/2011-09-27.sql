f27c3a52b53c890bebce6941774ce89e6ca8ec39
huangb <huangb@kuali.org>
2011-12-07 15:46:52 -0500

    KSENROLL-302: upgrade to rice 2.0.0-b1, remove comments
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@25371 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-sql/src/main/resources/upgrades/07-Rice2.0-M8 to Rice2.0-M9/2011-09-27.sql
+++ ks-core/ks-core-sql/src/main/resources/upgrades/07-Rice2.0-M8 to Rice2.0-M9/2011-09-27.sql
@@ -1,11 +1,7 @@
--- Notification PeopleFlowActionType
-
 insert into krms_typ_t
 (typ_id, nm, nmspc_cd, srvc_nm, actv, ver_nbr)
 values ('1000', 'notificationPeopleFlowActionType', 'KRMS', 'notificationPeopleFlowActionTypeService', 'Y', 1)
 ;
-
--- Approval PeopleFlowActionType
 
 insert into krms_typ_t
 (typ_id, nm, nmspc_cd, srvc_nm, actv, ver_nbr)
