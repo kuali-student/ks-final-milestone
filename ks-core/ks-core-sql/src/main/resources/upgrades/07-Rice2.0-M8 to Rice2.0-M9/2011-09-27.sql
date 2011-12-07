e341c6c022da5be390ec94d2b9f52c7d0eb8c4fe
huangb <huangb@kuali.org>
2011-12-07 15:48:23 -0500

    KSENROLL-302: upgrade to rice 2.0.0-b1, replace ; with /
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@25372 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-sql/src/main/resources/upgrades/07-Rice2.0-M8 to Rice2.0-M9/2011-09-27.sql
+++ ks-core/ks-core-sql/src/main/resources/upgrades/07-Rice2.0-M8 to Rice2.0-M9/2011-09-27.sql
@@ -1,9 +1,5 @@
-insert into krms_typ_t
-(typ_id, nm, nmspc_cd, srvc_nm, actv, ver_nbr)
-values ('1000', 'notificationPeopleFlowActionType', 'KRMS', 'notificationPeopleFlowActionTypeService', 'Y', 1)
-;
+insert into krms_typ_t (typ_id, nm, nmspc_cd, srvc_nm, actv, ver_nbr) values ('1000', 'notificationPeopleFlowActionType', 'KRMS', 'notificationPeopleFlowActionTypeService', 'Y', 1)
+/
 
-insert into krms_typ_t
-(typ_id, nm, nmspc_cd, srvc_nm, actv, ver_nbr)
-values ('1001', 'approvalPeopleFlowActionType', 'KRMS_TEST', 'approvalPeopleFlowActionTypeService', 'Y', 1)
-;
+insert into krms_typ_t (typ_id, nm, nmspc_cd, srvc_nm, actv, ver_nbr) values ('1001', 'approvalPeopleFlowActionType', 'KRMS_TEST', 'approvalPeopleFlowActionTypeService', 'Y', 1)
+/
