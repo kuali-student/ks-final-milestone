93159c1776cd3dc23dfab5e7d413e1b82503c32c
nwright <nwright@kuali.org>
2011-12-11 20:32:51 -0500

    removed name and descr in proporder so contractdoc would build
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@25432 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/lum/lo/dto/LoLoRelationInfo.java
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/lum/lo/dto/LoLoRelationInfo.java
@@ -20,7 +20,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 import java.io.Serializable;
 
-@XmlType(name = "LoInfo", propOrder = {"id", "typeKey", "stateKey", "name", "descr", "loId", "relatedLoId", "effectiveDate", "expirationDate", "meta", "attributes", "_futureElements"})
+@XmlType(name = "LoInfo", propOrder = {"id", "typeKey", "stateKey", "loId", "relatedLoId", "effectiveDate", "expirationDate", "meta", "attributes", "_futureElements"})
 @XmlAccessorType(XmlAccessType.FIELD)
 public class LoLoRelationInfo extends RelationshipInfo implements LoLoRelation, Serializable {
 
