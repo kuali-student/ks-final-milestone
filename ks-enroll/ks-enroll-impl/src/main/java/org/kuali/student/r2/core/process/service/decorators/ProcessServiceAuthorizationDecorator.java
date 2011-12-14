d5a2491b85f90479f525462cf783cee46d3045e8
tapresco <tapresco@kuali.org>
2011-12-14 17:13:04 -0500

    KSPROJPLAN-740 Flushed out authorization decorator methods
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-1.3-process-poc@25509 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/process/service/decorators/ProcessServiceAuthorizationDecorator.java
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/process/service/decorators/ProcessServiceAuthorizationDecorator.java
@@ -1,5 +1,6 @@
 package org.kuali.student.r2.core.process.service.decorators;
 
+import org.kuali.rice.kim.api.permission.PermissionService;
 import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.dto.StatusInfo;
 import org.kuali.student.r2.common.exceptions.AlreadyExistsException;
@@ -10,6 +11,7 @@ import org.kuali.student.r2.common.exceptions.MissingParameterException;
 import org.kuali.student.r2.common.exceptions.OperationFailedException;
 import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
 import org.kuali.student.r2.common.exceptions.ReadOnlyException;
+import org.kuali.student.r2.common.infc.HoldsPermissionService;
 import org.kuali.student.r2.core.process.dto.CheckInfo;
 import org.kuali.student.r2.core.process.dto.InstructionInfo;
 import org.kuali.student.r2.core.process.dto.ProcessCategoryInfo;
@@ -18,49 +20,91 @@ import org.kuali.student.r2.core.process.service.ProcessServiceDecorator;
 
 import java.util.List;
 
-public class ProcessServiceAuthorizationDecorator extends ProcessServiceDecorator {
+public class ProcessServiceAuthorizationDecorator extends ProcessServiceDecorator implements HoldsPermissionService {
+
+    private PermissionService permissionService;
+
+    public static final String ENRLLMENT_NAMESPACE = "KS-ENROLL";
+    public static final String SERVICE_NAME = "ProcessService.";
+
+    @Override
+    public PermissionService getPermissionService() {
+        return permissionService;
+    }
+    @Override
+    public void setPermissionService(PermissionService permissionService) {
+        this.permissionService = permissionService;
+    }
+
     @Override
     public ProcessCategoryInfo createProcessCategory(ProcessCategoryInfo processInfo, ContextInfo contextInfo) throws AlreadyExistsException, DataValidationErrorException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException, ReadOnlyException {
-        return super.createProcessCategory(processInfo, contextInfo);    // TODO needs implementation
+        if (!permissionService.isAuthorized(contextInfo.getPrincipalId(), ENRLLMENT_NAMESPACE, SERVICE_NAME + "createProcessCategory", null, null)) {
+            throw new PermissionDeniedException();
+        }
+        return getNextDecorator().createProcessCategory(processInfo, contextInfo);
     }
 
     @Override
     public StatusInfo addProcessToProcessCategory(String processKey, String processCategoryId, ContextInfo contextInfo) throws AlreadyExistsException, DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException {
-        return super.addProcessToProcessCategory(processKey, processCategoryId, contextInfo);    // TODO needs implementation
+        if (!permissionService.isAuthorized(contextInfo.getPrincipalId(), ENRLLMENT_NAMESPACE, SERVICE_NAME + "addProcessToProcessCategory", null, null)) {
+            throw new PermissionDeniedException();
+        }
+        return getNextDecorator().addProcessToProcessCategory(processKey, processCategoryId, contextInfo);
     }
 
     @Override
     public List<ProcessInfo> getProcessesForProcessCategory(String processCategoryId, ContextInfo contextInfo) throws InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException {
-        return super.getProcessesForProcessCategory(processCategoryId, contextInfo);    // TODO needs implementation
+        if (!permissionService.isAuthorized(contextInfo.getPrincipalId(), ENRLLMENT_NAMESPACE, SERVICE_NAME + "getProcessesForProcessCategory", null, null)) {
+            throw new PermissionDeniedException();
+        }
+        return getNextDecorator().getProcessesForProcessCategory(processCategoryId, contextInfo);
     }
 
     @Override
     public ProcessInfo createProcess(String processKey, ProcessInfo processInfo, ContextInfo contextInfo) throws AlreadyExistsException, DataValidationErrorException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException, ReadOnlyException {
-        return super.createProcess(processKey, processInfo, contextInfo);    // TODO needs implementation
+        if (!permissionService.isAuthorized(contextInfo.getPrincipalId(), ENRLLMENT_NAMESPACE, SERVICE_NAME + "createProcess", null, null)) {
+            throw new PermissionDeniedException();
+        }
+        return getNextDecorator().createProcess(processKey, processInfo, contextInfo);
     }
 
     @Override
     public CheckInfo createCheck(CheckInfo checkInfo, ContextInfo contextInfo) throws AlreadyExistsException, DataValidationErrorException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException, ReadOnlyException {
-        return super.createCheck(checkInfo, contextInfo);    // TODO needs implementation
+        if (!permissionService.isAuthorized(contextInfo.getPrincipalId(), ENRLLMENT_NAMESPACE, SERVICE_NAME + "createCheck", null, null)) {
+            throw new PermissionDeniedException();
+        }
+        return getNextDecorator().createCheck(checkInfo, contextInfo);
     }
 
     @Override
     public StatusInfo deleteCheck(String checkKey, ContextInfo contextInfo) throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException {
-        return super.deleteCheck(checkKey, contextInfo);    // TODO needs implementation
+        if (!permissionService.isAuthorized(contextInfo.getPrincipalId(), ENRLLMENT_NAMESPACE, SERVICE_NAME + "deleteCheck", null, null)) {
+            throw new PermissionDeniedException();
+        }
+        return getNextDecorator().deleteCheck(checkKey, contextInfo);
     }
 
     @Override
     public List<InstructionInfo> getInstructionsByProcess(String processKey, ContextInfo contextInfo) throws InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException {
-        return super.getInstructionsByProcess(processKey, contextInfo);    // TODO needs implementation
+        if (!permissionService.isAuthorized(contextInfo.getPrincipalId(), ENRLLMENT_NAMESPACE, SERVICE_NAME + "getInstructionsByProcess", null, null)) {
+            throw new PermissionDeniedException();
+        }
+        return getNextDecorator().getInstructionsByProcess(processKey, contextInfo);
     }
 
     @Override
     public InstructionInfo createInstruction(String processKey, String checkKey, InstructionInfo instructionInfo, ContextInfo contextInfo) throws AlreadyExistsException, DataValidationErrorException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException, ReadOnlyException {
-        return super.createInstruction(processKey, checkKey, instructionInfo, contextInfo);    // TODO needs implementation
+        if (!permissionService.isAuthorized(contextInfo.getPrincipalId(), ENRLLMENT_NAMESPACE, SERVICE_NAME + "createInstruction", null, null)) {
+            throw new PermissionDeniedException();
+        }
+        return getNextDecorator().createInstruction(processKey, checkKey, instructionInfo, contextInfo);
     }
 
     @Override
     public List<InstructionInfo> getInstructionsForEvaluation(String processKey, ContextInfo contextInfo) throws InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException {
-        return super.getInstructionsForEvaluation(processKey, contextInfo);    // TODO needs implementation
+        if (!permissionService.isAuthorized(contextInfo.getPrincipalId(), ENRLLMENT_NAMESPACE, SERVICE_NAME + "getInstructionsForEvaluation", null, null)) {
+            throw new PermissionDeniedException();
+        }
+        return getNextDecorator().getInstructionsForEvaluation(processKey, contextInfo);
     }
 }
