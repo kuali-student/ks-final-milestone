afd4b7b93965fcf5bac670b26070b81e1a0df6dc
vpremcha <vpremcha@kuali.org>
2011-12-14 13:03:35 -0500

    process - create tables
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-1.3-process-poc@25499 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-sql/src/main/resources/initial-db/proces/ks-enroll-process-constraints.sql
+++ ks-enroll/ks-enroll-sql/src/main/resources/initial-db/proces/ks-enroll-process-constraints.sql
@@ -5,13 +5,33 @@
 ALTER TABLE "KSEN_PROCESS" MODIFY ("ID" NOT NULL ENABLE)
 /
 
-ALTER TABLE "KSEN_PROCESS" MODIFY ("PROCESS_STATE_ID" NOT NULL ENABLE)
+ALTER TABLE "KSEN_PROCESS" MODIFY ("STATE_ID" NOT NULL ENABLE)
 /
 
-ALTER TABLE "KSEN_PROCESS" MODIFY ("PROCESS_TYPE_ID" NOT NULL ENABLE)
+ALTER TABLE "KSEN_PROCESS" MODIFY ("TYPE_ID" NOT NULL ENABLE)
 /
 
 ALTER TABLE "KSEN_PROCESS" ADD PRIMARY KEY ("ID") ENABLE
+/
+
+--------------------------------------------------------
+--  Constraints for Table KSEN_PROCESS_RICH_TEXT
+--------------------------------------------------------
+
+ALTER TABLE "KSEN_PROCESS_RICH_TEXT" MODIFY ("ID" NOT NULL ENABLE)
+/
+
+ALTER TABLE "KSEN_PROCESS_RICH_TEXT" ADD PRIMARY KEY ("ID") ENABLE
+/
+
+--------------------------------------------------------
+--  Constraints for Table KSEN_PROCESS_TYPE
+--------------------------------------------------------
+
+  ALTER TABLE "KSEN_PROCESS_TYPE" MODIFY ("TYPE_KEY" NOT NULL ENABLE)
+/
+
+  ALTER TABLE "KSEN_PROCESS_TYPE" ADD PRIMARY KEY ("TYPE_KEY") ENABLE
 /
 
 --------------------------------------------------------
@@ -31,13 +51,33 @@ ALTER TABLE "KSEN_PROCESS_ATTR" ADD PRIMARY KEY ("ID") ENABLE
 ALTER TABLE "KSEN_CHECK" MODIFY ("ID" NOT NULL ENABLE)
 /
 
-ALTER TABLE "KSEN_CHECK" MODIFY ("CHECK_STATE_ID" NOT NULL ENABLE)
+ALTER TABLE "KSEN_CHECK" MODIFY ("STATE_ID" NOT NULL ENABLE)
 /
 
-ALTER TABLE "KSEN_CHECK" MODIFY ("CHECK_TYPE_ID" NOT NULL ENABLE)
+ALTER TABLE "KSEN_CHECK" MODIFY ("TYPE_ID" NOT NULL ENABLE)
 /
 
 ALTER TABLE "KSEN_CHECK" ADD PRIMARY KEY ("ID") ENABLE
+/
+
+--------------------------------------------------------
+--  Constraints for Table KSEN_CHECK_RICH_TEXT
+--------------------------------------------------------
+
+ALTER TABLE "KSEN_CHECK_RICH_TEXT" MODIFY ("ID" NOT NULL ENABLE)
+/
+
+ALTER TABLE "KSEN_CHECK_RICH_TEXT" ADD PRIMARY KEY ("ID") ENABLE
+/
+
+--------------------------------------------------------
+--  Constraints for Table KSEN_CHECK_TYPE
+--------------------------------------------------------
+
+  ALTER TABLE "KSEN_CHECK_TYPE" MODIFY ("TYPE_KEY" NOT NULL ENABLE)
+/
+
+  ALTER TABLE "KSEN_CHECK_TYPE" ADD PRIMARY KEY ("TYPE_KEY") ENABLE
 /
 
 --------------------------------------------------------
@@ -57,10 +97,10 @@ ALTER TABLE "KSEN_CHECK_ATTR" ADD PRIMARY KEY ("ID") ENABLE
 ALTER TABLE "KSEN_INSTR" MODIFY ("ID" NOT NULL ENABLE)
 /
 
-ALTER TABLE "KSEN_INSTR" MODIFY ("INSTR_STATE_ID" NOT NULL ENABLE)
+ALTER TABLE "KSEN_INSTR" MODIFY ("STATE_ID" NOT NULL ENABLE)
 /
 
-ALTER TABLE "KSEN_INSTR" MODIFY ("INSTR_TYPE_ID" NOT NULL ENABLE)
+ALTER TABLE "KSEN_INSTR" MODIFY ("TYPE_ID" NOT NULL ENABLE)
 /
 
 ALTER TABLE "KSEN_INSTR" ADD PRIMARY KEY ("ID") ENABLE
@@ -74,6 +114,16 @@ ALTER TABLE "KSEN_INSTR_ATTR" MODIFY ("ID" NOT NULL ENABLE)
 /
 
 ALTER TABLE "KSEN_INSTR_ATTR" ADD PRIMARY KEY ("ID") ENABLE
+/
+
+--------------------------------------------------------
+--  Constraints for Table KSEN_INSTR_TYPE
+--------------------------------------------------------
+
+ALTER TABLE "KSEN_INSTR_TYPE" MODIFY ("TYPE_KEY" NOT NULL ENABLE)
+/
+
+ALTER TABLE "KSEN_INSTR_TYPE" ADD PRIMARY KEY ("TYPE_KEY") ENABLE
 /
 
 --------------------------------------------------------
