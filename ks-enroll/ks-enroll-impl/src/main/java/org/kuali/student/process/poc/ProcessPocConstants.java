ff18da339c2ea6c84564cf7f6816a10f52465e4b
nwright <nwright@kuali.org>
2011-12-16 10:25:00 -0500

    deleted poc population decroator, wrote one for Atp and tested it updating constants.
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-1.3-process-poc@25569 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/process/poc/ProcessPocConstants.java
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/process/poc/ProcessPocConstants.java
@@ -19,4 +19,10 @@ public class ProcessPocConstants {
     public static final String PERSON_ID_JOHNNY_MANNING_2374 = "2374";
     public static final String PERSON_ID_EDDIE_PITTMAN_2406 = "2406";
     public static final String PERSON_ID_TRACY_BURTON_2132 = "2132";
+    
+    public static final String SPRING_2011_TERM_KEY = "kuali.atp.SP2011";    
+    public static final String SUMMER_2011_TERM_KEY = "kuali.atp.SU2011";        
+    public static final String FALL_2011_TERM_KEY = "kuali.atp.FA2011";
+    public static final String SPRING_2012_TERM_KEY = "kuali.atp.SP2012";       
+
 }
