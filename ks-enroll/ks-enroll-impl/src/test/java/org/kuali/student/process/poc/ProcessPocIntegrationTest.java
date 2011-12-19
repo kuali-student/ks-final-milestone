226af7f4dd3c649847ed0549f3ac7ff5b3a3127f
nwright <nwright@kuali.org>
2011-12-19 09:50:08 -0500

    finished all 14 integration test cases
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-1.3-process-poc@25629 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/process/poc/ProcessPocIntegrationTest.java
+++ ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/process/poc/ProcessPocIntegrationTest.java
@@ -104,6 +104,8 @@ public class ProcessPocIntegrationTest {
         List<ValidationResultInfo> results = null;
         results = service.checkStudentEligibility(ProcessPocConstants.PERSON_ID_KARA_STONE_2272, context);
         assertEquals(1, results);
+        assertTrue(results.get(0).getIsError());
+        assertEquals("A key piece of data is wrong on your biographic record.  Please come to the Registrar's office to clear it up.", results.get(0).getMessage());
     }
 
     @Test
@@ -113,7 +115,168 @@ public class ProcessPocIntegrationTest {
         context.setPrincipalId("testPrincipal1");
 
         List<ValidationResultInfo> results = null;
-        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_KARA_STONE_2272, ProcessPocConstants.FALL_2011_TERM_KEY, context);
-        assertEquals(1, results.size ());
+        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_KARA_STONE_2272,
+                ProcessPocConstants.FALL_2011_TERM_KEY, context);
+        assertEquals(1, results.size());
+        assertTrue(results.get(0).getIsError());
+        assertEquals("A key piece of data is wrong on your biographic record.  Please come to the Registrar's office to clear it up.", results.get(0).getMessage());
     }
+
+    @Test
+    public void testCase4TooEarly() throws Exception {
+        System.out.println("case 4: Too Early");
+        ContextInfo context = new ContextInfo();
+        context.setPrincipalId("testPrincipal1");
+
+        List<ValidationResultInfo> results = null;
+        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_BARBARA_HARRIS_2016,
+                ProcessPocConstants.SPRING_2012_TERM_KEY, context);
+        assertEquals(1, results.size());
+        assertTrue(results.get(0).getIsError());
+        assertEquals("Registration period for this term has not yet begun", results.get(0).getMessage());
+    }
+
+    @Test
+    public void testCase5TooLate() throws Exception {
+        System.out.println("case 5: Too Late");
+        ContextInfo context = new ContextInfo();
+        context.setPrincipalId("testPrincipal1");
+
+        List<ValidationResultInfo> results = null;
+        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_BARBARA_HARRIS_2016,
+                ProcessPocConstants.SPRING_2011_TERM_KEY, context);
+        assertEquals(1, results.size());
+        assertTrue(results.get(0).getIsError());
+        assertEquals("Registration period for this term is closed", results.get(0).getMessage());
+    }
+
+    @Test
+    public void testCase6HasPaidLastTermsBill() throws Exception {
+        System.out.println("case 6: Has Paid Last Term's Bill");
+        ContextInfo context = new ContextInfo();
+        context.setPrincipalId("testPrincipal1");
+
+        List<ValidationResultInfo> results = null;
+        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_BARBARA_HARRIS_2016,
+                ProcessPocConstants.FALL_2011_TERM_KEY, context);
+        assertEquals(0, results.size());
+    }
+    
+    @Test
+    public void testCase7HasNotPaidLastTermsBill() throws Exception {
+        System.out.println("case 7: Has Not Paid Last Term's Bill");
+        ContextInfo context = new ContextInfo();
+        context.setPrincipalId("testPrincipal1");
+
+        List<ValidationResultInfo> results = null;
+        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_CLIFFORD_RIDDLE_2397,
+                ProcessPocConstants.FALL_2011_TERM_KEY, context);
+        assertEquals(1, results.size());
+        assertTrue(results.get(0).getIsError());
+        assertEquals("You have unpaid tuition charges from last term, please contact the bursars office to resolve this matter", results.get(0).getMessage());
+    }
+    
+        
+    @Test
+    public void testCase8HasAnOverdueBook() throws Exception {
+        System.out.println("case 8: Has an Overdue Book");
+        ContextInfo context = new ContextInfo();
+        context.setPrincipalId("testPrincipal1");
+
+        List<ValidationResultInfo> results = null;
+        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_BETTY_MARTIN_2005,
+                ProcessPocConstants.FALL_2011_TERM_KEY, context);
+        assertEquals(1, results.size());
+        assertTrue(results.get(0).getIsWarn());        
+        assertFalse(results.get(0).getIsError());
+        assertEquals("Please note: you have an overdue library book", results.get(0).getMessage());
+    }
+    
+    
+        
+    @Test
+    public void testCase9HasBothHolds() throws Exception {
+        System.out.println("case 9: Has Both Holds");
+        ContextInfo context = new ContextInfo();
+        context.setPrincipalId("testPrincipal1");
+
+        List<ValidationResultInfo> results = null;
+        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_NINA_WELCH_2166,
+                ProcessPocConstants.FALL_2011_TERM_KEY, context);
+        assertEquals(2, results.size());
+        assertTrue(results.get(0).getIsWarn());        
+        assertFalse(results.get(0).getIsError());
+        assertEquals("Please note: you have an overdue library book", results.get(0).getMessage());
+        assertTrue(results.get(1).getIsError());
+        assertEquals("You have unpaid tuition charges from last term, please contact the bursars office to resolve this matter", results.get(1).getMessage());
+    }
+    
+         
+    @Test
+    public void testCase10SummerOnlyStudentCannotRegister() throws Exception {
+        System.out.println("case 10: Summer Only Student Cannot Register");
+        ContextInfo context = new ContextInfo();
+        context.setPrincipalId("testPrincipal1");
+
+        List<ValidationResultInfo> results = null;
+        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_AMBER_HOPKINS_2155,
+                ProcessPocConstants.FALL_2011_TERM_KEY, context);
+        assertEquals(1, results.size());
+        assertTrue(results.get(0).getIsError());
+        assertEquals("Summer only students cannot register for fall, winter or spring terms", results.get(0).getMessage());
+    }  
+    
+    @Test
+    public void testCase11SummerOnlyStudentCanRegisterBecauseItIsSummer() throws Exception {
+        System.out.println("case 11: Summer Only Student Can Register Because it Is Summer");
+        ContextInfo context = new ContextInfo();
+        context.setPrincipalId("testPrincipal1");
+
+        List<ValidationResultInfo> results = null;
+        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_AMBER_HOPKINS_2155,
+                ProcessPocConstants.SUMMER_2011_TERM_KEY, context);
+        assertEquals(0, results.size());
+    }
+    
+    
+    @Test
+    public void testCase12TooEarlyButHasAnExemption() throws Exception {
+        System.out.println("case 12: Too Early But Has An Exemption");
+        ContextInfo context = new ContextInfo();
+        context.setPrincipalId("testPrincipal1");
+
+        List<ValidationResultInfo> results = null;
+        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_JOHNNY_MANNING_2374,
+                ProcessPocConstants.SPRING_2012_TERM_KEY, context);
+        assertEquals(0, results.size());
+    }
+    
+     
+    @Test
+    public void testCase13TooLateButHasAnExtensionExemption() throws Exception {
+        System.out.println("case 13: Too Late But Has An Extension Exemption");
+        ContextInfo context = new ContextInfo();
+        context.setPrincipalId("testPrincipal1");
+
+        List<ValidationResultInfo> results = null;
+        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_EDDIE_PITTMAN_2406,
+                ProcessPocConstants.SPRING_2011_TERM_KEY, context);
+        assertEquals(0, results.size());
+    }
+    
+             
+    @Test
+    public void testCase14TooLateEvenWithExtensionExemption() throws Exception {
+        System.out.println("case 14: Too Late Even With Extension Exemption");
+        ContextInfo context = new ContextInfo();
+        context.setPrincipalId("testPrincipal1");
+
+        List<ValidationResultInfo> results = null;
+        results = service.checkStudentEligibilityForTerm(ProcessPocConstants.PERSON_ID_TRACY_BURTON_2132,
+                ProcessPocConstants.SPRING_2011_TERM_KEY, context);
+        assertEquals(1, results.size());
+        assertTrue(results.get(0).getIsError());
+        // TODO: Shouldn't this message say something about the fact that the student has an extention but it is even too late for that?
+        assertEquals("Registration period for this term is closed", results.get(0).getMessage());
+    } 
 }
