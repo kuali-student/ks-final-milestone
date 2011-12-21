78ec78d96321debf1420a8bfa43e9b08c90f35ea
tapresco <tapresco@kuali.org>
2011-12-21 18:35:09 -0500

    KSPROJPLAN-740 added populations to instruction
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-1.3-process-poc@25701 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/population/model/PopulationEntity.java
@@ -0,0 +1,13 @@
+package org.kuali.student.r2.core.population.model;
+
+import org.kuali.student.r2.common.entity.AttributeOwner;
+import org.kuali.student.r2.common.entity.MetaEntity;
+import org.kuali.student.r2.core.process.model.InstructionAttributeEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "KSEN_POPULATION")
+public class PopulationEntity extends MetaEntity {
+}
