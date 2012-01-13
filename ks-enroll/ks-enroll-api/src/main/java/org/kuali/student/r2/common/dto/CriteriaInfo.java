7f4d9264cbda824e5867cd1eebfe1dae0a619831
tcoppeto <tcoppeto@kuali.org>
2012-01-13 18:19:51 -0500

    continued cleanup of common DTOs: license templates, current constructor patterns, null checks, and prop orders
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@26074 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/common/dto/CriteriaInfo.java
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/common/dto/CriteriaInfo.java
@@ -1,18 +1,19 @@
 /*
  * Copyright 2009 The Kuali Foundation
  *
- * Licensed under the Educational Community License, Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Educational Community License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
  *
  * http://www.opensource.org/licenses/ecl1.php
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
  */
+
 package org.kuali.student.r2.common.dto;
 
 import java.io.Serializable;
@@ -32,64 +33,76 @@ import org.kuali.student.r2.common.infc.Criteria;
 import org.w3c.dom.Element;
 
 /**
- * Query to return some information regarding LUI to person relationships.
+ * Query to return some information regarding LUI to person
+ * relationships.
  * 
- * @Author KSContractMojo
- * @Author Kamal
- * @Since Tue Mar 01 15:54:06 PST 2011
- * @See <a href=
- *      "https://wiki.kuali.org/display/KULSTU/luiPersonRelationCriteria+Structure"
- *      >LuiPersonRelationCriteria</a>
+ * @author Kamal
+ * @since Tue Mar 01 15:54:06 PST 2011
  */
+
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "CriteriaInfo", propOrder = { "comparisons", "maxResults",
+@XmlType(name = "CriteriaInfo", propOrder = { 
+                "comparisons", "maxResults",
 		"_futureElements" })
-public class CriteriaInfo implements Criteria, Serializable {
 
-	private static final long serialVersionUID = 1L;
+public class CriteriaInfo 
+    implements Criteria, Serializable {
 
-	@XmlElementWrapper(name = "comparisons")
-	@XmlElement(name = "comparison")
-	private List<ComparisonInfo> comparisons;
-	@XmlElement
-	private Integer maxResults;
-	@XmlAnyElement
-	private List<Element> _futureElements;
+    private static final long serialVersionUID = 1L;
 
-	public CriteriaInfo() {
-		comparisons = null;
-		maxResults = null;
-		_futureElements = null;
-	}
+    @XmlElementWrapper(name = "comparisons")
+    @XmlElement(name = "comparison")
+    private List<ComparisonInfo> comparisons;
+	
+    @XmlElement
+    private Integer maxResults;
+    
+    @XmlAnyElement
+    private List<Element> _futureElements;
 
-	public CriteriaInfo(Criteria builder) {
-		if (builder.getComparisons() == null) {
-			this.comparisons = null;
-		} else {
-			this.comparisons = new ArrayList<ComparisonInfo>();
-			Collections.copy(comparisons, comparisons);
-		}
-		this.maxResults = builder.getMaxResults();
-		this._futureElements = null;
-	}
 
-	@Override
-	public List<? extends Comparison> getComparisons() {
-		return this.comparisons;
-	}
+    /**
+     * Constructs a new CriteriaInfo.
+     */
+    public CriteriaInfo() {
+    }
 
-	public void setComparisons(List<ComparisonInfo> comparisons) {
-		this.comparisons = new ArrayList<ComparisonInfo>();
-		Collections.copy(comparisons, comparisons);
-	}
+    /**
+     * Constructs a new CriteriaInfo from another Criteria.
+     *
+     * @param criteria the criteria to copy
+     */
+    public CriteriaInfo(Criteria criteria) {
+        if (criteria != null) {
+            if (criteria.getComparisons() != null) {
+                this.comparisons = new ArrayList<ComparisonInfo>();
+                for (Comparison comparison : criteria.getComparisons()) {
+                    this.comparisons.add(new ComparisonInfo(comparison));
+                }
+            }
+            this.maxResults = criteria.getMaxResults();
+        }
+    }
 
-	@Override
-	public Integer getMaxResults() {
-		return this.maxResults;
-	}
+    @Override
+    public List<? extends Comparison> getComparisons() {
+        if (comparisons == null) {
+            comparisons = new ArrayList<ComparisonInfo>(0);
+        }
 
-	public void setMaxResults(Integer maxResults) {
-		this.maxResults = maxResults;
-	}
+        return this.comparisons;
+    }
+    
+    public void setComparisons(List<ComparisonInfo> comparisons) {
+        this.comparisons = comparisons;
+    }
 
+    @Override
+    public Integer getMaxResults() {
+        return this.maxResults;
+    }
+    
+    public void setMaxResults(Integer maxResults) {
+        this.maxResults = maxResults;
+    }
 }
--- ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/common/dto/HasAttributesAndMetaInfo.java
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/common/dto/HasAttributesAndMetaInfo.java
@@ -1,10 +1,18 @@
 /*
- * Copyright 2010 The Kuali Foundation Licensed under the Educational Community License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain a copy of the License at
- * http://www.osedu.org/licenses/ECL-2.0 Unless required by applicable law or agreed to in writing, software distributed
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied. See the License for the specific language governing permissions and limitations under the License.
+ * Copyright 2010 The Kuali Foundation Licensed under the Educational
+ * Community License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
+
 package org.kuali.student.r2.common.dto;
 
 import java.io.Serializable;
@@ -13,6 +21,12 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlTransient;
 
 import org.kuali.student.r2.common.infc.HasAttributesAndMeta;
+
+/**
+ * Information about a entities with attributes and meta information.
+ *
+ * @author tom
+ */
 
 @SuppressWarnings("serial")
 @XmlTransient
@@ -23,16 +37,26 @@ public abstract class HasAttributesAndMetaInfo
     @XmlElement
     private MetaInfo meta;
     
+
+    /**
+     * Constructs a new HasAttributesAndMetaInfo.
+     */
     public HasAttributesAndMetaInfo() {
-        meta = null;
     }
     
-    public HasAttributesAndMetaInfo(HasAttributesAndMeta hasAttsAndMeta) {
-        super(hasAttsAndMeta);
+    /**
+     * Constructs a new HasAttributesAndMetaInfo from another
+     * HasAttributesAndMeta.
+     *
+     * @param hasAttrsAndMeta the HasAttributesAndMeta to copy
+     */
+    public HasAttributesAndMetaInfo(HasAttributesAndMeta hasAttrsAndMeta) {
+        super(hasAttrsAndMeta);
         
-        if (null != hasAttsAndMeta) {
-            this.meta = null != hasAttsAndMeta.getMeta() ? MetaInfo
-                .getInstance(hasAttsAndMeta.getMeta()) : null;
+        if (hasAttrsAndMeta != null) {
+            if (hasAttrsAndMeta.getMeta() != null) {
+                this.meta = new MetaInfo(hasAttrsAndMeta.getMeta());
+            }
         }
     }
     
