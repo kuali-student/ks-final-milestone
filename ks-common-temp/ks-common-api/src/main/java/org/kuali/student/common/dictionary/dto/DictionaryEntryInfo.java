9b93cb96279527f77fcd4f8457c9084bf880068b
nina <nina@kuali.org>
2012-01-24 03:47:15 -0500

    KSCM-209 fix compile errors
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@26324 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-temp/ks-common-api/src/main/java/org/kuali/student/common/dictionary/dto/DictionaryEntryInfo.java
+++ ks-common-temp/ks-common-api/src/main/java/org/kuali/student/common/dictionary/dto/DictionaryEntryInfo.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.student.common.datadictionary.dto;
+package org.kuali.student.common.dictionary.dto;
 
 import java.io.Serializable;
 import java.util.ArrayList;
