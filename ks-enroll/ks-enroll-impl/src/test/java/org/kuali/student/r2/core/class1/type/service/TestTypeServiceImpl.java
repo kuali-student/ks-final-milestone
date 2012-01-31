2d5dfeef5c60dd54105869bc17afdb408e594487
sambit <sambit@kuali.org>
2012-01-31 16:35:20 -0500

    KSENROLL-471 Adding test cases
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3-services@26553 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/r2/core/class1/type/service/TestTypeServiceImpl.java
@@ -0,0 +1,118 @@
+package org.kuali.student.r2.core.class1.type.service;
+
+import org.junit.Test;
+import org.kuali.student.common.dto.StatusInfo;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.exceptions.*;
+import org.kuali.student.r2.common.util.constants.AtpServiceConstants;
+import org.kuali.student.r2.core.type.dto.TypeInfo;
+import org.kuali.student.r2.core.type.service.TypeService;
+
+import java.util.Date;
+import java.util.List;
+
+import static junit.framework.Assert.*;
+
+/**
+ * This class //TODO ...
+ *
+ * @author Kuali Student Team
+ */
+
+public class TestTypeServiceImpl {
+     private TypeService typeService;
+
+    public TypeService getTypeService() {
+        return typeService;
+    }
+
+    public void setTypeService(TypeService typeService) {
+        this.typeService = typeService;
+    }
+
+    @Test
+    public void testGetType() throws InvalidParameterException, MissingParameterException, PermissionDeniedException, OperationFailedException {
+
+        TypeInfo exitingType = null;
+        try {
+            exitingType = typeService.getType("kuali.atp.type.FallSpring", new ContextInfo());
+            assertNotNull(exitingType);
+
+            TypeInfo nonExistingType =  typeService.getType("kuali.atp.type.FallSpring1", new ContextInfo());
+
+            assertNotNull(nonExistingType);
+
+        } catch (DoesNotExistException e) {
+                fail("Failed with exception");
+        }
+
+
+
+    }
+
+    @Test
+    public void  testGetTypesByRefObjectUri() throws InvalidParameterException,  PermissionDeniedException, OperationFailedException {
+
+        List<TypeInfo> exitingType = null;
+        try {
+            exitingType = typeService.getTypesByRefObjectUri(AtpServiceConstants.REF_OBJECT_URI_ATP, new ContextInfo());
+
+            assertNotNull(exitingType);
+
+            List<TypeInfo> nullType =  typeService.getTypesByRefObjectUri(null, new ContextInfo());
+
+            } catch (DoesNotExistException e) {
+                fail("Failed with exception");
+            } catch (MissingParameterException e) {
+
+            }
+
+    }
+
+    public void testCreateType() throws InvalidParameterException, DataValidationErrorException, MissingParameterException, AlreadyExistsException, ReadOnlyException, PermissionDeniedException, OperationFailedException {
+
+        TypeInfo typeToCreate = new TypeInfo();
+        typeToCreate.setKey("kuali.atp.type.TestAtp");
+        typeToCreate.setEffectiveDate(new Date());
+        typeToCreate.setName("ATP test create");
+        typeToCreate.setRefObjectUri(AtpServiceConstants.REF_OBJECT_URI_ATP);
+
+        TypeInfo createdType = typeService.createType("kuali.atp.type.TestAtp", typeToCreate, new ContextInfo());
+
+        assertNotNull(createdType);
+
+        try{
+                    TypeInfo typeToCreateFail = new TypeInfo();
+                    typeToCreateFail.setKey("kuali.atp.type.TestAtp");
+                    typeToCreateFail.setEffectiveDate(new Date());
+                    typeToCreateFail.setName("ATP test create");
+                    typeToCreateFail.setRefObjectUri(AtpServiceConstants.REF_OBJECT_URI_ATP);
+                    TypeInfo noType = typeService.createType("kuali.atp.type.TestAtp", typeToCreate, new ContextInfo());
+
+        } catch (MissingParameterException ex){
+                 assertNotNull(ex);
+        }
+
+    }
+
+    public void testDeleteType() throws InvalidParameterException, DataValidationErrorException, MissingParameterException, AlreadyExistsException, ReadOnlyException, PermissionDeniedException, OperationFailedException {
+            TypeInfo typeToCreate = new TypeInfo();
+            typeToCreate.setKey("kuali.atp.type.TestAtp");
+            typeToCreate.setEffectiveDate(new Date());
+            typeToCreate.setName("ATP test create");
+            typeToCreate.setRefObjectUri(AtpServiceConstants.REF_OBJECT_URI_ATP);
+
+            TypeInfo createdType = typeService.createType("kuali.atp.type.TestAtp", typeToCreate, new ContextInfo());
+            assertNotNull(createdType);
+
+                try {
+                    org.kuali.student.r2.common.dto.StatusInfo status = typeService.deleteType("kuali.atp.type.TestAtp", new ContextInfo());
+                    assertEquals( status.getIsSuccess(), new Boolean(true));
+
+                } catch (DoesNotExistException e) {
+                    fail();
+                }
+
+    }
+
+}
