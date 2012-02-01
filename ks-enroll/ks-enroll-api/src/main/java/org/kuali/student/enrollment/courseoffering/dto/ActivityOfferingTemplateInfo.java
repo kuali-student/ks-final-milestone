e0011bcb57fbebb0145da4545529d638277c7a37
sambit <sambit@kuali.org>
2012-02-01 14:37:08 -0500

    KSENROLL-480 Initial DTOs for ReggroupTemplate and AOsTemplate
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3-services@26588 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/enrollment/courseoffering/dto/ActivityOfferingTemplateInfo.java
@@ -0,0 +1,15 @@
+package org.kuali.student.enrollment.courseoffering.dto;
+
+/**
+ * This class //TODO ...
+ *
+ * @author Kuali Student Team
+ */
+public class ActivityOfferingTemplateInfo {
+
+      private  String activityOfferingType;
+
+      private  int position;
+
+      private int quantity;
+}
---
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/enrollment/courseoffering/dto/RegistrationGroupTemplateInfo.java
@@ -0,0 +1,19 @@
+package org.kuali.student.enrollment.courseoffering.dto;
+
+import org.kuali.student.enrollment.courseoffering.infc.RegistrationGroupTemplate;
+import org.kuali.student.r2.core.enumerationmanagement.dto.EnumeratedValueInfo;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * This class //TODO ...
+ *
+ * @author Kuali Student Team
+ */
+public class RegistrationGroupTemplateInfo implements RegistrationGroupTemplate, Serializable {
+
+
+    List<RegistrationGroupActiviesOptionsInfo> regGroupActivitiesOptionsInfo;
+
+}
