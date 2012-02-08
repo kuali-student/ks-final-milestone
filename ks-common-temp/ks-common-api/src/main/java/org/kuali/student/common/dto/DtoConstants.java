e1e9b8d4958e01bc938351b2d65806c4bad74e18
jacobusroos <jacobusroos@kuali.org>
2012-02-08 07:43:55 -0500

    KSCM-274
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@26847 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-temp/ks-common-api/src/main/java/org/kuali/student/common/dto/DtoConstants.java
+++ ks-common-temp/ks-common-api/src/main/java/org/kuali/student/common/dto/DtoConstants.java
@@ -15,7 +15,8 @@ public class DtoConstants {
 
 	
 	public static final String DTO_STATE = "DtoState";
-	public static final String DTO_NEXT_STATE = "DtoNextState"; 
+	public static final String DTO_NEXT_STATE = "DtoNextState";
+	public static final String DTO_WORKFLOW_NODE = null; 
 
 	//FIXME: Need to split out proposal states (ie. workflow states) versus dto states
 	public enum DtoState {
