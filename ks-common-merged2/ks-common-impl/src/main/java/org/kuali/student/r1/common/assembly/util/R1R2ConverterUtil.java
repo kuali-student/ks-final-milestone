de4574f65a92bc098b4c9bcb265c774ebd8a6df2
pmarais <pmarais@kuali.org>
2012-02-24 08:02:23 -0500

    KSCM-323: Wrote a converter utility class that may be used to convert between r1 and r2 objects.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@27369 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-common-merged2/ks-common-impl/src/main/java/org/kuali/student/r1/common/assembly/util/R1R2ConverterUtil.java
@@ -0,0 +1,41 @@
+package org.kuali.student.r1.common.assembly.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.beanutils.ConstructorUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.BeanUtils;
+
+public class R1R2ConverterUtil {
+
+    private final static Logger LOG = Logger.getLogger(R1R2ConverterUtil.class);
+
+    public static <T, X> X convert(T source, X target, String[] ignoreProperties) {
+        try {
+            BeanUtils.copyProperties(source, target, ignoreProperties);
+        } catch (Exception e) {
+            LOG.error("Failed to convert " + source.getClass() + " to " + target.getClass());
+        }
+        return target;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T, X> List<X> convertLists(List<T> sourceList, Class<X> targetListType, String[] ignoreProperties) {
+        List<X> targetList = new ArrayList<X>();
+        for (T sourceObject : sourceList) {
+            X targetObject = null;
+            try {
+                targetObject = (X) ConstructorUtils.invokeConstructor(targetListType.getClass(), null);
+            } catch (Exception e) {
+                LOG.error("Failed to invoke a default constructor for " + targetListType);
+                LOG.error("Conversion will not continue further.");
+                break;
+            }
+            targetObject = convert(sourceObject, targetObject, ignoreProperties);
+            targetList.add(targetObject);
+
+        }
+        return targetList;
+    }
+}
