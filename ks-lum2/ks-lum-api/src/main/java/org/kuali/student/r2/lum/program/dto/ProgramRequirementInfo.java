c1c758c9f63f387eb93f90b1ba494db735cf5a70
carelmeintjes <carelmeintjes@kuali.org>
2012-02-24 02:09:36 -0500

    KSCM-332  Fix Compile errors in \ks-2.0-cm\ks-lum2\ks-lum-program
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@27321 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum2/ks-lum-api/src/main/java/org/kuali/student/r2/lum/program/dto/ProgramRequirementInfo.java
+++ ks-lum2/ks-lum-api/src/main/java/org/kuali/student/r2/lum/program/dto/ProgramRequirementInfo.java
@@ -160,4 +160,9 @@ public class ProgramRequirementInfo extends IdNamelessEntityInfo implements Prog
 		// TODO Auto-generated method stub
 		
 	}
+//TODO KSCM This method was required in MajorDisciplineProposalRpcServlet
+	public void setDescr(org.kuali.student.common.dto.RichTextInfo richTextInfo) {
+		// TODO Auto-generated method stub
+		
+	}
 }
