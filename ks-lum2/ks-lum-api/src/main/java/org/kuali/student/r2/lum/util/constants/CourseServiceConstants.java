b557ea33a2f4d5bf1926966beaa0ddc492bf73bf
nina <nina@kuali.org>
2012-02-27 04:31:55 -0500

    KSCM-284 fixed pom files to build merged2 projects and lum2 - added profile merged
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@27417 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum2/ks-lum-api/src/main/java/org/kuali/student/r2/lum/util/constants/CourseServiceConstants.java
+++ ks-lum2/ks-lum-api/src/main/java/org/kuali/student/r2/lum/util/constants/CourseServiceConstants.java
@@ -12,5 +12,9 @@ import org.kuali.student.r2.lum.course.dto.CourseInfo;
 public class CourseServiceConstants {
     public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "course";
     public static final String REF_OBJECT_URI_COURSE_OFFERING = NAMESPACE + "/" + CourseInfo.class.getSimpleName();
+	// TODO KSCM NINA confirm this is ok, copied these constants from CM-1.2.2
+    public static final String COURSE_NAMESPACE = "http://student.kuali.org/wsdl/course";
+    public static final String COURSE_NAMESPACE_URI = "{" + COURSE_NAMESPACE + "}courseInfo";
+
 
 }
