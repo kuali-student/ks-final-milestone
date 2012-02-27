e91b6628dbd64185210054de71c92b1d7068a735
pmarais <pmarais@kuali.org>
2012-02-27 02:33:25 -0500

    KSCM-323: Updated the converter utility to use the dozer library. All further class mappings can just be added to the dozerBeanMapping.xml file. No code changes needed.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@27399 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-merged2/ks-common-impl/src/main/java/org/kuali/student/r1/common/assembly/util/R1R2ConverterUtil.java
+++ ks-common-merged2/ks-common-impl/src/main/java/org/kuali/student/r1/common/assembly/util/R1R2ConverterUtil.java
@@ -3,39 +3,29 @@ package org.kuali.student.r1.common.assembly.util;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.beanutils.ConstructorUtils;
-import org.apache.log4j.Logger;
-import org.springframework.beans.BeanUtils;
+import org.dozer.DozerBeanMapperSingletonWrapper;
+import org.dozer.Mapper;
 
 public class R1R2ConverterUtil {
 
-    private final static Logger LOG = Logger.getLogger(R1R2ConverterUtil.class);
+    private final static Mapper dozerMapper = DozerBeanMapperSingletonWrapper.getInstance();
+    
+    public static <T, X> X convert(T source, Class<X> target) {
+        return dozerMapper.map(source, target);
+    }
 
-    public static <T, X> X convert(T source, X target, String[] ignoreProperties) {
-        try {
-            BeanUtils.copyProperties(source, target, ignoreProperties);
-        } catch (Exception e) {
-            LOG.error("Failed to convert " + source.getClass() + " to " + target.getClass());
-        }
+    public static <T, X> X convert(T source, X target) {
+        dozerMapper.map(source, target);
         return target;
     }
 
-    @SuppressWarnings("unchecked")
-    public static <T, X> List<X> convertLists(List<T> sourceList, Class<X> targetListType, String[] ignoreProperties) {
+    public static <T, X> List<X> convertLists(List<T> sourceList, Class<X> targetListType) {
         List<X> targetList = new ArrayList<X>();
         for (T sourceObject : sourceList) {
-            X targetObject = null;
-            try {
-                targetObject = (X) ConstructorUtils.invokeConstructor(targetListType.getClass(), null);
-            } catch (Exception e) {
-                LOG.error("Failed to invoke a default constructor for " + targetListType);
-                LOG.error("Conversion will not continue further.");
-                break;
-            }
-            targetObject = convert(sourceObject, targetObject, ignoreProperties);
+            X targetObject = convert(sourceObject, targetListType);
             targetList.add(targetObject);
-
         }
         return targetList;
     }
+    
 }
