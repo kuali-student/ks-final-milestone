f7ab3a7631440be8e154bddbde767dc524433468
nina <nina@kuali.org>
2012-02-27 09:40:19 -0500

    KSCM-223 fix ks-lum-ui compile errors
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@27437 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-merged2/ks-core-ui/src/main/java/org/kuali/student/core/statement/ui/client/widgets/rules/ReqComponentInfoUi.java
+++ ks-core-merged2/ks-core-ui/src/main/java/org/kuali/student/core/statement/ui/client/widgets/rules/ReqComponentInfoUi.java
@@ -15,7 +15,8 @@
 
 package org.kuali.student.core.statement.ui.client.widgets.rules;
 
-import org.kuali.student.r1.core.statement.dto.ReqComponentInfo;
+import org.kuali.student.r2.common.dto.MetaInfo;
+import org.kuali.student.r2.core.statement.dto.ReqComponentInfo;
 
 /**
  * @author glindhol
@@ -36,6 +37,12 @@ public class ReqComponentInfoUi extends ReqComponentInfo {
 	public void setPreviewNaturalLanguageTranslation(
 			String previewNaturalLanguageTranslation) {
 		this.previewNaturalLanguageTranslation = previewNaturalLanguageTranslation;
+	}
+
+	@Deprecated
+	public void setMetaInfo(MetaInfo metaInfo) {
+		this.setMeta(metaInfo);
+		
 	}
 
 }
