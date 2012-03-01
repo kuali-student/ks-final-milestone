143b8b1416740b27b14908b491af67b4e6fc3d35
pmarais <pmarais@kuali.org>
2012-03-01 04:34:46 -0500

    KSCM-334: Added example Dozer custom converters for unique scenarios.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@27626 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-conversion/src/test/java/org/kuali/student/conversion/util/CommonConverterTest.java
+++ ks-conversion/src/test/java/org/kuali/student/conversion/util/CommonConverterTest.java
@@ -1,8 +1,14 @@
 package org.kuali.student.conversion.util;
 
+import java.util.Date;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.kuali.student.r2.common.dto.AmountInfo;
+import org.kuali.student.r2.common.dto.CurrencyAmountInfo;
+import org.kuali.student.r2.common.dto.StatusInfo;
+import org.kuali.student.r2.common.dto.TypeInfo;
+import org.kuali.student.r2.common.messages.dto.MessageInfo;
 
 public class CommonConverterTest {
     
@@ -14,6 +20,34 @@ public class CommonConverterTest {
         AmountInfo r2 = R1R2ConverterUtil.convert(r1, AmountInfo.class);
         Assert.assertEquals(r1.getUnitQuantity(), r2.getUnitQuantity());
         Assert.assertEquals(r1.getUnitType(), r2.getUnitTypeKey());
+    }
+    
+    @Test
+    public void testCurrencyAmountInfo() {
+        org.kuali.student.r1.common.dto.CurrencyAmountInfo r1 = new org.kuali.student.r1.common.dto.CurrencyAmountInfo();
+        org.kuali.student.r1.common.dto.MetaInfo r1MetaInfo = new org.kuali.student.r1.common.dto.MetaInfo();
+        r1MetaInfo.setVersionInd("R1 Version Ind");
+        r1MetaInfo.setCreateTime(new Date());
+        r1.setMetaInfo(r1MetaInfo);
+        CurrencyAmountInfo r2 = R1R2ConverterUtil.convert(r1, CurrencyAmountInfo.class);
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+        Assert.assertEquals(r1.getMetaInfo().getCreateTime(), r2.getMeta().getCreateTime());
+    }
+    
+    @Test
+    public void testStatusInfo() {
+        org.kuali.student.r1.common.dto.StatusInfo r1 = new org.kuali.student.r1.common.dto.StatusInfo();
+        r1.setSuccess(true);
+        StatusInfo r2 = R1R2ConverterUtil.convert(r1, StatusInfo.class);
+        Assert.assertEquals(r1.getSuccess(), r2.getIsSuccess());
+    }
+    
+    @Test
+    public void testMessageInfo() {
+        org.kuali.student.r1.common.messages.dto.Message r1 = new org.kuali.student.r1.common.messages.dto.Message();
+        r1.setId("R1 Message Id");
+        MessageInfo r2 = R1R2ConverterUtil.convert(r1, MessageInfo.class);
+        Assert.assertEquals(r1.getId(), r2.getKey());
     }
 
 }
--- ks-conversion/src/test/java/org/kuali/student/conversion/util/CoreConverterTest.java
+++ ks-conversion/src/test/java/org/kuali/student/conversion/util/CoreConverterTest.java
@@ -1,10 +1,53 @@
 package org.kuali.student.conversion.util;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.kuali.student.r2.core.atp.dto.AtpInfo;
 import org.kuali.student.r2.core.document.dto.DocumentInfo;
+import org.kuali.student.r2.core.enumerationmanagement.dto.EnumerationInfo;
 
 public class CoreConverterTest {
+    
+    @Test
+    public void testAtpInfo() {
+        org.kuali.student.r1.core.atp.dto.AtpInfo r1 = new org.kuali.student.r1.core.atp.dto.AtpInfo();
+        r1.setId("R1 Atp Id");
+        org.kuali.student.r1.common.dto.RichTextInfo r1RichText = new org.kuali.student.r1.common.dto.RichTextInfo();
+        r1RichText.setFormatted("R1 Formatted Text");
+        r1.setDesc(r1RichText);
+        Map<String, String> r1Attributes = new HashMap<String, String>();
+        r1Attributes.put("R1-Key", "R1-Value");
+        r1.setAttributes(r1Attributes);
+        org.kuali.student.r1.common.dto.MetaInfo r1MetaInfo = new org.kuali.student.r1.common.dto.MetaInfo();
+        r1MetaInfo.setVersionInd("R1 Version Id");
+        r1.setMetaInfo(r1MetaInfo);
+        AtpInfo r2 = R1R2ConverterUtil.convert(r1, AtpInfo.class);
+        Assert.assertEquals(r1.getId(), r2.getId());
+        Assert.assertEquals(r1.getDesc().getFormatted(), r2.getDescr().getFormatted());
+        Assert.assertEquals(r1.getAttributes().get("R1-Key"), r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+    }
+    
+    @Test
+    public void testEnumerationInfo() {
+        org.kuali.student.r1.core.enumerationmanagement.dto.EnumerationInfo r1 = new org.kuali.student.r1.core.enumerationmanagement.dto.EnumerationInfo();
+        Map<String, String> r1Attributes = new HashMap<String, String>();
+        r1Attributes.put("R1-Key", "R1-Value");
+        r1.setAttributes(r1Attributes);
+        List<String> r1ContextDescriptors = new ArrayList<String>();
+        r1ContextDescriptors.add("R1 Context Descriptor");
+        r1.setContextDescriptors(r1ContextDescriptors);
+        r1.setDescr("R1 Descr");
+        EnumerationInfo r2 = R1R2ConverterUtil.convert(r1, EnumerationInfo.class);
+        Assert.assertEquals(r1.getAttributes().get("R1-Key"), r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getContextDescriptors().get(0), r2.getContextDescriptors().get(0));
+        Assert.assertEquals(r1.getDescr(), r2.getDescr().getPlain());
+    }
     
     @Test
     public void testDocumentInfo() {
