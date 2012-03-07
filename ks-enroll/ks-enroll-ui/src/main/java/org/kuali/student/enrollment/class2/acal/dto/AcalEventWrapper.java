4d523c78363ba180b699b0fc61ee18f2c9cb4f1a
huangb <huangb@kuali.org>
2012-03-06 20:35:42 -0500

    KSENROLL-436: a little code improvement
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@27850 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/dto/AcalEventWrapper.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/dto/AcalEventWrapper.java
@@ -1,19 +1,11 @@
 package org.kuali.student.enrollment.class2.acal.dto;
 
-import java.util.Date;
-
 import org.kuali.student.enrollment.acal.dto.AcalEventInfo;
 
-public class AcalEventWrapper {
+public class AcalEventWrapper extends TimeSetWrapper{
 
     private AcalEventInfo acalEventInfo;
     private String eventType;
-    private Date startDate;
-    private Date endDate;
-    private String startTime;
-    private String startTimeAmPm;
-    private String endTime;
-    private String endTimeAmPm;
 
     public AcalEventWrapper() {
         acalEventInfo = new AcalEventInfo();
@@ -34,56 +26,5 @@ public class AcalEventWrapper {
     public void setEventType(String eventType) {
         this.eventType = eventType;
     }
-    
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public String getStartTimeAmPm() {
-        return startTimeAmPm;
-    }
-
-    public void setStartTimeAmPm(String startTimeAmPm) {
-        this.startTimeAmPm = startTimeAmPm;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
-
-    public String getEndTimeAmPm() {
-        return endTimeAmPm;
-    }
-
-    public void setEndTimeAmPm(String endTimeAmPm) {
-        this.endTimeAmPm = endTimeAmPm;
-    }
-
-
 
 }
