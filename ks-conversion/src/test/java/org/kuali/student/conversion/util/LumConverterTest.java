21c38b6ee6d9cd7a2b008818d9fcc349e02f5042
pmarais <pmarais@kuali.org>
2012-03-07 07:29:11 -0500

    KSCM-334: Corrected mappings related to descr, metaInfo and attributes.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@27907 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-conversion/src/test/java/org/kuali/student/conversion/util/LumConverterTest.java
+++ ks-conversion/src/test/java/org/kuali/student/conversion/util/LumConverterTest.java
@@ -1,22 +1,182 @@
 package org.kuali.student.conversion.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
-import org.kuali.student.r1.common.versionmanagement.dto.VersionInfo;
+import org.kuali.student.r1.common.dto.CurrencyAmountInfo;
+import org.kuali.student.r1.lum.lu.dto.AffiliatedOrgInfo;
 import org.kuali.student.r2.lum.course.dto.ActivityInfo;
+import org.kuali.student.r2.lum.course.dto.CourseCrossListingInfo;
+import org.kuali.student.r2.lum.course.dto.CourseExpenditureInfo;
+import org.kuali.student.r2.lum.course.dto.CourseFeeInfo;
+import org.kuali.student.r2.lum.course.dto.CourseInfo;
+import org.kuali.student.r2.lum.course.dto.CourseRevenueInfo;
+import org.kuali.student.r2.lum.course.dto.FormatInfo;
 import org.kuali.student.r2.lum.lo.dto.LoCategoryInfo;
+import org.kuali.student.r2.lum.lo.dto.LoLoRelationInfo;
+import org.kuali.student.r2.lum.lo.dto.LoRepositoryInfo;
+import org.kuali.student.r2.lum.program.dto.CoreProgramInfo;
 import org.kuali.student.r2.lum.program.dto.CredentialProgramInfo;
+import org.kuali.student.r2.lum.program.dto.HonorsProgramInfo;
+import org.kuali.student.r2.lum.program.dto.MajorDisciplineInfo;
+import org.kuali.student.r2.lum.program.dto.MinorDisciplineInfo;
+import org.kuali.student.r2.lum.program.dto.ProgramRequirementInfo;
+import org.kuali.student.r2.lum.program.dto.ProgramVariationInfo;
 
 public class LumConverterTest {
 
     @Test
+    public void testActivityInfo() {
+        org.kuali.student.r1.lum.course.dto.ActivityInfo r1 = R1TestDataUtil.getActivityInfoData();
+        ActivityInfo r2 = R1R2ConverterUtil.convert(r1, ActivityInfo.class);
+        Assert.assertEquals(r1.getState(), r2.getStateKey());
+        Assert.assertEquals(r1.getActivityType(), r2.getTypeKey());
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+        Assert.assertEquals(r1.getContactHours().getUnitType(), r2.getContactHours().getUnitTypeKey());
+        Assert.assertEquals(r1.getDuration().getAtpDurationTypeKey(), r2.getDuration().getAtpDurationTypeKey());
+    }
+
+    @Test
+    public void testCourseCrossListingInfo() {
+        org.kuali.student.r1.lum.course.dto.CourseCrossListingInfo r1 = R1TestDataUtil.getCourseCrossListingInfoData();
+        CourseCrossListingInfo r2 = R1R2ConverterUtil.convert(r1, CourseCrossListingInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getDepartment(), r2.getDepartment());
+    }
+
+    @Test
+    public void testCourseExpenditureInfo() {
+        org.kuali.student.r1.lum.course.dto.CourseExpenditureInfo r1 = R1TestDataUtil.getCourseExpenditureInfoData();
+        CourseExpenditureInfo r2 = R1R2ConverterUtil.convert(r1, CourseExpenditureInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getAffiliatedOrgs().get(0).getId(), r2.getAffiliatedOrgs().get(0).getId());
+    }
+
+    @Test
+    public void testCourseFeeInfo() {
+        org.kuali.student.r1.lum.course.dto.CourseFeeInfo r1 = R1TestDataUtil.getCourseFeeInfoData();
+        CourseFeeInfo r2 = R1R2ConverterUtil.convert(r1, CourseFeeInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getDescr().getPlain(), r2.getDescr().getPlain());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+        Assert.assertEquals(r1.getFeeAmounts().get(0).getId(), r2.getFeeAmounts().get(0).getId());
+    }
+
+    //@Test
+    public void testCourseInfo() {
+        org.kuali.student.r1.lum.course.dto.CourseInfo r1 = new org.kuali.student.r1.lum.course.dto.CourseInfo();
+        r1.setAttributes(R1TestDataUtil.getAttributeData());
+        r1.setCourseSpecificLOs(R1TestDataUtil.getLoDisplayInfoDataList());
+        r1.setCreditOptions(R1TestDataUtil.getResultComponentInfoDataList());
+        r1.setCrossListings(R1TestDataUtil.getCourseCrossListingInfoDataList());
+        r1.setDescr(R1TestDataUtil.getRichTextInfoData());
+        r1.setDuration(R1TestDataUtil.getTimeAmountInfoData());
+        r1.setExpenditure(R1TestDataUtil.getCourseExpenditureInfoData());
+        r1.setFeeJustification(R1TestDataUtil.getRichTextInfoData());
+        r1.setFees(R1TestDataUtil.getCourseFeeInfoDataList());
+        r1.setFormats(R1TestDataUtil.getFormatInfoDataList());
+        r1.setInstructors(R1TestDataUtil.getCluInstructorInfoDataList());
+        r1.setJoints(R1TestDataUtil.getCourseJointInfoDataList());
+        r1.setMetaInfo(R1TestDataUtil.getMetadataInfoData());
+        r1.setOutOfClassHours(R1TestDataUtil.getAmountInfoData());
+        r1.setPrimaryInstructor(R1TestDataUtil.getCluInstructorInfoData());
+        r1.setRevenues(R1TestDataUtil.getCourseRevenueInfoDataList());
+        r1.setVariations(R1TestDataUtil.getCourseVariationInfoDataList());
+        r1.setVersionInfo(R1TestDataUtil.getVersionInfoData());
+        CourseInfo r2 = R1R2ConverterUtil.convert(r1, CourseInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getCourseSpecificLOs().get(0).getLoCategoryInfoList().get(0).getId(), r2.getCourseSpecificLOs().get(0).getLoCategoryInfoList().get(0).getId());
+        Assert.assertEquals(r1.getDescr().getPlain(), r2.getDescr().getPlain());
+        Assert.assertEquals(r1.getFeeJustification().getPlain(), r2.getFeeJustification().getPlain());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+        Assert.assertEquals(r1.getVersionInfo().getVersionIndId(), r2.getVersionInfo().getVersionIndId());
+    }
+
+    @Test
+    public void testCourseRevenueInfo() {
+        org.kuali.student.r1.lum.course.dto.CourseRevenueInfo r1 = R1TestDataUtil.getCourseRevenueInfoData();
+        CourseRevenueInfo r2 = R1R2ConverterUtil.convert(r1, CourseRevenueInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+    }
+
+    @Test
+    public void testFormatInfo() {
+        org.kuali.student.r1.lum.course.dto.FormatInfo r1 = R1TestDataUtil.getFormatInfoData();
+        FormatInfo r2 = R1R2ConverterUtil.convert(r1, FormatInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+    }
+
+    @Test
+    public void testLoDisplayInfo() {
+        org.kuali.student.r1.lum.course.dto.LoDisplayInfo r1 = R1TestDataUtil.getLoDisplayInfoData();
+        org.kuali.student.r2.lum.course.dto.LoDisplayInfo r2 = R1R2ConverterUtil.convert(r1,
+                org.kuali.student.r2.lum.course.dto.LoDisplayInfo.class);
+    }
+
+    @Test
     public void testLoCategoryInfo() {
-        org.kuali.student.r1.lum.lo.dto.LoCategoryInfo r1 = new org.kuali.student.r1.lum.lo.dto.LoCategoryInfo();
-        r1.setLoRepository("R1 Lo Repository");
+        org.kuali.student.r1.lum.lo.dto.LoCategoryInfo r1 = R1TestDataUtil.getLoCategoryInfoData();
         LoCategoryInfo r2 = R1R2ConverterUtil.convert(r1, LoCategoryInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getDesc().getPlain(), r2.getDescr().getPlain());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
         Assert.assertEquals(r1.getLoRepository(), r2.getLoRepositoryKey());
     }
-    
+
+    @Test
+    public void testLoInfo() {
+        org.kuali.student.r1.lum.lo.dto.LoInfo r1 = R1TestDataUtil.getLoInfoData();
+        org.kuali.student.r2.lum.lo.dto.LoInfo r2 = R1R2ConverterUtil.convert(r1,
+                org.kuali.student.r2.lum.lo.dto.LoInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getDesc().getPlain(), r2.getDescr().getPlain());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+    }
+
+    @Test
+    public void testLoLoRelationInfo() {
+        org.kuali.student.r1.lum.lo.dto.LoLoRelationInfo r1 = new org.kuali.student.r1.lum.lo.dto.LoLoRelationInfo();
+        r1.setAttributes(R1TestDataUtil.getAttributeData());
+        r1.setMetaInfo(R1TestDataUtil.getMetadataInfoData());
+        LoLoRelationInfo r2 = R1R2ConverterUtil.convert(r1, LoLoRelationInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+    }
+
+    @Test
+    public void testLoRepositoryInfo() {
+        org.kuali.student.r1.lum.lo.dto.LoRepositoryInfo r1 = new org.kuali.student.r1.lum.lo.dto.LoRepositoryInfo();
+        r1.setAttributes(R1TestDataUtil.getAttributeData());
+        r1.setDesc(R1TestDataUtil.getRichTextInfoData());
+        r1.setMetaInfo(R1TestDataUtil.getMetadataInfoData());
+        LoRepositoryInfo r2 = R1R2ConverterUtil.convert(r1, LoRepositoryInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getDesc().getPlain(), r2.getDescr().getPlain());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+    }
+
+    @Test
+    public void testCoreProgramInfo() {
+        org.kuali.student.r1.lum.program.dto.CoreProgramInfo r1 = new org.kuali.student.r1.lum.program.dto.CoreProgramInfo();
+        r1.setAttributes(R1TestDataUtil.getAttributeData());
+        r1.setCatalogDescr(R1TestDataUtil.getRichTextInfoData());
+        r1.setDescr(R1TestDataUtil.getRichTextInfoData());
+        r1.setLearningObjectives(R1TestDataUtil.getLoDisplayInfoDataList());
+        r1.setMetaInfo(R1TestDataUtil.getMetadataInfoData());
+        r1.setVersionInfo(R1TestDataUtil.getVersionInfoData());
+        CoreProgramInfo r2 = R1R2ConverterUtil.convert(r1, CoreProgramInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getCatalogDescr().getPlain(), r2.getCatalogDescr().getPlain());
+        Assert.assertEquals(r1.getDescr().getPlain(), r2.getDescr().getPlain());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+        Assert.assertEquals(r1.getVersionInfo().getVersionIndId(), r2.getVersion().getVersionIndId());
+    }
+
     @Test
     public void testCredentialProgramInfo() {
         org.kuali.student.r1.lum.program.dto.CredentialProgramInfo r1 = new org.kuali.student.r1.lum.program.dto.CredentialProgramInfo();
@@ -24,24 +184,92 @@ public class LumConverterTest {
         r1.setCredentialProgramType("R1 Credential Program Type");
         r1.setDescr(R1TestDataUtil.getRichTextInfoData());
         r1.setVersionInfo(R1TestDataUtil.getVersionInfoData());
+        r1.setAttributes(R1TestDataUtil.getAttributeData());
+        r1.setInstitution(R1TestDataUtil.getAdminOrgInfoData());
+        r1.setLearningObjectives(R1TestDataUtil.getLoDisplayInfoDataList());
+        r1.setMetaInfo(R1TestDataUtil.getMetadataInfoData());
         CredentialProgramInfo r2 = R1R2ConverterUtil.convert(r1, CredentialProgramInfo.class);
         Assert.assertEquals(r1.getType(), r2.getTypeKey());
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
         Assert.assertEquals(r1.getCredentialProgramType(), r2.getCredentialProgramType());
         Assert.assertEquals(r1.getDescr().getPlain(), r2.getDescr().getPlain());
         Assert.assertEquals(r1.getDescr().getFormatted(), r2.getDescr().getFormatted());
         Assert.assertEquals(r1.getVersionInfo().getVersionIndId(), r2.getVersion().getVersionIndId());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
     }
-    
+
     @Test
-    public void testActivityInfo() {
-        org.kuali.student.r1.lum.course.dto.ActivityInfo r1 = new org.kuali.student.r1.lum.course.dto.ActivityInfo();
-        r1.setState("R1 State");
-        r1.setActivityType("R1 ActivityType");
-        ActivityInfo r2 = R1R2ConverterUtil.convert(r1, ActivityInfo.class);
-        Assert.assertEquals(r1.getState(), r2.getStateKey());
-        Assert.assertEquals(r1.getActivityType(), r2.getTypeKey());
-        org.kuali.student.r1.lum.course.dto.ActivityInfo r1a = R1R2ConverterUtil.convert(r2, org.kuali.student.r1.lum.course.dto.ActivityInfo.class);
-        Assert.assertEquals(r2.getStateKey(), r1a.getState());
-        Assert.assertEquals(r2.getTypeKey(), r1a.getActivityType());
+    public void testHonorsProgramInfo() {
+        org.kuali.student.r1.lum.program.dto.HonorsProgramInfo r1 = new org.kuali.student.r1.lum.program.dto.HonorsProgramInfo();
+        r1.setAttributes(R1TestDataUtil.getAttributeData());
+        r1.setMetaInfo(R1TestDataUtil.getMetadataInfoData());
+        HonorsProgramInfo r2 = R1R2ConverterUtil.convert(r1, HonorsProgramInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
     }
+
+    @Test
+    public void testMajorDisciplineInfo() {
+        org.kuali.student.r1.lum.program.dto.MajorDisciplineInfo r1 = new org.kuali.student.r1.lum.program.dto.MajorDisciplineInfo();
+        //r1.setAccreditingAgencies(accreditingAgencies);
+        r1.setAttributes(R1TestDataUtil.getAttributeData());
+        r1.setCatalogDescr(R1TestDataUtil.getRichTextInfoData());
+        r1.setDescr(R1TestDataUtil.getRichTextInfoData());
+        r1.setLearningObjectives(R1TestDataUtil.getLoDisplayInfoDataList());
+        r1.setMetaInfo(R1TestDataUtil.getMetadataInfoData());
+        //r1.setOrgCoreProgram(orgCoreProgram);
+        r1.setPublishedInstructors(R1TestDataUtil.getCluInstructorInfoDataList());
+        r1.setStdDuration(R1TestDataUtil.getTimeAmountInfoData());
+        //r1.setVariations(variations);
+        r1.setVersionInfo(R1TestDataUtil.getVersionInfoData());
+        MajorDisciplineInfo r2 = R1R2ConverterUtil.convert(r1, MajorDisciplineInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getCatalogDescr().getPlain(), r2.getCatalogDescr().getPlain());
+        Assert.assertEquals(r1.getDescr().getPlain(), r2.getDescr().getPlain());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+        Assert.assertEquals(r1.getVersionInfo().getVersionIndId(), r2.getVersion().getVersionIndId());
+    }
+
+    @Test
+    public void testMinorDisciplineInfo() {
+        org.kuali.student.r1.lum.program.dto.MinorDisciplineInfo r1 = new org.kuali.student.r1.lum.program.dto.MinorDisciplineInfo();
+        r1.setAttributes(R1TestDataUtil.getAttributeData());
+        r1.setMetaInfo(R1TestDataUtil.getMetadataInfoData());
+        MinorDisciplineInfo r2 = R1R2ConverterUtil.convert(r1, MinorDisciplineInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+    }
+
+    @Test
+    public void testProgramRequirementInfo() {
+        org.kuali.student.r1.lum.program.dto.ProgramRequirementInfo r1 = new org.kuali.student.r1.lum.program.dto.ProgramRequirementInfo();
+        r1.setAttributes(R1TestDataUtil.getAttributeData());
+        r1.setDescr(R1TestDataUtil.getRichTextInfoData());
+        r1.setLearningObjectives(R1TestDataUtil.getLoDisplayInfoDataList());
+        r1.setMetaInfo(R1TestDataUtil.getMetadataInfoData());
+        //r1.setStatement(statement);
+        ProgramRequirementInfo r2 = R1R2ConverterUtil.convert(r1, ProgramRequirementInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getDescr().getPlain(), r2.getDescr().getPlain());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+    }
+
+    @Test
+    public void testProgramVariationInfo() {
+        org.kuali.student.r1.lum.program.dto.ProgramVariationInfo r1 = new org.kuali.student.r1.lum.program.dto.ProgramVariationInfo();
+        r1.setAttributes(R1TestDataUtil.getAttributeData());
+        r1.setCatalogDescr(R1TestDataUtil.getRichTextInfoData());
+        r1.setDescr(R1TestDataUtil.getRichTextInfoData());
+        r1.setLearningObjectives(R1TestDataUtil.getLoDisplayInfoDataList());
+        r1.setMetaInfo(R1TestDataUtil.getMetadataInfoData());
+        r1.setStdDuration(R1TestDataUtil.getTimeAmountInfoData());
+        r1.setVersionInfo(R1TestDataUtil.getVersionInfoData());
+        ProgramVariationInfo r2 = R1R2ConverterUtil.convert(r1, ProgramVariationInfo.class);
+        Assert.assertEquals("R1-Value", r2.getAttributes().get(0).getValue());
+        Assert.assertEquals(r1.getCatalogDescr().getPlain(), r2.getCatalogDescr().getPlain());
+        Assert.assertEquals(r1.getDescr().getPlain(), r2.getDescr().getPlain());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+        Assert.assertEquals(r1.getMetaInfo().getVersionInd(), r2.getMeta().getVersionInd());
+    }
+
 }
