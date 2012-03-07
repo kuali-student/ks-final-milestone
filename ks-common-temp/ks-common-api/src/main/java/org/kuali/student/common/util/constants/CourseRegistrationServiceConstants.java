98cdf1b9a062ccf325e838af05d8f893a9afe152
sw <sw@kuali.org>
2012-03-07 06:40:25 -0500

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-2.0-cm-sandbox@27893 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-temp/ks-common-api/src/main/java/org/kuali/student/common/util/constants/CourseRegistrationServiceConstants.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.student.common.util.constants;
-
-public class CourseRegistrationServiceConstants {
-    
-    public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "courseRegistrationService";
-
-}
--- ks-lum/ks-lum-sql/src/main/resources/upgrades/01-R1.1.0-M10 to R1.1.0/2010-11-16-2-lum-stmt-bootstrap.sql
+++
@@ -1,4 +0,0 @@
-UPDATE KSST_STMT_TYPE SET NAME = 'Enrollment Eligibility' WHERE TYPE_KEY = 'kuali.statement.type.course.enrollmentEligibility'
-/
-UPDATE KSST_STMT_TYPE SET NAME = 'Student Eligibility + Prerequisite' WHERE TYPE_KEY = 'kuali.statement.type.course.academicReadiness.studentEligibilityPrereq'
-/
