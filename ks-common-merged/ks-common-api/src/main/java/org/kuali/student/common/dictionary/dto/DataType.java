7c9db98c947bfee091d31a2e5ad46672a400bb05
sw <sw@kuali.org>
2012-03-07 06:40:25 -0500

    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@27893 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-merged/ks-common-api/src/main/java/org/kuali/student/common/dictionary/dto/DataType.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.student.common.dictionary.dto;
-
-import javax.xml.bind.annotation.XmlEnum;
-
-//TODO KSCM-285 wait for Service Team confirmation
-@XmlEnum
-public enum DataType {
-	STRING, DATE, TRUNCATED_DATE, BOOLEAN, INTEGER, FLOAT, DOUBLE, LONG, COMPLEX
-}
