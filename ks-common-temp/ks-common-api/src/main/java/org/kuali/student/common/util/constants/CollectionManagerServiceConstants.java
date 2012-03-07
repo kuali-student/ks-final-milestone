17f7c4891215cf30415077fb2935c3d38a67e250
sw <sw@kuali.org>
2012-03-07 06:41:53 -0500

    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@27894 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-temp/ks-common-api/src/main/java/org/kuali/student/common/util/constants/CollectionManagerServiceConstants.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.student.common.util.constants;
-
-public class CollectionManagerServiceConstants {
-
-    public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "collectionManager";
-
-}
