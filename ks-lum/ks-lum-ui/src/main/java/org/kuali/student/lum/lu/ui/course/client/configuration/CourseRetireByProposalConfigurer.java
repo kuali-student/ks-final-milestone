fac77fbe28a4c443302e67ccb34dc5f88a50683c
mikemcd <mikemcd@kuali.org>
2012-03-07 17:03:08 -0500

    KSCM-976 KSLAB-2577 Commiting an early course Retire by proposal Controller, which still needs more work; and an empty shell Configurer in case anyone needs something there to do other work.
    git-svn-id: https://svn.kuali.org/repos/student/trunk@28043 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/configuration/CourseRetireByProposalConfigurer.java
@@ -0,0 +1,16 @@
+package org.kuali.student.lum.lu.ui.course.client.configuration;
+
+
+/**
+ * Shell of Configurer for Retire by Proposal
+ * 
+ * @author mike
+ *
+ */
+public class CourseRetireByProposalConfigurer extends CourseAdminRetireConfigurer {
+
+	public CourseRetireByProposalConfigurer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
