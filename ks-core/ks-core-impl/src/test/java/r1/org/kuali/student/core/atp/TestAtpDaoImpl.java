6449b89c874bcbfd0c30359930e2469219f16637
dedewet <dedewet@kuali.org>
2012-03-08 04:53:23 -0500

    KSCM-367
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@28081 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-impl/src/test/java/r1/org/kuali/student/core/atp/TestAtpDaoImpl.java
+++ ks-core/ks-core-impl/src/test/java/r1/org/kuali/student/core/atp/TestAtpDaoImpl.java
@@ -13,17 +13,17 @@
  * permissions and limitations under the License.
  */
 
-package org.kuali.student.core.atp;
+package r1.org.kuali.student.core.atp;
 
 import org.junit.Test;
 import org.kuali.student.common.test.spring.AbstractTransactionalDaoTest;
 import org.kuali.student.common.test.spring.Dao;
 import org.kuali.student.common.test.spring.PersistenceFileLocation;
-import org.kuali.student.core.atp.dao.AtpDao;
+import org.kuali.student.r1.core.atp.dao.AtpDao;
 
 @PersistenceFileLocation("classpath:META-INF/atp-persistence.xml")
 public class TestAtpDaoImpl extends AbstractTransactionalDaoTest {
-	@Dao(value = "org.kuali.student.core.atp.dao.impl.AtpDaoImpl", testSqlFile = "classpath:ks-atp.sql")
+	@Dao(value = "org.kuali.student.r1.core.atp.dao.impl.AtpDaoImpl", testSqlFile = "classpath:ks-atp.sql")
 	public AtpDao dao;
 
 	@Test
