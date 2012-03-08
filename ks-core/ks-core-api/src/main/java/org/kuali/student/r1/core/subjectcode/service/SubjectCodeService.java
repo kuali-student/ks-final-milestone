6226a3968153d809bede7390947e19f3b2b293ed
sw <sw@kuali.org>
2012-03-08 06:30:16 -0500

    KSCM - 313 - Fix GWT code.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@28095 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-api/src/main/java/org/kuali/student/r1/core/subjectcode/service/SubjectCodeService.java
+++ ks-core/ks-core-api/src/main/java/org/kuali/student/r1/core/subjectcode/service/SubjectCodeService.java
@@ -5,7 +5,6 @@ import javax.jws.soap.SOAPBinding;
 
 import org.kuali.student.r1.common.search.service.SearchService;
 
-@Deprecated
 @WebService(name = "SubjectCodeService", targetNamespace = "http://student.kuali.org/wsdl/subjectCode")
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
 public interface SubjectCodeService extends SearchService {
