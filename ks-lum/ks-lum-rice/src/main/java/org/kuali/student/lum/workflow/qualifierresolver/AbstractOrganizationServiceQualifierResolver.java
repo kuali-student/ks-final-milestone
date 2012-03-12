8957ed4e2f92fa4904538344de1600afd462e78b
dedewet <dedewet@kuali.org>
2012-03-12 06:37:59 -0400

    KSCM-371: Modified TODOs for categorisation.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@28254 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-rice/src/main/java/org/kuali/student/lum/workflow/qualifierresolver/AbstractOrganizationServiceQualifierResolver.java
+++ ks-lum/ks-lum-rice/src/main/java/org/kuali/student/lum/workflow/qualifierresolver/AbstractOrganizationServiceQualifierResolver.java
@@ -131,7 +131,7 @@ public abstract class AbstractOrganizationServiceQualifierResolver implements Qu
             searchRequest.setParams(queryParamValues);
             try {
                 SearchResult result = null;
-             // TODO KSCM-371 result = getOrganizationService().search(searchRequest, null);
+             // TODO KSCM-165 result = getOrganizationService().search(searchRequest, null);
                 results = result.getRows();
             } catch (Exception e) {
                 LOG.error("Error calling org service");
