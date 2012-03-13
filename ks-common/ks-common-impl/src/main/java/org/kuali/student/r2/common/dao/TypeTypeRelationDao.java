d4a380f54ef930a554560cd51f227314df8e6761
dedewet <dedewet@kuali.org>
2012-03-13 08:09:30 -0400

    KSCM-355
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@28373 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-impl/src/main/java/org/kuali/student/r2/common/dao/TypeTypeRelationDao.java
+++ ks-common/ks-common-impl/src/main/java/org/kuali/student/r2/common/dao/TypeTypeRelationDao.java
@@ -2,12 +2,12 @@ package org.kuali.student.r2.common.dao;
 
 import java.util.List;
 
-import org.kuali.student.r2.core.class1.type.entity.TypeTypeRelationEntity;
+// TODO KSCM import org.kuali.student.r2.core.class1.type.entity.TypeTypeRelationEntity;
 
-public class TypeTypeRelationDao extends GenericEntityDao<TypeTypeRelationEntity>{
+// TODO KSCM public class TypeTypeRelationDao extends GenericEntityDao<TypeTypeRelationEntity>{
 	
 
-    @SuppressWarnings("unchecked")
+   /* @SuppressWarnings("unchecked")
     public List<TypeTypeRelationEntity> getTypeTypeRelationsByOwnerAndRelationTypes(String ownerTypeId, String relationTypeId) {
         return (List<TypeTypeRelationEntity>) em.createQuery("from TypeTypeRelationEntity rel where rel.type=:relationTypeId AND rel.ownerTypeId=:ownerTypeId").setParameter("relationTypeId", relationTypeId).setParameter("ownerTypeId", ownerTypeId).getResultList();
     }
@@ -16,4 +16,4 @@ public class TypeTypeRelationDao extends GenericEntityDao<TypeTypeRelationEntity
     public List<TypeTypeRelationEntity> getTypeTypeRelationsByOwnerType(String ownerTypeId) {
         return (List<TypeTypeRelationEntity>) em.createQuery("from TypeTypeRelationEntity rel where rel.ownerTypeId=:ownerTypeId").setParameter("ownerTypeId", ownerTypeId).getResultList();
     }
-}
+}*/
