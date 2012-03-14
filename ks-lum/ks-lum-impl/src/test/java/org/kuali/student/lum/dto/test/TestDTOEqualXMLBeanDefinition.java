ce216875451bb66e5d225cc0d58f16ff384fd12e
nina <nina@kuali.org>
2012-03-14 09:19:22 -0400

    kscm-367 - Fix test cases
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@28467 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum/ks-lum-impl/src/test/java/org/kuali/student/lum/dto/test/TestDTOEqualXMLBeanDefinition.java
@@ -0,0 +1,64 @@
+package org.kuali.student.lum.dto.test;
+
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.kuali.student.r1.common.dictionary.service.impl.ComplexSubstructuresHelper;
+import org.kuali.student.r2.core.statement.dto.ReqCompFieldInfo;
+import org.kuali.student.r2.core.statement.dto.ReqComponentInfo;
+import org.kuali.student.r2.core.statement.dto.StatementInfo;
+import org.kuali.student.r2.core.statement.dto.StatementTreeViewInfo;
+
+import r1.org.kuali.student.core.dictionary.service.DictionaryDiscrepencyTesterHelper;
+
+public class TestDTOEqualXMLBeanDefinition {
+
+
+
+
+	private String formatAsString(List<String> errors) {
+		int i = 0;
+		StringBuilder builder = new StringBuilder();
+		for (String error : errors) {
+			i++;
+			builder.append(i + ". " + error + "\n");
+		}
+		return builder.toString();
+	}
+
+	// @Test
+	public void getLinkedListFields() {
+		ComplexSubstructuresHelper helpme = new ComplexSubstructuresHelper();
+		ArrayList<Field> fields = new ArrayList<Field>();
+		fields = helpme.getAllFields(fields, StatementInfo.class);
+		for (Field field : fields) {
+			System.out.println(field.getName());
+		}
+	}
+
+//	@Test
+	public void testStatementDTOsAgainstDataDictionary() {
+		System.out.println("testing statement dictionary");
+
+		Set<String> startingClasses = new LinkedHashSet();
+		startingClasses.add(StatementInfo.class.getName());
+		startingClasses.add(ReqComponentInfo.class.getName());
+		startingClasses.add(ReqCompFieldInfo.class.getName());
+		startingClasses.add(StatementTreeViewInfo.class.getName());
+		String contextFile = "ks-statement-dictionary-context";
+		String outFile = "target/" + contextFile + ".txt";
+		DictionaryDiscrepencyTesterHelper helper = new DictionaryDiscrepencyTesterHelper(
+				outFile, startingClasses, contextFile + ".xml", false);
+		helper.setPrintDescrepenciesOnly(true);
+		List<String> errors = helper.doTest();
+		if (errors.size() > 0) {
+			fail("failed dictionary validation:\n" + formatAsString(errors));
+		}
+
+	}
+}
