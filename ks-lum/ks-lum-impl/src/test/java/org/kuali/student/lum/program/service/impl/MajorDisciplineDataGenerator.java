c146eee9425daa5fffcdabbcd4c1729631b12241
carelmeintjes <carelmeintjes@kuali.org>
2012-03-16 09:30:16 -0400

    KSCM-364 Run tests for org.kuali.student.lum
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@28602 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-impl/src/test/java/org/kuali/student/lum/program/service/impl/MajorDisciplineDataGenerator.java
+++ ks-lum/ks-lum-impl/src/test/java/org/kuali/student/lum/program/service/impl/MajorDisciplineDataGenerator.java
@@ -3,7 +3,7 @@ package org.kuali.student.lum.program.service.impl;
 import java.beans.IntrospectionException;
 import java.lang.reflect.InvocationTargetException;
 
-import org.kuali.student.lum.program.dto.MajorDisciplineInfo;
+import org.kuali.student.r2.lum.program.dto.MajorDisciplineInfo;
 import org.kuali.student.lum.program.service.assembler.ProgramAssemblerConstants;
 
 public class MajorDisciplineDataGenerator {
