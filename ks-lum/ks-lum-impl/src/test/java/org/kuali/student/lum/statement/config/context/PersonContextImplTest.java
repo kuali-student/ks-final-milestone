9d3960f6360e74cfc647d49f70df374398b43fbb
carelmeintjes <carelmeintjes@kuali.org>
2012-03-19 03:09:00 -0400

    KSCM-364 Run tests for org.kuali.student.lum
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@28643 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-impl/src/test/java/org/kuali/student/lum/statement/config/context/PersonContextImplTest.java
+++ ks-lum/ks-lum-impl/src/test/java/org/kuali/student/lum/statement/config/context/PersonContextImplTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import org.kuali.student.r2.common.exceptions.OperationFailedException;
 import org.kuali.student.r1.core.statement.dto.ReqCompFieldInfo;
 import org.kuali.student.r1.core.statement.dto.ReqComponentInfo;
-import org.kuali.student.lum.statement.typekey.ReqComponentFieldTypes;
+import org.kuali.student.r1.lum.statement.typekey.ReqComponentFieldTypes;
 
 public class PersonContextImplTest {
 
