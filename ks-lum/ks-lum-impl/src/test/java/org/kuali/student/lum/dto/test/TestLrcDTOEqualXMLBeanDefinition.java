e1a1d9d8901ed14bf27e4f0cb76f8ac182082fea
nina <nina@kuali.org>
2012-03-20 08:16:07 -0400

    Fixes for getting the app to run
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@28721 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-impl/src/test/java/org/kuali/student/lum/dto/test/TestLrcDTOEqualXMLBeanDefinition.java
+++ ks-lum/ks-lum-impl/src/test/java/org/kuali/student/lum/dto/test/TestLrcDTOEqualXMLBeanDefinition.java
@@ -43,9 +43,9 @@ public class TestLrcDTOEqualXMLBeanDefinition {
 		System.out.println("testing statement dictionary");
 
 		Set<String> startingClasses = new LinkedHashSet();
-//		startingClasses.add(ResultValueRangeInfo.class.getName());
-//		startingClasses.add(ResultValuesGroupInfo.class.getName());		
-//		startingClasses.add(ResultScaleInfo.class.getName());
+		startingClasses.add(ResultValueRangeInfo.class.getName());
+		startingClasses.add(ResultValuesGroupInfo.class.getName());		
+		startingClasses.add(ResultScaleInfo.class.getName());
 		startingClasses.add(ResultValueInfo.class.getName());
 		
 		String contextFile = "ks-lrc-dictionary-context";
