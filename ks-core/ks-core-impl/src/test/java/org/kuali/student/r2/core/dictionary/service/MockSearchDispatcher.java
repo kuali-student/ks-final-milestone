6cc951b0f2d496401a871da6dbb5c6b18f1e3282
dedewet <dedewet@kuali.org>
2012-03-27 10:06:28 -0400

    KSCM-44
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@29030 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-impl/src/test/java/org/kuali/student/r2/core/dictionary/service/MockSearchDispatcher.java
+++ ks-core/ks-core-impl/src/test/java/org/kuali/student/r2/core/dictionary/service/MockSearchDispatcher.java
@@ -9,26 +9,22 @@ import org.kuali.student.r1.common.search.dto.SearchResultCell;
 import org.kuali.student.r1.common.search.dto.SearchResultRow;
 import org.kuali.student.r1.common.search.service.SearchDispatcher;
 
-public class MockSearchDispatcher implements SearchDispatcher
-{
+public class MockSearchDispatcher implements SearchDispatcher {
 
- @Override
- public SearchResult dispatchSearch (SearchRequest searchRequest)
- {
-  SearchResult result = new SearchResult ();
-  List<SearchResultRow> rows = new ArrayList ();
-  SearchResultRow row = new SearchResultRow ();
-  rows.add (row);
-  SearchResultCell cell = new SearchResultCell ();
-  cell.setKey ("mockKey");
-  cell.setValue ("mockValue");
-  List<SearchResultCell> cells = new ArrayList ();
-  cells.add (cell);
-  row.setCells (cells);
-  result.setRows (rows);
-//  System.out.println ("Generating mock search result for " + searchRequest.getSearchKey ());
-  return result;
- }
-
-
+    @Override
+    public SearchResult dispatchSearch(SearchRequest searchRequest) {
+        SearchResult result = new SearchResult ();
+        List<SearchResultRow> rows = new ArrayList ();
+        SearchResultRow row = new SearchResultRow ();
+        rows.add (row);
+        SearchResultCell cell = new SearchResultCell ();
+        cell.setKey ("mockKey");
+        cell.setValue ("mockValue");
+        List<SearchResultCell> cells = new ArrayList ();
+        cells.add (cell);
+        row.setCells (cells);
+        result.setRows (rows);
+        System.out.println ("Generating mock search result for " + searchRequest.getSearchKey ());
+        return result;
+    }
 }
