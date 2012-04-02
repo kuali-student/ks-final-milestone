2d4f5bf1bd0068eb1d5a36c8c7a45c16723ceac2
tcoppeto <tcoppeto@kuali.org>
2012-04-02 12:15:40 -0400

    KSENROLL-719: fixes for course reg test inserts. the statement dependency still needs to be fixed.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3-merge@29239 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/test/resources/ks-courseReg.sql
+++ ks-enroll/ks-enroll-impl/src/test/resources/ks-courseReg.sql
@@ -65,27 +65,27 @@ insert into KSLU_CLU (ID, CREATEID, CREATETIME, UPDATEID, UPDATETIME, VER_NBR, C
 insert into KSLU_CLU (ID, CREATEID, CREATETIME, UPDATEID, UPDATETIME, VER_NBR, CAN_CREATE_LUI, DEF_ENRL_EST, DEF_MAX_ENRL, EFF_DT, EXPIR_DT, HAS_EARLY_DROP_DEDLN, IS_ENRL, IS_HAZR_DISBLD_STU, NEXT_REVIEW_PRD, REF_URL, ST, ATP_DUR_TYP_KEY, TM_QUANTITY, STDY_SUBJ_AREA, ACCT_ID, RT_DESCR_ID, FEE_ID, LUTYPE_ID, OFFIC_CLU_ID, PRI_INSTR_ID,VER_IND_ID,CURR_VER_START)  values ('CLU-5', 'CREATEID', {ts '2000-01-01 00:00:00.0'}, 'UPDATEID', {ts '2001-01-01 00:00:00.0'}, 1, 1, 1, 42, {ts '2002-01-01 00:00:00.0'}, {ts '2003-01-01 00:00:00.0'}, 1, 1, 0, 'NEXT_REVIEW_PRD', 'REF_URL', 'STATE1', 'ATP_DUR_TYP_KEY', 3, 'STDY_SUBJ_AREA', 'ACCT-1', 'RICHTEXT-101', 'FEE-1', 'kuali.lu.type.CreditCourse','IDENT-1', 'INSTR-1','CLU-5', {ts '2003-01-01 00:00:00.0'})
 
 //LRR types and states
-INSERT INTO KSEN_TYPE (ID, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.credential.awarded ', 'Credential Awarded', 'Indicates the credential that was awarded', 0)
-INSERT INTO KSEN_TYPE (ID, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.credits.earned', 'Credits Earned', 'Indicates the number of credits the student earned for successfully completing the course.  Often done via a calculation by looking at the calculated grade and either awarding the number of attempted credits if it is a passing grade.', 0)
-INSERT INTO KSEN_TYPE (ID, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.final.grade.administrative', 'Administrative Final Grade', 'Administrative Grade that was recorded for this student', 0)
-INSERT INTO KSEN_TYPE (ID, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.final.grade.assigned', 'Assigned Final Grade', 'Grade as it was originally submitted by the faculty member on the grade sheet.', 0)
-INSERT INTO KSEN_TYPE (ID, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.final.grade.calculated', 'Calculated Final Grade', 'Grade calculated based on the scale or option chosen and the grade actually submitted by the faculty member.', 0)
-INSERT INTO KSEN_TYPE (ID, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.interim.grade', 'Interim Grade', 'Grade assigned during at an interim period often used to flag students who might be struggling', 0)
-INSERT INTO KSEN_TYPE (ID, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.standardized.test.ranking', 'Standardized Test Ranking', 'The ranking of the student within the peer group who took the test during the same time period.  Often recorded as a percentage or a decile', 0)
-INSERT INTO KSEN_TYPE (ID, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.standardized.test.score', 'Standardized Test Score', 'A score a student got by taking standardized test.', 0)
+INSERT INTO KSEN_TYPE (TYPE_KEY, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.credential.awarded ', 'Credential Awarded', 'Indicates the credential that was awarded', 0)
+INSERT INTO KSEN_TYPE (TYPE_KEY, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.credits.earned', 'Credits Earned', 'Indicates the number of credits the student earned for successfully completing the course.  Often done via a calculation by looking at the calculated grade and either awarding the number of attempted credits if it is a passing grade.', 0)
+INSERT INTO KSEN_TYPE (TYPE_KEY, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.final.grade.administrative', 'Administrative Final Grade', 'Administrative Grade that was recorded for this student', 0)
+INSERT INTO KSEN_TYPE (TYPE_KEY, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.final.grade.assigned', 'Assigned Final Grade', 'Grade as it was originally submitted by the faculty member on the grade sheet.', 0)
+INSERT INTO KSEN_TYPE (TYPE_KEY, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.final.grade.calculated', 'Calculated Final Grade', 'Grade calculated based on the scale or option chosen and the grade actually submitted by the faculty member.', 0)
+INSERT INTO KSEN_TYPE (TYPE_KEY, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.interim.grade', 'Interim Grade', 'Grade assigned during at an interim period often used to flag students who might be struggling', 0)
+INSERT INTO KSEN_TYPE (TYPE_KEY, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.standardized.test.ranking', 'Standardized Test Ranking', 'The ranking of the student within the peer group who took the test during the same time period.  Often recorded as a percentage or a decile', 0)
+INSERT INTO KSEN_TYPE (TYPE_KEY, NAME, DESCR_PLAIN, VER_NBR) VALUES ('kuali.lrr.type.standardized.test.score', 'Standardized Test Score', 'A score a student got by taking standardized test.', 0)
 
-INSERT INTO KSEN_COMM_STATE (ID, NAME, PROCESS_KEY, DESCR, VER_NBR) VALUES ('kuali.lrr.state.saved', 'Saved', 'kuali.lrr.course.final.grading.lifecycle', 'The result (grade) has been entered and has been saved but not yet been submitted', 0)
-INSERT INTO KSEN_COMM_STATE (ID, NAME, PROCESS_KEY, DESCR, VER_NBR) VALUES ('kuali.lrr.state.submitted', 'Submitted', 'kuali.lrr.course.final.grading.lifecycle', 'The result (grade) has been formally submitted', 0)
-INSERT INTO KSEN_COMM_STATE (ID, NAME, PROCESS_KEY, DESCR, VER_NBR) VALUES ('kuali.lrr.state.accepted', 'Accepted', 'kuali.lrr.course.final.grading.lifecycle', 'The result (grade) has been vetted and is ready to be published to the student', 0)
+INSERT INTO KSEN_STATE (ID, NAME, PROCESS_KEY, DESCR, VER_NBR) VALUES ('kuali.lrr.state.saved', 'Saved', 'kuali.lrr.course.final.grading.lifecycle', 'The result (grade) has been entered and has been saved but not yet been submitted', 0)
+INSERT INTO KSEN_STATE (ID, NAME, PROCESS_KEY, DESCR, VER_NBR) VALUES ('kuali.lrr.state.submitted', 'Submitted', 'kuali.lrr.course.final.grading.lifecycle', 'The result (grade) has been formally submitted', 0)
+INSERT INTO KSEN_STATE (ID, NAME, PROCESS_KEY, DESCR, VER_NBR) VALUES ('kuali.lrr.state.accepted', 'Accepted', 'kuali.lrr.course.final.grading.lifecycle', 'The result (grade) has been vetted and is ready to be published to the student', 0)
 
 //Rich text for LRRs
 INSERT INTO KSEN_RICH_TEXT_T (ID, FORMATTED, PLAIN, VER_NBR) VALUES ('grade-final-lecture-desc', '<p>Final Grade for Lecture</p>', 'Final Grade for Lecture', 0)
 INSERT INTO KSEN_RICH_TEXT_T (ID, FORMATTED, PLAIN, VER_NBR) VALUES ('grade-interim-lecture-desc', '<p>Interim Grade for Lecture</p>', 'Interim Grade for Lecture', 0)
 
 //Course offerings for elegibility checking
-INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, EFF_DT, EXP_DT, VER_NBR, ATP_ID) VALUES ('courseOffering1', 'Lui one',  'CLU-1', 'kuali.lui.type.course.offering', 'kuali.lui.state.draft', 'CO-1-Desc', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 0,'testAtpId1')
-INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, EFF_DT, EXP_DT, VER_NBR, ATP_ID) VALUES ('courseOffering2', 'Lui two',  'CLU-4', 'kuali.lui.type.course.offering', 'kuali.lui.state.draft', 'CO-2-Desc', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 0,'testAtpId1')
-INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, EFF_DT, EXP_DT, VER_NBR, ATP_ID) VALUES ('courseOffering3', 'Lui three', 'CLU-3', 'kuali.lui.type.course.offering', 'kuali.lui.state.draft', 'CO-3-Desc', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 0,'testAtpId1')
+INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, EFF_DT, EXPIR_DT, VER_NBR, ATP_ID) VALUES ('courseOffering1', 'Lui one',  'CLU-1', 'kuali.lui.type.course.offering', 'kuali.lui.state.draft', 'CO-1-Desc', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 0,'testAtpId1')
+INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, EFF_DT, EXPIR_DT, VER_NBR, ATP_ID) VALUES ('courseOffering2', 'Lui two',  'CLU-4', 'kuali.lui.type.course.offering', 'kuali.lui.state.draft', 'CO-2-Desc', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 0,'testAtpId1')
+INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, EFF_DT, EXPIR_DT, VER_NBR, ATP_ID) VALUES ('courseOffering3', 'Lui three', 'CLU-3', 'kuali.lui.type.course.offering', 'kuali.lui.state.draft', 'CO-3-Desc', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 0,'testAtpId1')
 
 // LuiPersonRelationEntity
 INSERT INTO KSEN_LPR (ID, CREATEID, CREATETIME, UPDATEID, UPDATETIME, PERSONID, LUIID, COMMITMENTPERCENT, EFFECTIVEDATE, EXPIRATIONDATE, RELATION_TYPE_ID, RELATION_STATE_ID, VER_NBR) VALUES ('testStudent1Co1', 'admin', {ts '1900-01-01 00:00:00.0'}, 'admin', {ts '2000-01-01 00:00:00.0'}, 'testStudentId1', 'courseOffering1', .5, {ts '2000-01-01 00:00:00.0'}, {ts '2100-12-31 00:00:00.0'}, 'kuali.lpr.type.registrant', 'kuali.lpr.state.registered', 0)
@@ -115,7 +115,7 @@ INSERT INTO KSST_REF_STMT_REL(ID, CREATEID, CREATETIME, OBJ_ID, REF_OBJ_ID, REF_
 
 
 -- don't seem to be necessary so commented out
---INSERT INTO KSEN_COMM_STATE (ID, NAME, PROCESS_KEY, DESCR, VER_NBR) VALUES ('kuali.result.values.group.state.approved', 'Approved', 'kuali.result.values.group.process', 'The result has been approved to be used and awarded', 1)
+--INSERT INTO KSEN_STATE (ID, NAME, PROCESS_KEY, DESCR, VER_NBR) VALUES ('kuali.result.values.group.state.approved', 'Approved', 'kuali.result.values.group.process', 'The result has been approved to be used and awarded', 1)
 --INSERT INTO KSEN_RICH_TEXT_T (ID, FORMATTED, PLAIN, VER_NBR) VALUES ('RT-GRADELETTER', '<p>Standard A-F grading</p>', 'Standard A-F grading', 1)
 --INSERT INTO KSEN_LRC_RES_VAL_GRP (ID, NAME, RT_DESCR_ID, TYPE_ID, STATE_ID, RES_SCALE_ID) VALUES ('kuali.resultComponent.grade.letter', 'Standard A-F Grading', 'RT-GRADELETTER', 'kuali.result.values.group.type.multiple', 'kuali.result.values.group.state.approved', 'kuali.result.scale.grade.letter')
 
@@ -126,7 +126,7 @@ INSERT INTO KSST_REF_STMT_REL(ID, CREATEID, CREATETIME, OBJ_ID, REF_OBJ_ID, REF_
 -- title of course offering:
 INSERT INTO KSEN_LUI_IDENT (ID, CD, DIVISION, SUFX_CD, LNG_NAME, SHRT_NAME, VER_NBR) VALUES ('LUI-CO-1-IDENT', 'CRSOFFER1', 'CRSOFFER', '1', 'Course Offering 1', 'Crs Offer 1', 1)
 -- actual course offering:
-INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, OFFIC_LUI_ID, MAX_SEATS, ATP_ID, EFF_DT, EXP_DT, VER_NBR) VALUES ('LUI-CO-1', 'CO 1',  'CLU-1', 'kuali.lui.type.course.offering', 'kuali.lui.state.offered', 'LUI-CO-1-RT', 'LUI-CO-1-IDENT', 50, 'kuali.atp.type.winter', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 1)
+INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, OFFIC_LUI_ID, MAX_SEATS, ATP_ID, EFF_DT, EXPIR_DT, VER_NBR) VALUES ('LUI-CO-1', 'CO 1',  'CLU-1', 'kuali.lui.type.course.offering', 'kuali.lui.state.offered', 'LUI-CO-1-RT', 'LUI-CO-1-IDENT', 50, 'kuali.atp.type.winter', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 1)
 -- set grading method on course offering:  // undefined: kuali.resultComponent.grade.letter
 INSERT INTO KSEN_LUI_RV_GRP_RELTN (ID, LUI_ID, RV_GRP_ID) VALUES ('LUI-CO-1-RV-1', 'LUI-CO-1', 'kuali.resultComponent.grade.letter')
 -- final grade roster for course offering:
@@ -136,7 +136,7 @@ INSERT INTO KSEN_LPRROSTER_LUI_RELTN (LUI_ID, LPRROSTER_ID) VALUES ('LUI-CO-1', 
 -- title of activity:
 INSERT INTO KSEN_LUI_IDENT (ID, CD, DIVISION, SUFX_CD, LNG_NAME, SHRT_NAME, VER_NBR) VALUES ('LUI-ACT-1-IDENT', 'ACTIVITY1', 'ACTIVITY', '1', 'Activity 1', 'Actvty 1', 1)
 -- add activity to course offering:  // undefined: CLU-ACT-1
-INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, OFFIC_LUI_ID, MAX_SEATS, ATP_ID, EFF_DT, EXP_DT, VER_NBR) VALUES ('LUI-ACT-1', 'Activity 1',  'CLU-ACT-1', 'kuali.lui.type.activity.offering.lecture', 'kuali.lui.state.offered', 'LUI-ACT-1-RT', 'LUI-ACT-1-IDENT', 20, 'kuali.atp.type.winter', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 1)
+INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, OFFIC_LUI_ID, MAX_SEATS, ATP_ID, EFF_DT, EXPIR_DT, VER_NBR) VALUES ('LUI-ACT-1', 'Activity 1',  'CLU-ACT-1', 'kuali.lui.type.activity.offering.lecture', 'kuali.lui.state.offered', 'LUI-ACT-1-RT', 'LUI-ACT-1-IDENT', 20, 'kuali.atp.type.winter', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 1)
 -- set meeting time for activity:
 INSERT INTO KSEN_LUI_MTG_SCHE (ID, LUI_ID, SPACE_ID, TM_PRD, CREATETIME ,VER_NBR) VALUES ('LUI-ACT-1-MEET', 'LUI-CO-1', '', 'WE,TH;1300,1400', {ts '2011-01-01 00:00:00.0'}, 1)
 -- assign instructor for activity:
@@ -147,7 +147,7 @@ INSERT INTO KSEN_LUILUI_RELTN (ID, LUI_ID, RELATED_LUI_ID, STATE_ID, TYPE_ID, CR
 -- title of activity:
 INSERT INTO KSEN_LUI_IDENT (ID, CD, DIVISION, SUFX_CD, LNG_NAME, SHRT_NAME, VER_NBR) VALUES ('LUI-ACT-2-IDENT', 'ACTIVITY2', 'ACTIVITY', '2', 'Activity 2', 'Actvty 2', 1)
 -- add activity to course offering:  // undefined: CLU-ACT-1
-INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, OFFIC_LUI_ID, MAX_SEATS, ATP_ID, EFF_DT, EXP_DT, VER_NBR) VALUES ('LUI-ACT-2', 'Activity 2',  'CLU-ACT-2', 'kuali.lui.type.activity.offering.lab', 'kuali.lui.state.offered', 'LUI-ACT-2-RT', 'LUI-ACT-2-IDENT', 20, 'kuali.atp.type.winter', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 1)
+INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, OFFIC_LUI_ID, MAX_SEATS, ATP_ID, EFF_DT, EXPIR_DT, VER_NBR) VALUES ('LUI-ACT-2', 'Activity 2',  'CLU-ACT-2', 'kuali.lui.type.activity.offering.lab', 'kuali.lui.state.offered', 'LUI-ACT-2-RT', 'LUI-ACT-2-IDENT', 20, 'kuali.atp.type.winter', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 1)
 -- set meeting time for activity:
 INSERT INTO KSEN_LUI_MTG_SCHE (ID, LUI_ID, SPACE_ID, TM_PRD, CREATETIME ,VER_NBR) VALUES ('LUI-ACT-2-MEET', 'LUI-ACT-2', '', 'MO,TU,FR;0930,1030', {ts '2011-01-01 00:00:00.0'}, 1)
 -- assign instructor for activity:
@@ -156,7 +156,7 @@ INSERT INTO KSEN_LPR (ID, LUIID, PERSONID, RELATION_STATE_ID, RELATION_TYPE_ID, 
 INSERT INTO KSEN_LUILUI_RELTN (ID, LUI_ID, RELATED_LUI_ID, STATE_ID, TYPE_ID, CREATETIME, VER_NBR) VALUES ('LUI-REL-ACT2-CO1', 'LUI-ACT-2', 'LUI-CO-1', 'kuali.lui.lui.relation.state.active', 'kuali.lui.lui.relation.type.registeredforvia', {ts '2011-01-01 00:00:00.0'}, 1)
 
 -- create registration group to hold course offering & activities:
-INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, OFFIC_LUI_ID, ATP_ID, EFF_DT, EXP_DT, VER_NBR) VALUES ('LUI-RG-1', 'RegGroup 1', '', 'kuali.lui.type.registration.group', 'kuali.lui.state.offered', 'LUI-ACT-1-RT', 'LUI-ACT-1-IDENT', 'kuali.atp.type.winter', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 1)
+INSERT INTO KSEN_LUI (ID, NAME, CLU_ID, TYPE_ID, STATE_ID, RT_DESCR_ID, OFFIC_LUI_ID, ATP_ID, EFF_DT, EXPIR_DT, VER_NBR) VALUES ('LUI-RG-1', 'RegGroup 1', '', 'kuali.lui.type.registration.group', 'kuali.lui.state.offered', 'LUI-ACT-1-RT', 'LUI-ACT-1-IDENT', 'kuali.atp.type.winter', {ts '2011-01-01 00:00:00.0'}, {ts '2011-12-31 00:00:00.0'}, 1)
 -- associate registration group with course offering & activityies:
 INSERT INTO KSEN_LUILUI_RELTN (ID, LUI_ID, RELATED_LUI_ID, STATE_ID, TYPE_ID, CREATETIME, VER_NBR) VALUES ('LUI-REL-RG1-CO1', 'LUI-RG-1', 'LUI-CO-1', 'kuali.lui.lui.relation.state.active', 'kuali.lui.lui.relation.type.registeredforvia', {ts '2011-01-01 00:00:00.0'}, 1)
 INSERT INTO KSEN_LUILUI_RELTN (ID, LUI_ID, RELATED_LUI_ID, STATE_ID, TYPE_ID, CREATETIME, VER_NBR) VALUES ('LUI-REL-RG1-ACT1', 'LUI-RG-1', 'LUI-ACT-1', 'kuali.lui.lui.relation.state.active', 'kuali.lui.lui.relation.type.registeredforvia', {ts '2011-01-01 00:00:00.0'}, 1)
