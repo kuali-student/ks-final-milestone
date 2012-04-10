2d7263d512f953a264de88844be3c838d0343439
dedewet <dedewet@kuali.org>
2012-04-10 09:04:22 -0400

    KSCM-421 & KSCM-391
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@29618 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-impl/src/main/java/org/kuali/student/lum/course/service/impl/CourseServiceUtils.java
+++ ks-lum/ks-lum-impl/src/main/java/org/kuali/student/lum/course/service/impl/CourseServiceUtils.java
@@ -1,41 +1,21 @@
 package org.kuali.student.lum.course.service.impl;
 
-import java.util.List;
-
 import org.apache.commons.beanutils.PropertyUtils;
-import org.kuali.student.r1.lum.lrc.dto.ResultComponentInfo;
-import org.kuali.student.r1.lum.lu.service.LuService;
-import org.kuali.student.r1.lum.statement.typekey.ReqComponentFieldTypes;
-import org.kuali.student.r2.common.dto.ContextInfo;
-import org.kuali.student.r2.common.dto.CurrencyAmountInfo;
-import org.kuali.student.r2.common.exceptions.AlreadyExistsException;
-import org.kuali.student.r2.common.exceptions.CircularRelationshipException;
-import org.kuali.student.r2.common.exceptions.DataValidationErrorException;
-import org.kuali.student.r2.common.exceptions.DependentObjectsExistException;
-import org.kuali.student.r2.common.exceptions.DoesNotExistException;
-import org.kuali.student.r2.common.exceptions.InvalidParameterException;
-import org.kuali.student.r2.common.exceptions.MissingParameterException;
-import org.kuali.student.r2.common.exceptions.OperationFailedException;
-import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
-import org.kuali.student.r2.common.exceptions.UnsupportedActionException;
-import org.kuali.student.r2.common.exceptions.VersionMismatchException;
 import org.kuali.student.r1.core.statement.dto.ReqCompFieldInfo;
 import org.kuali.student.r1.core.statement.dto.ReqComponentInfo;
 import org.kuali.student.r1.core.statement.dto.StatementTreeViewInfo;
 import org.kuali.student.r1.core.statement.service.StatementService;
+import org.kuali.student.r1.lum.statement.typekey.ReqComponentFieldTypes;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.dto.CurrencyAmountInfo;
+import org.kuali.student.r2.common.exceptions.*;
 import org.kuali.student.r2.lum.clu.dto.AffiliatedOrgInfo;
 import org.kuali.student.r2.lum.clu.dto.CluSetInfo;
 import org.kuali.student.r2.lum.clu.service.CluService;
-import org.kuali.student.r2.lum.course.dto.ActivityInfo;
-import org.kuali.student.r2.lum.course.dto.CourseCrossListingInfo;
-import org.kuali.student.r2.lum.course.dto.CourseFeeInfo;
-import org.kuali.student.r2.lum.course.dto.CourseInfo;
-import org.kuali.student.r2.lum.course.dto.CourseJointInfo;
-import org.kuali.student.r2.lum.course.dto.CourseRevenueInfo;
-import org.kuali.student.r2.lum.course.dto.CourseVariationInfo;
-import org.kuali.student.r2.lum.course.dto.FormatInfo;
-import org.kuali.student.r2.lum.course.dto.LoDisplayInfo;
+import org.kuali.student.r2.lum.course.dto.*;
 import org.kuali.student.r2.lum.course.service.CourseService;
+
+import java.util.List;
 
 public class CourseServiceUtils {
 	public static void resetIds(CourseInfo course) {
@@ -109,7 +89,7 @@ public class CourseServiceUtils {
 	/**
 	 * Clears out ids recursively and also copies adhock clusets
 	 * @param statementTreeView
-	 * @param luService
+	 * @param cluService
 	 * @throws OperationFailedException
 	 */
 	private static void clearStatementTreeViewIdsRecursively(StatementTreeViewInfo statementTreeView, String newState, CluService cluService,ContextInfo contextInfo) throws OperationFailedException{
@@ -127,16 +107,15 @@ public class CourseServiceUtils {
 				   ReqComponentFieldTypes.PROGRAM_CLUSET_KEY.getId().equals(field.getType())||
 				   ReqComponentFieldTypes.CLUSET_KEY.getId().equals(field.getType())){
 					try {
-						//TODO KSCM-421 						cluService.getCluSetInfo(field.getValue(),contextInfo);
-//						cluSet.setId(null);
-//						cluSet.setState(newState);
-//						//Clear clu ids if membership info exists, they will be re-added based on membership info 
-//						if (cluSet.getMembershipQuery() != null){
-//							cluSet.getCluIds().clear();
-//							cluSet.getCluSetIds().clear();
-//						}
-//						cluSet = cluService.createCluSet(cluSet.getType(), cluSet,contextInfo);
-//						field.setValue(cluSet.getId());
+						CluSetInfo cluSet = cluService.getCluSetInfo(field.getValue(), contextInfo);
+						cluSet.setId(null);
+						cluSet.setStateKey(newState);
+						//Clear clu ids if membership info exists, they will be re-added based on membership info
+						if (cluSet.getMembershipQuery() != null){
+							cluSet.getCluIds().clear();
+							cluSet.getCluSetIds().clear();
+						}
+						field.setValue(cluSet.getId());
 					} catch (Exception e) {
 						throw new OperationFailedException("Error copying clusets.", e);
 					}
