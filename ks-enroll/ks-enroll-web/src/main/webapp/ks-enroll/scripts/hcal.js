023403e8d04c55df8147b0bb1843f101ebfb7db3
bobhurt <bobhurt@kuali.org>
2012-04-17 19:20:46 -0400

    KSENROLL-546: UX didn't like the Enter=Tab solution and anyway it causes a problem with selecting items from auto-populate dropdown lists.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@29941 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-web/src/main/webapp/ks-enroll/scripts/hcal.js
+++ ks-enroll/ks-enroll-web/src/main/webapp/ks-enroll/scripts/hcal.js
@@ -1,7 +1,9 @@
 jq(function() {
     // code to execute when the DOM is ready
 
-    jq("#KS-HolidayCalendar-MetaSection_div input[type='text']").bind('keypress', tabToNextInput);
+    // the following code is not currently being used, because a hidden ActionButton which
+    // does nothing was used instead to change the behavior of the Enter key
+    //jq("#KS-HolidayCalendar-MetaSection_div input[type='text']").bind('keypress', tabToNextInput);
 
     // until Rice 2.2 allows onXxxScript properties to work...
     jq('#viewlayout_div').delegate('#KS-HolidayCalendar-HolidaySection_group input[id^=allDayCheckbox]', 'click', function() {
