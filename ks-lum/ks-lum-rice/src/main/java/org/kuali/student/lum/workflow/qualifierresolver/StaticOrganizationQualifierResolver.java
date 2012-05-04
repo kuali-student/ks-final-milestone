75fa72c72043ff30bd2a5d8d7b439371d59eee85
jacobusroos <jacobusroos@kuali.org>
2012-05-04 06:42:35 -0400

    KSCM-392 : I left the todo in since we have not been able to test this logic yet
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@30618 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-rice/src/main/java/org/kuali/student/lum/workflow/qualifierresolver/StaticOrganizationQualifierResolver.java
+++ ks-lum/ks-lum-rice/src/main/java/org/kuali/student/lum/workflow/qualifierresolver/StaticOrganizationQualifierResolver.java
@@ -3,17 +3,6 @@
  */
 package org.kuali.student.lum.workflow.qualifierresolver;
 
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-
 import org.kuali.rice.kew.engine.RouteContext;
 import org.kuali.rice.kew.rule.xmlrouting.XPathHelper;
 import org.kuali.rice.student.bo.KualiStudentKimAttributes;
@@ -24,6 +13,16 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A QualifierResolver class that takes one or more organization ids from the Route Node configuration XML on the
@@ -94,8 +93,41 @@ public class StaticOrganizationQualifierResolver extends AbstractOrganizationSer
     }
 
     @Override
-    // TODO KSCM-392
-    public List<Map<String, String>> resolve(RouteContext routeContext) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    // TODO KSCM-392 we added the logic suplied in ks1.3 still neeeds to be tested.
+    public List<Map<String, String>> resolve(RouteContext context) {
+        List<Map<String,String>> attributeSets = new ArrayList<Map<String,String>>();
+        XPath xPath = XPathHelper.newXPath();
+        NodeList organizationElements;
+        try {
+            DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = db.parse(new InputSource(new StringReader(context.getNodeInstance().getRouteNode().getContentFragment())));
+            organizationElements = (NodeList) xPath.evaluate("//" + getOrganizationIdXmlTagName(), document, XPathConstants.NODESET);
+        } catch (Exception e) {
+            LOG.error(e);
+            throw new RuntimeException("Encountered an issue fetching organization ids using xml tag name '" + getOrganizationIdXmlTagName() + "'.", e);
+        }
+        if (organizationElements.getLength() == 0) {
+            LOG.error("No organizations found in Route Node xml configuration using xml tag name '" + getOrganizationIdXmlTagName() + "'");
+            throw new RuntimeException("No organizations found in Route Node xml configuration using xml tag name '" + getOrganizationIdXmlTagName() + "'");
+        }
+        String orgId = "";
+        try {
+            for (int i = 0; i < organizationElements.getLength(); i++) {
+                Node organizationElement = organizationElements.item(i);
+                orgId = "";
+                orgId = organizationElement.getTextContent();
+                OrgInfo orgInfo = getOrganizationService().getOrg(orgId, new ContextInfo());
+                Map<String,String> attrSet = new LinkedHashMap<String,String>();
+                attrSet.put(KualiStudentKimAttributes.QUALIFICATION_ORG_ID, orgInfo.getId());
+                attributeSets.add(attrSet);
+            }
+        } catch (DOMException e) {
+            LOG.error(e);
+            throw new RuntimeException("Error getting organization from XML node", e);
+        } catch (Exception e) {
+            LOG.error(e);
+            throw new RuntimeException("Error getting organization with id '" + orgId + "' from OrganizationService", e);
+        }
+        return attributeSets;
     }
 }
