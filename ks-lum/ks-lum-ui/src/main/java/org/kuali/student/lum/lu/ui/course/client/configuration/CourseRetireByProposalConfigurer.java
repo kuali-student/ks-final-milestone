e205634f00bff9cc2619d5fa909851262c0bf1cb
vgadiyak <vgadiyak@kuali.org>
2012-05-07 09:37:34 -0400

    KSCM-1680 KSLAB-2593 configure: logic to edit or just view
    KSCM-1783
    KSLAB-2592
    PREV_START_TERM
    
    git-svn-id: https://svn.kuali.org/repos/student/trunk@30684 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/configuration/CourseRetireByProposalConfigurer.java
+++ ks-lum/ks-lum-ui/src/main/java/org/kuali/student/lum/lu/ui/course/client/configuration/CourseRetireByProposalConfigurer.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.kuali.student.common.assembly.data.QueryPath;
 import org.kuali.student.common.dto.DtoConstants;
+import org.kuali.student.common.ui.client.configurable.mvc.FieldDescriptor;
 import org.kuali.student.common.ui.client.configurable.mvc.sections.Section;
 import org.kuali.student.common.ui.client.configurable.mvc.views.SectionView;
 import org.kuali.student.common.ui.client.mvc.Controller;
@@ -50,67 +51,76 @@ public class CourseRetireByProposalConfigurer extends CourseProposalConfigurer {
 
         groupName = LUUIConstants.COURSE_GROUP_NAME;
 
-    	addCluStartSection(layout);
-        String sections = getLabel(LUUIConstants.COURSE_SECTIONS);
+    	if (modelDefinition.getMetadata().isCanEdit()) {
+    		addCluStartSection(layout);
+    		String sections = getLabel(LUUIConstants.COURSE_SECTIONS);
 
-        layout.addMenu(sections);
+    		layout.addMenu(sections);
                 
-        //Retirement Info
-        layout.addMenuItem(sections, (SectionView)generateRetirementInfoSection(initSectionView(CourseSections.COURSE_INFO, getLabel(LUUIConstants.RETIREMENT_LABEL_KEY))));
+    		//Retirement Info
+    		layout.addMenuItem(sections, (SectionView)generateRetirementInfoSection(initSectionView(CourseSections.COURSE_INFO, getLabel(LUUIConstants.RETIREMENT_LABEL_KEY))));
 
-        //Collaborators
-        layout.addMenuItem(sections, new CollaboratorSectionView(CourseSections.PEOPLE_PERMISSONS, getLabel(LUUIConstants.SECTION_AUTHORS_AND_COLLABORATORS), COURSE_PROPOSAL_MODEL));
+    		//Collaborators
+    		layout.addMenuItem(sections, new CollaboratorSectionView(CourseSections.PEOPLE_PERMISSONS, getLabel(LUUIConstants.SECTION_AUTHORS_AND_COLLABORATORS), COURSE_PROPOSAL_MODEL));
 
-        //Documents
-        documentTool = new DocumentTool(LUUIConstants.REF_DOC_RELATION_PROPOSAL_TYPE,CourseSections.DOCUMENTS, getLabel(LUUIConstants.TOOL_DOCUMENTS_LABEL_KEY));
-        documentTool.setModelDefinition((DataModelDefinition)modelDefinition);
-        layout.addMenuItem(sections, documentTool);
+    		//Documents
+    		documentTool = new DocumentTool(LUUIConstants.REF_DOC_RELATION_PROPOSAL_TYPE,CourseSections.DOCUMENTS, getLabel(LUUIConstants.TOOL_DOCUMENTS_LABEL_KEY));
+    		documentTool.setModelDefinition((DataModelDefinition)modelDefinition);
+    		layout.addMenuItem(sections, documentTool);
         
-        //Add common buttons to sections except for sections with specific button behavior
-        List<Enum<?>> excludedViews = new ArrayList<Enum<?>>();
-        excludedViews.add(CourseSections.DOCUMENTS);
-        excludedViews.add(CourseSections.COURSE_REQUISITES);
-        layout.addCommonButton(LUUIConstants.COURSE_SECTIONS, layout.getSaveButton(), excludedViews);
-        layout.addCommonButton(LUUIConstants.COURSE_SECTIONS, layout.getCancelButton(CourseSections.SUMMARY), excludedViews);
+    		//Add common buttons to sections except for sections with specific button behavior
+    		List<Enum<?>> excludedViews = new ArrayList<Enum<?>>();
+    		excludedViews.add(CourseSections.DOCUMENTS);
+    		excludedViews.add(CourseSections.COURSE_REQUISITES);
+    		layout.addCommonButton(LUUIConstants.COURSE_SECTIONS, layout.getSaveButton(), excludedViews);
+    		layout.addCommonButton(LUUIConstants.COURSE_SECTIONS, layout.getCancelButton(CourseSections.SUMMARY), excludedViews);
 
-        //Summary
-        summaryConfigurer = GWT.create(CourseRetireSummaryConfigurer.class);
-        summaryConfigurer.init(type, state, groupName,(DataModelDefinition)modelDefinition, stmtTypes, (Controller)layout, COURSE_PROPOSAL_MODEL);
-        layout.addSpecialMenuItem(summaryConfigurer.generateProposalSummarySection(true), "Review and Submit");
+    		//Summary
+    		summaryConfigurer = GWT.create(CourseRetireSummaryConfigurer.class);
+    		summaryConfigurer.init(type, state, groupName,(DataModelDefinition)modelDefinition, stmtTypes, (Controller)layout, COURSE_PROPOSAL_MODEL);
+    		layout.addSpecialMenuItem(summaryConfigurer.generateProposalSummarySection(true), "Review and Submit");
         
-        //Specific buttons for certain views
-        //TODO people and permissions will use a different button than continue
-        layout.addButtonForView(CourseSections.DOCUMENTS, getContinueButton(layout));
+    		//Specific buttons for certain views
+    		//TODO people and permissions will use a different button than continue
+    		layout.addButtonForView(CourseSections.DOCUMENTS, getContinueButton(layout));
+    	} else {
+    		summaryConfigurer = GWT.create(CourseRetireSummaryConfigurer.class);
+    		summaryConfigurer.init(type, state, groupName,(DataModelDefinition)modelDefinition, stmtTypes, (Controller)layout, COURSE_PROPOSAL_MODEL);
+    		layout.removeMenuNavigation();
+    		layout.addView(summaryConfigurer.generateProposalSummarySection(false));
+    	}    	
         
-        // Proposal Status
-        layout.addContentWidget(layout.getWfUtilities().getProposalStatusLabel());
+   		// Proposal Status
+   		layout.addContentWidget(layout.getWfUtilities().getProposalStatusLabel());
         
-        // Comment Tool
-        final CommentTool commentTool = new CommentTool(CourseSections.COMMENTS, getLabel(LUUIConstants.TOOL_COMMENTS_LABEL_KEY), "kuali.comment.type.generalRemarks", "Proposal Comments");
-        commentTool.setController(layout);
+   		// Comment Tool
+   		final CommentTool commentTool = new CommentTool(CourseSections.COMMENTS, getLabel(LUUIConstants.TOOL_COMMENTS_LABEL_KEY), "kuali.comment.type.generalRemarks", "Proposal Comments");
+   		commentTool.setController(layout);
         
-        layout.addContentWidget(new KSButton("Comments", ButtonStyle.DEFAULT_ANCHOR, new ClickHandler() {
+   		layout.addContentWidget(new KSButton("Comments", ButtonStyle.DEFAULT_ANCHOR, new ClickHandler() {
             
-            @Override
-            public void onClick(ClickEvent event) {
-                commentTool.show();
-            }
-        }));
-        
-        // Decision Tool
-        final DecisionPanel decisionPanel = new DecisionPanel(CourseSections.DECISIONS, getLabel(LUUIConstants.TOOL_DECISION_LABEL_KEY), "kuali.comment.type.generalRemarks");
-        layout.addView(decisionPanel);
-        layout.addContentWidget(new KSButton("Decisions", ButtonStyle.DEFAULT_ANCHOR, new ClickHandler() {
+   			@Override
+   			public void onClick(ClickEvent event) {
+   				commentTool.show();
+   			}
+   		}));
+       
+   		// Decision Tool
+   		final DecisionPanel decisionPanel = new DecisionPanel(CourseSections.DECISIONS, getLabel(LUUIConstants.TOOL_DECISION_LABEL_KEY), "kuali.comment.type.generalRemarks");
+   		layout.addView(decisionPanel);
+   		layout.addContentWidget(new KSButton("Decisions", ButtonStyle.DEFAULT_ANCHOR, new ClickHandler() {
 
-            @Override
-            public void onClick(ClickEvent event) {
-            	decisionPanel.show();
-            }
-        }));
-
+   			@Override
+   			public void onClick(ClickEvent event) {
+    			decisionPanel.show();
+    		}
+   		}));
     }
-    
+    	
     protected Section generateRetirementInfoSection(Section section) {
+    	FieldDescriptor fd = addField(section, PROPOSAL_PATH + "/" + PREV_START_TERM, generateMessageInfo(LUUIConstants.PROPOSAL_PREV_START_TERM));
+        fd.getFieldWidget().setVisible(false);
+        fd.hideLabel();
     	
         addField(section, PROPOSAL_TITLE_PATH, generateMessageInfo(LUUIConstants.PROPOSED_PROPOSAL_TITLE_LABEL_KEY));
         addField(section, PROPOSAL_PATH + "/" + PROPOSED_RATIONALE, generateMessageInfo(LUUIConstants.RETIREMENT_RATIONALE_LABEL_KEY),
