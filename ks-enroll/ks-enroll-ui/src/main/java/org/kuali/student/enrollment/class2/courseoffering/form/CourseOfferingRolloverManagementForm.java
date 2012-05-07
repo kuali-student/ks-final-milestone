9fb47f8f193f2bf224f4f3d1675524a45c828124
cclin <cclin@kuali.org>
2012-05-07 16:53:20 -0400

    KSENROLL-1029 Copied code from RegistrationWindowsManagementForm to CourseOfferingRolloverManagementForm so it wouldn't crash (due to needing getters/setters in CourseOfferingRolloverManagementView.xml file)
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@30724 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/form/CourseOfferingRolloverManagementForm.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/form/CourseOfferingRolloverManagementForm.java
@@ -17,6 +17,12 @@
 package org.kuali.student.enrollment.class2.courseoffering.form;
 
 import org.kuali.rice.krad.web.form.UifFormBase;
+import org.kuali.student.enrollment.acal.dto.KeyDateInfo;
+import org.kuali.student.enrollment.acal.dto.TermInfo;
+import org.kuali.student.enrollment.class2.appointment.dto.AppointmentWindowWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class //TODO ...
@@ -24,9 +30,108 @@ import org.kuali.rice.krad.web.form.UifFormBase;
  * @author Kuali Student Team
  */
 public class CourseOfferingRolloverManagementForm extends UifFormBase {
-    
-    private String fakeData;
-    public CourseOfferingRolloverManagementForm() {
-        fakeData = "hello";
+
+    private TermInfo termInfo;
+    private String termType;
+    private String termYear;
+    //period drop-down field in Appointment Windows page
+    private String periodId;
+    private String periodName;
+    private String periodInfoDetails;
+    private List<KeyDateInfo> periodMilestones;
+    private List<AppointmentWindowWrapper> appointmentWindows;
+
+    //    private List<String> appointmentWindowIdsToDelete;
+    boolean showAddWindows;
+
+//    private Map<String, List<AppointmentWindowInfo>> periodAndWindowsMap;
+
+    public CourseOfferingRolloverManagementForm(){
+        termInfo = new TermInfo();
+        periodInfoDetails = new String();
+        periodMilestones = new ArrayList<KeyDateInfo>();
+        appointmentWindows = new ArrayList<AppointmentWindowWrapper>();
+//        appointmentWindowIdsToDelete = new ArrayList<String>();
+        showAddWindows = false;
+    }
+
+    public TermInfo getTermInfo(){
+        return termInfo;
+    }
+
+    public void setTermInfo(TermInfo termInfo) {
+        this.termInfo = termInfo;
+    }
+
+    public String getTermType(){
+        return termType;
+    }
+
+    public void setTermType(String termType) {
+        this.termType = termType;
+    }
+
+    public String getTermYear(){
+        return termYear;
+    }
+
+    public void setTermYear(String termYear) {
+        this.termYear = termYear;
+    }
+
+    public String getPeriodId() {
+        return periodId;
+    }
+
+    public void setPeriodId(String periodId) {
+        this.periodId = periodId;
+    }
+
+    public String getPeriodName() {
+        return periodName;
+    }
+
+    public void setPeriodName(String periodName) {
+        this.periodName = periodName;
+    }
+
+    public String getPeriodInfoDetails() {
+        return periodInfoDetails;
+    }
+
+    public void setPeriodInfoDetails(String periodInfoDetails) {
+        this.periodInfoDetails = periodInfoDetails;
+    }
+
+    public List<KeyDateInfo> getPeriodMilestones() {
+        return periodMilestones;
+    }
+
+    public void setPeriodMilestones(List<KeyDateInfo> periodMilestones){
+        this.periodMilestones = periodMilestones;
+    }
+
+    public List<AppointmentWindowWrapper> getAppointmentWindows(){
+        return appointmentWindows;
+    }
+
+    public void setAppointmentWindows(List<AppointmentWindowWrapper> appointmentWindows){
+        this.appointmentWindows = appointmentWindows;
+    }
+
+//    public List<String> getAppointmentWindowIdsToDelete() {
+//        return appointmentWindowIdsToDelete;
+//    }
+//
+//    public void setAppointmentWindowIdsToDelete(List<String> appointmentWindowIdsToDelete) {
+//        this.appointmentWindowIdsToDelete = appointmentWindowIdsToDelete;
+//    }
+
+    public boolean isShowAddWindows() {
+        return showAddWindows;
+    }
+
+    public void setShowAddWindows(boolean showAddWindows) {
+        this.showAddWindows = showAddWindows;
     }
 }
