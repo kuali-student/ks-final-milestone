1e659fabf022564dfe1709d9fa0cd4e244ec72e6
sw <sw@kuali.org>
2012-05-10 10:16:39 -0400

    KSCM-313 - Fix GWT debugger.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@30869 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-api/src/main/java/org/kuali/student/r1/common/dictionary/service/DictionaryService.java
+++ ks-common/ks-common-api/src/main/java/org/kuali/student/r1/common/dictionary/service/DictionaryService.java
@@ -10,19 +10,17 @@ import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.ResponseWrapper;
 
 import org.kuali.student.r1.common.dictionary.dto.ObjectStructureDefinition;
-import org.kuali.student.r2.common.dto.ContextInfo;
-import org.kuali.student.r2.common.exceptions.OperationFailedException;
 
 @WebService(name = "DictionaryService", targetNamespace = "http://student.kuali.org/wsdl/dictionary")
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
 public interface DictionaryService {
 	@WebMethod
-    @RequestWrapper(className="org.kuali.student.common.dictionary.service.jaxws.GetObjectTypes", targetNamespace="http://student.kuali.org/wsdl/dictionary")    
-    @ResponseWrapper(className="org.kuali.student.common.dictionary.service.jaxws.GetObjectTypesResponse", targetNamespace="http://student.kuali.org/wsdl/dictionary")
+    @RequestWrapper(className="org.kuali.student.r1.common.dictionary.service.jaxws.GetObjectTypes", targetNamespace="http://student.kuali.org/wsdl/dictionary")    
+    @ResponseWrapper(className="org.kuali.student.r1.common.dictionary.service.jaxws.GetObjectTypesResponse", targetNamespace="http://student.kuali.org/wsdl/dictionary")
     public List<String> getObjectTypes();
 
     @WebMethod
-    @RequestWrapper(className="org.kuali.student.common.dictionary.service.jaxws.GetObjectStructure", targetNamespace="http://student.kuali.org/wsdl/dictionary")    
-    @ResponseWrapper(className="org.kuali.student.common.dictionary.service.jaxws.GetObjectStructureResponse", targetNamespace="http://student.kuali.org/wsdl/dictionary")    
+    @RequestWrapper(className="org.kuali.student.r1.common.dictionary.service.jaxws.GetObjectStructure", targetNamespace="http://student.kuali.org/wsdl/dictionary")    
+    @ResponseWrapper(className="org.kuali.student.r1.common.dictionary.service.jaxws.GetObjectStructureResponse", targetNamespace="http://student.kuali.org/wsdl/dictionary")    
     public ObjectStructureDefinition getObjectStructure(@WebParam(name = "objectTypeKey") String objectTypeKey);
 }
