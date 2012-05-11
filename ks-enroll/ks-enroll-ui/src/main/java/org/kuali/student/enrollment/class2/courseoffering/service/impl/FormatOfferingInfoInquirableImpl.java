a17cdb20de542b4da6b22becf3b4adfd0642be92
miki.harmath <miki.harmath@kuali.org>
2012-05-11 15:47:46 -0400

    KSENROLL-1071 FormatOfferingInfoLookupAndInquiryView.xml initial implementation
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@30979 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/impl/FormatOfferingInfoInquirableImpl.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2012 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+package org.kuali.student.enrollment.class2.courseoffering.service.impl;
+
+import org.kuali.rice.krad.inquiry.InquirableImpl;
+import org.kuali.student.enrollment.class2.courseoffering.util.CourseOfferingResourceLoader;
+import org.kuali.student.enrollment.common.util.ContextBuilder;
+import org.kuali.student.enrollment.courseoffering.dto.FormatOfferingInfo;
+import org.kuali.student.enrollment.courseoffering.service.CourseOfferingService;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.exceptions.*;
+
+import java.util.Map;
+
+/**
+ * This class //TODO ...
+ *
+ * @author Kuali Student Team
+ */
+public class FormatOfferingInfoInquirableImpl extends InquirableImpl {
+    private transient CourseOfferingService courseOfferingService;
+    private ContextInfo contextInfo;
+    public final static String FORMAT_ID = "formatId";
+
+    @Override
+    public FormatOfferingInfo retrieveDataObject(Map<String, String> parameters) {
+
+        try {
+            FormatOfferingInfo formatOfferingInfo = getCourseOfferingService().getFormatOffering(parameters.get(FORMAT_ID), getContextInfo());
+            return formatOfferingInfo;
+        } catch (DoesNotExistException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (InvalidParameterException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (MissingParameterException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (OperationFailedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (PermissionDeniedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        FormatOfferingInfo formatOfferingInfo = new FormatOfferingInfo();
+        formatOfferingInfo.setCourseOfferingId("www");
+        formatOfferingInfo.setFormatId("sssssssss");
+        formatOfferingInfo.setTermId("ffffffffffffffffffff");
+        return formatOfferingInfo;
+        //return null;
+    }
+
+
+    public CourseOfferingService getCourseOfferingService() {
+        return CourseOfferingResourceLoader.loadCourseOfferingService(courseOfferingService);
+    }
+
+    public ContextInfo getContextInfo() {
+        return ContextBuilder.loadContextInfo(contextInfo);
+    }
+}
