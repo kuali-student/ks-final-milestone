460670022d83b342e2e258616f4222e9ad86b5ec
thusien99 <thusien99@kuali.org>
2012-05-15 16:25:07 -0400

    KSENROLL-1070. Created For Maintenance
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@31144 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/impl/EnrollmentFeeMaintainableImpl.java
@@ -0,0 +1,114 @@
+package org.kuali.student.enrollment.class2.courseoffering.service.impl;
+
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.krad.maintenance.MaintainableImpl;
+import org.kuali.rice.krad.maintenance.MaintenanceDocument;
+import org.kuali.rice.krad.util.GlobalVariables;
+import org.kuali.rice.krad.util.KRADConstants;
+import org.kuali.student.enrollment.class2.courseoffering.dto.EnrollmentFeeFormObject;
+import org.kuali.student.enrollment.class2.courseoffering.service.EnrollmentFeeMaintainable;
+import org.kuali.student.enrollment.courseoffering.dto.ActivityOfferingInfo;
+import org.kuali.student.enrollment.courseoffering.service.CourseOfferingService;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.dto.LocaleInfo;
+import org.kuali.student.r2.common.util.constants.CourseOfferingServiceConstants;
+import org.kuali.student.r2.common.util.constants.LuiServiceConstants;
+import org.kuali.student.r2.common.util.constants.StateServiceConstants;
+import org.kuali.student.r2.common.util.constants.TypeServiceConstants;
+import org.kuali.student.r2.core.state.service.StateService;
+import org.kuali.student.r2.core.type.service.TypeService;
+
+import javax.xml.namespace.QName;
+import java.util.Locale;
+import java.util.Map;
+
+public class EnrollmentFeeMaintainableImpl extends MaintainableImpl implements EnrollmentFeeMaintainable {
+
+    private CourseOfferingService courseOfferingService;
+    private ContextInfo contextInfo;
+    private TypeService typeService;
+    private StateService stateService;
+
+    @Override
+    public void saveDataObject() {
+        if(getMaintenanceAction().equals(KRADConstants.MAINTENANCE_NEW_ACTION) ||
+                getMaintenanceAction().equals(KRADConstants.MAINTENANCE_COPY_ACTION)) {
+            try {
+                EnrollmentFeeFormObject enrollmentFeeFormObject = (EnrollmentFeeFormObject) getDataObject();
+                ActivityOfferingInfo activityOfferingInfo = getCourseOfferingService().createActivityOffering(enrollmentFeeFormObject.getAoInfo().getFormatOfferingId(),enrollmentFeeFormObject.getAoInfo().getActivityId(), LuiServiceConstants.LECTURE_ACTIVITY_OFFERING_TYPE_KEY,enrollmentFeeFormObject.getAoInfo(),getContextInfo());
+                setDataObject(new EnrollmentFeeFormObject(activityOfferingInfo));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else {   //should be edit action
+            EnrollmentFeeFormObject enrollmentFeeFormObject = (EnrollmentFeeFormObject) getDataObject();
+            try {
+                ActivityOfferingInfo activityOfferingInfo = getCourseOfferingService().updateActivityOffering(enrollmentFeeFormObject.getAoInfo().getId(), enrollmentFeeFormObject.getAoInfo(), getContextInfo());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public Object retrieveObjectForEditOrCopy(MaintenanceDocument document, Map<String, String> dataObjectKeys) {
+        try {
+            ActivityOfferingInfo info = getCourseOfferingService().getActivityOffering(dataObjectKeys.get("aoInfo.id"),getContextInfo());
+            EnrollmentFeeFormObject formObject = new EnrollmentFeeFormObject(info);
+            document.getNewMaintainableObject().setDataObject(formObject);
+            document.getOldMaintainableObject().setDataObject(formObject);
+//            StateInfo state = getStateService().getState(formObject.getDto().getStateKey(), getContextInfo());
+//            formObject.setStateName(state.getName());
+            return formObject;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void processAfterNew(MaintenanceDocument document, Map<String, String[]> requestParameters) {
+        EnrollmentFeeFormObject formObject = (EnrollmentFeeFormObject)document.getNewMaintainableObject().getDataObject();
+        document.getDocumentHeader().setDocumentDescription("Activity Offering");
+        try {
+//            StateInfo state = getStateService().getState(formObject.getDto().getStateKey(), getContextInfo());
+//            formObject.setStateName(state.getName());
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    public ContextInfo getContextInfo() {
+        if (null == contextInfo) {
+            contextInfo = new ContextInfo();
+            contextInfo.setAuthenticatedPrincipalId(GlobalVariables.getUserSession().getPrincipalId());
+            contextInfo.setPrincipalId(GlobalVariables.getUserSession().getPrincipalId());
+            LocaleInfo localeInfo = new LocaleInfo();
+            localeInfo.setLocaleLanguage(Locale.getDefault().getLanguage());
+            localeInfo.setLocaleRegion(Locale.getDefault().getCountry());
+            contextInfo.setLocale(localeInfo);
+        }
+        return contextInfo;
+    }
+
+    public TypeService getTypeService() {
+        if(typeService == null) {
+            typeService = (TypeService) GlobalResourceLoader.getService(new QName(TypeServiceConstants.NAMESPACE, "TypeService"));
+        }
+        return this.typeService;
+    }
+
+    public StateService getStateService() {
+        if(stateService == null) {
+            stateService = (StateService) GlobalResourceLoader.getService(new QName(StateServiceConstants.NAMESPACE, "StateService"));
+        }
+        return stateService;
+    }
+
+    protected CourseOfferingService getCourseOfferingService() {
+        if (courseOfferingService == null) {
+            courseOfferingService = (CourseOfferingService) GlobalResourceLoader.getService(new QName(CourseOfferingServiceConstants.NAMESPACE, "CourseOfferingService"));
+        }
+        return courseOfferingService;
+    }
+}
