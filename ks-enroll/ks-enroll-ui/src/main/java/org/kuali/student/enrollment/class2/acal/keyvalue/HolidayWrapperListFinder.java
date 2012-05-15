152838edfc1273ac32f38252463847159f8dd65f
lpan <lpan@kuali.org>
2012-05-15 19:42:48 -0400

    ksenroll-1086: use searchForHc against start & end date
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@31157 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/keyvalue/HolidayWrapperListFinder.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/keyvalue/HolidayWrapperListFinder.java
@@ -1,14 +1,18 @@
 package org.kuali.student.enrollment.class2.acal.keyvalue;
 
 import org.apache.commons.lang.StringUtils;
+import org.kuali.rice.core.api.criteria.Predicate;
+import org.kuali.rice.core.api.criteria.QueryByCriteria;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
 import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
 import org.kuali.rice.krad.uif.view.ViewModel;
+import org.kuali.student.enrollment.acal.constants.AcademicCalendarServiceConstants;
 import org.kuali.student.enrollment.acal.dto.HolidayCalendarInfo;
 import org.kuali.student.enrollment.acal.service.AcademicCalendarService;
 import org.kuali.student.enrollment.class2.acal.form.AcademicCalendarForm;
+import org.kuali.student.enrollment.common.util.ContextBuilder;
 import org.kuali.student.r2.common.constants.CommonServiceConstants;
 import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.exceptions.*;
@@ -21,11 +25,19 @@ import java.util.List;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
+import static org.kuali.rice.core.api.criteria.PredicateFactory.*;
+import static org.kuali.rice.core.api.criteria.PredicateFactory.and;
+
 
 public class HolidayWrapperListFinder extends UifKeyValuesFinderBase implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private transient AcademicCalendarService acalService;
+
+    public final static String START_DATE = "startDate";
+    public final static String END_DATE = "endDate";
+    public final static String HCAL_STATE = "atpState";
+    public final static String HCAL_TYPE = "atpType";
 
     @Override
     public List<KeyValue> getKeyValues(ViewModel model) {
@@ -44,16 +56,11 @@ public class HolidayWrapperListFinder extends UifKeyValuesFinderBase implements 
 
         //When the user inputs both startDate and endDate,
         if (startDate != null && endDate != null)  {
-            Integer theStartYear = new Integer(simpleDateformat.format(startDate));
-            Integer theEndYear = new Integer(simpleDateformat.format(endDate));
-            if (theEndYear <= theStartYear){
-                //only query HC based on theStartYear
-                holidayCalendarInfoList = buildOfficialHolidayCalendarInfoList(theStartYear);
-                
-            }else{
-                for (int year=theStartYear.intValue(); year<=theEndYear.intValue(); year++ ){
-                    holidayCalendarInfoList.addAll(buildOfficialHolidayCalendarInfoList(new Integer(year)));
-                }
+            QueryByCriteria qbc = buildQueryByCriteria(startDate, endDate);
+            try{
+                holidayCalendarInfoList = getAcalService().searchForHolidayCalendars(qbc, getContextInfo());
+             } catch (Exception e) {
+                throw new RuntimeException(e);
             }
         }
         // When an user only inputs the startDate or endDate, use the year information from either startDate or
@@ -102,10 +109,34 @@ public class HolidayWrapperListFinder extends UifKeyValuesFinderBase implements 
         
     }
 
+    private QueryByCriteria buildQueryByCriteria(Date startDate, Date endDate){
+        List<Predicate> predicates = new ArrayList<Predicate>();
+
+        Predicate startDatePredicate = and(greaterThanOrEqual(START_DATE, startDate),
+                                                   lessThanOrEqual(START_DATE, endDate));
+
+        Predicate endDatePredicate = and(greaterThanOrEqual(END_DATE, startDate),
+                                                lessThanOrEqual(END_DATE, endDate));
+
+        predicates.add(or(startDatePredicate, endDatePredicate));
+        predicates.add(equal(HCAL_STATE, AtpServiceConstants.ATP_OFFICIAL_STATE_KEY));
+        predicates.add(equal(HCAL_TYPE, AtpServiceConstants.ATP_HOLIDAY_CALENDAR_TYPE_KEY));
+
+        QueryByCriteria.Builder qbcBuilder = QueryByCriteria.Builder.create();
+        qbcBuilder.setPredicates(predicates.toArray(new Predicate[predicates.size()]));
+        QueryByCriteria qbc = qbcBuilder.build();
+
+        return qbc;
+    }
+
     public AcademicCalendarService getAcalService() {
         if(acalService == null) {
             acalService = (AcademicCalendarService) GlobalResourceLoader.getService(new QName(CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "acal", "AcademicCalendarService"));
         }
         return this.acalService;
+    }
+
+   public ContextInfo getContextInfo() {
+        return ContextBuilder.loadContextInfo();
     }
 }
