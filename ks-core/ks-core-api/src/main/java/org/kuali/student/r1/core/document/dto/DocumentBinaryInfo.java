9c3801791622682d2893a9556b8a7081743b2320
sw <sw@kuali.org>
2012-05-17 07:15:25 -0400

    KSCM-597 - Removed R1 RichTextInfo
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@31236 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-api/src/main/java/org/kuali/student/r1/core/document/dto/DocumentBinaryInfo.java
+++ ks-core/ks-core-api/src/main/java/org/kuali/student/r1/core/document/dto/DocumentBinaryInfo.java
@@ -29,7 +29,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.kuali.student.r2.common.dto.MetaInfo;
-import org.kuali.student.r1.common.dto.RichTextInfo;
+import org.kuali.student.r2.common.dto.RichTextInfo;
 import org.kuali.student.core.ws.binding.JaxbAttributeMapListAdapter;
 
 /**
