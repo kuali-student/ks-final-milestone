e3ff0df728d4018dfd4e3cf9cb4ecf5f793c7cb7
gtaylor <gtaylor@kuali.org>
2012-05-17 15:59:48 -0400

    KSENROLL-1070: Updated labels and mapping in the main doc xml for desc and amount. Also updated the types and states.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@31279 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/dto/EnrollmentFeeFormObject.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/dto/EnrollmentFeeFormObject.java
@@ -4,6 +4,7 @@ import org.kuali.student.enrollment.acal.constants.AcademicCalendarServiceConsta
 import org.kuali.student.enrollment.acal.dto.TermInfo;
 import org.kuali.student.enrollment.courseoffering.dto.ActivityOfferingInfo;
 import org.kuali.student.enrollment.courseoffering.dto.FormatOfferingInfo;
+import org.kuali.student.r2.common.util.constants.FeeServiceConstants;
 import org.kuali.student.r2.common.util.constants.LuiServiceConstants;
 import org.kuali.student.r2.core.fee.dto.EnrollmentFeeInfo;
 
@@ -12,20 +13,12 @@ import java.io.Serializable;
 public class EnrollmentFeeFormObject implements Serializable{
 
     private EnrollmentFeeInfo efInfo;
-   // private FormatOfferingInfo formatOffering; Tanveer
-   // private TermInfo term; Tanveer
 
     public EnrollmentFeeFormObject(){
         efInfo = new EnrollmentFeeInfo();
-        //FIXME: As we dont have Lui types in DB and in mock, just assigning term state for now to get the ui working...
-        efInfo.setStateKey(AcademicCalendarServiceConstants.TERM_DRAFT_STATE_KEY);
-        efInfo.setTypeKey(LuiServiceConstants.LECTURE_ACTIVITY_OFFERING_TYPE_KEY);
-//        aoInfo.setActivityId("CLU-4");
-//        aoInfo.setName("Activity Offering");
+        efInfo.setStateKey(FeeServiceConstants.FEE_ACTIVE_STATE_KEY);
+        efInfo.setTypeKey(FeeServiceConstants.FEE_ENROLLMENT_TYPE_KEY);
 
-
-        // formatOffering = new FormatOfferingInfo();  Tanveer
-        // term = new TermInfo(); Tanveer
     }
 
     public EnrollmentFeeFormObject(EnrollmentFeeInfo info){
@@ -41,26 +34,4 @@ public class EnrollmentFeeFormObject implements Serializable{
         this.efInfo = efInfo;
     }
 
-    /*  Tanveer Start comments
- public EnrollmentFeeInfo getDescr() {
-      return descr;
- }
-
-public FormatOfferingInfo getFormatOffering() {
-     return formatOffering;
- }
-
- public void setFormatOffering(FormatOfferingInfo formatOffering) {
-     this.formatOffering = formatOffering;
- }
-
- public TermInfo getTerm() {
-     return term;
- }
-
- public void setTerm(TermInfo term) {
-     this.term = term;
- }
-
- Tanveer End comments */
 }
