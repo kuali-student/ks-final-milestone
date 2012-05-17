99a3c73a5c277512b2a1d9d8c2142ee93feae82b
thusien99 <thusien99@kuali.org>
2012-05-17 09:44:57 -0400

    KSENROLL-1070. Updated for Enrollment Fee Maintenance by changing aoInfo to efInfo
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@31255 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/dto/EnrollmentFeeFormObject.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/dto/EnrollmentFeeFormObject.java
@@ -5,53 +5,62 @@ import org.kuali.student.enrollment.acal.dto.TermInfo;
 import org.kuali.student.enrollment.courseoffering.dto.ActivityOfferingInfo;
 import org.kuali.student.enrollment.courseoffering.dto.FormatOfferingInfo;
 import org.kuali.student.r2.common.util.constants.LuiServiceConstants;
+import org.kuali.student.r2.core.fee.dto.EnrollmentFeeInfo;
 
 import java.io.Serializable;
 
 public class EnrollmentFeeFormObject implements Serializable{
 
-    private ActivityOfferingInfo aoInfo;
-    private FormatOfferingInfo formatOffering;
-    private TermInfo term;
+    private EnrollmentFeeInfo efInfo;
+   // private FormatOfferingInfo formatOffering; Tanveer
+   // private TermInfo term; Tanveer
 
     public EnrollmentFeeFormObject(){
-        aoInfo = new ActivityOfferingInfo();
+        efInfo = new EnrollmentFeeInfo();
         //FIXME: As we dont have Lui types in DB and in mock, just assigning term state for now to get the ui working...
-        aoInfo.setStateKey(AcademicCalendarServiceConstants.TERM_DRAFT_STATE_KEY);
-        aoInfo.setTypeKey(LuiServiceConstants.LECTURE_ACTIVITY_OFFERING_TYPE_KEY);
+        efInfo.setStateKey(AcademicCalendarServiceConstants.TERM_DRAFT_STATE_KEY);
+        efInfo.setTypeKey(LuiServiceConstants.LECTURE_ACTIVITY_OFFERING_TYPE_KEY);
 //        aoInfo.setActivityId("CLU-4");
 //        aoInfo.setName("Activity Offering");
-        formatOffering = new FormatOfferingInfo();
-        term = new TermInfo();
+
+
+        // formatOffering = new FormatOfferingInfo();  Tanveer
+        // term = new TermInfo(); Tanveer
     }
 
-    public EnrollmentFeeFormObject(ActivityOfferingInfo info){
+    public EnrollmentFeeFormObject(EnrollmentFeeInfo info){
         super();
-        aoInfo = info;
+        efInfo = info;
     }
 
-    public FormatOfferingInfo getFormatOffering() {
-        return formatOffering;
+    public EnrollmentFeeInfo getEfInfo() {
+        return efInfo;
     }
 
-    public void setFormatOffering(FormatOfferingInfo formatOffering) {
-        this.formatOffering = formatOffering;
+    public void setEfInfo(EnrollmentFeeInfo efInfo) {
+        this.efInfo = efInfo;
     }
 
-    public TermInfo getTerm() {
-        return term;
-    }
+    /*  Tanveer Start comments
+ public EnrollmentFeeInfo getDescr() {
+      return descr;
+ }
 
-    public void setTerm(TermInfo term) {
-        this.term = term;
-    }
+public FormatOfferingInfo getFormatOffering() {
+     return formatOffering;
+ }
 
-    public ActivityOfferingInfo getAoInfo() {
-        return aoInfo;
-    }
+ public void setFormatOffering(FormatOfferingInfo formatOffering) {
+     this.formatOffering = formatOffering;
+ }
 
-    public void setAoInfo(ActivityOfferingInfo aoInfo) {
-        this.aoInfo = aoInfo;
-    }
+ public TermInfo getTerm() {
+     return term;
+ }
 
+ public void setTerm(TermInfo term) {
+     this.term = term;
+ }
+
+ Tanveer End comments */
 }
