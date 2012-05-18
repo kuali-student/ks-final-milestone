702bdd0d8122b844fa23425932890741c2d65518
thusien99 <thusien99@kuali.org>
2012-05-18 13:58:15 -0400

    KSENROLL-1070. Updated to call web services
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@31320 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/impl/EnrollmentFeeInquirableImpl.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/impl/EnrollmentFeeInquirableImpl.java
@@ -14,15 +14,19 @@
  */
 package org.kuali.student.enrollment.class2.courseoffering.service.impl;
 
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 import org.kuali.rice.krad.inquiry.InquirableImpl;
-import org.kuali.student.enrollment.class2.courseoffering.util.ActivityOfferingConstants;
+import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.student.enrollment.class2.courseoffering.util.CourseOfferingResourceLoader;
-import org.kuali.student.enrollment.common.util.ContextBuilder;
-import org.kuali.student.enrollment.courseoffering.dto.ActivityOfferingInfo;
 import org.kuali.student.enrollment.courseoffering.service.CourseOfferingService;
 import org.kuali.student.r2.common.dto.ContextInfo;
-import org.kuali.student.r2.common.exceptions.*;
+import org.kuali.student.r2.common.dto.LocaleInfo;
+import org.kuali.student.r2.common.util.constants.FeeServiceConstants;
+import org.kuali.student.r2.core.fee.dto.EnrollmentFeeInfo;
+import org.kuali.student.r2.core.fee.service.FeeService;
 
+import javax.xml.namespace.QName;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -31,43 +35,55 @@ import java.util.Map;
  * @author Kuali Student Team
  */
 public class EnrollmentFeeInquirableImpl extends InquirableImpl {
-    private transient CourseOfferingService courseOfferingService;
+
+    private FeeService feeService;
     private ContextInfo contextInfo;
 
     @Override
-    public ActivityOfferingInfo retrieveDataObject(Map<String, String> parameters) {
+    public EnrollmentFeeInfo retrieveDataObject(Map<String, String> parameters) {
+
+        EnrollmentFeeInfo efiRet = null;
+
         try {
-            ActivityOfferingInfo activityOfferingInfo = getCourseOfferingService().getActivityOffering(parameters.get(ActivityOfferingConstants.ACTIVITYOFFERING_ID), getContextInfo());
-            return activityOfferingInfo;
-        } catch (DoesNotExistException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (InvalidParameterException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (MissingParameterException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (OperationFailedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (PermissionDeniedException e) {
+            String id = parameters.get("id");
+
+            if(id != null && !"".equals(id)){
+                efiRet = getFeeService().getFee(id,getContextInfo() );
+            }
+        } catch (Exception e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-
-        return null;
+        return efiRet;
     }
 
+    public FeeService getFeeService() {
+        if (feeService == null) {
+            this.feeService = (FeeService) GlobalResourceLoader.getService(new QName(FeeServiceConstants.NAMESPACE, FeeServiceConstants.SERVICE_NAME_LOCAL_PART));
+        }
+        return this.feeService;
+    }
+
+    public void setFeeService(FeeService feeService) {
+        this.feeService = feeService;
+    }
 
     public CourseOfferingService getCourseOfferingService() {
-        if(courseOfferingService == null)   {
-            courseOfferingService = CourseOfferingResourceLoader.loadCourseOfferingService();
-        }
-        return courseOfferingService;
+        return CourseOfferingResourceLoader.loadCourseOfferingService();
     }
 
     public ContextInfo getContextInfo() {
-        if (contextInfo == null){
-            contextInfo =  ContextBuilder.loadContextInfo();
+        if (null == contextInfo) {
+            contextInfo = new ContextInfo();
+            contextInfo.setAuthenticatedPrincipalId(GlobalVariables.getUserSession().getPrincipalId());
+            contextInfo.setPrincipalId(GlobalVariables.getUserSession().getPrincipalId());
+            LocaleInfo localeInfo = new LocaleInfo();
+            localeInfo.setLocaleLanguage(Locale.getDefault().getLanguage());
+            localeInfo.setLocaleRegion(Locale.getDefault().getCountry());
+            contextInfo.setLocale(localeInfo);
         }
         return contextInfo;
     }
-}
 
+
+}
 
