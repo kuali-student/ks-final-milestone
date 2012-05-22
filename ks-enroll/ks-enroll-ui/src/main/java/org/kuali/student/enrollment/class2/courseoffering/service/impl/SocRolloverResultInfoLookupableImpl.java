05933d6282ebe391b6e44697c3557c240d404da8
huangb <huangb@kuali.org>
2012-05-22 17:59:49 -0400

    KSENROLL-1073: Some improvements: 1)targetTermId is a required field for lookup 2)however blank search works fine. 3)now if you input any valid/invalid value in Source Term Id field, the lookup will fail and throw exception. This problem has been reported in KSENROLL-1194. 4)leave sourceTermId field as the part of lookup criteria so that we can use it to test the fix of KSENROLL-1194 later.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@31447 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/impl/SocRolloverResultInfoLookupableImpl.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/impl/SocRolloverResultInfoLookupableImpl.java
@@ -1,5 +1,6 @@
 package org.kuali.student.enrollment.class2.courseoffering.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.kuali.rice.core.api.criteria.PredicateFactory;
 import org.kuali.rice.core.api.criteria.QueryByCriteria;
 import org.kuali.rice.krad.lookup.LookupableImpl;
@@ -28,10 +29,18 @@ public class SocRolloverResultInfoLookupableImpl extends LookupableImpl {
 
         //Build up a term search criteria
         QueryByCriteria.Builder qbcBuilder = QueryByCriteria.Builder.create();
-        qbcBuilder.setPredicates(PredicateFactory.and(
-                PredicateFactory.equal(CourseOfferingConstants.SOCROLLOVERRESULTINFO_SOURCE_TERM_ID, sourceTermId),
-                PredicateFactory.equal(CourseOfferingConstants.SOCROLLOVERRESULTINFO_TARGET_TERM_ID, targetTermId)));
-
+        if (StringUtils.isNotBlank(targetTermId) && !targetTermId.isEmpty()) {
+            if (StringUtils.isNotBlank(sourceTermId) && !sourceTermId.isEmpty()){
+                qbcBuilder.setPredicates(PredicateFactory.and(
+                    PredicateFactory.equal(CourseOfferingConstants.SOCROLLOVERRESULTINFO_SOURCE_TERM_ID, sourceTermId),
+                    PredicateFactory.equal(CourseOfferingConstants.SOCROLLOVERRESULTINFO_TARGET_TERM_ID, targetTermId)));
+            }
+            else {
+                qbcBuilder.setPredicates(PredicateFactory.equal(CourseOfferingConstants.SOCROLLOVERRESULTINFO_TARGET_TERM_ID, targetTermId));
+            }
+        }else if (StringUtils.isNotBlank(sourceTermId) && !sourceTermId.isEmpty()){
+            qbcBuilder.setPredicates(PredicateFactory.equal(CourseOfferingConstants.SOCROLLOVERRESULTINFO_SOURCE_TERM_ID, sourceTermId));
+        }
         QueryByCriteria criteria = qbcBuilder.build();
 
         //Perform Search with Service Call
