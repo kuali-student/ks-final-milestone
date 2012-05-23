0014d3d4c6143f9cd8c8ca90ff70b924b7122529
bobhurt <bobhurt@kuali.org>
2012-05-22 20:30:06 -0400

    KSENROLL-1188: KRAD 2.2 changes in naming broke jQuery selection in hcal.js, but endDate UI can now be handled by KRAD.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@31457 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-web/src/main/webapp/ks-enroll/scripts/hcal.js
+++ ks-enroll/ks-enroll-web/src/main/webapp/ks-enroll/scripts/hcal.js
@@ -4,20 +4,6 @@ jq(function() {
     // the following code is not currently being used, because a hidden ActionButton which
     // does nothing was used instead to change the behavior of the Enter key
     //jq("#KS-HolidayCalendar-MetaSection_div input[type='text']").bind('keypress', tabToNextInput);
-
-    // until Rice 2.2 allows onXxxScript properties to work...
-    jq('#viewlayout_div').delegate('#KS-HolidayCalendar-HolidaySection_group input[id^=allDayCheckbox]', 'click', function() {
-        setAttributesFromAllDayCheckbox(jq(this));
-    });
-    jq('#viewlayout_div').delegate('#KS-HolidayCalendar-HolidaySection_group input[id^=dateRangeCheckbox]', 'click', function() {
-        setAttributesFromDateRangeCheckbox(jq(this));
-    });
-    jq('#viewlayout_div').delegate('#KS-HolidayCalendar-HolidaySection_group input[id^=timeField]', 'keyup', function() {
-        if (this.id.indexOf('AmPm') == -1) {  // ignore timeFieldXxxAmPm fields
-            setAttributesFromTimeField(jq(this));
-        }
-    });
-    // end of Rice 2.2 work-around
 });
 
 
@@ -26,6 +12,9 @@ function setAttributesFromAllDayCheckbox(checkbox) {
 
     if (row.find('input[id^=allDayCheckbox]:checked').length == 1) {
         // disable hh:mm & am/pm fields
+//        row.find('input[id^=timeFieldStart_]').val("");
+//        row.find('select[id^=timeFieldStartAmPm_]').focus();
+//        row.find('select[id^=timeFieldEndAmPm_]').focus();
         row.find('(input|select)[id^=timeField]').attr('disabled','true');//.attr('title','Enable by un-checking "All Day"');
     }
     else {
@@ -43,17 +32,17 @@ function setAttributesFromAllDayCheckbox(checkbox) {
         }
     }
 }
-function setAttributesFromDateRangeCheckbox(checkbox) {
-    var row = checkbox.closest('tr');
-    var fields = row.find('input[id^=endDate]');
-    if (row.find('input[id^=dateRangeCheckbox]:checked').length == 1) {
-        // enable end date field
-        fields.removeAttr('disabled');//.removeAttr('title');
-    }
-    else {
-        fields.attr('disabled','true');//.attr('title','Enable by un-checking "Date Range"');
-    }
-}
+//function setAttributesFromDateRangeCheckbox(checkbox) {
+//    var row = checkbox.closest('tr');
+//    var fields = row.find('input[id^=endDate]');
+//    if (row.find('input[id^=dateRangeCheckbox]:checked').length == 1) {
+//        // enable end date field
+//        fields.removeAttr('disabled');//.removeAttr('title');
+//    }
+//    else {
+//        fields.attr('disabled','true');//.attr('title','Enable by un-checking "Date Range"');
+//    }
+//}
 function setAttributesFromTimeField(timeField) {
     var row = timeField.closest('tr');
     var amPmFieldId = (timeField.attr('id').indexOf("Start") != -1) ? "timeFieldStartAmPm" : "timeFieldEndAmPm";
