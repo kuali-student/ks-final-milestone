35e9edd0a77b3df0b461bb58bf5de46717c40cd5
cclin <cclin@kuali.org>
2012-05-25 16:11:37 -0400

    KSENROLL-1305.  Thought I didn't need this.  Was wrong!
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@31657 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/CourseOfferingSetServiceConstants.java
+++ ks-enroll/ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/CourseOfferingSetServiceConstants.java
@@ -76,4 +76,6 @@ public class CourseOfferingSetServiceConstants {
     // canonical to course offering options
     public static final String CREDITS_MATCH_SCHEDULED_HOURS_OPTION_KEY = "kuali.canonical.course.to.course.offering.credits.match.scheduled.hours";
     public static final String NOT_COURSE_TITLE_OPTION_KEY = "kuali.canonical.course.to.course.offering.not.title";
+    // Course is R1.  Grading/credit options appear to have changed in R2, so not worth copying.
+    public static final String NOT_GRADING_CREDIT_OPTION_KEY = "kuali.canonical.course.to.course.offering.not.grading.credit.options";
 }
