82d32decfdb6ba94b26584a0cfa47e79a2ba00a3
gtaylor <gtaylor@kuali.org>
2012-05-25 15:23:27 -0400

    KSENROLL-1306: updated Term Lookup to work with ACal. Updated.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@31655 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/service/AcademicTermLookupableImpl.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/service/AcademicTermLookupableImpl.java
@@ -1,159 +1,151 @@
 package org.kuali.student.enrollment.class2.acal.service;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.kuali.rice.core.api.criteria.Predicate;
 import org.kuali.rice.core.api.criteria.QueryByCriteria;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 import org.kuali.rice.krad.lookup.LookupableImpl;
-import org.kuali.rice.krad.uif.UifParameters;
-import org.kuali.rice.krad.util.KRADConstants;
-import org.kuali.rice.krad.util.UrlFactory;
+import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.web.form.LookupForm;
 import org.kuali.student.enrollment.acal.constants.AcademicCalendarServiceConstants;
-import org.kuali.student.enrollment.acal.dto.AcademicCalendarInfo;
 import org.kuali.student.enrollment.acal.dto.TermInfo;
 import org.kuali.student.enrollment.acal.service.AcademicCalendarService;
-import org.kuali.student.mock.utilities.TestHelper;
+import org.kuali.student.enrollment.class2.acal.util.CalendarSearchViewHelperUtil;
 import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.dto.LocaleInfo;
 
 import javax.xml.namespace.QName;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
-import java.util.Properties;
 
 import static org.kuali.rice.core.api.criteria.PredicateFactory.*;
 
-//Core slice class.
-@Deprecated
+
 public class AcademicTermLookupableImpl  extends LookupableImpl {
 
-    public final static String TERM_TYPE = "typeKey";
-    public final static String TERM_NAME = "name";
-    public final static String TERM_START_DATE = "startDate";
-    public final static String TERM_END_DATE = "endDate";
-    public final static String TERM_STATE = "stateKey";
+    private transient AcademicCalendarService academicCalendarService;
+    private ContextInfo contextInfo;
+    final Logger logger = Logger.getLogger(AcademicTermLookupableImpl.class);
 
- 	private transient AcademicCalendarService academicCalendarService;
-
+    /*
+    public final static String NAME = "name";
+    public final static String START_DATE = "startDate";
+    public final static String END_DATE = "endDate";
+    public final static String CALENDAR_TYPE = "atpType";
+    */
     @Override
     protected List<?> getSearchResults(LookupForm lookupForm, Map<String, String> fieldValues, boolean unbounded) {
 
-    	TermInfo termInfo = null;
-    	List<TermInfo> termInfoList = new ArrayList<TermInfo>();
-        List<AcademicCalendarInfo> acalInfoList = new ArrayList<AcademicCalendarInfo>();
+        List<TermInfo> rList = null;
+        String name = fieldValues.get("code");
+        String year = fieldValues.get("startDate");
 
-    	String termName = fieldValues.get(TERM_NAME);
-        String termType = fieldValues.get(TERM_TYPE);
-        String termState = fieldValues.get(TERM_STATE);
-        String termStartDate = fieldValues.get(TERM_START_DATE);
-        String termEndDate = fieldValues.get(TERM_END_DATE);
+        try{
+            rList = CalendarSearchViewHelperUtil.searchForTerms(name, year, getContextInfo(), getAcademicCalendarService());
 
-    	ContextInfo context = new ContextInfo();
+            //rList = searchForTerms(name, year, getContextInfo());
+        }   catch (Exception ex){
+            logger.error("Error in AcademicTermLookupableImpl searching for term. name[" + name +"] year["+year +"]", ex);
+        }
 
+
+
+        return rList;
+
+
+    }
+                                             /*
+    private CalendarSearchViewHelperService getViewHelperService(LookupForm form){
+        if (form.getView().getViewHelperServiceClass() != null){
+            return (CalendarSearchViewHelperService)form.getView().getViewHelperService();
+        } else {
+            return (CalendarSearchViewHelperService)form.getPostedView().getViewHelperService();
+        }
+    }
+
+    public List<TermInfo> searchForTerms(String name, String year,ContextInfo context)throws Exception {
+
+        List<TermInfo> termInfoList = new ArrayList<TermInfo>();
+
+        QueryByCriteria.Builder query = buildQueryByCriteria(name,year,"Term");
+
+        List<TermInfo> terms = getAcademicCalendarService().searchForTerms(query.build(),context);
+        for (TermInfo term : terms) {
+            termInfoList.add(term);
+        }
+
+        return termInfoList;
+
+    }
+
+    private QueryByCriteria.Builder buildQueryByCriteria(String name, String year,String typeKey){
 
         QueryByCriteria.Builder qBuilder = QueryByCriteria.Builder.create();
         List<Predicate> pList = new ArrayList<Predicate>();
         Predicate p;
 
         qBuilder.setPredicates();
-        if (StringUtils.isNotBlank(termName)){
-            p = equal(TERM_NAME, termName);
-    		pList.add(p);
-        }
-
-        if (StringUtils.isNotBlank(termType)){
-            p = equal("atpType", termType);
-    		pList.add(p);
-        }
-
-        if (StringUtils.isNotBlank(termState)){
-            p = equal("atpState", termState);
-    		pList.add(p);
-        }
-
-        if (StringUtils.isNotBlank(termStartDate)){
-            try {
-                p = greaterThanOrEqual(TERM_START_DATE, new SimpleDateFormat("MM/dd/yyyy").parse(termStartDate));
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
-            }
+        if (StringUtils.isNotBlank(name)){
+            p = like(NAME, "%" + name + "%");
             pList.add(p);
         }
 
-        if (StringUtils.isNotBlank(termEndDate)){
-            try{
-                p = lessThanOrEqual(TERM_END_DATE,  new SimpleDateFormat("MM/dd/yyyy").parse(termEndDate));
-            } catch (ParseException e) {
+        if (StringUtils.isNotBlank(year)){
+            try {
+                //FIXME: Find some better way to check the year
+                Predicate startDatePredicate = and(greaterThanOrEqual(START_DATE, new SimpleDateFormat("MM/dd/yyyy").parse("01/01/" + year)),
+                        lessThanOrEqual(START_DATE, new SimpleDateFormat("MM/dd/yyyy").parse("12/31/" + year)));
+
+
+                Predicate endDatePredicate = and(greaterThanOrEqual(END_DATE, new SimpleDateFormat("MM/dd/yyyy").parse("01/01/" + year)),
+                        lessThanOrEqual(END_DATE, new SimpleDateFormat("MM/dd/yyyy").parse("12/31/" + year)));
+
+                pList.add(or(startDatePredicate, endDatePredicate));
+
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-    		pList.add(p);
+
         }
+
+        if (StringUtils.equalsIgnoreCase(typeKey, "Term")){
+            p = notIn(CALENDAR_TYPE,AcademicCalendarServiceConstants.ACADEMIC_CALENDAR_TYPE_KEY,AcademicCalendarServiceConstants.HOLIDAY_CALENDAR_TYPE_KEY);
+        }else{
+            p = equal(CALENDAR_TYPE,typeKey);
+        }
+
+        pList.add(p);
 
         if (!pList.isEmpty()){
             Predicate[] preds = new Predicate[pList.size()];
             pList.toArray(preds);
             qBuilder.setPredicates(and(preds));
         }
-
-        try {
-            termInfoList = getAcademicCalendarService().searchForTerms(qBuilder.build(),context);
-        } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-
-        return termInfoList;
+        return qBuilder;
     }
-
-
-    @Override
-    public boolean allowsMaintenanceNewOrCopyAction() {
-        return false;
-    }
-
-    @Override
-    protected String getActionUrlHref(LookupForm lookupForm, Object dataObject, String methodToCall,
-            List<String> pkNames) {
-
-        if (dataObject == null){
-            return StringUtils.EMPTY;
-        }
-
-        TermInfo term = (TermInfo)dataObject;
-
-        String acalId = null;
-        try {
-            List<AcademicCalendarInfo> atps = getAcademicCalendarService().getAcademicCalendarsForTerm(term.getId(), TestHelper.getContext1());
-            if (!atps.isEmpty()){
-                acalId = atps.get(0).getId();
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        if (StringUtils.isBlank(acalId)){
-           return StringUtils.EMPTY;
-        }
-
-        Properties props = new Properties();
-        props.put(KRADConstants.DISPATCH_REQUEST_PARAMETER, "start");
-        props.put("id",acalId);
-        props.put(UifParameters.VIEW_ID, "academicCalendarEditView");
-
-        if (StringUtils.isNotBlank(lookupForm.getReturnLocation())) {
-            props.put(KRADConstants.RETURN_LOCATION_PARAMETER, lookupForm.getReturnLocation());
-        }
-
-        return UrlFactory.parameterizeUrl("academicCalendar", props);
-    }
-
+      */
     protected AcademicCalendarService getAcademicCalendarService() {
          if(academicCalendarService == null) {
              academicCalendarService = (AcademicCalendarService) GlobalResourceLoader.getService(new QName(AcademicCalendarServiceConstants.NAMESPACE, AcademicCalendarServiceConstants.SERVICE_NAME_LOCAL_PART));
         }
         return this.academicCalendarService;
+    }
+
+    public ContextInfo getContextInfo() {
+        if (null == contextInfo) {
+            contextInfo = new ContextInfo();
+            contextInfo.setAuthenticatedPrincipalId(GlobalVariables.getUserSession().getPrincipalId());
+            contextInfo.setPrincipalId(GlobalVariables.getUserSession().getPrincipalId());
+            LocaleInfo localeInfo = new LocaleInfo();
+            localeInfo.setLocaleLanguage(Locale.getDefault().getLanguage());
+            localeInfo.setLocaleRegion(Locale.getDefault().getCountry());
+            contextInfo.setLocale(localeInfo);
+        }
+        return contextInfo;
     }
 
 }
