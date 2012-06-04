bd15a518c88352007fe1808e8008c0f551494e79
nina <nina@kuali.org>
2012-06-04 05:35:57 -0400

    KSENROLL-1352
    Create Test Template for execution of KRMS agendas in KS
    
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@31947 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-core/ks-core-krms/src/test/java/org/kuali/student/krms/ExecuteAgendaTest.java
@@ -0,0 +1,35 @@
+package org.kuali.student.krms;
+
+
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.kuali.rice.krms.test.KRMSTestCase;
+
+public class ExecuteAgendaTest extends KRMSTestCase {
+	
+//    private EngineResults engineExecute() {
+//        Map<String, String> contextQualifiers = new HashMap<String, String>();
+//        contextQualifiers.put("name", CONTEXT_NAME);
+//        contextQualifiers.put("namespaceCode", KrmsConstants.KRMS_NAMESPACE);
+//
+//        SelectionCriteria sc1 = SelectionCriteria.createCriteria(new DateTime(),
+//                contextQualifiers, Collections.<String,String>emptyMap());
+//
+//        Facts.Builder factsBuilder1 = Facts.Builder.create();
+////        factsBuilder1.addFact(TERM_NAME, 49999);
+//        factsBuilder1.addFact(TERM_NAME, "BL");
+//
+//        ExecutionOptions xOptions1 = new ExecutionOptions();
+//        xOptions1.setFlag(ExecutionFlag.LOG_EXECUTION, true);
+//
+//        EngineResults engineResults = KrmsApiServiceLocator.getEngine().execute(sc1, factsBuilder1.build(), xOptions1);
+//        assertNotNull(engineResults);
+//        assertTrue(engineResults.getAllResults().size() > 0);
+//        print(engineResults);
+//        return engineResults;
+//    }
+
+}
