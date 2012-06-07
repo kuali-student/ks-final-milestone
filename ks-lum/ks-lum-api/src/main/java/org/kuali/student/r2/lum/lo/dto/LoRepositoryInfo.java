4c78bb14f451e4b82ecce565b80c9b68bbb630a1
dedewet <dedewet@kuali.org>
2012-06-07 05:56:37 -0400

    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@32094 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-api/src/main/java/org/kuali/student/r2/lum/lo/dto/LoRepositoryInfo.java
+++ ks-lum/ks-lum-api/src/main/java/org/kuali/student/r2/lum/lo/dto/LoRepositoryInfo.java
@@ -25,7 +25,7 @@ import javax.xml.bind.annotation.XmlType;
 import java.io.Serializable;
 import java.util.Date;
 
-@XmlType(name = "LoRepositoryInfo", propOrder = {"key", "typeKey", "stateKey", "name", "descr", "rootLoId", "effectiveDate", "expirationDate","id", "meta", "attributes" })//, "_futureElements" }) TODO KSCM-372: Non-GWT translatable code})
+@XmlType(name = "LoRepositoryInfo", propOrder = {"key", "typeKey", "stateKey", "name", "descr", "rootLoId", "effectiveDate", "expirationDate", "meta", "attributes" })//, "_futureElements" }) TODO KSCM-372: Non-GWT translatable code})
 @XmlAccessorType(XmlAccessType.FIELD)
 public class LoRepositoryInfo extends KeyEntityInfo implements LoRepository, Serializable {
 
