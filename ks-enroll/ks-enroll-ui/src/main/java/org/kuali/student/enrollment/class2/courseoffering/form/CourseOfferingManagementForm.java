46f008273524b52d515697734d6bddded744960d
vpremcha <vpremcha@kuali.org>
2012-06-08 18:22:02 -0400

    KSENROLL-1120 Create Adding Activity Offering back end functionality  (wip)
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@32168 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/form/CourseOfferingManagementForm.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/form/CourseOfferingManagementForm.java
@@ -2,9 +2,8 @@ package org.kuali.student.enrollment.class2.courseoffering.form;
 
 import org.kuali.rice.krad.web.form.UifFormBase;
 import org.kuali.student.enrollment.acal.dto.TermInfo;
-import org.kuali.student.enrollment.courseoffering.dto.CourseOfferingInfo;
 import org.kuali.student.enrollment.courseoffering.dto.ActivityOfferingInfo;
-
+import org.kuali.student.enrollment.courseoffering.dto.CourseOfferingInfo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +17,18 @@ public class CourseOfferingManagementForm extends UifFormBase {
     private String inputCode;
     private boolean haveValidTerm;
     private List<CourseOfferingInfo> courseOfferingList;
-    private CourseOfferingInfo theCourseOffering;    
-    private List<ActivityOfferingInfo> activityOfferingList;
-    
+    private CourseOfferingInfo theCourseOffering;
+
+    private List<ActivityOfferingInfo> activityWrapperList;
+
+    //For Adding Activity
+    private String addFormatType;
+    private String addActivityType;
+    private int noOfActivityOfferings;
+
     public CourseOfferingManagementForm (){
         courseOfferingList = new ArrayList<CourseOfferingInfo>();
-        activityOfferingList = new ArrayList<ActivityOfferingInfo>();
+        activityWrapperList = new ArrayList<ActivityOfferingInfo>();
         haveValidTerm = false;
     }
 
@@ -98,12 +103,37 @@ public class CourseOfferingManagementForm extends UifFormBase {
     public void setTheCourseOffering(CourseOfferingInfo theCourseOffering)  {
         this.theCourseOffering = theCourseOffering;
     }
-    
-    public List<ActivityOfferingInfo> getActivityOfferingList(){
-        return activityOfferingList;
+
+    public String getAddFormatType() {
+        return addFormatType;
     }
 
-    public void setActivityOfferingList (List<ActivityOfferingInfo> activityOfferingList) {
-        this.activityOfferingList = activityOfferingList;
+    public void setAddFormatType(String addFormatType) {
+        this.addFormatType = addFormatType;
     }
+
+    public String getAddActivityType() {
+        return addActivityType;
+    }
+
+    public void setAddActivityType(String addActivityType) {
+        this.addActivityType = addActivityType;
+    }
+
+    public int getNoOfActivityOfferings() {
+        return noOfActivityOfferings;
+    }
+
+    public void setNoOfActivityOfferings(int noOfActivityOfferings) {
+        this.noOfActivityOfferings = noOfActivityOfferings;
+    }
+
+    public List<ActivityOfferingInfo> getActivityWrapperList() {
+        return activityWrapperList;
+    }
+
+    public void setActivityWrapperList(List<ActivityOfferingInfo> activityWrapperList) {
+        this.activityWrapperList = activityWrapperList;
+    }
+    
 }
