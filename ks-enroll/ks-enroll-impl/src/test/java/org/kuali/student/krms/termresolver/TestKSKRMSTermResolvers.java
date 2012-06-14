2ab40793e5d10c8284c558219856ec1b46fe2084
nina <nina@kuali.org>
2012-06-14 06:09:12 -0400

    KSENROLL-1246 Create Term Resolvers for KS KRMS CM
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@32390 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/krms/termresolver/TestKSKRMSTermResolvers.java
+++ ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/krms/termresolver/TestKSKRMSTermResolvers.java
@@ -4,6 +4,16 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kuali.rice.krms.api.engine.TermResolver;
+import org.kuali.rice.krms.api.repository.context.ContextDefinition;
+import org.kuali.rice.krms.api.repository.term.TermResolverDefinition;
+import org.kuali.rice.krms.framework.type.TermResolverTypeService;
+import org.kuali.rice.krms.impl.repository.ContextBoService;
+import org.kuali.rice.krms.impl.repository.KrmsRepositoryServiceLocator;
+import org.kuali.rice.krms.impl.repository.TermBoService;
+import org.kuali.rice.krms.impl.type.KrmsTypeResolver;
+
+
 import org.kuali.student.r2.common.dto.AttributeInfo;
 import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.dto.RichTextInfo;
@@ -18,27 +28,91 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import javax.persistence.EntityExistsException;
+
+import java.util.ArrayList;
 import java.util.List;
+import org.springframework.util.CollectionUtils;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:ks-krms-test-context.xml"})
 @Ignore
-public class TestKSKRMSTermResolvers {
-
+public class TestKSKRMSTermResolvers extends KRMSTestCase {
+	private KrmsTypeResolver typeResolver;
+	
+	@Resource(name = "termBoService")
+	private TermBoService termBoService;
+	
+	@Resource(name = "contextBoService")
+	public ContextBoService contextRepository;
     public ContextInfo callContext = null;
+    
+    @Resource(name = "ksKRMSTermResolverTypeService")
+    public KSTermResolverTypeService ksKRMSTermResolverTypeService;
 
     @Before
     public void setUp() {
+//    	this.setClearTables(false);
         callContext = new ContextInfo();
+        termBoService = KrmsRepositoryServiceLocator.getTermBoService();
+        contextRepository = KrmsRepositoryServiceLocator.getContextBoService();
 
+    }
+    
+
+    @Test
+    public void testKSKRMSTermResolverTypeServiceNotNull() {
+        assertNotNull(ksKRMSTermResolverTypeService);
+        assertNotNull(contextRepository);
+        assertNotNull(termBoService);
     }
 
     @Test
     public void testHello() {
         System.out.println("Test Hello");
-//    	assertNotNull(holdService);
+        assertNotNull(ksKRMSTermResolverTypeService);
+        TermResolverDefinition termResolverDefinition = null;
+		this.ksKRMSTermResolverTypeService.loadTermResolver(termResolverDefinition);
     }
+    
+    private void getAllTermResolverDefinitions(TermResolverDefinition contextDefinition) {
+		
+		List<TermResolverDefinition> termResolverDefs = 
+				termBoService.getTermResolversByNamespace(contextDefinition.getNamespace());
+			
+			List<TermResolver<?>> termResolvers = new ArrayList<TermResolver<?>>();
 
+			if (!CollectionUtils.isEmpty(termResolverDefs)) for (TermResolverDefinition termResolverDef : termResolverDefs) {
+				if (termResolverDef != null) {
+					TermResolver<?> termResolver = translateTermResolver(termResolverDef);
+					if (termResolver != null) termResolvers.add(termResolver);
+				}
+			}
+
+	}
+    
+	/**
+	 * This method translates a {@link TermResolverDefinition} into a {@link TermResolver}
+	 * 
+	 * @param termResolverDef
+	 * @return
+	 */
+	private TermResolver<?> translateTermResolver(TermResolverDefinition termResolverDef) {
+		if (termResolverDef == null) {
+			throw new IllegalArgumentException("termResolverDef must not be null");
+		}
+		TermResolverTypeService termResolverTypeService = 
+			typeResolver.getTermResolverTypeService(termResolverDef);
+		
+		TermResolver<?> termResolver = termResolverTypeService.loadTermResolver(termResolverDef);
+		// TODO: log warning when termResolver comes back null? or throw exception?
+		return termResolver;
+	}
+
+
+	private ContextDefinition getKRMSContext(String context) {
+		return contextRepository.getContextByNameAndNamespace(
+				context, "KR-RULE-TEST");
+	}
 }
