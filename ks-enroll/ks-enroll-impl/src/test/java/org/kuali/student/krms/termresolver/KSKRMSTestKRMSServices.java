1a03392095d50b3addaa1a1fa7a5ec8ae173c5c4
nina <nina@kuali.org>
2012-06-15 06:04:58 -0400

    KSENROLL-1246 Create Term Resolvers for KS KRMS CM
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@32471 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/krms/termresolver/KSKRMSTestKRMSServices.java
+++ ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/krms/termresolver/KSKRMSTestKRMSServices.java
@@ -1,16 +1,42 @@
 package org.kuali.student.krms.termresolver;
 
+import javax.xml.namespace.QName;
+
 import org.junit.Test;
+import org.kuali.rice.core.api.lifecycle.Lifecycle;
+import org.kuali.rice.core.framework.resourceloader.SpringResourceLoader;
 import org.kuali.rice.krms.api.repository.context.ContextDefinition;
 // import org.kuali.student.krms.KSKRMSTestCase;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class KSKRMSTestKRMSServices // extends KSKRMSTestCase
-{
+public class KSKRMSTestKRMSServices  {// extends KSKRMSTestCase {
 
-//	@Test
+//	@Autowired
+//	String acalAssembler;
+//	
+//	 @Test
 //	public void testThis() {
 //		ContextDefinition ctxDev = getKRMSContext(KSKRMSConstants.CONTEXT_STUD_ELIGIBILITY);
 //		System.out.println(ctxDev.getName());
+//	}
+//	
+//	 @Test
+//	public void testKSBeans() {
+//		assertNotNull(acalAssembler);
+//	}
+//	
+//	@Override
+//	protected Lifecycle getLoadApplicationLifecycle() {
+//		if (krmsTestResourceLoader == null) {
+//			krmsTestResourceLoader = new SpringResourceLoader(new QName(
+//					"KRMSTestHarnessApplicationResourceLoader"),
+//					"classpath:ks-krms-test-context-TEST.xml", null);
+//			krmsTestResourceLoader
+//					.setParentSpringResourceLoader(getTestHarnessSpringResourceLoader());
+//			getTestHarnessSpringResourceLoader().addResourceLoader(
+//					krmsTestResourceLoader);
+//		}
+//		return krmsTestResourceLoader;
 //	}
 	
 }
