a72e7446540b12a185e987ef0ee8020fabf3e273
nina <nina@kuali.org>
2012-06-21 20:27:07 -0400

    KSENROLL-1246 Create Term Resolvers for KS KRMS CM
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@32688 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/krms/termresolver/KSTermResolverTypeService.java
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/krms/termresolver/KSTermResolverTypeService.java
@@ -3,21 +3,30 @@ package org.kuali.student.krms.termresolver;
 import org.kuali.rice.krms.api.engine.TermResolver;
 import org.kuali.rice.krms.api.repository.term.TermResolverDefinition;
 import org.kuali.rice.krms.framework.type.TermResolverTypeService;
+import org.kuali.student.enrollment.academicrecord.service.AcademicRecordService;
 import org.kuali.student.krms.util.KSKRMSConstants;
 
 public class KSTermResolverTypeService implements TermResolverTypeService {
 
+	AcademicRecordService acadRecordService;
 	@Override
 	public TermResolver<?> loadTermResolver(
 			TermResolverDefinition termResolverDefinition) {
 		
-		if (termResolverDefinition.getName().equals(KSKRMSConstants.TERM_SPEC_COURSE)) {
+		if (termResolverDefinition.getName().equals(KSKRMSConstants.TERM_SPEC_RESOLVER_COURSE)) {
 			CompletedCourseTermResolver resolver = new CompletedCourseTermResolver();
-//			resolver.setAcadRecordService(acadRecordService);
-			return new CompletedCourseTermResolver();	
+			resolver.setAcadRecordService(acadRecordService);
+			return resolver;	
+		} else if (termResolverDefinition.getName().equals("xyz")) {
+			// TODO implement the Term resolvers by instansiating it and setting the required services...
 		}
-		// TODO Auto-generated method stub
 		return null;
+	}
+	public AcademicRecordService getAcadRecordService() {
+		return acadRecordService;
+	}
+	public void setAcadRecordService(AcademicRecordService acadRecordService) {
+		this.acadRecordService = acadRecordService;
 	}
 
 }
