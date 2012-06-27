2fe6120e5dfbdcdabd2d3ea19264699292183d14
haroon.rafique <haroon.rafique@kuali.org>
2012-06-27 11:05:33 -0400

    KSPROJPLAN-965: Merge ks-2.0-cm with ks-1.3 * signature for validator methods changed (added ContextInfo parameter)
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-1.3-merge-cm-2.0@32906 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/r2/common/service/impl/MockDefaultValidatorImpl.java
+++ ks-enroll/ks-enroll-impl/src/test/java/org/kuali/student/r2/common/service/impl/MockDefaultValidatorImpl.java
@@ -6,23 +6,24 @@ import java.util.Stack;
 
 import org.kuali.student.r1.common.dictionary.dto.FieldDefinition;
 import org.kuali.student.r1.common.dictionary.dto.ObjectStructureDefinition;
+import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.dto.ValidationResultInfo;
 import org.kuali.student.r2.common.validator.DefaultValidatorImpl;
 
 public class MockDefaultValidatorImpl extends DefaultValidatorImpl {
-	 @Override
-	 public List<ValidationResultInfo> validateObject (Object o,
-	                                                   ObjectStructureDefinition objStructure)
-	 {
-	  return new ArrayList ();
-	 }
+    @Override
+    public List<ValidationResultInfo> validateObject(Object o,
+                                                     ObjectStructureDefinition objStructure,
+                                                     ContextInfo contextInfo) {
+        return new ArrayList();
+    }
 
-	 @Override
-	 public List<ValidationResultInfo> validateObject (FieldDefinition field,
-	                                                   Object o,
-	                                                   ObjectStructureDefinition objStructure,
-	                                                   Stack<String> elementStack)
-	 {
-	  return new ArrayList ();
-	 }
+    @Override
+    public List<ValidationResultInfo> validateObject(FieldDefinition field,
+                                                     Object o,
+                                                     ObjectStructureDefinition objStructure,
+                                                     Stack<String> elementStack,
+                                                     ContextInfo contextInfo) {
+        return new ArrayList();
+    }
 }
