3a3b68f2c33c859fe5d3704bb7bc0a309af35a9b
cmann <cmann@kuali.org>
2012-06-28 16:02:51 -0400

    KSLAB-2475 Allow institution to completely override failure handling - Refactor solution based on code review with Will
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.2-sprint4@33003 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-ui/src/main/java/org/kuali/student/common/ui/client/security/AsyncCallbackFailureHandler.java
+++ ks-common/ks-common-ui/src/main/java/org/kuali/student/common/ui/client/security/AsyncCallbackFailureHandler.java
@@ -1,29 +1,75 @@
 package org.kuali.student.common.ui.client.security;
 
+import org.kuali.student.common.ui.client.service.exceptions.VersionMismatchClientException;
+import org.kuali.student.common.ui.client.widgets.KSErrorDialog;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
+
 /**
- * 
- * The implementing university should override this class if they
- * want to generically handle failure during async calls.
+ * Default KS implementation of failure handling.
  * <p>
- *
+ * Implementing institution should override this class to 
+ * custom handle failures.
  */
 public class AsyncCallbackFailureHandler {
-
+  
     /**
-     * 
-     * Override this method to handle an async failure.
-     * <p>
-     * Return 'true' if KS should handle the exception
-     * <p>
-     * Return 'false' if you handled the exception and KS 
-     * should NOT process it.
+     * Attempt to provide default session timeout handling.
+     */
+    protected static final SessionTimeoutHandler sessionTimeoutHandler = GWT.create(SpringSecurityLoginDialogHandler.class);
+    
+    public void onFailure(Throwable caught) {
+        // This code attempts to handle session timeouts
+        // it appears to be half finished.  The implementing
+        // institution may wish to override the method to
+        // to properly detect session timeout
+        
+        // The idea with the if/else below is to attempt to detect
+        // the type of error and handle it appropriately. It was
+        // not finished, so only these two errors are detected.  
+        // There is also inconsistent handling of errors in the 
+        // individual widgets as well. 
+        if (sessionTimeoutHandler.isSessionTimeout(caught)){
+             handleTimeout(caught);
+             sessionTimeoutHandler.handleSessionTimeout();
+        } else if (caught instanceof VersionMismatchClientException){
+             handleVersionMismatch(caught);
+         }else {        
+             handleFailure(caught);
+         } 
+    }
+     
+    /**
+     * Override this method to process any exceptions you wish to handle. The default implementation displays 
+     * an error dialog with the message and logs the exception.
      * 
      * @param caught
-     * @return
      */
-    public boolean handleFailure(Throwable caught) {
-        // By default we return true, which tells the KS code to
-        // process it.
-        return true;   
+    public void handleFailure(Throwable caught){
+        if (!sessionTimeoutHandler.isSessionTimeout(caught)){
+            KSErrorDialog.show(caught);
+        }
+        GWT.log("Exception:", caught);
     }
+
+    /**
+     * By default this defers to handleFailure. Override this handle a rpc failure due to timeout differently
+     * from other exceptions.
+     * 
+     * @param caught
+     */
+    public void handleTimeout(Throwable caught){
+        handleFailure(caught);
+    }
+        
+    public void handleVersionMismatch(Throwable caught){
+        String message = null;
+        if (caught.getMessage() != null){
+            message = "Version Error: " + caught.getMessage() + "\n\n";
+        }
+        message += "This page has been updated by another user since you loaded it. Please refresh and re-apply changes before saving.";
+        Window.alert(message);
+    }
+    
 }
