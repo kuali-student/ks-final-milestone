16d24d9ffde662ea52eb3a2bf8f1d0bac3edfc7f
huangb <huangb@kuali.org>
2012-06-28 16:10:11 -0400

    KSENROLL-1413:  After a Format is added, it is removed as an option from the drop down.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@33004 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/keyvalue/FormatOfferingTypeForEditCOTypeKeyValues.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/keyvalue/FormatOfferingTypeForEditCOTypeKeyValues.java
@@ -1,31 +1,102 @@
 package org.kuali.student.enrollment.class2.courseoffering.keyvalue;
 
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.core.api.util.ConcreteKeyValue;
+import org.kuali.rice.core.api.util.KeyValue;
+import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
 import org.kuali.rice.krad.uif.view.ViewModel;
+import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.web.form.MaintenanceForm;
 import org.kuali.student.enrollment.class2.courseoffering.dto.CourseOfferingEditWrapper;
+import org.kuali.student.enrollment.class2.courseoffering.dto.FormatOfferingWrapper;
+import org.kuali.student.enrollment.courseoffering.dto.CourseOfferingInfo;
+import org.kuali.student.enrollment.courseoffering.dto.FormatOfferingInfo;
+
+import org.kuali.student.enrollment.courseoffering.service.CourseOfferingService;
 import org.kuali.student.lum.course.dto.FormatInfo;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.dto.LocaleInfo;
+import org.kuali.student.r2.common.util.constants.CourseOfferingServiceConstants;
 
+import javax.xml.namespace.QName;
+import java.io.Serializable;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Locale;
 
-/**
- * Created with IntelliJ IDEA.
- * User: swedev
- * Date: 6/22/12
- * Time: 4:05 PM
- * To change this template use File | Settings | File Templates.
- */
-public class FormatOfferingTypeForEditCOTypeKeyValues extends AbstractFormatOfferingTypeKeyValues {
+public class FormatOfferingTypeForEditCOTypeKeyValues extends UifKeyValuesFinderBase implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private ContextInfo contextInfo;
+    private transient CourseOfferingService courseOfferingService;
 
     @Override
-    List<FormatInfo> getFormats(ViewModel model) {
+    public List<KeyValue> getKeyValues(ViewModel model) {
         MaintenanceForm form = (MaintenanceForm)model;
-        CourseOfferingEditWrapper coWrapper = (CourseOfferingEditWrapper)form.getDocument().getNewMaintainableObject().getDataObject();
+        CourseOfferingEditWrapper coEditWrapper = (CourseOfferingEditWrapper)form.getDocument().getNewMaintainableObject().getDataObject();
 
-        if(coWrapper.getCourse() != null){
+        List<KeyValue> keyValues = new ArrayList<KeyValue>();
+        List<FormatInfo> formatOptions = getFormatOptions(coEditWrapper);
+        List<String> availableFormatTypes;
+        try{
+            availableFormatTypes = getAvailableFormatTypes(coEditWrapper);
+        }catch(Exception e){
+            availableFormatTypes = new ArrayList<String>();
+        }
+
+        for (FormatInfo format : formatOptions) {
+            ConcreteKeyValue keyValue = new ConcreteKeyValue();
+            if(!availableFormatTypes.contains(format.getId())){
+                keyValue.setKey(format.getId());
+                keyValue.setValue(format.getType());
+                keyValues.add(keyValue);
+            }
+        }
+
+        return keyValues;
+    }
+
+    private List<FormatInfo> getFormatOptions(CourseOfferingEditWrapper coWrapper) {
+        if(coWrapper!=null && coWrapper.getCourse() != null){
             return coWrapper.getCourse().getFormats();
         }   else {
             return null;
         }
 
     }
+
+    private List<String> getAvailableFormatTypes(CourseOfferingEditWrapper coEditWrapper) throws Exception {
+        List<String> availableFormatTypes = new ArrayList<String>();
+        List <FormatOfferingWrapper> formatOfferingWrapperList= coEditWrapper.getFormatOfferingWrapperList();
+        for (FormatOfferingWrapper foWrapper:formatOfferingWrapperList){
+            availableFormatTypes.add(foWrapper.getFormatOfferingInfo().getFormatId());
+        }
+//        CourseOfferingInfo coInfo = coWrapper.getCoInfo();
+//        List<FormatOfferingInfo> formats = getCourseOfferingService().getFormatOfferingsByCourseOffering(coInfo.getId(), getContextInfo());
+//        for (FormatOfferingInfo format : formats){
+//            availableFormatTypes.add(format.getFormatId());
+//        }
+        return availableFormatTypes;
+
+    }
+
+    protected CourseOfferingService getCourseOfferingService() {
+        if (courseOfferingService == null) {
+            courseOfferingService = (CourseOfferingService) GlobalResourceLoader.getService(new QName(CourseOfferingServiceConstants.NAMESPACE, "CourseOfferingService"));
+        }
+        return courseOfferingService;
+    }
+
+    public ContextInfo getContextInfo() {
+        if (null == contextInfo) {
+            contextInfo = new ContextInfo();
+            contextInfo.setAuthenticatedPrincipalId(GlobalVariables.getUserSession().getPrincipalId());
+            contextInfo.setPrincipalId(GlobalVariables.getUserSession().getPrincipalId());
+            LocaleInfo localeInfo = new LocaleInfo();
+            localeInfo.setLocaleLanguage(Locale.getDefault().getLanguage());
+            localeInfo.setLocaleRegion(Locale.getDefault().getCountry());
+            contextInfo.setLocale(localeInfo);
+        }
+        return contextInfo;
+    }
 }
+
