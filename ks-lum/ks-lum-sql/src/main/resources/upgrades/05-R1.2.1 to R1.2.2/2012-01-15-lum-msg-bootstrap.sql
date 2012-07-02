d36be39b284ae481bbf090db11c98e1d2b4271d0
cmann <cmann@kuali.org>
2012-07-02 09:16:38 -0400

    KSLAB-2062 move message to proper folder
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.2-sprint4@33080 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-sql/src/main/resources/upgrades/05-R1.2.1 to R1.2.2/2012-01-15-lum-msg-bootstrap.sql
+++ ks-lum/ks-lum-sql/src/main/resources/upgrades/05-R1.2.1 to R1.2.2/2012-01-15-lum-msg-bootstrap.sql
@@ -6,10 +6,3 @@ VALUES ('D9E0819F1C514D03B80772D22E6E2D2C', 'collaboratorNameSuggest', 'en', 'co
 INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
 VALUES ('d073bc38-8c4d-48b8-b240-35ecea572c79', 'crossListedAndJoints', 'en', 'course', 'Cross Listed and Jointly Offered Courses', '64fcbc4a-fe76-4ed7-b334-ff31ade5a87f', 1)
 /
--- KSLAB-2062
--- adds XLS as one of the possible export format
-delete from KSMG_MESSAGE where MSG_ID = 'xlsFormat' 
-/
-INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
-VALUES ('644E21681A474D5BA3E829514EB91BDD', 'xlsFormat', 'en', 'common', '.xls (Microsoft Excel)', null, 1) 
-/
