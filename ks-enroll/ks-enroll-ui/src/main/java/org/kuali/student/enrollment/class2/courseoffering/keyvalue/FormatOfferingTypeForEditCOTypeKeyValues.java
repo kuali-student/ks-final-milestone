ba34fc4d25b34991dce0849dec897aff4624bb49
huangb <huangb@kuali.org>
2012-07-05 17:44:30 -0400

    KSENROLL-1413:  fix a bug introduced by rev 33052. Put the code back to fulfill the function that after a Format is added to the collection, it is removed from the drop down and is not available as an input option.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@33238 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/keyvalue/FormatOfferingTypeForEditCOTypeKeyValues.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/keyvalue/FormatOfferingTypeForEditCOTypeKeyValues.java
@@ -4,52 +4,76 @@ import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
 import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
 import org.kuali.rice.krad.uif.view.ViewModel;
+import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.web.form.MaintenanceForm;
+
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.dto.LocaleInfo;
 import org.kuali.student.enrollment.class2.courseoffering.dto.CourseOfferingEditWrapper;
 import org.kuali.student.enrollment.class2.courseoffering.dto.FormatOfferingWrapper;
-import org.kuali.student.enrollment.class2.courseoffering.form.CourseOfferingManagementForm;
-import org.kuali.student.enrollment.class2.courseoffering.service.impl.CourseOfferingEditMaintainableImpl;
-import org.kuali.student.enrollment.class2.courseoffering.service.impl.CourseOfferingManagementViewHelperServiceImpl;
 import org.kuali.student.enrollment.class2.courseoffering.util.CourseOfferingResourceLoader;
-import org.kuali.student.enrollment.courseoffering.dto.CourseOfferingInfo;
-import org.kuali.student.lum.course.dto.CourseInfo;
+import org.kuali.student.enrollment.courseoffering.service.CourseOfferingService;
 import org.kuali.student.lum.course.dto.FormatInfo;
 import org.kuali.student.lum.course.service.CourseService;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class FormatOfferingTypeForEditCOTypeKeyValues extends UifKeyValuesFinderBase implements Serializable {
-
+    private static final long serialVersionUID = 1L;
+    private ContextInfo contextInfo;
+    private transient CourseOfferingService courseOfferingService;
     private transient CourseService courseService;
 
     @Override
     public List<KeyValue> getKeyValues(ViewModel model) {
         MaintenanceForm form = (MaintenanceForm)model;
-        CourseOfferingEditWrapper coWrapper = (CourseOfferingEditWrapper)form.getDocument().getNewMaintainableObject().getDataObject();
+        CourseOfferingEditWrapper coEditWrapper = (CourseOfferingEditWrapper)form.getDocument().getNewMaintainableObject().getDataObject();
 
         List<KeyValue> keyValues = new ArrayList<KeyValue>();
         keyValues.add(new ConcreteKeyValue("", "Select Format Type"));
-        CourseOfferingInfo selectedCourseOffering = coWrapper.getCoInfo();
 
-        List<FormatInfo> formatInfos;
+        List<FormatInfo> formatOptions;
+        List<String> availableFormatTypes; 
+        
         try {
-
-            CourseInfo courseInfo = this.getCourseService().getCourse(selectedCourseOffering.getCourseId());
-            formatInfos = courseInfo.getFormats();
+            formatOptions = getFormatOptions(coEditWrapper);
+            availableFormatTypes = getAvailableFormatTypes(coEditWrapper);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e);            
         }
-        try {
-            for (FormatInfo formatInfo : formatInfos) {
-                keyValues.add(new ConcreteKeyValue(formatInfo.getId(), formatInfo.getName()));
+        for (FormatInfo format : formatOptions) {
+            ConcreteKeyValue keyValue = new ConcreteKeyValue();
+            if(!availableFormatTypes.contains(format.getId())){
+                keyValue.setKey(format.getId());
+                keyValue.setValue(format.getName());
+                keyValues.add(keyValue);
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
 
         return keyValues;
+    }
+
+    private List<FormatInfo> getFormatOptions(CourseOfferingEditWrapper coEditWrapper) throws Exception{
+        if(coEditWrapper!=null && coEditWrapper.getCourse() != null){
+            return coEditWrapper.getCourse().getFormats();
+        }else if(coEditWrapper!=null && coEditWrapper.getCourse() == null) {
+            return (getCourseService().getCourse(coEditWrapper.getCoInfo().getCourseId())).getFormats();
+        }
+        else{
+            throw new Exception("Error: CourseOfferingEditWrapper in this page is null");
+        }
+    }
+    
+    private List<String> getAvailableFormatTypes(CourseOfferingEditWrapper coEditWrapper) throws Exception {
+        List<String> availableFormatTypes = new ArrayList<String>();
+        List <FormatOfferingWrapper> formatOfferingWrapperList= coEditWrapper.getFormatOfferingWrapperList();
+        for (FormatOfferingWrapper foWrapper:formatOfferingWrapperList){
+            availableFormatTypes.add(foWrapper.getFormatOfferingInfo().getFormatId());
+        }
+        return availableFormatTypes;
     }
 
     public CourseService getCourseService() {
@@ -59,8 +83,17 @@ public class FormatOfferingTypeForEditCOTypeKeyValues extends UifKeyValuesFinder
         return this.courseService;
     }
 
-    public void setCourseService(CourseService courseService) {
-        this.courseService = courseService;
+    public ContextInfo getContextInfo() {
+        if (null == contextInfo) {
+            contextInfo = new ContextInfo();
+            contextInfo.setAuthenticatedPrincipalId(GlobalVariables.getUserSession().getPrincipalId());
+            contextInfo.setPrincipalId(GlobalVariables.getUserSession().getPrincipalId());
+            LocaleInfo localeInfo = new LocaleInfo();
+            localeInfo.setLocaleLanguage(Locale.getDefault().getLanguage());
+            localeInfo.setLocaleRegion(Locale.getDefault().getCountry());
+            contextInfo.setLocale(localeInfo);
+        }
+        return contextInfo;
     }
 }
 
