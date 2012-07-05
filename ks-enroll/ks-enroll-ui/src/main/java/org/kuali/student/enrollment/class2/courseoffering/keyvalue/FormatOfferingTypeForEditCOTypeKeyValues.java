1bc338082885184d5cf0a95773c477f9b3950323
gtaylor <gtaylor@kuali.org>
2012-07-05 14:48:26 -0400

    KSENROLL-1451: Added user friendly format offering names.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@33223 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/keyvalue/FormatOfferingTypeForEditCOTypeKeyValues.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/keyvalue/FormatOfferingTypeForEditCOTypeKeyValues.java
@@ -1,44 +1,66 @@
 package org.kuali.student.enrollment.class2.courseoffering.keyvalue;
 
+import org.kuali.rice.core.api.util.ConcreteKeyValue;
+import org.kuali.rice.core.api.util.KeyValue;
+import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
 import org.kuali.rice.krad.uif.view.ViewModel;
 import org.kuali.rice.krad.web.form.MaintenanceForm;
 import org.kuali.student.enrollment.class2.courseoffering.dto.CourseOfferingEditWrapper;
 import org.kuali.student.enrollment.class2.courseoffering.dto.FormatOfferingWrapper;
+import org.kuali.student.enrollment.class2.courseoffering.form.CourseOfferingManagementForm;
+import org.kuali.student.enrollment.class2.courseoffering.service.impl.CourseOfferingEditMaintainableImpl;
+import org.kuali.student.enrollment.class2.courseoffering.service.impl.CourseOfferingManagementViewHelperServiceImpl;
+import org.kuali.student.enrollment.class2.courseoffering.util.CourseOfferingResourceLoader;
+import org.kuali.student.enrollment.courseoffering.dto.CourseOfferingInfo;
+import org.kuali.student.lum.course.dto.CourseInfo;
 import org.kuali.student.lum.course.dto.FormatInfo;
+import org.kuali.student.lum.course.service.CourseService;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FormatOfferingTypeForEditCOTypeKeyValues extends AbstractFormatOfferingTypeKeyValues implements Serializable {
+public class FormatOfferingTypeForEditCOTypeKeyValues extends UifKeyValuesFinderBase implements Serializable {
+
+    private transient CourseService courseService;
 
     @Override
-    protected List<FormatInfo> getFormats(ViewModel model) {
+    public List<KeyValue> getKeyValues(ViewModel model) {
         MaintenanceForm form = (MaintenanceForm)model;
         CourseOfferingEditWrapper coWrapper = (CourseOfferingEditWrapper)form.getDocument().getNewMaintainableObject().getDataObject();
 
-        if(coWrapper.getCourse() != null){
-            return coWrapper.getCourse().getFormats();
-        }   else {
-            return null;
+        List<KeyValue> keyValues = new ArrayList<KeyValue>();
+        keyValues.add(new ConcreteKeyValue("", "Select Format Type"));
+        CourseOfferingInfo selectedCourseOffering = coWrapper.getCoInfo();
+
+        List<FormatInfo> formatInfos;
+        try {
+
+            CourseInfo courseInfo = this.getCourseService().getCourse(selectedCourseOffering.getCourseId());
+            formatInfos = courseInfo.getFormats();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            for (FormatInfo formatInfo : formatInfos) {
+                keyValues.add(new ConcreteKeyValue(formatInfo.getId(), formatInfo.getName()));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
+        return keyValues;
     }
 
-    @Override
-    protected List<String> getExistingFormatIdsFromFormatOfferings(ViewModel model) throws Exception {
-
-        MaintenanceForm form = (MaintenanceForm)model;
-        CourseOfferingEditWrapper coWrapper = (CourseOfferingEditWrapper)form.getDocument().getNewMaintainableObject().getDataObject();
-
-        List<String> availableFormatTypes = new ArrayList<String>();
-        List <FormatOfferingWrapper> formatOfferingWrapperList= coWrapper.getFormatOfferingWrapperList();
-
-        for (FormatOfferingWrapper wrapper : formatOfferingWrapperList){
-            availableFormatTypes.add(wrapper.getFormatOfferingInfo().getFormatId());
+    public CourseService getCourseService() {
+        if(courseService == null) {
+            courseService = CourseOfferingResourceLoader.loadCourseService();
         }
+        return this.courseService;
+    }
 
-        return availableFormatTypes;
+    public void setCourseService(CourseService courseService) {
+        this.courseService = courseService;
     }
 }
 
