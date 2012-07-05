4d3d7073352f5c72d5e02ab44e51e16cd80388eb
nina <nina@kuali.org>
2012-07-05 11:22:51 -0400

    KSENROLL-1652 Converting Test Classes to Utility that reads data from property files
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@33213 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core/ks-core-krms/src/main/java/org/kuali/student/krms/util/KSKRMSDataSetupUtility.java
+++ ks-core/ks-core-krms/src/main/java/org/kuali/student/krms/util/KSKRMSDataSetupUtility.java
@@ -1,15 +1,24 @@
 package org.kuali.student.krms.util;
 
 import org.kuali.rice.core.framework.resourceloader.SpringResourceLoader;
+import org.kuali.rice.krad.service.BusinessObjectService;
 import org.kuali.rice.krms.api.repository.context.ContextDefinition;
+import org.kuali.rice.krms.api.repository.term.TermResolverDefinition;
+import org.kuali.rice.krms.api.repository.term.TermSpecificationDefinition;
 import org.kuali.rice.krms.api.repository.type.KrmsTypeDefinition;
 import org.kuali.rice.krms.api.repository.type.KrmsTypeRepositoryService;
 import org.kuali.rice.krms.impl.repository.AgendaBoService;
 import org.kuali.rice.krms.impl.repository.ContextBoService;
-import org.kuali.rice.krms.impl.repository.FunctionBoServiceImpl;
-import org.kuali.rice.krms.impl.repository.KrmsRepositoryServiceLocator;
 import org.kuali.rice.krms.impl.repository.RuleBoService;
 import org.kuali.rice.krms.impl.repository.TermBoService;
+import org.kuali.rice.krms.impl.repository.TermSpecificationBo;
+import org.kuali.student.common.util.PropertiesFilterFactoryBean;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,63 +29,223 @@ import org.kuali.rice.krms.impl.repository.TermBoService;
  */
 public class KSKRMSDataSetupUtility {
 
-    KrmsTypeDefinition krmsTypeDefinition;
+    public static final String PROPERTY_namespace = "Namespace";
+    public static final String PROPERTY_CONTEXT = "Context";
+    public static final String PROPERTY_TERMSPEC = "TermSpec";
+    KrmsTypeDefinition krmsTypeForContext;
 
     private ContextBoService contextRepository;
     private KrmsTypeRepositoryService krmsTypeRepository;
     private AgendaBoService agendaBoService;
     private RuleBoService ruleBoService;
-    private FunctionBoServiceImpl functionBoService;
     private TermBoService termBoService;
     private SpringResourceLoader krmsTestResourceLoader;
+
+
+
+    private BusinessObjectService boService;
+
+    PropertiesFilterFactoryBean propertyUtil;
+    private String namespace;
 
     public static void main(String [ ] args)
     {
         KSKRMSDataSetupUtility utility = new KSKRMSDataSetupUtility();
         utility.setupRequiredServices();
-        utility.createAllContexts();
+        utility.createKSKRMSData();
+    }
 
+    public void createKSKRMSData() {
+        setupPropertyFile();
+        namespace = getPropertyValue(PROPERTY_namespace);
+        createKRMSContextFromPropertyFile();
+        createKRMSTermSpecificationsFromPropertyFile();
+    }
+
+    private void createKRMSTermSpecificationsFromPropertyFile() {
+        {
+            Properties properties = getProperties(PROPERTY_TERMSPEC);
+            //
+            Enumeration elements = properties.elements();
+            int i = 0;
+            //
+            while (elements.hasMoreElements()) {
+                String termSpecValues =  (String) elements.nextElement();
+                System.out.println(i + " - " + termSpecValues);
+
+                String delims = ",+"; // use + to treat consecutive delims as one;
+                                      // omit to treat consecutive delims separately
+                String[] tokens = termSpecValues.split(delims);
+                TermSpecificationDefinition termSpec = createKRMSTermSpecification(namespace,tokens[0],tokens[1],tokens[2]);
+                // NOTE the term resolver must be defined as a spring bean under the name given here.
+                createKRMSTermResolver(tokens[3], termSpec);
+                i++;
+            }
+            System.out.println("Created " + i + " TermSpecification and TermResolvers for KS KRMS");
+        }
+
+    }
+
+    public void createAllKRMSTermSpecificationsPhase1() {
+       
+        // Create all the terms specifications...
+        TermSpecificationDefinition termSpec = null;
+        termSpec = createKRMSTermSpecification(namespace,
+                KSKRMSReplaceWithPropertyFile.TERM_SPEC_CREDITS,
+                KSKRMSReplaceWithPropertyFile.CREDITS_DESCR, String.class.getCanonicalName());
+        // NOTE the term resolver must be defined as a spring bean under the name given here.
+        createKRMSTermResolver(KSKRMSReplaceWithPropertyFile.TERM_SPEC_RESOLVER_CREDITS, termSpec);
+        termSpec = createKRMSTermSpecification(namespace,
+                KSKRMSReplaceWithPropertyFile.TERM_SPEC_ORG_NUMBER,
+                KSKRMSReplaceWithPropertyFile.ORG_NUMBER_DESCR,
+                String.class.getCanonicalName());
+        createKRMSTermResolver(KSKRMSReplaceWithPropertyFile.TERM_SPEC_RESOLVER_ORG_NUMBER, termSpec);
+        termSpec = createKRMSTermSpecification(namespace,
+                KSKRMSReplaceWithPropertyFile.TERM_SPEC_COURSE, KSKRMSReplaceWithPropertyFile.COURSE_DESCR,
+                String.class.getCanonicalName());
+        createKRMSTermResolver(KSKRMSReplaceWithPropertyFile.TERM_SPEC_RESOLVER_COURSE, termSpec);
+        termSpec = createKRMSTermSpecification(namespace,
+                KSKRMSReplaceWithPropertyFile.TERM_SPEC_COURSE_NUMBER,
+                KSKRMSReplaceWithPropertyFile.COURSE_NUMBER_DESCR, "int");
+        createKRMSTermResolver(KSKRMSReplaceWithPropertyFile.TERM_SPEC_RESOLVER_COURSE_NUMBER, termSpec);
+        termSpec = createKRMSTermSpecification(namespace,
+                KSKRMSReplaceWithPropertyFile.TERM_SPEC_DATE, KSKRMSReplaceWithPropertyFile.DATE_DESCR,
+                "Java.util.Date");
+        createKRMSTermResolver(KSKRMSReplaceWithPropertyFile.TERM_SPEC_RESOLVER_DATE, termSpec);
+        termSpec = createKRMSTermSpecification(namespace,
+                KSKRMSReplaceWithPropertyFile.TERM_SPEC_GPA, KSKRMSReplaceWithPropertyFile.GPA_DESCR,
+                String.class.getCanonicalName());
+        createKRMSTermResolver(KSKRMSReplaceWithPropertyFile.TERM_SPEC_RESOLVER_GPA, termSpec);
+        termSpec = createKRMSTermSpecification(namespace,
+                KSKRMSReplaceWithPropertyFile.TERM_SPEC_GRADE, KSKRMSReplaceWithPropertyFile.GRADE_DESCR,
+                String.class.getCanonicalName());
+        createKRMSTermResolver(KSKRMSReplaceWithPropertyFile.TERM_SPEC_RESOLVER_GRADE, termSpec);
+        termSpec = createKRMSTermSpecification(namespace,
+                KSKRMSReplaceWithPropertyFile.TERM_SPEC_GRADE_TYPE,
+                KSKRMSReplaceWithPropertyFile.GRADE_TYPE_DESCR,
+                String.class.getCanonicalName());
+        createKRMSTermResolver(KSKRMSReplaceWithPropertyFile.TERM_SPEC_RESOLVER_GRADE_TYPE, termSpec);
+        termSpec = createKRMSTermSpecification(namespace,
+                KSKRMSReplaceWithPropertyFile.TERM_SPEC_LEARNING_OBJECTIVES,
+                KSKRMSReplaceWithPropertyFile.LEARNING_OBJECTIVES_DESCR,
+                String.class.getCanonicalName());
+        createKRMSTermResolver(KSKRMSReplaceWithPropertyFile.TERM_SPEC_RESOLVER_LEARNING_OBJECTIVES, termSpec);
+        termSpec = createKRMSTermSpecification(namespace,
+                KSKRMSReplaceWithPropertyFile.TERM_SPEC_SUBJECT_CODE,
+                KSKRMSReplaceWithPropertyFile.SUBJECT_CODE_DESCR,
+                String.class.getCanonicalName());
+        createKRMSTermResolver(KSKRMSReplaceWithPropertyFile.TERM_SPEC_RESOLVER_SUBJECT_CODE, termSpec);
+        termSpec = createKRMSTermSpecification(namespace,
+                KSKRMSReplaceWithPropertyFile.TERM_SPEC_TEXT, KSKRMSReplaceWithPropertyFile.TEXT_DESCR,
+                String.class.getCanonicalName());
+        createKRMSTermResolver(KSKRMSReplaceWithPropertyFile.TERM_SPEC_RESOLVER_TEXT, termSpec);
+    }
+
+    public void createKRMSTermResolver(String termResolverName, TermSpecificationDefinition termSpecDefinition) {
+        // KrmsType for TermResolver
+        KrmsTypeDefinition krmsTermResolverTypeDefinition = getKSKRMSType(KSKRMSReplaceWithPropertyFile.KSNAMESPACE, KSKRMSReplaceWithPropertyFile.KS_TERM_RESOLVER_TYPE, "ksKRMSTermResolverTypeService");
+
+        // TermResolver
+        TermResolverDefinition termResolverDef =
+                TermResolverDefinition.Builder.create(null, KSKRMSReplaceWithPropertyFile.KSNAMESPACE, termResolverName, krmsTermResolverTypeDefinition.getId(),
+                        TermSpecificationDefinition.Builder.create(termSpecDefinition),
+                        null,
+                        null,
+                        null).build();
+        termResolverDef = termBoService.createTermResolver(termResolverDef);
+    }
+
+    private TermSpecificationDefinition createKRMSTermSpecification(
+            String namespace, String termSpecName, String descr, String termType) {
+        Map<String, String> queryArgs = new HashMap<String, String>();
+        queryArgs.put("namespace", namespace);
+        queryArgs.put("name", termSpecName);
+        TermSpecificationBo termSpecBo = boService.findByPrimaryKey(
+                TermSpecificationBo.class, queryArgs);
+
+        // TODO Figure out how to set the Description
+        TermSpecificationDefinition termSpec = null;
+        if (termSpecBo == null) {
+
+            TermSpecificationDefinition.Builder termSpecDefBuilder = TermSpecificationDefinition.Builder
+                    .create(null, termSpecName, namespace, termType);
+            termSpecDefBuilder.setDescription(descr);
+
+            termSpec = termSpecDefBuilder.build();
+
+
+            termSpec = termBoService.createTermSpecification(termSpec);
+
+        } else {
+            termSpec = termSpecBo.to(termSpecBo);
+        }
+        System.out.println("Elmien :     " + termSpec.getDescription()
+                + "     " + termSpec.getName());
+        return termSpec;
+    }
+
+    public void createKRMSContextFromPropertyFile() {
+        getContextType();
+        Properties context = getProperties(PROPERTY_CONTEXT);
+        //
+        Enumeration elements = context.elements();
+        int i = 0;
+        //
+        while (elements.hasMoreElements()) {
+            String contextValue =  (String) elements.nextElement();
+            System.out.println(i + " - " + contextValue);
+            createContext(namespace, contextValue, krmsTypeForContext);
+            i++;
+        }
+        System.out.println("Created " + i + " context for KS KRMS");
+    }
+
+    public void createKRMSTermSpecificationFromPropertyFile() {
+        getContextType();
+        Properties termSpecificationProperites = getProperties(PROPERTY_TERMSPEC);
+        //
+        Enumeration elements = termSpecificationProperites.elements();
+        int i = 0;
+        //
+        while (elements.hasMoreElements()) {
+            String contextValue =  (String) elements.nextElement();
+            System.out.println(i + " - " + contextValue);
+            createContext(namespace, contextValue, krmsTypeForContext);
+            i++;
+        }
+        System.out.println("Created " + i + " context for KS KRMS");
+    }
+
+
+      private void setupPropertyFile() {
+        propertyUtil = new PropertiesFilterFactoryBean();
+        propertyUtil.setPropertyFile("classpath:KSKRMSDataToLoad.properties");
 
     }
 
     private void setupRequiredServices() {
         // Setup all the services...
-        termBoService = KrmsRepositoryServiceLocator.getTermBoService();
-        agendaBoService = KrmsRepositoryServiceLocator.getAgendaBoService();
-        contextRepository = KrmsRepositoryServiceLocator.getContextBoService();
-        ruleBoService = KrmsRepositoryServiceLocator.getRuleBoService();
-        krmsTypeRepository = KrmsRepositoryServiceLocator
-                .getKrmsTypeRepositoryService();
-        functionBoService = KrmsRepositoryServiceLocator.getBean("functionRepositoryService");
+//        termBoService = KrmsRepositoryServiceLocator.getTermBoService();
+//        agendaBoService = KrmsRepositoryServiceLocator.getAgendaBoService();
+//        contextRepository = KrmsRepositoryServiceLocator.getContextBoService();
+//        ruleBoService = KrmsRepositoryServiceLocator.getRuleBoService();
+//        krmsTypeRepository = KrmsRepositoryServiceLocator
+//                .getKrmsTypeRepositoryService();
+//        functionBoService = KrmsRepositoryServiceLocator.getBean("functionRepositoryService");
     }
 
-    public void createAllContexts() {
-        String nameSpace = KSKRMSReplaceWithPropertyFile.KSNAMESPACE;
-        // Create all the contexts...
-        krmsTypeDefinition = getKSKRMSType(nameSpace, KSKRMSReplaceWithPropertyFile.KS_AGENDA_TYPE, "testAgendaTypeService");
-        createContext(nameSpace, KSKRMSReplaceWithPropertyFile.CONTEXT_ANTI_REQUISITE,
-                krmsTypeDefinition);
-        createContext(nameSpace, KSKRMSReplaceWithPropertyFile.CONTEXT_CORE_REQUISITE,
-                krmsTypeDefinition);
-        createContext(nameSpace, KSKRMSReplaceWithPropertyFile.CONTEXT_COURSE_RESTRICTS,
-                krmsTypeDefinition);
-        createContext(nameSpace,
-                KSKRMSReplaceWithPropertyFile.CONTEXT_RECOMMENDED_PREPARATION,
-                krmsTypeDefinition);
-        createContext(nameSpace, KSKRMSReplaceWithPropertyFile.CONTEXT_REPEATED_CREDITS,
-                krmsTypeDefinition);
-        createContext(nameSpace,
-                KSKRMSReplaceWithPropertyFile.CONTEXT_STUD_ELIGIBILITY, krmsTypeDefinition);
-
+    private void getContextType() {
+        
+        krmsTypeForContext = getKSKRMSType(namespace, KSKRMSReplaceWithPropertyFile.KS_AGENDA_TYPE, "testAgendaTypeService");
     }
 
-    public ContextDefinition createContext(String nameSpace, String name,
+    public ContextDefinition createContext(String namespace, String name,
                                            KrmsTypeDefinition krmsContextTypeDefinition) {
 
-        ContextDefinition contextDefinition = contextRepository.getContextByNameAndNamespace(name, nameSpace);
+        ContextDefinition contextDefinition = contextRepository.getContextByNameAndNamespace(name, namespace);
         if (contextDefinition == null) {
             ContextDefinition.Builder contextBuilder = ContextDefinition.Builder
-                    .create(nameSpace, name);
+                    .create(namespace, name);
 
             contextBuilder.setTypeId(krmsContextTypeDefinition.getId());
             contextDefinition = contextBuilder.build();
@@ -88,14 +257,14 @@ public class KSKRMSDataSetupUtility {
     }
 
 
-    private KrmsTypeDefinition getKSKRMSType(String nameSpace, String typeName, String typeServiceName) {
+    private KrmsTypeDefinition getKSKRMSType(String namespace, String typeName, String typeServiceName) {
         KrmsTypeDefinition krmsAgendaType = krmsTypeRepository
-                .getTypeByName(nameSpace, typeName);
+                .getTypeByName(namespace, typeName);
 
         if (krmsAgendaType == null) {
 
             KrmsTypeDefinition.Builder krmsAgendaTypeDefinition = KrmsTypeDefinition.Builder
-                    .create(typeName, nameSpace);
+                    .create(typeName, namespace);
             krmsAgendaTypeDefinition.setServiceName(typeServiceName);
 
             // TODO KSKRMS not sure where the Attributes fit in and how they
@@ -119,5 +288,62 @@ public class KSKRMSDataSetupUtility {
         }
 
         return krmsAgendaType;
+    }
+
+    /**
+     * Service methods setup
+     */
+    public void setKrmsTypeDefinition(KrmsTypeDefinition krmsTypeDefinition) {
+        this.krmsTypeForContext = krmsTypeDefinition;
+    }
+
+    public void setContextRepository(ContextBoService contextRepository) {
+        this.contextRepository = contextRepository;
+    }
+
+    public void setKrmsTypeRepository(KrmsTypeRepositoryService krmsTypeRepository) {
+        this.krmsTypeRepository = krmsTypeRepository;
+    }
+
+    public void setAgendaBoService(AgendaBoService agendaBoService) {
+        this.agendaBoService = agendaBoService;
+    }
+
+    public void setRuleBoService(RuleBoService ruleBoService) {
+        this.ruleBoService = ruleBoService;
+    }
+
+    public void setTermBoService(TermBoService termBoService) {
+        this.termBoService = termBoService;
+    }
+
+    public void setBoService(BusinessObjectService boService) {
+        this.boService = boService;
+    }
+
+    private void writeError(Exception e) {
+        System.out.println("Error loading " + propertyUtil.getPrefix() + " from Propertyfile : " + propertyUtil.getPropertyFile() );
+        System.out.println(e.getMessage());
+    }
+
+    private String getPropertyValue(String propertyPrefix) {
+
+        Properties properties = getProperties(propertyPrefix);
+        Enumeration elements = properties.elements();
+        while (elements.hasMoreElements()) {
+            return (String) elements.nextElement();
+        }
+        return null;
+    }
+
+    private Properties getProperties(String propertyPrefix) {
+        propertyUtil.setPrefix(propertyPrefix);
+        Properties properties = null;
+        try {
+            properties = (Properties) propertyUtil.getObject();
+        } catch (Exception e) {
+            writeError(e);
+        }
+        return properties;
     }
 }
