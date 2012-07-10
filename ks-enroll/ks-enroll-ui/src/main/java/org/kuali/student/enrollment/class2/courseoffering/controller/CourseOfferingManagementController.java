1877ac372beb2d95f4aabfa9e4d656f7bacd8b4a
miki.harmath <miki.harmath@kuali.org>
2012-07-10 14:01:18 -0400

    KSENROLL-1542 Manage CO - Results do not refresh when values in the Term and SubjectCode/CourseID fields are changed and submitted
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@33402 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/controller/CourseOfferingManagementController.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/controller/CourseOfferingManagementController.java
@@ -104,11 +104,14 @@ public class CourseOfferingManagementController extends UifControllerBase  {
             } else if (courseOfferingList.size()>1) {
                 LOG.error("Error: Found more than one Course Offering for a Course Offering Code: "+courseOfferingCode+" in term: "+termCode);
                 GlobalVariables.getMessageMap().putError("inputCode", CourseOfferingConstants.COURSEOFFERING_MSG_ERROR_FOUND_MORE_THAN_ONE_COURSE_OFFERING, courseOfferingCode, termCode);
+                theForm.setCourseOfferingList(null);
+                theForm.setActivityWrapperList(null);
                 return getUIFModelAndView(theForm);
             } else {
                 LOG.error("Error: Can't find any Course Offering for a Course Offering Code: "+courseOfferingCode+" in term: "+termCode);
                 GlobalVariables.getMessageMap().putError("inputCode", CourseOfferingConstants.COURSEOFFERING_MSG_ERROR_NO_COURSE_OFFERING_IS_FOUND, "Course Offering", courseOfferingCode, termCode);
                 theForm.setCourseOfferingList(null);
+                theForm.setActivityWrapperList(null);
                 return getUIFModelAndView(theForm);
             }
         }
