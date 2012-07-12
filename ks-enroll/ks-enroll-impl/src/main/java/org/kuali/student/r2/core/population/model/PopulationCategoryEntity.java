a47bf4357b0dd2f420e5866bac36b4c7abe83500
mezba.mahtab <mezba.mahtab@kuali.org>
2012-07-12 12:11:06 -0400

    KSENROLL-752 PopulationService: created all the JPA classes.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3-services@33499 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/population/model/PopulationCategoryEntity.java
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/population/model/PopulationCategoryEntity.java
@@ -42,7 +42,7 @@ import java.util.Set;
  */
 
 @Entity
-@Table(name = "KSEN_POPULATION_CAT")
+@Table(name = "POPULATION_CAT")
 public class PopulationCategoryEntity extends MetaEntity implements AttributeOwner<PopulationCategoryAttributeEntity> {
 
     ////////////////////
