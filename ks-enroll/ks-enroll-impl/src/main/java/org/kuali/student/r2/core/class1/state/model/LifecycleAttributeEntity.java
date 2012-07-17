2981709ef9a8e7a84750929fe4d71ee945b443da
depstein <depstein@kuali.org>
2012-07-17 10:44:39 -0400

    KSENROLL-1759, fixed eclipse conflict code was committed
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@33669 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/class1/state/model/LifecycleAttributeEntity.java
+++ ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/r2/core/class1/state/model/LifecycleAttributeEntity.java
@@ -4,36 +4,18 @@ import org.kuali.student.r2.common.entity.BaseAttributeEntity;
 import org.kuali.student.r2.common.infc.Attribute;
 
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "KSEN_STATE_LIFECYCLE_ATTR")
 public class LifecycleAttributeEntity extends BaseAttributeEntity<LifecycleEntity> {
 
-<<<<<<< .mine
     public LifecycleAttributeEntity() {
         super();
     }
-=======
-	public LifecycleAttributeEntity() {
-		super();
-	}
-
-	public LifecycleAttributeEntity(Attribute att, LifecycleEntity owner) {
-		super(att, owner);
-	}
->>>>>>> .r33667
-<<<<<<< .mine
 
     public LifecycleAttributeEntity(Attribute att, LifecycleEntity owner) {
         super(att, owner);
     }
 
-
-=======
-    
-    
->>>>>>> .r33667
 }
