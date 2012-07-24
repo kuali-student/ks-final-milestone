c92a2b565566cd6b523c902c4e48502334a39f4c
bobhurt <bobhurt@kuali.org>
2012-07-24 13:15:30 -0400

    KSENROLL-1570: Should be fixed by Rice 2.2.0-M2, but changed GLOBAL_MESSAGES to GLOBAL_ERRORS.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@33941 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/appointment/service/impl/AppointmentViewHelperServiceImpl.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/appointment/service/impl/AppointmentViewHelperServiceImpl.java
@@ -86,7 +86,7 @@ public class AppointmentViewHelperServiceImpl extends ViewHelperServiceImpl impl
 
         //Check for exceptions
         if(terms == null || terms.isEmpty()){
-            GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_MESSAGES, AppointmentConstants.APPOINTMENT_MSG_ERROR_NO_TERMS_FOUND);
+            GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_ERRORS, AppointmentConstants.APPOINTMENT_MSG_ERROR_NO_TERMS_FOUND);
             return; //Nothing found
         }
 
@@ -123,7 +123,9 @@ public class AppointmentViewHelperServiceImpl extends ViewHelperServiceImpl impl
 
         //Check if there are no periods (might want to handle this somewhere else and surface to the user)
         if(form.getPeriodMilestones()==null||form.getPeriodMilestones().isEmpty()){
-            GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_MESSAGES, AppointmentConstants.APPOINTMENT_MSG_ERROR_NO_REG_PERIODS_FOR_TERM);
+            GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_ERRORS, AppointmentConstants.APPOINTMENT_MSG_ERROR_NO_REG_PERIODS_FOR_TERM);
+            //GlobalVariables.getMessageMap().putErrorForSectionId("KS-RegistrationWindowsManagement-SelectTermPage", AppointmentConstants.APPOINTMENT_MSG_ERROR_NO_REG_PERIODS_FOR_TERM);
+            //GlobalVariables.getMessageMap().putError("termType", AppointmentConstants.APPOINTMENT_MSG_ERROR_NO_REG_PERIODS_FOR_TERM);
         }
 
     }
