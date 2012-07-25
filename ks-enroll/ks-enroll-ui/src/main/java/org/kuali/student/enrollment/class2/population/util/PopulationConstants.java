b257f0ad30a5226277e08b445f5c9de0014d3823
huangb <huangb@kuali.org>
2012-07-25 14:02:21 -0400

    KSENROLL-1963: implement the rule: Population name must be unique.
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@34000 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/population/util/PopulationConstants.java
@@ -0,0 +1,12 @@
+package org.kuali.student.enrollment.class2.population.util;
+
+
+public class PopulationConstants {
+    //Attributes defined in PopulationWrapper
+    public static final class PopulationWrapper {
+        public static final String POPULATION_NAME = "dataObject.populationInfo.name";
+    }
+    //Message keys
+    public static final String POPULATION_MSG_ERROR_NAME_IS_NOT_UNIQUE = "error.enroll.population.nameIsNotUnique";
+
+}
