44818fcc5af8c949d21e5dddd9000e5e74a0108e
gert.meiring <gert.meiring@kuali.org>
2012-08-16 09:00:54 -0400

    JIRA KSLAB-2599 : CAS-503 : Access modiefiers changed for viewEnum and viewName
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-2.0-cm@34733 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-ui/src/main/java/org/kuali/student/common/ui/client/configurable/mvc/DelayedToolView.java
+++ ks-common/ks-common-ui/src/main/java/org/kuali/student/common/ui/client/configurable/mvc/DelayedToolView.java
@@ -34,8 +34,8 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public abstract class DelayedToolView extends LazyPanel implements ToolView{
     private Controller controller;    
-    private Enum<?> viewEnum;
-    private String viewName;    //View name is being used as menu item label   
+    protected Enum<?> viewEnum;
+    protected String viewName;    //View name is being used as menu item label   
 
     private HasReferenceId reference;
     
