cc3ae1387bd338d8e5076b9a782fea615af60577
cmuller <cmuller@kuali.org>
2012-08-17 15:24:58 -0400

    KSENROLL-2263 Fix: Formatting Issues
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@34881 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-sql/src/main/resources/upgrades/17/2012-08-17-KSENTypeUriFill.sql
+++ ks-enroll/ks-enroll-sql/src/main/resources/upgrades/17/2012-08-17-KSENTypeUriFill.sql
@@ -1,64 +1,64 @@
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET REF_OBJECT_URI='http://student.kuali.org/wsdl/lpr/LprInfo'
 WHERE TYPE_KEY like 'kuali.lpr.%' AND TYPE_KEY not like  'kuali.lpr.trans.%'
 /
 
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET REF_OBJECT_URI='http://student.kuali.org/wsdl/lpr/LprTransactionInfo'
 WHERE TYPE_KEY like 'kuali.lpr.trans%' AND TYPE_KEY not like  'kuali.lpr.trans.item%'
 /
 
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET REF_OBJECT_URI='http://student.kuali.org/wsdl/lpr/LprTransactionItemInfo'
 WHERE TYPE_KEY like 'kuali.lpr.trans.item%'
 /
 
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET REF_OBJECT_URI='http://student.kuali.org/wsdl/lrc/ResultScaleInfo'
 WHERE TYPE_KEY like 'kuali.result.scale%'
 /
 
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET REF_OBJECT_URI='http://student.kuali.org/wsdl/lrc/ResultValueInfo'
 WHERE TYPE_KEY like 'kuali.result.value.type%'
 /
 
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET REF_OBJECT_URI='http://student.kuali.org/wsdl/lrc/ResultValueGroupInfo'
 WHERE TYPE_KEY like 'kuali.result.values.group%' AND TYPE_KEY not like  'kuali.result.values.group.type.range%'
 /
 
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET REF_OBJECT_URI='http://student.kuali.org/wsdl/lrc/ResultValueRangeInfo'
 WHERE TYPE_KEY like 'kuali.result.values.group.type.range%'
 /
 
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET SERVICE_URI='http://student.kuali.org/wsdl/appointment/AppointmentService'
 WHERE TYPE_KEY like 'kuali.appointment%'
 /
 
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET SERVICE_URI='http://student.kuali.org/wsdl/atp/AtpService'
 WHERE TYPE_KEY like 'kuali.atp%'
 /
 
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET SERVICE_URI='http://student.kuali.org/wsdl/lu/LuService'
 WHERE TYPE_KEY like 'kuali.lu%' AND TYPE_KEY not like  'kuali.lui%'
 /
 
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET SERVICE_URI='http://student.kuali.org/wsdl/lui/LuiService'
 WHERE TYPE_KEY like 'kuali.lui%'
 /
 
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET SERVICE_URI='http://student.kuali.org/wsdl/hold/HoldService'
 WHERE TYPE_KEY like 'kuali.hold%'
 /
 
-UPDATE KSEMBEDDED.KSEN_TYPE
+UPDATE KSEN_TYPE
 SET SERVICE_URI='http://student.kuali.org/wsdl/lpr/LprService'
 WHERE TYPE_KEY like 'kuali.lpr%'
 /
