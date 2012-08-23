51e2f0f01b40b4ea66c4a2b4c77a768d7ae969d0
depstein <depstein@kuali.org>
2012-08-23 15:48:17 -0400

    KSENROLL-2441 - Changed the package to avoid class cast exception
    git-svn-id: https://svn.kuali.org/repos/student/branches/ks-1.3@35125 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/keyvalue/FinalExamOptionsKeyValues.java
+++ ks-enroll/ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/keyvalue/FinalExamOptionsKeyValues.java
@@ -24,7 +24,7 @@ import org.kuali.rice.krad.uif.view.ViewModel;
 import org.kuali.rice.krad.web.form.MaintenanceForm;
 import org.kuali.student.r2.common.util.ContextUtils;
 import org.kuali.student.r2.core.enumerationmanagement.dto.EnumeratedValueInfo;
-import org.kuali.student.r2.core.enumerationmanagement.service.EnumerationManagementService;
+import org.kuali.student.r1.core.enumerationmanagement.service.EnumerationManagementService;
 import org.kuali.student.r2.common.exceptions.DoesNotExistException;
 import org.kuali.student.r2.common.exceptions.InvalidParameterException;
 import org.kuali.student.r2.common.exceptions.MissingParameterException;
@@ -63,7 +63,7 @@ public class FinalExamOptionsKeyValues extends UifKeyValuesFinderBase implements
         String finalExamType = form.getCoInfo().getFinalExamType();
 
         try {
-            List<EnumeratedValueInfo> enumerationInfos = (List<EnumeratedValueInfo> ) getEnumerationManagementService().getEnumeratedValues("kuali.lu.finalExam.status", null, null, null, this.getContextInfo());
+            List<EnumeratedValueInfo> enumerationInfos = (List<EnumeratedValueInfo> ) getEnumerationManagementService().getEnumeratedValues("kuali.lu.finalExam.status", null, null, null);
             Collections.sort(enumerationInfos, new FinalExamComparator());
 
             for(EnumeratedValueInfo enumerationInfo : enumerationInfos) {
@@ -82,8 +82,6 @@ public class FinalExamOptionsKeyValues extends UifKeyValuesFinderBase implements
         } catch (MissingParameterException e) {
             throw new RuntimeException(e);
         } catch (OperationFailedException e) {
-            throw new RuntimeException(e);
-        } catch (PermissionDeniedException e) {
             throw new RuntimeException(e);
         }
 
