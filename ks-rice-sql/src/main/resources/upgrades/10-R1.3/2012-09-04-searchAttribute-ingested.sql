53fb85b7372b4e1cd675382b7e406207f274bd97
pmarais <pmarais@kuali.org>
2012-09-04 09:48:38 -0400

    KSENROLL-2490: Ingested 3 xml files. This is the resulting sql.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@35678 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-rice-sql/src/main/resources/upgrades/10-R1.3/2012-09-04-searchAttribute-ingested.sql
@@ -0,0 +1,13 @@
+UPDATE KREW_RULE_ATTR_T
+   SET NM               = 'CluCreditCourse.OrgSearchAttribute',
+       LBL              = 'CluCreditCourse Searchable attribute',
+       RULE_ATTR_TYP_CD = 'SearchableXmlAttribute',
+       CLS_NM           = 'org.kuali.student.lum.workflow.search.OrgSearchAttribute',
+       APPL_ID          = '',
+       DESC_TXT         = 'CluCreditCourse Searchable attribute',
+       XML              = null,
+       OBJ_ID           = 'caf6016f-3f41-4ca3-9d6b-0aa188d98721',
+       VER_NBR          = '2'
+ WHERE RULE_ATTR_ID = '1645'
+   AND VER_NBR = '1'
+/
