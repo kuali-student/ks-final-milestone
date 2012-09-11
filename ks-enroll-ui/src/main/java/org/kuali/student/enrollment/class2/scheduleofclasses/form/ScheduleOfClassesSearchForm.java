6f2fd92be5f083d50e9139d07c1aa632f34954ea
vgadiyak <vgadiyak@kuali.org>
2012-09-11 08:08:13 -0400

    KSENROLL-2769 New file
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@36046 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/scheduleofclasses/form/ScheduleOfClassesSearchForm.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2012 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * Created by vgadiyak on 9/10/12
+ */
+package org.kuali.student.enrollment.class2.scheduleofclasses.form;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.rice.krad.web.form.UifFormBase;
+import org.kuali.student.enrollment.courseoffering.dto.CourseOfferingDisplayInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class //TODO ...
+ *
+ * @author Kuali Student Team
+ */
+public class ScheduleOfClassesSearchForm extends UifFormBase {
+
+    private List<CourseOfferingDisplayInfo> coDisplayInfoList; //The core info (name+description+meta)
+    private String termCode;
+    private String searchType;
+    private String course;
+    private String department;
+    private String instructor;
+    private String titleDesc;
+    private String information;
+
+    public ScheduleOfClassesSearchForm (){
+        coDisplayInfoList = new ArrayList<CourseOfferingDisplayInfo>();
+    }
+
+    public List<CourseOfferingDisplayInfo> getCoDisplayInfoList() {
+        return coDisplayInfoList;
+    }
+
+    public void setCoDisplayInfoList(List<CourseOfferingDisplayInfo> coDisplayInfoList) {
+        this.coDisplayInfoList = coDisplayInfoList;
+    }
+
+    public String getTermCode(){
+        return termCode;
+    }
+
+    public void setTermCode(String termCode){
+        this.termCode = termCode;
+    }
+
+    public String getSearchType() {
+        return searchType;
+    }
+
+    public void setSearchType(String searchType) {
+        this.searchType = searchType;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getTitleDesc() {
+        return titleDesc;
+    }
+
+    public void setTitleDesc(String titleDesc) {
+        this.titleDesc = titleDesc;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+}
