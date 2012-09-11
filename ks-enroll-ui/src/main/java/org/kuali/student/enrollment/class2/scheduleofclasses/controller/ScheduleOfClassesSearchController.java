3c511f8610862984dc821583ca46f6bb50aa791f
vgadiyak <vgadiyak@kuali.org>
2012-09-11 08:07:50 -0400

    KSENROLL-2814 New file
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@36045 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/scheduleofclasses/controller/ScheduleOfClassesSearchController.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2012 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * Created by vgadiyak on 9/10/12
+ */
+package org.kuali.student.enrollment.class2.scheduleofclasses.controller;
+
+/**
+ * This class //TODO ...
+ *
+ * @author Kuali Student Team
+ */
+
+import org.kuali.rice.krad.web.controller.UifControllerBase;
+import org.kuali.rice.krad.web.form.UifFormBase;
+import org.kuali.student.enrollment.class2.scheduleofclasses.form.ScheduleOfClassesSearchForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping(value = "/scheduleOfClassesSearch")
+public class ScheduleOfClassesSearchController extends UifControllerBase {
+
+    @Override
+    protected UifFormBase createInitialForm(HttpServletRequest request) {
+        return new ScheduleOfClassesSearchForm();
+    }
+
+    @Override
+    @RequestMapping(method = RequestMethod.GET, params = "methodToCall=start")
+    public ModelAndView start(@ModelAttribute("KualiForm") UifFormBase form, BindingResult result,
+                              HttpServletRequest request, HttpServletResponse response) {
+        ScheduleOfClassesSearchForm scheduleOfClassesSearchForm = (ScheduleOfClassesSearchForm)form;
+
+        return super.start(form, result, request, response);
+    }
+
+}
