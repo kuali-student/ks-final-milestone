9ef87078de2eefa7db950aec50203c88409d62ca
gtaylor <gtaylor@kuali.org>
2012-09-13 15:50:23 -0400

    KSENROLL-2859: Now storing the principalId instead of the EntityId for Course and Activty Offring personnel.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@36167 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/transformer/OfferingInstructorTransformer.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/transformer/OfferingInstructorTransformer.java
@@ -65,7 +65,7 @@ public class OfferingInstructorTransformer {
 
     public static List<Person> getInstructorByPersonId(String personId){
         Map<String, String> searchCriteria = new HashMap<String, String>();
-        searchCriteria.put(KIMPropertyConstants.Person.ENTITY_ID, personId);
+        searchCriteria.put(KIMPropertyConstants.Person.PRINCIPAL_ID, personId);
         List<Person> lstPerson = getPersonService().findPeople(searchCriteria);
         return lstPerson;
     }
