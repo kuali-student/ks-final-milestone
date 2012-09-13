15c1fbe04e514e39f2169d83e037a3dd2de46839
vgadiyak <vgadiyak@kuali.org>
2012-09-13 08:36:11 -0400

    KSENROLL-2773 Nw file
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@36131 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/scheduleofclasses/service/ScheduleOfClassesViewHelperService.java
@@ -0,0 +1,11 @@
+package org.kuali.student.enrollment.class2.scheduleofclasses.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vgadiyak
+ * Date: 9/12/12
+ * Time: 2:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ScheduleOfClassesViewHelperService {
+}
