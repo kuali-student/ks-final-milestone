8d4ff5d968f8920c10ea62e59b27917fe11f25c7
vgadiyak <vgadiyak@kuali.org>
2012-09-14 13:21:47 -0400

    KSENROLL-2814 New file. Need wrapper as we need more info than just CODisplay
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@36203 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/scheduleofclasses/dto/CourseOfferingDisplayWrapper.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2012 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * Created by vgadiyak on 9/14/12
+ */
+package org.kuali.student.enrollment.class2.scheduleofclasses.dto;
+
+import org.kuali.student.enrollment.courseoffering.dto.ActivityOfferingDisplayInfo;
+import org.kuali.student.enrollment.courseoffering.dto.CourseOfferingDisplayInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class //TODO ...
+ *
+ * @author Kuali Student Team
+ */
+public class CourseOfferingDisplayWrapper {
+
+    private CourseOfferingDisplayInfo coDisplayInfo;
+    private String information;
+    private List<ActivityOfferingDisplayInfo> aoDisplayInfoList;
+
+    public CourseOfferingDisplayWrapper(){
+        aoDisplayInfoList = new ArrayList<ActivityOfferingDisplayInfo>();
+        coDisplayInfo = new CourseOfferingDisplayInfo();
+    }
+
+    public CourseOfferingDisplayInfo getCoDisplayInfo() {
+        return coDisplayInfo;
+    }
+
+    public void setCoDisplayInfo(CourseOfferingDisplayInfo coDisplayInfo) {
+        this.coDisplayInfo = coDisplayInfo;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    public List<ActivityOfferingDisplayInfo> getAoDisplayInfoList() {
+        return aoDisplayInfoList;
+    }
+
+    public void setAoDisplayInfoList(List<ActivityOfferingDisplayInfo> information) {
+        this.aoDisplayInfoList = aoDisplayInfoList;
+    }
+}
