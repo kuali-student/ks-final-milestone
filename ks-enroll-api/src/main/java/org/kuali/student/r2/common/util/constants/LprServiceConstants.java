ef4611ff43b45953c60945b4f4aa52ded20aa4e7
depstein <depstein@kuali.org>
2012-09-14 11:58:11 -0400

    KSENROLL-2873 implemented search by instructor for schedule of classes. Wired up LprService implemented search operation in Lpr, created transformatino filter. Renamed transcform to criteria transform for clarity.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/M5@36199 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/LprServiceConstants.java
+++ ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/LprServiceConstants.java
@@ -11,6 +11,7 @@
 package org.kuali.student.r2.common.util.constants;
 
 import org.kuali.student.enrollment.lpr.dto.LprInfo;
+import org.kuali.student.enrollment.lpr.service.LprService;
 import org.kuali.student.r2.common.constants.CommonServiceConstants;
 
 /**
@@ -24,6 +25,7 @@ public class LprServiceConstants {
      * Reference Object URI's
      */
     public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "lpr";
+    public static final String SERVICE_NAME_LOCAL_PART = LprService.class.getSimpleName();
     public static final String REF_OBJECT_URI_LUI_PERSON_RELATION = NAMESPACE + "/"
             + LprInfo.class.getSimpleName();
     /**
