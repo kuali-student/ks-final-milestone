1f0f5ad5f1b2fa82d6b1567c9ed7df0322f22e92
vgadiyak <vgadiyak@kuali.org>
2012-09-16 20:20:57 -0400

    KSENROLL-2814 New file
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@36243 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-web/src/main/webapp/ks-enroll/scripts/scheduleOfClasses.js
@@ -0,0 +1,7 @@
+function ajaxCallActivityOfferings(controllerMethod, courseOfferingId) {
+
+    alert("testing the method code = " + courseOfferingId);
+//    ajaxSubmitForm(controllerMethod, updateCollectionCallback,
+//        {updateComponentId: collectionGroupId, skipViewInit: 'true', selectedItemInputName: selectedItemId},
+//        elementToBlock, null,"update-component");
+}
