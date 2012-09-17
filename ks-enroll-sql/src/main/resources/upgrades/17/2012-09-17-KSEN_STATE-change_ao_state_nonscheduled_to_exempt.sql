a52a02e5d277abe5c17d6c41aab4411856c8beb1
chongzhu <chongzhu@kuali.org>
2012-09-17 15:42:33 -0400

    KSENROLL-2857:Removed duplicate set.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@36281 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/upgrades/17/2012-09-17-KSEN_STATE-change_ao_state_nonscheduled_to_exempt.sql
+++ ks-enroll-sql/src/main/resources/upgrades/17/2012-09-17-KSEN_STATE-change_ao_state_nonscheduled_to_exempt.sql
@@ -1,2 +1,2 @@
-UPDATE KSEN_STATE SET SET id = 'kuali.lui.activity.offering.scheduling.state.exempt', NAME = 'Exempt' where id = 'kuali.lui.activity.offering.scheduling.state.nonscheduled'
+UPDATE KSEN_STATE SET id = 'kuali.lui.activity.offering.scheduling.state.exempt', NAME = 'Exempt' where id = 'kuali.lui.activity.offering.scheduling.state.nonscheduled'
 /
