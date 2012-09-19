9db8c58799069ba07c03ca15af7e72aa06f7ece2
vpremcha <vpremcha@kuali.org>
2012-09-19 18:56:44 -0400

    KSENROLL-2861 - fixing NPE
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@36433 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/impl/ManageSOCViewHelperServiceImpl.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/impl/ManageSOCViewHelperServiceImpl.java
@@ -67,6 +67,12 @@ public class ManageSOCViewHelperServiceImpl extends ViewHelperServiceImpl implem
         try {
             List<String> socIds = getCourseOfferingSetService().getSocIdsByTerm(socForm.getTermInfo().getId(), ContextUtils.createDefaultContextInfo());
 
+            if (socIds.isEmpty()){
+                GlobalVariables.getMessageMap().putInfo(KRADConstants.GLOBAL_INFO, RiceKeyConstants.ERROR_CUSTOM,"SOC doesnt exists for this term");
+                socForm.setTermInfo(null);
+                return;
+            }
+
             if (socIds.size() > 1){   //Handle multiple soc when it is implemented (Not for M5)
                 GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_ERRORS, RiceKeyConstants.ERROR_CUSTOM,"Should not have multiple SOCs for a term (Not yet implemented departmental soc)");
             }
