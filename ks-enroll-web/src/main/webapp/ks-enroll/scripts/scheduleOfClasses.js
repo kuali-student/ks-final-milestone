d774f24ec46c8683f2307d69568d4509587965ad
vgadiyak <vgadiyak@kuali.org>
2012-09-21 17:22:37 -0400

    KSENROLL-2778 Basic (test) AJAX and display
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@36598 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-web/src/main/webapp/ks-enroll/scripts/scheduleOfClasses.js
+++ ks-enroll-web/src/main/webapp/ks-enroll/scripts/scheduleOfClasses.js
@@ -1,8 +1,25 @@
-function ajaxCallActivityOfferings(controllerMethod, courseOfferingId) {
+function ajaxCallActivityOfferings(controllerMethod, courseOfferingId, courseOfferingDesc) {
 
-//    alert("testing the method code = " + courseOfferingId);
+    var container;
+    var rowContent;
 
-    ajaxSubmitForm(controllerMethod, null,
-        {courseOfferingId: courseOfferingId},
-        null, null, null);
+//    ajaxSubmitForm(controllerMethod, updateTable,
+//        {courseOfferingId:courseOfferingId, coDisplayInfoId:courseOfferingId },
+//        jQuery('#' + courseOfferingId), null, "update-component");
+
+    ajaxSubmitForm(controllerMethod, updateTable, {courseOfferingId: courseOfferingId}, null, null, null);
+
+    function updateTable() {
+
+        container = jQuery('#' + courseOfferingId);
+//        row = jQuery(container).parents('tr');
+
+        if (jQuery('#scheduleOfClassesSearchResults').length > 0) {
+            rowContent = jQuery('#scheduleOfClassesSearchResults').html();
+//            row.after('<tr><td colspan="5" width="100%">' + rowContent + '</td></tr>');
+            container.append('<p>' + courseOfferingDesc + '</p>' + '<p>' + rowContent + '</p>')
+        }
+
+//        return false;
+    }
 }
