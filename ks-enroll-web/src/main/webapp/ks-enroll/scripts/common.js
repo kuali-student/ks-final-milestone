d711b21d72bc1e432d35366c477aebae10b97a41
hossein.aliabadi <hossein.aliabadi@kuali.org>
2012-09-28 13:08:53 -0400

    Jira KENROLL-3185: Added a jQuery function in commons.js to include some css files to the html element containing the iFrame.  When the lightbox is closed the css files are removed from the html element.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@37131 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-web/src/main/webapp/ks-enroll/scripts/common.js
+++ ks-enroll-web/src/main/webapp/ks-enroll/scripts/common.js
@@ -1,14 +1,14 @@
 /**
-* SCRIPT METHODS USED BY MULTIPLE .JS FILES
-*/
+ * SCRIPT METHODS USED BY MULTIPLE .JS FILES
+ */
 
 
 /**
-* Enter key causes next action button to fire, so this routine turns the Enter
-* keypress into a Tab keypress, essentially
-*/
+ * Enter key causes next action button to fire, so this routine turns the Enter
+ * keypress into a Tab keypress, essentially
+ */
 function tabToNextInput(e) {
-    if ( e.keyCode === 13 ) {
+    if (e.keyCode === 13) {
         e.preventDefault();
         var inputs = jq("input:visible,select:visible");
         var next = inputs.get(inputs.index(this) + 1);
@@ -18,29 +18,52 @@ function tabToNextInput(e) {
     }
 }
 
-function confirmDeletion(msg){
+function confirmDeletion(msg) {
     var response = confirm(msg);
 
-    if( response == true ){
+    if (response == true) {
         submitForm();
     }
 }
 
-function confirmDeletionResponse(msg){
+function confirmDeletionResponse(msg) {
     var response = confirm(msg);
 
     return response;
 }
 
-function confirmDeletion(msg){
+function confirmDeletion(msg) {
     var response = confirm(msg);
 
-    if( response == true ){
+    if (response == true) {
         submitForm();
     }
 }
 
-function stepBrowserBack(){
+function stepBrowserBack() {
     window.history.back(-2);
+}
+
+function showLightbox(componentId) {
+    // Insert the following css files before the lightbox appearing
+    parent.jQuery('link[href="rice-portal/css/portal.css"]').remove();
+    parent.jQuery('head').append('<link href="kr-dev/rice-portal/css/lightbox.css" rel="stylesheet" type="text/css">');
+    parent.jQuery('head').append('<link href="krad/css/global/base.css" rel="stylesheet" type="text/css">');
+    parent.jQuery('head').append('<link href="krad/css/ks/theme-krad.css" rel="stylesheet" type="text/css">');
+    parent.jQuery('head').append('<link href="krad/css/ks/fss-theme-krad.css" rel="stylesheet" type="text/css">');
+    parent.jQuery('head').append('<link href="ks-enroll/css/enroll.css" rel="stylesheet" type="text/css">');
+    parent.jQuery('head').append('<link href="ks-enroll/css/acal.css" rel="stylesheet" type="text/css">');
+
+    var overrideOptions = { afterClose:function () {
+        // Remove the following css files before the lightbox appearing
+        parent.jQuery('link[href="rice-portal/css/lightbox.css"]').remove();
+        parent.jQuery('link[href="krad/css/ks/theme-krad.css"]').remove();
+        parent.jQuery('link[href="krad/css/ks/fss-theme-krad.css"]').remove();
+        parent.jQuery('link[href="ks-enroll/css/enroll.css"]').remove();
+        parent.jQuery('link[href="ks-enroll/css/acal.css"]').remove();
+        parent.jQuery('link[href="krad/css/global/base.css"]').remove();
+        parent.jQuery('head').append('<link href="kr-dev/rice-portal/css/portal.css" rel="stylesheet" type="text/css">');
+    }};
+    showLightboxComponent(componentId, overrideOptions);
 }
 
