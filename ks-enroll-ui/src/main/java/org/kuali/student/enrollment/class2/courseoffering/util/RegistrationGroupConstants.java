90b9b0009e4f8baa0b752d45a1d5b07e5cfc3fbf
huangb <huangb@kuali.org>
2012-09-28 14:28:40 -0400

    KSENROLL-2744: Add the basic validate before call COS.getRegistrationGroupsByActivityOfferingCluster. Display the invalid cluster error message if any.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@37162 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/util/RegistrationGroupConstants.java
@@ -0,0 +1,13 @@
+package org.kuali.student.enrollment.class2.courseoffering.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: huangb
+ * Date: 9/28/12
+ * Time: 1:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RegistrationGroupConstants {
+    public static final String MSG_ERROR_INVALID_CLUSTER = "error.enroll.rg.invalid.cluster";
+
+}
