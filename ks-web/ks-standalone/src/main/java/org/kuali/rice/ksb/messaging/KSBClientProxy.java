7193d810ec52fb1ddf1a7fa2ce68a35879a2172f
sw <sw@kuali.org>
2012-10-03 04:27:26 -0400

    KSENROLL-3027 - Fixed ResultValuesGroupInfo data dictionary settings and overwrite KSBClientProxy in KS to unwrap the DataValidationException
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-deployments/trunk@37791 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-web/ks-standalone/src/main/java/org/kuali/rice/ksb/messaging/KSBClientProxy.java
+++ ks-web/ks-standalone/src/main/java/org/kuali/rice/ksb/messaging/KSBClientProxy.java
@@ -1,30 +1,30 @@
 /**
- * Copyright 2010 The Kuali Foundation Licensed under the
- * Educational Community License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may
- * obtain a copy of the License at
+ * Copyright 2005-2012 The Kuali Foundation
  *
- * http://www.osedu.org/licenses/ECL-2.0
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.kuali.rice.ksb.messaging;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.log4j.Logger;
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+
+import javax.xml.namespace.QName;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-
-import javax.xml.namespace.QName;
-
-import org.apache.commons.lang.exception.ExceptionUtils;
-import org.apache.log4j.Logger;
-import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 
 /**
  * This class creates a proxy for services deployed on KSB. A 
@@ -38,31 +38,54 @@ public class KSBClientProxy implements InvocationHandler {
 
 private static final Logger LOG = Logger.getLogger(KSBClientProxy.class);
     
-    QName serviceName;
-    Object service;
+    private QName serviceName;
+    private volatile Object service;
 
-    @SuppressWarnings("unchecked")
+
     public static <T> T newInstance(String serviceQName, Class<T> interfaceClass) throws InstantiationException, IllegalAccessException {
-        return (T)Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class[] { interfaceClass }, new KSBClientProxy(serviceQName));
+        if (StringUtils.isBlank(serviceQName)) {
+            throw new IllegalArgumentException("the qname was blank");
+    }
+
+        if (interfaceClass == null) {
+            throw new IllegalArgumentException("the interfaceClass was null");
+        }
+
+        @SuppressWarnings("unchecked")
+        final T t = (T)Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class[] { interfaceClass }, new KSBClientProxy(serviceQName));
+        return t;
     }
 
     public KSBClientProxy(String serviceQName){
+        if (StringUtils.isBlank(serviceQName)) {
+            throw new IllegalArgumentException("the qname was blank");
+        }
+
         this.serviceName = QName.valueOf(serviceQName);
     }
     
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if (this.service == null){
-            LOG.info("Getting service using GRL for: " + serviceName);
-            service = GlobalResourceLoader.getService(serviceName);
-            LOG.info("Obtained service using GRL for: " + serviceName);
+        //using DCL idiom
+        //see effective java 2nd ed. pg. 71
+        Object s = service;
+        if (s == null) {
+            synchronized (this) {
+                s = service;
+                if (s == null) {
+                    service = s = GlobalResourceLoader.getService(serviceName);
         }
-        
+            }
+        }
+
+        if (s != null) {
         try {
-            return method.invoke(service, args);
+                return method.invoke(s, args);
         } catch (InvocationTargetException e) {
             throw ExceptionUtils.getRootCause(e);
         }
-        
-    }
+        }
 
+        LOG.warn("serviceName: " + serviceName + " was not found");
+        return null;
+    }
 }
