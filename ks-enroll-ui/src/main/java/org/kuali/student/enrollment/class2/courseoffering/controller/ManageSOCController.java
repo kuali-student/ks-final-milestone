6d000112bce8c5ae7a7165ee7c170cb9978083b1
vpremcha <vpremcha@kuali.org>
2012-10-05 16:40:23 -0400

    KSENROLL-3292	Manage SOC - Browser Alerts
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@38041 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/controller/ManageSOCController.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/controller/ManageSOCController.java
@@ -1,8 +1,25 @@
+/**
+ * Copyright 2012 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * @author Kuali Student Team
+ */
+
 package org.kuali.student.enrollment.class2.courseoffering.controller;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.kuali.rice.core.api.util.RiceKeyConstants;
-import org.kuali.rice.krad.uif.UifParameters;
 import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.util.KRADConstants;
 import org.kuali.rice.krad.web.controller.UifControllerBase;
@@ -11,6 +28,7 @@ import org.kuali.student.enrollment.acal.dto.TermInfo;
 import org.kuali.student.enrollment.class2.courseoffering.form.ManageSOCForm;
 import org.kuali.student.enrollment.class2.courseoffering.service.ManageSOCViewHelperService;
 import org.kuali.student.enrollment.class2.courseoffering.util.CourseOfferingResourceLoader;
+import org.kuali.student.enrollment.class2.courseoffering.util.ManageSocConstants;
 import org.kuali.student.r2.common.util.ContextUtils;
 import org.kuali.student.r2.common.util.constants.CourseOfferingSetServiceConstants;
 import org.kuali.student.r2.core.class1.state.dto.StateInfo;
@@ -24,14 +42,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-import org.apache.log4j.Logger;
-
 /**
- * Created by IntelliJ IDEA.
- * User: venkat
- * Date: 9/14/12
- * Time: 3:09 PM
- * To change this template use File | Settings | File Templates.
+ * This class handles all the request for Managing SOC. This handles requests from ManageSOCView for different SOC state
+ * changes and scheduling/publishing events
  */
 @Controller
 @RequestMapping(value = "/manageSOC")
@@ -44,6 +57,7 @@ public class ManageSOCController extends UifControllerBase {
         ManageSOCForm form = new ManageSOCForm();
 
         try {
+            LOG.debug("Loading SOC states");
             List<StateInfo>  allSOCStates = CourseOfferingResourceLoader.loadStateService().getStatesByLifecycle(CourseOfferingSetServiceConstants.SOC_LIFECYCLE_KEY, ContextUtils.createDefaultContextInfo());
             for (StateInfo stateInfo : allSOCStates) {
                 form.getSocStateKeys2Names().put(stateInfo.getKey(), stateInfo.getName());
@@ -57,35 +71,67 @@ public class ManageSOCController extends UifControllerBase {
 
     @RequestMapping(params = "methodToCall=lockSOC")
     public ModelAndView lockSOC(@ModelAttribute("KualiForm") ManageSOCForm socForm, BindingResult result,
-                                            HttpServletRequest request, HttpServletResponse response){
+                                            HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        if (socForm.getSocInfo() == null){
-            throw new RuntimeException("SocInfo not exists in the form. Please enter the term code and click on GO button");
+        LOG.debug("Locking SOC");
+
+        String dialogName = ManageSocConstants.ConfirmDialogs.LOCK;
+
+        if (!hasDialogBeenAnswered(dialogName, socForm)){
+            return showDialog(dialogName, socForm, request, response);
         }
 
-        if (!StringUtils.equals(CourseOfferingSetServiceConstants.OPEN_SOC_STATE_KEY,socForm.getSocInfo().getStateKey())){
-            GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_ERRORS, RiceKeyConstants.ERROR_CUSTOM,"SOC should be in open state to lock");
+        boolean dialogAnswer = getBooleanDialogResponse(dialogName, socForm, request, response);
+        socForm.getDialogManager().resetDialogStatus(dialogName);
+
+        if (dialogAnswer){
+
+            if (socForm.getSocInfo() == null){
+                throw new RuntimeException("SocInfo not exists in the form. Please enter the term code and click on GO button");
+            }
+
+            if (!StringUtils.equals(CourseOfferingSetServiceConstants.OPEN_SOC_STATE_KEY,socForm.getSocInfo().getStateKey())){
+                GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_ERRORS, RiceKeyConstants.ERROR_CUSTOM,"SOC should be in open state to lock");
+                return getUIFModelAndView(socForm);
+            }
+
+            ManageSOCViewHelperService viewHelper = (ManageSOCViewHelperService)socForm.getView().getViewHelperService();
+            viewHelper.lockSOC(socForm);
+
+            return buildModel(socForm, result, request, response);
+
+        } else {
             return getUIFModelAndView(socForm);
         }
 
-        ManageSOCViewHelperService viewHelper = (ManageSOCViewHelperService)socForm.getView().getViewHelperService();
-        viewHelper.lockSOC(socForm);
-
-        return buildModel(socForm, result, request, response);
     }
 
     @RequestMapping(params = "methodToCall=sendApprovedActivitiesToScheduler")
     public ModelAndView sendApprovedActivitiesToScheduler (@ModelAttribute("KualiForm") ManageSOCForm socForm, BindingResult result,
-                                                            HttpServletRequest request, HttpServletResponse response){
+                                                            HttpServletRequest request, HttpServletResponse response)throws Exception {
+
         if ( ! StringUtils.equals(CourseOfferingSetServiceConstants.LOCKED_SOC_STATE_KEY,socForm.getSocInfo().getStateKey())){
             GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_ERRORS, RiceKeyConstants.ERROR_CUSTOM, "SOC should be in LOCKED state!");
             return getUIFModelAndView(socForm);
         }
 
-        // start send approved activities to scheduler
-        ManageSOCViewHelperService viewHelper = (ManageSOCViewHelperService) socForm.getView().getViewHelperService();
-        viewHelper.startMassScheduling(socForm);
-        return buildModel(socForm, result, request, response);
+        String dialogName = ManageSocConstants.ConfirmDialogs.MASS_SCHEDULING;
+
+        if (!hasDialogBeenAnswered(dialogName, socForm)){
+            return showDialog(dialogName, socForm, request, response);
+        }
+
+        boolean dialogAnswer = getBooleanDialogResponse(dialogName, socForm, request, response);
+        socForm.getDialogManager().resetDialogStatus(dialogName);
+
+        if (dialogAnswer){
+            // start send approved activities to scheduler
+            ManageSOCViewHelperService viewHelper = (ManageSOCViewHelperService) socForm.getView().getViewHelperService();
+            viewHelper.startMassScheduling(socForm);
+            return buildModel(socForm, result, request, response);
+        } else {
+            return getUIFModelAndView(socForm);
+        }
     }
 
     @RequestMapping(params = "methodToCall=buildModel")
@@ -118,7 +164,7 @@ public class ManageSOCController extends UifControllerBase {
 
     @RequestMapping(params = "methodToCall=allowFinalEdits")
     public ModelAndView allowFinalEdits(@ModelAttribute("KualiForm") ManageSOCForm socForm, BindingResult result,
-                                              HttpServletRequest request, HttpServletResponse response){
+                                              HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         if (socForm.getSocInfo() == null){
             throw new RuntimeException("SocInfo not exists in the form. Please enter the term code and click on GO button");
@@ -129,15 +175,30 @@ public class ManageSOCController extends UifControllerBase {
             return getUIFModelAndView(socForm);
         }
 
-        ManageSOCViewHelperService viewHelper = (ManageSOCViewHelperService)socForm.getView().getViewHelperService();
-        viewHelper.allowSOCFinalEdit(socForm);
+        String dialogName = ManageSocConstants.ConfirmDialogs.FINAL_EDITS;
 
-        return buildModel(socForm, result, request, response);
+        if (!hasDialogBeenAnswered(dialogName, socForm)){
+            return showDialog(dialogName, socForm, request, response);
+        }
+
+        boolean dialogAnswer = getBooleanDialogResponse(dialogName, socForm, request, response);
+        socForm.getDialogManager().resetDialogStatus(dialogName);
+
+        if (dialogAnswer){
+
+            ManageSOCViewHelperService viewHelper = (ManageSOCViewHelperService)socForm.getView().getViewHelperService();
+            viewHelper.allowSOCFinalEdit(socForm);
+
+            return buildModel(socForm, result, request, response);
+
+        } else {
+            return getUIFModelAndView(socForm);
+        }
     }
 
     @RequestMapping(params = "methodToCall=publishSOC")
     public ModelAndView publishSOC(@ModelAttribute("KualiForm") ManageSOCForm socForm, BindingResult result,
-                                              HttpServletRequest request, HttpServletResponse response){
+                                              HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         if (socForm.getSocInfo() == null){
             throw new RuntimeException("SocInfo not exists in the form. Please enter the term code and click on GO button");
@@ -148,21 +209,34 @@ public class ManageSOCController extends UifControllerBase {
             return getUIFModelAndView(socForm);
         }
 
-        ManageSOCViewHelperService viewHelper = (ManageSOCViewHelperService)socForm.getView().getViewHelperService();
-        try {
-            viewHelper.publishSOC(socForm);
-        } catch(Exception e) {
-            LOG.error("Could not start mass publishing event.", e);
-            GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_ERRORS, RiceKeyConstants.ERROR_CUSTOM, "Unable to initiate publishing.");
-            return getUIFModelAndView(socForm);
+        String dialogName = ManageSocConstants.ConfirmDialogs.MASS_PUBLISHLING;
+
+        if (!hasDialogBeenAnswered(dialogName, socForm)){
+            return showDialog(dialogName, socForm, request, response);
         }
 
-        return buildModel(socForm, result, request, response);
+        boolean dialogAnswer = getBooleanDialogResponse(dialogName, socForm, request, response);
+        socForm.getDialogManager().resetDialogStatus(dialogName);
+
+        if (dialogAnswer){
+            ManageSOCViewHelperService viewHelper = (ManageSOCViewHelperService)socForm.getView().getViewHelperService();
+            try {
+                viewHelper.publishSOC(socForm);
+            } catch(Exception e) {
+                LOG.error("Could not start mass publishing event.", e);
+                GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_ERRORS, RiceKeyConstants.ERROR_CUSTOM, "Unable to initiate publishing.");
+                return getUIFModelAndView(socForm);
+            }
+
+            return buildModel(socForm, result, request, response);
+        } else {
+            return getUIFModelAndView(socForm);
+        }
     }
 
     @RequestMapping(params = "methodToCall=closeSOC")
     public ModelAndView closeSOC(@ModelAttribute("KualiForm") ManageSOCForm socForm, BindingResult result,
-                                              HttpServletRequest request, HttpServletResponse response){
+                                              HttpServletRequest request, HttpServletResponse response) throws Exception {
         if (socForm.getSocInfo() == null){
             throw new RuntimeException("SocInfo not exists in the form. Please enter the term code and click on GO button");
         }
@@ -172,9 +246,22 @@ public class ManageSOCController extends UifControllerBase {
             return getUIFModelAndView(socForm);
         }
 
-        ManageSOCViewHelperService viewHelper = (ManageSOCViewHelperService)socForm.getView().getViewHelperService();
-        viewHelper.closeSOC(socForm);
+        String dialogName = ManageSocConstants.ConfirmDialogs.CLOSE_SET;
 
-        return buildModel(socForm, result, request, response);
+        if (!hasDialogBeenAnswered(dialogName, socForm)){
+            return showDialog(dialogName, socForm, request, response);
+        }
+
+        boolean dialogAnswer = getBooleanDialogResponse(dialogName, socForm, request, response);
+        socForm.getDialogManager().resetDialogStatus(dialogName);
+
+        if (dialogAnswer){
+            ManageSOCViewHelperService viewHelper = (ManageSOCViewHelperService)socForm.getView().getViewHelperService();
+            viewHelper.closeSOC(socForm);
+
+            return buildModel(socForm, result, request, response);
+        } else {
+            return getUIFModelAndView(socForm);
+        }
     }
 }
---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/util/ManageSocConstants.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2012 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * @author Kuali Student Team
+ */
+
+package org.kuali.student.enrollment.class2.courseoffering.util;
+
+public class ManageSocConstants {
+
+    public static class ConfirmDialogs{
+        public static final String MASS_SCHEDULING = "massScheduleConfirmDialog";
+        public static final String MASS_PUBLISHLING = "massPublishConfirmDialog";
+        public static final String LOCK = "lockConfirmDialog";
+        public static final String PUBLISH = "publishConfirmDialog";
+        public static final String FINAL_EDITS = "finalEditConfirmDialog";
+        public static final String CLOSE_SET = "closeConfirmDialog";
+    }
+}
