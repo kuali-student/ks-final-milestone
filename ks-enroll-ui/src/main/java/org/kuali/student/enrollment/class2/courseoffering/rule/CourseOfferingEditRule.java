e2d78f374f05861e693c3e0281b46494f5b2de9c
lpan <lpan@kuali.org>
2012-10-17 22:25:04 -0400

    ksenroll-2634: add checking duplicated suffix in coEditRule
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@38702 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/rule/CourseOfferingEditRule.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/rule/CourseOfferingEditRule.java
@@ -16,8 +16,21 @@
  */
 package org.kuali.student.enrollment.class2.courseoffering.rule;
 
+import org.apache.commons.lang.StringUtils;
+import org.kuali.rice.core.api.util.RiceKeyConstants;
 import org.kuali.rice.krad.maintenance.MaintenanceDocument;
 import org.kuali.rice.krad.rules.MaintenanceDocumentRuleBase;
+import org.kuali.rice.krad.util.GlobalVariables;
+import org.kuali.rice.krad.util.KRADConstants;
+import org.kuali.student.enrollment.class2.courseoffering.dto.CourseOfferingCreateWrapper;
+import org.kuali.student.enrollment.class2.courseoffering.dto.CourseOfferingEditWrapper;
+import org.kuali.student.enrollment.class2.courseoffering.util.CourseOfferingConstants;
+import org.kuali.student.enrollment.class2.courseoffering.util.CourseOfferingResourceLoader;
+import org.kuali.student.enrollment.courseoffering.dto.CourseOfferingInfo;
+import org.kuali.student.enrollment.courseoffering.service.CourseOfferingService;
+import org.kuali.student.r2.common.util.ContextUtils;
+
+import java.util.List;
 
 /**
  * This class //TODO ...
@@ -26,8 +39,48 @@ import org.kuali.rice.krad.rules.MaintenanceDocumentRuleBase;
  */
 public class CourseOfferingEditRule extends MaintenanceDocumentRuleBase {
 
+       private CourseOfferingService courseOfferingService;
+
+    private final static String EXISTING_CO_CODE_FOUND_ERROR = "That Course Offering Code is already in use.  Please enter a different, unique Course Offering Code for ";
+
     @Override
     protected boolean processGlobalSaveDocumentBusinessRules(MaintenanceDocument document) {
+        boolean valid = true;
+
+        if (document.getNewMaintainableObject().getDataObject() instanceof CourseOfferingEditWrapper){
+            CourseOfferingEditWrapper coWrapper = (CourseOfferingEditWrapper)document.getNewMaintainableObject().getDataObject();
+
+             validateDuplicateSuffix(coWrapper);
+        }
+
+        return valid;
+    }
+
+
+    protected boolean validateDuplicateSuffix(CourseOfferingEditWrapper coWrapper){
+        // Catalog course code is case INSENSITIVE, but the suffix is case SENSITIVE
+        String newCoCode = (coWrapper.getCourse().getCode().toUpperCase()) + coWrapper.getCoInfo().getCourseNumberSuffix();
+        try {
+            List<CourseOfferingInfo> wrapperList = getCourseOfferingService().getCourseOfferingsByCourseAndTerm(coWrapper.getCourse().getId(), coWrapper.getCoInfo().getTermId(), ContextUtils.createDefaultContextInfo());
+            for (CourseOfferingInfo courseOfferingInfo : wrapperList) {
+
+                if (StringUtils.equals(newCoCode, courseOfferingInfo.getCourseOfferingCode())) {
+                    StringBuilder sb = new StringBuilder(EXISTING_CO_CODE_FOUND_ERROR);
+                    sb.append(coWrapper.getCourse().getCode());
+                    GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_ERRORS, RiceKeyConstants.ERROR_CUSTOM, sb.toString());
+                    return false;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return true;
+    }
+
+    protected CourseOfferingService getCourseOfferingService() {
+        if (courseOfferingService == null) {
+            courseOfferingService = CourseOfferingResourceLoader.loadCourseOfferingService();
+        }
+        return courseOfferingService;
     }
 }
