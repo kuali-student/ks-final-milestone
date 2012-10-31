fa2a0e78b4345529ace09cf1a129c49007dde32a
llfourie <llfourie@kuali.org>
2012-10-31 09:02:33 -0400

    KSENROLL-3612
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@39239 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum-sql/src/main/resources/upgrades/17/2012-10-31-drop-ksap-tables.sql
@@ -0,0 +1,118 @@
+alter table KSAP_ATP
+drop constraint KSAP_ATP_FK2
+/
+
+alter table KSAP_ATP
+drop constraint KSAP_ATP_FK1
+/
+
+alter table KSAP_ATP_DUR_TYPE_ATTR
+drop constraint KSAP_ATP_DUR_TYPE_ATTR_FK1
+/
+
+alter table KSAP_ATP_SEASONAL_TYPE_ATTR
+drop constraint KSAP_ATP_SEAS_TYPE_ATTR_FK1
+/
+
+alter table KSAP_ATP_TYPE
+drop constraint KSAP_ATP_TYPE_FK1
+/
+
+alter table KSAP_ATP_TYPE
+drop constraint KSAP_ATP_TYPE_FK2
+/
+
+alter table KSAP_ATP_TYPE_ATTR
+drop constraint KSAP_ATP_TYPE_ATTR_FK1
+/
+
+alter table KSAP_DT_RANGE
+drop constraint KSAP_DT_RANGE_FK1
+/
+
+alter table KSAP_DT_RANGE
+drop constraint KSAP_DT_RANGE_FK3
+/
+
+alter table KSAP_DT_RANGE
+drop constraint KSAP_DT_RANGE_FK2
+/
+
+alter table KSAP_DT_RANGE_ATTR
+drop constraint KSAP_DT_RANGE_ATTR_FK1
+/
+
+alter table KSAP_DT_RANGE_TYPE_ATTR
+drop constraint KSAP_DT_RANGE_TYPE_ATTR_FK1
+/
+
+alter table KSAP_MLSTN
+drop constraint KSAP_MLSTN_FK2
+/
+
+alter table KSAP_MLSTN
+drop constraint KSAP_MLSTN_FK3
+/
+
+alter table KSAP_MLSTN
+drop constraint KSAP_MLSTN_FK1
+/
+
+alter table KSAP_MLSTN_ATTR
+drop constraint KSAP_MLSTN_ATTR_FK1
+/
+
+alter table KSAP_MLSTN_TYPE_ATTR
+drop constraint KSAP_MLSTN_TYPE_ATTR_FK1
+/
+
+drop table KSAP_ATP
+/
+
+drop table KSAP_ATP_ATTR
+/
+
+drop table KSAP_ATP_DUR_TYPE
+/
+
+drop table KSAP_ATP_DUR_TYPE_ATTR
+/
+
+drop table KSAP_ATP_SEASONAL_TYPE
+/
+
+drop table KSAP_ATP_SEASONAL_TYPE_ATTR
+/
+
+drop table KSAP_ATP_TYPE
+/
+
+drop table KSAP_ATP_TYPE_ATTR
+/
+
+drop table KSAP_DT_RANGE
+/
+
+drop table KSAP_DT_RANGE_ATTR
+/
+
+drop table KSAP_DT_RANGE_TYPE
+/
+
+drop table KSAP_DT_RANGE_TYPE_ATTR
+/
+
+drop table KSAP_MLSTN
+/
+
+drop table KSAP_MLSTN_ATTR
+/
+
+drop table KSAP_MLSTN_TYPE
+/
+
+drop table KSAP_MLSTN_TYPE_ATTR
+/
+
+drop table KSAP_RICH_TEXT_T
+/
