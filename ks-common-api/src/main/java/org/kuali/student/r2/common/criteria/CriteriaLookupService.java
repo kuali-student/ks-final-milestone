92df5f495e2d4b15efbdb621962e422635473310
chongzhu <chongzhu@kuali.org>
2012-11-01 15:02:14 -0400

    KSENROLL-3666:Modified CO service to remove compile errors.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/M6@39315 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-api/src/main/java/org/kuali/student/r2/common/criteria/CriteriaLookupService.java
+++ ks-common-api/src/main/java/org/kuali/student/r2/common/criteria/CriteriaLookupService.java
@@ -3,6 +3,8 @@ package org.kuali.student.r2.common.criteria;
 import org.kuali.rice.core.api.criteria.GenericQueryResults;
 import org.kuali.rice.core.api.criteria.QueryByCriteria;
 
+import java.util.List;
+
 public interface CriteriaLookupService {
 
     /**
@@ -26,5 +28,12 @@ public interface CriteriaLookupService {
      * @throws IllegalArgumentException if the criteria is null
      */
     <T> GenericQueryResults<T> lookup(final Class<T> queryClass, final QueryByCriteria criteria, final LookupCustomizer<T> customizer);
-    
+
+    <T> GenericQueryResults<String> lookupIds(Class<T> queryClass, QueryByCriteria criteria);
+
+    <T> GenericQueryResults<String> lookupIds(Class<T> queryClass, QueryByCriteria criteria, LookupCustomizer<T> customizer);
+
+    <T> GenericQueryResults<List<String>> genericLookup(final Class<T> queryClass, final QueryByCriteria criteria, List<String> fields);
+
+    <T> GenericQueryResults<List<String>> genericLookup(final Class<T> queryClass, final QueryByCriteria criteria, final LookupCustomizer<T> customizer, List<String> fields);
 }
