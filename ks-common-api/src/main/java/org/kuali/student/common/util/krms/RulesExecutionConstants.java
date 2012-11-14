5ee33b76dbe7e2ccc4adef9b0dd7a76d28d28652
sw <sw@kuali.org>
2012-11-14 03:35:38 -0500

    KSENROLL-1352 - KRMS with Rice integration test.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/M6@39839 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-api/src/main/java/org/kuali/student/common/util/krms/RulesExecutionConstants.java
+++ ks-common-api/src/main/java/org/kuali/student/common/util/krms/RulesExecutionConstants.java
@@ -22,6 +22,10 @@ public class RulesExecutionConstants {
     public static final String MILESTONES_BY_TYPE_TERM_NAME = "milestonesByType";
     public static final String MILESTONE_TERM_NAME = "milestone";
     public static final String STUDENT_REGISTRATION_HOLDS_TERM_NAME = "studentRegistrationHolds";
+
+    public static final String ORG_PERMISSION_TERM_NAME = "orgPermission";
+    public static final String NR_OF_COMPLETED_COURSES_TERM_NAME = "numberOfCompletedCourses";
+    public static final String COMPLETED_COURSE_TERM_NAME = "completedCourse";
     
     public static final Term completedCourseIdsTerm = new Term(STUDENT_COMPLETED_COURSE_IDS_TERM_NAME);
     public static final Term enrolledCourseIdsTerm = new Term(STUDENT_ENROLLED_COURSE_IDS_TERM_NAME);
@@ -34,6 +38,9 @@ public class RulesExecutionConstants {
     public static final String MILESTONE_ATP_KEY_TERM_PROPERTY = "milestoneAtpKeyProperty";
     public static final String MILESTONE_ID_TERM_PROPERTY = "milestoneIdProperty";
     public static final String ISSUE_KEY_TERM_PROPERTY = "issueKeyProperty";
+
+    public static final String REQUIRED_COURSES_PROPERTY = "requiredCourses";
+    public static final String ORGANIZATION_ID_PROPERTY = "organizationId";
 
     public static final String STATEMENT_EVENT_NAME = "statementEvent";
     public static final String PROCESS_EVENT_NAME = "processEvent";
