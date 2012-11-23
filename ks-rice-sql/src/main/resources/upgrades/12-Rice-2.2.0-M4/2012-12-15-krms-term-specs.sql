855e76c62b74b3b5f821c2aabbfceceb69c7552b
llfourie <llfourie@kuali.org>
2012-11-23 06:24:51 -0500

    KSENROLL-3856 : Create a term data script
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@40208 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-rice-sql/src/main/resources/upgrades/12-Rice-2.2.0-M4/2012-12-15-krms-term-specs.sql
@@ -0,0 +1,64 @@
+-- Completed Course
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'completedCourse', 'java.lang.Boolean', 'Y', 1, '????', 'KS-SYS')
+/
+
+-- Completed Courses
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'completedCourses', 'java.lang.Boolean', 'Y', 1, '????', 'KS-SYS')
+/
+
+-- Number of Completed Courses
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'numberOfCompletedCourses', 'java.lang.Integer', 'Y', 1, '????', 'KS-SYS')
+/
+
+-- Number of Credits from Completed Courses
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'numberOfCreditsFromCompletedCourses', 'java.lang.Integer', 'Y', 1, '????', 'KS-SYS')
+/
+
+-- Enrolled Courses
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'enrolledCourses', 'java.lang.Integer', 'Y', 1, '????', 'KS-SYS')
+/
+
+-- GPA for Courses
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'GPAForCourses', 'java.lang.Integer', 'Y', 1, '????', 'KS-SYS')
+/
+
+-- Grade Type for Courses
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'gradeTypeForCourses', 'java.lang.Integer', 'Y', 1, '????', 'KS-SYS')
+/
+
+-- Grade for Courses
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'gradeForCourses', 'java.lang.Integer', 'Y', 1, '????', 'KS-SYS')
+/
+
+-- Number of Credits
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'numberOfCredits', 'java.lang.Integer', 'Y', 1, '????', 'KS-SYS')
+/
+
+-- Number of Credits from Organization
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'numberOfCreditsFromOrganization', 'java.lang.Integer', 'Y', 1, '????', 'KS-SYS')
+/
+
+-- Admin Organization Permission Required
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'adminOrganizationPermissionRequired', 'java.lang.Boolean', 'Y', 1, '????', 'KS-SYS')
+/
+
+-- Score on Test
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'scoreOnTest', 'java.lang.Integer', 'Y', 1, '????', 'KS-SYS')
+/
+
+-- Admitted to Program
+insert into krms_term_spec_t (term_spec_id, nm, typ, actv, ver_nbr, desc_txt, nmspc_cd)
+ values (krms_term_spec_s.nextval, 'admittedToProgram', 'java.lang.Boolean', 'Y', 1, '????', 'KS-SYS')
+/
