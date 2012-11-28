afa6d6748a54e758d4213299a8eb0d3cd3a089fb
depstein <depstein@kuali.org>
2012-11-28 10:58:02 -0500

    KSENROLL-3755 updated view authorized (also fixed compilation error)
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@40357 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/main/view/KsViewAuthorizerBase.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/main/view/KsViewAuthorizerBase.java
@@ -1,44 +1,45 @@
 package org.kuali.student.enrollment.main.view;
 
-import org.kuali.rice.kim.api.KimConstants;
 import org.kuali.rice.kim.api.identity.Person;
+import org.kuali.rice.krad.uif.component.Component;
 import org.kuali.rice.krad.uif.view.View;
 import org.kuali.rice.krad.uif.view.ViewAuthorizerBase;
 import org.kuali.rice.krad.uif.view.ViewModel;
-import org.kuali.rice.krad.util.KRADConstants;
-
 import org.kuali.student.enrollment.class2.courseoffering.form.CourseOfferingManagementForm;
-//import org.kuali.rice.student.bo.KualiStudentKimAttributes;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+/**
+ * Custom Authorizer that resolves qualifiers from the model. This will need to be refactored and made more generic in
+ * the future (configuration of mapping from model object graph to role qualifiers, maybe using beanutils
+ * (object.field1[3].field2->qualifierId)
+ */
 public class KsViewAuthorizerBase extends ViewAuthorizerBase {
 
+    /**
+     * This method resolves the unitsDeploymentOrgId from the CourseOffering Model and passes it in as
+     * a role qualifier
+     */
     @Override
-    public boolean canOpenView(View view, ViewModel model, Person user) {
-        Map<String, String> additionalPermissionDetails = new HashMap<String, String>();
-        additionalPermissionDetails.put(KimConstants.AttributeConstants.NAMESPACE_CODE, view.getNamespaceCode());
-        additionalPermissionDetails.put(KimConstants.AttributeConstants.VIEW_ID, model.getViewId());
-
-        if (permissionExistsByTemplate(model, KRADConstants.KRAD_NAMESPACE,
-                KimConstants.PermissionTemplateNames.OPEN_VIEW, additionalPermissionDetails)) {
-            if (model instanceof CourseOfferingManagementForm) {
-                CourseOfferingManagementForm theForm = (CourseOfferingManagementForm) model;
-                String adminOrg = theForm.getAdminOrg();
-                Map<String, String> additionalRoleQualifications = new HashMap<String, String>();
-//                additionalRoleQualifications.put(KualiStudentKimAttributes.QUALIFICATION_ORG_ID,adminOrg);
-                additionalRoleQualifications.put("org",adminOrg);
-                return isAuthorizedByTemplate(model, KRADConstants.KRAD_NAMESPACE,
-                        KimConstants.PermissionTemplateNames.OPEN_VIEW, user.getPrincipalId(), additionalPermissionDetails,
-                        additionalRoleQualifications);
-
+    protected boolean isAuthorizedByTemplate(View view, Component component, ViewModel model, String permissionTemplateName, Person user, Map<String, String> additionalPermissionDetails, Map<String, String> additionalRoleQualifications, boolean checkPermissionExistence) {
+        if (model !=null && model instanceof CourseOfferingManagementForm) {
+            CourseOfferingManagementForm theForm = (CourseOfferingManagementForm) model;
+            if(theForm.getTheCourseOffering() != null){
+                //Pull out the org ids and pass in the first one as a role qualifier
+                List<String> orgIds = theForm.getTheCourseOffering().getUnitsDeploymentOrgIds();
+                if(orgIds !=null && !orgIds.isEmpty()){
+                    if(additionalRoleQualifications == null){
+                        //Instantiate if null was passed in
+                        additionalRoleQualifications = new HashMap<String, String>();
+                    }
+                    additionalRoleQualifications.put("org", orgIds.get(0));
+                }
             }
-            else
-                return isAuthorizedByTemplate(model, KRADConstants.KRAD_NAMESPACE,
-                    KimConstants.PermissionTemplateNames.OPEN_VIEW, user.getPrincipalId(), additionalPermissionDetails,
-                    null);
         }
-
-        return true;
+        // Make the actual call to is authorized by template
+        return super.isAuthorizedByTemplate(view, component, model, permissionTemplateName, user, additionalPermissionDetails, additionalRoleQualifications, checkPermissionExistence);
     }
+
 }
