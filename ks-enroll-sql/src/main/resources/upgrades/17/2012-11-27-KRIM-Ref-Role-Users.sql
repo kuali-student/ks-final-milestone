e64c9ec530333c02ef386fe6adae5ff814892103
depstein <depstein@kuali.org>
2012-11-28 13:15:19 -0500

    KSENROLL-3755 updated data from specs, refactored to three files that describe if ref or bootstrap data.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@40371 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/upgrades/17/2012-11-27-KRIM-Ref-Role-Users.sql
+++ ks-enroll-sql/src/main/resources/upgrades/17/2012-11-27-KRIM-Ref-Role-Users.sql
@@ -1,3 +1,4 @@
+--Create persona users in ref data
 select krim_entity_id_s.nextval from dual
 /
 insert into KRIM_ENTITY_T (ENTITY_ID, OBJ_ID, VER_NBR, ACTV_IND, LAST_UPDT_DT) values (KRIM_ENTITY_ID_S.NEXTVAL, SYS_GUID(), 1, 'Y', SYSDATE)
@@ -49,4 +50,18 @@ insert into KRIM_ENTITY_AFLTN_T (ENTITY_AFLTN_ID, OBJ_ID, VER_NBR, ENTITY_ID, AF
 insert into KRIM_ENTITY_ENT_TYP_T (ENT_TYP_CD, ENTITY_ID, OBJ_ID, VER_NBR, ACTV_IND, LAST_UPDT_DT) values ('PERSON', KRIM_ENTITY_ID_S.CURRVAL, SYS_GUID(), 1, 'Y', SYSDATE)
 /
 insert into KRIM_ENTITY_NM_T (ENTITY_NM_ID, OBJ_ID, VER_NBR, ENTITY_ID, NM_TYP_CD, FIRST_NM, MIDDLE_NM, LAST_NM, SUFFIX_NM, TITLE_NM, DFLT_IND, ACTV_IND, LAST_UPDT_DT) values (KRIM_ENTITY_NM_ID_S.NEXTVAL, SYS_GUID(), 1, KRIM_ENTITY_ID_S.CURRVAL, 'PRFR', 'student', null, 'student', null, null, 'Y', 'Y', SYSDATE)
+/
+
+--Role - Member
+insert into KRIM_ROLE_MBR_T (ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD, ACTV_FRM_DT, ACTV_TO_DT, LAST_UPDT_DT) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'KS Schedule Coordinator ' and nmspc_cd = 'KS-ENR'), 'martha', 'P', '', '', TO_DATE('10/24/2012', 'MM/DD/YYYY'))
+/
+insert into KRIM_ROLE_MBR_T (ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD, ACTV_FRM_DT, ACTV_TO_DT, LAST_UPDT_DT) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'KS Schedule Coordinator ' and nmspc_cd = 'KS-ENR'), 'mark', 'P', '', '', TO_DATE('10/24/2012', 'MM/DD/YYYY'))
+/
+insert into KRIM_ROLE_MBR_T (ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD, ACTV_FRM_DT, ACTV_TO_DT, LAST_UPDT_DT) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'KS Department Schedule Coordinator - Org ' and nmspc_cd = 'KS-ENR'), 'carol', 'P', '', '', TO_DATE('10/24/2012', 'MM/DD/YYYY'))
+/
+insert into KRIM_ROLE_MBR_T (ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD, ACTV_FRM_DT, ACTV_TO_DT, LAST_UPDT_DT) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'KS Schedule Coordinator View Only' and nmspc_cd = 'KS-ENR'), 'carol', 'P', '', '', TO_DATE('10/24/2012', 'MM/DD/YYYY'))
+/
+
+--Role Member Details
+insert into KRIM_ROLE_MBR_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, ROLE_MBR_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) values ('KRIM_ATTR_DATA_ID_S.NEXTVAL',  SYS_GUID(), 1, (SELECT ROLE_MBR_ID from KRIM_ROLE_MBR_T where ROLE_ID = (SELECT ROLE_ID FROM KRIM_ROLE_T where role_nm = 'KS Department Schedule Coordinator - Org ' and nmspc_cd = 'KS-ENR') AND MBR_ID = 'carol'), (SELECT kim_typ_id from krim_typ_t where nm = 'Default' and nmspc_cd = 'KUALI'), (SELECT MIN(TO_NUMBER(kim_attr_defn_id)) from krim_attr_defn_t where nm = 'org' and nmspc_cd = 'KS-ENR'), '1000ENGL')
 /
