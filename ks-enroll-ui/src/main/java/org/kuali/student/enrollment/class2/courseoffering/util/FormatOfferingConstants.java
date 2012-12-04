3f95a9f7ab4db493c090d6901ae8d0dcb067c2d6
dosoar <dosoar@kuali.org>
2012-12-04 17:37:09 -0500

    KSENROLL-4173 Created KS-FormatOfferingInfoByType-LookupView inside "courseoffering\FormatOfferingInfoLookupAndInquiryView.xml" Added method "getSearchResultsByType (String typeKey)" inside "FormatOfferingInfoLookupableImpl.java" Created "FormatOfferingConstants.java" Updated portal-link in "coApplications.tag"
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@40659 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/util/FormatOfferingConstants.java
@@ -0,0 +1,10 @@
+package org.kuali.student.enrollment.class2.courseoffering.util;
+
+/**
+ * @author Kuali Student Team
+ * Initial issuance - December/04/2012
+ */
+public class FormatOfferingConstants {
+    public final static String FORMAT_OFFERING_TYPE_KEY = "typeKey";
+    public final static String FORMAT_OFFERING_LUI_TYPE = "luiType";
+}
