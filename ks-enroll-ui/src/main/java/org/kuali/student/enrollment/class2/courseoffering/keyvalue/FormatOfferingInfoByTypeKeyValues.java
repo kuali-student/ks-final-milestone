3a0866e3c2eb34e85b2227797435fbf858ae1657
dosoar <dosoar@kuali.org>
2012-12-06 13:51:23 -0500

    KSENROLL-4154 Added a DropDownControl to the "KS-FormatOfferingInfoByType-LookupView" of the FormatOfferingInfoLookupAndInquiryView.xml coded an "optionsFinder" in FormatOfferingInfoByTypeKeyValues.java Added constants to FormatOfferingConstants.java
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@40765 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/keyvalue/FormatOfferingInfoByTypeKeyValues.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/keyvalue/FormatOfferingInfoByTypeKeyValues.java
@@ -4,19 +4,14 @@ import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
 import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
 import org.kuali.rice.krad.uif.view.ViewModel;
-import org.kuali.student.enrollment.common.util.ContextBuilder;
-import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.enrollment.class2.courseoffering.util.FormatOfferingConstants;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
 /**
- * Created with IntelliJ IDEA.
- * User: dosoar
- * Date: 12/5/12
- * Time: 2:22 PM
- * To change this template use File | Settings | File Templates.
+ *  @Date                     @Author                            @Work Description
+ *  December/06/2012          Kuali Student Team                 Initial  Issuance  - Hard-Coded Drop-Down value for "Format Offering Info TypeKey"
  */
 //public class FormatOfferingInfoByTypeKeyValues extends  AbstractFormatOfferingTypeKeyValues implements Serializable {
 public class FormatOfferingInfoByTypeKeyValues extends UifKeyValuesFinderBase implements Serializable {
@@ -24,38 +19,10 @@ public class FormatOfferingInfoByTypeKeyValues extends UifKeyValuesFinderBase im
     @Override
     public List<KeyValue> getKeyValues(ViewModel model) {
 
-       // List<KeyValue> keyValues = new ArrayList<KeyValue>();
-       String typeKey = "kuali.lui.type.course.format.offering";
-
         List<KeyValue> keyValues = new ArrayList<KeyValue>();
-        //Will get the correct FormatOfferingInfo later
-       // TypeService typeService = CourseOfferingResourceLoader.loadTypeService();
-       // QueryByCriteria.Builder qBuilder = QueryByCriteria.Builder.create();
-        try{
-           // List<String> types = typeService.searchForTypeKeys(qBuilder.build(), new ContextInfo());
-            keyValues.add(new ConcreteKeyValue("", "Select Term Type"));
-            keyValues.add(new ConcreteKeyValue("1234567890", typeKey));
-          //  for(int i=0;i<1;i++){
-               // keyValues.add(new ConcreteKeyValue(types.get(i),typeKey));
-           // }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+        keyValues.add(new ConcreteKeyValue(FormatOfferingConstants.BLANK, FormatOfferingConstants.SELECT_FORMAT_OFFERING_TYPE_KEY ));
+        keyValues.add(new ConcreteKeyValue(FormatOfferingConstants.FORMAT_OFFERING_TYPE_KEY_VALUE, FormatOfferingConstants.FORMAT_OFFERING_TYPE_KEY_VALUE));
 
         return keyValues;
-    }
-
-
-    public ContextInfo getContextInfo() {
-        ContextInfo contextInfo = new ContextInfo();
-        if (contextInfo == null){
-            contextInfo =  ContextBuilder.loadContextInfo();
-        }
-        return contextInfo;
-    }
-
-
-    public static int getLineNumber() {
-        return Thread.currentThread().getStackTrace()[2].getLineNumber();
     }
 }
--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/util/FormatOfferingConstants.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/util/FormatOfferingConstants.java
@@ -1,10 +1,13 @@
 package org.kuali.student.enrollment.class2.courseoffering.util;
 
 /**
- * @author Kuali Student Team
- * Initial issuance - December/04/2012
+ *  @Date                     @Author                            @Work Description
+ *  December/04/2012          Kuali Student Team                 Initial  Issuance
  */
 public class FormatOfferingConstants {
+    public final static String BLANK = "";
     public final static String FORMAT_OFFERING_TYPE_KEY = "typeKey";
     public final static String FORMAT_OFFERING_LUI_TYPE = "luiType";
+    public final static String FORMAT_OFFERING_TYPE_KEY_VALUE = "kuali.lui.type.course.format.offering";
+    public final static String SELECT_FORMAT_OFFERING_TYPE_KEY = "Select Format Offering Type Key";
 }
