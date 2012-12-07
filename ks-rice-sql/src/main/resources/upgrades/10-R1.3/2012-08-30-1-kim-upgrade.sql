3df7babfee981ba3273bbbf64494f4f2afa6efab
christoff.botha <christoff.botha@kuali.org>
2012-12-07 04:56:24 -0500

    KSENROLL-4083 Fixed the permissions that allow a proposer to withdraw their proposal.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/branches/CM-2.0@40813 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-rice-sql/src/main/resources/upgrades/10-R1.3/2012-08-30-1-kim-upgrade.sql
+++ ks-rice-sql/src/main/resources/upgrades/10-R1.3/2012-08-30-1-kim-upgrade.sql
@@ -313,7 +313,7 @@ insert into KRIM_ROLE_T (ROLE_ID, OBJ_ID, VER_NBR, ROLE_NM, NMSPC_CD, DESC_TXT, 
 values (krim_role_id_s.nextval, '35FDD79F13F94FFABC40A7EBA98846B2', 1, 'Student System User Role', 'KS-SYS', 'This role provides system user privileges in KS CM Application', '1', 'Y', to_date('09-05-2012 16:38:54', 'dd-mm-yyyy hh24:mi:ss'))
 /
 insert into KRIM_ROLE_MBR_T (ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD, ACTV_FRM_DT, ACTV_TO_DT, LAST_UPDT_DT)
-values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, '2C94749F29164D08B765325A1BD8D013', krim_role_mbr_id_s.currval, '5', 'P', null, null, to_date('09-05-2012 16:38:54', 'dd-mm-yyyy hh24:mi:ss'))
+values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, '2C94749F29164D08B765325A1BD8D013', krim_role_id_s.currval, '5', 'P', null, null, to_date('09-05-2012 16:38:54', 'dd-mm-yyyy hh24:mi:ss'))
 /
 --Administer Routing for Document 10056
 insert into KRIM_ROLE_PERM_T (ROLE_PERM_ID, OBJ_ID, VER_NBR, ROLE_ID, PERM_ID, ACTV_IND)
