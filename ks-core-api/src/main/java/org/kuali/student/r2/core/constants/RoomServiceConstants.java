be87d6b129808285d1412ce181ba782585edcafd
cclin <cclin@kuali.org>
2012-12-11 15:20:39 -0500

    KSENROLL-4281 Adding constants to RoomService.  Also, types to database KSEN_TYPE table.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/M6@40955 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-api/src/main/java/org/kuali/student/r2/core/constants/RoomServiceConstants.java
+++ ks-core-api/src/main/java/org/kuali/student/r2/core/constants/RoomServiceConstants.java
@@ -38,4 +38,59 @@ public class RoomServiceConstants {
     public static final String REF_OBJECT_URI_ROOM = NAMESPACE + "/" + RoomInfo.class.getSimpleName();
     public static final String REF_OBJECT_URI_BUILDING = NAMESPACE + "/" + BuildingInfo.class.getSimpleName();
     public static final String REF_OBJECT_URI_ROOM_RESPONSIBLE_ORG = NAMESPACE + "/" + RoomResponsibleOrg.class.getSimpleName();
+
+    // *-*-*-*-*-*-*-*-*-* Building *-*-*-*-*-*-*-*-*-*
+    /**
+     * Building States (KSENROLL-4281)
+     */
+    public static final String ROOM_BUILDING_ACTIVE_STATE_KEY = "kuali.room.building.state.active";
+    public static final String ROOM_BUILDING_INACTIVE_STATE_KEY = "kuali.room.building.state.inactive";
+
+    /**
+     * Building lifecycle (KSENROLL-4281)
+     */
+    public static final String ROOM_BUILDING_LIFECYCLE_KEY = "kuali.room.building.lifecycle";
+
+    // *-*-*-*-*-*-*-*-*-* Room *-*-*-*-*-*-*-*-*-*
+    /**
+     * Room types (KSENROLL-4281)
+     */
+    public static final String ROOM_TYPE_CLASSROOM_KEY = "kuali.room.type.classroom";
+    public static final String ROOM_TYPE_GENERAL_CLASSROOM_KEY = "kuali.room.type.classroom.general";
+
+    /**
+     * Room States (KSENROLL-4281)
+     */
+    public static final String ROOM_ROOM_ACTIVE_STATE_KEY = "kuali.room.room.state.active";
+    public static final String ROOM_ROOM_INACTIVE_STATE_KEY = "kuali.room.room.state.inactive";
+
+    /**
+     * Room lifecycle (KSENROLL-4281)
+     */
+    public static final String ROOM_ROOM_LIFECYCLE_KEY = "kuali.room.room.lifecycle";
+
+    // *-*-*-*-*-*-*-*-*-* Org Room Responsibility *-*-*-*-*-*-*-*-*-*
+    /**
+     * Org Room Responsibility types (KSENROLL-4281)
+     */
+    public static final String ORG_ROOM_RESPONSIBILITY_TYPE_SCHEDULING_KEY = "kuali.room.org.responsibility.scheduling";
+
+    // *-*-*-*-*-*-*-*-*-* Facility *-*-*-*-*-*-*-*-*-*
+    /**
+     * Facility types (KSENROLL-4281)
+     */
+    public static final String FACILITY_TYPE_BUILDING_KEY = "kuali.facility.type.building";
+    public static final String FACILITY_TYPE_REGION_KEY = "kuali.facility.type.region";
+    public static final String FACILITY_TYPE_PARTITION_KEY = "kuali.facility.type.partition";
+
+    /**
+     * Facility States (KSENROLL-4281)
+     */
+    public static final String ROOM_FACILITY_ONLINE_STATE_KEY = "kuali.room.facility.state.online";
+    public static final String ROOM_FACILITY_OFFLINE_STATE_KEY = "kuali.room.facility.state.offline";
+
+    /**
+     * Facility lifecycle (KSENROLL-4281)
+     */
+    public static final String ROOM_FACILITY_LIFECYCLE_KEY = "kuali.room.facility.lifecycle";
 }
