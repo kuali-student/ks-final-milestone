001395e091a0858fe11962ba9ae3dcfca5cbfc0a
brandon.gresham <brandon.gresham@kuali.org>
2012-12-17 18:25:25 -0500

    https://jira.kuali.org/browse/KSENROLL-4358 Added SQL-script to insert 'kuali.state.propagation.type.delete' into KSEN_TYPE table.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@41262 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/upgrades/17/2012-12-17-KSEN_TYPE-add-state-propagation-type-delete.sql
@@ -0,0 +1,3 @@
+-- State propagation type 'DELETE'
+insert into KSEN_TYPE ( TYPE_KEY, OBJ_ID, NAME, DESCR_PLAIN, DESCR_FORMATTED, EFF_DT, EXPIR_DT, REF_OBJECT_URI, SERVICE_URI, VER_NBR, CREATETIME, CREATEID, UPDATETIME, UPDATEID ) values ( 'kuali.state.propagation.type.delete', null, 'Delete State Propagation Type', 'Delete State Propagation Type', 'Delete State Propagation Type', null, null, 'http://student.kuali.org/wsdl/state/StatePropagationInfo', 'http://student.kuali.org/wsdl/state/StateService', 0, TIMESTAMP '2012-12-17 00:00:00', 'SYSTEMLOADER', null, null )
+/
