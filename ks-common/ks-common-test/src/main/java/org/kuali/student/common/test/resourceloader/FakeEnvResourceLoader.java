544add7527102619f97e68f7545cb78a94db44b6
haroon.rafique <haroon.rafique@kuali.org>
2012-12-17 22:24:42 -0500

    KSENROLL-4373: fixed failing tests in ks-enroll-impl after rice-2.2.0 upgrade * removed MockGlobalResourceLoader and "shadow" GlobalResourceLoader * setup FakeEnvResourceLoader with a spring based service locator to find services * got rid of any straggling bean references for systemPropertyConfigurer, globalResourceLoader   and mockGlobalResourceLoader
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@41269 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-test/src/main/java/org/kuali/student/common/test/resourceloader/FakeEnvResourceLoader.java
+++ ks-common/ks-common-test/src/main/java/org/kuali/student/common/test/resourceloader/FakeEnvResourceLoader.java
@@ -19,24 +19,19 @@ package org.kuali.student.common.test.resourceloader;
 import org.kuali.rice.core.api.CoreConstants;
 import org.kuali.rice.core.api.config.property.Config;
 import org.kuali.rice.core.api.config.property.ConfigContext;
-import org.kuali.rice.core.api.config.property.ConfigurationService;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 import org.kuali.rice.core.api.resourceloader.ResourceLoader;
 import org.kuali.rice.core.api.util.RiceConstants;
 import org.kuali.rice.core.framework.resourceloader.BaseResourceLoader;
-import org.kuali.rice.core.framework.resourceloader.SimpleServiceLocator;
 import org.kuali.rice.core.impl.config.property.JAXBConfigImpl;
-import org.kuali.rice.krad.service.KRADServiceLocator;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
+import org.kuali.rice.krad.datadictionary.DictionaryBeanFactoryPostProcessor;
 
 import javax.xml.namespace.QName;
-import java.util.Map;
 
 /**
  * This class sets up the {@link GlobalResourceLoader} for a fake environment (suitable for running in tests).
  * Mostly necessitated due to bean post processing addition in Rice 2.2.0 final causing NPE in
- * {@link DictionaryBeanFactoryPostProcesser#postProcessBeanFactory() postProcessBeanFactory}.
+ * {@link DictionaryBeanFactoryPostProcessor#postProcessBeanFactory() postProcessBeanFactory}.
  *
  * @author Kuali Student Team
  */
@@ -47,19 +42,10 @@ public class FakeEnvResourceLoader {
         Config config = new JAXBConfigImpl();
         config.putProperty(CoreConstants.Config.APPLICATION_ID, MOCK_APP_ID);
         ConfigContext.init(config);
-        SimpleServiceLocator serviceLocator = new SimpleServiceLocator();
-
-        ConfigurationService configurationService = new ConfigurationService() {
-            @Override public String getPropertyValueAsString(String key) { return "{0} message"; }
-            @Override public boolean getPropertyValueAsBoolean(String key) { return false; }
-            @Override public Map<String, String> getAllProperties() { return null; }
-        };
-
-        serviceLocator.addService(new QName(KRADServiceLocator.KUALI_CONFIGURATION_SERVICE), configurationService);
 
         ResourceLoader resourceLoader =
                 new BaseResourceLoader(
-                        new QName(MOCK_APP_ID, RiceConstants.DEFAULT_ROOT_RESOURCE_LOADER_NAME), serviceLocator);
+                        new QName(MOCK_APP_ID, RiceConstants.DEFAULT_ROOT_RESOURCE_LOADER_NAME), new SimpleSpringResourceLoader());
 
         try {
             GlobalResourceLoader.stop();
