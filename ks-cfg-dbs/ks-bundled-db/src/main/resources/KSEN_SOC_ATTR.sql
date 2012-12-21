d8512261ff86d8b5cf499d2f63b51fdb000498d9
gspinks <gspinks@kuali.org>
2012-12-21 12:55:36 -0500

    KSENROLL-4362 exported SQL of loaded data
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-deployments/branches/umd-enr-data@41401 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-cfg-dbs/ks-bundled-db/src/main/resources/KSEN_SOC_ATTR.sql
@@ -0,0 +1,16 @@
+insert into KSEN_SOC_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('e4efa89a-3d60-4966-96e8-9f5dd57c0a63', '572c15d7-4f05-43aa-a6c6-56c8d1c65ea9', 'kuali.soc.state.published', '2012-12-20T16:25:23-0500', '9f34b706-6848-48db-a20d-827bdbb74515')
+/
+insert into KSEN_SOC_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('52d53e79-20e1-4986-98ce-f149e6ccd80d', '8dce7d78-34f9-4852-9c24-c5b3f4ce2504', 'kuali.soc.state.published', '2012-12-20T16:25:23-0500', '25abfcfd-8afe-42b4-bf3a-265d81cf7fef')
+/
+insert into KSEN_SOC_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('01f90248-544b-4653-92be-f044f271a190', '89e2ac87-160f-4fa7-be63-4a777305f502', 'kuali.soc.state.published', '2012-12-20T16:25:23-0500', '25191d2f-436b-4975-b935-53be264194b7')
+/
+insert into KSEN_SOC_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('a68430e8-8abd-4ce3-ab28-709e936036a1', 'c0299568-b8d8-493b-ac82-844e4ddff88d', 'kuali.soc.state.published', '2012-12-20T16:25:23-0500', '0716ffd9-93c4-4ae3-90cd-97f5a5780789')
+/
+insert into KSEN_SOC_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('d1951da9-0d0b-4fdf-9fd9-4be866d6d486', '51f370b1-4cbd-4a90-b73f-7f975d7cd735', 'kuali.soc.state.published', '2012-12-20T16:25:23-0500', 'f41c9f5b-9634-40ef-b377-5575c11809cc')
+/
+insert into KSEN_SOC_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('b18b8aa6-bcbb-43d1-811f-9143c332980a', 'c350402e-5507-4a88-876b-a89050a447db', 'kuali.soc.state.published', '2012-12-20T16:25:23-0500', '07aa84fa-6fab-4177-a5e8-22073888b4df')
+/
+insert into KSEN_SOC_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('59a9873f-a014-4987-b817-52ffe9d20b54', '87ded224-831a-46e1-b8cf-e3998f5e79ba', 'kuali.soc.state.published', '2012-12-20T16:25:23-0500', 'e4161388-74c3-4eec-b639-e88fed0cc213')
+/
+insert into KSEN_SOC_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('aee4ffe3-b698-4151-9e1e-b149c0e7867b', '65e3e7d1-81aa-42d2-8b02-e399e28bdb24', 'kuali.soc.state.published', '2012-12-20T16:25:23-0500', 'dffe7401-e845-4eb2-b4e8-862f4094a46a')
+/
