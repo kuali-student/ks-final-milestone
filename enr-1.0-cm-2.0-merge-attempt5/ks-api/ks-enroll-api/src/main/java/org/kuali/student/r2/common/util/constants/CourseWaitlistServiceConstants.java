a3b6e0da571c9e5114d8a60a9ceb2598d1c4813f
lsymms <lsymms@kuali.org>
2013-01-04 14:49:06 -0500

    KSENROLL-4579 creating merge branches in sandbox
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/merges@41598 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- enr-1.0-cm-2.0-merge-attempt5/ks-api/ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/CourseWaitlistServiceConstants.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.student.r2.common.util.constants;
-
-import org.kuali.student.r2.common.constants.CommonServiceConstants;
-
-public class CourseWaitlistServiceConstants {
-    
-    public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "courseWaitlistService";
-
-}
