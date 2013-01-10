e63100b41922cf8b751cf194fffd9cd18a4a02a8
dedewet <dedewet@kuali.org>
2013-01-10 06:48:10 -0500

    KSENROLL-4637 - Agenda and Rule droplist changed to use TypeTypeRelationService
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@41801 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/krms/keyvalues/RequisiteAgendaTypeKeyValues.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/krms/keyvalues/RequisiteAgendaTypeKeyValues.java
@@ -2,82 +2,38 @@ package org.kuali.student.enrollment.class1.krms.keyvalues;
 
 import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
-import org.kuali.rice.krad.keyvalues.KeyValuesBase;
+import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
+import org.kuali.rice.krad.uif.view.ViewModel;
+import org.kuali.rice.krms.api.repository.type.KrmsTypeDefinition;
+import org.kuali.rice.krms.impl.repository.KrmsRepositoryServiceLocator;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: 21310823
- * Date: 7/19/12
- * Time: 5:12 PM
- * To change this template use File | Settings | File Templates.
- */
-public class RequisiteAgendaTypeKeyValues extends KeyValuesBase {
+public class RequisiteAgendaTypeKeyValues extends UifKeyValuesFinderBase implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Override
-    public List<KeyValue> getKeyValues() {
-        List<KeyValue> labels = new ArrayList<KeyValue>();
-        labels.add(new ConcreteKeyValue("0", "Select One"));
-        labels.add(new ConcreteKeyValue("1", "Enrollment Eligibility"));
-        labels.add(new ConcreteKeyValue("2", "Credit Constraints"));
-        return labels;
+    public List<KeyValue> getKeyValues(ViewModel model) {
+        List<KeyValue> keyValues = new ArrayList<KeyValue>();
+
+        //Use KRMS Type Repository Service to get Agenda Types
+        try {
+            List<KrmsTypeDefinition> krmsTypeDefinitionList = KrmsRepositoryServiceLocator.getKrmsTypeRepositoryService().findAllTypes();
+
+            for (KrmsTypeDefinition krmsTypeDefinition : krmsTypeDefinitionList) {
+                if(krmsTypeDefinition.getServiceName().equals("agendaTypeService")) {
+                    if(!krmsTypeDefinition.getId().equals("10000")) {
+                        ConcreteKeyValue keyValue = new ConcreteKeyValue();
+                        keyValue.setKey(krmsTypeDefinition.getId());
+                        keyValue.setValue(krmsTypeDefinition.getName());
+                        keyValues.add(keyValue);
+                    }
+                }
+            }
+        } catch (Exception ex) {}
+        return keyValues;
     }
-
-//    @Override
-//    public List<KeyValue> getKeyValues(ViewModel model) {
-//        List<KeyValue> keyValues = new ArrayList<KeyValue>();
-//
-//         //Use Type Type Relation to get Agenda Types
-//
-//        //KrmsRepositoryServiceLocator.getKrmsTypeRepositoryService().getTypeByName("kuali.statement.type.course.");
-//
-//
-//
-//        try {
-//
-//
-//
-//
-//            //kuali.statement.type.course.enrollmentEligibility
-//            //kuali.statement.type.course.creditConstraints
-//            List<TypeInfo> types = getTypeService().getTypesByRefObjectUri(RequisiteServiceConstants.REF_OBJECT_TYPE_URI_ISSUE, TestHelper.getContext1());
-//            for (TypeInfo type : types) {
-//                    ConcreteKeyValue keyValue = new ConcreteKeyValue();
-//                    keyValue.setKey(type.getKey());
-//                    keyValue.setValue(type.getName());
-//                    keyValues.add(keyValue);
-//                }
-//            }
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//        return keyValues;
-//    }
-//
-//    public KeyValue getTypeKeyValue(String typeKey) {
-//        ConcreteKeyValue keyValue = new ConcreteKeyValue();
-//
-//        ContextInfo context = TestHelper.getContext1();
-//        //No data exists for Agenda Type
-//        try {
-//            List<TypeInfo> types = getTypeService().getTypesByRefObjectUri(RequisiteServiceConstants.REF_OBJECT_TYPE_URI_ISSUE, context);
-//            for (TypeInfo type : types) {
-//                if(type.getKey().equals(typeKey)) { //TODO remove check after data is fixed
-//                    keyValue.setKey(type.getKey());
-//                    keyValue.setValue(type.getName());
-//                }
-//            }
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//        return keyValue;
-//    }
-//
-
-
-
 }
