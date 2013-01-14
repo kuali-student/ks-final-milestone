acd412e8b8e7d8f60046dec80a8031bb4f4a718c
dedewet <dedewet@kuali.org>
2013-01-14 09:52:33 -0500

    KSENROLL-4637 - Fixed droplists to use natural language
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@41918 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/krms/keyvalues/RuleTypeKeyValues.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/krms/keyvalues/RuleTypeKeyValues.java
@@ -5,11 +5,19 @@ import org.kuali.rice.core.api.util.KeyValue;
 import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
 import org.kuali.rice.krad.uif.view.ViewModel;
 import org.kuali.rice.krad.web.form.MaintenanceDocumentForm;
-import org.kuali.rice.krms.api.repository.type.KrmsTypeDefinition;
+import org.kuali.rice.krms.api.repository.language.NaturalLanguageTemplate;
+import org.kuali.rice.krms.api.repository.language.NaturalLanguageUsage;
+import org.kuali.rice.krms.api.repository.type.KrmsTypeRepositoryService;
 import org.kuali.rice.krms.api.repository.typerelation.TypeTypeRelation;
 import org.kuali.rice.krms.impl.repository.KrmsRepositoryServiceLocator;
+import org.kuali.rice.krms.impl.repository.NaturalLanguageTemplateBoService;
+import org.kuali.rice.krms.impl.repository.NaturalLanguageUsageBoService;
 import org.kuali.rice.krms.impl.repository.TypeTypeRelationBoService;
+import org.kuali.rice.krms.impl.util.KrmsImplConstants;
 import org.kuali.student.enrollment.class1.krms.dto.RuleEditor;
+import org.kuali.student.enrollment.class1.krms.util.KsKrmsConstants;
+import org.kuali.student.enrollment.class1.krms.util.KsKrmsRepositoryServiceLocator;
+import org.kuali.student.enrollment.class2.courseoffering.service.decorators.PermissionServiceConstants;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -27,23 +35,45 @@ public class RuleTypeKeyValues extends UifKeyValuesFinderBase implements Seriali
             agendaTypeId = ((RuleEditor)MaintenanceDocumentForm.getDocument().getNewMaintainableObject().getDataObject()).getAgendaType();
         }
         List<KeyValue> keyValues = new ArrayList<KeyValue>();
+        NaturalLanguageUsage usage = getNaturalLanguageUsageBoService().getNaturalLanguageUsageByName(PermissionServiceConstants.KS_SYS_NAMESPACE, KsKrmsConstants.KRMS_NL_TYPE_DESCRIPTION);
 
          //Use Type Type Relation to get Rule Types
         try {
             List<TypeTypeRelation> typeTypeRelationList = getTypeTypeRelationBoService().findTypeTypeRelationsByFromType(agendaTypeId);
 
             for (TypeTypeRelation typeTypeRelation : typeTypeRelationList) {
-                KrmsTypeDefinition krmsTypeDefinition = KrmsRepositoryServiceLocator.getKrmsTypeRepositoryService().getTypeById(typeTypeRelation.getToTypeId());
-                ConcreteKeyValue keyValue = new ConcreteKeyValue();
-                keyValue.setKey(krmsTypeDefinition.getId());
-                keyValue.setValue(krmsTypeDefinition.getName());
-                keyValues.add(keyValue);
+                NaturalLanguageTemplate template = null;
+                try{
+                    template = getNaturalLanguageTemplateBoService().findNaturalLanguageTemplateByLanguageCodeTypeIdAndNluId("en", typeTypeRelation.getToTypeId(), usage.getId());
+                }catch (IndexOutOfBoundsException e){
+                    //Ignore, rice error in NaturalLanguageTemplateBoServiceImpl line l
+                }
+
+                if (template != null){
+                    //Use the template in the dropdown
+                    keyValues.add(new ConcreteKeyValue(typeTypeRelation.getToTypeId(), template.getTemplate()));
+                } else {
+                    //If no template exist, display the type name
+                    keyValues.add(new ConcreteKeyValue(typeTypeRelation.getToTypeId(), getKrmsTypeRepositoryService().getTypeById(typeTypeRelation.getToTypeId()).getName()));
+                }
             }
         } catch (Exception ex) {}
         return keyValues;
     }
 
-    public TypeTypeRelationBoService getTypeTypeRelationBoService() {
+    private KrmsTypeRepositoryService getKrmsTypeRepositoryService() {
+        return KrmsRepositoryServiceLocator.getKrmsTypeRepositoryService();
+    }
+
+    private TypeTypeRelationBoService getTypeTypeRelationBoService() {
         return KrmsRepositoryServiceLocator.getTypeTypeRelationBoService();
+    }
+
+    private NaturalLanguageUsageBoService getNaturalLanguageUsageBoService() {
+        return KsKrmsRepositoryServiceLocator.getNaturalLanguageUsageBoService();
+    }
+
+    private NaturalLanguageTemplateBoService getNaturalLanguageTemplateBoService() {
+        return KsKrmsRepositoryServiceLocator.getNaturalLanguageTemplateBoService();
     }
 }
