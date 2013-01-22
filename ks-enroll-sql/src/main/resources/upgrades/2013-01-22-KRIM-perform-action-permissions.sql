8454478feb6e3beadcdd93f69ad3ba16e54f9ca7
vgadiyak <vgadiyak@kuali.org>
2013-01-22 15:36:08 -0500

    KSENROLL-4438 KSENROLL-4437 KSENROLL-4411 Added permissions for Admin and Dept Coordinator.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@42295 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/upgrades/2013-01-22-KRIM-perform-action-permissions.sql
+++ ks-enroll-sql/src/main/resources/upgrades/2013-01-22-KRIM-perform-action-permissions.sql
@@ -16,5 +16,5 @@ insert into KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, PERM_ID, KIM_T
 /
 insert into KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, PERM_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) values (KRIM_ATTR_DATA_ID_S.NEXTVAL, SYS_GUID(), 1, (SELECT perm_id from krim_perm_t where nm = 'Perform Action for Course Offering for Admin' and nmspc_cd = 'KS-ENR'), (SELECT kim_typ_id from krim_typ_t where nm = 'Default' and nmspc_cd = 'KUALI'), (SELECT MIN(TO_NUMBER(kim_attr_defn_id)) from krim_attr_defn_t where nm = 'actionEvent' and nmspc_cd = 'KR-KRAD'), 'delete')
 /
-insert into KRIM_ROLE_PERM_T (ROLE_PERM_ID, OBJ_ID, VER_NBR, ROLE_ID, PERM_ID, ACTV_IND) values (KRIM_ROLE_PERM_ID_S.NEXTVAL, SYS_GUID(), 1, (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'Kuali Student Admin' and nmspc_cd = 'KS-SYS'), (SELECT perm_id from krim_perm_t where nm = 'Perform Action for Course Offering for Admin' and nmspc_cd = 'KS-ENR'), 'Y')
+insert into KRIM_ROLE_PERM_T (ROLE_PERM_ID, OBJ_ID, VER_NBR, ROLE_ID, PERM_ID, ACTV_IND) values (KRIM_ROLE_PERM_ID_S.NEXTVAL, SYS_GUID(), 1, (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'KS Schedule Coordinator' and nmspc_cd = 'KS-ENR'), (SELECT perm_id from krim_perm_t where nm = 'Perform Action for Course Offering for Admin' and nmspc_cd = 'KS-ENR'), 'Y')
 /
