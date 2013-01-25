3317dbe5204f5200175f8a957dd9b93f0d7f973f
brandon.gresham <brandon.gresham@kuali.org>
2013-01-25 10:59:43 -0500

    https://jira.kuali.org/browse/KSENROLL-4978 Added lui-set colo-offering STATES to constants file and SQL.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@42454 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/upgrades/M6/2013-01-25-KSEN-Add-luiset-colocated-states.sql
@@ -0,0 +1,6 @@
+INSERT INTO ksen_type(TYPE_KEY, NAME, DESCR_PLAIN, DESCR_FORMATTED, REF_OBJECT_URI, SERVICE_URI, VER_NBR, CREATETIME, CREATEID)
+  VALUES( 'kuali.luiset.state.active', 'Active', 'The identifier is active', 'The identifier is active', 'http://student.kuali.org/wsdl/lui.LuiInfo', 'http://student.kuali.org/wsdl/lui/LuiService', 0, '25-JAN-13 08.39.00 AM', 01 )
+/
+INSERT INTO ksen_type(TYPE_KEY, NAME, DESCR_PLAIN, DESCR_FORMATTED, REF_OBJECT_URI, SERVICE_URI, VER_NBR, CREATETIME, CREATEID)
+  VALUES( 'kuali.luiset.state.inactive', 'Inactive', 'The identifier is inactive', 'The identifier is inactive', 'http://student.kuali.org/wsdl/lui.LuiInfo', 'http://student.kuali.org/wsdl/lui/LuiService', 0, '25-JAN-13 08.39.00 AM', 01 )
+/
