5e0edd6980e15d3a167e235e0701d3b2c6ce379a
dosoar <dosoar@kuali.org>
2013-01-29 18:30:47 -0500

    KSAP-12 Made active: a Mock-up working copy of the Degree Audit web page
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/KSAP@42688 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-api/src/main/java/org/kuali/student/myplan/audit/service/DegreeAuditServiceConstants.java
+++ ks-ap-api/src/main/java/org/kuali/student/myplan/audit/service/DegreeAuditServiceConstants.java
@@ -13,8 +13,22 @@ public class DegreeAuditServiceConstants {
     public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "da";
     public static final String SERVICE_NAME = "DegreeAuditService";
 
+
     public static final String AUDIT_TYPE_KEY_DEFAULT = "kauli.audit.type.default";
     public static final String AUDIT_TYPE_KEY_HTML = "kauli.audit.type.html";
     public static final String AUDIT_TYPE_KEY_PRINT = "kauli.audit.type.print";
     public static final String AUDIT_TYPE_KEY_SUMMARY = "kauli.audit.type.summary";
+    public static final String DEGREE_AUDIT_EN_US_LOCALE = "en_US";
+    public static final String DEGREE_AUDIT_MESSAGE_ID_PREFIX_KEY = "kuali.org.Message.Degree.Audit.Message.ID.";
+    public static final String DEGREE_AUDIT_HOME_PAGE_GROUP_NAME = "kuali.org.Message.Degree.Audit.Group.Name.Home.Page";
+    public static final String DEGREE_AUDIT_HOME_PAGE_MESSAGE_SELECT_A_PROGRAM_FROM = "selectAProgramFrom";
+    public static final String DEGREE_AUDIT_HOME_PAGE_MESSAGE_HOW_TO_USE_DEGREE_AUDIT = "howToUseDegreeAudit";
+    public static final String DEGREE_AUDIT_HOME_PAGE_MESSAGE_THE_AUDIT_FEATURE_DISABLED = "theAuditFeatureDisabled";
+    public static final String DEGREE_AUDIT_HOME_PAGE_MESSAGE_TRACK_YOUR_PROGRESS = "trackYourProgress";
+    public static final String DEGREE_AUDIT_HOME_PAGE_MESSAGE_STAY_ON_TOP_OF_YOUR_REQUIRED = "stayOnTopOfUrRequired";
+    public static final String DEGREE_AUDIT_HOME_PAGE_MESSAGE_SELECT_YOUR_PROGRAM = "selectYourProgram";
+    public static final String DEGREE_AUDIT_HOME_PAGE_MESSAGE_EXPLORE_PROGRAMS_AND_MINORS = "exploreProgramsAndMinors";
+    public static final String DEGREE_AUDIT_HOME_PAGE_MESSAGE_STILL_DECIDING = "stillDeciding";
+    public static final String DEGREE_AUDIT_HOME_PAGE_MESSAGE_SELECT_YOUR_CAMPUS = "selectYourCampus";
+    public static final String DEGREE_AUDIT_HOME_PAGE_MESSAGE_SOME_PROGRAMS_NOT_INCLUDED = "someProgramsNotIncluded";
 }
