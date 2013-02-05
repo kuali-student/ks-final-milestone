7af17bf1534328e5229af827db11145307396812
miki.harmath <miki.harmath@kuali.org>
2013-02-05 11:44:21 -0500

    KSENROLL-5052 added isAuthorizedByTemplate, canEditView, changed canOpenView < REVERTED (3:02:16 PM, Monday, February 04, 2013)
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@42998 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/main/view/KsMaintenanceViewAuthorizerBase.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/main/view/KsMaintenanceViewAuthorizerBase.java
@@ -1,6 +1,5 @@
 package org.kuali.student.enrollment.main.view;
 
-import org.apache.commons.lang.StringUtils;
 import org.kuali.rice.kim.api.KimConstants;
 import org.kuali.rice.kim.api.identity.Person;
 import org.kuali.rice.krad.datadictionary.DocumentEntry;
@@ -9,32 +8,18 @@ import org.kuali.rice.krad.maintenance.MaintenanceDocument;
 import org.kuali.rice.krad.maintenance.MaintenanceDocumentBase;
 import org.kuali.rice.krad.maintenance.MaintenanceViewAuthorizerBase;
 import org.kuali.rice.krad.service.KRADServiceLocatorWeb;
-import org.kuali.rice.krad.uif.UifParameters;
-import org.kuali.rice.krad.uif.component.Component;
-import org.kuali.rice.krad.uif.component.ComponentSecurity;
-import org.kuali.rice.krad.uif.container.Group;
-import org.kuali.rice.krad.uif.element.Action;
-import org.kuali.rice.krad.uif.field.Field;
-import org.kuali.rice.krad.uif.util.ObjectPropertyUtils;
 import org.kuali.rice.krad.uif.view.View;
 import org.kuali.rice.krad.uif.view.DocumentView;
 import org.kuali.rice.krad.uif.view.ViewModel;
-import org.kuali.rice.krad.uif.widget.Widget;
 import org.kuali.rice.krad.util.KRADConstants;
 import org.kuali.rice.krad.util.KRADUtils;
 import org.kuali.rice.krad.web.form.DocumentFormBase;
 import org.kuali.student.enrollment.class2.courseoffering.dto.ActivityOfferingWrapper;
 import org.kuali.student.enrollment.class2.courseoffering.dto.CourseOfferingEditWrapper;
-import org.kuali.student.enrollment.class2.courseoffering.dto.CourseOfferingListSectionWrapper;
-import org.kuali.student.enrollment.class2.courseoffering.form.ActivityOfferingForm;
-import org.kuali.student.enrollment.class2.courseoffering.form.CourseOfferingManagementForm;
 import org.kuali.student.enrollment.class2.courseoffering.service.impl.ActivityOfferingMaintainableImpl;
 import org.kuali.student.enrollment.class2.courseoffering.service.impl.CourseOfferingEditMaintainableImpl;
 
-import java.util.Collection;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -70,131 +55,19 @@ public class KsMaintenanceViewAuthorizerBase extends MaintenanceViewAuthorizerBa
     }
 
     @Override
-    protected boolean isAuthorizedByTemplate(View view, Component component, ViewModel model,
-                                             String permissionTemplateName, Person user, Map<String, String> additionalPermissionDetails,
-                                             Map<String, String> additionalRoleQualifications, boolean checkPermissionExistence) {
-        if(additionalRoleQualifications == null){
-            //Instantiate if null was passed in
-            additionalRoleQualifications = new HashMap<String, String>();
-        }
-        addRoleQualification(model, additionalRoleQualifications);
-
-        Map<String, String> permissionDetails = new HashMap<String, String>();
-        Map<String, String> roleQualifications = new HashMap<String, String>();
-
-        if (additionalPermissionDetails != null) {
-            permissionDetails.putAll(additionalPermissionDetails);
-        }
-
-        if (additionalRoleQualifications != null) {
-            roleQualifications.putAll(additionalRoleQualifications);
-        }
-
-        Object dataObjectForContext = getDataObjectContext(view, model);
-
-        // add permission details depending on the type of component
-        if (component instanceof Field) {
-            permissionDetails.putAll(getFieldPermissionDetails(view, dataObjectForContext, (Field) component));
-        } else if (component instanceof Group) {
-            permissionDetails.putAll(getGroupPermissionDetails(view, dataObjectForContext, (Group) component));
-        } else if (component instanceof Widget) {
-            permissionDetails.putAll(getWidgetPermissionDetails(view, dataObjectForContext, (Widget) component));
-        } else if (component instanceof Action) {
-            permissionDetails.putAll(getActionPermissionDetails(view, dataObjectForContext, (Action) component));
-        }
-
-        // pick up additional attributes and overrides from component security
-        ComponentSecurity componentSecurity = component.getComponentSecurity();
-
-        // add configured overrides
-        if (StringUtils.isNotBlank(componentSecurity.getNamespaceAttribute())) {
-            permissionDetails.put(KimConstants.AttributeConstants.NAMESPACE_CODE,
-                    componentSecurity.getNamespaceAttribute());
-        }
-        if (StringUtils.isNotBlank(componentSecurity.getComponentAttribute())) {
-            permissionDetails.put(KimConstants.AttributeConstants.COMPONENT_NAME,
-                    componentSecurity.getComponentAttribute());
-        }
-        if (StringUtils.isNotBlank(componentSecurity.getIdAttribute())) {
-            if (component instanceof Field) {
-                permissionDetails.put(KimConstants.AttributeConstants.FIELD_ID, componentSecurity.getIdAttribute());
-            } else if (component instanceof Group) {
-                permissionDetails.put(KimConstants.AttributeConstants.GROUP_ID, componentSecurity.getIdAttribute());
-            } else if (component instanceof Widget) {
-                permissionDetails.put(KimConstants.AttributeConstants.WIDGET_ID, componentSecurity.getIdAttribute());
-            }
-        }
-
-        if (componentSecurity.getAdditionalPermissionDetails() != null) {
-            permissionDetails.putAll(componentSecurity.getAdditionalPermissionDetails());
-        }
-
-        if (componentSecurity.getAdditionalRoleQualifiers() != null) {
-            roleQualifications.putAll(componentSecurity.getAdditionalRoleQualifiers());
-        }
-
-        // Permission for Activity Offering form
-        if (component instanceof Field) {
-            if (dataObjectForContext instanceof ActivityOfferingWrapper) {
-                ActivityOfferingWrapper theForm = (ActivityOfferingWrapper) dataObjectForContext;
-                Date termRegStartDate = theForm.getTermRegStartDate();
-                if (termRegStartDate != null) {
-                    Date now = new Date();
-                    if (now.before(termRegStartDate)) {
-                        permissionDetails.put("termRegStartDateLater", "true");
-                    }
-                }
-            }
-        }
-
-        boolean result = true;
-        if (!checkPermissionExistence || (checkPermissionExistence && permissionExistsByTemplate(dataObjectForContext,
-                "KS-ENR", permissionTemplateName, permissionDetails))) {
-            result = isAuthorizedByTemplate(dataObjectForContext, "KS-ENR", permissionTemplateName,
-                    user.getPrincipalId(), permissionDetails, roleQualifications);
-
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Performed permission check for: " + permissionTemplateName + " and got result: " + result);
-            }
-        }
-
-        return result;
-    }
-
-    @Override
     public boolean canOpenView(View view, ViewModel model, Person user) {
         Map<String, String> additionalPermissionDetails = new HashMap<String, String>();
         additionalPermissionDetails.put(KimConstants.AttributeConstants.NAMESPACE_CODE, view.getNamespaceCode());
         additionalPermissionDetails.put(KimConstants.AttributeConstants.VIEW_ID, model.getViewId());
 
-        if (permissionExistsByTemplate(model, "KS-ENR",
+        if (permissionExistsByTemplate(model, KRADConstants.KRAD_NAMESPACE,
                 KimConstants.PermissionTemplateNames.OPEN_VIEW, additionalPermissionDetails)) {
-            return isAuthorizedByTemplate(model, "KS-ENR",
+            return isAuthorizedByTemplate(model, KRADConstants.KRAD_NAMESPACE,
                     KimConstants.PermissionTemplateNames.OPEN_VIEW, user.getPrincipalId(), additionalPermissionDetails,
                     null);
         }
 
         return true;
     }
-
-    @Override
-    public boolean canEditView(View view, ViewModel model, Person user) {
-        DocumentFormBase documentForm = (DocumentFormBase) model;
-
-        Map<String, String> additionalPermissionDetails = new HashMap<String, String>();
-        additionalPermissionDetails.put(KimConstants.AttributeConstants.NAMESPACE_CODE, view.getNamespaceCode());
-        additionalPermissionDetails.put(KimConstants.AttributeConstants.VIEW_ID, model.getViewId());
-
-        if (permissionExistsByTemplate(model, "KS-ENR",
-                KimConstants.PermissionTemplateNames.EDIT_VIEW, additionalPermissionDetails)) {
-            return isAuthorizedByTemplate(model, "KS-ENR",
-                    KimConstants.PermissionTemplateNames.EDIT_VIEW, user.getPrincipalId(), additionalPermissionDetails,
-                    null) && canEdit(documentForm.getDocument(), user);
-        }
-
-        return super.canEditView(view, model, user) && canEdit(documentForm.getDocument(), user);
-
-    }
-
 
 }
