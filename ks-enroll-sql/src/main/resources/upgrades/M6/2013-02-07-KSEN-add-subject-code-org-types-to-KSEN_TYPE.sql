8fa9239aa1bdd92fb7c490809b58b26cd3c8bd31
cclin <cclin@kuali.org>
2013-02-12 11:48:17 -0500

    KSENROLL-4375. KSENROLL-4979. Add subject code data to Org tables.  Added a comment to the Subject Code org and org-org types.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@43331 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/upgrades/M6/2013-02-07-KSEN-add-subject-code-org-types-to-KSEN_TYPE.sql
+++ ks-enroll-sql/src/main/resources/upgrades/M6/2013-02-07-KSEN-add-subject-code-org-types-to-KSEN_TYPE.sql
@@ -1,3 +1,4 @@
+-- KSENROLL-4979 Convert Subject Code data into Organization and OrgOrgRelation data
 
 INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
   VALUES ('SYSTEMLOADER',TO_DATE( '20130207', 'YYYYMMDD' ),'Subject Code (Organization Type)','Subject Code (Organization Type)','Subject Code','http://student.kuali.org/wsdl/organization/OrgInfo','http://student.kuali.org/wsdl/organization/OrganizationService','kuali.org.SubjectCode',0)
