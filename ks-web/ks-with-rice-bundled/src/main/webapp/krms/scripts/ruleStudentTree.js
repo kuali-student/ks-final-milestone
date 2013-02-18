df39472df1dba264dd972d0b9dad5acc6a501e01
sw <sw@kuali.org>
2013-02-18 09:31:22 -0500

    KSENROLL-5418 - Removed RuleBo, PropositionBo and PropositionParameterBo from KS KRMS UI
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-deployments/trunk@43570 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-web/ks-with-rice-bundled/src/main/webapp/krms/scripts/ruleStudentTree.js
+++ ks-web/ks-with-rice-bundled/src/main/webapp/krms/scripts/ruleStudentTree.js
@@ -293,6 +293,70 @@ function initRuleTree(componentId){
 
 }
 
+function initPreviewTree(componentId){
+
+    // binding to tree loaded event
+    jq('#' + componentId).bind('loaded.jstree', function (event, data) {
+        /* make the tree load with all nodes expanded */
+        jq('#' + componentId).jstree('open_all');
+
+        // set type to 'logic' on logic nodes -- this prevents them from being selected
+        jq('a.compoundOpCodeNode').each( function() {
+            jq('#' + componentId).jstree('set_type', 'logic', this.parentNode);
+        });
+
+        /* update sister compound operators and update proposition summary */
+        jq("[name$='data.proposition.compoundOpCode']").change(function(){
+            var onChangeElementId = this.id;
+
+            jq("select").filter(function() {
+                return this.id.match(
+                    new RegExp(onChangeElementId.replace(/^(\d+_node_)(\d+)(_.*)$/, '^$1\\d+$3$$'))
+                );
+            }).val(jq(this).val());
+
+            ajaxCallPropositionTree('updateCompoundOperator', 'RuleStudentEditorView-Tree');
+        })
+
+    });
+
+
+    /* create the tree */
+    createTree(componentId, {
+        'plugins' : ['themes','html_data', 'ui', 'crrm', 'types' /*, 'dnd' */ ], // disabled drag and drop plugin
+        'ui' : { 'select_limit' : 1 },
+        'themes' : { 'theme':'krms','dots': true ,'icons': false },
+        'crrm' : {
+            /* This is where you can control what is draggable onto what within the tree: */
+            'move' : {
+                /*
+                 * m.o - the node being dragged
+                 * m.r - the target node
+                 */
+                'check_move' : function (m) {
+                    var p = this._get_parent(m.o);
+                    if(!p) return false;
+                    p = p == -1 ? this.get_container() : p;
+
+                    if (m.o.hasClass('logicNode')) return false;
+
+                    if(p === m.np) return true;
+                    if(p[0] && m.np[0] && p[0] === m.np[0]) return true;
+                    return false;
+                }
+            }
+        },
+        'types' : {
+            'types' : {
+                /* nodes set to type 'logic' will not be selectable */
+                'logic' : { 'select_node' : false }
+            }
+        },
+        'dnd' : { 'drag_target' : false, 'drop_target' : false }
+    } );
+
+}
+
 
 
 
