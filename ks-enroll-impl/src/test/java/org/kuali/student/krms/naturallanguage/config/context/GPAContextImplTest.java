2d45a06a0ddce82021178647eda8a298ce048b73
christoff.botha <christoff.botha@kuali.org>
2013-02-21 02:15:45 -0500

    KSENROLL-5422 GPA context impl test
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@43752 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/test/java/org/kuali/student/krms/naturallanguage/config/context/GPAContextImplTest.java
+++ ks-enroll-impl/src/test/java/org/kuali/student/krms/naturallanguage/config/context/GPAContextImplTest.java
@@ -2,22 +2,16 @@ package org.kuali.student.krms.naturallanguage.config.context;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kuali.rice.krms.api.repository.term.TermDefinitionContract;
 import org.kuali.rice.krms.api.repository.term.TermParameterDefinitionContract;
 import org.kuali.student.common.test.spring.AbstractServiceTest;
-import org.kuali.student.common.test.spring.Client;
 import org.kuali.student.enrollment.academicrecord.dto.GPAInfo;
 import org.kuali.student.krms.naturallanguage.KRMSDataGenerator;
 import org.kuali.student.krms.naturallanguage.TermParameterTypes;
-import org.kuali.student.krms.naturallanguage.config.context.util.NLCluSet;
-import org.kuali.student.r1.lum.statement.typekey.ReqComponentFieldTypes;
 import org.kuali.student.r2.common.exceptions.OperationFailedException;
 import org.kuali.student.r2.common.util.ContextUtils;
-import org.kuali.student.r2.lum.clu.dto.CluInfo;
-import org.kuali.student.r2.lum.clu.service.CluService;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -27,7 +21,6 @@ import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:nl-test-context.xml"})
-@Ignore
 public class GPAContextImplTest extends AbstractServiceTest {
 
     private GpaContextImpl gpaContext = new GpaContextImpl();
@@ -57,19 +50,19 @@ public class GPAContextImplTest extends AbstractServiceTest {
 	@Test
     public void testCreateContextMap_Gpa() throws OperationFailedException {
 		Map<String, Object> contextMap = gpaContext.createContextMap(term, ContextUtils.getContextInfo());
-		GPAInfo gpa = (GPAInfo) contextMap.get(GpaContextImpl.GPA_TOKEN);
+        String gpa = (String) contextMap.get(GpaContextImpl.GPA_TOKEN);
 
 		Assert.assertNotNull(contextMap);
-		Assert.assertEquals("2", gpa.getValue());
+		Assert.assertEquals("2", gpa);
 	}
 	
 	@Test
     public void testCreateContextMap_NullTokenValues() throws OperationFailedException {
         Map<String, Object> contextMap = gpaContext.createContextMap(term2, ContextUtils.getContextInfo());
-        GPAInfo gpa = (GPAInfo) contextMap.get(GpaContextImpl.GPA_TOKEN);
+        String gpa = (String) contextMap.get(GpaContextImpl.GPA_TOKEN);
 
         Assert.assertNotNull(contextMap);
-        Assert.assertEquals(null, gpa.getValue());
+        Assert.assertEquals(null, gpa);
 
 	}
 
