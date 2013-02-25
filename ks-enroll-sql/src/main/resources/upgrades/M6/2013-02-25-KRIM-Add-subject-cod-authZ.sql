2016b7d7e487c55343fb2f5e8fb83884b44a2733
depstein <depstein@kuali.org>
2013-02-25 15:16:19 -0500

    KSENROLL-5329 switched to carl CHEM (from fred BSCI)
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@43949 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/upgrades/M6/2013-02-25-KRIM-Add-subject-cod-authZ.sql
+++ ks-enroll-sql/src/main/resources/upgrades/M6/2013-02-25-KRIM-Add-subject-cod-authZ.sql
@@ -10,12 +10,12 @@ insert into KRIM_TYP_ATTR_T (KIM_TYP_ATTR_ID, OBJ_ID, VER_NBR, SORT_CD, KIM_TYP_
 --Update the subject code role to use the new type
 update KRIM_ROLE_T SET KIM_TYP_ID = (SELECT KIM_TYP_ID FROM KRIM_TYP_T WHERE NM='KS Subject Area Role Type') WHERE ROLE_NM = 'KS Department Schedule Coordinator - Subj' AND NMSPC_CD = 'KS-ENR'
 /
---Assign Fred as a BSCI subject code scheduling admin
-insert into KRIM_ROLE_MBR_T (ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD, ACTV_FRM_DT, ACTV_TO_DT, LAST_UPDT_DT) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'KS Department Schedule Coordinator - Org View Only' and nmspc_cd = 'KS-ENR'), 'fred', 'P', '', '', TO_DATE('10/24/2012', 'MM/DD/YYYY'))
+--Assign carl as a CHEM subject code scheduling admin
+insert into KRIM_ROLE_MBR_T (ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD, ACTV_FRM_DT, ACTV_TO_DT, LAST_UPDT_DT) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'KS Department Schedule Coordinator - Org View Only' and nmspc_cd = 'KS-ENR'), 'carl', 'P', '', '', TO_DATE('10/24/2012', 'MM/DD/YYYY'))
 /
-insert into KRIM_ROLE_MBR_T (ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD, ACTV_FRM_DT, ACTV_TO_DT, LAST_UPDT_DT) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'KS Department Schedule Coordinator - Subj' and nmspc_cd = 'KS-ENR'), 'fred', 'P', '', '', TO_DATE('10/24/2012', 'MM/DD/YYYY'))
+insert into KRIM_ROLE_MBR_T (ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD, ACTV_FRM_DT, ACTV_TO_DT, LAST_UPDT_DT) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'KS Department Schedule Coordinator - Subj' and nmspc_cd = 'KS-ENR'), 'carl', 'P', '', '', TO_DATE('10/24/2012', 'MM/DD/YYYY'))
 /
-insert into KRIM_ROLE_MBR_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, ROLE_MBR_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) values (KRIM_ATTR_DATA_ID_S.NEXTVAL,  SYS_GUID(), 1, (SELECT ROLE_MBR_ID from KRIM_ROLE_MBR_T where ROLE_ID = (SELECT ROLE_ID FROM KRIM_ROLE_T where role_nm = 'KS Department Schedule Coordinator - Subj' and nmspc_cd = 'KS-ENR') AND MBR_ID = 'fred'), (SELECT kim_typ_id from krim_typ_t where nm = 'KS Subject Area Role Type' and nmspc_cd = 'KS-SYS'), (SELECT MIN(TO_NUMBER(kim_attr_defn_id)) from krim_attr_defn_t where nm = 'subjectArea' and nmspc_cd = 'KS-SYS'), 'BSCI')
+insert into KRIM_ROLE_MBR_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, ROLE_MBR_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) values (KRIM_ATTR_DATA_ID_S.NEXTVAL,  SYS_GUID(), 1, (SELECT ROLE_MBR_ID from KRIM_ROLE_MBR_T where ROLE_ID = (SELECT ROLE_ID FROM KRIM_ROLE_T where role_nm = 'KS Department Schedule Coordinator - Subj' and nmspc_cd = 'KS-ENR') AND MBR_ID = 'carl'), (SELECT kim_typ_id from krim_typ_t where nm = 'KS Subject Area Role Type' and nmspc_cd = 'KS-SYS'), (SELECT MIN(TO_NUMBER(kim_attr_defn_id)) from krim_attr_defn_t where nm = 'subjectArea' and nmspc_cd = 'KS-SYS'), 'CHEM')
 /
 
 --Copy all the permissions from DSC-org to DSC-subj
