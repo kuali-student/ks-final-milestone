e776102407d907cef90b7c6d9ab4507c7342d92b
vpremcha <vpremcha@kuali.org>
2013-02-27 16:51:58 -0500

    KSENROLL-5710 - refactoring the transformer to use the new method
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@44192 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/transformer/CourseOfferingTransformer.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/transformer/CourseOfferingTransformer.java
@@ -508,7 +508,7 @@ public class CourseOfferingTransformer {
             info.setStateKey(luiIdentifierInfo.getStateKey());
             info.setCode(luiIdentifierInfo.getCode());
             info.setSubjectArea(luiIdentifierInfo.getDivision());
-            info.setDepartmentOrgId(luiIdentifierInfo.getOrgId());
+            info.setSubjectOrgId(luiIdentifierInfo.getOrgId());
             info.setCourseNumberSuffix(luiIdentifierInfo.getSuffixCode());
             info.setMeta(luiIdentifierInfo.getMeta());
             info.setAttributes(luiIdentifierInfo.getAttributes());
@@ -535,7 +535,7 @@ public class CourseOfferingTransformer {
             info.setDivision(crossListingInfo.getSubjectArea());
             info.setSuffixCode(crossListingInfo.getCourseNumberSuffix());
             //info.setVariation(crossListingInfo.);
-            info.setOrgId(crossListingInfo.getDepartmentOrgId());
+            info.setOrgId(crossListingInfo.getSubjectOrgId());
             info.setMeta(crossListingInfo.getMeta());
             info.setAttributes(crossListingInfo.getAttributes());
 
