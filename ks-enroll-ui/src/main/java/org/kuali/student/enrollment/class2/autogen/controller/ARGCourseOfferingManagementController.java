70e2b0415709c35236a9e07dba471427eff5d446
yindavid <yindavid@kuali.org>
2013-03-04 15:04:52 -0500

    KSENROLL-5805: Created skeleton project layout for autogen RG
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@44489 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/autogen/controller/ARGCourseOfferingManagementController.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2013 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * Created by David Yin on 3/4/13
+ */
+package org.kuali.student.enrollment.class2.autogen.controller;
+
+import org.kuali.rice.krad.web.controller.UifControllerBase;
+import org.kuali.rice.krad.web.form.UifFormBase;
+import org.kuali.student.enrollment.class2.courseoffering.form.CourseOfferingManagementForm;
+import org.kuali.student.enrollment.class2.courseoffering.util.CourseOfferingConstants;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * This class //TODO ...
+ *
+ * @author Kuali Student Team
+ */
+@Controller
+@RequestMapping(value = "/autoGenRegGroup")
+public class ARGCourseOfferingManagementController extends UifControllerBase {
+    @Override
+    protected UifFormBase createInitialForm(HttpServletRequest request) {
+        return new CourseOfferingManagementForm();
+    }
+
+
+
+}
---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/autogen/form/ARGCourseOfferingManagementForm.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2013 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * Created by David Yin on 3/4/13
+ */
+package org.kuali.student.enrollment.class2.autogen.form;
+
+import org.kuali.student.enrollment.class2.courseoffering.dto.ActivityOfferingWrapper;
+import org.kuali.student.enrollment.class2.courseoffering.dto.CourseOfferingCopyWrapper;
+import org.kuali.student.enrollment.class2.courseoffering.dto.CourseOfferingListSectionWrapper;
+import org.kuali.student.enrollment.class2.courseoffering.dto.CourseOfferingWrapper;
+import org.kuali.student.enrollment.uif.form.KSUifForm;
+import org.kuali.student.r2.core.acal.dto.TermInfo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * This class //TODO ...
+ *
+ * @author Kuali Student Team
+ */
+public class ARGCourseOfferingManagementForm extends KSUifForm {
+
+}
