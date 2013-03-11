1b22a8ee6bd172474aec82a0f3505628285b8ac6
miki.harmath <miki.harmath@kuali.org>
2013-03-11 08:37:49 -0400

    KSENROLL-5878 ARG - Cluster Actions - add new cluster toolbar button - code update
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@44800 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/autogen/keyvalue/FormatsForCreateRGKeyValues.java
@@ -0,0 +1,57 @@
+package org.kuali.student.enrollment.class2.autogen.keyvalue;
+
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.core.api.util.ConcreteKeyValue;
+import org.kuali.rice.core.api.util.KeyValue;
+import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
+import org.kuali.rice.krad.uif.view.ViewModel;
+import org.kuali.student.enrollment.class2.autogen.form.ARGCourseOfferingManagementForm;
+import org.kuali.student.enrollment.courseoffering.dto.CourseOfferingInfo;
+import org.kuali.student.enrollment.courseoffering.dto.FormatOfferingInfo;
+import org.kuali.student.enrollment.courseoffering.service.CourseOfferingService;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.util.ContextUtils;
+import org.kuali.student.r2.common.util.constants.CourseOfferingServiceConstants;
+
+import javax.xml.namespace.QName;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FormatsForCreateRGKeyValues extends UifKeyValuesFinderBase implements Serializable {
+    private transient CourseOfferingService courseOfferingService;
+    private static final long serialVersionUID = 1L;
+    @Override
+    public List<KeyValue> getKeyValues(ViewModel model) {
+
+        ARGCourseOfferingManagementForm rgForm = (ARGCourseOfferingManagementForm) model;
+
+        List<KeyValue> keyValues = new ArrayList<KeyValue>();
+//TODO: change back from hardcoded values
+//        CourseOfferingInfo selectedCourseOffering = rgForm.getCurrentCourseOfferingWrapper().getCourseOfferingInfo();
+
+        try {
+            ContextInfo contextInfo = ContextUtils.createDefaultContextInfo();
+//TODO: change back from hardcoded values
+CourseOfferingInfo selectedCourseOffering = getCourseOfferingService().getCourseOffering("e6ee1d27-63c4-46ac-927c-ab8f0ec2d2ac", contextInfo);
+            String courseOfferingId = selectedCourseOffering.getId();
+            List<FormatOfferingInfo> formatOfferingInfos =
+                    getCourseOfferingService().getFormatOfferingsByCourseOffering(courseOfferingId, contextInfo);
+            for (FormatOfferingInfo formatOfferingInfo : formatOfferingInfos) {
+                //In keyValues, key is the FormatOfferingInfo.id and value is the FormatOfferingInfo.name
+                keyValues.add(new ConcreteKeyValue(formatOfferingInfo.getId(), formatOfferingInfo.getName()));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Error getting Formats for course offering", e);
+        }
+        return keyValues;
+    }
+
+    protected CourseOfferingService getCourseOfferingService() {
+        if (courseOfferingService == null) {
+            courseOfferingService = (CourseOfferingService) GlobalResourceLoader.getService(new QName(CourseOfferingServiceConstants.NAMESPACE, "CourseOfferingService"));
+        }
+        return courseOfferingService;
+    }
+}
+
