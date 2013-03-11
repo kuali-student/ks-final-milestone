05fff8684a3fa36dfc2b96d97270877d2e4bae0e
jcaddel <jcaddel@kuali.org>
2013-03-11 17:57:29 -0400

    Automated Impex update
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-deployments/trunk@44837 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-dbs/ks-impex/ks-impex-bundled-db/src/main/resources/mysql/ks-impex-bundled-db.sql
+++ ks-dbs/ks-impex/ks-impex-bundled-db/src/main/resources/mysql/ks-impex-bundled-db.sql
@@ -15921,7 +15921,7 @@ CREATE TABLE KRIM_ROLE_PERM_ID_S
 	id bigint(19) not null auto_increment, primary key (id) 
 ) ENGINE MyISAM
 /
-ALTER TABLE KRIM_ROLE_PERM_ID_S auto_increment = 1251
+ALTER TABLE KRIM_ROLE_PERM_ID_S auto_increment = 1252
 /
 
 # -----------------------------------------------------------------------
