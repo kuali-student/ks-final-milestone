7f87150ee6a8bdf5764c6c6f9d115e38843a42b4
depstein <depstein@kuali.org>
2013-03-11 12:05:19 -0400

    KSENROLL-5843 locked down ability to edit roles and permissions so only tech admin can do all and IDM admin can only do roles.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@44810 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/upgrades/M6/2013-03-07-KRIM-enr-idm-permissions.sql
+++ ks-enroll-sql/src/main/resources/upgrades/M6/2013-03-07-KRIM-enr-idm-permissions.sql
@@ -23,3 +23,21 @@ insert into KRIM_ROLE_PERM_T (ROLE_PERM_ID, OBJ_ID, VER_NBR, ROLE_ID, PERM_ID, A
 
 insert into KRIM_ROLE_MBR_T (ROLE_MBR_ID, VER_NBR, OBJ_ID, ROLE_ID, MBR_ID, MBR_TYP_CD, ACTV_FRM_DT, ACTV_TO_DT, LAST_UPDT_DT) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'KS Enrollment IDM Administrator' and nmspc_cd = 'KS-ENR'), 'martha', 'P', '', '', TO_DATE('10/24/2012', 'MM/DD/YYYY'))
 /
+
+--Lock down who can edit permissions
+--Create two new permissions to lock down all KRIM document initialization, and a specific one for role documents
+insert into KRIM_PERM_T (PERM_ID, OBJ_ID, VER_NBR, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND) values (KRIM_PERM_ID_S.NEXTVAL, SYS_GUID(), 1, (SELECT perm_tmpl_id FROM krim_perm_tmpl_t where nm = 'Initiate Document' and nmspc_cd = 'KR-SYS'), 'KS-ENR', 'Initiate Documents for IdentityManagementDocument', 'Allows the user to Initiate Documents for IdentityManagementDocument', 'Y')
+/
+insert into KRIM_PERM_T (PERM_ID, OBJ_ID, VER_NBR, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND) values (KRIM_PERM_ID_S.NEXTVAL, SYS_GUID(), 1, (SELECT perm_tmpl_id FROM krim_perm_tmpl_t where nm = 'Initiate Document' and nmspc_cd = 'KR-SYS'), 'KS-ENR', 'Initiate Documents for IdentityManagementRoleDocument', 'Allows the user to Initiate Documents for IdentityManagementRoleDocument', 'Y')
+/
+insert into KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, PERM_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) values (KRIM_ATTR_DATA_ID_S.NEXTVAL, SYS_GUID(), 1, (SELECT perm_id from krim_perm_t where nm = 'Initiate Documents for IdentityManagementDocument' and nmspc_cd = 'KS-ENR'), (SELECT kim_typ_id from krim_typ_t where nm = 'Document Type & Routing Node or State' and nmspc_cd = 'KR-SYS'), (SELECT MIN(TO_NUMBER(kim_attr_defn_id)) from krim_attr_defn_t where nm = 'documentTypeName' and nmspc_cd = 'KR-WKFLW'), 'IdentityManagementDocument')
+/
+insert into KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, PERM_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) values (KRIM_ATTR_DATA_ID_S.NEXTVAL, SYS_GUID(), 1, (SELECT perm_id from krim_perm_t where nm = 'Initiate Documents for IdentityManagementRoleDocument' and nmspc_cd = 'KS-ENR'), (SELECT kim_typ_id from krim_typ_t where nm = 'Document Type & Routing Node or State' and nmspc_cd = 'KR-SYS'), (SELECT MIN(TO_NUMBER(kim_attr_defn_id)) from krim_attr_defn_t where nm = 'documentTypeName' and nmspc_cd = 'KR-WKFLW'), 'IdentityManagementRoleDocument')
+/
+--Assign all permissions to tech admin, and only one to enrollment admin
+insert into KRIM_ROLE_PERM_T (ROLE_PERM_ID, OBJ_ID, VER_NBR, ROLE_ID, PERM_ID, ACTV_IND) values (KRIM_ROLE_PERM_ID_S.NEXTVAL, SYS_GUID(), 1, (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'Technical Administrator' and nmspc_cd = 'KR-SYS'), (SELECT perm_id from krim_perm_t where nm = 'Initiate Documents for IdentityManagementDocument' and nmspc_cd = 'KS-ENR'), 'Y')
+/
+insert into KRIM_ROLE_PERM_T (ROLE_PERM_ID, OBJ_ID, VER_NBR, ROLE_ID, PERM_ID, ACTV_IND) values (KRIM_ROLE_PERM_ID_S.NEXTVAL, SYS_GUID(), 1, (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'Technical Administrator' and nmspc_cd = 'KR-SYS'), (SELECT perm_id from krim_perm_t where nm = 'Initiate Documents for IdentityManagementRoleDocument' and nmspc_cd = 'KS-ENR'), 'Y')
+/
+insert into KRIM_ROLE_PERM_T (ROLE_PERM_ID, OBJ_ID, VER_NBR, ROLE_ID, PERM_ID, ACTV_IND) values (KRIM_ROLE_PERM_ID_S.NEXTVAL, SYS_GUID(), 1, (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'KS Enrollment IDM Administrator' and nmspc_cd = 'KS-ENR'), (SELECT perm_id from krim_perm_t where nm = 'Initiate Documents for IdentityManagementRoleDocument' and nmspc_cd = 'KS-ENR'), 'Y')
+/
