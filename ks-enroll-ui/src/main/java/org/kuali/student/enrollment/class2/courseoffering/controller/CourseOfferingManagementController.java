41916d07ecc007b2c952f7573f529f9b45010523
brandon.gresham <brandon.gresham@kuali.org>
2013-03-11 14:16:55 -0400

    KSENROLL-5509 Show term-name on the AO confirm-delete view (accomplished via setting term-info from the form onto the wrapper).
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@44819 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/controller/CourseOfferingManagementController.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/controller/CourseOfferingManagementController.java
@@ -435,6 +435,7 @@ public class CourseOfferingManagementController extends UifControllerBase  {
     protected ModelAndView prepareManageAOsModelAndView(CourseOfferingManagementForm form, CourseOfferingListSectionWrapper selectedCO) throws  Exception {
 
         CourseOfferingWrapper currentCO = new CourseOfferingWrapper(selectedCO.isCrossListed(),selectedCO.getCourseOfferingCode(),selectedCO.getCourseOfferingDesc(),selectedCO.getAlternateCOCodes(),selectedCO.getCourseOfferingId());
+        currentCO.setTerm( form.getTermInfo() );
         CourseOfferingInfo coInfo = getCourseOfferingService().getCourseOffering(currentCO.getCourseOfferingId(),ContextUtils.createDefaultContextInfo());
 
         currentCO.setCourseOfferingInfo(coInfo);
