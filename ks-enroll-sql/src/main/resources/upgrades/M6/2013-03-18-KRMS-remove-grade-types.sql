9b99dc30fff1a151e4b7f4b493b9f58f30c56d20
llfourie <llfourie@kuali.org>
2013-03-19 02:30:39 -0400

    KSENROLL-5504 : Remove Grade options that are no longer needed
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@45206 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/upgrades/M6/2013-03-18-KRMS-remove-grade-types.sql
@@ -0,0 +1,12 @@
+delete from ksen_lrc_rvg
+where result_scale_id = 'kuali.result.scale.grade.narrative'
+/
+delete from ksen_lrc_result_scale
+where name = 'Narrative Grade'
+/
+delete from ksen_lrc_result_scale
+where name = 'Qualitative Review (Narrative)'
+/
+delete from ksen_lrc_result_scale
+where name = 'At Risk Flag'
+/
