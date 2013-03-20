67facbe30ffe13ba134f2f53b1b2468a5d534595
brandon.gresham <brandon.gresham@kuali.org>
2013-03-20 10:01:13 -0400

    KSENROLL-5969 Added DAO-support and impex-data for the new KSEN_STATE.IS_INITIAL_STATE.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@45250 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/upgrades/M6/2013-03-19-KSEN-Alter-add-initial-state-boolean.sql
+++ ks-enroll-sql/src/main/resources/upgrades/M6/2013-03-19-KSEN-Alter-add-initial-state-boolean.sql
@@ -1,3 +1,14 @@
 ALTER TABLE KSEN_STATE
 ADD IS_INITIAL_STATE number(1) default(0) NOT NULL
 /
+UPDATE KSEN_STATE
+SET is_initial_state = 1
+WHERE ID = 'kuali.lui.activity.offering.state.draft'
+OR ID = 'kuali.lui.course.offering.state.draft'
+OR ID = 'kuali.atp.state.Draft'
+OR ID = 'kuali.lui.format.offering.state.draft'
+OR ID = 'kuali.milestone.state.Draft'
+OR ID = 'kuali.soc.state.draft'
+OR ID = 'kuali.soc.scheduling.state.notstarted'
+OR ID = 'kuali.lui.registration.group.state.pending'
+/
