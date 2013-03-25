2f5194a64b323eed1cef8442ac58c0cdf1a3225f
huangb <huangb@kuali.org>
2013-03-25 13:53:31 -0400

    KSENROLL-5987: In ARGCOManageView.xml, we use courseOffering.js, in which the function handleAOToolbar define divId and a list of buttons that still point to beans and buttons defined in old COManageView.xml. This completely messed up two versions of manage CO. Solution: in  ARGCOManageView.xml, instead of use courseOffering.js, replace it with ARG.js, in which redefine the similar function and named it as handleARGAOToolbar, in which  divId and a list of buttons point to beans and buttons defined in ARGCOManageView.xml.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@45462 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-web/src/main/webapp/ks-enroll/scripts/ARG.js
@@ -0,0 +1,23 @@
+/**
+ * Configure toolbar on Manage AO page.
+ * divId -- The section contains hiddencolumns
+ * hiddenColumns -- each row of AO list contains hiddenColumns storing values processed in server side.
+ * buttons -- buttons on the toolbar
+ * Note: Each hiddenColumn matches with one button. hiddenColumns and buttons are in the same order.
+ */
+function handleARGAOToolbar(){
+    var divId = "KS-CourseOfferingManagement-ARG-ActivityOfferingListSection";
+
+    var hiddenColumns=['enableApproveButton', 'enableSuspendButton', 'enableCancelButton', 'enableReinstateButton', 'enableDeleteButton', 'enableDraftButton'];
+
+    var buttons = ['KS-CourseOfferingManagement-ToolBar-Approve-AO-ARG', 'KS-CourseOfferingManagement-ToolBar-Suspend-AO-ARG',
+                   'KS-CourseOfferingManagement-ToolBar-Cancel-AO-ARG', 'KS-CourseOfferingManagement-ToolBar-Reinstate-AO-ARG',
+                   'KS-CourseOfferingManagement-ToolBar-Delete-AO-ARG', 'KS-CourseOfferingManagement-ToolBar-Draft-AO-ARG'];
+
+//    var hiddenColumns=['enableApproveButton', 'enableDeleteButton', 'enableDraftButton'];
+//
+//    var buttons = ['KS-CourseOfferingManagement-ToolBar-Approve-AO', 'KS-CourseOfferingManagement-ToolBar-Delete-AO',
+//        'KS-CourseOfferingManagement-ToolBar-Draft-AO'];
+
+    handleToolbar(divId, hiddenColumns, buttons)
+}
