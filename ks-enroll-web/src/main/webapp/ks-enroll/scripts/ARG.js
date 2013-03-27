9c54ec8eff6d216e86b4e3b3a3df0fe9a86b79b4
huangb <huangb@kuali.org>
2013-03-27 06:48:33 -0400

    KSENROLL-5987: update the value of divId
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@45557 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-web/src/main/webapp/ks-enroll/scripts/ARG.js
+++ ks-enroll-web/src/main/webapp/ks-enroll/scripts/ARG.js
@@ -6,15 +6,15 @@
  * Note: Each hiddenColumn matches with one button. hiddenColumns and buttons are in the same order.
  */
 function handleARGAOToolbar(){
-    var divId = "KS-CourseOfferingManagement-ARG-ActivityOfferingListSection";
+    var divId = "KS-ARGCourseOfferingManagement-ActivityOfferingListSection";
 
-    var hiddenColumns=['enableApproveButton', 'enableSuspendButton', 'enableCancelButton', 'enableReinstateButton', 'enableDeleteButton', 'enableDraftButton'];
+//    var hiddenColumns=['enableApproveButton', 'enableSuspendButton', 'enableCancelButton', 'enableReinstateButton', 'enableDeleteButton', 'enableDraftButton'];
 
     var buttons = ['KS-CourseOfferingManagement-ToolBar-Approve-AO-ARG', 'KS-CourseOfferingManagement-ToolBar-Suspend-AO-ARG',
                    'KS-CourseOfferingManagement-ToolBar-Cancel-AO-ARG', 'KS-CourseOfferingManagement-ToolBar-Reinstate-AO-ARG',
                    'KS-CourseOfferingManagement-ToolBar-Delete-AO-ARG', 'KS-CourseOfferingManagement-ToolBar-Draft-AO-ARG'];
 
-//    var hiddenColumns=['enableApproveButton', 'enableDeleteButton', 'enableDraftButton'];
+   var hiddenColumns=['enableApproveButton', 'enableDeleteButton', 'enableDraftButton'];
 //
 //    var buttons = ['KS-CourseOfferingManagement-ToolBar-Approve-AO', 'KS-CourseOfferingManagement-ToolBar-Delete-AO',
 //        'KS-CourseOfferingManagement-ToolBar-Draft-AO'];
