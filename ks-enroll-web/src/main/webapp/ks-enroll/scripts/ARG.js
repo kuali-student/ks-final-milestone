8f7e54f45634bd6dc49b26f982cbf0d03ccb3764
huangb <huangb@kuali.org>
2013-03-27 10:18:49 -0400

    KSENROLL-6035: Problem: the function handleCOToolbar() invoked through isChecked field on manage COs page is defined in courseOffering.js. for ARG module, we have replace courseOffering.js with ARG.js. Solution: rename handleCOToolbar to handleARGCOToolbar and define  handleARGCOToolbar() function in ARG.js, in which divId, buttons are using bean ids defined in the ARG module.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@45562 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-web/src/main/webapp/ks-enroll/scripts/ARG.js
+++ ks-enroll-web/src/main/webapp/ks-enroll/scripts/ARG.js
@@ -1,4 +1,26 @@
 /**
+ * Configure toolbar on Manage CO page.
+ * divId -- The section contains hiddencolumns
+ * hiddenColumns -- each row of CO list contains hiddenColumns storing values processed in server side.
+ * buttons -- buttons on the toolbar
+ * Note: Each hiddenColumn matches with one button. hiddenColumns and buttons are in the same order.
+ */
+function handleARGCOToolbar(){
+    var divId = "KS-ARGCourseOfferingManagement-CourseOfferingListSection";
+
+//    var hiddenColumns=['enableApproveButton', 'enableSuspendButton', 'enableCancelButton', 'enableReinstateButton', 'enableDeleteButton'];
+//
+//    var bottons = ['KS-CourseOfferingManagement-ToolBar-Approve-CO', 'KS-CourseOfferingManagement-ToolBar-Suspend-CO',
+//                   'KS-CourseOfferingManagement-ToolBar-Cancel-CO', 'KS-CourseOfferingManagement-ToolBar-Reinstate-CO',
+//                   'KS-CourseOfferingManagement-ToolBar-Delete-CO'];
+
+    var hiddenColumns=['enableApproveButton', 'enableDeleteButton'];
+
+    var bottons = ['KS-CourseOfferingManagement-ToolBar-Approve-CO-ARG', 'KS-CourseOfferingManagement-ToolBar-Delete-CO-ARG'];
+
+    handleToolbar(divId, hiddenColumns, bottons);
+}
+/**
  * Configure toolbar on Manage AO page.
  * divId -- The section contains hiddencolumns
  * hiddenColumns -- each row of AO list contains hiddenColumns storing values processed in server side.
