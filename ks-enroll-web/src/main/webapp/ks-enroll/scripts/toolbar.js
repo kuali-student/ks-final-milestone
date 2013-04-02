aa1ec0c9d82901c99c83ab28f5765c4b6da4f170
huangb <huangb@kuali.org>
2013-04-02 15:31:04 -0400

    KSENROLL-6123: the original processToolbar method only gets one table from specified divId. We want to get all tables within a specified divId. So replace it with a new implementation.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@45759 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-web/src/main/webapp/ks-enroll/scripts/toolbar.js
+++ ks-enroll-web/src/main/webapp/ks-enroll/scripts/toolbar.js
@@ -2,21 +2,45 @@
  * handle toolbar to make buttons enabled/disabled based on values stored in the hiddenColumns
  * @param divId -- section contains hiddencolumns
  * @param hiddenColumns -- each hiddenColumn has true/false value
- * @param bottons -- button id in the toolbar
+ * @param buttons -- button id in the toolbar
  */
 
-function handleToolbar(divId, hiddenColumns, bottons){
+function handleToolbar(divId, hiddenColumns, buttons){
     var hiddenColumnsMap = new Object();
     var buttonsMap = new Object();
     for(var j=0; j<hiddenColumns.length; j++) {
         var field = getSelector(hiddenColumns[j]);
         hiddenColumnsMap[field]=[];
-        buttonsMap[field]=bottons[j];
+        buttonsMap[field]=buttons[j];
     }
 
     processToolbar(divId, hiddenColumnsMap, buttonsMap);
 }
 
+function processToolbar(divId, hiddenColumnsMap, buttonsMap) {
+    jQuery("#" + divId).find("table").each(function(){
+        var id=jQuery(this).attr('id');
+        var oTable = jQuery("#" + id).dataTable();
+        var selectedRows =  jQuery(oTable.fnGetNodes()).filter(':has(:input:checkbox:checked)');
+
+        jQuery(selectedRows).each(function(){
+            var cellData = oTable.fnGetData(this);
+            for(var i=0; i<cellData.length; i++)
+            {
+                for(var key in hiddenColumnsMap){
+                    if(jQuery(cellData[i]).find(key).val() == 'true' || jQuery(cellData[i]).find(key).val() == 'false'){
+                        hiddenColumnsMap[key].push(jQuery(cellData[i]).find(key).val());
+                    }
+                }
+            }
+        });
+
+    });
+    handleButtons(hiddenColumnsMap, buttonsMap);
+}
+
+/** The following method only get one table from specified divId. We want to support the case with
+ *  multiple tables. so replace it with the above one.
 function processToolbar(divId, hiddenColumnsMap, buttonsMap){
     var oTable = getDataTableFromDiv(divId);
 
@@ -37,16 +61,16 @@ function processToolbar(divId, hiddenColumnsMap, buttonsMap){
    handleButtons(hiddenColumnsMap, buttonsMap);
 }
 
+function getDataTableFromDiv(divId){
+ var id = jQuery("#" + divId).find("table").attr('id');
+ var oTable = jQuery("#" + id).dataTable();
+ return oTable;
+ }
+*/
+
 function getSelector(name){
     return "input[name$='" + name +"']";
 }
-
-function getDataTableFromDiv(divId){
-    var id = jQuery("#" + divId).find("table").attr('id');
-    var oTable = jQuery("#" + id).dataTable();
-    return oTable;
-}
-
 
 function getDataTableHandle(divId){
     var id = jQuery("#" + divId).attr('id');
