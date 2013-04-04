7646dd03693c7946cac9f0125795cd8250bd2fdf
jcaddel <jcaddel@kuali.org>
2013-04-04 18:55:25 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/spring-db-jc/ks-deployments/trunk@45900 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/DumpConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/DumpConfig.java
@@ -1,9 +1,13 @@
 package org.kuali.student.deploy.spring;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.kuali.common.impex.ImpexContextCloningFactoryBean;
 import org.kuali.common.impex.service.ImpexContext;
+import org.kuali.common.util.service.ScmService;
+import org.kuali.common.util.spring.ScmServiceFactoryBean;
 import org.kuali.common.util.spring.SpringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -24,10 +28,46 @@ public class DumpConfig {
 
 	@Bean
 	public ImpexContext riceDumpContext() {
-
 		String artifactId = KS_IMPEX_RICE_ARTIFACT_ID;
 		String include = SpringUtils.getProperty(env, "impex.rice.include");
+		return getBaseContext(env, artifactId, include);
+	}
 
+	@Bean
+	public ImpexContext appDumpContext() {
+		String artifactId = KS_IMPEX_APP_ARTIFACT_ID;
+		String include = SpringUtils.getProperty(env, "impex.ks.include");
+		return getBaseContext(env, artifactId, include);
+	}
+
+	@Bean
+	public ImpexContext bundledDumpContext() {
+		String artifactId = KS_IMPEX_BUNDLED_ARTIFACT_ID;
+		String include = SpringUtils.getProperty(env, "impex.rice.include") + "," + SpringUtils.getProperty(env, "impex.ks.include");
+		ImpexContext context = getBaseContext(env, artifactId, include);
+		// Bundled is the combination of Rice + App, so we don't need to physically copy the data files
+		context.setCopyDataFiles(false);
+		return context;
+	}
+
+	@Bean
+	public ScmService scmService() {
+		String url = SpringUtils.getProperty(env, "project.scm.developerConnection");
+		ScmServiceFactoryBean ssfb = new ScmServiceFactoryBean();
+		ssfb.setUrl(url);
+		return ssfb.getObject();
+	}
+
+	@Bean
+	public List<ImpexContext> dumpContexts() {
+		List<ImpexContext> contexts = new ArrayList<ImpexContext>();
+		contexts.add(riceDumpContext());
+		contexts.add(appDumpContext());
+		contexts.add(bundledDumpContext());
+		return contexts;
+	}
+
+	protected ImpexContext getBaseContext(Environment env, String artifactId, String include) {
 		String finalDirName = getFinalDirName(env, artifactId);
 		File finalDirectory = new File(finalDirName);
 		String schemaFileInclude = "**/" + artifactId + "*";
@@ -38,6 +78,8 @@ public class DumpConfig {
 		bean.setInclude(include);
 		bean.setCopyDataFiles(true);
 		bean.setSchemaFileInclude(schemaFileInclude);
+		// TODO Set this appropriately
+		bean.setSourceContext(null);
 		return bean.getObject();
 	}
 
