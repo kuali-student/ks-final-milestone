6b74e6564e24007fb4aee87954af5daa83da2248
gtaylor <gtaylor@kuali.org>
2013-04-05 23:08:53 -0400

    KSENROLL-6264: missed all possible ROS for a related ao. an ao can be in draf or approved to cause a reg group to be pending
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@46001 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-sql/src/main/resources/initial-db/state/data/KSEN_STATE_CNSTRNT_ROS.sql
+++ ks-core-sql/src/main/resources/initial-db/state/data/KSEN_STATE_CNSTRNT_ROS.sql
@@ -31,7 +31,10 @@ INSERT INTO KSEN_STATE_CNSTRNT_ROS (REL_OBJ_STATE_ID,STATE_CNSTRNT_ID)
   VALUES ('kuali.lui.activity.offering.state.offered','kuali.stateconstraint.registrationgroup.invalid.offered')
 /
 INSERT INTO KSEN_STATE_CNSTRNT_ROS (REL_OBJ_STATE_ID,STATE_CNSTRNT_ID)
-  VALUES ('kuali.lui.activity.offering.state.offered','kuali.stateconstraint.registrationgroup.invalid.pending')
+  VALUES ('kuali.lui.activity.offering.state.draft','kuali.stateconstraint.registrationgroup.invalid.pending')
+/
+INSERT INTO KSEN_STATE_CNSTRNT_ROS (REL_OBJ_STATE_ID,STATE_CNSTRNT_ID)
+  VALUES ('kuali.lui.activity.offering.state.approved','kuali.stateconstraint.registrationgroup.invalid.pending')
 /
 INSERT INTO KSEN_STATE_CNSTRNT_ROS (REL_OBJ_STATE_ID,STATE_CNSTRNT_ID)
   VALUES ('kuali.lui.activity.offering.state.offered','kuali.stateconstraint.registrationgroup.offered.pending')
