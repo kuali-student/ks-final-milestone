bb825413cc52d035b064fe9f35b4cade5f5f594e
huangb <huangb@kuali.org>
2013-04-08 10:26:36 -0400

    KSENROLL-6225: Add comments
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@46069 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/autogen/keyvalue/ARGListOfAOClustersForFOKeyValues.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/autogen/keyvalue/ARGListOfAOClustersForFOKeyValues.java
@@ -45,6 +45,24 @@ import java.util.HashSet;
 public class ARGListOfAOClustersForFOKeyValues extends UifKeyValuesFinderBase implements Serializable {
     private transient CourseOfferingService courseOfferingService;
     private static final long serialVersionUID = 1L;
+
+    /**
+     * The cluster drop-down list field would display:
+     * 1)If a CO has no FO, show "No Format Offering and No Clusters" with an empty (invalid) key
+     * 2)If a CO has only one FO, display a list of clusters for that FO
+     * 3)If a CO has more than one FO, go through all AOs to check – if AO is selected, check the AO belongs to which FO,
+     *   put the FO with selected AO(s) in a HashSet. at the end,
+     *    3a) if the FO set is empty, it indicates that no AO has been selected. show "No Activity has been selected"
+     *        with an empty (invalid) key.
+     *    3b) if the FO set has one element, it indicates that all selected AOs belong to one FO. Populate all clusters
+     *        only for that FO and display the list.
+     *    3c) if the FO set has more than one element, it indicates that selected AOs belong to more than one FO. And
+     *        we want to prevent users to move AOs across FOs, therefore show "Cannot move between format offerings"
+     *        with an empty (invalid) key.
+     *
+     * @param model  -- the Form
+     * @return a list of key-value pair for the display of the drop-down list
+     */
     @Override
     public List<KeyValue> getKeyValues(ViewModel model) {
         ARGCourseOfferingManagementForm theForm = (ARGCourseOfferingManagementForm) model;
