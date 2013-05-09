a1d4ec3f145a81244769fa81a4be0efbc17b1c9f
johglov <johglov@kuali.org>
2013-05-09 16:39:07 -0400

    KSAP-33 Implementation of basic shopping cart  functionality with helper interface for future development. Also error correction from version update.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/KSAP@47861 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-api/src/main/java/org/kuali/student/myplan/academicplan/service/AcademicPlanServiceConstants.java
+++ ks-ap-api/src/main/java/org/kuali/student/myplan/academicplan/service/AcademicPlanServiceConstants.java
@@ -19,6 +19,7 @@ public class AcademicPlanServiceConstants {
     public static final String LEARNING_PLAN_ITEM_TYPE_BACKUP = "kuali.academicplan.item.backup";
     public static final String LEARNING_PLAN_ITEM_TYPE_WISHLIST = "kuali.academicplan.item.wishlist";
     public static final String LEARNING_PLAN_ITEM_TYPE_WHATIF = "'kuali.academicplan.item.whatif";
+    public static final String LEARNING_PLAN_ITEM_TYPE_CART = "kuali.academicplan.item.cart";
 
     public static final String LEARNING_PLAN_ACTIVE_STATE_KEY  = "kuali.academicplan.plan.state.active";
     public static final String LEARNING_PLAN_ITEM_ACTIVE_STATE_KEY = "kuali.academicplan.planitem.state.active";
