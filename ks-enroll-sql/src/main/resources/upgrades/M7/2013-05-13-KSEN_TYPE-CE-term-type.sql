1f36ec584ae037c7a2220c1f261955d7430a3666
vgadiyak <vgadiyak@kuali.org>
2013-05-13 13:44:03 -0400

    KSENROLL-6762           New File
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@47987 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/upgrades/M7/2013-05-13-KSEN_TYPE-CE-term-type.sql
@@ -0,0 +1,13 @@
+-- KSENROLL-6762 Continuing Education terms
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
+  VALUES ('SYSTEMLOADER',TO_DATE( '20130513', 'YYYYMMDD' ),'CE First Term','CE First Term','CE Term 1','http://student.kuali.org/wsdl/atp/AtpInfo','http://student.kuali.org/wsdl/atp/AtpService','kuali.atp.type.CETerm1',0)
+/
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
+  VALUES ('SYSTEMLOADER',TO_DATE( '20130513', 'YYYYMMDD' ),'CE Second Term','CE Second Term','CE Term 2','http://student.kuali.org/wsdl/atp/AtpInfo','http://student.kuali.org/wsdl/atp/AtpService','kuali.atp.type.CETerm2',0)
+/
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
+  VALUES ('SYSTEMLOADER',TO_DATE( '20130513', 'YYYYMMDD' ),'CE Third Term','CE Third Term','CE Term 3','http://student.kuali.org/wsdl/atp/AtpInfo','http://student.kuali.org/wsdl/atp/AtpService','kuali.atp.type.CETerm3',0)
+/
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
+  VALUES ('SYSTEMLOADER',TO_DATE( '20130513', 'YYYYMMDD' ),'CE Fourth Term','CE Fourth Term','CE Term 4','http://student.kuali.org/wsdl/atp/AtpInfo','http://student.kuali.org/wsdl/atp/AtpService','kuali.atp.type.CETerm4',0)
+/
