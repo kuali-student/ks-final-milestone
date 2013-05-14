cfde474f05b87d4cacf8f818fa05d57d5a09617b
komandur <komandur@kuali.org>
2013-05-14 18:37:14 -0400

    KSENROLL-6851 Syncing up with latest DBA schema
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/branches/sched-service@48093 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/upgrades/M6/2013-05-08-SSR-DATA-MIGRATION.sql
+++ ks-enroll-sql/src/main/resources/upgrades/M6/2013-05-08-SSR-DATA-MIGRATION.sql
@@ -1,97 +1,97 @@
 -- Delete any lingering data
-delete from ksen_sched_rqst_set
+delete from KSEN_SCHED_RQST_SET
 
 /
 
-delete from ksen_lui_schedule
+delete from KSEN_LUI_SCHEDULE
 
 /
 
-delete from ksen_sched_rqst_ref_obj
+delete from KSEN_SCHED_RQST_REF_OBJ
 
 /
 
--- Populate ksen_lui_schedule join table
-insert into ksen_lui_schedule (lui_id, sched_id) (select id, schedule_id from ksen_lui where lui_type like 'kuali.lui.type.activity.offering%' and schedule_id is not null)
+-- Populate KSEN_LUI_SCHEDULE join table
+insert into KSEN_LUI_SCHEDULE (LUI_ID, SCHED_ID) (select ID, SCHEDULE_ID from KSEN_LUI where LUI_TYPE like 'kuali.lui.type.activity.offering%' and SCHEDULE_ID is not null)
 
 /
 
--- Start with assigning ksen_sched_rqst ids in rqst
+-- Start with assigning KSEN_SCHED_RQST ids in rqst
 -- (lot easier this way to create rqst_set in the next step)
-update ksen_sched_rqst set SCHED_RQST_SET_ID = SYS_GUID()
+update KSEN_SCHED_RQST set SCHED_RQST_SET_ID = SYS_GUID()
 
 /
 
--- Populate ksen_sched_rqst_set table
-Insert into ksen_sched_rqst_set (ID, OBJ_ID, SCHED_RQST_SET_TYPE, SCHED_RQST_SET_STATE, MAX_ENROLLMENT_SHARED_IND, MAX_ENROLLMENT,
-  REF_OBJ_TYPE, VER_NBR, CREATETIME, CREATEID)  (SELECT SCHED_RQST_SET_ID, id, 'kuali.scheduling.schedule.request.set.type.schedule.request',
+-- Populate KSEN_SCHED_RQST_SET table
+Insert into KSEN_SCHED_RQST_SET (ID, OBJ_ID, SCHED_RQST_SET_TYPE, SCHED_RQST_SET_STATE, MAX_ENROLLMENT_SHARED_IND, MAX_ENROLLMENT,
+  REF_OBJ_TYPE, VER_NBR, CREATETIME, CREATEID)  (SELECT SCHED_RQST_SET_ID, ID, 'kuali.scheduling.schedule.request.set.type.schedule.request',
   'kuali.scheduling.schedule.request.set.state.created', '0', '0', 'http://student.kuali.org/wsdl/courseOffering/ActivityOfferingInfo', '0',
-  TO_DATE( '20130503000000', 'YYYYMMDDHH24MISS' ), 'admin' from ksen_sched_rqst)
+  TO_DATE( '20130503000000', 'YYYYMMDDHH24MISS' ), 'admin' from KSEN_SCHED_RQST)
 
 /
 
--- Populate ksen_sched_rqst_ref_obj join table  with AO ids
-insert into ksen_sched_rqst_ref_obj (ref_obj_id, sched_rqst_set_id) (select ref_object_id, sched_rqst_set_id from ksen_sched_rqst where ref_object_type = 'http://student.kuali.org/wsdl/courseOffering/ActivityOfferingInfo')
+-- Populate KSEN_SCHED_RQST_REF_OBJ join table  with AO ids
+insert into KSEN_SCHED_RQST_REF_OBJ (REF_OBJ_ID, SCHED_RQST_SET_ID) (select REF_OBJECT_ID, SCHED_RQST_SET_ID from KSEN_SCHED_RQST where REF_OBJECT_TYPE = 'http://student.kuali.org/wsdl/courseOffering/ActivityOfferingInfo')
 
 /
 
--- Populate ksen_sched_rqst_ref_obj join table with AO ids from ColoSets
-insert into ksen_sched_rqst_ref_obj (ref_obj_id, sched_rqst_set_id) (select lui_id, subquery.sched_rqst_set_id from ksen_lui_set_lui, (select ref_object_id, sched_rqst_set_id from ksen_sched_rqst where ref_object_type = 'kuali.luiset.type.colocated.offering.set') subquery where subquery.ref_object_id =  lui_set_id)
+-- Populate KSEN_SCHED_RQST_REF_OBJ join table with AO ids from ColoSets
+insert into KSEN_SCHED_RQST_REF_OBJ (REF_OBJ_ID, SCHED_RQST_SET_ID) (select LUI_ID, subquery.SCHED_RQST_SET_ID from KSEN_LUI_set_lui, (select REF_OBJECT_ID, SCHED_RQST_SET_ID from KSEN_SCHED_RQST where REF_OBJECT_TYPE = 'kuali.luiset.type.colocated.offering.set') subquery where subquery.REF_OBJECT_ID =  lui_set_id)
 
 /
 
--- Populate sched_id in ksen_sched_rqst
-create table ssrtemp1_t as (select sched_id, sched_rqst_set_id from ksen_lui_schedule a inner join ksen_sched_rqst_ref_obj b on a.lui_id = b.ref_obj_id)
+-- Populate SCHED_ID in KSEN_SCHED_RQST
+create table SSRTEMP1_T as (select SCHED_ID, SCHED_RQST_SET_ID from KSEN_LUI_SCHEDULE a inner join KSEN_SCHED_RQST_REF_OBJ b on a.LUI_ID = b.REF_OBJ_ID)
 
 /
 
-update ksen_sched_rqst a set sched_id = (select sched_id from ssrtemp1_t where ssrtemp1_t.sched_rqst_set_id = a.sched_rqst_set_id and rownum = 1)
+update KSEN_SCHED_RQST a set SCHED_ID = (select SCHED_ID from SSRTEMP1_T where SSRTEMP1_T.SCHED_RQST_SET_ID = a.SCHED_RQST_SET_ID and ROWNUM = 1)
 
 /
 
-drop table ssrtemp1_t
+drop table SSRTEMP1_T
 
 /
 
 -- Create a temp table for colosets
-create table colosets_t as (select ref_object_id, sched_rqst_set_id from ksen_sched_rqst where ref_object_type = 'kuali.luiset.type.colocated.offering.set')
+create table COLOSETS_T as (select REF_OBJECT_ID, SCHED_RQST_SET_ID from KSEN_SCHED_RQST where REF_OBJECT_TYPE = 'kuali.luiset.type.colocated.offering.set')
 
 /
 
--- Create temp table with srsid, maxind boolean
-create table maxind_t as (select b.sched_rqst_set_id as srsid, a.attr_value as maxind from ksen_lui_set_attr a, colosets_t b where a.owner_id = b.ref_object_id and a.attr_key = 'kuali.attribute.colocatedset.is.max.enrollment.shared')
+-- Create temp table with SRSID, MAXIND boolean
+create table MAXIND_T as (select b.SCHED_RQST_SET_ID as SRSID, a.ATTR_VALUE as MAXIND from KSEN_LUI_SET_ATTR a, COLOSETS_T b where a.OWNER_ID = b.REF_OBJECT_ID and a.ATTR_KEY = 'kuali.attribute.colocatedset.is.max.enrollment.shared')
 
 /
 
--- Create temp table with srsid, maxenr
-create table maxenr_t as (select b.sched_rqst_set_id as srsid, a.attr_value as maxenr from ksen_lui_set_attr a, colosets_t b where a.owner_id = b.ref_object_id and a.attr_key = 'kuali.attribute.colocatedset.max.enrollment')
+-- Create temp table with SRSID, MAXENR
+create table MAXENR_T as (select b.SCHED_RQST_SET_ID as SRSID, a.ATTR_VALUE as MAXENR from KSEN_LUI_SET_ATTR a, COLOSETS_T b where a.OWNER_ID = b.REF_OBJECT_ID and a.ATTR_KEY = 'kuali.attribute.colocatedset.max.enrollment')
 
 /
 
-drop table colosets_t
+drop table COLOSETS_T
 
 /
 
--- Populate MAX_ENROLLMENT in ksen_sched_rqst_set
-update ksen_sched_rqst_set a set max_enrollment = (select to_number(trim(maxenr)) from maxenr_t where maxenr_t.srsid = a.id)
+-- Populate MAX_ENROLLMENT in KSEN_SCHED_RQST_SET
+update KSEN_SCHED_RQST_SET a set MAX_ENROLLMENT = (select TO_NUMBER(TRIM(MAXENR)) from MAXENR_T where MAXENR_T.SRSID = a.ID)
 
 /
 
-drop table maxenr_t
+drop table MAXENR_T
 
 /
 
--- Populate MAX_ENROLLMENT_SHARED_IND in ksen_sched_rqst_set
-update ksen_sched_rqst_set a set max_enrollment_shared_ind = (select
+-- Populate MAX_ENROLLMENT_SHARED_IND in KSEN_SCHED_RQST_SET
+update KSEN_SCHED_RQST_SET a set MAX_ENROLLMENT_SHARED_IND = (select
 case
-      when trim(maxind) = 'Y'  then 1
-      when trim(maxind) = 'N' then 0
+      when TRIM(MAXIND) = 'Y'  then 1
+      when TRIM(MAXIND) = 'N' then 0
     end
-from maxind_t b where b.srsid = a.id)
+from MAXIND_T b where b.SRSID = a.ID)
 
 /
 
-drop table maxind_t
+drop table MAXIND_T
 
 /
 
