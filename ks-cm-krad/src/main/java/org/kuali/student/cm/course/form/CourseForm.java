575aef534f467e0b582730ba4313dce422dcd5ac
pmarais <pmarais@kuali.org>
2013-05-15 11:01:41 -0400

    Jira KSCM-733 : Refactored the CourseForm class. Also updated affected property paths.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/branches/CM-KRAD@48128 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-cm-krad/src/main/java/org/kuali/student/cm/course/form/CourseForm.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/course/form/CourseForm.java
@@ -15,16 +15,9 @@
  */
 package org.kuali.student.cm.course.form;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.kuali.rice.krad.web.form.UifFormBase;
-import org.kuali.student.r2.common.dto.RichTextInfo;
 import org.kuali.student.r2.core.proposal.dto.ProposalInfo;
-import org.kuali.student.r2.lum.clu.dto.CluInstructorInfo;
-import org.kuali.student.r2.lum.course.dto.CourseCrossListingInfo;
-import org.kuali.student.r2.lum.course.dto.CourseJointInfo;
-import org.kuali.student.r2.lum.course.dto.CourseVariationInfo;
+import org.kuali.student.r2.lum.course.dto.CourseInfo;
 
 /**
  * This is the helper class for CourseView
@@ -35,112 +28,22 @@ import org.kuali.student.r2.lum.course.dto.CourseVariationInfo;
 public class CourseForm extends UifFormBase {
 
 	private static final long serialVersionUID = -988885314122936950L;
-
+	
+	private CourseInfo courseInfo;
+	
 	private ProposalInfo proposalInfo;
 
-	private String courseTitle;
-
-	private String transcriptCourseTitle;
-
-	private String subjectArea;
-	
-	private String startTerm;
-	
-	private boolean pilotCourse;
-	
-	private String endTerm;
-
-	private String courseNumberSuffix;
-
-	private List<CourseCrossListingInfo> crossListings;
-
-	private List<CourseJointInfo> joints;
-
-	private List<CourseVariationInfo> variations;
-
-	private List<CluInstructorInfo> instructors;
-
-	private RichTextInfo descr;
-
 	public CourseForm() {
+		this.courseInfo = new CourseInfo();
 		this.proposalInfo = new ProposalInfo();
-		this.crossListings = new ArrayList<CourseCrossListingInfo>();
-		this.joints = new ArrayList<CourseJointInfo>();
-		this.variations = new ArrayList<CourseVariationInfo>();
-		this.instructors = new ArrayList<CluInstructorInfo>();
-		this.descr = new RichTextInfo();
 	}
 
-	public String getCourseTitle() {
-		return courseTitle;
+	public CourseInfo getCourseInfo() {
+		return courseInfo;
 	}
 
-	public void setCourseTitle(String courseTitle) {
-		this.courseTitle = courseTitle;
-	}
-
-	public String getTranscriptCourseTitle() {
-		return transcriptCourseTitle;
-	}
-
-	public void setTranscriptCourseTitle(String transcriptCourseTitle) {
-		this.transcriptCourseTitle = transcriptCourseTitle;
-	}
-
-	public String getSubjectArea() {
-		return subjectArea;
-	}
-
-	public void setSubjectArea(String subjectArea) {
-		this.subjectArea = subjectArea;
-	}
-
-	public String getCourseNumberSuffix() {
-		return courseNumberSuffix;
-	}
-
-	public void setCourseNumberSuffix(String courseNumberSuffix) {
-		this.courseNumberSuffix = courseNumberSuffix;
-	}
-
-	public List<CourseCrossListingInfo> getCrossListings() {
-		return crossListings;
-	}
-
-	public void setCrossListings(List<CourseCrossListingInfo> crossListings) {
-		this.crossListings = crossListings;
-	}
-
-	public List<CourseJointInfo> getJoints() {
-		return joints;
-	}
-
-	public void setJoints(List<CourseJointInfo> joints) {
-		this.joints = joints;
-	}
-
-	public List<CourseVariationInfo> getVariations() {
-		return variations;
-	}
-
-	public void setVariations(List<CourseVariationInfo> variations) {
-		this.variations = variations;
-	}
-
-	public List<CluInstructorInfo> getInstructors() {
-		return instructors;
-	}
-
-	public void setInstructors(List<CluInstructorInfo> instructors) {
-		this.instructors = instructors;
-	}
-
-	public RichTextInfo getDescr() {
-		return descr;
-	}
-
-	public void setDescr(RichTextInfo descr) {
-		this.descr = descr;
+	public void setCourseInfo(CourseInfo courseInfo) {
+		this.courseInfo = courseInfo;
 	}
 
 	public ProposalInfo getProposalInfo() {
@@ -150,29 +53,5 @@ public class CourseForm extends UifFormBase {
 	public void setProposalInfo(ProposalInfo proposalInfo) {
 		this.proposalInfo = proposalInfo;
 	}
-
-    public String getStartTerm() {
-        return startTerm;
-    }
-
-    public void setStartTerm(String startTerm) {
-        this.startTerm = startTerm;
-    }
-
-    public String getEndTerm() {
-        return endTerm;
-    }
-
-    public void setEndTerm(String endTerm) {
-        this.endTerm = endTerm;
-    }
-
-    public boolean isPilotCourse() {
-        return this.pilotCourse;
-    }
-
-    public void setPilotCourse(boolean isPilotCourse) {
-        this.pilotCourse = isPilotCourse;
-    }
-    
+   
 }
