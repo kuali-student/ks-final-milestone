5eda0437dca81e3cd8f5fa48b245456461106783
peggy <peggy@kuali.org>
2013-05-21 07:27:05 -0400

    KSENROLL-6605  Revert Insert sql statement on intial DB to Update sql.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@48441 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/upgrades/M7/2013-05-21-KRMS-add-new-termSpec.sql
@@ -0,0 +1,3 @@
+INSERT INTO KRMS_TERM_SPEC_T (ACTV,DESC_TXT,NM,NMSPC_CD,TERM_SPEC_ID,TYP,VER_NBR)
+  VALUES ('Y','Number of enrollments for course','NumberOfEnrollmentsForCourse','KS-SYS','10015','java.lang.Integer',1)
+/
