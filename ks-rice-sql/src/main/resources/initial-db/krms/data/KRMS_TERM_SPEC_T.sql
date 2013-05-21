adef46c76166b8860d7930ed3f7c7f542974623f
peggy <peggy@kuali.org>
2013-05-21 07:27:05 -0400

    KSENROLL-6605  Revert Insert sql statement on intial DB to Update sql.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@48441 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-rice-sql/src/main/resources/initial-db/krms/data/KRMS_TERM_SPEC_T.sql
+++ ks-rice-sql/src/main/resources/initial-db/krms/data/KRMS_TERM_SPEC_T.sql
@@ -45,6 +45,3 @@ INSERT INTO KRMS_TERM_SPEC_T (ACTV,DESC_TXT,NM,NMSPC_CD,TERM_SPEC_ID,TYP,VER_NBR
 INSERT INTO KRMS_TERM_SPEC_T (ACTV,DESC_TXT,NM,NMSPC_CD,TERM_SPEC_ID,TYP,VER_NBR)
   VALUES ('Y','Instructor Permission Required','InstructorPermissionRequired','KS-SYS','10014','java.lang.Boolean',1)
 /
-INSERT INTO KRMS_TERM_SPEC_T (ACTV,DESC_TXT,NM,NMSPC_CD,TERM_SPEC_ID,TYP,VER_NBR)
-  VALUES ('Y','Number of enrollments for course','NumberOfEnrollmentsForCourse','KS-SYS','10015','java.lang.Integer',1)
-/
