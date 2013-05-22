dd13339174c21d3f4beb7dbb102f083dd90709fc
mwfyffe <mwfyffe@kuali.org>
2013-05-22 08:26:14 -0400

    KSAP-34 roll back Rice trunk patch until 2.3.0-M3
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/branches/KSAP@48549 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ksink/src/main/java/org/kuali/student/enrollment/kitchensink/KitchenSinkUserLoginFilter.java
+++ ks-enroll-ksink/src/main/java/org/kuali/student/enrollment/kitchensink/KitchenSinkUserLoginFilter.java
@@ -33,7 +33,7 @@ import java.io.IOException;
  *
  * @author Kuali Student Team
  */
-public class KitchenSinkUserLoginFilter extends org.kuali.rice.krad.web.filter.UserLoginFilter {
+public class KitchenSinkUserLoginFilter extends org.kuali.rice.kew.web.UserLoginFilter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
