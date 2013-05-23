593c080a7c7898a3ea27ffdcec585244871c4167
dirkjlouw <dirkjlouw@kuali.org>
2013-05-23 06:15:01 -0400

    KSCM-743 : Finalise ActiveDatesPage and lookup
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/branches/CM-KRAD@48631 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-cm-krad/src/main/java/org/kuali/student/lum/lu/ui/course/keyvalues/DatesKeyValuesFinder.java
+++ ks-cm-krad/src/main/java/org/kuali/student/lum/lu/ui/course/keyvalues/DatesKeyValuesFinder.java
@@ -17,11 +17,24 @@ package org.kuali.student.lum.lu.ui.course.keyvalues;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import javax.xml.namespace.QName;
+
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
-import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
-import org.kuali.rice.krad.uif.view.ViewModel;
+import org.kuali.rice.krad.keyvalues.KeyValuesBase;
+import org.kuali.rice.krad.util.GlobalVariables;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.dto.LocaleInfo;
+import org.kuali.student.r2.core.atp.service.AtpService;
+import org.kuali.student.r2.core.constants.AtpServiceConstants;
+import org.kuali.student.r2.core.search.dto.SearchParamInfo;
+import org.kuali.student.r2.core.search.dto.SearchRequestInfo;
+import org.kuali.student.r2.core.search.dto.SearchResultCellInfo;
+import org.kuali.student.r2.core.search.dto.SearchResultInfo;
+import org.kuali.student.r2.core.search.dto.SearchResultRowInfo;
 
 /**
  * This is the helper class for CourseView
@@ -29,32 +42,93 @@ import org.kuali.rice.krad.uif.view.ViewModel;
  * @author OpenCollab/rSmart KRAD CM Conversion Alliance!
  */
 
-public class DatesKeyValuesFinder extends UifKeyValuesFinderBase {
+public class DatesKeyValuesFinder extends KeyValuesBase {
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(DatesKeyValuesFinder.class);
+    
+    private boolean blankOption;
 
-    private static final long serialVersionUID = -177152125891655508L;
+    private AtpService atpService;
 
     @Override
-    public List<KeyValue> getKeyValues(ViewModel model) {
+    public List<KeyValue> getKeyValues() {
+        List<KeyValue> keyValues = new ArrayList<KeyValue>();
+        try {
+            SearchRequestInfo searchRequest = new SearchRequestInfo();
+            searchRequest.setSearchKey("atp.search.advancedAtpSearch");
+            List<SearchParamInfo> queryParamValueList = new ArrayList<SearchParamInfo>();
+            SearchParamInfo param = new SearchParamInfo();
+            param.setKey("atp.advancedAtpSearchParam.atpType"); //"atp.advancedAtpSearchParam.optionalAtpIds
 
-        List<KeyValue> options = new ArrayList<KeyValue>();
+            List<String> typeValues = new ArrayList<String>();
+            typeValues.add(AtpServiceConstants.ATP_SPRING_TYPE_KEY);
+            typeValues.add(AtpServiceConstants.ATP_FALL_TYPE_KEY);
+            typeValues.add(AtpServiceConstants.ATP_SUMMER_TYPE_KEY);
+            typeValues.add(AtpServiceConstants.ATP_SESSION_1_TYPE_KEY);
+            typeValues.add(AtpServiceConstants.ATP_SESSION_2_TYPE_KEY);
+            typeValues.add(AtpServiceConstants.ATP_MINI_MESTER_1_A_TYPE_KEY);
+            typeValues.add(AtpServiceConstants.ATP_MINI_MESTER_1_B_TYPE_KEY);
+            param.setValues(typeValues);
+            queryParamValueList.add(param);
+            searchRequest.setParams(queryParamValueList);
+            SearchResultInfo searchResult = this.getAtpService().search(searchRequest, getContextInfo());
 
-//        if (testForm.getField88().equals("Fruits")) {
-            options.add(new ConcreteKeyValue("Apples", "Apples"));
-            options.add(new ConcreteKeyValue("Bananas", "Bananas"));
-            options.add(new ConcreteKeyValue("Cherries", "Cherries"));
-            options.add(new ConcreteKeyValue("Oranges", "Oranges"));
-            options.add(new ConcreteKeyValue("Pears", "Pears"));
-//        } else if (testForm.getField88().equals("Vegetables")) {
-            options.add(new ConcreteKeyValue("Beans", "Beans"));
-            options.add(new ConcreteKeyValue("Broccoli", "Broccoli"));
-            options.add(new ConcreteKeyValue("Cabbage", "Cabbage"));
-            options.add(new ConcreteKeyValue("Carrots", "Carrots"));
-            options.add(new ConcreteKeyValue("Celery", "Celery"));
-            options.add(new ConcreteKeyValue("Corn", "Corn"));
-            options.add(new ConcreteKeyValue("Peas", "Peas"));
-//        }
+            if(blankOption){
+                keyValues.add(new ConcreteKeyValue("", ""));
+            }
+            
+            for (SearchResultRowInfo result : searchResult.getRows()) {
+                List<SearchResultCellInfo> cells = result.getCells();
+                String id = "";
+                String descrip = "";
+                for (SearchResultCellInfo cell : cells) {
+                    if (cell.getKey().contains("atp.resultColumn.atpId")) {
+                        id = cell.getKey().toString();
+                    }
+                    if (cell.getKey().contains("atp.resultColumn.atpShortName")) {
+                        descrip = cell.getValue().toString();
+                    }
+                }
+                keyValues.add(new ConcreteKeyValue(id, descrip));
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+        return keyValues;
+    }
 
-        return options;
+    private AtpService getAtpService() {
+        if (atpService == null)
+        {
+            QName qname = new QName(AtpServiceConstants.NAMESPACE, AtpServiceConstants.SERVICE_NAME_LOCAL_PART);
+            atpService = (AtpService) GlobalResourceLoader.getService(qname);
+        }
+        return atpService;
+    }
+
+    private ContextInfo getContextInfo() {
+        ContextInfo contextInfo = new ContextInfo();
+        contextInfo.setAuthenticatedPrincipalId(GlobalVariables.getUserSession().getPrincipalId());
+        contextInfo.setPrincipalId(GlobalVariables.getUserSession().getPrincipalId());
+        LocaleInfo localeInfo = new LocaleInfo();
+        localeInfo.setLocaleLanguage(Locale.getDefault().getLanguage());
+        localeInfo.setLocaleRegion(Locale.getDefault().getCountry());
+        contextInfo.setLocale(localeInfo);
+
+        return contextInfo;
+    }
+    
+    /**
+     * @return the blankOption
+     */
+    public boolean isBlankOption() {
+        return this.blankOption;
+    }
+
+    /**
+     * @param blankOption the blankOption to set
+     */
+    public void setBlankOption(boolean blankOption) {
+        this.blankOption = blankOption;
     }
 
 }
