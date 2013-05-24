f805226997fcdc1e11e6b224391ef049957ea5f1
sw <sw@kuali.org>
2013-05-24 03:55:51 -0400

    KSENROLL-7044 Create a wrapper cluset for the clu ids and the membershipqueries.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@48713 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/krms/builder/MultiCourseComponentBuilder.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/krms/builder/MultiCourseComponentBuilder.java
@@ -1,15 +1,11 @@
 package org.kuali.student.enrollment.class1.krms.builder;
 
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
-import org.kuali.rice.krms.api.repository.term.TermDefinition;
-import org.kuali.rice.krms.api.repository.term.TermParameterDefinition;
 import org.kuali.rice.krms.builder.ComponentBuilder;
-import org.kuali.rice.krms.dto.TermEditor;
-import org.kuali.rice.krms.dto.TermParameterEditor;
+import org.kuali.rice.krms.builder.ComponentBuilderUtils;
 import org.kuali.student.enrollment.class1.krms.dto.CluInformation;
 import org.kuali.student.enrollment.class1.krms.dto.CluSetInformation;
 import org.kuali.student.enrollment.class1.krms.dto.EnrolPropositionEditor;
-import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.util.ContextUtils;
 import org.kuali.student.r2.core.search.dto.SearchRequestInfo;
 import org.kuali.student.r2.core.search.dto.SearchResultCellInfo;
@@ -26,7 +22,6 @@ import org.kuali.student.r2.lum.lrc.dto.ResultValuesGroupInfo;
 import org.kuali.student.r2.lum.lrc.service.LRCService;
 import org.kuali.student.r2.lum.util.constants.CluServiceConstants;
 import org.kuali.student.r2.lum.util.constants.LrcServiceConstants;
-import org.springframework.util.ObjectUtils;
 
 import javax.xml.namespace.QName;
 import java.util.*;
@@ -41,7 +36,6 @@ import java.util.*;
 public class MultiCourseComponentBuilder implements ComponentBuilder<EnrolPropositionEditor> {
 
     private CluService cluService;
-
     private LRCService lrcService;
 
     private static final String CLUSET_KEY = "kuali.term.parameter.type.course.cluSet.id";
@@ -72,12 +66,13 @@ public class MultiCourseComponentBuilder implements ComponentBuilder<EnrolPropos
     public Map<String, String> buildTermParameters(EnrolPropositionEditor propositionEditor) {
         Map<String, String> termParameters = new HashMap<String, String>();
         if (propositionEditor.getCluSet() != null) {
-            if(propositionEditor.getCluSet().getCluSetInfo() == null){
-                propositionEditor.getCluSet().setCluSetInfo(this.createCourseSet(propositionEditor.getCluSet()));
+            if (propositionEditor.getCluSet().getCluSetInfo() != null) {
+                termParameters.put(CLUSET_KEY, propositionEditor.getCluSet().getCluSetInfo().getId());
+            } else {
+                termParameters.put(CLUSET_KEY, null);
             }
-            termParameters.put(CLUSET_KEY, propositionEditor.getCluSet().getCluSetInfo().getId());
         }
-        if (propositionEditor.getGradeScale() != null){
+        if (propositionEditor.getGradeScale() != null) {
             termParameters.put(GRADE_TYPE_KEY, propositionEditor.getGradeScale());
             termParameters.put(GRADE_KEY, propositionEditor.getTermParameter());
         }
@@ -88,24 +83,13 @@ public class MultiCourseComponentBuilder implements ComponentBuilder<EnrolPropos
     public void onSubmit(EnrolPropositionEditor propositionEditor) {
         //Create the courseset
         try {
+            propositionEditor.getCluSet().setCluSetInfo(this.buildCourseSet(propositionEditor.getCluSet()));
             CluSetInfo cluSetInfo = propositionEditor.getCluSet().getCluSetInfo();
-            if(cluSetInfo.getId()==null){
+            if (cluSetInfo.getId() == null) {
                 cluSetInfo = this.getCluService().createCluSet(cluSetInfo.getTypeKey(), cluSetInfo, ContextUtils.getContextInfo());
+                ComponentBuilderUtils.updateTermParameter(propositionEditor.getTerm(), CLUSET_KEY, cluSetInfo.getId());
 
-                //Update the termparameter value
-                boolean updated = false;
-                for(TermParameterEditor parm : propositionEditor.getTerm().getEditorParameters()){
-                    if(parm.getName().equals(CLUSET_KEY)){
-                        parm.setValue(cluSetInfo.getId());
-                        updated = true;
-                    }
-                }
-
-                if(!updated){
-                    TermParameterEditor parm = new TermParameterEditor(CLUSET_KEY, cluSetInfo.getId());
-                    propositionEditor.getTerm().getEditorParameters().add(parm);
-                }
-            }else{
+            } else {
                 this.getCluService().updateCluSet(cluSetInfo.getId(), cluSetInfo, ContextUtils.getContextInfo());
             }
         } catch (Exception ex) {
@@ -113,54 +97,48 @@ public class MultiCourseComponentBuilder implements ComponentBuilder<EnrolPropos
         }
     }
 
+    /**
+     * This methos assumes that there can only be a maximum of one wrapped cluset
+     * for cluids and one for membershipqueries.
+     *
+     * @param cluSetId
+     * @return
+     */
     public CluSetInformation getCluSetInformation(String cluSetId) {
+
         CluSetInformation result = new CluSetInformation();
-        CluSetInfo cluSetInfo = getCluSetInfo(cluSetId);
-        result.setCluSetInfo(cluSetInfo);
+        result.setCluSetInfo(this.getCluSetInfo(cluSetId));
 
-        //Set the Clu Informations
-        List<CluInformation> clus = getCluInformations(cluSetInfo.getCluIds());
-        result.setClus(clus);
+        List<String> cluIds = result.getCluSetInfo().getCluIds();
+        result.setMembershipQueryInfo(result.getCluSetInfo().getMembershipQuery());
 
-        //Set the Clu set infos
-        List<CluSetInfo> cluSetInfos = getCluSetInfos(cluSetInfo.getCluSetIds());
-        result.setCluSets(cluSetInfos);
+        // goes through the list of sub clusets and ignore the ones that are not reusable
+        List<CluSetInfo> cluSetInfos = getCluSetInfos(result.getCluSetInfo().getCluSetIds());
+        if (cluSetInfos != null) {
+            List<CluSetInfo> unWrappedCluSets = new ArrayList<CluSetInfo>();
+            for (CluSetInfo subCluSet : cluSetInfos) {
+                if (subCluSet.getIsReusable()) {
+                    unWrappedCluSets.add(subCluSet);
+                } else {
 
-        //Query info.
-        final MembershipQueryInfo membershipQueryInfo = cluSetInfo.getMembershipQuery();
-        if (membershipQueryInfo != null) {
-            SearchRequestInfo searchRequest = new SearchRequestInfo();
-            searchRequest.setSearchKey(membershipQueryInfo.getSearchTypeKey());
-            searchRequest.setParams(membershipQueryInfo.getQueryParamValues());
-            SearchResultInfo searchResult = null;
-            try {
-                searchResult = this.getCluService().search(searchRequest, ContextUtils.getContextInfo());
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-            List<CluInformation> clusInRange = new ArrayList<CluInformation>();
-            List<SearchResultRowInfo> rows = searchResult.getRows();
-            for (SearchResultRowInfo row : rows) {
-                List<SearchResultCellInfo> cells = row.getCells();
-                CluInformation cluInformation = new CluInformation();
-                for (SearchResultCellInfo cell : cells) {
-                    if (cell.getKey().equals("lu.resultColumn.cluId")) {
-                        cluInformation.setCluId(cell.getValue());
+                    //Retrieve the information from the wrapped clu cluset.
+                    if (subCluSet.getCluIds() != null && !subCluSet.getCluIds().isEmpty()) {
+                        cluIds = subCluSet.getCluIds();
                     }
-                    if (cell.getKey().equals("lu.resultColumn.luOptionalCode")) {
-                        cluInformation.setCode(cell.getValue());
-                    }
-                    if (cell.getKey().equals("lu.resultColumn.luOptionalShortName")) {
-                        cluInformation.setTitle(cell.getValue());
+
+                    //Retrieve the information from the wrapped membership cluset.
+                    MembershipQueryInfo mqInfo = subCluSet.getMembershipQuery();
+                    if (mqInfo != null && mqInfo.getSearchTypeKey() != null && !mqInfo.getSearchTypeKey().isEmpty()) {
+                        result.setMembershipQueryInfo(mqInfo);
                     }
                 }
-                clusInRange.add(cluInformation);
             }
-            result.setMembershipQueryInfo(membershipQueryInfo);
-            result.setClusInRange(clusInRange);
+            result.setCluSets(unWrappedCluSets);
         }
-        if (result.getClus() != null)
-            Collections.sort(result.getClus());
+
+        result.setClus(this.getCluInformations(cluIds));
+        result.setClusInRange(this.getClusInRange(result.getMembershipQueryInfo()));
+
         return result;
     }
 
@@ -168,10 +146,24 @@ public class MultiCourseComponentBuilder implements ComponentBuilder<EnrolPropos
         List<CluSetInfo> clusetInfos = new ArrayList<CluSetInfo>();
         if (cluSetIds != null) {
             for (String cluSetId : cluSetIds) {
-                clusetInfos.add(getCluSetInfo(cluSetId));
+                clusetInfos.add(this.getCluSetInfo(cluSetId));
             }
         }
         return clusetInfos;
+    }
+
+    private CluSetInfo getCluSetInfo(String cluSetId) {
+        CluSetInfo cluSetInfo = null;
+        try {
+            // note: the cluIds returned by cluService.getCluSetInfo also contains the clus
+            //       that are the result of query parameter search.  Set to null here and
+            //       retrieve the clus that are direct members.
+            cluSetInfo = this.getCluService().getCluSet(cluSetId, ContextUtils.getContextInfo());
+            cluSetInfo.setCluIds(this.getCluService().getCluIdsFromCluSet(cluSetId, ContextUtils.getContextInfo()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return cluSetInfo;
     }
 
     private List<CluInformation> getCluInformations(List<String> cluIds) {
@@ -261,97 +253,124 @@ public class MultiCourseComponentBuilder implements ComponentBuilder<EnrolPropos
                 }
             }
         }
+        if (result != null) {
+            Collections.sort(result);
+        }
         return result;
     }
 
-    private void upWrap(CluSetInfo cluSetInfo, ContextInfo contextInfo) {
-        List<String> cluSetIds = (cluSetInfo == null) ? null : cluSetInfo.getCluSetIds();
-        List<String> unWrappedCluSetIds = null;
-        List<CluSetInfo> wrappedCluSets = null;
-        List<CluSetInfo> subCluSets = null;
-
-        try {
-            if (cluSetIds != null && !cluSetIds.isEmpty()) {
-                subCluSets = this.getCluService().getCluSetsByIds(cluSetIds, contextInfo);
+    public List<CluInformation> getClusInRange(MembershipQueryInfo membershipQueryInfo) {
+        //Query info.
+        if (membershipQueryInfo != null) {
+            SearchRequestInfo searchRequest = new SearchRequestInfo();
+            searchRequest.setSearchKey(membershipQueryInfo.getSearchTypeKey());
+            searchRequest.setParams(membershipQueryInfo.getQueryParamValues());
+            SearchResultInfo searchResult = null;
+            try {
+                searchResult = this.getCluService().search(searchRequest, ContextUtils.getContextInfo());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        // goes through the list of sub clusets and ignore the ones that are not reusable
-        if (subCluSets != null) {
-            for (CluSetInfo subCluSet : subCluSets) {
-                if (subCluSet.getIsReusable()) {
-                    unWrappedCluSetIds = (unWrappedCluSetIds == null) ?
-                            new ArrayList<String>() : unWrappedCluSetIds;
-                    unWrappedCluSetIds.add(subCluSet.getId());
-                } else {
-                    wrappedCluSets = (wrappedCluSets == null) ?
-                            new ArrayList<CluSetInfo>() : wrappedCluSets;
-                    wrappedCluSets.add(subCluSet);
+            List<CluInformation> clusInRange = new ArrayList<CluInformation>();
+            List<SearchResultRowInfo> rows = searchResult.getRows();
+            for (SearchResultRowInfo row : rows) {
+                List<SearchResultCellInfo> cells = row.getCells();
+                CluInformation cluInformation = new CluInformation();
+                for (SearchResultCellInfo cell : cells) {
+                    if (cell.getKey().equals("lu.resultColumn.cluId")) {
+                        cluInformation.setCluId(cell.getValue());
+                    }
+                    if (cell.getKey().equals("lu.resultColumn.luOptionalCode")) {
+                        cluInformation.setCode(cell.getValue());
+                    }
+                    if (cell.getKey().equals("lu.resultColumn.luOptionalShortName")) {
+                        cluInformation.setTitle(cell.getValue());
+                    }
                 }
+                clusInRange.add(cluInformation);
             }
+            return clusInRange;
         }
-        cluSetInfo.setCluSetIds(unWrappedCluSetIds);
-        if (wrappedCluSets != null) {
-            for (CluSetInfo wrappedCluSet : wrappedCluSets) {
-                MembershipQueryInfo mqInfo = wrappedCluSet.getMembershipQuery();
-                if (wrappedCluSet.getCluIds() != null && !wrappedCluSet.getCluIds().isEmpty()) {
-                    cluSetInfo.setCluIds(wrappedCluSet.getCluIds());
-                }
-                if (mqInfo != null && mqInfo.getSearchTypeKey() != null && !mqInfo.getSearchTypeKey().isEmpty()) {
-                    cluSetInfo.setMembershipQuery(mqInfo);
-                }
-            }
-        }
+        return null;
     }
 
-    private CluSetInfo getCluSetInfo(String cluSetId) {
-        List<String> cluIds = null;
-        CluSetInfo cluSetInfo = null;
-        try {
-            // note: the cluIds returned by cluService.getCluSetInfo also contains the clus
-            //       that are the result of query parameter search.  Set to null here and
-            //       retrieve the clus that are direct members.
-            cluSetInfo = this.getCluService().getCluSet(cluSetId, ContextUtils.getContextInfo());
-            cluSetInfo.setCluIds(null);
-            cluIds = this.getCluService().getCluIdsFromCluSet(cluSetId, ContextUtils.getContextInfo());
-            cluSetInfo.setCluIds(cluIds);
-            upWrap(cluSetInfo, ContextUtils.getContextInfo());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return cluSetInfo;
-    }
+    public CluSetInfo buildCourseSet(CluSetInformation cluSetInformation) {
 
-    public CluSetInfo createCourseSet(CluSetInformation cluSetInformation) {
-        CluSetInfo cluSetInfo = new CluSetInfo();
+        // Create a Cluset if not exist.
+        if (cluSetInformation.getCluSetInfo() == null) {
+            cluSetInformation.setCluSetInfo(new CluSetInfo());
+        }
+
+        // Set default properties.
+        CluSetInfo cluSetInfo = cluSetInformation.getCluSetInfo();
         cluSetInfo.setTypeKey(CluServiceConstants.CLUSET_TYPE_CREDIT_COURSE);
         cluSetInfo.setStateKey("Active");
-        cluSetInfo.setName("adhoc");
+        cluSetInfo.setName("AdHock");
         cluSetInfo.setEffectiveDate(new Date());
         cluSetInfo.setIsReferenceable(Boolean.TRUE);
         cluSetInfo.setIsReusable(Boolean.FALSE);
 
-        //Set the clu ids on the cluset
-        if(cluSetInformation.getClus()!=null){
-            for(CluInformation clu : cluSetInformation.getClus()){
-                cluSetInfo.getCluIds().add(clu.getVerIndependentId());
-            }
-        }
+        //Clear all current values
+        cluSetInfo.getCluSetIds().clear();
+        cluSetInfo.setMembershipQuery(null);
+        cluSetInfo.getCluIds().clear();
+
+        boolean hasCluIds = cluSetInformation.hasClus();
+        boolean hasMembershipQuery = cluSetInformation.hasMembershipQuery();
 
         //Set the cluset ids on the cluset
-        if(cluSetInformation.getCluSets()!=null){
-            for(CluSetInfo cluset : cluSetInformation.getCluSets()){
+        if ((cluSetInformation.getCluSets() == null) && (cluSetInformation.getCluSets().isEmpty())) {
+            if (hasCluIds && !hasMembershipQuery) {
+                cluSetInfo.setCluIds(cluSetInformation.getCluIds());
+                return cluSetInfo;
+            } else if (!hasCluIds && hasMembershipQuery) {
+                cluSetInfo.setMembershipQuery(cluSetInformation.getMembershipQueryInfo());
+                return cluSetInfo;
+            }
+        } else {
+            for (CluSetInfo cluset : cluSetInformation.getCluSets()) {
                 cluSetInfo.getCluSetIds().add(cluset.getId());
             }
         }
 
-        //Set the membershipquery on the cluset
-        if(cluSetInformation.getMembershipQueryInfo()!=null){
-            cluSetInfo.setMembershipQuery(cluSetInformation.getMembershipQueryInfo());
+        if (hasCluIds) {
+            CluSetInfo wrapperCluSet = new CluSetInfo();
+            wrapperCluSet.setCluIds(cluSetInformation.getCluIds());
+            cluSetInfo.getCluSetIds().add(saveWrapperCluSet(wrapperCluSet, cluSetInformation.getCluSetInfo()));
+        }
+
+        if (hasMembershipQuery) {
+            CluSetInfo wrapperCluSet = new CluSetInfo();
+            wrapperCluSet.setMembershipQuery(cluSetInformation.getMembershipQueryInfo());
+            cluSetInfo.getCluSetIds().add(saveWrapperCluSet(wrapperCluSet, cluSetInformation.getCluSetInfo()));
         }
 
         return cluSetInfo;
+    }
+
+    private String saveWrapperCluSet(CluSetInfo wrapperCluSet, CluSetInfo cluSetInfo) {
+
+        //Set the properties to match parent cluset.
+        wrapperCluSet.setAdminOrg(cluSetInfo.getAdminOrg());
+        wrapperCluSet.setEffectiveDate(cluSetInfo.getEffectiveDate());
+        wrapperCluSet.setExpirationDate(cluSetInfo.getExpirationDate());
+        wrapperCluSet.setIsReusable(false);
+        wrapperCluSet.setIsReferenceable(false);
+        wrapperCluSet.setName(cluSetInfo.getName());
+        wrapperCluSet.setStateKey(cluSetInfo.getStateKey());
+        wrapperCluSet.setTypeKey(cluSetInfo.getTypeKey());
+
+        try {
+            if (wrapperCluSet.getId() == null) {
+                wrapperCluSet = cluService.createCluSet(wrapperCluSet.getTypeKey(), wrapperCluSet, ContextUtils.getContextInfo());
+            } else {
+                cluService.updateCluSet(wrapperCluSet.getId(), wrapperCluSet, ContextUtils.getContextInfo());
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        return wrapperCluSet.getId();
     }
 
     protected CluService getCluService() {
