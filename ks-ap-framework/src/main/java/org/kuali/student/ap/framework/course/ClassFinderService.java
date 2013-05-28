fbad100c4e4922de6555b70b945f624621b13fb1
mwfyffe <mwfyffe@kuali.org>
2013-05-28 07:08:34 -0400

    KSAP-34 roll back class finder interface work
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@48825 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-framework/src/main/java/org/kuali/student/ap/framework/course/ClassFinderService.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.student.ap.framework.course;
-
-public interface ClassFinderService {
-
-	String saveCriteria(ClassFinderForm form);
-
-	void restoreCriteria(String key, ClassFinderForm form);
-
-}
