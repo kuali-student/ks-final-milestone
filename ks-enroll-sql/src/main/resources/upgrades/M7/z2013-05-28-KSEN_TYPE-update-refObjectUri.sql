2636bee5b9a53aee847faa569032e91f0b569134
lpan <lpan@kuali.org>
2013-05-29 13:26:19 -0400

    ksenroll-6479: rollback changes on ref_object_uri because it breaks create co
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/branches/sched-service@48913 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/upgrades/M7/z2013-05-28-KSEN_TYPE-update-refObjectUri.sql
+++
@@ -1,4 +0,0 @@
-update ksen_type set ref_object_uri = 'http://student.kuali.org/wsdl/courseOffering/FormatOfferingInfo' where type_key='kuali.lui.type.course.format.offering'
-/
-update ksen_type set ref_object_uri = 'http://student.kuali.org/wsdl/courseOffering/CourseOfferingInfo' where type_key='kuali.lui.type.course.offering'
-/
