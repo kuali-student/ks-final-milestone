e9070930508f7e8c81a64e078aa20bd5376ac071
miki.harmath <miki.harmath@kuali.org>
2013-05-29 14:39:24 -0400

    KSENROLL-7179 Rollover Final tweaks
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@48919 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/controller/CourseOfferingRolloverController.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/controller/CourseOfferingRolloverController.java
@@ -660,12 +660,15 @@ public class CourseOfferingRolloverController extends UifControllerBase {
     public ModelAndView confirmReleaseToDepts(@ModelAttribute("KualiForm") CourseOfferingRolloverManagementForm form, @SuppressWarnings("unused") BindingResult result,
                                               @SuppressWarnings("unused") HttpServletRequest request, @SuppressWarnings("unused") HttpServletResponse response) throws Exception {
         LOGGER.info("confirmReleaseToDepts ");
-        if (!hasDialogBeenDisplayed("releaseToDepts", form)){
+
+//        if (!hasDialogBeenDisplayed("releaseToDepts", form)){
+        if (form.getActionParamaterValue("confirm") == null || form.getActionParamaterValue("confirm").equals("")) {
             // redirect back to client to display lightbox
             return showDialog("releaseToDepts", form, request, response);
         } else if (form.getActionParamaterValue("confirm").equals("cancel") ){
             form.getDialogManager().removeAllDialogs();
             form.setLightboxScript("closeLightbox('releaseToDepts');");
+            form.getDialogManager().resetDialogStatus("releaseToDepts");
         } else if (form.getActionParamaterValue("confirm").equals("do") ){
             form = releaseToDepts(form, result, request, response);
             form.getDialogManager().removeAllDialogs();
