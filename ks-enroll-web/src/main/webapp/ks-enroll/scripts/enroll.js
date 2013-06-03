b9618c1c70ee29e06029fa786770614ec85eb937
hossein.aliabadi <hossein.aliabadi@kuali.org>
2013-06-03 10:26:37 -0400

    KSENROLL-7148: Added code to highlight invalid controls. It also displays a message in a popup when hovered over.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@49118 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-web/src/main/webapp/ks-enroll/scripts/enroll.js
+++ ks-enroll-web/src/main/webapp/ks-enroll/scripts/enroll.js
@@ -102,199 +102,6 @@ function renameDialogButtons(labelsToReplace) {
     });
 }
 
-function createErrorDivFor(parent, message, url) {
-    var parentId = jQuery(parent).attr("id");
-    var childId = parentId + "_errors";
-    var div1 = jQuery("<div id='" + childId + "' class='uif-validationMessages' style='display: none' data-messagesfor='" + parentId + "'>");
-    var div2 = jQuery("<div class='uif-clientMessageItems uif-clientErrorDiv'>");
-    jQuery(div1).append(div2);
-    var ul = jQuery("<ul/>");
-    var li = jQuery("<li class='uif-errorMessageItem-field'/>");
-    var image = jQuery("<img class='uif-validationImage' src='" + url + "/krad/images/validation/error.png' alt='Error'/>");
-    jQuery(image).text(message);
-    jQuery(li).append(image);
-    jQuery(ul).append(li);
-    jQuery(div2).append(ul);
-
-    return div1;
-}
-
-
-function validateCredits(textBox, url, courseTypeKey) {
-    var table = jQuery('<table id="errorTable" style="display: none; position: absolute;"/>');
-    var tbody = jQuery('<tbody/>');
-    jQuery(table).append(tbody);
-    var tr1 = jQuery('<tr/>');
-    jQuery(tr1).append('<td class="jquerybubblepopup-top-left" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/top-left.png); padding : 0px;"/>');
-    jQuery(tr1).append('<td class="jquerybubblepopup-top-middle" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/top-middle.png); padding : 0px;"/>');
-    jQuery(tr1).append('<td class="jquerybubblepopup-top-right" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/top-right.png); padding : 0px;"/>');
-    var tr2 = jQuery('<tr/>');
-    jQuery(tr2).append('<td class="jquerybubblepopup-middle-left" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/middle-left.png); padding : 0px;"/>');
-    var td2 = jQuery('<td class="jquerybubblepopup-innerHtml" style="padding : 0px;" />');
-    jQuery(tr2).append(td2);
-    var div2 = jQuery('<div class="uif-clientMessageItems uif-clientErrorDiv"/>');
-    jQuery(td2).append(div2);
-    var ul = jQuery('<ul style="padding : 0px;">');
-    jQuery(div2).append(ul);
-    var li = jQuery('<li class="uif-errorMessageItem-field"/>');
-    jQuery(ul).append(li);
-    var image2 = jQuery('<img class="uif-validationImage" src="' + url + '/krad/images/validation/error.png" alt="Error" />');
-    jQuery(li).append(image2);
-    jQuery(tr2).append('<td class="jquerybubblepopup-middle-right" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/middle-right.png); padding : 0px;"/>');
-
-    var tr3 = jQuery('<tr/>');
-    jQuery(tr3).append('<td class="jquerybubblepopup-bottom-left" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/bottom-left.png); padding : 0px;"/>');
-    var td3 = jQuery('<td class="jquerybubblepopup-bottom-middle" style="background-image: url(http://localhost:8081/ks-with-rice-bundled-dev/krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/bottom-middle.png); text-align: left; padding : 0px; "/>');
-    var image3 = jQuery('<img src="../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/tail-bottom.png" alt="" class="jquerybubblepopup-tail"/>');
-    jQuery(td3).append(image3);
-    jQuery(tr3).append(td3);
-    jQuery(tr3).append('<td class="jquerybubblepopup-bottom-right" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/bottom-right.png); padding : 0px;"/>');
-    jQuery(tbody).append(tr1);
-    jQuery(tbody).append(tr2);
-    jQuery(tbody).append(tr3);
-    jQuery(document).append(table);
-
-    if (courseTypeKey) {
-        if (jQuery(textBox).val().trim() != '') {
-            var foundMatch = false;
-            var textValue;
-            var labelValue;
-            var allowedValues = '';
-            jQuery("input[name='document.newMaintainableObject.dataObject.creditOption.credits']").each(function () {
-                var labelForId = jQuery(this).attr("id");
-                textValue = jQuery(textBox).val().trim();
-                var label = jQuery("label[for='" + labelForId + "']");
-                labelValue = parseFloat(jQuery(label).text());
-                allowedValues += labelValue + ', ';
-                if (textValue == labelValue) {
-                    foundMatch = true;
-                }
-            });
-
-            var lastComaIndex = allowedValues.lastIndexOf(',');
-            if (lastComaIndex > 0) {
-                allowedValues = allowedValues.substring(0, lastComaIndex);
-            }
-
-            var div = jQuery(jQuery(textBox)).closest('div');
-            var errorMessage = "Please enter one of the values: " + allowedValues;
-            //var errorChild = createErrorDivFor(div, errorMessage  , url);
-            //jQuery(div).append(errorChild);
-            div.find('#errorTable').remove();
-            if (!foundMatch) {
-                jQuery(textBox).addClass("error").removeClass("valid");
-                jQuery(textBox).attr("aria-invalid", "true");
-                jQuery(div).addClass("uif-hasError");
-                if (jQuery(div).find('img').length == 0) {
-                    jQuery(div).append('<img class="uif-validationImage" src="' + url + '/krad/images/validation/error.png" alt="Error" />');
-                }
-                // jQuery(div).attr('title', 'Allowed values are: ' + allowedValues);
-                jQuery(li).append(errorMessage);
-                jQuery(div).prepend(table);
-                var moveLeft = -20;
-                var moveDown = -60;
-                jQuery(div).hover(
-                    function (e) {
-                        jQuery('table#errorTable').show().css('top', jQuery(textBox).offset().top + moveDown).css('left', jQuery(textBox).offset().left + moveLeft);
-                    },
-                    function (e) {
-                        jQuery('table#errorTable').hide();
-                    }
-                );
-            } else {
-                if (jQuery(textBox).attr("aria-invalid") != undefined) {
-                    jQuery(textBox).attr("aria-invalid").remove();
-                }
-                jQuery(textBox).addClass("valid").removeClass("error");
-                jQuery(div).removeClass("uif-hasError");
-                jQuery(div).find('img').remove();
-                jQuery(div).unbind('mouseenter mouseleave');
-            }
-        }
-    }
-}
-
-function showErrorPopup(formId) {
-
-    var div1 = jQuery('<div class="fancybox-wrap fancybox-desktop fancybox-type-html fancybox-opened" style="width: 418px; height: auto; display: block; position: fixed; top: 474px; left: 743px; opacity: 1;">');
-    var div2 = jQuery('<div class="fancybox-skin" style="padding: 15px;">');
-    var div3 = jQuery('<div class="fancybox-outer">');
-    var div4 = jQuery('<div class="fancybox-inner" style="width: 388px; height: auto; overflow: auto;">');
-    var text = 'The form contains errors. Please correct these errors and try again.';
-    var div5 = jQuery('<div title="Close" class="fancybox-item fancybox-close">');
-
-    jQuery(div1).append(div2);
-    jQuery(div2).append(div3);
-    jQuery(div3).append(div4);
-    jQuery(div4).text(text);
-    jQuery(div4).append(div5);
-
-    jQuery(div1).appendTo(jQuery('body'));
-    jQuery(div1).show();
-    jQuery('#formId').unbind('submit');
-    return false;
-}
-
-function showFixedOptions(textBox, url, courseTypeKey) {
-    var parent = jQuery("#KS-CourseOfferingEdit-CreditType_OptionTypeFixed");
-    jQuery(textBox).attr("disabled", "disabled");
-
-    var child;
-
-    if (jQuery(parent).find('#div_fixed_options').length > 0) {
-        child = jQuery('#div_fixed_options');
-    } else {
-        child = jQuery('<div id="div_fixed_options" style="position: absolute; display: none; border: 1px solid black; line-style: none;"/>');
-        jQuery(child).width(jQuery(textBox).width() + 10);
-        jQuery(child).css("background-color", jQuery(textBox).css("background-color"));
-        jQuery(child).css("opacity", "2");
-        jQuery(parent).append(child);
-
-        var ul = jQuery('<ul/>');
-        var emptyLi = jQuery('<li/>');
-        jQuery(emptyLi).text(jQuery(textBox).val());
-        emptyLi.click(function () {
-            jQuery(textBox).val(jQuery(this).text());
-            jQuery(textBox).removeAttr("disabled");
-            jQuery('#div_fixed_options').hide();
-        });
-        jQuery(ul).append(emptyLi);
-
-        jQuery(child).append(ul);
-        jQuery("input[name='document.newMaintainableObject.dataObject.creditOption.credits']").each(function () {
-            var labelForId = jQuery(this).attr("id");
-            textValue = jQuery(textBox).val().trim();
-            var label = jQuery("label[for='" + labelForId + "']");
-            labelValue = parseFloat(jQuery(label).text());
-
-            var li = jQuery('<li/>');
-            jQuery(li).text(labelValue);
-            jQuery(ul).append(li);
-
-            li.click(function () {
-                jQuery(textBox).val(jQuery(this).text());
-                jQuery(textBox).removeAttr("disabled");
-                jQuery('#div_fixed_options').hide();
-            });
-
-        });
-    }
-
-    jQuery(child).mouseleave(function () {
-        jQuery(textBox).removeAttr("disabled");
-        jQuery('#div_fixed_options').hide();
-        //jQuery(parent).find('#div_fixed_options').remove();
-    });
-
-    jQuery(textBox).mouseenter(function () {
-        jQuery(textBox).attr("disabled", "disabled");
-        jQuery('#div_fixed_options').hide();
-        jQuery('#div_fixed_options').show().css('top', jQuery(textBox).offset().top).css('left', jQuery(textBox).offset().left);
-    });
-
-    jQuery('#div_fixed_options').show().css('top', jQuery(textBox).offset().top).css('left', jQuery(textBox).offset().left);
-}
-
 function removeCheckboxColumns(column, componentId, functionToCall) {
     var components = jQuery('div[id^="' + componentId + '"]');
     jQuery.each(components, function (index) {
@@ -824,8 +631,88 @@ function removeZebraColoring(id){
 
 }
 
-function highlightElements(validationJSONString, isValid){
+function createErrorDiv(message, url, controlId) {
+    var div = jQuery('<div id="' + controlId + '_messageDiv" class="uif-clientMessageItems uif-clientErrorDiv" style="display: none;"/>');
+    var ul = jQuery("<ul/>");
+    var li = jQuery("<li class='uif-errorMessageItem-field'/>");
+    var image = jQuery("<img class='uif-validationImage' src='" + url + "/krad/images/validation/error.png' alt='Error'/>");
+    jQuery(image).text(message);
+    jQuery(li).append(image);
+    jQuery(ul).append(li);
+    jQuery(div).append(ul);
 
+    return div;
+}
+
+function createErrorTable(url) {
+    var table = jQuery("#errorTable");
+    if (jQuery(table).length) {
+        return table;
+    }
+    table = jQuery('<table id="errorTable" style="display: none; position: absolute;"/>');
+    var tbody = jQuery('<tbody/>');
+    jQuery(table).append(tbody);
+    var tr1 = jQuery('<tr/>');
+    jQuery(tr1).append('<td class="jquerybubblepopup-top-left" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/top-left.png); padding : 0px;"/>');
+    jQuery(tr1).append('<td class="jquerybubblepopup-top-middle" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/top-middle.png); padding : 0px;"/>');
+    jQuery(tr1).append('<td class="jquerybubblepopup-top-right" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/top-right.png); padding : 0px;"/>');
+    var tr2 = jQuery('<tr/>');
+    jQuery(tr2).append('<td class="jquerybubblepopup-middle-left" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/middle-left.png); padding : 0px;"/>');
+    var td2 = jQuery('<td class="jquerybubblepopup-innerHtml" style="padding : 0px;" />');
+    jQuery(tr2).append(td2);
+    var div2 = jQuery('<div id="_messageDiv" class="uif-clientMessageItems uif-clientErrorDiv"/>');
+    jQuery(td2).append(div2);
+    jQuery(tr2).append('<td class="jquerybubblepopup-middle-right" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/middle-right.png); padding : 0px;"/>');
+    var tr3 = jQuery('<tr/>');
+    jQuery(tr3).append('<td class="jquerybubblepopup-bottom-left" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/bottom-left.png); padding : 0px;"/>');
+    var td3 = jQuery('<td class="jquerybubblepopup-bottom-middle" style="background-image: url(http://localhost:8081/ks-with-rice-bundled-dev/krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/bottom-middle.png); text-align: left; padding : 0px; "/>');
+    var image3 = jQuery('<img src="../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/tail-bottom.png" alt="" class="jquerybubblepopup-tail"/>');
+    jQuery(td3).append(image3);
+    jQuery(tr3).append(td3);
+    jQuery(tr3).append('<td class="jquerybubblepopup-bottom-right" style="background-image:url(../krad/plugins/tooltip/jquerybubblepopup-theme/kr-error-cs/bottom-right.png); padding : 0px;"/>');
+    jQuery(tbody).append(tr1);
+    jQuery(tbody).append(tr2);
+    jQuery(tbody).append(tr3);
+    jQuery(document.body).append(table);
+    return table;
+}
+
+
+function highlightElements(validationJSONString, isValid, url) {
+    if (!isValid) {
+        var table = createErrorTable(url);
+        jQuery.each(validationJSONString, function (id, message) {
+            var errorContainer = jQuery('[id^="' + id + '"][id$="_errors"]:first');
+            var controlDiv = jQuery('[id^="' + id + '"][id$="_control"]:first');
+            var parentDiv = jQuery(controlDiv).parent().closest('div');
+            var id = jQuery(parentDiv).attr("id");
+            var errorDiv = createErrorDiv(message, url, id);
+            console.log(jQuery(errorDiv).text());
+            //var table = createErrorTable(url);
+
+            jQuery(parentDiv).append(errorDiv);
+            jQuery(controlDiv).addClass("ks-uif-hasError");
+            jQuery(controlDiv).hover(
+                function (e) {
+                    var moveLeft = -20;
+                    var moveDown = -60;
+                    //var table = createErrorTable(url);
+                    var id = jQuery(this).parent().closest('div').attr("id");
+                    var thisErrorDiv = jQuery('#' + id + '_messageDiv');
+                    console.log(jQuery(thisErrorDiv).html());
+                    console.log(jQuery(thisErrorDiv).text());
+                    var div = jQuery(table).find('[id$="_messageDiv"]');
+                    jQuery(table).find('[id$="_messageDiv"]').html(jQuery(thisErrorDiv).html());
+                    jQuery(table).find('[id$="_messageDiv"]').text(jQuery(thisErrorDiv).text());
+                    console.log(jQuery(jQuery(table).find('[id$="_messageDiv"]')).text());
+                    jQuery('table#errorTable').show().css('top', jQuery(this).offset().top + moveDown).css('left', jQuery(this).offset().left + moveLeft);
+                },
+                function (e) {
+                    jQuery('table#errorTable').hide();
+                }
+            );
+        });
+    }
 }
 
 function updateContextBar(){
