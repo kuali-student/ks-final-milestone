4cc6ed479756eb794e039f5da8dcd0a3c9cfba95
yindavid <yindavid@kuali.org>
2013-06-04 10:23:37 -0400

    KSENROLL-5693: update using type_key instead of obj_id in the WHERE clause
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@49192 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/upgrades/M7/2013-05-30-11-KSEN_TYPE.sql
+++ ks-enroll-sql/src/main/resources/upgrades/M7/2013-05-30-11-KSEN_TYPE.sql
@@ -1,34 +1,34 @@
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Last Day of Classes', DESCR_PLAIN = 'Last Day of Classes', NAME = 'Last Day of Classes' WHERE OBJ_ID = '2D56AC48BB1A48C58A1D3A3867D8D383'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Last Day of Classes', DESCR_PLAIN = 'Last Day of Classes', NAME = 'Last Day of Classes' WHERE TYPE_KEY='kuali.atp.milestone.lastdayofclasses'
 /
 UPDATE KSEN_TYPE SET NAME = 'Instructional Period' WHERE TYPE_KEY = 'kuali.atp.milestone.InstructionalPeriod'
 /
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Junior Registration', DESCR_PLAIN = 'Junior Registration', NAME = 'Junior Registration' WHERE OBJ_ID = '4DA04C7DC3C54329B9A8C38FC3FD8F81'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Junior Registration', DESCR_PLAIN = 'Junior Registration', NAME = 'Junior Registration' WHERE TYPE_KEY = 'kuali.atp.milestone.JuniorRegistration'
 /
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Freshmen Registration', DESCR_PLAIN = 'Freshmen Registration', NAME = 'Freshmen Registration' WHERE OBJ_ID = 'C7A49AB20B1F419D84C9D3A341A14149'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Freshmen Registration', DESCR_PLAIN = 'Freshmen Registration', NAME = 'Freshmen Registration' WHERE TYPE_KEY = 'kuali.atp.milestone.FreshmenRegistration'
 /
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'New Student Orientation', DESCR_PLAIN = 'New Student Orientation', NAME = 'New Student Orientation' WHERE OBJ_ID = '3B4D4900A69043D08466C4D091E88B1A'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'New Student Orientation', DESCR_PLAIN = 'New Student Orientation', NAME = 'New Student Orientation' WHERE TYPE_KEY = 'kuali.atp.milestone.NewStudentOrientation'
 /
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Advanced Registration Period', DESCR_PLAIN = 'Advanced Registration Period', NAME = 'Advanced Registration Period' WHERE OBJ_ID = '6FCEFCB59DEA43D18A3726A1F3071A98'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Advanced Registration Period', DESCR_PLAIN = 'Advanced Registration Period', NAME = 'Advanced Registration Period' WHERE TYPE_KEY = 'kuali.atp.milestone.AdvancedRegistrationPeriod'
 /
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Registration Begins for MBA', DESCR_PLAIN = 'Registration Begins for MBA', NAME = 'Registration Begins for MBA' WHERE OBJ_ID = '12CCBFA9BD4143B6A5F2DAA7BA0DA4CB'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Registration Begins for MBA', DESCR_PLAIN = 'Registration Begins for MBA', NAME = 'Registration Begins for MBA' WHERE TYPE_KEY = 'kuali.atp.milestone.RegistrationBeginsForMBA'
 /
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Senior Registration', DESCR_PLAIN = 'Senior Registration', NAME = 'Senior Registration' WHERE OBJ_ID = 'D3375D735D434940BAB4CD76DDA1D075'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Senior Registration', DESCR_PLAIN = 'Senior Registration', NAME = 'Senior Registration' WHERE TYPE_KEY = 'kuali.atp.milestone.SeniorRegistration'
 /
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Sophomore Registration', DESCR_PLAIN = 'Sophomore Registration', NAME = 'Sophomore Registration' WHERE OBJ_ID = '3D345E36F378432B83096BBF7DC67FAF'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Sophomore Registration', DESCR_PLAIN = 'Sophomore Registration', NAME = 'Sophomore Registration' WHERE TYPE_KEY = 'kuali.atp.milestone.SophomoreRegistration'
 /
 UPDATE KSEN_TYPE SET NAME = 'Last Day to Drop with Clear Transcript' WHERE TYPE_KEY = 'kuali.atp.milestone.AddDropPeriod2'
 /
 UPDATE KSEN_TYPE SET NAME = 'Last Day to Drop with Annual Pass' WHERE TYPE_KEY = 'kuali.atp.milestone.AddDropPeriod3'
 /
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Last Day UG to Change Credits', DESCR_PLAIN = 'Last Day UG to Change Credits', NAME = 'Last Day UG to Change Credits' WHERE OBJ_ID = 'DE9E05C8CBF345E4BA600E90D61A582D'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Last Day UG to Change Credits', DESCR_PLAIN = 'Last Day UG to Change Credits', NAME = 'Last Day UG to Change Credits' WHERE TYPE_KEY = 'kuali.atp.milestone.CourseSelectionPeriodEnd2'
 /
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Last Day UG to Drop without a "W"', DESCR_PLAIN = 'Last Day UG to Drop without a "W"', NAME = 'Last Day UG to Drop without a "W"' WHERE OBJ_ID = '34F824E9A2654D948F7D8F35B7CC0383'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Last Day UG to Drop without a "W"', DESCR_PLAIN = 'Last Day UG to Drop without a "W"', NAME = 'Last Day UG to Drop without a "W"' WHERE TYPE_KEY = 'kuali.atp.milestone.CourseSelectionPeriodEnd4'
 /
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Last Day GR to Drop', DESCR_PLAIN = 'Last Day GR to Drop', NAME = 'Last Day GR to Drop' WHERE OBJ_ID = 'F2E505BE3C5446C2803FC3DE2CA9E8C4'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Last Day GR to Drop', DESCR_PLAIN = 'Last Day GR to Drop', NAME = 'Last Day GR to Drop' WHERE TYPE_KEY = 'kuali.atp.milestone.DropDate2'
 /
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Last Day GR to Change Credits', DESCR_PLAIN = 'Last Day GR to Change Credits', NAME = 'Last Day GR to Change Credits' WHERE OBJ_ID = '7F2BADCE2E4C4D118388D7FEDD56A5D2'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Last Day GR to Change Credits', DESCR_PLAIN = 'Last Day GR to Change Credits', NAME = 'Last Day GR to Change Credits' WHERE TYPE_KEY = 'kuali.atp.milestone.DropDate3'
 /
-UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Last Day UG to Drop with a "W"', DESCR_PLAIN = 'Last Day UG to Drop with a "W"', NAME = 'Last Day UG to Drop with a "W"' WHERE OBJ_ID = '0DD5A55DC2354B01B65D42FC0F5B4AE2'
+UPDATE KSEN_TYPE SET DESCR_FORMATTED = 'Last Day UG to Drop with a "W"', DESCR_PLAIN = 'Last Day UG to Drop with a "W"', NAME = 'Last Day UG to Drop with a "W"' WHERE TYPE_KEY = 'kuali.atp.milestone.DropDate4'
 /
 
 -- KSENROLL-5663
