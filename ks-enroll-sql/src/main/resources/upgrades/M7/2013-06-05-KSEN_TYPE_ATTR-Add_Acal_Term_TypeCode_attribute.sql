7e5ab443b900d64833c8d0b93018ea1285d04167
vgadiyak <vgadiyak@kuali.org>
2013-06-05 15:10:07 -0400

    KSENROLL-6768 Changed keysto be consistent
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@49278 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/upgrades/M7/2013-06-05-KSEN_TYPE_ATTR-Add_Acal_Term_TypeCode_attribute.sql
+++ ks-enroll-sql/src/main/resources/upgrades/M7/2013-06-05-KSEN_TYPE_ATTR-Add_Acal_Term_TypeCode_attribute.sql
@@ -9,11 +9,11 @@ insert into KSEN_TYPE_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values (
 /
 insert into KSEN_TYPE_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('0A7186DA3F114A4EBD8219DC08C6B203', '9570F12DAAD04ACCA6865DBC6BB909B8', 'kuali.attribute.type.atp.term.type.code', '08','kuali.atp.type.Fall')
 /
-insert into KSEN_TYPE_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('6AA41FECB8B744F38AF7607B6AAE2877', 'D7BCE25D1F22403CB45DD181FAF2E38E', 'kuali.attribute.type.atp.term.type.code', '21','kuali.atp.type.CETermI')
+insert into KSEN_TYPE_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('6AA41FECB8B744F38AF7607B6AAE2877', 'D7BCE25D1F22403CB45DD181FAF2E38E', 'kuali.attribute.type.atp.term.type.code', '21','kuali.atp.type.CETerm1')
 /
-insert into KSEN_TYPE_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('19C69D669C6E4D74B6F87E8CEB48BECB', '5E5EE7856594440B8D2DE70648618405', 'kuali.attribute.type.atp.term.type.code', '22','kuali.atp.type.CETermII')
+insert into KSEN_TYPE_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('19C69D669C6E4D74B6F87E8CEB48BECB', '5E5EE7856594440B8D2DE70648618405', 'kuali.attribute.type.atp.term.type.code', '22','kuali.atp.type.CETerm2')
 /
-insert into KSEN_TYPE_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('C464EC2C40354342BE4E776480C775F6', 'A020D670B2324CB89184D8EF53B1DB0A', 'kuali.attribute.type.atp.term.type.code', '23','kuali.atp.type.CETermIII')
+insert into KSEN_TYPE_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('C464EC2C40354342BE4E776480C775F6', 'A020D670B2324CB89184D8EF53B1DB0A', 'kuali.attribute.type.atp.term.type.code', '23','kuali.atp.type.CETerm3')
 /
-insert into KSEN_TYPE_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('67EFD48C30984F4A874F7BB9826B21D6', 'CA29A9DE57A5424EB7CCD218FF3F9D67', 'kuali.attribute.type.atp.term.type.code', '24','kuali.atp.type.CETermIV')
+insert into KSEN_TYPE_ATTR (ID, OBJ_ID, ATTR_KEY, ATTR_VALUE, OWNER_ID) values ('67EFD48C30984F4A874F7BB9826B21D6', 'CA29A9DE57A5424EB7CCD218FF3F9D67', 'kuali.attribute.type.atp.term.type.code', '24','kuali.atp.type.CETerm4')
 /
