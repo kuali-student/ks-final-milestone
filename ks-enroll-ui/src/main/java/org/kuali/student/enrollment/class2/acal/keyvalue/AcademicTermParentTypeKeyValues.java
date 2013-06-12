7f4e58a812c608efda778a322f1868711e719e34
vgadiyak <vgadiyak@kuali.org>
2013-06-12 14:50:22 -0400

    KSENROLL-7282 new file with parent key-value finder
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@49676 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/keyvalue/AcademicTermParentTypeKeyValues.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2013 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * Created by vgadiyak on 6/12/13
+ */
+package org.kuali.student.enrollment.class2.acal.keyvalue;
+
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.core.api.util.ConcreteKeyValue;
+import org.kuali.rice.core.api.util.KeyValue;
+import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
+import org.kuali.rice.krad.uif.view.ViewModel;
+import org.kuali.student.enrollment.class2.acal.dto.AcademicTermWrapper;
+import org.kuali.student.enrollment.class2.acal.form.AcademicCalendarForm;
+import org.kuali.student.r2.common.constants.CommonServiceConstants;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.exceptions.DoesNotExistException;
+import org.kuali.student.r2.common.exceptions.InvalidParameterException;
+import org.kuali.student.r2.common.exceptions.MissingParameterException;
+import org.kuali.student.r2.common.exceptions.OperationFailedException;
+import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
+import org.kuali.student.r2.core.acal.service.AcademicCalendarService;
+import org.kuali.student.r2.core.class1.type.dto.TypeInfo;
+import org.kuali.student.r2.core.constants.AcademicCalendarServiceConstants;
+
+import javax.xml.namespace.QName;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This class //TODO ...
+ *
+ * @author Kuali Student Team
+ */
+public class AcademicTermParentTypeKeyValues extends UifKeyValuesFinderBase implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private transient AcademicCalendarService acalService;
+
+    private static List<TypeInfo> acalTermTypes;
+
+    @Override
+    public List<KeyValue> getKeyValues(ViewModel model) {
+
+        List<KeyValue> keyValues = new ArrayList<KeyValue>();
+        List<String> availableTermTypes = new ArrayList();
+
+        if (model instanceof AcademicCalendarForm){
+            AcademicCalendarForm acalForm = (AcademicCalendarForm)model;
+
+            for (AcademicTermWrapper termWrapper : acalForm.getTermWrapperList()) {
+                availableTermTypes.add(termWrapper.getTermType());
+            }
+        }
+
+        keyValues.add(new ConcreteKeyValue("", "Select Term Type"));
+
+        List<TypeInfo> types = null;
+        try {
+
+            types = getAcalTermTypes();
+
+            for (TypeInfo type : types) {
+                if (!availableTermTypes.contains(type.getKey())){
+                    ConcreteKeyValue keyValue = new ConcreteKeyValue();
+                    keyValue.setKey(type.getKey());
+                    keyValue.setValue(type.getName());
+                    keyValues.add(keyValue);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return keyValues;
+    }
+
+    private List<TypeInfo> getAcalTermTypes() throws InvalidParameterException, MissingParameterException, DoesNotExistException, PermissionDeniedException, OperationFailedException {
+
+        if(acalTermTypes == null) {
+
+            //TODO:Build real context.
+            ContextInfo context = new ContextInfo();
+
+            acalTermTypes = Collections.unmodifiableList(getAcalService().getTermTypesForAcademicCalendarType(AcademicCalendarServiceConstants.ACADEMIC_CALENDAR_TYPE_KEY, context));
+        }
+
+        return acalTermTypes;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public AcademicCalendarService getAcalService() {
+        if(acalService == null) {
+            acalService = (AcademicCalendarService) GlobalResourceLoader.getService(new QName(CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "acal", "AcademicCalendarService"));
+        }
+        return this.acalService;
+    }
+
+}
