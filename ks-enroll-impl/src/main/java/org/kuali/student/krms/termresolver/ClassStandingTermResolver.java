5c15b5becab4d56dd52c322b5bc1bee298078445
peggy <peggy@kuali.org>
2013-06-13 09:13:42 -0400

    KSENROLL-6601 +KSENROLL-6602 +KSENROLL-6603 Term Resolvers
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@49728 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-impl/src/main/java/org/kuali/student/krms/termresolver/ClassStandingTermResolver.java
@@ -0,0 +1,13 @@
+package org.kuali.student.krms.termresolver;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Peggy
+ * Date: 2013/13/06
+ * Time: 03:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClassStandingTermResolver {
+
+
+}
---
+++ ks-enroll-impl/src/main/java/org/kuali/student/krms/termresolver/ProgramCourseCampusTermResolver.java
@@ -0,0 +1,13 @@
+package org.kuali.student.krms.termresolver;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Peggy
+ * Date: 2013/07/06
+ * Time: 1:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProgramCourseCampusTermResolver {
+
+
+}
