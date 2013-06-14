2b1af8bb565470f569d71438039a1974fd0e68cc
depstein <depstein@kuali.org>
2013-06-14 12:03:02 -0400

    KSENROLL-5590 used search service for acal search for terms and holiday calendars
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@49821 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-util/src/main/java/org/kuali/student/common/util/CalendarSearchViewHelperUtil.java
+++ ks-common/ks-common-util/src/main/java/org/kuali/student/common/util/CalendarSearchViewHelperUtil.java
@@ -1,44 +1,74 @@
 package org.kuali.student.common.util;
 
-import org.apache.commons.lang.StringUtils;
-import org.kuali.rice.core.api.criteria.Predicate;
-import org.kuali.rice.core.api.criteria.QueryByCriteria;
 import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.util.date.DateFormatters;
 import org.kuali.student.r2.core.acal.dto.AcademicCalendarInfo;
 import org.kuali.student.r2.core.acal.dto.HolidayCalendarInfo;
 import org.kuali.student.r2.core.acal.dto.TermInfo;
-import org.kuali.student.r2.core.acal.service.AcademicCalendarService;
 import org.kuali.student.r2.core.atp.service.AtpService;
+import org.kuali.student.r2.core.class1.type.dto.TypeTypeRelationInfo;
+import org.kuali.student.r2.core.class1.type.service.TypeService;
 import org.kuali.student.r2.core.constants.AcademicCalendarServiceConstants;
 import org.kuali.student.r2.core.search.dto.SearchRequestInfo;
 import org.kuali.student.r2.core.search.dto.SearchResultCellInfo;
 import org.kuali.student.r2.core.search.dto.SearchResultInfo;
 import org.kuali.student.r2.core.search.dto.SearchResultRowInfo;
-import org.kuali.student.r2.core.search.infc.SearchResultCell;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.kuali.rice.core.api.criteria.PredicateFactory.*;
-
 public class CalendarSearchViewHelperUtil {
 
-    public final static String NAME = "name";
-    public final static String START_DATE = "startDate";
-    public final static String END_DATE = "endDate";
-    public final static String CALENDAR_TYPE = "atpType";
+    public static List<TermInfo> searchForTerms(String nameParam, String yearParam, ContextInfo context, AtpService atpService, TypeService typeService) throws Exception {
 
-
-    public static List<TermInfo> searchForTerms(String name, String year, ContextInfo context, AcademicCalendarService academicCalendarService) throws Exception {
+        List<TypeTypeRelationInfo> typeRelations= typeService.getTypeTypeRelationsByOwnerAndType("kuali.atp.type.group.term", "kuali.type.type.relation.type.group", context);
+        List<String> termTypeKeys = new ArrayList<String>(typeRelations.size());
+        for(TypeTypeRelationInfo typeRelation:typeRelations){
+            termTypeKeys.add(typeRelation.getRelatedTypeKey());
+        }
 
         List<TermInfo> termInfoList = new ArrayList<TermInfo>();
 
-        QueryByCriteria.Builder query = buildQueryByCriteria(name, year, "Term");
+        SearchRequestInfo searchRequest = new SearchRequestInfo("atp.search.advancedAtpSearch");
+        searchRequest.addParam("atp.advancedAtpSearchParam.atpType", termTypeKeys);
+        if(nameParam!=null&&!nameParam.isEmpty()){
+            searchRequest.addParam("atp.advancedAtpSearchParam.atpShortName", nameParam);
+        }
+        if(yearParam!=null&&!yearParam.isEmpty()){
+            searchRequest.addParam("atp.advancedAtpSearchParam.atpYear", yearParam);
+        }
+        SearchResultInfo searchResults = atpService.search(searchRequest, context);
 
-        List<TermInfo> terms = academicCalendarService.searchForTerms(query.build(), context);
-        for (TermInfo term : terms) {
-            termInfoList.add(term);
+
+        for(SearchResultRowInfo row : searchResults.getRows()){
+            String id = null;
+            String name = null;
+            String startDate = null;
+            String endDate = null;
+            String stateKey = null;
+
+            for(SearchResultCellInfo cell : row.getCells()){
+                if("atp.resultColumn.atpId".equals(cell.getKey())){
+                    id = cell.getValue();
+                }else if("atp.resultColumn.atpShortName".equals(cell.getKey())){
+                    name = cell.getValue();
+                }else if("atp.resultColumn.atpStartDate".equals(cell.getKey())){
+                    startDate = cell.getValue();
+                }else if("atp.resultColumn.atpEndDate".equals(cell.getKey())){
+                    endDate = cell.getValue();
+                }else if("atp.resultColumn.atpState".equals(cell.getKey())){
+                    stateKey = cell.getValue();
+                }
+            }
+
+            TermInfo termInfo = new TermInfo();
+            termInfo.setId(id);
+            termInfo.setName(name);
+            termInfo.setStartDate(DateFormatters.DEFAULT_YEAR_MONTH_24HOUR_MILLISECONDS_FORMATTER.parse(startDate));
+            termInfo.setEndDate(DateFormatters.DEFAULT_YEAR_MONTH_24HOUR_MILLISECONDS_FORMATTER.parse(endDate));
+            termInfo.setStateKey(stateKey);
+
+            termInfoList.add(termInfo);
         }
 
         return termInfoList;
@@ -95,68 +125,53 @@ public class CalendarSearchViewHelperUtil {
 
     }
 
-    public static List<HolidayCalendarInfo> searchForHolidayCalendars(String name, String year, ContextInfo context, AcademicCalendarService academicCalendarService) throws Exception {
-
+    public static List<HolidayCalendarInfo> searchForHolidayCalendars(String nameParam, String yearParam, ContextInfo context, AtpService atpService) throws Exception {
         List<HolidayCalendarInfo> hCals = new ArrayList<HolidayCalendarInfo>();
 
-        QueryByCriteria.Builder query = buildQueryByCriteria(name, year, AcademicCalendarServiceConstants.HOLIDAY_CALENDAR_TYPE_KEY);
+        SearchRequestInfo searchRequest = new SearchRequestInfo("atp.search.advancedAtpSearch");
+        searchRequest.addParam("atp.advancedAtpSearchParam.atpType", AcademicCalendarServiceConstants.HOLIDAY_CALENDAR_TYPE_KEY);
+        if(nameParam!=null&&!nameParam.isEmpty()){
+            searchRequest.addParam("atp.advancedAtpSearchParam.atpShortName", nameParam);
+        }
+        if(yearParam!=null&&!yearParam.isEmpty()){
+            searchRequest.addParam("atp.advancedAtpSearchParam.atpYear", yearParam);
+        }
+        SearchResultInfo searchResults = atpService.search(searchRequest, context);
 
-        List<HolidayCalendarInfo> hcs = academicCalendarService.searchForHolidayCalendars(query.build(), context);
-        for (HolidayCalendarInfo hc : hcs) {
-            hCals.add(hc);
+
+        for(SearchResultRowInfo row : searchResults.getRows()){
+            String id = null;
+            String name = null;
+            String startDate = null;
+            String endDate = null;
+            String stateKey = null;
+
+            for(SearchResultCellInfo cell : row.getCells()){
+                if("atp.resultColumn.atpId".equals(cell.getKey())){
+                    id = cell.getValue();
+                }else if("atp.resultColumn.atpShortName".equals(cell.getKey())){
+                    name = cell.getValue();
+                }else if("atp.resultColumn.atpStartDate".equals(cell.getKey())){
+                    startDate = cell.getValue();
+                }else if("atp.resultColumn.atpEndDate".equals(cell.getKey())){
+                    endDate = cell.getValue();
+                }else if("atp.resultColumn.atpState".equals(cell.getKey())){
+                    stateKey = cell.getValue();
+                }
+            }
+
+            HolidayCalendarInfo hcalInfo = new HolidayCalendarInfo();
+            hcalInfo.setId(id);
+            hcalInfo.setName(name);
+            hcalInfo.setStartDate(DateFormatters.DEFAULT_YEAR_MONTH_24HOUR_MILLISECONDS_FORMATTER.parse(startDate));
+            hcalInfo.setEndDate(DateFormatters.DEFAULT_YEAR_MONTH_24HOUR_MILLISECONDS_FORMATTER.parse(endDate));
+            hcalInfo.setStateKey(stateKey);
+
+            hCals.add(hcalInfo);
         }
 
         return hCals;
 
-
     }
-
-    private static QueryByCriteria.Builder buildQueryByCriteria(String name, String year, String typeKey) {
-
-        QueryByCriteria.Builder qBuilder = QueryByCriteria.Builder.create();
-        List<Predicate> pList = new ArrayList<Predicate>();
-        Predicate p;
-
-        qBuilder.setPredicates();
-        if (StringUtils.isNotBlank(name)) {
-            p = like(NAME, "%" + name + "%");
-            pList.add(p);
-        }
-
-        if (StringUtils.isNotBlank(year)) {
-            try {
-                //FIXME: Find some better way to check the year
-                Predicate startDatePredicate = and(greaterThanOrEqual(START_DATE, DateFormatters.MONTH_DAY_YEAR_DATE_FORMATTER.parse("01/01/" + year)),
-                        lessThanOrEqual(START_DATE, DateFormatters.MONTH_DAY_YEAR_DATE_FORMATTER.parse("12/31/" + year)));
-
-
-                Predicate endDatePredicate = and(greaterThanOrEqual(END_DATE, DateFormatters.MONTH_DAY_YEAR_DATE_FORMATTER.parse("01/01/" + year)),
-                        lessThanOrEqual(END_DATE, DateFormatters.MONTH_DAY_YEAR_DATE_FORMATTER.parse("12/31/" + year)));
-
-
-                pList.add(or(startDatePredicate, endDatePredicate));
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-
-        }
-
-        if (StringUtils.equalsIgnoreCase(typeKey, "Term")) {
-            p = notIn(CALENDAR_TYPE, AcademicCalendarServiceConstants.ACADEMIC_CALENDAR_TYPE_KEY, AcademicCalendarServiceConstants.HOLIDAY_CALENDAR_TYPE_KEY);
-        } else {
-            p = equal(CALENDAR_TYPE, typeKey);
-        }
-
-        pList.add(p);
-
-        if (!pList.isEmpty()) {
-            Predicate[] preds = new Predicate[pList.size()];
-            pList.toArray(preds);
-            qBuilder.setPredicates(and(preds));
-        }
-        return qBuilder;
-    }
-
 
 }
