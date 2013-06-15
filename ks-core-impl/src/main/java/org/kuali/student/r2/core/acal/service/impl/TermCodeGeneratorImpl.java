5fb4d835346994780a43034f4b0f1a82e604e983
vgadiyak <vgadiyak@kuali.org>
2013-06-14 20:27:45 -0400

    KSENROLL-7448 KSENROLL-7449 When no code -> return null (not error)
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@49860 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-impl/src/main/java/org/kuali/student/r2/core/acal/service/impl/TermCodeGeneratorImpl.java
+++ ks-core-impl/src/main/java/org/kuali/student/r2/core/acal/service/impl/TermCodeGeneratorImpl.java
@@ -15,7 +15,6 @@
  */
 package org.kuali.student.r2.core.acal.service.impl;
 
-import org.apache.commons.httpclient.util.DateUtil;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.util.ContextUtils;
@@ -71,8 +70,9 @@ public class TermCodeGeneratorImpl implements TermCodeGenerator {
 
         // if the term is not of a type that is handled by the defined formula, return null, since the value for the atp code is undefined at that point
         if(typeCode == null || typeCode.equals("")) {
-            throw new RuntimeException("Error: missing term code attribute. Please configure a type attribute of '" + TypeServiceConstants.ATP_TERM_TYPE_CODE_ATTR +
-                    "' in KSEN_TYPE_ATTTR table. For term type " + term.getTypeKey());
+//            throw new RuntimeException("Error: missing term code attribute. Please configure a type attribute of '" + TypeServiceConstants.ATP_TERM_TYPE_CODE_ATTR +
+//                    "' in KSEN_TYPE_ATTTR table. For term type " + term.getTypeKey());
+            return null;
         }
 
         StringBuilder result = new StringBuilder(DateFormatters.DEFULT_YEAR_FORMATTER.format(term.getStartDate()));
