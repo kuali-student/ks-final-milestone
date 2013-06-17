f0448ab869951e7e3e33cccc3eb036430a4685b0
gtaylor <gtaylor@kuali.org>
2013-06-17 15:08:58 -0400

    KSENROLL-7424: Added another null check for the instrcution days keyday. if it doesn't exist, then there are 0 instructional days.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@49917 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-impl/src/main/java/org/kuali/student/r2/core/acal/service/impl/AcademicCalendarServiceImpl.java
+++ ks-core-impl/src/main/java/org/kuali/student/r2/core/acal/service/impl/AcademicCalendarServiceImpl.java
@@ -2136,7 +2136,9 @@ public class AcademicCalendarServiceImpl implements AcademicCalendarService {
         }
 
         // If there are no Instructional Days configured for a term, it should be 0.
-        if(instructionalPeriodKeyDate == null){
+        if(instructionalPeriodKeyDate == null &&
+                instructionalPeriodKeyDate.getStartDate() != null &&
+                instructionalPeriodKeyDate.getEndDate() != null ){
             return 0;
         }
         
