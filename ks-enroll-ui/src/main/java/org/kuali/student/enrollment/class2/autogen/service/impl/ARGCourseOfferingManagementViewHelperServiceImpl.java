a070bdc165c23e0aa27bfc32fd3b7b220f348410
johglov <johglov@kuali.org>
2013-06-21 09:42:24 -0400

    KSENROLL-7375 - Added functionality to check parent term ids as well during validation of mulitple terms per cluster.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@50241 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/autogen/service/impl/ARGCourseOfferingManagementViewHelperServiceImpl.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/autogen/service/impl/ARGCourseOfferingManagementViewHelperServiceImpl.java
@@ -471,6 +471,8 @@ public class ARGCourseOfferingManagementViewHelperServiceImpl extends CO_AO_RG_V
         // Test the Cluster for Multiple AO types and Term types
         if(aoTypeKeys.size()>1){
             List<String> termIds = new ArrayList<String>();
+
+            // Tests for multiple subTerms
             for(ActivityOfferingWrapper aoWrapper : cluster.getAoWrapperList()){
                 if(termIds.size()==0){
                     termIds.add(aoWrapper.getSubTermId());
@@ -484,6 +486,26 @@ public class ARGCourseOfferingManagementViewHelperServiceImpl extends CO_AO_RG_V
                     }
                 }
                 if(newTerm) termIds.add(aoWrapper.getSubTermId());
+            }
+            if(termIds.size()>1){
+                GlobalVariables.getMessageMap().putWarningForSectionId("activityOfferingsPerCluster_line" + clusterIndex, RegistrationGroupConstants.MSG_ERROR_CLUSTER_MULTIPLE_TERMS, cluster.getAoCluster().getPrivateName());
+            }
+
+            // Test for multiple Terms
+            termIds = new ArrayList<String>();
+            for(ActivityOfferingWrapper aoWrapper : cluster.getAoWrapperList()){
+                if(termIds.size()==0){
+                    termIds.add(aoWrapper.getTermId());
+                    continue;
+                }
+                boolean newTerm = false;
+                for(String id : termIds){
+                    if(id == null) continue;
+                    if(aoWrapper.getTermId().compareTo(id)!=0){
+                        newTerm=true;
+                    }
+                }
+                if(newTerm) termIds.add(aoWrapper.getTermId());
             }
             if(termIds.size()>1){
                 GlobalVariables.getMessageMap().putWarningForSectionId("activityOfferingsPerCluster_line" + clusterIndex, RegistrationGroupConstants.MSG_ERROR_CLUSTER_MULTIPLE_TERMS, cluster.getAoCluster().getPrivateName());
