b398af2c3dc30e8f1952b804875d6ce705178d38
dedewet <dedewet@kuali.org>
2013-06-24 06:57:04 -0400

    KSENROLL-7708 - Fixed index out of bounds exception for move left function
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@50326 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/rice/krms/controller/RuleEditorController.java
+++ ks-enroll-ui/src/main/java/org/kuali/rice/krms/controller/RuleEditorController.java
@@ -569,7 +569,11 @@ public class RuleEditorController extends MaintenanceDocumentController {
                     } else if(parent.getChildren().size() == 3) {
                         GlobalVariables.getMessageMap().putWarning("editWithObjectTree", "warning.krms.tree.compound.single.simple", parent.getData().getProposition().getKey());
                     }
-                    granny.getData().getProposition().getCompoundEditors().add((newIndex / 2) + 1, prop);
+                    if(granny.getData().getProposition().getCompoundEditors().isEmpty()) {
+                        granny.getData().getProposition().getCompoundEditors().add(newIndex, prop);
+                    } else {
+                        granny.getData().getProposition().getCompoundEditors().add((newIndex / 2) + 1, prop);
+                    }
                     this.getViewHelper(form).refreshInitTrees(ruleEditor);
                 }
             }
