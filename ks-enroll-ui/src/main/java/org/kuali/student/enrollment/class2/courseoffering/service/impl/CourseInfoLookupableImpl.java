75e4ae7c6ed7e7fe8c4172e8e9a14250cb79df33
vpremcha <vpremcha@kuali.org>
2013-06-27 13:57:47 -0400

    KSENROLL-3889	 - Add front end validation against blank entry (no search for all)
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@50529 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/impl/CourseInfoLookupableImpl.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/impl/CourseInfoLookupableImpl.java
@@ -1,17 +1,22 @@
 package org.kuali.student.enrollment.class2.courseoffering.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.core.api.util.RiceKeyConstants;
 import org.kuali.rice.krad.lookup.LookupableImpl;
+import org.kuali.rice.krad.util.GlobalVariables;
+import org.kuali.rice.krad.util.KRADConstants;
 import org.kuali.rice.krad.web.form.LookupForm;
-import org.kuali.student.r2.common.dto.ContextInfo;
-import org.kuali.student.r2.core.search.dto.*;
-import org.kuali.student.r2.core.search.dto.SearchParamInfo;
+import org.kuali.student.r2.common.dto.RichTextInfo;
 import org.kuali.student.r2.common.util.ContextUtils;
+import org.kuali.student.r2.core.search.dto.SearchParamInfo;
+import org.kuali.student.r2.core.search.dto.SearchRequestInfo;
+import org.kuali.student.r2.core.search.dto.SearchResultCellInfo;
+import org.kuali.student.r2.core.search.dto.SearchResultInfo;
+import org.kuali.student.r2.core.search.dto.SearchResultRowInfo;
 import org.kuali.student.r2.lum.clu.service.CluService;
 import org.kuali.student.r2.lum.course.dto.CourseInfo;
-import org.kuali.student.r2.lum.course.service.CourseService;
 import org.kuali.student.r2.lum.util.constants.CluServiceConstants;
-import org.kuali.student.r2.lum.util.constants.CourseServiceConstants;
 
 import javax.xml.namespace.QName;
 import java.util.ArrayList;
@@ -24,7 +29,6 @@ public class CourseInfoLookupableImpl extends LookupableImpl {
 	private static final long serialVersionUID = 1L;	
 	
     private transient CluService cluService;
-    private transient CourseService courseService;
 
     public enum QueryParamEnum {
         ID("lu.queryParam.luOptionalId","id"),
@@ -50,9 +54,30 @@ public class CourseInfoLookupableImpl extends LookupableImpl {
     }
 
     @Override
+    public boolean validateSearchParameters(LookupForm form, Map<String, String> searchCriteria) {
+        boolean valid = super.validateSearchParameters(form,searchCriteria);
+
+        if (valid){
+            String courseCodeParam = searchCriteria.get(QueryParamEnum.CODE.getFieldValue());
+            String courseTitleParam = searchCriteria.get(QueryParamEnum.TITLE.getFieldValue());
+
+            if (StringUtils.isBlank(courseCodeParam) && StringUtils.isBlank(courseTitleParam)){
+                GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_ERRORS, RiceKeyConstants.ERROR_CUSTOM,"Please enter atleast one field.");
+                valid = false;
+            } else if ((StringUtils.isBlank(courseCodeParam) && StringUtils.length(courseTitleParam) < 2) ||
+                       (StringUtils.length(courseCodeParam) < 2 && StringUtils.isBlank(courseTitleParam))) {
+                GlobalVariables.getMessageMap().putError(KRADConstants.GLOBAL_ERRORS, RiceKeyConstants.ERROR_CUSTOM,"Please enter atleast 2 characters for the search.");
+                valid = false;
+            }
+        }
+
+        return valid;
+    }
+
+    @Override
     protected List<?> getSearchResults(LookupForm lookupForm, Map<String, String> fieldValues, boolean unbounded) {
         List <CourseInfo> courseInfoList = new ArrayList<CourseInfo>();
-        String courseId;
+
         List<SearchParamInfo> searchParams = new ArrayList<SearchParamInfo>();
         SearchParamInfo qpv1 = new SearchParamInfo();
         qpv1.setKey("lu.queryParam.luOptionalType");
@@ -78,25 +103,27 @@ public class CourseInfoLookupableImpl extends LookupableImpl {
         searchRequest.setParams(searchParams);
         searchRequest.setSearchKey("lu.search.mostCurrent.union");
 
-        ContextInfo contextInfo = ContextUtils.getContextInfo();
-
         try {
             SearchResultInfo searchResult = getCluService().search(searchRequest, ContextUtils.getContextInfo());
 
-            List<String> courseIds = new ArrayList<String>();
             if (searchResult.getRows().size() > 0) {
                 for(SearchResultRowInfo srrow : searchResult.getRows()){
                     List<SearchResultCellInfo> srCells = srrow.getCells();
                     if(srCells != null && srCells.size() > 0){
+                        CourseInfo course = new CourseInfo();
                         for(SearchResultCellInfo srcell : srCells){
-                            if (srcell.getKey().equals("lu.resultColumn.cluId")) {
-                                courseIds.add(srcell.getValue());
+                            if (srcell.getKey().equals("lu.resultColumn.luOptionalCode")) {
+                                course.setCode(srcell.getValue());
+                            } else if (srcell.getKey().equals("lu.resultColumn.luOptionalLongName")) {
+                                course.setCourseTitle(srcell.getValue());
+                            }else if (srcell.getKey().equals("lu.resultColumn.luOptionalDescr")) {
+                                RichTextInfo desc = new RichTextInfo();
+                                desc.setFormatted(srcell.getValue());
+                                course.setDescr(desc);
                             }
                         }
+                        courseInfoList.add(course);
                     }
-                }
-                if (!courseIds.isEmpty()){
-                    courseInfoList = getCourseService().getCoursesByIds(courseIds,contextInfo);
                 }
             }
 
@@ -106,7 +133,6 @@ public class CourseInfoLookupableImpl extends LookupableImpl {
         }
     }
 
-    //Note: here I am using r1 CluService implementation!!!
     protected CluService getCluService() {
         if(cluService == null) {
             cluService = (CluService)GlobalResourceLoader.getService(new QName(CluServiceConstants.CLU_NAMESPACE,CluServiceConstants.SERVICE_NAME_LOCAL_PART));
@@ -114,10 +140,4 @@ public class CourseInfoLookupableImpl extends LookupableImpl {
         return this.cluService;
     }
 
-    protected CourseService getCourseService() {
-        if(courseService == null) {
-            courseService = (CourseService)GlobalResourceLoader.getService(new QName(CourseServiceConstants.COURSE_NAMESPACE,"CourseService"));
-        }
-        return this.courseService;
-    }
 }
