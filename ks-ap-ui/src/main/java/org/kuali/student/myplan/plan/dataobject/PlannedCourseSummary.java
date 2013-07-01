20e5b56f7dd724f163054eb8888147d3a70dce0a
mwfyffe <mwfyffe@kuali.org>
2013-06-30 23:04:13 -0400

    KSAP-34 course details button overrides
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/branches/ks-ap-0.6-iu-dev@50677 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-ui/src/main/java/org/kuali/student/myplan/plan/dataobject/PlannedCourseSummary.java
+++ ks-ap-ui/src/main/java/org/kuali/student/myplan/plan/dataobject/PlannedCourseSummary.java
@@ -8,90 +8,96 @@ import java.util.List;
 
 /**
  * kmuthu Don't forget to add comment
- *
- * @Author kmuthu
- * Date: 3/4/13
+ * 
+ * @Author kmuthu Date: 3/4/13
  */
 public class PlannedCourseSummary {
 
- // Plan related information
-    private transient List<PlanItemDataObject> plannedList;
-    private transient List<PlanItemDataObject> backupList;
-    private List<AcademicRecordDataObject> acadRecList;
-    private List<String> academicTerms;
-    private transient String savedItemId;
-    private String savedItemDateCreated;
+	// Plan related information
+	private transient List<PlanItemDataObject> plannedList;
+	private transient List<PlanItemDataObject> backupList;
+	private List<AcademicRecordDataObject> acadRecList;
+	private List<String> academicTerms;
+	private transient String savedItemId;
+	private String savedItemDateCreated;
 
+	public List<PlanItemDataObject> getPlannedList() {
+		if (plannedList == null) {
+			plannedList = new ArrayList<PlanItemDataObject>();
+		}
+		return plannedList;
+	}
 
-   public List<PlanItemDataObject> getPlannedList() {
-        if (plannedList == null) {
-            plannedList = new ArrayList<PlanItemDataObject>();
-        }
-        return plannedList;
-    }
+	public void setPlannedList(List<PlanItemDataObject> plannedList) {
+		this.plannedList = plannedList;
+	}
 
-    public void setPlannedList(List<PlanItemDataObject> plannedList) {
-        this.plannedList = plannedList;
-    }
+	public List<PlanItemDataObject> getBackupList() {
+		if (backupList == null) {
+			backupList = new ArrayList<PlanItemDataObject>();
+		}
+		return backupList;
+	}
 
-    public List<PlanItemDataObject> getBackupList() {
-        if (backupList == null) {
-            backupList = new ArrayList<PlanItemDataObject>();
-        }
-        return backupList;
-    }
+	public void setBackupList(List<PlanItemDataObject> backupList) {
+		this.backupList = backupList;
+	}
 
-    public void setBackupList(List<PlanItemDataObject> backupList) {
-        this.backupList = backupList;
-    }
+	public String getSavedItemId() {
+		return savedItemId;
+	}
 
-    public String getSavedItemId() {
-        return savedItemId;
-    }
+	public void setSavedItemId(String savedItemId) {
+		this.savedItemId = savedItemId;
+	}
 
-    public void setSavedItemId(String savedItemId) {
-        this.savedItemId = savedItemId;
-    }
+	public String getSavedItemDateCreated() {
+		return savedItemDateCreated;
+	}
 
-    public String getSavedItemDateCreated() {
-        return savedItemDateCreated;
-    }
+	public void setSavedItemDateCreated(String savedItemDateCreated) {
+		this.savedItemDateCreated = savedItemDateCreated;
+	}
 
-    public void setSavedItemDateCreated(String savedItemDateCreated) {
-        this.savedItemDateCreated = savedItemDateCreated;
-    }
+	public List<AcademicRecordDataObject> getAcadRecList() {
+		if (acadRecList == null) {
+			acadRecList = new ArrayList<AcademicRecordDataObject>();
+		}
+		return acadRecList;
+	}
 
-    public List<AcademicRecordDataObject> getAcadRecList() {
-        if (acadRecList == null) {
-            acadRecList = new ArrayList<AcademicRecordDataObject>();
-        }
-        return acadRecList;
-    }
+	public void setAcadRecList(List<AcademicRecordDataObject> acadRecList) {
+		this.acadRecList = acadRecList;
+	}
 
-   public void setAcadRecList(List<AcademicRecordDataObject> acadRecList) {
-        this.acadRecList = acadRecList;
-    }
+	@JsonIgnore
+	public boolean getInPlannedCourseList() {
+		return (plannedList != null && plannedList.size() > 1) ? true : false;
+	}
 
-    @JsonIgnore
-    public boolean getInPlannedCourseList() {
-        return (plannedList != null && plannedList.size() > 1) ? true : false;
-    }
+	@JsonIgnore
+	public boolean getInSavedCourseList() {
+		return (StringUtils.hasText(savedItemId)) ? true : false;
+	}
 
-    @JsonIgnore
-    public boolean getInSavedCourseList() {
-        return (StringUtils.hasText(savedItemId)) ? true : false;
-    }
+	public List<String> getAcademicTerms() {
+		if (academicTerms == null) {
+			academicTerms = new ArrayList<String>();
+		}
+		return academicTerms;
+	}
 
+	public void setAcademicTerms(List<String> academicTerms) {
+		this.academicTerms = academicTerms;
+	}
 
-    public List<String> getAcademicTerms() {
-        if (academicTerms == null) {
-            academicTerms = new ArrayList<String>();
-        }
-        return academicTerms;
-    }
+	public boolean isBookmarked() {
+		return getSavedItemId() != null && !isPlanned();
+	}
 
-    public void setAcademicTerms(List<String> academicTerms) {
-        this.academicTerms = academicTerms;
-    }
+	public boolean isPlanned() {
+		return (plannedList != null && !plannedList.isEmpty())
+				|| (backupList != null && !backupList.isEmpty());
+	}
 
 }
