b660e698df9926159c54ba27e7d700611c9157f3
jcaddel <jcaddel@kuali.org>
2013-07-07 13:38:30 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51181 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/DbExportExecutableConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/DbExportExecutableConfig.java
@@ -43,7 +43,7 @@ public class DbExportExecutableConfig extends ExecutableConfig {
 		List<Executable> executables = new ArrayList<Executable>();
 
 		// First export the schema + MPX files all to one directory
-		executables.add(databaseExportConfig.exportDatabaseExecutable());
+		executables.add(databaseExportConfig.dumpDatabaseExecutable());
 
 		// Then split them up as needed into the various sub directories
 		executables.add(syncFilesConfig.syncFilesExecutable());
