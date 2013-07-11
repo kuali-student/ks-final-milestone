121de8ca262dd190e70d734acf548ce76cc72148
brandon.gresham <brandon.gresham@kuali.org>
2013-07-11 12:14:01 -0400

    KSENROLL-1645 Improved name of property-key because it was misleading.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@51456 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/util/CourseOfferingConstants.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/util/CourseOfferingConstants.java
@@ -11,7 +11,7 @@ public class CourseOfferingConstants {
 
     //  Keys for features which are institutionally configurable via config file params
     public final static String CONFIG_PARAM_KEY_SELECTIVE_CROSSLISTING = "kuali.ks.enrollment.options.selective-crosslisting-allowed";
-    public final static String CONFIG_PARAM_KEY_EDIT_ACTIVITY_CODE = "kuali.ks.enrollment.options.edit-activity-offering-allowed";
+    public final static String CONFIG_PARAM_KEY_EDIT_ACTIVITY_CODE = "kuali.ks.enrollment.options.edit-activity-offering-code-allowed";
 
     //Attributes defined in CourseOfferingInfo
     public final static String COURSEOFFERING_ID = "id";
