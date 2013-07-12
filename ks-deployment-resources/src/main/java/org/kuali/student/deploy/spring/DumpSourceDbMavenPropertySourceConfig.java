efb6910429f2b806e9c195bc2f90293e958dc948
jcaddel <jcaddel@kuali.org>
2013-07-12 08:04:36 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51538 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/DumpSourceDbMavenPropertySourceConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/DumpSourceDbMavenPropertySourceConfig.java
@@ -3,6 +3,7 @@ package org.kuali.student.deploy.spring;
 import java.util.List;
 
 import org.kuali.common.impex.DumpProjectContext;
+import org.kuali.common.impex.MavenProjectContext;
 import org.kuali.common.jdbc.JdbcProjectContext;
 import org.kuali.common.util.ProjectContext;
 import org.kuali.common.util.property.ProjectProperties;
@@ -17,8 +18,9 @@ public class DumpSourceDbMavenPropertySourceConfig extends MavenPropertySourceCo
 	protected List<ProjectProperties> getOtherProjectProperties() {
 		ProjectContext jdbc = new JdbcProjectContext();
 		ProjectContext dump = new DumpProjectContext();
+		ProjectContext mavenDump = new MavenProjectContext();
 		ProjectContext sourceDb = new DumpSourceDbProjectContext();
-		return ConfigUtils.getProjectProperties(jdbc, dump, sourceDb);
+		return ConfigUtils.getProjectProperties(jdbc, dump, mavenDump, sourceDb);
 	}
 
 }
