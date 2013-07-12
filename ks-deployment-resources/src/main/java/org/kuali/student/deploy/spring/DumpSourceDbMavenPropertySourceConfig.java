8c7a8b7cc5b3b7854e0c2759233136fd937f5f41
jcaddel <jcaddel@kuali.org>
2013-07-12 07:50:15 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51533 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/DumpSourceDbMavenPropertySourceConfig.java
@@ -0,0 +1,23 @@
+package org.kuali.student.deploy.spring;
+
+import java.util.List;
+
+import org.kuali.common.impex.DumpAndStageProjectContext;
+import org.kuali.common.jdbc.JdbcProjectContext;
+import org.kuali.common.util.ProjectContext;
+import org.kuali.common.util.property.ProjectProperties;
+import org.kuali.common.util.spring.ConfigUtils;
+import org.kuali.common.util.spring.MavenPropertySourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DumpSourceDbMavenPropertySourceConfig extends MavenPropertySourceConfig {
+
+	@Override
+	protected List<ProjectProperties> getOtherProjectProperties() {
+		ProjectContext jdbc = new JdbcProjectContext();
+		ProjectContext dump = new DumpAndStageProjectContext();
+		return ConfigUtils.getProjectProperties(jdbc, dump);
+	}
+
+}
---
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/DumpSourceDbProjectContext.java
@@ -0,0 +1,23 @@
+package org.kuali.student.deploy.spring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.common.util.DefaultProjectContext;
+import org.kuali.common.util.ProjectUtils;
+
+public class DumpSourceDbProjectContext extends DefaultProjectContext {
+
+	public DumpSourceDbProjectContext() {
+		super(Constants.GROUP_ID, Constants.ARTIFACT_ID, getLocations());
+	}
+
+	protected static List<String> getLocations() {
+		String prefix = ProjectUtils.getClassPathPrefix(Constants.GROUP_ID_BASE, Constants.KS_SOURCE_DB);
+		List<String> list = new ArrayList<String>();
+		list.add(prefix + "/common.properties");
+		list.add(prefix + "/dump.properties");
+		return list;
+	}
+
+}
