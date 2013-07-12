3c3dd04ada5134dcda74f09673e0e336bfa680c8
jcaddel <jcaddel@kuali.org>
2013-07-12 09:48:55 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51551 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/PrepareMavenPropertySourceConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/PrepareMavenPropertySourceConfig.java
@@ -1,11 +1,13 @@
 package org.kuali.student.deploy.spring;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.kuali.common.impex.DumpProjectContext;
 import org.kuali.common.impex.MavenProjectContext;
+import org.kuali.common.impex.StagingProjectContext;
 import org.kuali.common.jdbc.JdbcProjectContext;
 import org.kuali.common.util.ProjectContext;
+import org.kuali.common.util.ScmProjectContext;
 import org.kuali.common.util.property.ProjectProperties;
 import org.kuali.common.util.spring.ConfigUtils;
 import org.kuali.common.util.spring.MavenPropertySourceConfig;
@@ -16,12 +18,16 @@ public class PrepareMavenPropertySourceConfig extends MavenPropertySourceConfig 
 
 	@Override
 	protected List<ProjectProperties> getOtherProjectProperties() {
+		// General SCM related config
+		ProjectContext scm = new ScmProjectContext();
 		// General JDBC related config
 		ProjectContext jdbc = new JdbcProjectContext();
-		// Config specific to dumping a database using Maven
+		// General dump related config
+		ProjectContext dump = new DumpProjectContext();
 		ProjectContext maven = new MavenProjectContext();
+		ProjectContext staging = new StagingProjectContext();
 		ProjectContext prepare = new PrepareProjectContext();
-		return ConfigUtils.getProjectProperties(Arrays.asList(jdbc, maven, prepare));
+		return ConfigUtils.getProjectProperties(scm, jdbc, dump, maven, staging, prepare);
 	}
 
 }
