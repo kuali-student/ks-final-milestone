ad51dac544435f78461bc3c9c0ca49dd11f32b1b
dedewet <dedewet@kuali.org>
2013-07-14 12:45:01 -0400

    KSENROLL-7132 - Changes for KRMS permissions
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@51636 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/rice/krms/dto/PermissionWrapper.java
+++ ks-enroll-ui/src/main/java/org/kuali/rice/krms/dto/PermissionWrapper.java
@@ -1,14 +1,189 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kuali.rice.krms.dto;
 
 import java.io.Serializable;
 
 /**
- * Created with IntelliJ IDEA.
- * User: SW Genis
- * Date: 2013/07/12
- * Time: 8:10 AM
- * To change this template use File | Settings | File Templates.
+ *
+ * @author Kuali Student Team
  */
 public class PermissionWrapper implements Serializable {
 
+    private boolean addRequisiteAction;
+    private boolean deleteRequisiteAction;
+    private boolean compareAction;
+    private boolean addRuleAction;
+    private boolean editRuleAction;
+    private boolean groupRuleAction;
+    private boolean moveUpRuleAction;
+    private boolean moveDownRuleAction;
+    private boolean moveOutRuleAction;
+    private boolean moveInRuleAction;
+    private boolean copyRuleAction;
+    private boolean cutRuleAction;
+    private boolean pasteRuleAction;
+    private boolean deleteRuleAction;
+    private boolean editRuleLogicTab;
+    private boolean updateRuleAction;
+
+    public PermissionWrapper() {
+        this.addRequisiteAction = true;
+        this.deleteRequisiteAction = true;
+        this.compareAction = true;
+        this.addRuleAction = true;
+        this.editRuleAction = true;
+        this.groupRuleAction = true;
+        this.moveUpRuleAction = true;
+        this.moveDownRuleAction = true;
+        this.moveOutRuleAction = true;
+        this.moveInRuleAction = true;
+        this.copyRuleAction = true;
+        this.cutRuleAction = true;
+        this.pasteRuleAction = true;
+        this.deleteRuleAction = true;
+        this.editRuleLogicTab = true;
+        this.updateRuleAction = true;
+    }
+
+    public boolean isAddRequisiteAction() {
+        return addRequisiteAction;
+    }
+
+    public void setAddRequisiteAction(boolean addRequisiteAction) {
+        this.addRequisiteAction = addRequisiteAction;
+    }
+
+    public boolean isDeleteRequisiteAction() {
+        return deleteRequisiteAction;
+    }
+
+    public void setDeleteRequisiteAction(boolean deleteRequisiteAction) {
+        this.deleteRequisiteAction = deleteRequisiteAction;
+    }
+
+    public boolean isCompareAction() {
+        return compareAction;
+    }
+
+    public void setCompareAction(boolean compareAction) {
+        this.compareAction = compareAction;
+    }
+
+    public boolean isAddRuleAction() {
+        return addRuleAction;
+    }
+
+    public void setAddRuleAction(boolean addRuleAction) {
+        this.addRuleAction = addRuleAction;
+    }
+
+    public boolean isEditRuleAction() {
+        return editRuleAction;
+    }
+
+    public void setEditRuleAction(boolean editRuleAction) {
+        this.editRuleAction = editRuleAction;
+    }
+
+    public boolean isGroupRuleAction() {
+        return groupRuleAction;
+    }
+
+    public void setGroupRuleAction(boolean groupRuleAction) {
+        this.groupRuleAction = groupRuleAction;
+    }
+
+    public boolean isMoveUpRuleAction() {
+        return moveUpRuleAction;
+    }
+
+    public void setMoveUpRuleAction(boolean moveUpRuleAction) {
+        this.moveUpRuleAction = moveUpRuleAction;
+    }
+
+    public boolean isMoveDownRuleAction() {
+        return moveDownRuleAction;
+    }
+
+    public void setMoveDownRuleAction(boolean moveDownRuleAction) {
+        this.moveDownRuleAction = moveDownRuleAction;
+    }
+
+    public boolean isMoveOutRuleAction() {
+        return moveOutRuleAction;
+    }
+
+    public void setMoveOutRuleAction(boolean moveOutRuleAction) {
+        this.moveOutRuleAction = moveOutRuleAction;
+    }
+
+    public boolean isMoveInRuleAction() {
+        return moveInRuleAction;
+    }
+
+    public void setMoveInRuleAction(boolean moveInRuleAction) {
+        this.moveInRuleAction = moveInRuleAction;
+    }
+
+    public boolean isCopyRuleAction() {
+        return copyRuleAction;
+    }
+
+    public void setCopyRuleAction(boolean copyRuleAction) {
+        this.copyRuleAction = copyRuleAction;
+    }
+
+    public boolean isCutRuleAction() {
+        return cutRuleAction;
+    }
+
+    public void setCutRuleAction(boolean cutRuleAction) {
+        this.cutRuleAction = cutRuleAction;
+    }
+
+    public boolean isPasteRuleAction() {
+        return pasteRuleAction;
+    }
+
+    public void setPasteRuleAction(boolean pasteRuleAction) {
+        this.pasteRuleAction = pasteRuleAction;
+    }
+
+    public boolean isDeleteRuleAction() {
+        return deleteRuleAction;
+    }
+
+    public void setDeleteRuleAction(boolean deleteRuleAction) {
+        this.deleteRuleAction = deleteRuleAction;
+    }
+
+    public boolean isEditRuleLogicTab() {
+        return editRuleLogicTab;
+    }
+
+    public void setEditRuleLogicTab(boolean editRuleLogicTab) {
+        this.editRuleLogicTab = editRuleLogicTab;
+    }
+
+    public boolean isUpdateRuleAction() {
+        return updateRuleAction;
+    }
+
+    public void setUpdateRuleAction(boolean updateRuleAction) {
+        this.updateRuleAction = updateRuleAction;
+    }
 }
--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/krms/util/KSKRMSPermissionHelper.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/krms/util/KSKRMSPermissionHelper.java
@@ -1,17 +1,174 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kuali.student.enrollment.class1.krms.util;
 
+import org.apache.commons.lang.StringUtils;
+import org.kuali.rice.kim.api.KimConstants;
+import org.kuali.rice.kim.api.permission.PermissionService;
+import org.kuali.rice.kim.api.services.KimApiServiceLocator;
+import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.web.form.MaintenanceDocumentForm;
+import org.kuali.rice.krms.dto.AgendaEditor;
+import org.kuali.rice.krms.dto.PermissionWrapper;
+import org.kuali.rice.krms.dto.RuleEditor;
+import org.kuali.student.enrollment.class1.krms.dto.CORuleManagementWrapper;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Created with IntelliJ IDEA.
- * User: SW Genis
- * Date: 2013/07/12
- * Time: 8:31 AM
- * To change this template use File | Settings | File Templates.
+ *
+ * @author Kuali Student Team
  */
 public class KSKRMSPermissionHelper {
 
+    private static PermissionService permissionService = getPermissionService();
+
     public static void processActionPermissionForUser(MaintenanceDocumentForm document) {
+        CORuleManagementWrapper wrapper = (CORuleManagementWrapper) document.getDocument().getNewMaintainableObject().getDataObject();
+
+        String principalId = GlobalVariables.getUserSession().getPerson().getPrincipalId();
+
+        String socState = StringUtils.lowerCase(wrapper.getContextBar().getTermSocState());
+
+        Map<String,String> permissionDetails = new HashMap<String,String>();
+        Map<String,String> roleQualifications = new HashMap<String,String>();
+
+        roleQualifications.put("offeringAdminOrgId", wrapper.getAdminOrg());
+
+        permissionDetails.put("socState", socState);
+        permissionDetails.put(KimConstants.AttributeConstants.VIEW_ID, document.getViewId());
+
+        for(AgendaEditor agenda : wrapper.getAgendas()) {
+            for(RuleEditor rule : agenda.getRuleEditors().values()) {
+
+                if(rule.getPermission() == null) {
+                    rule.setPermission(new PermissionWrapper());
+                }
+
+                resetActions(rule);
+
+                permissionDetails.put("ruleType", rule.getRuleTypeInfo().getType());
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "addRequisite");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setAddRequisiteAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "deleteRequisite");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setDeleteRequisiteAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "compareAction");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setCompareAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "addRule");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setAddRuleAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "editRule");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setEditRuleAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "groupRule");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setGroupRuleAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "moveUpRule");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setMoveUpRuleAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "moveDownRule");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setMoveDownRuleAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "moveInRule");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setMoveInRuleAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "moveOutRule");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setMoveOutRuleAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "copyRule");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setCopyRuleAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "cutRule");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setCutRuleAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "pasteRule");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setPasteRuleAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "deleteRule");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setDeleteRuleAction(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "editLogic");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setEditRuleLogicTab(true);
+                }
+
+                permissionDetails.put(KimConstants.AttributeConstants.ACTION_EVENT, "updateRule");
+                if (permissionService.isAuthorizedByTemplate(principalId, "KS-ENR", KimConstants.PermissionTemplateNames.PERFORM_ACTION, permissionDetails, roleQualifications)) {
+                    rule.getPermission().setUpdateRuleAction(true);
+                }
+            }
+        }
     }
 
+    private static void resetActions(RuleEditor ruleEditor) {
+        ruleEditor.getPermission().setAddRequisiteAction(false);
+        ruleEditor.getPermission().setDeleteRequisiteAction(false);
+        ruleEditor.getPermission().setCompareAction(false);
+        ruleEditor.getPermission().setAddRuleAction(false);
+        ruleEditor.getPermission().setEditRuleAction(false);
+        ruleEditor.getPermission().setGroupRuleAction(false);
+        ruleEditor.getPermission().setMoveUpRuleAction(false);
+        ruleEditor.getPermission().setMoveDownRuleAction(false);
+        ruleEditor.getPermission().setMoveOutRuleAction(false);
+        ruleEditor.getPermission().setMoveInRuleAction(false);
+        ruleEditor.getPermission().setCopyRuleAction(false);
+        ruleEditor.getPermission().setCutRuleAction(false);
+        ruleEditor.getPermission().setPasteRuleAction(false);
+        ruleEditor.getPermission().setDeleteRuleAction(false);
+        ruleEditor.getPermission().setEditRuleLogicTab(false);
+        ruleEditor.getPermission().setUpdateRuleAction(false);
+    }
+
+    private static PermissionService getPermissionService() {
+        if(permissionService==null){
+            permissionService = KimApiServiceLocator.getPermissionService();
+        }
+        return permissionService;
+    }
 }
