18446b41fcc95eb357b93aed40eb86c350f39f2a
jcaddel <jcaddel@kuali.org>
2013-07-15 04:21:27 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51666 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/StudentSqlMetaInfPropertySourceConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/StudentSqlMetaInfPropertySourceConfig.java
@@ -7,7 +7,7 @@ import org.kuali.common.util.metainf.spring.SqlMetaInfPropertySourceConfig;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MetaInfSqlMavenPropertySourceConfig extends SqlMetaInfPropertySourceConfig {
+public class StudentSqlMetaInfPropertySourceConfig extends SqlMetaInfPropertySourceConfig {
 
 	protected static final String KS_METAINF_SQL_CONFIG_ID = Constants.GROUP_ID + ":" + Constants.ARTIFACT_ID + ":" + SqlMetaInfPropertySourceConfig.METAINF_SQL_CONTEXT_ID;
 
