7de4531a58e0df14067bdf65d5c8ccd3bb26b0eb
peggy <peggy@kuali.org>
2013-07-15 05:50:03 -0400

    KSENROLL-8005:  Change Rule types  that are miscategorized  and inserted on Antirequisite category  instead of Student Eligibility and Prerequisite
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@51670 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M7/2013-05-20-KRMS_TYPE-remove-ao-and-co-types.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M7/2013-05-20-KRMS_TYPE-remove-ao-and-co-types.sql
@@ -199,16 +199,14 @@ INSERT INTO KRMS_TYP_RELN_T (ACTV,FROM_TYP_ID,RELN_TYP,SEQ_NO,TO_TYP_ID,TYP_RELN
 INSERT INTO KRMS_TYP_RELN_T (ACTV,FROM_TYP_ID,RELN_TYP,SEQ_NO,TO_TYP_ID,TYP_RELN_ID,VER_NBR)
   VALUES ('Y',(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.course.academicReadiness.studentEligibilityPrereq'),'A',25,(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.less.than.class.standing'),'KS-KRMS-NL-USAGE-1158',0)
 /
-
---KSENROLL 7076 antireqs
 INSERT INTO KRMS_TYP_RELN_T (ACTV,FROM_TYP_ID,RELN_TYP,SEQ_NO,TO_TYP_ID,TYP_RELN_ID,VER_NBR)
-  VALUES ('Y',(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.course.academicReadiness.antireq'),'A',7,(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.notadmitted.to.program'),'KS-KRMS-NL-USAGE-1159',0)
+  VALUES ('Y',(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.course.academicReadiness.studentEligibilityPrereq'),'A',7,(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.notadmitted.to.program'),'KS-KRMS-NL-USAGE-1159',0)
 /
 INSERT INTO KRMS_TYP_RELN_T (ACTV,FROM_TYP_ID,RELN_TYP,SEQ_NO,TO_TYP_ID,TYP_RELN_ID,VER_NBR)
-  VALUES ('Y',(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.course.academicReadiness.antireq'),'A',8,(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.notadmitted.to.program.in.class.standing'),'KS-KRMS-NL-USAGE-1160',0)
+  VALUES ('Y',(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.course.academicReadiness.studentEligibilityPrereq'),'A',8,(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.notadmitted.to.program.in.class.standing'),'KS-KRMS-NL-USAGE-1160',0)
 /
 INSERT INTO KRMS_TYP_RELN_T (ACTV,FROM_TYP_ID,RELN_TYP,SEQ_NO,TO_TYP_ID,TYP_RELN_ID,VER_NBR)
-  VALUES ('Y',(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.course.academicReadiness.antireq'),'A',9,(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.notin.class.standing'),'KS-KRMS-NL-USAGE-1161',0)
+  VALUES ('Y',(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.course.academicReadiness.studentEligibilityPrereq'),'A',9,(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.notin.class.standing'),'KS-KRMS-NL-USAGE-1161',0)
 /
 
 --KSENROLL 7076 remove relationships
