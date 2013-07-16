2540887591575e4e65430333927e925b91ccaa9f
jcaddel <jcaddel@kuali.org>
2013-07-16 14:56:03 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51843 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/DeployConfigConstants.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/DeployConfigConstants.java
@@ -13,8 +13,11 @@ public class DeployConfigConstants {
 
 	protected static List<ConfigRequest> getInitSourceDbConfigRequests() {
 		List<ConfigRequest> requests = new ArrayList<ConfigRequest>();
+		// Need all the sql and jdbc related properties
 		requests.addAll(JdbcConfigConstants.JDBC_CONFIG);
+		// This is included so we can re-use the properties that determined the names of the .resources files
 		requests.add(new SqlMetaInfConfig());
+		// KS specific config for connecting to Amazon RDS
 		requests.add(new InitializeSourceDbConfigRequest());
 		return requests;
 	}
