a9bad0921bb22754f953aa0b1e5d96758fef1447
jcaddel <jcaddel@kuali.org>
2013-07-16 15:37:30 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51861 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/InitializeSourceDbConfigRequest.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/InitializeSourceDbConfigRequest.java
@@ -1,12 +1,14 @@
 package org.kuali.student.deploy.config;
 
+import org.kuali.student.deploy.spring.DeployProjectConstants;
 
 public class InitializeSourceDbConfigRequest extends StudentConfigRequest {
 
 	public static final String CONTEXT_ID = "source-db:initialize";
+	public static final String ARTIFACT_ID = DeployProjectConstants.ARTIFACT_ID;
 
 	public InitializeSourceDbConfigRequest() {
-		super(CONTEXT_ID);
+		super(ARTIFACT_ID, CONTEXT_ID);
 	}
 
 }
