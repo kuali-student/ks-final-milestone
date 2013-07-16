bc27d8b09b3e22d367dd57f54d384bd6841e11f7
jcaddel <jcaddel@kuali.org>
2013-07-16 15:44:21 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51863 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/InitializeSourceDbPropertySourceConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/InitializeSourceDbPropertySourceConfig.java
@@ -3,12 +3,12 @@ package org.kuali.student.deploy.spring;
 import java.util.List;
 
 import org.kuali.common.util.config.ConfigUtils;
-import org.kuali.common.util.config.spring.BasicPropertySourceConfig;
+import org.kuali.common.util.config.spring.BuildPropertySourceConfig;
 import org.kuali.student.deploy.config.DeployConfigConstants;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class InitializeSourceDbPropertySourceConfig extends BasicPropertySourceConfig {
+public class InitializeSourceDbPropertySourceConfig extends BuildPropertySourceConfig {
 
 	@Override
 	protected List<String> getConfigIds() {
