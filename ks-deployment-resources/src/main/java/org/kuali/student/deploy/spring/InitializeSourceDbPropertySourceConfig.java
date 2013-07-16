7641c89d51c9d20885a82842d9f9ac940e33ae9b
jcaddel <jcaddel@kuali.org>
2013-07-16 14:35:54 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51836 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/InitializeSourceDbPropertySourceConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/InitializeSourceDbPropertySourceConfig.java
@@ -1,32 +1,18 @@
 package org.kuali.student.deploy.spring;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.kuali.common.jdbc.config.JdbcConfigRequest;
-import org.kuali.common.jdbc.config.SqlConfigRequest;
-import org.kuali.common.util.config.ConfigRequest;
 import org.kuali.common.util.config.ConfigUtils;
 import org.kuali.common.util.config.spring.BasicPropertySourceConfig;
-import org.kuali.student.deploy.config.InitializeSourceDbConfigRequest;
+import org.kuali.student.deploy.config.ConfigConstants;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class InitializeSourceDbPropertySourceConfig extends BasicPropertySourceConfig {
 
-	protected static final List<ConfigRequest> CONFIG_REQUESTS = getConfigRequests();
-
 	@Override
 	protected List<String> getConfigIds() {
-		return ConfigUtils.getConfigIds(CONFIG_REQUESTS);
-	}
-
-	protected static List<ConfigRequest> getConfigRequests() {
-		List<ConfigRequest> requests = new ArrayList<ConfigRequest>();
-		requests.add(new SqlConfigRequest());
-		requests.add(new JdbcConfigRequest());
-		requests.add(new InitializeSourceDbConfigRequest());
-		return requests;
+		return ConfigUtils.getConfigIds(ConfigConstants.INIT_SOURCE_DB);
 	}
 
 }
