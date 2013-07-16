e632969eefd98476ed7f08e5839861a95c54e358
jcaddel <jcaddel@kuali.org>
2013-07-16 16:50:54 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51867 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/DeployConfigConstants.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/DeployConfigConstants.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.kuali.common.jdbc.config.JdbcConfigConstants;
 import org.kuali.common.util.config.ConfigRequest;
-import org.kuali.common.util.metainf.SqlMetaInfConfig;
 
 public class DeployConfigConstants {
 
@@ -16,7 +15,7 @@ public class DeployConfigConstants {
 		// Need all the sql and jdbc related properties
 		requests.addAll(JdbcConfigConstants.JDBC_CONFIG);
 		// Re-use the properties from metainf:sql that created the .resources files
-		requests.add(new SqlMetaInfConfig());
+		requests.add(new ConfigRequest("org.kuali.common:kuali-util:metainf:sql:runtime"));
 		// KS specific config for connecting to Amazon RDS
 		requests.add(new InitializeSourceDbConfigRequest());
 		return requests;
