da81477843e3d790b158661240929603c5135deb
jcaddel <jcaddel@kuali.org>
2013-07-16 17:37:01 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51877 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/InitializeSourceDbPropertySourceConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/InitializeSourceDbPropertySourceConfig.java
@@ -3,26 +3,16 @@ package org.kuali.student.deploy.spring;
 import java.util.List;
 
 import org.kuali.common.util.config.ConfigUtils;
-import org.kuali.common.util.config.spring.RuntimePropertySourceConfig;
+import org.kuali.common.util.config.spring.BuildPropertySourceConfig;
 import org.kuali.student.deploy.config.DeployConfigConstants;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class InitializeSourceDbPropertySourceConfig extends RuntimePropertySourceConfig {
+public class InitializeSourceDbPropertySourceConfig extends BuildPropertySourceConfig {
 
 	@Override
 	protected List<String> getConfigIds() {
 		return ConfigUtils.getConfigIds(DeployConfigConstants.INIT_SOURCE_DB_CONFIG);
-	}
-
-	@Override
-	protected String getGroupId() {
-		return DeployProjectConstants.GROUP_ID;
-	}
-
-	@Override
-	protected String getArtifactId() {
-		return DeployProjectConstants.ARTIFACT_ID;
 	}
 
 }
