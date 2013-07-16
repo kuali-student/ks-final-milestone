7a70c249e7fbf2f1ab627114845d7f1b01e555d3
jcaddel <jcaddel@kuali.org>
2013-07-15 21:44:21 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51729 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/Constants.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/Constants.java
@@ -1,15 +1,15 @@
 package org.kuali.student.deploy.spring;
 
+import org.kuali.common.util.ProjectConstants;
 import org.kuali.common.util.ProjectUtils;
 
 public abstract class Constants {
 
-	public static final String GROUP_ID = "org.kuali.student.deployments";
-	public static final String GROUP_ID_BASE = "org.kuali.student";
+	public static final String GROUP_ID = ProjectConstants.STUDENT_GROUP_ID;
+	public static final String GROUP_ID_BASE = GROUP_ID;
 	public static final String ARTIFACT_ID = "ks-deployment-resources";
-	public static final String KS_DB_GROUP_ID = "org.kuali.student.db";
 	public static final String KS_SOURCE_DB = "ks-source-db";
-	public static final String KS_SOURCE_DB_RESOURCE_PREFIX = ProjectUtils.getClassPathPrefix(Constants.GROUP_ID_BASE, Constants.KS_SOURCE_DB);
+	public static final String KS_SOURCE_DB_RESOURCE_PREFIX = ProjectUtils.getClassPathPrefix(GROUP_ID, Constants.KS_SOURCE_DB);
 	public static final String KS_SOURCE_DB_COMMON = KS_SOURCE_DB_RESOURCE_PREFIX + "/common.properties";
 
 }
