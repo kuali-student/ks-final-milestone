91560c08828c9f297752cd0effbf3621025d94e0
jcaddel <jcaddel@kuali.org>
2013-07-17 16:03:26 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51943 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/MetaInfConfigConstants.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/MetaInfConfigConstants.java
@@ -1,9 +1,9 @@
 package org.kuali.student.deploy.config;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import org.kuali.common.util.CollectionUtils;
 import org.kuali.common.util.config.ConfigUtils;
 import org.kuali.student.deploy.spring.DeployProjectConstants;
 
@@ -12,9 +12,21 @@ public class MetaInfConfigConstants {
 	// Shorthand for GroupId + ArtifactId
 	private static final String GA = DeployProjectConstants.GROUP_ID + ":" + DeployProjectConstants.ARTIFACT_ID;
 
-	public static final String SQL_METAINF_CONFIG_ID = ConfigUtils.getIdString(GA, org.kuali.common.util.metainf.MetaInfConfigConstants.SQL_CONTEXT_ID);
+	// Shorthand for stuff in kuali-util constants area
+	private static final String UTIL_SQL_CONTEXT_ID = org.kuali.common.util.metainf.MetaInfConfigConstants.SQL_CONTEXT_ID;
+	private static final List<String> UTIL_SQL_BUILD_CONTEXT_IDS = org.kuali.common.util.metainf.MetaInfConfigConstants.SQL_BUILD_CONFIG_IDS;
 
-	public static final List<String> SQL_METAINF_CONFIG_IDS = CollectionUtils.combine(org.kuali.common.util.metainf.MetaInfConfigConstants.SQL_BUILD_CONFIG_IDS,
-			Arrays.asList(SQL_METAINF_CONFIG_ID));
+	// Re-use the context id from kuali-util
+	public static final String SQL_METAINF_CONFIG_ID = ConfigUtils.getIdString(GA, UTIL_SQL_CONTEXT_ID);
+
+	// Produce the unmodifiable list of config ids needed to property package KS SQL
+	public static final List<String> SQL_METAINF_CONFIG_IDS = getSqlMetaInfConfigIds();
+
+	protected static List<String> getSqlMetaInfConfigIds() {
+		List<String> list = new ArrayList<String>();
+		list.addAll(UTIL_SQL_BUILD_CONTEXT_IDS);
+		list.add(SQL_METAINF_CONFIG_ID);
+		return Collections.unmodifiableList(list);
+	}
 
 }
