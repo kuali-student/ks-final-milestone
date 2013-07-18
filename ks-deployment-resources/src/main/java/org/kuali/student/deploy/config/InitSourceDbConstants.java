6f1c6bb10edef845db6cdb212eaa7cedde57ce3d
jcaddel <jcaddel@kuali.org>
2013-07-18 03:13:52 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@52008 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/InitSourceDbConstants.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/InitSourceDbConstants.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.kuali.common.jdbc.config.JdbcConfigConstants;
+import org.kuali.common.jdbc.config.KualiJdbcConfig;
+import org.kuali.common.jdbc.config.KualiSqlConfig;
 import org.kuali.common.util.config.KualiUtilConfig;
 
 public class InitSourceDbConstants {
@@ -14,17 +15,10 @@ public class InitSourceDbConstants {
 
 	protected static List<String> getSourceDbConfigIds() {
 		List<String> ids = new ArrayList<String>();
-
-		// We are connecting to a database so we need whatever it is JDBC needs
-		ids.addAll(JdbcConfigConstants.DEFAULT_CONFIG_IDS);
-
-		// Re-use properties from org.kuali.common:kuali-util:metainf:sql that helped create the .resources files
+		ids.add(KualiSqlConfig.DEFAULT.getConfigId());
+		ids.add(KualiJdbcConfig.DEFAULT.getConfigId());
 		ids.add(KualiUtilConfig.METAINF_SQL.getConfigId());
-
-		// KS specific config for connecting to Amazon RDS
 		ids.add(KSDeploymentResourcesConfig.SOURCE_DB_INIT.getConfigId());
-
-		// Make sure the list is read-only
 		return Collections.unmodifiableList(ids);
 	}
 
