84851ffe15a69cd3d3fd2205882c0d45636daea7
jcaddel <jcaddel@kuali.org>
2013-07-18 03:45:21 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@52011 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/UpdateScmPropertySourceConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/UpdateScmPropertySourceConfig.java
@@ -3,9 +3,9 @@ package org.kuali.student.deploy.spring;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.kuali.common.impex.StagingConfigConstants;
+import org.kuali.common.impex.config.KualiImpexExportConfig;
+import org.kuali.common.util.config.KualiUtilConfig;
 import org.kuali.common.util.config.spring.BuildPropertySourceConfig;
-import org.kuali.common.util.scm.ScmConstants;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,8 +14,8 @@ public class UpdateScmPropertySourceConfig extends BuildPropertySourceConfig {
 	@Override
 	protected List<String> getConfigIds() {
 		List<String> list = new ArrayList<String>();
-		list.addAll(StagingConfigConstants.CONFIG_IDS);
-		list.addAll(ScmConstants.CONFIG_IDS);
+		list.add(KualiUtilConfig.SCM.getConfigId());
+		list.add(KualiImpexExportConfig.STAGING_BUILD.getConfigId());
 		return list;
 	}
 
