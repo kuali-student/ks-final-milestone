587253339c9101bb2b24fedd40f5f0910c77e2a5
jcaddel <jcaddel@kuali.org>
2013-07-17 23:46:02 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51991 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/KSDeploymentResourcesConfig.java
@@ -0,0 +1,48 @@
+package org.kuali.student.deploy.config;
+
+import org.kuali.common.util.config.ConfigUtils;
+import org.kuali.common.util.config.KualiUtilConfig;
+import org.kuali.common.util.config.ProjectConfig;
+import org.kuali.common.util.project.ImmutableProject;
+import org.kuali.student.deploy.spring.DeployProjectConstants;
+
+public enum KSDeploymentResourcesConfig implements ProjectConfig {
+
+	METAINF_SQL(KualiUtilConfig.METAINF_SQL.getContextId()), //
+	SOURCE_DB_INIT("source-db:initialize"), //
+	SOURCE_DB_DUMP("source-db:dump");
+
+	private final ImmutableProject project = DeployProjectConstants.PROJECT;
+	private final String contextId;
+	private final String configId;
+
+	private KSDeploymentResourcesConfig() {
+		this(null);
+	}
+
+	private KSDeploymentResourcesConfig(String contextId) {
+		this.contextId = contextId;
+		this.configId = ConfigUtils.getConfigId(this);
+	}
+
+	@Override
+	public String getGroupId() {
+		return project.getGroupId();
+	}
+
+	@Override
+	public String getArtifactId() {
+		return project.getArtifactId();
+	}
+
+	@Override
+	public String getContextId() {
+		return contextId;
+	}
+
+	@Override
+	public String getConfigId() {
+		return configId;
+	}
+
+}
