632902de998e2758106bfc0d0d2cee642c0c4352
jcaddel <jcaddel@kuali.org>
2013-07-18 11:00:21 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@52046 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/InitSourceDbConstants.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/InitSourceDbConstants.java
@@ -1,25 +1,6 @@
 package org.kuali.student.deploy.config;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import org.kuali.common.jdbc.config.KualiJdbcConfig;
-import org.kuali.common.jdbc.config.KualiSqlConfig;
-import org.kuali.common.util.config.KualiUtilConfig;
 
 public class InitSourceDbConstants {
-
-	// The complete list of configId's needed to initialize KS_SOURCE_DB
-	public static final List<String> SOURCE_DB_CONFIG_IDS = getSourceDbConfigIds();
-
-	protected static List<String> getSourceDbConfigIds() {
-		List<String> ids = new ArrayList<String>();
-		ids.add(KualiSqlConfig.DEFAULT.getConfigId());
-		ids.add(KualiJdbcConfig.DEFAULT.getConfigId());
-		ids.add(KualiUtilConfig.METAINF_SQL.getConfigId());
-		ids.add(KSDeploymentResourcesConfig.SOURCE_DB_INIT.getConfigId());
-		return Collections.unmodifiableList(ids);
-	}
 
 }
--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/SourceDbConstants.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/SourceDbConstants.java
@@ -7,13 +7,26 @@ import java.util.List;
 import org.kuali.common.impex.config.KualiImpexExportConfig;
 import org.kuali.common.jdbc.config.KualiJdbcConfig;
 import org.kuali.common.jdbc.config.KualiSqlConfig;
+import org.kuali.common.util.config.KualiUtilConfig;
 
 public class SourceDbConstants {
 
 	// The complete list of configId's needed to dump KS_SOURCE_DB
-	public static final List<String> DUMP_SOURCE_DB_CONFIG_IDS = getSourceDbConfigIds();
+	public static final List<String> DUMP_SOURCE_DB_CONFIG_IDS = getDumpSourceDbConfigIds();
 
-	protected static List<String> getSourceDbConfigIds() {
+	// The complete list of configId's needed to initialize KS_SOURCE_DB
+	public static final List<String> INIT_SOURCE_DB_CONFIG_IDS = getInitSourceDbConfigIds();
+
+	protected static List<String> getInitSourceDbConfigIds() {
+		List<String> ids = new ArrayList<String>();
+		ids.add(KualiSqlConfig.DEFAULT.getConfigId());
+		ids.add(KualiJdbcConfig.DEFAULT.getConfigId());
+		ids.add(KualiUtilConfig.METAINF_SQL.getConfigId());
+		ids.add(KSDeploymentResourcesConfig.SOURCE_DB_INIT.getConfigId());
+		return Collections.unmodifiableList(ids);
+	}
+
+	protected static List<String> getDumpSourceDbConfigIds() {
 		List<String> ids = new ArrayList<String>();
 		ids.add(KualiSqlConfig.DEFAULT.getConfigId());
 		ids.add(KualiJdbcConfig.DEFAULT.getConfigId());
