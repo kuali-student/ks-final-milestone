1c407bd8bcc3ffbb06f8442ad75cd9a1b4aa9f70
jcaddel <jcaddel@kuali.org>
2013-07-18 03:36:32 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@52010 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/DumpSourceDbConstants.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/DumpSourceDbConstants.java
@@ -4,27 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.kuali.common.impex.DumpConfigConstants;
+import org.kuali.common.impex.config.KualiImpexExportConfig;
 import org.kuali.common.jdbc.config.KualiJdbcConfig;
 import org.kuali.common.jdbc.config.KualiSqlConfig;
-import org.kuali.common.util.Str;
-import org.kuali.student.deploy.DeployProjectConstants;
 
 public class DumpSourceDbConstants {
-
-	// Shorthand for GroupId + ArtifactId
-	private static final String GA = Str.getId(DeployProjectConstants.GROUP_ID, DeployProjectConstants.ARTIFACT_ID);
-
-	// Config related to dumping something
-	// TODO Move to kuali-impex-export?
-	public static final String DUMP = "dump";
-
-	// Context id for config related to dumping a source database
-	// TODO Move to kuali-impex-export?
-	public static final String SOURCE_DB_CONTEXT_ID = Str.getId(SourceDbConstants.SOURCE_DB, DUMP);
-
-	// Fully qualified config id related to dumping KS_SOURCE_DB
-	public static final String SOURCE_DB_CONFIG_ID = Str.getId(GA, SOURCE_DB_CONTEXT_ID);
 
 	// The complete list of configId's needed to dump KS_SOURCE_DB
 	public static final List<String> SOURCE_DB_CONFIG_IDS = getSourceDbConfigIds();
@@ -33,15 +17,9 @@ public class DumpSourceDbConstants {
 		List<String> ids = new ArrayList<String>();
 		ids.add(KualiSqlConfig.DEFAULT.getConfigId());
 		ids.add(KualiJdbcConfig.DEFAULT.getConfigId());
-
-		// Add whatever kuali-impex-export says it needs
-		ids.addAll(DumpConfigConstants.CONFIG_IDS);
-
-		// KS launches this via the Maven CLI as a build process
-		ids.add(DumpConfigConstants.BUILD_CONFIG_ID);
-
-		// KS specific config for connecting to Amazon RDS
-		ids.add(SOURCE_DB_CONFIG_ID);
+		ids.add(KualiImpexExportConfig.DUMP.getConfigId());
+		ids.add(KualiImpexExportConfig.DUMP_BUILD.getConfigId());
+		ids.add(KSDeploymentResourcesConfig.SOURCE_DB_DUMP.getConfigId());
 		return Collections.unmodifiableList(ids);
 	}
 
--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/DumpSourceDbPropertySourceConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/DumpSourceDbPropertySourceConfig.java
@@ -1,9 +1,13 @@
 package org.kuali.student.deploy.spring;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.kuali.common.impex.config.KualiImpexExportConfig;
+import org.kuali.common.jdbc.config.KualiJdbcConfig;
+import org.kuali.common.jdbc.config.KualiSqlConfig;
 import org.kuali.common.util.config.spring.BuildPropertySourceConfig;
-import org.kuali.student.deploy.config.DumpSourceDbConstants;
+import org.kuali.student.deploy.config.KSDeploymentResourcesConfig;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,7 +15,13 @@ public class DumpSourceDbPropertySourceConfig extends BuildPropertySourceConfig 
 
 	@Override
 	protected List<String> getConfigIds() {
-		return DumpSourceDbConstants.SOURCE_DB_CONFIG_IDS;
+		List<String> ids = new ArrayList<String>();
+		ids.add(KualiSqlConfig.DEFAULT.getConfigId());
+		ids.add(KualiJdbcConfig.DEFAULT.getConfigId());
+		ids.add(KualiImpexExportConfig.DUMP.getConfigId());
+		ids.add(KualiImpexExportConfig.DUMP_BUILD.getConfigId());
+		ids.add(KSDeploymentResourcesConfig.SOURCE_DB_DUMP.getConfigId());
+		return ids;
 	}
 
 }
