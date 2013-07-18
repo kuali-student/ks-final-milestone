21b5c12ddf284a57a1644b0c58651cb0a90ea43b
jcaddel <jcaddel@kuali.org>
2013-07-18 01:41:25 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@52003 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/MetaInfConstants.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/MetaInfConstants.java
@@ -14,9 +14,16 @@ public class MetaInfConstants {
 	public static final List<String> SQL_METAINF_CONFIG_IDS = getSqlMetaInfConfigIds();
 
 	protected static List<String> getSqlMetaInfConfigIds() {
+		System.out.println("1");
 		List<String> configIds = new ArrayList<String>();
+		System.out.println("2");
 		configIds.add(KualiUtilConfig.METAINF_SQL_BUILD.getConfigId());
+		System.out.println("3");
 		configIds.add(KSDeploymentResourcesConfig.METAINF_SQL.getConfigId());
+		System.out.println("configIds.size()=" + configIds.size());
+		for (String configId : configIds) {
+			System.out.println("configId=" + configId);
+		}
 		return Collections.unmodifiableList(configIds);
 	}
 
