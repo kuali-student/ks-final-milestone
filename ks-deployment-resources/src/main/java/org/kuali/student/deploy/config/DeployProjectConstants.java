2b7b44dbf1e0c1840715d78096787a11090efcde
jcaddel <jcaddel@kuali.org>
2013-07-21 15:28:16 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@52239 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/DeployProjectConstants.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/DeployProjectConstants.java
@@ -5,7 +5,7 @@ import org.kuali.common.util.project.KualiConstants;
 public abstract class DeployProjectConstants {
 
 	public static final String GROUP_ID = KualiConstants.STUDENT_GROUP_ID;
-	public static final String ARTIFACT_ID = "ks-deployment-rsources";
+	public static final String ARTIFACT_ID = "ks-deployment-resources";
 	public static final String PROJECT_ID = GROUP_ID + ":" + ARTIFACT_ID;
 
 }
