260ef18afe61c62a58a93d3bacd1b43306ca43ea
jcaddel <jcaddel@kuali.org>
2013-07-21 14:38:51 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@52231 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/DeployProjectConstants.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/DeployProjectConstants.java
@@ -1,17 +1,17 @@
 package org.kuali.student.deploy;
 
-import org.kuali.common.util.KualiProjectConstants;
 import org.kuali.common.util.project.ImmutableProject;
+import org.kuali.common.util.project.KualiConstants;
 
 public abstract class DeployProjectConstants {
 
-	public static final ImmutableProject PROJECT = new ImmutableProject(KualiProjectConstants.STUDENT_GROUP_ID, "ks-deployment-resources");
+	public static final ImmutableProject PROJECT = new ImmutableProject(KualiConstants.STUDENT_GROUP_ID, "ks-deployment-resources");
 
 	@Deprecated
 	public static final String KS_SOURCE_DB_ARTIFACT_ID = "ks-source-db";
 
 	@Deprecated
-	public static final String GROUP_ID = KualiProjectConstants.STUDENT_GROUP_ID;
+	public static final String GROUP_ID = KualiConstants.STUDENT_GROUP_ID;
 
 	@Deprecated
 	public static final String ARTIFACT_ID = "ks-deployment-resources";
