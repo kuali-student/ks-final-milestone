169e8f387fb5863583944b0c351b9bc0b49bea30
jcaddel <jcaddel@kuali.org>
2013-07-21 15:08:30 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@52234 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/DeployProjectConstants.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/DeployProjectConstants.java
@@ -1,28 +1,11 @@
 package org.kuali.student.deploy;
 
-import org.kuali.common.util.project.ImmutableProject;
 import org.kuali.common.util.project.KualiConstants;
 
 public abstract class DeployProjectConstants {
 
-	public static final ImmutableProject PROJECT = new ImmutableProject(KualiConstants.STUDENT_GROUP_ID, "ks-deployment-resources");
-
-	@Deprecated
-	public static final String KS_SOURCE_DB_ARTIFACT_ID = "ks-source-db";
-
-	@Deprecated
 	public static final String GROUP_ID = KualiConstants.STUDENT_GROUP_ID;
-
-	@Deprecated
-	public static final String ARTIFACT_ID = "ks-deployment-resources";
-
-	@Deprecated
-	public static final String GROUP_ID_BASE = GROUP_ID;
-
-	@Deprecated
-	public static final String KS_SOURCE_DB_RESOURCE_PREFIX = null; // ProjectUtils.getClassPathPrefix(GROUP_ID, KS_SOURCE_DB_ARTIFACT_ID);
-
-	@Deprecated
-	public static final String KS_SOURCE_DB_COMMON = KS_SOURCE_DB_RESOURCE_PREFIX + "/common.properties";
+	public static final String ARTIFACT_ID = "ks-deployment-rsources";
+	public static final String PROJECT_ID = GROUP_ID + ":" + ARTIFACT_ID;
 
 }
--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/KSDeploymentResourcesConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/KSDeploymentResourcesConfig.java
@@ -3,7 +3,6 @@ package org.kuali.student.deploy.config;
 import org.kuali.common.util.config.ConfigUtils;
 import org.kuali.common.util.config.KualiUtilConfig;
 import org.kuali.common.util.config.ProjectConfig;
-import org.kuali.common.util.project.ImmutableProject;
 import org.kuali.student.deploy.DeployProjectConstants;
 
 public enum KSDeploymentResourcesConfig implements ProjectConfig {
@@ -14,7 +13,6 @@ public enum KSDeploymentResourcesConfig implements ProjectConfig {
 	SOURCE_DB_SCM("source-db:scm"), //
 	INIT_APP_DB("init-app");
 
-	private final ImmutableProject project = DeployProjectConstants.PROJECT;
 	private final String contextId;
 	private final String configId;
 
@@ -25,12 +23,12 @@ public enum KSDeploymentResourcesConfig implements ProjectConfig {
 
 	@Override
 	public String getGroupId() {
-		return project.getGroupId();
+		return DeployProjectConstants.GROUP_ID;
 	}
 
 	@Override
 	public String getArtifactId() {
-		return project.getArtifactId();
+		return DeployProjectConstants.ARTIFACT_ID;
 	}
 
 	@Override
