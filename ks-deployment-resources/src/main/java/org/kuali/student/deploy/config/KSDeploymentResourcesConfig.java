170fe91de6327cb39cd49d157fa3eb4b3649d179
jcaddel <jcaddel@kuali.org>
2013-07-23 17:35:48 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@52440 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/KSDeploymentResourcesConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/KSDeploymentResourcesConfig.java
@@ -6,39 +6,39 @@ import org.kuali.common.util.config.ProjectConfig;
 
 public enum KSDeploymentResourcesConfig implements ProjectConfig {
 
-	METAINF_SQL(KualiUtilConfig.METAINF_SQL.getContextId()), //
-	SOURCE_DB_INIT("source-db:initialize"), //
-	SOURCE_DB_DUMP("source-db:dump"), //
-	SOURCE_DB_SCM("source-db:scm"), //
-	APP_DB_INIT("app-db:init"),
-    DEPLOY("DEPLOY");
+    METAINF_SQL(KualiUtilConfig.METAINF_SQL.getContextId()), //
+    SOURCE_DB_INIT("source-db:initialize"), //
+    SOURCE_DB_DUMP("source-db:dump"), //
+    SOURCE_DB_SCM("source-db:scm"), //
+    APP_DB_INIT("app-db:init"), //
+    DEPLOY("deploy");
 
-	private final String contextId;
-	private final String configId;
+    private final String contextId;
+    private final String configId;
 
-	private KSDeploymentResourcesConfig(String contextId) {
-		this.contextId = contextId;
-		this.configId = ConfigUtils.getConfigId(this);
-	}
+    private KSDeploymentResourcesConfig(String contextId) {
+        this.contextId = contextId;
+        this.configId = ConfigUtils.getConfigId(this);
+    }
 
-	@Override
-	public String getGroupId() {
-		return DeployProjectConstants.GROUP_ID;
-	}
+    @Override
+    public String getGroupId() {
+        return DeployProjectConstants.GROUP_ID;
+    }
 
-	@Override
-	public String getArtifactId() {
-		return DeployProjectConstants.ARTIFACT_ID;
-	}
+    @Override
+    public String getArtifactId() {
+        return DeployProjectConstants.ARTIFACT_ID;
+    }
 
-	@Override
-	public String getContextId() {
-		return contextId;
-	}
+    @Override
+    public String getContextId() {
+        return contextId;
+    }
 
-	@Override
-	public String getConfigId() {
-		return configId;
-	}
+    @Override
+    public String getConfigId() {
+        return configId;
+    }
 
 }
