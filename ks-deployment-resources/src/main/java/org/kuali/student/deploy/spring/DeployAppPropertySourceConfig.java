1a55e30b728495fd29399042fd16ccc82ef34c6a
jcaddel <jcaddel@kuali.org>
2013-07-24 17:17:37 -0400

    Add metainf properties
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@52549 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/DeployAppPropertySourceConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/DeployAppPropertySourceConfig.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.kuali.common.deploy.config.KualiDeployConfig;
+import org.kuali.common.util.config.KualiUtilConfig;
 import org.kuali.student.deploy.config.KSDeploymentResourcesConfig;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,7 @@ public class DeployAppPropertySourceConfig extends InitAppDbPropertySourceConfig
     protected List<String> getConfigIds() {
         List<String> configIds = new ArrayList<String>();
         configIds.addAll(super.getBaseConfigIds());
+        configIds.add(KualiUtilConfig.METAINF_MPX.getConfigId());
         configIds.add(KualiDeployConfig.DEFAULT.getConfigId());
         configIds.add(super.getAppConfigId(BASE_CONFIG_ID));
         return configIds;
