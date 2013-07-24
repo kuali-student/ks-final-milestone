745ccd91dccb6ef255713e7a762f2f9d0dc12731
jcaddel <jcaddel@kuali.org>
2013-07-24 15:59:09 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@52535 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/KSDeploymentResourcesConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/config/KSDeploymentResourcesConfig.java
@@ -14,6 +14,7 @@ public enum KSDeploymentResourcesConfig implements ProjectConfig {
     DB_APP_INIT_KS_IMPEX_RICE_DB("db:app:init:ks-impex-rice-db"), //
     DB_APP_INIT_KS_IMPEX_APP_DB("db:app:init:ks-impex-app-db"), //
     DB_APP_INIT_KS_IMPEX_BUNDLED_DB("db:app:init:ks-impex-bundled-db"), //
+    DEPLOY("deploy"), //
     DEPLOY_KS_IMPEX_RICE_DB("deploy:ks-impex-rice-db"), //
     DEPLOY_KS_IMPEX_APP_DB("deploy:ks-impex-app-db"), //
     DEPLOY_KS_IMPEX_BUNDLED_DB("deploy:ks-impex-bundled-db"); //
