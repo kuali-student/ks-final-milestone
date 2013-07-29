a309d95cdb330959b2afe861c233c79f84160a53
sw <sw@kuali.org>
2013-07-29 09:28:47 -0400

    KSENROLL-8166 Set the routepath to null.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@52821 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M7/2013-07-26-KRMS-update-doctype-url-and-update-corequisite-help-text.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M7/2013-07-26-KRMS-update-doctype-url-and-update-corequisite-help-text.sql
@@ -6,3 +6,11 @@ update KREW_DOC_TYP_T set DOC_HDLR_URL = '${application.url}/kr-krad/maintenance
 
 update KRMS_NL_TMPL_T set TMPL = 'Add courses to be taken at the same time as this course.' where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-1404'
 /
+
+INSERT INTO KREW_DOC_TYP_PROC_T (DOC_TYP_PROC_ID, DOC_TYP_ID, INIT_RTE_NODE_ID, NM, INIT_IND, VER_NBR)
+  VALUES ('KS-KREW-DOC-TYP-PROC-3034', (select DOC_TYP_ID from KREW_DOC_TYP_T where DOC_TYP_NM='CORuleMaintenanceDocument' and CUR_IND=1), '', 'PRIMARY', 1, 1)
+/
+
+INSERT INTO KREW_DOC_TYP_PROC_T (DOC_TYP_PROC_ID, DOC_TYP_ID, INIT_RTE_NODE_ID, NM, INIT_IND, VER_NBR)
+  VALUES ('KS-KREW-DOC-TYP-PROC-3035', (select DOC_TYP_ID from KREW_DOC_TYP_T where DOC_TYP_NM='AORuleMaintenanceDocument' and CUR_IND=1), '', 'PRIMARY', 1, 1)
+/
