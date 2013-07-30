c0fb9baae69a2bf5d37db3cc602fb4899739c468
matthew.edgren <matthew.edgren@kuali.org>
2013-07-30 11:36:17 -0400

    KSENROLL-7482 merging from trunk to branch
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/M6@52905 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/CourseWaitListServiceConstants.java
+++ ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/CourseWaitListServiceConstants.java
@@ -11,4 +11,11 @@ public class CourseWaitListServiceConstants {
     public static final String REF_OBJECT_URI_WAIT_LIST = NAMESPACE + "/" + CourseWaitListInfo.class.getSimpleName();
     public static final String REF_OBJECT_URI_WAIT_LIST_ENTRY = NAMESPACE + "/" + CourseWaitListEntryInfo.class.getSimpleName();
 
+
+    public static final String COURSE_WAIT_LIST_WAIT_TYPE_KEY = "kuali.course.waitlist.type.course.waitlist";
+
+
+    public static final String COURSE_WAIT_LIST_ACTIVE_STATE_KEY = "kuali.course.waitlist.state.active";
+    public static final String COURSE_WAIT_LIST_INACTIVE_STATE_KEY = "kuali.course.waitlist.state.inactive";
+
 }
