ba5a29c38b4f5907143e1ce0bcbda7ae41136035
depstein <depstein@kuali.org>
2013-07-30 10:31:25 -0400

    KSENROLL-8191 Added more documentation on the class.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@52898 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/controller/ActivityOfferingControllerTransactionHelper.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/controller/ActivityOfferingControllerTransactionHelper.java
@@ -15,6 +15,20 @@ import javax.servlet.http.HttpServletResponse;
  * Date: 7/23/13
  * Time: 10:54 AM
  * This method is a round-about way to provide a new transaction when calling MaintenanceDocumentController.route()
+ *
+ * In krad-base-servlet.xml lives this gem:
+ *   <aop:config>
+         <aop:pointcut id="controllerTransaction"
+            expression="execution(* org.kuali.rice.krad.web.controller.UifControllerBase+.*(..))"/>
+         <aop:advisor advice-ref="txAdvice" pointcut-ref="controllerTransaction"/>
+     </aop:config>
+ *
+ * Which was proxying all UifControllerBase in a transaction. This is bad if you want to handle exceptions and display
+ * messages to the user without a stacktrace. To get around this, we created this indirect class that can be
+ * transactionally proxied. (Note @Transactional annotation). The propagation is set to requires new, which means that
+ * it will be in a different transaction than the  Controller's transaction and any rollbacks will stop at the proxy.
+ * Then in the controller we can catch exceptions without having our initial transaction rolling back.
+ *
  */
 public class ActivityOfferingControllerTransactionHelper {
 
