68cc1be87b14bb5a5a9fbe5c278dec43c6ef3920
dedewet <dedewet@kuali.org>
2013-08-01 04:31:22 -0400

    KSENROLL-8369 - Reverted change
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@53074 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M7/2013-07-12-KRIM-ks-krms-Permissions.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M7/2013-07-12-KRIM-ks-krms-Permissions.sql
@@ -15,6 +15,8 @@ insert into KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, PERM_ID, KIM_T
 /
 insert into KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, PERM_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL) values ('KS-KRIM-PERM-ATTR-DATA-2010', SYS_GUID(), 1, (SELECT perm_id from krim_perm_t where nm = 'Perform Action for Add/Edit Course Offering Antirequisite Rule' and nmspc_cd = 'KS-ENR'), (SELECT kim_typ_id from krim_typ_t where nm = 'KS ENR Permission Expression' and nmspc_cd = 'KS-ENR'), (SELECT kim_attr_defn_id from krim_attr_defn_t where nm = 'permissionExpression' and nmspc_cd = 'KS-ENR'), '({"open","final","published"}.contains(#socState) and {"kuali.krms.rule.type.course.academicReadiness.antireq"}.contains(#ruleType))')
 /
+insert into KRIM_ROLE_PERM_T (ROLE_PERM_ID, OBJ_ID, VER_NBR, ROLE_ID, PERM_ID, ACTV_IND) values ('KS-KRIM-ROLE-PERM-2004', SYS_GUID(), 1, (SELECT ROLE_ID FROM KRIM_ROLE_T where ROLE_NM = 'KS Department Schedule Coordinator - Org' and nmspc_cd = 'KS-ENR'), (SELECT perm_id from krim_perm_t where nm = 'Perform Action for Add/Edit Course Offering Antirequisite Rule' and nmspc_cd = 'KS-ENR'), 'Y')
+/
 
 insert into KRIM_PERM_T (PERM_ID, OBJ_ID, VER_NBR, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND) values ('KS-KRIM-PERM-2005', SYS_GUID(), 1, (SELECT perm_tmpl_id FROM krim_perm_tmpl_t where nm = 'Perform Action' and nmspc_cd = 'KS-ENR'), 'KS-ENR', 'Perform Action for Add/Edit Course Offering Corequisite Rule', 'Allows the user to Add/Edit Course Offering Corequisite Rule', 'Y')
 /
