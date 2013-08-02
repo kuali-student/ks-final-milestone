804ffd385b807abca405915beb4ca8a783dcdf1d
jcaddel <jcaddel@kuali.org>
2013-08-02 06:31:11 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@53152 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/SqlMetaInfPropertySourceConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/SqlMetaInfPropertySourceConfig.java
@@ -1,17 +1,35 @@
 package org.kuali.student.deploy.spring;
 
 import java.util.List;
+import java.util.Properties;
 
-import org.kuali.common.util.config.spring.ProjectPropertySourceConfig;
-import org.kuali.student.deploy.config.MetaInfConstants;
+import org.kuali.common.util.properties.Location;
+import org.kuali.common.util.properties.PropertiesService;
+import org.kuali.common.util.properties.spring.ProjectPropertiesServiceConfig;
+import org.kuali.common.util.spring.service.PropertySourceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
 
 @Configuration
-public class SqlMetaInfPropertySourceConfig extends ProjectPropertySourceConfig {
+@Import({ SqlLocationsConfig.class, ProjectPropertiesServiceConfig.class })
+public class SqlMetaInfPropertySourceConfig implements PropertySourceConfig {
+
+	@Autowired
+	SqlLocationsConfig sqlLocationsConfig;
+
+	@Autowired
+	ProjectPropertiesServiceConfig projectPropertiesServiceConfig;
 
 	@Override
-	protected List<String> getConfigIds() {
-		return MetaInfConstants.METAINF_SQL_CONFIG_IDS;
+	@Bean
+	public PropertySource<?> propertySource() {
+		List<Location> locations = sqlLocationsConfig.metaInfSqlLocations();
+		PropertiesService service = projectPropertiesServiceConfig.projectPropertiesService();
+		Properties properties = service.getProperties(locations);
+		return new PropertiesPropertySource("propertiesPropertySource", properties);
 	}
-
 }
