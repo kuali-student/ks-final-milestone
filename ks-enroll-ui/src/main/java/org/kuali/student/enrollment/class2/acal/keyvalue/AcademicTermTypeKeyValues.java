2227f038e5d886d4deb1d7ae75390bdbd01de0e8
depstein <depstein@kuali.org>
2013-08-05 11:42:40 -0400

    KSENROLL-8487 Revert:  (was not supposed to be in M7)
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@53271 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/keyvalue/AcademicTermTypeKeyValues.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/acal/keyvalue/AcademicTermTypeKeyValues.java
@@ -20,29 +20,23 @@ import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
 import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
 import org.kuali.rice.krad.uif.view.ViewModel;
+import org.kuali.student.r2.core.acal.service.AcademicCalendarService;
 import org.kuali.student.enrollment.class2.acal.dto.AcademicTermWrapper;
 import org.kuali.student.enrollment.class2.acal.form.AcademicCalendarForm;
-import org.kuali.student.r2.common.exceptions.DoesNotExistException;
-import org.kuali.student.r2.common.exceptions.InvalidParameterException;
-import org.kuali.student.r2.common.exceptions.MissingParameterException;
-import org.kuali.student.r2.common.exceptions.OperationFailedException;
-import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
-import org.kuali.student.r2.common.util.ContextUtils;
+import org.kuali.student.r2.common.constants.CommonServiceConstants;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.exceptions.*;
+import org.kuali.student.r2.core.constants.AcademicCalendarServiceConstants;
 import org.kuali.student.r2.core.class1.type.dto.TypeInfo;
-import org.kuali.student.r2.core.class1.type.dto.TypeTypeRelationInfo;
-import org.kuali.student.r2.core.class1.type.service.TypeService;
-import org.kuali.student.r2.core.constants.AtpServiceConstants;
-import org.kuali.student.r2.core.constants.TypeServiceConstants;
 
 import javax.xml.namespace.QName;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Keyfinder to display a list of available term types
- *
- * This filters out types already on the form as well as any subterms where the parent term is not on the form
  *
  * @author Kuali Student Team
  */
@@ -50,91 +44,64 @@ public class AcademicTermTypeKeyValues extends UifKeyValuesFinderBase implements
 
     private static final long serialVersionUID = 1L;
 
-    private transient TypeService typeService;
+    private transient AcademicCalendarService acalService;
+
+    private static List<TypeInfo> acalTermTypes;
 
     @Override
     public List<KeyValue> getKeyValues(ViewModel model) {
 
-        assert (model instanceof AcademicCalendarForm);
-
         List<KeyValue> keyValues = new ArrayList<KeyValue>();
+        List<String> availableTermTypes = new ArrayList();
 
-        //Grab a list of current term types
-        List<String> currentTermTypes = new ArrayList<String>();
-        for (AcademicTermWrapper termWrapper : ((AcademicCalendarForm) model).getTermWrapperList()) {
-            currentTermTypes.add(termWrapper.getTermType());
+        if (model instanceof AcademicCalendarForm){
+            AcademicCalendarForm acalForm = (AcademicCalendarForm)model;
+
+            for (AcademicTermWrapper termWrapper : acalForm.getTermWrapperList()) {
+                 availableTermTypes.add(termWrapper.getTermType());
+            }
         }
 
+        keyValues.add(new ConcreteKeyValue("", "Select Term Type"));
+
+        List<TypeInfo> types = null;
         try {
-            //Get the subterm types
-            List<String> subtermTypeKeys = getSubtermTypeKeys();
 
-            keyValues.add(new ConcreteKeyValue("", "Select Term Type"));
+            types = getAcalTermTypes();
 
-            for (TypeInfo type : getAcalTermTypes()) {
-                //Filter the keyvalues to terms that are not already in the form and if they are subterms,
-                // the parent term must exist in form
-                if (!currentTermTypes.contains(type.getKey()) &&
-                        (!subtermTypeKeys.contains(type.getKey()) || parentExists(type.getKey(), currentTermTypes))) {
+            for (TypeInfo type : types) {
+                if (!availableTermTypes.contains(type.getKey())){
                     ConcreteKeyValue keyValue = new ConcreteKeyValue();
                     keyValue.setKey(type.getKey());
                     keyValue.setValue(type.getName());
                     keyValues.add(keyValue);
                 }
             }
-
         } catch (Exception e) {
-            throw new RuntimeException("Error getting term key values.", e);
+            throw new RuntimeException(e);
         }
 
         return keyValues;
     }
 
-    /**
-     * @return List of subterm type keys from the type service
-     */
-    private List<String> getSubtermTypeKeys() throws InvalidParameterException, MissingParameterException, DoesNotExistException, PermissionDeniedException, OperationFailedException {
-        List<String> subtermTypeKeys = new ArrayList<String>();
-        List<TypeInfo> subtermTypes = getTypeService().getTypesForGroupType(AtpServiceConstants.ATP_SUBTERM_GROUPING_TYPE_KEY, ContextUtils.createDefaultContextInfo());
-        if (subtermTypes != null) {
-            for (TypeInfo subtermType : subtermTypes) {
-                subtermTypeKeys.add(subtermType.getKey());
-            }
-        }
-        return subtermTypeKeys;
-    }
-
-    /**
-     * Checks if the subterm key has a parent term from currentTermTypes
-     * This is used to restrict subterm values that don't have existing parent terms
-     *
-     * @param subtermKey       subterm key you are check
-     * @param currentTermTypes list of existing term types
-     * @return true if the current TermTypes contains the parent Term of the subterm key
-     */
-    private boolean parentExists(String subtermKey, List<String> currentTermTypes) throws InvalidParameterException, MissingParameterException, DoesNotExistException, PermissionDeniedException, OperationFailedException {
-        //Get a list of parent terms from the type service
-        List<TypeTypeRelationInfo> subtermParentTypeRelations = getTypeService().getTypeTypeRelationsByRelatedTypeAndType(subtermKey, TypeServiceConstants.TYPE_TYPE_RELATION_CONTAINS_TYPE_KEY, ContextUtils.createDefaultContextInfo());
-        if (subtermParentTypeRelations != null) {
-            //Check that the parent term exists (a parent term will have a type relation to the subterm
-            for (TypeTypeRelationInfo subtermParentTypeRelation : subtermParentTypeRelations) {
-                if (currentTermTypes.contains(subtermParentTypeRelation.getOwnerTypeKey())) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
     private List<TypeInfo> getAcalTermTypes() throws InvalidParameterException, MissingParameterException, DoesNotExistException, PermissionDeniedException, OperationFailedException {
-        return getTypeService().getTypesForGroupType(AtpServiceConstants.ATP_TERM_GROUPING_TYPE_KEY, ContextUtils.createDefaultContextInfo());
+
+        if(acalTermTypes == null) {
+
+            //TODO:Build real context.
+            ContextInfo context = new ContextInfo();
+
+            acalTermTypes = Collections.unmodifiableList(getAcalService().getTermTypesForAcademicCalendarType(AcademicCalendarServiceConstants.ACADEMIC_CALENDAR_TYPE_KEY, context));
+        }
+
+        return acalTermTypes;  //To change body of created methods use File | Settings | File Templates.
     }
 
-    public TypeService getTypeService() {
-        if (typeService == null) {
-            typeService = (TypeService) GlobalResourceLoader.getService(new QName(TypeServiceConstants.NAMESPACE, TypeServiceConstants.SERVICE_NAME_LOCAL_PART));
+    public AcademicCalendarService getAcalService() {
+        if(acalService == null) {
+            acalService = (AcademicCalendarService) GlobalResourceLoader.getService(new QName(CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "acal", "AcademicCalendarService"));
         }
-        return this.typeService;
+        return this.acalService;
     }
 
 }
