b9f08b89b455506709fdaa5d4d3c2c8f7cbdd31a
jcaddel <jcaddel@kuali.org>
2013-08-06 21:12:17 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@53411 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/SqlPropertySourceConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/SqlPropertySourceConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ SqlBuildLocationsConfig.class, ProjectPropertiesServiceConfig.class })
-public class SqlBuildPropertySourceConfig extends LocationPropertySourceConfig {
+@Import({ SqlBuildLocationsConfig.class, ProjectPropertiesServiceConfig.class, LocationPropertySourceConfig.class })
+public class SqlPropertySourceConfig {
 
 }
