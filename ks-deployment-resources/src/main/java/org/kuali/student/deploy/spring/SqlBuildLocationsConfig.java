337977cb98c12b6db74cc95ce4743fadeaf88481
jcaddel <jcaddel@kuali.org>
2013-08-06 20:02:13 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@53405 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/SqlBuildLocationsConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/SqlBuildLocationsConfig.java
@@ -12,20 +12,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ SourceDbLocationsConfig.class, org.kuali.common.util.metainf.spring.SqlLocationsConfig.class })
+@Import({ SourceDbLocationsConfig.class })
 public class SqlBuildLocationsConfig implements PropertyLocationsConfig {
 
 	@Autowired
 	SourceDbLocationsConfig sourceDbLocationsConfig;
 
-	@Autowired
-	org.kuali.common.util.metainf.spring.SqlLocationsConfig sqlLocationsConfig;
-
 	@Override
 	@Bean
 	public List<Location> propertyLocations() {
 		List<Location> locations = new ArrayList<Location>();
-		locations.addAll(sqlLocationsConfig.metaInfSqlBuildLocations());
 		locations.add(sourceDbLocationsConfig.ksSourceDbCommon());
 		return Collections.unmodifiableList(locations);
 	}
