adaad4aa6f6a07c5ccf247b9ddf799ed28b0ed5d
jcaddel <jcaddel@kuali.org>
2013-08-06 21:16:46 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@53412 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/SqlBuildLocationsConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/SqlBuildLocationsConfig.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.kuali.common.util.metainf.spring.SqlPropertyLocationsConfig;
 import org.kuali.common.util.properties.Location;
 import org.kuali.common.util.properties.spring.PropertyLocationsConfig;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +13,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ SourceDbLocationsConfig.class })
+@Import({ SqlPropertyLocationsConfig.class, SourceDbLocationsConfig.class })
 public class SqlBuildLocationsConfig implements PropertyLocationsConfig {
 
 	@Autowired
 	SourceDbLocationsConfig sourceDbLocationsConfig;
+
+	@Autowired
+	SqlPropertyLocationsConfig sqlPropertyLocationsConfig;
 
 	@Override
 	@Bean
 	public List<Location> propertyLocations() {
 		List<Location> locations = new ArrayList<Location>();
 		locations.add(sourceDbLocationsConfig.ksSourceDbCommon());
+		locations.add(sqlPropertyLocationsConfig.metaInfSqlLocation());
 		return Collections.unmodifiableList(locations);
 	}
-
 }
