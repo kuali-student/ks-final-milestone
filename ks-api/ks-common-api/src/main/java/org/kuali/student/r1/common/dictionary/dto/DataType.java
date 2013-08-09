11fe800914dd75444ef5230025bf6eea7518a7af
michael.ocleirigh <michael.ocleirigh@kuali.org>
2013-08-09 17:11:30 -0400

    KSENROLL-8475 copy ks-api from sandbox/ksenroll-8475/ks-api to contrib/CM/ks-api/trunk
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ksenroll-8475@53651 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-api/ks-common-api/src/main/java/org/kuali/student/r1/common/dictionary/dto/DataType.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.student.r1.common.dictionary.dto;
-
-import javax.xml.bind.annotation.XmlEnum;
-
-@Deprecated
-@XmlEnum
-public enum DataType {
-	STRING, DATE, TRUNCATED_DATE, BOOLEAN, INTEGER, FLOAT, DOUBLE, LONG, COMPLEX
-}
