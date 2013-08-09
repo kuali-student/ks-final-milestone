3a261c0fd3f9e7955eaa322741b60294c17f088c
michael.ocleirigh <michael.ocleirigh@kuali.org>
2013-08-09 17:11:32 -0400

    KSENROLL-8475 copy ks-core from sandbox/ksenroll-8475/ks-core to contrib/CM/ks-core/trunk
    git-svn-id: https://svn.kuali.org/repos/student/contrib/CM/ks-core/trunk@53652 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum/ks-lum-impl/src/main/java/org/kuali/student/r2/lum/course/service/impl/CourseServiceMethodInvoker.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.student.r2.lum.course.service.impl;
-
-import org.kuali.student.r2.lum.service.assembler.LumServiceMethodInvoker;
-
-public class CourseServiceMethodInvoker extends LumServiceMethodInvoker{
-	
-}
--- ks-lum/ks-lum-sql/src/main/resources/upgrades/17/2012-09-28-r1-EXP_FIRST_ATP-to-r2.sql
+++
@@ -1,8 +0,0 @@
-UPDATE  KSLU_CLU
-SET     EXP_FIRST_ATP = (SELECT ID
-                          FROM  KSEN_ATP
-                          WHERE NAME = (SELECT  NAME
-                                        FROM    KSAP_ATP
-                                        WHERE   ID = KSLU_CLU.EXP_FIRST_ATP))
-WHERE 	EXP_FIRST_ATP LIKE 'kuali%'
-/
