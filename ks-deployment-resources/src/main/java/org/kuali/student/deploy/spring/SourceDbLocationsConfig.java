dcc69622e87c5a8ea51e6c946c88b6cdc9352280
jcaddel <jcaddel@kuali.org>
2013-08-12 01:31:19 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@53728 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/SourceDbLocationsConfig.java
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/SourceDbLocationsConfig.java
@@ -2,7 +2,7 @@ package org.kuali.student.deploy.spring;
 
 import org.kuali.common.util.properties.Location;
 import org.kuali.common.util.properties.PropertiesLocationService;
-import org.kuali.common.util.properties.spring.PropertiesServiceConfig;
+import org.kuali.common.util.properties.spring.PropertiesLocationServiceConfig;
 import org.kuali.student.deploy.config.DeployProjectConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ PropertiesServiceConfig.class })
+@Import({ PropertiesLocationServiceConfig.class })
 public class SourceDbLocationsConfig {
 
 	@Autowired
