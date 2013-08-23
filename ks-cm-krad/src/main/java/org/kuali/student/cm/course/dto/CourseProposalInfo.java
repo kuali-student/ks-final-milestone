dbe7ac814039fbcf5fa230c64610e32e8f735600
lprzybylski <lprzybylski@kuali.org>
2013-08-23 05:37:27 -0400

    KSCM-1024 adding properties to the wrapper
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/branches/CM-KRAD@54470 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-cm-krad/src/main/java/org/kuali/student/cm/course/dto/CourseProposalInfo.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/course/dto/CourseProposalInfo.java
@@ -30,7 +30,6 @@ import org.kuali.student.r2.lum.course.infc.Course;
  */
 @XmlType(name = "CoursePropsalInfo", propOrder = {"courseInfo",
         "proposalInfo" }) 
-
 @XmlAccessorType(XmlAccessType.FIELD)
 public class CourseProposalInfo extends IdEntityInfo implements CourseProposal, Serializable {
 
@@ -39,6 +38,34 @@ public class CourseProposalInfo extends IdEntityInfo implements CourseProposal, 
     private CourseInfo course;
     @XmlElement
     private ProposalInfo proposal;
+    @XmlElement
+    private boolean audit;
+    @XmlElement
+    private boolean passFail;
+    @XmlElement
+    private List<CluInstructorInfoWrapper> instructorDisplays;
+    @XmlElement        
+    private List<CourseJointInfoWrapper> courseJointDisplays;
+        @XmlElement
+    private List<ResultValuesGroupInfoWrapper> creditOptionsDisplay;
+    @XmlElement
+    private String finalExamStatus;
+    @XmlElement
+    private String finalExamRationale;
+    @XmlElement
+    private List<CommentInfo> commentInfos;
+    @XmlElement
+    private LearningObjectiveDialogWrapper loDialogWrapper;
+    @XmlElement
+    private Boolean showAll;
+    @XmlElement
+    private String userId;
+    @XmlElement
+    private List<DecisionInfo> decisions;
+    @XmlElement
+    private List<OrganizationInfoWrapper> administeringOrganizations;
+    @XmlElement
+    private String lastUpdated;
 
     @Override
     public ProposalInfo getProposal() {
@@ -58,4 +85,147 @@ public class CourseProposalInfo extends IdEntityInfo implements CourseProposal, 
         this.course = course;
     }
 
+    /**
+     * Gets the value of audit
+     *
+     * @return the value of audit
+     */
+    public boolean isAudit() {
+        return this.audit;
+    }
+
+    /**
+     * Sets the value of audit
+     *
+     * @param argAudit Value to assign to this.audit
+     */
+    public void setAudit(final boolean argAudit) {
+        this.audit = argAudit;
+    }
+
+    /**
+     * Gets the value of passFail
+     *
+     * @return the value of passFail
+     */
+    public boolean isPassFail() {
+        return this.passFail;
+    }
+
+    /**
+     * Sets the value of passFail
+     *
+     * @param argPassFail Value to assign to this.passFail
+     */
+    public void setPassFail(final boolean argPassFail) {
+        this.passFail = argPassFail;
+    }
+
+    /**
+     * Gets the value of finalExamStatus
+     *
+     * @return the value of finalExamStatus
+     */
+    public String getFinalExamStatus() {
+        return this.finalExamStatus;
+    }
+
+    /**
+     * Sets the value of finalExamStatus
+     *
+     * @param argFinalExamStatus Value to assign to this.finalExamStatus
+     */
+    public void setFinalExamStatus(final String argFinalExamStatus) {
+        this.finalExamStatus = argFinalExamStatus;
+    }
+
+    /**
+     * Gets the value of finalExamRationale
+     *
+     * @return the value of finalExamRationale
+     */
+    public String getFinalExamRationale() {
+        return this.finalExamRationale;
+    }
+
+    /**
+     * Sets the value of finalExamRationale
+     *
+     * @param argFinalExamRationale Value to assign to this.finalExamRationale
+     */
+    public void setFinalExamRationale(final String argFinalExamRationale) {
+        this.finalExamRationale = argFinalExamRationale;
+    }
+
+    /**
+     * Gets the value of loDialogWrapper
+     *
+     * @return the value of loDialogWrapper
+     */
+    public LearningObjectiveDialogWrapper getLoDialogWrapper() {
+        return this.loDialogWrapper;
+    }
+
+    /**
+     * Sets the value of loDialogWrapper
+     *
+     * @param argLoDialogWrapper Value to assign to this.loDialogWrapper
+     */
+    public void setLoDialogWrapper(final LearningObjectiveDialogWrapper argLoDialogWrapper) {
+        this.loDialogWrapper = argLoDialogWrapper;
+    }
+
+    /**
+     * Gets the value of showAll
+     *
+     * @return the value of showAll
+     */
+    public Boolean getShowAll() {
+        return this.showAll;
+    }
+
+    /**
+     * Sets the value of showAll
+     *
+     * @param argShowAll Value to assign to this.showAll
+     */
+    public void setShowAll(final Boolean argShowAll) {
+        this.showAll = argShowAll;
+    }
+
+    /**
+     * Gets the value of userId
+     *
+     * @return the value of userId
+     */
+    public String getUserId() {
+        return this.userId;
+    }
+
+    /**
+     * Sets the value of userId
+     *
+     * @param argUserId Value to assign to this.userId
+     */
+    public void setUserId(final String argUserId) {
+        this.userId = argUserId;
+    }
+
+    /**
+     * Gets the value of lastUpdated
+     *
+     * @return the value of lastUpdated
+     */
+    public String getLastUpdated() {
+        return this.lastUpdated;
+    }
+
+    /**
+     * Sets the value of lastUpdated
+     *
+     * @param argLastUpdated Value to assign to this.lastUpdated
+     */
+    public void setLastUpdated(final String argLastUpdated) {
+        this.lastUpdated = argLastUpdated;
+    }
 }
