406097bfc80c230e5df788633a61b04b6f543c2b
sw <sw@kuali.org>
2013-08-29 03:30:05 -0400

    KSENROLL-9064 - Added subcollectionsuffix to collection that exist inside tree nodes.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@54898 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-ui/src/main/java/org/kuali/rice/krms/util/ViewTreeGroup.java
+++ ks-core-ui/src/main/java/org/kuali/rice/krms/util/ViewTreeGroup.java
@@ -15,19 +15,34 @@
  */
 package org.kuali.rice.krms.util;
 
+import org.apache.commons.lang.StringUtils;
 import org.kuali.rice.core.api.util.tree.Node;
 import org.kuali.rice.core.api.util.tree.Tree;
+import org.kuali.rice.krad.uif.UifConstants;
+import org.kuali.rice.krad.uif.container.CollectionGroup;
 import org.kuali.rice.krad.uif.container.Group;
+import org.kuali.rice.krad.uif.container.NodePrototype;
 import org.kuali.rice.krad.uif.container.TreeGroup;
 import org.kuali.rice.krad.uif.element.Message;
+import org.kuali.rice.krad.uif.util.ComponentUtils;
 import org.kuali.rice.krad.uif.util.ObjectPropertyUtils;
 import org.kuali.rice.krad.uif.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Kuali Student Team
  */
 public class ViewTreeGroup extends TreeGroup {
 
+
+    /**
+     * This method is overridden to add a null check.
+     *
+     * @param view
+     * @param model
+     */
     protected void buildTreeGroups(View view, Object model) {
         // get Tree data property
         Tree<Object, String> treeData = ObjectPropertyUtils.getPropertyValue(model, getBindingInfo().getBindingPath());
@@ -41,9 +56,70 @@ public class ViewTreeGroup extends TreeGroup {
 
         String bindingPrefix = getBindingInfo().getBindingPrefixForNested();
         Node<Group, Message> rootNode = buildTreeNode(treeData.getRootElement(),
-                bindingPrefix + ".rootElement", "root");
+                bindingPrefix + ".rootElement", "_root");
         treeGroups.setRootElement(rootNode);
 
         setTreeGroups(treeGroups);
+    }
+
+    /**
+     * This method is overridden to set suffix on subcollections.
+     *
+     * @param nodeData
+     * @param bindingPrefix
+     * @param parentNode
+     * @return
+     */
+    @Override
+    protected Node<Group, Message> buildTreeNode(Node<Object, String> nodeData, String bindingPrefix, String parentNode) {
+
+        if (nodeData == null) {
+            return null;
+        }
+
+        Node<Group, Message> node = new Node<Group, Message>();
+        node.setNodeType(nodeData.getNodeType());
+
+        //Note: view tree does noet use the prototype map.
+        NodePrototype prototype = this.getDefaultNodePrototype();
+
+        Message message = ComponentUtils.copy(prototype.getLabelPrototype(), parentNode);
+        ComponentUtils.pushObjectToContext(message, UifConstants.ContextVariableNames.NODE, nodeData);
+        message.setMessageText(nodeData.getNodeLabel());
+        node.setNodeLabel(message);
+
+        Group nodeGroup = ComponentUtils.copyComponent(prototype.getDataGroupPrototype(), bindingPrefix + ".data",
+                parentNode);
+        ComponentUtils.pushObjectToContext(nodeGroup, UifConstants.ContextVariableNames.NODE, nodeData);
+
+        String nodePath = bindingPrefix + ".data";
+        if (StringUtils.isNotBlank(getBindingInfo().getBindingObjectPath())) {
+            nodePath = getBindingInfo().getBindingObjectPath() + "." + nodePath;
+        }
+        ComponentUtils.pushObjectToContext(nodeGroup, UifConstants.ContextVariableNames.NODE_PATH, nodePath);
+
+        /*Overridden section*/
+        List<CollectionGroup> components = ComponentUtils.getComponentsOfTypeShallow(nodeGroup, CollectionGroup.class);
+        for (CollectionGroup fieldCollectionGroup : components) {
+            fieldCollectionGroup.setSubCollectionSuffix(parentNode);
+        }
+        node.setData(nodeGroup);
+
+        List<Node<Group, Message>> nodeChildren = new ArrayList<Node<Group, Message>>();
+
+        int childIndex = 0;
+        for (Node<Object, String> childDataNode : nodeData.getChildren()) {
+            String nextBindingPrefix = bindingPrefix + ".children[" + childIndex + "]";
+            Node<Group, Message> childNode = buildTreeNode(childDataNode, nextBindingPrefix,
+                    "_node" + childIndex + parentNode); //Removed unnecessary suffix elements.
+
+            nodeChildren.add(childNode);
+
+            // Don't forget about me:
+            ++childIndex;
+        }
+        node.setChildren(nodeChildren);
+
+        return node;
     }
 }
