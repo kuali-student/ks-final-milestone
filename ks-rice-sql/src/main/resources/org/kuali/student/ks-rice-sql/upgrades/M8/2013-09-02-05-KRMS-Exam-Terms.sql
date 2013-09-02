63b85595a72139c8ecd84e5718b97fe7a5f5a8cd
sw <sw@kuali.org>
2013-09-02 10:32:12 -0400

    KSENROLL-9237 - Term Spec configuration data for final exam matrix.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@55088 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-rice-sql/src/main/resources/org/kuali/student/ks-rice-sql/upgrades/M8/2013-09-02-05-KRMS-Exam-Terms.sql
@@ -0,0 +1,9 @@
+INSERT INTO KRMS_TERM_SPEC_T (ACTV,DESC_TXT,NM,NMSPC_CD,TERM_SPEC_ID,TYP,VER_NBR)
+  VALUES ('Y','Matching Timeslot','MatchingTimeSlot','KS-SYS','KS-TERM-SPEC-10019','java.lang.Boolean',1)
+/
+INSERT INTO KRMS_TERM_SPEC_T (ACTV,DESC_TXT,NM,NMSPC_CD,TERM_SPEC_ID,TYP,VER_NBR)
+  VALUES ('Y','Matching Course Code','MatchingCourse','KS-SYS','KS-TERM-SPEC-10020','java.lang.Boolean',1)
+/
+INSERT INTO KRMS_TERM_SPEC_T (ACTV,DESC_TXT,NM,NMSPC_CD,TERM_SPEC_ID,TYP,VER_NBR)
+  VALUES ('Y','Matching Course Set','MatchingCourseSet','KS-SYS','KS-TERM-SPEC-10021','java.lang.Boolean',1)
+/
