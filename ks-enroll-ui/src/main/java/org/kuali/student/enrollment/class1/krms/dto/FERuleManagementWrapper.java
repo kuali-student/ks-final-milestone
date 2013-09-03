59ca24900b283a84828f8de90280b554a7fe06da
peggy <peggy@kuali.org>
2013-09-03 09:36:43 -0400

    KSENROLL-9247: Created the action table UI for Final Exam to verify configuration data
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@55109 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/krms/dto/FERuleManagementWrapper.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/krms/dto/FERuleManagementWrapper.java
@@ -16,10 +16,123 @@
 package org.kuali.student.enrollment.class1.krms.dto;
 
 import org.kuali.rice.krms.dto.RuleManagementWrapper;
+import org.kuali.student.r2.core.room.dto.BuildingInfo;
+import org.kuali.student.r2.core.room.dto.RoomInfo;
 
 /**
  * @author Kuali Student Team
  */
 public class FERuleManagementWrapper extends RuleManagementWrapper {
+    private BuildingInfo building;
+    private RoomInfo room;
 
+    //Properties
+    private String days;
+    private String startTime;
+    private String startTimeAMPM;
+    private String endTime;
+    private String endTimeAMPM;
+
+    private boolean tba;
+
+    private String buildingCode;
+    private String buildingId;
+    private String roomCode;
+    private int roomCapacity;
+
+    public BuildingInfo getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(BuildingInfo building) {
+        this.building = building;
+    }
+
+    public RoomInfo getRoom() {
+        return room;
+    }
+
+    public void setRoom(RoomInfo room) {
+        this.room = room;
+    }
+
+    public String getDays() {
+        return days;
+    }
+
+    public void setDays(String days) {
+        this.days = days;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getStartTimeAMPM() {
+        return startTimeAMPM;
+    }
+
+    public void setStartTimeAMPM(String startTimeAMPM) {
+        this.startTimeAMPM = startTimeAMPM;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getEndTimeAMPM() {
+        return endTimeAMPM;
+    }
+
+    public void setEndTimeAMPM(String endTimeAMPM) {
+        this.endTimeAMPM = endTimeAMPM;
+    }
+
+    public boolean isTba() {
+        return tba;
+    }
+
+    public void setTba(boolean tba) {
+        this.tba = tba;
+    }
+
+    public String getBuildingCode() {
+        return buildingCode;
+    }
+
+    public void setBuildingCode(String buildingCode) {
+        this.buildingCode = buildingCode;
+    }
+
+    public String getBuildingId() {
+        return buildingId;
+    }
+
+    public void setBuildingId(String buildingId) {
+        this.buildingId = buildingId;
+    }
+
+    public String getRoomCode() {
+        return roomCode;
+    }
+
+    public void setRoomCode(String roomCode) {
+        this.roomCode = roomCode;
+    }
+
+    public int getRoomCapacity() {
+        return roomCapacity;
+    }
+
+    public void setRoomCapacity(int roomCapacity) {
+        this.roomCapacity = roomCapacity;
+    }
 }
