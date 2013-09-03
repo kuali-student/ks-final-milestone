182cd41306950727f316169294e0174ba4cb8641
sw <sw@kuali.org>
2013-09-03 08:34:23 -0400

    KSENROLL-9236 - Actions reference data.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@55106 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-rice-sql/src/main/resources/org/kuali/student/ks-rice-sql/upgrades/M8/2013-09-02-01-KRMS-Exam-types.sql
+++ ks-rice-sql/src/main/resources/org/kuali/student/ks-rice-sql/upgrades/M8/2013-09-02-01-KRMS-Exam-types.sql
@@ -16,6 +16,8 @@ Insert into KRMS_TYP_T (ACTV,TYP_ID,NM,NMSPC_CD,SRVC_NM,VER_NBR) values ('Y',CON
 /
 Insert into KRMS_TYP_T (ACTV,TYP_ID,NM,NMSPC_CD,SRVC_NM,VER_NBR) values ('Y',CONCAT('KS-KRMS-TYP-', KS_RICE_ID_S.NEXTVAL),'kuali.krms.proposition.type.final.exam.courseset','KS-SYS','simplePropositionTypeService',0)
 /
+Insert into KRMS_TYP_T (ACTV,TYP_ID,NM,NMSPC_CD,SRVC_NM,VER_NBR) values ('Y',CONCAT('KS-KRMS-TYP-', KS_RICE_ID_S.NEXTVAL),'kuali.krms.action.type.requested.delivery.logistic','KS-SYS','actionTypeService',0)
+/
 
 --Type relations
 Insert into KRMS_TYP_RELN_T (TYP_RELN_ID,FROM_TYP_ID,TO_TYP_ID,RELN_TYP,SEQ_NO,VER_NBR,ACTV) values (CONCAT('KS-KRMS-TYP-RELN-', KS_RICE_ID_S.NEXTVAL),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.agenda.type.final.exam'),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.agenda.type.final.exam.standard'),'A',1,0,'Y')
