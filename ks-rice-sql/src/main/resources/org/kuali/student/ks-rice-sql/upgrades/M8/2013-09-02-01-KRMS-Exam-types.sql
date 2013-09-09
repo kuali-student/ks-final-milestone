3d1b60c17523d6972d53ddfe9540df6d2db021e7
christoff.botha <christoff.botha@kuali.org>
2013-09-09 05:34:11 -0400

    KSENROLL-9235 Exam matrix ref data Conflict resolution
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@55457 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-rice-sql/src/main/resources/org/kuali/student/ks-rice-sql/upgrades/M8/2013-09-02-01-KRMS-Exam-types.sql
+++ ks-rice-sql/src/main/resources/org/kuali/student/ks-rice-sql/upgrades/M8/2013-09-02-01-KRMS-Exam-types.sql
@@ -32,10 +32,14 @@ Insert into KRMS_TYP_RELN_T (TYP_RELN_ID,FROM_TYP_ID,TO_TYP_ID,RELN_TYP,SEQ_NO,V
 /
 Insert into KRMS_TYP_RELN_T (TYP_RELN_ID,FROM_TYP_ID,TO_TYP_ID,RELN_TYP,SEQ_NO,VER_NBR,ACTV) values (CONCAT('KS-KRMS-TYP-RELN-', KS_RICE_ID_S.NEXTVAL),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.final.exam.standard'),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.final.exam.timeslot'),'A',1,0,'Y')
 /
+Insert into KRMS_TYP_RELN_T (TYP_RELN_ID,FROM_TYP_ID,TO_TYP_ID,RELN_TYP,SEQ_NO,VER_NBR,ACTV) values (CONCAT('KS-KRMS-TYP-RELN-', KS_RICE_ID_S.NEXTVAL),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.final.exam.standard'),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.freeform.text'),'A',2,0,'Y')
+/
 
 Insert into KRMS_TYP_RELN_T (TYP_RELN_ID,FROM_TYP_ID,TO_TYP_ID,RELN_TYP,SEQ_NO,VER_NBR,ACTV) values (CONCAT('KS-KRMS-TYP-RELN-', KS_RICE_ID_S.NEXTVAL),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.agenda.type.final.exam.common'),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.final.exam.common'),'A',1,0,'Y')
 /
 Insert into KRMS_TYP_RELN_T (TYP_RELN_ID,FROM_TYP_ID,TO_TYP_ID,RELN_TYP,SEQ_NO,VER_NBR,ACTV) values (CONCAT('KS-KRMS-TYP-RELN-', KS_RICE_ID_S.NEXTVAL),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.final.exam.common'),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.final.exam.course'),'A',1,0,'Y')
 /
-Insert into KRMS_TYP_RELN_T (TYP_RELN_ID,FROM_TYP_ID,TO_TYP_ID,RELN_TYP,SEQ_NO,VER_NBR,ACTV) values (CONCAT('KS-KRMS-TYP-RELN-', KS_RICE_ID_S.NEXTVAL),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.final.exam.common'),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.final.exam.courseset'),'A',1,0,'Y')
+Insert into KRMS_TYP_RELN_T (TYP_RELN_ID,FROM_TYP_ID,TO_TYP_ID,RELN_TYP,SEQ_NO,VER_NBR,ACTV) values (CONCAT('KS-KRMS-TYP-RELN-', KS_RICE_ID_S.NEXTVAL),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.final.exam.common'),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.final.exam.courseset'),'A',2,0,'Y')
+/
+Insert into KRMS_TYP_RELN_T (TYP_RELN_ID,FROM_TYP_ID,TO_TYP_ID,RELN_TYP,SEQ_NO,VER_NBR,ACTV) values (CONCAT('KS-KRMS-TYP-RELN-', KS_RICE_ID_S.NEXTVAL),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.rule.type.final.exam.common'),(SELECT TYP_ID FROM KRMS_TYP_T WHERE NM = 'kuali.krms.proposition.type.freeform.text'),'A',3,0,'Y')
 /
