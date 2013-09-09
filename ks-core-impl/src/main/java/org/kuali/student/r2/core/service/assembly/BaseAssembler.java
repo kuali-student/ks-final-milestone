3d9b38b6bbaaf77fb788f40c6f7544a3982c6bd0
yindavid <yindavid@kuali.org>
2013-09-09 15:15:47 -0400

    KSCM-1101: modified toGenericAttributes() method to find the old attributes and remove them using attribute key instead of id, which will resolve the duplicated CLU attribute key issue.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@55508 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-impl/src/main/java/org/kuali/student/r2/core/service/assembly/BaseAssembler.java
+++ ks-core-impl/src/main/java/org/kuali/student/r2/core/service/assembly/BaseAssembler.java
@@ -65,7 +65,7 @@ public class BaseAssembler {
 
         // Find all the old attributes(if the owner is not null)
         for (A attribute : owner.getAttributes()) {
-            currentAttributes.put(attribute.getId(), attribute);
+            currentAttributes.put(attribute.getName(), attribute);
         }
 
         //Clear out the attributes
@@ -78,8 +78,8 @@ public class BaseAssembler {
         //Update anything that exists, or create a new attribute if it doesn't
         for (AttributeInfo attr : attributeList) {
             A attribute;
-            if (currentAttributes.containsKey(attr.getId())) {
-                attribute = currentAttributes.remove(attr.getId());
+            if (currentAttributes.containsKey(attr.getKey())) {
+                attribute = currentAttributes.remove(attr.getKey());
             } else {
                 try {
                     attribute = attributeClass.newInstance();
