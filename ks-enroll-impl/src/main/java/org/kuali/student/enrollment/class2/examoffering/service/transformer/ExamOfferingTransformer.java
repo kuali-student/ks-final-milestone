3b9488b31a9cc0cce6773ba9611f96c847a8374f
sw <sw@kuali.org>
2013-09-13 04:37:35 -0400

    KSENROLL-9413 - Added scheduling state to Exam Offering Transformer.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@55867 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/examoffering/service/transformer/ExamOfferingTransformer.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/examoffering/service/transformer/ExamOfferingTransformer.java
@@ -13,8 +13,14 @@ import org.kuali.student.r2.common.exceptions.MissingParameterException;
 import org.kuali.student.r2.common.exceptions.OperationFailedException;
 import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
 import org.kuali.student.r2.common.infc.Attribute;
+import org.kuali.student.r2.common.util.constants.ExamOfferingServiceConstants;
 import org.kuali.student.r2.common.util.constants.LuiServiceConstants;
-import org.kuali.student.r2.lum.clu.dto.LuCodeInfo;
+import org.kuali.student.r2.core.scheduling.dto.ScheduleComponentInfo;
+import org.kuali.student.r2.core.scheduling.dto.ScheduleInfo;
+import org.kuali.student.r2.core.scheduling.dto.ScheduleRequestComponentInfo;
+import org.kuali.student.r2.core.scheduling.dto.ScheduleRequestInfo;
+import org.kuali.student.r2.core.scheduling.dto.ScheduleRequestSetInfo;
+import org.kuali.student.r2.core.scheduling.service.SchedulingService;
 
 import javax.xml.namespace.QName;
 import java.util.ArrayList;
@@ -31,26 +37,50 @@ public class ExamOfferingTransformer {
     /**
      * Transform a list of LuiInfos into ExamOfferingInfos. It is the bulk version of lui2ExamOffering transformer
      *
-     * @param examOfferingIds     the list of examOfferingIds which is used to retrieve the list of LuiInfos
-     * @param eos                   the reference of ExamOfferingInfo list whith points to the transformed ExamOfferingInfo list
-     * @param context               information containing the principalId and locale
-     *                              information about the caller of service operation
-     * @throws org.kuali.student.r2.common.exceptions.DoesNotExistException     ActivityOfferingDisplayInfo is not found
-     * @throws org.kuali.student.r2.common.exceptions.InvalidParameterException contextInfo is not valid
-     * @throws org.kuali.student.r2.common.exceptions.MissingParameterException courseOfferingIds, cos, stateService, or context is missing or null
-     * @throws org.kuali.student.r2.common.exceptions.OperationFailedException  unable to complete request
-     * @throws org.kuali.student.r2.common.exceptions.PermissionDeniedException an authorization failure occurred
+     * @param examOfferingIds the list of examOfferingIds which is used to retrieve the list of LuiInfos
+     * @param eos             the reference of ExamOfferingInfo list whith points to the transformed ExamOfferingInfo list
+     * @param context         information containing the principalId and locale
+     *                        information about the caller of service operation
+     * @throws org.kuali.student.r2.common.exceptions.DoesNotExistException
+     *          ActivityOfferingDisplayInfo is not found
+     * @throws org.kuali.student.r2.common.exceptions.InvalidParameterException
+     *          contextInfo is not valid
+     * @throws org.kuali.student.r2.common.exceptions.MissingParameterException
+     *          courseOfferingIds, cos, stateService, or context is missing or null
+     * @throws org.kuali.student.r2.common.exceptions.OperationFailedException
+     *          unable to complete request
+     * @throws org.kuali.student.r2.common.exceptions.PermissionDeniedException
+     *          an authorization failure occurred
      */
-    public void luis2ExamOfferings(List<String> examOfferingIds, List<ExamOfferingInfo> eos, ContextInfo context) throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException {
-        if(examOfferingIds == null || examOfferingIds.isEmpty()){
+    public void luis2ExamOfferings(List<String> examOfferingIds, List<ExamOfferingInfo> eos, SchedulingService schedulingService,
+                                   ContextInfo context) throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException {
+        if (examOfferingIds == null || examOfferingIds.isEmpty()) {
             LOG.warn("invalid courseOfferingIds");
             return;
         }
 
+        //Retrieve the luiInfos
         List<LuiInfo> luiInfos = luiService.getLuisByIds(examOfferingIds, context);
+
+        List<String> scheduleIds = new ArrayList<String>();
+        for (LuiInfo luiInfo : luiInfos) {
+            scheduleIds.addAll(luiInfo.getScheduleIds());
+        }
+
+        //Bulk load a list a ScheduleInfos by a list of scheduleIds. Cache the results set in a map.
+        Map<String, ScheduleInfo> scheduleIdToScheduleMap = new HashMap<String, ScheduleInfo>();
+        if (scheduleIds != null && !scheduleIds.isEmpty()) {
+            List<ScheduleInfo> scheduleInfos = schedulingService.getSchedulesByIds(scheduleIds, context);
+            for (ScheduleInfo scheduleInfo : scheduleInfos) {
+                scheduleIdToScheduleMap.put(scheduleInfo.getId(), scheduleInfo);
+            }
+        }
+
+        Map<String, List<ScheduleRequestInfo>> luiToScheduleRequestsMap = buildLuiToScheduleRequestsMap(examOfferingIds, schedulingService, context);
+
         for (LuiInfo luiInfo : luiInfos) {
             ExamOfferingInfo eo = new ExamOfferingInfo();
-            lui2ExamOffering(luiInfo, eo, context);
+            lui2ExamOffering(luiInfo, eo, scheduleIdToScheduleMap, luiToScheduleRequestsMap, schedulingService, context);
 
             eos.add(eo);
 
@@ -60,10 +90,12 @@ public class ExamOfferingTransformer {
     /**
      * Transform a LuiInfo into an ExamOfferingInfo.
      *
-     * @param lui                   the LuiInfo that is transformed into ExamOfferingInfo
-     * @param eo                    the reference of ExamOfferingInfo that is transformed from LuiInfo
+     * @param lui the LuiInfo that is transformed into ExamOfferingInfo
+     * @param eo  the reference of ExamOfferingInfo that is transformed from LuiInfo
      */
-    public void lui2ExamOffering(LuiInfo lui, ExamOfferingInfo eo, ContextInfo context) {
+    public void lui2ExamOffering(LuiInfo lui, ExamOfferingInfo eo, Map<String, ScheduleInfo> scheduleIdToScheduleMap,
+                                 Map<String, List<ScheduleRequestInfo>> luiToScheduleRequestsMap,
+                                 SchedulingService schedulingService, ContextInfo context) {
 
         eo.setId(lui.getId());
         eo.setTypeKey(lui.getTypeKey());
@@ -72,10 +104,58 @@ public class ExamOfferingTransformer {
         eo.setMeta(lui.getMeta());
 
         //These still need to be mapped
-        //eo.setExamPeriodId();
-        //eo.setExamId();
-        //eo.setScheduleId();
-        //eo.setSchedulingStateKey();
+        eo.setExamPeriodId(lui.getAtpId());
+        eo.setExamId(lui.getCluId());
+        for (String scheduleId : lui.getScheduleIds()) {
+            eo.setScheduleId(scheduleId);
+            break;  //ExamOffering should only have one scheduleId.
+        }
+
+        // if there is an actual schedule tied to the AO, and at least one of the components is not marked TBA, then the AO scheduling state is Scheduled
+        if (eo.getScheduleId() == null) {
+            eo.setSchedulingStateKey(getSchedulingState(eo, scheduleIdToScheduleMap));
+        } else {
+            eo.setSchedulingStateKey(getSchedulingStateByScheduleRequest(eo, luiToScheduleRequestsMap.get(eo.getId())));
+        }
+
+        //Dynamic attributes
+        List<AttributeInfo> attributes = eo.getAttributes();
+        for (Attribute attr : lui.getAttributes()) {
+            attributes.add(new AttributeInfo(attr));
+        }
+        eo.setAttributes(attributes);
+
+        return;
+    }
+
+    /**
+     * Transform a LuiInfo into an ExamOfferingInfo.
+     *
+     * @param lui the LuiInfo that is transformed into ExamOfferingInfo
+     * @param eo  the reference of ExamOfferingInfo that is transformed from LuiInfo
+     */
+    public void lui2ExamOffering(LuiInfo lui, ExamOfferingInfo eo, SchedulingService schedulingService, ContextInfo context) throws MissingParameterException, PermissionDeniedException, InvalidParameterException, OperationFailedException, DoesNotExistException {
+
+        eo.setId(lui.getId());
+        eo.setTypeKey(lui.getTypeKey());
+        eo.setStateKey(lui.getStateKey());
+        eo.setDescr(lui.getDescr());
+        eo.setMeta(lui.getMeta());
+
+        //These still need to be mapped
+        eo.setExamPeriodId(lui.getAtpId());
+        eo.setExamId(lui.getCluId());
+        for (String scheduleId : lui.getScheduleIds()) {
+            eo.setScheduleId(scheduleId);
+            break;  //ExamOffering should only have one scheduleId.
+        }
+
+        // if there is an actual schedule tied to the EO, and at least one of the components is not marked TBA, then the EO scheduling state is Scheduled
+        if (eo.getScheduleId() == null) {
+            eo.setSchedulingStateKey(getSchedulingState(eo, schedulingService, context));
+        } else {
+            eo.setSchedulingStateKey(getSchedulingStateByScheduleRequest(eo, schedulingService, context));
+        }
 
         //Dynamic attributes
         List<AttributeInfo> attributes = eo.getAttributes();
@@ -96,19 +176,20 @@ public class ExamOfferingTransformer {
         lui.setMeta(eo.getMeta());
 
         //These still need to be mapped
-        //eo.getExamPeriodId();
-        //eo.getExamId();
-        //eo.getScheduleId();
-        //eo.getSchedulingStateKey();
+        lui.setAtpId(eo.getExamPeriodId());
+        lui.setCluId(eo.getExamId());
+
+        lui.getScheduleIds().clear();
+        lui.getScheduleIds().add(eo.getScheduleId());
 
         //Dynamic Attributes
         HashMap<String, AttributeInfo> attributesMap = new HashMap<String, AttributeInfo>();
         List<AttributeInfo> attributes = new ArrayList<AttributeInfo>();
         for (AttributeInfo attr : lui.getAttributes()) {
-            attributesMap.put(attr.getKey(), attr) ;
+            attributesMap.put(attr.getKey(), attr);
         }
         for (AttributeInfo attr : eo.getAttributes()) {
-            attributesMap.put(attr.getKey(), attr) ;
+            attributesMap.put(attr.getKey(), attr);
         }
 
         //AttributeInfo courseNumberInternalSuffix = new AttributeInfo();
@@ -124,8 +205,85 @@ public class ExamOfferingTransformer {
 
     }
 
+    private static String getSchedulingState(ExamOfferingInfo eo, SchedulingService schedulingService, ContextInfo context)
+            throws PermissionDeniedException, MissingParameterException, InvalidParameterException, OperationFailedException, DoesNotExistException {
+        ScheduleInfo schedule = schedulingService.getSchedule(eo.getScheduleId(), context);
+
+        for (ScheduleComponentInfo componentInfo : schedule.getScheduleComponents()) {
+            if (!componentInfo.getIsTBA()) {
+                return null; //Should be SCHEDULED_STATE_KEY
+            }
+        }
+
+        return ExamOfferingServiceConstants.EXAM_OFFERING_SCHEDULING_EXEMPT_STATE_KEY;
+    }
+
+    private static String getSchedulingState(ExamOfferingInfo eo, Map<String, ScheduleInfo> scheduleIdToScheduleMap) {
+        ScheduleInfo schedule = scheduleIdToScheduleMap.get(eo.getScheduleId());
+
+        for (ScheduleComponentInfo componentInfo : schedule.getScheduleComponents()) {
+            if (!componentInfo.getIsTBA()) {
+                return null; //Should be SCHEDULED_STATE_KEY
+            }
+        }
+
+        return ExamOfferingServiceConstants.EXAM_OFFERING_SCHEDULING_EXEMPT_STATE_KEY;
+    }
+
+    private static String getSchedulingStateByScheduleRequest(ExamOfferingInfo eo, SchedulingService schedulingService, ContextInfo context)
+            throws MissingParameterException, InvalidParameterException, OperationFailedException, PermissionDeniedException {
+        // get the schedule request for this AO
+        List<ScheduleRequestInfo> requests = schedulingService.getScheduleRequestsByRefObject(ExamOfferingServiceConstants.REF_OBJECT_URI_EXAM_OFFERING, eo.getId(), context);
+
+        return getSchedulingStateByScheduleRequest(eo, requests);
+    }
+
+    private static String getSchedulingStateByScheduleRequest(ExamOfferingInfo eo, List<ScheduleRequestInfo> requests) {
+        if (requests == null || requests.isEmpty()) {
+            // if there are no requests, the AO scheduling state is Unscheduled
+            return ExamOfferingServiceConstants.EXAM_OFFERING_SCHEDULING_UNSCHEDULED_STATE_KEY;
+        }
+
+        for (ScheduleRequestInfo request : requests) {
+            // if all the schedule request components are set as TBA, the AO scheduling state is Exempt
+            // otherwise, it's Unscheduled
+            for (ScheduleRequestComponentInfo reqComp : request.getScheduleRequestComponents()) {
+                if (!reqComp.getIsTBA()) {
+                    return ExamOfferingServiceConstants.EXAM_OFFERING_SCHEDULING_UNSCHEDULED_STATE_KEY;
+                }
+            }
+        }
+
+        return ExamOfferingServiceConstants.EXAM_OFFERING_SCHEDULING_EXEMPT_STATE_KEY;
+    }
+
+    /*Bulk load a list a ScheduleRequestInfo objects and return the results set in a Map of ActivityOffering ids to a list of ScheduleRequestInfo objects.*/
+    private static Map<String, List<ScheduleRequestInfo>> buildLuiToScheduleRequestsMap(List<String> luiIds, SchedulingService schedulingService, ContextInfo context)
+            throws MissingParameterException, InvalidParameterException, OperationFailedException, PermissionDeniedException, DoesNotExistException {
+        Map<String, List<ScheduleRequestInfo>> luiToScheduleRequestsMap = new HashMap<String, List<ScheduleRequestInfo>>();
+
+        if (luiIds != null && !luiIds.isEmpty()) {
+            for (String luiId : luiIds) {
+                List<ScheduleRequestSetInfo> scheduleRequestSets = schedulingService.getScheduleRequestSetsByRefObject(ExamOfferingServiceConstants.REF_OBJECT_URI_EXAM_OFFERING, luiId, context);
+                for (ScheduleRequestSetInfo srs : scheduleRequestSets) {
+                    List<ScheduleRequestInfo> scheduleRequestInfos = schedulingService.getScheduleRequestsByScheduleRequestSet(srs.getId(), context);
+                    if (scheduleRequestInfos != null && !scheduleRequestInfos.isEmpty()) {
+                        List<ScheduleRequestInfo> scheduleRequestInfoList = luiToScheduleRequestsMap.get(luiId);
+                        if (scheduleRequestInfoList == null) {
+                            scheduleRequestInfoList = new ArrayList<ScheduleRequestInfo>();
+                            luiToScheduleRequestsMap.put(luiId, scheduleRequestInfoList);
+                        }
+                        scheduleRequestInfoList.addAll(scheduleRequestInfos);
+                    }
+                }
+            }
+        }
+
+        return luiToScheduleRequestsMap;
+    }
+
     public LuiService getLuiService() {
-        if(luiService == null){
+        if (luiService == null) {
             luiService = GlobalResourceLoader.getService(new QName(LuiServiceConstants.NAMESPACE, LuiServiceConstants.SERVICE_NAME_LOCAL_PART));
         }
         return luiService;
