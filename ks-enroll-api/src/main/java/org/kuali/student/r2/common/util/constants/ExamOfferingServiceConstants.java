c68b5079377578535c409af35a46ac3f2bb93744
sw <sw@kuali.org>
2013-09-13 04:37:35 -0400

    KSENROLL-9413 - Added scheduling state to Exam Offering Transformer.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/2.0.0-Mx@55867 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/ExamOfferingServiceConstants.java
+++ ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/ExamOfferingServiceConstants.java
@@ -16,6 +16,8 @@
  */
 package org.kuali.student.r2.common.util.constants;
 
+import org.kuali.student.enrollment.examoffering.dto.ExamOfferingInfo;
+import org.kuali.student.enrollment.examoffering.service.ExamOfferingService;
 import org.kuali.student.r2.common.constants.CommonServiceConstants;
 
 /**
@@ -24,8 +26,10 @@ import org.kuali.student.r2.common.constants.CommonServiceConstants;
  * @author Mezba Mahtab (mezba.mahtab@utoronto.ca)
  */
 public class ExamOfferingServiceConstants {
+
     public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "examOffering";
-    public static final String SERVICE_NAME_LOCAL_PART = "ExamOfferingService";
+    public static final String SERVICE_NAME_LOCAL_PART = ExamOfferingService.class.getSimpleName();
+    public static final String REF_OBJECT_URI_EXAM_OFFERING = NAMESPACE + "/" + ExamOfferingInfo.class.getSimpleName();
 
     /////////////////
     // TYPES
