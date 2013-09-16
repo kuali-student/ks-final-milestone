0c9b51d92c1a1cb000a750fd79f007c548b3ab27
yindavid <yindavid@kuali.org>
2013-09-16 16:04:04 -0400

    KSENROLL-9588: configured approved types for exam offering
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@56014 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M8/2013-09-16-KSEN_TYPE_ExamOffering.sql
@@ -0,0 +1,7 @@
+-- KSENROLL-9588
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
+  VALUES ('SYSTEMLOADER',TO_DATE( '20130916000000', 'YYYYMMDDHH24MISS' ),'Exam Offering','Exam Offering','Exam Offering','http://student.kuali.org/wsdl/lui/LuiInfo','http://student.kuali.org/wsdl/lui/LuiService','kuali.lui.type.exam.offering',0)
+/
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
+  VALUES ('SYSTEMLOADER',TO_DATE( '20130916000000', 'YYYYMMDDHH24MISS' ),'Final Exam Offering','Final Exam Offering','Final Exam Offering','http://student.kuali.org/wsdl/lui/LuiInfo','http://student.kuali.org/wsdl/lui/LuiService','kuali.lui.type.exam.offering.final',0)
+/
