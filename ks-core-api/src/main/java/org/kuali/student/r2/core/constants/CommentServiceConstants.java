e2de0fd04a44edd0d035dfc4aa3128614f1856f3
matthew.edgren <matthew.edgren@kuali.org>
2013-09-17 12:47:34 -0400

    KSENROLL-9424 Added all constants that are currently being used.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/trunk@56054 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-api/src/main/java/org/kuali/student/r2/core/constants/CommentServiceConstants.java
+++ ks-core-api/src/main/java/org/kuali/student/r2/core/constants/CommentServiceConstants.java
@@ -16,4 +16,17 @@ public class CommentServiceConstants {
      */
     public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "comment";
     public static final String REF_OBJECT_URI_COMMENT = NAMESPACE + "/" + CommentInfo.class.getSimpleName();
+
+    //comment types
+    public static final String GENERAL_REMARKS_COMMENT_TYPE = "kuali.comment.type.generalRemarks";
+
+    public static final String ACKNOWLEDGE_WORKFLOW_DECISION_COMMENT_TYPE = "kuali.comment.type.workflowDecisionRationale.acknowledge";
+    public static final String APPROVE_WORKFLOW_DECISION_COMMENT_TYPE = "kuali.comment.type.workflowDecisionRationale.approve";
+    public static final String BLANKET_APPROVE_WORKFLOW_DECISION_COMMENT_TYPE = "kuali.comment.type.workflowDecisionRationale.blanketApprove";
+    public static final String CANCEL_WORKFLOW_DECISION_COMMENT_TYPE = "kuali.comment.type.workflowDecisionRationale.cancelWorkflow";
+    public static final String FYI_WORKFLOW_DECISION_COMMENT_TYPE = "kuali.comment.type.workflowDecisionRationale.fyi";
+    public static final String REJECT_WORKFLOW_DECISION_COMMENT_TYPE = "kuali.comment.type.workflowDecisionRationale.reject";
+    public static final String RETURN_WORKFLOW_DECISION_COMMENT_TYPE = "kuali.comment.type.workflowDecisionRationale.return";
+    public static final String WITHDRAW_WORKFLOW_DECISION_COMMENT_TYPE = "kuali.comment.type.workflowDecisionRationale.withdraw";
+
 }
