91c640ccbda6497d4e2f60411987f1a8ecd74b81
gtaylor <gtaylor@kuali.org>
2013-09-23 10:14:47 -0400

    KSENROLL-9656: updated method names based on Michael O. input
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@56352 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-util/src/main/java/org/kuali/student/common/util/CommonUtil.java
+++ ks-common/ks-common-util/src/main/java/org/kuali/student/common/util/CommonUtil.java
@@ -15,7 +15,23 @@ import java.util.List;
  */
 public class CommonUtil {
 
-    public static <T> T getZeroElement(List<T> list) throws OperationFailedException{
+    /**
+     * Cardinality of one
+     *
+     * This is a generic method so you will not need to worry about casting when using this method
+     *
+     * Using get(0) in a non-test class will result in a critical sonar issue being reported which will result in a JIRA issue.
+     * Correcting get(0) depends on the cardinality constraints on the list.
+     * If a list constraint is changed, get(0) might not be the right answer anymore and we would prefer to expose that problem
+     * at the source rather than requiring implementers to track down the cardinality constraint enforcement in java.
+     * This is handled via OperationFailedException being thrown in the examples belowCardinality of one and only one element:
+     *
+     * @param list   Typed list passed into method
+     * @param <T>    Type of element to return
+     * @return       the get(0) element in the list.
+     * @throws OperationFailedException
+     */
+    public static <T> T getRequiredZeroElement(List<T> list) throws OperationFailedException{
         T t;
         if (list.size() == 1) {
             t = list.get(0);
@@ -26,4 +42,37 @@ public class CommonUtil {
         return t;
     }
 
+    /**
+     * Cardinality of zero or one. Will return the value or null depending on what's in the list.
+     *
+     * List has 1 element, return element.
+     * List has 0 elements, return null.
+     * else throw exception.
+     *
+     * This is a generic method so you will not need to worry about casting when using this method
+     *
+     * Using get(0) in a non-test class will result in a critical sonar issue being reported which will result in a JIRA issue.
+     * Correcting get(0) depends on the cardinality constraints on the list.
+     * If a list constraint is changed, get(0) might not be the right answer anymore and we would prefer to expose that problem
+     * at the source rather than requiring implementers to track down the cardinality constraint enforcement in java.
+     * This is handled via OperationFailedException being thrown in the examples belowCardinality of one and only one element:
+     *
+     *
+     * @param list   Typed list passed into method
+     * @param <T>    Type of element to return
+     * @return       the get(0) element in the list. If there are no elements, return null.
+     * @throws OperationFailedException
+     */
+    public static <T> T getOptionalZeroElement(List<T> list) throws OperationFailedException{
+        T t;
+        if (list.size() == 1) {
+            t = list.get(0);
+        } else if (list.size() == 0) {
+            t = null;
+        } else {
+            throw new OperationFailedException("list size exceeds limit of 1");
+        }
+
+        return t;
+    }
 }
