5e74b1d734b2bbb9faca98ce38bfed303052306e
chongzhu <chongzhu@kuali.org>
2013-09-24 14:25:52 -0400

    KSENROLL-9136: Added event to the js to handle the click event.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@56453 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-web/src/main/webapp/themes/ksboot/scripts/scheduleOfClasses.js
+++ ks-common/ks-common-web/src/main/webapp/themes/ksboot/scripts/scheduleOfClasses.js
@@ -65,39 +65,38 @@ function showOrHideAOClusters(expandAction, headerId) {
 /**
  * This method sets the expandAction expand/collapse link
  *
+ * @param event AO cluster disclosure event
  * @param expandAction expand/collapse link id of a particular CO section
  * @param headerId AO cluster header Id
- * @param lineNum AO cluster line number
  */
-function clusterDisclosureClicked(expandAction, headerId) {
+function clusterDisclosureClicked(event, expandAction, headerId) {
 
     var expandText = jQuery("#" + expandAction).text().trim();
-
-//    if (expandText == 'Collapse All'){
-//        jQuery("#" + expandAction).text('Expand All');
-//    }
+    var clickedDisclosureId = jQuery(event).attr('currentTarget').id;
 
     var aSelector = headerId  + "_line";
     var length = jQuery("[id^=" + aSelector + "]").find("a[data-role='" + kradVariables.DATA_ROLES.DISCLOSURE_LINK + "']").length;
     var allExpanded = 0;
 
     for(var i = 0; i<length; i++) {
+        var disclosureId = jQuery("[id^=" + aSelector + "]").find("a[data-role='" + kradVariables.DATA_ROLES.DISCLOSURE_LINK + "']")[i].id;
+
         var contentId = jQuery(jQuery("[id^=" + aSelector + "]").find("a[data-role='" + kradVariables.DATA_ROLES.DISCLOSURE_LINK + "']")[i]).attr("data-linkfor");
         if (jQuery("#" + contentId).attr(kradVariables.ATTRIBUTES.DATA_OPEN) == "true") {
             allExpanded += 1;
+            if(clickedDisclosureId == disclosureId)  {
+                allExpanded -= 1;
+            }
+        } else {
+            if(clickedDisclosureId == disclosureId)  {
+                allExpanded += 1;
+            }
         }
     }
 
-    if(length == 1) {
-      if (expandText == 'Collapse All'){
-        jQuery("#" + expandAction).text('Expand All');
-      } else {
-          jQuery("#" + expandAction).text('Collapse All');
-      }
+    if(length == allExpanded) {
+        jQuery("#" + expandAction).text('Collapse All');
     } else {
         jQuery("#" + expandAction).text('Expand All');
-        if(length > 1 && allExpanded == length - 1) {
-            jQuery("#" + expandAction).text('Collapse All');
-        }
     }
 }
