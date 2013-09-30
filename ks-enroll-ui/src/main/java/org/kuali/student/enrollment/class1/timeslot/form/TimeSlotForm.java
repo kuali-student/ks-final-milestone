1cf327bf7d7451140f6fefe32bd790407336e4d9
chongzhu <chongzhu@kuali.org>
2013-09-30 18:37:11 -0400

    KSENROLL-9701: Added some UI formats and display data for (CO 18.1) Build Interface for Adding Time Slots.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@56878 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/timeslot/form/TimeSlotForm.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/timeslot/form/TimeSlotForm.java
@@ -14,6 +14,7 @@ public class TimeSlotForm extends KSUifForm {
     //  Term type multi-select drop-down selections.
     private List<String> termTypeSelections;
     private boolean timeSlotsLoaded = false;
+    private boolean enableAddButton = true;
 
     //  Storage for Time Slot search results.
     private List<TimeSlotWrapper> timeSlotResults;
@@ -45,5 +46,13 @@ public class TimeSlotForm extends KSUifForm {
 
     public void setTimeSlotsLoaded(boolean timeSlotsLoaded) {
         this.timeSlotsLoaded = timeSlotsLoaded;
+    }
+
+    public boolean isEnableAddButton() {
+        return enableAddButton;
+    }
+
+    public void setEnableAddButton(boolean enableAddButton) {
+        this.enableAddButton = enableAddButton;
     }
 }
