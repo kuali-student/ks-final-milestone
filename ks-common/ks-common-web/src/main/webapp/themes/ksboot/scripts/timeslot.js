6f91adf0b641af412e0a0c4b2617d5a0001825f1
vpremcha <vpremcha@kuali.org>
2013-10-01 20:02:00 -0400

    KSENROLL-9701 - add/edit ts logic
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@56983 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-common/ks-common-web/src/main/webapp/themes/ksboot/scripts/timeslot.js
@@ -0,0 +1,39 @@
+/**
+ *
+ */
+function reInitializePopupModel(event, addOrEditAction, editLineIndex)
+{
+    jQuery('#addOrEditDays_control').val('');
+    jQuery('#addOrEditTermKey_control').val('');
+    jQuery('#addOrEditStartTime_control').val('');
+    jQuery('#addOrEditStartTimeAmPm_control').val('');
+    jQuery('#addOrEditEndTime_control').val('');
+    jQuery('#addOrEditEndTimeAmPm_control').val('');
+
+    if (addOrEditAction == 'EDIT'){
+        jQuery('#addOrEdit_action').text('Save');
+        var termTypeId = 'timeSlotResults[' + editLineIndex + '].typeKey';
+        var termType = jQuery(name=termTypeId).text().trim();
+        var days = jQuery('#days_line' + editLineIndex + '_control').text();
+        var startTimeWithAmPm = jQuery('#startTime_line' + editLineIndex + '_control').text().trim();
+        var split = startTimeWithAmPm.split(" ");
+        var startTime = split[0];
+        var startTimeAmPm = split[1].trim();
+        var endTimeWithAmPm = jQuery('#startTime_line' + editLineIndex + '_control').text().trim();
+        var split = endTimeWithAmPm.split(" ");
+        var endTime = split[0];
+        var endTimeAmPm = split[1].trim();
+
+        var spaceStrippedDays = days.trim().replace(" ","");
+        jQuery('#addOrEditDays_control').val(spaceStrippedDays);
+
+        jQuery('#addOrEditTermKey_control').val(termType);
+        jQuery('#addOrEditStartTime_control').val(startTime);
+        jQuery('#addOrEditStartTimeAmPm_control').val(startTimeAmPm);
+        jQuery('#addOrEditEndTime_control').val(endTime);
+        jQuery('#addOrEditEndTimeAmPm_control').val(endTimeAmPm);
+
+    } else {
+        jQuery('#addOrEdit_action').text('Add Slot');
+    }
+}
