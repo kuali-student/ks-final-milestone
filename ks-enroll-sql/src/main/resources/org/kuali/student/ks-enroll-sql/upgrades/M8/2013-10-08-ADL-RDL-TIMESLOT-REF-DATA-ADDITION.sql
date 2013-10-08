0b02e6cef02cef18f5784477704f6984b54a0e90
komandur <komandur@kuali.org>
2013-10-08 14:51:01 -0400

    KSENROLL-9763 Rollback of work-in-progress files committed by accident
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@57533 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M8/2013-10-08-ADL-RDL-TIMESLOT-REF-DATA-ADDITION.sql
+++
@@ -1,5 +0,0 @@
--- KSENROLL-9763 Emergency ref-data for UX/AFT Timeslot work LUI id:5c8a727e-ba70-4cb5-9358-12eef1abd9b3  ENGL393 section 0801
-update ksen_sched_cmp_tmslot set tm_slot_id = 'E82B4CC3CD2C24D1E040440A9B9A1BBB' where sched_cmp_id = 'f8c95023-570a-4a37-be14-5cfb0143ac69'
-/
-update ksen_sched_rqst_cmp_tmslot set  tm_slot_id = 'E82B4CC3CD2C24D1E040440A9B9A1BBB' where cmp_id= 'adbe813d-9c33-4b35-bb53-088005a0b216'
-/
