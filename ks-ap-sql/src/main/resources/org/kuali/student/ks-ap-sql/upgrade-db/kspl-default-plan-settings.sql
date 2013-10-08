25a5264647727ea67177ca8c59e99e2096733b82
johglov <johglov@kuali.org>
2013-10-08 16:47:07 -0400

    KSAP-74 - Created simple learning plan for admin user.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@57545 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-ap-sql/src/main/resources/org/kuali/student/ks-ap-sql/upgrade-db/kspl-default-plan-settings.sql
@@ -0,0 +1,27 @@
+INSERT INTO KSPL_LRNG_PLAN_TYPE (NAME,OBJ_ID,TYPE_DESC,TYPE_KEY,VER_NBR)
+  VALUES ('Learning Plan','obj-plan-type-1','Learning plan description','kuali.academicplan.type.plan',0)
+/
+INSERT INTO KSPL_LRNG_PLAN_TYPE (NAME,OBJ_ID,TYPE_DESC,TYPE_KEY,VER_NBR)
+  VALUES ('Learning Plan Template','obj-plan-type-2','Learning plan template description','kuali.academicplan.type.plan.template',0)
+/
+INSERT INTO KSPL_LRNG_PLAN_ITEM_TYPE (NAME,OBJ_ID,TYPE_DESC,TYPE_KEY,VER_NBR)
+  VALUES ('Backup Item','obj-plan-item-backup','Backup item description','kuali.academicplan.item.backup',0)
+/
+INSERT INTO KSPL_LRNG_PLAN_ITEM_TYPE (NAME,OBJ_ID,TYPE_DESC,TYPE_KEY,VER_NBR)
+  VALUES ('Planned Item','obj-plan-item-planned','Planned item description','kuali.academicplan.item.planned',0)
+/
+INSERT INTO KSPL_LRNG_PLAN_ITEM_TYPE (NAME,OBJ_ID,TYPE_DESC,TYPE_KEY,VER_NBR)
+  VALUES ('What-if Item','obj-plan-item-whatif','What-if description','kuali.academicplan.item.whatif',0)
+/
+INSERT INTO KSPL_LRNG_PLAN_ITEM_TYPE (NAME,OBJ_ID,TYPE_DESC,TYPE_KEY,VER_NBR)
+  VALUES ('Wishlist Item','obj-plan-item-wishlist','Wishlist item description','kuali.academicplan.item.wishlist',0)
+/
+INSERT INTO KSPL_LRNG_PLAN_ITEM_TYPE (NAME,OBJ_ID,TYPE_DESC,TYPE_KEY,VER_NBR)
+  VALUES ('Shopping Cart Item','obj-plan-item-cart','A plan item that has been added to the shopping cart.','kuali.academicplan.item.cart',0)
+/
+INSERT INTO KSCO_REFERENCE_TYPE (EFF_DT,NAME,OBJ_ID,TYPE_DESC,TYPE_KEY,VER_NBR)
+  VALUES (CURRENT_TIMESTAMP,'Term Note','E9252EBABFDE4CC9B0E52ABC46C216C2','Term Note','kuali.ap.type.note.term',0)
+/
+INSERT INTO KSCO_COMMENT_TYPE (EFF_DT,NAME,OBJ_ID,TYPE_DESC,TYPE_KEY,VER_NBR)
+  VALUES (CURRENT_TIMESTAMP,'Term Note','E9252EBABFDE4CC9B0E52ABC46C216C2','Term Note','kuali.ap.type.note.term',0)
+/
