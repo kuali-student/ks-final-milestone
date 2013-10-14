67e458f7fe643657fda8e94990c61967517f3ac6
rileymg <rileymg@kuali.org>
2013-10-14 18:34:43 -0400

    KSAP-87 added code to mock registration service to support injection of fake student course registration classes. Also: 1. added/injected mock StudentCourseRecordAssemblerMockAllGradeA that returns a grade of A and 3.0 credits for every class it is invoked for (...until better / more realistic registration & academic record  data is available) 2. added method to DefaultTermHelper to retrieve a termId for a given termName and course begin/end date...AS A WORKAROUND until core services adds termId to StudentCourseRecordInfo
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/branches/KSAP@57961 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/academicrecord/service/assembler/StudentCourseRecordAssemblerMockAllGradeA.java
@@ -0,0 +1,94 @@
+package org.kuali.student.enrollment.class2.academicrecord.service.assembler;
+
+import org.kuali.student.enrollment.academicrecord.dto.StudentCourseRecordInfo;
+import org.kuali.student.enrollment.courseregistration.dto.CourseRegistrationInfo;
+import org.kuali.student.enrollment.grading.dto.GradeRosterEntryInfo;
+import org.kuali.student.enrollment.grading.service.GradingService;
+import org.kuali.student.enrollment.lui.dto.LuiIdentifierInfo;
+import org.kuali.student.enrollment.lui.dto.LuiInfo;
+import org.kuali.student.enrollment.lui.service.LuiService;
+import org.kuali.student.r2.common.assembler.AssemblyException;
+import org.kuali.student.r2.common.assembler.DTOAssembler;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.exceptions.DisabledIdentifierException;
+import org.kuali.student.r2.common.exceptions.DoesNotExistException;
+import org.kuali.student.r2.common.exceptions.InvalidParameterException;
+import org.kuali.student.r2.common.exceptions.MissingParameterException;
+import org.kuali.student.r2.common.exceptions.OperationFailedException;
+import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
+import org.kuali.student.r2.core.atp.dto.AtpInfo;
+import org.kuali.student.r2.core.atp.service.AtpService;
+import org.kuali.student.r2.lum.lrc.dto.ResultValueInfo;
+import org.kuali.student.r2.lum.lrc.service.LRCService;
+import org.kuali.student.r2.lum.util.constants.LrcServiceConstants;
+
+/**
+ * Mock StudentCourseRecordAssembler...to  use until better registration & student record data is setup & useable
+ */
+public class StudentCourseRecordAssemblerMockAllGradeA extends StudentCourseRecordAssembler
+    implements DTOAssembler<StudentCourseRecordInfo, CourseRegistrationInfo> {
+
+	@Override
+	public StudentCourseRecordInfo assemble(CourseRegistrationInfo courseReg, ContextInfo context) throws AssemblyException {
+            StudentCourseRecordInfo courseRecord = new StudentCourseRecordInfo();
+            
+            courseRecord.setCourseRegistrationId(courseReg.getId());
+            courseRecord.setPersonId(courseReg.getStudentId());
+
+            try {
+            
+                LuiInfo lui = this.getLuiService().getLui(courseReg.getCourseOfferingId(), context);
+                LuiIdentifierInfo identifier = lui.getOfficialIdentifier();
+                courseRecord.setCourseTitle(identifier != null ? identifier.getLongName() : null);
+                courseRecord.setCourseCode(identifier != null ? identifier.getCode() : null);
+
+                //TODO:The code or number of the primary activity. how to determine which activity is primary?
+                /*            RegGroupRegistrationInfo regGroup = courseReg.getRegGroupRegistration();
+                              if (regGroup.getActivityRegistrations()!= null && !regGroup.getActivityRegistrations().isEmpty()) {
+                              courseRecord.setActivityCode(regGroup.getActivityRegistrations().get(0).getActivityOffering().getActivityCode());
+                              }
+                */
+
+                if (lui.getAtpId() != null) {
+                    AtpInfo atp = getAtpService().getAtp(lui.getAtpId(), context);
+                    courseRecord.setTermName(atp.getName());
+                    courseRecord.setCourseBeginDate(atp.getStartDate());
+                    courseRecord.setCourseEndDate(atp.getEndDate());
+                }
+
+//                GradeRosterEntryInfo finalRosterEntry = null;
+//                finalRosterEntry = gradingService.getFinalGradeForStudentInCourseOffering(courseReg.getStudentId(), lui.getId(), context);
+//                courseRecord.setAssignedGradeValue(getValue(finalRosterEntry.getAssignedGradeKey(), context));
+//                courseRecord.setAssignedGradeScaleKey(getScaleKey(finalRosterEntry.getAssignedGradeKey(), context));
+//                courseRecord.setAdministrativeGradeValue(getValue(finalRosterEntry.getAdministrativeGradeKey(), context));
+//                courseRecord.setAdministrativeGradeScaleKey(getScaleKey(finalRosterEntry.getAdministrativeGradeKey(), context));
+//                courseRecord.setCalculatedGradeValue(getValue(finalRosterEntry.getCalculatedGradeKey(), context));
+//                courseRecord.setCalculatedGradeScaleKey(getScaleKey(finalRosterEntry.getCalculatedGradeKey(), context));
+                courseRecord.setAssignedGradeValue("A");
+                courseRecord.setAssignedGradeScaleKey(LrcServiceConstants.RESULT_VALUE_KEY_GRADE_LETTER_A);
+                courseRecord.setAdministrativeGradeValue("A");
+                courseRecord.setAdministrativeGradeScaleKey(LrcServiceConstants.RESULT_VALUE_KEY_GRADE_LETTER_A);
+                courseRecord.setCalculatedGradeValue("A");
+                courseRecord.setCalculatedGradeScaleKey(LrcServiceConstants.RESULT_VALUE_KEY_GRADE_LETTER_A);
+                courseRecord.setCreditsAttempted("3.0");
+                courseRecord.setCreditsEarned("3.0");
+                courseRecord.setCreditsForGPA("3.0");
+
+//            } catch (DisabledIdentifierException e) {
+//                throw new AssemblyException("DisabledIdentifierException: " + e.getMessage());
+            } catch (DoesNotExistException e) {
+                throw new AssemblyException("DoesNotExistException: " + e.getMessage());
+            } catch (InvalidParameterException e) {
+                throw new AssemblyException("InvalidParameterException: " + e.getMessage());
+            } catch (MissingParameterException e) {
+                throw new AssemblyException("MissingParameterException: " + e.getMessage());
+            } catch (OperationFailedException e) {
+                throw new AssemblyException("OperationFailedException: " + e.getMessage());
+            } catch (PermissionDeniedException e) {
+                throw new AssemblyException("PermissionDeniedException: "  + e.getMessage());
+            }	
+            
+            return courseRecord;
+	}
+
+}
