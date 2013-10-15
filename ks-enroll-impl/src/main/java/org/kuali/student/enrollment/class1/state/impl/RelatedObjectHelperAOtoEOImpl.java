a3b8e4b2f6869d7f11686a9ae26830521a0087d2
sw <sw@kuali.org>
2013-10-15 02:19:30 -0400

    KSENROLL-9045 - Cancel the exam offering if related activity offering is canceled.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@57979 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class1/state/impl/RelatedObjectHelperAOtoEOImpl.java
@@ -0,0 +1,60 @@
+package org.kuali.student.enrollment.class1.state.impl;
+
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.student.enrollment.courseoffering.dto.ActivityOfferingInfo;
+import org.kuali.student.enrollment.courseoffering.dto.FormatOfferingInfo;
+import org.kuali.student.enrollment.courseoffering.service.CourseOfferingService;
+import org.kuali.student.enrollment.examoffering.dto.ExamOfferingInfo;
+import org.kuali.student.enrollment.examoffering.dto.ExamOfferingRelationInfo;
+import org.kuali.student.enrollment.examoffering.service.ExamOfferingService;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.exceptions.DoesNotExistException;
+import org.kuali.student.r2.common.exceptions.InvalidParameterException;
+import org.kuali.student.r2.common.exceptions.MissingParameterException;
+import org.kuali.student.r2.common.exceptions.OperationFailedException;
+import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
+import org.kuali.student.r2.common.util.constants.CourseOfferingServiceConstants;
+import org.kuali.student.r2.common.util.constants.ExamOfferingServiceConstants;
+import org.kuali.student.r2.core.class1.state.service.RelatedObjectHelper;
+
+import javax.xml.namespace.QName;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Version 2.0
+ * @Author Sri komandur@uw.edu
+ */
+
+public class RelatedObjectHelperAOtoEOImpl implements RelatedObjectHelper {
+
+    private ExamOfferingService examOfferingService;
+
+    public RelatedObjectHelperAOtoEOImpl() {
+    }
+
+    @Override
+    public Map<String, String> getRelatedObjectsIdAndState(String activityOfferingId, ContextInfo contextInfo)
+            throws DoesNotExistException, InvalidParameterException, MissingParameterException, OperationFailedException, PermissionDeniedException {
+
+        Map<String, String> idsAndStatesMap = new HashMap<String, String>();
+
+        List<String> eoRelationIds = this.getExamOfferingService().getExamOfferingRelationIdsByActivityOffering(activityOfferingId, contextInfo);
+        List<ExamOfferingRelationInfo> eoRelations = this.getExamOfferingService().getExamOfferingRelationsByIds(eoRelationIds, contextInfo);
+        for(ExamOfferingRelationInfo eoRelation : eoRelations){
+            ExamOfferingInfo examOffering = this.getExamOfferingService().getExamOffering(eoRelation.getExamOfferingId(), contextInfo);
+            idsAndStatesMap.put(examOffering.getId(), examOffering.getStateKey());
+        }
+
+        return idsAndStatesMap;
+    }
+
+    protected ExamOfferingService getExamOfferingService(){
+        if (examOfferingService == null){
+            examOfferingService = (ExamOfferingService) GlobalResourceLoader.getService(new QName(ExamOfferingServiceConstants.NAMESPACE, ExamOfferingServiceConstants.SERVICE_NAME_LOCAL_PART));
+        }
+        return  examOfferingService;
+    }
+
+}
