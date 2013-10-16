bac38025410bf77ef2bbf573fdff3a7b6b86ccb0
gtaylor <gtaylor@kuali.org>
2013-10-16 14:27:58 -0400

    KSENROLL-7492: Updated the GenericEntityDao + the unit tests so they pass.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@58144 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/test/java/org/kuali/student/r2/common/dao/TestGenericEntityDao.java
+++ ks-enroll-impl/src/test/java/org/kuali/student/r2/common/dao/TestGenericEntityDao.java
@@ -12,15 +12,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:genericentitydao-test-context.xml"})
+@ContextConfiguration(locations = {"classpath:state-test-context.xml"})
 @TransactionConfiguration(transactionManager = "JtaTxManager", defaultRollback = true)
 @Transactional
 public class TestGenericEntityDao {
@@ -30,9 +32,23 @@ public class TestGenericEntityDao {
 
     protected  List<String> primaryKeys = new ArrayList<String>();
 
+    protected  StringBuilder queryStringRefLocal = new  StringBuilder();
+
+    protected class TestStateDao extends GenericEntityDao<StateEntity> {
+        @Override
+        protected Query buildQuery(StringBuilder queryStringRef, String primaryKeyMemberName, Set<String> primaryKeySet) {
+           Query q =  super.buildQuery(queryStringRef, primaryKeyMemberName, primaryKeySet);    //To change body of overridden methods use File | Settings | File Templates.
+            queryStringRefLocal = queryStringRef;
+            return q;
+        }
+    }
+
+    protected    TestStateDao localTest = new TestStateDao();
+
     @Before
     public void setUp() {
 
+        localTest.setEm(stateDao.getEm());
         //Loading data and preparing primaryKeys list.
         for(int i=0 ; i<4000 ; i++) {
 
@@ -46,12 +62,12 @@ public class TestGenericEntityDao {
             stateEntity.setLifecycleKey("kuali.scheduling.timeslot.lifecycle"+i);
             stateEntity.setName("Active"+i);
             stateEntity.setVersionNumber(0L);
-            stateDao.persist(stateEntity);
+            localTest.persist(stateEntity);
 
             primaryKeys.add(stateEntity.getId());
         }
 
-        stateDao.em.flush();
+        localTest.em.flush();
 
     }
 
@@ -60,23 +76,47 @@ public class TestGenericEntityDao {
 
         try {
 
-            List<StateEntity> lstStateEntity1 = stateDao.findByIds("id", primaryKeys);
+            List<StateEntity> lstStateEntity1 = localTest.findByIds("id", primaryKeys);
 
-            assertNotEquals(0,lstStateEntity1.size());
+            int queryCount = getSubstringCount(queryStringRefLocal.toString(), "in");
+
+            assertEquals("We have asked for 4K ids, so the query should have 4", queryCount, 4);
 
 
-            stateDao.setEnableMaxIdFetch(false);
+            localTest.setEnableMaxIdFetch(false);
             List<StateEntity> lstStateEntity2 = null;
             try {
-                lstStateEntity2 = stateDao.findByIds("id", primaryKeys);
+                lstStateEntity2 = localTest.findByIds("id", primaryKeys);
             } catch(Exception e) {
+                // for most DBs this will throw a javax.transaction.xa.XAException.
                 e.printStackTrace();
             }
+
+
             assertNull("Maximum \'IN CLAUSE\' elements check : This test case can be failed if target database could allow more than 1000 elements in IN clause. In such case IGNORE this failure : ",lstStateEntity2);
+
+
 
         } catch(Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    private int getSubstringCount(String fullString, String subString){
+
+        int lastIndex = 0;
+        int count =0;
+
+        while(lastIndex != -1){
+
+            lastIndex = fullString.indexOf(subString,lastIndex);
+
+            if( lastIndex != -1){
+                count ++;
+                lastIndex+=subString.length();
+            }
+        }
+        return count;
     }
 }
