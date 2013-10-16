a8aabd74275bf6acb716bcb4f7fa282b2770d125
miki.harmath <miki.harmath@kuali.org>
2013-10-16 09:31:01 -0400

    KSENROLL-8843 Reference data request for courses that have exams scheduled off-matrix
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@58120 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M8/2013-10-16-KSEN_LUI_ATTR-update-final-exam-use-matrix.sql
@@ -0,0 +1,67 @@
+-- KSENROLL-8843 Reference data request for courses that have exams scheduled off-matrix
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = 'fed36cf2-a60a-4020-9964-ba2aa3c75ab4' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '194f1799-7f12-4908-9cd2-e947c88fbf63' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '382b17df-ac61-4d8a-ae80-4da1d1983887' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '544855b7-13a2-47fe-aa3d-6bc4ae4d3d7f' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '070cfaf1-8e54-4f3d-a209-b3101688672b' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '33148f61-b2da-44a7-86ba-ed06c009c555' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = 'c03ba4c5-83d1-4333-8728-7fa073aa07c4' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '66a1b5b3-ab20-414a-866a-9eb13148b623' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = 'd6f9e517-0286-43b1-874d-f100563f0696' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '3a479bad-e419-4f20-9c6d-b74e5dd08d93' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '47e433f2-c811-4b29-889b-90e35aea17bd' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '2c1ba287-ba7c-4619-976e-cf6a6c856d2e' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '3df26d46-cac6-4217-9cd8-9814ab06a907' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = 'c0c9913f-419e-435f-aa94-51ec6aecc0fb' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = 'fda16468-62d6-4f54-b491-88c26a411b08' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = 'b3cd7784-b4c4-4bba-b0b0-0448ca93b19b' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '03ac3fd5-bee3-4b8c-97c9-c1bf8c2a252e' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '9d226983-4433-4566-838e-c11102447a64' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '71efd160-2c1c-485e-833b-8923ce371f9e' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = 'de7b4989-d46e-42e4-a438-b5a1a38ea969' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '1d7906a2-ceb8-4f43-8f8e-9d46f7de728d' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '5ebffba2-c857-4575-ac2c-c68633757bda' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '438c5eb6-4340-4a56-b74f-634c0393390c' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '65c0dd18-3244-4f3d-a060-6f9ce2b24eb7' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = 'a3295808-3c0d-482b-aac9-5964f18f58b4' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '113f393c-b062-4184-a7db-a3043620a122' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = 'dbb97e50-b737-4b3c-955e-4d81e9f58b1f' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '81c4ed32-0a77-4a67-9572-36ef227d79b2' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '5bf3186a-926e-4e09-b03d-e3a1feef4d2d' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = 'f9ece2dd-2b2b-4fbd-ad4f-5a420ab17f22' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '116c6d6a-4da3-4922-894b-fd7c868835b8' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = 'ca6184f5-5a2c-4425-bcb6-2407a767199e' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
+update KSEN_LUI_ATTR set ATTR_VALUE = 'false' where OWNER_ID = '9b297b0d-b97d-48b8-9f29-d360b1d764b4' and ATTR_KEY = 'kuali.attribute.final.exam.use.matrix' and ATTR_VALUE = 'true'
+/
