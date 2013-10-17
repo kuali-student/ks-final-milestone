e0f3206e2a28546ddc232fccd76eccdaf69ec6d1
yindavid <yindavid@kuali.org>
2013-10-17 15:41:59 -0400

    KSENROLL-9404: modified the AO->EO suspended->canceled state propagation
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@58268 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M8/2013-10-11-KSEN-stateconfig-eo-cancel.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M8/2013-10-11-KSEN-stateconfig-eo-cancel.sql
@@ -3,6 +3,8 @@ insert into KSEN_STATE_CHG (ID, STATE_CHG_TYPE, STATE_CHG_STATE, OBJ_ID, FROM_ST
 /
 insert into KSEN_STATE_CHG (ID, STATE_CHG_TYPE, STATE_CHG_STATE, OBJ_ID, FROM_STATE_ID, TO_STATE_ID, EFF_DT, EXPIR_DT, VER_NBR, CREATETIME, CREATEID, UPDATETIME, UPDATEID) values ('kuali.statechange.examoffering.draft.canceled', 'kuali.state.change.type', 'kuali.state.change.state.active', null, 'kuali.lui.exam.offering.state.draft', 'kuali.lui.exam.offering.state.canceled', null, null, 0, TO_DATE ('8/11/2013', 'MM/DD/YYYY'), 'SYSTEMLOADER', TO_DATE ('8/12/2013', 'MM/DD/YYYY'), 'SYSTEMLOADER' )
 /
+insert into KSEN_STATE_CHG (ID, STATE_CHG_TYPE, STATE_CHG_STATE, OBJ_ID, FROM_STATE_ID, TO_STATE_ID, EFF_DT, EXPIR_DT, VER_NBR, CREATETIME, CREATEID, UPDATETIME, UPDATEID) values ('kuali.statechange.examoffering.suspended.canceled', 'kuali.state.change.type', 'kuali.state.change.state.active', null, 'kuali.lui.exam.offering.state.suspended', 'kuali.lui.exam.offering.state.canceled', null, null, 0, TO_DATE ('8/11/2013', 'MM/DD/YYYY'), 'SYSTEMLOADER', TO_DATE ('8/12/2013', 'MM/DD/YYYY'), 'SYSTEMLOADER' )
+/
 
 -- Insert  KSEN_STATE_PROPAGT
 insert into KSEN_STATE_PROPAGT (CREATEID, CREATETIME, OBJ_ID, ID, STATE_PROPAGT_TYPE, STATE_PROPAGT_STATE, TARGET_STATE_CHG_ID, UPDATEID, UPDATETIME, VER_NBR) values ('SYSTEMLOADER', TO_DATE ('8/11/2013', 'MM/DD/YYYY'), null, 'kuali.statepropagation.ao2eo.offered.canceled.2.draft.canceled', 'kuali.state.propagation.type', 'kuali.state.propagation.state', 'kuali.statechange.examoffering.draft.canceled', 'SYSTEMLOADER', TO_DATE ('8/11/2013', 'MM/DD/YYYY'), 0 )
@@ -13,6 +15,8 @@ insert into KSEN_STATE_PROPAGT (CREATEID, CREATETIME, OBJ_ID, ID, STATE_PROPAGT_
 /
 insert into KSEN_STATE_PROPAGT (CREATEID, CREATETIME, OBJ_ID, ID, STATE_PROPAGT_TYPE, STATE_PROPAGT_STATE, TARGET_STATE_CHG_ID, UPDATEID, UPDATETIME, VER_NBR) values ('SYSTEMLOADER', TO_DATE ('8/11/2013', 'MM/DD/YYYY'), null, 'kuali.statepropagation.ao2eo.suspended.canceled.2.draft.canceled', 'kuali.state.propagation.type', 'kuali.state.propagation.state', 'kuali.statechange.examoffering.draft.canceled', 'SYSTEMLOADER', TO_DATE ('8/11/2013', 'MM/DD/YYYY'), 0 )
 /
+insert into KSEN_STATE_PROPAGT (CREATEID, CREATETIME, OBJ_ID, ID, STATE_PROPAGT_TYPE, STATE_PROPAGT_STATE, TARGET_STATE_CHG_ID, UPDATEID, UPDATETIME, VER_NBR) values ('SYSTEMLOADER', TO_DATE ('8/11/2013', 'MM/DD/YYYY'), null, 'kuali.statepropagation.ao2eo.suspended.canceled.2.suspended.canceled', 'kuali.state.propagation.type', 'kuali.state.propagation.state', 'kuali.statechange.examoffering.suspended.canceled', 'SYSTEMLOADER', TO_DATE ('8/11/2013', 'MM/DD/YYYY'), 0 )
+/
 
 -- KSEN_STATE_CHG_PROPAGT
 insert into KSEN_STATE_CHG_PROPAGT (STATE_CHG_ID, STATE_PROPAGT_ID) values ('kuali.statechange.activityoffering.offered.canceled', 'kuali.statepropagation.ao2eo.offered.canceled.2.draft.canceled' )
@@ -21,5 +25,5 @@ insert into KSEN_STATE_CHG_PROPAGT (STATE_CHG_ID, STATE_PROPAGT_ID) values ('kua
 /
 insert into KSEN_STATE_CHG_PROPAGT (STATE_CHG_ID, STATE_PROPAGT_ID) values ('kuali.statechange.activityoffering.approved.canceled', 'kuali.statepropagation.ao2eo.approved.canceled.2.draft.canceled' )
 /
-insert into KSEN_STATE_CHG_PROPAGT (STATE_CHG_ID, STATE_PROPAGT_ID) values ('kuali.statechange.activityoffering.suspended.canceled', 'kuali.statepropagation.ao2eo.suspended.canceled.2.draft.canceled' )
+insert into KSEN_STATE_CHG_PROPAGT (STATE_CHG_ID, STATE_PROPAGT_ID) values ('kuali.statechange.activityoffering.suspended.canceled', 'kuali.statepropagation.ao2eo.suspended.canceled.2.suspended.canceled' )
 /
