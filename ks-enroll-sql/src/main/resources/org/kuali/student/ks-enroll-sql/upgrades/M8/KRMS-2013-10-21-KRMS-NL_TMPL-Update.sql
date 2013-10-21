9dd4058caf02da3f28339b6ebc8897f0db044fe2
peggy <peggy@kuali.org>
2013-10-21 07:57:03 -0400

    KSENROLL-10262 	Change Rule Statement Option text - common finals matrix
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@58421 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M8/KRMS-2013-10-21-KRMS-NL_TMPL-Update.sql
@@ -0,0 +1,9 @@
+Update KRMS_NL_TMPL_T
+   set TMPL = 'if course is <Course>'
+ where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-55698'
+ /        
+         
+Update KRMS_NL_TMPL_T
+   set TMPL = 'if course is part of <Courses>'
+ where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-55701'
+ /   
