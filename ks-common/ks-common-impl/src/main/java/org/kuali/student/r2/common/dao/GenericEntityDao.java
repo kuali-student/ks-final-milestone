6372da7470c38ebe2b3fe73ef695114595e8fa93
depstein <depstein@kuali.org>
2013-10-21 09:53:50 -0400

    KSENROLL-7492 cleaned up code and added some documentation
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@58436 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-impl/src/main/java/org/kuali/student/r2/common/dao/GenericEntityDao.java
+++ ks-common/ks-common-impl/src/main/java/org/kuali/student/r2/common/dao/GenericEntityDao.java
@@ -20,10 +20,19 @@ import org.apache.commons.lang.StringUtils;
 import org.kuali.student.r2.common.entity.PersistableEntity;
 import org.kuali.student.r2.common.exceptions.DoesNotExistException;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author Kuali Student Team
@@ -61,11 +70,9 @@ public class GenericEntityDao<T extends PersistableEntity<String>> implements En
         // remove duplicates from the key list
         primaryKeySet.addAll(primaryKeys);
 
-
-
         StringBuilder queryString = new StringBuilder();
 
-        Query query = buildQuery(queryString, primaryKeyMemberName, primaryKeySet);
+        TypedQuery<T> query = buildQuery(queryString, primaryKeyMemberName, primaryKeySet);
 
         List<T> resultList = query.getResultList();
 
@@ -74,30 +81,38 @@ public class GenericEntityDao<T extends PersistableEntity<String>> implements En
         return resultList;
     }
 
-    protected Query buildQuery(StringBuilder queryStringRef, String primaryKeyMemberName, Set<String> primaryKeySet){
+    /**
+     * Returns a query of find by ids. Breaks up the query into multiple ORed IN() clauses if the set of ids is larger
+     * than maxInClauseElements
+     * @param queryStringRef a reference to the query string
+     * @param primaryKeyMemberName name of the column that is the primary key
+     * @param primaryKeySet set of primary key strings
+     * @return a typed query that finds entities for the set of keys
+     */
+    protected TypedQuery<T> buildQuery(StringBuilder queryStringRef, String primaryKeyMemberName, Set<String> primaryKeySet) {
 
-        Query queryRef;
+        TypedQuery<T> queryRef;
 
-        if(!enableMaxIdFetch || primaryKeySet.size()<=maxInClauseElements) {
+        if (!enableMaxIdFetch || primaryKeySet.size() <= maxInClauseElements) {
 
             queryStringRef.append("from ").append(entityClass.getSimpleName()).append(" where ").append(primaryKeyMemberName).append(" in (:ids)");
-            queryRef = em.createQuery(queryStringRef.toString()).setParameter("ids", primaryKeySet);
+            queryRef = em.createQuery(queryStringRef.toString(), entityClass).setParameter("ids", primaryKeySet);
 
         } else {
-
+            //Max fetchh is enabled so break uip the where clause into multiple IN() clauses
             List<List<String>> brokenLists = new ArrayList<List<String>>();
             List<String> lst = new ArrayList<String>();
 
             queryStringRef.append("from ").append(entityClass.getSimpleName());
 
             Iterator<String> itr = primaryKeySet.iterator();
-            for(int index=0; itr.hasNext() ; index++) {
+            for (int index = 0; itr.hasNext(); index++) {
 
-                if(index%maxInClauseElements==0) {
+                if (index % maxInClauseElements == 0) {
 
                     brokenLists.add(lst);
 
-                    if(brokenLists.size()==1) {
+                    if (brokenLists.size() == 1) {
                         queryStringRef.append(" where ").append(primaryKeyMemberName).append(" in (:ids1)");
                     } else {
                         queryStringRef.append(" or ").append(primaryKeyMemberName).append(" in (:ids").append(brokenLists.size()).append(")");
@@ -107,9 +122,9 @@ public class GenericEntityDao<T extends PersistableEntity<String>> implements En
                 lst.add(itr.next());
             }
 
-            queryRef = em.createQuery(queryStringRef.toString());
+            queryRef = em.createQuery(queryStringRef.toString(), entityClass);
 
-            for(int i=1 ; i<=brokenLists.size() ; i++) {
+            for (int i = 1; i <= brokenLists.size(); i++) {
                 queryRef.setParameter("ids" + i, brokenLists.get(i - 1));
             }
 
@@ -145,12 +160,14 @@ public class GenericEntityDao<T extends PersistableEntity<String>> implements En
         return true;
         
     }
+
     protected void verifyResults(List<T> resultList, Set<String> primaryKeys) throws DoesNotExistException {
 
-    	 if (resultList.size() == 0)
-         	throw new DoesNotExistException("No data was found for : " + StringUtils.join(primaryKeys, ", "));
-    	 
-         else if (resultList.size() != primaryKeys.size()) {
+    	 if (resultList.size() == 0){
+
+             throw new DoesNotExistException("No data was found for : " + StringUtils.join(primaryKeys, ", "));
+
+         } else if (resultList.size() != primaryKeys.size()) {
         	 // only found some of the keys given.
          	Set<String> unmatchedKeySet = new HashSet<String> ();
          
@@ -172,43 +189,6 @@ public class GenericEntityDao<T extends PersistableEntity<String>> implements En
         return this.findByIds("id", primaryKeys);
 
     }
-
-    /**
-     * Use this method if the size of primary keys >= 1000. 1000 is the "in" limit for many databases.
-     *
-     * @param primaryKeys
-     * @return
-     * @throws org.kuali.student.r2.common.exceptions.DoesNotExistException
-     */
-    protected List<T> findByIdsMaxKeys(List<String> primaryKeys) throws DoesNotExistException {
-        List<T> resultList = new ArrayList<T>();
-
-     // fix for jira KSENROLL-2949
-        if (primaryKeys.isEmpty())
-         	return new ArrayList<T>();
-        
-        Set<String>primaryKeySet = new HashSet<String>(primaryKeys.size());
-		// remove duplicate keys
-		primaryKeySet.addAll(primaryKeys);
-
-        
-        for (String primaryKey : primaryKeys) {
-
-            T entity = find(primaryKey);
-
-            if (entity == null) {
-
-                throw new DoesNotExistException("No data was found for :" + primaryKey);
-
-            }
-            resultList.add(entity);
-        }
-        
-        verifyResults (resultList, primaryKeySet);
-        
-        return resultList;
-    }
-
 
     @Override
     @SuppressWarnings("unchecked")
