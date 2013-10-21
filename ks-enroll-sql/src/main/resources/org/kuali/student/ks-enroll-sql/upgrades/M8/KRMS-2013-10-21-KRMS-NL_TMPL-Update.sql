144b31c03121bf8a8947ba48b6a6f973b7dd3823
peggy <peggy@kuali.org>
2013-10-21 08:56:28 -0400

    KSENROLL-10262 removed space before / causing impex failure
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@58424 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M8/KRMS-2013-10-21-KRMS-NL_TMPL-Update.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M8/KRMS-2013-10-21-KRMS-NL_TMPL-Update.sql
@@ -1,9 +1,5 @@
-Update KRMS_NL_TMPL_T
-   set TMPL = 'if course is <Course>'
- where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-55698'
- /        
+update KRMS_NL_TMPL_T set TMPL = 'if course is <Course>' where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-55698'
+/
          
-Update KRMS_NL_TMPL_T
-   set TMPL = 'if course is part of <Courses>'
- where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-55701'
- /   
+update KRMS_NL_TMPL_T set TMPL = 'if course is part of <Courses>' where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-55701'
+/
