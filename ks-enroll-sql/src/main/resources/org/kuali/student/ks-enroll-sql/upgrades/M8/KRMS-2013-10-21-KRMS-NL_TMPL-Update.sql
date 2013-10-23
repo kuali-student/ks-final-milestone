a97df638f20e6791b3495be8848db3dd272ba13c
peggy <peggy@kuali.org>
2013-10-23 09:26:29 -0400

    KKSENROLL-10262: renamed  to slq naming standard, and updated the NL tamplate
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@58691 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M8/KRMS-2013-10-21-KRMS-NL_TMPL-Update.sql
+++
@@ -1,5 +0,0 @@
-update KRMS_NL_TMPL_T set TMPL = 'if course is <Course>' where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-55698'
-/
-         
-update KRMS_NL_TMPL_T set TMPL = 'if course is part of <Courses>' where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-55701'
-/
