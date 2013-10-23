5ee16bc6b241db46f8da7c27f5f9a6efff842b64
dedewet <dedewet@kuali.org>
2013-10-23 08:37:43 -0400

    KSENROLL-10344 - Display changed of AO requisites on SOC Reg Group
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@58686 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/scheduleofclasses/util/ScheduleOfClassesTableLayoutManager.java
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/scheduleofclasses/util/ScheduleOfClassesTableLayoutManager.java
@@ -1,21 +1,19 @@
 package org.kuali.student.enrollment.class2.scheduleofclasses.util;
 
+import org.apache.commons.lang.StringUtils;
 import org.kuali.rice.krad.uif.UifConstants;
 import org.kuali.rice.krad.uif.component.Component;
 import org.kuali.rice.krad.uif.container.CollectionGroup;
 import org.kuali.rice.krad.uif.element.Action;
-import org.kuali.rice.krad.uif.element.Label;
 import org.kuali.rice.krad.uif.field.Field;
 import org.kuali.rice.krad.uif.field.FieldGroup;
 import org.kuali.rice.krad.uif.layout.TableLayoutManager;
-import org.kuali.rice.krad.uif.util.ComponentFactory;
 import org.kuali.rice.krad.uif.util.ComponentUtils;
 import org.kuali.rice.krad.uif.view.View;
 import org.kuali.student.enrollment.class2.courseoffering.dto.ActivityOfferingWrapper;
+import org.kuali.student.enrollment.class2.courseoffering.dto.RegistrationGroupWrapper;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeMap;
 
 /**
  * This class overrides the default table layout manager to hide the details
@@ -26,30 +24,101 @@ import java.util.TreeMap;
 public class ScheduleOfClassesTableLayoutManager extends TableLayoutManager {
 
     private Field requisitesField;
+    private Field commonRequisiteField;
+    private String name = StringUtils.EMPTY;
 
-    @Override
     public void buildLine(View view, Object model, CollectionGroup collectionGroup, List<Field> lineFields, List<FieldGroup> subCollectionFields,
                           String bindingPath, List<Action> actions, String idSuffix, Object currentLine, int lineIndex) {
         super.buildLine(view, model, collectionGroup, lineFields, subCollectionFields, bindingPath, actions, idSuffix,
                 currentLine, lineIndex);
 
-        ActivityOfferingWrapper aoWrapper = (ActivityOfferingWrapper) currentLine;
+        if(currentLine instanceof ActivityOfferingWrapper) {
+            ActivityOfferingWrapper aoWrapper = (ActivityOfferingWrapper) currentLine;
 
-        if((aoWrapper.getRequisite()!=null)&&(!aoWrapper.getRequisite().isEmpty())){
-            Field requisitesPrototype = this.getRequisitesField();
+            if((aoWrapper.getRequisite()!=null)&&(!aoWrapper.getRequisite().isEmpty())){
+                Field requisitesPrototype = this.getRequisitesField();
 
-            Field requisites = ComponentUtils.copy(requisitesPrototype,
-                idSuffix + UifConstants.IdSuffixes.SUB + lineIndex);
+                Field requisites = ComponentUtils.copy(requisitesPrototype,
+                        idSuffix + UifConstants.IdSuffixes.SUB + lineIndex);
 
-            requisites.setColSpan(this.getNumberOfDataColumns());
-            this.getAllRowFields().add(requisites);
+                for(Field field : lineFields) {
+                    if(field instanceof FieldGroup) {
+                        field.setRowSpan(2);
+                    }
+                }
+
+                requisites.setColSpan(this.getNumberOfDataColumns() - 1);
+                this.getAllRowFields().add(requisites);
+            }
+        } else {
+            RegistrationGroupWrapper rgWrapper = (RegistrationGroupWrapper) currentLine;
+
+            if(name.isEmpty() || !rgWrapper.getRgInfo().getName().equals(name)){
+
+                name = rgWrapper.getRgInfo().getName();
+
+                for (Field field : lineFields) {
+                    if(field.getId().contains("regGroupName")) {
+                        field.setRowSpan(getSpanSize(rgWrapper));
+                    }
+                }
+            } else {
+                for (Field field : lineFields) {
+                    if(field.getId().contains("regGroupName")) {
+                        field.setHidden(true);
+                        field.setCellStyle("border-top:none;");
+                        field.setRowSpan(getSpanSize(rgWrapper));
+                    }
+                }
+            }
+
+            if((rgWrapper.getCommonRequisite()!=null)&&(!rgWrapper.getCommonRequisite().isEmpty())){
+                Field commonRequisitesPrototype = this.getCommonRequisiteField();
+
+                Field commonRequisites = ComponentUtils.copy(commonRequisitesPrototype,
+                        idSuffix + UifConstants.IdSuffixes.SUB + lineIndex);
+
+                commonRequisites.setColSpan(this.getNumberOfDataColumns() - 1);
+
+                int index = (this.getAllRowFields().size() - lineFields.size()) + 1;
+                this.getAllRowFields().add(index, commonRequisites);
+            }
+
+            if((rgWrapper.getRequisite()!=null)&&(!rgWrapper.getRequisite().isEmpty())){
+                Field requisitesPrototype = this.getRequisitesField();
+
+                Field requisites = ComponentUtils.copy(requisitesPrototype,
+                        idSuffix + UifConstants.IdSuffixes.SUB + lineIndex);
+
+                requisites.setColSpan(this.getNumberOfDataColumns() - 2);
+
+                for(Field field : lineFields) {
+                    if(field.getId().contains("activityOfferingCode")) {
+                        field.setRowSpan(2);
+                    }
+                }
+
+                this.getAllRowFields().add(requisites);
+            }
         }
+    }
+
+    private int getSpanSize(RegistrationGroupWrapper registrationGroupWrapper) {
+        int spanSize = 1;
+        if (registrationGroupWrapper.getRequisite() != null) {
+            spanSize++;
+        }
+        if (registrationGroupWrapper.getCommonRequisite() != null) {
+            spanSize++;
+        }
+        return spanSize;
     }
 
     @Override
     public List<Component> getComponentPrototypes() {
         List<Component> components = super.getComponentPrototypes();
         components.add(this.getRequisitesField());
+        components.add(this.getCommonRequisiteField());
         return components;
     }
 
@@ -61,6 +130,14 @@ public class ScheduleOfClassesTableLayoutManager extends TableLayoutManager {
         this.requisitesField = requisitesField;
     }
 
+    public Field getCommonRequisiteField() {
+        return commonRequisiteField;
+    }
+
+    public void setCommonRequisiteField(Field commonRequisiteField) {
+        this.commonRequisiteField = commonRequisiteField;
+    }
+
     @Override
     protected <T> void copyProperties(T layoutManager) {
         super.copyProperties(layoutManager);
@@ -68,6 +145,9 @@ public class ScheduleOfClassesTableLayoutManager extends TableLayoutManager {
         ScheduleOfClassesTableLayoutManager tableLayoutManagerCopy = (ScheduleOfClassesTableLayoutManager) layoutManager;
         if (this.getRequisitesField() != null) {
             tableLayoutManagerCopy.setRequisitesField((Field) this.getRequisitesField().copy());
+        }
+        if (this.getCommonRequisiteField() != null) {
+            tableLayoutManagerCopy.setCommonRequisiteField((Field) this.getCommonRequisiteField().copy());
         }
     }
 }
