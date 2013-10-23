77125ac95296a0db10578f340784179decc847ef
brandon.gresham <brandon.gresham@kuali.org>
2013-10-22 20:08:54 -0400

    KSENROLL-10129 Additional time-slot ref-data to test standard time-slot type-ahead functionality.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@58618 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M8/2013-10-22z-KSEN_SCHED_TMSLOT-Add_Ref_Data.sql
@@ -0,0 +1,12 @@
+-- Additional time-slot ref-data to test standard time-slot type-ahead functionality --
+-- KSENROLL-10129 --
+INSERT INTO KSEN_SCHED_TMSLOT (CREATEID,CREATETIME,END_TIME_MS,ID,NAME, START_TIME_MS,TM_SLOT_STATE,TM_SLOT_TYPE,UPDATEID,UPDATETIME,VER_NBR,WEEKDAYS) VALUES ('batchjob',TO_DATE( '20131022133000', 'YYYYMMDDHH24MISS' ),30900000, SYS_GUID(), '1695', 28800000, 'kuali.scheduling.timeslot.state.active', 'kuali.scheduling.time.slot.type.activityoffering.standard.fullterm.fall', 'batchjob',TO_DATE( '20131022133000', 'YYYYMMDDHH24MISS' ),0, 'MWF')
+/
+INSERT INTO KSEN_SCHED_TMSLOT (CREATEID,CREATETIME,END_TIME_MS,ID,NAME, START_TIME_MS,TM_SLOT_STATE,TM_SLOT_TYPE,UPDATEID,UPDATETIME,VER_NBR,WEEKDAYS) VALUES ('batchjob',TO_DATE( '20131022133000', 'YYYYMMDDHH24MISS' ),65880000, SYS_GUID(), '1696', 61200000, 'kuali.scheduling.timeslot.state.active', 'kuali.scheduling.time.slot.type.activityoffering.standard.fullterm.fall', 'batchjob',TO_DATE( '20131022133000', 'YYYYMMDDHH24MISS' ),0, 'TH')
+/
+INSERT INTO KSEN_SCHED_TMSLOT (CREATEID,CREATETIME,END_TIME_MS,ID,NAME, START_TIME_MS,TM_SLOT_STATE,TM_SLOT_TYPE,UPDATEID,UPDATETIME,VER_NBR,WEEKDAYS) VALUES ('batchjob',TO_DATE( '20131022133000', 'YYYYMMDDHH24MISS' ),63000000, SYS_GUID(), '1697', 55800000, 'kuali.scheduling.timeslot.state.active', 'kuali.scheduling.time.slot.type.activityoffering.standard.fullterm.spring', 'batchjob',TO_DATE( '20131022133000', 'YYYYMMDDHH24MISS' ),0, 'TH')
+/
+INSERT INTO KSEN_SCHED_TMSLOT (CREATEID,CREATETIME,END_TIME_MS,ID,NAME, START_TIME_MS,TM_SLOT_STATE,TM_SLOT_TYPE,UPDATEID,UPDATETIME,VER_NBR,WEEKDAYS) VALUES ('batchjob',TO_DATE( '20131022133000', 'YYYYMMDDHH24MISS' ),53100000, SYS_GUID(), '1698', 45000000, 'kuali.scheduling.timeslot.state.active', 'kuali.scheduling.time.slot.type.activityoffering.standard.fullterm.spring', 'batchjob',TO_DATE( '20131022133000', 'YYYYMMDDHH24MISS' ),0, 'TH')
+/
+INSERT INTO KSEN_SCHED_TMSLOT (CREATEID,CREATETIME,END_TIME_MS,ID,NAME, START_TIME_MS,TM_SLOT_STATE,TM_SLOT_TYPE,UPDATEID,UPDATETIME,VER_NBR,WEEKDAYS) VALUES ('batchjob',TO_DATE( '20131022133000', 'YYYYMMDDHH24MISS' ),44100000, SYS_GUID(), '1699', 39600000, 'kuali.scheduling.timeslot.state.active', 'kuali.scheduling.time.slot.type.activityoffering.standard.fullterm.spring', 'batchjob',TO_DATE( '20131022133000', 'YYYYMMDDHH24MISS' ),0, 'MWF')
+/
