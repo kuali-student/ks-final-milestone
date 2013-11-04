c6eaabfe545c49ab18471f1c2d3ed280727270cc
hossein.aliabadi <hossein.aliabadi@kuali.org>
2013-11-04 13:45:47 -0500

    KSENROLL-10607: Manage CO select all rows checkbox isue, Approve button should be enabled
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@59434 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-web/src/main/webapp/themes/ksboot/scripts/toolbar.js
+++ ks-common/ks-common-web/src/main/webapp/themes/ksboot/scripts/toolbar.js
@@ -59,7 +59,7 @@ function handleButton(arr, id) {
 
 function evaluateButton(arr, id){
     if(jQuery.inArray("true", arr) != -1){
-        enableButton(id);
+        enableTBButton(id);
     } else{
         disableTBButton(id);
     }
@@ -68,26 +68,26 @@ function evaluateButton(arr, id){
 function enableTBButton(id){
     jQuery("#" + id).removeClass('disabled');
     jQuery("#" + id).removeAttr("disabled");
-    var img = jQuery("#" + id).find('img') ,
+   /* var img = jQuery("#" + id).find('img') ,
            src = img.attr('src') ,
            disable = /\_disabled\.png$/;
 
     if(src.match(disable)) {
         img.attr('src', src.replace(disable, '_enabled.png'));
-    }
+    }*/
 
 }
 
 function disableTBButton(id){
     jQuery("#" + id).addClass("disabled");
     jQuery("#" + id).attr("disabled", "disabled");
-    var img = jQuery("#" + id).find('img') ,
+   /* var img = jQuery("#" + id).find('img') ,
            src = img.attr('src') ,
            enable = /\_enabled\.png$/ ;
 
     if(src.match(enable)) {
         img.attr('src', src.replace(enable, '_disabled.png'));
-    }
+    }*/
 }
 
 function refreshAddAODropdowns(){
