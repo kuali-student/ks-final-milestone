8a816c0f70577657a560f12a45d8cae1aa063381
christoff.botha <christoff.botha@kuali.org>
2013-11-04 08:45:17 -0500

    KSENROLL-10433 Remove the krms class standing proposition types.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@59419 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/RC/2013-11-04-KRMS-remove-class-standing-types.sql
@@ -0,0 +1,91 @@
+--KSENROLL-10433
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Must not have been admitted to the Womens Studies Program with a class standing of Freshman', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-10225'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Senior', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-10228'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Must not be in a class standing of Freshman', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-10229'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or less', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-10230'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12869'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12870'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12871'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Sophomore or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12874'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12876'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12893'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12902'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Senior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12912'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Senior', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12945'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13023'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12814'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12815'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12828'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12830'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12836'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Sophomore or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12838'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12839'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12840'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Senior', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-12856'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13280'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13316'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Sophomore or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13331'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13405'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13408'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13411'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13414'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13417'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13420'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13423'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13040'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Sophomore or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13062'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13085'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13087'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13089'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13091'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13141'
+/
+UPDATE KRMS_TERM_PARM_T SET VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13196'
+/
+update KRMS_TERM_PARM_T set VAL = 'Student must be in a class standing of Junior or greater', NM = 'kuali.term.parameter.type.free.text' where TERM_PARM_ID = 'KS-KRMS-TERM-PARM-13207'
+/
+update KRMS_PROP_T set TYP_ID = '10031' where TYP_ID in ('10064','10066','10067','10068','10069')
+/
+DELETE FROM KRMS_TERM_PARM_T WHERE TERM_PARM_ID = 'KS-KRMS-TERM-PARM-10226'
+/
+delete from KRMS_TYP_T where TYP_ID in ('10064','10066','10067','10068','10069')
+/
