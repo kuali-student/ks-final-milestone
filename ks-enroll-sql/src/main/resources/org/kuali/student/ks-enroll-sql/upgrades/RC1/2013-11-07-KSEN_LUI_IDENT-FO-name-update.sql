0241593b4bec05a21b27b0c1c3fcf6a19e40572b
vgadiyak <vgadiyak@kuali.org>
2013-11-07 15:00:37 -0500

    KSENROLL-10606 : new file with updates for KSEN_LUI_IDENT
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@59792 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/RC1/2013-11-07-KSEN_LUI_IDENT-FO-name-update.sql
@@ -0,0 +1,59 @@
+-- KSENROLL-10606
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '6d6259ed-1244-45cd-a8f9-7a3a5cf43f47'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Discussion/Lab', SHRT_NAME = 'DIS/LAB' where LUI_ID = '7e4c21f6-c185-45ee-b9a7-1a504aa04d97'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '3b6923ee-ac45-45db-97b8-233968138978'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '3f723f1f-fb7c-4bd7-a1c8-1eb5c20d5be9'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '45aeeaad-1212-4cad-a975-3c4d6b57742a'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '9c6e3d2e-555c-41fe-9d04-d1657f143a59'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Discussion/Lab', SHRT_NAME = 'DIS/LAB' where LUI_ID = 'fa00eec7-9732-4fe7-bc37-9b1b6bb4ce0d'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '70c5646d-894f-4e60-acd5-156eb533474d'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = 'b8d7087f-2dc0-449e-8db5-c86e2ce4d5cf'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '69a00fa0-8ab8-4dbd-ab9a-e7e020de7942'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '43ec9534-a65c-4433-9bf4-a90d9c4ead6b'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Discussion/Lab', SHRT_NAME = 'DIS/LAB' where LUI_ID = 'c7aaae91-9a22-4fac-ab3c-7b9587727170'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '5f396d72-0578-4467-9f00-590e51a2b8fb'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Discussion/Lab', SHRT_NAME = 'DIS/LAB' where LUI_ID = 'b11dce52-4e9f-4e8c-b745-8ecaf689f203'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '9a852c90-0ada-48a6-9e63-0d21cdbd687a'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '5236aa09-34a7-4e12-8636-26bd34952416'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = 'b07dc56c-5988-4c36-bdac-e1542299099b'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = 'e6ff9d51-70b1-4a04-b51a-3dfb97c61555'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '350782e0-9052-4885-93a0-9bffbdf62b27'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Discussion/Lab', SHRT_NAME = 'DIS/LAB' where LUI_ID = '908b16d1-8bab-46fe-be27-c5fc5bba400f'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = 'a84b241d-ef14-4758-af01-5572c50d0c7b'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '95329a72-d6c9-48b9-ba69-749c562248ea'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '9759c1b9-cca2-4970-a22a-742190a34c28'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '8d4c9db9-1b97-4bcc-a45c-26e7bcb33299'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '07b807aa-b067-4abf-aed0-5f5c5a7094ad'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '0c2e8d8d-5b09-4e5e-9b4b-1f846849196c'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '8a8cae8e-c0b5-4265-af92-9ef6fee90f77'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '7c1c7dbe-d224-49db-bc21-c32ac5a5ccd3'
+/
+update KSEN_LUI_IDENT set LNG_NAME = 'Lecture/Lab', SHRT_NAME = 'LEC/LAB' where LUI_ID = '082c0f44-3e1a-4f7b-84b0-45569aed5044'
+/
