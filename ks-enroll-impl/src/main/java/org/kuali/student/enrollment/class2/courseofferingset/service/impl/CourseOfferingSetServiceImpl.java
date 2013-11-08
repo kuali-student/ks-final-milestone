00af9ddf3932dcfc918f8c116321a635d79f8912
christoff.botha <christoff.botha@kuali.org>
2013-11-08 04:16:51 -0500

    KSENROLL-4747 Manage SOC status change logging fix.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@59858 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseofferingset/service/impl/CourseOfferingSetServiceImpl.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseofferingset/service/impl/CourseOfferingSetServiceImpl.java
@@ -789,6 +789,7 @@ public class CourseOfferingSetServiceImpl implements CourseOfferingSetService {
                 }
 
                 // Log the state change
+                contextInfo.setCurrentDate(new Date());
                 logStateChange(entity, nextStateKey, contextInfo);
                 LOG.warn(String.format("Updated SOC [%s] state to [%s].", socId, nextStateKey));
 
