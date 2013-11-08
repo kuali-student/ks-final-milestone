42c3e3b9147b1d4d0d7e9222144abcbf340958f7
sw <sw@kuali.org>
2013-11-08 05:58:40 -0500

    KSENROLL-10735 - Fixed search on program.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@59862 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-ui/src/main/java/org/kuali/rice/krms/controller/RuleEditorController.java
+++ ks-core-ui/src/main/java/org/kuali/rice/krms/controller/RuleEditorController.java
@@ -19,7 +19,6 @@ import org.apache.commons.lang.StringUtils;
 import org.kuali.rice.core.api.util.tree.Node;
 import org.kuali.rice.krad.uif.UifParameters;
 import org.kuali.rice.krad.util.GlobalVariables;
-import org.kuali.rice.krad.util.KRADConstants;
 import org.kuali.rice.krad.web.controller.MaintenanceDocumentController;
 import org.kuali.rice.krad.web.form.MaintenanceDocumentForm;
 import org.kuali.rice.krad.web.form.UifFormBase;
