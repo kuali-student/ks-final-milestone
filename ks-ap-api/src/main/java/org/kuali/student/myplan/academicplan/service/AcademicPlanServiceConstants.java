71da98d4b3a8b6baf57fe858f2bd77ea6e8e4df5
rileymg <rileymg@kuali.org>
2013-11-11 11:46:33 -0500

    KSAP-182 changed ksap to use current type services (based on KSEN_TYPE)
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/KSAP@59986 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-api/src/main/java/org/kuali/student/myplan/academicplan/service/AcademicPlanServiceConstants.java
+++ ks-ap-api/src/main/java/org/kuali/student/myplan/academicplan/service/AcademicPlanServiceConstants.java
@@ -14,12 +14,13 @@ public class AcademicPlanServiceConstants {
 
     public static final String LEARNING_PLAN_TYPE_PLAN = "kuali.academicplan.type.plan";
     public static final String LEARNING_PLAN_TYPE_PLAN_TEMPLATE = "kuali.academicplan.type.plan.template";
+    public static final String LEARNING_PLAN_TYPE_PLAN_REVIEW = "kuali.academicplan.type.plan.review";
 
-    public static final String LEARNING_PLAN_ITEM_TYPE_PLANNED = "kuali.academicplan.item.planned";
-    public static final String LEARNING_PLAN_ITEM_TYPE_BACKUP = "kuali.academicplan.item.backup";
-    public static final String LEARNING_PLAN_ITEM_TYPE_WISHLIST = "kuali.academicplan.item.wishlist";
-    public static final String LEARNING_PLAN_ITEM_TYPE_WHATIF = "'kuali.academicplan.item.whatif";
-    public static final String LEARNING_PLAN_ITEM_TYPE_CART = "kuali.academicplan.item.cart";
+    public static final String LEARNING_PLAN_ITEM_TYPE_PLANNED = "kuali.academicplan.item.type.planned";
+    public static final String LEARNING_PLAN_ITEM_TYPE_BACKUP = "kuali.academicplan.item.type.backup";
+    public static final String LEARNING_PLAN_ITEM_TYPE_WISHLIST = "kuali.academicplan.item.type.wishlist";
+    public static final String LEARNING_PLAN_ITEM_TYPE_WHATIF = "'kuali.academicplan.item.type.whatif";
+    public static final String LEARNING_PLAN_ITEM_TYPE_CART = "kuali.academicplan.item.type.cart";
 
     public static final String LEARNING_PLAN_ACTIVE_STATE_KEY  = "kuali.academicplan.plan.state.active";
     public static final String LEARNING_PLAN_ITEM_ACTIVE_STATE_KEY = "kuali.academicplan.planitem.state.active";
