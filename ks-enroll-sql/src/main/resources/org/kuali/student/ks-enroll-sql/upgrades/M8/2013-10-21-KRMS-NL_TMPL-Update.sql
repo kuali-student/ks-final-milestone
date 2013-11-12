abc8dc5569ba1d23c495040c9013759911b80fb3
peggy <peggy@kuali.org>
2013-11-12 04:52:21 -0500

    KSENROLL-10645: Capitalize 'if' in common final exam rule statements
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@60050 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M8/2013-10-21-KRMS-NL_TMPL-Update.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/M8/2013-10-21-KRMS-NL_TMPL-Update.sql
@@ -1,4 +1,4 @@
-update KRMS_NL_TMPL_T set TMPL = 'if course is part of <Courses>' where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-55699'
+update KRMS_NL_TMPL_T set TMPL = 'If course is part of <Courses>' where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-55699'
 /
-update KRMS_NL_TMPL_T set TMPL = 'if course is <Course>' where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-55696'
+update KRMS_NL_TMPL_T set TMPL = 'If course is <Course>' where NL_TMPL_ID = 'KS-KRMS-NL-TMPL-55696'
 /
