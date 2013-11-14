24da96c48d38acb972817cd44f6f5b6ee3d86d8f
jonrcook <jonrcook@kuali.org>
2013-11-14 12:35:22 -0500

    KSENROLL-10762 added SERVICE_NAME_LOCAL_PART
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/trunk@60287 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-api/src/main/java/org/kuali/student/r2/core/constants/RuleServiceConstants.java
+++ ks-core-api/src/main/java/org/kuali/student/r2/core/constants/RuleServiceConstants.java
@@ -24,5 +24,6 @@ import org.kuali.student.r2.core.rule.dto.RuleInfo;
  */
 public class RuleServiceConstants {
     public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "rule";
-    public static final String REF_OBJECT_URI_COMMENT = NAMESPACE + "/" + RuleInfo.class.getSimpleName();
+    public static final String REF_OBJECT_URI_RULE = NAMESPACE + "/" + RuleInfo.class.getSimpleName();
+    public static final String SERVICE_NAME_LOCAL_PART = "RuleService";
 }
