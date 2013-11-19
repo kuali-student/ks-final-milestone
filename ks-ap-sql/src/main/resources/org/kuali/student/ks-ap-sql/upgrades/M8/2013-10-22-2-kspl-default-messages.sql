460a56a2d013261b0d03b1551f9e6823e55d725b
johglov <johglov@kuali.org>
2013-11-19 14:15:32 -0500

    KSAP-307 - Changed locale for messages to en from en_US
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@60554 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-sql/src/main/resources/org/kuali/student/ks-ap-sql/upgrades/M8/2013-10-22-2-kspl-default-messages.sql
+++ ks-ap-sql/src/main/resources/org/kuali/student/ks-ap-sql/upgrades/M8/2013-10-22-2-kspl-default-messages.sql
@@ -3,7 +3,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_SECTION_NUMBER_HEADING','Section',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_SECTION_NUMBER_HEADING','Section',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -16,7 +16,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_CREDITS_HEADING','Credit',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_CREDITS_HEADING','Credit',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -29,7 +29,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_TERMS_TOOLTIP_TEXT','Projected courses indicate which term the course might be offered next. Contact the department for verification. Scheduled courses appear in the current Time Schedule.',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_TERMS_TOOLTIP_TEXT','Projected courses indicate which term the course might be offered next. Contact the department for verification. Scheduled courses appear in the current Time Schedule.',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -42,7 +42,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','RESULTS_CREDITS_FACET_NAME','Credit',
+	        SUBSTR(sys_guid(),21),'en','RESULTS_CREDITS_FACET_NAME','Credit',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -54,20 +54,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_QUARTERS_HEADER','Terms',
-	        SUBSTR(sys_guid(),1,8) || '-' ||
-	        SUBSTR(sys_guid(),9,4) || '-' ||
-	        SUBSTR(sys_guid(),13,4) || '-' ||
-	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),1)
-/
-
-INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
-  VALUES ('myplan',	SUBSTR(sys_guid(),1,8) || '-' ||
-	        SUBSTR(sys_guid(),9,4) || '-' ||
-	        SUBSTR(sys_guid(),13,4) || '-' ||
-	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_TERMSOFFERED_LABEL','Projected Term Info',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_QUARTERS_HEADER','Terms',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -80,7 +67,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_COURSEDESCRIPTION_BULLETIN_SOURCE_NAME','Bulletin',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_TERMSOFFERED_LABEL','Projected Term Info',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -93,7 +80,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_COURSEDESCRIPTION_SOURCE_NAME','Course Catalog',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_COURSEDESCRIPTION_BULLETIN_SOURCE_NAME','Bulletin',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -106,7 +93,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_REQUISITE_SOURCE_NAME','Course Catalog',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_COURSEDESCRIPTION_SOURCE_NAME','Course Catalog',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -119,7 +106,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_CURRICULUM_HEADER','Curriculum',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_REQUISITE_SOURCE_NAME','Course Catalog',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -132,7 +119,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_GENED_TOOLTIP_TEXT','&lt;strong&gt;General Education Requirements&lt;/strong&gt; &lt;/br&gt; &lt;strong&gt;C&lt;/strong&gt; - English Composition&lt;/br&gt; &lt;strong&gt;W&lt;/strong&gt; - Additional Writing&lt;/br&gt; &lt;strong&gt;Q&lt;/strong&gt; - Quantitative and Symbolic Reasoning&lt;/br&gt; &lt;strong&gt;VLPA&lt;/strong&gt;  - Visual, Literary, and Performing Arts&lt;/br&gt; &lt;strong&gt;I&amp;S&lt;/strong&gt; - Individuals and Societies&lt;/br&gt; &lt;strong&gt;NW&lt;/strong&gt; - Natural World&lt;/br&gt; &lt;strong&gt;None&lt;/strong&gt; - Does not count towards General Education Requirements',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_CURRICULUM_HEADER','Curriculum',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -145,7 +132,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_TERMS_SCHEDULED_SOURCE_NAME','Time Schedule',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_GENED_TOOLTIP_TEXT','&lt;strong&gt;General Education Requirements&lt;/strong&gt; &lt;/br&gt; &lt;strong&gt;C&lt;/strong&gt; - English Composition&lt;/br&gt; &lt;strong&gt;W&lt;/strong&gt; - Additional Writing&lt;/br&gt; &lt;strong&gt;Q&lt;/strong&gt; - Quantitative and Symbolic Reasoning&lt;/br&gt; &lt;strong&gt;VLPA&lt;/strong&gt;  - Visual, Literary, and Performing Arts&lt;/br&gt; &lt;strong&gt;I&amp;S&lt;/strong&gt; - Individuals and Societies&lt;/br&gt; &lt;strong&gt;NW&lt;/strong&gt; - Natural World&lt;/br&gt; &lt;strong&gt;None&lt;/strong&gt; - Does not count towards General Education Requirements',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -158,7 +145,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_REQUISITE_TOOLTOP_COREQ_TEXT','&lt;strong&gt;Prerequisite&lt;/strong&gt; - a course that must be completed satisfactorily before you can take the course in question. &lt;/br&gt; &lt;/br&gt;',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_TERMS_SCHEDULED_SOURCE_NAME','Time Schedule',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -171,7 +158,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_REQUISITE_TOOLTOP_PREREQ_TEXT','&lt;strong&gt;Co-requisite&lt;/strong&gt; - a course that must be taken at the same time as the course in question.Co-requisites are sometimes listed in the Time Schedule as &quot;concurrent registration required in....&quot;',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_REQUISITE_TOOLTOP_COREQ_TEXT','&lt;strong&gt;Prerequisite&lt;/strong&gt; - a course that must be completed satisfactorily before you can take the course in question. &lt;/br&gt; &lt;/br&gt;',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -184,7 +171,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_TERMS_TOOLTIP_TEXT','Projected courses indicate which term the course might be offered next. Contact the department for verification. Scheduled courses appear in the current Time Schedule.',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_REQUISITE_TOOLTOP_PREREQ_TEXT','&lt;strong&gt;Co-requisite&lt;/strong&gt; - a course that must be taken at the same time as the course in question.Co-requisites are sometimes listed in the Time Schedule as &quot;concurrent registration required in....&quot;',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -197,7 +184,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','DETAILS_TERMS_TYPICALLYOFFERED_SOURCE_NAME','Course Catalog',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_TERMS_TOOLTIP_TEXT','Projected courses indicate which term the course might be offered next. Contact the department for verification. Scheduled courses appear in the current Time Schedule.',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -210,7 +197,7 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','RESULTS_CREDITS_FACET_NAME','Credits',
+	        SUBSTR(sys_guid(),21),'en','DETAILS_TERMS_TYPICALLYOFFERED_SOURCE_NAME','Course Catalog',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
@@ -223,7 +210,20 @@ INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
 	        SUBSTR(sys_guid(),17,4) || '-' ||
-	        SUBSTR(sys_guid(),21),'en_US','SEARCH_SEARCHTERMS_PLACEHOLDER_TEXT','e.g. COM, Math 124, ENGL 1XX, sun',
+	        SUBSTR(sys_guid(),21),'en','RESULTS_CREDITS_FACET_NAME','Credits',
+	        SUBSTR(sys_guid(),1,8) || '-' ||
+	        SUBSTR(sys_guid(),9,4) || '-' ||
+	        SUBSTR(sys_guid(),13,4) || '-' ||
+	        SUBSTR(sys_guid(),17,4) || '-' ||
+	        SUBSTR(sys_guid(),21),1)
+/
+
+INSERT INTO KSMG_MESSAGE (GRP_NAME,ID,LOCALE,MSG_ID,MSG_VALUE,OBJ_ID,VER_NBR)
+  VALUES ('myplan',	SUBSTR(sys_guid(),1,8) || '-' ||
+	        SUBSTR(sys_guid(),9,4) || '-' ||
+	        SUBSTR(sys_guid(),13,4) || '-' ||
+	        SUBSTR(sys_guid(),17,4) || '-' ||
+	        SUBSTR(sys_guid(),21),'en','SEARCH_SEARCHTERMS_PLACEHOLDER_TEXT','e.g. COM, Math 124, ENGL 1XX, sun',
 	        SUBSTR(sys_guid(),1,8) || '-' ||
 	        SUBSTR(sys_guid(),9,4) || '-' ||
 	        SUBSTR(sys_guid(),13,4) || '-' ||
