6ad590f1bbfd756f910c2e6185cd61f5a43adfb1
nelson.leduc <nelson.leduc@kuali.org>
2013-11-22 15:42:14 -0500

    KSENROLL-10889 -- avoid NullPointerException
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/2.0.0-Mx@60718 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-api/src/main/java/org/kuali/student/r2/core/scheduling/dto/TimeSlotInfo.java
+++ ks-core-api/src/main/java/org/kuali/student/r2/core/scheduling/dto/TimeSlotInfo.java
@@ -100,12 +100,18 @@ public class TimeSlotInfo extends IdEntityInfo implements TimeSlot, Serializable
     public boolean equals (Object obj) {
         TimeSlotInfo ts = (TimeSlotInfo) obj; // will throw a ClassCastException
         //  To avoid IndexOutOfBoundsExceptions see if the weekdays collections have the same element count before trying to compare the elements.
-        if (this.getWeekdays().size() != ts.getWeekdays().size()) { return false; }
+        if ((this.weekdays == null) || (ts.weekdays == null) || (this.getWeekdays().size() != ts.getWeekdays().size())) {
+            return false;
+        }
         for (int i=0; i<this.weekdays.size(); i++)  {
             if (!this.weekdays.get(i).equals(ts.weekdays.get(i))) { return false; }
         }
-        if (!this.startTime.equals(ts.startTime)) { return false; }
-        if (!this.endTime.equals(ts.endTime)) { return false; }
+        if ((this.startTime == null) || (ts.startTime == null) || (!this.startTime.equals(ts.startTime))) {
+            return false;
+        }
+        if ((this.endTime == null) || (ts.endTime == null) || (!this.endTime.equals(ts.endTime))) {
+            return false;
+        }
         return true;
     }
 
