a965ab1a400830a4d4de2a88119048cd36f40f3a
cclin <cclin@kuali.org>
2013-12-03 12:13:44 -0500

    KSENROLL-11100 Had to partly implement the CourseOfferingServiceMockExtender to pass tests.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@61101 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/extender/CourseOfferingServiceMockExtenderImpl.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/extender/CourseOfferingServiceMockExtenderImpl.java
@@ -38,9 +38,12 @@ import java.util.Map;
  * resources wired in even if it doesn't use it, this mock (which does nothing) helps to allow the
  * wiring to succeed and the unit tests to pass.
  *
+ * Note: copyActivityOffering had to be partly implemented to pass the tests
+ *
  * @author cclin
  */
 public class CourseOfferingServiceMockExtenderImpl implements CourseOfferingServiceExtender {
+
     @Override
     public List<String> getActivityTypesForFormatId(String id, ContextInfo context) throws InvalidParameterException, MissingParameterException, PermissionDeniedException, OperationFailedException {
         throw new UnsupportedOperationException("getActivityTypesForFormatId");
@@ -62,8 +65,24 @@ public class CourseOfferingServiceMockExtenderImpl implements CourseOfferingServ
     }
 
     @Override
-    public ActivityOfferingInfo copyActivityOffering(String operation, ActivityOfferingInfo sourceAo, CourseOfferingService coService, FormatOfferingInfo targetFo, String targetTermId, ContextInfo context, List<String> optionKeys) throws PermissionDeniedException, MissingParameterException, InvalidParameterException, OperationFailedException, DoesNotExistException, ReadOnlyException, DataValidationErrorException {
-        throw new UnsupportedOperationException("copyActivityOffering");
+    public ActivityOfferingInfo copyActivityOffering(String operation,
+                                                     ActivityOfferingInfo sourceAo,
+                                                     CourseOfferingService coService,
+                                                     FormatOfferingInfo targetFo,
+                                                     String targetTermId,
+                                                     ContextInfo context,
+                                                     List<String> optionKeys) throws PermissionDeniedException, MissingParameterException, InvalidParameterException, OperationFailedException, DoesNotExistException, ReadOnlyException, DataValidationErrorException {
+        ActivityOfferingInfo targetAo = new ActivityOfferingInfo(sourceAo);
+        targetAo.setId(null);
+        if (targetFo != null) {
+            targetAo.setFormatOfferingId(targetFo.getId());
+        }
+        if (targetTermId != null) {
+            targetAo.setTermId(targetTermId);
+        }
+        targetAo.getScheduleIds().clear();
+        ActivityOfferingInfo resultAo = coService.createActivityOffering(targetAo.getFormatOfferingId(), targetAo.getActivityId(), targetAo.getTypeKey(), targetAo, context);
+        return resultAo;
     }
 
     @Override
