a47b589bb1553040016b91d332a4d33b7969e210
johglov <johglov@kuali.org>
2013-12-10 12:33:47 -0500

    KSAP-410 - Added override of copy method to the ksap override of the pagegroup.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@61545 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-ui/src/main/java/org/kuali/rice/krad/uif/container/extension/KSAPPageGroup.java
+++ ks-ap-ui/src/main/java/org/kuali/rice/krad/uif/container/extension/KSAPPageGroup.java
@@ -1,6 +1,8 @@
 package org.kuali.rice.krad.uif.container.extension;
 
 import org.kuali.rice.krad.uif.container.PageGroup;
+import org.kuali.rice.krad.uif.util.BreadcrumbItem;
+import org.kuali.rice.krad.uif.util.PageBreadcrumbOptions;
 
 /**
  * Fix under KSAP-265
@@ -8,31 +10,54 @@ import org.kuali.rice.krad.uif.container.PageGroup;
  */
 public class KSAPPageGroup extends PageGroup {
     /**
-    * Wrapper ID for pages intended for use as an inner view.
-    * @see KULRICE-8862
-    */
+     * Wrapper ID for pages intended for use as an inner view.
+     * @see KULRICE-8862
+     */
     private String innerViewWrapperId;
 
     /**
-    * Get the inner view wrapper ID for this page, if this page is intended for use as an inner
-    * view.
-    *
-    * @return The inner view wrapper ID for this page, null if the page is not used as an inner
-    *         view.
-    */
+     * Get the inner view wrapper ID for this page, if this page is intended for use as an inner
+     * view.
+     *
+     * @return The inner view wrapper ID for this page, null if the page is not used as an inner
+     *         view.
+     */
     public String getInnerViewWrapperId() {
         return this.innerViewWrapperId;
     }
 
     /**
-    * Set the inner view wrapper ID for this page, if this page is intended for use as an inner
-    * view.
-    *
-    * @param innerViewWrapperId The inner view wrapper ID for this page, null if the page is not
-    *        used as an inner view.
-    */
+     * Set the inner view wrapper ID for this page, if this page is intended for use as an inner
+     * view.
+     *
+     * @param innerViewWrapperId The inner view wrapper ID for this page, null if the page is not
+     *        used as an inner view.
+     */
     public void setInnerViewWrapperId(String innerViewWrapperId) {
         this.innerViewWrapperId = innerViewWrapperId;
     }
+
+    /**
+     * @see org.kuali.rice.krad.uif.component.ComponentBase#copy()
+     */
+    @Override
+    protected <T> void copyProperties(T component) {
+        super.copyProperties(component);
+
+        KSAPPageGroup pageGroupCopy = (KSAPPageGroup) component;
+
+        pageGroupCopy.setAutoFocus(this.isAutoFocus());
+        pageGroupCopy.setStickyFooter(this.isStickyFooter());
+        pageGroupCopy.setInnerViewWrapperId(this.getInnerViewWrapperId());
+
+        if (this.getBreadcrumbOptions() != null) {
+            pageGroupCopy.setBreadcrumbOptions((PageBreadcrumbOptions) this.getBreadcrumbOptions().copy());
+        }
+
+        if (this.getBreadcrumbItem() != null) {
+            pageGroupCopy.setBreadcrumbItem((BreadcrumbItem) this.getBreadcrumbItem().copy());
+        }
+    }
+
 
 }
