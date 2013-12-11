2f7267b7f8e84003dfa221fa07dcc4d23f3736ea
komandur <komandur@kuali.org>
2013-12-10 19:57:11 -0500

    KSENROLL-11243 Added non-std ts approval to Spring 2013, ENGL 262, AO: A
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@61581 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/RC1/2013-12-10-KSEN_LUI_ATTR-adding-nonstd-ts-indicator.sql
@@ -0,0 +1,3 @@
+-- Adding non-std TS to Spring 2013, ENGL 262, AO: A (KSENROLL-11243)
+insert into KSEN_LUI_ATTR (OBJ_ID, ATTR_KEY, OWNER_ID, ATTR_VALUE, ID) values (SYS_GUID(), 'kuali.attribute.nonstd.ts.indicator', '51625652-76e9-4151-83df-9f6391278949', 'true', SYS_GUID())
+/
