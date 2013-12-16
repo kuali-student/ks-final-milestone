5a6f0f78a1e1624862e7d09f0fc0503eec712b25
johglov <johglov@kuali.org>
2013-12-16 09:11:38 -0500

    KSAP-410 - Implement revision from code review
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@61919 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-ui/src/main/java/org/kuali/rice/krad/uif/container/extension/KSAPPageGroup.java
+++ ks-ap-ui/src/main/java/org/kuali/rice/krad/uif/container/extension/KSAPPageGroup.java
@@ -46,17 +46,7 @@ public class KSAPPageGroup extends PageGroup {
 
         KSAPPageGroup pageGroupCopy = (KSAPPageGroup) component;
 
-        pageGroupCopy.setAutoFocus(this.isAutoFocus());
-        pageGroupCopy.setStickyFooter(this.isStickyFooter());
         pageGroupCopy.setInnerViewWrapperId(this.getInnerViewWrapperId());
-
-        if (this.getBreadcrumbOptions() != null) {
-            pageGroupCopy.setBreadcrumbOptions((PageBreadcrumbOptions) this.getBreadcrumbOptions().copy());
-        }
-
-        if (this.getBreadcrumbItem() != null) {
-            pageGroupCopy.setBreadcrumbItem((BreadcrumbItem) this.getBreadcrumbItem().copy());
-        }
     }
 
 
