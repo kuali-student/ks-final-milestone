66b4bc9dabc89881c0969efeeb49bf7923760044
nwright <nwright@kuali.org>
2014-01-06 15:40:49 -0500

    KSENROLL-11192 - added decorator and test for creating GES values and parameters -- @Ignored test until GES service gets fixed
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-rules-credit-limit-poc@62927 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ src/test/java/org/kuali/student/r2/core/process/ProcessPocGesServiceDecorator.java
@@ -0,0 +1,117 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kuali.student.r2.core.process;
+
+import java.util.Arrays;
+import java.util.List;
+import org.kuali.student.core.constants.GesServiceConstants;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.core.ges.dto.ParameterInfo;
+import org.kuali.student.r2.core.ges.dto.ValueInfo;
+import org.kuali.student.r2.core.ges.service.GesService;
+import org.kuali.student.r2.core.ges.service.GesServiceDecorator;
+
+/**
+ *
+ * @author nwright
+ */
+public class ProcessPocGesServiceDecorator extends GesServiceDecorator {
+
+    public ProcessPocGesServiceDecorator() {
+    }
+
+    public ProcessPocGesServiceDecorator(GesService nextDecorator) {
+        super();
+        this.setNextDecorator(nextDecorator);
+        init();
+    }
+
+    public void init() {
+        ContextInfo contextInfo = new ContextInfo();
+        contextInfo.setPrincipalId("POC-Initializer");
+        _createParam(GesServiceConstants.PARAMETER_KEY_CREDIT_MINIMUM, "Credit Minimum",
+                "Minimum number of credits that a student can register for in a given term",
+                GesServiceConstants.GES_VALUE_TYPE_KEY_KUALIDECIMAL, contextInfo);
+        _createParam(GesServiceConstants.PARAMETER_KEY_CREDIT_LIMIT, "Credit Limit",
+                "Maximum number of credits that a student can register for in a given term",
+                GesServiceConstants.GES_VALUE_TYPE_KEY_KUALIDECIMAL, contextInfo);
+
+        _createValue(GesServiceConstants.PARAMETER_KEY_CREDIT_LIMIT, 1, "6", "kuali.population.everyone", "kuali.atp.type.Summer",
+                contextInfo);
+        _createValue(GesServiceConstants.PARAMETER_KEY_CREDIT_LIMIT, 2, "9", "kuali.population.everyone",
+                "kuali.atp.type.Fall, kuali.atp.type.Spring", contextInfo);
+        _createValue(GesServiceConstants.PARAMETER_KEY_CREDIT_LIMIT, 3, "15", "kuali.population.freshman",
+                "kuali.atp.type.Fall, kuali.atp.type.Spring", contextInfo);
+        _createValue(GesServiceConstants.PARAMETER_KEY_CREDIT_LIMIT, 4, "18", "kuali.population.upperclass",
+                "kuali.atp.type.Fall, kuali.atp.type.Spring", contextInfo);
+        _createValue(GesServiceConstants.PARAMETER_KEY_CREDIT_LIMIT, 5, "18", "kuali.population.law.school.students", "",
+                contextInfo);
+        _createValue(GesServiceConstants.PARAMETER_KEY_CREDIT_LIMIT, 6, "17", "kuali.population.graduate", "", contextInfo);
+
+        _createValue(GesServiceConstants.PARAMETER_KEY_CREDIT_MINIMUM, 1, "12", "kuali.population.athletes",
+                "kuali.atp.type.Fall, kuali.atp.type.Spring", contextInfo);
+        _createValue(GesServiceConstants.PARAMETER_KEY_CREDIT_MINIMUM, 2, "9", "kuali.population.fin.aid.recipients",
+                "kuali.atp.type.Fall, kuali.atp.type.Spring", contextInfo);
+        _createValue(GesServiceConstants.PARAMETER_KEY_CREDIT_MINIMUM, 3, "1", "kuali.population.everyone",
+                "kuali.atp.type.Fall, kuali.atp.type.Spring", contextInfo);
+        _createValue(GesServiceConstants.PARAMETER_KEY_CREDIT_MINIMUM, 4, "0", "kuali.population.everyone",
+                "kuali.atp.type.Summer", contextInfo);
+
+    }
+
+    private ParameterInfo _createParam(String key, String name, String descr, String valueTypeKey, ContextInfo context) {
+
+        ParameterInfo info = new ParameterInfo();
+        info.setKey(key);
+        info.setValueTypeKey(valueTypeKey);
+        info.setTypeKey(GesServiceConstants.GES_PARAMETER_TYPE_KEY);
+        info.setStateKey(GesServiceConstants.GES_PARAMETER_ACTIVE_STATE_KEY);
+        info.setRequireUniquePriorities(true);
+        try {
+            info = this.createParameter(info.getValueTypeKey(), info.getTypeKey(), info.getKey(), info, context);
+        } catch (Exception ex) {
+            throw new RuntimeException("unexpected", ex);
+        }
+        return info;
+    }
+
+    private ValueInfo _createValue(String paramKey, int priority, String value, String populationId, String atpKeys,
+            ContextInfo context) {
+        ValueInfo info = new ValueInfo();
+        info.setParameterId(paramKey);
+        info.setTypeKey(GesServiceConstants.GES_VALUE_TYPE_KEY_KUALIDECIMAL);
+        info.setStateKey(GesServiceConstants.GES_VALUE_ACTIVE_STATE_KEY);
+        info.setPriority(priority);
+        info.setStringValue(_nullIt(value));
+        info.setPopulationId(populationId);
+        // TODO: turn this into a list once the GES service gets fixed
+        info.setAtpTypeKey(atpKeys);
+        try {
+            info = this.createValue(info.getTypeKey(), paramKey, info, context);
+        } catch (Exception ex) {
+            throw new RuntimeException("unexpected", ex);
+        }
+        return info;
+    }
+
+    private List<String> _splitIt(String str) {
+        if (str.trim().isEmpty()) {
+            return null;
+        }
+        String[] strs = str.split(",");
+        for (int i = 0; i < strs.length; i++) {
+            strs[i] = strs[i].trim();
+        }
+        List<String> list = Arrays.asList(strs);
+        return list;
+    }
+
+    private String _nullIt(String str) {
+        if (str.trim().isEmpty()) {
+            return null;
+        }
+        return str;
+    }
+}
---
+++ src/test/java/org/kuali/student/r2/core/process/ProcessPocGesServiceDecoratorTest.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kuali.student.r2.core.process;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.kuali.student.r2.common.dto.ContextInfo;
+//import org.kuali.student.r2.core.acal.service.assembler.AcademicCalendarAssembler;
+//import org.kuali.student.r2.core.acal.service.assembler.TermAssembler;
+//import org.kuali.student.r2.core.acal.service.impl.AcademicCalendarServiceImpl;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Ignore;
+import org.kuali.student.core.constants.GesServiceConstants;
+import org.kuali.student.enrollment.class2.ges.service.impl.GesServiceMapImpl;
+import org.kuali.student.r2.core.ges.dto.ParameterInfo;
+import org.kuali.student.r2.core.ges.dto.ValueInfo;
+import org.kuali.student.r2.core.ges.service.GesService;
+
+/**
+ *
+ * @author nwright
+ */
+@Ignore // undo once the GES service is fixed
+public class ProcessPocGesServiceDecoratorTest {
+
+    public ProcessPocGesServiceDecoratorTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGesMethods() throws Exception {
+        ContextInfo contextInfo = new ContextInfo();
+        contextInfo.setPrincipalId("POC-tester");
+
+        GesService service = new GesServiceMapImpl();
+        service = new ProcessPocGesServiceDecorator(service);
+
+        ParameterInfo creditLimitParameter = null;
+        List<ValueInfo> values = null;
+
+        creditLimitParameter = service.getParameter(GesServiceConstants.PARAMETER_KEY_CREDIT_LIMIT, contextInfo);
+        values = service.getValuesByParameter(GesServiceConstants.PARAMETER_KEY_CREDIT_LIMIT,contextInfo);
+        assertEquals(6, values.size());
+        
+        
+        creditLimitParameter = service.getParameter(GesServiceConstants.PARAMETER_KEY_CREDIT_MINIMUM, contextInfo);
+        values = service.getValuesByParameter(GesServiceConstants.PARAMETER_KEY_CREDIT_MINIMUM,contextInfo);
+        assertEquals(4, values.size());
+    }
+}
