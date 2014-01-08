2b7997e585f496aaa65b019330d0603cc0c2b57a
nwright <nwright@kuali.org>
2014-01-08 11:31:10 -0500

    KSENROLL-11192 - moved cr constants back to LprServiceConstnats
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/trunk@63107 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/LprServiceConstants.java
+++ ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/LprServiceConstants.java
@@ -71,6 +71,16 @@ public class LprServiceConstants {
         INSTRUCTOR_ASSISTANT_TYPE_KEY, INSTRUCTOR_SUPPORT_TYPE_KEY};
     public static final String[] COURSE_STUDENT_TYPE_KEYS = {REGISTRANT_TYPE_KEY};
 
+    
+      // lpr transaction item types (operations)
+    public static final String REQ_ITEM_ADD_TYPE_KEY = "kuali.course.registrration.request.item.type.add";
+    public static final String REQ_ITEM_DROP_TYPE_KEY = "kuali.course.registrration.request.item.type.drop";
+    public static final String REQ_ITEM_SWAP_TYPE_KEY = "kuali.course.registrration.request.item.type.swap";
+    public static final String REQ_ITEM_UPDATE_TYPE_KEY = "kuali.course.registrration.request.item.type.update";
+    
+    public static final String REQ_ITEM_ADD_TO_WAITLIST_TYPE_KEY = "kuali.course.registrration.request.item.type.add.to.waitlist";
+    public static final String REQ_ITEM_ADD_TO_HOLD_UNTIL_LIST_TYPE_KEY = "kuali.course.registrration.request.item.type.add.to.hold.until.list";
+
     /**
      * LprRoster types
      */
