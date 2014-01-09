4950c77f1d4d9efb3b615b883cd7adb01560b775
depstein <depstein@kuali.org>
2014-01-09 13:36:24 -0500

    KSENROLL-11480 added custom json provider that lets you set headers and configured for our services
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@63179 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/client/service/impl/CustomHeaderJacksonJsonProvider.java
@@ -0,0 +1,35 @@
+package org.kuali.student.enrollment.registration.client.service.impl;
+
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+@Provider
+@Consumes({MediaType.APPLICATION_JSON, "text/json"})
+@Produces({MediaType.APPLICATION_JSON, "text/json"})
+public class CustomHeaderJacksonJsonProvider extends JacksonJsonProvider {
+    private Map<String, String> headers;
+
+    @Override
+    public void writeTo(Object o, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            httpHeaders.putSingle(entry.getKey(), entry.getValue());
+        }
+        super.writeTo(o, type, genericType, annotations, mediaType, httpHeaders, entityStream);
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+}
