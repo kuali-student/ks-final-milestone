5c80e19f8c1fd18019cbb93d7ae9ac9edf6346e3
cclin <cclin@kuali.org>
2014-01-09 13:00:13 -0500

    KSENROLL-114278 Added LPR types
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@63176 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/FR2/2014-01-09-AddLprTypesDeleteOldLprTypes.sql
@@ -0,0 +1,10 @@
+--KSENROLL-11478 (cclin)
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,UPDATEID,UPDATETIME,VER_NBR)
+  VALUES ('TESTUSER',TO_DATE( '20140109', 'YYYYMMDD' ),'AO-Registrant LPR type','AO-Registrant LPR type','kuali.lpr.type.registrant.activity.offering','http://student.kuali.org/wsdl/lpr/LprInfo','http://student.kuali.org/wsdl/lpr/LprService','kuali.lpr.type.registrant.activity.offering','TESTUSER',TO_DATE( '20140109', 'YYYYMMDD' ),0)
+/
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,UPDATEID,UPDATETIME,VER_NBR)
+  VALUES ('TESTUSER',TO_DATE( '20140109', 'YYYYMMDD' ),'CO-Registrant LPR type','CO-Registrant LPR type','kuali.lpr.type.registrant.course.offering','http://student.kuali.org/wsdl/lpr/LprInfo','http://student.kuali.org/wsdl/lpr/LprService','kuali.lpr.type.registrant.course.offering','TESTUSER',TO_DATE( '20140109', 'YYYYMMDD' ),0)
+/
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,UPDATEID,UPDATETIME,VER_NBR)
+  VALUES ('TESTUSER',TO_DATE( '20140109', 'YYYYMMDD' ),'RG-Registrant LPR type','RG-Registrant LPR type','kuali.lpr.type.registrant.registration.group','http://student.kuali.org/wsdl/lpr/LprInfo','http://student.kuali.org/wsdl/lpr/LprService','kuali.lpr.type.registrant.registration.group','TESTUSER',TO_DATE( '20140109', 'YYYYMMDD' ),0)
+/
