9837db2833cbe77ed5075f932498d92374b4c3ec
huangb <huangb@kuali.org>
2014-01-13 13:31:22 -0500

    KSAP-549:  Change the type of Academic Plan Item to kuali.academicplan.planitem
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@63345 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-sql/src/main/resources/org/kuali/student/ks-ap-sql/upgrades/M8/2013-10-22-1-kspl-default-plan-settings.sql
+++ ks-ap-sql/src/main/resources/org/kuali/student/ks-ap-sql/upgrades/M8/2013-10-22-1-kspl-default-plan-settings.sql
@@ -15,12 +15,14 @@ INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_
   'kuali.academicplan.type.plan.review',0)
 /
 
-/* Academic Plan Item types */
+/* The type of Academic Plan Item */
 INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
-  VALUES ('SYSTEMLOADER',TO_DATE( '20131107000000', 'YYYYMMDDHH24MISS' ),'Student learning plan course item type.','Student learning plan course item type.','Course Item',
+  VALUES ('SYSTEMLOADER',TO_DATE( '20131107000000', 'YYYYMMDDHH24MISS' ),'Academic plan item type.','Academic plan item type.','Academic Plan Item',
   'http://student.kuali.org/wsdl/acadplan/PlanItemInfo','http://student.kuali.org/wsdl/acadplan/AcademicPlanService',
-  'kuali.academicplan.type.item.course',0)
+  'kuali.academicplan.planitem',0)
 /
+
+/* The type of Term Note */
 INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
   VALUES ('SYSTEMLOADER',TO_DATE( '20131107000000', 'YYYYMMDDHH24MISS' ),'Term Note.','Term Note.','Term Note',
   'http://student.kuali.org/wsdl/comment/CommentInfo','http://student.kuali.org/wsdl/comment/CommnetService',
