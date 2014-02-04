858c911a74b800532d586b8be7de182d645cb524
tbornholtz <tbornholtz@kuali.org>
2014-02-04 16:50:58 -0500

    Very rudimentary error handling when adding a new course
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@64572 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/angular/reg-cart/app/scripts/controllers/cart.js
+++ ks-enroll-ui/src/main/angular/reg-cart/app/scripts/controllers/cart.js
@@ -7,6 +7,17 @@ cartServiceModule.controller('CartCtrl', ['$scope', 'CartService',
     function ($scope, CartService) {
         $scope.cart = CartService.getCart().query({termCode:'Fall2012'});
 
+        $scope.add = function() {
+
+            var code = $scope.courseCode;
+            var regGroup = $scope.regCode;
+
+            $scope.courseCode = "Test Value";
+
+            $scope.error = "Cannot find the course \"" + code + "\" in term " + $scope.termId;
+            $scope.courseCode.setValidity("coursecheck", false);
+        }
+
         $scope.delete = function(index) {
             var course = $scope.cart[index];
 
