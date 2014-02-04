017aba280a68776e96d209acee565e00f9b330ad
tbornholtz <tbornholtz@kuali.org>
2014-02-04 15:55:56 -0500

    Remove a course from the list
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@64567 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/angular/reg-cart/app/scripts/controllers/cart.js
+++ ks-enroll-ui/src/main/angular/reg-cart/app/scripts/controllers/cart.js
@@ -1,7 +1,24 @@
 'use strict';
 
-angular.module('regCartApp')
-    .controller('CartCtrl', ['$scope', 'CartService',
+
+var cartServiceModule = angular.module('regCartApp');
+
+cartServiceModule.controller('CartCtrl', ['$scope', 'CartService',
     function ($scope, CartService) {
         $scope.cart = CartService.getCart().query({termCode:'Fall2012'});
+
+        $scope.delete = function(index) {
+            var course = $scope.cart[index];
+
+            // call the backend service here to persist something
+            // API.delete(course){ id: couse.id }, function (success) {
+            //      $scope.cart.splice(index, 1);
+            // });
+
+            $scope.cart.splice(index, 1);
+        };
+
     }]);
+
+
+
