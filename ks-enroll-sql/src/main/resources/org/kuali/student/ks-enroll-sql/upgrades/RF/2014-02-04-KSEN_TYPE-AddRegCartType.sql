5ef16191769991e2a6e16f01f1691afa5393762f
cclin <cclin@kuali.org>
2014-02-04 11:42:01 -0500

    KSENROLL-11784.  Committed reg request item types that Norm put in constants file and deleted older versions of same types
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@64545 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/RF/2014-02-04-KSEN_TYPE-AddRegCartType.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/RF/2014-02-04-KSEN_TYPE-AddRegCartType.sql
@@ -4,3 +4,28 @@ INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_
   VALUES ('SYSTEMLOADER',TO_DATE( '20140204', 'YYYYMMDD' ),'Reg cart type','Reg cart type','Reg cart type','http://student.kuali.org/wsdl/lpr/LprTransactionInfo',
   'http://student.kuali.org/wsdl/lpr/LprService','kuali.lpr.trans.type.registration.cart',0)
 /
+
+-- Delete the ones that exist
+DELETE FROM KSEN_TYPE WHERE TYPE_KEY = 'kuali.lpr.trans.item.type.add'
+/
+
+DELETE FROM KSEN_TYPE WHERE TYPE_KEY = 'kuali.lpr.trans.item.type.drop'
+/
+
+-- Put in add, drop, update, swap
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
+  VALUES ('SYSTEMLOADER',TO_DATE( '20140204', 'YYYYMMDD' ),'Reg request item ADD type','Reg request item ADD type','Reg request item ADD type','http://student.kuali.org/wsdl/lpr/LprTransactionItemInfo',
+  'http://student.kuali.org/wsdl/lpr/LprService','kuali.course.registration.request.item.type.add',0)
+/
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
+  VALUES ('SYSTEMLOADER',TO_DATE( '20140204', 'YYYYMMDD' ),'Reg request item DROP type','Reg request item DROP type','Reg request item DROP type','http://student.kuali.org/wsdl/lpr/LprTransactionItemInfo',
+  'http://student.kuali.org/wsdl/lpr/LprService','kuali.course.registration.request.item.type.drop',0)
+/
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
+  VALUES ('SYSTEMLOADER',TO_DATE( '20140204', 'YYYYMMDD' ),'Reg request item UPDATE type','Reg request item UPDATE type','Reg request item UPDATE type','http://student.kuali.org/wsdl/lpr/LprTransactionItemInfo',
+  'http://student.kuali.org/wsdl/lpr/LprService','kuali.course.registration.request.item.type.update',0)
+/
+INSERT INTO KSEN_TYPE (CREATEID,CREATETIME,DESCR_FORMATTED,DESCR_PLAIN,NAME,REF_OBJECT_URI,SERVICE_URI,TYPE_KEY,VER_NBR)
+  VALUES ('SYSTEMLOADER',TO_DATE( '20140204', 'YYYYMMDD' ),'Reg request item SWAP type','Reg request item SWAP type','Reg request item SWAP type','http://student.kuali.org/wsdl/lpr/LprTransactionItemInfo',
+  'http://student.kuali.org/wsdl/lpr/LprService','kuali.course.registration.request.item.type.swap',0)
+/
