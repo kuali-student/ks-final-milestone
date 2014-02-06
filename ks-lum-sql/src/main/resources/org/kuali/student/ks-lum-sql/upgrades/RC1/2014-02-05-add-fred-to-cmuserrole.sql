fda03f963a9b7f40dab081d9a89319f886f07916
vpremcha <vpremcha@kuali.org>
2014-02-05 20:09:42 -0500

    KSCM-1452 - Create Course (Fred) - Fred Needs Create Course Access
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/branches/CM-KRAD-MERGE@64626 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum-sql/src/main/resources/org/kuali/student/ks-lum-sql/upgrades/RC1/2014-02-05-add-fred-to-cmuserrole.sql
@@ -0,0 +1,4 @@
+-- Adding fred to 'Kuali Student CM User' role (KSCM-1452) for now. It needs more analysis which role should fred belong.
+
+INSERT INTO KRIM_ROLE_MBR_T (ACTV_FRM_DT,ACTV_TO_DT,LAST_UPDT_DT,MBR_ID,MBR_TYP_CD,OBJ_ID,ROLE_ID,ROLE_MBR_ID,VER_NBR) VALUES (null,null,sysdate,'fred','P',SYS_GUID(),(select role_id from KRIM_ROLE_T where ROLE_NM='Kuali Student CM User'),KRIM_ROLE_MBR_ID_S.NEXTVAL,1);
+/
