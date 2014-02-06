3d0105267b34713f27653acff0cb99c0798b5dab
depstein <depstein@kuali.org>
2014-02-06 13:40:49 -0500

    KSENROLL-11756, KSENROLL-11757 added comments, added initial update cart method. Fixed some logic.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@64677 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/client/service/CourseRegistrationCartClientService.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/client/service/CourseRegistrationCartClientService.java
@@ -1,12 +1,16 @@
 package org.kuali.student.enrollment.registration.client.service;
 
+import org.kuali.student.enrollment.courseregistration.dto.RegistrationRequestItemInfo;
 import org.kuali.student.enrollment.courseregistration.dto.RegistrationResponseInfo;
 import org.kuali.student.r2.common.exceptions.AlreadyExistsException;
+import org.kuali.student.r2.common.exceptions.DataValidationErrorException;
 import org.kuali.student.r2.common.exceptions.DoesNotExistException;
 import org.kuali.student.r2.common.exceptions.InvalidParameterException;
 import org.kuali.student.r2.common.exceptions.MissingParameterException;
 import org.kuali.student.r2.common.exceptions.OperationFailedException;
 import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
+import org.kuali.student.r2.common.exceptions.ReadOnlyException;
+import org.kuali.student.r2.common.exceptions.VersionMismatchException;
 
 import javax.security.auth.login.LoginException;
 import javax.ws.rs.GET;
@@ -21,12 +25,75 @@ import javax.ws.rs.core.Response;
  */
 public interface CourseRegistrationCartClientService {
 
+    /**
+     * The REST version of submitCart
+     *
+     * @param userId override of principal ID
+     * @param cartId ID of the registrationRequest representing the cart
+     * @return A Response containing the Boolean value of TRUE or a server error response.
+     */
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     @Path("/submitCart")
     public Response submitCartRS(@QueryParam("userId") String userId,
                                  @QueryParam("cartId") String cartId);
 
+    /**
+     * This method takes a cart and calls submit on the course registration service
+     *
+     * @param userId override of principal ID
+     * @param cartId ID of the registrationRequest representing the cart
+     * @return A Response with the Boolean value of TRUE
+     * @throws InvalidParameterException
+     * @throws MissingParameterException
+     * @throws DoesNotExistException
+     * @throws OperationFailedException
+     * @throws PermissionDeniedException
+     * @throws AlreadyExistsException
+     * @throws LoginException
+     */
     public RegistrationResponseInfo submitCart(String userId, String cartId) throws InvalidParameterException, MissingParameterException, DoesNotExistException, OperationFailedException, PermissionDeniedException, AlreadyExistsException, LoginException;
+
+
+    /**
+     * The REST version of updateCartItem
+     *
+     * @param userId     override of principal ID
+     * @param cartId     ID of the registrationRequest representing the cart
+     * @param cartItemId ID of the specific item being updated
+     * @param credits    The numeric string value of credit student registration option. Must convert to KualiDecimal.
+     * @param grading    the RVG key of grading student registration option. (org.kuali.rvg.grading.PassFail, org.kuali.rvg.grading.Letter)
+     * @return Response containing the cart item that was updated or a server error response.
+     */
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("/updateCartItem")
+    public Response updateCartItemRS(@QueryParam("userId") String userId,
+                                     @QueryParam("cartItem") String cartId,
+                                     @QueryParam("cartItemId") String cartItemId,
+                                     @QueryParam("credits") String credits,
+                                     @QueryParam("grading") String grading);
+
+    /**
+     * This method allows users to set credit and grading options on an item in their cart using the course registration
+     * service's update method.
+     *
+     * @param userId     override of principal ID
+     * @param cartId     ID of the registrationRequest representing the cart
+     * @param cartItemId ID of the specific item being updated
+     * @param credits    The numeric string value of credit student registration option. Must convert to KualiDecimal.
+     * @param grading    the RVG key of grading student registration option. (org.kuali.rvg.grading.PassFail, org.kuali.rvg.grading.Letter)
+     * @return The updated cartItem
+     * @throws LoginException
+     * @throws InvalidParameterException
+     * @throws MissingParameterException
+     * @throws DoesNotExistException
+     * @throws OperationFailedException
+     * @throws PermissionDeniedException
+     * @throws DataValidationErrorException
+     * @throws ReadOnlyException
+     * @throws VersionMismatchException
+     */
+    public RegistrationRequestItemInfo updateCartItem(String userId, String cartId, String cartItemId, String credits, String grading) throws LoginException, InvalidParameterException, MissingParameterException, DoesNotExistException, OperationFailedException, PermissionDeniedException, DataValidationErrorException, ReadOnlyException, VersionMismatchException;
 
 }
--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/client/service/impl/CourseRegistrationCartClientServiceImpl.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/client/service/impl/CourseRegistrationCartClientServiceImpl.java
@@ -4,17 +4,23 @@ package org.kuali.student.enrollment.registration.client.service.impl;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.core.api.util.type.KualiDecimal;
 import org.kuali.student.enrollment.courseregistration.dto.CourseRegistrationInfo;
+import org.kuali.student.enrollment.courseregistration.dto.RegistrationRequestInfo;
+import org.kuali.student.enrollment.courseregistration.dto.RegistrationRequestItemInfo;
 import org.kuali.student.enrollment.courseregistration.dto.RegistrationResponseInfo;
 import org.kuali.student.enrollment.courseregistration.service.CourseRegistrationService;
 import org.kuali.student.enrollment.registration.client.service.CourseRegistrationCartClientService;
 import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.exceptions.AlreadyExistsException;
+import org.kuali.student.r2.common.exceptions.DataValidationErrorException;
 import org.kuali.student.r2.common.exceptions.DoesNotExistException;
 import org.kuali.student.r2.common.exceptions.InvalidParameterException;
 import org.kuali.student.r2.common.exceptions.MissingParameterException;
 import org.kuali.student.r2.common.exceptions.OperationFailedException;
 import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
+import org.kuali.student.r2.common.exceptions.ReadOnlyException;
+import org.kuali.student.r2.common.exceptions.VersionMismatchException;
 import org.kuali.student.r2.common.util.ContextUtils;
 import org.kuali.student.r2.common.util.constants.CourseRegistrationServiceConstants;
 
@@ -35,7 +41,8 @@ public class CourseRegistrationCartClientServiceImpl implements CourseRegistrati
         Response.ResponseBuilder response;
 
         try {
-            response = Response.ok(submitCart(userId, cartId));
+            submitCart(userId, cartId);
+            response = Response.ok(Boolean.TRUE);
         } catch (Throwable t) {
             LOGGER.warn(t);
             response = Response.serverError().entity(t.getMessage());
@@ -46,6 +53,68 @@ public class CourseRegistrationCartClientServiceImpl implements CourseRegistrati
 
     @Override
     public RegistrationResponseInfo submitCart(String userId, String cartId) throws InvalidParameterException, MissingParameterException, DoesNotExistException, OperationFailedException, PermissionDeniedException, AlreadyExistsException, LoginException {
+        ContextInfo contextInfo = getContextAndCheckLogin(userId);
+
+        //Make sure that the user is the owner of the cart!
+        CourseRegistrationInfo courseRegistrationInfo = getCourseRegistrationService().getCourseRegistration(cartId, contextInfo);
+        if (!StringUtils.equals(courseRegistrationInfo.getPersonId(), contextInfo.getPrincipalId())) {
+            throw new PermissionDeniedException("User does not have permission to submit on this registration cart");
+        }
+
+        //Call submit on the registration service
+        RegistrationResponseInfo registrationResponse = getCourseRegistrationService().submitRegistrationRequest(cartId, contextInfo);
+
+        return registrationResponse;
+    }
+
+    @Override
+    public Response updateCartItemRS(String userId, String cartId, String cartItemId, String credits, String grading) {
+        Response.ResponseBuilder response;
+
+        try {
+            //This will need to be changed to the cartItemResponse object in the future!
+            response = Response.ok(updateCartItem(userId, cartId, cartItemId, credits, grading));
+        } catch (Throwable t) {
+            LOGGER.warn(t);
+            response = Response.serverError().entity(t.getMessage());
+        }
+
+        return response.build();
+    }
+
+    @Override
+    //This will need to be changed to the cartItemResponse object in the future!
+    public RegistrationRequestItemInfo updateCartItem(String userId, String cartId, String cartItemId, String credits, String grading) throws LoginException, InvalidParameterException, MissingParameterException, DoesNotExistException, OperationFailedException, PermissionDeniedException, DataValidationErrorException, ReadOnlyException, VersionMismatchException {
+        ContextInfo contextInfo = getContextAndCheckLogin(userId);
+
+        //Get the Cart from services
+        RegistrationRequestInfo cart = getCourseRegistrationService().getRegistrationRequest(cartId, contextInfo);
+
+        //Check that it is the users' cart
+        if (!StringUtils.equals(cart.getRequestorId(), contextInfo.getPrincipalId())) {
+            throw new PermissionDeniedException("User does not have permission to edit items on this registration cart");
+        }
+
+        //Find the matching cartItem id to edit the registration options
+        for (RegistrationRequestItemInfo requestItem : cart.getRegistrationRequestItems()) {
+            if (StringUtils.equals(cartItemId, requestItem.getId())) {
+                //Set the Item registration options
+                requestItem.setCredits(new KualiDecimal(credits));
+                requestItem.setGradingOptionId(grading);
+                //Save the newly updated cart
+                RegistrationRequestInfo updatedCart = getCourseRegistrationService().updateRegistrationRequest(cartId, cart, contextInfo);
+                //Look for the updated request item to return
+                for (RegistrationRequestItemInfo updatedRequestItem : updatedCart.getRegistrationRequestItems()) {
+                    if (StringUtils.equals(cartItemId, updatedRequestItem.getId())) {
+                        return updatedRequestItem;
+                    }
+                }
+            }
+        }
+        throw new DoesNotExistException("No matching cart item was found.");
+    }
+
+    private ContextInfo getContextAndCheckLogin(String userId) throws LoginException {
         ContextInfo contextInfo = ContextUtils.createDefaultContextInfo();
 
         if (!StringUtils.isEmpty(userId)) {
@@ -56,16 +125,7 @@ public class CourseRegistrationCartClientServiceImpl implements CourseRegistrati
             throw new LoginException("User must be logged in to access this service");
         }
 
-        //Make sure that the user is the owner of the cart!
-        CourseRegistrationInfo courseRegistrationInfo = getCourseRegistrationService().getCourseRegistration(cartId, contextInfo);
-        if(!StringUtils.equals(courseRegistrationInfo.getPersonId(), contextInfo.getPrincipalId())){
-            throw new PermissionDeniedException("User does not have permission to submit on this registration cart");
-        }
-
-        //Call submit on the registration service
-        RegistrationResponseInfo registrationResponse = getCourseRegistrationService().submitRegistrationRequest(cartId, contextInfo);
-
-        return registrationResponse;
+        return contextInfo;
     }
 
     public CourseRegistrationService getCourseRegistrationService() {
