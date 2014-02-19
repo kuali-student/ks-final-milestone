2145aa4874b8f59ccbedd34a840c08736cdf0051
lsymms <lsymms@kuali.org>
2014-02-19 13:50:57 -0500

    KSENROLL-11105 Cleaned up Dictionary Validation for tests
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/rice-2.4-mx-upgrade@65285 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-api/src/main/java/org/kuali/student/r2/common/datadictionary/RiceDataDictionaryValidatorImpl.java
+++ ks-common-api/src/main/java/org/kuali/student/r2/common/datadictionary/RiceDataDictionaryValidatorImpl.java
@@ -19,6 +19,7 @@ import java.util.List;
 import org.kuali.rice.krad.datadictionary.DataObjectEntry;
 import org.kuali.rice.krad.datadictionary.validation.result.DictionaryValidationResult;
 import org.kuali.rice.krad.service.DictionaryValidationService;
+import org.kuali.rice.krad.service.KRADServiceLocatorWeb;
 import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.dto.ValidationResultInfo;
 import org.kuali.student.r2.common.exceptions.InvalidParameterException;
@@ -81,7 +82,7 @@ public class RiceDataDictionaryValidatorImpl implements DataDictionaryValidator 
         if (dictEntry == null) {
             throw new OperationFailedException("Dictionary entry for " + entryName + " does not exist");
         }
-        DictionaryValidationResult dvr = this.riceDictionaryValidationService.validate(info,
+        DictionaryValidationResult dvr = this.getRiceDictionaryValidationService().validate(info,
           entryName,
           dictEntry,
           doOptionalProcessing);
