0bdceb2caca9821ed227338ba897fa357ceb0f0b
prasannag <prasannag@kuali.org>
2014-02-20 12:57:14 -0500

    KSCM-1491 - Renamed Kuali Student CM Admin role to Curriculum Specialist
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@65330 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum-sql/src/main/resources/org/kuali/student/ks-lum-sql/upgrades/06-FR2-M1/2014-02-20-rename-role-kuali-student-cm-to-cs.sql
+++ ks-lum-sql/src/main/resources/org/kuali/student/ks-lum-sql/upgrades/06-FR2-M1/2014-02-20-rename-role-kuali-student-cm-to-cs.sql
@@ -1,2 +1,2 @@
-UPDATE KRIM_ROLE_T SET  ROLE_NM='Curriculum Specialist' WHERE NMSPC_CD='KS-SYS' ROLE_NM='Kuali Student CM Admin'
+UPDATE KRIM_ROLE_T SET  ROLE_NM='Curriculum Specialist' WHERE NMSPC_CD='KS-SYS' AND ROLE_NM='Kuali Student CM Admin'
 /
