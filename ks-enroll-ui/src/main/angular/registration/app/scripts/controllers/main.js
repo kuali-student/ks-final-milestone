76312baaa6e08921f67506d241571447a40e2c9b
gtaylor <gtaylor@kuali.org>
2014-02-21 10:27:00 -0500

    KSENROLL-11979: now defaulting to spring 2012.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@65391 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/angular/registration/app/scripts/controllers/main.js
+++ ks-enroll-ui/src/main/angular/registration/app/scripts/controllers/main.js
@@ -8,11 +8,16 @@ angular.module('regCartApp')
         $scope.appUrl = APP_URL.replace('/services/', '/');
 
         $scope.terms = TermsService.query({active:true}, function () {
+            if(!$scope.termId){ // if the termId isn't set, defualt to spring 2012
+                $scope.termId= 'kuali.atp.2012Spring';
+            }
             // default to the current term
+            /* Commenting this out for now. In real world use below
             angular.forEach($scope.terms, function (term) {
                 if (term.currentTerm) {
                     $scope.termId = term.termId;
                 }
             });
+            */
         });
     });
