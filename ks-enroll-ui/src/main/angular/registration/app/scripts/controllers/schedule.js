bf907c51c7a54396506ee7586621c4be03d245bf
gtaylor <gtaylor@kuali.org>
2014-02-21 16:11:23 -0500

    KSENROLL-11978: show credit counts.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@65464 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/angular/registration/app/scripts/controllers/schedule.js
+++ ks-enroll-ui/src/main/angular/registration/app/scripts/controllers/schedule.js
@@ -4,8 +4,34 @@ var cartServiceModule = angular.module('regCartApp');
 
 cartServiceModule.controller('ScheduleCtrl', ['$scope', 'ScheduleService',
     function ($scope, ScheduleService) {
-        $scope.schedules = ScheduleService.getCart().query({ person:'admin' }, function (result) {
+        $scope.schedules = ScheduleService.getCart().query({ userId:'admin', termId:$scope.termId }, function (result) {
             console.log('got schedule data back');
             console.log(result);
+            var creditCount = 0;
+            angular.forEach(result, function (schedule) {
+                angular.forEach(schedule.courseOfferings, function (course) {
+                    console.log(course.credits);
+                    creditCount += parseFloat(course.credits);
+                });
+            });
+
+            $scope.registeredCredits = creditCount;
+            console.log('Registered Credits');
+            console.log($scope.registeredCredits);
         });
+
+           /*
+        $scope.$watch('schedules', function (newSchedules) {
+            console.log('schedule has changed');
+            $scope.registeredCredits = 0;
+            if (newSchedules) {       // TODO: KSENROLL-11755: the first time the page is loaded, this is null. not sure why
+                angular.forEach(newSchedules, function (schedule) {
+                    angular.forEach(schedule.courseOfferings, function (course) {
+                        $scope.registeredCredits += course.credits;
+                    });
+                });
+            }
+        });
+        */
+
     }]);
