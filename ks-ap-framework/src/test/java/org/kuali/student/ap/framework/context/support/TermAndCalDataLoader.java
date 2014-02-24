2ca8b73d350df893237753cafa04f9bdf6ab60de
johglov <johglov@kuali.org>
2014-02-24 16:13:41 -0500

    KSAP-464 - Create unit tests for YearTerm
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@65557 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-framework/src/test/java/org/kuali/student/ap/framework/context/support/TermAndCalDataLoader.java
+++ ks-ap-framework/src/test/java/org/kuali/student/ap/framework/context/support/TermAndCalDataLoader.java
@@ -4,6 +4,8 @@ import org.kuali.student.ap.framework.config.KsapFrameworkServiceLocator;
 import org.kuali.student.enrollment.courseofferingset.dto.SocInfo;
 import org.kuali.student.enrollment.courseofferingset.service.CourseOfferingSetService;
 import org.kuali.student.r2.common.constants.CommonServiceConstants;
+import org.kuali.student.r2.common.dto.RichTextInfo;
+import org.kuali.student.r2.common.exceptions.AlreadyExistsException;
 import org.kuali.student.r2.common.exceptions.DataValidationErrorException;
 import org.kuali.student.r2.common.exceptions.DoesNotExistException;
 import org.kuali.student.r2.common.exceptions.InvalidParameterException;
@@ -14,11 +16,16 @@ import org.kuali.student.r2.common.exceptions.ReadOnlyException;
 import org.kuali.student.r2.common.util.RichTextHelper;
 import org.kuali.student.r2.common.util.constants.CourseOfferingSetServiceConstants;
 import org.kuali.student.r2.common.util.date.DateFormatters;
+import org.kuali.student.r2.common.util.date.KSDateTimeFormatter;
 import org.kuali.student.r2.core.acal.dto.AcademicCalendarInfo;
 import org.kuali.student.r2.core.acal.service.AcademicCalendarService;
 import org.kuali.student.r2.core.atp.dto.AtpInfo;
 import org.kuali.student.r2.core.atp.service.AtpService;
+import org.kuali.student.r2.core.class1.type.dto.TypeInfo;
+import org.kuali.student.r2.core.class1.type.dto.TypeTypeRelationInfo;
+import org.kuali.student.r2.core.class1.type.service.TypeService;
 import org.kuali.student.r2.core.constants.AtpServiceConstants;
+import org.kuali.student.r2.core.constants.TypeServiceConstants;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -35,6 +42,7 @@ public class TermAndCalDataLoader {
     private AtpService atpService;
     private AcademicCalendarService acalService;
     private CourseOfferingSetService courseOfferingSetService;
+    private TypeService typeService;
 
     //private static String principalId = TermAndCalDataLoader.class.getSimpleName();
 
@@ -46,6 +54,14 @@ public class TermAndCalDataLoader {
         this.atpService = atpService;
         this.acalService = acalService;
         this.courseOfferingSetService = courseOfferingSetService;
+        this.typeService = null;
+    }
+
+    public TermAndCalDataLoader(AtpService atpService, AcademicCalendarService acalService, CourseOfferingSetService courseOfferingSetService, TypeService typeService) {
+        this.atpService = atpService;
+        this.acalService = acalService;
+        this.courseOfferingSetService = courseOfferingSetService;
+        this.typeService = typeService;
     }
 
     public void loadData() throws Exception {
@@ -71,6 +87,17 @@ public class TermAndCalDataLoader {
         loadAtp("ksapAtpFuture", "ksapAtpFuture", tomorrow, dayAfterTomorrow, AtpServiceConstants.ATP_FALL_TYPE_KEY, AtpServiceConstants.ATP_OFFICIAL_STATE_KEY, "Next Term");
         loadSoc("ksapAtpFuture");
 
+        loadAtp("kuali.atp.Fall3123", "Fall 3123", DateFormatters.MONTH_DAY_YEAR_DATE_FORMATTER.parse("08/01/3123"), DateFormatters.MONTH_DAY_YEAR_DATE_FORMATTER.parse("12/11/3123"), AtpServiceConstants.ATP_FALL_TYPE_KEY, AtpServiceConstants.ATP_OFFICIAL_STATE_KEY, "Fall 3123");
+        loadAtp("kuali.atp.Spring3123", "Spring 3123", DateFormatters.MONTH_DAY_YEAR_DATE_FORMATTER.parse("02/01/3123"), DateFormatters.MONTH_DAY_YEAR_DATE_FORMATTER.parse("05/11/3123"), AtpServiceConstants.ATP_SPRING_TYPE_KEY, AtpServiceConstants.ATP_OFFICIAL_STATE_KEY, "Spring 3123");
+        loadAtp("kuali.atp.Winter3123", "Winter 3123", DateFormatters.MONTH_DAY_YEAR_DATE_FORMATTER.parse("12/11/3123"), DateFormatters.MONTH_DAY_YEAR_DATE_FORMATTER.parse("01/11/3124"), AtpServiceConstants.ATP_WINTER_TYPE_KEY, AtpServiceConstants.ATP_OFFICIAL_STATE_KEY, "Winter 3123");
+
+        loadTypeTypeRelation(AtpServiceConstants.ATP_TERM_GROUPING_TYPE_KEY, 0, AtpServiceConstants.ATP_FALL_TYPE_KEY,"kuali.type.type.relation.state.active", TypeServiceConstants.TYPE_TYPE_RELATION_GROUP_TYPE_KEY);
+        loadTypeTypeRelation(AtpServiceConstants.ATP_TERM_GROUPING_TYPE_KEY, 0, AtpServiceConstants.ATP_SPRING_TYPE_KEY,"kuali.type.type.relation.state.active", TypeServiceConstants.TYPE_TYPE_RELATION_GROUP_TYPE_KEY);
+        loadTypeTypeRelation(AtpServiceConstants.ATP_TERM_GROUPING_TYPE_KEY, 0, AtpServiceConstants.ATP_WINTER_TYPE_KEY,"kuali.type.type.relation.state.active", TypeServiceConstants.TYPE_TYPE_RELATION_GROUP_TYPE_KEY);
+        loadType("Term Group", "", "http://student.kuali.org/wsdl/atp/AtpInfo", "http://student.kuali.org/wsdl/atp/AtpService", AtpServiceConstants.ATP_TERM_GROUPING_TYPE_KEY);
+        loadType("Fall Term", "", "http://student.kuali.org/wsdl/atp/AtpInfo", "http://student.kuali.org/wsdl/atp/AtpService", AtpServiceConstants.ATP_FALL_TYPE_KEY);
+        loadType("Spring Term", "", "http://student.kuali.org/wsdl/atp/AtpInfo", "http://student.kuali.org/wsdl/atp/AtpService", AtpServiceConstants.ATP_SPRING_TYPE_KEY);
+        loadType("Winter Term", "", "http://student.kuali.org/wsdl/atp/AtpInfo", "http://student.kuali.org/wsdl/atp/AtpService", AtpServiceConstants.ATP_WINTER_TYPE_KEY);
     }
 
 
@@ -127,4 +154,24 @@ public class TermAndCalDataLoader {
         courseOfferingSetService.createSoc(termId, socInfo.getTypeKey(), socInfo, KsapFrameworkServiceLocator.getContext().getContextInfo());
     }
 
+    private void loadTypeTypeRelation(String ownerId, int rank, String relatedId, String state, String type) throws PermissionDeniedException, DataValidationErrorException, InvalidParameterException, ReadOnlyException, OperationFailedException, MissingParameterException, DoesNotExistException {
+        TypeTypeRelationInfo relation = new TypeTypeRelationInfo();
+        relation.setOwnerTypeKey(ownerId);
+        relation.setRank(rank);
+        relation.setRelatedTypeKey(relatedId);
+        relation.setStateKey(state);
+        relation.setTypeKey(type);
+        typeService.createTypeTypeRelation(type, ownerId, relatedId, relation, KsapFrameworkServiceLocator.getContext().getContextInfo());
+
+    }
+
+    private void loadType(String name, String descr, String refObj, String service, String type) throws PermissionDeniedException, DataValidationErrorException, InvalidParameterException, ReadOnlyException, OperationFailedException, MissingParameterException, DoesNotExistException, AlreadyExistsException {
+        TypeInfo relation = new TypeInfo();
+        relation.setName(name);
+        relation.setDescr(new RichTextInfo(descr,descr));
+        relation.setRefObjectUri(refObj);
+        relation.setServiceUri(service);
+        relation.setKey(type);
+        typeService.createType(type, relation, KsapFrameworkServiceLocator.getContext().getContextInfo());
+    }
 }
---
+++ ks-ap-framework/src/test/java/org/kuali/student/ap/framework/context/support/YearTermTest.java
@@ -0,0 +1,81 @@
+package org.kuali.student.ap.framework.context.support;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kuali.student.ap.framework.config.KsapFrameworkServiceLocator;
+import org.kuali.student.ap.framework.context.YearTerm;
+import org.kuali.student.r2.core.constants.AtpServiceConstants;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:termHelper-test-context.xml" })
+@Transactional
+@TransactionConfiguration(transactionManager = "JtaTxManager", defaultRollback = true)
+public class YearTermTest {
+
+    YearTerm testYearTerm;
+
+    @Before
+    public void setUp() throws Throwable {
+        DefaultKsapContext.before("student1");
+        testYearTerm = new DefaultYearTerm("kuali.atp.Fall3123", AtpServiceConstants.ATP_FALL_TYPE_KEY,3123);
+        try {
+            loadData();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @After
+    public void tearDown() throws Throwable {
+        DefaultKsapContext.after();
+    }
+
+    private void loadData() throws Exception {
+        TermAndCalDataLoader loader = new TermAndCalDataLoader(KsapFrameworkServiceLocator.getAtpService(),
+                KsapFrameworkServiceLocator.getAcademicCalendarService(), KsapFrameworkServiceLocator.getCourseOfferingSetService(), KsapFrameworkServiceLocator.getTypeService());
+        loader.loadData();
+    }
+
+    @Test
+    public void testIsThisThingOn() throws Exception {
+        assertNotNull("KSAP Context not found",
+                KsapFrameworkServiceLocator.getContext());
+        assertNotNull("KSAP TermHelper not found",
+                KsapFrameworkServiceLocator.getTermHelper());
+    }
+
+    @Test
+    public void testRetrieveInformation() throws Exception {
+        assertEquals("kuali.atp.Fall3123",testYearTerm.getTermId());
+        assertEquals(AtpServiceConstants.ATP_FALL_TYPE_KEY,testYearTerm.getTermType());
+        assertEquals(3123 , testYearTerm.getYear());
+    }
+
+    @Test
+    public void testCompareTo() throws Exception {
+        YearTerm spring = new DefaultYearTerm("kuali.atp.Spring3123", AtpServiceConstants.ATP_SPRING_TYPE_KEY,3123);
+        YearTerm fall = new DefaultYearTerm("kuali.atp.Fall3123", AtpServiceConstants.ATP_FALL_TYPE_KEY,3123);
+        YearTerm winter = new DefaultYearTerm("kuali.atp.Winter3123", AtpServiceConstants.ATP_WINTER_TYPE_KEY,3123);
+        assertEquals(0, testYearTerm.compareTo(fall));
+        assertEquals(1, testYearTerm.compareTo(spring));
+        assertEquals(-1,testYearTerm.compareTo(winter) );
+    }
+
+    @Test
+    public void testTermNames() throws Exception {
+        assertEquals("Fall 3123",testYearTerm.getLongName());
+        assertEquals("Fall",testYearTerm.getTermName());
+        assertEquals("FA 3123",testYearTerm.getShortName());
+
+    }
+
+}
