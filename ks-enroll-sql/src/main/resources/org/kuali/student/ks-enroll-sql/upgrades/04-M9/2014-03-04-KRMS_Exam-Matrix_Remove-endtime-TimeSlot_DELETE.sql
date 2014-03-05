863214b005f687b0f5e023b4c05a69d5981a101a
peggy <peggy@kuali.org>
2014-03-05 08:28:54 -0500

    KSENROLL-12115 : Fix the reference data for the time slot to remove the EndDate time.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@65985 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/04-M9/2014-03-04-KRMS_Exam-Matrix_Remove-endtime-TimeSlot_DELETE.sql
@@ -0,0 +1,27 @@
+---KSENROLL-12115 Remove start time from timeslot -Fix Ref Data
+UPDATE KRMS_NL_TMPL_T NL
+    SET NL.TMPL = '$weekdays at $startTime'
+  where NL.TYP_ID =
+        (SELECT TYP_ID
+           FROM KRMS_TYP_T
+          WHERE NM = 'kuali.krms.proposition.type.final.exam.timeslot')
+    and NL.NL_USAGE_ID =
+        (SELECT NL_USAGE_ID
+           FROM KRMS_NL_USAGE_T
+          where NM like 'kuali.krms.catalog')
+/        
+UPDATE KRMS_NL_TMPL_T NL
+    SET NL.TMPL = '$weekdays at $startTime'
+  where NL.TYP_ID =
+        (SELECT TYP_ID
+           FROM KRMS_TYP_T
+          WHERE NM = 'kuali.krms.proposition.type.final.exam.timeslot')
+    and NL.NL_USAGE_ID =
+        (SELECT NL_USAGE_ID
+           FROM KRMS_NL_USAGE_T
+          where NM like 'kuali.krms.edit')
+/
+
+DELETE FROM KRMS_TERM_PARM_T T
+ where T.NM = 'kuali.term.parameter.type.timeslot.end'
+/
