58c43fd0a2200b96c7fc3e2baed22565c494c4d0
depstein <depstein@kuali.org>
2014-03-07 09:52:23 -0500

    KSENROLL-11705 added interceptor code to login back in
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@66160 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/angular/registration/app/scripts/services/interceptors.js
+++ ks-enroll-ui/src/main/angular/registration/app/scripts/services/interceptors.js
@@ -8,17 +8,15 @@ angular.module('regCartApp').factory('loginInterceptor', function ($q, $injector
                 console.log('Failed to execute request - trying to login');
                 var LoginService = $injector.get('LoginService');
                 //This should be removed for production. In the future, we should handle logouts in a user-friendly way.
-//                LoginService.logOnAsAdmin().query({userId: 'admin', password: 'admin'}, function () {
-//                    //After logging in, reload the page.
-//                    console.log('Logged in, reloading page.');
-//                    var $state = $injector.get('$state');
-//                    $state.reload();
-//                }, function () {
-//                    //After logging in, reload the page.
-//                    console.log('Not Logged in, reloading page.');
-//                    var $state = $injector.get('$state');
-//                    $state.reload();
-//                });
+                LoginService.logOnAsAdmin().query({userId: 'admin', password: 'admin'}, function () {
+                    //After logging in, reload the page.
+                    console.log('Logged in, reloading page.');
+                    $window.location.reload();
+                }, function () {
+                    //After logging in, reload the page.
+                    console.log('Not Logged in, reloading page.');
+                    $window.location.reload();
+                });
             }
             return $q.reject(rejection);
         }
