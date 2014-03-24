c3e92b5e8bfe8276c20871685ace6c4c2dc3ac26
gtaylor <gtaylor@kuali.org>
2014-03-24 11:16:11 -0400

    KSENROLL-12139: Reverting this change. didn't mean to commit this file.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@66863 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/engine/node/impl/CourseRegistrationVerifyRegRequestNode.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/engine/node/impl/CourseRegistrationVerifyRegRequestNode.java
@@ -2,23 +2,10 @@ package org.kuali.student.enrollment.registration.engine.node.impl;
 
 import org.kuali.student.enrollment.registration.engine.dto.RegistrationRequestEngineMessage;
 import org.kuali.student.enrollment.registration.engine.node.AbstractCourseRegistrationNode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CourseRegistrationVerifyRegRequestNode extends AbstractCourseRegistrationNode<RegistrationRequestEngineMessage, RegistrationRequestEngineMessage> {
-    private static final Logger LOG = LoggerFactory.getLogger(CourseRegistrationVerifyRegRequestNode.class);
-
     @Override
     public RegistrationRequestEngineMessage process(RegistrationRequestEngineMessage message) {
-        try {
-            //long delay =  (System.currentTimeMillis() % 3);
-            long delay =  20;
-            LOG.info(delay + " second simulated delay in verify");
-            Thread.sleep(delay * 1000);
-        } catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
-
         return message;
     }
 }
