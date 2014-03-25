a50fdcfa3d3cae9eecb8eff97d34267266f6aabc
gtaylor <gtaylor@kuali.org>
2014-03-25 10:44:22 -0400

    KSENROLL-12272: added watcher for term changes.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@66924 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/angular/registration/app/scripts/controllers/main.js
+++ ks-enroll-ui/src/main/angular/registration/app/scripts/controllers/main.js
@@ -7,11 +7,21 @@ angular.module('regCartApp')
 
         $scope.appUrl = APP_URL.replace('/services/', '/');
 
+
+        // update the term name if the termId changes
+        $scope.$watch('termId', function (newValue) {
+            if (newValue) {
+                $scope.termName = TermsService.getTermNameForTermId($scope.terms, newValue);
+            }
+        });
+
         $scope.terms = TermsService.getTermsFromServer().query(({active: true}, function (result) {
             $scope.termId = 'kuali.atp.2012Spring';
             TermsService.setTermId($scope.termId);//FIXME Term service is just a service handle, don't put business logic in it!!!
             $scope.termName = TermsService.getTermNameForTermId(result, $scope.termId);
         }));
+
+
 
 
 
