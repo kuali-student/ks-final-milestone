39031c44a99700d900543ea7de60d0d2d4d60fa9
depstein <depstein@kuali.org>
2014-03-28 16:19:29 -0400

    KSENROLL-12314 KSENROLL-12317 KSENROLL-12374 - Initial stub of WL Services
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/2.0.0-Mx@67146 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/LprServiceConstants.java
+++ ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/LprServiceConstants.java
@@ -46,6 +46,9 @@ public class LprServiceConstants {
     public static final String REGISTRANT_CO_TYPE_KEY = "kuali.lpr.type.registrant.course.offering";
     public static final String REGISTRANT_AO_TYPE_KEY = "kuali.lpr.type.registrant.activity.offering";
     public static final String REGISTRANT_RG_TYPE_KEY = "kuali.lpr.type.registrant.registration.group";
+    public static final String WAITLIST_FO_TYPE_KEY = "kuali.lpr.type.waitlist.format.offering";
+    public static final String WAITLIST_AO_TYPE_KEY = "kuali.lpr.type.waitlist.activity.offering";
+    public static final String WAITLIST_RG_TYPE_KEY = "kuali.lpr.type.waitlist.registration.group";
     public static final String ENROLLEE_TYPE_KEY = "kuali.lpr.type.enrollee";
     public static final String ADVISOR_TYPE_KEY = "kuali.lpr.type.advisor";
 
@@ -91,7 +94,10 @@ public class LprServiceConstants {
     public static final String REQ_ITEM_DROP_TYPE_KEY = "kuali.course.registration.request.item.type.drop";
     public static final String REQ_ITEM_SWAP_TYPE_KEY = "kuali.course.registration.request.item.type.swap";
     public static final String REQ_ITEM_UPDATE_TYPE_KEY = "kuali.course.registration.request.item.type.update";
-    
+
+    public static final String REQ_ITEM_DROP_WAITLIST_TYPE_KEY = "kuali.course.registration.request.item.type.drop.waitlist";
+    public static final String REQ_ITEM_UPDATE_WAITLIST_TYPE_KEY = "kuali.course.registration.request.item.type.update.waitlist";
+
     public static final String REQ_ITEM_ADD_TO_WAITLIST_TYPE_KEY = "kuali.course.registration.request.item.type.add.to.waitlist";
     public static final String REQ_ITEM_ADD_TO_HOLD_UNTIL_LIST_TYPE_KEY = "kuali.course.registration.request.item.type.add.to.hold.until.list";
 
