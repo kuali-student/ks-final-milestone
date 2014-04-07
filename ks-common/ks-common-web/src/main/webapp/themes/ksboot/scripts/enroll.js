1f05ed4072998c15430161f94f05ba40372a8200
hossein.aliabadi <hossein.aliabadi@kuali.org>
2014-04-07 13:20:04 -0400

    KSENROLL-112365: No longer getting a calendar picking tool when adding/modifying term dates.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@67642 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-web/src/main/webapp/themes/ksboot/scripts/enroll.js
+++ ks-common/ks-common-web/src/main/webapp/themes/ksboot/scripts/enroll.js
@@ -759,7 +759,7 @@ jQuery(document).on("click", ".dataTable input[type=checkbox]", function() {
 
 /* Set custom font icons for calendar datepickers */
 jQuery(document).on('DOMNodeInserted', 'button.ui-datepicker-trigger', function() {
-    jQuery(this).empty().addClass('btn-link ks-fontello-icon-calendar-1').attr('alt', 'Date picker').attr('value', 'Date picker');
+    jQuery(this).empty().addClass('btn-link ks-fontello-icon-calendar').attr('alt', 'Date picker').attr('value', 'Date picker');
 });
 
 /*
