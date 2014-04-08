24ebc97a145bca499b27177c6863c9bd31468ee9
vgadiyak <vgadiyak@kuali.org>
2014-04-08 14:16:15 -0400

    KSENROLL-12329 : making type of no-WL inactive
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@67712 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/06-FR2-M1/2014-04-07-KSEN_CWL_waitlist_and_max_enroll.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/06-FR2-M1/2014-04-07-KSEN_CWL_waitlist_and_max_enroll.sql
@@ -1,6 +1,6 @@
 -- KSENROLL-12329
 -- 201208 (Fall 2012) ENGL101 (1009) - no WL
-update KSEN_CWL set MAX_SIZE = 0 where ID = (select CWL_ID from KSEN_CWL_ACTIV_OFFER where ACTIV_OFFER_ID = '4de6b9e2-57af-4eb4-8f69-6c83c2074c58')
+update KSEN_CWL set CWL_STATE = 'kuali.course.waitlist.state.inactive' where ID = (select CWL_ID from KSEN_CWL_ACTIV_OFFER where ACTIV_OFFER_ID = '4de6b9e2-57af-4eb4-8f69-6c83c2074c58')
 /
 -- 201208 (Fall 2012) ENGL101 (1001) - max enroll for AO = 1
 update KSEN_LUI set MAX_SEATS = 1, MIN_SEATS = 1 where ID = '35828d6a-8adb-491a-9861-c06f12de963e'
