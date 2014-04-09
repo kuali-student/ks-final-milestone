1ec4d06e6478fb8a98db76a8eea6c0531b1a5609
davpoind <davpoind@kuali.org>
2014-04-09 18:28:41 -0400

    KSAP-993 - (CS 13) Course Details / Implement redesign of Course Overview section of Course Details screen
    - Adding course details back link knows where you came from
    - Adding positioning of back link container
    - Setting up default state when referrer doesn't match expected pattern
    
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@67840 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-web/src/main/webapp/ks-ap/scripts/ksap.search.details.js
+++ ks-ap-web/src/main/webapp/ks-ap/scripts/ksap.search.details.js
@@ -4,3 +4,16 @@ jQuery(function(){
         sessionStorage.setItem('back_search', sessionStorage.getItem('last_search'));
     }
 });
+
+function detectReferrerForBackLinkText() {
+    var backLink = jQuery('.uif-link.ks-fontello-icon-left-dir.cdp-back-link:visible');
+    var referrer = document.referrer.split('/')[5].split('?')[0];
+    if ( referrer == "course" ) {
+        //from course search, change link text
+        backLink.text('Return to search results');
+    } else if ( referrer == "planner" ) {
+        //from planner, change link test
+        backLink.text('Return to planner');
+    }
+
+}
