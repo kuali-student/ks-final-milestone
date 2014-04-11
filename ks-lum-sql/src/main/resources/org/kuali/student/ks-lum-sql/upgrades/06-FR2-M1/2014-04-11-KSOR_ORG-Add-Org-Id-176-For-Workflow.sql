dbb48db48602b199cc52c43c50a1763c4d8a336d
komandur <komandur@kuali.org>
2014-04-11 18:46:47 -0400

    KSCM-1545: Add missing org id '176' so the workflow may go through
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@68002 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum-sql/src/main/resources/org/kuali/student/ks-lum-sql/upgrades/06-FR2-M1/2014-04-11-KSOR_ORG-Add-Org-Id-176-For-Workflow.sql
@@ -0,0 +1,4 @@
+-- KSCM-1545: This is to add the missing org id 176 currently hardcoded and used in the workflow
+insert into KSOR_ORG (ID, CREATEID, CREATETIME, UPDATEID, UPDATETIME, VER_NBR, SHRT_DESCR, LNG_DESCR, EFF_DT, EXPIR_DT, LNG_NAME, SHRT_NAME, ST, TYPE)
+values ('176', 'GREENTEAM', TO_DATE( '20140411000000', 'YYYYMMDDHH24MISS' ), 'GREENTEAM', TO_DATE( '20140411000000', 'YYYYMMDDHH24MISS' ), 1, '', 'Publication Office', TO_DATE( '20000101000000', 'YYYYMMDDHH24MISS' ), null, 'Publication Office', 'PublicationOffice', 'Active', 'kuali.org.Office');
+/
