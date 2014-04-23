35ffd67d926d6d686099ad0e7c342a63621d71d3
depstein <depstein@kuali.org>
2014-04-23 16:25:11 -0400

    KSENROLL-12527 used constants for SYSTEM user per review
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@68523 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/engine/node/impl/CourseRegistrationWaitlistManagerNode.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/engine/node/impl/CourseRegistrationWaitlistManagerNode.java
@@ -5,19 +5,23 @@ import org.kuali.student.enrollment.courseregistration.infc.RegistrationRequest;
 import org.kuali.student.enrollment.registration.engine.node.AbstractCourseRegistrationNode;
 import org.kuali.student.enrollment.registration.engine.service.WaitlistManagerService;
 import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.util.constants.KimIdentityServiceConstants;
 
 import java.util.List;
 
 /**
- * Created by Daniel on 4/16/14.
+ * Node that processes people off of the waitlist. This uses the waitlist manager service
+ * to get a list of registration requests and sends them to the registration engine for processing off of the waitlist.
  */
 public class CourseRegistrationWaitlistManagerNode extends AbstractCourseRegistrationNode<List<String>, List<RegistrationRequest>> {
     private WaitlistManagerService waitlistManagerService;
+
     @Override
     public List<RegistrationRequest> process(List<String> message) {
+        //Set system user in context since this is a system task
         ContextInfo context = ContextUtils.createDefaultContextInfo();
-        context.setPrincipalId("SYSTEM");//Need some system user here
-        context.setAuthenticatedPrincipalId("SYSTEM");//Need some system user here
+        context.setPrincipalId(KimIdentityServiceConstants.SYSTEM_ENTITY_TYPE_KEY);
+        context.setAuthenticatedPrincipalId(KimIdentityServiceConstants.SYSTEM_ENTITY_TYPE_KEY);
         try {
             return waitlistManagerService.processPeopleOffOfWaitlist(message, context);
         } catch (Exception e) {
