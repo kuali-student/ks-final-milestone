640e75cb7c41928e906b1ae8bac0819341249ea5
rohitrdesh <rohitrdesh@kuali.org>
2014-04-24 12:10:06 -0400

    KSCM-1856 committing the changes.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@68565 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-cm-krad/src/main/java/org/kuali/student/cm/course/service/impl/CourseInfoMaintainableImpl.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/course/service/impl/CourseInfoMaintainableImpl.java
@@ -873,6 +873,19 @@ public class CourseInfoMaintainableImpl extends RuleEditorMaintainableImpl imple
         if (courseInfoWrapper.getCollaboratorWrappers().isEmpty()) {
             courseInfoWrapper.getCollaboratorWrappers().add(new CollaboratorWrapper());
         }
+        // Initialize CrossListings
+        if (courseInfoWrapper.getCourseInfo().getCrossListings().isEmpty()) {
+            courseInfoWrapper.getCourseInfo().getCrossListings().add(new CourseCrossListingInfo());
+        }
+        // Initialize Variations
+        if (courseInfoWrapper.getCourseInfo().getVariations().isEmpty()) {
+            courseInfoWrapper.getCourseInfo().getVariations().add(new CourseVariationInfo());
+        }
+        // Initialize CourseJointWrapper
+        if (courseInfoWrapper.getCourseJointWrappers().isEmpty()) {
+            courseInfoWrapper.getCourseJointWrappers().add(new CourseJointInfoWrapper());
+        }
+
 
         if (requestParameters.get(CourseController.URL_PARAM_USE_CURRICULUM_REVIEW) != null &&
                 requestParameters.get(CourseController.URL_PARAM_USE_CURRICULUM_REVIEW).length != 0) {
