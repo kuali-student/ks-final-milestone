c6cb98a99834b478949fb43e6b5dac318ca50b51
eswaran.muthu <eswaran.muthu@kuali.org>
2014-04-29 14:22:17 -0400

    KSCM-858 Course Information - Advanced Search does not display 'No Results Found' text when no results for search criteria are found
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@68827 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-cm-krad/src/main/java/org/kuali/student/cm/common/util/CurriculumManagementConstants.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/common/util/CurriculumManagementConstants.java
@@ -117,6 +117,7 @@ public class CurriculumManagementConstants {
         public final static String ERROR_NO_RESULTS_FOUND = "error.search.result.notfound";
         public final static String ERROR_DATA_NOT_FOUND = "error.cm.course.data.notfound";
         public final static String ERROR_DATA_MULTIPLE_MATCH_FOUND = "error.cm.course.data.multiplematch";
+        public final static String ERROR_NO_RESULT_FOUND           = "error.no.result.found";
 
     }
 
--- ks-cm-krad/src/main/java/org/kuali/student/cm/course/service/impl/CourseJointInfoLookupableImpl.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/course/service/impl/CourseJointInfoLookupableImpl.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2005-2013 The Kuali Foundation
- * 
+ *
  * Licensed under the Educational Community License, Version 1.0 (the
  * "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl1.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -17,12 +17,14 @@ package org.kuali.student.cm.course.service.impl;
 
 import org.apache.commons.lang.StringUtils;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
-import org.kuali.rice.krad.lookup.LookupableImpl;
 import org.kuali.rice.krad.lookup.LookupForm;
+import org.kuali.rice.krad.lookup.LookupableImpl;
+import org.kuali.rice.krad.util.GlobalVariables;
+import org.kuali.student.cm.common.util.CurriculumManagementConstants;
 import org.kuali.student.cm.course.form.CourseJointInfoWrapper;
+import org.kuali.student.common.util.security.ContextUtils;
 import org.kuali.student.lum.lu.ui.course.keyvalues.CourseJointKeyValuesFinder.SearchByKeys;
 import org.kuali.student.r2.common.dto.RichTextInfo;
-import org.kuali.student.common.util.security.ContextUtils;
 import org.kuali.student.r2.core.search.dto.SearchParamInfo;
 import org.kuali.student.r2.core.search.dto.SearchRequestInfo;
 import org.kuali.student.r2.core.search.dto.SearchResultCellInfo;
@@ -41,23 +43,23 @@ import java.util.Map;
 
 public class CourseJointInfoLookupableImpl extends LookupableImpl {
 
-	private static final long serialVersionUID = 730938705006420306L;
+    private static final long serialVersionUID = 730938705006420306L;
     private static final Logger LOG = LoggerFactory.getLogger(CourseJointInfoLookupableImpl.class);
 
 
     private CluService cluService;
-	
-	@Override
-	public List<?> performSearch(LookupForm form,
-			Map<String, String> searchCriteria, boolean bounded) {
-		List<CourseJointInfoWrapper> courseJointInfoDisplays = new ArrayList<CourseJointInfoWrapper>();
-		
+
+    @Override
+    public List<?> performSearch(LookupForm form,
+                                 Map<String, String> searchCriteria, boolean bounded) {
+        List<CourseJointInfoWrapper> courseJointInfoDisplays = new ArrayList<CourseJointInfoWrapper>();
+
         List<SearchParamInfo> queryParamValueList = new ArrayList<SearchParamInfo>();
         SearchByKeys searchByKey = SearchByKeys.valueOf(searchCriteria.get(CourseServiceConstants.SEARCHBY_SEARCH));
         String courseTitle = searchCriteria.get(CourseServiceConstants.COURSETITLE_SEARCH);
         String subjectArea = searchCriteria.get(CourseServiceConstants.COURSECODE_SEARCH);
         String description = searchCriteria.get(CourseServiceConstants.DESCR_PLAIN_SEARCH);
-        
+
         if (StringUtils.isNotBlank(courseTitle)) {
             SearchParamInfo courseTitleParam = new SearchParamInfo();
             courseTitleParam.setKey(CourseServiceConstants.OPTIONAL_LONGNAME_PARAM);
@@ -76,47 +78,47 @@ public class CourseJointInfoLookupableImpl extends LookupableImpl {
             descriptionParam.getValues().add(description);
             queryParamValueList.add(descriptionParam);
         }
-        
+
         SearchParamInfo typeParam = new SearchParamInfo();
         typeParam.setKey(CourseServiceConstants.OPTIONAL_TYPE_PARAM);
         typeParam.getValues().add(CluServiceConstants.CREDIT_COURSE_LU_TYPE_KEY);
-        
+
         SearchParamInfo stateParam = new SearchParamInfo();
         stateParam.setKey(CourseServiceConstants.OPTIONAL_STATE_PARAM);
         List<String> states = new ArrayList<String>();
         switch (searchByKey) {
-	        case COURSES_AND_PROPOSALS:
-	        	states.add("Draft");
-	            states.add("Submitted");
-	            states.add("Withdrawn");
-	            states.add("Approved");
-	            states.add("Active");
-	            break;
-	        case COURSES_ONLY:
-	        	states.add("Active");
-	        	states.add("Approved");
-	        	states.add("Retired");
-	        	states.add("Suspended");
-	        	break;
-	        case PROPOSALS_ONLY:
-	        	states.add("Draft");
-    			states.add("Submitted");
-				states.add("Withdrawn");
-				states.add("Approved");
+            case COURSES_AND_PROPOSALS:
+                states.add("Draft");
+                states.add("Submitted");
+                states.add("Withdrawn");
+                states.add("Approved");
+                states.add("Active");
+                break;
+            case COURSES_ONLY:
+                states.add("Active");
+                states.add("Approved");
+                states.add("Retired");
+                states.add("Suspended");
+                break;
+            case PROPOSALS_ONLY:
+                states.add("Draft");
+                states.add("Submitted");
+                states.add("Withdrawn");
+                states.add("Approved");
         }
-        
+
         stateParam.setValues(states);
         queryParamValueList.add(stateParam);
 
         String searchKey = "";
         SearchRequestInfo searchRequest = new SearchRequestInfo();
         switch (searchByKey) {
-        	case COURSES_AND_PROPOSALS:
-        		searchKey = CourseServiceConstants.GENERIC_LU_SEARCH;
-        		break;
-        	case COURSES_ONLY:
-        	case PROPOSALS_ONLY:
-        		searchKey = CourseServiceConstants.MOSTCURRENT_UNION_SEARCH;
+            case COURSES_AND_PROPOSALS:
+                searchKey = CourseServiceConstants.GENERIC_LU_SEARCH;
+                break;
+            case COURSES_ONLY:
+            case PROPOSALS_ONLY:
+                searchKey = CourseServiceConstants.MOSTCURRENT_UNION_SEARCH;
         }
         searchRequest.setSearchKey(searchKey);
         searchRequest.setParams(queryParamValueList);
@@ -128,31 +130,34 @@ public class CourseJointInfoLookupableImpl extends LookupableImpl {
                 CourseJointInfoWrapper courseJointInfoDisplay = new CourseJointInfoWrapper();
                 for (SearchResultCellInfo cell : cells) {
                     if (CourseServiceConstants.ID_RESULT.equals(cell.getKey())) {
-                    	courseJointInfoDisplay.setCourseId(cell.getValue());
+                        courseJointInfoDisplay.setCourseId(cell.getValue());
                     } else if (CourseServiceConstants.LONGNAME_OPTIONAL_RESULT.equals(cell.getKey())) {
-                    	courseJointInfoDisplay.setName(cell.getValue());
+                        courseJointInfoDisplay.setName(cell.getValue());
                     } else if (CourseServiceConstants.OPTIONALCODE_RESULT.equals(cell.getKey())) {
-                    	courseJointInfoDisplay.setCourseCode(cell.getValue());
-                    }
-                    else if (CourseServiceConstants.DESCR_RESULT.equals(cell.getKey())) {
-                    	RichTextInfo descr = new RichTextInfo();
-                    	descr.setPlain(cell.getValue());
-                    	courseJointInfoDisplay.setDescr(descr);
+                        courseJointInfoDisplay.setCourseCode(cell.getValue());
+                    } else if (CourseServiceConstants.DESCR_RESULT.equals(cell.getKey())) {
+                        RichTextInfo descr = new RichTextInfo();
+                        descr.setPlain(cell.getValue());
+                        courseJointInfoDisplay.setDescr(descr);
                     }
                 }
                 courseJointInfoDisplays.add(courseJointInfoDisplay);
             }
+            if (courseJointInfoDisplays.size() == 0) {
+                GlobalVariables.getMessageMap().putError("Jointly-offered-courses", CurriculumManagementConstants.MessageKeys.ERROR_NO_RESULT_FOUND);
+            }
+
         } catch (Exception e) {
             LOG.error("An error occurred retrieving the courseJointInfoDisplay", e);
         }
         return courseJointInfoDisplays;
-	}	
-	
-	private CluService getCluService() {
-		if (cluService == null) {
-			cluService = GlobalResourceLoader.getService(new QName(CluServiceConstants.CLU_NAMESPACE, CluService.class.getSimpleName()));
-		}
-		return cluService;
-	}
+    }
+
+    private CluService getCluService() {
+        if (cluService == null) {
+            cluService = GlobalResourceLoader.getService(new QName(CluServiceConstants.CLU_NAMESPACE, CluService.class.getSimpleName()));
+        }
+        return cluService;
+    }
 
 }
