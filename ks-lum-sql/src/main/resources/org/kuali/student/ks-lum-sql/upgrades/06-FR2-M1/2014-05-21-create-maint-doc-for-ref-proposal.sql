a115dfd745cdefe6d655251f5ee29822a1b803ad
vpremcha <vpremcha@kuali.org>
2014-05-21 17:03:00 -0400

    KSCM-1933 - Fix bad ref data in KS Proposal table
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@69744 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum-sql/src/main/resources/org/kuali/student/ks-lum-sql/upgrades/06-FR2-M1/2014-05-21-create-maint-doc-for-ref-proposal.sql
+++ ks-lum-sql/src/main/resources/org/kuali/student/ks-lum-sql/upgrades/06-FR2-M1/2014-05-21-create-maint-doc-for-ref-proposal.sql
@@ -16,11 +16,11 @@ insert into KRNS_MAINT_DOC_T (DOC_HDR_ID,OBJ_ID,VER_NBR) VALUES ('3013',SYS_GUID
 insert into KRNS_MAINT_DOC_T (DOC_HDR_ID,OBJ_ID,VER_NBR) VALUES ('3014',SYS_GUID(),1)
 /
 -- Update ref proposal to use the workflow/maintenace doc id.
-update KREW_DOC_HDR_T set DOC_HDR_ID='3011' where DOC_HDR_ID='d4d0b3ce-8bb4-41c1-a629-ae26f1bd1262'
+update KREW_DOC_HDR_T set DOC_HDR_ID='3011' where DOC_HDR_ID='KS-KREW-DOC-HDR-1000'
 /
-update KREW_DOC_HDR_T set DOC_HDR_ID='3012' where DOC_HDR_ID='20dc6b94-1d0a-4712-85ca-5846c7e82ca6'
+update KREW_DOC_HDR_T set DOC_HDR_ID='3012' where DOC_HDR_ID='KS-KREW-DOC-HDR-1001'
 /
-update KREW_DOC_HDR_T set DOC_HDR_ID='3013' where DOC_HDR_ID='d8b6cb16-0c95-4994-925d-b8061afb4d27'
+update KREW_DOC_HDR_T set DOC_HDR_ID='3013' where DOC_HDR_ID='KS-KREW-DOC-HDR-1002'
 /
-update KREW_DOC_HDR_T set DOC_HDR_ID='3014' where DOC_HDR_ID='cee1f753-a3d8-49b8-a1f7-12fbf62f7226'
+update KREW_DOC_HDR_T set DOC_HDR_ID='3014' where DOC_HDR_ID='KS-KREW-DOC-HDR-1003'
 /
