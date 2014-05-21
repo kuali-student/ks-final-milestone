39d4e7008273f4e74f50acdb2d94b5f9c3e9b211
vpremcha <vpremcha@kuali.org>
2014-05-21 16:46:46 -0400

    KSCM-1933 - Fix bad ref data in KS Proposal table
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@69742 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum-sql/src/main/resources/org/kuali/student/ks-lum-sql/upgrades/06-FR2-M1/2014-05-21-create-maint-doc-for-ref-proposal.sql
+++ ks-lum-sql/src/main/resources/org/kuali/student/ks-lum-sql/upgrades/06-FR2-M1/2014-05-21-create-maint-doc-for-ref-proposal.sql
@@ -7,7 +7,6 @@ insert into KRNS_DOC_HDR_T (DOC_HDR_ID,FDOC_DESC,OBJ_ID,VER_NBR) VALUES('3013','
 /
 insert into KRNS_DOC_HDR_T (DOC_HDR_ID,FDOC_DESC,OBJ_ID,VER_NBR) VALUES('3014','Modify: Biology of Cancer',SYS_GUID(),1)
 /
-
 insert into KRNS_MAINT_DOC_T (DOC_HDR_ID,OBJ_ID,VER_NBR) VALUES ('3011',SYS_GUID(),1)
 /
 insert into KRNS_MAINT_DOC_T (DOC_HDR_ID,OBJ_ID,VER_NBR) VALUES ('3012',SYS_GUID(),1)
@@ -15,7 +14,7 @@ insert into KRNS_MAINT_DOC_T (DOC_HDR_ID,OBJ_ID,VER_NBR) VALUES ('3012',SYS_GUID
 insert into KRNS_MAINT_DOC_T (DOC_HDR_ID,OBJ_ID,VER_NBR) VALUES ('3013',SYS_GUID(),1)
 /
 insert into KRNS_MAINT_DOC_T (DOC_HDR_ID,OBJ_ID,VER_NBR) VALUES ('3014',SYS_GUID(),1)
-
+/
 -- Update ref proposal to use the workflow/maintenace doc id.
 update KREW_DOC_HDR_T set DOC_HDR_ID='3011' where DOC_HDR_ID='d4d0b3ce-8bb4-41c1-a629-ae26f1bd1262'
 /
