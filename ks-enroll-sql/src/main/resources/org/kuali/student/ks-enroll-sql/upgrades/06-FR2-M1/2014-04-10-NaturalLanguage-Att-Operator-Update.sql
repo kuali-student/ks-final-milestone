b5a4e14448adb5f75511c241dfa096b39bc78988
dirkjlouw <dirkjlouw@kuali.org>
2014-05-28 14:45:18 -0400

    KSENROLL-13037:REVERT wrong place commit SQL addition
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@69995 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/06-FR2-M1/2014-04-10-NaturalLanguage-Att-Operator-Update.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/06-FR2-M1/2014-04-10-NaturalLanguage-Att-Operator-Update.sql
@@ -6,7 +6,7 @@ UPDATE KRMS_NL_TMPL_ATTR_T TMP
  WHERE tmp.attr_defn_id = 'KS-KRMS-ATTR-DERN-10007'
    and tmp.nl_tmpl_attr_id = 'KS-KRMS-NL-TMPL-ATTR-10000'
    and tmp.nl_tmpl_id = 'KS-KRMS-NL-TMPL-55755'
-   /
+/
 
 --adding plain OR operator
    UPDATE KRMS_NL_TMPL_ATTR_T TMP
@@ -14,10 +14,4 @@ UPDATE KRMS_NL_TMPL_ATTR_T TMP
  WHERE tmp.attr_defn_id = 'KS-KRMS-ATTR-DERN-10007'
    and tmp.nl_tmpl_attr_id = 'KS-KRMS-NL-TMPL-ATTR-10002'
    and tmp.nl_tmpl_id = 'KS-KRMS-NL-TMPL-55754'
-   /
-
---KSENROLL:13037 - Adding for Fall Natural language
-    UPDATE krms_prop_t prop
-    SET prop.TYP_ID = 'KS-KRMS-TYP-55748'
-    WHERE prop.prop_id = 'KS-KRMS-PROP-14810'
-    /
+/
---
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/06-FR2-M1/2014-05-28-NaturalLanguage-Proposition-Data-Correction.sql
@@ -0,0 +1,5 @@
+--KSENROLL:13037 - Adding for Fall Natural language
+UPDATE krms_prop_t prop
+   SET prop.TYP_ID = 'KS-KRMS-TYP-55748'
+ WHERE prop.prop_id = 'KS-KRMS-PROP-14810'
+/
