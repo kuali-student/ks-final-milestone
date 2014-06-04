49422069e532011f67eb7a1032d0d72b1c055d50
rileymg <rileymg@kuali.org>
2014-06-04 09:36:12 -0400

    completed KSAP-1212 (fix KSAP type validation logic) ...correcting type-type type
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@70271 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/06-FR2-M1/2014-06-02-KSEN_TYPETYPE_RELTN-academic-plan-item-course-type-constraint.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/06-FR2-M1/2014-06-02-KSEN_TYPETYPE_RELTN-academic-plan-item-course-type-constraint.sql
@@ -3,7 +3,7 @@ INSERT INTO KSEN_TYPETYPE_RELTN
 (ID, OBJ_ID,
   TYPETYPE_RELTN_TYPE, TYPETYPE_RELTN_STATE, EFF_DT, EXPIR_DT,
   OWNER_TYPE_ID, RELATED_TYPE_ID, RANK, VER_NBR, CREATETIME, CREATEID, UPDATETIME, UPDATEID)
-  VALUES ('kuali.type.type.relation.type.allowed.item2object.kuali.academicplan.type.planitem.kuali.lu.type.CreditCourse', null,
+  VALUES ('kuali.type.type.relation.type.allowed.kuali.academicplan.type.planitem.kuali.lu.type.CreditCourse', null,
   'kuali.type.type.relation.type.allowed', 'kuali.type.type.relation.state.active', null, null,
   'kuali.academicplan.type.planitem', 'kuali.lu.type.CreditCourse', 1, 0, TIMESTAMP '2013-08-09 00:00:00', 'SYSTEMLOADER', null, null)
 /
