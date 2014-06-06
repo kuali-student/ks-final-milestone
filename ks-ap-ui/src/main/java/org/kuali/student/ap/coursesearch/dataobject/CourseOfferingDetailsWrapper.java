611a6bd53aabf5d569930bd3dc99389413e7077c
chmaurer <chmaurer@kuali.org>
2014-06-06 10:22:55 -0400

    KSAP-1479 Initial create of data object
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@70378 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-ap-ui/src/main/java/org/kuali/student/ap/coursesearch/dataobject/CourseOfferingDetailsWrapper.java
@@ -0,0 +1,11 @@
+package org.kuali.student.ap.coursesearch.dataobject;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chmaurer
+ * Date: 6/6/14
+ * Time: 8:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CourseOfferingDetailsWrapper {
+}
