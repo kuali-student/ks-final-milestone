7ea993810aafb36345b3b4f4154f00baefaa52ec
chmaurer <chmaurer@kuali.org>
2014-06-06 10:24:18 -0400

    KSAP-1405 Initial create of controller
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@70381 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-ap-ui/src/main/java/org/kuali/student/ap/coursesearch/controller/CourseSectionDetailsController.java
@@ -0,0 +1,50 @@
+package org.kuali.student.ap.coursesearch.controller;
+
+import org.kuali.rice.krad.web.controller.MethodAccessible;
+import org.kuali.rice.krad.web.controller.extension.KsapControllerBase;
+import org.kuali.rice.krad.web.form.UifFormBase;
+import org.kuali.student.ap.coursesearch.form.CourseSectionDetailsForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chmaurer
+ * Date: 6/6/14
+ * Time: 8:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequestMapping(value = "/course/details/**")
+public class CourseSectionDetailsController extends KsapControllerBase {
+
+    private static final String COURSE_SECTION_DETAILS_FORM = "CourseDetails-FormView";
+
+    @Override
+    protected UifFormBase createInitialForm(HttpServletRequest httpServletRequest) {
+        return new CourseSectionDetailsForm();
+    }
+
+    @MethodAccessible
+    @RequestMapping(params = "methodToCall=startCourseSectionDetails")
+    public ModelAndView startCourseSectionDetails(@RequestParam(value = "courseId") String courseId,
+                              @ModelAttribute("KualiForm") CourseSectionDetailsForm form,
+                              HttpServletRequest request,
+                              HttpServletResponse response) {
+        super.start(form, request, response);
+        form.setCourseTitle("Hello world! - " + courseId);
+
+        form.setViewId(COURSE_SECTION_DETAILS_FORM);
+        form.setView(super.getViewService().getViewById(COURSE_SECTION_DETAILS_FORM));
+
+        return getUIFModelAndView(form);
+    }
+
+
+}
