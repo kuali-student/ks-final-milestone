2a3ac0281f6d98bd2673011cfbef91993976d7ed
chmaurer <chmaurer@kuali.org>
2014-06-06 11:40:05 -0400

    KSAP-1480 Add more details.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@70388 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-ui/src/main/java/org/kuali/student/ap/coursesearch/dataobject/CourseTermDetailsWrapper.java
+++ ks-ap-ui/src/main/java/org/kuali/student/ap/coursesearch/dataobject/CourseTermDetailsWrapper.java
@@ -3,18 +3,44 @@ package org.kuali.student.ap.coursesearch.dataobject;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
- * User: chmaurer
- * Date: 6/6/14
- * Time: 8:29 AM
- * To change this template use File | Settings | File Templates.
+ * Wrapper class used to feed the CourseSectionDetailsUI.xml page
  */
 public class CourseTermDetailsWrapper {
 
-    String termId;
-    String termName;
-    boolean openDisclosure;
-    List<CourseOfferingDetailsWrapper> courseOfferingDetailWrappers;
+    private String termId;
+    private String termName;
+    private boolean openDisclosure;
+    private List<CourseOfferingDetailsWrapper> courseOfferingDetailWrappers;
 
+    public String getTermId() {
+        return termId;
+    }
 
+    public void setTermId(String termId) {
+        this.termId = termId;
+    }
+
+    public String getTermName() {
+        return termName;
+    }
+
+    public void setTermName(String termName) {
+        this.termName = termName;
+    }
+
+    public boolean isOpenDisclosure() {
+        return openDisclosure;
+    }
+
+    public void setOpenDisclosure(boolean openDisclosure) {
+        this.openDisclosure = openDisclosure;
+    }
+
+    public List<CourseOfferingDetailsWrapper> getCourseOfferingDetailWrappers() {
+        return courseOfferingDetailWrappers;
+    }
+
+    public void setCourseOfferingDetailWrappers(List<CourseOfferingDetailsWrapper> courseOfferingDetailWrappers) {
+        this.courseOfferingDetailWrappers = courseOfferingDetailWrappers;
+    }
 }
