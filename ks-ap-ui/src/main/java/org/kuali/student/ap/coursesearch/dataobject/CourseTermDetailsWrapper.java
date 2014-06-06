4fe5c454f39c8bb2724dcfbbdc94e2f044f5d073
chmaurer <chmaurer@kuali.org>
2014-06-06 10:23:17 -0400

    KSAP-1480 Initial create of data object
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@70379 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-ap-ui/src/main/java/org/kuali/student/ap/coursesearch/dataobject/CourseTermDetailsWrapper.java
@@ -0,0 +1,20 @@
+package org.kuali.student.ap.coursesearch.dataobject;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chmaurer
+ * Date: 6/6/14
+ * Time: 8:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CourseTermDetailsWrapper {
+
+    String termId;
+    String termName;
+    boolean openDisclosure;
+    List<CourseOfferingDetailsWrapper> courseOfferingDetailWrappers;
+
+
+}
