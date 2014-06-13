8da93bb7948a9e1e01e8a51fe6e23d9177a76643
jmorris <jmorris@kuali.org>
2014-06-13 08:47:05 -0400

    KSENROLL-12595: Added comments to sticky directive in registration.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@70712 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/angular/registration/app/scripts/directives/sticky.js
+++ ks-enroll-ui/src/main/angular/registration/app/scripts/directives/sticky.js
@@ -2,6 +2,13 @@
 
 angular.module('regCartApp')
 
+    /**
+     * Sticky directive causes elements to stick at the top of the page when the user scrolls past it.
+     *
+     * @example <div sticky></div>
+     * @example <div class="sticky">abcd</div>
+     * @example <div sticky offset="20" min-width="600" max-width="1000">abcd</div>
+     */
     .directive('sticky', ['$timeout', '$window', '$document', function($timeout, $window, $document) {
         return {
             restrict: 'CA',
@@ -26,12 +33,16 @@ angular.module('regCartApp')
                     elem.addClass('util-sticky');
 
 
-
+                    /**
+                     * Check and sticky/unstick the element depending on the element & scroll position.
+                     */
                     function check () {
-                        // Enforce the width constraints
+                        // Enforce the width constraints.
+                        // Scrolling is disabled when the screen width is outside of the width range (minWidth - maxWidth).
                         var width = w.outerWidth();
                         if ((minWidth > 0 && width < minWidth) || (maxWidth > 0 && width > maxWidth)) {
                             if (isStuck) {
+                                // Sticky should be disabled, but it is currently stuck. Unstick it.
                                 console.log('Unsticking - width constraints met: ' + minWidth + ' < ' + width + ' < ' + maxWidth);
                                 unstick();
                             }
@@ -40,22 +51,29 @@ angular.module('regCartApp')
                         }
 
 
-                        var scrollTop = (w.pageYOffset || d.scrollTop()) - (d.clientTop || 0),
-                            shouldStick = (scrollTop >= stickyLine);
+                        var scrollTop = (w.pageYOffset || d.scrollTop()) - (d.clientTop || 0), // Calculate the top of the screen
+                            shouldStick = (scrollTop >= stickyLine); // The element should be stuck if its top is below the 'stickyLine'.
 
                         if (!isStuck && shouldStick) {
+                            // Element is not stuck but should be.
                             // console.log('Stick it!', d.scrollTop(), stickyLine, stick);
                             stick();
                         } else if (isStuck && !shouldStick) {
+                            // Element is stuck but shouldn't be.
                             // console.log('Unstick it!', d.scrollTop(), stickyLine, stick);
                             unstick();
-                        } else if (elem.isStuck && shouldStick) {
+                        } else if (isStuck && shouldStick) {
+                            // Element is stuck & should be, nothing to do.
                             // console.log('Still stuck', scrollTop, stickyLine);
                         } else {
+                            // Element is not stuck & shouldn't be, nothing to do.
                             // console.log('Still unstuck', scrollTop, stickyLine);
                         }
                     }
 
+                    /**
+                     * Make the element sticky on the page.
+                     */
                     function stick () {
                         // Insert a placeholder element before the sticky element.
                         // The placeholder prevents the page height from shifting when we pop it out of the DOM flow.
@@ -73,6 +91,9 @@ angular.module('regCartApp')
                         isStuck = true;
                     }
 
+                    /**
+                     * Return the element to its pre-sticky state.
+                     */
                     function unstick () {
                         // The placeholder is no longer needed
                         if (placeholder) {
