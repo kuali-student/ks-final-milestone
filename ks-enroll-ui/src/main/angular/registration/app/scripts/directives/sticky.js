f8fb6126f95e12661f8d8c84f159e27b1fe2abef
jmorris <jmorris@kuali.org>
2014-06-20 14:37:44 -0400

    KSENROLL-12595: Removed unnecessary/unused functionality from sticky directive in registration. Created angular unit test covering sticky directive.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@70996 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/angular/registration/app/scripts/directives/sticky.js
+++ ks-enroll-ui/src/main/angular/registration/app/scripts/directives/sticky.js
@@ -7,25 +7,17 @@ angular.module('regCartApp')
      *
      * @example <div sticky></div>
      * @example <div class="sticky">abcd</div>
-     * @example <div sticky offset="20" min-width="600" max-width="1000">abcd</div>
      */
     .directive('sticky', ['$timeout', '$window', '$document', function($timeout, $window, $document) {
         return {
             restrict: 'CA',
             scope: {
-                offset: '@', // Offset from the top of page to stick the element
-                minWidth: '@', // Min-width (px) of the page to make the element sticky
-                maxWidth: '@' // Max-width (px) of the page to make the element sticky
             },
             link: function(scope, elem) {
                 $timeout(function() {
-                    var offset = scope.offset || 0,
-                        minWidth = scope.minWidth || -1,
-                        maxWidth = scope.maxWidth || -1,
-                        w = angular.element($window),
+                    var w = angular.element($window),
                         d = angular.element($document),
-                        stickyLine = elem.offset().top - offset, // Scroll position to stick at
-                        initialOffset = elem.offset().top,
+                        stickyLine = elem.offset().top, // Scroll position to stick at
                         placeholder,
                         isStuck = false;
 
@@ -37,21 +29,7 @@ angular.module('regCartApp')
                      * Check and sticky/unstick the element depending on the element & scroll position.
                      */
                     function check () {
-                        // Enforce the width constraints.
-                        // Scrolling is disabled when the screen width is outside of the width range (minWidth - maxWidth).
-                        var width = w.outerWidth();
-                        if ((minWidth > 0 && width < minWidth) || (maxWidth > 0 && width > maxWidth)) {
-                            if (isStuck) {
-                                // Sticky should be disabled, but it is currently stuck. Unstick it.
-                                console.log('Unsticking - width constraints met: ' + minWidth + ' < ' + width + ' < ' + maxWidth);
-                                unstick();
-                            }
-
-                            return;
-                        }
-
-
-                        var scrollTop = (w.pageYOffset || d.scrollTop()) - (d.clientTop || 0), // Calculate the top of the screen
+                        var scrollTop = ($window.pageYOffset || d.scrollTop()) - (d.clientTop || 0), // Calculate the top of the screen
                             shouldStick = (scrollTop >= stickyLine); // The element should be stuck if its top is below the 'stickyLine'.
 
                         if (!isStuck && shouldStick) {
@@ -78,19 +56,17 @@ angular.module('regCartApp')
                         // Insert a placeholder element before the sticky element.
                         // The placeholder prevents the page height from shifting when we pop it out of the DOM flow.
                         if (!placeholder) {
-                            placeholder = angular.element('<div class="util-sticky-placeholder"></div>');
+                            placeholder = angular.element('<div class="util-sticky-placeholder"/>');
                         }
 
                         placeholder.css('height', elem.outerHeight() + 'px');
                         placeholder.insertBefore(elem);
 
-                        // Add the stuck attributes
-                        elem.css('top', offset + 'px');
+                        // Add the stuck class
                         elem.addClass('util-sticky--stuck');
 
                         isStuck = true;
                     }
-
                     /**
                      * Return the element to its pre-sticky state.
                      */
@@ -101,8 +77,7 @@ angular.module('regCartApp')
                             placeholder = null;
                         }
 
-                        // Remove the stuck attributes
-                        elem.css('top', initialOffset + 'px');
+                        // Remove the stuck class
                         elem.removeClass('util-sticky--stuck');
 
                         isStuck = false;
--- ks-enroll-ui/src/main/angular/registration/karma.conf.js
+++ ks-enroll-ui/src/main/angular/registration/karma.conf.js
@@ -11,6 +11,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'bower_components/jquery/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-animate/angular-animate.js',
