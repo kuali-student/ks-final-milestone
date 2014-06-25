f0eb603c8bf6e0390b63227ad4e28d1be8ce371d
sw <sw@kuali.org>
2014-06-25 06:49:06 -0400

    KSENROLL-13307 - Fixed insert statements
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@71107 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/06-FR2-M1/2014-06-25-KSEN-SOC-ATTR_exam_slotting_audit.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/06-FR2-M1/2014-06-25-KSEN-SOC-ATTR_exam_slotting_audit.sql
@@ -1,18 +1,18 @@
 insert into ksen_soc_attr (ATTR_KEY, ATTR_VALUE, ID, OBJ_ID, OWNER_ID)
-values ('kuali.eo.slotting.state.inprogress', '2013-06-25T09:30:36.985+0200', '913bf57b-19b3-44e4-8a0a-63bb06ca73bb', '1b646729-e5e3-4120-95f8-c635ea94ba19', '60bb46ef-b868-4791-a248-e4fc5a7fbadd');
+values ('kuali.eo.slotting.state.inprogress', '2013-06-25T09:30:36.985+0200', '913bf57b-19b3-44e4-8a0a-63bb06ca73bb', '1b646729-e5e3-4120-95f8-c635ea94ba19', '60bb46ef-b868-4791-a248-e4fc5a7fbadd')
 /
 insert into ksen_soc_attr (ATTR_KEY, ATTR_VALUE, ID, OBJ_ID, OWNER_ID)
-values ('kuali.eo.slotting.state.completed', '2013-06-25T09:35:36.985+0200', '3be7bf71-b188-4d0f-a2fc-86f4b034913e', 'bb15a6b5-5905-454b-8201-0b6424bff424', '60bb46ef-b868-4791-a248-e4fc5a7fbadd');
+values ('kuali.eo.slotting.state.completed', '2013-06-25T09:35:36.985+0200', '3be7bf71-b188-4d0f-a2fc-86f4b034913e', 'bb15a6b5-5905-454b-8201-0b6424bff424', '60bb46ef-b868-4791-a248-e4fc5a7fbadd')
 /
 insert into ksen_soc_attr (ATTR_KEY, ATTR_VALUE, ID, OBJ_ID, OWNER_ID)
-values ('kuali.eo.slotting.state.inprogress', '2014-06-25T09:30:36.985+0200', 'd91597d3-4ed9-4f99-b5c1-ad5ded7b03fc', 'f88ffdb5-0fe9-482f-8dbe-801fb82c852e', 'C47038CE-1BA2-4B80-94E2-E11847B2BF8B');
+values ('kuali.eo.slotting.state.inprogress', '2014-06-25T09:30:36.985+0200', 'd91597d3-4ed9-4f99-b5c1-ad5ded7b03fc', 'f88ffdb5-0fe9-482f-8dbe-801fb82c852e', 'C47038CE-1BA2-4B80-94E2-E11847B2BF8B')
 /
 insert into ksen_soc_attr (ATTR_KEY, ATTR_VALUE, ID, OBJ_ID, OWNER_ID)
-values ('kuali.eo.slotting.state.completed', '2014-06-25T09:35:36.985+0200', '1db73c95-b4ee-4997-988e-4d5975e00d25', '0afd7af4-b53f-4119-97df-19ca98799d47', 'C47038CE-1BA2-4B80-94E2-E11847B2BF8B');
+values ('kuali.eo.slotting.state.completed', '2014-06-25T09:35:36.985+0200', '1db73c95-b4ee-4997-988e-4d5975e00d25', '0afd7af4-b53f-4119-97df-19ca98799d47', 'C47038CE-1BA2-4B80-94E2-E11847B2BF8B')
 /
 insert into ksen_soc_attr (ATTR_KEY, ATTR_VALUE, ID, OBJ_ID, OWNER_ID)
-values ('kuali.eo.slotting.state.inprogress', '2012-09-25T09:30:36.985+0200', 'd4aa6f91-198e-4df0-8737-4967383ab0e1', 'a1a86800-59ac-4dff-bd43-bb991cdd4798', 'bfc8460e-38da-4494-a096-2446c47991fd');
+values ('kuali.eo.slotting.state.inprogress', '2012-09-25T09:30:36.985+0200', 'd4aa6f91-198e-4df0-8737-4967383ab0e1', 'a1a86800-59ac-4dff-bd43-bb991cdd4798', 'bfc8460e-38da-4494-a096-2446c47991fd')
 /
 insert into ksen_soc_attr (ATTR_KEY, ATTR_VALUE, ID, OBJ_ID, OWNER_ID)
-values ('kuali.eo.slotting.state.completed', '2012-09-25T09:35:36.985+0200', '5020ffb8-3bdf-4bb1-904f-c5f1f0123789', 'd1250d49-f26b-4c07-b816-d545c4088199', 'bfc8460e-38da-4494-a096-2446c47991fd');
+values ('kuali.eo.slotting.state.completed', '2012-09-25T09:35:36.985+0200', '5020ffb8-3bdf-4bb1-904f-c5f1f0123789', 'd1250d49-f26b-4c07-b816-d545c4088199', 'bfc8460e-38da-4494-a096-2446c47991fd')
 /
