b5ef882d840c9a579e719ac1d37b7c5fa280b453
vgadiyak <vgadiyak@kuali.org>
2014-07-07 14:05:43 -0400

    KSENROLL-13330 : Term resolver
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@71612 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseoffering/krms/termresolver/CourseRegistrationKeyDateTermResolver.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright 2014 The Kuali Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * Created by vgadiyak on 7/7/14
+ */
+package org.kuali.student.enrollment.class2.courseoffering.krms.termresolver;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.rice.krms.api.engine.TermResolutionException;
+import org.kuali.rice.krms.api.engine.TermResolver;
+import org.kuali.student.common.util.krms.RulesExecutionConstants;
+import org.kuali.student.enrollment.courseoffering.dto.ActivityOfferingInfo;
+import org.kuali.student.enrollment.courseoffering.dto.CourseOfferingInfo;
+import org.kuali.student.enrollment.courseoffering.dto.RegistrationGroupInfo;
+import org.kuali.student.enrollment.courseoffering.service.CourseOfferingService;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.krms.util.KSKRMSExecutionUtil;
+import org.kuali.student.r2.core.acal.dto.KeyDateInfo;
+import org.kuali.student.r2.core.acal.service.AcademicCalendarService;
+import org.kuali.student.r2.core.constants.KSKRMSServiceConstants;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This class //TODO ...
+ *
+ * @author Kuali Student Team
+ */
+public class CourseRegistrationKeyDateTermResolver implements TermResolver<Boolean> {
+
+    private CourseOfferingService courseOfferingService;
+    private AcademicCalendarService academicCalendarService;
+
+    @Override
+    public Set<String> getPrerequisites() {
+        Set<String> prereqs = new HashSet<String>(2);
+        prereqs.add(RulesExecutionConstants.CONTEXT_INFO_TERM.getName());
+        prereqs.add(RulesExecutionConstants.REGISTRATION_GROUP_TERM.getName());
+        prereqs.add("userActionDate");
+        return Collections.unmodifiableSet(prereqs);
+    }
+
+    @Override
+    public String getOutput() {
+        return KSKRMSServiceConstants.TERM_RESOLVER_SCHEDULEADJUSTMENTPERIOD;
+    }
+
+    @Override
+    public Set<String> getParameterNames() {
+        Set<String> parameters = new HashSet<String>(2);
+        parameters.add(KSKRMSServiceConstants.TERM_PARAMETER_TYPE_TERM_KEYDATE_TYPE_KEY);
+        return Collections.unmodifiableSet(parameters);
+    }
+
+    @Override
+    public int getCost() {
+        return 0;
+    }
+
+    @Override
+    public Boolean resolve(Map<String, Object> resolvedPrereqs, Map<String, String> parameters) throws TermResolutionException {
+        ContextInfo context = (ContextInfo) resolvedPrereqs.get(RulesExecutionConstants.CONTEXT_INFO_TERM.getName());
+        RegistrationGroupInfo regGroupInfo = (RegistrationGroupInfo) resolvedPrereqs.get(RulesExecutionConstants.REGISTRATION_GROUP_TERM.getName());
+        Date userActionDate = context.getCurrentDate();
+
+        try {
+            CourseOfferingInfo coInfo = getCourseOfferingService().getCourseOffering(regGroupInfo.getCourseOfferingId(), context);
+            List<ActivityOfferingInfo> aoInfos = getCourseOfferingService().getActivityOfferingsByIds(regGroupInfo.getActivityOfferingIds(), context);
+            String coTermId = coInfo.getTermId();
+            String termId = aoInfos.get(0).getTermId();
+            for (ActivityOfferingInfo aoInfo : aoInfos) {
+                if (!StringUtils.equals(termId, aoInfo.getTermId())) {
+                    termId = coTermId;
+                    break;
+                }
+            }
+
+            List<KeyDateInfo> keyDateInfos = getAcademicCalendarService().getKeyDatesForTerm(termId, context);
+            for (KeyDateInfo keyDateInfo : keyDateInfos) {
+                if ((StringUtils.equals(keyDateInfo.getTypeKey(), parameters.get(KSKRMSServiceConstants.TERM_PARAMETER_TYPE_TERM_KEYDATE_TYPE_KEY)))) {
+                    if (keyDateInfo.getStartDate().compareTo(userActionDate) > 0 || keyDateInfo.getEndDate().compareTo(userActionDate) < 0) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } catch (Exception e) {
+            KSKRMSExecutionUtil.convertExceptionsToTermResolutionException(parameters, e, this);
+        }
+
+        return false;
+    }
+
+    public CourseOfferingService getCourseOfferingService() {
+        return courseOfferingService;
+    }
+
+    public void setCourseOfferingService(CourseOfferingService courseOfferingService) {
+        this.courseOfferingService = courseOfferingService;
+    }
+
+    public AcademicCalendarService getAcademicCalendarService() { return academicCalendarService; }
+
+    public void setAcademicCalendarService(AcademicCalendarService academicCalendarService) { this.academicCalendarService = academicCalendarService; }
+}
