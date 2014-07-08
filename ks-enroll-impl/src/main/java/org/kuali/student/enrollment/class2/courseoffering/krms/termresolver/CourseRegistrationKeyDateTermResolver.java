164dccd6f9dd9e1a27e2165d2e159ca2079a7857
gtaylor <gtaylor@kuali.org>
2014-07-08 12:16:17 -0400

    KSENROLL-13330: changed implementation to improve performance. now using LuiService.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@71670 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseoffering/krms/termresolver/CourseRegistrationKeyDateTermResolver.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseoffering/krms/termresolver/CourseRegistrationKeyDateTermResolver.java
@@ -16,14 +16,13 @@
  */
 package org.kuali.student.enrollment.class2.courseoffering.krms.termresolver;
 
-import org.apache.commons.lang.StringUtils;
 import org.kuali.rice.krms.api.engine.TermResolutionException;
 import org.kuali.rice.krms.api.engine.TermResolver;
+import org.kuali.student.common.collection.KSCollectionUtils;
 import org.kuali.student.common.util.krms.RulesExecutionConstants;
-import org.kuali.student.enrollment.courseoffering.dto.ActivityOfferingInfo;
-import org.kuali.student.enrollment.courseoffering.dto.CourseOfferingInfo;
 import org.kuali.student.enrollment.courseoffering.dto.RegistrationGroupInfo;
-import org.kuali.student.enrollment.courseoffering.service.CourseOfferingService;
+import org.kuali.student.enrollment.lui.dto.LuiInfo;
+import org.kuali.student.enrollment.lui.service.LuiService;
 import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.krms.util.KSKRMSExecutionUtil;
 import org.kuali.student.r2.core.atp.dto.MilestoneInfo;
@@ -44,7 +43,7 @@ import java.util.Set;
  */
 public class CourseRegistrationKeyDateTermResolver implements TermResolver<Boolean> {
 
-    private CourseOfferingService courseOfferingService;
+    private LuiService luiService;
     private AtpService atpService;
 
     @Override
@@ -80,15 +79,16 @@ public class CourseRegistrationKeyDateTermResolver implements TermResolver<Boole
         Date userActionDate = context.getCurrentDate();
 
         try {
-            CourseOfferingInfo coInfo = getCourseOfferingService().getCourseOffering(regGroupInfo.getCourseOfferingId(), context);
-            List<ActivityOfferingInfo> aoInfos = getCourseOfferingService().getActivityOfferingsByIds(regGroupInfo.getActivityOfferingIds(), context);
-            String coTermId = coInfo.getTermId();
-            String termId = aoInfos.get(0).getTermId();
-            for (ActivityOfferingInfo aoInfo : aoInfos) {
-                if (!StringUtils.equals(termId, aoInfo.getTermId())) {
-                    termId = coTermId;
-                    break;
-                }
+            String termId;
+            LuiInfo coLui = getLuiService().getLui(regGroupInfo.getCourseOfferingId(), context);
+            List<LuiInfo> aoLuis = getLuiService().getLuisByIds(regGroupInfo.getActivityOfferingIds(), context);
+
+            // We have co and ao terms.  There is special business logic to determine which term to use.
+            // find out if all ao terms are the same, if so use the ao term, else use co term
+            if( isAllAtpIdsTheSame(aoLuis)){
+                termId = KSCollectionUtils.getRequiredZeroElement(aoLuis).getAtpId();
+            } else {
+                termId = coLui.getAtpId();
             }
 
             List<MilestoneInfo> mstones = getAtpService().getMilestonesByTypeForAtp(termId, KSKRMSServiceConstants.TERM_PARAMETER_TYPE_TERM_KEYDATE_TYPE_KEY, context);
@@ -105,12 +105,31 @@ public class CourseRegistrationKeyDateTermResolver implements TermResolver<Boole
         return false;
     }
 
-    public CourseOfferingService getCourseOfferingService() {
-        return courseOfferingService;
+    /**
+     * helper method that takes in a list of luis and returns if all atpIds are the same
+     * @param l
+     * @return
+     */
+    private static boolean isAllAtpIdsTheSame(List<LuiInfo> l) {
+        Set<String> set = new HashSet<String>(l.size());
+        for (LuiInfo o : l) {
+            if (set.isEmpty()) {
+                set.add(o.getAtpId());
+            } else {
+                if (set.add(o.getAtpId())) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
-    public void setCourseOfferingService(CourseOfferingService courseOfferingService) {
-        this.courseOfferingService = courseOfferingService;
+    public LuiService getLuiService() {
+        return luiService;
+    }
+
+    public void setLuiService(LuiService luiService) {
+        this.luiService = luiService;
     }
 
     public AtpService getAtpService() {
