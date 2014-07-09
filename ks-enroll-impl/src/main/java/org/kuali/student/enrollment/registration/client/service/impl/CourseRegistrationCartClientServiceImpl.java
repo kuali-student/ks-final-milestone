589d894d18e3a1374acd694e64b148d1a48e464c
gtaylor <gtaylor@kuali.org>
2014-07-09 15:20:32 -0400

    KSENROLL-13410: committed error debug logging to track down issue that only happens in distributed randomly.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@71749 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/client/service/impl/CourseRegistrationCartClientServiceImpl.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/client/service/impl/CourseRegistrationCartClientServiceImpl.java
@@ -66,11 +66,20 @@ public class CourseRegistrationCartClientServiceImpl extends CourseRegistrationC
             //This will need to be changed to the cartItemResponse object in the future!
             response = Response.ok(result);
         } catch (MissingOptionException e) {
+            String technicalInfo = String.format("Unable to add item to cart. Technical Info:(cartId:[%s] courseCode:[%s] regGroupCode:[%s] regGroupId:[%s] gradingOptionId:[%s] credits:[%s] )",
+                    cartId, courseCode, regGroupCode, regGroupId, gradingOptionId, credits);
+            LOGGER.error(technicalInfo,e);
             response = getResponse(Response.Status.BAD_REQUEST, e.getCartItemOptions());
         } catch (DoesNotExistException e) {
+            String technicalInfo = String.format("Unable to add item to cart. Technical Info:(cartId:[%s] courseCode:[%s] regGroupCode:[%s] regGroupId:[%s] gradingOptionId:[%s] credits:[%s] )",
+                    cartId, courseCode, regGroupCode, regGroupId, gradingOptionId, credits);
+            LOGGER.error(technicalInfo,e);
             //The reg request does not exist (HTTP status 404 Not Found)
             response = getResponse(Response.Status.NOT_FOUND, e.getMessage());
         } catch (GenericUserException e) {
+            String technicalInfo = String.format("Unable to add item to cart. Technical Info:(cartId:[%s] courseCode:[%s] regGroupCode:[%s] regGroupId:[%s] gradingOptionId:[%s] credits:[%s] )",
+                    cartId, courseCode, regGroupCode, regGroupId, gradingOptionId, credits);
+            LOGGER.error(technicalInfo,e);
             response = getResponse(Response.Status.INTERNAL_SERVER_ERROR, e.getUserMessage());
         } catch (Exception e) {
             // Convert the generic user message into something useful to the UI.
@@ -82,6 +91,7 @@ public class CourseRegistrationCartClientServiceImpl extends CourseRegistrationC
             userMessage.setConsoleMessage(technicalInfo);
             userMessage.setType(UserMessageResult.MessageTypes.ERROR);
             response = getResponse(Response.Status.INTERNAL_SERVER_ERROR, userMessage);
+            LOGGER.error(technicalInfo,e);
         }
 
         return response.build();
