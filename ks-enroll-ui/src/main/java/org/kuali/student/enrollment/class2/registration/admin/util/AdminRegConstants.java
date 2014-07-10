136c23b7dd82ea4ec31dc28f1d46436c18a75222
peggy <peggy@kuali.org>
2014-07-10 09:39:29 -0400

    KSENROLL-13470: Student Information validation messages
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@71798 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/registration/admin/util/AdminRegConstants.java
@@ -0,0 +1,22 @@
+package org.kuali.student.enrollment.class2.registration.admin.util;
+
+/**
+ * Created by SW Genis on 2014-07-10.
+ */
+public class AdminRegConstants {
+
+    // Collection component ids.
+    public static String REG_COLL_ID = "KS-AdminRegistration-Registered";
+    public static String WAITLIST_COLL_ID = "KS-AdminRegistration-Waitlist";
+    public static String ISSUES_COLL_ID = "KS-AdminRegistration-Issues";
+
+    // Dialog ids.
+    public static String REG_CONFIRM_DIALOG = "registerConfirmDialog";
+    public static String DROP_COURSE_DIALOG = "dropCourseDialog";
+    public static String DROP_WAITLISTED_DIALOG = "dropWaitlistedDialog";
+
+    //Message keys
+    public static final String ADMIN_REG_MSG_ERROR_INVALID_STUDEND = "error.admin.reg.studentId.invalid";
+    public static final String ADMIN_REG_MSG_ERROR_STUDEND_REQUIRED = "error.admin.reg.studentId.required";
+
+}
