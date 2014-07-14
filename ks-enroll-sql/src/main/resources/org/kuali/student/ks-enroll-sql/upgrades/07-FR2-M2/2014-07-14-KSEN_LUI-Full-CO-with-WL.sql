6906ebc069b0ce8418f9945d9f09cff99e5e432a
vgadiyak <vgadiyak@kuali.org>
2014-07-14 14:54:15 -0400

    KSENROLL-13570 : new users with static dates added, new sqls
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@72047 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/07-FR2-M2/2014-07-14-KSEN_LUI-Full-CO-with-WL.sql
@@ -0,0 +1,6 @@
+-- KSENROLL-13570
+-- Fall 2012, HIST266 -> changing max enroll to 4
+update KSEN_LUI set OBJ_ID='68a310c6-da92-4789-9f0b-1b507476edf9', VER_NBR=2, UPDATEID='admin', UPDATETIME=TIMESTAMP '2014-07-14 14:31:51.576', ATP_ID='kuali.atp.2012Fall', CLU_ID='cd841f27-7cd7-46ce-bcc4-6e04debdcfc4', EFF_DT=TIMESTAMP '2012-08-28 20:00:00.0', EXPIR_DT='', DESCR_FORMATTED='Courses with lecture', LUI_STATE='kuali.lui.activity.offering.state.offered', LUI_TYPE='kuali.lui.type.activity.offering.lecture', MAX_SEATS=4, MIN_SEATS=4, NAME='Lecture', DESCR_PLAIN='Courses with lecture', REF_URL='null' where ID='5732c691-ef9c-4b30-bbda-9fa5decaa41f' and VER_NBR=1
+/
+
+-- b.janek
