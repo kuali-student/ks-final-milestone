7c653d63dfcd01b04c17e546aa5e08969dc59d4c
yindavid <yindavid@kuali.org>
2014-07-15 13:54:37 -0400

    KSENROLL-13522: refactored ActivityOfferingTransformer to retrieve scheduling state from dynamic attribute and set it to DTO
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/2.0.0-Mx@72121 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/CourseOfferingServiceConstants.java
+++ ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/CourseOfferingServiceConstants.java
@@ -67,6 +67,7 @@ public class CourseOfferingServiceConstants {
     public static final String COLOCATED_SET_MAXIMUM_ENROLLMENT_ATTR = "kuali.attribute.colocatedset.max.enrollment";
     public static final String IS_MAX_ENROLLMENT_SHARED_ATTR = "kuali.attribute.colocatedset.is.max.enrollment.shared";
     public static final String IS_AO_APPROVED_FOR_NON_STANDARD_TIME_SLOTS = "kuali.attribute.nonstd.ts.indicator";
+    public static final String ACTIVITY_OFFERING_SCHEDULING_STATE_ATTR = "kuali.attribute.activity.offering.scheduling.state.key";
 
     // The type/states are defined in LuiServiceConstants.java
 
