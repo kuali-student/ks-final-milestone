9a99cdaa6b9eeb969774d9c88f4e60b713f047b0
dcrouse <dcrouse@kuali.org>
2014-07-17 04:26:57 -0400

    KSENROLL-13595: caches for courseOffering
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/2.0.0-Mx@72279 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/CourseOfferingServiceConstants.java
+++ ks-enroll-api/src/main/java/org/kuali/student/r2/common/util/constants/CourseOfferingServiceConstants.java
@@ -36,6 +36,7 @@ public class CourseOfferingServiceConstants {
 
     public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "courseOffering";
     public static final String SERVICE_NAME_LOCAL_PART = CourseOfferingService.class.getSimpleName();
+    public static final String CACHED_SERVICE_NAME_LOCAL_PART = CourseOfferingService.class.getSimpleName() + "Cached";
     public static final String REF_OBJECT_URI_COURSE_OFFERING = NAMESPACE + "/" + CourseOfferingInfo.class.getSimpleName();
     public static final String REF_OBJECT_URI_ACTIVITY_OFFERING = NAMESPACE + "/" + ActivityOfferingInfo.class.getSimpleName();
     public static final String REF_OBJECT_URI_REGISTRATION_GROUP = NAMESPACE + "/" + RegistrationGroupInfo.class.getSimpleName();
