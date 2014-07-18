82057bda2fb1389b20bb06e501ea503b461a312e
johglov <johglov@kuali.org>
2014-07-18 16:19:51 -0400

    KSAP-1462 - Added facet strategy unit tests
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@72391 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-ui/src/test/java/org/kuali/student/ap/coursesearch/controller/CourseFacetStrategyTest.java
+++ ks-ap-ui/src/test/java/org/kuali/student/ap/coursesearch/controller/CourseFacetStrategyTest.java
@@ -7,15 +7,13 @@ import org.kuali.student.r2.common.dto.ContextInfo;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-/**
- * Created with IntelliJ IDEA.
- * User: chmaurer
- * Date: 6/4/14
- * Time: 10:36 AM
- * To change this template use File | Settings | File Templates.
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:ks-ap-test-context.xml" })
 public class CourseFacetStrategyTest {
@@ -35,6 +33,27 @@ public class CourseFacetStrategyTest {
     @Test
     public void testIsThisThingOn() {
          assertNotNull("Strategy is null", strategy);
-     }
+    }
+
+    @Test
+    public void testGetFacetColumnsReversed(){
+        List<String> facets = strategy.getFacetColumnsReversed();
+        assertEquals(5,facets.size());
+        assertEquals("facet_quarter",facets.get(0));
+        assertEquals("facet_genedureq",facets.get(1));
+        assertEquals("facet_credits",facets.get(2));
+        assertEquals("facet_level",facets.get(3));
+        assertEquals("facet_curriculum",facets.get(4));
+    }
+
+    @Test
+    public void testGetFacetColumns(){
+        Map<String, Integer> facets = strategy.getFacetColumns();
+        assertEquals((long)facets.get("facet_curriculum"),(long)4);
+        assertEquals((long)facets.get("facet_credits"),(long)2);
+        assertEquals((long)facets.get("facet_genedureq"),(long)1);
+        assertEquals((long)facets.get("facet_level"),(long)3);
+        assertEquals((long)facets.get("facet_quarter"),(long)0);
+    }
 
 }
