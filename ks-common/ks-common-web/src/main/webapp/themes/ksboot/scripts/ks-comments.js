b38dd74db6c5751ee18ffcfe5b9b728b117be65e
hossein.aliabadi <hossein.aliabadi@kuali.org>
2014-07-22 15:45:49 -0400

    KSENROLL-13514:Add and code delete icon.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@72547 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common/ks-common-web/src/main/webapp/themes/ksboot/scripts/ks-comments.js
+++ ks-common/ks-common-web/src/main/webapp/themes/ksboot/scripts/ks-comments.js
@@ -21,16 +21,17 @@ function toggleCommentButtons(elem) {
     });
 }
 
-function toggleDeleteElements(elem){
+function toggleDeleteElements(elem) {
     var rowContainer = getRowContainer(elem);
     jQuery(rowContainer).find('.ks-toggle-delete').each(function () {
         jQuery(this).toggle();
     });
+    jQuery(rowContainer).find('.ks-deleted-comment').each(function () {
+        jQuery(this).toggleClass("ks-deleting-comment");
+    });
 }
 
-function deleteComment(elemId) {
-    var elem = jQuery("#" + elemId);
-    toggleDeleteElements(elem);
+function addDeletedObject(elem){
     var rowContainer = getRowContainer(elem);
     var rowId = jQuery(rowContainer).attr('id');
     var deletedObject = {};
@@ -60,17 +61,79 @@ function deleteComment(elemId) {
     deletedObjects.push({"key": rowId, "deletedObject": deletedObject});
 }
 
-function undeleteComment(elemId){
-    var elem = jQuery("#" + elemId);
-//    toggleDeleteElements(elem);
+function restoreDeletedObject(){
     var rowContainer = getRowContainer(elem);
-    var rowId = jQuery(rowContainer).attr('id');
-    var itemtoRemove = deletedObjects[rowId];
-    deletedObjects.splice(jQuery.inArray(itemtoRemove, deletedObjects),1);
+    var itemToRemove = getDeletedComment(rowContainer.attr('id'));
+
+    jQuery(rowContainer).find('.ks-comment-textArea-field').each(function () {
+        jQuery(this).find(":input").each(function () {
+            var textId = jQuery(this).attr('id');
+            var textValue = itemToRemove.deletedObject.textField.filter(function (){
+                return this.id = textId;
+            });
+            jQuery(this).val(textValue);
+        });
+    });
+    jQuery(rowContainer).find('.ks-comment-metaData-container').each(function () {
+        jQuery(this).find('.ks-comment-metaData-item').each(function () {
+            var key = jQuery(this).attr('id');
+            var value = itemToRemove.deletedObject.metadata.filter(function (){
+                return this.key = key;
+            })
+            jQuery(this).text(value);
+        });
+    });
+    deletedObjects.splice(jQuery.inArray(itemtoRemove, deletedObjects), 1);
 }
 
-function getDeletedComment(key){
-    deletedObjects.filter(function(){
+function deleteComment(baseUrl, elem) {
+    commentAjaxController(elem, baseUrl, 'ajaxDeleteComment', function (data) {
+        toggleDeleteElements(elem);
+        //addDeletedObject(elem);
+    });
+}
+
+function undeleteComment(baseUrl, elem) {
+    var rowContainer = getRowContainer(elem);
+    var deletedComment = getDeletedComment(rowContainer.attr('id'));
+    commentAjaxController(elem, baseUrl, 'ajaxUndeleteComment', function (data) {
+        toggleDeleteElements(elem);
+        restoreDeletedObject(elem);
+//        var rowContainer = getRowContainer(elem);
+//        var rowId = jQuery(rowContainer).attr('id');
+//        var itemtoRemove = deletedObjects[rowId];
+//        deletedObjects.splice(jQuery.inArray(itemtoRemove, deletedObjects), 1);
+    });
+}
+
+function getDeletedComment(key) {
+    deletedObjects.filter(function () {
         return this.key === key;
+    });
+}
+
+function commentAjaxController(elem, baseUrl, methodToCall, callbackFunction) {
+    var index = -1;
+    var rowContainer = getRowContainer(elem);
+    var input = jQuery(rowContainer).find(':input')[0];
+    if(input !== undefined){
+        var name = jQuery(input).attr('name');
+        index = name.match(/\[(\d+)\]/)[1];
+    }
+    var formData = jQuery('#kualiForm').serialize() + '&' + jQuery.param({"selectedLineIndex" : index});
+    var targetUrl = baseUrl + "/kr-krad/ksComment?methodToCall=" + methodToCall;
+    jQuery.ajax({
+        dataType: "json",
+        url: targetUrl,
+        type: "POST",
+        data: formData,
+        success: function (data, textStatus, jqXHR) {
+            callbackFunction(data);
+        },
+        error: function (jqXHR, status, error) {
+            if (console) {
+                console.log("Error Occured...............");
+            }
+        }
     });
 }
