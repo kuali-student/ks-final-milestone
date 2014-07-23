7ff3419af839b57c401a44b5231586f87adf1761
vpremcha <vpremcha@kuali.org>
2014-07-23 12:49:31 -0400

    KSCM-1221 - View a Course - Create Course View
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@72610 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-cm-krad/src/main/java/org/kuali/student/cm/common/util/CurriculumManagementConstants.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/common/util/CurriculumManagementConstants.java
@@ -56,7 +56,8 @@ public class CurriculumManagementConstants {
     public static class CourseViewPageIds {
         public final static String REVIEW_PROPOSAL = "KS-CourseView-ReviewProposalPage";
         public final static String CREATE_COURSE = "KS-CourseView-CoursePage";
-        public final static String CREATE_COURSE_ID = "KS-CourseView";
+        public final static String CREATE_COURSE_VIEW = "KS-CourseView";
+        public final static String COURSE_DETAILED_VIEW = "CourseDetailedView";
     }
 
     /**
