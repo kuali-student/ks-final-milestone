70d0011c5921b0e0df8c5998e40b8064f9bbbbf6
depstein <depstein@kuali.org>
2014-07-28 09:33:03 -0400

    KSENROLL-13749 Got rid of thread (caused initial searches not to work), added comments
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@72874 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/search/elastic/ElasticEmbedded.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/search/elastic/ElasticEmbedded.java
@@ -1,19 +1,13 @@
 package org.kuali.student.enrollment.registration.search.elastic;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.log4j.Logger;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
 import org.kuali.student.common.util.security.ContextUtils;
-import org.kuali.student.enrollment.registration.client.service.ScheduleOfClassesService;
 import org.kuali.student.enrollment.registration.client.service.dto.CourseSearchResult;
 import org.kuali.student.enrollment.registration.client.service.impl.util.SearchResultHelper;
 import org.kuali.student.enrollment.registration.search.service.impl.CourseRegistrationSearchServiceImpl;
@@ -24,28 +18,19 @@ import org.kuali.student.r2.common.exceptions.MissingParameterException;
 import org.kuali.student.r2.common.exceptions.OperationFailedException;
 import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
 import org.kuali.student.r2.core.search.dto.SearchRequestInfo;
-import org.kuali.student.r2.core.search.infc.SearchRequest;
 import org.kuali.student.r2.core.search.infc.SearchResult;
 import org.kuali.student.r2.core.search.service.SearchService;
 import org.kuali.student.r2.lum.lrc.dto.ResultValueInfo;
 import org.kuali.student.r2.lum.lrc.service.LRCService;
 
-import javax.ws.rs.core.Cookie;
-import javax.ws.rs.core.Response;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
 /**
- * Created by Daniel on 7/9/14.
+ * A simple class that instantiates an elasticsearch Node and obtains a client
+ * This is intended for use as a singleton inside a spring context
  */
 public class ElasticEmbedded {
 
@@ -60,46 +45,52 @@ public class ElasticEmbedded {
         super();
     }
 
-    private Date lastUpdated;
-    private long timeTORefreshMs = (5*60*1000);
+    private Date lastUpdated; //Keep track of timeout for the elastic "cache"
+    private static final long TIME_TO_REFRESH_MS = (5 * 60 * 1000); //Max time before refreshing the cache/reindexing
 
+    /**
+     * Starts up a node and gets a handle to the client. This is a hook for spring application context to start up
+     * embedded elastic during application startup.
+     *
+     * @throws MissingParameterException
+     * @throws InvalidParameterException
+     * @throws OperationFailedException
+     * @throws PermissionDeniedException
+     * @throws IOException
+     * @throws DoesNotExistException
+     */
     public void init() throws MissingParameterException, InvalidParameterException, OperationFailedException, PermissionDeniedException, IOException, DoesNotExistException {
         LOG.info("Starting Elastic Client");
         node = NodeBuilder.nodeBuilder().local(false).clusterName("ks.elastic.cluster").node();
         client = node.client();
-//        client
-//                .admin()
-//                .indices()
-//                .preparePutMapping("ks")
-//                .setType("courseoffering")
-//                .setSource(XContentFactory.jsonBuilder()
-//                        .startObject()
-//                        .startObject("courseoffering")
-//                        .startObject("properties")
-//                        .startObject("termId")
-//                        .field("type", "string")
-//                        .field("index", "not_analyzed")
-//                        .endObject()
-//                        .endObject()
-//                        .endObject()
-//                        .endObject())
-//                .setIgnoreConflicts(true)
-//                .execute()
-//                .actionGet();
     }
 
+    /**
+     * This method pushes course data into elastic search
+     *
+     * @throws DoesNotExistException
+     * @throws MissingParameterException
+     * @throws InvalidParameterException
+     * @throws OperationFailedException
+     * @throws PermissionDeniedException
+     * @throws IOException
+     */
     private void indexCourseOfferingData() throws DoesNotExistException, MissingParameterException, InvalidParameterException, OperationFailedException, PermissionDeniedException, IOException {
         LOG.info("Loading Data into Elastic");
         Date startTime = new Date();
+
+        //Grab all the data from the services
         List<CourseSearchResult> courses = getAllCourseOfferings();
         BulkRequestBuilder bulkRequest = client.prepareBulk();
         ObjectMapper mapper = new ObjectMapper();
 
+        //Create a bulk request to push all data into elastic
         for (CourseSearchResult searchResult : courses) {
             String json = mapper.writeValueAsString(searchResult);
             bulkRequest.add(client.prepareIndex("ks", "courseoffering", searchResult.getCourseId()).setSource(json));
         }
 
+        //Execute the bulk operation
         BulkResponse bulkResponse = bulkRequest.execute().actionGet();
         if (bulkResponse.hasFailures()) {
             throw new RuntimeException("Error Bulk Loading elasticsearch courseofferings");
@@ -108,16 +99,28 @@ public class ElasticEmbedded {
         LOG.info("Done Loading Data - " + (System.currentTimeMillis() - startTime.getTime()) + "ms");
     }
 
+    /**
+     * Method to grab all of the course offerings from a search service (get the data that will be pushed into elastic)
+     *
+     * @return list of all active course search results
+     * @throws MissingParameterException
+     * @throws InvalidParameterException
+     * @throws OperationFailedException
+     * @throws PermissionDeniedException
+     * @throws DoesNotExistException
+     */
     private List<CourseSearchResult> getAllCourseOfferings() throws MissingParameterException, InvalidParameterException, OperationFailedException, PermissionDeniedException, DoesNotExistException {
 
         List<CourseSearchResult> courseSearchResults = new ArrayList<>();
 
+        //Use search service to grab the course data
         SearchRequestInfo searchRequest = new SearchRequestInfo(CourseRegistrationSearchServiceImpl.CO_SEARCH_INFO_SEARCH_KEY);
         ContextInfo context = ContextUtils.createDefaultContextInfo();
 
         SearchResult searchResults = searchService.search(searchRequest, context);
 
-        for(SearchResultHelper.KeyValue keyValue:SearchResultHelper.wrap(searchResults)){
+        //Process the results
+        for (SearchResultHelper.KeyValue keyValue : SearchResultHelper.wrap(searchResults)) {
 
             CourseSearchResult courseSearchResult = new CourseSearchResult();
             courseSearchResult.setCourseCode(keyValue.get(CourseRegistrationSearchServiceImpl.SearchResultColumns.LUI_CODE));
@@ -130,11 +133,13 @@ public class ElasticEmbedded {
             courseSearchResult.setTermId(keyValue.get(CourseRegistrationSearchServiceImpl.SearchResultColumns.ATP_ID));
             courseSearchResult.setCourseDescription(keyValue.get(CourseRegistrationSearchServiceImpl.SearchResultColumns.LUI_DESC));
 
-            //These two calls are cached
+            // Grab Learning Results data
+            // These two calls are cached so they are actually more performant
             List<String> creditResultsValueKeys = lrcService.getResultValuesGroup(keyValue.get(CourseRegistrationSearchServiceImpl.SearchResultColumns.CREDITS), context).getResultValueKeys();
             List<ResultValueInfo> creditResultValues = lrcService.getResultValuesByKeys(creditResultsValueKeys, context);
 
-            for(ResultValueInfo creditResultValue:creditResultValues){
+            //Add all the credits from the result value group
+            for (ResultValueInfo creditResultValue : creditResultValues) {
                 courseSearchResult.getCreditOptions().add(creditResultValue.getValue());
             }
 
@@ -144,6 +149,9 @@ public class ElasticEmbedded {
         return courseSearchResults;
     }
 
+    /**
+     * Hook to close the node, should be called by spring application context.
+     */
     public void shutdown() {
         node.close();
     }
@@ -156,17 +164,20 @@ public class ElasticEmbedded {
         this.node = node;
     }
 
+    /**
+     * This method is quick way of keeping the indexed course data up-to-date by refreshing every timeToRefreshMs.
+     *
+     * @return an elastic search client
+     */
     public synchronized Client getClient() {
-        if(lastUpdated == null || System.currentTimeMillis() > (lastUpdated.getTime() + timeTORefreshMs) ){
-            new Thread(){
-                public void run(){
-                    try{
-                        indexCourseOfferingData();
-                    } catch (Exception e) {
-                        throw new RuntimeException("Error updating data",e);
-                    }
-                }
-            }.start();
+        if (lastUpdated == null || System.currentTimeMillis() > (lastUpdated.getTime() + TIME_TO_REFRESH_MS)) {
+
+            try {
+                indexCourseOfferingData();
+            } catch (Exception e) {
+                throw new RuntimeException("Error updating data", e);
+            }
+
             lastUpdated = new Date();
         }
 
