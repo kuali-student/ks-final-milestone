faf17f38954999eb278045a540112f00e28a4a92
jmorris <jmorris@kuali.org>
2014-07-29 13:44:53 -0400

    KSENROLL-13461: Updated credit option facet to bucket on integer values & into a 6+ grouping.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@72963 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-ui/src/main/angular/registration/app/scripts/config/search-facets.js
+++ ks-enroll-ui/src/main/angular/registration/app/scripts/config/search-facets.js
@@ -12,7 +12,11 @@ angular.module('regCartApp')
      * {
      *     label: Required string label
      *     id: Required string ID of facet - used to create a unique ID in the DOM.
-     *     optionsField: Optional string key corresponding to the field that the facet value in the search result items
+     *
+     *     autoCollapse: Optional boolean to collapse the facet if only 1 option is discovered - Default = True.
+     *
+     *     optionsKey: Optional string key corresponding to the field that the facet value in the search result items
+     *                 Required if using the default filter.
      *
      *     optionsProvider: Optional function to parse the search results & return a list of option objects
      *         @param array of search results
@@ -23,12 +27,20 @@ angular.module('regCartApp')
      *         @param array of selected facet options values (values only)
      *         @return boolean of whether or not to display the item based on the selected facet options
      *
-     *     prepare: Optional function called prior to checking the optionsProvider/optionsKey. Useful for doing some initial processing of the search results.
+     *     prepare: Optional function called prior to checking the optionsProvider/optionsKey.
+     *              Useful for doing some initial processing of the search results.
      *         @param array of search results
+     *
+     *     refine: Optional function called after the options have been determined.
+     *             Useful for performing some refinement or aggregation of the facet options.
+     *         @param array of options
+     *         @param array of search results
+     *         @return array of options
      * }
      *
      * When not explicitly set, optionsProvider & filter default to showing a 1-1 list of the values detected on the optionsField.
-     * Either optionsKey or optionsProvider must be set.
+     * One of optionsKey or optionsProvider is required. If both are defined, preference is given to optionsKey.
+     * - If using the default filter, optionsKey is required.
      *
      * Facets will be displayed in the order they are configured here.
      */
@@ -37,6 +49,7 @@ angular.module('regCartApp')
         {
             label: 'Seats',
             id: 'seatsAvailable',
+            autoCollapse: false, // Don't auto-collapse Seats Available
             optionsProvider: function(results) {
                 var itemsWithSeats = 0;
                 angular.forEach(results, function(item) {
@@ -65,22 +78,62 @@ angular.module('regCartApp')
         {
             label: 'Credits',
             id: 'creditOptions',
-            optionsKey: 'creditOptions' // The default optionsProvider & filter can handle arrays
+            optionsKey: 'creditOptions', // The default optionsProvider & filter can handle arrays
+            refine: function(originalOptions) {
+                var options = [];
+
+                // Bucket all credit options into integer values where 1 matches 1 <= X < 2.
+                // Furthermore, bucket everything larger than 6 into a 6+ option.
+                angular.forEach(originalOptions, function(option) {
+                    var newValue = Math.floor(option.value),
+                        newLabel = newValue;
+
+
+                    // Enforce the 6+ limit
+                    if (newValue > 6) {
+                        newValue = 6;
+                        newLabel = '6+';
+                    }
+
+                    // Check to see if this new option has already been handled
+                    var existingOption = null;
+                    for (var i = 0; i < options.length; i++) {
+                        if (options[i].value.indexOf(newValue) !== -1) {
+                            existingOption = options[i];
+                            break;
+                        }
+                    }
+
+                    if (existingOption === null) {
+                        // The option has not yet been handled. Create it.
+                        option.label = newLabel;
+                        option.count = 0; // Reset the count since it'll be retallied later.
+
+                        // New credit option values are stored as an array since 1 option can match multiple values.
+                        option.value = [option.value];
+                        if (option.value.indexOf(newValue) === -1) {
+                            option.value.push(newValue);
+                        }
+
+                        options.push(option);
+                    } else {
+                        // The option has already been handled. Make sure it contains the value of the current option.
+                        if (existingOption.value.indexOf(option.value) === -1) {
+                            existingOption.value.push(newValue);
+                        }
+
+                        existingOption.label = newLabel;
+                    }
+                });
+
+                return options;
+            }
         },
 
         {
             label: 'Course Level',
             id: 'courseLevel',
-            optionsKey: 'courseLevel',
-            prepare: function(results) {
-                // Create the courseLevel field on the result item.
-                angular.forEach(results, function(item) {
-                    if (angular.isUndefined(item.courseLevel)) {
-                        var level = item.courseNumber.substring(0, 1) + '00';
-                        item.courseLevel = level;
-                    }
-                });
-            }
+            optionsKey: 'courseLevel'
         },
 
         {
--- ks-enroll-ui/src/main/angular/registration/app/scripts/directives/search-facet.js
+++ ks-enroll-ui/src/main/angular/registration/app/scripts/directives/search-facet.js
@@ -25,7 +25,7 @@ angular.module('regCartApp')
                     $scope.facet.optionsProvider = function(results) {
                         var options = [];
 
-                        // The default options provider requiress the optionsKey. Don't proceed if it's not set.
+                        // The default options provider requires the optionsKey. Don't proceed if it's not set.
                         if (angular.isDefined($scope.facet.optionsKey) && $scope.facet.optionsKey !== null) {
                             // Loop through the results and configure an option object for each unique value.
                             angular.forEach(results, function(item) {
@@ -76,23 +76,37 @@ angular.module('regCartApp')
                 // Ensure a filter exists.
                 if (!angular.isFunction($scope.facet.filter)) {
                     $scope.facet.filter = function(item, selectedValues) {
+
+                        // Recursive function used to traverse an array to see if the value exists at any
+                        // level of the array. This allows a single option to be matched to multiple values.
+                        function valueExists(value, array) {
+                            var exists = false;
+                            if (angular.isArray(value)) {
+                                angular.forEach(value, function(v) {
+                                    if (!exists) {
+                                        exists = valueExists(v, array);
+                                    }
+                                });
+                            } else {
+                                if (angular.isArray(array)) {
+                                    angular.forEach(array, function(item) {
+                                        if (!exists) {
+                                            exists = valueExists(value, item);
+                                        }
+                                    });
+                                } else {
+                                    exists = value === array;
+                                }
+                            }
+
+                            return exists;
+                        }
+
                         // The default filter requires the optionsKey field to be set.
                         if (angular.isDefined($scope.facet.optionsKey) && $scope.facet.optionsKey !== null) {
                             if (angular.isDefined(item[$scope.facet.optionsKey])) { // Make sure the item has the optionsKey field
                                 var value = item[$scope.facet.optionsKey];
-
-                                if (!angular.isArray(value)) {
-                                    return selectedValues.indexOf(value) !== -1;
-                                } else {
-                                    var selected = false;
-                                    angular.forEach(value, function(v) {
-                                        if (!selected && selectedValues.indexOf(v) !== -1) {
-                                            selected = true;
-                                        }
-                                    });
-
-                                    return selected;
-                                }
+                                return valueExists(value, selectedValues);
                             }
                         }
 
@@ -102,17 +116,74 @@ angular.module('regCartApp')
 
 
                 /**
-                 * Refresh the facet options based on the current search results
+                 * Tally the count of results that match each option.
+                 * Uses the facet's filter method to check if an option would match a result item.
+                 *
+                 * @param array of options
+                 * @param array of results
+                 * @return array of options with updated counts
                  */
-                function populateOptions(results) {
+                function tallyOptionCounts (options, results) {
+                    // First, reset the counts
+                    angular.forEach(options, function(option) {
+                        option.count = 0;
+                    });
+
+                    // Iterate through the results, if the filter method matches the option, increment the count.
+                    angular.forEach(results, function(item) {
+                        angular.forEach(options, function(option) {
+                            if ($scope.facet.filter(item, option.value)) {
+                                option.count++;
+                            }
+                        });
+                    });
+
+                    return options;
+                }
+
+                /**
+                 * Calculate the array of options from the results list using the facet config.
+                 *
+                 * @param array of results
+                 * @return array of options
+                 */
+                function calculateOptions(results) {
                     // 1. Call prepare() on the facet if that method is defined
                     if (angular.isFunction($scope.facet.prepare)) {
                         $scope.facet.prepare(results);
                     }
 
                     // 2. Build out the facet options based on the current search results.
-                    $scope.options = $scope.facet.optionsProvider(results);
-                    $scope.facet.selectedOptions = [];
+                    var options = $scope.facet.optionsProvider(results);
+
+                    // 3. Call refine() on the facet if that method is defined
+                    if (angular.isFunction($scope.facet.refine)) {
+                        options = $scope.facet.refine(options, results);
+
+                        // The option counts are now unreliable. Retally them.
+                        options = tallyOptionCounts(options, results);
+                    }
+
+                    return options;
+                }
+
+                /**
+                 * Refresh the facet options based on the current search results
+                 *
+                 * @param array of results
+                 */
+                function reset (results) {
+                    // Clear out the selected options
+                    $scope.clearSelectedOptions();
+
+                    // Set the new options into the scope
+                    var options = calculateOptions(results);
+                    $scope.options = options;
+
+                    // Enforce the autoCollapse option when there is only 1 option.
+                    if ((!angular.isDefined($scope.facet.autoCollapse) || $scope.facet.autoCollapse) && options.length === 1) {
+                        $scope.isCollapsed = true;
+                    }
                 }
 
 
@@ -123,7 +194,7 @@ angular.module('regCartApp')
                 // Watch for changes on the results and rebuild if they occur
                 $scope.$watch('results', function(results) {
                     // console.log('Search Results changed, updating facets');
-                    populateOptions(results);
+                    reset(results);
                 });
 
 
