951bb7dd75e3b82459dcf3baadac750524810cbb
vpremcha <vpremcha@kuali.org>
2014-07-31 20:13:15 -0400

    KSCM-2455	 - Decisions - Create a lightbox to show decisions.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/2.0.0-Mx@73185 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-api/src/main/java/org/kuali/student/r2/core/constants/CommentServiceConstants.java
+++ ks-core-api/src/main/java/org/kuali/student/r2/core/constants/CommentServiceConstants.java
@@ -30,16 +30,46 @@ public class CommentServiceConstants {
     public static final String COMMENT_RETURN_WORKFLOW_DECISION_TYPE_KEY = "kuali.comment.type.workflowDecisionRationale.return";
     public static final String COMMENT_WITHDRAW_WORKFLOW_DECISION_TYPE_KEY = "kuali.comment.type.workflowDecisionRationale.withdraw";
 
-    public static final String[] ALL_WORKFLOW_COMMENT = {
-            COMMENT_ACKNOWLEDGE_WORKFLOW_DECISION_TYPE_KEY,
-            COMMENT_APPROVE_WORKFLOW_DECISION_TYPE_KEY,
-            COMMENT_BLANKET_APPROVE_WORKFLOW_DECISION_TYPE_KEY,
-            COMMENT_CANCEL_WORKFLOW_DECISION_TYPE_KEY,
-            COMMENT_FYI_WORKFLOW_DECISION_TYPE_KEY,
-            COMMENT_REJECT_WORKFLOW_DECISION_TYPE_KEY,
-            COMMENT_RETURN_WORKFLOW_DECISION_TYPE_KEY,
-            COMMENT_WITHDRAW_WORKFLOW_DECISION_TYPE_KEY
-    };
+    public static enum WORKFLOW_DECISIONS {
+        GENERAL(COMMENT_GENERAL_REMARKS_TYPE_KEY, "General"),//This will go away once we're done with cm decisions.
+        APPROVE(COMMENT_APPROVE_WORKFLOW_DECISION_TYPE_KEY, "Approved"),
+        REJECT(COMMENT_REJECT_WORKFLOW_DECISION_TYPE_KEY, "Rejected"),
+        RETURN_TO_PREVIOUS(COMMENT_RETURN_WORKFLOW_DECISION_TYPE_KEY, "Sent for Revisions"),
+        WITHDRAW(COMMENT_WITHDRAW_WORKFLOW_DECISION_TYPE_KEY, "Withdrawn"),
+        ACKNOWLEDGE(COMMENT_ACKNOWLEDGE_WORKFLOW_DECISION_TYPE_KEY, "Acknowledged"),
+        FYI(COMMENT_FYI_WORKFLOW_DECISION_TYPE_KEY, "FYI"),
+        CANCEL_WORKFLOW(COMMENT_CANCEL_WORKFLOW_DECISION_TYPE_KEY, "Cancelled"),
+        BLANKET_APPROVE(COMMENT_BLANKET_APPROVE_WORKFLOW_DECISION_TYPE_KEY, "Blanket Approved")
+        ;
+
+        private String type = "";
+        private String label = "";
+
+
+
+        private WORKFLOW_DECISIONS(String type, String label) {
+            this.type = type;
+            this.label = label;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+
+        public static WORKFLOW_DECISIONS getByType(String type) {
+            for (WORKFLOW_DECISIONS detail : WORKFLOW_DECISIONS.values()) {
+                if (detail.getType().equals(type)) {
+                    return detail;
+                }
+            }
+            return null;
+        }
+
+    }
 
     //comment states
     public static final String COMMENT_ACTIVE_STATE_KEY = "kuali.comment.comment.state.active";
