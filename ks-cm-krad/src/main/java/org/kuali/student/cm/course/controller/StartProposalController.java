7ec04a5d7d407136369a31474ca07b38f2bf7d1c
eswaran.muthu <eswaran.muthu@kuali.org>
2014-08-01 17:26:44 -0400

    KSCM-2522 Add "copy from approved course" widgets to the init page
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@73238 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-cm-krad/src/main/java/org/kuali/student/cm/course/controller/StartProposalController.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/course/controller/StartProposalController.java
@@ -76,7 +76,9 @@ public class StartProposalController extends UifControllerBase {
         urlParameters.put(KRADConstants.PARAMETER_COMMAND, KewApiConstants.INITIATE_COMMAND);
         urlParameters.put(KRADConstants.DATA_OBJECT_CLASS_ATTRIBUTE, CourseInfoWrapper.class.getName());
         urlParameters.put(KRADConstants.RETURN_LOCATION_PARAMETER, CMUtils.getCMHomeUrl() );
-        urlParameters.put(CourseController.UrlParams.COPY_CLU_ID, ((CourseInitialForm)form).getCourseCode());
+        if(((CourseInitialForm)form).getCourseCode() != null){
+            urlParameters.put(CourseController.UrlParams.COPY_CLU_ID, ((CourseInitialForm) form).getCourseCode());
+        }
 
         String uri = request.getRequestURL().toString().replace(CurriculumManagementConstants.ControllerRequestMappings.START_PROPOSAL,CurriculumManagementConstants.ControllerRequestMappings.COURSE_MAINTENANCE);
 
