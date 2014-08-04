e039c2f2fbdd7951168bbd4fb118515506de6fe8
vpremcha <vpremcha@kuali.org>
2014-08-04 15:31:51 -0400

    KSCM-2523 - Add action link to View Course to create a course proposal
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@73342 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-cm-krad/src/main/java/org/kuali/student/cm/course/controller/ViewCourseController.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/course/controller/ViewCourseController.java
@@ -17,13 +17,19 @@
 package org.kuali.student.cm.course.controller;
 
 import org.apache.commons.lang.StringUtils;
+import org.kuali.rice.kew.api.KewApiConstants;
+import org.kuali.rice.krad.uif.UifConstants;
+import org.kuali.rice.krad.util.KRADConstants;
 import org.kuali.rice.krad.web.controller.KsUifControllerBase;
 import org.kuali.rice.krad.web.controller.MethodAccessible;
 import org.kuali.rice.krad.web.form.UifFormBase;
+import org.kuali.student.cm.common.util.CMUtils;
 import org.kuali.student.cm.common.util.CurriculumManagementConstants;
+import org.kuali.student.cm.course.form.CourseInitialForm;
 import org.kuali.student.cm.course.form.ViewCourseForm;
 import org.kuali.student.cm.course.form.wrapper.CourseInfoWrapper;
 import org.kuali.student.cm.course.service.CourseMaintainable;
+import org.kuali.student.cm.course.util.CourseProposalUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +37,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Properties;
 
 /**
  * This controller maps to 'View Course' View.
@@ -78,6 +85,34 @@ public class ViewCourseController extends KsUifControllerBase{
         }
 
         return getUIFModelAndView(form);
+    }
+
+    @MethodAccessible
+    @RequestMapping(params = "methodToCall=copyCourse")
+    public ModelAndView copyCourse(@ModelAttribute("KualiForm") UifFormBase form, HttpServletRequest request,
+                              HttpServletResponse response) {
+
+        ViewCourseForm detailedViewForm = (ViewCourseForm) form;
+
+        Properties urlParameters = new Properties();
+        if (!CourseProposalUtil.isUserCurriculumSpecialist()) {
+            // if user is not a CS user, then curriculum review must be used because only CS users can disable curriculum review
+            urlParameters.put(CourseController.UrlParams.USE_CURRICULUM_REVIEW,Boolean.TRUE.toString());
+        } else {
+            // if user is a CS user, check the checkbox value
+            urlParameters.put(CourseController.UrlParams.USE_CURRICULUM_REVIEW,Boolean.toString(((CourseInitialForm) form).isUseReviewProcess()));
+        }
+        urlParameters.put(UifConstants.UrlParams.PAGE_ID, CurriculumManagementConstants.CourseViewPageIds.CREATE_COURSE);
+        urlParameters.put(KRADConstants.DISPATCH_REQUEST_PARAMETER, KRADConstants.DOC_HANDLER_METHOD);
+        urlParameters.put(KRADConstants.PARAMETER_COMMAND, KewApiConstants.INITIATE_COMMAND);
+        urlParameters.put(KRADConstants.DATA_OBJECT_CLASS_ATTRIBUTE, CourseInfoWrapper.class.getName());
+        urlParameters.put(KRADConstants.RETURN_LOCATION_PARAMETER, CMUtils.getCMHomeUrl() );
+        urlParameters.put(CourseController.UrlParams.COPY_CLU_ID, detailedViewForm.getCourseInfoWrapper().getCourseInfo().getId());
+
+        String uri = request.getRequestURL().toString().replace(CurriculumManagementConstants.ControllerRequestMappings.START_PROPOSAL,CurriculumManagementConstants.ControllerRequestMappings.COURSE_MAINTENANCE);
+
+        return performRedirect(form, uri, urlParameters);
+
     }
 
 }
