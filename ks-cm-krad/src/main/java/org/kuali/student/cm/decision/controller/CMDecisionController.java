2c947e7e6543a8087104c0c5c1336ad9d2db93df
delyea <delyea@kuali.org>
2014-08-11 18:03:42 -0400

    KSCM-2575 -- decision rationale lightbox fix for names
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@73777 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-cm-krad/src/main/java/org/kuali/student/cm/decision/controller/CMDecisionController.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/decision/controller/CMDecisionController.java
@@ -18,6 +18,9 @@ package org.kuali.student.cm.decision.controller;
 
 import org.apache.commons.lang.StringUtils;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.kim.api.identity.Person;
+import org.kuali.rice.kim.api.identity.PersonService;
+import org.kuali.rice.kim.api.services.KimApiServiceLocator;
 import org.kuali.rice.krad.web.controller.KsUifControllerBase;
 import org.kuali.rice.krad.web.controller.MethodAccessible;
 import org.kuali.rice.krad.web.form.UifFormBase;
@@ -26,35 +29,23 @@ import org.kuali.student.cm.decision.form.CMDecisionForm;
 import org.kuali.student.cm.decision.form.wrapper.CMDecisionWrapper;
 import org.kuali.student.common.util.security.ContextUtils;
 import org.kuali.student.r1.common.rice.StudentIdentityConstants;
-import org.kuali.student.r1.core.personsearch.service.impl.QuickViewByGivenName;
 import org.kuali.student.r2.common.util.date.DateFormatters;
-import org.kuali.student.r2.core.class1.type.service.TypeService;
 import org.kuali.student.r2.core.comment.dto.CommentInfo;
 import org.kuali.student.r2.core.comment.service.CommentService;
 import org.kuali.student.r2.core.constants.CommentServiceConstants;
 import org.kuali.student.r2.core.constants.ProposalServiceConstants;
-import org.kuali.student.r2.core.constants.TypeServiceConstants;
 import org.kuali.student.r2.core.proposal.dto.ProposalInfo;
 import org.kuali.student.r2.core.proposal.service.ProposalService;
-import org.kuali.student.r2.core.search.dto.SearchParamInfo;
-import org.kuali.student.r2.core.search.dto.SearchRequestInfo;
-import org.kuali.student.r2.core.search.dto.SearchResultCellInfo;
-import org.kuali.student.r2.core.search.dto.SearchResultInfo;
-import org.kuali.student.r2.core.search.dto.SearchResultRowInfo;
-import org.kuali.student.r2.core.search.service.SearchService;
-import org.kuali.student.r2.lum.util.constants.CourseServiceConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.apache.commons.lang3.ArrayUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.namespace.QName;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -71,6 +62,7 @@ public class CMDecisionController extends KsUifControllerBase {
 
     private static final Logger LOG = LoggerFactory.getLogger(CMDecisionController.class);
 
+    protected PersonService personService;
     protected CommentService commentService;
     protected ProposalService proposalService;
 
@@ -121,14 +113,16 @@ public class CMDecisionController extends KsUifControllerBase {
             throw new RuntimeException("Error retrieving decision(s) for the proposal [id=" + proposal.getId() + "]", e);
         }
 
-        List userIds = new ArrayList<>();
+        Map<String,String> personId2DisplayName = new HashMap<>();
 
         for (CommentInfo comment : decisions) {
 
             if (isWorkflowDecision(comment.getTypeKey())){
                 CMDecisionWrapper wrapper = new CMDecisionWrapper(comment);
-                wrapper.setActor(comment.getCommenterId());
-                userIds.add(comment.getCommenterId());
+                if (!personId2DisplayName.containsKey(comment.getCommenterId())) {
+                    personId2DisplayName.put(comment.getCommenterId(), getDisplayNameForPrincipalId(comment.getCommenterId()));
+                }
+                wrapper.setActor(personId2DisplayName.get(comment.getCommenterId()));
                 wrapper.setDate(DateFormatters.MONTH_DAY_YEAR_DATE_FORMATTER.format(comment.getMeta().getCreateTime()));
                 wrapper.setDecision(CommentServiceConstants.WORKFLOW_DECISIONS.getByType(comment.getTypeKey()).getLabel());
 
@@ -138,20 +132,20 @@ public class CMDecisionController extends KsUifControllerBase {
         }
 
         if (!form.getDecisions().isEmpty()){
-
-            Map<String,String> personId2DisplayName = getUserNames(userIds);
-
-            for (CMDecisionWrapper wrapper : form.getDecisions()) {
-                String displayName = personId2DisplayName.get(wrapper.getActor());
-                wrapper.setActor(displayName);
-            }
-
             Collections.sort(form.getDecisions());
         }
 
         LOG.debug("There are " + form.getDecisions().size() + " decisions for proposal " + proposal.getId());
 
 
+    }
+
+    protected String getDisplayNameForPrincipalId(String principalId) {
+        Person person = getPersonService().getPerson(principalId);
+        if (person == null) {
+            throw new RuntimeException("Error fetching person for principal id '" + principalId + "'");
+        }
+        return (new StringBuilder(person.getFirstName())).append(" ").append(person.getLastName()).toString();
     }
 
     protected boolean isWorkflowDecision(String typeKey){
@@ -163,56 +157,11 @@ public class CMDecisionController extends KsUifControllerBase {
         return false;
     }
 
-    protected Map<String,String> getUserNames(List<String> userIds){
-
-        Map<String,String> results = new HashMap<>();
-
-        SearchParamInfo personIdParam = new SearchParamInfo();
-        personIdParam.setKey(QuickViewByGivenName.ID_PARAM);
-        personIdParam.getValues().addAll(userIds);
-        List<SearchParamInfo> queryParamValueList = new ArrayList<SearchParamInfo>();
-        queryParamValueList.add(personIdParam);
-
-        SearchRequestInfo searchRequest = new SearchRequestInfo();
-        searchRequest.setSearchKey(QuickViewByGivenName.SEARCH_TYPE);
-        searchRequest.setParams(queryParamValueList);
-        searchRequest.setStartAt(0);
-        searchRequest.setNeededTotalResults(false);
-        searchRequest.setSortColumn(QuickViewByGivenName.DISPLAY_NAME_RESULT);
-
-        SearchResultInfo searchResult = null;
-        try {
-            SearchService searchService = GlobalResourceLoader.getService(new QName(CourseServiceConstants.NAMESPACE_PERSONSEACH, CourseServiceConstants.PERSONSEACH_SERVICE_NAME_LOCAL_PART));
-            searchResult = searchService.search(searchRequest, ContextUtils.createDefaultContextInfo());
-
-            for (SearchResultRowInfo result : searchResult.getRows()) {
-
-                List<SearchResultCellInfo> cells = result.getCells();
-
-                String displayName = "";
-                String userId = "";
-
-                for (SearchResultCellInfo cell : cells) {
-
-                    if (QuickViewByGivenName.PERSON_ID_RESULT.equals(cell.getKey())) {
-                        userId = cell.getValue();
-                    } else if (QuickViewByGivenName.GIVEN_NAME_RESULT.equals(cell.getKey())) {
-                        String firstName = StringUtils.substringAfter(cell.getValue(),",").trim();
-                        String lastName = StringUtils.substringBefore(cell.getValue(),",").trim();
-                        displayName = firstName + " " + lastName;
-                    }
-                }
-
-                if (StringUtils.isNotBlank(displayName) && StringUtils.isNotBlank(userId)){
-                    results.put(userId,displayName);
-                }
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Error getting decision user names",e);
+    protected PersonService getPersonService() {
+        if (personService == null) {
+            personService = KimApiServiceLocator.getPersonService();
         }
-
-        return results;
-
+        return personService;
     }
 
     protected CommentService getCommentService() {
