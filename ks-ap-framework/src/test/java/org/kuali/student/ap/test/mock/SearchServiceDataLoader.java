679bc30803e4efc638ee0f7218aad780a89679ab
chmaurer <chmaurer@kuali.org>
2014-08-12 13:05:10 -0400

    KSAP-1770 First pass at search cleanup by removing ones that have active replacements as custom ksap searches. The searches that we have left in lu-search-config.xml are ksap.course.info.gened and ksap.course.getcluid and will further look into moving those out as well.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@73848 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-framework/src/test/java/org/kuali/student/ap/test/mock/SearchServiceDataLoader.java
+++ ks-ap-framework/src/test/java/org/kuali/student/ap/test/mock/SearchServiceDataLoader.java
@@ -31,36 +31,6 @@ public class SearchServiceDataLoader extends SearchServiceMockImplAbstractDataLo
         Map<String,String> row2 = new HashMap<String, String>();
         Map<String,String> row3 = new HashMap<String, String>();
 
-        params.put( "subject", new String[]{"PHIL"} );
-        row1.put("lu.resultColumn.cluId", "asdf");
-        this.searchServiceMock.addSearchResult(makeSearchRequestInfo("ksap.course.prereqsearch.subject", params), makeSearchResultInfo(Arrays.asList(row1)));
-
-
-
-        params = new HashMap<String, String[]>();
-        row1 = new HashMap<String, String>();
-
-        params.put( "subject", new String[]{"PHIL"} );
-        params.put( "range", new String[]{"8__"} );
-        row1.put("lu.resultColumn.cluId", "asdf");
-        this.searchServiceMock.addSearchResult(makeSearchRequestInfo("ksap.course.prereqsearch.range", params), makeSearchResultInfo(Arrays.asList(row1)));
-
-        params = new HashMap<String, String[]>();
-        row1 = new HashMap<String, String>();
-        row2 = new HashMap<String, String>();
-
-        params.put( "subject", new String[]{"PHIL"} );
-        params.put( "range", new String[]{"8__"} );
-        row1.put("lu.resultColumn.cluId", "asdf");
-        row1.put("lu.resultColumn.luOptionalCode", "868");
-        row2.put("lu.resultColumn.cluId", "qwerty");
-        row2.put("lu.resultColumn.luOptionalCode", "777");
-        this.searchServiceMock.addSearchResult(makeSearchRequestInfo("ksap.course.prereqsearch.exclusions", params), makeSearchResultInfo(Arrays.asList(row1, row2)));
-
-        params = new HashMap<String, String[]>();
-        row1 = new HashMap<String, String>();
-        row2 = new HashMap<String, String>();
-        row3 = new HashMap<String, String>();
         row1.put("lu.resultColumn.division", "CHEM");
         row2.put("lu.resultColumn.division", "ENGL");
         row3.put("lu.resultColumn.division", "PHYS");
