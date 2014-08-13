e0b1b82cdfefa85f40ae5e26dfacea99a1a1d039
vgadiyak <vgadiyak@kuali.org>
2014-08-13 15:03:44 -0400

    KSENROLL-14115 : searchForCoAndAoInfoByCoId : added letter grading
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@73977 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/search/service/impl/CourseRegistrationSearchServiceImpl.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/registration/search/service/impl/CourseRegistrationSearchServiceImpl.java
@@ -1523,7 +1523,7 @@ public class CourseRegistrationSearchServiceImpl extends SearchServiceAbstractHa
                         // looking for grading and credit options for given CO
                         "LEFT OUTER JOIN KSEN_LUI_RESULT_VAL_GRP coRes " +
                         "ON coRes.LUI_ID = coId.LUI_ID " +
-                        "AND (coRes.RESULT_VAL_GRP_ID in (" + getStudentRegGradingOptionsStr() + ")" +
+                        "AND (coRes.RESULT_VAL_GRP_ID in (" + getStudentRegGradingOptionsStr() + ",'" + LrcServiceConstants.RESULT_GROUP_KEY_GRADE_LETTER + "')" +
                         "     OR coRes.RESULT_VAL_GRP_ID LIKE 'kuali.creditType.credit%') " +
                         // looking for cross-listed courses for given CO
                         "LEFT OUTER JOIN KSEN_LUI_IDENT coClId " +
