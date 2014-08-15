fe911e1359913429e1ab6c572addf35e9a29f6df
lsymms <lsymms@kuali.org>
2014-08-15 13:52:30 -0400

    KSENROLL-14080 minor fix to previous
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-fa/trunk@74135 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-fa-sql/src/main/resource/org/kuali/student/ks-fa-sql/upgrades/01-Phase1/2014-08-15-Sample.sql
+++ ks-fa-sql/src/main/resource/org/kuali/student/ks-fa-sql/upgrades/01-Phase1/2014-08-15-Sample.sql
@@ -1,6 +1,6 @@
 -- Delimiter is / on its own line.  No need for blank line at end of file
 
-begin execute immediate 'drop table KSPL_LRNG_PI_RICH_TEXT CASCADE CONSTRAINTS PURGE'; exception when others then null; end;
+begin execute immediate 'drop table KSFA_SAMPLE_TABLE CASCADE CONSTRAINTS PURGE'; exception when others then null; end;
 /
 CREATE TABLE KSFA_SAMPLE_TABLE
 (
