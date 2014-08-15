a7f1289d2880806eeebb719ce288510a6ac248a1
lsymms <lsymms@kuali.org>
2014-08-15 13:51:52 -0400

    KSENROLL-14080 added test script to ksfa to test impex integration
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-fa/trunk@74134 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-fa-sql/src/main/resource/org/kuali/student/ks-fa-sql/upgrades/01-Phase1/2014-08-15-Sample.sql
+++ ks-fa-sql/src/main/resource/org/kuali/student/ks-fa-sql/upgrades/01-Phase1/2014-08-15-Sample.sql
@@ -1,4 +1,15 @@
 -- Delimiter is / on its own line.  No need for blank line at end of file
+
+begin execute immediate 'drop table KSPL_LRNG_PI_RICH_TEXT CASCADE CONSTRAINTS PURGE'; exception when others then null; end;
+/
+CREATE TABLE KSFA_SAMPLE_TABLE
+(
+  ID VARCHAR2(255) NOT NULL
+, OBJ_ID VARCHAR2(36)
+, CONSTRAINT KSFA_SAMPLE_TABLE_P1 PRIMARY KEY(ID)
+)
+/
+
 select sysdate from dual
 /
 
