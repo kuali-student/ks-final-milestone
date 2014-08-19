b413eecf74cbe143994fbb920296813f0b536417
eswaran.muthu <eswaran.muthu@kuali.org>
2014-08-18 21:46:25 -0400

    KSCM-1536 Cancel Course - Develop Cancel Proposal functionality
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@74284 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-cm-krad/src/main/java/org/kuali/student/cm/common/util/CurriculumManagementConstants.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/common/util/CurriculumManagementConstants.java
@@ -188,7 +188,7 @@ public class CurriculumManagementConstants {
     public final static String FILE_SIZE_CONSTRAINT = "Maximum File Size - 7.5MB";
 
     public final static String COURSE_SUBMIT_CONFIRMATION_DIALOG = "CM-Proposal-Review-Course-ConfirmSubmit-Dialog";
-    public final static String CANCEL_COURSE_CONFIRMATION_DIALOG = "CM-Cancel-Course-Dialog";
+    public final static String CANCEL_COURSE_CONFIRMATION_DIALOG = "CM-Proposal-Review-Course-cancelCourse-Dialog";
 
     public final static String COURSE_RETURN_TO_PREVIOUS_NODE_DIALOG = "CM-Proposal-Review-Course-ConfirmReturnToPreviousNode-Dialog";
     public final static String COURSE_APPROVE_CONFIRMATION_DIALOG = "CM-Proposal-Review-Course-ConfirmApprove-Dialog";
--- ks-cm-krad/src/main/java/org/kuali/student/cm/course/controller/CourseController.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/course/controller/CourseController.java
@@ -333,15 +333,24 @@ public class CourseController extends CourseRuleEditorController {
     @RequestMapping(params = "methodToCall=cancelCourse")
     public ModelAndView cancelCourse(@ModelAttribute("KualiForm") DocumentFormBase form, BindingResult result, HttpServletRequest request, HttpServletResponse response) {
 
+        DialogManager dm = form.getDialogManager();
+        String dialogId = dm.getCurrentDialogId();
+        if(dialogId != null) {
+            dm.setDialogAnswer(dialogId, form.getDialogResponse());
+            dm.setDialogExplanation(dialogId, form.getDialogExplanation());
+            dm.setCurrentDialogId(null);
+        }
+
         String dialog = CurriculumManagementConstants.CANCEL_COURSE_CONFIRMATION_DIALOG;
-        if ( ! hasDialogBeenDisplayed(dialog, form)) {
+        if (!hasDialogBeenDisplayed(dialog, form)) {
             return showDialog(dialog, form, request, response);
         }
         else {
             if (hasDialogBeenAnswered(dialog, form)) {
                 boolean confirmSubmit = getBooleanDialogResponse(dialog, form, request, response);
                 if (confirmSubmit) {
-                    super.cancel(form,result,request,response);
+                    ModelAndView modelAndView = super.cancel(form,result,request,response);
+                    return modelAndView;
                 }   else {
                     form.getDialogManager().removeDialog(dialog);
                 }
