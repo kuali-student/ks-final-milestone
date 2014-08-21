3d75e713d8a7384d8df09c774c97ede060d52456
dcrouse <dcrouse@kuali.org>
2014-08-21 06:43:50 -0400

    KSENROLL-14463: holdIssue api updates
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/2.0.0-Mx@74463 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-api/src/main/java/org/kuali/student/r2/core/hold/dto/HoldIssueInfo.java
+++ ks-core-api/src/main/java/org/kuali/student/r2/core/hold/dto/HoldIssueInfo.java
@@ -15,6 +15,7 @@
 package org.kuali.student.r2.core.hold.dto;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -23,35 +24,62 @@ import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
+import org.apache.xerces.impl.dtd.XMLElementDecl;
 import org.kuali.student.r2.common.dto.IdEntityInfo;
 import org.kuali.student.r2.core.hold.infc.HoldIssue;
-//import org.w3c.dom.Element;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "IssueInfo", propOrder = {
-    "id", "typeKey", "stateKey", "name",
-    "descr", "organizationId", "meta",
-    "attributes", "_futureElements" }) 
+        "id", "typeKey", "stateKey", "name", "descr",
+        "organizationId", "isHoldIssueTermAndDateBased", "isHoldIssueDateBasedOnly",
+        "firstAppliedDate", "lastAppliedDate","firstApplicationTermId", "lastApplicationTermId",
+        "maintainHistoryOfApplicationOfHold","holdCode",
+        "meta",
+        "attributes", "_futureElements" })
 public class HoldIssueInfo extends IdEntityInfo implements HoldIssue, Serializable {
 
     private static final long serialVersionUID = 1L;
     @XmlElement
     private String organizationId;
+    @XmlElement
+    private Boolean isHoldIssueTermAndDateBased;
+    @XmlElement
+    private Boolean isHoldIssueDateBasedOnly;
+    @XmlElement
+    private Date firstAppliedDate;
+    @XmlElement
+    private Date lastAppliedDate;
+    @XmlElement
+    private String firstApplicationTermId;
+    @XmlElement
+    private String lastApplicationTermId;
+    @XmlElement
+    private Boolean maintainHistoryOfApplicationOfHold;
+    @XmlElement
+    private String holdCode;
     @XmlAnyElement
-    private List<Object> _futureElements;  
+    private List<Object> _futureElements;
 
     public HoldIssueInfo() {
     }
 
     /**
      * Constructs a new IssueInfo from another Issue.
-     * 
+     *
      * @param issue the Issue to copy
      */
     public HoldIssueInfo(HoldIssue issue) {
         super(issue);
         if (null != issue) {
             this.organizationId = issue.getOrganizationId();
+            this.isHoldIssueTermAndDateBased = issue.getIsHoldIssueTermAndDateBased();
+            this.isHoldIssueDateBasedOnly = issue.getIsHoldIssueDateBasedOnly();
+            this.firstAppliedDate = issue.getFirstAppliedDate();
+            this.lastAppliedDate = issue.getLastAppliedDate();
+            this.firstApplicationTermId = issue.getFirstApplicationTermId();
+            this.lastApplicationTermId = issue.getLastApplicationTermId();
+            this.maintainHistoryOfApplicationOfHold = issue.getMaintainHistoryOfApplicationOfHold();
+            this.holdCode = issue.getHoldCode();
         }
     }
 
@@ -62,5 +90,69 @@ public class HoldIssueInfo extends IdEntityInfo implements HoldIssue, Serializab
 
     public void setOrganizationId(String orgId) {
         this.organizationId = orgId;
+    }
+
+    public Boolean getIsHoldIssueTermAndDateBased() {
+        return isHoldIssueTermAndDateBased;
+    }
+
+    public void setIsHoldIssueTermAndDateBased(Boolean holdIssueTermAndDateBased) {
+        isHoldIssueTermAndDateBased = holdIssueTermAndDateBased;
+    }
+
+    public Boolean getIsHoldIssueDateBasedOnly() {
+        return isHoldIssueDateBasedOnly;
+    }
+
+    public void setIsHoldIssueDateBasedOnly(Boolean holdIssueDateBasedOnly) {
+        isHoldIssueDateBasedOnly = holdIssueDateBasedOnly;
+    }
+
+    public Date getFirstAppliedDate() {
+        return firstAppliedDate;
+    }
+
+    public void setFirstAppliedDate(Date firstAppliedDate) {
+        this.firstAppliedDate = firstAppliedDate;
+    }
+
+    public Date getLastAppliedDate() {
+        return lastAppliedDate;
+    }
+
+    public void setLastAppliedDate(Date lastAppliedDate) {
+        this.lastAppliedDate = lastAppliedDate;
+    }
+
+    public String getFirstApplicationTermId() {
+        return firstApplicationTermId;
+    }
+
+    public void setFirstApplicationTermId(String firstApplicationTermId) {
+        this.firstApplicationTermId = firstApplicationTermId;
+    }
+
+    public String getLastApplicationTermId() {
+        return lastApplicationTermId;
+    }
+
+    public void setLastApplicationTermId(String lastApplicationTermId) {
+        this.lastApplicationTermId = lastApplicationTermId;
+    }
+
+    public Boolean getMaintainHistoryOfApplicationOfHold() {
+        return maintainHistoryOfApplicationOfHold;
+    }
+
+    public void setMaintainHistoryOfApplicationOfHold(Boolean maintainHistoryOfApplicationOfHold) {
+        this.maintainHistoryOfApplicationOfHold = maintainHistoryOfApplicationOfHold;
+    }
+
+    public String getHoldCode() {
+        return holdCode;
+    }
+
+    public void setHoldCode(String holdCode) {
+        this.holdCode = holdCode;
     }
 }
--- ks-core-api/src/main/java/org/kuali/student/r2/core/hold/infc/HoldIssue.java
+++ ks-core-api/src/main/java/org/kuali/student/r2/core/hold/infc/HoldIssue.java
@@ -17,21 +17,65 @@ package org.kuali.student.r2.core.hold.infc;
 
 import org.kuali.student.r2.common.infc.IdEntity;
 
+import java.util.Date;
+
 
 /**
  * Information about an Issue.
  *
  * @author tom
  * @since Sun May 1 14:22:34 EDT 2011
- */ 
+ */
 
-public interface HoldIssue 
-    extends IdEntity {
+public interface HoldIssue
+        extends IdEntity {
 
     /**
-     * The organization related to this hold category.
+     * The organization related to this hold issue.
      * @name Organization Id
      * @required
      */
     public String getOrganizationId();
+
+    /**
+     * The flag that stores if the HoldIssue is a Term+Date based.
+     * @required
+     */
+    public Boolean getIsHoldIssueTermAndDateBased ();
+
+    /**
+     * The flag that stores if the HoldIssue is only Date-based.
+     * @required
+     */
+    public Boolean getIsHoldIssueDateBasedOnly ();
+
+    /**
+     * Gets the first applied date as of which this HoldIssue can be applied to students.
+     */
+    public Date getFirstAppliedDate ();
+
+    /**
+     * Gets the last applied date as of which this HoldIssue can no longer be applied to students.
+     */
+    public Date getLastAppliedDate ();
+
+    /**
+     * Gets the id of the first application term as of which this (Term-based) HoldIssue may be applied to students.
+     */
+    public String getFirstApplicationTermId();
+
+    /**
+     * Gets the id of the last application term as of which this (Term-based) HoldIssue may no longer be applied to students.
+     */
+    public String getLastApplicationTermId();
+
+    /**
+     * Gets the flag that specifies whether or not a history the application of this Hold to a student is to be maintained.
+     */
+    public Boolean getMaintainHistoryOfApplicationOfHold ();
+
+    /**
+     * Gets the Hold Code (the public identifier for the Hold) for the HoldIssue.
+     */
+    public String getHoldCode ();
 }
