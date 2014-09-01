f644f924e072bb1e7f1789b3800b1c9d46b3fa39
gtaylor <gtaylor@kuali.org>
2014-09-01 10:33:14 -0400

    KSENROLL-14667:remove invalid chars
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@75150 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/07-FR2-M2/2014-09-01-KSEN-TYPE-Fix_lectureorseminar_type.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/07-FR2-M2/2014-09-01-KSEN-TYPE-Fix_lectureorseminar_type.sql
@@ -13,7 +13,7 @@ type_key = 'kuali.lui.type.activity.offering.LectureORSeminar'
 /
 -- add back relationship but with proper values
 insert into KSEN_TYPETYPE_RELTN (CREATEID, CREATETIME, EFF_DT, EXPIR_DT, ID, OBJ_ID, OWNER_TYPE_ID, RANK, RELATED_TYPE_ID, TYPETYPE_RELTN_STATE, TYPETYPE_RELTN_TYPE, UPDATEID, UPDATETIME, VER_NBR)
-values ('SYSTEMLOADER', TIMESTAMP '2012-02-29 19:00:00', null, null, 'kuali.type.type.relation.type.group.kuali.lui.type.grouping.activity.kuali.lui.type.activity.offering.lectureorseminar', null, 'kuali.lui.type.grouping.activity', 0, 'kuali.lui.type.activity.offering.lectureorseminar', 'kuali.type.type.relation.state.active', 'kuali.type.type.relation.type.group', null, null, 0);
+values ('SYSTEMLOADER', TIMESTAMP '2012-02-29 19:00:00', null, null, 'kuali.type.type.relation.type.group.kuali.lui.type.grouping.activity.kuali.lui.type.activity.offering.lectureorseminar', null, 'kuali.lui.type.grouping.activity', 0, 'kuali.lui.type.activity.offering.lectureorseminar', 'kuali.type.type.relation.state.active', 'kuali.type.type.relation.type.group', null, null, 0)
 /
 
 -- -------------------------------------
@@ -31,5 +31,5 @@ type_key = 'kuali.lu.type.activity.LectureORSeminar'
 /
 -- add back relationship but with proper values
 insert into KSEN_TYPETYPE_RELTN (CREATEID, CREATETIME, EFF_DT, EXPIR_DT, ID, OBJ_ID, OWNER_TYPE_ID, RANK, RELATED_TYPE_ID, TYPETYPE_RELTN_STATE, TYPETYPE_RELTN_TYPE, UPDATEID, UPDATETIME, VER_NBR)
-values ('SYSTEMLOADER', TIMESTAMP '2012-02-29 19:00:00', null, null, 'kuali.type.type.relation.type.group.kuali.lu.type.grouping.activity.kuali.lu.type.activity.lectureorseminar', null, 'kuali.lu.type.grouping.activity', 0, 'kuali.lu.type.activity.lectureorseminar', 'kuali.type.type.relation.state.active', 'kuali.type.type.relation.type.group', null, null, 0);
+values ('SYSTEMLOADER', TIMESTAMP '2012-02-29 19:00:00', null, null, 'kuali.type.type.relation.type.group.kuali.lu.type.grouping.activity.kuali.lu.type.activity.lectureorseminar', null, 'kuali.lu.type.grouping.activity', 0, 'kuali.lu.type.activity.lectureorseminar', 'kuali.type.type.relation.state.active', 'kuali.type.type.relation.type.group', null, null, 0)
 /
