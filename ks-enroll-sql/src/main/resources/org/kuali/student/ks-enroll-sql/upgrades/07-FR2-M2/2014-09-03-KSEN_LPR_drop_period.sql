e6fd7632c1e96aaf6ba21ce8ab677ec2fc18976f
vgadiyak <vgadiyak@kuali.org>
2014-09-03 10:41:17 -0400

    KSENROLL-14636 : new file
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@75273 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/07-FR2-M2/2014-09-03-KSEN_LPR_drop_period.sql
@@ -0,0 +1,17 @@
+--  KSENROLL-14636
+-- User L.Victorp registered for 201208/CHEM699/1001 (picked 2 credits)
+insert into KSEN_LPR (OBJ_ID, VER_NBR, CREATEID, CREATETIME, UPDATEID, UPDATETIME, ATP_ID, COMMIT_PERCT, CREDITS, CROSSLIST, EFF_DT, EXPIR_DT, GRADING_OPT_ID, LUI_ID, MASTER_LPR_ID, PERS_ID, LPR_STATE, LPR_TYPE, ID) values ('4cf1ad9c-5bec-48ed-bdf7-fdb6eda90233', 0, 'L.VICTORP', TIMESTAMP '2014-09-03 07:49:26.539', 'L.VICTORP', TIMESTAMP '2014-09-03 07:49:26.539', 'kuali.atp.2012Fall', '', '2.0', '', TIMESTAMP '2014-09-03 00:00:00.0', '', 'kuali.resultComponent.grade.letter', '0691bb96-4254-425f-88cc-c223bd659d44', '9a8b06b1-dd9a-47d5-85c5-42ec409842d5', 'KS-4993', 'kuali.lpr.state.active', 'kuali.lpr.type.registrant.registration.group', '9a8b06b1-dd9a-47d5-85c5-42ec409842d5')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP (LPR_ID, RESULT_VAL_GRP_ID) values ('9a8b06b1-dd9a-47d5-85c5-42ec409842d5', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP (LPR_ID, RESULT_VAL_GRP_ID) values ('9a8b06b1-dd9a-47d5-85c5-42ec409842d5', 'kuali.result.value.credit.degree.2.0')
+/
+insert into KSEN_LPR (OBJ_ID, VER_NBR, CREATEID, CREATETIME, UPDATEID, UPDATETIME, ATP_ID, COMMIT_PERCT, CREDITS, CROSSLIST, EFF_DT, EXPIR_DT, GRADING_OPT_ID, LUI_ID, MASTER_LPR_ID, PERS_ID, LPR_STATE, LPR_TYPE, ID) values ('33bbcb8c-9fe1-4ad4-bccd-54b387909de6', 0, 'L.VICTORP', TIMESTAMP '2014-09-03 07:49:26.539', 'L.VICTORP', TIMESTAMP '2014-09-03 07:49:26.539', 'kuali.atp.2012Fall', '', '2.0', '', TIMESTAMP '2014-09-03 00:00:00.0', '', 'kuali.resultComponent.grade.letter', 'ce30b18d-5003-430c-9b8d-07d320785f4d', '9a8b06b1-dd9a-47d5-85c5-42ec409842d5', 'KS-4993', 'kuali.lpr.state.active', 'kuali.lpr.type.registrant.course.offering', 'f09e7ad7-6001-4fde-866e-150260a040da')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP (LPR_ID, RESULT_VAL_GRP_ID) values ('f09e7ad7-6001-4fde-866e-150260a040da', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP (LPR_ID, RESULT_VAL_GRP_ID) values ('f09e7ad7-6001-4fde-866e-150260a040da', 'kuali.result.value.credit.degree.2.0')
+/
+insert into KSEN_LPR (OBJ_ID, VER_NBR, CREATEID, CREATETIME, UPDATEID, UPDATETIME, ATP_ID, COMMIT_PERCT, CREDITS, CROSSLIST, EFF_DT, EXPIR_DT, GRADING_OPT_ID, LUI_ID, MASTER_LPR_ID, PERS_ID, LPR_STATE, LPR_TYPE, ID) values ('9d390871-ab79-4486-b63e-3392c104d89d', 0, 'L.VICTORP', TIMESTAMP '2014-09-03 07:49:26.539', 'L.VICTORP', TIMESTAMP '2014-09-03 07:49:26.539', 'kuali.atp.2012Fall', '', '', '', TIMESTAMP '2014-09-03 00:00:00.0', '', '', '47fd1950-ac29-4a8b-99e2-1d2a329df74d', '9a8b06b1-dd9a-47d5-85c5-42ec409842d5', 'KS-4993', 'kuali.lpr.state.active', 'kuali.lpr.type.registrant.activity.offering', 'bc401138-c34a-45a0-977d-d8b8e138afa5')
+/
+
