95c2416adf8d6ad83125ea731744127d8c6c6cf5
paulrichardson <paulrichardson@kuali.org>
2014-09-09 15:29:49 -0400

    KSENROLL-14781: updated GES configuration for new data structure
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@75680 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/07-FR2-M2/2014-07-17-KSEN_GES-max-credits.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/07-FR2-M2/2014-07-17-KSEN_GES-max-credits.sql
@@ -10,15 +10,9 @@ insert into KSEN_GES_PARM (ID, NAME, DESCR_PLAIN, GES_PARM_TYPE, GES_PARM_STATE,
 -- Max credits for Summer 1 atp type is 8
 insert into KSEN_GES_VALUE (GES_PARM_ID, GES_VALUE, GES_VALUE_STATE, GES_VALUE_TYPE, GES_VALUE_TYPE_ID, ID, PRIORITY, POPULATION_ID, VER_NBR, CREATEID, CREATETIME) VALUES ('kuali.ges.credit.limit', '8', 'kuali.ges.value.state.active', 'kuali.ges.value.type', 'KUALI_DECIMAL', 'kuali.Summer1.max.credits', 1, 'kuali.population.student.key.everyone', 0, 'admin', TIMESTAMP '2014-07-17 10:57:03')
 /
-insert into KSEN_GES_VALUE_ATP_TYPE (ATP_TYPE, VALUE_ID) VALUES ('kuali.atp.type.Summer1', 'kuali.Summer1.max.credits')
-/
 
 -- Max credits for fall and spring atp types is 20
 insert into KSEN_GES_VALUE (GES_PARM_ID, GES_VALUE, GES_VALUE_STATE, GES_VALUE_TYPE, GES_VALUE_TYPE_ID, ID, PRIORITY, POPULATION_ID, VER_NBR, CREATEID, CREATETIME) VALUES ('kuali.ges.credit.limit', '20', 'kuali.ges.value.state.active', 'kuali.ges.value.type', 'KUALI_DECIMAL', 'kuali.fall.spring.max.credits', 2, 'kuali.population.student.key.everyone', 0, 'admin', TIMESTAMP '2014-07-17 10:57:03')
-/
-insert into KSEN_GES_VALUE_ATP_TYPE (ATP_TYPE, VALUE_ID) VALUES ('kuali.atp.type.Fall', 'kuali.fall.spring.max.credits')
-/
-insert into KSEN_GES_VALUE_ATP_TYPE (ATP_TYPE, VALUE_ID) VALUES ('kuali.atp.type.Spring', 'kuali.fall.spring.max.credits')
 /
 
 -- load calculation value
--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/07-FR2-M2/2014-08-18-KSEN_GES-course-repeatability.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/07-FR2-M2/2014-08-18-KSEN_GES-course-repeatability.sql
@@ -7,19 +7,3 @@ insert into KSEN_GES_PARM (ID, NAME, DESCR_PLAIN, GES_PARM_TYPE, GES_PARM_STATE,
 -- max repeatable value is 2 by default
 insert into KSEN_GES_VALUE (GES_PARM_ID, GES_VALUE, GES_VALUE_STATE, GES_VALUE_TYPE, GES_VALUE_TYPE_ID, ID, PRIORITY, POPULATION_ID, VER_NBR, CREATEID, CREATETIME) VALUES ('kuali.ges.max.repeatable', '2', 'kuali.ges.value.state.active', 'kuali.ges.value.type', 'KUALI_DECIMAL', 'kuali.max.repeatable.default', 1, 'kuali.population.student.key.everyone', 0, 'admin', TIMESTAMP '2014-08-18 15:39:05')
 /
-
--- set the default for all atp types
-insert into KSEN_GES_VALUE_ATP_TYPE (ATP_TYPE, VALUE_ID) VALUES ('kuali.atp.type.Fall', 'kuali.max.repeatable.default')
-/
-insert into KSEN_GES_VALUE_ATP_TYPE (ATP_TYPE, VALUE_ID) VALUES ('kuali.atp.type.HalfFall1', 'kuali.max.repeatable.default')
-/
-insert into KSEN_GES_VALUE_ATP_TYPE (ATP_TYPE, VALUE_ID) VALUES ('kuali.atp.type.HalfFall2', 'kuali.max.repeatable.default')
-/
-insert into KSEN_GES_VALUE_ATP_TYPE (ATP_TYPE, VALUE_ID) VALUES ('kuali.atp.type.Spring', 'kuali.max.repeatable.default')
-/
-insert into KSEN_GES_VALUE_ATP_TYPE (ATP_TYPE, VALUE_ID) VALUES ('kuali.atp.type.Summer1', 'kuali.max.repeatable.default')
-/
-insert into KSEN_GES_VALUE_ATP_TYPE (ATP_TYPE, VALUE_ID) VALUES ('kuali.atp.type.Summer2', 'kuali.max.repeatable.default')
-/
-insert into KSEN_GES_VALUE_ATP_TYPE (ATP_TYPE, VALUE_ID) VALUES ('kuali.atp.type.Winter', 'kuali.max.repeatable.default')
-/
