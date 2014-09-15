5e548fe5071d141fa6a6b36ac4b8da7d6a675864
eswaran.muthu <eswaran.muthu@kuali.org>
2014-09-15 17:05:52 -0400

    KSCM-2755 Conditionally render the Modify with New Version options on init screen
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@76077 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-cm-krad/src/main/java/org/kuali/student/cm/course/controller/StartProposalController.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/course/controller/StartProposalController.java
@@ -57,7 +57,8 @@ public class StartProposalController extends UifControllerBase {
         String versionIndId = httpServletRequest.getParameter(CurriculumManagementConstants.UrlParams.VERSION_IND_ID);
         courseForm.setVersionIndId(versionIndId);
         String courseId = httpServletRequest.getParameter(CurriculumManagementConstants.UrlParams.CLU_ID);
-        courseForm.setModifiableCourse(new Boolean(httpServletRequest.getParameter(CurriculumManagementConstants.UrlParams.IS_MODIFY_NEW_VERSION)));
+        String isModify = httpServletRequest.getParameter(CurriculumManagementConstants.UrlParams.IS_MODIFY_NEW_VERSION);
+        courseForm.setModifiableCourse(new Boolean(isModify));
         courseForm.setCourseWithVersion(new Boolean(httpServletRequest.getParameter(CurriculumManagementConstants.UrlParams.IS_COURSE_WITH_VERSION)));
         courseForm.setCourseId(courseId);
         return courseForm;
