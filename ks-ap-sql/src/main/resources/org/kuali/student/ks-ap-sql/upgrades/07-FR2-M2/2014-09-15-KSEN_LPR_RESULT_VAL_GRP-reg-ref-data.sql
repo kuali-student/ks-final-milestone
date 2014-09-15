145b4c6f33dd1c6e3b875f3186853ada5ee240a0
huangb <huangb@kuali.org>
2014-09-15 15:32:09 -0400

    KSAP-1803: commit SQL scripts to insert 30 registered records for various students. Each registered record maps to 1 LPR record for CO, 1 LPR record for RG and at least 1 LPR record for AO. If a RG contains multiple AOs, need to create 1 LPR record for each AO. Credits info for registration records is stored in KSEN_LPR_RESULT_VAL_GRP table. For each CO and RG type of LPR records, need to create two data entries in KSEN_LPR_RESULT_VAL_GRP, where KSEN_LPR_RESULT_VAL_GRP.LPR_ID == KSEN_LPR.ID
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/trunk@76063 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-ap-sql/src/main/resources/org/kuali/student/ks-ap-sql/upgrades/07-FR2-M2/2014-09-15-KSEN_LPR_RESULT_VAL_GRP-reg-ref-data.sql
@@ -0,0 +1,240 @@
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('b9a09f9e-fca3-41ab-8e16-a3a3bd137_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('b9a09f9e-fca3-41ab-8e16-a3a3bd137_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('b9a09f9e-fca3-41ab-8e16-a3a3bd137_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('b9a09f9e-fca3-41ab-8e16-a3a3bd137_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('be1dd649-8093-4397-9a48-e7d10e70d_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('be1dd649-8093-4397-9a48-e7d10e70d_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('be1dd649-8093-4397-9a48-e7d10e70d_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('be1dd649-8093-4397-9a48-e7d10e70d_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('fd4a3f92-7625-42a0-bf75-03e34863a_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('fd4a3f92-7625-42a0-bf75-03e34863a_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('fd4a3f92-7625-42a0-bf75-03e34863a_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('fd4a3f92-7625-42a0-bf75-03e34863a_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('6ba5fff0-0a09-45d2-8bda-b24513b8e_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('6ba5fff0-0a09-45d2-8bda-b24513b8e_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('6ba5fff0-0a09-45d2-8bda-b24513b8e_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('6ba5fff0-0a09-45d2-8bda-b24513b8e_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('4b0e0860-3815-4219-b419-a386000fc_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('4b0e0860-3815-4219-b419-a386000fc_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('4b0e0860-3815-4219-b419-a386000fc_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('4b0e0860-3815-4219-b419-a386000fc_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('0cc297e0-56f2-451e-b51b-0d7529360_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('0cc297e0-56f2-451e-b51b-0d7529360_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('0cc297e0-56f2-451e-b51b-0d7529360_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('0cc297e0-56f2-451e-b51b-0d7529360_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('1f7b1fea-2394-4489-a7a9-bbe0cd1ec_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('1f7b1fea-2394-4489-a7a9-bbe0cd1ec_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('1f7b1fea-2394-4489-a7a9-bbe0cd1ec_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('1f7b1fea-2394-4489-a7a9-bbe0cd1ec_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('7f6f2852-7a82-4d07-a6d9-836f1bf54_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('7f6f2852-7a82-4d07-a6d9-836f1bf54_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('7f6f2852-7a82-4d07-a6d9-836f1bf54_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('7f6f2852-7a82-4d07-a6d9-836f1bf54_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('e547cc6a-95c9-44db-8dda-98cb5149b_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('e547cc6a-95c9-44db-8dda-98cb5149b_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('e547cc6a-95c9-44db-8dda-98cb5149b_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('e547cc6a-95c9-44db-8dda-98cb5149b_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('2fc57107-5be1-4c74-8e87-0f6c2fac2_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('2fc57107-5be1-4c74-8e87-0f6c2fac2_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('2fc57107-5be1-4c74-8e87-0f6c2fac2_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('2fc57107-5be1-4c74-8e87-0f6c2fac2_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('79fa67fc-3b5e-42a8-bf3d-b2268aac3_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('79fa67fc-3b5e-42a8-bf3d-b2268aac3_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('79fa67fc-3b5e-42a8-bf3d-b2268aac3_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('79fa67fc-3b5e-42a8-bf3d-b2268aac3_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('bcea2bd9-ab09-4c42-93ab-0aed347f6_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('bcea2bd9-ab09-4c42-93ab-0aed347f6_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('bcea2bd9-ab09-4c42-93ab-0aed347f6_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('bcea2bd9-ab09-4c42-93ab-0aed347f6_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('fafe066d-496a-462e-80f0-f62065320_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('fafe066d-496a-462e-80f0-f62065320_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('fafe066d-496a-462e-80f0-f62065320_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('fafe066d-496a-462e-80f0-f62065320_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('b65ee83a-8008-4a8b-98b6-e6c671578_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('b65ee83a-8008-4a8b-98b6-e6c671578_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('b65ee83a-8008-4a8b-98b6-e6c671578_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('b65ee83a-8008-4a8b-98b6-e6c671578_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('97a9b086-6003-4fc8-92ec-8953b32f4_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('97a9b086-6003-4fc8-92ec-8953b32f4_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('97a9b086-6003-4fc8-92ec-8953b32f4_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('97a9b086-6003-4fc8-92ec-8953b32f4_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('28b37f1e-27fa-48e7-aa1c-1266cf607_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('28b37f1e-27fa-48e7-aa1c-1266cf607_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('28b37f1e-27fa-48e7-aa1c-1266cf607_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('28b37f1e-27fa-48e7-aa1c-1266cf607_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('632cc5cb-79bd-4330-8fec-1c9cf8005_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('632cc5cb-79bd-4330-8fec-1c9cf8005_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('632cc5cb-79bd-4330-8fec-1c9cf8005_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('632cc5cb-79bd-4330-8fec-1c9cf8005_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('0339747b-56bc-42b0-b5ab-93f1c5afd_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('0339747b-56bc-42b0-b5ab-93f1c5afd_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('0339747b-56bc-42b0-b5ab-93f1c5afd_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('0339747b-56bc-42b0-b5ab-93f1c5afd_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('e6fc607b-bfb0-4cdd-89e1-5927a8c15_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('e6fc607b-bfb0-4cdd-89e1-5927a8c15_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('e6fc607b-bfb0-4cdd-89e1-5927a8c15_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('e6fc607b-bfb0-4cdd-89e1-5927a8c15_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('75462d2b-3958-4418-84eb-da24ef9c2_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('75462d2b-3958-4418-84eb-da24ef9c2_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('75462d2b-3958-4418-84eb-da24ef9c2_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('75462d2b-3958-4418-84eb-da24ef9c2_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('a6fdb8fd-ca75-4d77-8210-391325ea4_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('a6fdb8fd-ca75-4d77-8210-391325ea4_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('a6fdb8fd-ca75-4d77-8210-391325ea4_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('a6fdb8fd-ca75-4d77-8210-391325ea4_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('5ff4cd18-8d5c-4563-bfa4-6204a1998_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('5ff4cd18-8d5c-4563-bfa4-6204a1998_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('5ff4cd18-8d5c-4563-bfa4-6204a1998_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('5ff4cd18-8d5c-4563-bfa4-6204a1998_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('7dd46d94-c4c1-4529-8e30-47366850d_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('7dd46d94-c4c1-4529-8e30-47366850d_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('7dd46d94-c4c1-4529-8e30-47366850d_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('7dd46d94-c4c1-4529-8e30-47366850d_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('287f53f6-c1df-43d6-ba1d-605c1603e_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('287f53f6-c1df-43d6-ba1d-605c1603e_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('287f53f6-c1df-43d6-ba1d-605c1603e_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('287f53f6-c1df-43d6-ba1d-605c1603e_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('041ff197-b4ff-446b-a016-69658f210_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('041ff197-b4ff-446b-a016-69658f210_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('041ff197-b4ff-446b-a016-69658f210_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('041ff197-b4ff-446b-a016-69658f210_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('e9fd721c-c2cf-48b0-b81b-543a837a0_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('e9fd721c-c2cf-48b0-b81b-543a837a0_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('e9fd721c-c2cf-48b0-b81b-543a837a0_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('e9fd721c-c2cf-48b0-b81b-543a837a0_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('34bdbdd4-4491-44c3-9b17-6cd02675d_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('34bdbdd4-4491-44c3-9b17-6cd02675d_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('34bdbdd4-4491-44c3-9b17-6cd02675d_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('34bdbdd4-4491-44c3-9b17-6cd02675d_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('7b73f2d4-4e31-491c-bc98-2bd7c6f1f_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('7b73f2d4-4e31-491c-bc98-2bd7c6f1f_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('7b73f2d4-4e31-491c-bc98-2bd7c6f1f_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('7b73f2d4-4e31-491c-bc98-2bd7c6f1f_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('d435749d-8551-4178-a94e-0e70f824a_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('d435749d-8551-4178-a94e-0e70f824a_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('d435749d-8551-4178-a94e-0e70f824a_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('d435749d-8551-4178-a94e-0e70f824a_RG', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('b661e024-91cf-45a4-8f61-8dd215bc2_CO', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('b661e024-91cf-45a4-8f61-8dd215bc2_CO', 'kuali.resultComponent.grade.letter')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('b661e024-91cf-45a4-8f61-8dd215bc2_RG', 'kuali.result.value.credit.degree.3.0')
+/
+insert into KSEN_LPR_RESULT_VAL_GRP(LPR_ID, RESULT_VAL_GRP_ID) values ('b661e024-91cf-45a4-8f61-8dd215bc2_RG', 'kuali.resultComponent.grade.letter')
+/
