ce5ea79f90ccaacf9c24b568706c65415ecf3bb4
komandur <komandur@kuali.org>
2014-09-17 14:35:58 -0400

    kscm-2800 Preliminary checkin to get QA going TODO: Postprocessor
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/2.0.0-Mx@76260 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-api/src/main/java/org/kuali/student/r2/core/constants/AtpSearchServiceConstants.java
+++ ks-core-api/src/main/java/org/kuali/student/r2/core/constants/AtpSearchServiceConstants.java
@@ -54,6 +54,7 @@ public class AtpSearchServiceConstants {
     public static final String ATP_ADVANCED_QUERYPARAM_ATP_TYPE = "atp.advancedAtpSearchParam.atpType";
     public static final String ATP_ADVANCED_QUERYPARAM_ATP_YEAR = "atp.advancedAtpSearchParam.atpYear";
     public static final String ATP_ADVANCED_QUERYPARAM_ATP_SHORT_NAME = "atp.advancedAtpSearchParam.atpShortName";
+    public static final String ATP_ADVANCED_QUERYPARAM_ATP_END_DATE_CONSTRAINT_EXCLUSIVE="atp.advancedAtpSearchParam.atpEndDateAtpConstraintIdExclusive";
     public static final String ATP_ADVANCED_ATP_SEARCH_PARAM_OPTIONAL_ATP_IDS = "atp.advancedAtpSearchParam.optionalAtpIds";
 
     public static final String ATP_RESULTCOLUMN_ATP_ID = "atp.resultColumn.atpId";
