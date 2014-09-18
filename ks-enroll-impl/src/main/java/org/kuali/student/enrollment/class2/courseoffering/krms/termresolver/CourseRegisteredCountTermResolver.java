2750be30e2581676b37ccfa8193acc2aad4c7842
paulrichardson <paulrichardson@kuali.org>
2014-09-18 15:46:45 -0400

    KSENROLL-14953,  KSENROLL-14954: dupe reg check is now only for the same course code and term
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@76360 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseoffering/krms/termresolver/CourseRegisteredCountTermResolver.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class2/courseoffering/krms/termresolver/CourseRegisteredCountTermResolver.java
@@ -20,11 +20,17 @@
 package org.kuali.student.enrollment.class2.courseoffering.krms.termresolver;
 
 import org.kuali.rice.krms.api.engine.TermResolutionException;
-import org.kuali.rice.krms.api.engine.TermResolver;
-import org.kuali.student.enrollment.academicrecord.dto.StudentCourseRecordInfo;
-import org.kuali.student.r2.common.util.constants.AcademicRecordServiceConstants;
+import org.kuali.student.common.util.krms.RulesExecutionConstants;
+import org.kuali.student.enrollment.class2.courseoffering.krms.termresolver.util.CourseOfferingTermResolverSupport;
+import org.kuali.student.enrollment.courseregistration.dto.CourseRegistrationInfo;
+import org.kuali.student.enrollment.courseregistration.dto.RegistrationRequestItemInfo;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.krms.util.KSKRMSExecutionUtil;
 import org.kuali.student.r2.core.constants.KSKRMSServiceConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -36,7 +42,9 @@ import java.util.Set;
  *
  * @author Kuali Student Team
  */
-public class CourseRegisteredCountTermResolver implements TermResolver<Integer> {
+public class CourseRegisteredCountTermResolver extends CourseOfferingTermResolverSupport<Integer> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CourseRegisteredCountTermResolver.class);
 
     @Override
     public String getOutput() {
@@ -50,29 +58,50 @@ public class CourseRegisteredCountTermResolver implements TermResolver<Integer> 
 
     @Override
     public Set<String> getPrerequisites() {
-        Set<String> prereqs = new HashSet<>(1);
-        prereqs.add(KSKRMSServiceConstants.TERM_RESOLVER_COURSE_RECORD_FOR_STUDENT);
+        Set<String> prereqs = new HashSet<>();
+        prereqs.add(RulesExecutionConstants.CONTEXT_INFO_TERM.getName());
+        prereqs.add(RulesExecutionConstants.REGISTRATION_REQUEST_ITEM_TERM.getName());
+        prereqs.add(RulesExecutionConstants.EXISTING_REGISTRATIONS_TERM.getName());
+        prereqs.add(RulesExecutionConstants.EXISTING_WAITLISTED_REGISTRATIONS_TERM.getName());
+        prereqs.add(RulesExecutionConstants.SIMULATED_REGISTRATIONS_TERM.getName());
 
         return Collections.unmodifiableSet(prereqs);
     }
 
     @Override
     public int getCost() {
-        return 0;
+        return 1;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public Integer resolve(Map<String, Object> resolvedPrereqs, Map<String, String> parameters) throws TermResolutionException {
 
         int registeredCount = 0;
 
-        @SuppressWarnings("unchecked")
-        List<StudentCourseRecordInfo> studentCourseRecordInfoList = (List<StudentCourseRecordInfo>) resolvedPrereqs.get(KSKRMSServiceConstants.TERM_RESOLVER_COURSE_RECORD_FOR_STUDENT);
+        // Resolve pre-requisite terms
+        ContextInfo contextInfo = (ContextInfo) resolvedPrereqs.get(RulesExecutionConstants.CONTEXT_INFO_TERM.getName());
+        RegistrationRequestItemInfo requestItemInfo = (RegistrationRequestItemInfo) resolvedPrereqs.get(RulesExecutionConstants.REGISTRATION_REQUEST_ITEM_TERM.getName());
+        List<CourseRegistrationInfo> existingRegistrations = (List<CourseRegistrationInfo>) resolvedPrereqs.get(RulesExecutionConstants.EXISTING_REGISTRATIONS_TERM.getName());
+        List<CourseRegistrationInfo> existingWaitlist = (List<CourseRegistrationInfo>) resolvedPrereqs.get(RulesExecutionConstants.EXISTING_WAITLISTED_REGISTRATIONS_TERM.getName());
+        List<CourseRegistrationInfo> simulatedRegistrations = (List<CourseRegistrationInfo>) resolvedPrereqs.get(RulesExecutionConstants.SIMULATED_REGISTRATIONS_TERM.getName());
+        try {
+            CourseRegistrationInfo regItem = createNewCourseRegistration(requestItemInfo, contextInfo);
+            String regCourseOfferingId = regItem.getCourseOfferingId();
 
-        for (StudentCourseRecordInfo studentCourseRecordInfo:studentCourseRecordInfoList) {
-            if (studentCourseRecordInfo.getStateKey().equals(AcademicRecordServiceConstants.STUDENTCOURSERECORD_STATE_KEY_REGISTERED)) {
-                registeredCount++;
+            List<CourseRegistrationInfo> crList = new ArrayList<>();
+            crList.addAll(existingRegistrations);
+            crList.addAll(existingWaitlist);
+            crList.addAll(simulatedRegistrations);
+
+            for (CourseRegistrationInfo cr:crList) {
+                if (regCourseOfferingId.equals(cr.getCourseOfferingId())) {
+                    registeredCount++;
+                }
             }
+        } catch (Exception ex) {
+            LOGGER.error("Exception trying to course registered count", ex);
+            KSKRMSExecutionUtil.convertExceptionsToTermResolutionException(parameters, ex, this);
         }
 
         return registeredCount;
