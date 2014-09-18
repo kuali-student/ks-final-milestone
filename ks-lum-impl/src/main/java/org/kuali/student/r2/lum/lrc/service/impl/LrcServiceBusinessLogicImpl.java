8a63d7acb0b5b5921766ec94106c3fa3e3d23221
depstein <depstein@kuali.org>
2014-09-18 11:51:22 -0400

    KSENROLL-14910 update course reg to use RVG for credits and not values.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@76324 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum-impl/src/main/java/org/kuali/student/r2/lum/lrc/service/impl/LrcServiceBusinessLogicImpl.java
+++ ks-lum-impl/src/main/java/org/kuali/student/r2/lum/lrc/service/impl/LrcServiceBusinessLogicImpl.java
@@ -53,7 +53,7 @@ public class LrcServiceBusinessLogicImpl implements LrcServiceBusinessLogic {
             throws InvalidParameterException {
 
         if (scaleKey.equals(LrcServiceConstants.RESULT_SCALE_KEY_CREDIT_DEGREE)) {
-            return LrcServiceConstants.RESULT_GROUP_KEY_KUALI_CREDITTYPE_CREDIT_BASE_OLD + creditValue;
+            return LrcServiceConstants.RESULT_GROUP_KEY_KUALI_CREDITTYPE_CREDIT_BASE + creditValue;
         }
         if (scaleKey.equals(LrcServiceConstants.RESULT_SCALE_KEY_CREDIT_REMEDIAL)) {
             return LrcServiceConstants.RESULT_GROUP_KEY_CREDIT_REMEDIAL_FIXED_BASE + creditValue;
@@ -166,7 +166,7 @@ public class LrcServiceBusinessLogicImpl implements LrcServiceBusinessLogic {
 
         if (scaleKey.equals(LrcServiceConstants.RESULT_SCALE_KEY_CREDIT_DEGREE)) {
             StringBuilder sb = new StringBuilder();
-            sb.append(LrcServiceConstants.RESULT_GROUP_KEY_KUALI_CREDITTYPE_CREDIT_BASE_OLD);
+            sb.append(LrcServiceConstants.RESULT_GROUP_KEY_KUALI_CREDITTYPE_CREDIT_BASE);
             sb.append(creditValueMin).append("-").append(creditValueMax);
             if (!creditValueIncrement.equals("1")) {
                 sb.append(".by.").append(creditValueIncrement);
@@ -339,8 +339,8 @@ public class LrcServiceBusinessLogicImpl implements LrcServiceBusinessLogic {
             StringBuilder sb = new StringBuilder();
 
             // so the base string has a "." at the end... remove it
-            String baseType = LrcServiceConstants.RESULT_GROUP_KEY_KUALI_CREDITTYPE_CREDIT_BASE_OLD.substring(0,
-                                LrcServiceConstants.RESULT_GROUP_KEY_KUALI_CREDITTYPE_CREDIT_BASE_OLD.length() - 1);
+            String baseType = LrcServiceConstants.RESULT_GROUP_KEY_KUALI_CREDITTYPE_CREDIT_BASE.substring(0,
+                                LrcServiceConstants.RESULT_GROUP_KEY_KUALI_CREDITTYPE_CREDIT_BASE.length() - 1);
 
             sb.append(baseType);
             sb.append(".");
