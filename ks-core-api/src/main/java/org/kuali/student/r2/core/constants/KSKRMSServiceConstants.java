cdafd6ee8473ed33978f5cea8445b0c6c91cb720
jmorris <jmorris@kuali.org>
2014-09-18 15:41:43 -0400

    KSENROLL-14915: Implemented term resolver for Reg Group state & integrated it as a reg group is offered check in the course add processes.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/2.0.0-Mx@76359 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-api/src/main/java/org/kuali/student/r2/core/constants/KSKRMSServiceConstants.java
+++ ks-core-api/src/main/java/org/kuali/student/r2/core/constants/KSKRMSServiceConstants.java
@@ -278,6 +278,7 @@ public class KSKRMSServiceConstants {
     public static final String TERM_RESOLVER_CUSTOM_COURSE_REPEATABILITY_RULE = "CustomCourseRepeatabilityRule";
     public static final String TERM_RESOLVER_BEST_EFFORT_CREDIT_LOAD = "BestEffortCreditLoad";
     public static final String TERM_RESOLVER_APPOINTMENT_WINDOW = "AppointmentWindow";
+    public static final String TERM_RESOLVER_REG_GROUP_STATE = "RegGroupState";
 
     // natural language usage keys
     public static final String KRMS_NL_RULE_EDIT = "kuali.krms.edit";
