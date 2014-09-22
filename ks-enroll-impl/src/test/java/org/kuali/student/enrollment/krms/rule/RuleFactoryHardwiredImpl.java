08e2bde7da5a16e151e49175eb1512901d6123bf
paulrichardson <paulrichardson@kuali.org>
2014-09-22 18:31:55 -0400

    KSENROLL-14977: delete deprecated code
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@76580 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/test/java/org/kuali/student/enrollment/krms/rule/RuleFactoryHardwiredImpl.java
+++ ks-enroll-impl/src/test/java/org/kuali/student/enrollment/krms/rule/RuleFactoryHardwiredImpl.java
@@ -3,10 +3,7 @@ package org.kuali.student.enrollment.krms.rule;
 import org.kuali.rice.krms.framework.engine.BasicRule;
 import org.kuali.rice.krms.framework.engine.Proposition;
 import org.kuali.rice.krms.framework.engine.Rule;
-import org.kuali.student.core.krms.proposition.IsAliveProposition;
 import org.kuali.student.core.krms.rule.RuleFactory;
-import org.kuali.student.enrollment.krms.proposition.BestEffortCreditLoadProposition;
-import org.kuali.student.enrollment.krms.proposition.BestEffortTimeConflictProposition;
 import org.kuali.student.enrollment.krms.proposition.CreditLimitProposition;
 import org.kuali.student.enrollment.krms.proposition.CreditLoadProposition;
 import org.kuali.student.enrollment.krms.proposition.CreditMinimumProposition;
@@ -15,9 +12,6 @@ import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.exceptions.DoesNotExistException;
 import org.kuali.student.r2.common.exceptions.OperationFailedException;
 import org.kuali.student.r2.core.constants.KSKRMSServiceConstants;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by SW Genis on 2014/06/14.
@@ -34,40 +28,32 @@ public class RuleFactoryHardwiredImpl extends
 
     // checks
     public static final String RULE_ID_CREDIT_LOAD = "kuali.rule.credit.load";
-    public static final String RULE_ID_TIME_CONFLICT_BEST_EFFORT = "kuali.rule.best.effort.time.conflict";
     public static final String RULE_ID_CREDIT_LIMIT = "kuali.rule.credit.limit";
     public static final String RULE_ID_CREDIT_MINIMUM = "kuali.rule.credit.minimum";
-    public static final String RULE_ID_CREDIT_LOAD_BEST_EFFORT = "kuali.rule.best.effort.credit.load";
     public static final String RULE_ID_REQUISITES = "kuali.rule.requisites";
 
     protected Proposition getProposition(String ruleId, ContextInfo contextInfo) throws DoesNotExistException,
             OperationFailedException {
-
-        if (ruleId.equals(RULE_ID_CREDIT_LOAD)) {
-            Proposition prop = new CreditLoadProposition();
-            return prop;
+        Proposition prop = null;
+        switch (ruleId) {
+            case RULE_ID_CREDIT_LOAD:
+                prop = new CreditLoadProposition();
+                break;
+            case RULE_ID_CREDIT_LIMIT:
+                prop = new CreditLimitProposition();
+                break;
+            case RULE_ID_CREDIT_MINIMUM:
+                prop = new CreditMinimumProposition();
+                break;
+            case RULE_ID_REQUISITES:
+                prop = new RequisitesProposition(KSKRMSServiceConstants.AGENDA_TYPE_COURSE_ENROLLMENTELIGIBILITY,
+                        KSKRMSServiceConstants.RULE_TYPE_COURSE_ACADEMICREADINESS_ANTIREQ);
+                break;
+            default:
+                super.getProposition(ruleId, contextInfo);
+                break;
         }
-        if (ruleId.equals(RULE_ID_TIME_CONFLICT_BEST_EFFORT)) {
-            Proposition prop = new BestEffortTimeConflictProposition();
-            return prop;
-        }
-        if (ruleId.equals(RULE_ID_CREDIT_LIMIT)) {
-            Proposition prop = new CreditLimitProposition();
-            return prop;
-        }
-        if (ruleId.equals(RULE_ID_CREDIT_MINIMUM)) {
-            Proposition prop = new CreditMinimumProposition();
-            return prop;
-        }
-        if (ruleId.equals(RULE_ID_CREDIT_LOAD_BEST_EFFORT)) {
-            Proposition prop = new BestEffortCreditLoadProposition();
-            return prop;
-        }
-        if (ruleId.equals(RULE_ID_REQUISITES)) {
-            Proposition prop = new RequisitesProposition(KSKRMSServiceConstants.AGENDA_TYPE_COURSE_ENROLLMENTELIGIBILITY, KSKRMSServiceConstants.RULE_TYPE_COURSE_ACADEMICREADINESS_ANTIREQ);
-            return prop;
-        }
-        return super.getProposition(ruleId, contextInfo);
+        return prop;
     }
 
 }
