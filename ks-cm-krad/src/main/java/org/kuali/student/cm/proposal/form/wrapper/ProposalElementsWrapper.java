698049b7f4627e1845dda637bbac13049142385c
prasannag <prasannag@kuali.org>
2014-09-24 03:06:49 -0400

    KSCM-2781 - Header text renders incorrectly on modify action - Fixed
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-lum/trunk@76724 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-cm-krad/src/main/java/org/kuali/student/cm/proposal/form/wrapper/ProposalElementsWrapper.java
+++ ks-cm-krad/src/main/java/org/kuali/student/cm/proposal/form/wrapper/ProposalElementsWrapper.java
@@ -265,7 +265,7 @@ public abstract class ProposalElementsWrapper extends LURuleManagementWrapper im
 
             if (isModifyWithoutNewVersionProposal()) {
                 headerSuffixText = " (Admin Update)";
-            } else if (!isUseReviewProcess() && isModifyWithNewVersionProposal()) {
+            } else if (!isUseReviewProcess()) {
                 headerSuffixText = " (Admin Proposal)";
             } else {
                 headerSuffixText = " (Proposal)";
