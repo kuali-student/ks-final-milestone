17dabc574927247d9b86cec728c9ce08f8b87977
sw <sw@kuali.org>
2014-09-29 06:18:30 -0400

    KSENROLL-15010 - Fixed requisites check to only perform check once.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@76986 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/main/java/org/kuali/student/enrollment/krms/proposition/RequisitesProposition.java
+++ ks-enroll-impl/src/main/java/org/kuali/student/enrollment/krms/proposition/RequisitesProposition.java
@@ -17,6 +17,7 @@ import org.kuali.rice.krms.framework.engine.Rule;
 import org.kuali.rice.krms.framework.engine.result.BasicResult;
 import org.kuali.student.common.util.krms.RulesExecutionConstants;
 import org.kuali.student.common.util.krms.proposition.AbstractLeafProposition;
+import org.kuali.student.core.process.evaluator.AbstractCheckProposition;
 import org.kuali.student.core.process.evaluator.KRMSEvaluator;
 import org.kuali.student.enrollment.class2.courseoffering.krms.service.RequisitesService;
 import org.kuali.student.enrollment.courseoffering.infc.RegistrationGroup;
@@ -28,6 +29,8 @@ import org.kuali.student.r2.common.dto.ContextInfo;
 import org.kuali.student.r2.common.dto.ValidationResultInfo;
 import org.kuali.student.r2.common.infc.ValidationResult;
 import org.kuali.student.r2.common.util.constants.LprServiceConstants;
+import org.kuali.student.r2.core.process.dto.CheckInfo;
+import org.kuali.student.r2.core.process.dto.InstructionInfo;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -55,37 +58,27 @@ public class RequisitesProposition extends AbstractLeafProposition {
     @Override
     public PropositionResult evaluate(ExecutionEnvironment environment) {
         ContextInfo contextInfo = environment.resolveTerm(RulesExecutionConstants.CONTEXT_INFO_TERM, this);
-        RegistrationRequestInfo request = environment.resolveTerm(RulesExecutionConstants.REGISTRATION_REQUEST_TERM, this);
+        RegistrationRequestItemInfo requestItem = environment.resolveTerm(RulesExecutionConstants.REGISTRATION_REQUEST_ITEM_TERM, this);
 
-        List<ValidationResult> validationResults = new ArrayList<ValidationResult>();
         // get all the needed services from the execution context
         // we do this so we can locally cache some services and the cache lives just for the length of krms execution
         CourseOfferingService courseOfferingService = environment.resolveTerm(RulesExecutionConstants.COURSE_OFFERING_SERVICE_TERM, this);
-        for (RegistrationRequestItemInfo item : request.getRegistrationRequestItems()) {
-            RegistrationGroup regGroup;
-            try {
-                regGroup = courseOfferingService.getRegistrationGroup(item.getRegistrationGroupId(), contextInfo);
-            } catch (Exception ex) {
-                return KRMSEvaluator.constructExceptionPropositionResult(environment, ex, this);
-            }
+        try {
+            RegistrationGroup regGroup = courseOfferingService.getRegistrationGroup(requestItem.getRegistrationGroupId(), contextInfo);
 
             if (!executeRuleForRegGroupAndType(environment, regGroup)) {
-                String msg = RegistrationValidationResultsUtil.marshallSimpleMessage(LprServiceConstants.LPRTRANS_ITEM_FAILED_ANTI_REQUISITES_MESSAGE_KEY);
                 ValidationResultInfo vr = new ValidationResultInfo();
                 vr.setLevel(ValidationResult.ErrorLevel.ERROR);
-                String elt = "registrationRequestItems['" + item.getId() + "']";
-                vr.setElement(elt);
-                vr.setMessage(msg);
-                validationResults.add(vr);
+                vr.setElement("registrationRequestItems['" + requestItem.getId() + "']");
+                vr.setMessage(RegistrationValidationResultsUtil.marshallSimpleMessage(LprServiceConstants.LPRTRANS_ITEM_FAILED_ANTI_REQUISITES_MESSAGE_KEY));
+                return KRMSEvaluator.buildPropositionResult(environment, vr, false, this);
             }
+
+        } catch (Exception ex) {
+            return KRMSEvaluator.constructExceptionPropositionResult(environment, ex, this);
         }
 
-        Map<String, Object> executionDetails = new LinkedHashMap<String, Object>();
-        executionDetails.put(RulesExecutionConstants.PROCESS_EVALUATION_RESULTS, validationResults);
-        PropositionResult result = new PropositionResult(validationResults.size() > 0 ? false : true, executionDetails);
-        BasicResult br = new BasicResult(ResultEvent.PROPOSITION_EVALUATED, this, environment, result.getResult());
-        environment.getEngineResults().addResult(br);
-        return result;
+        return KRMSEvaluator.buildPropositionResult(environment, true, this);
     }
 
     private boolean executeRuleForRegGroupAndType(ExecutionEnvironment environment, RegistrationGroup regGroup) {
