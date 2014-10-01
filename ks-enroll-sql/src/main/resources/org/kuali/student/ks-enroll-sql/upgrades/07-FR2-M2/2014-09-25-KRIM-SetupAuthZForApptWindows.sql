00cefd57f3d2dcce1e5674e439aa330553fd429b
jmorris <jmorris@kuali.org>
2014-10-01 16:14:07 -0400

    KSENROLL-15094: Added authz role for RegWindows page w/ martha & admin as its members.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/trunk@77165 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/07-FR2-M2/2014-09-25-KRIM-SetupAuthZForApptWindows.sql
+++ ks-enroll-sql/src/main/resources/org/kuali/student/ks-enroll-sql/upgrades/07-FR2-M2/2014-09-25-KRIM-SetupAuthZForApptWindows.sql
@@ -8,7 +8,26 @@ insert into KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, ATTR_VAL, KIM_ATTR_DEFN_ID, KIM
 values ('KS-KRIM-PERM-ATTR-DATA-4007', 'registrationWindowsManagementView', '47', '1', '1488072BB63E6C71E050440A9B9A1ED2', 'KS-KRIM-PERM-4004', 1)
 /
 
---Tie the permission to the 'Central Registration Personnel Role'
+
+--KSENROLL-15094 Add new role specifically for Appt Windows w/ martha & admin in it
+
+--Create the new role
+insert into KRIM_ROLE_T (ACTV_IND, DESC_TXT, KIM_TYP_ID, LAST_UPDT_DT, NMSPC_CD, OBJ_ID, ROLE_ID, ROLE_NM, VER_NBR)
+values ('Y', 'KS Administrator with global abilities Appointment Windows Permission', 1, SYSDATE, 'KS-ENR', '0d71c654-667f-4c3c-b9e0-af52b5e4d08f', 'KS-10008', 'Appointment Windows Personnel Role', 1)
+/
+
+
+--Add 'martha' & 'admin' to the new role
+insert into KRIM_ROLE_MBR_T (LAST_UPDT_DT, MBR_ID, MBR_TYP_CD, OBJ_ID, ROLE_ID, ROLE_MBR_ID, VER_NBR)
+values (SYSDATE, 'admin', 'P', '06ba8d9e-a2ca-449e-bceb-17b9d3b12913', 'KS-10008', 'KS-10014', 1)
+/
+
+insert into KRIM_ROLE_MBR_T (LAST_UPDT_DT, MBR_ID, MBR_TYP_CD, OBJ_ID, ROLE_ID, ROLE_MBR_ID, VER_NBR)
+values (SYSDATE, 'martha', 'P', 'b0cbf342-2dc9-444e-a92f-f9bb5054a9da', 'KS-10008', 'KS-10015', 1)
+/
+
+
+--Tie the permission to the new role
 insert into KRIM_ROLE_PERM_T (ACTV_IND, OBJ_ID, PERM_ID, ROLE_ID, ROLE_PERM_ID, VER_NBR)
-values ('Y', '1477072DD1386C71E050440A9B9A3CE3', 'KS-KRIM-PERM-4004', 'KS-10003', 'KS-KRIM-ROLE-PERM-4005', 1)
+values ('Y', 'faa57c50-83cd-423c-ab56-fdc7ad4b0c09', 'KS-KRIM-PERM-4004', 'KS-10008', 'KS-KRIM-ROLE-PERM-4006', 1)
 /
