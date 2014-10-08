51a7478e0e8fb577f6a8ab8c54aebbb5af60dafb
sw <sw@kuali.org>
2014-10-08 03:43:37 -0400

    KSENROLL-15163 - Fixed term based hold check.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/trunk@77530 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-impl/src/main/java/org/kuali/student/core/process/evaluator/HoldCheckProposition.java
+++ ks-core-impl/src/main/java/org/kuali/student/core/process/evaluator/HoldCheckProposition.java
@@ -14,11 +14,17 @@ package org.kuali.student.core.process.evaluator;
 
 import org.kuali.rice.krms.api.engine.ExecutionEnvironment;
 import org.kuali.rice.krms.framework.engine.PropositionResult;
+import org.kuali.student.common.date.utils.EffectiveDateUtils;
 import org.kuali.student.common.util.krms.RulesExecutionConstants;
+import org.kuali.student.r2.core.atp.infc.Atp;
+import org.kuali.student.r2.core.atp.service.AtpService;
 import org.kuali.student.r2.core.hold.dto.AppliedHoldInfo;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.core.hold.infc.HoldIssue;
 import org.kuali.student.r2.core.hold.service.HoldService;
 import org.kuali.student.r2.core.process.dto.CheckInfo;
 import org.kuali.student.r2.core.process.dto.InstructionInfo;
@@ -39,19 +45,37 @@ public class HoldCheckProposition extends AbstractCheckProposition {
         ContextInfo contextInfo = environment.resolveTerm(RulesExecutionConstants.CONTEXT_INFO_TERM, this);
         String personId = environment.resolveTerm(RulesExecutionConstants.PERSON_ID_TERM, this);
         HoldService holdService = environment.resolveTerm(RulesExecutionConstants.HOLD_SERVICE_TERM, this);
+        AtpService atpService = environment.resolveTerm(RulesExecutionConstants.ATP_SERVICE_TERM, this);
 
         // Business may decide this method should take an as of date and if so what that date should be
         // if so these lines are how we may want to evaluate the as of date as of the start of the term if the term is available
-//        String atpId = environment.resolveTerm(RulesExecutionConstants.ATP_ID_TERM, this);
-//        Date asOfDate = environment.resolveTerm(RulesExecutionConstants.AS_OF_DATE_TERM, this);
-        List<AppliedHoldInfo> appliedHolds;
+        List<AppliedHoldInfo> returnAppliedHolds = new ArrayList<>();
         try {
-            appliedHolds = holdService.getActiveAppliedHoldsByIssueAndPerson(check.getHoldIssueId(), personId, contextInfo);
+            List<AppliedHoldInfo> appliedHolds = holdService.getActiveAppliedHoldsByIssueAndPerson(check.getHoldIssueId(), personId, contextInfo);
+            for (AppliedHoldInfo appliedholdInfo : appliedHolds) {
+
+                HoldIssue holdIssue = holdService.getHoldIssue(appliedholdInfo.getHoldIssueId(), contextInfo);
+                if (holdIssue.getIsHoldIssueTermBased()) {
+                    Date startDate = atpService.getAtp(appliedholdInfo.getApplicationEffectiveTermId(), contextInfo).getStartDate();
+                    Date endDate = null;
+                    if (appliedholdInfo.getApplicationExpirationTermId() != null) {
+                        endDate = atpService.getAtp(appliedholdInfo.getApplicationExpirationTermId(), contextInfo).getEndDate();
+                    }
+
+                    //Check if registering term is within applied term range.
+                    Atp atp = environment.resolveTerm(RulesExecutionConstants.ATP_TERM, this);
+                    if(EffectiveDateUtils.isTargetDateEffective(startDate, endDate, atp.getStartDate())){
+                        returnAppliedHolds.add(appliedholdInfo);
+                    }
+                } else { //Not term based
+                    returnAppliedHolds.add(appliedholdInfo);
+                }
+            }
         } catch (Exception ex) {
             return KRMSEvaluator.constructExceptionPropositionResult(environment, ex, this);
         }
 
-        if (appliedHolds.isEmpty()) {
+        if (returnAppliedHolds.isEmpty()) {
             return this.recordSuccessResult(environment);
         }
         return this.recordFailureResultOrExemption(environment);
